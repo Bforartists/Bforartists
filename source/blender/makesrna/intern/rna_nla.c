blender/blender

commit 5f18d863a9782c728ae4e9809cadffdbfe16632f

Bugfix: Toggling "auto-blends" option on NLA Strips doesn't update them immediately

source/blender/makesrna/intern/rna_nla.c
@@ -231,6 +231,28 @@ static void rna_NlaStrip_blend_out_set(PointerRNA *ptr, float value)
 	data->blendout = value;
 }
 
+static void rna_NlaStrip_use_auto_blend_set(PointerRNA *ptr, int value)
+{
+	NlaStrip *data = (NlaStrip *)ptr->data;
+	
+	if (value) {
+		/* set the flag, then make sure a curve for this exists */
+		data->flag |= NLASTRIP_FLAG_USR_INFLUENCE;
+		
+		/* validate state to ensure that auto-blend gets applied immediately */
+		if (ptr->id.data) {
+			IdAdtTemplate *iat = (IdAdtTemplate *)ptr->id.data;
+			
+			if (iat->adt) {
+				BKE_nla_validate_state(iat->adt);
+			}
+		}
+	}
+	else {
+		data->flag &= ~NLASTRIP_FLAG_USR_INFLUENCE;
+	}
+}
+
 static int rna_NlaStrip_action_editable(PointerRNA *ptr)
 {
 	NlaStrip *strip = (NlaStrip *)ptr->data;
@@ -502,6 +524,7 @@ static void rna_def_nlastrip(BlenderRNA *brna)
 	
 	prop = RNA_def_property(srna, "use_auto_blend", PROP_BOOLEAN, PROP_NONE);
 	RNA_def_property_boolean_sdna(prop, NULL, "flag", NLASTRIP_FLAG_AUTO_BLENDS);
+	RNA_def_property_boolean_funcs(prop, NULL, "rna_NlaStrip_use_auto_blend_set");
 	RNA_def_property_ui_text(prop, "Auto Blend In/Out",
 	                         "Number of frames for Blending In/Out is automatically determined from "
 	                         "overlapping strips");

Bforartists/Bforartists

commit 2a1eb2d045444076e61866c4a9b49d7bf0343fa8

Fix T39015: NLA Clip Auto Blend In/Out Check Box Not Working
Fix for regression accidentally introduced in 5f18d863a97 (affecting current 2.70 rc-1)
which means that this setting wasn't working anymore at all.

source/blender/makesrna/intern/rna_nla.c
@@ -236,8 +236,8 @@ static void rna_NlaStrip_use_auto_blend_set(PointerRNA *ptr, int value)
 	NlaStrip *data = (NlaStrip *)ptr->data;
 	
 	if (value) {
-		/* set the flag, then make sure a curve for this exists */
-		data->flag |= NLASTRIP_FLAG_USR_INFLUENCE;
+		/* set the flag */
+		data->flag |= NLASTRIP_FLAG_AUTO_BLENDS;
 		
 		/* validate state to ensure that auto-blend gets applied immediately */
 		if (ptr->id.data) {
@@ -249,7 +249,13 @@ static void rna_NlaStrip_use_auto_blend_set(PointerRNA *ptr, int value)
 		}
 	}
 	else {
-		data->flag &= ~NLASTRIP_FLAG_USR_INFLUENCE;
+		/* clear the flag */
+		data->flag &= ~NLASTRIP_FLAG_AUTO_BLENDS;
+		
+		/* clear the values too, so that it's clear that there has been an effect */
+		/* TODO: it's somewhat debatable whether it's better to leave these in instead... */
+		data->blendin  = 0.0f;
+		data->blendout = 0.0f;
 	}
 }
 