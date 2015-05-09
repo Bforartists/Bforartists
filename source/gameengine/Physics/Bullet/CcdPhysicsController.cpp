blender/blender

commit eb811538960147e68fd6c2c530ed3ca67fa875e6

Cleanup: warning (sequence-point)

source/gameengine/Physics/Bullet/CcdPhysicsController.cpp
@@ -2436,7 +2436,7 @@ bool CcdShapeConstructionInfo::UpdateMesh(class KX_GameObject *gameobj, class RA
 		m_polygonIndexArray.resize(tot_bt_tris);
 
 
-		for (int p = 0; p < numpolys;) {
+		for (int p = 0; p < numpolys; p++) {
 			RAS_Polygon *poly = meshobj->GetPolygon(p);
 
 			if (poly->IsCollider()) {
@@ -2467,10 +2467,10 @@ bool CcdShapeConstructionInfo::UpdateMesh(class KX_GameObject *gameobj, class RA
 			}
 			m_polygonIndexArray[p] = p;
 			if (poly->VertexCount() == 4) {
+				p++;
 				// if the poly is a quad we transform it in two triangles
-				m_polygonIndexArray[p + 1] = p++;
+				m_polygonIndexArray[p] = p;
 			}
-			p++;
 		}
 	}
 

Bforartists/Bforartists

commit 178b2bd65a0c744f179604f30fd2b2235accecab

Fix T38335: incorrect triangle index in raycast with more than 2 quads
eb81153896 broke the fix for T38335, and this fix was incomplete, now we iterate by triangles and polys in the same while block.

source/gameengine/Physics/Bullet/CcdPhysicsController.cpp
@@ -2436,7 +2436,9 @@ bool CcdShapeConstructionInfo::UpdateMesh(class KX_GameObject *gameobj, class RA
 		m_polygonIndexArray.resize(tot_bt_tris);
 
 
-		for (int p = 0; p < numpolys; p++) {
+		int p = 0;
+		int t = 0;
+		while (t < tot_bt_tris) {
 			RAS_Polygon *poly = meshobj->GetPolygon(p);
 
 			if (poly->IsCollider()) {
@@ -2465,12 +2467,17 @@ bool CcdShapeConstructionInfo::UpdateMesh(class KX_GameObject *gameobj, class RA
 					*tri_pt++ = vert_remap_array[v_orig];
 				}
 			}
-			m_polygonIndexArray[p] = p;
+			// first triangle
+			m_polygonIndexArray[t] = p;
+
+			// if the poly is a quad we transform it in two triangles
 			if (poly->VertexCount() == 4) {
-				p++;
-				// if the poly is a quad we transform it in two triangles
-				m_polygonIndexArray[p] = p;
+				t++;
+				// second triangle
+				m_polygonIndexArray[t] = p;
 			}
+			t++;
+			p++;
 		}
 	}
 