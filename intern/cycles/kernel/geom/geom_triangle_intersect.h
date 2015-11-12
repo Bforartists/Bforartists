002b791bc4f1c8b1bd781be457a269a7702adf53
ReinerBforartists <reiner.prokein@t-online.de>
2015-11-12 18:46:50 +0100

    Blender 2.76 A update. Cycles: Fix for watertight intersection
    Issue: http://www.bforartists.de/content/add-fixes-blender-276-and-b-bforartists
    Blender Task: http://developer.blender.org/rBf84cbae

--- intern/cycles/kernel/geom/geom_triangle_intersect.h
+++ intern/cycles/kernel/geom/geom_triangle_intersect.h
@@ -51,11 +51,7 @@ typedef struct IsectPrecalc {
 
 #if defined(__KERNEL_CUDA__)
 #  if (defined(i386) || defined(_M_IX86))
-#    if __CUDA_ARCH__ > 500
 ccl_device_noinline
-#    else  /* __CUDA_ARCH__ > 500 */
-ccl_device_inline
-#    endif  /* __CUDA_ARCH__ > 500 */
 #  else  /* (defined(i386) || defined(_M_IX86)) */
 #    if defined(__KERNEL_EXPERIMENTAL__) && (__CUDA_ARCH__ >= 500)
 ccl_device_noinline
@@ -142,12 +138,8 @@ ccl_device_inline bool triangle_intersect(KernelGlobals *kg,
 	float U = Cx * By - Cy * Bx;
 	float V = Ax * Cy - Ay * Cx;
 	float W = Bx * Ay - By * Ax;
-	const int sign_mask = (__float_as_int(U) & 0x80000000);
-	/* TODO(sergey): Check if multiplication plus sign check is faster
-	 * or at least same speed (but robust for endian types).
-	 */
-	if(sign_mask != (__float_as_int(V) & 0x80000000) ||
-	   sign_mask != (__float_as_int(W) & 0x80000000))
+	if((U < 0.0f || V < 0.0f || W < 0.0f) &&
+	   (U > 0.0f || V > 0.0f || W > 0.0f))
 	{
 		return false;
 	}
@@ -162,9 +154,10 @@ ccl_device_inline bool triangle_intersect(KernelGlobals *kg,
 	 * the hit distance.
 	 */
 	const float T = (U * A_kz + V * B_kz + W * C_kz) * Sz;
-	const float sign_T = xor_signmask(T, sign_mask);
+	const int sign_det = (__float_as_int(det) & 0x80000000);
+	const float sign_T = xor_signmask(T, sign_det);
 	if((sign_T < 0.0f) ||
-	   (sign_T > isect->t * xor_signmask(det, sign_mask)))
+	   (sign_T > isect->t * xor_signmask(det, sign_det)))
 	{
 		return false;
 	}
@@ -204,7 +197,13 @@ ccl_device_inline bool triangle_intersect(KernelGlobals *kg,
  */
 
 #ifdef __SUBSURFACE__
-ccl_device_inline void triangle_intersect_subsurface(
+
+#if defined(__KERNEL_CUDA__) && (defined(i386) || defined(_M_IX86))
+ccl_device_noinline
+#else
+ccl_device_inline
+#endif
+void triangle_intersect_subsurface(
         KernelGlobals *kg,
         const IsectPrecalc *isect_precalc,
         Intersection *isect_array,
@@ -245,13 +244,12 @@ ccl_device_inline void triangle_intersect_subsurface(
 
 	/* Calculate scaled barycentric coordinates. */
 	float U = Cx * By - Cy * Bx;
-	int sign_mask = (__float_as_int(U) & 0x80000000);
 	float V = Ax * Cy - Ay * Cx;
-	if(sign_mask != (__float_as_int(V) & 0x80000000)) {
-		return;
-	}
 	float W = Bx * Ay - By * Ax;
-	if(sign_mask != (__float_as_int(W) & 0x80000000)) {
+
+	if((U < 0.0f || V < 0.0f || W < 0.0f) &&
+	   (U > 0.0f || V > 0.0f || W > 0.0f))
+	{
 		return;
 	}
 
@@ -264,10 +262,11 @@ ccl_device_inline void triangle_intersect_subsurface(
 	/* Calculate scaled z−coordinates of vertices and use them to calculate
 	 * the hit distance.
 	 */
+	const int sign_det = (__float_as_int(det) & 0x80000000);
 	const float T = (U * A_kz + V * B_kz + W * C_kz) * Sz;
-	const float sign_T = xor_signmask(T, sign_mask);
+	const float sign_T = xor_signmask(T, sign_det);
 	if((sign_T < 0.0f) ||
-	   (sign_T > tmax * xor_signmask(det, sign_mask)))
+	   (sign_T > tmax * xor_signmask(det, sign_det)))
 	{
 		return;
 	}
