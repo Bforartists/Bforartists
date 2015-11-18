da8f074b2dedbaecf880d7a596100275f3efd809
ReinerBforartists <reiner.prokein@t-online.de>
2015-11-18 10:29:42 +0100

    Edit Mode, Curve, removed double entry recalculate normals
    Issue: http://www.bforartists.de/content/edit-mode-curve-remove-double-entry-recalculate-normals#comment-666

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -2435,7 +2435,6 @@ class VIEW3D_MT_edit_curve_ctrlpoints(Menu):
             layout.separator()
 
             layout.operator_menu_enum("curve.handle_type_set", "type")
-            layout.operator("curve.normals_make_consistent")
 
             layout.separator()
 
