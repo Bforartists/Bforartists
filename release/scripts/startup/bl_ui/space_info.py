aed7c98f4eecd82fd5b275fefd69cf1bfdfcac6a
ReinerBforartists <reiner.prokein@t-online.de>
2015-11-21 18:31:01 +0100

    debug menu panel needed menu entry
    Issue: http://www.bforartists.de/content/debug-menu-panel-needs-menu-entry#comment-722

--- release/scripts/startup/bl_ui/space_info.py
+++ release/scripts/startup/bl_ui/space_info.py
@@ -290,6 +290,8 @@ class INFO_MT_window(Menu):
         layout.separator()
 
         layout.menu("WM_OT_redraw_timer", icon='BLENDER') #Redraw timer sub menu - Debug stuff
+        layout.operator("wm.debug_menu") # debug menu
+
 
 class INFO_MT_help(Menu):
     bl_label = "Help"
