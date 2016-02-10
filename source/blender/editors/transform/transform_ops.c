79e10698b9a078efa8dd94de08e4e49556c4d448
ReinerBforartists <reiner.prokein@t-online.de>
2016-02-10 12:51:19 +0100

    Changed tooltip for mirror tool, vertices became geometry.
    Issue: http://www.bforartists.de/content/mirror-tool-tooltip-object-mode-mirror-selected-vertices

--- source/blender/editors/transform/transform_ops.c
+++ source/blender/editors/transform/transform_ops.c
@@ -816,7 +816,7 @@ static void TRANSFORM_OT_mirror(struct wmOperatorType *ot)
 {
 	/* identifiers */
 	ot->name   = "Mirror";
-	ot->description = "Mirror selected vertices around one or more axes";
+	ot->description = "Mirror selected geometry around one or more axes";
 	ot->idname = OP_MIRROR;
 	ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO | OPTYPE_BLOCKING;
 
