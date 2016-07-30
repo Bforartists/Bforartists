cddcf902143e8f3f20b6af668916eabe8f181c2f
ReinerBforartists <reiner.prokein@t-online.de>
2016-07-30 11:12:06 +0200

    Removed double entry Join
    Issue: http://www.bforartists.de/content/3d-view-object-mode-remove-double-entry-join

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -1203,7 +1203,6 @@ class VIEW3D_MT_object(Menu):
 
         layout.separator()
 
-        layout.operator("object.join")
         layout.operator("object.data_transfer")
         layout.operator("object.datalayout_transfer")
 
