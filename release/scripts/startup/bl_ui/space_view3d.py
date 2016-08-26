a853c5bbb1a6bebc685f8f07a59ccc80297a9802
ReinerBforartists <reiner.prokein@t-online.de>
2016-08-26 09:24:23 +0200

    3D View, removed double entry OpenGL Render
    Issue: http://www.bforartists.de/content/3d-view-remove-double-entry-opengl-render

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -103,11 +103,6 @@ class VIEW3D_HT_header(Header):
             if (mode == 'EDIT' and obj.type == 'MESH'):
                 layout.prop(toolsettings, "use_mesh_automerge", text="", icon='AUTOMERGE_ON')
 
-        # OpenGL render
-        row = layout.row(align=True)
-        row.operator("render.opengl", text="", icon='RENDER_STILL')
-        row.operator("render.opengl", text="", icon='RENDER_ANIMATION').animation = True
-
 
 # bfa - show hide the editormenu
 class ALL_MT_editormenu(Menu):
