c60981655e80761d78e0b6c790e2bcf5c3364474
ReinerBforartists <reiner.prokein@t-online.de>
2016-09-03 10:05:13 +0200

    Separated tooltip for View Selected All Regions
    Issue: http://www.bforartists.de/content/separate-tooltip-view-selected-all-regions

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -340,7 +340,7 @@ class VIEW3D_MT_uv_map(Menu):
 class VIEW3D_MT_view_all_all_regions(bpy.types.Operator):
     """View All all Regions\nView all objects in scene in all four Quad View views\nJust relevant for Quad View """      # blender will use this as a tooltip for menu items and buttons.
     bl_idname = "view3d.view_all_all_regions"        # unique identifier for buttons and menu items to reference.
-    bl_label = "View All all Regions"         # display name in the interface.
+    bl_label = "View All all Regionsa"         # display name in the interface.
     bl_options = {'REGISTER', 'UNDO'}  # enable undo for the operator.
 
     def execute(self, context):        # execute() is called by blender when running the operator.
@@ -357,6 +357,18 @@ class VIEW3D_MT_view_center_cursor_and_view_all(bpy.types.Operator):
     def execute(self, context):        # execute() is called by blender when running the operator.
         bpy.ops.view3d.view_all(center = True)
         return {'FINISHED'}  
+
+    # Workaround to separate the tooltips
+class VIEW3D_MT_view_view_selected_all_regions(bpy.types.Operator):
+    """View Selected All Regions\nMove the View to the selection center in all Quad View views"""      # blender will use this as a tooltip for menu items and buttons.
+    bl_idname = "view3d.view_selected_all_regions"        # unique identifier for buttons and menu items to reference.
+    bl_label = "View Selected All Regions"         # display name in the interface.
+    bl_options = {'REGISTER', 'UNDO'}  # enable undo for the operator.
+
+    def execute(self, context):        # execute() is called by blender when running the operator.
+        bpy.ops.view3d.view_selected(use_all_regions = True)
+        return {'FINISHED'}  
+
 
 class VIEW3D_MT_view(Menu):
     bl_label = "View"
@@ -411,7 +423,8 @@ class VIEW3D_MT_view(Menu):
         layout.separator()
 
         layout.operator("view3d.localview", text="View Global/Local")
-        layout.operator("view3d.view_selected", text = "View Selected all Regions" ).use_all_regions = True
+        #layout.operator("view3d.view_selected", text = "View Selected all Regions" ).use_all_regions = True
+        layout.operator("view3d.view_selected_all_regions", text = "View Selected all Regions" )
         layout.operator("view3d.view_selected").use_all_regions = False
         #layout.operator("view3d.view_all", text = "View All all Regions" ).use_all_regions = True
         #layout.operator("view3d.view_all", text="Center Cursor and View All").center = True
