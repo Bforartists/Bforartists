014938aca28097c39925c98ec7741475d0bdb4fb
ReinerBforartists <reiner.prokein@t-online.de>
2016-09-29 11:57:36 +0200

    Fixed Bolt Factory - add_mesh_BoltFactory\__init__.py addon
    Issue: http://www.bforartists.de/content/fix-broken-addons#comment-2671

--- release/scripts/addons/add_mesh_BoltFactory/__init__.py
+++ release/scripts/addons/add_mesh_BoltFactory/__init__.py
@@ -1,4 +1,4 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
+﻿# ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
@@ -16,12 +16,14 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+# fixed for Bforartists. Shows in Tool Shelf now.
+
 bl_info = {
     "name": "BoltFactory",
     "author": "Aaron Keith",
     "version": (3, 9),
     "blender": (2, 63, 0),
-    "location": "View3D > Add > Mesh",
+    "location": "View3D > Tool Shelf > Create > Add Misc",
     "description": "Add a bolt or nut",
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/"
                 "Scripts/Add_Mesh/BoltFactory",
@@ -47,13 +49,13 @@ def add_mesh_bolt_button(self, context):
 def register():
     bpy.utils.register_module(__name__)
 
-    bpy.types.INFO_MT_mesh_add.append(add_mesh_bolt_button)
+    bpy.types.VIEW3D_PT_tools_add_misc.append(add_mesh_bolt_button)
     #bpy.types.VIEW3D_PT_tools_objectmode.prepend(add_mesh_bolt_button) #just for testing
 
 def unregister():
     bpy.utils.unregister_module(__name__)
 
-    bpy.types.INFO_MT_mesh_add.remove(add_mesh_bolt_button)
+    bpy.types.VIEW3D_PT_tools_add_misc.remove(add_mesh_bolt_button)
     #bpy.types.VIEW3D_PT_tools_objectmode.remove(add_mesh_bolt_button) #just for testing
 
 if __name__ == "__main__":
