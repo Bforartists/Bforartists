b137ee507e52d3fc7f9b4175843def7c0284ef23
ReinerBforartists <reiner.prokein@t-online.de>
2016-09-29 10:35:35 +0200

    Fixed Add extra curve object types - add_curve_extra_objects\__init__.py
    Issue: http://www.bforartists.de/content/fix-broken-addons#comment-2665

--- release/scripts/addons/add_curve_extra_objects/__init__.py
+++ release/scripts/addons/add_curve_extra_objects/__init__.py
@@ -1,4 +1,4 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
+﻿# ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
@@ -17,6 +17,8 @@
 # ##### END GPL LICENSE BLOCK #####
 # Contributed to by
 # testscreenings, Alejandro Omar Chocano Vasquez, Jimmy Hazevoet, meta-androcto #
+
+# fixed for Bforartists. Shows in Tool Shelf now.
 
 bl_info = {
     "name": "Extra Objects",
@@ -70,13 +72,15 @@ def register():
     bpy.utils.register_module(__name__)
 
     # Add "Extras" menu to the "Add Curve" menu
-    bpy.types.INFO_MT_curve_add.append(menu_func)
+    #bpy.types.INFO_MT_curve_add.append(menu_func)
+    bpy.types.VIEW3D_PT_tools_add_misc.append(menu_func)
 
 def unregister():
     bpy.utils.unregister_module(__name__)
 
     # Remove "Extras" menu from the "Add Curve" menu.
-    bpy.types.INFO_MT_curve_add.remove(menu_func)
+    #bpy.types.INFO_MT_curve_add.remove(menu_func)
+    bpy.types.VIEW3D_PT_tools_add_misc.remove(menu_func)
 
 if __name__ == "__main__":
     register()
