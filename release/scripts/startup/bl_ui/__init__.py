ffadf533316db4d7cf7df54f9d8d6fc39d7f04b5
ReinerBforartists <reiner.prokein@t-online.de>
2016-11-11 12:28:53 +0100

    Properties Editor - Render Tab - Sampling Panel - Put the options into a subtab
    Issue: http://www.bforartists.de/content/properties-editor-render-tab-sampling-panel-put-options-subtab

--- release/scripts/startup/bl_ui/__init__.py
+++ release/scripts/startup/bl_ui/__init__.py
@@ -108,6 +108,7 @@ def register():
     bpy.types.WindowManager.SP_render_metadata_stampoptions = bpy.props.BoolProperty(name="Stamp Options", description="Contains Options for Stamp output", default = False)
     bpy.types.WindowManager.SP_render_metadata_enabled = bpy.props.BoolProperty(name="Enabled Metadata", description="Contains the enabled / disabled Metadata Options", default = False)
     bpy.types.WindowManager.SP_render_dimensions_options = bpy.props.BoolProperty(name="Advanced", description="Contains advanced Options", default = False)
+    bpy.types.WindowManager.SP_render_sampling_options = bpy.props.BoolProperty(name="Options", description="Contains Options", default = False)
 
     # bfa - Our data block for icon or text buttons
     bpy.utils.register_class(UITweaksData) # Our data block
@@ -174,6 +175,7 @@ def unregister():
     del bpy.types.WindowManager.SP_render_metadata_stampoptions
     del bpy.types.WindowManager.SP_render_metadata_enabled
     del bpy.types.WindowManager.SP_render_dimensions_options
+    del bpy.types.WindowManager.SP_render_sampling_options
        
 
     # bfa - Our data block for icon or text buttons
