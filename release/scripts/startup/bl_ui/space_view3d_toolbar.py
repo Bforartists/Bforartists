9d17474addbff87e00e3ac8f81d04c5b1d0d1760
ReinerBforartists <reiner.prokein@t-online.de>
2016-11-28 09:11:18 +0100

    Put extrude individual out of the Extrude dropdown box again
    Issue: http://www.bforartists.de/content/put-extrude-individual-out-extrude-dropdown-box-again

--- release/scripts/startup/bl_ui/space_view3d_toolbar.py
+++ release/scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -737,8 +737,7 @@ class VIEW3D_PT_tools_meshedit(View3DPanel, Panel):
 
             col.menu("VIEW3D_MT_edit_mesh_extrude")
             col.operator("view3d.edit_mesh_extrude_move_normal", icon='EXTRUDE_REGION', text="Extrude Region")
-            #col.operator("mesh.dupli_extrude_cursor" , icon='DUPLI_EXTRUDE',  text = " DupliExtrude    ").rotate_source = False
-            #col.operator("mesh.dupli_extrude_cursor", icon='DUPLI_EXTRUDE_ROTATE', text = " DupliExRotate  ").rotate_source = True
+            col.operator("view3d.edit_mesh_extrude_individual_move", icon='EXTRUDE_INDIVIDUAL', text="Individual        "),
            
             row = col.row(align=True)
             row.operator("mesh.spin", icon='SPIN')
@@ -814,8 +813,8 @@ class VIEW3D_PT_tools_meshedit(View3DPanel, Panel):
             row = col.row(align=False)
             row.alignment = 'LEFT' 
             row.operator("view3d.edit_mesh_extrude_move_normal", icon='EXTRUDE_REGION', text="")
-            #row.operator("mesh.dupli_extrude_cursor" , icon='DUPLI_EXTRUDE',  text = "").rotate_source = False
-            #row.operator("mesh.dupli_extrude_cursor", icon='DUPLI_EXTRUDE_ROTATE', text = "").rotate_source = True
+            row.operator("view3d.edit_mesh_extrude_individual_move", icon='EXTRUDE_INDIVIDUAL', text=""),
+
             row.operator("mesh.spin", icon='SPIN', text="")
             row.operator("mesh.screw", icon='SCREW', text="")
             
