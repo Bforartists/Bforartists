d71dc618b8b85752d442b6470a86795c7ebade75
ReinerBforartists <reiner.prokein@t-online.de>
2016-12-18 18:24:40 +0100

    toolbar implementation part 2
    Issue: http://www.bforartists.de/content/toolbar-editor

---
+++ release/scripts/startup/bl_ui/space_toolbar.py
@@ -0,0 +1,75 @@
+﻿# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# <pep8 compliant>
+import bpy
+from bpy.types import Header, Menu
+
+
+class Toolbar_HT_header(Header):
+    bl_space_type = 'TOOLBAR'
+
+    def draw(self, context):
+        layout = self.layout
+
+        window = context.window
+        scene = context.scene
+
+        ALL_MT_editormenu.draw_hidden(context, layout) # bfa - show hide the editormenu
+        Toolbar_MT_editor_menus.draw_collapsible(context, layout)
+
+# bfa - show hide the editormenu
+class ALL_MT_editormenu(Menu):
+    bl_label = ""
+
+    def draw(self, context):
+        self.draw_menus(self.layout, context)
+
+    @staticmethod
+    def draw_menus(layout, context):
+
+        row = layout.row(align=True)
+        row.template_header() # editor type menus
+
+# --------------------------------menu items
+# Everything menu in this class is collapsible. See line 35.
+class Toolbar_MT_editor_menus(Menu):
+    bl_idname = "TOOLBAR_MT_editor_menus"
+    bl_label = ""
+
+    def draw(self, context):
+        self.draw_menus(self.layout, context)
+
+    @staticmethod
+    def draw_menus(layout, context):
+        scene = context.scene
+        rd = scene.render
+
+        layout.menu("Toolbar_MT_file") # see class TOOLBAR_MT_file below
+
+class Toolbar_MT_file(Menu):
+    bl_label = "File"
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.operator_context = 'INVOKE_AREA'
+        layout.operator("wm.read_homefile", text="New", icon='NEW')
+
+if __name__ == "__main__":  # only for live edit.
+    bpy.utils.register_module(__name__)
