8563022c80caa863499611a2530830436103c14f
ReinerBforartists <reiner.prokein@t-online.de>
2016-12-21 11:57:32 +0100

    Toolbar Editor. Added icons for Load Save section. Implemented Load Save section into toolbar.
    Issue: http://www.bforartists.de/content/toolbar-editor

--- release/scripts/startup/bl_ui/space_toolbar.py
+++ release/scripts/startup/bl_ui/space_toolbar.py
@@ -46,8 +46,73 @@ class TOOLBAR_HT_header(Header):
             row = layout.row(align=True)
 
             row.operator("wm.open_mainfile", text="", icon='FILE_FOLDER')
+            row.menu("INFO_MT_file_open_recent", text="", icon='OPEN_RECENT')
+
+            row = layout.row(align=True)
+
             row.operator("wm.save_mainfile", text="", icon='FILE_TICK')
             row.operator("wm.save_as_mainfile", text="", icon='SAVE_AS')
+            row.operator("wm.save_as_mainfile", text="", icon='SAVE_COPY')
+
+        ## ------------------ Link Append
+
+        if scene.toolbar_linkappend.linkappend_bool: 
+
+            row = layout.row(align=True)
+
+            row.operator("wm.link", text="", icon='LINK_BLEND')
+            row.operator("wm.append", text="", icon='APPEND_BLEND')
+
+        ## ------------------ Import common
+
+        if scene.toolbar_importcommon.importcommon_bool:
+
+            row = layout.row(align=True)
+
+            row.operator("import_scene.fbx", text="", icon='LOAD_FBX')
+            row.operator("import_scene.obj", text="", icon='LOAD_OBJ')
+            row.operator("wm.collada_import", text="", icon='LOAD_DAE')
+            row.operator("import_anim.bvh", text="", icon='LOAD_BVH')
+            row.operator("import_scene.autodesk_3ds", text="", icon='LOAD_3DS')
+
+        ## ------------------ Import uncommon
+
+        if scene.toolbar_importuncommon.importuncommon_bool:
+
+            row = layout.row(align=True)
+
+            row.operator("import_mesh.stl", text="", icon='LOAD_STL')
+            row.operator("import_mesh.ply", text="", icon='LOAD_PLY')
+            row.operator("import_scene.x3d", text="", icon='LOAD_WRL')
+            row.operator("import_curve.svg", text="", icon='LOAD_SVG')
+            
+        ## ------------------ Export common
+
+        if scene.toolbar_exportcommon.exportcommon_bool:
+
+            row = layout.row(align=True)
+
+            row.operator("export_scene.fbx", text="", icon='SAVE_FBX')
+            row.operator("export_scene.obj", text="", icon='SAVE_OBJ')
+            row.operator("wm.collada_export", text="", icon='SAVE_DAE')
+            row.operator("export_anim.bvh", text="", icon='SAVE_BVH')
+            row.operator("export_scene.autodesk_3ds", text="", icon='SAVE_3DS')
+
+        ## ------------------ Export uncommon
+
+        if scene.toolbar_exportuncommon.exportuncommon_bool:
+
+            row = layout.row(align=True)
+
+            row.operator("export_mesh.stl", text="", icon='SAVE_STL')
+            row.operator("export_mesh.ply", text="", icon='SAVE_PLY')
+            row.operator("export_scene.x3d", text="", icon='SAVE_WRL')
+
+            
+
+            
+
+            
 
 # bfa - show hide the editormenu
 class ALL_MT_editormenu(Menu):
@@ -86,6 +151,13 @@ class TOOLBAR_MT_file(Menu):
 
         scene = context.scene
         layout.prop(scene.toolbar_loadsave, "loadsave_bool") # Our checkbox
+
+        layout.prop(scene.toolbar_linkappend, "linkappend_bool") # Our checkbox
+
+        layout.prop(scene.toolbar_importcommon, "importcommon_bool") # Our checkbox
+        layout.prop(scene.toolbar_importuncommon, "importuncommon_bool") # Our checkbox
+        layout.prop(scene.toolbar_exportcommon, "exportcommon_bool") # Our checkbox
+        layout.prop(scene.toolbar_exportuncommon, "exportuncommon_bool") # Our checkbox
 
 
 if __name__ == "__main__":  # only for live edit.
