f31da638d12bdcb5665a438f3e5788d7abffb70a
ReinerBforartists <reiner.prokein@t-online.de>
2016-12-27 13:08:09 +0100

    Added camera toolbar in the toolbar type View, changed icon for reset view, added icons for Active Camera and Set Active Object as Camera
    Issue: http://www.bforartists.de/content/toolbar-editor#comment-3182

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -520,8 +520,8 @@ class VIEW3D_MT_view(Menu):
 
         layout.separator()
 
-        layout.operator("view3d.object_as_camera")
-        layout.operator("view3d.viewnumpad", text="Active Camera").type = 'CAMERA'
+        layout.operator("view3d.object_as_camera", icon = 'VIEW_SWITCHACTIVECAM')
+        layout.operator("view3d.viewnumpad", text="Active Camera", icon = 'VIEW_SWITCHTOCAM').type = 'CAMERA'
         layout.operator("view3d.view_center_camera")
 
         layout.separator()
