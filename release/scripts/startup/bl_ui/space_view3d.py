2783408f29e15e927cdcc7e4e4d0a9fd3a7512b5
ReinerBforartists <reiner.prokein@t-online.de>
2017-01-21 18:28:44 +0100

    port 278 - version 021 - removed double entry Extrude menu
    Issue: http://www.bforartists.de/content/port-278-021-changes

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -2227,7 +2227,6 @@ class VIEW3D_MT_edit_mesh(Menu):
         layout.separator()
 
         layout.operator("mesh.duplicate_move")
-        layout.menu("VIEW3D_MT_edit_mesh_extrude")
         layout.menu("VIEW3D_MT_edit_mesh_delete")
 
         layout.separator()
