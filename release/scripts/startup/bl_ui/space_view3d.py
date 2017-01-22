8a402cc31ee63f63ceb625adcd634e0eb57558ab
ReinerBforartists <reiner.prokein@t-online.de>
2017-01-22 10:05:06 +0100

    port 278 - version 021 - Edit Mode, removed double entry Smooth Vertex.
    Issue: http://www.bforartists.de/content/port-278-021-changes

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -2384,7 +2384,6 @@ class VIEW3D_MT_edit_mesh_vertices(Menu):
         layout.operator("mesh.bevel").vertex_only = True
         if with_bullet:
             layout.operator("mesh.convex_hull")
-        layout.operator("mesh.vertices_smooth")
 
         layout.operator("mesh.blend_from_shape")
 
