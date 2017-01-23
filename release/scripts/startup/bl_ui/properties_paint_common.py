7eeff0f9a3fe2fb239afd0e77963f237137707f7
ReinerBforartists <reiner.prokein@t-online.de>
2017-01-23 10:21:55 +0100

    port 278 - version 021 - 3D View, Image Paint, stencil brush control needed menu entry.
    Issue: http://www.bforartists.de/content/port-278-021-changes

--- release/scripts/startup/bl_ui/properties_paint_common.py
+++ release/scripts/startup/bl_ui/properties_paint_common.py
@@ -291,6 +291,28 @@ def brush_mask_texture_settings(layout, brush):
             layout.operator("brush.stencil_fit_image_aspect").mask = True
         layout.operator("brush.stencil_reset_transform").mask = True
 
+        # stencil brush controls hotkeys.
+        col = layout.column()
+        col.label(text="Stencil Brush Control:")
+        row = col.row(align=True)
+        row.operator("brush.stencil_control", text = "Trans").mode = 'TRANSLATION'
+        row.operator("brush.stencil_control", text = "Rotate").mode = 'ROTATION'
+        row.operator("brush.stencil_control", text = "Scale").mode = 'SCALE'
+
+        col = layout.column()
+        col.label(text="Stencil Brush Control Secondary:")
+        row = col.row(align=True)
+        myvar = row.operator("brush.stencil_control", text = "Trans")
+        myvar.mode = 'TRANSLATION'
+        myvar.texmode = 'SECONDARY'
+        myvar = row.operator("brush.stencil_control", text = "Rotate")
+        myvar.mode = 'ROTATION'
+        myvar.texmode = 'SECONDARY'
+        myvar = row.operator("brush.stencil_control", text = "Scale")
+        myvar.mode = 'SCALE'
+        myvar.texmode = 'SECONDARY'
+
+
     col = layout.column()
     col.prop(brush, "use_pressure_masking", text="")
     # angle and texture_angle_source
