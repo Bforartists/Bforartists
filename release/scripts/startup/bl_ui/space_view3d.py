aca595b488950cd15c746b5b0612b60dbeff322e
ReinerBforartists <reiner.prokein@t-online.de>
2017-01-25 18:43:45 +0100

    Port to 2.78 - 0.2.2 changes - 3D View, View menu, removed double entry View Selected.
    Issue: http://www.bforartists.de/content/port-278-022-changes

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -544,7 +544,6 @@ class VIEW3D_MT_view_align(Menu):
         layout.operator("view3d.view_all", text="Center Cursor and View All").center = True
         layout.operator("view3d.camera_to_view", text="Align Active Camera to View")
         layout.operator("view3d.camera_to_view_selected", text="Align Active Camera to Selected")
-        layout.operator("view3d.view_selected")
         layout.operator("view3d.view_center_cursor")
 
         layout.separator()
