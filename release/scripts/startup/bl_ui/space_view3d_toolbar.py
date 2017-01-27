0584ad7ed66b9ce02f87deecb6a4cba4f6d54d4c
ReinerBforartists <reiner.prokein@t-online.de>
2017-01-27 12:38:22 +0100

    Port to 2.78 - 0.3.0 changes - Lattice. Replaced the Tools panel in Edit mode by the one from Bforartists 080.
    Issue: http://www.bforartists.de/wiki/bforartistsreleasenotev030

--- release/scripts/startup/bl_ui/space_view3d_toolbar.py
+++ release/scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -1577,19 +1577,29 @@ class VIEW3D_PT_tools_latticeedit(View3DPanel, Panel):
 
     def draw(self, context):
         layout = self.layout
+        scene = context.scene # Our data for the icon_or_text flag is in the current scene
 
-        col = layout.column(align=True)
-        col.label(text="Transform:")
-        col.operator("transform.translate")
-        col.operator("transform.rotate")
-        col.operator("transform.resize", text="Scale")
+        if not scene.UItweaks.icon_or_text: 
 
-        col = layout.column(align=True)
-        col.operator("lattice.make_regular")
+            col = layout.column(align=True)
+            col.operator("lattice.make_regular", icon = 'MAKE_REGULAR', text = "Make Regular  ")
 
-        col = layout.column(align=True)
-        col.label(text="Deform:")
-        col.operator("transform.vertex_random")
+            col = layout.column(align=True)
+            col.label(text="Deform:")
+            col.operator("transform.vertex_random", icon = 'RANDOMIZE', text = "Randomize      ")
+        else:
+            col = layout.column(align=True)
+            col.label(text="Transform:")
+
+            row = col.row(align=False)
+            row.alignment = 'LEFT'
+            row.operator("lattice.make_regular", icon = 'MAKE_REGULAR', text = "")
+
+            col = layout.column(align=True)
+            col.label(text="Deform:")
+            row = col.row(align=False)
+            row.alignment = 'LEFT'
+            row.operator("transform.vertex_random", icon = 'RANDOMIZE', text = "")
 
 
 # ********** default tools for pose-mode ****************
