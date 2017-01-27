33c2bc6598ae31495a3e156ba890c54d4e625f2a
ReinerBforartists <reiner.prokein@t-online.de>
2017-01-27 18:09:02 +0100

    Port to 2.78 - 0.3.1 changes - - 3D View - removed double entry for Set Origin in Object mode.
    Issue: http://www.bforartists.de/wiki/bforartistsreleasenotev031

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -286,12 +286,6 @@ class VIEW3D_MT_transform_object(VIEW3D_MT_transform_base):
 
         layout.operator_context = 'EXEC_AREA'
 
-        layout.operator("object.origin_set", text="Geometry to Origin").type = 'GEOMETRY_ORIGIN'
-        layout.operator("object.origin_set", text="Origin to Geometry").type = 'ORIGIN_GEOMETRY'
-        layout.operator("object.origin_set", text="Origin to 3D Cursor").type = 'ORIGIN_CURSOR'
-        layout.operator("object.origin_set", text="Origin to Center of Mass").type = 'ORIGIN_CENTER_OF_MASS'
-        layout.separator()
-
         layout.operator("object.randomize_transform")
         layout.operator("object.align")
 
