8ed6e9cd5a6a03fbc16dab008c9ec9b4dce587c0
ReinerBforartists <reiner.prokein@t-online.de>
2017-01-29 18:22:33 +0100

    Port to 2.78 - 0.4.1 changes - Text Editor - Separated tooltip for Toggle Maximize Area
    Issue: http://www.bforartists.de/content/port-278-041-changes

--- release/scripts/startup/bl_ui/space_text.py
+++ release/scripts/startup/bl_ui/space_text.py
@@ -209,7 +209,7 @@ class TEXT_MT_view(Menu):
         layout.separator()
 
         layout.operator("screen.area_dupli")
-        layout.operator("screen.screen_full_area")
+        layout.operator("screen.toggle_maximized_area", text="Toggle Maximize Area") # bfa - the new separated tooltip. Class is in space_text.py
         layout.operator("screen.screen_full_area", text="Toggle Fullscreen Area").use_hide_panels = True
 
 
