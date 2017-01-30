a7acdec97c52af6aa7ce242b34b269ba8a516b3c
ReinerBforartists <reiner.prokein@t-online.de>
2017-01-30 09:46:27 +0100

    Port to 2.78 - 0.4.1 changes - UV Editor - Separated tooltip for Toggle Maximize Area
    Issue: http://www.bforartists.de/content/port-278-041-changes

--- release/scripts/startup/bl_ui/space_image.py
+++ release/scripts/startup/bl_ui/space_image.py
@@ -129,7 +129,7 @@ class IMAGE_MT_view(Menu):
             layout.separator()
 
         layout.operator("screen.area_dupli")
-        layout.operator("screen.screen_full_area")
+        layout.operator("screen.toggle_maximized_area", text="Toggle Maximize Area") # bfa - the separated tooltip. Class is in space_text.py
         layout.operator("screen.screen_full_area", text="Toggle Fullscreen Area").use_hide_panels = True
 
 
