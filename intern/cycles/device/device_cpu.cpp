fjuhec/blender

Cycles: Add names to buffer allocations
This is to help debug and track memory usage for generic buffers. We
have similar for textures already since those require a name, but for
buffers the name is only for debugging proposes.

intern/cycles/device/device_cpu.cpp
@@ -237,8 +237,14 @@ public:
 		return (TaskScheduler::num_threads() == 1);
 	}
 
-	void mem_alloc(device_memory& mem, MemoryType /*type*/)
+	void mem_alloc(const char *name, device_memory& mem, MemoryType /*type*/)
 	{
+		if(name) {
+			VLOG(1) << "Buffer allocate: " << name << ", "
+				    << string_human_readable_number(mem.memory_size()) << " bytes. ("
+				    << string_human_readable_size(mem.memory_size()) << ")";
+		}
+
 		mem.device_pointer = mem.data_pointer;
 
 		if(!mem.device_pointer) {
@@ -824,7 +830,7 @@ protected:
 	virtual void alloc_kernel_globals(device_memory& mem)
 	{
 		mem.resize(sizeof(KernelGlobals));
-		mem_alloc(mem, MEM_READ_WRITE);
+		mem_alloc("kernel_globals", mem, MEM_READ_WRITE);
 
 		KernelGlobals *kg = (KernelGlobals*)mem.device_pointer;
 		*kg = thread_kernel_globals_init();

intern/cycles/device/opencl/opencl_split.cpp
@@ -100,7 +100,7 @@ public:
 		} KernelGlobals;
 
 		mem.resize(sizeof(KernelGlobals));
-		mem_alloc(mem, MEM_READ_WRITE);
+		mem_alloc("kernel_globals", mem, MEM_READ_WRITE);
 	}
 
 	virtual void free_kernel_globals(device_memory& mem)

Bforartists/Bforartists

intern/cycles/device/device_cpu.cpp
@@ -231,8 +231,14 @@ public:
 		return (TaskScheduler::num_threads() == 1);
 	}
 
-	void mem_alloc(device_memory& mem, MemoryType /*type*/)
+	void mem_alloc(const char *name, device_memory& mem, MemoryType /*type*/)
 	{
+		if(name) {
+			VLOG(1) << "Buffer allocate: " << name << ", "
+				    << string_human_readable_number(mem.memory_size()) << " bytes. ("
+				    << string_human_readable_size(mem.memory_size()) << ")";
+		}
+
 		mem.device_pointer = mem.data_pointer;
 
 		if(!mem.device_pointer) {
@@ -437,7 +443,7 @@ public:
 		/* allocate buffer for kernel globals */
 		device_memory kgbuffer;
 		kgbuffer.resize(sizeof(KernelGlobals));
-		mem_alloc(kgbuffer, MEM_READ_WRITE);
+		mem_alloc("kernel_globals", kgbuffer, MEM_READ_WRITE);
 
 		KernelGlobals *kg = (KernelGlobals*)kgbuffer.device_pointer;
 		*kg = thread_kernel_globals_init();

intern/cycles/device/opencl/opencl_split.cpp
@@ -115,7 +115,7 @@ public:
 			/* Allocate buffer for kernel globals */
 			device_memory kgbuffer;
 			kgbuffer.resize(sizeof(KernelGlobals));
-			mem_alloc(kgbuffer, MEM_READ_WRITE);
+			mem_alloc("kernel_globals", kgbuffer, MEM_READ_WRITE);
 
 			/* Keep rendering tiles until done. */
 			while(task->acquire_tile(this, tile)) {