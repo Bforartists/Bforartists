fjuhec/blender

Cycles: Remove ccl_fetch and SOA

intern/cycles/kernel/split/kernel_shadow_blocked.h
@@ -94,7 +94,7 @@ ccl_device void kernel_shadow_blocked(KernelGlobals *kg)
 
 		float3 shadow;
 		update_path_radiance = !(shadow_blocked(kg,
-		                                        kernel_split_state.sd_DL_shadow,
+		                                        &kernel_split_state.sd_DL_shadow[ray_index],
 		                                        state,
 		                                        light_ray_global,
 		                                        &shadow));

Bforartists/Bforartists

intern/cycles/kernel/split/kernel_shadow_blocked.h
@@ -94,7 +94,7 @@ ccl_device void kernel_shadow_blocked(KernelGlobals *kg)
 
 		float3 shadow;
 		update_path_radiance = !(shadow_blocked(kg,
-		                                        kernel_split_state.sd_DL_shadow,
+		                                        &kernel_split_state.sd_DL_shadow[thread_index],
 		                                        state,
 		                                        light_ray_global,
 		                                        &shadow));