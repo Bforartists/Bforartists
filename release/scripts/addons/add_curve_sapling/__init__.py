6b8a35f591445a7771a24d1bc92f24692f597265
ReinerBforartists <reiner.prokein@t-online.de>
2017-03-09 18:44:21 +0100

    Fixed sapling addon to work with Bforartists
    Issue: Issue: http://www.bforartists.de/content/recheck-addons-if-they-work-add-menu

--- release/scripts/addons/add_curve_sapling/__init__.py
+++ release/scripts/addons/add_curve_sapling/__init__.py
@@ -1,4 +1,4 @@
-#====================== BEGIN GPL LICENSE BLOCK ======================
+﻿#====================== BEGIN GPL LICENSE BLOCK ======================
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
@@ -21,7 +21,7 @@ bl_info = {
     "author": "Andrew Hale (TrumanBlending), Aaron Buchler",
     "version": (0, 3, 2),
     "blender": (2, 77, 0),
-    "location": "View3D > Add > Curve",
+    "location": "View3D > Tool Shelf > Create > Add Misc",
     "description": ("Adds a parametric tree. The method is presented by "
     "Jason Weber & Joseph Penn in their paper 'Creation and Rendering of "
     "Realistic Trees'."),
@@ -925,12 +925,12 @@ def menu_func(self, context):
 def register():
     bpy.utils.register_module(__name__)
 
-    bpy.types.INFO_MT_curve_add.append(menu_func)
+    bpy.types.VIEW3D_PT_tools_add_misc.append(menu_func)
 
 def unregister():
     bpy.utils.unregister_module(__name__)
 
-    bpy.types.INFO_MT_curve_add.remove(menu_func)
+    bpy.types.VIEW3D_PT_tools_add_misc.remove(menu_func)
 
 if __name__ == "__main__":
     register()
