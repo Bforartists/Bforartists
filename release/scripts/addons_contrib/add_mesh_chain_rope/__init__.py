676f2092d3a1c85d7fbb2b3673bc9aeea4236985
ReinerBforartists <reiner.prokein@t-online.de>
2017-03-12 11:53:56 +0100

    Fixed Oscurart Chain and Rope Maker to work with Bforartists
    Issue: http://www.bforartists.de/content/recheck-addons-if-they-work-add-menu

--- release/scripts/addons_contrib/add_mesh_chain_rope/__init__.py
+++ release/scripts/addons_contrib/add_mesh_chain_rope/__init__.py
@@ -1,9 +1,10 @@
+﻿###
 bl_info = {
-    "name": "Oscurart Chain and Rope Maker",
+    "name": "Oscurart Chain and Rope Maker - Bforartists version",
     "author": "Oscurart",
     "version": (1, 1),
     "blender": (2, 63, 0),
-    "location": "Add > Mesh",
+    "location": "View3D > Tool Shelf > Create > Add Misc",
     "description": "Create chains and ropes along armatures/curves",
     "warning": "",
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/"
@@ -18,15 +19,15 @@ from .oscurart_chain_maker import *
 
 def register():
     bpy.utils.register_class(OBJECT_OT_add_object)
-    bpy.types.INFO_MT_curve_add.append(oscRopeButton)
+    bpy.types.VIEW3D_PT_tools_add_misc.append(oscRopeButton)
     bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_mesh_add.append(menu_oscChain)
+    bpy.types.VIEW3D_PT_tools_add_misc.append(menu_oscChain)
 
 def unregister():
     bpy.utils.unregister_class(OBJECT_OT_add_object)
-    bpy.types.INFO_MT_curve_add.remove(oscRopeButton)
+    bpy.types.VIEW3D_PT_tools_add_misc.remove(oscRopeButton)
     bpy.utils.unregister_module(__name__)
-    bpy.types.INFO_MT_mesh_add.remove(menu_oscChain)
+    bpy.types.VIEW3D_PT_tools_add_misc.remove(menu_oscChain)
 
 if __name__ == "__main__":
     register()
