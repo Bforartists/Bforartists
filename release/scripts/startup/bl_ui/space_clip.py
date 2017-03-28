00422cd2a8f02660ecfdd7fcb80b538e6c79f451
ReinerBforartists <reiner.prokein@t-online.de>
2017-03-28 11:22:03 +0200

    Clip Editor - subeditor Dopesheet - Double separator in View menu
    Issue: http://www.bforartists.de/content/clip-editor-subeditor-dopesheet-double-separator-view-menu

--- release/scripts/startup/bl_ui/space_clip.py
+++ release/scripts/startup/bl_ui/space_clip.py
@@ -1240,9 +1240,9 @@ class CLIP_MT_view(Menu):
 
             layout.prop(sc, "show_seconds")
             layout.prop(sc, "show_locked_time")
-            layout.separator()
 
         layout.separator()
+
         layout.operator("screen.area_dupli")
         layout.operator("screen.screen_full_area")
         layout.operator("screen.screen_full_area", text="Toggle Fullscreen Area").use_hide_panels = True
