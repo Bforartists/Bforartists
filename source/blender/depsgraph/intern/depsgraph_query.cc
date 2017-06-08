blender/blender

commit 23c93873f4073e440d3817465252d8f86134654c

Remove selection color from the base
Use indirect access to it via object.

It was already flushing from base to object, now we can avoid such flushing.

Still weird to have selection color filled in by dependency graph, but now
there is no synchronization going on at least.

source/blender/depsgraph/intern/depsgraph_query.cc
@@ -135,7 +135,6 @@ static void deg_flush_base_flags_and_settings(Object *ob, Base *base, const int 
 {
 	ob->base_flag = (base->flag | BASE_FLUSH_FLAGS) & flag;
 	ob->base_collection_properties = base->collection_properties;
-	ob->base_selection_color = base->selcol;
 }
 
 static bool deg_objects_dupli_iterator_next(BLI_Iterator *iter)

source/blender/depsgraph/intern/builder/deg_builder_nodes_scene.cc
@@ -81,12 +81,12 @@ void DepsgraphNodeBuilder::build_scene(Main *bmain, Scene *scene)
 	}
 
 	/* scene objects */
-	int selection_color = 1;
+	int select_color = 1;
 	for (SceneLayer *sl = (SceneLayer *)scene->render_layers.first; sl; sl = sl->next) {
 		for (Base *base = (Base *)sl->object_bases.first; base; base = base->next) {
 			/* object itself */
 			build_object(scene, base->object);
-			base->selcol = selection_color++;
+			base->object->select_color = select_color++;
 		}
 	}
 

source/blender/draw/intern/draw_armature.c
@@ -1297,7 +1297,7 @@ static void draw_armature_pose(Object *ob, const float const_color[4])
 		}
 
 		if (arm->flag & ARM_POSEMODE) {
-			index = ob->base_selection_color;
+			index = ob->select_color;
 		}
 	}
 

source/blender/draw/intern/draw_manager.c
@@ -3052,7 +3052,7 @@ void DRW_draw_select_loop(
 			DEG_OBJECT_ITER(graph, ob, DEG_OBJECT_ITER_FLAG_DUPLI)
 			{
 				if ((ob->base_flag & BASE_SELECTABLED) != 0) {
-					DRW_select_load_id(ob->base_selection_color);
+					DRW_select_load_id(ob->select_color);
 					DRW_engines_cache_populate(ob);
 				}
 			}

source/blender/editors/armature/armature_select.c
@@ -98,7 +98,7 @@ void *get_bone_from_selectbuffer(
 				/* Determine what the current bone is */
 				if (obedit == NULL || base->object != obedit) {
 					/* no singular posemode, so check for correct object */
-					if (base->selcol == (hitresult & 0xFFFF)) {
+					if (base->object->select_color == (hitresult & 0xFFFF)) {
 						bone = get_indexed_bone(base->object, hitresult);
 						
 						if (findunsel)

source/blender/editors/space_view3d/drawarmature.c
@@ -2001,7 +2001,7 @@ static void draw_pose_bones(Scene *scene, SceneLayer *sl, View3D *v3d, ARegion *
 	/* if solid we draw that first, with selection codes, but without names, axes etc */
 	if (dt > OB_WIRE) {
 		if (arm->flag & ARM_POSEMODE) 
-			index = base->selcol;
+			index = base->object->select_color;
 		
 		for (pchan = ob->pose->chanbase.first; pchan; pchan = pchan->next) {
 			bone = pchan->bone;
@@ -2104,7 +2104,7 @@ static void draw_pose_bones(Scene *scene, SceneLayer *sl, View3D *v3d, ARegion *
 	    (draw_wire || (dt <= OB_WIRE)) )
 	{
 		if (arm->flag & ARM_POSEMODE)
-			index = base->selcol;
+			index = base->object->select_color;
 			
 		/* only draw custom bone shapes that need to be drawn as wires */
 		for (pchan = ob->pose->chanbase.first; pchan; pchan = pchan->next) {
@@ -2175,7 +2175,7 @@ static void draw_pose_bones(Scene *scene, SceneLayer *sl, View3D *v3d, ARegion *
 		/* draw line check first. we do selection indices */
 		if (ELEM(arm->drawtype, ARM_LINE, ARM_WIRE)) {
 			if (arm->flag & ARM_POSEMODE) 
-				index = base->selcol;
+				index = base->object->select_color;
 		}
 		/* if solid && posemode, we draw again with polygonoffset */
 		else if ((dt > OB_WIRE) && (arm->flag & ARM_POSEMODE)) {
@@ -2184,7 +2184,7 @@ static void draw_pose_bones(Scene *scene, SceneLayer *sl, View3D *v3d, ARegion *
 		else {
 			/* and we use selection indices if not done yet */
 			if (arm->flag & ARM_POSEMODE) 
-				index = base->selcol;
+				index = base->object->select_color;
 		}
 
 		if (is_cull_enabled == false) {

source/blender/editors/space_view3d/drawobject.c
@@ -1727,7 +1727,7 @@ static void draw_viewport_object_reconstruction(
 			continue;
 
 		if (dflag & DRAW_PICKING)
-			GPU_select_load_id(base->selcol + (tracknr << 16));
+			GPU_select_load_id(base->object->select_color + (tracknr << 16));
 
 		gpuPushMatrix();
 		gpuTranslate3fv(track->bundle_pos);
@@ -1889,7 +1889,7 @@ static void draw_viewport_reconstruction(
 	GPU_basic_shader_bind(GPU_SHADER_USE_COLOR);
 
 	if (dflag & DRAW_PICKING)
-		GPU_select_load_id(base->selcol);
+		GPU_select_load_id(base->object->select_color);
 }
 
 /* camera frame */

source/blender/editors/space_view3d/view3d_draw_legacy.c
@@ -914,7 +914,7 @@ static void view3d_draw_xray_select(Scene *scene, SceneLayer *sl, ARegion *ar, V
 
 	v3d->xray = true;
 	while ((v3da = BLI_pophead(&v3d->afterdraw_xray))) {
-		if (GPU_select_load_id(v3da->base->selcol)) {
+		if (GPU_select_load_id(v3da->base->object->select_color)) {
 			draw_object_select(scene, sl, ar, v3d, v3da->base, v3da->dflag);
 		}
 		MEM_freeN(v3da);
@@ -1301,10 +1301,10 @@ void ED_view3d_draw_select_loop(
 				if (((base->flag & BASE_SELECTABLED) == 0) ||
 				    (use_obedit_skip && (scene->obedit->data == base->object->data)))
 				{
-					base->selcol = 0;
+					base->object->select_color = 0;
 				}
 				else {
-					base->selcol = code;
+					base->object->select_color = code;
 
 					if (use_nearest && (base->object->dtx & OB_DRAWXRAY)) {
 						ED_view3d_after_add(&v3d->afterdraw_xray, base, dflag);

Bforartists/Bforartists

commit 4539190ff958f31ac3b997011d5c4c706d490c7a

Fix T51750: Group selection broken
This was introduced in 23c93873f40.

This could be moved to deg_flush_base_flags_and_settings but since we
only need this for duplis I think it's fine to be handled separately.

source/blender/depsgraph/intern/depsgraph_query.cc
@@ -161,6 +161,7 @@ static bool deg_objects_dupli_iterator_next(BLI_Iterator *iter)
 		/* Temporary object to evaluate. */
 		data->temp_dupli_object = *dob->ob;
 		copy_m4_m4(data->temp_dupli_object.obmat, dob->mat);
+		data->temp_dupli_object.select_color = data->base->object->select_color;
 
 		deg_flush_base_flags_and_settings(&data->temp_dupli_object,
 		                                  data->base,