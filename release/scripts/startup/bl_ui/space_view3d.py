896a47c11c1daf1b69752b70eef52d7b5f03e4eb
ReinerBforartists <reiner.prokein@t-online.de>
2017-07-09 11:13:54 +0200

    Remove double menu entry Unified Settings in all Paint modes
    Issue: https://www.bforartists.de/content/remove-double-menu-entry-unified-settings-all-paint-modes

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -1909,13 +1909,6 @@ class VIEW3D_MT_brush(Menu):
         settings = UnifiedPaintPanel.paint_settings(context)
         brush = settings.brush
 
-        ups = context.tool_settings.unified_paint_settings
-        layout.prop(ups, "use_unified_size", text="Unified Size")
-        layout.prop(ups, "use_unified_strength", text="Unified Strength")
-        if context.image_paint_object or context.vertex_paint_object:
-            layout.prop(ups, "use_unified_color", text="Unified Color")
-        layout.separator()
-
         # brush paint modes
         layout.menu("VIEW3D_MT_brush_paint_modes")
 
