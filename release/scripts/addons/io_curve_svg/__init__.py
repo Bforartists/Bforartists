ae71aabac63dae16b058445821eb7cb6c2d6b368
ReinerBforartists <reiner.prokein@t-online.de>
2017-12-18 09:53:59 +0100

    Toolbar - Missing icons in import export menus
    Issue: https://www.bforartists.de/content/toolbar-missing-icons-import-export-menus

--- release/scripts/addons/io_curve_svg/__init__.py
+++ release/scripts/addons/io_curve_svg/__init__.py
@@ -1,4 +1,4 @@
-# ##### BEGIN GPL LICENSE BLOCK #####
+﻿# ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
@@ -62,8 +62,7 @@ class ImportSVG(bpy.types.Operator, ImportHelper):
 
 
 def menu_func_import(self, context):
-    self.layout.operator(ImportSVG.bl_idname,
-        text="Scalable Vector Graphics (.svg)")
+    self.layout.operator(ImportSVG.bl_idname, text="Scalable Vector Graphics (.svg)", icon='LOAD_SVG')
 
 
 def register():
