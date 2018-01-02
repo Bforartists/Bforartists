blender/blender

commit c9cb5dc3dab0af76f4b6090b6f6f1f4741ce98eb

Collections: Allow collections from different "parents" to have the same name
This allows users to have "Support", "Rig", "Characters" collections nested to
different collections without having to resort to "House.Rig", "House.Characters"
or "Rig.001", "Characters.003" :/

This is part of T53495.

source/blender/blenkernel/intern/collection.c
@@ -50,6 +50,7 @@
 #include "MEM_guardedalloc.h"
 
 /* Prototypes. */
+static SceneCollection *find_collection_parent(const struct SceneCollection *sc_child, struct SceneCollection *sc_parent);
 static bool is_collection_in_tree(const struct SceneCollection *sc_reference, struct SceneCollection *sc_parent);
 
 static SceneCollection *collection_master_from_id(const ID *owner_id)
@@ -89,8 +90,8 @@ SceneCollection *BKE_collection_add(ID *owner_id, SceneCollection *sc_parent, co
 		}
 	}
 
-	BKE_collection_rename((Scene *)owner_id, sc, name);
 	BLI_addtail(&sc_parent->scene_collections, sc);
+	BKE_collection_rename((Scene *)owner_id, sc, name);
 
 	BKE_layer_sync_new_scene_collection(owner_id, sc_parent, sc);
 
@@ -291,33 +292,11 @@ struct UniqueNameCheckData {
 	SceneCollection *lookup_sc;
 };
 
-static bool collection_unique_name_check(void *arg, const char *name)
-{
-	struct UniqueNameCheckData *data = arg;
-
-	for (SceneCollection *sc = data->lb->first; sc; sc = sc->next) {
-		struct UniqueNameCheckData child_data = {.lb = &sc->scene_collections, .lookup_sc = data->lookup_sc};
-
-		if (sc != data->lookup_sc) {
-			if (STREQ(sc->name, name)) {
-				return true;
-			}
-		}
-		if (collection_unique_name_check(&child_data, name)) {
-			return true;
-		}
-	}
-
-	return false;
-}
-
 static void collection_rename(const ID *owner_id, SceneCollection *sc, const char *name)
 {
-	SceneCollection *sc_master = collection_master_from_id(owner_id);
-	struct UniqueNameCheckData data = {.lb = &sc_master->scene_collections, .lookup_sc = sc};
-
+	SceneCollection *sc_parent = find_collection_parent(sc, collection_master_from_id(owner_id));
 	BLI_strncpy(sc->name, name, sizeof(sc->name));
-	BLI_uniquename_cb(collection_unique_name_check, &data, DATA_("Collection"), '.', sc->name, sizeof(sc->name));
+	BLI_uniquename(&sc_parent->scene_collections, sc, DATA_("Collection"), '.', offsetof(SceneCollection, name), sizeof(sc->name));
 }
 
 void BKE_collection_rename(const Scene *scene, SceneCollection *sc, const char *name)

Bforartists/Bforartists

commit 50d59a68b84a8f994ce9d98a14669e7e419eb10a

Layers: Fix collection_rename unittest
This was broken since c9cb5dc3dab0af when we supported same name for
non-sibling collections.

tests/python/view_layer/test_collection_rename.py
@@ -34,7 +34,8 @@ class UnitTesting(ViewLayerTesting):
         family = self.setup_family()
 
         family['mom'].name = family['daughter'].name
-        self.assertNotEqual(family['mom'].name, family['daughter'].name)
+        # Since they are not siblings, we allow them to have the same name.
+        self.assertEqual(family['mom'].name, family['daughter'].name)
 
     def test_rename_b(self):
         family = self.setup_family()
@@ -46,13 +47,20 @@ class UnitTesting(ViewLayerTesting):
         family = self.setup_family()
 
         family['cousin'].name = family['daughter'].name
-        self.assertNotEqual(family['cousin'].name, family['daughter'].name)
+        # Since they are not siblings, we allow them to have the same name.
+        self.assertEqual(family['cousin'].name, family['daughter'].name)
 
     def test_rename_d(self):
         family = self.setup_family()
 
         family['son'].name = family['daughter'].name
         self.assertNotEqual(family['son'].name, family['daughter'].name)
+
+    def test_rename_e(self):
+        family = self.setup_family()
+
+        family['grandma'].name = family['grandpa'].name
+        self.assertNotEqual(family['grandma'].name, family['grandpa'].name)
 
     def test_add_equal_name_a(self):
         family = self.setup_family()
@@ -64,7 +72,14 @@ class UnitTesting(ViewLayerTesting):
         family = self.setup_family()
 
         other_aunt = family['grandma'].collections.new(family['daughter'].name)
-        self.assertNotEqual(other_aunt.name, family['daughter'].name)
+        # Since they are not siblings, we allow them to have the same name.
+        self.assertEqual(other_aunt.name, family['daughter'].name)
+
+    def test_add_equal_name_c(self):
+        family = self.setup_family()
+
+        other_aunt = family['grandma'].collections.new(family['mom'].name)
+        self.assertNotEqual(other_aunt.name, family['mom'].name)
 
 
 # ############################################################