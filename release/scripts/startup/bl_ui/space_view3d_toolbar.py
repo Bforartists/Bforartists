c27177347957f0eaec9be873129383418fb1db83
ReinerBforartists <reiner.prokein@posteo.de>
2018-03-05 11:29:41 +0100

    Sculpt Workflow: Tab Options - Double entry "Show Brush" [Task]
    Issue: https://www.bforartists.de/content/sculpt-workflow-tab-options-double-entry-show-brush-task

--- release/scripts/startup/bl_ui/space_view3d_toolbar.py
+++ release/scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -2470,7 +2470,6 @@ class VIEW3D_PT_sculpt_options(Panel, View3DPaintPanel):
 
         layout.prop(sculpt, "use_threaded", text="Threaded Sculpt")
         layout.prop(sculpt, "show_low_resolution")
-        layout.prop(sculpt, "show_brush")
         layout.prop(sculpt, "use_deform_only")
         layout.prop(sculpt, "show_diffuse_color")
         layout.prop(sculpt, "show_mask")
