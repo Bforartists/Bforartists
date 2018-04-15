2731091c4bbe382936da6efdcc8872e9050dc1a4
ReinerBforartists <reiner.prokein@posteo.de>
2018-04-15 12:52:46 +0200

    Updated the addons

--- release/scripts/addons/modules/cycles_shader_compat.py
+++ release/scripts/addons/modules/cycles_shader_compat.py
@@ -19,6 +19,7 @@
 # <pep8 compliant>
 
 import bpy
+import math
 
 __all__ = (
     "CyclesShaderWrapper",
@@ -392,7 +393,7 @@ class CyclesShaderWrapper():
 
     def hardness_value_set(self, value):
         node = self.node_mix_color_hard
-        node.inputs["Color1"].default_value = (value,) * 4
+        node.inputs["Color1"].default_value = (math.sqrt(max(value, 0.0)),) * 4
 
     def hardness_image_set(self, image):
         node = self.node_mix_color_hard
