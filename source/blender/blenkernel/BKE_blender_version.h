da9f0fefd726d44b6d12356ca1d31ff8ce3c25b3
ReinerBforartists <reiner.prokein@posteo.de>
2018-05-04 09:00:28 +0200

    Changed Splash screen to Bforartists 1.0.0 RC1

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -39,7 +39,7 @@
 /* alpha/beta/rc/release, docs use this */
 #define BLENDER_VERSION_CYCLE   alpha
 
-#define BFORARTISTS_VERSION     097
+#define BFORARTISTS_VERSION     100
 
 extern char versionstr[]; /* from blender.c */
 
