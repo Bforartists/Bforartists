81e49701b930814b7ceb90fa0b9f92beab7b6eb8
ktotheoz <naether.markus@gmail.com>
2018-05-16 08:03:14 +0200

    Fixed issues with install_cuda.

--- build_files/build_environment/windows/install_cuda.cmd
+++ build_files/build_environment/windows/install_cuda.cmd
@@ -1,8 +1,8 @@
 @echo off
 echo Downloading CUDA toolkit 8
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.61_windows-exe -FileName cuda_8.0.44_windows.exe
+appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_windows-exe -FileName cuda_8.0.61_windows.exe
 echo Installing CUDA toolkit 8
-cuda_8.0.44_windows.exe -s compiler_8.0 ^
+cuda_8.0.61_windows.exe -s compiler_8.0 ^
                            cublas_8.0 ^
                            cublas_dev_8.0 ^
                            cudart_8.0 ^
