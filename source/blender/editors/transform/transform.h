Bforartists/Bforartists

commit cd7aea897d7bb138a7c9c3ca21b47afcdc748650

Manipulators/Tools: always draw a cursor while interacting
Cursor can be a system one or drawn with OpenGL.
Before, dragging the transform manipulator would hide the cursor and reset it to the initial positon.

source/blender/editors/transform/transform.h
@@ -483,7 +483,7 @@ typedef struct TransInfo {
 	short		persp;
 	short		around;
 	char		spacetype;		/* spacetype where transforming is      */
-	char		helpline;		/* helpline modes (not to be confused with hotline) */
+	char		helpline;		/* choice of custom cursor with or without a help line from the gizmo to the mouse pos */
 	short		obedit_type;	/* Avoid looking inside TransDataContainer obedit. */
 
 	float		vec[3];			/* translation, to show for widget   	*/

source/blender/editors/transform/transform_input.c
@@ -30,10 +30,13 @@
 
 #include "DNA_screen_types.h"
 
+#include "BKE_context.h"
+
 #include "BLI_math.h"
 #include "BLI_utildefines.h"
 
 #include "WM_types.h"
+#include "WM_api.h"
 
 #include "transform.h"
 
@@ -339,6 +342,25 @@ void initMouseInputMode(TransInfo *t, MouseInput *mi, MouseInputMode mode)
 		default:
 			mi->apply = NULL;
 			break;
+	}
+
+	/* setup for the mouse cursor: either set a custom one,
+	 * or hide it if it will be drawn with the helpline */
+	wmWindow *win = CTX_wm_window(t->context);
+	switch(t->helpline) {
+        case HLP_NONE:
+                /* INPUT_VECTOR, INPUT_CUSTOM_RATIO, INPUT_CUSTOM_RATIO_FLIP */
+                WM_cursor_set(win, BC_HANDCURSOR);
+                break;
+        case HLP_SPRING:
+        case HLP_ANGLE:
+        case HLP_TRACKBALL:
+        case HLP_HARROW:
+        case HLP_VARROW:
+                WM_cursor_set(win, CURSOR_NONE);
+                break;
+        default:
+                break;
 	}
 
 	/* if we've allocated new data, free the old data

source/blender/windowmanager/intern/wm_event_system.c
@@ -1903,7 +1903,10 @@ static int wm_handler_operator_call(bContext *C, ListBase *handlers, wmEventHand
 
 				/* remove modal handler, operator itself should have been canceled and freed */
 				if (retval & (OPERATOR_CANCELLED | OPERATOR_FINISHED)) {
-					WM_cursor_grab_disable(CTX_wm_window(C), NULL);
+					/* set cursor back to the default for the region */
+					wmWindow *win = CTX_wm_window(C);
+					WM_cursor_grab_disable(win, NULL);
+					ED_region_cursor_set(win, CTX_wm_area(C), CTX_wm_region(C));
 
 					BLI_remlink(handlers, handler);
 					wm_event_free_handler(handler);

source/blender/editors/transform/transform_manipulator.c
@@ -1191,17 +1191,14 @@ static ManipulatorGroup *manipulatorgroup_init(wmManipulatorGroup *mgroup)
 #define MANIPULATOR_NEW_ARROW(v, draw_style) { \
 	man->manipulators[v] = WM_manipulator_new_ptr(wt_arrow, mgroup, NULL); \
 	RNA_enum_set(man->manipulators[v]->ptr, "draw_style", draw_style); \
-	WM_manipulator_set_flag(man->manipulators[v], WM_MANIPULATOR_GRAB_CURSOR, true); \
 } ((void)0)
 #define MANIPULATOR_NEW_DIAL(v, draw_options) { \
 	man->manipulators[v] = WM_manipulator_new_ptr(wt_dial, mgroup, NULL); \
 	RNA_enum_set(man->manipulators[v]->ptr, "draw_options", draw_options); \
-	WM_manipulator_set_flag(man->manipulators[v], WM_MANIPULATOR_GRAB_CURSOR, true); \
 } ((void)0)
 #define MANIPULATOR_NEW_PRIM(v, draw_style) { \
 	man->manipulators[v] = WM_manipulator_new_ptr(wt_prim, mgroup, NULL); \
 	RNA_enum_set(man->manipulators[v]->ptr, "draw_style", draw_style); \
-	WM_manipulator_set_flag(man->manipulators[v], WM_MANIPULATOR_GRAB_CURSOR, true); \
 } ((void)0)
 
 	/* add/init widgets - order matters! */

commit 7907aba8ec717848e0917513db9652b165ba708e

Fix T55178: Cursor fails to restore on cancel
cd7aea897d7bb138a7c9c3ca21b47afcdc748650 caused complications
restoring the cursor.

Use WM_cursor_modal_set/restore which is intended for operator use,
calling ED_region_cursor_set after an operator runs isn't reliable.

source/blender/editors/transform/transform.h
@@ -601,6 +601,8 @@ typedef struct TransInfo {
 	/** #TransInfo.center has been set, don't change it. */
 #define T_OVERRIDE_CENTER	(1 << 25)
 
+#define T_MODAL_CURSOR_SET	(1 << 26)
+
 /* TransInfo->modifiers */
 #define	MOD_CONSTRAINT_SELECT	0x01
 #define	MOD_PRECISION			0x02

source/blender/editors/transform/transform_generics.c
@@ -1629,6 +1629,10 @@ void postTrans(bContext *C, TransInfo *t)
 	if (t->draw_handle_cursor)
 		WM_paint_cursor_end(CTX_wm_manager(C), t->draw_handle_cursor);
 
+	if (t->flag & T_MODAL_CURSOR_SET) {
+		WM_cursor_modal_restore(CTX_wm_window(C));
+	}
+
 	/* Free all custom-data */
 	freeTransCustomDataContainer(t, NULL, &t->custom);
 	FOREACH_TRANS_DATA_CONTAINER (t, tc) {

source/blender/editors/transform/transform_input.c
@@ -351,7 +351,8 @@ void initMouseInputMode(TransInfo *t, MouseInput *mi, MouseInputMode mode)
 		case HLP_NONE:
 			/* INPUT_VECTOR, INPUT_CUSTOM_RATIO, INPUT_CUSTOM_RATIO_FLIP */
 			if (t->flag & T_MODAL) {
-				WM_cursor_set(win, BC_NSEW_SCROLLCURSOR);
+				t->flag |= T_MODAL_CURSOR_SET;
+				WM_cursor_modal_set(win, BC_NSEW_SCROLLCURSOR);
 			}
 			break;
 		case HLP_SPRING:
@@ -360,7 +361,8 @@ void initMouseInputMode(TransInfo *t, MouseInput *mi, MouseInputMode mode)
 		case HLP_HARROW:
 		case HLP_VARROW:
 			if (t->flag & T_MODAL) {
-				WM_cursor_set(win, CURSOR_NONE);
+				t->flag |= T_MODAL_CURSOR_SET;
+				WM_cursor_modal_set(win, CURSOR_NONE);
 			}
 			break;
 		default:

source/blender/windowmanager/intern/wm_event_system.c
@@ -1958,14 +1958,7 @@ static int wm_handler_operator_call(bContext *C, ListBase *handlers, wmEventHand
 
 				/* remove modal handler, operator itself should have been canceled and freed */
 				if (retval & (OPERATOR_CANCELLED | OPERATOR_FINISHED)) {
-					/* set cursor back to the default for the region */
-					wmWindow *win = CTX_wm_window(C);
-					WM_cursor_grab_disable(win, NULL);
-					/* Causes crash when joining areas: T55166, seems this is not even needed. */
-#if 0
-					ED_region_cursor_set(win, area, region);
-#endif
-
+					WM_cursor_grab_disable(CTX_wm_window(C), NULL);
 
 					BLI_remlink(handlers, handler);
 					wm_event_free_handler(handler);