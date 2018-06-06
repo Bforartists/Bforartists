Bforartists/Bforartists

commit ceafcd06df0c23558442c9ee21582ff089568240

Cleanup: use new accessors to blendfile path (Main.name).

source/blender/blenkernel/intern/blender_undo.c
@@ -66,10 +66,11 @@
 
 bool BKE_memfile_undo_decode(MemFileUndoData *mfu, bContext *C)
 {
-	char mainstr[sizeof(G.main->name)];
+	Main *bmain = CTX_data_main(C);
+	char mainstr[sizeof(bmain->name)];
 	int success = 0, fileflags;
 
-	BLI_strncpy(mainstr, G.main->name, sizeof(mainstr));    /* temporal store */
+	BLI_strncpy(mainstr, BKE_main_blendfile_path(bmain), sizeof(mainstr));    /* temporal store */
 
 	fileflags = G.fileflags;
 	G.fileflags |= G_FILE_NO_UI;
@@ -82,7 +83,7 @@ bool BKE_memfile_undo_decode(MemFileUndoData *mfu, bContext *C)
 	}
 
 	/* restore */
-	BLI_strncpy(G.main->name, mainstr, sizeof(G.main->name)); /* restore */
+	BLI_strncpy(bmain->name, mainstr, sizeof(bmain->name)); /* restore */
 	G.fileflags = fileflags;
 
 	if (success) {

commit 0836d060dad889ec959d6e635ad6e26da6e66809

Fix error using freed bmain
Regression in ceafcd06df0c2

source/blender/blenkernel/intern/blender_undo.c
@@ -82,13 +82,14 @@ bool BKE_memfile_undo_decode(MemFileUndoData *mfu, bContext *C)
 		success = BKE_blendfile_read_from_memfile(C, &mfu->memfile, NULL, 0);
 	}
 
-	/* restore */
-	BLI_strncpy(bmain->name, mainstr, sizeof(bmain->name)); /* restore */
+	/* Restore, bmain has been re-allocated. */
+	bmain = CTX_data_main(C);
+	BLI_strncpy(bmain->name, mainstr, sizeof(bmain->name));
 	G.fileflags = fileflags;
 
 	if (success) {
 		/* important not to update time here, else non keyed tranforms are lost */
-		DAG_on_visible_update(G.main, false);
+		DAG_on_visible_update(bmain, false);
 	}
 
 	return success;