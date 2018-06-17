62c48bcd9b366f15656521535cdc6470d809edc2
ReinerBforartists <reiner.prokein@posteo.de>
2018-06-17 10:10:03 +0200

    Updated the addons in release/scripts/addons to the newest Blender versions

---
+++ release/scripts/addons/presets/pov/lamp/11_(5000K)_18W_Standard_Fluorescent_T8.py
@@ -0,0 +1,13 @@
+#since 1973 
+
+import bpy
+bpy.context.object.data.type = 'AREA'
+lampdata = bpy.context.object.data
+
+lampdata.size = 0.026
+lampdata.size_y = 0.59
+lampdata.shadow_ray_samples_x = 1
+lampdata.shadow_ray_samples_y = 2
+lampdata.color = (0.95686274766922, 1.0, 0.9803921580314636)
+lampdata.energy = 1.25898#1350lm/21.446(=lux)*0.004*2.5(distance) *2 for distance is the point of half strength
+lampdata.distance = 1.0 #dist values multiplied by 10 for area lights for same power as bulb/spot/...
