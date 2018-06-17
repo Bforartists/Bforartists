541503a34c02f230d684ad8a9555e4e691020b6b
ReinerBforartists <reiner.prokein@posteo.de>
2018-06-17 10:10:03 +0200

    Updated the addons in release/scripts/addons to the newest Blender versions

---
+++ release/scripts/addons/presets/pov/lamp/12_(4200K)_18W_Cool_White_Fluorescent_T8.py
@@ -0,0 +1,15 @@
+#Available since 1979
+#more common than the warm white
+
+import bpy
+bpy.context.object.data.type = 'AREA'
+lampdata = bpy.context.object.data
+
+lampdata.size = 0.026
+lampdata.size_y = 0.59
+lampdata.shadow_ray_samples_x = 1
+lampdata.shadow_ray_samples_y = 2
+lampdata.color = (0.8313725590705872, 0.9215686321258545, 1.0)
+lampdata.energy = 1.25898#1350lm/21.446(=lux)*0.004*2.5(distance) *2 for distance is the point of half strength
+lampdata.distance = 1.0 #dist values multiplied by 10 for area lights for same power as bulb/spot/...
+#lampdata.falloff_type = 'INVERSE_SQUARE'
---
+++ release/scripts/addons/presets/pov/lamp/17_(1700K)_135W_Low_Pressure_Sodium.py
@@ -0,0 +1,11 @@
+#(1700K) 135W Low Pressure Sodium Vapor Starting from 1932
+#Mostly used for Outdoor city lighting, security lighting, long tunnel lighting 
+
+import bpy
+bpy.context.object.data.type = 'POINT'
+lampdata = bpy.context.object.data
+
+lampdata.color = (1.0, 0.5764706134796143, 0.16078431904315948)
+lampdata.energy = 8.43048#22600lm/21.446(=lux)*0.004(distance) *2 for distance is the point of half strength
+lampdata.distance = 1.0
+lampdata.falloff_type = 'INVERSE_SQUARE'
