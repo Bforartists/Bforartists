blender/blender

commit b4ccec67429b4bd9f738c11627240c07f60d6bd0

Fix ID buttons not working in popover menus.

source/blender/editors/interface/interface_templates.c
@@ -394,31 +394,14 @@ void UI_context_active_but_prop_get_templateID(
 	PointerRNA *r_ptr, PropertyRNA **r_prop)
 {
 	TemplateID *template_ui;
-	ARegion *ar = CTX_wm_region(C);
-	uiBlock *block;
-	uiBut *but;
+	uiBut *but = UI_context_active_but_get(C);
 
-	memset(r_ptr, 0, sizeof(*r_ptr));
-	*r_prop = NULL;
-
-	if (!ar)
-		return;
-
-	for (block = ar->uiblocks.first; block; block = block->next) {
-		for (but = block->buttons.first; but; but = but->next) {
-			/* find the button before the active one */
-			if ((but->flag & (UI_BUT_LAST_ACTIVE | UI_ACTIVE))) {
-				if (but->func_argN) {
-					template_ui = but->func_argN;
-					*r_ptr = template_ui->ptr;
-					*r_prop = template_ui->prop;
-					return;
-				}
-			}
-		}
+	if (but && but->func_argN) {
+		template_ui = but->func_argN;
+		*r_ptr = template_ui->ptr;
+		*r_prop = template_ui->prop;
 	}
 }
-
 
 static void template_id_cb(bContext *C, void *arg_litem, void *arg_event)
 {

Bforartists/Bforartists

commit 092bde9b389a3cb8f18ddac94c1ed5545b776f43

Fix T55880: Crash dragging file into image editor
Caused by b4ccec67429b4

source/blender/editors/interface/interface_templates.c
@@ -390,11 +390,14 @@ static uiBlock *id_search_menu(bContext *C, ARegion *ar, void *arg_litem)
 
 /* for new/open operators */
 void UI_context_active_but_prop_get_templateID(
-	bContext *C,
-	PointerRNA *r_ptr, PropertyRNA **r_prop)
+        bContext *C,
+        PointerRNA *r_ptr, PropertyRNA **r_prop)
 {
 	TemplateID *template_ui;
 	uiBut *but = UI_context_active_but_get(C);
+
+	memset(r_ptr, 0, sizeof(*r_ptr));
+	*r_prop = NULL;
 
 	if (but && but->func_argN) {
 		template_ui = but->func_argN;