Bforartists/Bforartists

commit 9be266f120e00c22592b6fafc9c96755acfbc241

UI: don't flip contents of popovers, avoid closing when size changes.

source/blender/editors/interface/interface_region_popup.c
@@ -591,6 +591,17 @@ uiBlock *ui_popup_block_refresh(
 	else {
 		/* clip block with window boundary */
 		ui_popup_block_clip(window, block);
+
+		/* Avoid menu moving down and losing cursor focus by keeping it at
+		 * the same height. */
+		if (block_old && handle->prev_block_rect.ymax > block->rect.ymax) {
+			float offset = handle->prev_block_rect.ymax - block->rect.ymax;
+			ui_block_translate(block, 0, offset);
+			block->rect.ymin = handle->prev_block_rect.ymin;
+		}
+
+		handle->prev_block_rect = block->rect;
+
 		/* the block and buttons were positioned in window space as in 2.4x, now
 		 * these menu blocks are regions so we bring it back to region space.
 		 * additionally we add some padding for the menu shadow or rounded menus */

source/blender/editors/interface/interface_intern.h
@@ -572,6 +572,9 @@ struct uiPopupBlockHandle {
 	/* menu direction */
 	int direction;
 
+	/* previous rect for refresh */
+	rctf prev_block_rect;
+
 /* #ifdef USE_DRAG_POPUP */
 	bool is_grab;
 	int     grab_xy_prev[2];

source/blender/editors/interface/interface_region_popover.c
@@ -95,12 +95,8 @@ static void ui_popover_create_block(bContext *C, uiPopover *pup, int opcontext)
 			uiLayoutContextCopy(pup->layout, pup->but->context);
 		}
 	}
-	else {
-		/* Some enums reversing is strange, currently we have no good way to
-		 * reverse some enum's but not others, so reverse all so the first menu
-		 * items are always close to the mouse cursor. */
-		pup->block->flag |= UI_BLOCK_NO_FLIP;
-	}
+
+	pup->block->flag |= UI_BLOCK_NO_FLIP;
 }
 
 static uiBlock *ui_block_func_POPOVER(bContext *C, uiPopupBlockHandle *handle, void *arg_pup)

commit 103f66cbdc8d6c532512562a7c3e8976a0ac6db5

Fix T60608: Popup dialogs Y-axis reset after drag
Caused by 9be266f120e00

source/blender/editors/interface/interface_region_popup.c
@@ -75,6 +75,10 @@ void ui_popup_translate(ARegion *ar, const int mdiff[2])
 
 	/* update blocks */
 	for (block = ar->uiblocks.first; block; block = block->next) {
+		uiPopupBlockHandle *handle = block->handle;
+		/* Make empty, will be initialized on next use, see T60608. */
+		BLI_rctf_init(&handle->prev_block_rect, 0, 0, 0, 0);
+
 		uiSafetyRct *saferct;
 		for (saferct = block->saferct.first; saferct; saferct = saferct->next) {
 			BLI_rctf_translate(&saferct->parent, UNPACK2(mdiff));