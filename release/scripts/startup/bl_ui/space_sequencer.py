84c24e7e84a1f49684117d04c6bd11db248d7bb1
ReinerBforartists <reiner.prokein@posteo.de>
2019-01-23 09:24:11 +0100

    View menus, rename Frame selected, View Center etc. to View Selected #451

--- release/scripts/startup/bl_ui/space_sequencer.py
+++ release/scripts/startup/bl_ui/space_sequencer.py
@@ -193,9 +193,9 @@ class SEQUENCER_MT_view(Menu):
 
         if is_sequencer_view:
             layout.operator_context = 'INVOKE_REGION_WIN'
-            layout.operator("sequencer.view_all", text="View all Sequences")
-            layout.operator("sequencer.view_selected")
-            layout.operator("sequencer.view_frame")
+            layout.operator("sequencer.view_all", text="View all Sequences", icon = "VIEWALL" )
+            layout.operator("sequencer.view_selected", text = "View Selected", icon='VIEW_SELECTED')
+            layout.operator("sequencer.view_frame", icon = "VIEW_FRAME" )
             layout.operator_context = 'INVOKE_DEFAULT'
         if is_preview:
             layout.operator_context = 'INVOKE_REGION_PREVIEW'
