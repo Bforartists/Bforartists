f440da16656af2cb4e22c05d3841f1061cb7796b
ReinerBforartists <reiner.prokein@posteo.de>
2019-02-15 11:43:40 +0100

    Motion Tracking - Missing Anim icon #642

--- release/scripts/startup/bl_ui/space_clip.py
+++ release/scripts/startup/bl_ui/space_clip.py
@@ -211,7 +211,7 @@ class CLIP_HT_header(Header):
                     sub.prop(sc, "show_graph_frames", icon='SEQUENCE', text="")
 
                     row.prop(sc, "show_graph_tracks_motion", icon='GRAPH', text="")
-                    row.prop(sc, "show_graph_tracks_error", icon='ANIM', text="")
+                    row.prop(sc, "show_graph_tracks_error", icon='ANIM_DATA', text="")
                 else:
                     row.prop(sc, "show_filters", icon='DISCLOSURE_TRI_RIGHT',
                              text="Filters")
