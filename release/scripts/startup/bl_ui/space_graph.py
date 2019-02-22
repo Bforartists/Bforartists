4ffb7361b4174e8d107f8639745b36a3d999a017
ReinerBforartists <reiner.prokein@posteo.de>
2019-02-22 09:33:00 +0100

    Graph editor - Key menu - Snap submenu needs icons #655

--- release/scripts/startup/bl_ui/space_graph.py
+++ release/scripts/startup/bl_ui/space_graph.py
@@ -346,7 +346,7 @@ class GRAPH_MT_key(Menu):
 
         layout.menu("GRAPH_MT_key_transform", text="Transform")
 
-        layout.operator_menu_enum("graph.snap", "type", text="Snap")
+        layout.menu("GRAPH_MT_key_snap")
         layout.operator_menu_enum("graph.mirror", "type", text="Mirror")
 
         layout.separator()
@@ -381,6 +381,19 @@ class GRAPH_MT_key(Menu):
 
         layout.separator()
         layout.operator("graph.euler_filter", text="Discontinuity (Euler) Filter", icon = "DISCONTINUE_EULER")
+
+class GRAPH_MT_key_snap(Menu):
+    bl_label = "Snap"
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.operator("graph.snap", text="Current Frame", icon = "SNAP_CURRENTFRAME").type= 'CFRA'
+        layout.operator("graph.snap", text="Cursor Value", icon = "SNAP_CURSORVALUE").type= 'VALUE'
+        layout.operator("graph.snap", text="Nearest Frame", icon = "SNAP_NEARESTFRAME").type= 'NEAREST_FRAME'
+        layout.operator("graph.snap", text="Nearest Second", icon = "SNAP_NEARESTSECOND").type= 'NEAREST_SECOND'
+        layout.operator("graph.snap", text="Nearest Marker", icon = "SNAP_NEARESTMARKER").type= 'NEAREST_MARKER'
+        layout.operator("graph.snap", text="Flatten Handles", icon = "FLATTEN_HANDLER").type= 'HORIZONTAL'
 
 
 class GRAPH_MT_key_transform(Menu):
@@ -435,6 +448,7 @@ class GRAPH_MT_specials(Menu):
 
         layout.operator_menu_enum("graph.mirror", "type", text="Mirror")
         layout.operator_menu_enum("graph.snap", "type", text="Snap")
+
 
 
 class GRAPH_MT_pivot_pie(Menu):
@@ -518,6 +532,7 @@ classes = (
     GRAPH_MT_channel_extrapolation,
     GRAPH_MT_channel_move,
     GRAPH_MT_key,
+    GRAPH_MT_key_snap,
     GRAPH_MT_key_transform,
     GRAPH_MT_delete,
     GRAPH_MT_specials,
