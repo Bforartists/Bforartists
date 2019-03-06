b99ffcbbbfd32805ba7c0c9616dc37290d44f96f
ReinerBforartists <reiner.prokein@posteo.de>
2019-03-06 18:38:11 +0100

    Iconize and refine parent menus #657

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -2457,7 +2457,7 @@ class VIEW3D_MT_object_parent(Menu):
         layout = self.layout
 
         layout.operator_enum("object.parent_set", "type")
-        layout.operator("object.parent_no_inverse_set", text = "Make Parent no Inverse" )
+        layout.operator("object.parent_no_inverse_set", text = "Make Parent no Inverse", icon = "PARENT" )
 
         layout.separator()
 
