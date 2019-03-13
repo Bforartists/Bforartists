Bforartists/Bforartists

commit f2f7872498886b9eb5a7f085b211d619ac6f599d

GHOST: Fix `processEvents` not notifying events handled by the system if the window is hidden.
Reviewed By: @LazyDodo
Differential Revision: https://developer.blender.org/D3154

intern/ghost/intern/GHOST_SystemWin32.cpp
@@ -396,12 +396,12 @@ GHOST_TSuccess GHOST_SystemWin32::disposeContext(GHOST_IContext *context)
 bool GHOST_SystemWin32::processEvents(bool waitForEvent)
 {
 	MSG msg;
-	bool anyProcessed = false;
+	bool hasEventHandled = false;
 
 	do {
 		GHOST_TimerManager *timerMgr = getTimerManager();
 
-		if (waitForEvent && !::PeekMessage(&msg, 0, 0, 0, PM_NOREMOVE)) {
+		if (waitForEvent && !::PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE)) {
 #if 1
 			::Sleep(1);
 #else
@@ -420,20 +420,26 @@ bool GHOST_SystemWin32::processEvents(bool waitForEvent)
 		}
 
 		if (timerMgr->fireTimers(getMilliSeconds())) {
-			anyProcessed = true;
+			hasEventHandled = true;
 		}
 
 		// Process all the events waiting for us
-		while (::PeekMessageW(&msg, 0, 0, 0, PM_REMOVE) != 0) {
+		while (::PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE) != 0) {
 			// TranslateMessage doesn't alter the message, and doesn't change our raw keyboard data.
 			// Needed for MapVirtualKey or if we ever need to get chars from wm_ime_char or similar.
 			::TranslateMessage(&msg);
 			::DispatchMessageW(&msg);
-			anyProcessed = true;
 		}
-	} while (waitForEvent && !anyProcessed);
 
-	return anyProcessed;
+		if (hasEventHandled == false) {
+			// Check if we have events handled by the system
+			// (for example the `GHOST_kEventWindowClose`).
+			hasEventHandled = m_eventManager->getNumEvents() != 0;
+		}
+
+	} while (waitForEvent && !hasEventHandled);
+
+	return hasEventHandled;
 }
 
 

commit 2febe6b4a7ee23f360d77d9ea5c1c802492a41e7

Fix T57305: NDOF frozen w/ lock view to camera
This effectively reverts 664cc0d9e20c59b and f2f7872498886b,
which caused this issue.

intern/ghost/intern/GHOST_System.cpp
@@ -211,12 +211,10 @@ bool GHOST_System::getFullScreen(void)
 void GHOST_System::dispatchEvents()
 {
 #ifdef WITH_INPUT_NDOF
-  #ifndef WIN32
 	// NDOF Motion event is sent only once per dispatch, so do it now:
 	if (m_ndofManager) {
 		m_ndofManager->sendMotionEvent();
 	}
-  #endif
 #endif
 
 	if (m_eventManager) {

intern/ghost/intern/GHOST_SystemWin32.cpp
@@ -435,14 +435,8 @@ bool GHOST_SystemWin32::processEvents(bool waitForEvent)
 			// Needed for MapVirtualKey or if we ever need to get chars from wm_ime_char or similar.
 			::TranslateMessage(&msg);
 			::DispatchMessageW(&msg);
+			hasEventHandled = true;
 		}
-
-		if (hasEventHandled == false) {
-			// Check if we have events handled by the system
-			// (for example the `GHOST_kEventWindowClose`).
-			hasEventHandled = m_eventManager->getNumEvents() != 0;
-		}
-
 	} while (waitForEvent && !hasEventHandled);
 
 	return hasEventHandled;
@@ -1105,7 +1099,6 @@ LRESULT WINAPI GHOST_SystemWin32::s_wndProc(HWND hwnd, UINT msg, WPARAM wParam, 
 #ifdef WITH_INPUT_NDOF
 						case RIM_TYPEHID:
 							if (system->processNDOF(raw)) {
-								system->m_ndofManager->sendMotionEvent();
 								eventHandled = true;
 							}
 							break;