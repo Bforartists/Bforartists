4b45e6dd1f2f989c7ef2fbeae2bb1c0a64ca7a08
ReinerBforartists <reiner.prokein@posteo.de>
2019-03-23 09:43:38 +0100

    Weight paint - put radial control buttons into the brush menu #716

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -2696,6 +2696,36 @@ class VIEW3D_MT_brush(Menu):
         elif context.weight_paint_object:
             layout.prop_menu_enum(brush, "weight_tool")
 
+            #radial control button brush size
+            myvar = layout.operator("wm.radial_control", text = "Brush Radius", icon = "BRUSHSIZE")
+            myvar.data_path_primary = 'tool_settings.weight_paint.brush.size'
+            myvar.data_path_secondary = 'tool_settings.unified_paint_settings.size'
+            myvar.use_secondary = 'tool_settings.unified_paint_settings.use_unified_size'
+            myvar.rotation_path = 'tool_settings.weight_paint.brush.texture_slot.angle'
+            myvar.color_path = 'tool_settings.weight_paint.brush.cursor_color_add'
+            myvar.fill_color_path = ''
+            myvar.fill_color_override_path = ''
+            myvar.fill_color_override_test_path = ''
+            myvar.zoom_path = ''
+            myvar.image_id = 'tool_settings.weight_paint.brush'
+            myvar.secondary_tex = False
+
+            #radial control button brush strength
+            myvar = layout.operator("wm.radial_control", text = "Brush Strength", icon = "BRUSHSTRENGTH")
+            myvar.data_path_primary = 'tool_settings.weight_paint.brush.strength'
+            myvar.data_path_secondary = 'tool_settings.unified_paint_settings.strength'
+            myvar.use_secondary = 'tool_settings.unified_paint_settings.use_unified_strength'
+            myvar.rotation_path = 'tool_settings.weight_paint.brush.texture_slot.angle'
+            myvar.color_path = 'tool_settings.weight_paint.brush.cursor_color_add'
+            myvar.fill_color_path = ''
+            myvar.fill_color_override_path = ''
+            myvar.fill_color_override_test_path = ''
+            myvar.zoom_path = ''
+            myvar.image_id = 'tool_settings.weight_paint.brush'
+            myvar.secondary_tex = False
+
+
+
         # TODO: still missing a lot of brush options here
 
         # sculpt options
