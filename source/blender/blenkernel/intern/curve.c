Bforartists/Bforartists

commit 3265308a8579791d98950147e5a45b7b5db0d8be

Curves: change default fill mode to full, increase bevel resolution.
Ref D4438.

source/blender/blenkernel/intern/curve.c
@@ -141,7 +141,7 @@ void BKE_curve_init(Curve *cu)
 	/* BLI_assert(MEMCMP_STRUCT_AFTER_IS_ZERO(cu, id)); */  /* cu->type is already initialized... */
 
 	copy_v3_fl(cu->size, 1.0f);
-	cu->flag = CU_FRONT | CU_BACK | CU_DEFORM_BOUNDS_OFF | CU_PATH_RADIUS;
+	cu->flag = CU_DEFORM_BOUNDS_OFF | CU_PATH_RADIUS;
 	cu->pathlen = 100;
 	cu->resolu = cu->resolv = (cu->type == OB_SURF) ? 4 : 12;
 	cu->width = 1.0;
@@ -157,6 +157,7 @@ void BKE_curve_init(Curve *cu)
 	cu->bevfac2 = 1.0f;
 	cu->bevfac1_mapping = CU_BEVFAC_MAP_RESOLU;
 	cu->bevfac2_mapping = CU_BEVFAC_MAP_RESOLU;
+	cu->bevresol = 4;
 
 	cu->bb = BKE_boundbox_alloc_unit();
 

commit 8685fe2644702893226fbd2942c36ae6f870bef2

Fix T62866: 3D text not filled by default
Regression in 3265308a8579791d98950

See D4581

source/blender/blenkernel/intern/curve.c
@@ -162,6 +162,7 @@ void BKE_curve_init(Curve *cu)
 	cu->bb = BKE_boundbox_alloc_unit();
 
 	if (cu->type == OB_FONT) {
+		cu->flag |= CU_FRONT | CU_BACK;
 		cu->vfont = cu->vfontb = cu->vfonti = cu->vfontbi = BKE_vfont_builtin_get();
 		cu->vfont->id.us += 4;
 		cu->str = MEM_malloc_arrayN(12, sizeof(unsigned char), "str");