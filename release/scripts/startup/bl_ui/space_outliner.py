e22fe98b263094a8ce0f6ebb33c420f44ec9b9ed
ReinerBforartists <reiner.prokein@posteo.de>
2019-04-03 18:31:33 +0200

    All select menus - Separated tooltip for (de)select all #393

--- release/scripts/startup/bl_ui/space_outliner.py
+++ release/scripts/startup/bl_ui/space_outliner.py
@@ -153,7 +153,31 @@ class OUTLINER_MT_view_hide_one_level(bpy.types.Operator):
 
     def execute(self, context):        # execute() is called by blender when running the operator.
         bpy.ops.outliner.show_one_level(open = False)
-        return {'FINISHED'}  
+        return {'FINISHED'}
+
+
+# Workaround to separate the tooltips
+class OUTLINER_MT_view_select_inverse(bpy.types.Operator):
+    """Inverse\nInverts the current selection """      # blender will use this as a tooltip for menu items and buttons.
+    bl_idname = "outliner.select_all_inverse"        # unique identifier for buttons and menu items to reference.
+    bl_label = "Select Inverse"         # display name in the interface.
+    bl_options = {'REGISTER', 'UNDO'}  # enable undo for the operator.
+
+    def execute(self, context):        # execute() is called by blender when running the operator.
+        bpy.ops.outliner.select_all(action = 'INVERT')
+        return {'FINISHED'}
+
+# Workaround to separate the tooltips
+class OUTLINER_MT_view_select_none(bpy.types.Operator):
+    """None\nDeselects everything """      # blender will use this as a tooltip for menu items and buttons.
+    bl_idname = "outliner.select_all_none"        # unique identifier for buttons and menu items to reference.
+    bl_label = "Select None"         # display name in the interface.
+    bl_options = {'REGISTER', 'UNDO'}  # enable undo for the operator.
+
+    def execute(self, context):        # execute() is called by blender when running the operator.
+        bpy.ops.outliner.select_all(action = 'DESELECT')
+        return {'FINISHED'}
+
 
 class OUTLINER_MT_view(Menu):
     bl_label = "View"
@@ -177,8 +201,8 @@ class OUTLINER_MT_view(Menu):
         layout.separator()
 
         layout.operator("outliner.select_all", text = "Select All", icon='SELECT_ALL').action = 'SELECT'
-        layout.operator("outliner.select_all", text = "Deselect All", icon = 'SELECT_NONE').action = 'DESELECT'
-        layout.operator("outliner.select_all", text = "Invert Selection", icon ='INVERSE').action = 'INVERT'
+        layout.operator("outliner.select_all_none", text="None", icon='SELECT_NONE') # bfa - separated tooltip
+        layout.operator("outliner.select_all_inverse", text="Inverse", icon='INVERSE') # bfa - separated tooltip
 
         layout.separator()
 
@@ -408,6 +432,8 @@ classes = (
     ALL_MT_editormenu,
     OUTLINER_MT_editor_menus,
     OUTLINER_MT_view_hide_one_level,
+    OUTLINER_MT_view_select_inverse,
+    OUTLINER_MT_view_select_none,
     OUTLINER_MT_view,
     OUTLINER_MT_edit_datablocks,
     OUTLINER_MT_collection,
