Bforartists/Bforartists

commit 873cdeb81d43a08dbdf37a4f649381d00ca4933b

Fix T63399: Obj exporter does not respect "Apply Modifiers" settings

source/blender/blenkernel/intern/mesh_convert.c
@@ -869,7 +869,7 @@ Mesh *BKE_mesh_new_from_object(
 
 	Object *object_input = ob;
 	Object *object_eval = DEG_get_evaluated_object(depsgraph, object_input);
-	Object object_for_eval = *object_eval;
+	Object object_for_eval;
 
 	if (object_eval == object_input) {
 		/* Evaluated mesh contains all modifiers applied already.
@@ -882,12 +882,17 @@ Mesh *BKE_mesh_new_from_object(
 		else {
 			effective_apply_modifiers = true;
 		}
+		object_for_eval = *object_eval;
 	}
 	else {
 		if (apply_modifiers) {
+			object_for_eval = *object_eval;
 			if (object_for_eval.runtime.mesh_orig != NULL) {
 				object_for_eval.data = object_for_eval.runtime.mesh_orig;
 			}
+		}
+		else {
+			object_for_eval = *object_input;
 		}
 	}
 

commit 7cb2b407d2a9f409ca87088122ba290604779e72

Fix T63441: Cycles applies modifiers twice
Was a mistake in 873cdeb81d4, which caused access uninitialized variable.

source/blender/blenkernel/intern/mesh_convert.c
@@ -876,7 +876,7 @@ Mesh *BKE_mesh_new_from_object(
 		 * The other types of object has them applied, but are stored in other
 		 * data structures than a mesh. So need to apply modifiers again on a
 		 * temporary copy before converting result to mesh. */
-		if (object_for_eval.type == OB_MESH) {
+		if (object_input->type == OB_MESH) {
 			effective_apply_modifiers = false;
 		}
 		else {