blender/blender

commit 6b082278d19599100cc0b50cc96e4fa00865d093

UI: expose tool settings in the 3D view side-bar
Internally tool settings have been moved to the 3D view.
Added the ability for to draw panels from another space/region
so they can be mirrored in the properties editor.

release/scripts/startup/bl_ui/space_view3d.py
@@ -4717,6 +4717,7 @@ class VIEW3D_PT_active_tool(Panel):
 
     def draw(self, context):
         layout = self.layout
+        tool_mode = context.mode
 
         # Panel display of topbar tool settings.
         # currently displays in tool settings, keep here since the same functionality is used for the topbar.
@@ -4725,7 +4726,12 @@ class VIEW3D_PT_active_tool(Panel):
         layout.use_property_decorate = False
 
         from .space_toolsystem_common import ToolSelectPanelHelper
-        ToolSelectPanelHelper.draw_active_tool_header(context, layout, show_tool_name=True)
+        ToolSelectPanelHelper.draw_active_tool_header(
+            context,
+            layout,
+            show_tool_name=True,
+            tool_key=('VIEW_3D', tool_mode),
+        )
 
 
 class VIEW3D_PT_view3d_properties(Panel):

release/scripts/startup/bl_ui/properties_workspace.py
@@ -24,11 +24,15 @@ from bpy.types import (
 
 from rna_prop_ui import PropertyPanel
 
-
 class WorkSpaceButtonsPanel:
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = ".workspace"
+    # bl_space_type = 'PROPERTIES'
+    # bl_region_type = 'WINDOW'
+    # bl_context = ".workspace"
+
+    # Developer note: this is displayed in tool settings as well as the 3D view.
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Tool"
 
 
 class WORKSPACE_PT_main(WorkSpaceButtonsPanel, Panel):

release/scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -38,8 +38,8 @@ from bl_ui.utils import PresetPanel
 
 
 class View3DPanel:
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
 
 
 # **************** standard tool clusters ******************
@@ -82,7 +82,6 @@ class VIEW3D_PT_tools_meshedit_options(View3DPanel, Panel):
     bl_category = "Options"
     bl_context = ".mesh_edit"  # dot on purpose (access from topbar)
     bl_label = "Options"
-    bl_options = {'DEFAULT_CLOSED'}
 
     @classmethod
     def poll(cls, context):
@@ -204,8 +203,9 @@ class VIEW3D_PT_tools_posemode_options(View3DPanel, Panel):
 
 
 class View3DPaintPanel(UnifiedPaintPanel):
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Tool"
 
 
 class VIEW3D_PT_tools_particlemode(Panel, View3DPaintPanel):
@@ -576,6 +576,7 @@ class VIEW3D_MT_tools_projectpaint_uvlayer(Menu):
 
 
 class VIEW3D_PT_slots_projectpaint(View3DPanel, Panel):
+    bl_category = "Tool"
     bl_context = ".imagepaint"  # dot on purpose (access from topbar)
     bl_label = "Texture Slots"
 
@@ -650,6 +651,7 @@ class VIEW3D_PT_slots_projectpaint(View3DPanel, Panel):
 
 
 class VIEW3D_PT_stencil_projectpaint(View3DPanel, Panel):
+    bl_category = "Tool"
     bl_context = ".imagepaint"  # dot on purpose (access from topbar)
     bl_label = "Mask"
     bl_options = {'DEFAULT_CLOSED'}
@@ -789,6 +791,7 @@ class VIEW3D_PT_tools_brush_texture(Panel, View3DPaintPanel):
 
 # TODO, move to space_view3d.py
 class VIEW3D_PT_tools_mask_texture(Panel, View3DPaintPanel):
+    bl_category = "Tool"
     bl_context = ".imagepaint"  # dot on purpose (access from topbar)
     bl_label = "Texture Mask"
     bl_options = {'DEFAULT_CLOSED'}
@@ -1523,6 +1526,7 @@ class VIEW3D_MT_tools_projectpaint_stencil(Menu):
 # TODO, move to space_view3d.py
 class VIEW3D_PT_tools_particlemode_options(View3DPanel, Panel):
     """Default tools for particle mode"""
+    bl_category = "Options"
     bl_context = ".particlemode"
     bl_label = "Options"
     bl_options = {'DEFAULT_CLOSED'}
@@ -1572,6 +1576,7 @@ class VIEW3D_PT_tools_particlemode_options(View3DPanel, Panel):
 
 class VIEW3D_PT_tools_particlemode_options_shapecut(View3DPanel, Panel):
     """Default tools for particle mode"""
+    bl_category = "Options"
     bl_parent_id = "VIEW3D_PT_tools_particlemode_options"
     bl_label = "Cut Particles to Shape"
     bl_options = {'DEFAULT_CLOSED'}
@@ -1590,6 +1595,7 @@ class VIEW3D_PT_tools_particlemode_options_shapecut(View3DPanel, Panel):
 
 class VIEW3D_PT_tools_particlemode_options_display(View3DPanel, Panel):
     """Default tools for particle mode"""
+    bl_category = "Options"
     bl_parent_id = "VIEW3D_PT_tools_particlemode_options"
     bl_label = "Viewport Display"
 
@@ -1616,7 +1622,7 @@ class VIEW3D_PT_tools_particlemode_options_display(View3DPanel, Panel):
 
 
 class VIEW3D_PT_tools_meshedit_normal(View3DPanel, Panel):
-    bl_category = ""
+    bl_category = "Options"
     bl_context = ".mesh_edit"
     bl_label = "Normals"
     bl_options = {'DEFAULT_CLOSED'}
@@ -1639,6 +1645,7 @@ class VIEW3D_PT_tools_meshedit_normal(View3DPanel, Panel):
 class VIEW3D_PT_tools_grease_pencil_brush(View3DPanel, Panel):
     bl_context = ".greasepencil_paint"
     bl_label = "Brush"
+    bl_category = "Tool"
 
     @classmethod
     def poll(cls, context):
@@ -1695,6 +1702,7 @@ class VIEW3D_PT_tools_grease_pencil_brush(View3DPanel, Panel):
 class VIEW3D_PT_tools_grease_pencil_brush_option(View3DPanel, Panel):
     bl_context = ".greasepencil_paint"
     bl_label = "Options"
+    bl_category = "Tool"
     bl_options = {'DEFAULT_CLOSED'}
 
     @classmethod
@@ -1744,6 +1752,7 @@ class VIEW3D_PT_tools_grease_pencil_brush_stabilizer(View3DPanel, Panel):
     bl_context = ".greasepencil_paint"
     bl_parent_id = 'VIEW3D_PT_tools_grease_pencil_brush_option'
     bl_label = "Stabilize"
+    bl_category = "Tool"
     bl_options = {'DEFAULT_CLOSED'}
 
     @classmethod
@@ -1773,6 +1782,7 @@ class VIEW3D_PT_tools_grease_pencil_brush_settings(View3DPanel, Panel):
     bl_context = ".greasepencil_paint"
     bl_parent_id = 'VIEW3D_PT_tools_grease_pencil_brush_option'
     bl_label = "Post-Processing"
+    bl_category = "Tool"
     bl_options = {'DEFAULT_CLOSED'}
 
     @classmethod
@@ -1814,6 +1824,7 @@ class VIEW3D_PT_tools_grease_pencil_brush_random(View3DPanel, Panel):
     bl_context = ".greasepencil_paint"
     bl_parent_id = 'VIEW3D_PT_tools_grease_pencil_brush_option'
     bl_label = "Randomize"
+    bl_category = "Tool"
     bl_options = {'DEFAULT_CLOSED'}
 
     @classmethod
@@ -1848,6 +1859,7 @@ class VIEW3D_PT_tools_grease_pencil_brush_random(View3DPanel, Panel):
 class VIEW3D_PT_tools_grease_pencil_brushcurves(View3DPanel, Panel):
     bl_context = ".greasepencil_paint"
     bl_label = "Curves"
+    bl_category = "Tool"
     bl_options = {'DEFAULT_CLOSED'}
 
     @classmethod
@@ -1862,6 +1874,7 @@ class VIEW3D_PT_tools_grease_pencil_brushcurves(View3DPanel, Panel):
 class VIEW3D_PT_tools_grease_pencil_brushcurves_sensitivity(View3DPanel, Panel):
     bl_context = ".greasepencil_paint"
     bl_label = "Sensitivity"
+    bl_category = "Tool"
     bl_parent_id = "VIEW3D_PT_tools_grease_pencil_brushcurves"
 
     def draw(self, context):
@@ -1878,6 +1891,7 @@ class VIEW3D_PT_tools_grease_pencil_brushcurves_sensitivity(View3DPanel, Panel):
 class VIEW3D_PT_tools_grease_pencil_brushcurves_strength(View3DPanel, Panel):
     bl_context = ".greasepencil_paint"
     bl_label = "Strength"
+    bl_category = "Tool"
     bl_parent_id = "VIEW3D_PT_tools_grease_pencil_brushcurves"
 
     def draw(self, context):
@@ -1894,6 +1908,7 @@ class VIEW3D_PT_tools_grease_pencil_brushcurves_strength(View3DPanel, Panel):
 class VIEW3D_PT_tools_grease_pencil_brushcurves_jitter(View3DPanel, Panel):
     bl_context = ".greasepencil_paint"
     bl_label = "Jitter"
+    bl_category = "Tool"
     bl_parent_id = "VIEW3D_PT_tools_grease_pencil_brushcurves"
 
     def draw(self, context):
@@ -1910,6 +1925,7 @@ class VIEW3D_PT_tools_grease_pencil_brushcurves_jitter(View3DPanel, Panel):
 # Grease Pencil stroke editing tools
 class VIEW3D_PT_tools_grease_pencil_edit(GreasePencilStrokeEditPanel, Panel):
     bl_space_type = 'VIEW_3D'
+    bl_category = "Tool"
 
 
 # Grease Pencil stroke interpolation tools
@@ -1964,6 +1980,7 @@ class VIEW3D_PT_tools_grease_pencil_sculpt(GreasePencilStrokeSculptPanel, View3D
     bl_context = ".greasepencil_sculpt"
     bl_category = "Tools"
     bl_label = "Brush"
+    bl_category = "Tool"
 
 
 # Grease Pencil weight painting tools
@@ -1971,6 +1988,7 @@ class VIEW3D_PT_tools_grease_pencil_weight_paint(View3DPanel, Panel):
     bl_context = ".greasepencil_weight"
     bl_category = "Tools"
     bl_label = "Brush"
+    bl_category = "Tool"
 
     def draw(self, context):
         layout = self.layout
@@ -1995,22 +2013,26 @@ class VIEW3D_PT_tools_grease_pencil_weight_paint(View3DPanel, Panel):
 class VIEW3D_PT_tools_grease_pencil_paint_appearance(GreasePencilAppearancePanel, View3DPanel, Panel):
     bl_context = ".greasepencil_paint"
     bl_label = "Display"
+    bl_category = "Tool"
 
 
 class VIEW3D_PT_tools_grease_pencil_sculpt_appearance(GreasePencilAppearancePanel, View3DPanel, Panel):
     bl_context = ".greasepencil_sculpt"
     bl_label = "Display"
+    bl_category = "Tool"
 
 
 class VIEW3D_PT_tools_grease_pencil_sculpt_options(GreasePencilSculptOptionsPanel, View3DPanel, Panel):
     bl_context = ".greasepencil_sculpt"
     bl_label = "Sculpt Strokes"
     bl_parent_id = 'VIEW3D_PT_tools_grease_pencil_sculpt'
+    bl_category = "Tool"
 
 
 class VIEW3D_PT_tools_grease_pencil_weight_appearance(GreasePencilAppearancePanel, View3DPanel, Panel):
     bl_context = ".greasepencil_weight"
     bl_label = "Display"
+    bl_category = "Tool"
 
 
 class VIEW3D_PT_gpencil_brush_presets(PresetPanel, Panel):

source/blender/editors/include/ED_screen.h
@@ -87,9 +87,12 @@ void ED_region_panels_ex(const struct bContext *C,
 void ED_region_panels(const struct bContext *C, struct ARegion *ar);
 void ED_region_panels_layout_ex(const struct bContext *C,
                                 struct ARegion *ar,
+                                struct ListBase *paneltypes,
                                 const char *contexts[],
                                 int contextnr,
-                                const bool vertical);
+                                const bool vertical,
+                                const char *category_override);
+
 void ED_region_panels_layout(const struct bContext *C, struct ARegion *ar);
 void ED_region_panels_draw(const struct bContext *C, struct ARegion *ar);
 

source/blender/editors/include/ED_view3d.h
@@ -730,4 +730,9 @@ void ED_view3d_gizmo_mesh_preselect_get_active(struct bContext *C,
                                                struct Base **r_base,
                                                struct BMElem **r_ele);
 
+/* space_view3d.c */
+void ED_view3d_buttons_region_layout_ex(const struct bContext *C,
+                                        struct ARegion *ar,
+                                        const char *category_override);
+
 #endif /* __ED_VIEW3D_H__ */

source/blender/editors/interface/interface_panel.c
@@ -2069,7 +2069,11 @@ void UI_panel_category_draw_all(ARegion *ar, const char *category_id_active)
   ui_fontscale(&fstyle_points, aspect / (U.pixelsize * 1.1f));
   BLF_size(fontid, fstyle_points, U.dpi);
 
-  BLI_assert(UI_panel_category_is_visible(ar));
+  /* Check the region type supports categories to avoid an assert
+   * for showing 3D view panels in the properties space. */
+  if ((1 << ar->regiontype) & RGN_TYPE_HAS_CATEGORY_MASK) {
+    BLI_assert(UI_panel_category_is_visible(ar));
+  }
 
   /* calculate tab rect's and check if we need to scale down */
   for (pc_dyn = ar->panels_category.first; pc_dyn; pc_dyn = pc_dyn->next) {

source/blender/editors/screen/area.c
@@ -2300,14 +2300,48 @@ static void ed_panel_draw(const bContext *C,
  * Matching against any of these strings will draw the panel.
  * Can be NULL to skip context checks.
  */
-void ED_region_panels_layout_ex(
-    const bContext *C, ARegion *ar, const char *contexts[], int contextnr, const bool vertical)
+void ED_region_panels_layout_ex(const bContext *C,
+                                ARegion *ar,
+                                ListBase *paneltypes,
+                                const char *contexts[],
+                                int contextnr,
+                                const bool vertical,
+                                const char *category_override)
 {
+  /* collect panels to draw */
+  LinkNode *panel_types_stack = NULL;
+  for (PanelType *pt = paneltypes->last; pt; pt = pt->prev) {
+    WorkSpace *workspace = CTX_wm_workspace(C);
+    /* Only draw top level panels. */
+    if (pt->parent) {
+      continue;
+    }
+
+    if (category_override) {
+      if (!STREQ(pt->category, category_override)) {
+        continue;
+      }
+    }
+
+    /* verify context */
+    if (contexts && pt->context[0] && !streq_array_any(pt->context, contexts)) {
+      continue;
+    }
+
+    /* If we're tagged, only use compatible. */
+    if (pt->owner_id[0] && BKE_workspace_owner_id_check(workspace, pt->owner_id) == false) {
+      continue;
+    }
+
+    /* draw panel */
+    if (pt->draw && (!pt->poll || pt->poll(C, pt))) {
+      BLI_linklist_prepend_alloca(&panel_types_stack, pt);
+    }
+  }
+
   ar->runtime.category = NULL;
 
-  const WorkSpace *workspace = CTX_wm_workspace(C);
   ScrArea *sa = CTX_wm_area(C);
-  PanelType *pt;
   View2D *v2d = &ar->v2d;
   int x, y, w, em;
   bool is_context_new = 0;
@@ -2315,15 +2349,14 @@ void ED_region_panels_layout_ex(
 
   /* XXX, should use some better check? */
   /* For now also has hardcoded check for clip editor until it supports actual toolbar. */
-  bool use_category_tabs = ((1 << ar->regiontype) & RGN_TYPE_HAS_CATEGORY_MASK) ||
-                           (ar->regiontype == RGN_TYPE_TOOLS && sa->spacetype == SPACE_CLIP);
+  bool use_category_tabs = (category_override == NULL) &&
+                           ((((1 << ar->regiontype) & RGN_TYPE_HAS_CATEGORY_MASK) ||
+                             (ar->regiontype == RGN_TYPE_TOOLS && sa->spacetype == SPACE_CLIP)));
   /* offset panels for small vertical tab area */
   const char *category = NULL;
   const int category_tabs_width = UI_PANEL_CATEGORY_MARGIN_WIDTH;
   int margin_x = 0;
   const bool region_layout_based = ar->flag & RGN_FLAG_DYNAMIC_SIZE;
-
-  BLI_SMALLSTACK_DECLARE(pt_stack, PanelType *);
 
   if (contextnr != -1) {
     is_context_new = UI_view2d_tab_set(v2d, contextnr);
@@ -2348,42 +2381,19 @@ void ED_region_panels_layout_ex(
 
   scroll = v2d->scroll;
 
-  /* collect panels to draw */
-  for (pt = ar->type->paneltypes.last; pt; pt = pt->prev) {
-    /* Only draw top level panels. */
-    if (pt->parent) {
-      continue;
-    }
-
-    /* verify context */
-    if (contexts && pt->context[0] && !streq_array_any(pt->context, contexts)) {
-      continue;
-    }
-
-    /* If we're tagged, only use compatible. */
-    if (pt->owner_id[0] && BKE_workspace_owner_id_check(workspace, pt->owner_id) == false) {
-      continue;
-    }
-
-    /* draw panel */
-    if (pt->draw && (!pt->poll || pt->poll(C, pt))) {
-      BLI_SMALLSTACK_PUSH(pt_stack, pt);
-    }
-  }
-
   /* collect categories */
   if (use_category_tabs) {
     UI_panel_category_clear_all(ar);
 
     /* gather unique categories */
-    BLI_SMALLSTACK_ITER_BEGIN (pt_stack, pt) {
+    for (LinkNode *pt_link = panel_types_stack; pt_link; pt_link = pt_link->next) {
+      PanelType *pt = pt_link->link;
       if (pt->category[0]) {
         if (!UI_panel_category_find(ar, pt->category)) {
           UI_panel_category_add(ar, pt->category);
         }
       }
     }
-    BLI_SMALLSTACK_ITER_END;
 
     if (!UI_panel_category_is_visible(ar)) {
       use_category_tabs = false;
@@ -2411,7 +2421,8 @@ void ED_region_panels_layout_ex(
   /* set view2d view matrix  - UI_block_begin() stores it */
   UI_view2d_view_ortho(v2d);
 
-  BLI_SMALLSTACK_ITER_BEGIN (pt_stack, pt) {
+  for (LinkNode *pt_link = panel_types_stack; pt_link; pt_link = pt_link->next) {
+    PanelType *pt = pt_link->link;
     Panel *panel = UI_panel_find_by_type(&ar->panels, pt);
 
     if (use_category_tabs && pt->category[0] && !STREQ(category, pt->category)) {
@@ -2422,7 +2433,6 @@ void ED_region_panels_layout_ex(
 
     ed_panel_draw(C, sa, ar, &ar->panels, pt, panel, w, em, vertical);
   }
-  BLI_SMALLSTACK_ITER_END;
 
   /* align panels and return size */
   UI_panels_end(C, ar, &x, &y);
@@ -2493,9 +2503,11 @@ void ED_region_panels_layout_ex(
     ar->runtime.category = category;
   }
 }
+
 void ED_region_panels_layout(const bContext *C, ARegion *ar)
 {
-  ED_region_panels_layout_ex(C, ar, NULL, -1, true);
+  bool vertical = true;
+  ED_region_panels_layout_ex(C, ar, &ar->type->paneltypes, NULL, -1, vertical, NULL);
 }
 
 void ED_region_panels_draw(const bContext *C, ARegion *ar)
@@ -2544,7 +2556,7 @@ void ED_region_panels_ex(
     const bContext *C, ARegion *ar, const char *contexts[], int contextnr, const bool vertical)
 {
   /* TODO: remove? */
-  ED_region_panels_layout_ex(C, ar, contexts, contextnr, vertical);
+  ED_region_panels_layout_ex(C, ar, &ar->type->paneltypes, contexts, contextnr, vertical, NULL);
   ED_region_panels_draw(C, ar);
 }
 

source/blender/editors/space_buttons/space_buttons.c
@@ -34,6 +34,7 @@
 
 #include "ED_space_api.h"
 #include "ED_screen.h"
+#include "ED_view3d.h" /* To draw toolbar UI. */
 
 #include "WM_api.h"
 #include "WM_types.h"
@@ -198,105 +199,7 @@ static void buttons_main_region_layout_properties(const bContext *C,
   }
 
   const bool vertical = true;
-  ED_region_panels_layout_ex(C, ar, contexts, sbuts->mainb, vertical);
-}
-
-static void buttons_main_region_layout_tool(const bContext *C, ARegion *ar)
-{
-  const enum eContextObjectMode mode = CTX_data_mode_enum(C);
-
-  const char *contexts_base[5] = {NULL};
-  contexts_base[0] = ".active_tool";
-  const char **contexts = &contexts_base[1];
-
-  /* Hard coded to 3D view. */
-  {
-    switch (mode) {
-      case CTX_MODE_EDIT_MESH:
-        ARRAY_SET_ITEMS(contexts, ".mesh_edit");
-        break;
-      case CTX_MODE_EDIT_CURVE:
-        ARRAY_SET_ITEMS(contexts, ".curve_edit");
-        break;
-      case CTX_MODE_EDIT_SURFACE:
-        ARRAY_SET_ITEMS(contexts, ".curve_edit");
-        break;
-      case CTX_MODE_EDIT_TEXT:
-        ARRAY_SET_ITEMS(contexts, ".text_edit");
-        break;
-      case CTX_MODE_EDIT_ARMATURE:
-        ARRAY_SET_ITEMS(contexts, ".armature_edit");
-        break;
-      case CTX_MODE_EDIT_METABALL:
-        ARRAY_SET_ITEMS(contexts, ".mball_edit");
-        break;
-      case CTX_MODE_EDIT_LATTICE:
-        ARRAY_SET_ITEMS(contexts, ".lattice_edit");
-        break;
-      case CTX_MODE_POSE:
-        ARRAY_SET_ITEMS(contexts, ".posemode");
-        break;
-      case CTX_MODE_SCULPT:
-        ARRAY_SET_ITEMS(contexts, ".paint_common", ".sculpt_mode");
-        break;
-      case CTX_MODE_PAINT_WEIGHT:
-        ARRAY_SET_ITEMS(contexts, ".paint_common", ".weightpaint");
-        break;
-      case CTX_MODE_PAINT_VERTEX:
-        ARRAY_SET_ITEMS(contexts, ".paint_common", ".vertexpaint");
-        break;
-      case CTX_MODE_PAINT_TEXTURE:
-        ARRAY_SET_ITEMS(contexts, ".paint_common", ".imagepaint");
-        break;
-      case CTX_MODE_PARTICLE:
-        ARRAY_SET_ITEMS(contexts, ".paint_common", ".particlemode");
-        break;
-      case CTX_MODE_OBJECT:
-        ARRAY_SET_ITEMS(contexts, ".objectmode");
-        break;
-      case CTX_MODE_PAINT_GPENCIL:
-        ARRAY_SET_ITEMS(contexts, ".greasepencil_paint");
-        break;
-      case CTX_MODE_SCULPT_GPENCIL:
-        ARRAY_SET_ITEMS(contexts, ".greasepencil_sculpt");
-        break;
-      case CTX_MODE_WEIGHT_GPENCIL:
-        ARRAY_SET_ITEMS(contexts, ".greasepencil_weight");
-        break;
-      default:
-        break;
-    }
-  }
-
-  /* for grease pencil we don't use tool system yet, so we need check outside
-   * workspace->tools_space_type because this value is not available
-   */
-  switch (mode) {
-    case CTX_MODE_PAINT_GPENCIL:
-      ARRAY_SET_ITEMS(contexts, ".greasepencil_paint");
-      break;
-    case CTX_MODE_SCULPT_GPENCIL:
-      ARRAY_SET_ITEMS(contexts, ".greasepencil_sculpt");
-      break;
-    case CTX_MODE_WEIGHT_GPENCIL:
-      ARRAY_SET_ITEMS(contexts, ".greasepencil_weight");
-      break;
-    case CTX_MODE_EDIT_GPENCIL:
-      ARRAY_SET_ITEMS(contexts, ".greasepencil_edit");
-      break;
-    default:
-      break;
-  }
-
-  int i = 0;
-  while (contexts_base[i]) {
-    i++;
-  }
-  BLI_assert(i < ARRAY_SIZE(contexts_base));
-  contexts_base[i] = ".workspace";
-
-  const bool vertical = true;
-  ED_region_panels_layout_ex(C, ar, contexts_base, -1, vertical);
+  ED_region_panels_layout_ex(C, ar, &ar->type->paneltypes, contexts, sbuts->mainb, vertical, NULL);
 }
 
 static void buttons_main_region_layout(const bContext *C, ARegion *ar)
@@ -305,7 +208,7 @@ static void buttons_main_region_layout(const bContext *C, ARegion *ar)
   SpaceProperties *sbuts = CTX_wm_space_properties(C);
 
   if (sbuts->mainb == BCONTEXT_TOOL) {
-    buttons_main_region_layout_tool(C, ar);
+    ED_view3d_buttons_region_layout_ex(C, ar, "Tool");
   }
   else {
     buttons_main_region_layout_properties(C, sbuts, ar);

Bforartists/Bforartists

commit 0eb4d8caf18ea5727758b6a706ef33ba7bd44b6c

Fix top-bar showing paint popover panels
Regression from 6b082278d1959 when panels were moved into the 3D view.

release/scripts/startup/bl_ui/space_view3d.py
@@ -79,16 +79,16 @@ class VIEW3D_HT_tool_header(Header):
         # Note: general mode options should be added to 'draw_mode_settings'.
         if tool_mode == 'SCULPT':
             if (tool is not None) and tool.has_datablock:
-                layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".paint_common", category="")
+                layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".paint_common", category="Tool")
         elif tool_mode == 'PAINT_VERTEX':
             if (tool is not None) and tool.has_datablock:
-                layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".paint_common", category="")
+                layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".paint_common", category="Tool")
         elif tool_mode == 'PAINT_WEIGHT':
             if (tool is not None) and tool.has_datablock:
-                layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".paint_common", category="")
+                layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".paint_common", category="Tool")
         elif tool_mode == 'PAINT_TEXTURE':
             if (tool is not None) and tool.has_datablock:
-                layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".paint_common", category="")
+                layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".paint_common", category="Tool")
         elif tool_mode == 'EDIT_ARMATURE':
             pass
         elif tool_mode == 'EDIT_CURVE':
@@ -100,15 +100,15 @@ class VIEW3D_HT_tool_header(Header):
         elif tool_mode == 'PARTICLE':
             # Disable, only shows "Brush" panel, which is already in the top-bar.
             # if tool.has_datablock:
-            #     layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".paint_common", category="")
+            #     layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".paint_common", category="Tool")
             pass
         elif tool_mode == 'PAINT_GPENCIL':
             if (tool is not None) and tool.has_datablock:
-                layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".greasepencil_paint", category="")
+                layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".greasepencil_paint", category="Tool")
         elif tool_mode == 'SCULPT_GPENCIL':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".greasepencil_sculpt", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".greasepencil_sculpt", category="Tool")
         elif tool_mode == 'WEIGHT_GPENCIL':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".greasepencil_weight", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".greasepencil_weight", category="Tool")
 
     def draw_mode_settings(self, context):
         layout = self.layout
@@ -120,31 +120,31 @@ class VIEW3D_HT_tool_header(Header):
         tool_mode = context.mode if tool is None else tool.mode
 
         if tool_mode == 'SCULPT':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".sculpt_mode", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".sculpt_mode", category="Tool")
         elif tool_mode == 'PAINT_VERTEX':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".vertexpaint", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".vertexpaint", category="Tool")
         elif tool_mode == 'PAINT_WEIGHT':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".weightpaint", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".weightpaint", category="Tool")
         elif tool_mode == 'PAINT_TEXTURE':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".imagepaint", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".imagepaint", category="Tool")
         elif tool_mode == 'EDIT_TEXT':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".text_edit", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".text_edit", category="Tool")
         elif tool_mode == 'EDIT_ARMATURE':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".armature_edit", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".armature_edit", category="Tool")
         elif tool_mode == 'EDIT_METABALL':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".mball_edit", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".mball_edit", category="Tool")
         elif tool_mode == 'EDIT_LATTICE':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".lattice_edit", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".lattice_edit", category="Tool")
         elif tool_mode == 'EDIT_CURVE':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".curve_edit", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".curve_edit", category="Tool")
         elif tool_mode == 'EDIT_MESH':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".mesh_edit", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".mesh_edit", category="Tool")
         elif tool_mode == 'POSE':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".posemode", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".posemode", category="Tool")
         elif tool_mode == 'PARTICLE':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".particlemode", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".particlemode", category="Tool")
         elif tool_mode == 'OBJECT':
-            layout.popover_group(space_type='PROPERTIES', region_type='WINDOW', context=".objectmode", category="")
+            layout.popover_group(space_type='VIEW_3D', region_type='UI', context=".objectmode", category="Tool")
         elif tool_mode in {'PAINT_GPENCIL', 'EDIT_GPENCIL', 'SCULPT_GPENCIL', 'WEIGHT_GPENCIL'}:
             # Grease pencil layer.
             gpl = context.active_gpencil_layer