1afd8447f373d7f0be63b663d416dee37c164dd7
ReinerBforartists <reiner.prokein@posteo.de>
2019-05-24 09:01:26 +0200

    Import Export menus - readd missing icons #861

--- release/scripts/addons/io_scene_gltf2/__init__.py
+++ release/scripts/addons/io_scene_gltf2/__init__.py
@@ -505,7 +505,7 @@ class ExportGLTF2(bpy.types.Operator, ExportGLTF2_Base, ExportHelper):
 
 
 def menu_func_export(self, context):
-    self.layout.operator(ExportGLTF2.bl_idname, text='glTF 2.0 (.glb/.gltf)')
+    self.layout.operator(ExportGLTF2.bl_idname, text='glTF 2.0 (.glb/.gltf)', icon = 'SAVE_GITF')
 
 
 class ImportGLTF2(Operator, ImportHelper):
@@ -583,7 +583,7 @@ class ImportGLTF2(Operator, ImportHelper):
 
 
 def menu_func_import(self, context):
-    self.layout.operator(ImportGLTF2.bl_idname, text='glTF 2.0 (.glb/.gltf)')
+    self.layout.operator(ImportGLTF2.bl_idname, text='glTF 2.0 (.glb/.gltf)', icon = 'LOAD_GITF')
 
 
 classes = (
