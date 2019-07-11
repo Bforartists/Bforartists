97efb22c999f3a5035ee8e6bb6889c45967f2b8b
ReinerBforartists <reiner.prokein@posteo.de>
2019-07-11 10:33:31 +0200

    Edit Mode - UV Menu - put Live Unwrap into the sidebar #1019

--- release/scripts/startup/bl_operators/uvcalc_smart_project.py
+++ release/scripts/startup/bl_operators/uvcalc_smart_project.py
@@ -983,7 +983,7 @@ from bpy.props import FloatProperty, BoolProperty
 
 
 class SmartProject(Operator):
-    """This script projection unwraps the selected faces of a mesh """ \
+    """Smart UV Project\nThis script projection unwraps the selected faces of a mesh """ \
         """(it operates on all selected mesh objects, and can be used """ \
         """to unwrap selected faces, or all faces)"""
     bl_idname = "uv.smart_project"
