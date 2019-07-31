Bforartists/Bforartists

commit 3b4065f2e7ef8210f073d27beb8d4714807013b3

Fix T66378: Missing animation update when switching view layer
Current frame is stored in a scene, and scene might have multiple view
layers. The inactive view layers were not informed about scene's frame
being changed, so when user switched back to view after changing scene
frame it was in an inconsistent state between current scene frame and
animation.

Now we tag scene for time changes, so dependency graph can catch up
and do proper update.

Currently tagging is from quite generic place. Probably better approach
would be to tag from where frame is actually being assigned. Downside
of this is that it's easy to miss some places.

Reviewers: brecht, mont29

Reviewed By: brecht

Maniphest Tasks: T66378

Differential Revision: https://developer.blender.org/D5332

source/blender/depsgraph/intern/eval/deg_eval_flush.cc
@@ -39,6 +39,7 @@
 
 extern "C" {
 #include "DNA_object_types.h"
+#include "DNA_scene_types.h"
 
 #include "DRW_engine.h"
 } /* extern "C" */
@@ -53,6 +54,7 @@ extern "C" {
 #include "intern/node/deg_node_factory.h"
 #include "intern/node/deg_node_id.h"
 #include "intern/node/deg_node_operation.h"
+#include "intern/node/deg_node_time.h"
 
 #include "intern/eval/deg_eval_copy_on_write.h"
 
@@ -348,8 +350,15 @@ void deg_graph_flush_updates(Main *bmain, Depsgraph *graph)
   BLI_assert(bmain != NULL);
   BLI_assert(graph != NULL);
   /* Nothing to update, early out. */
-  if (BLI_gset_len(graph->entry_tags) == 0) {
+  if (BLI_gset_len(graph->entry_tags) == 0 && !graph->need_update_time) {
     return;
+  }
+  if (graph->need_update_time) {
+    const Scene *scene_orig = graph->scene;
+    const float ctime = scene_orig->r.cfra + scene_orig->r.subframe;
+    DEG::TimeSourceNode *time_source = graph->find_time_source();
+    graph->ctime = ctime;
+    time_source->tag_update(graph, DEG::DEG_UPDATE_SOURCE_TIME);
   }
   /* Reset all flags, get ready for the flush. */
   flush_prepare(graph);

source/blender/depsgraph/intern/depsgraph.cc
@@ -68,6 +68,7 @@ template<typename T> static void remove_from_vector(vector<T> *vector, const T &
 Depsgraph::Depsgraph(Scene *scene, ViewLayer *view_layer, eEvaluationMode mode)
     : time_source(NULL),
       need_update(true),
+      need_update_time(false),
       scene(scene),
       view_layer(view_layer),
       mode(mode),

source/blender/depsgraph/intern/depsgraph.h
@@ -160,6 +160,10 @@ struct Depsgraph {
   /* Nodes which have been tagged as "directly modified". */
   GSet *entry_tags;
 
+  /* Special entry tag for time source. Allows to tag invisible dependency graphs for update when
+   * scene frame changes, so then when dependency graph becomes visible it is on a proper state. */
+  bool need_update_time;
+
   /* Convenience Data ................... */
 
   /* XXX: should be collected after building (if actually needed?) */

source/blender/depsgraph/intern/depsgraph_eval.cc
@@ -61,6 +61,7 @@ void DEG_evaluate_on_refresh(Depsgraph *graph)
     BKE_scene_frame_set(deg_graph->scene_cow, deg_graph->ctime);
   }
   DEG::deg_evaluate_on_refresh(deg_graph);
+  deg_graph->need_update_time = false;
 }
 
 /* Frame-change happened for root scene that graph belongs to. */
@@ -79,6 +80,7 @@ void DEG_evaluate_on_framechange(Main *bmain, Depsgraph *graph, float ctime)
   }
   /* Perform recalculation updates. */
   DEG::deg_evaluate_on_refresh(deg_graph);
+  deg_graph->need_update_time = false;
 }
 
 bool DEG_needs_eval(Depsgraph *graph)

source/blender/depsgraph/intern/depsgraph_tag.cc
@@ -229,6 +229,9 @@ void depsgraph_tag_to_component_opcode(const ID *id,
     case ID_RECALC_PARAMETERS:
       *component_type = NodeType::PARAMETERS;
       break;
+    case ID_RECALC_TIME:
+      BLI_assert(!"Should be handled outside of this function");
+      break;
     case ID_RECALC_ALL:
     case ID_RECALC_PSYS_ALL:
       BLI_assert(!"Should not happen");
@@ -357,6 +360,12 @@ static void graph_id_tag_update_single_flag(Main *bmain,
   if (tag == ID_RECALC_EDITORS) {
     if (graph != NULL) {
       depsgraph_update_editors_tag(bmain, graph, id);
+    }
+    return;
+  }
+  else if (tag == ID_RECALC_TIME) {
+    if (graph != NULL) {
+      graph->need_update_time = true;
     }
     return;
   }
@@ -664,6 +673,8 @@ const char *DEG_update_tag_as_string(IDRecalcFlag flag)
       return "AUDIO";
     case ID_RECALC_PARAMETERS:
       return "PARAMETERS";
+    case ID_RECALC_TIME:
+      return "TIME";
     case ID_RECALC_ALL:
       return "ALL";
   }

source/blender/editors/screen/screen_edit.c
@@ -1457,6 +1457,8 @@ void ED_update_for_newframe(Main *bmain, Depsgraph *depsgraph)
 {
   Scene *scene = DEG_get_input_scene(depsgraph);
 
+  DEG_id_tag_update_ex(bmain, &scene->id, ID_RECALC_TIME);
+
 #ifdef DURIAN_CAMERA_SWITCH
   void *camera = BKE_scene_camera_switch_find(scene);
   if (camera && scene->camera != camera) {

source/blender/makesdna/DNA_ID.h
@@ -616,6 +616,10 @@ typedef enum IDRecalcFlag {
 
   ID_RECALC_PARAMETERS = (1 << 21),
 
+  /* Makes it so everything what depends on time.
+   * Basically, the same what changing frame in a timeline will do. */
+  ID_RECALC_TIME = (1 << 22),
+
   /***************************************************************************
    * Pseudonyms, to have more semantic meaning in the actual code without
    * using too much low-level and implementation specific tags. */

commit 63fa9cca6de2531b5dedfdf79fd6a47fbc24fa64

Fix T67883: Time Remapping doesn't work
Was caused by 3b4065f2e7e.

source/blender/depsgraph/intern/eval/deg_eval_flush.cc
@@ -30,6 +30,7 @@
 #include <cmath>
 
 #include "BKE_object.h"
+#include "BKE_scene.h"
 
 #include "BLI_utildefines.h"
 #include "BLI_listbase.h"
@@ -355,7 +356,7 @@ void deg_graph_flush_updates(Main *bmain, Depsgraph *graph)
   }
   if (graph->need_update_time) {
     const Scene *scene_orig = graph->scene;
-    const float ctime = scene_orig->r.cfra + scene_orig->r.subframe;
+    const float ctime = BKE_scene_frame_get(scene_orig);
     DEG::TimeSourceNode *time_source = graph->find_time_source();
     graph->ctime = ctime;
     time_source->tag_update(graph, DEG::DEG_UPDATE_SOURCE_TIME);