ef5947b5c60be8fc07f251cd4c409777b89ece18
ReinerBforartists <reiner.prokein@posteo.de>
2019-12-30 09:19:02 +0100

    UV Editor - Header - UV Map Prop -remove double tooltip titles #1325

--- source/blender/makesrna/intern/rna_mesh.c
+++ source/blender/makesrna/intern/rna_mesh.c
@@ -2414,7 +2414,7 @@ static void rna_def_uv_layers(BlenderRNA *brna, PropertyRNA *cprop)
   RNA_def_property_pointer_funcs(
       prop, "rna_Mesh_uv_layer_active_get", "rna_Mesh_uv_layer_active_set", NULL, NULL);
   RNA_def_property_flag(prop, PROP_EDITABLE | PROP_NEVER_UNLINK);
-  RNA_def_property_ui_text(prop, "Active UV Loop Layer", "Active UV loop layer");
+  RNA_def_property_ui_text(prop, "Active UV Loop Layer", "The currently active UV loop layer");
   RNA_def_property_update(prop, 0, "rna_Mesh_update_data");
 
   prop = RNA_def_property(srna, "active_index", PROP_INT, PROP_UNSIGNED);
