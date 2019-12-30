201abcfe91697316fa74a4b4a5a841b0e043f057
ReinerBforartists <reiner.prokein@posteo.de>
2019-12-30 09:25:53 +0100

    Top area - remove View Layer and Scene props #1324

--- release/scripts/startup/bl_ui/space_topbar.py
+++ release/scripts/startup/bl_ui/space_topbar.py
@@ -67,17 +67,6 @@ class TOPBAR_HT_upper_bar(Header):
             layout.template_reports_banner()
             layout.template_running_jobs()
 
-        # Active workspace view-layer is retrieved through window, not through workspace.
-        layout.template_ID(window, "scene", new="scene.new",
-                           unlink="scene.delete")
-
-        row = layout.row(align=True)
-        row.template_search(
-            window, "view_layer",
-            scene, "view_layers",
-            new="scene.view_layer_add",
-            unlink="scene.view_layer_remove")
-
 
 class TOPBAR_PT_tool_settings_extra(Panel):
     """
