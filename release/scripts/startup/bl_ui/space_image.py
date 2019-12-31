9f8a4ce912f8a622fd455d1bec14d6b5d295a196
ReinerBforartists <reiner.prokein@posteo.de>
2019-12-31 11:59:12 +0100

    UV Editor - UV Menu - put pin above unpin #1313

--- release/scripts/startup/bl_ui/space_image.py
+++ release/scripts/startup/bl_ui/space_image.py
@@ -463,8 +463,8 @@ class IMAGE_MT_uvs(Menu):
         layout.operator("uv.unwrap", text = "Unwrap ABF", icon='UNWRAP_ABF').method = 'ANGLE_BASED'
         layout.operator("uv.unwrap", text = "Unwrap Conformal", icon='UNWRAP_LSCM').method = 'CONFORMAL'
         layout.operator("uv.follow_active_quads", icon = "FOLLOWQUADS")
-        layout.operator("uv.pin", text="Unpin", icon = "UNPINNED").clear = True
         layout.operator("uv.pin", icon = "PINNED").clear = False
+        layout.operator("uv.pin", text="Unpin", icon = "UNPINNED").clear = True      
 
         layout.separator()
 
