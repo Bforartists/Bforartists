9a7802d6c2607959bc687179e95f5ee94f931ed8
ReinerBforartists <reiner.prokein@posteo.de>
2020-02-29 09:49:02 +0100

    added experimental snap panel to the tool shelf

--- release/scripts/startup/bl_ui/space_toolsystem_toolbar.py
+++ release/scripts/startup/bl_ui/space_toolsystem_toolbar.py
@@ -2317,12 +2317,84 @@ class SEQUENCER_PT_tools_active(ToolSelectPanelHelper, Panel):
         ],
     }
 
+# bfa - experimental testpanel
+
+class VIEW3D_PT_snappanel_toolshelf(Panel):
+    bl_label = "Snap"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    #bl_options = {'HIDE_HEADER'}
+    #bl_category = "Create"
+
+    @staticmethod
+    def ts_width(layout, region, scale_y):
+        """
+        Choose an appropriate layout for the toolbar.
+        """
+        # Currently this just checks the width,
+        # we could have different layouts as preferences too.
+        system = bpy.context.preferences.system
+        view2d = region.view2d
+        view2d_scale = (
+            view2d.region_to_view(1.0, 0.0)[0] -
+            view2d.region_to_view(0.0, 0.0)[0]
+        )
+        width_scale = region.width * view2d_scale / system.ui_scale
+
+        if width_scale > 160.0:
+            show_text = True
+        else:
+            show_text = False
+
+        return show_text 
+
+    def draw(self, _context):
+        layout = self.layout
+
+        show_text = self.ts_width(layout, _context.region, scale_y= 1.75)   # Get the state of the show_text flag
+
+        if show_text is True:
+
+            layout.operator("view3d.snap_selected_to_cursor", text="Selection to Cursor", icon = "SELECTIONTOCURSOR").use_offset = False
+            layout.operator("view3d.snap_selected_to_cursor", text="Selection to Cursor (Keep Offset)", icon = "SELECTIONTOCURSOROFFSET").use_offset = True
+            layout.operator("view3d.snap_selected_to_active", text="Selection to Active", icon = "SELECTIONTOACTIVE")
+            layout.operator("view3d.snap_selected_to_grid", text="Selection to Grid", icon = "SELECTIONTOGRID")
+
+            layout.separator()
+
+            layout.operator("view3d.snap_cursor_to_selected", text="Cursor to Selected", icon = "CURSORTOSELECTION")
+            layout.operator("view3d.snap_cursor_to_center", text="Cursor to World Origin", icon = "CURSORTOCENTER")
+            layout.operator("view3d.snap_cursor_to_active", text="Cursor to Active", icon = "CURSORTOACTIVE")
+            layout.operator("view3d.snap_cursor_to_grid", text="Cursor to Grid", icon = "CURSORTOGRID")
+
+        else:
+
+            col = layout.column(align=True)
+            col.label(text="Selection to ... :")
+            row = col.row(align=False)
+            row.alignment = 'LEFT'
+
+            row.operator("view3d.snap_selected_to_cursor", text = "", icon = "SELECTIONTOCURSOR").use_offset = False
+            row.operator("view3d.snap_selected_to_cursor", text = "", icon = "SELECTIONTOCURSOROFFSET").use_offset = True
+            row.operator("view3d.snap_selected_to_active", text = "", icon = "SELECTIONTOACTIVE")
+            row.operator("view3d.snap_selected_to_grid", text = "", icon = "SELECTIONTOGRID")
+
+            col.label(text="Cursor to ... :")
+            row = col.row(align=False)
+            row.alignment = 'LEFT'
+
+            row.operator("view3d.snap_cursor_to_selected", text = "", icon = "CURSORTOSELECTION")         
+            row.operator("view3d.snap_cursor_to_center", text = "", icon = "CURSORTOCENTER")         
+            row.operator("view3d.snap_cursor_to_active", text = "", icon = "CURSORTOACTIVE")
+            row.operator("view3d.snap_cursor_to_grid", text = "", icon = "CURSORTOGRID")
+
 
 classes = (
     IMAGE_PT_tools_active,
     NODE_PT_tools_active,
     VIEW3D_PT_tools_active,
     SEQUENCER_PT_tools_active,
+    VIEW3D_PT_snappanel_toolshelf,
 )
 
 if __name__ == "__main__":  # only for live edit.
