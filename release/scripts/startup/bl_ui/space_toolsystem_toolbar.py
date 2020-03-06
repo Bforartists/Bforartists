c85c3f5e2fe2ff5fd7a4dc949092d3706c0ccbec
ReinerBforartists <reiner.prokein@posteo.de>
2020-03-06 11:32:43 +0100

    Active Tool panel - align checkboxes left #1396 - part 3

--- release/scripts/startup/bl_ui/space_toolsystem_toolbar.py
+++ release/scripts/startup/bl_ui/space_toolsystem_toolbar.py
@@ -515,7 +515,9 @@ class _defs_edit_mesh:
         def draw_settings(_context, layout, tool):
             props = tool.operator_properties("mesh.polybuild_face_at_cursor_move")
             props_macro = props.MESH_OT_polybuild_face_at_cursor
+            layout.use_property_split = False
             layout.prop(props_macro, "create_quads")
+            
         return dict(
             idname="builtin.poly_build",
             label="Poly Build",
