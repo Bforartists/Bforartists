Bforartists/Bforartists

commit aaba1ec23834d1bc3fc8df439c8d19f7159e98b6

Fix T73921: Eevee volume render test memory leak in Mantaflow
Fixed memory leak that showed up after the original issue (crash) had been fixed in 48cdf259934d. The fix ensures that light cache bakes free up GPU smoke textures and the smoke domain list correctly.

This commit also removes the workaround (3f381b097a66) that disabled light cache bakes for fluid objects.

source/blender/draw/engines/eevee/eevee_lightcache.c
@@ -777,6 +777,8 @@ static void eevee_lightbake_delete_resources(EEVEE_LightBake *lbake)
   if (!lbake->resource_only) {
     BLI_mutex_unlock(lbake->mutex);
   }
+
+  EEVEE_volumes_free_smoke_textures();
 }
 
 /* Cache as in draw cache not light cache. */

source/blender/draw/engines/eevee/eevee_volumes.c
@@ -178,8 +178,6 @@ void EEVEE_volumes_init(EEVEE_ViewLayerData *sldata, EEVEE_Data *vedata)
 
   const float *viewport_size = DRW_viewport_size_get();
 
-  BLI_listbase_clear(&e_data.smoke_domains);
-
   const int tile_size = scene_eval->eevee.volumetric_tile_size;
 
   /* Find Froxel Texture resolution. */

tests/python/eevee_render_tests.py
@@ -35,13 +35,6 @@ def setup():
         # mat.use_screen_refraction = True
         mat.use_sss_translucency = True
 
-    # Workaround for crash with Mantaflow (T73921).
-    use_light_cache_bake = True
-    for ob in bpy.data.objects:
-        for mod in ob.modifiers:
-            if mod.type == 'FLUID':
-                use_light_cache_bake = False
-
     cubemap = None
     grid = None
     # Does not work in edit mode
@@ -86,8 +79,7 @@ def setup():
     eevee.gi_visibility_resolution = '16'
     eevee.gi_irradiance_smoothing = 0
 
-    if use_light_cache_bake:
-        bpy.ops.scene.light_cache_bake()
+    bpy.ops.scene.light_cache_bake()
 
 
 # When run from inside Blender, render and exit.

commit 3467954ae39c625e2f932bc328390526e75a527e

Fix T74901: Smoke Simulation crashes on eevee and is not persistent on Cycles
Issue was introduced in aaba1ec23834 (fix for T73921).

source/blender/draw/engines/eevee/eevee_lightcache.c
@@ -777,8 +777,6 @@ static void eevee_lightbake_delete_resources(EEVEE_LightBake *lbake)
   if (!lbake->resource_only) {
     BLI_mutex_unlock(lbake->mutex);
   }
-
-  EEVEE_volumes_free_smoke_textures();
 }
 
 /* Cache as in draw cache not light cache. */
@@ -1347,6 +1345,9 @@ void EEVEE_lightbake_job(void *custom_data, short *stop, short *do_update, float
   }
 
   eevee_lightbake_delete_resources(lbake);
+
+  /* Free GPU smoke textures and the smoke domain list correctly: See also T73921.*/
+  EEVEE_volumes_free_smoke_textures();
 }
 
 /* This is to update the world irradiance and reflection contribution from