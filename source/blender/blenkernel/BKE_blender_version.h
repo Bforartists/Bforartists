1008ba7271f7139f2b098d0577d931d19e3eaa1f
ReinerBforartists <reiner.prokein@posteo.de>
2020-04-12 10:34:06 +0200

    Changed splashscreen and versioning number to Bforartists 2 1.0.0 RC

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -40,7 +40,7 @@
 /** Optionally set to 1,2,... for example to get alpha1 or rc2. */
 #define BLENDER_VERSION_CYCLE_NUMBER
 
-#define BFORARTISTS_VERSION 091
+#define BFORARTISTS_VERSION 100RC
 
 /** Defined in from blender.c */
 extern char versionstr[];
