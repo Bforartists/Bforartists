c89f7b5bd5ff33fd6520f23de344ac0dd7712601
ReinerBforartists <reiner.prokein@posteo.de>
2020-04-18 10:03:13 +0200

    Changed splash screen and versioning number to bforartists 2 version 2.0.0

--- release/scripts/startup/bl_operators/wm.py
+++ release/scripts/startup/bl_operators/wm.py
@@ -2553,7 +2553,7 @@ class WM_MT_splash(Menu):
 
         layout.separator()
         
-        layout.label(text = "Bforartists 2 1.0.0 Release Candidate is based on Blender 2.83 Alpha")
+        layout.label(text = "Bforartists 2.0.0 is based on Blender 2.83 Beta")
 
         layout.separator()
 
