Bforartists/Bforartists

commit 2b65c92718852267f1df580aaa8ea9541ec3f0c9

Fix extra undo step when switching object modes
Changing between modes would always add a user visible undo step
that set object mode.

Avoid storing this extra undo step on object mode switching.

source/blender/editors/object/object_edit.c
@@ -1408,28 +1408,34 @@ static int object_mode_set_exec(bContext *C, wmOperator *op)
     return OPERATOR_PASS_THROUGH;
   }
 
-  if (ob->mode != mode) {
-    /* we should be able to remove this call, each operator calls  */
-    ED_object_mode_compat_set(C, ob, mode, op->reports);
+  if (toggle == false) {
+    if (ob->mode != mode) {
+      if (mode != OB_MODE_OBJECT) {
+        /* Enter new mode. */
+        ED_object_mode_toggle(C, mode);
+      }
+      else {
+        ED_object_mode_compat_set(C, ob, mode, op->reports);
+      }
+    }
   }
+  else {
+    /* Exit current mode if it's not the mode we're setting */
+    if (mode != OB_MODE_OBJECT) {
+      /* Enter new mode. */
+      ED_object_mode_toggle(C, mode);
+    }
 
-  /* Exit current mode if it's not the mode we're setting */
-  if (mode != OB_MODE_OBJECT && (ob->mode != mode || toggle)) {
-    /* Enter new mode */
-    ED_object_mode_toggle(C, mode);
-  }
-
-  if (toggle) {
     /* Special case for Object mode! */
-    if (mode == OB_MODE_OBJECT && restore_mode == OB_MODE_OBJECT &&
-        ob->restore_mode != OB_MODE_OBJECT) {
+    if ((mode == OB_MODE_OBJECT) && (restore_mode == OB_MODE_OBJECT) &&
+        (ob->restore_mode != OB_MODE_OBJECT)) {
       ED_object_mode_toggle(C, ob->restore_mode);
     }
     else if (ob->mode == mode) {
       /* For toggling, store old mode so we know what to go back to */
       ob->restore_mode = restore_mode;
     }
-    else if (ob->restore_mode != OB_MODE_OBJECT && ob->restore_mode != mode) {
+    else if ((ob->restore_mode != OB_MODE_OBJECT) && (ob->restore_mode != mode)) {
       ED_object_mode_toggle(C, ob->restore_mode);
     }
   }

source/blender/editors/sculpt_paint/paint_vertex.c
@@ -1450,17 +1450,13 @@ static int wpaint_mode_toggle_exec(bContext *C, wmOperator *op)
   return OPERATOR_FINISHED;
 }
 
-/* for switching to/from mode */
-static bool paint_poll_test(bContext *C)
+static bool paint_mode_toggle_poll_test(bContext *C)
 {
   Object *ob = CTX_data_active_object(C);
   if (ob == NULL || ob->type != OB_MESH) {
     return 0;
   }
   if (!ob->data || ID_IS_LINKED(ob->data)) {
-    return 0;
-  }
-  if (CTX_data_edit_object(C)) {
     return 0;
   }
   return 1;
@@ -1476,7 +1472,7 @@ void PAINT_OT_weight_paint_toggle(wmOperatorType *ot)
 
   /* api callbacks */
   ot->exec = wpaint_mode_toggle_exec;
-  ot->poll = paint_poll_test;
+  ot->poll = paint_mode_toggle_poll_test;
 
   /* flags */
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
@@ -2683,7 +2679,7 @@ void PAINT_OT_vertex_paint_toggle(wmOperatorType *ot)
 
   /* api callbacks */
   ot->exec = vpaint_mode_toggle_exec;
-  ot->poll = paint_poll_test;
+  ot->poll = paint_mode_toggle_poll_test;
 
   /* flags */
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;

commit 0144bd1448083f6a1292ce98d9e53acfed41d46a

Fix T77217: Object mode toggle doesn't work
Toggling object mode to the previous wasn't working, also resolves a
case when toggling modes would cause an extra, unnecessary mode switch.
Own regression in 2b65c92718852.

source/blender/editors/object/object_edit.c
@@ -1403,7 +1403,6 @@ static int object_mode_set_exec(bContext *C, wmOperator *op)
   bool use_submode = STREQ(op->idname, "OBJECT_OT_mode_set_with_submode");
   Object *ob = CTX_data_active_object(C);
   eObjectMode mode = RNA_enum_get(op->ptr, "mode");
-  eObjectMode restore_mode = ob->mode;
   const bool toggle = RNA_boolean_get(op->ptr, "toggle");
 
   /* by default the operator assume is a mesh, but if gp object change mode */
@@ -1417,33 +1416,49 @@ static int object_mode_set_exec(bContext *C, wmOperator *op)
 
   if (toggle == false) {
     if (ob->mode != mode) {
-      if (mode != OB_MODE_OBJECT) {
-        /* Enter new mode. */
-        ED_object_mode_toggle(C, mode);
+      if (mode == OB_MODE_OBJECT) {
+        ED_object_mode_compat_set(C, ob, OB_MODE_OBJECT, op->reports);
       }
       else {
-        ED_object_mode_compat_set(C, ob, mode, op->reports);
+        /* Enter new mode. */
+        ED_object_mode_toggle(C, mode);
       }
     }
   }
   else {
-    /* Exit current mode if it's not the mode we're setting */
-    if (mode != OB_MODE_OBJECT) {
-      /* Enter new mode. */
-      ED_object_mode_toggle(C, mode);
-    }
-
+    const eObjectMode restore_mode = ob->mode;
     /* Special case for Object mode! */
-    if ((mode == OB_MODE_OBJECT) && (restore_mode == OB_MODE_OBJECT) &&
-        (ob->restore_mode != OB_MODE_OBJECT)) {
-      ED_object_mode_toggle(C, ob->restore_mode);
+    if (mode == OB_MODE_OBJECT) {
+      if (ob->mode != OB_MODE_OBJECT) {
+        /* Set object mode if the object is not already in object mode. */
+        if (ED_object_mode_compat_set(C, ob, OB_MODE_OBJECT, op->reports)) {
+          ob->restore_mode = restore_mode;
+        }
+      }
+      else {
+        if (ob->restore_mode != OB_MODE_OBJECT) {
+          ED_object_mode_toggle(C, ob->restore_mode);
+        }
+      }
     }
-    else if (ob->mode == mode) {
-      /* For toggling, store old mode so we know what to go back to */
-      ob->restore_mode = restore_mode;
-    }
-    else if ((ob->restore_mode != OB_MODE_OBJECT) && (ob->restore_mode != mode)) {
-      ED_object_mode_toggle(C, ob->restore_mode);
+    else {
+      if (ob->mode != mode) {
+        ED_object_mode_toggle(C, mode);
+        if (ob->mode == mode) {
+          /* For toggling, store old mode so we know what to go back to. */
+          ob->restore_mode = restore_mode;
+        }
+      }
+      else {
+        if (ob->restore_mode != OB_MODE_OBJECT) {
+          /* Toggle directly into the restore mode. */
+          ED_object_mode_toggle(C, ob->restore_mode);
+        }
+        else {
+          /* Enter new mode. */
+          ED_object_mode_toggle(C, mode);
+        }
+      }
     }
   }
 