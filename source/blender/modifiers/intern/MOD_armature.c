Bforartists/Bforartists

commit eed5f9a25d6662b5db37520c0f61165c65194594

Modifier: skip edit-mesh conversion for armature, curve & lattice
This is an improvement over 2.7x which converted edit-mesh to mesh
(CDDM) for all 3 modifiers.

Overall this increases performance in edit-mode by around 15-20%.

source/blender/modifiers/intern/MOD_armature.c
@@ -163,62 +163,47 @@ static void deformVerts(ModifierData *md,
 static void deformVertsEM(ModifierData *md,
                           const ModifierEvalContext *ctx,
                           struct BMEditMesh *em,
-                          Mesh *mesh,
+                          Mesh *UNUSED(mesh),
                           float (*vertexCos)[3],
                           int numVerts)
 {
   ArmatureModifierData *amd = (ArmatureModifierData *)md;
-  Mesh *mesh_src = MOD_deform_mesh_eval_get(ctx->object, em, mesh, NULL, numVerts, false, false);
-
-  /* TODO(Campbell): use edit-mode data only (remove this line). */
-  if (mesh_src != NULL) {
-    BKE_mesh_wrapper_ensure_mdata(mesh_src);
-  }
 
   MOD_previous_vcos_store(md, vertexCos); /* if next modifier needs original vertices */
 
-  BKE_armature_deform_coords_with_mesh(amd->object,
-                                       ctx->object,
-                                       vertexCos,
-                                       NULL,
-                                       numVerts,
-                                       amd->deformflag,
-                                       amd->vert_coords_prev,
-                                       amd->defgrp_name,
-                                       mesh_src);
+  BKE_armature_deform_coords_with_editmesh(amd->object,
+                                           ctx->object,
+                                           vertexCos,
+                                           NULL,
+                                           numVerts,
+                                           amd->deformflag,
+                                           amd->vert_coords_prev,
+                                           amd->defgrp_name,
+                                           em);
 
   /* free cache */
   MEM_SAFE_FREE(amd->vert_coords_prev);
-
-  if (mesh_src != mesh) {
-    BKE_id_free(NULL, mesh_src);
-  }
 }
 
 static void deformMatricesEM(ModifierData *md,
                              const ModifierEvalContext *ctx,
                              struct BMEditMesh *em,
-                             Mesh *mesh,
+                             Mesh *UNUSED(mesh),
                              float (*vertexCos)[3],
                              float (*defMats)[3][3],
                              int numVerts)
 {
   ArmatureModifierData *amd = (ArmatureModifierData *)md;
-  Mesh *mesh_src = MOD_deform_mesh_eval_get(ctx->object, em, mesh, NULL, numVerts, false, false);
 
-  BKE_armature_deform_coords_with_mesh(amd->object,
-                                       ctx->object,
-                                       vertexCos,
-                                       defMats,
-                                       numVerts,
-                                       amd->deformflag,
-                                       NULL,
-                                       amd->defgrp_name,
-                                       mesh_src);
-
-  if (mesh_src != mesh) {
-    BKE_id_free(NULL, mesh_src);
-  }
+  BKE_armature_deform_coords_with_editmesh(amd->object,
+                                           ctx->object,
+                                           vertexCos,
+                                           defMats,
+                                           numVerts,
+                                           amd->deformflag,
+                                           NULL,
+                                           amd->defgrp_name,
+                                           em);
 }
 
 static void deformMatrices(ModifierData *md,

source/blender/modifiers/intern/MOD_curve.c
@@ -34,6 +34,7 @@
 
 #include "BKE_context.h"
 #include "BKE_curve.h"
+#include "BKE_deform.h"
 #include "BKE_editmesh.h"
 #include "BKE_lib_id.h"
 #include "BKE_lib_query.h"
@@ -129,8 +130,9 @@ static void deformVerts(ModifierData *md,
   int defgrp_index = -1;
   MOD_get_vgroup(ctx->object, mesh_src, cmd->name, &dvert, &defgrp_index);
 
-  /* silly that defaxis and BKE_curve_deform_coords are off by 1
+  /* Silly that defaxis and BKE_curve_deform_coords are off by 1
    * but leave for now to save having to call do_versions */
+
   BKE_curve_deform_coords(cmd->object,
                           ctx->object,
                           vertexCos,
@@ -147,22 +149,41 @@ static void deformVerts(ModifierData *md,
 
 static void deformVertsEM(ModifierData *md,
                           const ModifierEvalContext *ctx,
-                          struct BMEditMesh *em,
-                          Mesh *mesh,
+                          BMEditMesh *em,
+                          Mesh *UNUSED(mesh),
                           float (*vertexCos)[3],
                           int numVerts)
 {
-  Mesh *mesh_src = MOD_deform_mesh_eval_get(ctx->object, em, mesh, NULL, numVerts, false, false);
+  CurveModifierData *cmd = (CurveModifierData *)md;
+  bool use_dverts = false;
+  int defgrp_index = -1;
 
-  /* TODO(Campbell): use edit-mode data only (remove this line). */
-  if (mesh_src != NULL) {
-    BKE_mesh_wrapper_ensure_mdata(mesh_src);
+  if (ctx->object->type == OB_MESH && cmd->name[0] != '\0') {
+    defgrp_index = BKE_object_defgroup_name_index(ctx->object, cmd->name);
+    if (defgrp_index != -1) {
+      use_dverts = true;
+    }
   }
 
-  deformVerts(md, ctx, mesh_src, vertexCos, numVerts);
-
-  if (!ELEM(mesh_src, NULL, mesh)) {
-    BKE_id_free(NULL, mesh_src);
+  if (use_dverts) {
+    BKE_curve_deform_coords_with_editmesh(cmd->object,
+                                          ctx->object,
+                                          vertexCos,
+                                          numVerts,
+                                          defgrp_index,
+                                          cmd->flag,
+                                          cmd->defaxis - 1,
+                                          em);
+  }
+  else {
+    BKE_curve_deform_coords(cmd->object,
+                            ctx->object,
+                            vertexCos,
+                            numVerts,
+                            NULL,
+                            defgrp_index,
+                            cmd->flag,
+                            cmd->defaxis - 1);
   }
 }
 

source/blender/modifiers/intern/MOD_lattice.c
@@ -130,23 +130,16 @@ static void deformVerts(ModifierData *md,
 static void deformVertsEM(ModifierData *md,
                           const ModifierEvalContext *ctx,
                           struct BMEditMesh *em,
-                          struct Mesh *mesh,
+                          struct Mesh *UNUSED(mesh),
                           float (*vertexCos)[3],
                           int numVerts)
 {
-  struct Mesh *mesh_src = MOD_deform_mesh_eval_get(
-      ctx->object, em, mesh, NULL, numVerts, false, false);
+  LatticeModifierData *lmd = (LatticeModifierData *)md;
 
-  /* TODO(Campbell): use edit-mode data only (remove this line). */
-  if (mesh_src != NULL) {
-    BKE_mesh_wrapper_ensure_mdata(mesh_src);
-  }
+  MOD_previous_vcos_store(md, vertexCos); /* if next modifier needs original vertices */
 
-  deformVerts(md, ctx, mesh_src, vertexCos, numVerts);
-
-  if (!ELEM(mesh_src, NULL, mesh)) {
-    BKE_id_free(NULL, mesh_src);
-  }
+  BKE_lattice_deform_coords_with_editmesh(
+      lmd->object, ctx->object, vertexCos, numVerts, lmd->flag, lmd->name, lmd->strength, em);
 }
 
 static void panel_draw(const bContext *C, Panel *panel)

source/blender/blenkernel/BKE_armature.h
@@ -28,6 +28,7 @@
 extern "C" {
 #endif
 
+struct BMEditMesh;
 struct Bone;
 struct Depsgraph;
 struct ListBase;
@@ -370,6 +371,16 @@ void BKE_armature_deform_coords_with_mesh(struct Object *ob_arm,
                                           float (*vert_coords_prev)[3],
                                           const char *defgrp_name,
                                           const struct Mesh *me_target);
+
+void BKE_armature_deform_coords_with_editmesh(struct Object *ob_arm,
+                                              struct Object *ob_target,
+                                              float (*vert_coords)[3],
+                                              float (*vert_deform_mats)[3][3],
+                                              int vert_coords_len,
+                                              int deformflag,
+                                              float (*vert_coords_prev)[3],
+                                              const char *defgrp_name,
+                                              struct BMEditMesh *em_target);
 
 /** \} */
 

source/blender/blenkernel/BKE_curve.h
@@ -29,6 +29,7 @@
 extern "C" {
 #endif
 
+struct BMEditMesh;
 struct BezTriple;
 struct Curve;
 struct Depsgraph;
@@ -316,6 +317,16 @@ void BKE_curve_deform_coords(struct Object *ob_curve,
                              const int defgrp_index,
                              const short flag,
                              const short defaxis);
+
+void BKE_curve_deform_coords_with_editmesh(Object *ob_curve,
+                                           Object *ob_target,
+                                           float (*vert_coords)[3],
+                                           const int vert_coords_len,
+                                           const int defgrp_index,
+                                           const short flag,
+                                           const short defaxis,
+                                           struct BMEditMesh *em_target);
+
 void BKE_curve_deform_co(struct Object *ob_curve,
                          struct Object *ob_target,
                          const float orco[3],

source/blender/blenkernel/BKE_lattice.h
@@ -30,6 +30,7 @@
 extern "C" {
 #endif
 
+struct BMEditMesh;
 struct BPoint;
 struct Depsgraph;
 struct Lattice;
@@ -123,6 +124,15 @@ void BKE_lattice_deform_coords_with_mesh(struct Object *ob_lattice,
                                          const char *defgrp_name,
                                          const float influence,
                                          const struct Mesh *me_target);
+
+void BKE_lattice_deform_coords_with_editmesh(struct Object *ob_lattice,
+                                             struct Object *ob_target,
+                                             float (*vert_coords)[3],
+                                             const int vert_coords_len,
+                                             const short flag,
+                                             const char *defgrp_name,
+                                             const float influence,
+                                             struct BMEditMesh *em_target);
 /** \} */
 
 #ifdef __cplusplus

source/blender/blenkernel/intern/armature_deform.c
@@ -48,6 +48,7 @@
 #include "BKE_action.h"
 #include "BKE_armature.h"
 #include "BKE_deform.h"
+#include "BKE_editmesh.h"
 #include "BKE_lattice.h"
 
 #include "DEG_depsgraph_build.h"
@@ -261,13 +262,17 @@ typedef struct ArmatureUserdata {
 
   float premat[4][4];
   float postmat[4][4];
+
+  /** Specific data types. */
+  struct {
+    int cd_dvert_offset;
+  } bmesh;
 } ArmatureUserdata;
 
-static void armature_vert_task(void *__restrict userdata,
-                               const int i,
-                               const TaskParallelTLS *__restrict UNUSED(tls))
+static void armature_vert_task_with_dvert(const ArmatureUserdata *data,
+                                          const int i,
+                                          const MDeformVert *dvert)
 {
-  const ArmatureUserdata *data = userdata;
   float(*const vert_coords)[3] = data->vert_coords;
   float(*const vert_deform_mats)[3][3] = data->vert_deform_mats;
   float(*const vert_coords_prev)[3] = data->vert_coords_prev;
@@ -276,7 +281,6 @@ static void armature_vert_task(void *__restrict userdata,
   const bool use_dverts = data->use_dverts;
   const int armature_def_nr = data->armature_def_nr;
 
-  const MDeformVert *dvert;
   DualQuat sumdq, *dq = NULL;
   bPoseChannel *pchan;
   float *co, dco[3];
@@ -298,27 +302,6 @@ static void armature_vert_task(void *__restrict userdata,
       zero_m3(summat);
       smat = summat;
     }
-  }
-
-  if (use_dverts || armature_def_nr != -1) {
-    if (data->me_target) {
-      BLI_assert(i < data->me_target->totvert);
-      if (data->me_target->dvert != NULL) {
-        dvert = data->me_target->dvert + i;
-      }
-      else {
-        dvert = NULL;
-      }
-    }
-    else if (data->dverts && i < data->dverts_len) {
-      dvert = data->dverts + i;
-    }
-    else {
-      dvert = NULL;
-    }
-  }
-  else {
-    dvert = NULL;
   }
 
   if (armature_def_nr != -1 && dvert) {
@@ -434,6 +417,51 @@ static void armature_vert_task(void *__restrict userdata,
   }
 }
 
+static void armature_vert_task(void *__restrict userdata,
+                               const int i,
+                               const TaskParallelTLS *__restrict UNUSED(tls))
+{
+  const ArmatureUserdata *data = userdata;
+  const MDeformVert *dvert;
+  if (data->use_dverts || data->armature_def_nr != -1) {
+    if (data->me_target) {
+      BLI_assert(i < data->me_target->totvert);
+      if (data->me_target->dvert != NULL) {
+        dvert = data->me_target->dvert + i;
+      }
+      else {
+        dvert = NULL;
+      }
+    }
+    else if (data->dverts && i < data->dverts_len) {
+      dvert = data->dverts + i;
+    }
+    else {
+      dvert = NULL;
+    }
+  }
+  else {
+    dvert = NULL;
+  }
+
+  armature_vert_task_with_dvert(data, i, dvert);
+}
+
+static void armature_vert_task_editmesh(void *__restrict userdata, MempoolIterData *iter)
+{
+  const ArmatureUserdata *data = userdata;
+  BMVert *v = (BMVert *)iter;
+  MDeformVert *dvert = BM_ELEM_CD_GET_VOID_P(v, data->bmesh.cd_dvert_offset);
+  armature_vert_task_with_dvert(data, BM_elem_index_get(v), dvert);
+}
+
+static void armature_vert_task_editmesh_no_dvert(void *__restrict userdata, MempoolIterData *iter)
+{
+  const ArmatureUserdata *data = userdata;
+  BMVert *v = (BMVert *)iter;
+  armature_vert_task_with_dvert(data, BM_elem_index_get(v), NULL);
+}
+
 static void armature_deform_coords_impl(Object *ob_arm,
                                         Object *ob_target,
                                         float (*vert_coords)[3],
@@ -443,6 +471,7 @@ static void armature_deform_coords_impl(Object *ob_arm,
                                         float (*vert_coords_prev)[3],
                                         const char *defgrp_name,
                                         const Mesh *me_target,
+                                        BMEditMesh *em_target,
                                         bGPDstroke *gps_target)
 {
   bArmature *arm = ob_arm->data;
@@ -456,6 +485,7 @@ static void armature_deform_coords_impl(Object *ob_arm,
   int i, dverts_len = 0; /* safety for vertexgroup overflow */
   bool use_dverts = false;
   int armature_def_nr;
+  int cd_dvert_offset = -1;
 
   /* in editmode, or not an armature */
   if (arm->edbo || (ob_arm->pose == NULL)) {
@@ -476,10 +506,12 @@ static void armature_deform_coords_impl(Object *ob_arm,
     defbase_len = BLI_listbase_count(&ob_target->defbase);
 
     if (ob_target->type == OB_MESH) {
-      Mesh *me = ob_target->data;
-      dverts = me->dvert;
-      if (dverts) {
-        dverts_len = me->totvert;
+      if (em_target == NULL) {
+        Mesh *me = ob_target->data;
+        dverts = me->dvert;
+        if (dverts) {
+          dverts_len = me->totvert;
+        }
       }
     }
     else if (ob_target->type == OB_LATTICE) {
@@ -501,7 +533,11 @@ static void armature_deform_coords_impl(Object *ob_arm,
   if (deformflag & ARM_DEF_VGROUP) {
     if (ELEM(ob_target->type, OB_MESH, OB_LATTICE, OB_GPENCIL)) {
       /* if we have a Mesh, only use dverts if it has them */
-      if (me_target) {
+      if (em_target) {
+        cd_dvert_offset = CustomData_get_offset(&em_target->bm->vdata, CD_MDEFORMVERT);
+        use_dverts = (cd_dvert_offset != -1);
+      }
+      else if (me_target) {
         use_dverts = (me_target->dvert != NULL);
       }
       else if (dverts) {
@@ -543,6 +579,10 @@ static void armature_deform_coords_impl(Object *ob_arm,
       .dverts_len = dverts_len,
       .pchan_from_defbase = pchan_from_defbase,
       .defbase_len = defbase_len,
+      .bmesh =
+          {
+              .cd_dvert_offset = cd_dvert_offset,
+          },
   };
 
   float obinv[4][4];
@@ -551,10 +591,25 @@ static void armature_deform_coords_impl(Object *ob_arm,
   mul_m4_m4m4(data.postmat, obinv, ob_arm->obmat);
   invert_m4_m4(data.premat, data.postmat);
 
-  TaskParallelSettings settings;
-  BLI_parallel_range_settings_defaults(&settings);
-  settings.min_iter_per_thread = 32;
-  BLI_task_parallel_range(0, vert_coords_len, &data, armature_vert_task, &settings);
+  if (em_target != NULL) {
+    /* While this could cause an extra loop over mesh data, in most cases this will
+     * have already been properly set. */
+    BM_mesh_elem_index_ensure(em_target->bm, BM_VERT);
+
+    if (use_dverts) {
+      BLI_task_parallel_mempool(em_target->bm->vpool, &data, armature_vert_task_editmesh, true);
+    }
+    else {
+      BLI_task_parallel_mempool(
+          em_target->bm->vpool, &data, armature_vert_task_editmesh_no_dvert, true);
+    }
+  }
+  else {
+    TaskParallelSettings settings;
+    BLI_parallel_range_settings_defaults(&settings);
+    settings.min_iter_per_thread = 32;
+    BLI_task_parallel_range(0, vert_coords_len, &data, armature_vert_task, &settings);
+  }
 
   if (pchan_from_defbase) {
     MEM_freeN(pchan_from_defbase);
@@ -580,6 +635,7 @@ void BKE_armature_deform_coords_with_gpencil_stroke(Object *ob_arm,
                               vert_coords_prev,
                               defgrp_name,
                               NULL,
+                              NULL,
                               gps_target);
 }
 
@@ -602,6 +658,30 @@ void BKE_armature_deform_coords_with_mesh(Object *ob_arm,
                               vert_coords_prev,
                               defgrp_name,
                               me_target,
+                              NULL,
+                              NULL);
+}
+
+void BKE_armature_deform_coords_with_editmesh(Object *ob_arm,
+                                              Object *ob_target,
+                                              float (*vert_coords)[3],
+                                              float (*vert_deform_mats)[3][3],
+                                              int vert_coords_len,
+                                              int deformflag,
+                                              float (*vert_coords_prev)[3],
+                                              const char *defgrp_name,
+                                              BMEditMesh *em_target)
+{
+  armature_deform_coords_impl(ob_arm,
+                              ob_target,
+                              vert_coords,
+                              vert_deform_mats,
+                              vert_coords_len,
+                              deformflag,
+                              vert_coords_prev,
+                              defgrp_name,
+                              NULL,
+                              em_target,
                               NULL);
 }
 

commit 2d0540b5783fa3afc49dbbe699845065e6b06bfe

Fix T79440: Deform modifiers fail in edit-mode when not first
Lattice, armature & curve only worked when an edit-mesh was passed in,
the mesh argument was being ignored.

Regression in eed5f9a25d666

source/blender/modifiers/intern/MOD_armature.c
@@ -165,10 +165,15 @@ static void deformVerts(ModifierData *md,
 static void deformVertsEM(ModifierData *md,
                           const ModifierEvalContext *ctx,
                           struct BMEditMesh *em,
-                          Mesh *UNUSED(mesh),
+                          Mesh *mesh,
                           float (*vertexCos)[3],
                           int numVerts)
 {
+  if (mesh != NULL) {
+    deformVerts(md, ctx, mesh, vertexCos, numVerts);
+    return;
+  }
+
   ArmatureModifierData *amd = (ArmatureModifierData *)md;
 
   MOD_previous_vcos_store(md, vertexCos); /* if next modifier needs original vertices */

source/blender/modifiers/intern/MOD_curve.c
@@ -150,10 +150,15 @@ static void deformVerts(ModifierData *md,
 static void deformVertsEM(ModifierData *md,
                           const ModifierEvalContext *ctx,
                           BMEditMesh *em,
-                          Mesh *UNUSED(mesh),
+                          Mesh *mesh,
                           float (*vertexCos)[3],
                           int numVerts)
 {
+  if (mesh != NULL) {
+    deformVerts(md, ctx, mesh, vertexCos, numVerts);
+    return;
+  }
+
   CurveModifierData *cmd = (CurveModifierData *)md;
   bool use_dverts = false;
   int defgrp_index = -1;

source/blender/modifiers/intern/MOD_lattice.c
@@ -130,10 +130,15 @@ static void deformVerts(ModifierData *md,
 static void deformVertsEM(ModifierData *md,
                           const ModifierEvalContext *ctx,
                           struct BMEditMesh *em,
-                          struct Mesh *UNUSED(mesh),
+                          struct Mesh *mesh,
                           float (*vertexCos)[3],
                           int numVerts)
 {
+  if (mesh != NULL) {
+    deformVerts(md, ctx, mesh, vertexCos, numVerts);
+    return;
+  }
+
   LatticeModifierData *lmd = (LatticeModifierData *)md;
 
   MOD_previous_vcos_store(md, vertexCos); /* if next modifier needs original vertices */