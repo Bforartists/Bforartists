Bforartists/Bforartists

commit d38cd8df435b4e8503e1813c54258c950cf0fd8f

UI Code Quality: Use derived struct for HSV Cube buttons
For the main rationale behind this design, see b58507cf3907. Further,
this removes users of uiBut.a1, which is a very ugly design
choice (hard to reason about).

Part of T74432.

source/blender/editors/interface/interface_handlers.c
@@ -5941,9 +5941,11 @@ static void clamp_axis_max_v3(float v[3], const float max)
   }
 }
 
-static void ui_rgb_to_color_picker_HSVCUBE_compat_v(uiBut *but, const float rgb[3], float hsv[3])
+static void ui_rgb_to_color_picker_HSVCUBE_compat_v(const uiButHSVCube *hsv_but,
+                                                    const float rgb[3],
+                                                    float hsv[3])
 {
-  if (but->a1 == UI_GRAD_L_ALT) {
+  if (hsv_but->gradient_type == UI_GRAD_L_ALT) {
     rgb_to_hsl_compat_v(rgb, hsv);
   }
   else {
@@ -5951,9 +5953,11 @@ static void ui_rgb_to_color_picker_HSVCUBE_compat_v(uiBut *but, const float rgb[
   }
 }
 
-static void ui_rgb_to_color_picker_HSVCUBE_v(uiBut *but, const float rgb[3], float hsv[3])
+static void ui_rgb_to_color_picker_HSVCUBE_v(const uiButHSVCube *hsv_but,
+                                             const float rgb[3],
+                                             float hsv[3])
 {
-  if (but->a1 == UI_GRAD_L_ALT) {
+  if (hsv_but->gradient_type == UI_GRAD_L_ALT) {
     rgb_to_hsl_v(rgb, hsv);
   }
   else {
@@ -5961,9 +5965,11 @@ static void ui_rgb_to_color_picker_HSVCUBE_v(uiBut *but, const float rgb[3], flo
   }
 }
 
-static void ui_color_picker_to_rgb_HSVCUBE_v(uiBut *but, const float hsv[3], float rgb[3])
+static void ui_color_picker_to_rgb_HSVCUBE_v(const uiButHSVCube *hsv_but,
+                                             const float hsv[3],
+                                             float rgb[3])
 {
-  if (but->a1 == UI_GRAD_L_ALT) {
+  if (hsv_but->gradient_type == UI_GRAD_L_ALT) {
     hsl_to_rgb_v(hsv, rgb);
   }
   else {
@@ -5978,6 +5984,7 @@ static bool ui_numedit_but_HSVCUBE(uiBut *but,
                                    const enum eSnapType snap,
                                    const bool shift)
 {
+  const uiButHSVCube *hsv_but = (uiButHSVCube *)but;
   ColorPicker *cpicker = but->custom_data;
   float *hsv = cpicker->color_data;
   float rgb[3];
@@ -5999,7 +6006,7 @@ static bool ui_numedit_but_HSVCUBE(uiBut *but,
   ui_but_v3_get(but, rgb);
   ui_scene_linear_to_color_picker_space(but, rgb);
 
-  ui_rgb_to_color_picker_HSVCUBE_compat_v(but, rgb, hsv);
+  ui_rgb_to_color_picker_HSVCUBE_compat_v(hsv_but, rgb, hsv);
 
   /* only apply the delta motion, not absolute */
   if (shift) {
@@ -6014,10 +6021,10 @@ static bool ui_numedit_but_HSVCUBE(uiBut *but,
 
     copy_v3_v3(hsvo, hsv);
 
-    ui_rgb_to_color_picker_HSVCUBE_compat_v(but, rgb, hsvo);
+    ui_rgb_to_color_picker_HSVCUBE_compat_v(hsv_but, rgb, hsvo);
 
     /* and original position */
-    ui_hsvcube_pos_from_vals(but, &rect_i, hsvo, &xpos, &ypos);
+    ui_hsvcube_pos_from_vals(hsv_but, &rect_i, hsvo, &xpos, &ypos);
 
     mx_fl = xpos - (data->dragstartx - mx_fl);
     my_fl = ypos - (data->dragstarty - my_fl);
@@ -6029,7 +6036,7 @@ static bool ui_numedit_but_HSVCUBE(uiBut *but,
   CLAMP(x, 0.0f, 1.0f);
   CLAMP(y, 0.0f, 1.0f);
 
-  switch ((int)but->a1) {
+  switch (hsv_but->gradient_type) {
     case UI_GRAD_SV:
       hsv[1] = x;
       hsv[2] = y;
@@ -6067,16 +6074,16 @@ static bool ui_numedit_but_HSVCUBE(uiBut *but,
   }
 
   if (snap != SNAP_OFF) {
-    if (ELEM((int)but->a1, UI_GRAD_HV, UI_GRAD_HS, UI_GRAD_H)) {
+    if (ELEM(hsv_but->gradient_type, UI_GRAD_HV, UI_GRAD_HS, UI_GRAD_H)) {
       ui_color_snap_hue(snap, &hsv[0]);
     }
   }
 
-  ui_color_picker_to_rgb_HSVCUBE_v(but, hsv, rgb);
+  ui_color_picker_to_rgb_HSVCUBE_v(hsv_but, hsv, rgb);
   ui_color_picker_to_scene_linear_space(but, rgb);
 
   /* clamp because with color conversion we can exceed range [#34295] */
-  if (but->a1 == UI_GRAD_V_ALT) {
+  if (hsv_but->gradient_type == UI_GRAD_V_ALT) {
     clamp_axis_max_v3(rgb, but->softmax);
   }
 
@@ -6161,6 +6168,7 @@ static void ui_ndofedit_but_HSVCUBE(uiBut *but,
 static int ui_do_but_HSVCUBE(
     bContext *C, uiBlock *block, uiBut *but, uiHandleButtonData *data, const wmEvent *event)
 {
+  const uiButHSVCube *hsv_but = (uiButHSVCube *)but;
   int mx, my;
 
   mx = event->x;
@@ -6199,7 +6207,7 @@ static int ui_do_but_HSVCUBE(
 #endif /* WITH_INPUT_NDOF */
     /* XXX hardcoded keymap check.... */
     if (event->type == EVT_BACKSPACEKEY && event->val == KM_PRESS) {
-      if (ELEM(but->a1, UI_GRAD_V_ALT, UI_GRAD_L_ALT)) {
+      if (ELEM(hsv_but->gradient_type, UI_GRAD_V_ALT, UI_GRAD_L_ALT)) {
         int len;
 
         /* reset only value */
@@ -6212,15 +6220,15 @@ static int ui_do_but_HSVCUBE(
           float *hsv = cpicker->color_data;
 
           RNA_property_float_get_default_array(&but->rnapoin, but->rnaprop, def);
-          ui_rgb_to_color_picker_HSVCUBE_v(but, def, def_hsv);
+          ui_rgb_to_color_picker_HSVCUBE_v(hsv_but, def, def_hsv);
 
           ui_but_v3_get(but, rgb);
-          ui_rgb_to_color_picker_HSVCUBE_compat_v(but, rgb, hsv);
+          ui_rgb_to_color_picker_HSVCUBE_compat_v(hsv_but, rgb, hsv);
 
           def_hsv[0] = hsv[0];
           def_hsv[1] = hsv[1];
 
-          ui_color_picker_to_rgb_HSVCUBE_v(but, def_hsv, rgb);
+          ui_color_picker_to_rgb_HSVCUBE_v(hsv_but, def_hsv, rgb);
           ui_but_v3_set(but, rgb);
 
           RNA_property_update(C, &but->rnapoin, but->rnaprop);

source/blender/editors/include/UI_interface.h
@@ -378,7 +378,7 @@ typedef enum {
 #define BUTTYPE (63 << 9)
 
 /** Gradient types, for color picker #UI_BTYPE_HSVCUBE etc. */
-enum {
+typedef enum eButGradientType {
   UI_GRAD_SV = 0,
   UI_GRAD_HV = 1,
   UI_GRAD_HS = 2,
@@ -388,7 +388,7 @@ enum {
 
   UI_GRAD_V_ALT = 9,
   UI_GRAD_L_ALT = 10,
-};
+} eButGradientType;
 
 /* Drawing
  *

source/blender/editors/interface/interface.c
@@ -3793,6 +3793,10 @@ static void ui_but_alloc_info(const eButType type,
       alloc_size = sizeof(uiButProgressbar);
       alloc_str = "uiButProgressbar";
       break;
+    case UI_BTYPE_HSVCUBE:
+      alloc_size = sizeof(uiButHSVCube);
+      alloc_str = "uiButHSVCube";
+      break;
     default:
       alloc_size = sizeof(uiBut);
       alloc_str = "uiBut";

source/blender/editors/interface/interface_intern.h
@@ -172,7 +172,6 @@ struct uiBut {
 
   /**
    * For #uiBut.type:
-   * - UI_BTYPE_HSVCUBE:      Use UI_GRAD_* values.
    * - UI_BTYPE_NUM:          Use to store RNA 'step' value, for dragging and click-step.
    * - UI_BTYPE_LABEL:        Use `(a1 == 1.0f)` to use a2 as a blending factor (imaginative!).
    * - UI_BTYPE_SCROLL:       Use as scroll size.
@@ -327,6 +326,12 @@ typedef struct uiButProgressbar {
   /* 0..1 range */
   float progress;
 } uiButProgressbar;
+
+typedef struct uiButHSVCube {
+  uiBut but;
+
+  eButGradientType gradient_type;
+} uiButHSVCube;
 
 /**
  * Additional, superimposed icon for a button, invoking an operator.
@@ -535,7 +540,7 @@ extern void ui_hsvcircle_vals_from_pos(
 extern void ui_hsvcircle_pos_from_vals(
     const ColorPicker *cpicker, const rcti *rect, const float *hsv, float *xpos, float *ypos);
 extern void ui_hsvcube_pos_from_vals(
-    const struct uiBut *but, const rcti *rect, const float *hsv, float *xp, float *yp);
+    const struct uiButHSVCube *hsv_but, const rcti *rect, const float *hsv, float *xp, float *yp);
 
 extern void ui_but_string_get_ex(uiBut *but,
                                  char *str,
@@ -777,7 +782,10 @@ extern void ui_draw_aligned_panel(struct uiStyle *style,
 extern void ui_draw_dropshadow(
     const rctf *rct, float radius, float aspect, float alpha, int select);
 
-void ui_draw_gradient(const rcti *rect, const float hsv[3], const int type, const float alpha);
+void ui_draw_gradient(const rcti *rect,
+                      const float hsv[3],
+                      const eButGradientType type,
+                      const float alpha);
 
 void ui_draw_but_TAB_outline(const rcti *rect,
                              float rad,

source/blender/editors/interface/interface_region_color_picker.c
@@ -369,6 +369,7 @@ static void ui_colorpicker_circle(uiBlock *block,
                                   ColorPicker *cpicker)
 {
   uiBut *bt;
+  uiButHSVCube *hsv_but;
 
   /* HS circle */
   bt = uiDefButR_prop(block,
@@ -392,91 +393,99 @@ static void ui_colorpicker_circle(uiBlock *block,
 
   /* value */
   if (U.color_picker_type == USER_CP_CIRCLE_HSL) {
-    bt = uiDefButR_prop(block,
-                        UI_BTYPE_HSVCUBE,
-                        0,
-                        "",
-                        PICKER_W + PICKER_SPACE,
-                        0,
-                        PICKER_BAR,
-                        PICKER_H,
-                        ptr,
-                        prop,
-                        -1,
-                        0.0,
-                        0.0,
-                        UI_GRAD_L_ALT,
-                        0,
-                        "Lightness");
-    UI_but_func_set(bt, ui_colorpicker_rna_cb, bt, NULL);
+    hsv_but = (uiButHSVCube *)uiDefButR_prop(block,
+                                             UI_BTYPE_HSVCUBE,
+                                             0,
+                                             "",
+                                             PICKER_W + PICKER_SPACE,
+                                             0,
+                                             PICKER_BAR,
+                                             PICKER_H,
+                                             ptr,
+                                             prop,
+                                             -1,
+                                             0.0,
+                                             0.0,
+                                             0,
+                                             0,
+                                             "Lightness");
+    hsv_but->gradient_type = UI_GRAD_L_ALT;
+    UI_but_func_set(&hsv_but->but, ui_colorpicker_rna_cb, &hsv_but->but, NULL);
   }
   else {
-    bt = uiDefButR_prop(block,
-                        UI_BTYPE_HSVCUBE,
-                        0,
-                        "",
-                        PICKER_W + PICKER_SPACE,
-                        0,
-                        PICKER_BAR,
-                        PICKER_H,
-                        ptr,
-                        prop,
-                        -1,
-                        0.0,
-                        0.0,
-                        UI_GRAD_V_ALT,
-                        0,
-                        TIP_("Value"));
-    UI_but_func_set(bt, ui_colorpicker_rna_cb, bt, NULL);
+    hsv_but = (uiButHSVCube *)uiDefButR_prop(block,
+                                             UI_BTYPE_HSVCUBE,
+                                             0,
+                                             "",
+                                             PICKER_W + PICKER_SPACE,
+                                             0,
+                                             PICKER_BAR,
+                                             PICKER_H,
+                                             ptr,
+                                             prop,
+                                             -1,
+                                             0.0,
+                                             0.0,
+                                             0,
+                                             0,
+                                             TIP_("Value"));
+    hsv_but->gradient_type = UI_GRAD_V_ALT;
+    UI_but_func_set(&hsv_but->but, ui_colorpicker_rna_cb, &hsv_but->but, NULL);
   }
-  bt->custom_data = cpicker;
+  hsv_but->but.custom_data = cpicker;
 }
 
-static void ui_colorpicker_square(
-    uiBlock *block, PointerRNA *ptr, PropertyRNA *prop, int type, ColorPicker *cpicker)
+static void ui_colorpicker_square(uiBlock *block,
+                                  PointerRNA *ptr,
+                                  PropertyRNA *prop,
+                                  eButGradientType type,
+                                  ColorPicker *cpicker)
 {
-  uiBut *bt;
-  int bartype = type + 3;
+  uiButHSVCube *hsv_but;
+
+  BLI_assert(type <= UI_GRAD_HS);
 
   /* HS square */
-  bt = uiDefButR_prop(block,
-                      UI_BTYPE_HSVCUBE,
-                      0,
-                      "",
-                      0,
-                      PICKER_BAR + PICKER_SPACE,
-                      PICKER_TOTAL_W,
-                      PICKER_H,
-                      ptr,
-                      prop,
-                      -1,
-                      0.0,
-                      0.0,
-                      type,
-                      0,
-                      TIP_("Color"));
-  UI_but_func_set(bt, ui_colorpicker_rna_cb, bt, NULL);
-  bt->custom_data = cpicker;
+  hsv_but = (uiButHSVCube *)uiDefButR_prop(block,
+                                           UI_BTYPE_HSVCUBE,
+                                           0,
+                                           "",
+                                           0,
+                                           PICKER_BAR + PICKER_SPACE,
+                                           PICKER_TOTAL_W,
+                                           PICKER_H,
+                                           ptr,
+                                           prop,
+                                           -1,
+                                           0.0,
+                                           0.0,
+                                           0,
+                                           0,
+                                           TIP_("Color"));
+  hsv_but->gradient_type = type;
+  UI_but_func_set(&hsv_but->but, ui_colorpicker_rna_cb, &hsv_but->but, NULL);
+  hsv_but->but.custom_data = cpicker;
 
   /* value */
-  bt = uiDefButR_prop(block,
-                      UI_BTYPE_HSVCUBE,
-                      0,
-                      "",
-                      0,
-                      0,
-                      PICKER_TOTAL_W,
-                      PICKER_BAR,
-                      ptr,
-                      prop,
-                      -1,
-                      0.0,
-                      0.0,
-                      bartype,
-                      0,
-                      TIP_("Value"));
-  UI_but_func_set(bt, ui_colorpicker_rna_cb, bt, NULL);
-  bt->custom_data = cpicker;
+  hsv_but = (uiButHSVCube *)uiDefButR_prop(block,
+                                           UI_BTYPE_HSVCUBE,
+                                           0,
+                                           "",
+                                           0,
+                                           0,
+                                           PICKER_TOTAL_W,
+                                           PICKER_BAR,
+                                           ptr,
+                                           prop,
+                                           -1,
+                                           0.0,
+                                           0.0,
+                                           0,
+                                           0,
+                                           TIP_("Value"));
+  hsv_but->gradient_type = type + 3;
+  UI_but_func_set(&hsv_but->but, ui_colorpicker_rna_cb, &hsv_but->but, NULL);
+  hsv_but->but.custom_data = cpicker;
 }
 
 /* a HS circle, V slider, rgb/hsv/hex sliders */

commit 11c54f433014d51f7bf7c7aef009b8aebf73ffb1

Fix compile error with WITH_INPUT_NDOF after last own commit
Caused by d38cd8df435b.

source/blender/editors/interface/interface_handlers.c
@@ -6096,23 +6096,23 @@ static bool ui_numedit_but_HSVCUBE(uiBut *but,
 }
 
 #ifdef WITH_INPUT_NDOF
-static void ui_ndofedit_but_HSVCUBE(uiBut *but,
+static void ui_ndofedit_but_HSVCUBE(uiButHSVCube *hsv_but,
                                     uiHandleButtonData *data,
                                     const wmNDOFMotionData *ndof,
                                     const enum eSnapType snap,
                                     const bool shift)
 {
-  ColorPicker *cpicker = but->custom_data;
+  ColorPicker *cpicker = hsv_but->but.custom_data;
   float *hsv = cpicker->color_data;
-  const float hsv_v_max = max_ff(hsv[2], but->softmax);
+  const float hsv_v_max = max_ff(hsv[2], hsv_but->but.softmax);
   float rgb[3];
   float sensitivity = (shift ? 0.15f : 0.3f) * ndof->dt;
 
-  ui_but_v3_get(but, rgb);
-  ui_scene_linear_to_color_picker_space(but, rgb);
-  ui_rgb_to_color_picker_HSVCUBE_compat_v(but, rgb, hsv);
+  ui_but_v3_get(&hsv_but->but, rgb);
+  ui_scene_linear_to_color_picker_space(&hsv_but->but, rgb);
+  ui_rgb_to_color_picker_HSVCUBE_compat_v(hsv_but, rgb, hsv);
 
-  switch ((int)but->a1) {
+  switch (hsv_but->gradient_type) {
     case UI_GRAD_SV:
       hsv[1] += ndof->rvec[2] * sensitivity;
       hsv[2] += ndof->rvec[0] * sensitivity;
@@ -6141,7 +6141,7 @@ static void ui_ndofedit_but_HSVCUBE(uiBut *but,
       /* exception only for value strip - use the range set in but->min/max */
       hsv[2] += ndof->rvec[0] * sensitivity;
 
-      CLAMP(hsv[2], but->softmin, but->softmax);
+      CLAMP(hsv[2], hsv_but->but.softmin, hsv_but->but.softmax);
       break;
     default:
       assert(!"invalid hsv type");
@@ -6149,7 +6149,7 @@ static void ui_ndofedit_but_HSVCUBE(uiBut *but,
   }
 
   if (snap != SNAP_OFF) {
-    if (ELEM((int)but->a1, UI_GRAD_HV, UI_GRAD_HS, UI_GRAD_H)) {
+    if (ELEM(hsv_but->gradient_type, UI_GRAD_HV, UI_GRAD_HS, UI_GRAD_H)) {
       ui_color_snap_hue(snap, &hsv[0]);
     }
   }
@@ -6157,18 +6157,18 @@ static void ui_ndofedit_but_HSVCUBE(uiBut *but,
   /* ndof specific: the changes above aren't clamping */
   hsv_clamp_v(hsv, hsv_v_max);
 
-  ui_color_picker_to_rgb_HSVCUBE_v(but, hsv, rgb);
-  ui_color_picker_to_scene_linear_space(but, rgb);
+  ui_color_picker_to_rgb_HSVCUBE_v(hsv_but, hsv, rgb);
+  ui_color_picker_to_scene_linear_space(&hsv_but->but, rgb);
 
   copy_v3_v3(data->vec, rgb);
-  ui_but_v3_set(but, data->vec);
+  ui_but_v3_set(&hsv_but->but, data->vec);
 }
 #endif /* WITH_INPUT_NDOF */
 
 static int ui_do_but_HSVCUBE(
     bContext *C, uiBlock *block, uiBut *but, uiHandleButtonData *data, const wmEvent *event)
 {
-  const uiButHSVCube *hsv_but = (uiButHSVCube *)but;
+  uiButHSVCube *hsv_but = (uiButHSVCube *)but;
   int mx, my;
 
   mx = event->x;
@@ -6197,7 +6197,7 @@ static int ui_do_but_HSVCUBE(
       const wmNDOFMotionData *ndof = event->customdata;
       const enum eSnapType snap = ui_event_to_snap(event);
 
-      ui_ndofedit_but_HSVCUBE(but, data, ndof, snap, event->shift != 0);
+      ui_ndofedit_but_HSVCUBE(hsv_but, data, ndof, snap, event->shift != 0);
 
       button_activate_state(C, but, BUTTON_STATE_EXIT);
       ui_apply_but(C, but->block, but, data, true);