blender/blender

commit e4a50e3f47de157b4213062d1dea9ff5c1ab0104

2.5 filebrowser * fix crash when filebrowser tried to invoke the file_exec or file_cancel in cases no operator was set.

source/blender/editors/space_file/file_ops.c
@@ -526,6 +526,16 @@ int file_cancel_exec(bContext *C, wmOperator *unused)
 	return OPERATOR_FINISHED;
 }
 
+int file_operator_poll(bContext *C)
+{
+	int poll = ED_operator_file_active(C);
+	SpaceFile *sfile= CTX_wm_space_file(C);
+
+	if (!sfile->op) poll= 0;
+
+	return poll;
+}
+
 void FILE_OT_cancel(struct wmOperatorType *ot)
 {
 	/* identifiers */
@@ -534,7 +544,7 @@ void FILE_OT_cancel(struct wmOperatorType *ot)
 	
 	/* api callbacks */
 	ot->exec= file_cancel_exec;
-	ot->poll= ED_operator_file_active;
+	ot->poll= file_operator_poll;
 }
 
 /* sends events now, so things get handled on windowqueue level */
@@ -612,7 +622,7 @@ void FILE_OT_execute(struct wmOperatorType *ot)
 	
 	/* api callbacks */
 	ot->exec= file_exec;
-	ot->poll= ED_operator_file_active; /* <- important, handler is on window level */
+	ot->poll= file_operator_poll; 
 }
 
 

Bforartists/Bforartists

commit 2090991df84936fd11f2fb276b5c43c58759a07f

Fix T79989: File browser `Return` doesn't open a directory
Regression in e4a50e3f47de1.

source/blender/editors/space_file/file_ops.c
@@ -1722,7 +1722,11 @@ void FILE_OT_execute(struct wmOperatorType *ot)
   /* api callbacks */
   ot->invoke = file_exec_invoke;
   ot->exec = file_exec;
-  ot->poll = file_operator_poll;
+  /* Important since handler is on window level.
+   *
+   * Avoid using #file_operator_poll since this is also used for entering directories
+   * which is used even when the file manager doesn't have an operator. */
+  ot->poll = ED_operator_file_active;
 
   /* properties */
   prop = RNA_def_boolean(ot->srna,