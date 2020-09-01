02933c4f67906cef3492f3df51d0d7bf1267db2f
ReinerBforartists <reiner.prokein@posteo.de>
2020-09-01 10:34:59 +0200

    changed splahs screen and versioning info to bforartists 2 2.5.0

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -37,7 +37,7 @@ extern "C" {
 /** Blender release cycle stage: alpha/beta/rc/release. */
 #define BLENDER_VERSION_CYCLE alpha
 
-#define BFORARTISTS_VERSION 24
+#define BFORARTISTS_VERSION 25
 /* Blender patch version for bugfix releases. */
 #define BFORARTISTS_VERSION_PATCH 0
 /** Blender release cycle stage: alpha/beta/rc/release. */
