Bforartists/Bforartists

commit 03b0883abae51fa56aa2d3e419a03d67b8b8484f

Use DrawManager for Image/UV Editor
This project moves the current UV/Image editor drawing to the draw manager.
Why would we do this:

**Performance**:

Current implementation would draw each texel per time. Multiple texels could be
drawn per pixel what would overwrite the previous result. You can notice this
when working with large textures. Repeat image drawing made this visible by
drawing for a small period of time and stop drawing the rest. Now the rendering
is fast and all repeated images are drawn.

**Alpha drawing**:

Current implementation would draw directly in display space. Giving incorrect
results when displaying alpha transparent images.
This addresses {T52680}, {T74709}, {T79518}
The image editor now can show emission only colors. See {D8234} for
examples.

**Current Limitations**

Using images that are larger than supported by your GPU are resized (eg larger
than 16000x16000 are resized to 8k). This leaves some blurring artifacts. It is
a low priority to add support back of displaying individual pixels of huge
images. There is a design task {T80113} with more detail.

**Implementation overview**
Introduced an Image Engine in the draw module. this engine is responsible for
drawing the texture in the main area of the UV/Image editor. The overlay engine
has a edit_uv overlay which is responsible to draw the UV's, shadows and
overlays specifically for the UV Image editor. The background + checker pattern
is drawn by the overlay_background.

The patch will allow us to share overlays between the 3d viewport and UV/Image
editor more easily. In most cases we just need to switch the `pos` with the `u`
attribute in the vertex shader.

The project can be activated in the user preferences as experimental features.
In a later commit this will be reversed.

Reviewed By: Clément Foucault

Differential Revision: https://developer.blender.org/D8234

source/blender/draw/engines/overlay/shaders/edit_uv_edges_vert.glsl
@@ -1,0 +1,32 @@
+#pragma BLENDER_REQUIRE(common_globals_lib.glsl)
+#pragma BLENDER_REQUIRE(common_view_lib.glsl)
+
+in vec3 pos;
+in vec2 u;
+in int flag;
+
+out float selectionFac;
+noperspective out vec2 stipplePos;
+flat out vec2 stippleStart;
+
+void main()
+{
+  vec3 world_pos = point_object_to_world(vec3(u, 0.0));
+  gl_Position = point_world_to_ndc(world_pos);
+  /* Snap vertices to the pixel grid to reduce artifacts. */
+  vec2 half_viewport_res = sizeViewport.xy * 0.5;
+  vec2 half_pixel_offset = sizeViewportInv * 0.5;
+  gl_Position.xy = floor(gl_Position.xy * half_viewport_res) / half_viewport_res +
+                   half_pixel_offset;
+
+  bool is_select = (flag & VERT_UV_SELECT) != 0;
+  selectionFac = is_select ? 1.0 : 0.0;
+  /* Move selected edges to the top 
+   * Vertices are between 0.0 and 0.2, Edges between 0.2 and 0.4
+   * actual pixels are at 0.75, 1.0 is used for the background. */
+  float depth = is_select ? 0.25 : 0.35;
+  gl_Position.z = depth;
+
+  /* Avoid precision loss. */
+  stippleStart = stipplePos = 500.0 + 500.0 * (gl_Position.xy / gl_Position.w);
+}

source/blender/draw/engines/overlay/shaders/edit_uv_face_dots_vert.glsl
@@ -1,0 +1,18 @@
+#pragma BLENDER_REQUIRE(common_globals_lib.glsl)
+#pragma BLENDER_REQUIRE(common_view_lib.glsl)
+
+uniform float pointSize;
+
+in vec2 u;
+in int flag;
+
+flat out vec4 finalColor;
+
+void main()
+{
+  vec3 world_pos = point_object_to_world(vec3(u, 0.0));
+  gl_Position = point_world_to_ndc(world_pos);
+
+  finalColor = ((flag & FACE_UV_SELECT) != 0) ? colorVertexSelect : vec4(colorWire.rgb, 1.0);
+  gl_PointSize = pointSize;
+}

source/blender/draw/engines/overlay/shaders/edit_uv_faces_vert.glsl
@@ -1,0 +1,22 @@
+#pragma BLENDER_REQUIRE(common_globals_lib.glsl)
+#pragma BLENDER_REQUIRE(common_view_lib.glsl)
+
+uniform float uvOpacity;
+
+in vec2 u;
+in int flag;
+
+flat out vec4 finalColor;
+
+void main()
+{
+  vec3 world_pos = point_object_to_world(vec3(u, 0.0));
+  gl_Position = point_world_to_ndc(world_pos);
+
+  bool is_selected = (flag & FACE_UV_SELECT) != 0;
+  bool is_active = (flag & FACE_UV_ACTIVE) != 0;
+
+  finalColor = (is_selected) ? colorFaceSelect : colorFace;
+  finalColor = (is_active) ? colorEditMeshActive : finalColor;
+  finalColor.a *= uvOpacity;
+}

source/blender/draw/engines/overlay/shaders/edit_uv_verts_vert.glsl
@@ -1,0 +1,44 @@
+#pragma BLENDER_REQUIRE(common_globals_lib.glsl)
+#pragma BLENDER_REQUIRE(common_view_lib.glsl)
+
+uniform float pointSize;
+uniform float outlineWidth;
+
+in vec2 u;
+in int flag;
+
+out vec4 fillColor;
+out vec4 outlineColor;
+out vec4 radii;
+
+/* TODO Theme? */
+const vec4 pinned_col = vec4(1.0, 0.0, 0.0, 1.0);
+
+void main()
+{
+  bool is_selected = (flag & (VERT_UV_SELECT | FACE_UV_SELECT)) != 0;
+  bool is_pinned = (flag & VERT_UV_PINNED) != 0;
+  vec4 deselect_col = (is_pinned) ? pinned_col : vec4(colorWire.rgb, 1.0);
+  fillColor = (is_selected) ? colorVertexSelect : deselect_col;
+  outlineColor = (is_pinned) ? pinned_col : vec4(fillColor.rgb, 0.0);
+
+  vec3 world_pos = point_object_to_world(vec3(u, 0.0));
+  /* Move selected vertices to the top
+   * Vertices are between 0.0 and 0.2, Edges between 0.2 and 0.4
+   * actual pixels are at 0.75, 1.0 is used for the background. */
+  float depth = is_selected ? 0.05 : 0.15;
+  gl_Position = vec4(point_world_to_ndc(world_pos).xy, depth, 1.0);
+  gl_PointSize = pointSize;
+
+  // calculate concentric radii in pixels
+  float radius = 0.5 * pointSize;
+
+  // start at the outside and progress toward the center
+  radii[0] = radius;
+  radii[1] = radius - 1.0;
+  radii[2] = radius - outlineWidth;
+  radii[3] = radius - outlineWidth - 1.0;
+
+  // convert to PointCoord units
+  radii /= pointSize;
+}

release/datafiles/userdef/userdef_default_theme.c
@@ -714,6 +714,7 @@ const bTheme U_theme_default = {
     .paint_curve_pivot = RGBA(0xff7f7f7f),
     .paint_curve_handle = RGBA(0x7fff7f7f),
     .metadatatext = RGBA(0xffffffff),
+    .grid = RGBA(0x505050ff),
   },
   .space_text = {
     .back = RGBA(0x30303000),

release/scripts/startup/bl_ui/space_userpref.py
@@ -2176,6 +2176,7 @@ class USERPREF_PT_experimental_prototypes(ExperimentalPanel, Panel):
         self._draw_items(
             context, (
                 ({"property": "use_new_hair_type"}, "T68981"),
+                ({"property": "use_drw_image_editor"}, "T67530"),
             ),
         )
 

source/blender/blenkernel/intern/image_gpu.c
@@ -272,6 +272,13 @@ static GPUTexture *image_get_gpu_texture(Image *ima,
    * context and might as well ensure we have as much space free as possible. */
   gpu_free_unused_buffers();
 
+  /* Free GPU textures when requesting a different render pass/layer. */
+  if (ima->gpu_pass != iuser->pass || ima->gpu_layer != iuser->layer) {
+    ima->gpu_pass = iuser->pass;
+    ima->gpu_layer = iuser->layer;
+    ima->gpuflag |= IMA_GPU_REFRESH;
+  }
+
   /* currently, gpu refresh tagging is used by ima sequences */
   if (ima->gpuflag & IMA_GPU_REFRESH) {
     image_free_gpu(ima, true);
@@ -282,7 +289,10 @@ static GPUTexture *image_get_gpu_texture(Image *ima,
   BKE_image_tag_time(ima);
 
   /* Test if we already have a texture. */
-  GPUTexture **tex = get_image_gpu_texture_ptr(ima, textarget, iuser ? iuser->multiview_eye : 0);
+  const int current_view = iuser ? ((iuser->flag & IMA_SHOW_STEREO) != 0 ? iuser->multiview_eye :
+                                                                           iuser->view) :
+                                   0;
+  GPUTexture **tex = get_image_gpu_texture_ptr(ima, textarget, current_view);
   if (*tex) {
     return *tex;
   }

source/blender/blenloader/intern/versioning_userdef.c
@@ -233,6 +233,8 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
      * the outliner's, and it's less disruptive to just copy them. */
     copy_v4_v4_uchar(btheme->space_file.back, btheme->space_outliner.back);
     copy_v4_v4_uchar(btheme->space_file.row_alternate, btheme->space_outliner.row_alternate);
+
+    FROM_DEFAULT_V4_UCHAR(space_image.grid);
   }
 
 #undef FROM_DEFAULT_V4_UCHAR

commit 0ec6e1ff79cf5542b6ea4b172e66a0b281766376

Fix T80800: Active UVMap Drawing
Regression introduced by D8234. In stead of using the active uv map, the
render uv map was drawn. This change will use the active uv map.

source/blender/draw/engines/overlay/shaders/edit_uv_edges_vert.glsl
@@ -2,7 +2,7 @@
 #pragma BLENDER_REQUIRE(common_view_lib.glsl)
 
 in vec3 pos;
-in vec2 u;
+in vec2 au;
 in int flag;
 
 out float selectionFac;
@@ -11,7 +11,7 @@ flat out vec2 stippleStart;
 
 void main()
 {
-  vec3 world_pos = point_object_to_world(vec3(u, 0.0));
+  vec3 world_pos = point_object_to_world(vec3(au, 0.0));
   gl_Position = point_world_to_ndc(world_pos);
   /* Snap vertices to the pixel grid to reduce artifacts. */
   vec2 half_viewport_res = sizeViewport.xy * 0.5;

source/blender/draw/engines/overlay/shaders/edit_uv_face_dots_vert.glsl
@@ -3,14 +3,14 @@
 
 uniform float pointSize;
 
-in vec2 u;
+in vec2 au;
 in int flag;
 
 flat out vec4 finalColor;
 
 void main()
 {
-  vec3 world_pos = point_object_to_world(vec3(u, 0.0));
+  vec3 world_pos = point_object_to_world(vec3(au, 0.0));
   gl_Position = point_world_to_ndc(world_pos);
 
   finalColor = ((flag & FACE_UV_SELECT) != 0) ? colorVertexSelect : vec4(colorWire.rgb, 1.0);

source/blender/draw/engines/overlay/shaders/edit_uv_faces_vert.glsl
@@ -3,14 +3,14 @@
 
 uniform float uvOpacity;
 
-in vec2 u;
+in vec2 au;
 in int flag;
 
 flat out vec4 finalColor;
 
 void main()
 {
-  vec3 world_pos = point_object_to_world(vec3(u, 0.0));
+  vec3 world_pos = point_object_to_world(vec3(au, 0.0));
   gl_Position = point_world_to_ndc(world_pos);
 
   bool is_selected = (flag & FACE_UV_SELECT) != 0;

source/blender/draw/engines/overlay/shaders/edit_uv_verts_vert.glsl
@@ -4,7 +4,7 @@
 uniform float pointSize;
 uniform float outlineWidth;
 
-in vec2 u;
+in vec2 au;
 in int flag;
 
 out vec4 fillColor;
@@ -22,7 +22,7 @@ void main()
   fillColor = (is_selected) ? colorVertexSelect : deselect_col;
   outlineColor = (is_pinned) ? pinned_col : vec4(fillColor.rgb, 0.0);
 
-  vec3 world_pos = point_object_to_world(vec3(u, 0.0));
+  vec3 world_pos = point_object_to_world(vec3(au, 0.0));
   /* Move selected vertices to the top
    * Vertices are between 0.0 and 0.2, Edges between 0.2 and 0.4
    * actual pixels are at 0.75, 1.0 is used for the background. */