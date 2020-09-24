bf38e6e0b023c1cabaa2acade267b05bdd3d3f14
ReinerBforartists <reiner.prokein@posteo.de>
2020-09-24 09:04:57 +0200

    changed splash screen and versioning number to bforartist 2 version 2.5.1

--- release/scripts/startup/bl_operators/wm.py
+++ release/scripts/startup/bl_operators/wm.py
@@ -2576,7 +2576,7 @@ class WM_MT_splash(Menu):
 
         layout.separator()
 
-        layout.label(text = "Bforartists 2.5.0 is based on Blender 2.91 Alpha")
+        layout.label(text = "Bforartists 2.5.1 is based on Blender 2.91 Alpha")
 
         layout.separator()
 
