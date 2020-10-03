Bforartists/Bforartists

commit 9c9fd994b20d9d7e91ab22974e8918e9c131d0a5

Cleanup: Remove duplicated View2D zoom polling logic
`view_zoomdrag_init()` would perform a number of sanity checks that
`view_zoom_poll()` already executed. The design of operators forsees that
(non-expensive) context sanity checks are done by the `poll()` callbacks, and
the execution callbacks can then just assume that happened. No reason to be
overly pedantic, we don't do that elsewhere either.

Note that this code is from the earliest days of an operator design. So it's
not surprising that it wasn't using the design "properly".

source/blender/editors/interface/view2d_ops.c
@@ -807,35 +807,6 @@ static void view_zoom_axis_lock_defaults(bContext *C, bool r_do_zoom_xy[2])
   }
 }
 
-/* initialize panning customdata */
-static bool view_zoomdrag_init(bContext *C, wmOperator *op)
-{
-  ARegion *region = CTX_wm_region(C);
-  v2dViewZoomData *vzd;
-  View2D *v2d;
-
-  /* regions now have v2d-data by default, so check for region */
-  if (region == NULL) {
-    return false;
-  }
-  v2d = &region->v2d;
-
-  /* check that 2d-view is zoomable */
-  if ((v2d->keepzoom & V2D_LOCKZOOM_X) && (v2d->keepzoom & V2D_LOCKZOOM_Y)) {
-    return false;
-  }
-
-  /* set custom-data for operator */
-  vzd = MEM_callocN(sizeof(v2dViewZoomData), "v2dViewZoomData");
-  op->customdata = vzd;
-
-  /* set pointers to owners */
-  vzd->v2d = v2d;
-  vzd->region = region;
-
-  return true;
-}
-
 /* check if step-zoom can be applied */
 static bool view_zoom_poll(bContext *C)
 {
@@ -861,6 +832,21 @@ static bool view_zoom_poll(bContext *C)
 
   /* view is zoomable */
   return true;
+}
+
+/* initialize panning customdata */
+static void view_zoomdrag_init(bContext *C, wmOperator *op)
+{
+  /* Should've been checked before. */
+  BLI_assert(view_zoom_poll(C));
+
+  /* set custom-data for operator */
+  v2dViewZoomData *vzd = MEM_callocN(sizeof(v2dViewZoomData), "v2dViewZoomData");
+  op->customdata = vzd;
+
+  /* set pointers to owners */
+  vzd->region = CTX_wm_region(C);
+  vzd->v2d = &vzd->region->v2d;
 }
 
 /* apply transform to view (i.e. adjust 'cur' rect) */
@@ -1024,9 +1010,7 @@ static int view_zoomin_invoke(bContext *C, wmOperator *op, const wmEvent *event)
 {
   v2dViewZoomData *vzd;
 
-  if (!view_zoomdrag_init(C, op)) {
-    return OPERATOR_PASS_THROUGH;
-  }
+  view_zoomdrag_init(C, op);
 
   vzd = op->customdata;
 
@@ -1092,9 +1076,7 @@ static int view_zoomout_invoke(bContext *C, wmOperator *op, const wmEvent *event
 {
   v2dViewZoomData *vzd;
 
-  if (!view_zoomdrag_init(C, op)) {
-    return OPERATOR_PASS_THROUGH;
-  }
+  view_zoomdrag_init(C, op);
 
   vzd = op->customdata;
 
@@ -1255,10 +1237,7 @@ static void view_zoomdrag_cancel(bContext *C, wmOperator *op)
 /* for 'redo' only, with no user input */
 static int view_zoomdrag_exec(bContext *C, wmOperator *op)
 {
-  if (!view_zoomdrag_init(C, op)) {
-    return OPERATOR_PASS_THROUGH;
-  }
-
+  view_zoomdrag_init(C, op);
   view_zoomdrag_apply(C, op);
   view_zoomdrag_exit(C, op);
   return OPERATOR_FINISHED;
@@ -1272,9 +1251,7 @@ static int view_zoomdrag_invoke(bContext *C, wmOperator *op, const wmEvent *even
   View2D *v2d;
 
   /* set up customdata */
-  if (!view_zoomdrag_init(C, op)) {
-    return OPERATOR_PASS_THROUGH;
-  }
+  view_zoomdrag_init(C, op);
 
   vzd = op->customdata;
   v2d = vzd->v2d;

commit 9f4cf03b9982de90c4d950e9d83984e2a602c3e7

Fix compile error when compiling with WITH_INPUT_NDOF after View2D changes
Actually, fixes compile error and an incorrect poll.

Caused by 9c9fd994b20d and ab7c564c747e.
The poll has to perfrom the `view_zoom_poll()` logic to ensure correct context.

source/blender/editors/interface/view2d_ops.c
@@ -1635,25 +1635,23 @@ static int view2d_ndof_invoke(bContext *C, wmOperator *op, const wmEvent *event)
   }
 
   if (has_zoom) {
-    if (view_zoomdrag_init(C, op)) {
-      v2dViewZoomData *vzd;
-      float zoom_factor = zoom_sensitivity * ndof->dt * -ndof->tvec[2];
+    float zoom_factor = zoom_sensitivity * ndof->dt * -ndof->tvec[2];
 
-      bool do_zoom_xy[2];
+    bool do_zoom_xy[2];
 
-      if (U.ndof_flag & NDOF_ZOOM_INVERT) {
-        zoom_factor = -zoom_factor;
-      }
-
-      view_zoom_axis_lock_defaults(C, do_zoom_xy);
-
-      vzd = op->customdata;
-
-      view_zoomstep_apply_ex(
-          C, vzd, false, do_zoom_xy[0] ? zoom_factor : 0.0f, do_zoom_xy[1] ? zoom_factor : 0.0f);
-
-      view_zoomstep_exit(op);
+    if (U.ndof_flag & NDOF_ZOOM_INVERT) {
+      zoom_factor = -zoom_factor;
     }
+
+    view_zoom_axis_lock_defaults(C, do_zoom_xy);
+
+    view_zoomdrag_init(C, op);
+
+    v2dViewZoomData *vzd = op->customdata;
+    view_zoomstep_apply_ex(
+        C, vzd, do_zoom_xy[0] ? zoom_factor : 0.0f, do_zoom_xy[1] ? zoom_factor : 0.0f);
+
+    view_zoomstep_exit(op);
   }
 
   return OPERATOR_FINISHED;
@@ -1668,7 +1666,7 @@ static void VIEW2D_OT_ndof(wmOperatorType *ot)
 
   /* api callbacks */
   ot->invoke = view2d_ndof_invoke;
-  ot->poll = view2d_poll;
+  ot->poll = view_zoom_poll;
 
   /* flags */
   ot->flag = OPTYPE_LOCK_BYPASS;