Bforartists/Bforartists

commit e97498be408c72c7d0d60754a7f20eacabdfde42

PyAPI: throw exception when cycles is given an invalid device name

intern/cycles/blender/blender_python.cpp
@@ -410,6 +410,11 @@ static PyObject *available_devices_func(PyObject * /*self*/, PyObject *args)
   }
 
   DeviceType type = Device::type_from_string(type_name);
+  if ((type == DEVICE_NONE) && (type_name[0] != '\0')) {
+    PyErr_Format(PyExc_ValueError, "Device \"%s\" not known.", type_name);
+    return NULL;
+  }
+
   uint mask = (type == DEVICE_NONE) ? DEVICE_MASK_ALL : DEVICE_MASK(type);
   mask |= DEVICE_MASK_CPU;
 

commit ba634a67086d7364f2efb557a70c83893c1e1be4

Fix T81806: Cycles error when GPU device_type is NONE
Own regression in e97498be408c7

intern/cycles/blender/blender_python.cpp
@@ -410,7 +410,8 @@ static PyObject *available_devices_func(PyObject * /*self*/, PyObject *args)
   }
 
   DeviceType type = Device::type_from_string(type_name);
-  if ((type == DEVICE_NONE) && (type_name[0] != '\0')) {
+  /* "NONE" is defined by the add-on, see: `CyclesPreferences.get_device_types`. */
+  if ((type == DEVICE_NONE) && (strcmp(type_name, "NONE") != 0)) {
     PyErr_Format(PyExc_ValueError, "Device \"%s\" not known.", type_name);
     return NULL;
   }