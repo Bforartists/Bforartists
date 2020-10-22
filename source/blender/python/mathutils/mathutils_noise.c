Bforartists/Bforartists

commit aece069e0ed9f52d68c3d98249185cf85693b0f8

Cleanup: use post increment/decrement
When the result isn't used, prefer post increment/decrement
(already used nearly everywhere in Blender).

source/blender/python/mathutils/mathutils_noise.c
@@ -140,11 +140,11 @@ static void next_state(void)
   left = N;
   next = state;
 
-  for (j = N - M + 1; --j; p++) {
+  for (j = N - M + 1; j--; p++) {
     *p = p[M] ^ TWIST(p[0], p[1]);
   }
 
-  for (j = M; --j; p++) {
+  for (j = M; j--; p++) {
     *p = p[M - N] ^ TWIST(p[0], p[1]);
   }
 

commit b904a8bd91d4a6698532751ef642c521e01707ae

Fix out of bounds array access in mathutils.noise
Regression in aece069e0ed9f.

source/blender/python/mathutils/mathutils_noise.c
@@ -140,11 +140,11 @@ static void next_state(void)
   left = N;
   next = state;
 
-  for (j = N - M + 1; j--; p++) {
+  for (j = N - M + 1; --j; p++) {
     *p = p[M] ^ TWIST(p[0], p[1]);
   }
 
-  for (j = M; j--; p++) {
+  for (j = M; --j; p++) {
     *p = p[M - N] ^ TWIST(p[0], p[1]);
   }
 