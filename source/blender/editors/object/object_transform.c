7c616c1e716eae3c488b7872ecb01c8d1594dbf5
iyadahmed <theinfamousandr0iddev@gmail.com>
2020-10-28 12:14:08 +0200

    edit hint message

--- source/blender/editors/object/object_transform.c
+++ source/blender/editors/object/object_transform.c
@@ -2049,8 +2049,11 @@ void OBJECT_OT_transform_axis_target(wmOperatorType *ot)
   /* identifiers */
   ot->name = "Interactive Light Track to Cursor";
   ot->description =
-      "Interactively point lights to a location (Ctrl translates and aligns to target normal)\n"
-      "(optionally select a target Object and a Light, then use this operator)";
+      "Interactively point a light towards an object (Ctrl translates and aligns to target "
+      "normal)\n"
+      "Select object and shift select light > "
+      "Perform tool and point to target\nWithout target object the light rotates freely\n"
+      "Hotkey tool !Please use the hotkey !";
   ot->idname = "OBJECT_OT_transform_axis_target";
 
   /* api callbacks */
