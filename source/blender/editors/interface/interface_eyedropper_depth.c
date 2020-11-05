ddea3e2ba2cf8c8aa889a422588ae24542f0daf7
ReinerBforartists <reiner.prokein@posteo.de>
2020-11-05 12:37:18 +0100

    Camera DOF panel - Add menu entry for hotkey only tool Focus Distance Picker #1689

--- source/blender/editors/interface/interface_eyedropper_depth.c
+++ source/blender/editors/interface/interface_eyedropper_depth.c
@@ -372,7 +372,7 @@ void UI_OT_eyedropper_depth(wmOperatorType *ot)
   /* identifiers */
   ot->name = "Eyedropper Depth";
   ot->idname = "UI_OT_eyedropper_depth";
-  ot->description = "Sample depth from the 3D view";
+  ot->description = "Sample depth from the 3D view\nHotkey only tool with crazy design! This button is not clickable!\nYou need to be with the mouse over the Focus Distance slider\nThen press the hotkey";
 
   /* api callbacks */
   ot->invoke = depthdropper_invoke;
