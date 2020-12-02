e94408f3be7efb6bdc987daf5d0df804849f8e8a
ReinerBforartists <reiner.prokein@posteo.de>
2020-12-02 13:09:46 +0100

    Go through the tooltips and remove data-block nerdish speech #1687 - refine data preview menu

--- source/blender/windowmanager/intern/wm_operators.c
+++ source/blender/windowmanager/intern/wm_operators.c
@@ -3376,10 +3376,10 @@ static int previews_ensure_exec(bContext *C, wmOperator *UNUSED(op))
 
 static void WM_OT_previews_ensure(wmOperatorType *ot)
 {
-  ot->name = "Refresh Data Set Previews";
+  ot->name = "Refresh Data Previews";
   ot->idname = "WM_OT_previews_ensure";
   ot->description =
-      "Ensure data set previews are available and up-to-date "
+      "Ensure data previews are available and up-to-date "
       "(to be saved in .blend file, only for some types like materials, textures, etc.)";
 
   ot->exec = previews_ensure_exec;
@@ -3510,10 +3510,10 @@ static int previews_clear_exec(bContext *C, wmOperator *op)
 
 static void WM_OT_previews_clear(wmOperatorType *ot)
 {
-  ot->name = "Clear Data Set Previews";
+  ot->name = "Clear Data Previews";
   ot->idname = "WM_OT_previews_clear";
   ot->description =
-      "Clear data set previews (only for some types like objects, materials, textures, etc.)";
+      "Clear data previews (only for some types like objects, materials, textures, etc.)";
 
   ot->exec = previews_clear_exec;
   ot->invoke = WM_menu_invoke;
