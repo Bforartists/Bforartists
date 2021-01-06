Bforartists/Bforartists

commit cfe80d1119c79b0bb2006fd288f4ae6ce034e752

Fix custom-normal support for mesh editing operations
Account for custom normals for edit-mesh tools:

- Limited Dissolve
- Split
- Split (Edges/Vertices)
- Triangulate

source/blender/editors/mesh/editmesh_tools.c
@@ -1765,10 +1765,15 @@ static bool edbm_edge_split_selected_edges(wmOperator *op, Object *obedit, BMEdi
   if (bm->totedgesel == 0) {
     return false;
   }
+
+  BM_custom_loop_normals_to_vector_layer(em->bm);
+
   if (!EDBM_op_call_and_selectf(
           em, op, "edges.out", false, "split_edges edges=%he", BM_ELEM_SELECT)) {
     return false;
   }
+
+  BM_custom_loop_normals_from_vector_layer(em->bm, false);
 
   EDBM_select_flush(em);
   EDBM_update_generic(obedit->data, true, true);
@@ -1805,6 +1810,8 @@ static bool edbm_edge_split_selected_verts(wmOperator *op, Object *obedit, BMEdi
     }
   }
 
+  BM_custom_loop_normals_to_vector_layer(em->bm);
+
   if (!EDBM_op_callf(em,
                      op,
                      "split_edges edges=%he verts=%hv use_verts=%b",
@@ -1835,6 +1842,8 @@ static bool edbm_edge_split_selected_verts(wmOperator *op, Object *obedit, BMEdi
       }
     }
   }
+
+  BM_custom_loop_normals_from_vector_layer(em->bm, false);
 
   EDBM_select_flush(em);
   EDBM_update_generic(obedit->data, true, true);
@@ -5284,6 +5293,8 @@ static int edbm_quads_convert_to_tris_exec(bContext *C, wmOperator *op)
     BMOIter oiter;
     BMFace *f;
 
+    BM_custom_loop_normals_to_vector_layer(em->bm);
+
     EDBM_op_init(em,
                  &bmop,
                  op,
@@ -5307,6 +5318,8 @@ static int edbm_quads_convert_to_tris_exec(bContext *C, wmOperator *op)
     if (!EDBM_op_finish(em, &bmop, op, true)) {
       continue;
     }
+
+    BM_custom_loop_normals_from_vector_layer(em->bm, false);
 
     EDBM_update_generic(obedit->data, true, true);
   }
@@ -5997,6 +6010,8 @@ static int edbm_dissolve_limited_exec(bContext *C, wmOperator *op)
       dissolve_flag = BM_ELEM_SELECT;
     }
 
+    BM_custom_loop_normals_to_vector_layer(em->bm);
+
     EDBM_op_call_and_selectf(
         em,
         op,
@@ -6008,6 +6023,8 @@ static int edbm_dissolve_limited_exec(bContext *C, wmOperator *op)
         angle_limit,
         use_dissolve_boundaries,
         delimit);
+
+    BM_custom_loop_normals_from_vector_layer(em->bm, false);
 
     EDBM_update_generic(obedit->data, true, true);
   }
@@ -6233,12 +6250,16 @@ static int edbm_split_exec(bContext *C, wmOperator *op)
     if ((em->bm->totvertsel == 0) && (em->bm->totedgesel == 0) && (em->bm->totfacesel == 0)) {
       continue;
     }
+    BM_custom_loop_normals_to_vector_layer(em->bm);
+
     BMOperator bmop;
     EDBM_op_init(em, &bmop, op, "split geom=%hvef use_only_faces=%b", BM_ELEM_SELECT, false);
     BMO_op_exec(em->bm, &bmop);
     BM_mesh_elem_hflag_disable_all(em->bm, BM_VERT | BM_EDGE | BM_FACE, BM_ELEM_SELECT, false);
     BMO_slot_buffer_hflag_enable(
         em->bm, bmop.slots_out, "geom.out", BM_ALL_NOLOOP, BM_ELEM_SELECT, true);
+
+    BM_custom_loop_normals_from_vector_layer(em->bm, false);
 
     if (!EDBM_op_finish(em, &bmop, op, true)) {
       continue;

commit 31f2c98aa6e2a9e1b3db9e4f22c92637b3731a62

Fix T84426: Limit dissolve ignores selection with custom normals
Regression in cfe80d1119c79b0bb2006fd288f4ae6ce034e752.

Add note that custom normal calculation functions write into to tags.

source/blender/bmesh/intern/bmesh_mesh.c
@@ -669,6 +669,9 @@ bool BM_loop_check_cyclic_smooth_fan(BMLoop *l_curr)
  * BMesh version of BKE_mesh_normals_loop_split() in mesh_evaluate.c
  * Will use first clnors_data array, and fallback to cd_loop_clnors_offset
  * (use NULL and -1 to not use clnors).
+ *
+ * \note This sets #BM_ELEM_TAG which is used in tool code (e.g. T84426).
+ * we could add a low-level API flag for this, see #BM_ELEM_API_FLAG_ENABLE and friends.
  */
 static void bm_mesh_loops_calc_normals(BMesh *bm,
                                        const float (*vcos)[3],
@@ -1555,6 +1558,10 @@ void BM_lnorspace_rebuild(BMesh *bm, bool preserve_clnor)
 #endif
 }
 
+/**
+ * \warning This function sets #BM_ELEM_TAG on loops & edges via #bm_mesh_loops_calc_normals,
+ * take care to run this before setting up tags.
+ */
 void BM_lnorspace_update(BMesh *bm)
 {
   if (bm->lnor_spacearr == NULL) {
@@ -1889,6 +1896,10 @@ void BM_loop_normal_editdata_array_free(BMLoopNorEditDataArray *lnors_ed_arr)
   MEM_freeN(lnors_ed_arr);
 }
 
+/**
+ * \warning This function sets #BM_ELEM_TAG on loops & edges via #bm_mesh_loops_calc_normals,
+ * take care to run this before setting up tags.
+ */
 bool BM_custom_loop_normals_to_vector_layer(BMesh *bm)
 {
   BMFace *f;

source/blender/editors/mesh/editmesh_tools.c
@@ -1790,6 +1790,8 @@ static bool edbm_edge_split_selected_verts(wmOperator *op, Object *obedit, BMEdi
     return false;
   }
 
+  BM_custom_loop_normals_to_vector_layer(em->bm);
+
   /* Flush from vertices to edges. */
   BMIter iter;
   BMEdge *eed;
@@ -1808,8 +1810,6 @@ static bool edbm_edge_split_selected_verts(wmOperator *op, Object *obedit, BMEdi
       } while ((l_iter = l_iter->radial_next) != eed->l);
     }
   }
-
-  BM_custom_loop_normals_to_vector_layer(em->bm);
 
   if (!EDBM_op_callf(em,
                      op,
@@ -5966,6 +5966,8 @@ static int edbm_dissolve_limited_exec(bContext *C, wmOperator *op)
       continue;
     }
 
+    BM_custom_loop_normals_to_vector_layer(em->bm);
+
     if (em->selectmode == SCE_SELECT_FACE) {
       /* flush selection to tags and untag edges/verts with partially selected faces */
       BMIter iter;
@@ -5996,8 +5998,6 @@ static int edbm_dissolve_limited_exec(bContext *C, wmOperator *op)
     else {
       dissolve_flag = BM_ELEM_SELECT;
     }
-
-    BM_custom_loop_normals_to_vector_layer(em->bm);
 
     EDBM_op_call_and_selectf(
         em,