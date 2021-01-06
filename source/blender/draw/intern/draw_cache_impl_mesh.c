Bforartists/Bforartists

commit 8a5605d5b8ad49dff2a0b88f18c4008013cb725f

Fix T77359: Crash adding UV's in edit-mode with linked duplicates
This prevents UV layer mix up in
MeshBatchCache.cd_used/cd_needed/cd_used_over_time which depends on the
extraction method.

One object's mesh can be accessed with MR_EXTRACT_MESH, another object
that uses the same mesh can use MR_EXTRACT_BMESH based on
(Object.mode & OB_MODE_EDIT), this causes a problem as the edit-mesh
and the mesh aren't always in sync, the custom data layers wont
necessarily match up, causing T77359.

Reviewed by @jbakker, @brecht

Ref D8645

source/blender/draw/intern/draw_cache_impl_mesh.c
@@ -1187,7 +1187,15 @@ void DRW_mesh_batch_cache_create_requested(struct TaskGraph *task_graph,
     BLI_assert(me->edit_mesh->mesh_eval_final != NULL);
   }
 
-  const bool is_editmode = (me->edit_mesh != NULL) && DRW_object_is_in_edit_mode(ob);
+  /* Don't check `DRW_object_is_in_edit_mode(ob)` here because it means the same mesh
+   * may draw with edit-mesh data and regular mesh data.
+   * In this case the custom-data layers used wont always match in `me->runtime.batch_cache`.
+   * If we want to display regular mesh data, we should have a separate cache for the edit-mesh.
+   * See T77359. */
+  const bool is_editmode = (me->edit_mesh != NULL) /* && DRW_object_is_in_edit_mode(ob) */;
+
+  /* This could be set for paint mode too, currently it's only used for edit-mode. */
+  const bool is_mode_active = is_editmode && DRW_object_is_in_edit_mode(ob);
 
   DRWBatchFlag batch_requested = cache->batch_requested;
   cache->batch_requested = 0;
@@ -1508,6 +1516,7 @@ void DRW_mesh_batch_cache_create_requested(struct TaskGraph *task_graph,
                                        me,
                                        is_editmode,
                                        is_paint_mode,
+                                       is_mode_active,
                                        ob->obmat,
                                        false,
                                        true,
@@ -1525,6 +1534,7 @@ void DRW_mesh_batch_cache_create_requested(struct TaskGraph *task_graph,
                                        me,
                                        is_editmode,
                                        is_paint_mode,
+                                       is_mode_active,
                                        ob->obmat,
                                        false,
                                        false,
@@ -1541,6 +1551,7 @@ void DRW_mesh_batch_cache_create_requested(struct TaskGraph *task_graph,
                                      me,
                                      is_editmode,
                                      is_paint_mode,
+                                     is_mode_active,
                                      ob->obmat,
                                      true,
                                      false,

source/blender/draw/intern/draw_cache_extract.h
@@ -260,6 +260,7 @@ void mesh_buffer_cache_create_requested(struct TaskGraph *task_graph,
                                         Mesh *me,
                                         const bool is_editmode,
                                         const bool is_paint_mode,
+                                        const bool is_mode_active,
                                         const float obmat[4][4],
                                         const bool do_final,
                                         const bool do_uvedit,

source/blender/draw/intern/draw_cache_extract_mesh.c
@@ -313,9 +313,14 @@ static void mesh_render_data_update_normals(MeshRenderData *mr,
   }
 }
 
+/**
+ * \param is_mode_active: When true, use the modifiers from the edit-data,
+ * otherwise don't use modifiers as they are not from this object.
+ */
 static MeshRenderData *mesh_render_data_create(Mesh *me,
                                                const bool is_editmode,
                                                const bool is_paint_mode,
+                                               const bool is_mode_active,
                                                const float obmat[4][4],
                                                const bool do_final,
                                                const bool do_uvedit,
@@ -335,7 +340,7 @@ static MeshRenderData *mesh_render_data_create(Mesh *me,
     mr->bm = me->edit_mesh->bm;
     mr->edit_bmesh = me->edit_mesh;
     mr->me = (do_final) ? me->edit_mesh->mesh_eval_final : me->edit_mesh->mesh_eval_cage;
-    mr->edit_data = mr->me->runtime.edit_data;
+    mr->edit_data = is_mode_active ? mr->me->runtime.edit_data : NULL;
 
     if (mr->edit_data) {
       EditMeshData *emd = mr->edit_data;
@@ -350,8 +355,9 @@ static MeshRenderData *mesh_render_data_create(Mesh *me,
       mr->bm_poly_centers = mr->edit_data->polyCos;
     }
 
-    bool has_mdata = (mr->me->runtime.wrapper_type == ME_WRAPPER_TYPE_MDATA);
-    bool use_mapped = has_mdata && !do_uvedit && mr->me && !mr->me->runtime.is_original;
+    bool has_mdata = is_mode_active && (mr->me->runtime.wrapper_type == ME_WRAPPER_TYPE_MDATA);
+    bool use_mapped = is_mode_active &&
+                      (has_mdata && !do_uvedit && mr->me && !mr->me->runtime.is_original);
 
     int bm_ensure_types = BM_VERT | BM_EDGE | BM_LOOP | BM_FACE;
 
@@ -5412,6 +5418,7 @@ void mesh_buffer_cache_create_requested(struct TaskGraph *task_graph,
 
                                         const bool is_editmode,
                                         const bool is_paint_mode,
+                                        const bool is_mode_active,
                                         const float obmat[4][4],
                                         const bool do_final,
                                         const bool do_uvedit,
@@ -5511,6 +5518,7 @@ void mesh_buffer_cache_create_requested(struct TaskGraph *task_graph,
   MeshRenderData *mr = mesh_render_data_create(me,
                                                is_editmode,
                                                is_paint_mode,
+                                               is_mode_active,
                                                obmat,
                                                do_final,
                                                do_uvedit,

commit 454a21f8b5b11f25484e57c6948c7604dce1991d

Fix T82952: Crash changing mesh data block and switching scenes
Regression in 8a5605d5b8ad49dff2a0b88f18c4008013cb725f.

source/blender/draw/intern/draw_cache_impl_mesh.c
@@ -1203,7 +1203,12 @@ void DRW_mesh_batch_cache_create_requested(struct TaskGraph *task_graph,
    * In this case the custom-data layers used wont always match in `me->runtime.batch_cache`.
    * If we want to display regular mesh data, we should have a separate cache for the edit-mesh.
    * See T77359. */
-  const bool is_editmode = (me->edit_mesh != NULL) /* && DRW_object_is_in_edit_mode(ob) */;
+  const bool is_editmode = (me->edit_mesh != NULL) &&
+                           /* In rare cases we have the edit-mode data but not the generated cache.
+                            * This can happen when switching an objects data to a mesh which
+                            * happens to be in edit-mode in another scene, see: T82952. */
+                           (me->edit_mesh->mesh_eval_final !=
+                            NULL) /* && DRW_object_is_in_edit_mode(ob) */;
 
   /* This could be set for paint mode too, currently it's only used for edit-mode. */
   const bool is_mode_active = is_editmode && DRW_object_is_in_edit_mode(ob);