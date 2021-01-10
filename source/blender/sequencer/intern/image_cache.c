Bforartists/Bforartists

commit 4ddaac810602be61e1fcdd5ff6e8333a17bedbf6

VSE: handle IO errors when reading disk cache header and version
Respond to return values of `fscanf()` and `fread()` to detect and
handle I/O errors. Not only is error handling a good idea, this also
prevents warnings from GCC that `fread()` and `fscanf()` return values
are ignored.

Reviewed By: ISS

Differential Revision: https://developer.blender.org/D9915

source/blender/sequencer/intern/image_cache.c
@@ -425,7 +425,10 @@ static void seq_disk_cache_handle_versioning(SeqDiskCache *disk_cache)
     FILE *file = BLI_fopen(path_version_file, "r");
 
     if (file) {
-      fscanf(file, "%d", &version);
+      const int num_items_read = fscanf(file, "%d", &version);
+      if (num_items_read == 0) {
+        version = -1;
+      }
       fclose(file);
     }
 
@@ -510,10 +513,14 @@ static size_t inflate_file_to_imbuf(ImBuf *ibuf, FILE *file, DiskCacheHeaderEntr
       ibuf->rect_float, header_entry->size_raw, file, header_entry->offset);
 }
 
-static void seq_disk_cache_read_header(FILE *file, DiskCacheHeader *header)
+static bool seq_disk_cache_read_header(FILE *file, DiskCacheHeader *header)
 {
   fseek(file, 0, 0);
-  fread(header, sizeof(*header), 1, file);
+  const size_t num_items_read = fread(header, sizeof(*header), 1, file);
+  if (num_items_read < 1) {
+    perror("unable to read disk cache header");
+    return false;
+  }
 
   for (int i = 0; i < DCACHE_IMAGES_PER_FILE; i++) {
     if ((ENDIAN_ORDER == B_ENDIAN) && header->entry[i].encoding == 0) {
@@ -523,6 +530,8 @@ static void seq_disk_cache_read_header(FILE *file, DiskCacheHeader *header)
       BLI_endian_switch_uint64(&header->entry[i].size_raw);
     }
   }
+
+  return true;
 }
 
 static size_t seq_disk_cache_write_header(FILE *file, DiskCacheHeader *header)
@@ -611,8 +620,12 @@ static bool seq_disk_cache_write_file(SeqDiskCache *disk_cache, SeqCacheKey *key
 
   DiskCacheHeader header;
   memset(&header, 0, sizeof(header));
-  seq_disk_cache_read_header(file, &header);
+  if (!seq_disk_cache_read_header(file, &header)) {
+    fclose(file);
+    return false;
+  }
   int entry_index = seq_disk_cache_add_header_entry(key, ibuf, &header);
+
   size_t bytes_written = deflate_imbuf_to_file(
       ibuf, file, seq_disk_cache_compression_level(), &header.entry[entry_index]);
 
@@ -644,7 +657,10 @@ static ImBuf *seq_disk_cache_read_file(SeqDiskCache *disk_cache, SeqCacheKey *ke
     return NULL;
   }
 
-  seq_disk_cache_read_header(file, &header);
+  if (!seq_disk_cache_read_header(file, &header)) {
+    fclose(file);
+    return NULL;
+  }
   int entry_index = seq_disk_cache_get_header_entry(key, &header);
 
   /* Item not found. */

commit 07c03bc7f3c47accf8b37d6f37f0044f250f2b86

Fix sequencer disk cache not writing data
Error handling added in 4ddaac810602 caused that reading header of new
file failed, since it is empty.

Don't attempt to read header if file is empty. If header can not be read
anyway, try to delete the file. Add asserts, as this should never
happen.

Reviewed By: sybren

Differential Revision: https://developer.blender.org/D9954

source/blender/sequencer/intern/image_cache.c
@@ -518,6 +518,7 @@ static bool seq_disk_cache_read_header(FILE *file, DiskCacheHeader *header)
   fseek(file, 0, 0);
   const size_t num_items_read = fread(header, sizeof(*header), 1, file);
   if (num_items_read < 1) {
+    BLI_assert(!"unable to read disk cache header");
     perror("unable to read disk cache header");
     return false;
   }
@@ -618,10 +619,14 @@ static bool seq_disk_cache_write_file(SeqDiskCache *disk_cache, SeqCacheKey *key
     seq_disk_cache_add_file_to_list(disk_cache, path);
   }
 
+  DiskCacheFile *cache_file = seq_disk_cache_get_file_entry_by_path(disk_cache, path);
   DiskCacheHeader header;
   memset(&header, 0, sizeof(header));
-  if (!seq_disk_cache_read_header(file, &header)) {
+  /* BLI_make_existing_file() above may create an empty file. This is fine, don't atttempt reading
+   * the header in that case. */
+  if (cache_file->fstat.st_size != 0 && !seq_disk_cache_read_header(file, &header)) {
     fclose(file);
+    seq_disk_cache_delete_file(disk_cache, cache_file);
     return false;
   }
   int entry_index = seq_disk_cache_add_header_entry(key, ibuf, &header);