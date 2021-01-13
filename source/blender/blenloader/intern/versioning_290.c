Bforartists/Bforartists

commit b2dcf7d77578a73849ec62a4c519e8d151bf4936

Compositor: Alpha Mode
{D9211} introduced pre-multiplying the color for the keying node. This
pre-multiplication should also be done by other keying nodes and should be
the default operation for alpha node.

This patch will change the logic of keying nodes (Cryptomatte Node,
Channel Matte, Chroma Matte, Color Matte, Difference Matte, Distance
Matte, Luminance Matte) and breaks old files.

The Set alpha node has a mode parameter. This parameter changes
the logic to `Apply Mask` the alpha on the RGBA channels of the input color
or only replace the alpha channel (old behavior).

The replace mode is automatically set for older files. When adding
new files the the multiply mode is set.

Reviewed By: Sergey Sharybin

Differential Revision: https://developer.blender.org/D9630

source/blender/blenloader/intern/versioning_290.c
@@ -1490,5 +1490,22 @@ void blo_do_versions_290(FileData *fd, Library *UNUSED(lib), Main *bmain)
    */
   {
     /* Keep this block, even when empty. */
+    if (!DNA_struct_find(fd->filesdna, "NodeSetAlpha")) {
+      LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
+        bNodeTree *nodetree = scene->nodetree;
+        if (nodetree == NULL) {
+          continue;
+        }
+
+        LISTBASE_FOREACH (bNode *, node, &nodetree->nodes) {
+          if (node->type != CMP_NODE_SETALPHA) {
+            continue;
+          }
+          NodeSetAlpha *storage = MEM_callocN(sizeof(NodeSetAlpha), "NodeSetAlpha");
+          storage->mode = CMP_NODE_SETALPHA_MODE_REPLACE_ALPHA;
+          node->storage = storage;
+        }
+      }
+    }
   }
 }

source/blender/compositor/CMakeLists.txt
@@ -355,8 +355,6 @@ set(SRC
   operations/COM_KeyingDespillOperation.h
   operations/COM_KeyingOperation.cpp
   operations/COM_KeyingOperation.h
-  operations/COM_KeyingSetAlphaOperation.cpp
-  operations/COM_KeyingSetAlphaOperation.h
 
   operations/COM_ColorSpillOperation.cpp
   operations/COM_ColorSpillOperation.h
@@ -461,8 +459,10 @@ set(SRC
   operations/COM_MapRangeOperation.h
   operations/COM_MapValueOperation.cpp
   operations/COM_MapValueOperation.h
-  operations/COM_SetAlphaOperation.cpp
-  operations/COM_SetAlphaOperation.h
+  operations/COM_SetAlphaMultiplyOperation.cpp
+  operations/COM_SetAlphaMultiplyOperation.h
+  operations/COM_SetAlphaReplaceOperation.cpp
+  operations/COM_SetAlphaReplaceOperation.h
 
   # Distort operation
   operations/COM_DisplaceOperation.cpp

source/blender/compositor/nodes/COM_ChannelMatteNode.cpp
@@ -20,7 +20,7 @@
 #include "BKE_node.h"
 #include "COM_ChannelMatteOperation.h"
 #include "COM_ConvertOperation.h"
-#include "COM_SetAlphaOperation.h"
+#include "COM_SetAlphaMultiplyOperation.h"
 
 ChannelMatteNode::ChannelMatteNode(bNode *editorNode) : Node(editorNode)
 {
@@ -64,7 +64,7 @@ void ChannelMatteNode::convertToOperations(NodeConverter &converter,
   operation->setSettings((NodeChroma *)node->storage, node->custom2);
   converter.addOperation(operation);
 
-  SetAlphaOperation *operationAlpha = new SetAlphaOperation();
+  SetAlphaMultiplyOperation *operationAlpha = new SetAlphaMultiplyOperation();
   converter.addOperation(operationAlpha);
 
   if (convert != nullptr) {

source/blender/compositor/nodes/COM_ChromaMatteNode.cpp
@@ -20,7 +20,7 @@
 #include "BKE_node.h"
 #include "COM_ChromaMatteOperation.h"
 #include "COM_ConvertOperation.h"
-#include "COM_SetAlphaOperation.h"
+#include "COM_SetAlphaMultiplyOperation.h"
 
 ChromaMatteNode::ChromaMatteNode(bNode *editorNode) : Node(editorNode)
 {
@@ -48,7 +48,7 @@ void ChromaMatteNode::convertToOperations(NodeConverter &converter,
   operation->setSettings((NodeChroma *)editorsnode->storage);
   converter.addOperation(operation);
 
-  SetAlphaOperation *operationAlpha = new SetAlphaOperation();
+  SetAlphaMultiplyOperation *operationAlpha = new SetAlphaMultiplyOperation();
   converter.addOperation(operationAlpha);
 
   converter.mapInputSocket(inputSocketImage, operationRGBToYCC_Image->getInputSocket(0));

source/blender/compositor/nodes/COM_ColorMatteNode.cpp
@@ -20,7 +20,7 @@
 #include "BKE_node.h"
 #include "COM_ColorMatteOperation.h"
 #include "COM_ConvertOperation.h"
-#include "COM_SetAlphaOperation.h"
+#include "COM_SetAlphaMultiplyOperation.h"
 
 ColorMatteNode::ColorMatteNode(bNode *editorNode) : Node(editorNode)
 {
@@ -46,7 +46,7 @@ void ColorMatteNode::convertToOperations(NodeConverter &converter,
   operation->setSettings((NodeChroma *)editorsnode->storage);
   converter.addOperation(operation);
 
-  SetAlphaOperation *operationAlpha = new SetAlphaOperation();
+  SetAlphaMultiplyOperation *operationAlpha = new SetAlphaMultiplyOperation();
   converter.addOperation(operationAlpha);
 
   converter.mapInputSocket(inputSocketImage, operationRGBToHSV_Image->getInputSocket(0));

source/blender/compositor/nodes/COM_CryptomatteNode.cpp
@@ -19,13 +19,13 @@
 #include "COM_CryptomatteNode.h"
 #include "COM_ConvertOperation.h"
 #include "COM_CryptomatteOperation.h"
-#include "COM_SetAlphaOperation.h"
 
 #include "BLI_assert.h"
 #include "BLI_hash_mm3.h"
 #include "BLI_listbase.h"
 #include "BLI_string.h"
 
+#include "COM_SetAlphaMultiplyOperation.h"
 #include <iterator>
 
 CryptomatteNode::CryptomatteNode(bNode *editorNode) : Node(editorNode)
@@ -61,13 +61,13 @@ void CryptomatteNode::convertToOperations(NodeConverter &converter,
   separateOperation->setChannel(3);
   converter.addOperation(separateOperation);
 
-  SetAlphaOperation *operationAlpha = new SetAlphaOperation();
+  SetAlphaMultiplyOperation *operationAlpha = new SetAlphaMultiplyOperation();
   converter.addOperation(operationAlpha);
 
   converter.addLink(operation->getOutputSocket(0), separateOperation->getInputSocket(0));
   converter.addLink(separateOperation->getOutputSocket(0), operationAlpha->getInputSocket(1));
 
-  SetAlphaOperation *clearAlphaOperation = new SetAlphaOperation();
+  SetAlphaMultiplyOperation *clearAlphaOperation = new SetAlphaMultiplyOperation();
   converter.addOperation(clearAlphaOperation);
   converter.addInputValue(clearAlphaOperation->getInputSocket(1), 1.0f);
 

source/blender/compositor/nodes/COM_DifferenceMatteNode.cpp
@@ -19,7 +19,7 @@
 #include "COM_DifferenceMatteNode.h"
 #include "BKE_node.h"
 #include "COM_DifferenceMatteOperation.h"
-#include "COM_SetAlphaOperation.h"
+#include "COM_SetAlphaMultiplyOperation.h"
 
 DifferenceMatteNode::DifferenceMatteNode(bNode *editorNode) : Node(editorNode)
 {
@@ -43,7 +43,7 @@ void DifferenceMatteNode::convertToOperations(NodeConverter &converter,
   converter.mapInputSocket(inputSocket2, operationSet->getInputSocket(1));
   converter.mapOutputSocket(outputSocketMatte, operationSet->getOutputSocket(0));
 
-  SetAlphaOperation *operation = new SetAlphaOperation();
+  SetAlphaMultiplyOperation *operation = new SetAlphaMultiplyOperation();
   converter.addOperation(operation);
 
   converter.mapInputSocket(inputSocket, operation->getInputSocket(0));

source/blender/compositor/nodes/COM_DistanceMatteNode.cpp
@@ -21,7 +21,7 @@
 #include "COM_ConvertOperation.h"
 #include "COM_DistanceRGBMatteOperation.h"
 #include "COM_DistanceYCCMatteOperation.h"
-#include "COM_SetAlphaOperation.h"
+#include "COM_SetAlphaMultiplyOperation.h"
 
 DistanceMatteNode::DistanceMatteNode(bNode *editorNode) : Node(editorNode)
 {
@@ -39,7 +39,7 @@ void DistanceMatteNode::convertToOperations(NodeConverter &converter,
   NodeOutput *outputSocketImage = this->getOutputSocket(0);
   NodeOutput *outputSocketMatte = this->getOutputSocket(1);
 
-  SetAlphaOperation *operationAlpha = new SetAlphaOperation();
+  SetAlphaMultiplyOperation *operationAlpha = new SetAlphaMultiplyOperation();
   converter.addOperation(operationAlpha);
 
   /* work in RGB color space */

commit 176dd13bf737e8d61ca86e0e06a452d8c6a9e986

Fix T84646: Compositor: In Blender 292 "Set Alpha" node is undefined
Incorrect version code introduced by b2dcf7d77578 would not check inside
node groups.

source/blender/blenloader/intern/versioning_290.c
@@ -1491,13 +1491,11 @@ void blo_do_versions_290(FileData *fd, Library *UNUSED(lib), Main *bmain)
 
   if (!MAIN_VERSION_ATLEAST(bmain, 292, 10)) {
     if (!DNA_struct_find(fd->filesdna, "NodeSetAlpha")) {
-      LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
-        bNodeTree *nodetree = scene->nodetree;
-        if (nodetree == NULL) {
+      LISTBASE_FOREACH (bNodeTree *, ntree, &bmain->nodetrees) {
+        if (ntree->type != NTREE_COMPOSIT) {
           continue;
         }
-
-        LISTBASE_FOREACH (bNode *, node, &nodetree->nodes) {
+        LISTBASE_FOREACH (bNode *, node, &ntree->nodes) {
           if (node->type != CMP_NODE_SETALPHA) {
             continue;
           }