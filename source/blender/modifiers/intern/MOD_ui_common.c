55b6e4bb818bf4aca7134cf3eff725b31bedbe22
ReinerBforartists <reiner.prokein@posteo.de>
2021-01-21 09:55:09 +0100

    Modifier header menu - missing icon for copy to selected #2184

--- source/blender/modifiers/intern/MOD_ui_common.c
+++ source/blender/modifiers/intern/MOD_ui_common.c
@@ -253,7 +253,7 @@ static void modifier_ops_extra_draw(bContext *C, uiLayout *layout, void *md_v)
 
   uiItemO(layout,
           CTX_IFACE_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Copy to Selected"),
-          0,
+          ICON_COPYDOWN,
           "OBJECT_OT_modifier_copy_to_selected");
 
   uiItemS(layout);
