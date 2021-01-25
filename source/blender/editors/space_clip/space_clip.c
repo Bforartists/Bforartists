Bforartists/Bforartists

commit 79b6a00caebf2f42e67c1083e167d51e2461583a

Tracking: Fix wrong projection for post-draw pass
Allows to draw proportional editing circle at a proper location.

source/blender/editors/space_clip/space_clip.c
@@ -987,7 +987,10 @@ static void clip_main_region_draw(const bContext *C, ARegion *region)
   }
 
   /* callback */
+  GPU_matrix_push_projection();
+  wmOrtho2(region->v2d.cur.xmin, region->v2d.cur.xmax, region->v2d.cur.ymin, region->v2d.cur.ymax);
   ED_region_draw_cb_draw(C, region, REGION_DRAW_POST_VIEW);
+  GPU_matrix_pop_projection();
 
   /* reset view matrix */
   UI_view2d_view_restore(C);

commit 5a5f3fdd106152878988295017770ffaf5971395

Fix T84990: Proportional Editing falloff drawing for masks in MCE is broken
This got broken in 79b6a00caeb. A fix which will be consistent with image
editor is not obviously clear, so for the time being revert the change and
go back to state which was working for a long time.

source/blender/editors/space_clip/space_clip.c
@@ -993,10 +993,14 @@ static void clip_main_region_draw(const bContext *C, ARegion *region)
   }
 
   /* callback */
-  GPU_matrix_push_projection();
-  wmOrtho2(region->v2d.cur.xmin, region->v2d.cur.xmax, region->v2d.cur.ymin, region->v2d.cur.ymax);
+  /* TODO(sergey): For being consistent with space image the projection needs to be configured
+   * the way how the commented out code does it. This works corrent for tracking data, but it
+   * causes wrong aspect correction for mask editor (see T84990). */
+  // GPU_matrix_push_projection();
+  // wmOrtho2(region->v2d.cur.xmin, region->v2d.cur.xmax, region->v2d.cur.ymin,
+  //          region->v2d.cur.ymax);
   ED_region_draw_cb_draw(C, region, REGION_DRAW_POST_VIEW);
-  GPU_matrix_pop_projection();
+  // GPU_matrix_pop_projection();
 
   /* reset view matrix */
   UI_view2d_view_restore(C);