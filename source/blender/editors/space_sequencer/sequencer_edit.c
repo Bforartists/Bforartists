Bforartists/Bforartists

commit 37685c35cb2a67f4bb095211a2ef73a69b793459

Fix T77764: VSE: snap strip to current frame does not move associated effect strips keyframes along
Maniphest Tasks: T77764

Differential Revision: https://developer.blender.org/D7996

source/blender/editors/space_sequencer/sequencer_edit.c
@@ -1417,12 +1417,15 @@ static int sequencer_snap_exec(bContext *C, wmOperator *op)
   for (seq = ed->seqbasep->first; seq; seq = seq->next) {
     if (seq->type & SEQ_TYPE_EFFECT) {
       if (seq->seq1 && (seq->seq1->flag & SELECT)) {
+        BKE_sequencer_offset_animdata(scene, seq, (snap_frame - seq->startdisp));
         BKE_sequence_calc(scene, seq);
       }
       else if (seq->seq2 && (seq->seq2->flag & SELECT)) {
+        BKE_sequencer_offset_animdata(scene, seq, (snap_frame - seq->startdisp));
         BKE_sequence_calc(scene, seq);
       }
       else if (seq->seq3 && (seq->seq3->flag & SELECT)) {
+        BKE_sequencer_offset_animdata(scene, seq, (snap_frame - seq->startdisp));
         BKE_sequence_calc(scene, seq);
       }
     }

commit 1e3ddf23cfb79c54834236d7deb5256436062e70

Fix T84705: Snapping strip handle offset animation
Only offset animation if whole strip is snapped.

Bug introduced in 37685c35cb2a.

Reviewed By: ISS

Differential Revision: https://developer.blender.org/D10116

source/blender/editors/space_sequencer/sequencer_edit.c
@@ -324,19 +324,27 @@ static int sequencer_snap_exec(bContext *C, wmOperator *op)
     }
   }
 
-  /* Recalculate bounds of effect strips. */
+  /* Recalculate bounds of effect strips, offsetting the keyframes if not snapping any handle. */
   for (seq = ed->seqbasep->first; seq; seq = seq->next) {
     if (seq->type & SEQ_TYPE_EFFECT) {
+      const bool either_handle_selected = (seq->flag & (SEQ_LEFTSEL | SEQ_RIGHTSEL)) != 0;
+
       if (seq->seq1 && (seq->seq1->flag & SELECT)) {
-        SEQ_offset_animdata(scene, seq, (snap_frame - seq->startdisp));
+        if (!either_handle_selected) {
+          SEQ_offset_animdata(scene, seq, (snap_frame - seq->startdisp));
+        }
         SEQ_time_update_sequence(scene, seq);
       }
       else if (seq->seq2 && (seq->seq2->flag & SELECT)) {
-        SEQ_offset_animdata(scene, seq, (snap_frame - seq->startdisp));
+        if (!either_handle_selected) {
+          SEQ_offset_animdata(scene, seq, (snap_frame - seq->startdisp));
+        }
         SEQ_time_update_sequence(scene, seq);
       }
       else if (seq->seq3 && (seq->seq3->flag & SELECT)) {
-        SEQ_offset_animdata(scene, seq, (snap_frame - seq->startdisp));
+        if (!either_handle_selected) {
+          SEQ_offset_animdata(scene, seq, (snap_frame - seq->startdisp));
+        }
         SEQ_time_update_sequence(scene, seq);
       }
     }