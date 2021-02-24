Bforartists/Bforartists

commit a8bb3cd3bb387640574b3e723e25ce7e761bc511

PyAPI: use postponed annotations to support Python 3.10
Support Python 3.10a5 or 3.9x with support explicitly enabled.

- Enable Python's postponed annotations for Blender's RNA classes
  types registered on startup.

- Using postponed annotations has implications for how they are defined,
  since they must evaluate in the modules name-space instead of the
  classes name-space. See changes to annotations in `release/scripts`.

- Use `from __future__ import annotations` at the top of the module
  to ensure the script will run with Python 3.10.

- Old logic is kept since it could be used if PEP-649 is supported.

Resolves T83626

Ref D10474

release/scripts/startup/bl_operators/wm.py
@@ -17,6 +17,7 @@
 # ##### END GPL LICENSE BLOCK #####
 
 # <pep8 compliant>
+from __future__ import annotations
 
 import bpy
 from bpy.types import (
@@ -859,9 +860,7 @@ class WM_OT_url_open_preset(Operator):
 
     type: EnumProperty(
         name="Site",
-        items=lambda self, _context: (
-            item for (item, _) in WM_OT_url_open_preset.preset_items
-        ),
+        items=WM_OT_url_open_preset._preset_items,
     )
 
     id: StringProperty(
@@ -915,6 +914,10 @@ class WM_OT_url_open_preset(Operator):
           "Lists committers to Blender's source code"),
          "https://www.blender.org/about/credits/"),
     ]
+
+    @staticmethod
+    def _preset_items(_self, _context):
+        return (item for (item, _) in WM_OT_url_open_preset.preset_items)
 
     def execute(self, context):
         url = None

intern/cycles/blender/addon/__init__.py
@@ -15,6 +15,7 @@
 #
 
 # <pep8 compliant>
+from __future__ import annotations
 
 bl_info = {
     "name": "Cycles Render Engine",

intern/cycles/blender/addon/engine.py
@@ -15,6 +15,7 @@
 #
 
 # <pep8 compliant>
+from __future__ import annotations
 
 
 def _is_using_buggy_driver():

intern/cycles/blender/addon/operators.py
@@ -15,6 +15,7 @@
 #
 
 # <pep8 compliant>
+from __future__ import annotations
 
 import bpy
 from bpy.types import Operator

intern/cycles/blender/addon/osl.py
@@ -15,6 +15,7 @@
 #
 
 # <pep8 compliant>
+from __future__ import annotations
 
 import bpy
 import _cycles

intern/cycles/blender/addon/presets.py
@@ -15,6 +15,7 @@
 #
 
 # <pep8 compliant>
+from __future__ import annotations
 
 from bl_operators.presets import AddPresetBase
 from bpy.types import Operator

intern/cycles/blender/addon/properties.py
@@ -15,6 +15,7 @@
 #
 
 # <pep8 compliant>
+from __future__ import annotations
 
 import bpy
 from bpy.props import (
@@ -841,7 +842,7 @@ class CyclesRenderSettings(bpy.types.PropertyGroup):
             ('MEGA', "Mega", ""),
             ('SPLIT', "Split", ""),
         ),
-        update=_devices_update_callback
+        update=CyclesRenderSettings._devices_update_callback
     )
 
     debug_opencl_device_type: EnumProperty(
@@ -855,10 +856,8 @@ class CyclesRenderSettings(bpy.types.PropertyGroup):
             ('GPU', "GPU", ""),
             ('ACCELERATOR', "Accelerator", ""),
         ),
-        update=_devices_update_callback
+        update=CyclesRenderSettings._devices_update_callback
     )
-
-    del _devices_update_callback
 
     debug_use_opencl_debug: BoolProperty(name="Debug OpenCL", default=False)
 
@@ -1481,7 +1480,7 @@ class CyclesPreferences(bpy.types.AddonPreferences):
     compute_device_type: EnumProperty(
         name="Compute Device Type",
         description="Device to use for computation (rendering with Cycles)",
-        items=get_device_types,
+        items=CyclesPreferences.get_device_types,
     )
 
     devices: bpy.props.CollectionProperty(type=CyclesDeviceSettings)

intern/cycles/blender/addon/ui.py
@@ -15,6 +15,7 @@
 #
 
 # <pep8 compliant>
+from __future__ import annotations
 
 import bpy
 from bpy_extras.node_utils import find_node_input

commit be6c96dc8a6ea4c29d26c2c21bb27cfc37d02eca

Fix T85930: Custom Property Error: Wrong Subtype
Regression in a8bb3cd3bb387640574b3e723e25ce7e761bc511

Unfortunately lambda functions don't work with postponed annotations.

release/scripts/startup/bl_operators/wm.py
@@ -1220,10 +1220,13 @@ class WM_OT_properties_edit(Operator):
     )
     subtype: EnumProperty(
         name="Subtype",
-        items=lambda self, _context: WM_OT_properties_edit.subtype_items,
+        items=WM_OT_properties_edit._subtype_items_fn,
     )
 
     subtype_items = rna_vector_subtype_items
+
+    def _subtype_items_fn(_self, _context):
+        return WM_OT_properties_edit.subtype_items
 
     def _init_subtype(self, prop_type, is_array, subtype):
         subtype = subtype or 'NONE'