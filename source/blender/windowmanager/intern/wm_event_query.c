Bforartists/Bforartists

commit 72862d481c67858c7752d942b79daf977cc7c81e

Fix T86116: Failure to detect click events with modifier held
Click detection logic relied on the event queue not accumulating events
as newly added events at the end of the queue adjusted
`wm->eventstate->prev{val/type}` which was then compared with events
at the front of the queue.

This made Ctrl-Click fail to detect the click as releasing Ctrl
set the previous value before the previous event was handled.

In practice this only happened in heavy scenes,
updating 10 fps or lower. It also made automated tests fail that
accumulate events before handling them.

source/blender/windowmanager/intern/wm_event_system.c
@@ -2960,16 +2960,16 @@ static int wm_handlers_do(bContext *C, wmEvent *event, ListBase *handlers)
     if (wm_action_not_handled(action)) {
       if (event->check_drag) {
         wmWindow *win = CTX_wm_window(C);
-        if (WM_event_drag_test(event, &win->eventstate->prevclickx)) {
+        if (WM_event_drag_test(event, &event->prevclickx)) {
           int x = event->x;
           int y = event->y;
           short val = event->val;
           short type = event->type;
 
-          event->x = win->eventstate->prevclickx;
-          event->y = win->eventstate->prevclicky;
+          event->x = event->prevclickx;
+          event->y = event->prevclicky;
           event->val = KM_CLICK_DRAG;
-          event->type = win->eventstate->type;
+          event->type = event->prevtype;
 
           CLOG_INFO(WM_LOG_HANDLERS, 1, "handling PRESS_DRAG");
 
@@ -3012,12 +3012,12 @@ static int wm_handlers_do(bContext *C, wmEvent *event, ListBase *handlers)
         }
       }
 
-      if (win && win->eventstate->prevtype == event->type) {
+      if (win && event->prevtype == event->type) {
 
         if (event->val == KM_RELEASE) {
-          if (win->eventstate->prevval == KM_PRESS) {
+          if (event->prevval == KM_PRESS) {
             if (win->eventstate->check_click == true) {
-              if (WM_event_drag_test(event, &win->eventstate->prevclickx)) {
+              if (WM_event_drag_test(event, &event->prevclickx)) {
                 win->eventstate->check_click = 0;
                 win->eventstate->check_drag = 0;
               }
@@ -3071,7 +3071,7 @@ static int wm_handlers_do(bContext *C, wmEvent *event, ListBase *handlers)
     else {
       wmWindow *win = CTX_wm_window(C);
       if (win) {
-        if (ISKEYMODIFIER(win->eventstate->type)) {
+        if (ISKEYMODIFIER(event->prevtype)) {
           win->eventstate->check_click = 0;
         }
       }
@@ -4358,15 +4358,15 @@ static wmWindow *wm_event_cursor_other_windows(wmWindowManager *wm, wmWindow *wi
   return NULL;
 }
 
-static bool wm_event_is_double_click(const wmEvent *event, const wmEvent *event_state)
+static bool wm_event_is_double_click(const wmEvent *event)
 {
-  if ((event->type == event_state->prevtype) && (event_state->prevval == KM_RELEASE) &&
+  if ((event->type == event->prevtype) && (event->prevval == KM_RELEASE) &&
       (event->val == KM_PRESS)) {
-    if (ISMOUSE(event->type) && WM_event_drag_test(event, &event_state->prevclickx)) {
+    if (ISMOUSE(event->type) && WM_event_drag_test(event, &event->prevclickx)) {
       /* Pass. */
     }
     else {
-      if ((PIL_check_seconds_timer() - event_state->prevclicktime) * 1000 < U.dbl_click_time) {
+      if ((PIL_check_seconds_timer() - event->prevclicktime) * 1000 < U.dbl_click_time) {
         return true;
       }
     }
@@ -4561,7 +4561,7 @@ void wm_event_add_ghostevent(wmWindowManager *wm, wmWindow *win, int type, void 
       evt->type = event.type;
 
       /* Double click test. */
-      if (wm_event_is_double_click(&event, evt)) {
+      if (wm_event_is_double_click(&event)) {
         CLOG_INFO(WM_LOG_HANDLERS, 1, "Send double click");
         event.val = KM_DBL_CLICK;
       }
@@ -4699,7 +4699,7 @@ void wm_event_add_ghostevent(wmWindowManager *wm, wmWindow *win, int type, void 
 
       /* Double click test. */
       /* If previous event was same type, and previous was release, and now it presses... */
-      if (wm_event_is_double_click(&event, evt)) {
+      if (wm_event_is_double_click(&event)) {
         CLOG_INFO(WM_LOG_HANDLERS, 1, "Send double click");
         event.val = KM_DBL_CLICK;
       }

commit 3d125e12112dcf3b52ed29008fb1d226ff716810

Fix click-drag regression in fix for T86116
6fb2666d6f5fcb188d6f2e24c9c9b66f6a643d8a &
72862d481c67858c7752d942b79daf977cc7c81e
caused a regression with click-drag (not tweak which has it's own logic).

Restore some changes from these commits with added comments.

Minor changes from previous functionality from
0f38c4277d19f3cd779b5561353977603820ce21.

- `prevval` & `prevtype` are now set for all kinds of new events
  in the queue previously this was not done for some kinds of events
  (mouse wheel, ndof - for example).

- Set `prevval` & `prevtype` for other windows for mouse buttons.

source/blender/windowmanager/intern/wm_event_query.c
@@ -280,8 +280,11 @@ int WM_event_drag_threshold(const struct wmEvent *event)
   if (WM_event_is_tablet(event)) {
     drag_threshold = U.drag_threshold_tablet;
   }
-  else if (ISMOUSE(event->type)) {
-    /* Mouse button or mouse motion. */
+  else if (ISMOUSE(event->prevtype)) {
+    BLI_assert(event->prevtype != MOUSEMOVE);
+    /* Using the previous type is important is we want to check the last pressed/released button,
+     * The `event->type` would include #MOUSEMOVE which is always the case when dragging
+     * and does not help us know which threshold to use. */
     drag_threshold = U.drag_threshold_mouse;
   }
   else {

source/blender/windowmanager/intern/wm_event_system.c
@@ -4458,6 +4458,19 @@ void wm_event_add_ghostevent(wmWindowManager *wm, wmWindow *win, int type, void 
   event = *evt;
   event.is_repeat = false;
 
+  /**
+   * Always support accessing the last key press/release. This is set from `win->eventstate`,
+   * so it will always be a valid event type to store in the previous state.
+   *
+   * Note that these values are intentionally _not_ set in the `win->eventstate`,
+   * as copying these values only makes sense when `win->eventstate->{val/type}` would be
+   * written to (which only happens for some kinds of events).
+   * If this was done it could leave `win->eventstate` previous and current value
+   * set to the same key press/release state which doesn't make sense.
+   */
+  event.prevtype = event.type;
+  event.prevval = event.val;
+
   /* Ensure the event state is correct, any deviation from this may cause bugs. */
 #ifndef NDEBUG
   if ((evt->type || evt->val) && /* Ignore cleared event state. */
@@ -4497,6 +4510,10 @@ void wm_event_add_ghostevent(wmWindowManager *wm, wmWindow *win, int type, void 
         wmEvent oevent, *oevt = owin->eventstate;
 
         oevent = *oevt;
+
+        /* See comment for this operation on `event` for details. */
+        oevent.prevtype = oevent.type;
+        oevent.prevval = oevent.val;
 
         copy_v2_v2_int(&oevent.x, &event.x);
         oevent.type = MOUSEMOVE;
@@ -4593,8 +4610,12 @@ void wm_event_add_ghostevent(wmWindowManager *wm, wmWindow *win, int type, void 
       if (owin) {
         wmEvent oevent = *(owin->eventstate);
 
-        oevent.x = event.x;
-        oevent.y = event.y;
+        /* See comment for this operation on `event` for details. */
+        oevent.prevtype = oevent.type;
+        oevent.prevval = oevent.val;
+
+        copy_v2_v2_int(&oevent.x, &event.x);
+
         oevent.type = event.type;
         oevent.val = event.val;
         oevent.tablet = event.tablet;