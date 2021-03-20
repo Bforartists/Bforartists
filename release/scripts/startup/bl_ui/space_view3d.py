383d9b0104501d4c3a66c93c5cda2a2109c7ec3b
ReinerBforartists <reiner.prokein@posteo.de>
2021-03-20 11:27:16 +0100

    Sculpt mode - mask menu - separate tooltips for the mask slice operators #2292 - added new mask slice icon

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -3831,12 +3831,12 @@ class VIEW3D_MT_mask(Menu):
 
         layout.separator()
 
-        props = layout.operator("mesh.paint_mask_slice", text="Mask Slice", icon = "MOD_MASK")
+        props = layout.operator("mesh.paint_mask_slice", text="Mask Slice", icon = "MASK_SLICE")
         props.fill_holes = False
         props.new_object = False
-        props = layout.operator("mesh.paint_mask_slice", text="Mask Slice and Fill Holes", icon = "MOD_MASK")
+        props = layout.operator("mesh.paint_mask_slice", text="Mask Slice and Fill Holes", icon = "MASK_SLICE")
         props.new_object = False
-        props = layout.operator("mesh.paint_mask_slice", text="Mask Slice to New Object", icon = "MOD_MASK")
+        props = layout.operator("mesh.paint_mask_slice", text="Mask Slice to New Object", icon = "MASK_SLICE")
 
         layout.separator()
 
