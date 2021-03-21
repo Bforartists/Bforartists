Bforartists/Bforartists

commit 660a0f78ce21f7b8a2bdf0b9644ca0022918ac19

Fix T85823: bpy.ops.transform... ignoring 'center_override' for Bezier control points
`transform_around_single_fallback_ex` ignored that the center could be overwritten.

source/blender/editors/transform/transform_convert.c
@@ -73,12 +73,20 @@ bool transform_mode_use_local_origins(const TransInfo *t)
  */
 void transform_around_single_fallback_ex(TransInfo *t, int data_len_all)
 {
-  if ((ELEM(t->around, V3D_AROUND_CENTER_BOUNDS, V3D_AROUND_CENTER_MEDIAN, V3D_AROUND_ACTIVE)) &&
-      transform_mode_use_local_origins(t)) {
-    if (data_len_all == 1) {
-      t->around = V3D_AROUND_LOCAL_ORIGINS;
-    }
+  if (data_len_all != 1) {
+    return;
   }
+  if (!ELEM(t->around, V3D_AROUND_CENTER_BOUNDS, V3D_AROUND_CENTER_MEDIAN, V3D_AROUND_ACTIVE)) {
+    return;
+  }
+  if (!transform_mode_use_local_origins(t)) {
+    return;
+  }
+  if (t->flag | T_OVERRIDE_CENTER) {
+    return;
+  }
+
+  t->around = V3D_AROUND_LOCAL_ORIGINS;
 }
 
 void transform_around_single_fallback(TransInfo *t)

commit 1ceda48a676843212df50cd9cb7ecbf1ff1278e8

Fix failure to set transform rotate around single
Regression in 660a0f78ce21f7b8a2bdf0b9644ca0022918ac19

source/blender/editors/transform/transform_convert.c
@@ -82,7 +82,7 @@ void transform_around_single_fallback_ex(TransInfo *t, int data_len_all)
   if (!transform_mode_use_local_origins(t)) {
     return;
   }
-  if (t->flag | T_OVERRIDE_CENTER) {
+  if (t->flag & T_OVERRIDE_CENTER) {
     return;
   }
 