Bforartists/Bforartists

commit 5640c46b1da5aba068cd628f60518d0c29b3c4c8

Fixup for Setup Tracking Scene
Issues introduced on: 055a84a67cf4503d63a289cf2cc02e3bc2ec95b0
(own error for the records).

release/scripts/startup/bl_operators/clip.py
@@ -641,7 +641,7 @@ class CLIP_OT_setup_tracking_scene(Operator):
         master_collection = context.scene.collection
         collection = bpy.data.collections.get(collection_name)
 
-        if collection.library:
+        if collection and collection.library:
             # We need a local collection instead.
             collection = None
 
@@ -965,7 +965,7 @@ class CLIP_OT_setup_tracking_scene(Operator):
 
         # And set everything on background layer to shadow catcher.
         if hasattr(scene, "cycles"):
-            self.setup_shadow_catcher_collection(bg_coll)
+            setup_shadow_catcher_objects(bg_coll)
 
     def execute(self, context):
         scene = context.scene

commit 56029ca258388b39cd91afcd6745cfba06587343

Fix error in clip.setup_tracking_scene with cycles
Regression in 5640c46b1da5aba068cd628f60518d0c29b3c4c8

release/scripts/startup/bl_operators/clip.py
@@ -960,7 +960,7 @@ class CLIP_OT_setup_tracking_scene(Operator):
             for ob in collection.objects:
                 ob.cycles.is_shadow_catcher = True
                 for child in collection.children:
-                    setup_shadow_catcher_collection(child)
+                    setup_shadow_catcher_objects(child)
 
         scene = context.scene
         fg_coll = bpy.data.collections["foreground", None]