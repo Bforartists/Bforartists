ca827a86460a64e4c4849fe86c4a4488332b9ca4
ReinerBforartists <reiner.prokein@posteo.de>
2021-03-27 12:55:56 +0100

    Separated tooltips - Implement the different tooltips in the original operator #2052 - node editor - select none /select inverse

--- release/scripts/presets/keyconfig/bforartists.py
+++ release/scripts/presets/keyconfig/bforartists.py
@@ -4427,8 +4427,20 @@ keyconfig_data = \
        ],
       },
      ),
-    ("node.select_all_none", {"type": 'A', "value": 'PRESS', "alt": True, "repeat": True}, None),
-    ("node.select_all_inverse", {"type": 'I', "value": 'PRESS', "ctrl": True, "repeat": True}, None),
+    ("node.select_all",
+     {"type": 'A', "value": 'PRESS', "alt": True, "repeat": True},
+     {"properties":
+      [("action", 'DESELECT'),
+       ],
+      },
+     ),
+    ("node.select_all",
+     {"type": 'I', "value": 'PRESS', "ctrl": True, "repeat": True},
+     {"properties":
+      [("action", 'INVERT'),
+       ],
+      },
+     ),
     ("node.select_all",
      {"type": 'A', "value": 'DOUBLE_CLICK'},
      {"properties":
