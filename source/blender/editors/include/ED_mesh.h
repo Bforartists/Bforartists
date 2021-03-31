Bforartists/Bforartists

commit 343230a682623e766218681770106163196b0454

Knife: support vert/edge snapping when not directly over a face
Respect the distance argument to EDBM_face_find_nearest,
when zero, sample a single pixel, otherwise sample a region.

Knife uses the selection-buffer to pick a face when the ray-cast failed.
This was meant to allow snapping to nearby faces however as the margin
was ignored, it was only used in edge cases where the ray-cast missed
but the pixel didn't.

Now the face-picking threshold is working as expected.

Note that other callers to EDBM_face_find_nearest have been updated
so set their distance argument to zero so this only impacts the knife.
Regular selection and path select could be modified separately if users
prefer this behavior.

source/blender/editors/mesh/editmesh_path.c
@@ -646,6 +646,9 @@ static BMElem *edbm_elem_find_nearest(ViewContext *vc, const char htype)
     return (BMElem *)EDBM_edge_find_nearest(vc, &dist);
   }
   if ((em->selectmode & SCE_SELECT_FACE) && (htype == BM_FACE)) {
+    /* Only pick faces directly under the cursor.
+     * We could look into changing this, for now it matches regular face selection. */
+    dist = 0.0f;
     return (BMElem *)EDBM_face_find_nearest(vc, &dist);
   }
 

source/blender/editors/mesh/editmesh_select.c
@@ -715,14 +715,27 @@ BMFace *EDBM_face_find_nearest_ex(ViewContext *vc,
   uint base_index = 0;
 
   if (!XRAY_FLAG_ENABLED(vc->v3d)) {
-    float dist_test = 0.0f;
+    float dist_test;
     uint index;
     BMFace *efa;
 
     {
+      uint dist_px = 0;
+      if (*r_dist != 0.0f) {
+        dist_px = (uint)ED_view3d_backbuf_sample_size_clamp(vc->region, *r_dist);
+      }
+
       DRW_select_buffer_context_create(bases, bases_len, SCE_SELECT_FACE);
 
-      index = DRW_select_buffer_sample_point(vc->depsgraph, vc->region, vc->v3d, vc->mval);
+      if (dist_px == 0) {
+        index = DRW_select_buffer_sample_point(vc->depsgraph, vc->region, vc->v3d, vc->mval);
+        dist_test = 0.0f;
+      }
+      else {
+        index = DRW_select_buffer_find_nearest_to_point(
+            vc->depsgraph, vc->region, vc->v3d, vc->mval, 1, UINT_MAX, &dist_px);
+        dist_test = dist_px;
+      }
 
       if (index) {
         efa = (BMFace *)edbm_select_id_bm_elem_get(bases, index, &base_index);
@@ -875,6 +888,13 @@ static bool unified_findnearest(ViewContext *vc,
   /* no afterqueue (yet), so we check it now, otherwise the em_xxxofs indices are bad */
 
   if ((dist > 0.0f) && (em->selectmode & SCE_SELECT_FACE)) {
+
+    /* Force zero distance, this is a historic exception for faces
+     * as this function didn't originally support using a margin.
+     * Only pick faces directly under the cursor to prevent unexpected changes in behavior.
+     * While this could be changed, take care this isn't causing issues from a user perspective. */
+    dist = 0.0f;
+
     float dist_center = 0.0f;
     float *dist_center_p = (em->selectmode & (SCE_SELECT_EDGE | SCE_SELECT_VERTEX)) ?
                                &dist_center :
@@ -885,6 +905,9 @@ static bool unified_findnearest(ViewContext *vc,
     BMFace *efa_test = EDBM_face_find_nearest_ex(
         vc, &dist, dist_center_p, true, use_cycle, &efa_zbuf, bases, bases_len, &base_index);
 
+    if (efa_test == NULL) {
+      dist = dist_init;
+    }
     if (efa_test && dist_center_p) {
       dist = min_ff(dist_margin, dist_center);
     }

commit 05a48f3aacadd32754cb385790ac989f134f648e

Fix T87080: Select shortest path fails in face mode
Regression in 343230a682623e766218681770106163196b0454.

Unfortunately keeping selection picking behavior as well as
supporting finding the nearest face within a radius requires an
inconsistency between x-ray and back-buffer selection that
doesn't work well using the current arguments.

Resolve by adding an argument that causes the input distance
to be ignored for back-buffer selection.

This is used by selection picking but not the knife tool.

This changes behavior for path-selection in face mode,
which now uses a margin for back-buffer selection.

From my own testing this doesn't seem to be a problem like it could be
for regular selection picking.

source/blender/editors/include/ED_mesh.h
@@ -186,6 +186,7 @@ struct BMEdge *EDBM_edge_find_nearest(struct ViewContext *vc, float *r_dist);
 struct BMFace *EDBM_face_find_nearest_ex(struct ViewContext *vc,
                                          float *r_dist,
                                          float *r_dist_center,
+                                         const bool use_zbuf_single_px,
                                          const bool use_select_bias,
                                          bool use_cycle,
                                          struct BMFace **r_efa_zbuf,

source/blender/editors/mesh/editmesh_path.c
@@ -646,9 +646,6 @@ static BMElem *edbm_elem_find_nearest(ViewContext *vc, const char htype)
     return (BMElem *)EDBM_edge_find_nearest(vc, &dist);
   }
   if ((em->selectmode & SCE_SELECT_FACE) && (htype == BM_FACE)) {
-    /* Only pick faces directly under the cursor.
-     * We could look into changing this, for now it matches regular face selection. */
-    dist = 0.0f;
     return (BMElem *)EDBM_face_find_nearest(vc, &dist);
   }
 

source/blender/editors/mesh/editmesh_select.c
@@ -702,9 +702,17 @@ static void findnearestface__doClosest(void *userData,
   }
 }
 
+/**
+ * \param use_zbuf_single_px: Special case, when using the back-buffer selection,
+ * only use the pixel at `vc->mval` instead of using `r_dist` to search over a larger region.
+ * This is needed because historically selection worked this way for a long time,
+ * however it's reasonable that some callers might want to expand the region too.
+ * So add an argument to do this,
+ */
 BMFace *EDBM_face_find_nearest_ex(ViewContext *vc,
                                   float *r_dist,
                                   float *r_dist_center,
+                                  const bool use_zbuf_single_px,
                                   const bool use_select_bias,
                                   bool use_cycle,
                                   BMFace **r_efa_zbuf,
@@ -721,7 +729,7 @@ BMFace *EDBM_face_find_nearest_ex(ViewContext *vc,
 
     {
       uint dist_px = 0;
-      if (*r_dist != 0.0f) {
+      if (*r_dist != 0.0f && (use_zbuf_single_px == false)) {
         dist_px = (uint)ED_view3d_backbuf_sample_size_clamp(vc->region, *r_dist);
       }
 
@@ -842,7 +850,7 @@ BMFace *EDBM_face_find_nearest_ex(ViewContext *vc,
 BMFace *EDBM_face_find_nearest(ViewContext *vc, float *r_dist)
 {
   Base *base = BKE_view_layer_base_find(vc->view_layer, vc->obact);
-  return EDBM_face_find_nearest_ex(vc, r_dist, NULL, false, false, NULL, &base, 1, NULL);
+  return EDBM_face_find_nearest_ex(vc, r_dist, NULL, false, false, false, NULL, &base, 1, NULL);
 }
 
 #undef FIND_NEAR_SELECT_BIAS
@@ -888,13 +896,6 @@ static bool unified_findnearest(ViewContext *vc,
   /* no afterqueue (yet), so we check it now, otherwise the em_xxxofs indices are bad */
 
   if ((dist > 0.0f) && (em->selectmode & SCE_SELECT_FACE)) {
-
-    /* Force zero distance, this is a historic exception for faces
-     * as this function didn't originally support using a margin.
-     * Only pick faces directly under the cursor to prevent unexpected changes in behavior.
-     * While this could be changed, take care this isn't causing issues from a user perspective. */
-    dist = 0.0f;
-
     float dist_center = 0.0f;
     float *dist_center_p = (em->selectmode & (SCE_SELECT_EDGE | SCE_SELECT_VERTEX)) ?
                                &dist_center :
@@ -903,11 +904,8 @@ static bool unified_findnearest(ViewContext *vc,
     uint base_index = 0;
     BMFace *efa_zbuf = NULL;
     BMFace *efa_test = EDBM_face_find_nearest_ex(
-        vc, &dist, dist_center_p, true, use_cycle, &efa_zbuf, bases, bases_len, &base_index);
+        vc, &dist, dist_center_p, true, true, use_cycle, &efa_zbuf, bases, bases_len, &base_index);
 
-    if (efa_test == NULL) {
-      dist = dist_init;
-    }
     if (efa_test && dist_center_p) {
       dist = min_ff(dist_margin, dist_center);
     }