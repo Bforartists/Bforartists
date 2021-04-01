c2d6f51a41c81a91026d32514d1d55a2fd00b262
ReinerBforartists <reiner.prokein@posteo.de>
2021-04-01 18:07:46 +0200

    Separated tooltips - Implement the different tooltips in the original operator #2052 - uv editor, select none, select inverse

--- release/scripts/presets/keyconfig/Bforartists-macOS.py
+++ release/scripts/presets/keyconfig/Bforartists-macOS.py
@@ -6609,8 +6609,20 @@ keyconfig_data = \
        ],
       },
      ),
-    ("uv.select_all_none", {"type": 'A', "value": 'PRESS', "alt": True, "repeat": True}, None),
-    ("uv.select_all_inverse", {"type": 'I', "value": 'PRESS', "ctrl": True, "repeat": True}, None),
+    ("uv.select_all",
+     {"type": 'A', "value": 'PRESS', "alt": True, "repeat": True},
+     {"properties":
+      [("action", 'DESELECT'),
+       ],
+      },
+     ),
+    ("uv.select_all",
+     {"type": 'I', "value": 'PRESS', "oskey": True, "repeat": True},
+     {"properties":
+      [("action", 'INVERT'),
+       ],
+      },
+     ),
     ("uv.select_all",
      {"type": 'A', "value": 'DOUBLE_CLICK'},
      {"properties":
