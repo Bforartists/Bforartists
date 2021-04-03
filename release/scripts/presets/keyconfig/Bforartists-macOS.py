fecef2afbbceb32fa22724c0c67d03c57fc5e0d2
ReinerBforartists <reiner.prokein@posteo.de>
2021-04-03 11:42:21 +0200

    Separated tooltips - Implement the different tooltips in the original operator #2052 - grease pencil in edit mode, select none, select inverse

--- release/scripts/presets/keyconfig/Bforartists-macOS.py
+++ release/scripts/presets/keyconfig/Bforartists-macOS.py
@@ -2425,8 +2425,20 @@ keyconfig_data = \
        ],
       },
      ),
-    ("gpencil.select_all_none", {"type": 'A', "value": 'PRESS', "alt": True, "repeat": True}, None),
-    ("gpencil.select_all_inverse", {"type": 'I', "value": 'PRESS', "ctrl": True, "repeat": True}, None),
+    ("gpencil.select_all",
+     {"type": 'A', "value": 'PRESS', "alt": True, "repeat": True},
+     {"properties":
+      [("action", 'DESELECT'),
+       ],
+      },
+     ),
+    ("gpencil.select_all",
+     {"type": 'I', "value": 'PRESS', "oskey": True, "repeat": True},
+     {"properties":
+      [("action", 'INVERT'),
+       ],
+      },
+     ),
     ("gpencil.select_all",
      {"type": 'A', "value": 'DOUBLE_CLICK'},
      {"properties":
