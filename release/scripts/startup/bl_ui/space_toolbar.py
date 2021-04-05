557edacda4c1230892167cd119a4403a3fcee99e
ReinerBforartists <reiner.prokein@posteo.de>
2021-04-05 18:00:37 +0200

    Add menu - add new grease pencil types to the toolbar #2299 - missing object type

--- release/scripts/startup/bl_ui/space_toolbar.py
+++ release/scripts/startup/bl_ui/space_toolbar.py
@@ -783,6 +783,7 @@ class TOOLBAR_MT_primitives(Menu):
                 row = layout.row(align=True)
                 row.operator("object.gpencil_add", text="", icon='LINEART_SCENE').type= 'LRT_SCENE'
                 row.operator("object.gpencil_add", text="", icon='LINEART_COLLECTION').type= 'LRT_COLLECTION'
+                row.operator("object.gpencil_add", text="", icon='LINEART_OBJECT').type= 'LRT_OBJECT'
 
             if addon_prefs.primitives_light:
 
