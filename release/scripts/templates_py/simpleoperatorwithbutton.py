0aa639f5aba562a4a5b03bb30cfaf3a981770296
ReinerBforartists <reiner.prokein@posteo.de>
2021-04-13 10:31:29 +0200

    Failing python example scripts #2354

--- release/scripts/templates_py/simpleoperatorwithbutton.py
+++ release/scripts/templates_py/simpleoperatorwithbutton.py
@@ -1,5 +1,5 @@
 # This script is a simple example for an operator that gets called by a button.
-# The button to execute it is in the Properties sidebar of the 3D view, in the View tab. 
+# The button to execute it is in the Properties sidebar of the 3D view, in the View tab.
 # The output of the operator gets printed to the console.
 # You can't read it in the Info editor.
 
@@ -11,27 +11,27 @@ class XX_OT_print_me(bpy.types.Operator):
     bl_idname = "view.print_me"
     bl_label = "Print hotkey"
     bl_options = {'REGISTER', 'UNDO'}
-    
+
     def execute(self, context):
         print("Operator is executing")
         return {'FINISHED'}
-    
+
 # Our Panel
 class XX_PT_printmepanel(bpy.types.Panel):
     bl_label = "Print hotkey"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = "View"
-    
+
     def draw(self, context):
         layout = self.layout
-        
+
         layout.operator(XX_OT_print_me.bl_idname, text="Print")
-    
+
 # Registering our classes. The classes tuple.
 classes = (
     XX_OT_print_me,
-    XX_PT_printmepanel, 
+    XX_PT_printmepanel,
 )
 
 # ------------------- Register Unregister
@@ -44,7 +44,7 @@ def unregister():
     from bpy.utils import unregister_class
     for cls in classes:
        unregister_class(cls)
-    
+
 # For scripting only
 if __name__ == "__main__":
     register()
