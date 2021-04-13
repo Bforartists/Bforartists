543d4ead6669652207290326e6501eda73735dbc
ReinerBforartists <reiner.prokein@posteo.de>
2021-04-13 10:31:29 +0200

    Failing python example scripts #2354

--- release/scripts/templates_py/ui_checkbox_prop_saveable.py
+++ release/scripts/templates_py/ui_checkbox_prop_saveable.py
@@ -1,5 +1,5 @@
-﻿# This script is a simple example for a checkbox prop. It creates a bool.
-# This bool can then be used to do things. 
+# This script is a simple example for a checkbox prop. It creates a bool.
+# This bool can then be used to do things.
 # In our example it changes a text button into an icon button and back.
 # Note that the state of the bool can be stored with saving the startup.blend
 # When you reopen the app, then the checkbox starts with the value that you had when you saved the startup.blend
@@ -18,12 +18,12 @@ class CheckboxpropPanel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        
+
         scene = context.scene # Our data is in the current scene
         layout.prop(scene.my_addon_data, "checkbox_bool") # Our checkbox
-         
-        # Example useage of the flag. 
-        # Our buttons. When the checkbox is off then we have the text buttons. 
+
+        # Example useage of the flag.
+        # Our buttons. When the checkbox is off then we have the text buttons.
         # When the checkbox is on, then we have icon buttons
         if scene.my_addon_data.checkbox_bool:
             layout.label(text="Mesh:")
@@ -38,7 +38,7 @@ class CheckboxpropPanel(bpy.types.Panel):
             row.operator("mesh.primitive_cylinder_add", text="", icon='MESH_CYLINDER')
             row.operator("mesh.primitive_cone_add", text="", icon='MESH_CONE')
             row.operator("mesh.primitive_torus_add", text="", icon='MESH_TORUS')
-            
+
         else:
             col = layout.column(align=True)
             col.label(text="Mesh:")
@@ -49,8 +49,8 @@ class CheckboxpropPanel(bpy.types.Panel):
             col.operator("mesh.primitive_ico_sphere_add", text="Ico Sphere", icon='MESH_ICOSPHERE')
             col.operator("mesh.primitive_cylinder_add", text="Cylinder", icon='MESH_CYLINDER')
             col.operator("mesh.primitive_cone_add", text="Cone", icon='MESH_CONE')
-            col.operator("mesh.primitive_torus_add", text="Torus", icon='MESH_TORUS')        
-    
+            col.operator("mesh.primitive_torus_add", text="Torus", icon='MESH_TORUS')
+
 def register():
     bpy.utils.register_class(CheckboxpropPanel)
     #Our data block
@@ -61,6 +61,6 @@ def unregister():
     del bpy.types.Scene.my_addon_data # Unregister our data block when unregister.
     bpy.utils.unregister_class(MyData) # Our data block
     bpy.utils.unregister_class(CheckboxpropPanel)
-    
+
 if __name__ == "__main__":
     register()
