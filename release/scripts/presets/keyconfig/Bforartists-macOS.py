549d526396a18d98261f0080d18d127852327c89
ReinerBforartists <reiner.prokein@posteo.de>
2021-04-13 17:56:20 +0200

    Hotkeys - Reset to Default with hover + Backspace missing #2343

--- release/scripts/presets/keyconfig/Bforartists-macOS.py
+++ release/scripts/presets/keyconfig/Bforartists-macOS.py
@@ -7020,6 +7020,13 @@ keyconfig_data = \
     ("anim.driver_button_remove", {"type": 'D', "value": 'PRESS', "oskey": True, "alt": True}, None),
     ("anim.keyingset_button_add", {"type": 'K', "value": 'PRESS'}, None),
     ("anim.keyingset_button_remove", {"type": 'K', "value": 'PRESS', "alt": True}, None),
+    ("ui.reset_default_button",
+     {"type": 'BACK_SPACE', "value": 'PRESS'},
+     {"properties":
+      [("all", True),
+       ],
+      },
+     ),
     ],
    },
   ),
