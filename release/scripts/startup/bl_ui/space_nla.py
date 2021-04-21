5e634bedb158a10ac2b67c6386d9f6d005be5143
ReinerBforartists <reiner.prokein@posteo.de>
2021-04-21 08:55:36 +0200

    Add a show / hide prop for the channel list in the view menus of the animation editors #1569

--- release/scripts/startup/bl_ui/space_nla.py
+++ release/scripts/startup/bl_ui/space_nla.py
@@ -152,6 +152,7 @@ class NLA_MT_view(Menu):
 
         st = context.space_data
 
+        layout.prop(st, "show_region_channels") # bfa - channels
         layout.prop(st, "show_region_ui")
 
         layout.separator()
