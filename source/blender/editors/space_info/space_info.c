Bforartists/Bforartists

commit f594b84ab164d9824c2ee76a790ff3ede3f8a22a

Fix build issue due to previous commit
Revert in rB05dddb71b098 did a poor job and left some
rubbish.

source/blender/editors/space_info/space_info.c
@@ -255,7 +255,7 @@ static void info_header_region_message_subscribe(const wmRegionMessageSubscribeP
   struct wmMsgBus *mbus = params->message_bus;
   ARegion *region = params->region;
 
-  wmMsgSubscribeValue msg_sub_value_region_tag_redraw{};
+  wmMsgSubscribeValue msg_sub_value_region_tag_redraw;
   msg_sub_value_region_tag_redraw.owner = region;
   msg_sub_value_region_tag_redraw.user_data = region;
   msg_sub_value_region_tag_redraw.notify = ED_region_do_msg_notify_tag_redraw;

commit 4a0fe17053ae62c24841c948e294aeac162544a4

Fix T87799: Crash when switching to Info Editor
Use of uninitialized variable.

Problem introduced in f594b84ab164d9824c2ee76a790ff3ede3f8a22a.

source/blender/editors/space_info/space_info.c
@@ -255,7 +255,7 @@ static void info_header_region_message_subscribe(const wmRegionMessageSubscribeP
   struct wmMsgBus *mbus = params->message_bus;
   ARegion *region = params->region;
 
-  wmMsgSubscribeValue msg_sub_value_region_tag_redraw;
+  wmMsgSubscribeValue msg_sub_value_region_tag_redraw = {NULL};
   msg_sub_value_region_tag_redraw.owner = region;
   msg_sub_value_region_tag_redraw.user_data = region;
   msg_sub_value_region_tag_redraw.notify = ED_region_do_msg_notify_tag_redraw;