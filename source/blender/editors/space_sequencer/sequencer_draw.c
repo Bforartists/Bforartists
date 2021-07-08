Bforartists/Bforartists

commit ff9f8b98dcd82ed30bcf546260a0a35420a64d7e

VSE: Fix cache bar not visible
Cache bar was only visible when Frame overlay was enabled

Reviewed By: jbakker

Differential Revision: https://developer.blender.org/D11779

source/blender/editors/space_sequencer/sequencer_draw.c
@@ -2479,12 +2479,13 @@ void draw_timeline_seq_display(const bContext *C, ARegion *region)
   const SpaceSeq *sseq = CTX_wm_space_seq(C);
   View2D *v2d = &region->v2d;
 
+  UI_view2d_view_ortho(v2d);
+  draw_cache_view(C);
+
   if (scene->ed && scene->ed->over_flag & SEQ_EDIT_OVERLAY_SHOW) {
-    UI_view2d_view_ortho(v2d);
-    draw_cache_view(C);
     draw_overlap_frame_indicator(scene, v2d);
-    UI_view2d_view_restore(C);
   }
+  UI_view2d_view_restore(C);
 
   ED_time_scrub_draw_current_frame(region, scene, !(sseq->flag & SEQ_DRAWFRAMES), true);
   UI_view2d_scrollers_draw(v2d, NULL);

commit 1da6d829b66da812c565542ea25b56317f77b049

Fix crash displaying the sequencer without a valid 'scene->ed'
Regression in ff9f8b98dcd82ed30bcf546260a0a35420a64d7e

source/blender/editors/space_sequencer/sequencer_draw.c
@@ -2479,13 +2479,14 @@ void draw_timeline_seq_display(const bContext *C, ARegion *region)
   const SpaceSeq *sseq = CTX_wm_space_seq(C);
   View2D *v2d = &region->v2d;
 
-  UI_view2d_view_ortho(v2d);
-  draw_cache_view(C);
-
-  if (scene->ed && scene->ed->over_flag & SEQ_EDIT_OVERLAY_SHOW) {
-    draw_overlap_frame_indicator(scene, v2d);
+  if (scene->ed != NULL) {
+    UI_view2d_view_ortho(v2d);
+    draw_cache_view(C);
+    if (scene->ed->over_flag & SEQ_EDIT_OVERLAY_SHOW) {
+      draw_overlap_frame_indicator(scene, v2d);
+    }
+    UI_view2d_view_restore(C);
   }
-  UI_view2d_view_restore(C);
 
   ED_time_scrub_draw_current_frame(region, scene, !(sseq->flag & SEQ_DRAWFRAMES), true);
   UI_view2d_scrollers_draw(v2d, NULL);