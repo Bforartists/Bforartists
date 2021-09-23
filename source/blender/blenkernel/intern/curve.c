Bforartists/Bforartists

commit 19a868852b42b329d15200365b5b71a6087f1bfd

DNA: defaults for ID types

source/blender/blenkernel/intern/curve.c
@@ -36,6 +36,7 @@
 #include "DNA_anim_types.h"
 #include "DNA_curve_types.h"
 #include "DNA_material_types.h"
+#include "DNA_defaults.h"
 
 /* for dereferencing pointers */
 #include "DNA_key_types.h"
@@ -145,28 +146,13 @@ void BKE_curve_free(Curve *cu)
   MEM_SAFE_FREE(cu->tb);
 }
 
-void BKE_curve_init(Curve *cu)
+void BKE_curve_init(Curve *cu, const short curve_type)
 {
-  /* BLI_assert(MEMCMP_STRUCT_AFTER_IS_ZERO(cu, id)); */ /* cu->type is already initialized... */
+  BLI_assert(MEMCMP_STRUCT_AFTER_IS_ZERO(cu, id));
 
-  copy_v3_fl(cu->size, 1.0f);
-  cu->flag = CU_DEFORM_BOUNDS_OFF | CU_PATH_RADIUS;
-  cu->pathlen = 100;
-  cu->resolu = cu->resolv = (cu->type == OB_SURF) ? 4 : 12;
-  cu->width = 1.0;
-  cu->wordspace = 1.0;
-  cu->spacing = cu->linedist = 1.0;
-  cu->fsize = 1.0;
-  cu->ulheight = 0.05;
-  cu->texflag = CU_AUTOSPACE;
-  cu->smallcaps_scale = 0.75f;
-  /* XXX: this one seems to be the best one in most cases, at least for curve deform... */
-  cu->twist_mode = CU_TWIST_MINIMUM;
-  cu->bevfac1 = 0.0f;
-  cu->bevfac2 = 1.0f;
-  cu->bevfac1_mapping = CU_BEVFAC_MAP_RESOLU;
-  cu->bevfac2_mapping = CU_BEVFAC_MAP_RESOLU;
-  cu->bevresol = 4;
+  MEMCPY_STRUCT_AFTER(cu, DNA_struct_default_get(Curve), id);
+
+  cu->type = curve_type;
 
   cu->bb = BKE_boundbox_alloc_unit();
 
@@ -182,6 +168,9 @@ void BKE_curve_init(Curve *cu)
     cu->tb = MEM_calloc_arrayN(MAXTEXTBOX, sizeof(TextBox), "textbox");
     cu->tb[0].w = cu->tb[0].h = 0.0;
   }
+  else if (cu->type == OB_SURF) {
+    cu->resolv = 4;
+  }
 }
 
 Curve *BKE_curve_add(Main *bmain, const char *name, int type)
@@ -189,9 +178,8 @@ Curve *BKE_curve_add(Main *bmain, const char *name, int type)
   Curve *cu;
 
   cu = BKE_libblock_alloc(bmain, ID_CU, name, 0);
-  cu->type = type;
 
-  BKE_curve_init(cu);
+  BKE_curve_init(cu, type);
 
   return cu;
 }

source/blender/blenkernel/BKE_curve.h
@@ -72,7 +72,7 @@ typedef struct CVKeyIndex {
 /* ** Curve ** */
 void BKE_curve_free(struct Curve *cu);
 void BKE_curve_editfont_free(struct Curve *cu);
-void BKE_curve_init(struct Curve *cu);
+void BKE_curve_init(struct Curve *cu, const short curve_type);
 struct Curve *BKE_curve_add(struct Main *bmain, const char *name, int type);
 void BKE_curve_copy_data(struct Main *bmain,
                          struct Curve *cu_dst,

source/blender/blenkernel/intern/brush.c
@@ -24,6 +24,7 @@
 #include "DNA_scene_types.h"
 #include "DNA_object_types.h"
 #include "DNA_gpencil_types.h"
+#include "DNA_defaults.h"
 
 #include "BLI_math.h"
 #include "BLI_rand.h"
@@ -60,74 +61,48 @@ void BKE_brush_system_exit(void)
 
 static void brush_defaults(Brush *brush)
 {
-  brush->blend = 0;
-  brush->flag = 0;
 
-  brush->ob_mode = OB_MODE_ALL_PAINT;
+  const Brush *brush_def = DNA_struct_default_get(Brush);
 
-  /* BRUSH SCULPT TOOL SETTINGS */
-  brush->weight = 1.0f; /* weight of brush 0 - 1.0 */
-  brush->size = 35;     /* radius of the brush in pixels */
-  brush->alpha = 0.5f;  /* brush strength/intensity probably variable should be renamed? */
-  brush->autosmooth_factor = 0.0f;
-  brush->topology_rake_factor = 0.0f;
-  brush->crease_pinch_factor = 0.5f;
-  brush->normal_radius_factor = 0.5f;
-  brush->sculpt_plane = SCULPT_DISP_DIR_AREA;
-  /* How far above or below the plane that is found by averaging the faces. */
-  brush->plane_offset = 0.0f;
-  brush->plane_trim = 0.5f;
-  brush->clone.alpha = 0.5f;
-  brush->normal_weight = 0.0f;
-  brush->fill_threshold = 0.2f;
-  brush->flag |= BRUSH_ALPHA_PRESSURE;
+#define FROM_DEFAULT(member) memcpy(&brush->member, &brush_def->member, sizeof(brush->member))
+#define FROM_DEFAULT_PTR(member) memcpy(brush->member, brush_def->member, sizeof(brush->member))
 
-  /* BRUSH PAINT TOOL SETTINGS */
-  /* Default rgb color of the brush when painting - white. */
-  brush->rgb[0] = 1.0f;
-  brush->rgb[1] = 1.0f;
-  brush->rgb[2] = 1.0f;
+  FROM_DEFAULT(blend);
+  FROM_DEFAULT(flag);
+  FROM_DEFAULT(weight);
+  FROM_DEFAULT(size);
+  FROM_DEFAULT(alpha);
+  FROM_DEFAULT(autosmooth_factor);
+  FROM_DEFAULT(topology_rake_factor);
+  FROM_DEFAULT(crease_pinch_factor);
+  FROM_DEFAULT(normal_radius_factor);
+  FROM_DEFAULT(sculpt_plane);
+  FROM_DEFAULT(plane_offset);
+  FROM_DEFAULT(clone.alpha);
+  FROM_DEFAULT(normal_weight);
+  FROM_DEFAULT(fill_threshold);
+  FROM_DEFAULT(flag);
+  FROM_DEFAULT_PTR(rgb);
+  FROM_DEFAULT_PTR(secondary_rgb);
+  FROM_DEFAULT(spacing);
+  FROM_DEFAULT(smooth_stroke_radius);
+  FROM_DEFAULT(smooth_stroke_factor);
+  FROM_DEFAULT(rate);
+  FROM_DEFAULT(jitter);
+  FROM_DEFAULT(texture_sample_bias);
+  FROM_DEFAULT(texture_overlay_alpha);
+  FROM_DEFAULT(mask_overlay_alpha);
+  FROM_DEFAULT(cursor_overlay_alpha);
+  FROM_DEFAULT(overlay_flags);
+  FROM_DEFAULT_PTR(add_col);
+  FROM_DEFAULT_PTR(sub_col);
+  FROM_DEFAULT(stencil_pos);
+  FROM_DEFAULT(stencil_dimension);
+  FROM_DEFAULT(mtex);
+  FROM_DEFAULT(mask_mtex);
 
-  zero_v3(brush->secondary_rgb);
-
-  /* BRUSH STROKE SETTINGS */
-  brush->flag |= (BRUSH_SPACE | BRUSH_SPACE_ATTEN);
-  /* How far each brush dot should be spaced as a percentage of brush diameter. */
-  brush->spacing = 10;
-
-  brush->smooth_stroke_radius = 75;
-  brush->smooth_stroke_factor = 0.9f;
-
-  /* Time delay between dots of paint or sculpting when doing airbrush mode. */
-  brush->rate = 0.1f;
-
-  brush->jitter = 0.0f;
-
-  /* BRUSH TEXTURE SETTINGS */
-  BKE_texture_mtex_default(&brush->mtex);
-  BKE_texture_mtex_default(&brush->mask_mtex);
-
-  brush->texture_sample_bias = 0; /* value to added to texture samples */
-  brush->texture_overlay_alpha = 33;
-  brush->mask_overlay_alpha = 33;
-  brush->cursor_overlay_alpha = 33;
-  brush->overlay_flags = 0;
-
-  /* brush appearance  */
-
-  brush->add_col[0] = 1.00; /* add mode color is light red */
-  brush->add_col[1] = 0.39;
-  brush->add_col[2] = 0.39;
-
-  brush->sub_col[0] = 0.39; /* subtract mode color is light blue */
-  brush->sub_col[1] = 0.39;
-  brush->sub_col[2] = 1.00;
-
-  brush->stencil_pos[0] = 256;
-  brush->stencil_pos[1] = 256;
-
-  brush->stencil_dimension[0] = 256;
-  brush->stencil_dimension[1] = 256;
+#undef FROM_DEFAULT
+#undef FROM_DEFAULT_PTR
 }
 
 /* Datablock add/copy/free/make_local */
@@ -136,15 +111,10 @@ void BKE_brush_init(Brush *brush)
 {
   BLI_assert(MEMCMP_STRUCT_AFTER_IS_ZERO(brush, id));
 
+  MEMCPY_STRUCT_AFTER(brush, DNA_struct_default_get(Brush), id);
+
   /* enable fake user by default */
   id_fake_user_set(&brush->id);
-
-  brush_defaults(brush);
-
-  brush->sculpt_tool = SCULPT_TOOL_DRAW; /* sculpting defaults to the draw tool for new brushes */
-
-  /* A kernel radius of 1 has almost no effect (T63233). */
-  brush->blur_kernel_radius = 2;
 
   /* the default alpha falloff curve */
   BKE_brush_curve_preset(brush, CURVE_PRESET_SMOOTH);

source/blender/blenkernel/intern/camera.c
@@ -30,6 +30,7 @@
 #include "DNA_scene_types.h"
 #include "DNA_view3d_types.h"
 #include "DNA_ID.h"
+#include "DNA_defaults.h"
 
 #include "BLI_math.h"
 #include "BLI_listbase.h"
@@ -56,25 +57,7 @@ void BKE_camera_init(Camera *cam)
 {
   BLI_assert(MEMCMP_STRUCT_AFTER_IS_ZERO(cam, id));
 
-  cam->lens = 50.0f;
-  cam->sensor_x = DEFAULT_SENSOR_WIDTH;
-  cam->sensor_y = DEFAULT_SENSOR_HEIGHT;
-  cam->clip_start = 0.1f;
-  cam->clip_end = 1000.0f;
-  cam->drawsize = 1.0f;
-  cam->ortho_scale = 6.0;
-  cam->flag |= CAM_SHOWPASSEPARTOUT;
-  cam->passepartalpha = 0.5f;
-
-  cam->dof.aperture_fstop = 2.8f;
-  cam->dof.aperture_ratio = 1.0f;
-  cam->dof.focus_distance = 10.0f;
-
-  /* stereoscopy 3d */
-  cam->stereo.interocular_distance = 0.065f;
-  cam->stereo.convergence_distance = 30.f * 0.065f;
-  cam->stereo.pole_merge_angle_from = DEG2RADF(60.0f);
-  cam->stereo.pole_merge_angle_to = DEG2RADF(75.0f);
+  MEMCPY_STRUCT_AFTER(cam, DNA_struct_default_get(Camera), id);
 }
 
 void *BKE_camera_add(Main *bmain, const char *name)

source/blender/blenkernel/intern/image.c
@@ -57,6 +57,7 @@
 #include "DNA_meshdata_types.h"
 #include "DNA_light_types.h"
 #include "DNA_world_types.h"
+#include "DNA_defaults.h"
 
 #include "BLI_blenlib.h"
 #include "BLI_math_vector.h"
@@ -298,10 +299,7 @@ static void image_init(Image *ima, short source, short type)
 
   ima->ok = IMA_OK;
 
-  ima->aspx = ima->aspy = 1.0;
-  ima->gen_x = 1024;
-  ima->gen_y = 1024;
-  ima->gen_type = IMA_GENTYPE_GRID;
+  MEMCPY_STRUCT_AFTER(ima, DNA_struct_default_get(Image), id);
 
   ima->source = source;
   ima->type = type;
@@ -318,8 +316,6 @@ static void image_init(Image *ima, short source, short type)
 
   BKE_color_managed_colorspace_settings_init(&ima->colorspace_settings);
   ima->stereo3d_format = MEM_callocN(sizeof(Stereo3dFormat), "Image Stereo Format");
-
-  ima->gpuframenr = INT_MAX;
 }
 
 void BKE_image_init(struct Image *image)

source/blender/blenkernel/intern/lattice.c
@@ -41,6 +41,7 @@
 #include "DNA_lattice_types.h"
 #include "DNA_curve_types.h"
 #include "DNA_key_types.h"
+#include "DNA_defaults.h"
 
 #include "BKE_animsys.h"
 #include "BKE_anim.h"
@@ -248,13 +249,10 @@ void BKE_lattice_init(Lattice *lt)
 {
   BLI_assert(MEMCMP_STRUCT_AFTER_IS_ZERO(lt, id));
 
-  lt->flag = LT_GRID;
-
-  lt->typeu = lt->typev = lt->typew = KEY_BSPLINE;
+  MEMCPY_STRUCT_AFTER(lt, DNA_struct_default_get(Lattice), id);
 
   lt->def = MEM_callocN(sizeof(BPoint), "lattvert"); /* temporary */
   BKE_lattice_resize(lt, 2, 2, 2, NULL);             /* creates a uniform lattice */
-  lt->actbp = LT_ACTBP_NONE;
 }
 
 Lattice *BKE_lattice_add(Main *bmain, const char *name)

source/blender/blenkernel/intern/library.c
@@ -1271,7 +1271,7 @@ void BKE_libblock_init_empty(ID *id)
       BKE_mesh_init((Mesh *)id);
       break;
     case ID_CU:
-      BKE_curve_init((Curve *)id);
+      BKE_curve_init((Curve *)id, 0);
       break;
     case ID_MB:
       BKE_mball_init((MetaBall *)id);

source/blender/blenkernel/intern/light.c
@@ -32,6 +32,7 @@
 #include "DNA_object_types.h"
 #include "DNA_scene_types.h"
 #include "DNA_texture_types.h"
+#include "DNA_defaults.h"
 
 #include "BLI_math.h"
 #include "BLI_utildefines.h"
@@ -48,40 +49,9 @@ void BKE_light_init(Light *la)
 {
   BLI_assert(MEMCMP_STRUCT_AFTER_IS_ZERO(la, id));
 
-  la->r = la->g = la->b = la->k = 1.0f;
-  la->energy = 10.0f;
-  la->dist = 25.0f;
-  la->spotsize = DEG2RADF(45.0f);
-  la->spotblend = 0.15f;
-  la->att2 = 1.0f;
-  la->mode = LA_SHADOW;
-  la->bufsize = 512;
-  la->clipsta = 0.05f;
-  la->clipend = 40.0f;
-  la->bleedexp = 2.5f;
-  la->samp = 3;
-  la->bias = 1.0f;
-  la->soft = 3.0f;
-  la->area_size = la->area_sizey = la->area_sizez = 0.25f;
-  la->buffers = 1;
-  la->preview = NULL;
-  la->falloff_type = LA_FALLOFF_INVSQUARE;
-  la->coeff_const = 1.0f;
-  la->coeff_lin = 0.0f;
-  la->coeff_quad = 0.0f;
-  la->curfalloff = BKE_curvemapping_add(1, 0.0f, 1.0f, 1.0f, 0.0f);
-  la->cascade_max_dist = 200.0f;
-  la->cascade_count = 4;
-  la->cascade_exponent = 0.8f;
-  la->cascade_fade = 0.1f;
-  la->contact_dist = 0.2f;
-  la->contact_bias = 0.03f;
-  la->contact_spread = 0.2f;
-  la->contact_thickness = 0.2f;
-  la->spec_fac = 1.0f;
-  la->att_dist = 40.0f;
-  la->sun_angle = DEG2RADF(0.526f);
+  MEMCPY_STRUCT_AFTER(la, DNA_struct_default_get(Light), id);
 
+  la->curfalloff = BKE_curvemapping_add(1, 0.0f, 1.0f, 1.0f, 0.0f);
   BKE_curvemapping_initialize(la->curfalloff);
 }
 

commit 2b4c2d16aa3492556a63a6c0a4322d252f914cb2

Fix default surface resolution U/V mis-match
The resolution for surfaces was 12 for U, 4 for V,
where both should have been set to 4.

Regression in 19a868852b42b329d15200365b5b71a6087f1bfd

source/blender/blenkernel/intern/curve.c
@@ -404,6 +404,7 @@ void BKE_curve_init(Curve *cu, const short curve_type)
   }
   else if (cu->type == OB_SURF) {
     cu->flag |= CU_3D;
+    cu->resolu = 4;
     cu->resolv = 4;
   }
   cu->bevel_profile = NULL;