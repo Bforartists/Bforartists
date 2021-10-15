34c60b205a61599fe36a9f38ff54d112ed7f9689
ReinerBforartists <reiner.prokein@posteo.de>
2021-10-15 11:07:01 +0200

    Grease pencil in edit mode - expose set handle type menu to the regular UI #2714

--- source/blender/editors/gpencil/gpencil_edit_curve.c
+++ source/blender/editors/gpencil/gpencil_edit_curve.c
@@ -51,6 +51,9 @@
 
 #include "gpencil_intern.h"
 
+#include "UI_interface.h" /*bfa - for the icons*/
+#include "UI_resources.h" /*bfa - for the icons*/
+
 /* Poll callback for checking if there is an active layer and we are in curve edit mode. */
 static bool gpencil_curve_edit_mode_poll(bContext *C)
 {
@@ -184,10 +187,10 @@ static int gpencil_editcurve_set_handle_type_exec(bContext *C, wmOperator *op)
 void GPENCIL_OT_stroke_editcurve_set_handle_type(wmOperatorType *ot)
 {
   static const EnumPropertyItem editcurve_handle_type_items[] = {
-      {HD_FREE, "FREE", 0, "Free", ""},
-      {HD_AUTO, "AUTOMATIC", 0, "Automatic", ""},
-      {HD_VECT, "VECTOR", 0, "Vector", ""},
-      {HD_ALIGN, "ALIGNED", 0, "Aligned", ""},
+      {HD_FREE, "FREE", ICON_HANDLE_FREE, "Free", ""},
+      {HD_AUTO, "AUTOMATIC", ICON_HANDLE_AUTO, "Automatic", ""},
+      {HD_VECT, "VECTOR", ICON_HANDLE_VECTOR, "Vector", ""},
+      {HD_ALIGN, "ALIGNED", ICON_HANDLE_ALIGNED, "Aligned", ""},
       {0, NULL, 0, NULL, NULL},
   };
 
