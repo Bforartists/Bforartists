Bforartists/Bforartists

commit 65c81f53fe628c4d781ca80d13a171fea7fe4909

Cleanup: Remove unused headers in asset files
Also move system includes first, like we have it elsewhere in Blender.

source/blender/blenkernel/intern/asset_library_service.cc
@@ -20,15 +20,10 @@
 
 #include "asset_library_service.hh"
 
-#include "BKE_asset_library.hh"
 #include "BKE_blender.h"
-#include "BKE_callbacks.h"
 
-#include "BLI_fileops.h"
 #include "BLI_path_util.h"
 #include "BLI_string_ref.hh"
-
-#include "MEM_guardedalloc.h"
 
 #include "CLG_log.h"
 

source/blender/blenkernel/intern/asset.cc
@@ -21,14 +21,12 @@
 #include <cstring>
 
 #include "DNA_ID.h"
-#include "DNA_asset_types.h"
 #include "DNA_defaults.h"
 
 #include "BLI_listbase.h"
 #include "BLI_string.h"
 #include "BLI_string_ref.hh"
 #include "BLI_string_utils.h"
-#include "BLI_utildefines.h"
 #include "BLI_uuid.h"
 
 #include "BKE_asset.h"

source/blender/blenkernel/intern/asset_catalog.cc
@@ -18,24 +18,19 @@
  * \ingroup bke
  */
 
+#include <fstream>
+#include <set>
+
 #include "BKE_asset_catalog.hh"
 #include "BKE_asset_library.h"
-#include "BKE_preferences.h"
 
 #include "BLI_fileops.h"
 #include "BLI_path_util.h"
-#include "BLI_set.hh"
-#include "BLI_string_ref.hh"
-
-#include "DNA_userdef_types.h"
 
 /* For S_ISREG() and S_ISDIR() on Windows. */
 #ifdef WIN32
 #  include "BLI_winstuff.h"
 #endif
-
-#include <fstream>
-#include <set>
 
 namespace blender::bke {
 

source/blender/blenkernel/intern/asset_library.cc
@@ -18,9 +18,9 @@
  * \ingroup bke
  */
 
-#include "BKE_asset_catalog.hh"
+#include <memory>
+
 #include "BKE_asset_library.hh"
-#include "BKE_callbacks.h"
 #include "BKE_main.h"
 #include "BKE_preferences.h"
 
@@ -29,11 +29,7 @@
 #include "DNA_asset_types.h"
 #include "DNA_userdef_types.h"
 
-#include "MEM_guardedalloc.h"
-
 #include "asset_library_service.hh"
-
-#include <memory>
 
 bool blender::bke::AssetLibrary::save_catalogs_when_file_is_saved = true;
 

source/blender/editors/asset/intern/asset_catalog.cc
@@ -19,7 +19,6 @@
  */
 
 #include "BKE_asset_catalog.hh"
-#include "BKE_asset_catalog_path.hh"
 #include "BKE_asset_library.hh"
 #include "BKE_main.h"
 

source/blender/editors/asset/intern/asset_filter.cc
@@ -22,7 +22,6 @@
 
 #include "BLI_listbase.h"
 
-#include "DNA_ID.h"
 #include "DNA_asset_types.h"
 
 #include "ED_asset_filter.h"

source/blender/editors/asset/intern/asset_handle.cc
@@ -26,7 +26,6 @@
 
 #include <string>
 
-#include "DNA_asset_types.h"
 #include "DNA_space_types.h"
 
 #include "BLO_readfile.h"

source/blender/editors/asset/intern/asset_library_reference_enum.cc
@@ -27,7 +27,6 @@
 
 #include "BKE_preferences.h"
 
-#include "DNA_asset_types.h"
 #include "DNA_userdef_types.h"
 
 #include "UI_resources.h"

commit 1eaad3c5b0345896cccb76c343598880e95ea384

Fix compile error on Windows
Caused by 65c81f53fe62. This undid part of f243021f3004. Added a comment
for why this include is needed, to avoid this error from happening
again.

source/blender/blenkernel/intern/asset_library_service.cc
@@ -22,6 +22,7 @@
 
 #include "BKE_blender.h"
 
+#include "BLI_fileops.h" /* For PATH_MAX (at least on Windows). */
 #include "BLI_path_util.h"
 #include "BLI_string_ref.hh"
 

source/blender/blenkernel/intern/asset_library_service_test.cc
@@ -19,7 +19,7 @@
 
 #include "asset_library_service.hh"
 
-#include "BLI_fileops.h"
+#include "BLI_fileops.h" /* For PATH_MAX (at least on Windows). */
 #include "BLI_path_util.h"
 
 #include "BKE_appdir.h"