Bforartists/Bforartists

commit b6f83a1b5b5204bca94e5f53e1457efb9ae88da6

DNA: fix asan warning when casting types
Reviewers: brecht

Differential Revision: https://developer.blender.org/D8865

source/blender/makesdna/intern/dna_genfile.c
@@ -805,83 +805,91 @@ static eSDNA_Type sdna_type_nr(const char *dna_type)
 static void cast_elem(
     const char *ctype, const char *otype, int name_array_len, char *curdata, const char *olddata)
 {
-  double val = 0.0;
-  int curlen = 1, oldlen = 1;
-
   eSDNA_Type ctypenr, otypenr;
-
   if ((otypenr = sdna_type_nr(otype)) == -1 || (ctypenr = sdna_type_nr(ctype)) == -1) {
     return;
   }
 
   /* define lengths */
-  oldlen = DNA_elem_type_size(otypenr);
-  curlen = DNA_elem_type_size(ctypenr);
+  const int oldlen = DNA_elem_type_size(otypenr);
+  const int curlen = DNA_elem_type_size(ctypenr);
+
+  double old_value_f = 0.0;
+  uint64_t old_value_i = 0;
 
   while (name_array_len > 0) {
     switch (otypenr) {
       case SDNA_TYPE_CHAR:
-        val = *olddata;
+        old_value_i = *olddata;
+        old_value_f = (double)old_value_i;
         break;
       case SDNA_TYPE_UCHAR:
-        val = *((unsigned char *)olddata);
+        old_value_i = *((unsigned char *)olddata);
+        old_value_f = (double)old_value_i;
         break;
       case SDNA_TYPE_SHORT:
-        val = *((short *)olddata);
+        old_value_i = *((short *)olddata);
+        old_value_f = (double)old_value_i;
         break;
       case SDNA_TYPE_USHORT:
-        val = *((unsigned short *)olddata);
+        old_value_i = *((unsigned short *)olddata);
+        old_value_f = (double)old_value_i;
         break;
       case SDNA_TYPE_INT:
-        val = *((int *)olddata);
+        old_value_i = *((int *)olddata);
+        old_value_f = (double)old_value_i;
         break;
       case SDNA_TYPE_FLOAT:
-        val = *((float *)olddata);
+        old_value_f = *((float *)olddata);
+        old_value_i = (uint64_t)(int64_t)old_value_f;
         break;
       case SDNA_TYPE_DOUBLE:
-        val = *((double *)olddata);
+        old_value_f = *((double *)olddata);
+        old_value_i = (uint64_t)(int64_t)old_value_f;
         break;
       case SDNA_TYPE_INT64:
-        val = *((int64_t *)olddata);
+        old_value_i = (uint64_t) * ((int64_t *)olddata);
+        old_value_f = (double)old_value_i;
         break;
       case SDNA_TYPE_UINT64:
-        val = *((uint64_t *)olddata);
+        old_value_i = *((uint64_t *)olddata);
+        old_value_f = (double)old_value_i;
         break;
     }
 
     switch (ctypenr) {
       case SDNA_TYPE_CHAR:
-        *curdata = val;
+        *curdata = (char)old_value_i;
         break;
       case SDNA_TYPE_UCHAR:
-        *((unsigned char *)curdata) = val;
+        *((unsigned char *)curdata) = (unsigned char)old_value_i;
         break;
       case SDNA_TYPE_SHORT:
-        *((short *)curdata) = val;
+        *((short *)curdata) = (short)old_value_i;
         break;
       case SDNA_TYPE_USHORT:
-        *((unsigned short *)curdata) = val;
+        *((unsigned short *)curdata) = (unsigned short)old_value_i;
         break;
       case SDNA_TYPE_INT:
-        *((int *)curdata) = val;
+        *((int *)curdata) = (int)old_value_i;
         break;
       case SDNA_TYPE_FLOAT:
         if (otypenr < 2) {
-          val /= 255;
+          old_value_f /= 255.0;
         }
-        *((float *)curdata) = val;
+        *((float *)curdata) = old_value_f;
         break;
       case SDNA_TYPE_DOUBLE:
         if (otypenr < 2) {
-          val /= 255;
+          old_value_f /= 255.0;
         }
-        *((double *)curdata) = val;
+        *((double *)curdata) = old_value_f;
         break;
       case SDNA_TYPE_INT64:
-        *((int64_t *)curdata) = val;
+        *((int64_t *)curdata) = (int64_t)old_value_i;
         break;
       case SDNA_TYPE_UINT64:
-        *((uint64_t *)curdata) = val;
+        *((uint64_t *)curdata) = old_value_i;
         break;
     }
 

commit 22dd8a4ea19f03b45ca5896a4432d6f8e8eef20c

Fix dna_genfile error converting signed int types to floating point
Regression in b6f83a1b5b5204bca94e5f53e1457efb9ae88da6
caused negative values to overflow into a uint64_t.

Resolve by casting to a signed int from originally signed types.

This caused D14033 not to work properly.

source/blender/makesdna/intern/dna_genfile.c
@@ -733,6 +733,9 @@ static void cast_primitive_type(const eSDNA_Type old_type,
   const int curlen = DNA_elem_type_size(new_type);
 
   double old_value_f = 0.0;
+  /* Intentionally overflow signed values into an unsigned type.
+   * Casting back to a signed value preserves the sign (when the new value is signed).
+   * It's also important to cast to `int64_t` when setting `old_value_f` from a signed value. */
   uint64_t old_value_i = 0;
 
   for (int a = 0; a < array_len; a++) {
@@ -752,7 +755,7 @@ static void cast_primitive_type(const eSDNA_Type old_type,
       case SDNA_TYPE_SHORT: {
         const short value = *((short *)old_data);
         old_value_i = value;
-        old_value_f = (double)old_value_i;
+        old_value_f = (double)(int64_t)old_value_i;
         break;
       }
       case SDNA_TYPE_USHORT: {
@@ -764,7 +767,7 @@ static void cast_primitive_type(const eSDNA_Type old_type,
       case SDNA_TYPE_INT: {
         const int value = *((int *)old_data);
         old_value_i = value;
-        old_value_f = (double)old_value_i;
+        old_value_f = (double)(int64_t)old_value_i;
         break;
       }
       case SDNA_TYPE_FLOAT: {
@@ -782,7 +785,7 @@ static void cast_primitive_type(const eSDNA_Type old_type,
       case SDNA_TYPE_INT64: {
         const int64_t value = *((int64_t *)old_data);
         old_value_i = (uint64_t)value;
-        old_value_f = (double)old_value_i;
+        old_value_f = (double)(int64_t)old_value_i;
         break;
       }
       case SDNA_TYPE_UINT64: {
@@ -794,7 +797,7 @@ static void cast_primitive_type(const eSDNA_Type old_type,
       case SDNA_TYPE_INT8: {
         const int8_t value = *((int8_t *)old_data);
         old_value_i = (uint64_t)value;
-        old_value_f = (double)old_value_i;
+        old_value_f = (double)(int64_t)old_value_i;
       }
     }
 