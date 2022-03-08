Bforartists/Bforartists

commit 478b52c6b97fa19abb7f316ff335cd2cc28644ae

T77086 Animation: Passing Dependency Graph to Drivers
Custom driver functions need access to the dependency graph that is
triggering the evaluation of the driver. This patch passes the
dependency graph pointer through all the animation-related calls.

Instead of passing the evaluation time to functions, the code now passes
an `AnimationEvalContext` pointer:

```
typedef struct AnimationEvalContext {
  struct Depsgraph *const depsgraph;
  const float eval_time;
} AnimationEvalContext;
```

These structs are read-only, meaning that the code cannot change the
evaluation time. Note that the `depsgraph` pointer itself is const, but
it points to a non-const depsgraph.

FCurves and Drivers can be evaluated at a different time than the
current scene time, for example when evaluating NLA strips. This means
that, even though the current time is stored in the dependency graph, we
need an explicit evaluation time.

There are two functions that allow creation of `AnimationEvalContext`
objects:

- `BKE_animsys_eval_context_construct(Depsgraph *depsgraph, float
  eval_time)`, which creates a new context object from scratch, and
- `BKE_animsys_eval_context_construct_at(AnimationEvalContext
  *anim_eval_context, float eval_time)`, which can be used to create a
  `AnimationEvalContext` with the same depsgraph, but at a different
  time. This makes it possible to later add fields without changing any
  of the code that just want to change the eval time.

This also provides a fix for T75553, although it does require a change
to the custom driver function. The driver should call
`custom_function(depsgraph)`, and the function should use that depsgraph
instead of information from `bpy.context`.

Reviewed By: brecht, sergey

Differential Revision: https://developer.blender.org/D8047

source/blender/python/intern/bpy_driver.c
@@ -33,18 +33,21 @@
 #include "BLI_math_base.h"
 #include "BLI_string.h"
 
+#include "BKE_animsys.h"
 #include "BKE_fcurve_driver.h"
 #include "BKE_global.h"
+
+#include "RNA_access.h"
+#include "RNA_types.h"
 
 #include "bpy_rna_driver.h" /* for pyrna_driver_get_variable_value */
 
 #include "bpy_intern_string.h"
 
 #include "bpy_driver.h"
+#include "bpy_rna.h"
 
 #include "BPY_extern.h"
-
-extern void BPY_update_rna_module(void);
 
 #define USE_RNA_AS_PYOBJECT
 
@@ -377,6 +380,37 @@ static bool bpy_driver_secure_bytecode_validate(PyObject *expr_code, PyObject *d
 
 #endif /* USE_BYTECODE_WHITELIST */
 
+static PyObject *bpy_pydriver_depsgraph_as_pyobject(struct Depsgraph *depsgraph)
+{
+  /* This should never happen, but it's probably better to have None in Python
+   * than a NULL-wrapping Depsgraph py struct. */
+  BLI_assert(depsgraph != NULL);
+  if (depsgraph == NULL) {
+    Py_RETURN_NONE;
+  }
+
+  struct PointerRNA depsgraph_ptr;
+  RNA_pointer_create(NULL, &RNA_Depsgraph, depsgraph, &depsgraph_ptr);
+  return pyrna_struct_CreatePyObject(&depsgraph_ptr);
+}
+
+/* Adds a variable 'depsgraph' to the driver variables. This can then be used to obtain evaluated
+ * datablocks, and the current view layer and scene. See T75553. */
+static void bpy_pydriver_namespace_add_depsgraph(PyObject *driver_vars,
+                                                 struct Depsgraph *depsgraph)
+{
+  PyObject *py_depsgraph = bpy_pydriver_depsgraph_as_pyobject(depsgraph);
+  const char *depsgraph_variable_name = "depsgraph";
+
+  if (PyDict_SetItemString(driver_vars, depsgraph_variable_name, py_depsgraph) == -1) {
+    fprintf(stderr,
+            "\tBPY_driver_eval() - couldn't add variable '%s' to namespace\n",
+            depsgraph_variable_name);
+    PyErr_Print();
+    PyErr_Clear();
+  }
+}
+
 /* This evals py driver expressions, 'expr' is a Python expression that
  * should evaluate to a float number, which is returned.
  *
@@ -396,7 +430,7 @@ static bool bpy_driver_secure_bytecode_validate(PyObject *expr_code, PyObject *d
 float BPY_driver_exec(struct PathResolvedRNA *anim_rna,
                       ChannelDriver *driver,
                       ChannelDriver *driver_orig,
-                      const float evaltime)
+                      const AnimationEvalContext *anim_eval_context)
 {
   PyObject *driver_vars = NULL;
   PyObject *retval = NULL;
@@ -456,7 +490,7 @@ float BPY_driver_exec(struct PathResolvedRNA *anim_rna,
   }
 
   /* update global namespace */
-  bpy_pydriver_namespace_update_frame(evaltime);
+  bpy_pydriver_namespace_update_frame(anim_eval_context->eval_time);
 
   if (driver_orig->flag & DRIVER_FLAG_USE_SELF) {
     bpy_pydriver_namespace_update_self(anim_rna);
@@ -588,6 +622,8 @@ float BPY_driver_exec(struct PathResolvedRNA *anim_rna,
     }
   }
 #endif /* USE_BYTECODE_WHITELIST */
+
+  bpy_pydriver_namespace_add_depsgraph(driver_vars, anim_eval_context->depsgraph);
 
 #if 0 /* slow, with this can avoid all Py_CompileString above. */
   /* execute expression to get a value */

source/blender/blenkernel/BKE_action.h
@@ -32,6 +32,7 @@ extern "C" {
 #endif
 
 /* The following structures are defined in DNA_action_types.h, and DNA_anim_types.h */
+struct AnimationEvalContext;
 struct FCurve;
 struct Main;
 struct Object;
@@ -202,7 +203,7 @@ void what_does_obaction(struct Object *ob,
                         struct bPose *pose,
                         struct bAction *act,
                         char groupname[],
-                        float cframe);
+                        const struct AnimationEvalContext *anim_eval_context);
 
 /* for proxy */
 void BKE_pose_copy_pchan_result(struct bPoseChannel *pchanto,

source/blender/blenkernel/BKE_animsys.h
@@ -48,6 +48,23 @@ struct bAction;
 struct bActionGroup;
 struct bContext;
 
+/* Container for data required to do FCurve and Driver evaluation. */
+typedef struct AnimationEvalContext {
+  /* For drivers, so that they have access to the dependency graph and the current view layer. See
+   * T77086. */
+  struct Depsgraph *const depsgraph;
+
+  /* FCurves and Drivers can be evaluated at a different time than the current scene time, for
+   * example when evaluating NLA strips. This means that, even though the current time is stored in
+   * the dependency graph, we need an explicit evaluation time. */
+  const float eval_time;
+} AnimationEvalContext;
+
+AnimationEvalContext BKE_animsys_eval_context_construct(struct Depsgraph *depsgraph,
+                                                        float eval_time);
+AnimationEvalContext BKE_animsys_eval_context_construct_at(
+    const AnimationEvalContext *anim_eval_context, float eval_time);
+
 /* ************************************* */
 /* KeyingSets API */
 
@@ -172,11 +189,12 @@ void BKE_fcurves_id_cb(struct ID *id, ID_FCurve_Edit_Callback func, void *user_d
 
 typedef struct NlaKeyframingContext NlaKeyframingContext;
 
-struct NlaKeyframingContext *BKE_animsys_get_nla_keyframing_context(struct ListBase *cache,
-                                                                    struct PointerRNA *ptr,
-                                                                    struct AnimData *adt,
-                                                                    float ctime,
-                                                                    const bool flush_to_original);
+struct NlaKeyframingContext *BKE_animsys_get_nla_keyframing_context(
+    struct ListBase *cache,
+    struct PointerRNA *ptr,
+    struct AnimData *adt,
+    const struct AnimationEvalContext *anim_eval_context,
+    const bool flush_to_original);
 bool BKE_animsys_nla_remap_keyframe_values(struct NlaKeyframingContext *context,
                                            struct PointerRNA *prop_ptr,
                                            struct PropertyRNA *prop,
@@ -209,7 +227,7 @@ bool BKE_animsys_write_rna_setting(struct PathResolvedRNA *anim_rna, const float
 /* Evaluation loop for evaluating animation data  */
 void BKE_animsys_evaluate_animdata(struct ID *id,
                                    struct AnimData *adt,
-                                   float ctime,
+                                   const struct AnimationEvalContext *anim_eval_context,
                                    eAnimData_Recalc recalc,
                                    const bool flush_to_original);
 
@@ -229,14 +247,14 @@ void BKE_animsys_evaluate_all_animation(struct Main *main,
 /* Evaluate Action (F-Curve Bag) */
 void animsys_evaluate_action(struct PointerRNA *ptr,
                              struct bAction *act,
-                             float ctime,
+                             const struct AnimationEvalContext *anim_eval_context,
                              const bool flush_to_original);
 
 /* Evaluate Action Group */
 void animsys_evaluate_action_group(struct PointerRNA *ptr,
                                    struct bAction *act,
                                    struct bActionGroup *agrp,
-                                   float ctime);
+                                   const struct AnimationEvalContext *anim_eval_context);
 
 /* ************************************* */
 

source/blender/blenkernel/BKE_fcurve.h
@@ -36,6 +36,7 @@ struct FCurve;
 struct FModifier;
 
 struct AnimData;
+struct AnimationEvalContext;
 struct BezTriple;
 struct LibraryForeachIDData;
 struct PathResolvedRNA;
@@ -281,10 +282,12 @@ float evaluate_fcurve_only_curve(struct FCurve *fcu, float evaltime);
 float evaluate_fcurve_driver(struct PathResolvedRNA *anim_rna,
                              struct FCurve *fcu,
                              struct ChannelDriver *driver_orig,
-                             float evaltime);
+                             const struct AnimationEvalContext *anim_eval_context);
 bool BKE_fcurve_is_empty(struct FCurve *fcu);
 /* evaluate fcurve and store value */
-float calculate_fcurve(struct PathResolvedRNA *anim_rna, struct FCurve *fcu, float evaltime);
+float calculate_fcurve(struct PathResolvedRNA *anim_rna,
+                       struct FCurve *fcu,
+                       const struct AnimationEvalContext *anim_eval_context);
 
 /* ************* F-Curve Samples API ******************** */
 

source/blender/blenkernel/BKE_fcurve_driver.h
@@ -30,6 +30,7 @@
 extern "C" {
 #endif
 
+struct AnimationEvalContext;
 struct ChannelDriver;
 struct DriverTarget;
 struct DriverVar;
@@ -97,7 +98,7 @@ void BKE_driver_invalidate_expression(struct ChannelDriver *driver,
 float evaluate_driver(struct PathResolvedRNA *anim_rna,
                       struct ChannelDriver *driver,
                       struct ChannelDriver *driver_orig,
-                      const float evaltime);
+                      const struct AnimationEvalContext *anim_eval_context);
 
 #ifdef __cplusplus
 }

source/blender/blenkernel/intern/action.c
@@ -1612,8 +1612,12 @@ void BKE_pose_tag_recalc(Main *bmain, bPose *pose)
 /* For the calculation of the effects of an Action at the given frame on an object
  * This is currently only used for the Action Constraint
  */
-void what_does_obaction(
-    Object *ob, Object *workob, bPose *pose, bAction *act, char groupname[], float cframe)
+void what_does_obaction(Object *ob,
+                        Object *workob,
+                        bPose *pose,
+                        bAction *act,
+                        char groupname[],
+                        const AnimationEvalContext *anim_eval_context)
 {
   bActionGroup *agrp = BKE_action_group_find_name(act, groupname);
 
@@ -1669,7 +1673,7 @@ void what_does_obaction(
     RNA_id_pointer_create(&workob->id, &id_ptr);
 
     /* execute action for this group only */
-    animsys_evaluate_action_group(&id_ptr, act, agrp, cframe);
+    animsys_evaluate_action_group(&id_ptr, act, agrp, anim_eval_context);
   }
   else {
     AnimData adt = {NULL};
@@ -1680,6 +1684,6 @@ void what_does_obaction(
     adt.action = act;
 
     /* execute effects of Action on to workob (or it's PoseChannels) */
-    BKE_animsys_evaluate_animdata(&workob->id, &adt, cframe, ADT_RECALC_ANIM, false);
+    BKE_animsys_evaluate_animdata(&workob->id, &adt, anim_eval_context, ADT_RECALC_ANIM, false);
   }
 }

source/blender/blenkernel/intern/constraint.c
@@ -53,6 +53,7 @@
 
 #include "BKE_action.h"
 #include "BKE_anim_path.h"
+#include "BKE_animsys.h"
 #include "BKE_armature.h"
 #include "BKE_bvhutils.h"
 #include "BKE_cachefile.h"
@@ -2625,7 +2626,7 @@ static void actcon_flush_tars(bConstraint *con, ListBase *list, bool no_copy)
   }
 }
 
-static void actcon_get_tarmat(struct Depsgraph *UNUSED(depsgraph),
+static void actcon_get_tarmat(struct Depsgraph *depsgraph,
                               bConstraint *con,
                               bConstraintOb *cob,
                               bConstraintTarget *ct,
@@ -2679,6 +2680,8 @@ static void actcon_get_tarmat(struct Depsgraph *UNUSED(depsgraph),
     s = (vec[axis] - data->min) / (data->max - data->min);
     CLAMP(s, 0, 1);
     t = (s * (data->end - data->start)) + data->start;
+    const AnimationEvalContext anim_eval_context = BKE_animsys_eval_context_construct(depsgraph,
+                                                                                      t);
 
     if (G.debug & G_DEBUG) {
       printf("do Action Constraint %s - Ob %s Pchan %s\n",
@@ -2693,7 +2696,7 @@ static void actcon_get_tarmat(struct Depsgraph *UNUSED(depsgraph),
 
       /* evaluate using workob */
       /* FIXME: we don't have any consistent standards on limiting effects on object... */
-      what_does_obaction(cob->ob, &workob, NULL, data->act, NULL, t);
+      what_does_obaction(cob->ob, &workob, NULL, data->act, NULL, &anim_eval_context);
       BKE_object_to_mat4(&workob, ct->matrix);
     }
     else if (cob->type == CONSTRAINT_OBTYPE_BONE) {
@@ -2710,7 +2713,7 @@ static void actcon_get_tarmat(struct Depsgraph *UNUSED(depsgraph),
       tchan->rotmode = pchan->rotmode;
 
       /* evaluate action using workob (it will only set the PoseChannel in question) */
-      what_does_obaction(cob->ob, &workob, &pose, data->act, pchan->name, t);
+      what_does_obaction(cob->ob, &workob, &pose, data->act, pchan->name, &anim_eval_context);
 
       /* convert animation to matrices for use here */
       BKE_pchan_calc_mat(tchan);

commit 53d30fd78e300553a96f7699ebb3bebf91842455

Fix memory leak evaluating PyDrivers
Missed decref in 478b52c6b97fa19abb7f316ff335cd2cc28644ae caused every
driver evaluation to create the BPy_StructRNA depsgraph without freeing
the previously allocated depsgraph.

source/blender/python/intern/bpy_driver.c
@@ -401,6 +401,7 @@ static void bpy_pydriver_namespace_add_depsgraph(PyObject *driver_vars,
     PyErr_Print();
     PyErr_Clear();
   }
+  Py_DECREF(py_depsgraph);
 }
 
 float BPY_driver_exec(struct PathResolvedRNA *anim_rna,