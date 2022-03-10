Bforartists/Bforartists

commit 90a8f85025d9612e93f7e3677bd38cb0e074077a

PyAPI: use iterators for ID property methods (keys, values & items)
- Matches changes in Python 3.x dictionary methods.

- Iterating now raises a run-time error if the property-group changes
  size during iteration.

- IDPropertyGroup.iteritems() has been removed.

- IDPropertyGroup View & Iterator types have been added.

- Some set functionality from dict_keys/values/items aren't yet
  supported (isdisjoint method and boolean set style operations).

Proposed as part of T85675.

tests/python/bl_pyapi_idprop.py
@@ -2,6 +2,7 @@
 
 # ./blender.bin --background -noaudio --python tests/python/bl_pyapi_idprop.py -- --verbose
 import bpy
+import idprop
 import unittest
 import numpy as np
 from array import array
@@ -138,6 +139,51 @@ class TestIdPropertyCreation(TestHelper, unittest.TestCase):
     def test_invalid_type(self):
         with self.assertRaises(TypeError):
             self.id["a"] = self
+
+class TestIdPropertyGroupView(TestHelper, unittest.TestCase):
+
+    def test_type(self):
+        self.assertEqual(type(self.id.keys()), idprop.types.IDPropertyGroupViewKeys)
+        self.assertEqual(type(self.id.values()), idprop.types.IDPropertyGroupViewValues)
+        self.assertEqual(type(self.id.items()), idprop.types.IDPropertyGroupViewItems)
+
+        self.assertEqual(type(iter(self.id.keys())), idprop.types.IDPropertyGroupIterKeys)
+        self.assertEqual(type(iter(self.id.values())), idprop.types.IDPropertyGroupIterValues)
+        self.assertEqual(type(iter(self.id.items())), idprop.types.IDPropertyGroupIterItems)
+
+    def test_basic(self):
+        text = ["A", "B", "C"]
+        for i, ch in enumerate(text):
+            self.id[ch] = i
+        self.assertEqual(len(self.id.keys()), len(text))
+        self.assertEqual(list(self.id.keys()), text)
+        self.assertEqual(list(reversed(self.id.keys())), list(reversed(text)))
+
+        self.assertEqual(len(self.id.values()), len(text))
+        self.assertEqual(list(self.id.values()), list(range(len(text))))
+        self.assertEqual(list(reversed(self.id.values())), list(reversed(range(len(text)))))
+
+        self.assertEqual(len(self.id.items()), len(text))
+        self.assertEqual(list(self.id.items()), [(k, v) for v, k in enumerate(text)])
+        self.assertEqual(list(reversed(self.id.items())), list(reversed([(k, v) for v, k in enumerate(text)])))
+
+    def test_contains(self):
+        # Check `idprop.types.IDPropertyGroupView{Keys/Values/Items}.__contains__`
+        text = ["A", "B", "C"]
+        for i, ch in enumerate(text):
+            self.id[ch] = i
+
+        self.assertIn("A", self.id)
+        self.assertNotIn("D", self.id)
+
+        self.assertIn("A", self.id.keys())
+        self.assertNotIn("D", self.id.keys())
+
+        self.assertIn(2, self.id.values())
+        self.assertNotIn(3, self.id.values())
+
+        self.assertIn(("A", 0), self.id.items())
+        self.assertNotIn(("D", 3), self.id.items())
 
 
 class TestBufferProtocol(TestHelper, unittest.TestCase):

release/scripts/modules/rna_prop_ui.py
@@ -235,7 +235,7 @@ def draw(layout, context, context_member, property_type, use_edit=True):
 
     assert(isinstance(rna_item, property_type))
 
-    items = rna_item.items()
+    items = list(rna_item.items())
     items.sort()
 
     # TODO: Allow/support adding new custom props to overrides.

source/blender/python/generic/idprop_py_api.h
@@ -25,15 +25,34 @@ struct ID;
 struct IDProperty;
 
 extern PyTypeObject BPy_IDArray_Type;
-extern PyTypeObject BPy_IDGroup_Iter_Type;
 extern PyTypeObject BPy_IDGroup_Type;
+
+extern PyTypeObject BPy_IDGroup_ViewKeys_Type;
+extern PyTypeObject BPy_IDGroup_ViewValues_Type;
+extern PyTypeObject BPy_IDGroup_ViewItems_Type;
+
+extern PyTypeObject BPy_IDGroup_IterKeys_Type;
+extern PyTypeObject BPy_IDGroup_IterValues_Type;
+extern PyTypeObject BPy_IDGroup_IterItems_Type;
 
 #define BPy_IDArray_Check(v) (PyObject_TypeCheck(v, &BPy_IDArray_Type))
 #define BPy_IDArray_CheckExact(v) (Py_TYPE(v) == &BPy_IDArray_Type)
-#define BPy_IDGroup_Iter_Check(v) (PyObject_TypeCheck(v, &BPy_IDGroup_Iter_Type))
-#define BPy_IDGroup_Iter_CheckExact(v) (Py_TYPE(v) == &BPy_IDGroup_Iter_Type)
 #define BPy_IDGroup_Check(v) (PyObject_TypeCheck(v, &BPy_IDGroup_Type))
 #define BPy_IDGroup_CheckExact(v) (Py_TYPE(v) == &BPy_IDGroup_Type)
+
+#define BPy_IDGroup_ViewKeys_Check(v) (PyObject_TypeCheck(v, &BPy_IDGroup_ViewKeys_Type))
+#define BPy_IDGroup_ViewKeys_CheckExact(v) (Py_TYPE(v) == &BPy_IDGroup_ViewKeys_Type)
+#define BPy_IDGroup_ViewValues_Check(v) (PyObject_TypeCheck(v, &BPy_IDGroup_ViewValues_Type))
+#define BPy_IDGroup_ViewValues_CheckExact(v) (Py_TYPE(v) == &BPy_IDGroup_ViewValues_Type)
+#define BPy_IDGroup_ViewItems_Check(v) (PyObject_TypeCheck(v, &BPy_IDGroup_ViewItems_Type))
+#define BPy_IDGroup_ViewItems_CheckExact(v) (Py_TYPE(v) == &BPy_IDGroup_ViewItems_Type)
+
+#define BPy_IDGroup_IterKeys_Check(v) (PyObject_TypeCheck(v, &BPy_IDGroup_IterKeys_Type))
+#define BPy_IDGroup_IterKeys_CheckExact(v) (Py_TYPE(v) == &BPy_IDGroup_IterKeys_Type)
+#define BPy_IDGroup_IterValues_Check(v) (PyObject_TypeCheck(v, &BPy_IDGroup_IterValues_Type))
+#define BPy_IDGroup_IterValues_CheckExact(v) (Py_TYPE(v) == &BPy_IDGroup_IterValues_Type)
+#define BPy_IDGroup_IterItems_Check(v) (PyObject_TypeCheck(v, &BPy_IDGroup_IterItems_Type))
+#define BPy_IDGroup_IterItems_CheckExact(v) (Py_TYPE(v) == &BPy_IDGroup_IterItems_Type)
 
 typedef struct BPy_IDProperty {
   PyObject_VAR_HEAD
@@ -52,12 +71,28 @@ typedef struct BPy_IDGroup_Iter {
   PyObject_VAR_HEAD
   BPy_IDProperty *group;
   struct IDProperty *cur;
-  int mode;
+  /** Use for detecting manipulation during iteration (which is not allowed). */
+  int len_init;
+  /** Iterate in the reverse direction. */
+  bool reversed;
 } BPy_IDGroup_Iter;
+
+/** Use to implement `IDPropertyGroup.keys/values/items` */
+typedef struct BPy_IDGroup_View {
+  PyObject_VAR_HEAD
+  /** This will be NULL when accessing keys on data that has no ID properties. */
+  BPy_IDProperty *group;
+  bool reversed;
+} BPy_IDGroup_View;
 
 PyObject *BPy_Wrap_GetKeys(struct IDProperty *prop);
 PyObject *BPy_Wrap_GetValues(struct ID *id, struct IDProperty *prop);
 PyObject *BPy_Wrap_GetItems(struct ID *id, struct IDProperty *prop);
+
+PyObject *BPy_Wrap_GetKeys_View_WithID(struct ID *id, struct IDProperty *prop);
+PyObject *BPy_Wrap_GetValues_View_WithID(struct ID *id, struct IDProperty *prop);
+PyObject *BPy_Wrap_GetItems_View_WithID(struct ID *id, struct IDProperty *prop);
+
 int BPy_Wrap_SetMapItem(struct IDProperty *prop, PyObject *key, PyObject *val);
 
 PyObject *BPy_IDGroup_MapDataToPy(struct IDProperty *prop);
@@ -67,6 +102,3 @@ bool BPy_IDProperty_Map_ValidateAndCreate(PyObject *key, struct IDProperty *grou
 void IDProp_Init_Types(void);
 
 PyObject *BPyInit_idprop(void);
-
-#define IDPROP_ITER_KEYS 0
-#define IDPROP_ITER_ITEMS 1

source/blender/python/intern/bpy_rna.c
@@ -3562,7 +3562,7 @@ PyDoc_STRVAR(pyrna_struct_keys_doc,
              "   dictionary function of the same name).\n"
              "\n"
              "   :return: custom property keys.\n"
-             "   :rtype: list of strings\n"
+             "   :rtype: :class:`idprop.type.IDPropertyGroupViewKeys`\n"
              "\n" BPY_DOC_ID_PROP_TYPE_NOTE);
 static PyObject *pyrna_struct_keys(BPy_PropertyRNA *self)
 {
@@ -3573,13 +3573,9 @@ static PyObject *pyrna_struct_keys(BPy_PropertyRNA *self)
     return NULL;
   }
 
+  /* `group` may be NULL. */
   group = RNA_struct_idprops(&self->ptr, 0);
-
-  if (group == NULL) {
-    return PyList_New(0);
-  }
-
-  return BPy_Wrap_GetKeys(group);
+  return BPy_Wrap_GetKeys_View_WithID(self->ptr.owner_id, group);
 }
 
 PyDoc_STRVAR(pyrna_struct_items_doc,
@@ -3589,7 +3585,7 @@ PyDoc_STRVAR(pyrna_struct_items_doc,
              "   dictionary function of the same name).\n"
              "\n"
              "   :return: custom property key, value pairs.\n"
-             "   :rtype: list of key, value tuples\n"
+             "   :rtype: :class:`idprop.type.IDPropertyGroupViewItems`\n"
              "\n" BPY_DOC_ID_PROP_TYPE_NOTE);
 static PyObject *pyrna_struct_items(BPy_PropertyRNA *self)
 {
@@ -3600,13 +3596,9 @@ static PyObject *pyrna_struct_items(BPy_PropertyRNA *self)
     return NULL;
   }
 
+  /* `group` may be NULL. */
   group = RNA_struct_idprops(&self->ptr, 0);
-
-  if (group == NULL) {
-    return PyList_New(0);
-  }
-
-  return BPy_Wrap_GetItems(self->ptr.owner_id, group);
+  return BPy_Wrap_GetItems_View_WithID(self->ptr.owner_id, group);
 }
 
 PyDoc_STRVAR(pyrna_struct_values_doc,
@@ -3616,7 +3608,7 @@ PyDoc_STRVAR(pyrna_struct_values_doc,
              "   dictionary function of the same name).\n"
              "\n"
              "   :return: custom property values.\n"
-             "   :rtype: list\n"
+             "   :rtype: :class:`idprop.type.IDPropertyGroupViewValues`\n"
              "\n" BPY_DOC_ID_PROP_TYPE_NOTE);
 static PyObject *pyrna_struct_values(BPy_PropertyRNA *self)
 {
@@ -3628,13 +3620,9 @@ static PyObject *pyrna_struct_values(BPy_PropertyRNA *self)
     return NULL;
   }
 
+  /* `group` may be NULL. */
   group = RNA_struct_idprops(&self->ptr, 0);
-
-  if (group == NULL) {
-    return PyList_New(0);
-  }
-
-  return BPy_Wrap_GetValues(self->ptr.owner_id, group);
+  return BPy_Wrap_GetValues_View_WithID(self->ptr.owner_id, group);
 }
 
 PyDoc_STRVAR(pyrna_struct_is_property_set_doc,

commit 0fbd710c4bf9d35c717564383d3e6202ea18edcb

Fix T94121: PyAPI: ID property group returns wrong type with iter()
Regression in 90a8f85025d9612e93f7e3677bd38cb0e074077a.
Where iterating directly on a property group failed, e.g.:
`iter(group)`, tests missed this since only `group.keys()`
was checked.

source/blender/python/generic/idprop_py_api.c
@@ -756,7 +756,16 @@ static int BPy_IDGroup_Map_SetItem(BPy_IDProperty *self, PyObject *key, PyObject
 
 static PyObject *BPy_IDGroup_iter(BPy_IDProperty *self)
 {
-  return BPy_IDGroup_ViewKeys_CreatePyObject(self);
+  PyObject *iterable = BPy_IDGroup_ViewKeys_CreatePyObject(self);
+  PyObject *ret;
+  if (iterable) {
+    ret = PyObject_GetIter(iterable);
+    Py_DECREF(iterable);
+  }
+  else {
+    ret = NULL;
+  }
+  return ret;
 }
 
 PyObject *BPy_IDGroup_MapDataToPy(IDProperty *prop)

tests/python/bl_pyapi_idprop.py
@@ -172,6 +172,14 @@ class TestIdPropertyGroupView(TestHelper, unittest.TestCase):
         self.assertEqual(list(self.id.items()), [(k, v) for v, k in enumerate(text)])
         self.assertEqual(list(reversed(self.id.items())), list(reversed([(k, v) for v, k in enumerate(text)])))
 
+        # Check direct iteration is working as expected.
+        self.id["group"] = {ch: i for i, ch in enumerate(text)}
+        group = self.id["group"]
+
+        self.assertEqual(len(group), len(text))
+        self.assertEqual(list(iter(group)), text)
+
+
     def test_contains(self):
         # Check `idprop.types.IDPropertyGroupView{Keys/Values/Items}.__contains__`
         text = ["A", "B", "C"]