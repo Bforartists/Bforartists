ca5b3238bf80050190328a018a561b1d574ebf7b
ReinerBforartists <reiner.prokein@posteo.de>
2022-03-14 18:26:49 +0100

    Import Export menus - missing icons #2926

--- release/scripts/addons/io_curve_svg/__init__.py
+++ release/scripts/addons/io_curve_svg/__init__.py
@@ -44,8 +44,7 @@ class ImportSVG(bpy.types.Operator, ImportHelper):
 
 
 def menu_func_import(self, context):
-    self.layout.operator(ImportSVG.bl_idname,
-        text="Scalable Vector Graphics (.svg)")
+    self.layout.operator(ImportSVG.bl_idname, text="Scalable Vector Graphics (.svg)", icon = "LOAD_SVG")
 
 
 def register():
