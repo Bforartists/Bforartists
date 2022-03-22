blender/blender

commit 52be06301257a82a1b4a5746e91ff60daa637ded

Fix (unreported): Issues with 'SNAP_NOT_SELECTED' for pose and edit modes
This is a regression partially introduced in rB0a6f428be7f0.

Bones being transformed into edit mode were snapping to themselves.

And the bones of the pose mode weren't even snapping.

(Curious that this was not reported).

source/blender/editors/transform/transform_snap_object.c
@@ -477,7 +477,10 @@ static void iter_snap_objects(SnapObjectContext *sctx,
       }
     }
     else if (snap_select == SNAP_NOT_SELECTED) {
-      if ((base->flag & BASE_SELECTED) || (base->flag_legacy & BA_WAS_SEL)) {
+      if (is_object_active && !(base->object->mode & OB_MODE_OBJECT)) {
+        /* Pass. Consider the selection of elements being edited. */
+      }
+      else if ((base->flag & BASE_SELECTED) || (base->flag_legacy & BA_WAS_SEL)) {
         continue;
       }
     }
@@ -1818,6 +1821,7 @@ static short snapArmature(SnapObjectContext *sctx,
                           const struct SnapObjectParams *params,
                           Object *ob_eval,
                           const float obmat[4][4],
+                          bool is_object_active,
                           /* read/write args */
                           float *dist_px,
                           /* return args */
@@ -1838,9 +1842,10 @@ static short snapArmature(SnapObjectContext *sctx,
   dist_squared_to_projected_aabb_precalc(
       &neasrest_precalc, lpmat, sctx->runtime.win_size, sctx->runtime.mval);
 
-  bool use_obedit = ((bArmature *)ob_eval->data)->edbo != NULL;
+  bArmature *arm = ob_eval->data;
+  const bool is_editmode = arm->edbo != NULL;
 
-  if (use_obedit == false) {
+  if (is_editmode == false) {
     /* Test BoundBox */
     BoundBox *bb = BKE_armature_boundbox_get(ob_eval);
     if (bb && !snap_bound_box_check_dist(bb->vec[0],
@@ -1859,10 +1864,11 @@ static short snapArmature(SnapObjectContext *sctx,
     mul_v4_m4v4(clip_planes_local[i], tobmat, sctx->runtime.clip_plane[i]);
   }
 
-  const eSnapSelect snap_select = params->snap_select;
-  bool is_persp = sctx->runtime.view_proj == VIEW_PROJ_PERSP;
+  const bool is_posemode = is_object_active && (ob_eval->mode & OB_MODE_POSE);
+  const bool skip_selected = (is_editmode || is_posemode) &&
+                             (params->snap_select == SNAP_NOT_SELECTED);
+  const bool is_persp = sctx->runtime.view_proj == VIEW_PROJ_PERSP;
 
-  bArmature *arm = ob_eval->data;
   if (arm->edbo) {
     LISTBASE_FOREACH (EditBone *, eBone, arm->edbo) {
       if (eBone->layer & arm->layer) {
@@ -1872,7 +1878,7 @@ static short snapArmature(SnapObjectContext *sctx,
         }
 
         const bool is_selected = (eBone->flag & (BONE_ROOTSEL | BONE_TIPSEL)) != 0;
-        if (is_selected && snap_select == SNAP_NOT_SELECTED) {
+        if (is_selected && skip_selected) {
           continue;
         }
         bool has_vert_snap = false;
@@ -1916,10 +1922,16 @@ static short snapArmature(SnapObjectContext *sctx,
   else if (ob_eval->pose && ob_eval->pose->chanbase.first) {
     LISTBASE_FOREACH (bPoseChannel *, pchan, &ob_eval->pose->chanbase) {
       Bone *bone = pchan->bone;
-      /* skip hidden bones */
       if (!bone || (bone->flag & (BONE_HIDDEN_P | BONE_HIDDEN_PG))) {
+        /* Skip hidden bones. */
         continue;
       }
+
+      const bool is_selected = (bone->flag & (BONE_SELECTED | BONE_ROOTSEL | BONE_TIPSEL)) != 0;
+      if (is_selected && skip_selected) {
+        continue;
+      }
+
       bool has_vert_snap = false;
       const float *head_vec = pchan->pose_head;
       const float *tail_vec = pchan->pose_tail;
@@ -2685,7 +2697,7 @@ static void snap_obj_fn(SnapObjectContext *sctx,
                         const struct SnapObjectParams *params,
                         Object *ob_eval,
                         float obmat[4][4],
-                        bool UNUSED(is_object_active),
+                        bool is_object_active,
                         void *data)
 {
   struct SnapObjUserData *dt = data;
@@ -2721,8 +2733,15 @@ static void snap_obj_fn(SnapObjectContext *sctx,
       break;
     }
     case OB_ARMATURE:
-      retval = snapArmature(
-          sctx, params, ob_eval, obmat, dt->dist_px, dt->r_loc, dt->r_no, dt->r_index);
+      retval = snapArmature(sctx,
+                            params,
+                            ob_eval,
+                            obmat,
+                            is_object_active,
+                            dt->dist_px,
+                            dt->r_loc,
+                            dt->r_no,
+                            dt->r_index);
       break;
     case OB_CURVE:
       retval = snapCurve(

source/blender/editors/transform/transform_snap.c
@@ -628,13 +628,16 @@ static short snap_select_type_get(TransInfo *t)
       else if (!t->tsnap.snap_self) {
         r_snap_select = SNAP_NOT_ACTIVE;
       }
+      else {
+        r_snap_select = SNAP_NOT_SELECTED;
+      }
     }
     else if ((obedit_type == -1) && base_act && base_act->object &&
              (base_act->object->mode & OB_MODE_PARTICLE_EDIT)) {
       /* Particles edit mode. */
     }
     else if (obedit_type == -1) {
-      /* Object mode */
+      /* Object or pose mode. */
       r_snap_select = SNAP_NOT_SELECTED;
     }
   }

Bforartists/Bforartists

commit 052793d2868a504337c1c97a49c17fee9d88cce0

Fix T96711: snap in edit mode for selected objects does not work
Regression introduced in 52be06301257a82a1b4a5746e91ff60daa637ded

The `SNAP_NOT_SELECTED` option should only consider base selected if
we are in object mode.

source/blender/editors/transform/transform_snap_object.c
@@ -440,6 +440,44 @@ typedef void (*IterSnapObjsCallback)(SnapObjectContext *sctx,
                                      bool is_object_active,
                                      void *data);
 
+static bool snap_object_is_snappable(const SnapObjectContext *sctx,
+                                     const eSnapSelect snap_select,
+                                     const Base *base_act,
+                                     const Base *base,
+                                     const bool is_in_object_mode)
+{
+  if (!BASE_VISIBLE(sctx->runtime.v3d, base)) {
+    return false;
+  }
+
+  if ((snap_select == SNAP_ALL) || (base->flag_legacy & BA_TRANSFORM_LOCKED_IN_PLACE)) {
+    return true;
+  }
+
+  if (base->flag_legacy & BA_SNAP_FIX_DEPS_FIASCO) {
+    return false;
+  }
+
+  if (snap_select == SNAP_NOT_ACTIVE) {
+    return base_act == base;
+  }
+
+  if (snap_select == SNAP_NOT_SELECTED) {
+    if (is_in_object_mode) {
+      return !((base->flag & BASE_SELECTED) || (base->flag_legacy & BA_WAS_SEL));
+    }
+
+    /* What is selectable or not is part of the object and depends on the mode. */
+    return true;
+  }
+
+  if (snap_select == SNAP_SELECTABLE) {
+    return (base->flag & BASE_SELECTABLE) != 0;
+  }
+
+  return true;
+}
+
 /**
  * Walks through all objects in the scene to create the list of objects to snap.
  */
@@ -458,38 +496,13 @@ static void iter_snap_objects(SnapObjectContext *sctx,
     return;
   }
 
+  const bool is_in_object_mode = base_act && base_act->object->mode == OB_MODE_OBJECT;
   for (Base *base = view_layer->object_bases.first; base != NULL; base = base->next) {
-    if (!BASE_VISIBLE(sctx->runtime.v3d, base)) {
-      continue;
-    }
-
-    if ((snap_select == SNAP_ALL) || (base->flag_legacy & BA_TRANSFORM_LOCKED_IN_PLACE)) {
-      /* pass */
-    }
-    else if (base->flag_legacy & BA_SNAP_FIX_DEPS_FIASCO) {
+    if (!snap_object_is_snappable(sctx, snap_select, base_act, base, is_in_object_mode)) {
       continue;
     }
 
     const bool is_object_active = (base == base_act);
-    if (snap_select == SNAP_NOT_ACTIVE) {
-      if (is_object_active) {
-        continue;
-      }
-    }
-    else if (snap_select == SNAP_NOT_SELECTED) {
-      if (is_object_active && base->object->mode != OB_MODE_OBJECT) {
-        /* Pass. Consider the selection of elements being edited. */
-      }
-      else if ((base->flag & BASE_SELECTED) || (base->flag_legacy & BA_WAS_SEL)) {
-        continue;
-      }
-    }
-    else if (snap_select == SNAP_SELECTABLE) {
-      if (!(base->flag & BASE_SELECTABLE)) {
-        continue;
-      }
-    }
-
     Object *obj_eval = DEG_get_evaluated_object(sctx->runtime.depsgraph, base->object);
     if (obj_eval->transflag & OB_DUPLI || BKE_object_has_geometry_set_instances(obj_eval)) {
       ListBase *lb = object_duplilist(sctx->runtime.depsgraph, sctx->scene, obj_eval);