Bforartists/Bforartists

commit 478912cf62416bc8e2faa4b3fb1ccaf49bc9a0b0

Fix T96711: snap in edit mode for selected objects does not work
Regression introduced in 2da00e7e4ca262db0f8c16e93934ad214e55c72b

The `SNAP_NOT_SELECTED` option should only consider base selected if
we are in object mode.

source/blender/editors/transform/transform_snap_object.c
@@ -440,6 +440,44 @@ typedef void (*IterSnapObjsCallback)(SnapObjectContext *sctx,
                                      bool is_object_active,
                                      void *data);
 
+static bool snap_object_is_snappable(const SnapObjectContext *sctx,
+                                     const eSnapSelect snap_select,
+                                     const Base *base_act,
+                                     const Base *base,
+                                     const bool is_in_object_mode)
+{
+  if (!BASE_VISIBLE(sctx->runtime.v3d, base)) {
+    return false;
+  }
+
+  if ((snap_select == SNAP_ALL) || (base->flag_legacy & BA_TRANSFORM_LOCKED_IN_PLACE)) {
+    return true;
+  }
+
+  if (base->flag_legacy & BA_SNAP_FIX_DEPS_FIASCO) {
+    return false;
+  }
+
+  if (snap_select == SNAP_NOT_ACTIVE) {
+    return base_act == base;
+  }
+
+  if (snap_select == SNAP_NOT_SELECTED) {
+    if (is_in_object_mode) {
+      return !((base->flag & BASE_SELECTED) || (base->flag_legacy & BA_WAS_SEL));
+    }
+
+    /* What is selectable or not is part of the object and depends on the mode. */
+    return true;
+  }
+
+  if (snap_select == SNAP_SELECTABLE) {
+    return (base->flag & BASE_SELECTABLE) != 0;
+  }
+
+  return true;
+}
+
 /**
  * Walks through all objects in the scene to create the list of objects to snap.
  */
@@ -458,38 +496,13 @@ static void iter_snap_objects(SnapObjectContext *sctx,
     return;
   }
 
+  const bool is_in_object_mode = base_act && base_act->object->mode == OB_MODE_OBJECT;
   for (Base *base = view_layer->object_bases.first; base != NULL; base = base->next) {
-    if (!BASE_VISIBLE(sctx->runtime.v3d, base)) {
-      continue;
-    }
-
-    if ((snap_select == SNAP_ALL) || (base->flag_legacy & BA_TRANSFORM_LOCKED_IN_PLACE)) {
-      /* pass */
-    }
-    else if (base->flag_legacy & BA_SNAP_FIX_DEPS_FIASCO) {
+    if (!snap_object_is_snappable(sctx, snap_select, base_act, base, is_in_object_mode)) {
       continue;
     }
 
     const bool is_object_active = (base == base_act);
-    if (snap_select == SNAP_NOT_ACTIVE) {
-      if (is_object_active) {
-        continue;
-      }
-    }
-    else if (snap_select == SNAP_NOT_SELECTED) {
-      if (is_object_active && base->object->mode != OB_MODE_OBJECT) {
-        /* Pass. Consider the selection of elements being edited. */
-      }
-      else if ((base->flag & BASE_SELECTED) || (base->flag_legacy & BA_WAS_SEL)) {
-        continue;
-      }
-    }
-    else if (snap_select == SNAP_SELECTABLE) {
-      if (!(base->flag & BASE_SELECTABLE)) {
-        continue;
-      }
-    }
-
     Object *obj_eval = DEG_get_evaluated_object(sctx->runtime.depsgraph, base->object);
     if (obj_eval->transflag & OB_DUPLI || BKE_object_has_geometry_set_instances(obj_eval)) {
       ListBase *lb = object_duplilist(sctx->runtime.depsgraph, sctx->scene, obj_eval);

commit 06d600aa611b5a8c340062604be639475074ba58

Fix T96812: Regression: Snapping is broken with proportional editing
This was a mistake in the conditional structure introduced in 478912cf6241

This commit also adds a new type of snap exclusion: `SNAP_NOT_EDITED`.

Thanks to @Ethan1080 for pointing out the error.

source/blender/editors/include/ED_transform_snap_object_context.h
@@ -29,8 +29,9 @@ typedef enum {
   SNAP_ALL = 0,
   SNAP_NOT_SELECTED = 1,
   SNAP_NOT_ACTIVE = 2,
-  SNAP_ONLY_ACTIVE = 3,
-  SNAP_SELECTABLE = 4,
+  SNAP_NOT_EDITED = 3,
+  SNAP_ONLY_ACTIVE = 4,
+  SNAP_SELECTABLE = 5,
 } eSnapSelect;
 
 typedef enum {

source/blender/editors/transform/transform_snap.c
@@ -639,7 +639,7 @@ static short snap_select_type_get(TransInfo *t)
 
       if ((obedit_type == OB_MESH) && (t->flag & T_PROP_EDIT)) {
         /* Exclude editmesh if using proportional edit */
-        r_snap_select = SNAP_NOT_ACTIVE;
+        r_snap_select = SNAP_NOT_EDITED;
       }
       else if (!t->tsnap.snap_self) {
         r_snap_select = SNAP_NOT_ACTIVE;

source/blender/editors/transform/transform_snap_object.c
@@ -459,7 +459,11 @@ static bool snap_object_is_snappable(const SnapObjectContext *sctx,
   }
 
   if (snap_select == SNAP_NOT_ACTIVE) {
-    return base_act == base;
+    return base_act != base;
+  }
+
+  if (snap_select == SNAP_NOT_EDITED) {
+    return base->object->mode != OB_MODE_EDIT;
   }
 
   if (snap_select == SNAP_NOT_SELECTED) {