Bforartists/Bforartists

commit 5b1486625934d0e444780cd32848880bd15dfe08

Cleanup: Move remaining mesh draw code to C++
After this commit, all mesh data extraction and drawing code is in C++,
including headers, making it possible to use improved types for future
performance improvements and simplifications.

The only non-trivial changes are in `draw_cache_impl_mesh.cc`,
where use of certain features and macros in C necessitated larger
changes.

Differential Revision: https://developer.blender.org/D15088

source/blender/draw/intern/draw_cache_extract.hh
@@ -7,11 +7,13 @@
 
 #pragma once
 
-struct DRWSubdivCache;
-struct MeshRenderData;
-struct TaskGraph;
+#include <algorithm>
+
+#include "BLI_utildefines.h"
 
 #include "DNA_customdata_types.h"
+#include "DNA_mesh_types.h"
+#include "DNA_view3d_enums.h"
 
 #include "BKE_attribute.h"
 #include "BKE_object.h"
@@ -22,8 +24,12 @@ struct TaskGraph;
 
 #include "draw_attributes.h"
 
+struct DRWSubdivCache;
+struct MeshRenderData;
+struct TaskGraph;
+
 /* Vertex Group Selection and display options */
-typedef struct DRW_MeshWeightState {
+struct DRW_MeshWeightState {
   int defgroup_active;
   int defgroup_len;
 
@@ -37,7 +43,7 @@ typedef struct DRW_MeshWeightState {
   /* Set of all locked and unlocked deform bones for Lock Relative mode. */
   bool *defgroup_locked;   /* [defgroup_len] */
   bool *defgroup_unlocked; /* [defgroup_len] */
-} DRW_MeshWeightState;
+};
 
 /* DRW_MeshWeightState.flags */
 enum {
@@ -46,7 +52,7 @@ enum {
   DRW_MESH_WEIGHT_STATE_LOCK_RELATIVE = (1 << 2),
 };
 
-typedef struct DRW_MeshCDMask {
+struct DRW_MeshCDMask {
   uint32_t uv : 8;
   uint32_t tan : 8;
   uint32_t vcol : 8;
@@ -56,20 +62,20 @@ typedef struct DRW_MeshCDMask {
   /** Edit uv layer is from the base edit mesh as
    *  modifiers could remove it. (see T68857) */
   uint32_t edit_uv : 1;
-} DRW_MeshCDMask;
+};
 /* Keep `DRW_MeshCDMask` struct within an `uint32_t`.
  * bit-wise and atomic operations are used to compare and update the struct.
  * See `mesh_cd_layers_type_*` functions. */
 BLI_STATIC_ASSERT(sizeof(DRW_MeshCDMask) <= sizeof(uint32_t), "DRW_MeshCDMask exceeds 32 bits")
-typedef enum eMRIterType {
+enum eMRIterType {
   MR_ITER_LOOPTRI = 1 << 0,
   MR_ITER_POLY = 1 << 1,
   MR_ITER_LEDGE = 1 << 2,
   MR_ITER_LVERT = 1 << 3,
-} eMRIterType;
+};
 ENUM_OPERATORS(eMRIterType, MR_ITER_LVERT)
 
-typedef enum eMRDataType {
+enum eMRDataType {
   MR_DATA_NONE = 0,
   MR_DATA_POLY_NOR = 1 << 1,
   MR_DATA_LOOP_NOR = 1 << 2,
@@ -78,25 +84,37 @@ typedef enum eMRDataType {
   /** Force loop normals calculation. */
   MR_DATA_TAN_LOOP_NOR = 1 << 5,
   MR_DATA_POLYS_SORTED = 1 << 6,
-} eMRDataType;
+};
 ENUM_OPERATORS(eMRDataType, MR_DATA_POLYS_SORTED)
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 BLI_INLINE int mesh_render_mat_len_get(const Object *object, const Mesh *me)
 {
   if (me->edit_mesh != NULL) {
     const Mesh *editmesh_eval_final = BKE_object_get_editmesh_eval_final(object);
     if (editmesh_eval_final != NULL) {
-      return MAX2(1, editmesh_eval_final->totcol);
+      return std::max<int>(1, editmesh_eval_final->totcol);
     }
   }
-  return MAX2(1, me->totcol);
+  return std::max<int>(1, me->totcol);
 }
 
-typedef struct MeshBufferList {
+struct MeshBufferList {
+
+  // enum class BufferItem {
+  //   PosNor,
+  //   LNor,
+  //   EdgeFac,
+  //   Weights,
+  //   UV,
+  //   Tan,
+  //   VCol,
+  //   SculptData,
+  //   Orco,
+  //   EditData,
+  //   EditUVData,
+  //   EditUVStretchArea,
+
+  // }
   /* Every VBO below contains at least enough
    * data for every loops in the mesh (except fdots and skin roots).
    * For some VBOs, it extends to (in this exact order) :
@@ -148,9 +166,9 @@ typedef struct MeshBufferList {
     GPUIndexBuf *edituv_points;
     GPUIndexBuf *edituv_fdots;
   } ibo;
-} MeshBufferList;
+};
 
-typedef struct MeshBatchList {
+struct MeshBatchList {
   /* Surfaces / Render */
   GPUBatch *surface;
   GPUBatch *surface_weights;
@@ -184,7 +202,7 @@ typedef struct MeshBatchList {
   GPUBatch *wire_loops;     /* Loops around faces. no edges between selected faces */
   GPUBatch *wire_loops_uvs; /* Same as wire_loops but only has uvs. */
   GPUBatch *sculpt_overlays;
-} MeshBatchList;
+};
 
 #define MBC_BATCH_LEN (sizeof(MeshBatchList) / sizeof(void *))
 #define MBC_VBO_LEN (sizeof(((MeshBufferList){0}).vbo) / sizeof(void *))
@@ -192,7 +210,7 @@ typedef struct MeshBatchList {
 
 #define MBC_BATCH_INDEX(batch) (offsetof(MeshBatchList, batch) / sizeof(void *))
 
-typedef enum DRWBatchFlag {
+enum DRWBatchFlag {
   MBC_SURFACE = (1u << MBC_BATCH_INDEX(surface)),
   MBC_SURFACE_WEIGHTS = (1u << MBC_BATCH_INDEX(surface_weights)),
   MBC_EDIT_TRIANGLES = (1u << MBC_BATCH_INDEX(edit_triangles)),
@@ -221,23 +239,25 @@ typedef enum DRWBatchFlag {
   MBC_WIRE_LOOPS = (1u << MBC_BATCH_INDEX(wire_loops)),
   MBC_WIRE_LOOPS_UVS = (1u << MBC_BATCH_INDEX(wire_loops_uvs)),
   MBC_SCULPT_OVERLAYS = (1u << MBC_BATCH_INDEX(sculpt_overlays)),
-} DRWBatchFlag;
+  MBC_SURFACE_PER_MAT = (1u << MBC_BATCH_LEN),
+};
+ENUM_OPERATORS(DRWBatchFlag, MBC_SURFACE_PER_MAT);
 
 BLI_STATIC_ASSERT(MBC_BATCH_LEN < 32, "Number of batches exceeded the limit of bit fields");
 
-typedef struct MeshExtractLooseGeom {
+struct MeshExtractLooseGeom {
   int edge_len;
   int vert_len;
   int *verts;
   int *edges;
-} MeshExtractLooseGeom;
+};
 
 /**
  * Data that are kept around between extractions to reduce rebuilding time.
  *
  * - Loose geometry.
  */
-typedef struct MeshBufferCache {
+struct MeshBufferCache {
   MeshBufferList buff;
 
   MeshExtractLooseGeom loose_geom;
@@ -247,7 +267,7 @@ typedef struct MeshBufferCache {
     int *mat_tri_len;
     int visible_tri_len;
   } poly_sorted;
-} MeshBufferCache;
+};
 
 #define FOREACH_MESH_BUFFER_CACHE(batch_cache, mbc) \
   for (MeshBufferCache *mbc = &batch_cache->final; \
@@ -256,7 +276,7 @@ typedef struct MeshBufferCache {
                  &batch_cache->cage : \
                  ((mbc == &batch_cache->cage) ? &batch_cache->uv_cage : NULL))
 
-typedef struct MeshBatchCache {
+struct MeshBatchCache {
   MeshBufferCache final, cage, uv_cage;
 
   MeshBatchList batch;
@@ -302,11 +322,13 @@ typedef struct MeshBatchCache {
 
   eV3DShadingColorType color_type;
   bool pbvh_is_drawing;
-} MeshBatchCache;
+};
 
 #define MBC_EDITUV \
   (MBC_EDITUV_FACES_STRETCH_AREA | MBC_EDITUV_FACES_STRETCH_ANGLE | MBC_EDITUV_FACES | \
    MBC_EDITUV_EDGES | MBC_EDITUV_VERTS | MBC_EDITUV_FACEDOTS | MBC_WIRE_LOOPS_UVS)
+
+namespace blender::draw {
 
 void mesh_buffer_cache_create_requested(struct TaskGraph *task_graph,
                                         MeshBatchCache *cache,
@@ -328,6 +350,4 @@ void mesh_buffer_cache_create_requested_subdiv(MeshBatchCache *cache,
                                                struct DRWSubdivCache *subdiv_cache,
                                                struct MeshRenderData *mr);
 
-#ifdef __cplusplus
-}
-#endif
+}  // namespace blender::draw

source/blender/draw/CMakeLists.txt
@@ -39,8 +39,8 @@ set(INC
 set(SRC
   intern/draw_cache.c
   intern/draw_cache_extract_mesh.cc
-  intern/draw_cache_extract_mesh_render_data.c
-  intern/mesh_extractors/extract_mesh.c
+  intern/draw_cache_extract_mesh_render_data.cc
+  intern/mesh_extractors/extract_mesh.cc
   intern/mesh_extractors/extract_mesh_ibo_edituv.cc
   intern/mesh_extractors/extract_mesh_ibo_fdots.cc
   intern/mesh_extractors/extract_mesh_ibo_lines.cc
@@ -75,7 +75,7 @@ set(SRC
   intern/draw_cache_impl_displist.c
   intern/draw_cache_impl_gpencil.c
   intern/draw_cache_impl_lattice.c
-  intern/draw_cache_impl_mesh.c
+  intern/draw_cache_impl_mesh.cc
   intern/draw_cache_impl_metaball.c
   intern/draw_cache_impl_particles.c
   intern/draw_cache_impl_pointcloud.c
@@ -201,7 +201,7 @@ set(SRC
   intern/DRW_render.h
   intern/draw_attributes.h
   intern/draw_cache.h
-  intern/draw_cache_extract.h
+  intern/draw_cache_extract.hh
   intern/draw_cache_impl.h
   intern/draw_cache_inline.h
   intern/draw_color_management.h
@@ -221,7 +221,7 @@ set(SRC
   intern/draw_texture_pool.h
   intern/draw_view.h
   intern/draw_view_data.h
-  intern/mesh_extractors/extract_mesh.h
+  intern/mesh_extractors/extract_mesh.hh
   intern/smaa_textures.h
   engines/basic/basic_engine.h
   engines/basic/basic_private.h

source/blender/draw/intern/draw_attributes.cc
@@ -84,9 +84,9 @@ DRW_AttributeRequest *drw_attributes_add_request(DRW_Attributes *attrs,
 bool drw_custom_data_match_attribute(const CustomData *custom_data,
                                      const char *name,
                                      int *r_layer_index,
-                                     int *r_type)
+                                     eCustomDataType *r_type)
 {
-  const int possible_attribute_types[7] = {
+  const eCustomDataType possible_attribute_types[7] = {
       CD_PROP_BOOL,
       CD_PROP_INT8,
       CD_PROP_INT32,
@@ -97,7 +97,7 @@ bool drw_custom_data_match_attribute(const CustomData *custom_data,
   };
 
   for (int i = 0; i < ARRAY_SIZE(possible_attribute_types); i++) {
-    const int attr_type = possible_attribute_types[i];
+    const eCustomDataType attr_type = possible_attribute_types[i];
     int layer_index = CustomData_get_named_layer(custom_data, attr_type, name);
     if (layer_index == -1) {
       continue;

source/blender/draw/intern/draw_attributes.h
@@ -53,7 +53,7 @@ DRW_AttributeRequest *drw_attributes_add_request(DRW_Attributes *attrs,
 bool drw_custom_data_match_attribute(const CustomData *custom_data,
                                      const char *name,
                                      int *r_layer_index,
-                                     int *r_type);
+                                     eCustomDataType *r_type);
 
 #ifdef __cplusplus
 }

source/blender/draw/intern/draw_cache_extract_mesh.cc
@@ -24,11 +24,11 @@
 
 #include "GPU_capabilities.h"
 
-#include "draw_cache_extract.h"
+#include "draw_cache_extract.hh"
 #include "draw_cache_inline.h"
 #include "draw_subdivision.h"
 
-#include "mesh_extractors/extract_mesh.h"
+#include "mesh_extractors/extract_mesh.hh"
 
 // #define DEBUG_TIME
 
@@ -551,21 +551,21 @@ static struct TaskNode *mesh_extract_render_data_node_create(struct TaskGraph *t
 /** \name Extract Loop
  * \{ */
 
-static void mesh_buffer_cache_create_requested(struct TaskGraph *task_graph,
-                                               MeshBatchCache *cache,
-                                               MeshBufferCache *mbc,
-                                               Object *object,
-                                               Mesh *me,
+void mesh_buffer_cache_create_requested(struct TaskGraph *task_graph,
+                                        MeshBatchCache *cache,
+                                        MeshBufferCache *mbc,
+                                        Object *object,
+                                        Mesh *me,
 
-                                               const bool is_editmode,
-                                               const bool is_paint_mode,
-                                               const bool is_mode_active,
-                                               const float obmat[4][4],
-                                               const bool do_final,
-                                               const bool do_uvedit,
-                                               const Scene *scene,
-                                               const ToolSettings *ts,
-                                               const bool use_hide)
+                                        const bool is_editmode,
+                                        const bool is_paint_mode,
+                                        const bool is_mode_active,
+                                        const float obmat[4][4],
+                                        const bool do_final,
+                                        const bool do_uvedit,
+                                        const Scene *scene,
+                                        const ToolSettings *ts,
+                                        const bool use_hide)
 {
   /* For each mesh where batches needs to be updated a sub-graph will be added to the task_graph.
    * This sub-graph starts with an extract_render_data_node. This fills/converts the required
@@ -772,10 +772,10 @@ static void mesh_buffer_cache_create_requested(struct TaskGraph *task_graph,
 /** \name Subdivision Extract Loop
  * \{ */
 
-static void mesh_buffer_cache_create_requested_subdiv(MeshBatchCache *cache,
-                                                      MeshBufferCache *mbc,
-                                                      DRWSubdivCache *subdiv_cache,
-                                                      MeshRenderData *mr)
+void mesh_buffer_cache_create_requested_subdiv(MeshBatchCache *cache,
+                                               MeshBufferCache *mbc,
+                                               DRWSubdivCache *subdiv_cache,
+                                               MeshRenderData *mr)
 {
   /* Create an array containing all the extractors that needs to be executed. */
   ExtractorRunDatas extractors;
@@ -908,46 +908,3 @@ static void mesh_buffer_cache_create_requested_subdiv(MeshBatchCache *cache,
 /** \} */
 
 }  // namespace blender::draw
-
-extern "C" {
-void mesh_buffer_cache_create_requested(struct TaskGraph *task_graph,
-                                        MeshBatchCache *cache,
-                                        MeshBufferCache *mbc,
-                                        Object *object,
-                                        Mesh *me,
-
-                                        const bool is_editmode,
-                                        const bool is_paint_mode,
-                                        const bool is_mode_active,
-                                        const float obmat[4][4],
-                                        const bool do_final,
-                                        const bool do_uvedit,
-                                        const Scene *scene,
-                                        const ToolSettings *ts,
-                                        const bool use_hide)
-{
-  blender::draw::mesh_buffer_cache_create_requested(task_graph,
-                                                    cache,
-                                                    mbc,
-                                                    object,
-                                                    me,
-                                                    is_editmode,
-                                                    is_paint_mode,
-                                                    is_mode_active,
-                                                    obmat,
-                                                    do_final,
-                                                    do_uvedit,
-                                                    scene,
-                                                    ts,
-                                                    use_hide);
-}
-
-void mesh_buffer_cache_create_requested_subdiv(MeshBatchCache *cache,
-                                               MeshBufferCache *mbc,
-                                               DRWSubdivCache *subdiv_cache,
-                                               MeshRenderData *mr)
-{
-  blender::draw::mesh_buffer_cache_create_requested_subdiv(cache, mbc, subdiv_cache, mr);
-}
-
-}  // extern "C"

source/blender/draw/intern/draw_cache_extract_mesh_render_data.cc
@@ -9,7 +9,7 @@
 
 #include "MEM_guardedalloc.h"
 
-#include "BLI_alloca.h"
+#include "BLI_array.hh"
 #include "BLI_bitmap.h"
 #include "BLI_math.h"
 #include "BLI_task.h"
@@ -22,7 +22,7 @@
 
 #include "ED_mesh.h"
 
-#include "mesh_extractors/extract_mesh.h"
+#include "mesh_extractors/extract_mesh.hh"
 
 /* ---------------------------------------------------------------------- */
 /** \name Update Loose Geometry
@@ -78,7 +78,8 @@ static void mesh_render_data_loose_geom_mesh(const MeshRenderData *mr, MeshBuffe
 {
   BLI_bitmap *lvert_map = BLI_BITMAP_NEW(mr->vert_len, __func__);
 
-  cache->loose_geom.edges = MEM_mallocN(mr->edge_len * sizeof(*cache->loose_geom.edges), __func__);
+  cache->loose_geom.edges = static_cast<int *>(
+      MEM_mallocN(mr->edge_len * sizeof(*cache->loose_geom.edges), __func__));
   const MEdge *med = mr->medge;
   for (int med_index = 0; med_index < mr->edge_len; med_index++, med++) {
     if (med->flag & ME_LOOSEEDGE) {
@@ -89,19 +90,20 @@ static void mesh_render_data_loose_geom_mesh(const MeshRenderData *mr, MeshBuffe
     BLI_BITMAP_ENABLE(lvert_map, med->v2);
   }
   if (cache->loose_geom.edge_len < mr->edge_len) {
-    cache->loose_geom.edges = MEM_reallocN(
-        cache->loose_geom.edges, cache->loose_geom.edge_len * sizeof(*cache->loose_geom.edges));
+    cache->loose_geom.edges = static_cast<int *>(MEM_reallocN(
+        cache->loose_geom.edges, cache->loose_geom.edge_len * sizeof(*cache->loose_geom.edges)));
   }
 
-  cache->loose_geom.verts = MEM_mallocN(mr->vert_len * sizeof(*cache->loose_geom.verts), __func__);
+  cache->loose_geom.verts = static_cast<int *>(
+      MEM_mallocN(mr->vert_len * sizeof(*cache->loose_geom.verts), __func__));
   for (int v = 0; v < mr->vert_len; v++) {
     if (!BLI_BITMAP_TEST(lvert_map, v)) {
       cache->loose_geom.verts[cache->loose_geom.vert_len++] = v;
     }
   }
   if (cache->loose_geom.vert_len < mr->vert_len) {
-    cache->loose_geom.verts = MEM_reallocN(
-        cache->loose_geom.verts, cache->loose_geom.vert_len * sizeof(*cache->loose_geom.verts));
+    cache->loose_geom.verts = static_cast<int *>(MEM_reallocN(
+        cache->loose_geom.verts, cache->loose_geom.vert_len * sizeof(*cache->loose_geom.verts)));
   }
 
   MEM_freeN(lvert_map);
@@ -112,15 +114,16 @@ static void mesh_render_data_lverts_bm(const MeshRenderData *mr, MeshBufferCache
   int elem_id;
   BMIter iter;
   BMVert *eve;
-  cache->loose_geom.verts = MEM_mallocN(mr->vert_len * sizeof(*cache->loose_geom.verts), __func__);
+  cache->loose_geom.verts = static_cast<int *>(
+      MEM_mallocN(mr->vert_len * sizeof(*cache->loose_geom.verts), __func__));
   BM_ITER_MESH_INDEX (eve, &iter, bm, BM_VERTS_OF_MESH, elem_id) {
     if (eve->e == NULL) {
       cache->loose_geom.verts[cache->loose_geom.vert_len++] = elem_id;
     }
   }
   if (cache->loose_geom.vert_len < mr->vert_len) {
-    cache->loose_geom.verts = MEM_reallocN(
-        cache->loose_geom.verts, cache->loose_geom.vert_len * sizeof(*cache->loose_geom.verts));
+    cache->loose_geom.verts = static_cast<int *>(MEM_reallocN(
+        cache->loose_geom.verts, cache->loose_geom.vert_len * sizeof(*cache->loose_geom.verts)));
   }
 }
 
@@ -129,15 +132,16 @@ static void mesh_render_data_ledges_bm(const MeshRenderData *mr, MeshBufferCache
   int elem_id;
   BMIter iter;
   BMEdge *ede;
-  cache->loose_geom.edges = MEM_mallocN(mr->edge_len * sizeof(*cache->loose_geom.edges), __func__);
+  cache->loose_geom.edges = static_cast<int *>(
+      MEM_mallocN(mr->edge_len * sizeof(*cache->loose_geom.edges), __func__));
   BM_ITER_MESH_INDEX (ede, &iter, bm, BM_EDGES_OF_MESH, elem_id) {
     if (ede->l == NULL) {
       cache->loose_geom.edges[cache->loose_geom.edge_len++] = elem_id;
     }
   }
   if (cache->loose_geom.edge_len < mr->edge_len) {
-    cache->loose_geom.edges = MEM_reallocN(
-        cache->loose_geom.edges, cache->loose_geom.edge_len * sizeof(*cache->loose_geom.edges));
+    cache->loose_geom.edges = static_cast<int *>(MEM_reallocN(
+        cache->loose_geom.edges, cache->loose_geom.edge_len * sizeof(*cache->loose_geom.edges)));
   }
 }
 
@@ -192,12 +196,13 @@ static void mesh_render_data_polys_sorted_ensure(MeshRenderData *mr, MeshBufferC
 
 static void mesh_render_data_polys_sorted_build(MeshRenderData *mr, MeshBufferCache *cache)
 {
-  int *tri_first_index = MEM_mallocN(sizeof(*tri_first_index) * mr->poly_len, __func__);
+  int *tri_first_index = static_cast<int *>(
+      MEM_mallocN(sizeof(*tri_first_index) * mr->poly_len, __func__));
   int *mat_tri_len = mesh_render_data_mat_tri_len_build(mr);
 
   /* Apply offset. */
   int visible_tri_len = 0;
-  int *mat_tri_offs = BLI_array_alloca(mat_tri_offs, mr->mat_len);
+  blender::Array<int, 32> mat_tri_offs(mr->mat_len);
   {
     for (int i = 0; i < mr->mat_len; i++) {
       mat_tri_offs[i] = visible_tri_len;
@@ -245,8 +250,8 @@ static void mesh_render_data_mat_tri_len_bm_range_fn(void *__restrict userdata,
                                                      const int iter,
                                                      const TaskParallelTLS *__restrict tls)
 {
-  MeshRenderData *mr = userdata;
-  int *mat_tri_len = tls->userdata_chunk;
+  MeshRenderData *mr = static_cast<MeshRenderData *>(userdata);
+  int *mat_tri_len = static_cast<int *>(tls->userdata_chunk);
 
   BMesh *bm = mr->bm;
   BMFace *efa = BM_face_at_index(bm, iter);
@@ -260,8 +265,8 @@ static void mesh_render_data_mat_tri_len_mesh_range_fn(void *__restrict userdata
                                                        const int iter,
                                                        const TaskParallelTLS *__restrict tls)
 {
-  MeshRenderData *mr = userdata;
-  int *mat_tri_len = tls->userdata_chunk;
+  MeshRenderData *mr = static_cast<MeshRenderData *>(userdata);
+  int *mat_tri_len = static_cast<int *>(tls->userdata_chunk);
 
   const MPoly *mp = &mr->mpoly[iter];
   if (!(mr->use_hide && (mp->flag & ME_HIDE))) {
@@ -274,9 +279,9 @@ static void mesh_render_data_mat_tri_len_reduce_fn(const void *__restrict userda
                                                    void *__restrict chunk_join,
                                                    void *__restrict chunk)
 {
-  const MeshRenderData *mr = userdata;
-  int *dst_mat_len = chunk_join;
-  int *src_mat_len = chunk;
+  const MeshRenderData *mr = static_cast<const MeshRenderData *>(userdata);
+  int *dst_mat_len = static_cast<int *>(chunk_join);
+  int *src_mat_len = static_cast<int *>(chunk);
   for (int i = 0; i < mr->mat_len; i++) {
     dst_mat_len[i] += src_mat_len[i];
   }
@@ -288,7 +293,7 @@ static int *mesh_render_data_mat_tri_len_build_threaded(MeshRenderData *mr,
 {
   /* Extending the #MatOffsetUserData with an int per material slot. */
   size_t mat_tri_len_size = sizeof(int) * mr->mat_len;
-  int *mat_tri_len = MEM_callocN(mat_tri_len_size, __func__);
+  int *mat_tri_len = static_cast<int *>(MEM_callocN(mat_tri_len_size, __func__));
 
   TaskParallelSettings settings;
   BLI_parallel_range_settings_defaults(&settings);
@@ -330,7 +335,8 @@ void mesh_render_data_update_looptris(MeshRenderData *mr,
       /* NOTE(campbell): It's possible to skip allocating tessellation,
        * the tessellation can be calculated as part of the iterator, see: P2188.
        * The overall advantage is small (around 1%), so keep this as-is. */
-      mr->mlooptri = MEM_mallocN(sizeof(*mr->mlooptri) * mr->tri_len, "MR_DATATYPE_LOOPTRI");
+      mr->mlooptri = static_cast<MLoopTri *>(
+          MEM_mallocN(sizeof(*mr->mlooptri) * mr->tri_len, "MR_DATATYPE_LOOPTRI"));
       if (mr->poly_normals != NULL) {
         BKE_mesh_recalc_looptri_with_normals(me->mloop,
                                              me->mpoly,
@@ -368,8 +374,10 @@ void mesh_render_data_update_normals(MeshRenderData *mr, const eMRDataType data_
       mr->poly_normals = BKE_mesh_poly_normals_ensure(mr->me);
     }
     if (((data_flag & MR_DATA_LOOP_NOR) && is_auto_smooth) || (data_flag & MR_DATA_TAN_LOOP_NOR)) {
-      mr->loop_normals = MEM_mallocN(sizeof(*mr->loop_normals) * mr->loop_len, __func__);
-      short(*clnors)[2] = CustomData_get_layer(&mr->me->ldata, CD_CUSTOMLOOPNORMAL);
+      mr->loop_normals = static_cast<float(*)[3]>(
+          MEM_mallocN(sizeof(*mr->loop_normals) * mr->loop_len, __func__));
+      short(*clnors)[2] = static_cast<short(*)[2]>(
+          CustomData_get_layer(&mr->me->ldata, CD_CUSTOMLOOPNORMAL));
       BKE_mesh_normals_loop_split(mr->me->mvert,
                                   mr->vert_normals,
                                   mr->vert_len,
@@ -405,7 +413,8 @@ void mesh_render_data_update_normals(MeshRenderData *mr, const eMRDataType data_
         poly_normals = mr->bm_poly_normals;
       }
 
-      mr->loop_normals = MEM_mallocN(sizeof(*mr->loop_normals) * mr->loop_len, __func__);
+      mr->loop_normals = static_cast<float(*)[3]>(
+          MEM_mallocN(sizeof(*mr->loop_normals) * mr->loop_len, __func__));
       const int clnors_offset = CustomData_get_offset(&mr->bm->ldata, CD_CUSTOMLOOPNORMAL);
       BM_loops_calc_normal_vcos(mr->bm,
                                 vert_coords,
@@ -432,7 +441,7 @@ MeshRenderData *mesh_render_data_create(Object *object,
                                         const bool do_uvedit,
                                         const ToolSettings *ts)
 {
-  MeshRenderData *mr = MEM_callocN(sizeof(*mr), __func__);
+  MeshRenderData *mr = static_cast<MeshRenderData *>(MEM_callocN(sizeof(*mr), __func__));
   mr->toolsettings = ts;
   mr->mat_len = mesh_render_mat_len_get(object, me);
 
@@ -491,9 +500,12 @@ MeshRenderData *mesh_render_data_create(Object *object,
 #endif
 
     if (use_mapped) {
-      mr->v_origindex = CustomData_get_layer(&mr->me->vdata, CD_ORIGINDEX);
-      mr->e_origindex = CustomData_get_layer(&mr->me->edata, CD_ORIGINDEX);
-      mr->p_origindex = CustomData_get_layer(&mr->me->pdata, CD_ORIGINDEX);
+      mr->v_origindex = static_cast<const int *>(
+          CustomData_get_layer(&mr->me->vdata, CD_ORIGINDEX));
+      mr->e_origindex = static_cast<const int *>(
+          CustomData_get_layer(&mr->me->edata, CD_ORIGINDEX));
+      mr->p_origindex = static_cast<const int *>(
+          CustomData_get_layer(&mr->me->pdata, CD_ORIGINDEX));
 
       use_mapped = (mr->v_origindex || mr->e_origindex || mr->p_origindex);
     }
@@ -513,9 +525,12 @@ MeshRenderData *mesh_render_data_create(Object *object,
 
     bool use_mapped = is_paint_mode && mr->me && !mr->me->runtime.is_original;
     if (use_mapped) {
-      mr->v_origindex = CustomData_get_layer(&mr->me->vdata, CD_ORIGINDEX);
-      mr->e_origindex = CustomData_get_layer(&mr->me->edata, CD_ORIGINDEX);
-      mr->p_origindex = CustomData_get_layer(&mr->me->pdata, CD_ORIGINDEX);
+      mr->v_origindex = static_cast<const int *>(
+          CustomData_get_layer(&mr->me->vdata, CD_ORIGINDEX));
+      mr->e_origindex = static_cast<const int *>(
+          CustomData_get_layer(&mr->me->edata, CD_ORIGINDEX));
+      mr->p_origindex = static_cast<const int *>(
+          CustomData_get_layer(&mr->me->pdata, CD_ORIGINDEX));
 
       use_mapped = (mr->v_origindex || mr->e_origindex || mr->p_origindex);
     }
@@ -531,14 +546,14 @@ MeshRenderData *mesh_render_data_create(Object *object,
     mr->poly_len = mr->me->totpoly;
     mr->tri_len = poly_to_tri_count(mr->poly_len, mr->loop_len);
 
-    mr->mvert = CustomData_get_layer(&mr->me->vdata, CD_MVERT);
-    mr->medge = CustomData_get_layer(&mr->me->edata, CD_MEDGE);
-    mr->mloop = CustomData_get_layer(&mr->me->ldata, CD_MLOOP);
-    mr->mpoly = CustomData_get_layer(&mr->me->pdata, CD_MPOLY);
+    mr->mvert = static_cast<MVert *>(CustomData_get_layer(&mr->me->vdata, CD_MVERT));
+    mr->medge = static_cast<MEdge *>(CustomData_get_layer(&mr->me->edata, CD_MEDGE));
+    mr->mloop = static_cast<MLoop *>(CustomData_get_layer(&mr->me->ldata, CD_MLOOP));
+    mr->mpoly = static_cast<MPoly *>(CustomData_get_layer(&mr->me->pdata, CD_MPOLY));
 
-    mr->v_origindex = CustomData_get_layer(&mr->me->vdata, CD_ORIGINDEX);
-    mr->e_origindex = CustomData_get_layer(&mr->me->edata, CD_ORIGINDEX);
-    mr->p_origindex = CustomData_get_layer(&mr->me->pdata, CD_ORIGINDEX);
+    mr->v_origindex = static_cast<const int *>(CustomData_get_layer(&mr->me->vdata, CD_ORIGINDEX));
+    mr->e_origindex = static_cast<const int *>(CustomData_get_layer(&mr->me->edata, CD_ORIGINDEX));
+    mr->p_origindex = static_cast<const int *>(CustomData_get_layer(&mr->me->pdata, CD_ORIGINDEX));
   }
   else {
     /* #BMesh */

source/blender/draw/intern/draw_cache_impl_curves.cc
@@ -512,7 +512,7 @@ static bool curves_ensure_attributes(const Curves &curves,
   ListBase gpu_attrs = GPU_material_attributes(gpu_material);
   LISTBASE_FOREACH (GPUMaterialAttribute *, gpu_attr, &gpu_attrs) {
     const char *name = gpu_attr->name;
-    int type = gpu_attr->type;
+    eCustomDataType type = static_cast<eCustomDataType>(gpu_attr->type);
     int layer = -1;
     eAttrDomain domain;
 

commit 4fc1dac1927eea2d521d2eccda984c59522f8e01

Fix Windows compiler error
This error was introduced in 5b1486625934d0e444780cd32848880bd15dfe08

Fix provided by @deadpin

source/blender/draw/intern/draw_cache_extract.hh
@@ -196,8 +196,8 @@ struct MeshBatchList {
 };
 
 #define MBC_BATCH_LEN (sizeof(MeshBatchList) / sizeof(void *))
-#define MBC_VBO_LEN (sizeof(((MeshBufferList){0}).vbo) / sizeof(void *))
-#define MBC_IBO_LEN (sizeof(((MeshBufferList){0}).ibo) / sizeof(void *))
+#define MBC_VBO_LEN (sizeof((MeshBufferList{0}).vbo) / sizeof(void *))
+#define MBC_IBO_LEN (sizeof((MeshBufferList{0}).ibo) / sizeof(void *))
 
 #define MBC_BATCH_INDEX(batch) (offsetof(MeshBatchList, batch) / sizeof(void *))
 