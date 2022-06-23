1d8d16a22852959e118b8e9361182ded13fcbe97
Iyad Ahmed <iyadahmed430@gmail.com>
2022-06-23 22:06:07 +0200

    #2983: Refactor icon system

--- release/scripts/addons/bfa_brush_panel/weight_paint.py
+++ release/scripts/addons/bfa_brush_panel/weight_paint.py
@@ -1,29 +1,26 @@
-import os
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Dict, List, Union
+from typing import Dict, List
 
 import bpy
-import bpy.utils.previews
 
-from .common import DEFAULT_ICON_FOR_BLEND_MODE, DEFAULT_ICON_FOR_IDNAME, _icon_value_from_icon_name, column_count
+from .common import column_count
+from .icon_manager import BrushIcon, get_brush_icon
 
 
-def get_weight_brush_icon(weight_brush: bpy.types.Brush):
+def icon_name_from_weight_brush(weight_brush: bpy.types.Brush):
     # Values should match toolsystem
     # release\scripts\startup\bl_ui\space_toolsystem_toolbar.py
     icon_prefix = "brush.paint_weight."
     idname = weight_brush.weight_tool
     icon_name = icon_prefix + idname.lower()
-    print("Loading brush icon", weight_brush, idname)
-    return _icon_value_from_icon_name(icon_name)
+    return icon_name
 
 
 @dataclass
 class WeightBrushButton:
     brush_name: str
-    icon_name: str
-    icon_value: int
+    brush_icon: BrushIcon
 
     def draw(self, context: bpy.types.Context, layout: bpy.types.UILayout, icon_only=False):
         active_brush = context.tool_settings.weight_paint.brush
@@ -34,8 +31,8 @@ class WeightBrushButton:
         op = layout.operator(
             "bfa.set_brush",
             text="" if icon_only else self.brush_name,
-            icon=self.icon_name,
-            icon_value=self.icon_value,
+            icon=self.brush_icon.icon_name,
+            icon_value=self.brush_icon.icon_value,
             depress=is_active,
         )
         # TODO: make "weight_paint" variable to support other paint modes
@@ -54,39 +51,20 @@ def get_weight_brush_buttons():
     for brush in sorted(bpy.data.brushes, key=lambda b: b.name):
         if not brush.use_paint_weight:
             continue
-        if brush.use_custom_icon and brush.icon_filepath:
-            pcoll = preview_collections["main"]
-            filepath = os.path.abspath(bpy.path.abspath(brush.icon_filepath))
-            try:
-                preview = pcoll.load(filepath, filepath, "IMAGE")
-            except KeyError:
-                # Blender API raises KeyError if image is already in the collection
-                preview = pcoll[filepath]
-
-            icon_value = preview.icon_id
-            icon_name = "NONE"
-        else:
-            # TODO: refactor
-            icon_value = 0
-            if brush.blend == "MIX":
-                icon_name = DEFAULT_ICON_FOR_IDNAME.get(brush.weight_tool, None)
-            else:
-                icon_name = DEFAULT_ICON_FOR_BLEND_MODE.get(brush.blend, None)
-            if icon_name is None:
-                icon_name = "NONE"
-                icon_value = get_weight_brush_icon(brush)
-        buttons[brush.weight_tool].append(WeightBrushButton(brush.name, icon_name, icon_value))
+        icon = get_brush_icon(brush, icon_name_from_weight_brush)
+        buttons[brush.weight_tool].append(WeightBrushButton(brush.name, icon))
         # TODO: support linked brushes
     return buttons
 
 
 class WeightBrushPanelBase(bpy.types.Panel):
+    bl_label = "Brush"  # Override this
     bl_region_type = "TOOLS"
     bl_space_type = "VIEW_3D"
     bl_category = "Brushes"
     bl_options = {"HIDE_BG"}
 
-    tool_name = ""
+    tool_name = ""  # Override this
 
     @classmethod
     def poll(cls, context):
@@ -114,16 +92,7 @@ panel_classes = [type(f"BFA_PT_brush_weight_{tool_name}", (WeightBrushPanelBase,
                  for tool_name in WEIGHT_TOOLS]
 
 
-# Custom Image Previews
-preview_collections: Dict[
-    str,
-    Union[bpy.utils.previews.ImagePreviewCollection, Dict[str, bpy.types.ImagePreview]],
-] = {}
-
-
 def register():
-    pcoll = bpy.utils.previews.new()
-    preview_collections["main"] = pcoll
     for cls in panel_classes:
         bpy.utils.register_class(cls)
 
@@ -131,7 +100,3 @@ def register():
 def unregister():
     for cls in panel_classes:
         bpy.utils.unregister_class(cls)
-
-    for pcoll in preview_collections.values():
-        bpy.utils.previews.remove(pcoll)
-    preview_collections.clear()
