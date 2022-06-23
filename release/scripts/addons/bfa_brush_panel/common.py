baf4dcc14d45c5bfab4062019a25b87fcdc74d13
Iyad Ahmed <iyadahmed430@gmail.com>
2022-06-23 16:59:02 +0200

    #2983 Move common code out of weight_paint.py

--- release/scripts/addons/bfa_brush_panel/common.py
+++ release/scripts/addons/bfa_brush_panel/common.py
@@ -65,3 +65,21 @@ def _icon_value_from_icon_name(icon_name):
             icon_value = 0
     __icon_cache[icon_name] = icon_value
     return icon_value
+
+
+def column_count(region: bpy.types.Region):
+    system = bpy.context.preferences.system
+    view2d = region.view2d
+    view2d_scale = view2d.region_to_view(1.0, 0.0)[0] - view2d.region_to_view(0.0, 0.0)[0]
+    width_scale = region.width * view2d_scale / system.ui_scale
+
+    if width_scale > 160.0:
+        column_count = 4
+    elif width_scale > 120.0:
+        column_count = 3
+    elif width_scale > 80:
+        column_count = 2
+    else:
+        column_count = 1
+
+    return column_count
