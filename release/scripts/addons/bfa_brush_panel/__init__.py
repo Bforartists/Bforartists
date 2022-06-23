c64503ffea47c0ea41b2e27334f243ae16e94f25
Iyad Ahmed <iyadahmed430@gmail.com>
2022-06-23 16:59:02 +0200

    #2983 Move common code out of weight_paint.py

--- release/scripts/addons/bfa_brush_panel/__init__.py
+++ release/scripts/addons/bfa_brush_panel/__init__.py
@@ -32,7 +32,7 @@ import sys
 
 from bpy.utils import register_submodule_factory
 
-submodule_names = ["weight_paint"]
+submodule_names = ["operators", "weight_paint"]
 
 
 register, _unregister = register_submodule_factory(__name__, submodule_names)
