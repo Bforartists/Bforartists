5f1858c2ea61fdf856d7b7191a33d1fb40ad7975
Iyad Ahmed <iyadahmed430@gmail.com>
2022-06-23 23:44:20 +0200

    #2983: Refactor brush buttons drawing

--- release/scripts/addons/bfa_brush_panel/weight_paint.py
+++ release/scripts/addons/bfa_brush_panel/weight_paint.py
@@ -1,9 +1,6 @@
-from collections import defaultdict
-from typing import Dict, List
-
 import bpy
 
-from .common import BrushButton, column_count
+from .common import column_count, draw_brush_button
 
 
 def icon_name_from_weight_brush(weight_brush: bpy.types.Brush):
@@ -13,18 +10,6 @@ def icon_name_from_weight_brush(weight_brush: bpy.types.Brush):
     idname = weight_brush.weight_tool
     icon_name = icon_prefix + idname.lower()
     return icon_name
-
-
-def get_weight_brush_buttons():
-    """Get mapping from tool name to weight brush buttons from brushes in the blend file"""
-    buttons: Dict[str, List[BrushButton]] = defaultdict(list)
-    brush: bpy.types.Brush
-    for brush in sorted(bpy.data.brushes, key=lambda b: b.name):
-        if not brush.use_paint_weight:
-            continue
-        buttons[brush.weight_tool].append(BrushButton(brush, "weight_paint", icon_name_from_weight_brush))
-        # TODO: support linked brushes
-    return buttons
 
 
 # TODO: generalize this base calss to work with all modes
@@ -44,7 +29,6 @@ class WeightBrushPanelBase(bpy.types.Panel):
     def draw(self, context: bpy.types.Context):
         layout = self.layout
         layout.scale_y = 2
-        buttons = get_weight_brush_buttons()
         num_cols = column_count(context.region)
         if num_cols == 4:
             col = layout.column(align=True)
@@ -53,8 +37,18 @@ class WeightBrushPanelBase(bpy.types.Panel):
             col = layout.column_flow(columns=num_cols, align=True)
             icon_only = True
 
-        for but in buttons[self.tool_name]:
-            but.draw(context, col, icon_only=icon_only)
+        for brush in sorted(bpy.data.brushes, key=lambda brush: brush.name):
+            if not brush.use_paint_weight:
+                continue
+            if brush.weight_tool == self.tool_name:
+                draw_brush_button(
+                    context,
+                    col,
+                    icon_only,
+                    brush,
+                    "weight_paint",
+                    icon_name_from_weight_brush,
+                )
 
 
 WEIGHT_TOOLS = ["DRAW", "SMEAR", "AVERAGE", "BLUR"]
