038835428a26abed69b8a9d0eaad4e3412b95d1f
Iyad Ahmed <iyadahmed430@gmail.com>
2022-06-23 23:44:20 +0200

    #2983: Refactor brush buttons drawing

--- release/scripts/addons/bfa_brush_panel/common.py
+++ release/scripts/addons/bfa_brush_panel/common.py
@@ -1,4 +1,3 @@
-
 from typing import Callable
 
 import bpy
@@ -9,7 +8,9 @@ from .icon_system import get_brush_icon
 def column_count(region: bpy.types.Region):
     system = bpy.context.preferences.system
     view2d = region.view2d
-    view2d_scale = view2d.region_to_view(1.0, 0.0)[0] - view2d.region_to_view(0.0, 0.0)[0]
+    view2d_scale = (
+        view2d.region_to_view(1.0, 0.0)[0] - view2d.region_to_view(0.0, 0.0)[0]
+    )
     width_scale = region.width * view2d_scale / system.ui_scale
 
     if width_scale > 160.0:
@@ -24,26 +25,29 @@ def column_count(region: bpy.types.Region):
     return column_count
 
 
-class BrushButton:
-    def __init__(self, brush: bpy.types.Brush, tool_settings_attribute_name: str,
-                 icon_name_from_brush: Callable[[bpy.types.Brush], str]):
-        self.brush_name = brush.name
-        self.tool_settings_attribute_name = tool_settings_attribute_name
-        self.icon = get_brush_icon(brush, icon_name_from_brush)
+def draw_brush_button(
+    context: bpy.types.Context,
+    layout: bpy.types.UILayout,
+    icon_only: bool,
+    brush: bpy.types.Brush,
+    tool_settings_attribute_name: str,
+    icon_name_from_brush: Callable[[bpy.types.Brush], str],
+):
+    brush_name = brush.name
+    icon = get_brush_icon(brush, icon_name_from_brush)
+    active_brush = context.tool_settings.weight_paint.brush
+    is_active = False
+    if active_brush is not None:
+        is_active = active_brush.name == brush_name
 
-    def draw(self, context: bpy.types.Context, layout: bpy.types.UILayout, icon_only=False):
-        active_brush = context.tool_settings.weight_paint.brush
-        is_active = False
-        if active_brush is not None:
-            is_active = active_brush.name == self.brush_name
+    op = layout.operator(
+        "bfa.set_brush",
+        text="" if icon_only else brush_name,
+        icon=icon.icon_name,
+        icon_value=icon.icon_value,
+        depress=is_active,
+    )
 
-        op = layout.operator(
-            "bfa.set_brush",
-            text="" if icon_only else self.brush_name,
-            icon=self.icon.icon_name,
-            icon_value=self.icon.icon_value,
-            depress=is_active,
-        )
-        op.paint_settings_attr_name = self.tool_settings_attribute_name
-        op.brush_name = self.brush_name
-        op.dynamic_description = self.brush_name if icon_only else "Set Brush"
+    op.paint_settings_attr_name = tool_settings_attribute_name
+    op.brush_name = brush_name
+    op.dynamic_description = brush_name if icon_only else "Set Brush"
