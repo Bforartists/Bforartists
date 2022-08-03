c9d7db97bfb9cebd87ad05ecca676849346cd29f
Iyad Ahmed <iyadahmed430@gmail.com>
2022-08-03 14:22:04 +0200

    Refactor Brush Panels add-on

--- release/scripts/addons/bfa_brush_panel/texture_paint.py
+++ release/scripts/addons/bfa_brush_panel/texture_paint.py
@@ -1,42 +1,17 @@
 import bpy
 
-from .common import BrushPanelBase
+from .panels import panel_factory_view3d
 
 
-def icon_name_from_texture_brush(texture_brush: bpy.types.Brush):
-    # Values should match toolsystem
-    # release\scripts\startup\bl_ui\space_toolsystem_toolbar.py
-    icon_prefix = "brush.paint_texture."
-    tool_name = texture_brush.image_tool
-    icon_name = icon_prefix + tool_name.lower()
-    return icon_name
-
-
-def tool_name_from_texture_brush(texture_brush: bpy.types.Brush):
-    return texture_brush.image_tool
-
-
-def filter_texture_brush(texture_brush: bpy.types.Brush):
-    return texture_brush.use_paint_image
-
-
-TEXTURE_TOOLS = ["DRAW", "SOFTEN", "SMEAR", "CLONE", "FILL", "MASK"]
-panel_classes = [
-    type(
-        f"BFA_PT_brush_texture_{tool_name}",
-        (BrushPanelBase,),
-        {
-            "bl_label": tool_name.capitalize(),
-            "tool_name": tool_name,
-            "mode": "PAINT_TEXTURE",
-            "tool_settings_attribute_name": "image_paint",
-            "filter_brush": staticmethod(filter_texture_brush),
-            "icon_name_from_brush": staticmethod(icon_name_from_texture_brush),
-            "tool_name_from_brush": staticmethod(tool_name_from_texture_brush),
-        },
-    )
-    for tool_name in TEXTURE_TOOLS
-]
+panel_classes = list(
+    panel_factory_view3d(
+        tools=["DRAW", "SOFTEN", "SMEAR", "CLONE", "FILL", "MASK"],
+        icon_prefix="brush.paint_texture.",
+        tool_name_attr="image_tool",
+        use_paint_attr="use_paint_image",
+        tool_settings_attr="image_paint",
+        mode="PAINT_TEXTURE")
+)
 
 
 def register():
--- release/scripts/addons/bfa_brush_panel/vertex_paint.py
+++ release/scripts/addons/bfa_brush_panel/vertex_paint.py
@@ -1,42 +1,17 @@
 import bpy
 
-from .common import BrushPanelBase
+from .panels import panel_factory_view3d
 
 
-def icon_name_from_vertex_brush(vertex_brush: bpy.types.Brush):
-    # Values should match toolsystem
-    # release\scripts\startup\bl_ui\space_toolsystem_toolbar.py
-    icon_prefix = "brush.paint_vertex."
-    tool_name = vertex_brush.vertex_tool
-    icon_name = icon_prefix + tool_name.lower()
-    return icon_name
-
-
-def tool_name_from_vertex_brush(vertex_brush: bpy.types.Brush):
-    return vertex_brush.vertex_tool
-
-
-def filter_vertex_brush(vertex_brush: bpy.types.Brush):
-    return vertex_brush.use_paint_vertex
-
-
-VERTEX_TOOLS = ["DRAW", "SMEAR", "AVERAGE", "BLUR"]
-panel_classes = [
-    type(
-        f"BFA_PT_brush_vertex_{tool_name}",
-        (BrushPanelBase,),
-        {
-            "bl_label": tool_name.capitalize(),
-            "tool_name": tool_name,
-            "mode": "PAINT_VERTEX",
-            "tool_settings_attribute_name": "vertex_paint",
-            "filter_brush": staticmethod(filter_vertex_brush),
-            "icon_name_from_brush": staticmethod(icon_name_from_vertex_brush),
-            "tool_name_from_brush": staticmethod(tool_name_from_vertex_brush),
-        },
-    )
-    for tool_name in VERTEX_TOOLS
-]
+panel_classes = list(
+    panel_factory_view3d(
+        tools=["DRAW", "SMEAR", "AVERAGE", "BLUR"],
+        icon_prefix="brush.paint_vertex.",
+        tool_name_attr="vertex_tool",
+        use_paint_attr="use_paint_vertex",
+        tool_settings_attr="vertex_paint",
+        mode="PAINT_VERTEX")
+)
 
 
 def register():
