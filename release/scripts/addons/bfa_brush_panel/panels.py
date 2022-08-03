9bd31ee5aa80ef7e253091dd130e0e0766bf90a2
Iyad Ahmed <iyadahmed430@gmail.com>
2022-08-03 14:22:04 +0200

    Refactor Brush Panels add-on

---
+++ release/scripts/addons/bfa_brush_panel/panels.py
@@ -0,0 +1,58 @@
+from typing import Callable, Iterable
+
+import bpy
+
+from .common import BrushPanelBase
+
+
+def panel_factory(
+        tools: Iterable[str],
+        icon_prefix: str,
+        tool_name_attr: str,
+        use_paint_attr: str,
+        tool_settings_attr: str,
+        poll: Callable[[bpy.types.Panel, bpy.types.Context], bool],
+        panel_class_name_suffix: str
+):
+
+    @staticmethod
+    def tool_name_from_brush(brush: bpy.types.Brush):
+        return getattr(brush, tool_name_attr)
+
+    @staticmethod
+    def filter_brush(brush: bpy.types.Brush):
+        return getattr(brush, use_paint_attr)
+
+    @staticmethod
+    def icon_name_from_brush(brush: bpy.types.Brush):
+        return icon_prefix + tool_name_from_brush(brush).lower()
+
+    for tool_name in tools:
+        yield type(
+            f"BFA_PT_brush_{tool_name_attr}_{tool_name}_{panel_class_name_suffix}",
+            (BrushPanelBase,),
+            {
+                "bl_label": tool_name.capitalize(),
+                "tool_name": tool_name,
+                "poll": poll,
+                "tool_settings_attribute_name": tool_settings_attr,
+                "filter_brush": filter_brush,
+                "icon_name_from_brush": icon_name_from_brush,
+                "tool_name_from_brush": tool_name_from_brush,
+            },
+        )
+
+
+def panel_factory_view3d(
+        tools: Iterable[str],
+        icon_prefix: str,
+        tool_name_attr: str,
+        use_paint_attr: str,
+        tool_settings_attr: str,
+        mode: str):
+
+    @classmethod
+    def poll(cls, context):
+        return context.mode == mode
+
+    yield from panel_factory(tools, icon_prefix, tool_name_attr, use_paint_attr, tool_settings_attr, poll, mode)
