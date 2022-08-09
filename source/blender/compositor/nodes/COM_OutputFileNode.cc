Bforartists/Bforartists

commit db2383cffc3213ba0666a7c2a6da0b088be724ac

T84815: Missing preview image in compositing file output node
Hello,
I saw that this revision was stalled for a few months so I tried to update it.
https://developer.blender.org/D10995
I added a function that adds a preview on the first connected input of the file output node.
I removed the preview on the single layer format
Thanks

Reviewed By: #compositing, jbakker

Maniphest Tasks: T84815

Differential Revision: https://developer.blender.org/D14219

source/blender/compositor/nodes/COM_OutputFileNode.cc
@@ -35,11 +35,21 @@ void OutputFileNode::map_input_sockets(NodeConverter &converter,
   }
 }
 
+void OutputFileNode::add_preview_to_first_linked_input(NodeConverter &converter) const
+{
+  NodeInput *first_socket = this->get_input_socket(0);
+  if (first_socket->is_linked()) {
+    converter.add_node_input_preview(first_socket);
+  }
+}
+
 void OutputFileNode::convert_to_operations(NodeConverter &converter,
                                            const CompositorContext &context) const
 {
   NodeImageMultiFile *storage = (NodeImageMultiFile *)this->get_bnode()->storage;
   const bool is_multiview = (context.get_render_data()->scemode & R_MULTIVIEW) != 0;
+
+  add_preview_to_first_linked_input(converter);
 
   if (!context.is_rendering()) {
     /* only output files when rendering a sequence -
@@ -81,7 +91,6 @@ void OutputFileNode::convert_to_operations(NodeConverter &converter,
     map_input_sockets(converter, *output_operation);
   }
   else { /* single layer format */
-    bool preview_added = false;
     for (NodeInput *input : inputs_) {
       if (input->is_linked()) {
         NodeImageMultiFileSocket *sockdata =
@@ -133,11 +142,6 @@ void OutputFileNode::convert_to_operations(NodeConverter &converter,
 
         converter.add_operation(output_operation);
         converter.map_input_socket(input, output_operation->get_input_socket(0));
-
-        if (!preview_added) {
-          converter.add_node_input_preview(input);
-          preview_added = true;
-        }
       }
     }
   }

source/blender/compositor/nodes/COM_OutputFileNode.h
@@ -22,6 +22,7 @@ class OutputFileNode : public Node {
                              const CompositorContext &context) const override;
 
  private:
+  void add_preview_to_first_linked_input(NodeConverter &converter) const;
   void add_input_sockets(OutputOpenExrMultiLayerOperation &operation) const;
   void map_input_sockets(NodeConverter &converter,
                          OutputOpenExrMultiLayerOperation &operation) const;

commit 87c5f29a40821cc4c355078d8788a189edfd9ae6

Fix T99949: Crash when last input from File Output node is deleted
Regression since db2383cffc32.

Need to check inputs exist prior to requesting first input as it
might not exist.

source/blender/compositor/nodes/COM_OutputFileNode.cc
@@ -37,6 +37,10 @@ void OutputFileNode::map_input_sockets(NodeConverter &converter,
 
 void OutputFileNode::add_preview_to_first_linked_input(NodeConverter &converter) const
 {
+  if (get_input_sockets().is_empty()) {
+    return;
+  }
+
   NodeInput *first_socket = this->get_input_socket(0);
   if (first_socket->is_linked()) {
     converter.add_node_input_preview(first_socket);