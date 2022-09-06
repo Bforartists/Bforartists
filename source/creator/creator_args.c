7e65bd08d54e756c13d122b053ab6c6854344127
Iyad Ahmed <iyadahmed430@gmail.com>
2022-09-06 16:22:07 +0200

    Fix #3223: Display Bforartists version instead of Blender version when using -v CLI argument

--- source/creator/creator_args.c
+++ source/creator/creator_args.c
@@ -436,7 +436,7 @@ static void arg_py_context_restore(bContext *C, struct BlendePyContextStore *c_p
 
 static void print_version_full(void)
 {
-  printf("Blender %s\n", BKE_blender_version_string());
+  printf("Bforartists %s\n", BKE_bforartists_version_string());
 #  ifdef BUILD_DATE
   printf("\tbuild date: %s\n", build_date);
   printf("\tbuild time: %s\n", build_time);
