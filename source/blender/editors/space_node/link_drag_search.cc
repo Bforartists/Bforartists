blender/blender

commit 4a71765f9a41d6e13b36a53b121338bc373887ac

Fix T100521: Nodes added with link drag search not added to frame
Use macro NODE_OT_translate_attach for attaching node created through
link-drag-search to frame, as suggested by Leon Schittek (@lone_noel)
in D15888.

Differential Revision: https://developer.blender.org/D15920

source/blender/editors/space_node/link_drag_search.cc
@@ -227,7 +227,7 @@ static void link_drag_search_exec_fn(bContext *C, void *arg1, void *arg2)
   ED_node_tree_propagate_change(C, &bmain, snode.edittree);
 
   /* Start translation operator with the new node. */
-  wmOperatorType *ot = WM_operatortype_find("TRANSFORM_OT_translate", true);
+  wmOperatorType *ot = WM_operatortype_find("NODE_OT_translate_attach", true);
   BLI_assert(ot);
   PointerRNA ptr;
   WM_operator_properties_create_ptr(&ptr, ot);

Bforartists/Bforartists

commit dbe007fedbaa52c5715a3eb3162f6cb8bfeabbe6

Fix: Node edge pan and remove on cancel doesn't work
Caused by 4a71765f9a41 which used an operator that didn't have the
properties it expected.

source/blender/editors/space_node/link_drag_search.cc
@@ -227,12 +227,10 @@ static void link_drag_search_exec_fn(bContext *C, void *arg1, void *arg2)
   ED_node_tree_propagate_change(C, &bmain, snode.edittree);
 
   /* Start translation operator with the new node. */
-  wmOperatorType *ot = WM_operatortype_find("NODE_OT_translate_attach", true);
+  wmOperatorType *ot = WM_operatortype_find("NODE_OT_translate_attach_remove_on_cancel", true);
   BLI_assert(ot);
   PointerRNA ptr;
   WM_operator_properties_create_ptr(&ptr, ot);
-  RNA_boolean_set(&ptr, "view2d_edge_pan", true);
-  RNA_boolean_set(&ptr, "remove_on_cancel", true);
   WM_operator_name_call_ptr(C, ot, WM_OP_INVOKE_DEFAULT, &ptr, nullptr);
   WM_operator_properties_free(&ptr);
 }

source/blender/editors/space_node/node_ops.cc
@@ -144,7 +144,7 @@ void ED_operatormacros_node()
   WM_operatortype_macro_define(ot, "NODE_OT_attach");
   WM_operatortype_macro_define(ot, "NODE_OT_insert_offset");
 
-  /* NODE_OT_translate_attach with remove_on_canel set to true */
+  /* NODE_OT_translate_attach with remove_on_cancel set to true. */
   ot = WM_operatortype_append_macro("NODE_OT_translate_attach_remove_on_cancel",
                                     "Move and Attach",
                                     "Move nodes and attach to frame",