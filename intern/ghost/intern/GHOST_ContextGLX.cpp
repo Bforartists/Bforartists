blender/blender

commit 93e4b15767cf958d5a07ba6acce25438f244bf22

Cleanup: clang-tidy for GHOST X11/SDL/Wayland/NULL backends
Also early exit in some functions.

intern/ghost/intern/GHOST_ContextGLX.cpp
@@ -47,23 +47,24 @@ GHOST_ContextGLX::GHOST_ContextGLX(bool stereoVisual,
       m_contextResetNotificationStrategy(contextResetNotificationStrategy),
       m_context(None)
 {
-  assert(m_display != NULL);
+  assert(m_display != nullptr);
 }
 
 GHOST_ContextGLX::~GHOST_ContextGLX()
 {
-  if (m_display != NULL) {
+  if (m_display != nullptr) {
     if (m_context != None) {
-      if (m_window != 0 && m_context == ::glXGetCurrentContext())
-        ::glXMakeCurrent(m_display, None, NULL);
-
+      if (m_window != 0 && m_context == ::glXGetCurrentContext()) {
+        ::glXMakeCurrent(m_display, None, nullptr);
+      }
       if (m_context != s_sharedContext || s_sharedCount == 1) {
         assert(s_sharedCount > 0);
 
         s_sharedCount--;
 
-        if (s_sharedCount == 0)
-          s_sharedContext = NULL;
+        if (s_sharedCount == 0) {
+          s_sharedContext = nullptr;
+        }
 
         ::glXDestroyContext(m_display, m_context);
       }
@@ -80,22 +81,18 @@ GHOST_TSuccess GHOST_ContextGLX::swapBuffers()
 
 GHOST_TSuccess GHOST_ContextGLX::activateDrawingContext()
 {
-  if (m_display) {
-    return ::glXMakeCurrent(m_display, m_window, m_context) ? GHOST_kSuccess : GHOST_kFailure;
-  }
-  else {
+  if (m_display == nullptr) {
     return GHOST_kFailure;
   }
+  return ::glXMakeCurrent(m_display, m_window, m_context) ? GHOST_kSuccess : GHOST_kFailure;
 }
 
 GHOST_TSuccess GHOST_ContextGLX::releaseDrawingContext()
 {
-  if (m_display) {
-    return ::glXMakeCurrent(m_display, None, NULL) ? GHOST_kSuccess : GHOST_kFailure;
-  }
-  else {
+  if (m_display == nullptr) {
     return GHOST_kFailure;
   }
+  return ::glXMakeCurrent(m_display, None, nullptr) ? GHOST_kSuccess : GHOST_kFailure;
 }
 
 void GHOST_ContextGLX::initContextGLXEW()
@@ -113,15 +110,15 @@ GHOST_TSuccess GHOST_ContextGLX::initializeDrawingContext()
 #ifdef USE_GLXEW_INIT_WORKAROUND
   const GLubyte *extStart = (GLubyte *)"";
   const GLubyte *extEnd;
-  if (glXQueryExtension(m_display, NULL, NULL)) {
+  if (glXQueryExtension(m_display, nullptr, nullptr)) {
     extStart = (const GLubyte *)glXGetClientString(m_display, GLX_EXTENSIONS);
-    if ((extStart == NULL) ||
+    if ((extStart == nullptr) ||
         (glXChooseFBConfig = (PFNGLXCHOOSEFBCONFIGPROC)glXGetProcAddressARB(
-             (const GLubyte *)"glXChooseFBConfig")) == NULL ||
+             (const GLubyte *)"glXChooseFBConfig")) == nullptr ||
         (glXCreateContextAttribsARB = (PFNGLXCREATECONTEXTATTRIBSARBPROC)glXGetProcAddressARB(
-             (const GLubyte *)"glXCreateContextAttribsARB")) == NULL ||
+             (const GLubyte *)"glXCreateContextAttribsARB")) == nullptr ||
         (glXCreatePbuffer = (PFNGLXCREATEPBUFFERPROC)glXGetProcAddressARB(
-             (const GLubyte *)"glXCreatePbuffer")) == NULL) {
+             (const GLubyte *)"glXCreatePbuffer")) == nullptr) {
       extStart = (GLubyte *)"";
     }
   }
@@ -161,11 +158,12 @@ GHOST_TSuccess GHOST_ContextGLX::initializeDrawingContext()
     int profileBitES = m_contextProfileMask & GLX_CONTEXT_ES_PROFILE_BIT_EXT;
 #endif
 
-    if (!GLXEW_ARB_create_context_profile && profileBitCore)
+    if (!GLXEW_ARB_create_context_profile && profileBitCore) {
       fprintf(stderr, "Warning! OpenGL core profile not available.\n");
-
-    if (!GLXEW_ARB_create_context_profile && profileBitCompat)
+    }
+    if (!GLXEW_ARB_create_context_profile && profileBitCompat) {
       fprintf(stderr, "Warning! OpenGL compatibility profile not available.\n");
+    }
 
 #ifdef WITH_GLEW_ES
     if (!GLXEW_EXT_create_context_es_profile && profileBitES && m_contextMajorVersion == 1)
@@ -177,20 +175,21 @@ GHOST_TSuccess GHOST_ContextGLX::initializeDrawingContext()
 
     int profileMask = 0;
 
-    if (GLXEW_ARB_create_context_profile && profileBitCore)
+    if (GLXEW_ARB_create_context_profile && profileBitCore) {
       profileMask |= profileBitCore;
-
-    if (GLXEW_ARB_create_context_profile && profileBitCompat)
+    }
+    if (GLXEW_ARB_create_context_profile && profileBitCompat) {
       profileMask |= profileBitCompat;
+    }
 
 #ifdef WITH_GLEW_ES
     if (GLXEW_EXT_create_context_es_profile && profileBitES)
       profileMask |= profileBitES;
 #endif
 
-    if (profileMask != m_contextProfileMask)
+    if (profileMask != m_contextProfileMask) {
       fprintf(stderr, "Warning! Ignoring untested OpenGL context profile mask bits.");
-
+    }
     /* max 10 attributes plus terminator */
     int attribs[11];
     int i = 0;
@@ -238,7 +237,7 @@ GHOST_TSuccess GHOST_ContextGLX::initializeDrawingContext()
       }
     }
     else {
-      GLXFBConfig *framebuffer_config = NULL;
+      GLXFBConfig *framebuffer_config = nullptr;
       {
         int glx_attribs[64];
         int fbcount = 0;
@@ -269,12 +268,12 @@ GHOST_TSuccess GHOST_ContextGLX::initializeDrawingContext()
 
   GHOST_TSuccess success;
 
-  if (m_context != NULL) {
+  if (m_context != nullptr) {
     const unsigned char *version;
 
-    if (!s_sharedContext)
+    if (!s_sharedContext) {
       s_sharedContext = m_context;
-
+    }
     s_sharedCount++;
 
     glXMakeCurrent(m_display, m_window, m_context);
@@ -319,14 +318,11 @@ GHOST_TSuccess GHOST_ContextGLX::releaseNativeHandles()
 
 GHOST_TSuccess GHOST_ContextGLX::setSwapInterval(int interval)
 {
-  if (GLXEW_EXT_swap_control) {
+  if (!GLXEW_EXT_swap_control) {
     ::glXSwapIntervalEXT(m_display, m_window, interval);
-
     return GHOST_kSuccess;
   }
-  else {
-    return GHOST_kFailure;
-  }
+  return GHOST_kFailure;
 }
 
 GHOST_TSuccess GHOST_ContextGLX::getSwapInterval(int &intervalOut)
@@ -340,9 +336,7 @@ GHOST_TSuccess GHOST_ContextGLX::getSwapInterval(int &intervalOut)
 
     return GHOST_kSuccess;
   }
-  else {
-    return GHOST_kFailure;
-  }
+  return GHOST_kFailure;
 }
 
 /**
@@ -406,30 +400,36 @@ int GHOST_X11_GL_GetAttributes(
 static GLuint _glewStrLen(const GLubyte *s)
 {
   GLuint i = 0;
-  if (s == NULL)
+  if (s == nullptr) {
     return 0;
-  while (s[i] != '\0')
+  }
+  while (s[i] != '\0') {
     i++;
+  }
   return i;
 }
 
 static GLuint _glewStrCLen(const GLubyte *s, GLubyte c)
 {
   GLuint i = 0;
-  if (s == NULL)
+  if (s == nullptr) {
     return 0;
-  while (s[i] != '\0' && s[i] != c)
+  }
+  while (s[i] != '\0' && s[i] != c) {
     i++;
+  }
   return (s[i] == '\0' || s[i] == c) ? i : 0;
 }
 
 static GLboolean _glewStrSame(const GLubyte *a, const GLubyte *b, GLuint n)
 {
   GLuint i = 0;
-  if (a == NULL || b == NULL)
-    return (a == NULL && b == NULL && n == 0) ? GL_TRUE : GL_FALSE;
-  while (i < n && a[i] != '\0' && b[i] != '\0' && a[i] == b[i])
+  if (a == nullptr || b == nullptr) {
+    return (a == nullptr && b == nullptr && n == 0) ? GL_TRUE : GL_FALSE;
+  }
+  while (i < n && a[i] != '\0' && b[i] != '\0' && a[i] == b[i]) {
     i++;
+  }
   return i == n ? GL_TRUE : GL_FALSE;
 }
 
@@ -440,8 +440,9 @@ static GLboolean _glewSearchExtension(const char *name, const GLubyte *start, co
   p = start;
   while (p < end) {
     GLuint n = _glewStrCLen(p, ' ');
-    if (len == n && _glewStrSame((const GLubyte *)name, p, n))
+    if (len == n && _glewStrSame((const GLubyte *)name, p, n)) {
       return GL_TRUE;
+    }
     p += n + 1;
   }
   return GL_FALSE;

intern/ghost/GHOST_C-api.h
@@ -414,7 +414,7 @@ extern GHOST_TSuccess GHOST_SetCursorPosition(GHOST_SystemHandle systemhandle,
  */
 extern GHOST_TSuccess GHOST_SetCursorGrab(GHOST_WindowHandle windowhandle,
                                           GHOST_TGrabCursorMode mode,
-                                          GHOST_TAxisFlag warp_axis,
+                                          GHOST_TAxisFlag wrap_axis,
                                           int bounds[4],
                                           const int mouse_ungrab_xy[2]);
 
@@ -727,7 +727,7 @@ extern unsigned int GHOST_GetContextDefaultOpenGLFramebuffer(GHOST_ContextHandle
 /**
  * Get the OpenGL frame-buffer handle that serves as a default frame-buffer.
  */
-extern unsigned int GHOST_GetDefaultOpenGLFramebuffer(GHOST_WindowHandle windwHandle);
+extern unsigned int GHOST_GetDefaultOpenGLFramebuffer(GHOST_WindowHandle windowhandle);
 
 /**
  * Set which tablet API to use. Only affects Windows, other platforms have a single API.

intern/ghost/intern/GHOST_C-api.cpp
@@ -7,8 +7,8 @@
  * C Api for GHOST
  */
 
-#include <stdlib.h>
-#include <string.h>
+#include <cstdlib>
+#include <cstring>
 
 #include "GHOST_C-api.h"
 #include "GHOST_IEvent.h"
@@ -206,13 +206,15 @@ GHOST_WindowHandle GHOST_BeginFullScreen(GHOST_SystemHandle systemhandle,
                                          const int stereoVisual)
 {
   GHOST_ISystem *system = (GHOST_ISystem *)systemhandle;
-  GHOST_IWindow *window = NULL;
+  GHOST_IWindow *window = nullptr;
   bool bstereoVisual;
 
-  if (stereoVisual)
+  if (stereoVisual) {
     bstereoVisual = true;
-  else
+  }
+  else {
     bstereoVisual = false;
+  }
 
   system->beginFullScreen(*setting, &window, bstereoVisual);
 
@@ -371,7 +373,7 @@ GHOST_TSuccess GHOST_SetCursorGrab(GHOST_WindowHandle windowhandle,
   }
 
   return window->setCursorGrab(
-      mode, wrap_axis, bounds ? &bounds_rect : NULL, mouse_ungrab_xy ? mouse_xy : NULL);
+      mode, wrap_axis, bounds ? &bounds_rect : nullptr, mouse_ungrab_xy ? mouse_xy : nullptr);
 }
 
 GHOST_TSuccess GHOST_GetModifierKeyState(GHOST_SystemHandle systemhandle,
@@ -509,8 +511,8 @@ char *GHOST_GetTitle(GHOST_WindowHandle windowhandle)
 
   char *ctitle = (char *)malloc(title.size() + 1);
 
-  if (ctitle == NULL) {
-    return NULL;
+  if (ctitle == nullptr) {
+    return nullptr;
   }
 
   strcpy(ctitle, title.c_str());
@@ -521,7 +523,7 @@ char *GHOST_GetTitle(GHOST_WindowHandle windowhandle)
 GHOST_RectangleHandle GHOST_GetWindowBounds(GHOST_WindowHandle windowhandle)
 {
   GHOST_IWindow *window = (GHOST_IWindow *)windowhandle;
-  GHOST_Rect *rectangle = NULL;
+  GHOST_Rect *rectangle = nullptr;
 
   rectangle = new GHOST_Rect();
   window->getWindowBounds(*rectangle);
@@ -532,7 +534,7 @@ GHOST_RectangleHandle GHOST_GetWindowBounds(GHOST_WindowHandle windowhandle)
 GHOST_RectangleHandle GHOST_GetClientBounds(GHOST_WindowHandle windowhandle)
 {
   GHOST_IWindow *window = (GHOST_IWindow *)windowhandle;
-  GHOST_Rect *rectangle = NULL;
+  GHOST_Rect *rectangle = nullptr;
 
   rectangle = new GHOST_Rect();
   window->getClientBounds(*rectangle);
@@ -646,10 +648,8 @@ GHOST_TSuccess GHOST_ActivateOpenGLContext(GHOST_ContextHandle contexthandle)
   if (context) {
     return context->activateDrawingContext();
   }
-  else {
-    GHOST_PRINTF("%s: Context not valid\n", __func__);
-    return GHOST_kFailure;
-  }
+  GHOST_PRINTF("%s: Context not valid\n", __func__);
+  return GHOST_kFailure;
 }
 
 GHOST_TSuccess GHOST_ReleaseOpenGLContext(GHOST_ContextHandle contexthandle)
@@ -717,9 +717,9 @@ GHOST_TSuccess GHOST_IsEmptyRectangle(GHOST_RectangleHandle rectanglehandle)
 {
   GHOST_TSuccess result = GHOST_kFailure;
 
-  if (((GHOST_Rect *)rectanglehandle)->isEmpty())
+  if (((GHOST_Rect *)rectanglehandle)->isEmpty()) {
     result = GHOST_kSuccess;
-
+  }
   return result;
 }
 
@@ -727,9 +727,9 @@ GHOST_TSuccess GHOST_IsValidRectangle(GHOST_RectangleHandle rectanglehandle)
 {
   GHOST_TSuccess result = GHOST_kFailure;
 
-  if (((GHOST_Rect *)rectanglehandle)->isValid())
+  if (((GHOST_Rect *)rectanglehandle)->isValid()) {
     result = GHOST_kSuccess;
-
+  }
   return result;
 }
 
@@ -753,9 +753,9 @@ GHOST_TSuccess GHOST_IsInsideRectangle(GHOST_RectangleHandle rectanglehandle, in
 {
   GHOST_TSuccess result = GHOST_kFailure;
 
-  if (((GHOST_Rect *)rectanglehandle)->isInside(x, y))
+  if (((GHOST_Rect *)rectanglehandle)->isInside(x, y)) {
     result = GHOST_kSuccess;
-
+  }
   return result;
 }
 
@@ -785,9 +785,9 @@ GHOST_TSuccess GHOST_ClipRectangle(GHOST_RectangleHandle rectanglehandle,
 {
   GHOST_TSuccess result = GHOST_kFailure;
 
-  if (((GHOST_Rect *)rectanglehandle)->clip(*(GHOST_Rect *)anotherrectanglehandle))
+  if (((GHOST_Rect *)rectanglehandle)->clip(*(GHOST_Rect *)anotherrectanglehandle)) {
     result = GHOST_kSuccess;
-
+  }
   return result;
 }
 
@@ -824,8 +824,9 @@ void GHOST_UseWindowFocus(int use_focus)
 float GHOST_GetNativePixelSize(GHOST_WindowHandle windowhandle)
 {
   GHOST_IWindow *window = (GHOST_IWindow *)windowhandle;
-  if (window)
+  if (window) {
     return window->getNativePixelSize();
+  }
   return 1.0f;
 }
 

intern/ghost/intern/GHOST_ContextEGL.cpp
@@ -40,7 +40,7 @@ static const char *get_egl_error_enum_string(EGLint error)
     CASE_CODE_RETURN_STR(EGL_BAD_NATIVE_WINDOW)
     CASE_CODE_RETURN_STR(EGL_CONTEXT_LOST)
     default:
-      return NULL;
+      return nullptr;
   }
 }
 
@@ -106,11 +106,14 @@ static const char *get_egl_error_message_string(EGLint error)
           "and objects to continue rendering.");
 
     default:
-      return NULL;
+      return nullptr;
   }
 }
 
-static bool egl_chk(bool result, const char *file = NULL, int line = 0, const char *text = NULL)
+static bool egl_chk(bool result,
+                    const char *file = nullptr,
+                    int line = 0,
+                    const char *text = nullptr)
 {
   if (!result) {
     const EGLint error = eglGetError();
@@ -158,7 +161,7 @@ static inline bool bindAPI(EGLenum api)
 }
 
 #ifdef WITH_GL_ANGLE
-HMODULE GHOST_ContextEGL::s_d3dcompiler = NULL;
+HMODULE GHOST_ContextEGL::s_d3dcompiler = nullptr;
 #endif
 
 EGLContext GHOST_ContextEGL::s_gl_sharedContext = EGL_NO_CONTEXT;
@@ -170,7 +173,9 @@ EGLint GHOST_ContextEGL::s_gles_sharedCount = 0;
 EGLContext GHOST_ContextEGL::s_vg_sharedContext = EGL_NO_CONTEXT;
 EGLint GHOST_ContextEGL::s_vg_sharedCount = 0;
 
-#pragma warning(disable : 4715)
+#ifdef _MSC_VER
+#  pragma warning(disable : 4715)
+#endif
 
 template<typename T> T &choose_api(EGLenum api, T &a, T &b, T &c)
 {
@@ -223,23 +228,24 @@ GHOST_ContextEGL::~GHOST_ContextEGL()
     bindAPI(m_api);
 
     if (m_context != EGL_NO_CONTEXT) {
-      if (m_context == ::eglGetCurrentContext())
+      if (m_context == ::eglGetCurrentContext()) {
         EGL_CHK(::eglMakeCurrent(m_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT));
-
+      }
       if (m_context != m_sharedContext || m_sharedCount == 1) {
         assert(m_sharedCount > 0);
 
         m_sharedCount--;
 
-        if (m_sharedCount == 0)
+        if (m_sharedCount == 0) {
           m_sharedContext = EGL_NO_CONTEXT;
-
+        }
         EGL_CHK(::eglDestroyContext(m_display, m_context));
       }
     }
 
-    if (m_surface != EGL_NO_SURFACE)
+    if (m_surface != EGL_NO_SURFACE) {
       EGL_CHK(::eglDestroySurface(m_display, m_surface));
+    }
   }
 }
 
@@ -256,13 +262,9 @@ GHOST_TSuccess GHOST_ContextEGL::setSwapInterval(int interval)
 
       return GHOST_kSuccess;
     }
-    else {
-      return GHOST_kFailure;
-    }
-  }
-  else {
     return GHOST_kFailure;
   }
+  return GHOST_kFailure;
 }
 
 GHOST_TSuccess GHOST_ContextEGL::getSwapInterval(int &intervalOut)
@@ -293,13 +295,10 @@ GHOST_TSuccess GHOST_ContextEGL::activateDrawingContext()
 {
   if (m_display) {
     bindAPI(m_api);
-
     return EGL_CHK(::eglMakeCurrent(m_display, m_surface, m_surface, m_context)) ? GHOST_kSuccess :
                                                                                    GHOST_kFailure;
   }
-  else {
-    return GHOST_kFailure;
-  }
+  return GHOST_kFailure;
 }
 
 GHOST_TSuccess GHOST_ContextEGL::releaseDrawingContext()
@@ -311,9 +310,7 @@ GHOST_TSuccess GHOST_ContextEGL::releaseDrawingContext()
                GHOST_kSuccess :
                GHOST_kFailure;
   }
-  else {
-    return GHOST_kFailure;
-  }
+  return GHOST_kFailure;
 }
 
 bool GHOST_ContextEGL::initContextEGLEW()
@@ -322,7 +319,7 @@ bool GHOST_ContextEGL::initContextEGLEW()
    * it requires a display argument. glewInit() does the same, but we only want
    * to initialize EGLEW here. */
   eglGetDisplay = (PFNEGLGETDISPLAYPROC)eglGetProcAddress("eglGetDisplay");
-  if (eglGetDisplay == NULL) {
+  if (eglGetDisplay == nullptr) {
     return false;
   }
 
@@ -353,9 +350,9 @@ GHOST_TSuccess GHOST_ContextEGL::initializeDrawingContext()
   std::vector<EGLint> attrib_list;
   EGLint num_config = 0;
 
-  if (m_stereoVisual)
+  if (m_stereoVisual) {
     fprintf(stderr, "Warning! Stereo OpenGL ES contexts are not supported.\n");
-
+  }
   m_stereoVisual = false; /* It doesn't matter what the Window wants. */
 
   if (!initContextEGLEW()) {
@@ -364,12 +361,12 @@ GHOST_TSuccess GHOST_ContextEGL::initializeDrawingContext()
 
 #ifdef WITH_GL_ANGLE
   /* `d3dcompiler_XX.dll` needs to be loaded before ANGLE will work. */
-  if (s_d3dcompiler == NULL) {
+  if (s_d3dcompiler == nullptr) {
     s_d3dcompiler = LoadLibrary(D3DCOMPILER);
 
-    WIN32_CHK(s_d3dcompiler != NULL);
+    WIN32_CHK(s_d3dcompiler != nullptr);
 
-    if (s_d3dcompiler == NULL) {
+    if (s_d3dcompiler == nullptr) {
       fprintf(stderr, "LoadLibrary(\"" D3DCOMPILER "\") failed!\n");
       return GHOST_kFailure;
     }
@@ -383,18 +380,19 @@ GHOST_TSuccess GHOST_ContextEGL::initializeDrawingContext()
 
   EGLint egl_major, egl_minor;
 
-  if (!EGL_CHK(::eglInitialize(m_display, &egl_major, &egl_minor)))
+  if (!EGL_CHK(::eglInitialize(m_display, &egl_major, &egl_minor))) {
     goto error;
-
+  }
 #ifdef WITH_GHOST_DEBUG
   fprintf(stderr, "EGL Version %d.%d\n", egl_major, egl_minor);
 #endif
 
-  if (!EGL_CHK(::eglMakeCurrent(m_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT)))
+  if (!EGL_CHK(::eglMakeCurrent(m_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT))) {
     goto error;
-
-  if (!bindAPI(m_api))
+  }
+  if (!bindAPI(m_api)) {
     goto error;
+  }
 
   /* Build attribute list. */
 
@@ -462,15 +460,17 @@ GHOST_TSuccess GHOST_ContextEGL::initializeDrawingContext()
 
   attrib_list.push_back(EGL_NONE);
 
-  if (!EGL_CHK(::eglChooseConfig(m_display, &(attrib_list[0]), &m_config, 1, &num_config)))
+  if (!EGL_CHK(::eglChooseConfig(m_display, &(attrib_list[0]), &m_config, 1, &num_config))) {
     goto error;
+  }
 
   /* A common error is to assume that ChooseConfig worked because it returned EGL_TRUE. */
-  if (num_config != 1) /* `num_config` should be exactly 1. */
+  if (num_config != 1) { /* `num_config` should be exactly 1. */
     goto error;
+  }
 
   if (m_nativeWindow != 0) {
-    m_surface = ::eglCreateWindowSurface(m_display, m_config, m_nativeWindow, NULL);
+    m_surface = ::eglCreateWindowSurface(m_display, m_config, m_nativeWindow, nullptr);
   }
   else {
     static const EGLint pb_attrib_list[] = {
@@ -483,9 +483,9 @@ GHOST_TSuccess GHOST_ContextEGL::initializeDrawingContext()
     m_surface = ::eglCreatePbufferSurface(m_display, m_config, pb_attrib_list);
   }
 
-  if (!EGL_CHK(m_surface != EGL_NO_SURFACE))
+  if (!EGL_CHK(m_surface != EGL_NO_SURFACE)) {
     goto error;
-
+  }
   attrib_list.clear();
 
   if (EGLEW_VERSION_1_5 || EGLEW_KHR_create_context) {
@@ -524,9 +524,10 @@ GHOST_TSuccess GHOST_ContextEGL::initializeDrawingContext()
       }
     }
     else {
-      if (m_contextProfileMask != 0)
+      if (m_contextProfileMask != 0) {
         fprintf(
             stderr, "Warning! Cannot select profile for %s contexts.", api_string(m_api).c_str());
+      }
     }
 
     if (m_api == EGL_OPENGL_API || EGLEW_VERSION_1_5) {
@@ -583,16 +584,19 @@ GHOST_TSuccess GHOST_ContextEGL::initializeDrawingContext()
 
   m_context = ::eglCreateContext(m_display, m_config, m_sharedContext, &(attrib_list[0]));
 
-  if (!EGL_CHK(m_context != EGL_NO_CONTEXT))
+  if (!EGL_CHK(m_context != EGL_NO_CONTEXT)) {
     goto error;
+  }
 
-  if (m_sharedContext == EGL_NO_CONTEXT)
+  if (m_sharedContext == EGL_NO_CONTEXT) {
     m_sharedContext = m_context;
+  }
 
   m_sharedCount++;
 
-  if (!EGL_CHK(::eglMakeCurrent(m_display, m_surface, m_surface, m_context)))
+  if (!EGL_CHK(::eglMakeCurrent(m_display, m_surface, m_surface, m_context))) {
     goto error;
+  }
 
   initContextGLEW();
 
@@ -602,16 +606,16 @@ GHOST_TSuccess GHOST_ContextEGL::initializeDrawingContext()
   return GHOST_kSuccess;
 
 error:
-  if (prev_display != EGL_NO_DISPLAY)
+  if (prev_display != EGL_NO_DISPLAY) {
     EGL_CHK(eglMakeCurrent(prev_display, prev_draw, prev_read, prev_context));
-
+  }
   return GHOST_kFailure;
 }
 
 GHOST_TSuccess GHOST_ContextEGL::releaseNativeHandles()
 {
   m_nativeWindow = 0;
-  m_nativeDisplay = NULL;
+  m_nativeDisplay = nullptr;
 
   return GHOST_kSuccess;
 }

intern/ghost/intern/GHOST_ContextSDL.cpp
@@ -15,7 +15,7 @@
 #include <cstdio>
 #include <cstring>
 
-SDL_GLContext GHOST_ContextSDL::s_sharedContext = NULL;
+SDL_GLContext GHOST_ContextSDL::s_sharedContext = nullptr;
 int GHOST_ContextSDL::s_sharedCount = 0;
 
 GHOST_ContextSDL::GHOST_ContextSDL(bool stereoVisual,
@@ -27,36 +27,39 @@ GHOST_ContextSDL::GHOST_ContextSDL(bool stereoVisual,
                                    int contextResetNotificationStrategy)
     : GHOST_Context(stereoVisual),
       m_window(window),
-      m_hidden_window(NULL),
+      m_hidden_window(nullptr),
       m_contextProfileMask(contextProfileMask),
       m_contextMajorVersion(contextMajorVersion),
       m_contextMinorVersion(contextMinorVersion),
       m_contextFlags(contextFlags),
       m_contextResetNotificationStrategy(contextResetNotificationStrategy),
-      m_context(NULL)
+      m_context(nullptr)
 {
-  // assert(m_window  != NULL);
+  // assert(m_window  != nullptr);
 }
 
 GHOST_ContextSDL::~GHOST_ContextSDL()
 {
-  if (m_context != NULL) {
-    if (m_window != NULL && m_context == SDL_GL_GetCurrentContext())
-      SDL_GL_MakeCurrent(m_window, NULL);
+  if (m_context == nullptr) {
+    return;
+  }
 
-    if (m_context != s_sharedContext || s_sharedCount == 1) {
-      assert(s_sharedCount > 0);
+  if (m_window != nullptr && m_context == SDL_GL_GetCurrentContext()) {
+    SDL_GL_MakeCurrent(m_window, nullptr);
+  }
+  if (m_context != s_sharedContext || s_sharedCount == 1) {
+    assert(s_sharedCount > 0);
 
-      s_sharedCount--;
+    s_sharedCount--;
 
-      if (s_sharedCount == 0)
-        s_sharedContext = NULL;
-
-      SDL_GL_DeleteContext(m_context);
+    if (s_sharedCount == 0) {
+      s_sharedContext = nullptr;
     }
+    SDL_GL_DeleteContext(m_context);
+  }
 
-    if (m_hidden_window != NULL)
-      SDL_DestroyWindow(m_hidden_window);
+  if (m_hidden_window != nullptr) {
+    SDL_DestroyWindow(m_hidden_window);
   }
 }
 
@@ -69,23 +72,19 @@ GHOST_TSuccess GHOST_ContextSDL::swapBuffers()
 
 GHOST_TSuccess GHOST_ContextSDL::activateDrawingContext()
 {
-  if (m_context) {
-    return SDL_GL_MakeCurrent(m_window, m_context) ? GHOST_kSuccess : GHOST_kFailure;
-  }
-  else {
+  if (m_context == nullptr) {
     return GHOST_kFailure;
   }
+  return SDL_GL_MakeCurrent(m_window, m_context) ? GHOST_kSuccess : GHOST_kFailure;
 }
 
 GHOST_TSuccess GHOST_ContextSDL::releaseDrawingContext()
 {
-  if (m_context) {
-    /* Untested, may not work */
-    return SDL_GL_MakeCurrent(NULL, NULL) ? GHOST_kSuccess : GHOST_kFailure;
-  }
-  else {
+  if (m_context == nullptr) {
     return GHOST_kFailure;
   }
+  /* Untested, may not work. */
+  return SDL_GL_MakeCurrent(nullptr, nullptr) ? GHOST_kSuccess : GHOST_kFailure;
 }
 
 GHOST_TSuccess GHOST_ContextSDL::initializeDrawingContext()
@@ -115,7 +114,7 @@ GHOST_TSuccess GHOST_ContextSDL::initializeDrawingContext()
     SDL_GL_SetAttribute(SDL_GL_STEREO, 1);
   }
 
-  if (m_window == NULL) {
+  if (m_window == nullptr) {
     m_hidden_window = SDL_CreateWindow("Offscreen Context Windows",
                                        SDL_WINDOWPOS_UNDEFINED,
                                        SDL_WINDOWPOS_UNDEFINED,
@@ -131,10 +130,10 @@ GHOST_TSuccess GHOST_ContextSDL::initializeDrawingContext()
 
   GHOST_TSuccess success;
 
-  if (m_context != NULL) {
-    if (!s_sharedContext)
+  if (m_context != nullptr) {
+    if (!s_sharedContext) {
       s_sharedContext = m_context;
-
+    }
     s_sharedCount++;
 
     success = (SDL_GL_MakeCurrent(m_window, m_context) < 0) ? GHOST_kFailure : GHOST_kSuccess;
@@ -155,19 +154,17 @@ GHOST_TSuccess GHOST_ContextSDL::initializeDrawingContext()
 
 GHOST_TSuccess GHOST_ContextSDL::releaseNativeHandles()
 {
-  m_window = NULL;
+  m_window = nullptr;
 
   return GHOST_kSuccess;
 }
 
 GHOST_TSuccess GHOST_ContextSDL::setSwapInterval(int interval)
 {
-  if (SDL_GL_SetSwapInterval(interval) != -1) {
-    return GHOST_kSuccess;
-  }
-  else {
+  if (SDL_GL_SetSwapInterval(interval) == -1) {
     return GHOST_kFailure;
   }
+  return GHOST_kSuccess;
 }
 
 GHOST_TSuccess GHOST_ContextSDL::getSwapInterval(int &intervalOut)

intern/ghost/intern/GHOST_DisplayManager.cpp
@@ -12,15 +12,15 @@
 #include "GHOST_DisplayManager.h"
 #include "GHOST_Debug.h"
 
-GHOST_DisplayManager::GHOST_DisplayManager(void) : m_settingsInitialized(false)
+GHOST_DisplayManager::GHOST_DisplayManager() : m_settingsInitialized(false)
 {
 }
 
-GHOST_DisplayManager::~GHOST_DisplayManager(void)
+GHOST_DisplayManager::~GHOST_DisplayManager()
 {
 }
 
-GHOST_TSuccess GHOST_DisplayManager::initialize(void)
+GHOST_TSuccess GHOST_DisplayManager::initialize()
 {
   GHOST_TSuccess success;
   if (!m_settingsInitialized) {
@@ -139,7 +139,7 @@ GHOST_TSuccess GHOST_DisplayManager::findMatch(uint8_t display,
   return success;
 }
 
-GHOST_TSuccess GHOST_DisplayManager::initializeSettings(void)
+GHOST_TSuccess GHOST_DisplayManager::initializeSettings()
 {
   uint8_t numDisplays;
   GHOST_TSuccess success = getNumDisplays(numDisplays);

intern/ghost/intern/GHOST_DisplayManagerSDL.cpp
@@ -109,7 +109,7 @@ GHOST_TSuccess GHOST_DisplayManagerSDL::setCurrentDisplaySetting(
 
       SDL_GetDisplayMode(display, i, &mode);
 
-      if (setting.xPixels > mode.w || setting.yPixels > mode.h) {
+      if ((int)setting.xPixels > mode.w || (int)setting.yPixels > mode.h) {
         continue;
       }
 
@@ -122,9 +122,9 @@ GHOST_TSuccess GHOST_DisplayManagerSDL::setCurrentDisplaySetting(
       }
     }
 
-    if (best_fit == -1)
+    if (best_fit == -1) {
       return GHOST_kFailure;
-
+    }
     SDL_GetDisplayMode(display, best_fit, &mode);
   }
 
@@ -142,12 +142,10 @@ GHOST_TSuccess GHOST_DisplayManagerSDL::setCurrentDisplaySetting(
 
     return GHOST_kSuccess;
   }
-  else {
-    /* this is a problem for the BGE player :S, perhaps SDL2 will resolve at some point.
-     * we really need SDL_SetDisplayModeForDisplay() to become an API func! - campbell */
-    printf("no windows available, can't fullscreen\n");
+  /* This is a problem for the BGE player :S, perhaps SDL2 will resolve at some point.
+   * we really need SDL_SetDisplayModeForDisplay() to become an API func! - campbell. */
+  printf("no windows available, can't fullscreen\n");
 
-    /* do not fail, we will try again later when the window is created - wander */
-    return GHOST_kSuccess;
-  }
+  /* do not fail, we will try again later when the window is created - wander */
+  return GHOST_kSuccess;
 }

intern/ghost/intern/GHOST_DisplayManagerX11.cpp
@@ -39,8 +39,9 @@ GHOST_TSuccess GHOST_DisplayManagerX11::getNumDisplaySettings(uint8_t display,
 
   GHOST_ASSERT(display < 1, "Only single display systems are currently supported.\n");
 
-  if (dpy == NULL)
+  if (dpy == nullptr) {
     return GHOST_kFailure;
+  }
 
   majorVersion = minorVersion = 0;
   if (!XF86VidModeQueryVersion(dpy, &majorVersion, &minorVersion)) {
@@ -77,8 +78,9 @@ GHOST_TSuccess GHOST_DisplayManagerX11::getDisplaySetting(uint8_t display,
 {
   Display *dpy = m_system->getXDisplay();
 
-  if (dpy == NULL)
+  if (dpy == nullptr) {
     return GHOST_kFailure;
+  }
 
   (void)display;
 
@@ -143,8 +145,9 @@ GHOST_TSuccess GHOST_DisplayManagerX11::setCurrentDisplaySetting(
   Display *dpy = m_system->getXDisplay();
   int scrnum, num_vidmodes;
 
-  if (dpy == NULL)
+  if (dpy == nullptr) {
     return GHOST_kFailure;
+  }
 
   scrnum = DefaultScreen(dpy);
 

Bforartists/Bforartists

commit 73b2d18ec70f21a80b0ed6e2de0e59edc36a0485

Fix T101776: wrong logic for GLX setSwapInterval
Regression in 93e4b15767cf958d5a07ba6acce25438f244bf22.

intern/ghost/intern/GHOST_ContextGLX.cpp
@@ -306,7 +306,7 @@ GHOST_TSuccess GHOST_ContextGLX::releaseNativeHandles()
 
 GHOST_TSuccess GHOST_ContextGLX::setSwapInterval(int interval)
 {
-  if (!epoxy_has_glx_extension(m_display, DefaultScreen(m_display), "GLX_EXT_swap_control")) {
+  if (epoxy_has_glx_extension(m_display, DefaultScreen(m_display), "GLX_EXT_swap_control")) {
     ::glXSwapIntervalEXT(m_display, m_window, interval);
     return GHOST_kSuccess;
   }