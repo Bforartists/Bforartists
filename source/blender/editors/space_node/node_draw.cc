blender/blender

commit 90ea1b76434fe175e99d43da8f463f28a108d0ad

Nodes: Use persistent integer to identify to nodes
This patch adds an integer identifier to nodes that doesn't change when
the node name changes. This identifier can be used by different systems
to reference a node. This may be important to store caches and simulation
states per node, because otherwise those would always be invalidated
when a node name changes.

Additionally, this kind of identifier could make some things more efficient,
because with it an integer is enough to identify a node and one does not
have to store the node name.

I observed a 10% improvement in evaluation time in a file with an extreme
number of simple math nodes, due to reduced logging overhead-- from
0.226s to 0.205s.

Differential Revision: https://developer.blender.org/D15775

source/blender/editors/space_node/node_draw.cc
@@ -245,57 +245,22 @@ static bool compare_nodes(const bNode *a, const bNode *b)
 
 void node_sort(bNodeTree &ntree)
 {
-  /* Merge sort is the algorithm of choice here. */
-  int totnodes = BLI_listbase_count(&ntree.nodes);
+  Array<bNode *> sort_nodes = ntree.all_nodes();
+  std::stable_sort(sort_nodes.begin(), sort_nodes.end(), compare_nodes);
 
-  int k = 1;
-  while (k < totnodes) {
-    bNode *first_a = (bNode *)ntree.nodes.first;
-    bNode *first_b = first_a;
+  /* If nothing was changed, exit early. Otherwise the node tree's runtime
+   * node vector needs to be rebuilt, since it cannot be reordered in place. */
+  if (sort_nodes == ntree.all_nodes()) {
+    return;
+  }
 
-    do {
-      /* Set up first_b pointer. */
-      for (int b = 0; b < k && first_b; b++) {
-        first_b = first_b->next;
-      }
-      /* All batches merged? */
-      if (first_b == nullptr) {
-        break;
-      }
+  BKE_ntree_update_tag_node_reordered(&ntree);
 
-      /* Merge batches. */
-      bNode *node_a = first_a;
-      bNode *node_b = first_b;
-      int a = 0;
-      int b = 0;
-      while (a < k && b < k && node_b) {
-        if (compare_nodes(node_a, node_b) == 0) {
-          node_a = node_a->next;
-          a++;
-        }
-        else {
-          bNode *tmp = node_b;
-          node_b = node_b->next;
-          b++;
-          BLI_remlink(&ntree.nodes, tmp);
-          BLI_insertlinkbefore(&ntree.nodes, node_a, tmp);
-          BKE_ntree_update_tag_node_reordered(&ntree);
-        }
-      }
-
-      /* Set up first pointers for next batch. */
-      first_b = node_b;
-      for (; b < k; b++) {
-        /* All nodes sorted? */
-        if (first_b == nullptr) {
-          break;
-        }
-        first_b = first_b->next;
-      }
-      first_a = first_b;
-    } while (first_b);
-
-    k = k << 1;
+  ntree.runtime->nodes_by_id.clear();
+  BLI_listbase_clear(&ntree.nodes);
+  for (const int i : sort_nodes.index_range()) {
+    BLI_addtail(&ntree.nodes, sort_nodes[i]);
+    ntree.runtime->nodes_by_id.add_new(sort_nodes[i]);
   }
 }
 
@@ -1691,7 +1656,7 @@ static void node_add_error_message_button(TreeDrawContext &tree_draw_ctx,
 
   Span<geo_log::NodeWarning> warnings;
   if (tree_draw_ctx.geo_tree_log) {
-    geo_log::GeoNodeLog *node_log = tree_draw_ctx.geo_tree_log->nodes.lookup_ptr(node.name);
+    geo_log::GeoNodeLog *node_log = tree_draw_ctx.geo_tree_log->nodes.lookup_ptr(node.identifier);
     if (node_log != nullptr) {
       warnings = node_log->warnings;
     }
@@ -1751,7 +1716,8 @@ static std::optional<std::chrono::nanoseconds> node_get_execution_time(
         }
       }
       else {
-        if (const geo_log::GeoNodeLog *node_log = tree_log->nodes.lookup_ptr_as(tnode->name)) {
+        if (const geo_log::GeoNodeLog *node_log = tree_log->nodes.lookup_ptr_as(
+                tnode->identifier)) {
           found_node = true;
           run_time += node_log->run_time;
         }
@@ -1762,7 +1728,7 @@ static std::optional<std::chrono::nanoseconds> node_get_execution_time(
     }
     return std::nullopt;
   }
-  if (const geo_log::GeoNodeLog *node_log = tree_log->nodes.lookup_ptr(node.name)) {
+  if (const geo_log::GeoNodeLog *node_log = tree_log->nodes.lookup_ptr(node.identifier)) {
     return node_log->run_time;
   }
   return std::nullopt;
@@ -1903,7 +1869,7 @@ static std::optional<NodeExtraInfoRow> node_get_accessed_attributes_row(
     }
   }
   tree_draw_ctx.geo_tree_log->ensure_used_named_attributes();
-  geo_log::GeoNodeLog *node_log = tree_draw_ctx.geo_tree_log->nodes.lookup_ptr(node.name);
+  geo_log::GeoNodeLog *node_log = tree_draw_ctx.geo_tree_log->nodes.lookup_ptr(node.identifier);
   if (node_log == nullptr) {
     return std::nullopt;
   }
@@ -1944,15 +1910,17 @@ static Vector<NodeExtraInfoRow> node_get_extra_info(TreeDrawContext &tree_draw_c
     }
   }
 
-  if (snode.edittree->type == NTREE_GEOMETRY && tree_draw_ctx.geo_tree_log != nullptr) {
-    tree_draw_ctx.geo_tree_log->ensure_debug_messages();
-    const geo_log::GeoNodeLog *node_log = tree_draw_ctx.geo_tree_log->nodes.lookup_ptr(node.name);
-    if (node_log != nullptr) {
-      for (const StringRef message : node_log->debug_messages) {
-        NodeExtraInfoRow row;
-        row.text = message;
-        row.icon = ICON_INFO;
-        rows.append(std::move(row));
+  if (snode.edittree->type == NTREE_GEOMETRY) {
+    if (geo_log::GeoTreeLog *tree_log = tree_draw_ctx.geo_tree_log) {
+      tree_log->ensure_debug_messages();
+      const geo_log::GeoNodeLog *node_log = tree_log->nodes.lookup_ptr(node.identifier);
+      if (node_log != nullptr) {
+        for (const StringRef message : node_log->debug_messages) {
+          NodeExtraInfoRow row;
+          row.text = message;
+          row.icon = ICON_INFO;
+          rows.append(std::move(row));
+        }
       }
     }
   }

source/blender/blenkernel/BKE_compute_contexts.hh
@@ -8,6 +8,8 @@
 
 #include "BLI_compute_context.hh"
 
+struct bNode;
+
 namespace blender::bke {
 
 class ModifierComputeContext : public ComputeContext {
@@ -32,12 +34,20 @@ class NodeGroupComputeContext : public ComputeContext {
  private:
   static constexpr const char *s_static_type = "NODE_GROUP";
 
-  std::string node_name_;
+  int32_t node_id_;
+
+#ifdef DEBUG
+  std::string debug_node_name_;
+#endif
 
  public:
-  NodeGroupComputeContext(const ComputeContext *parent, std::string node_name);
+  NodeGroupComputeContext(const ComputeContext *parent, int32_t node_id);
+  NodeGroupComputeContext(const ComputeContext *parent, const bNode &node);
 
-  StringRefNull node_name() const;
+  int32_t node_id() const
+  {
+    return node_id_;
+  }
 
  private:
   void print_current_in_line(std::ostream &stream) const override;

source/blender/blenkernel/BKE_node.h
@@ -668,6 +668,7 @@ void nodeUnlinkNode(struct bNodeTree *ntree, struct bNode *node);
  * Find the first available, non-duplicate name for a given node.
  */
 void nodeUniqueName(struct bNodeTree *ntree, struct bNode *node);
+void nodeUniqueID(struct bNodeTree *ntree, struct bNode *node);
 
 /**
  * Delete node, associated animation data and ID user count.
@@ -687,16 +688,17 @@ namespace blender::bke {
 
 /**
  * \note keeps socket list order identical, for copying links.
- * \note `unique_name` should usually be true, unless the \a dst_tree is temporary,
- * or the names can already be assumed valid.
+ * \param use_unique: If true, make sure the node's identifier and name are unique in the new
+ * tree. Must be *true* if the \a dst_tree had nodes that weren't in the source node's tree.
+ * Must be *false* when simply copying a node tree, so that identifiers don't change.
  */
 bNode *node_copy_with_mapping(bNodeTree *dst_tree,
                               const bNode &node_src,
                               int flag,
-                              bool unique_name,
+                              bool use_unique,
                               Map<const bNodeSocket *, bNodeSocket *> &new_socket_map);
 
-bNode *node_copy(bNodeTree *dst_tree, const bNode &src_node, int flag, bool unique_name);
+bNode *node_copy(bNodeTree *dst_tree, const bNode &src_node, int flag, bool use_unique);
 
 }  // namespace blender::bke
 

source/blender/blenkernel/BKE_node_runtime.hh
@@ -8,6 +8,7 @@
 #include "BLI_multi_value_map.hh"
 #include "BLI_utility_mixins.hh"
 #include "BLI_vector.hh"
+#include "BLI_vector_set.hh"
 
 #include "DNA_node_types.h"
 
@@ -23,6 +24,36 @@ struct FieldInferencingInterface;
 class NodeDeclaration;
 struct GeometryNodesLazyFunctionGraphInfo;
 }  // namespace blender::nodes
+
+namespace blender {
+
+struct NodeIDHash {
+  uint64_t operator()(const bNode *node) const
+  {
+    return node->identifier;
+  }
+  uint64_t operator()(const int32_t id) const
+  {
+    return id;
+  }
+};
+
+struct NodeIDEquality {
+  bool operator()(const bNode *a, const bNode *b) const
+  {
+    return a->identifier == b->identifier;
+  }
+  bool operator()(const bNode *a, const int32_t b) const
+  {
+    return a->identifier == b;
+  }
+  bool operator()(const int32_t a, const bNode *b) const
+  {
+    return this->operator()(b, a);
+  }
+};
+
+}  // namespace blender
 
 namespace blender::bke {
 
@@ -45,6 +76,13 @@ class bNodeTreeRuntime : NonCopyable, NonMovable {
    * #eNodeTreeRuntimeFlag.
    */
   uint8_t runtime_flag = 0;
+
+  /**
+   * Storage of nodes based on their identifier. Also used as a contiguous array of nodes to
+   * allow simpler and more cache friendly iteration. Supports lookup by integer or by node.
+   * Unlike other caches, this is maintained eagerly while changing the tree.
+   */
+  VectorSet<bNode *, DefaultProbingStrategy, NodeIDHash, NodeIDEquality> nodes_by_id;
 
   /** Execution data.
    *
@@ -91,7 +129,6 @@ class bNodeTreeRuntime : NonCopyable, NonMovable {
   mutable std::atomic<int> allow_use_dirty_topology_cache = 0;
 
   /** Only valid when #topology_cache_is_dirty is false. */
-  Vector<bNode *> nodes;
   Vector<bNodeLink *> links;
   Vector<bNodeSocket *> sockets;
   Vector<bNodeSocket *> input_sockets;
@@ -292,6 +329,28 @@ inline bool topology_cache_is_available(const bNodeSocket &socket)
 /** \name #bNodeTree Inline Methods
  * \{ */
 
+inline blender::Span<const bNode *> bNodeTree::all_nodes() const
+{
+  return this->runtime->nodes_by_id.as_span();
+}
+
+inline blender::Span<bNode *> bNodeTree::all_nodes()
+{
+  return this->runtime->nodes_by_id;
+}
+
+inline bNode *bNodeTree::node_by_id(const int32_t identifier)
+{
+  bNode *const *node = this->runtime->nodes_by_id.lookup_key_ptr_as(identifier);
+  return node ? *node : nullptr;
+}
+
+inline const bNode *bNodeTree::node_by_id(const int32_t identifier) const
+{
+  const bNode *const *node = this->runtime->nodes_by_id.lookup_key_ptr_as(identifier);
+  return node ? *node : nullptr;
+}
+
 inline blender::Span<bNode *> bNodeTree::nodes_by_type(const blender::StringRefNull type_idname)
 {
   BLI_assert(blender::bke::node_tree_runtime::topology_cache_is_available(*this));
@@ -327,18 +386,6 @@ inline blender::Span<bNode *> bNodeTree::toposort_right_to_left()
 {
   BLI_assert(blender::bke::node_tree_runtime::topology_cache_is_available(*this));
   return this->runtime->toposort_right_to_left;
-}
-
-inline blender::Span<const bNode *> bNodeTree::all_nodes() const
-{
-  BLI_assert(blender::bke::node_tree_runtime::topology_cache_is_available(*this));
-  return this->runtime->nodes;
-}
-
-inline blender::Span<bNode *> bNodeTree::all_nodes()
-{
-  BLI_assert(blender::bke::node_tree_runtime::topology_cache_is_available(*this));
-  return this->runtime->nodes;
 }
 
 inline blender::Span<const bNode *> bNodeTree::group_nodes() const

source/blender/blenkernel/intern/compute_contexts.cc
@@ -1,5 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "DNA_node_types.h"
+
 #include "BKE_compute_contexts.hh"
 
 namespace blender::bke {
@@ -17,22 +19,30 @@ void ModifierComputeContext::print_current_in_line(std::ostream &stream) const
   stream << "Modifier: " << modifier_name_;
 }
 
-NodeGroupComputeContext::NodeGroupComputeContext(const ComputeContext *parent,
-                                                 std::string node_name)
-    : ComputeContext(s_static_type, parent), node_name_(std::move(node_name))
+NodeGroupComputeContext::NodeGroupComputeContext(const ComputeContext *parent, const int node_id)
+    : ComputeContext(s_static_type, parent), node_id_(node_id)
 {
   hash_.mix_in(s_static_type, strlen(s_static_type));
-  hash_.mix_in(node_name_.data(), node_name_.size());
+  hash_.mix_in(&node_id_, sizeof(int32_t));
 }
 
-StringRefNull NodeGroupComputeContext::node_name() const
+NodeGroupComputeContext::NodeGroupComputeContext(const ComputeContext *parent, const bNode &node)
+    : NodeGroupComputeContext(parent, node.identifier)
 {
-  return node_name_;
+#ifdef DEBUG
+  debug_node_name_ = node.name;
+#endif
 }
 
 void NodeGroupComputeContext::print_current_in_line(std::ostream &stream) const
 {
-  stream << "Node: " << node_name_;
+#ifdef DEBUG
+  if (!debug_node_name_.empty()) {
+    stream << "Node: " << debug_node_name_;
+    return;
+  }
+#endif
+  stream << "Node ID: " << node_id_;
 }
 
 }  // namespace blender::bke

source/blender/blenkernel/intern/node.cc
@@ -36,6 +36,7 @@
 #include "BLI_listbase.h"
 #include "BLI_map.hh"
 #include "BLI_path_util.h"
+#include "BLI_rand.hh"
 #include "BLI_set.hh"
 #include "BLI_stack.hh"
 #include "BLI_string.h"
@@ -83,6 +84,8 @@
 #include "DEG_depsgraph_build.h"
 
 #include "BLO_read_write.h"
+
+#include "PIL_time.h"
 
 #define NODE_DEFAULT_MAX_WIDTH 700
 
@@ -146,12 +149,14 @@ static void ntree_copy_data(Main * /*bmain*/, ID *id_dst, const ID *id_src, cons
   Map<const bNode *, bNode *> node_map;
   Map<const bNodeSocket *, bNodeSocket *> socket_map;
 
+  ntree_dst->runtime->nodes_by_id.reserve(ntree_src->all_nodes().size());
   BLI_listbase_clear(&ntree_dst->nodes);
   LISTBASE_FOREACH (const bNode *, src_node, &ntree_src->nodes) {
     /* Don't find a unique name for every node, since they should have valid names already. */
     bNode *new_node = blender::bke::node_copy_with_mapping(
         ntree_dst, *src_node, flag_subdata, false, socket_map);
     node_map.add(src_node, new_node);
+    ntree_dst->runtime->nodes_by_id.add_new(new_node);
   }
 
   /* copy links */
@@ -679,6 +684,15 @@ void ntreeBlendReadData(BlendDataReader *reader, ID *owner_id, bNodeTree *ntree)
     node->runtime = MEM_new<bNodeRuntime>(__func__);
     node->typeinfo = nullptr;
 
+    /* Create the `nodes_by_id` cache eagerly so it can be expected to be valid. Because
+     * we create it here we also have to check for zero identifiers from previous versions. */
+    if (ntree->runtime->nodes_by_id.contains_as(node->identifier)) {
+      nodeUniqueID(ntree, node);
+    }
+    else {
+      ntree->runtime->nodes_by_id.add_new(node);
+    }
+
     BLO_read_list(reader, &node->inputs);
     BLO_read_list(reader, &node->outputs);
 
@@ -764,6 +778,7 @@ void ntreeBlendReadData(BlendDataReader *reader, ID *owner_id, bNodeTree *ntree)
     }
   }
   BLO_read_list(reader, &ntree->links);
+  BLI_assert(ntree->all_nodes().size() == BLI_listbase_count(&ntree->nodes));
 
   /* and we connect the rest */
   LISTBASE_FOREACH (bNode *, node, &ntree->nodes) {
@@ -2176,11 +2191,28 @@ void nodeUniqueName(bNodeTree *ntree, bNode *node)
       &ntree->nodes, node, DATA_("Node"), '.', offsetof(bNode, name), sizeof(node->name));
 }
 
+void nodeUniqueID(bNodeTree *ntree, bNode *node)
+{
+  /* Use a pointer cast to avoid overflow warnings. */
+  const double time = PIL_check_seconds_timer() * 1000000.0;
+  blender::RandomNumberGenerator id_rng{*reinterpret_cast<const uint32_t *>(&time)};
+
+  /* In the unlikely case that the random ID doesn't match, choose a new one until it does. */
+  int32_t new_id = id_rng.get_int32();
+  while (ntree->runtime->nodes_by_id.contains_as(new_id)) {
+    new_id = id_rng.get_int32();
+  }
+
+  node->identifier = new_id;
+  ntree->runtime->nodes_by_id.add_new(node);
+}
+
 bNode *nodeAddNode(const bContext *C, bNodeTree *ntree, const char *idname)
 {
   bNode *node = MEM_cnew<bNode>("new node");
   node->runtime = MEM_new<bNodeRuntime>(__func__);
   BLI_addtail(&ntree->nodes, node);
+  nodeUniqueID(ntree, node);
 
   BLI_strncpy(node->idname, idname, sizeof(node->idname));
   node_set_typeinfo(C, ntree, node, nodeTypeFind(idname));
@@ -2241,7 +2273,7 @@ namespace blender::bke {
 bNode *node_copy_with_mapping(bNodeTree *dst_tree,
                               const bNode &node_src,
                               const int flag,
-                              const bool unique_name,
+                              const bool use_unique,
                               Map<const bNodeSocket *, bNodeSocket *> &socket_map)
 {
   bNode *node_dst = (bNode *)MEM_mallocN(sizeof(bNode), __func__);
@@ -2251,8 +2283,9 @@ bNode *node_copy_with_mapping(bNodeTree *dst_tree,
 
   /* Can be called for nodes outside a node tree (e.g. clipboard). */
   if (dst_tree) {
-    if (unique_name) {
+    if (use_unique) {
       nodeUniqueName(dst_tree, node_dst);
+      nodeUniqueID(dst_tree, node_dst);
     }
     BLI_addtail(&dst_tree->nodes, node_dst);
   }
@@ -2314,13 +2347,10 @@ bNode *node_copy_with_mapping(bNodeTree *dst_tree,
   return node_dst;
 }
 
-bNode *node_copy(bNodeTree *dst_tree,
-                 const bNode &src_node,
-                 const int flag,
-                 const bool unique_name)
+bNode *node_copy(bNodeTree *dst_tree, const bNode &src_node, const int flag, const bool use_unique)
 {
   Map<const bNodeSocket *, bNodeSocket *> socket_map;
-  return node_copy_with_mapping(dst_tree, src_node, flag, unique_name, socket_map);
+  return node_copy_with_mapping(dst_tree, src_node, flag, use_unique, socket_map);
 }
 
 }  // namespace blender::bke
@@ -2910,8 +2940,20 @@ static void node_unlink_attached(bNodeTree *ntree, bNode *parent)
   }
 }
 
-/* Free the node itself. ID user refcounting is up the caller,
- * that does not happen here. */
+static void rebuild_nodes_vector(bNodeTree &node_tree)
+{
+  /* Rebuild nodes #VectorSet which must have the same order as the list. */
+  node_tree.runtime->nodes_by_id.clear();
+  LISTBASE_FOREACH (bNode *, node, &node_tree.nodes) {
+    node_tree.runtime->nodes_by_id.add_new(node);
+  }
+}
+
+/**
+ * Free the node itself.
+ *
+ * \note: ID user refcounting and changing the `nodes_by_id` vector are up to the caller.
+ */
 static void node_free_node(bNodeTree *ntree, bNode *node)
 {
   /* since it is called while free database, node->id is undefined */
@@ -2919,6 +2961,8 @@ static void node_free_node(bNodeTree *ntree, bNode *node)
   /* can be called for nodes outside a node tree (e.g. clipboard) */
   if (ntree) {
     BLI_remlink(&ntree->nodes, node);
+    /* Rebuild nodes #VectorSet which must have the same order as the list. */
+    rebuild_nodes_vector(*ntree);
 
     /* texture node has bad habit of keeping exec data around */
     if (ntree->type == NTREE_TEXTURE && ntree->runtime->execdata) {
@@ -2976,6 +3020,7 @@ void ntreeFreeLocalNode(bNodeTree *ntree, bNode *node)
   node_unlink_attached(ntree, node);
 
   node_free_node(ntree, node);
+  rebuild_nodes_vector(*ntree);
 }
 
 void nodeRemoveNode(Main *bmain, bNodeTree *ntree, bNode *node, bool do_id_user)
@@ -3035,6 +3080,7 @@ void nodeRemoveNode(Main *bmain, bNodeTree *ntree, bNode *node, bool do_id_user)
 
   /* Free node itself. */
   node_free_node(ntree, node);
+  rebuild_nodes_vector(*ntree);
 }
 
 static void node_socket_interface_free(bNodeTree * /*ntree*/,

source/blender/blenkernel/intern/node_runtime.cc
@@ -45,15 +45,16 @@ static void double_checked_lock_with_task_isolation(std::mutex &mutex,
 static void update_node_vector(const bNodeTree &ntree)
 {
   bNodeTreeRuntime &tree_runtime = *ntree.runtime;
-  tree_runtime.nodes.clear();
+  const Span<bNode *> nodes = tree_runtime.nodes_by_id;
   tree_runtime.group_nodes.clear();
   tree_runtime.has_undefined_nodes_or_sockets = false;
-  LISTBASE_FOREACH (bNode *, node, &ntree.nodes) {
-    node->runtime->index_in_tree = tree_runtime.nodes.append_and_get_index(node);
-    node->runtime->owner_tree = const_cast<bNodeTree *>(&ntree);
-    tree_runtime.has_undefined_nodes_or_sockets |= node->typeinfo == &NodeTypeUndefined;
-    if (node->is_group()) {
-      tree_runtime.group_nodes.append(node);
+  for (const int i : nodes.index_range()) {
+    bNode &node = *nodes[i];
+    node.runtime->index_in_tree = i;
+    node.runtime->owner_tree = const_cast<bNodeTree *>(&ntree);
+    tree_runtime.has_undefined_nodes_or_sockets |= node.typeinfo == &NodeTypeUndefined;
+    if (node.is_group()) {
+      tree_runtime.group_nodes.append(&node);
     }
   }
 }
@@ -73,7 +74,7 @@ static void update_socket_vectors_and_owner_node(const bNodeTree &ntree)
   tree_runtime.sockets.clear();
   tree_runtime.input_sockets.clear();
   tree_runtime.output_sockets.clear();
-  for (bNode *node : tree_runtime.nodes) {
+  for (bNode *node : tree_runtime.nodes_by_id) {
     bNodeRuntime &node_runtime = *node->runtime;
     node_runtime.inputs.clear();
     node_runtime.outputs.clear();
@@ -99,7 +100,7 @@ static void update_socket_vectors_and_owner_node(const bNodeTree &ntree)
 static void update_internal_link_inputs(const bNodeTree &ntree)
 {
   bNodeTreeRuntime &tree_runtime = *ntree.runtime;
-  for (bNode *node : tree_runtime.nodes) {
+  for (bNode *node : tree_runtime.nodes_by_id) {
     for (bNodeSocket *socket : node->runtime->outputs) {
       socket->runtime->internal_link_input = nullptr;
     }
@@ -112,7 +113,7 @@ static void update_internal_link_inputs(const bNodeTree &ntree)
 static void update_directly_linked_links_and_sockets(const bNodeTree &ntree)
 {
   bNodeTreeRuntime &tree_runtime = *ntree.runtime;
-  for (bNode *node : tree_runtime.nodes) {
+  for (bNode *node : tree_runtime.nodes_by_id) {
     for (bNodeSocket *socket : node->runtime->inputs) {
       socket->runtime->directly_linked_links.clear();
       socket->runtime->directly_linked_sockets.clear();
@@ -207,9 +208,10 @@ static void find_logical_origins_for_socket_recursive(
 static void update_logical_origins(const bNodeTree &ntree)
 {
   bNodeTreeRuntime &tree_runtime = *ntree.runtime;
-  threading::parallel_for(tree_runtime.nodes.index_range(), 128, [&](const IndexRange range) {
+  Span<bNode *> nodes = tree_runtime.nodes_by_id;
+  threading::parallel_for(nodes.index_range(), 128, [&](const IndexRange range) {
     for (const int i : range) {
-      bNode &node = *tree_runtime.nodes[i];
+      bNode &node = *nodes[i];
       for (bNodeSocket *socket : node.runtime->inputs) {
         Vector<bNodeSocket *, 16> sockets_in_current_chain;
         socket->runtime->logically_linked_sockets.clear();
@@ -229,7 +231,7 @@ static void update_nodes_by_type(const bNodeTree &ntree)
 {
   bNodeTreeRuntime &tree_runtime = *ntree.runtime;
   tree_runtime.nodes_by_type.clear();
-  for (bNode *node : tree_runtime.nodes) {
+  for (bNode *node : tree_runtime.nodes_by_id) {
     tree_runtime.nodes_by_type.add(node->typeinfo, node);
   }
 }
@@ -237,8 +239,9 @@ static void update_nodes_by_type(const bNodeTree &ntree)
 static void update_sockets_by_identifier(const bNodeTree &ntree)
 {
   bNodeTreeRuntime &tree_runtime = *ntree.runtime;
-  threading::parallel_for(tree_runtime.nodes.index_range(), 128, [&](const IndexRange range) {
-    for (bNode *node : tree_runtime.nodes.as_span().slice(range)) {
+  Span<bNode *> nodes = tree_runtime.nodes_by_id;
+  threading::parallel_for(nodes.index_range(), 128, [&](const IndexRange range) {
+    for (bNode *node : nodes.slice(range)) {
       node->runtime->inputs_by_identifier.clear();
       node->runtime->outputs_by_identifier.clear();
       for (bNodeSocket *socket : node->runtime->inputs) {
@@ -337,11 +340,11 @@ static void update_toposort(const bNodeTree &ntree,
 {
   bNodeTreeRuntime &tree_runtime = *ntree.runtime;
   r_sorted_nodes.clear();
-  r_sorted_nodes.reserve(tree_runtime.nodes.size());
+  r_sorted_nodes.reserve(tree_runtime.nodes_by_id.size());
   r_cycle_detected = false;
 
-  Array<ToposortNodeState> node_states(tree_runtime.nodes.size());
-  for (bNode *node : tree_runtime.nodes) {
+  Array<ToposortNodeState> node_states(tree_runtime.nodes_by_id.size());
+  for (bNode *node : tree_runtime.nodes_by_id) {
     if (node_states[node->runtime->index_in_tree].is_done) {
       /* Ignore nodes that are done already. */
       continue;
@@ -355,9 +358,9 @@ static void update_toposort(const bNodeTree &ntree,
     toposort_from_start_node(direction, *node, node_states, r_sorted_nodes, r_cycle_detected);
   }
 
-  if (r_sorted_nodes.size() < tree_runtime.nodes.size()) {
+  if (r_sorted_nodes.size() < tree_runtime.nodes_by_id.size()) {
     r_cycle_detected = true;
-    for (bNode *node : tree_runtime.nodes) {
+    for (bNode *node : tree_runtime.nodes_by_id) {
       if (node_states[node->runtime->index_in_tree].is_done) {
         /* Ignore nodes that are done already. */
         continue;
@@ -367,13 +370,13 @@ static void update_toposort(const bNodeTree &ntree,
     }
   }
 
-  BLI_assert(tree_runtime.nodes.size() == r_sorted_nodes.size());
+  BLI_assert(tree_runtime.nodes_by_id.size() == r_sorted_nodes.size());
 }
 
 static void update_root_frames(const bNodeTree &ntree)
 {
   bNodeTreeRuntime &tree_runtime = *ntree.runtime;
-  Span<bNode *> nodes = tree_runtime.nodes;
+  Span<bNode *> nodes = tree_runtime.nodes_by_id;
 
   tree_runtime.root_frames.clear();
 
@@ -387,7 +390,7 @@ static void update_root_frames(const bNodeTree &ntree)
 static void update_direct_frames_childrens(const bNodeTree &ntree)
 {
   bNodeTreeRuntime &tree_runtime = *ntree.runtime;
-  Span<bNode *> nodes = tree_runtime.nodes;
+  Span<bNode *> nodes = tree_runtime.nodes_by_id;
 
   for (bNode *node : nodes) {
     node->runtime->direct_children_in_frame.clear();
@@ -432,7 +435,7 @@ static void ensure_topology_cache(const bNodeTree &ntree)
         update_internal_link_inputs(ntree);
         update_directly_linked_links_and_sockets(ntree);
         threading::parallel_invoke(
-            tree_runtime.nodes.size() > 32,
+            tree_runtime.nodes_by_id.size() > 32,
             [&]() { update_logical_origins(ntree); },
             [&]() { update_nodes_by_type(ntree); },
             [&]() { update_sockets_by_identifier(ntree); },

source/blender/blenkernel/intern/node_tree_update.cc
@@ -1009,6 +1009,15 @@ class NodeTreeMainUpdater {
       result.interface_changed = true;
     }
 
+#ifdef DEBUG
+    /* Check the uniqueness of node identifiers. */
+    Set<int32_t> node_identifiers;
+    LISTBASE_FOREACH (bNode *, node, &ntree.nodes) {
+      BLI_assert(node->identifier >= 0);
+      node_identifiers.add_new(node->identifier);
+    }
+#endif
+
     return result;
   }
 

Bforartists/Bforartists

commit 03ef600febf9aab911580fe5722cbfc30bdaa681

Fix: Node sorting broken after node identifier commit
90ea1b76434fe175 broke the sorting that happens as nodes are selected.
The compare function for stable sort had different requirements than
the previous implementation.

source/blender/editors/space_node/node_draw.cc
@@ -187,7 +187,8 @@ static void node_socket_add_tooltip_in_node_editor(TreeDrawContext * /*tree_draw
                                                    const bNodeSocket *sock,
                                                    uiLayout *layout);
 
-static bool compare_nodes(const bNode *a, const bNode *b)
+/** Return true when \a a should be behind \a b and false otherwise. */
+static bool compare_node_depth(const bNode *a, const bNode *b)
 {
   /* These tell if either the node or any of the parent nodes is selected.
    * A selected parent means an unselected node is also in foreground! */
@@ -200,7 +201,7 @@ static bool compare_nodes(const bNode *a, const bNode *b)
   for (bNode *parent = a->parent; parent; parent = parent->parent) {
     /* If B is an ancestor, it is always behind A. */
     if (parent == b) {
-      return true;
+      return false;
     }
     /* Any selected ancestor moves the node forward. */
     if (parent->flag & NODE_ACTIVE) {
@@ -213,7 +214,7 @@ static bool compare_nodes(const bNode *a, const bNode *b)
   for (bNode *parent = b->parent; parent; parent = parent->parent) {
     /* If A is an ancestor, it is always behind B. */
     if (parent == a) {
-      return false;
+      return true;
     }
     /* Any selected ancestor moves the node forward. */
     if (parent->flag & NODE_ACTIVE) {
@@ -226,17 +227,23 @@ static bool compare_nodes(const bNode *a, const bNode *b)
 
   /* One of the nodes is in the background and the other not. */
   if ((a->flag & NODE_BACKGROUND) && !(b->flag & NODE_BACKGROUND)) {
-    return false;
-  }
-  if (!(a->flag & NODE_BACKGROUND) && (b->flag & NODE_BACKGROUND)) {
     return true;
+  }
+  if ((b->flag & NODE_BACKGROUND) && !(a->flag & NODE_BACKGROUND)) {
+    return false;
   }
 
   /* One has a higher selection state (active > selected > nothing). */
-  if (!b_active && a_active) {
+  if (a_active && !b_active) {
+    return false;
+  }
+  if (b_active && !a_active) {
     return true;
   }
   if (!b_select && (a_active || a_select)) {
+    return false;
+  }
+  if (!a_select && (b_active || b_select)) {
     return true;
   }
 
@@ -246,7 +253,7 @@ static bool compare_nodes(const bNode *a, const bNode *b)
 void node_sort(bNodeTree &ntree)
 {
   Array<bNode *> sort_nodes = ntree.all_nodes();
-  std::stable_sort(sort_nodes.begin(), sort_nodes.end(), compare_nodes);
+  std::stable_sort(sort_nodes.begin(), sort_nodes.end(), compare_node_depth);
 
   /* If nothing was changed, exit early. Otherwise the node tree's runtime
    * node vector needs to be rebuilt, since it cannot be reordered in place. */