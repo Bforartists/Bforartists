Bforartists/Bforartists

commit 7ec37f38e02ad8ee75a456e7a2981e9318370306

Mesh: Move material indices to a generic attribute
This patch moves material indices from the mesh `MPoly` struct to a
generic integer attribute. The builtin material index was already
exposed in geometry nodes, but this makes it a "proper" attribute
accessible with Python and visible in the "Attributes" panel.

The goals of the refactor are code simplification and memory and
performance improvements, mainly because the attribute doesn't have
to be stored and processed if there are no materials. However, until
4.0, material indices will still be read and written in the old
format, meaning there may be a temporary increase in memory usage.

Further notes:
* Completely removing the `MPoly.mat_nr` after 4.0 may require
  changes to DNA or introducing a new `MPoly` type.
* Geometry nodes regression tests didn't look at material indices,
  so the change reveals a bug in the realize instances node that I fixed.
* Access to material indices from the RNA `MeshPolygon` type is slower
  with this patch. The `material_index` attribute can be used instead.
* Cycles is changed to read from the attribute instead.
* BMesh isn't changed in this patch. Theoretically it could be though,
  to save 2 bytes per face when less than two materials are used.
* Eventually we could use a 16 bit integer attribute type instead.

Ref T95967

Differential Revision: https://developer.blender.org/D15675

source/blender/nodes/geometry/nodes/node_geo_material_selection.cc
@@ -23,19 +23,30 @@ static void node_declare(NodeDeclarationBuilder &b)
 static void select_mesh_by_material(const Mesh &mesh,
                                     const Material *material,
                                     const IndexMask mask,
-                                    const MutableSpan<bool> r_selection)
+                                    MutableSpan<bool> r_selection)
 {
   BLI_assert(mesh.totpoly >= r_selection.size());
-  Vector<int> material_indices;
+  Vector<int> slots;
   for (const int i : IndexRange(mesh.totcol)) {
     if (mesh.mat[i] == material) {
-      material_indices.append(i);
+      slots.append(i);
     }
   }
+  const AttributeAccessor attributes = bke::mesh_attributes(mesh);
+  const VArray<int> material_indices = attributes.lookup_or_default<int>(
+      "material_index", ATTR_DOMAIN_FACE, 0);
+  if (material != nullptr && material_indices.is_single() &&
+      material_indices.get_internal_single() == 0) {
+    r_selection.fill_indices(mask, false);
+    return;
+  }
+
+  const VArraySpan<int> material_indices_span(material_indices);
+
   threading::parallel_for(mask.index_range(), 1024, [&](IndexRange range) {
     for (const int i : range) {
       const int face_index = mask[i];
-      r_selection[i] = material_indices.contains(mesh.mpoly[face_index].mat_nr);
+      r_selection[i] = slots.contains(material_indices_span[face_index]);
     }
   });
 }

intern/cycles/blender/mesh.cpp
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: Apache-2.0
  * Copyright 2011-2022 Blender Foundation */
 
+#include <optional>
+
 #include "blender/session.h"
 #include "blender/sync.h"
 #include "blender/util.h"
@@ -879,6 +881,23 @@ static void attr_create_random_per_island(Scene *scene,
 
 /* Create Mesh */
 
+static std::optional<BL::IntAttribute> find_material_index_attribute(BL::Mesh b_mesh)
+{
+  for (BL::Attribute &b_attribute : b_mesh.attributes) {
+    if (b_attribute.domain() != BL::Attribute::domain_FACE) {
+      continue;
+    }
+    if (b_attribute.data_type() != BL::Attribute::data_type_INT) {
+      continue;
+    }
+    if (b_attribute.name() != "material_index") {
+      continue;
+    }
+    return BL::IntAttribute{b_attribute};
+  }
+  return std::nullopt;
+}
+
 static void create_mesh(Scene *scene,
                         Mesh *mesh,
                         BL::Mesh &b_mesh,
@@ -950,13 +969,22 @@ static void create_mesh(Scene *scene,
     }
   }
 
+  std::optional<BL::IntAttribute> material_indices = find_material_index_attribute(b_mesh);
+  auto get_material_index = [&](const int poly_index) -> int {
+    if (material_indices) {
+      return clamp(material_indices->data[poly_index].value(), 0, used_shaders.size() - 1);
+    }
+    return 0;
+  };
+
   /* create faces */
   if (!subdivision) {
     for (BL::MeshLoopTriangle &t : b_mesh.loop_triangles) {
-      BL::MeshPolygon p = b_mesh.polygons[t.polygon_index()];
+      const int poly_index = t.polygon_index();
+      BL::MeshPolygon p = b_mesh.polygons[poly_index];
       int3 vi = get_int3(t.vertices());
 
-      int shader = clamp(p.material_index(), 0, used_shaders.size() - 1);
+      int shader = get_material_index(poly_index);
       bool smooth = p.use_smooth() || use_loop_normals;
 
       if (use_loop_normals) {
@@ -977,9 +1005,10 @@ static void create_mesh(Scene *scene,
   else {
     vector<int> vi;
 
-    for (BL::MeshPolygon &p : b_mesh.polygons) {
+    for (int poly_index = 0; poly_index < numfaces; poly_index++) {
+      BL::MeshPolygon p = b_mesh.polygons[poly_index];
       int n = p.loop_total();
-      int shader = clamp(p.material_index(), 0, used_shaders.size() - 1);
+      int shader = get_material_index(poly_index);
       bool smooth = p.use_smooth() || use_loop_normals;
 
       vi.resize(n);

release/scripts/startup/bl_ui/properties_data_mesh.py
@@ -580,7 +580,7 @@ class DATA_PT_mesh_attributes(MeshButtonsPanel, Panel):
         colliding_names = []
         for collection in (
                 # Built-in names.
-                {"position": None, "material_index": None, "shade_smooth": None, "normal": None, "crease": None},
+                {"position": None, "shade_smooth": None, "normal": None, "crease": None},
                 mesh.attributes,
                 mesh.uv_layers,
                 ob.vertex_groups,

source/blender/blenkernel/BKE_mesh.h
@@ -6,6 +6,9 @@
  * \ingroup bke
  */
 
+#include "DNA_mesh_types.h"
+
+#include "BKE_customdata.h"
 #include "BKE_mesh_types.h"
 #include "BLI_compiler_attrs.h"
 #include "BLI_utildefines.h"
@@ -1018,6 +1021,30 @@ char *BKE_mesh_debug_info(const struct Mesh *me)
     ATTR_NONNULL(1) ATTR_MALLOC ATTR_WARN_UNUSED_RESULT;
 void BKE_mesh_debug_print(const struct Mesh *me) ATTR_NONNULL(1);
 #endif
+
+/**
+ * \return The material index for each polygon. May be null.
+ * \note In C++ code, prefer using the attribute API (#MutableAttributeAccessor)/
+ */
+BLI_INLINE const int *BKE_mesh_material_indices(const Mesh *mesh)
+{
+  return (const int *)CustomData_get_layer_named(&mesh->pdata, CD_PROP_INT32, "material_index");
+}
+
+/**
+ * \return The material index for each polygon. Create the layer if it doesn't exist.
+ * \note In C++ code, prefer using the attribute API (#MutableAttributeAccessor)/
+ */
+BLI_INLINE int *BKE_mesh_material_indices_for_write(Mesh *mesh)
+{
+  int *indices = (int *)CustomData_duplicate_referenced_layer_named(
+      &mesh->pdata, CD_PROP_INT32, "material_index", mesh->totpoly);
+  if (indices) {
+    return indices;
+  }
+  return (int *)CustomData_add_layer_named(
+      &mesh->pdata, CD_PROP_INT32, CD_SET_DEFAULT, NULL, mesh->totpoly, "material_index");
+}
 
 #ifdef __cplusplus
 }

source/blender/blenkernel/BKE_mesh_legacy_convert.h
@@ -28,6 +28,16 @@ void BKE_mesh_legacy_convert_hide_layers_to_flags(struct Mesh *mesh);
 void BKE_mesh_legacy_convert_flags_to_hide_layers(struct Mesh *mesh);
 
 /**
+ * Move material indices from a generic attribute to #MPoly.
+ */
+void BKE_mesh_legacy_convert_material_indices_to_mpoly(struct Mesh *mesh);
+/**
+ * Move material indices from the #MPoly struct to a generic attributes.
+ * Only add the attribute when the indices are not all zero.
+ */
+void BKE_mesh_legacy_convert_mpoly_to_material_indices(struct Mesh *mesh);
+
+/**
  * Recreate #MFace Tessellation.
  *
  * \note This doesn't use multi-threading like #BKE_mesh_recalc_looptri since

source/blender/blenkernel/intern/customdata.cc
@@ -2375,7 +2375,7 @@ bool CustomData_merge(const CustomData *source,
 
 static bool attribute_stored_in_bmesh_flag(const StringRef name)
 {
-  return ELEM(name, ".hide_vert", ".hide_edge", ".hide_poly");
+  return ELEM(name, ".hide_vert", ".hide_edge", ".hide_poly", "material_index");
 }
 
 static CustomData shallow_copy_remove_non_bmesh_attributes(const CustomData &src)

source/blender/blenkernel/intern/fluid.c
@@ -3247,7 +3247,8 @@ static Mesh *create_liquid_geometry(FluidDomainSettings *fds,
     mp_example = *mpoly;
   }
 
-  const short mp_mat_nr = mp_example.mat_nr;
+  const int *orig_material_indices = BKE_mesh_material_indices(orgmesh);
+  const short mp_mat_nr = orig_material_indices ? orig_material_indices[0] : 0;
   const char mp_flag = mp_example.flag;
 
   int i;
@@ -3358,10 +3359,12 @@ static Mesh *create_liquid_geometry(FluidDomainSettings *fds,
     }
   }
 
+  int *material_indices = BKE_mesh_material_indices_for_write(me);
+
   /* Loop for triangles. */
   for (i = 0; i < num_faces; i++, mpolys++, mloops += 3) {
     /* Initialize from existing face. */
-    mpolys->mat_nr = mp_mat_nr;
+    material_indices[i] = mp_mat_nr;
     mpolys->flag = mp_flag;
 
     mpolys->loopstart = i * 3;

source/blender/blenkernel/intern/geometry_component_mesh.cc
@@ -842,16 +842,6 @@ static void tag_component_positions_changed(void *owner)
   }
 }
 
-static int get_material_index(const MPoly &mpoly)
-{
-  return static_cast<int>(mpoly.mat_nr);
-}
-
-static void set_material_index(MPoly &mpoly, int index)
-{
-  mpoly.mat_nr = static_cast<short>(std::clamp(index, 0, SHRT_MAX));
-}
-
 static bool get_shade_smooth(const MPoly &mpoly)
 {
   return mpoly.flag & ME_SMOOTH;
@@ -1201,18 +1191,17 @@ static ComponentAttributeProviders create_attribute_providers_for_mesh()
                                            make_array_write_attribute<int>,
                                            nullptr);
 
-  static BuiltinCustomDataLayerProvider material_index(
-      "material_index",
-      ATTR_DOMAIN_FACE,
-      CD_PROP_INT32,
-      CD_MPOLY,
-      BuiltinAttributeProvider::NonCreatable,
-      BuiltinAttributeProvider::Writable,
-      BuiltinAttributeProvider::NonDeletable,
-      face_access,
-      make_derived_read_attribute<MPoly, int, get_material_index>,
-      make_derived_write_attribute<MPoly, int, get_material_index, set_material_index>,
-      nullptr);
+  static BuiltinCustomDataLayerProvider material_index("material_index",
+                                                       ATTR_DOMAIN_FACE,
+                                                       CD_PROP_INT32,
+                                                       CD_PROP_INT32,
+                                                       BuiltinAttributeProvider::Creatable,
+                                                       BuiltinAttributeProvider::Writable,
+                                                       BuiltinAttributeProvider::Deletable,
+                                                       face_access,
+                                                       make_array_read_attribute<int>,
+                                                       make_array_write_attribute<int>,
+                                                       nullptr);
 
   static BuiltinCustomDataLayerProvider shade_smooth(
       "shade_smooth",

commit 171e04b8042e0bd0787822290d504108b2f80756

Fix T103663: Set material node can fail for single material
Caused by 7ec37f38e02ad8ee75a4, which filled the wrong value.
I have noticed several problems:
- Using a full array as single result.
- Checking single material index for 0. If we have a list of all slots,
  then we must check this in the list.
- The result was filled false. Simple fix.
- Fixed problem with incorrect recording by mask indices, not polygons.
    - Added domain specifics to names to avoid confusion.

Differential Revision: https://developer.blender.org/D16926

source/blender/nodes/geometry/nodes/node_geo_material_selection.cc
@@ -20,35 +20,40 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_output<decl::Bool>(N_("Selection")).field_source();
 }
 
-static void select_mesh_by_material(const Mesh &mesh,
-                                    const Material *material,
-                                    const IndexMask mask,
-                                    MutableSpan<bool> r_selection)
+static VArray<bool> select_mesh_faces_by_material(const Mesh &mesh,
+                                                  const Material *material,
+                                                  const IndexMask face_mask)
 {
-  BLI_assert(mesh.totpoly >= r_selection.size());
   Vector<int> slots;
-  for (const int i : IndexRange(mesh.totcol)) {
-    if (mesh.mat[i] == material) {
-      slots.append(i);
+  for (const int slot_i : IndexRange(mesh.totcol)) {
+    if (mesh.mat[slot_i] == material) {
+      slots.append(slot_i);
     }
   }
+  if (slots.is_empty()) {
+    return VArray<bool>::ForSingle(false, mesh.totpoly);
+  }
+
   const AttributeAccessor attributes = mesh.attributes();
   const VArray<int> material_indices = attributes.lookup_or_default<int>(
       "material_index", ATTR_DOMAIN_FACE, 0);
-  if (material != nullptr && material_indices.is_single() &&
-      material_indices.get_internal_single() == 0) {
-    r_selection.fill_indices(mask, false);
-    return;
+  if (material_indices.is_single()) {
+    const int slot_i = material_indices.get_internal_single();
+    return VArray<bool>::ForSingle(slots.contains(slot_i), mesh.totpoly);
   }
 
   const VArraySpan<int> material_indices_span(material_indices);
 
-  threading::parallel_for(mask.index_range(), 1024, [&](IndexRange range) {
+  Array<bool> face_selection(face_mask.min_array_size());
+  threading::parallel_for(face_mask.index_range(), 1024, [&](IndexRange range) {
     for (const int i : range) {
-      const int face_index = mask[i];
-      r_selection[i] = slots.contains(material_indices_span[face_index]);
+      const int face_index = face_mask[i];
+      const int slot_i = material_indices_span[face_index];
+      face_selection[face_index] = slots.contains(slot_i);
     }
   });
+
+  return VArray<bool>::ForContainer(std::move(face_selection));
 }
 
 class MaterialSelectionFieldInput final : public bke::GeometryFieldInput {
@@ -72,19 +77,12 @@ class MaterialSelectionFieldInput final : public bke::GeometryFieldInput {
     if (mesh == nullptr) {
       return {};
     }
+
     const eAttrDomain domain = context.domain();
-    if (domain == ATTR_DOMAIN_FACE) {
-      Array<bool> selection(mask.min_array_size());
-      select_mesh_by_material(*mesh, material_, mask, selection);
-      return VArray<bool>::ForContainer(std::move(selection));
-    }
+    const IndexMask domain_mask = (domain == ATTR_DOMAIN_FACE) ? mask : IndexMask(mesh->totpoly);
 
-    Array<bool> selection(mesh->totpoly);
-    select_mesh_by_material(*mesh, material_, IndexMask(mesh->totpoly), selection);
-    return mesh->attributes().adapt_domain<bool>(
-        VArray<bool>::ForContainer(std::move(selection)), ATTR_DOMAIN_FACE, domain);
-
-    return nullptr;
+    VArray<bool> selection = select_mesh_faces_by_material(*mesh, material_, domain_mask);
+    return mesh->attributes().adapt_domain<bool>(std::move(selection), ATTR_DOMAIN_FACE, domain);
   }
 
   uint64_t hash() const override