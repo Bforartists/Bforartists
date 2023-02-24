Bforartists/Bforartists

commit 36ee63ba6064922c5279ea62e6472bf7f77efc1e

Mesh: Store active & default color attributes with strings
Attributes are unifying around a name-based API, and we would like to
be able to move away from CustomData in the future. This patch moves
the identification of active and fallback (render) color attributes
to strings on the mesh from flags on CustomDataLayer. This also
removes some ugliness used to retrieve these attributes and maintain
the active status.

The design is described more here: T98366

The patch keeps forward compatibility working until 4.0 with
the same method as the mesh struct of array refactors (T95965).

The strings are allowed to not correspond to an attribute, to allow
setting the active/default attribute independently of actually filling
its data. When applying a modifier, if the strings don't match an
attribute, they will be removed.

The realize instances / join node and join operator take the names from
the first / active input mesh. While other heuristics may be helpful
(and could be a future improvement), just using the first is simple
and predictable.

Differential Revision: https://developer.blender.org/D15169

source/blender/editors/mesh/mesh_data.cc
@@ -377,79 +377,71 @@ bool ED_mesh_uv_remove_named(Mesh *me, const char *name)
   return false;
 }
 
-int ED_mesh_color_add(Mesh *me,
-                      const char *name,
-                      const bool active_set,
-                      const bool do_init,
-                      ReportList * /*reports*/)
+int ED_mesh_color_add(
+    Mesh *me, const char *name, const bool active_set, const bool do_init, ReportList *reports)
 {
-  /* NOTE: keep in sync with #ED_mesh_uv_add. */
+  /* If no name is supplied, provide a backwards compatible default. */
+  if (!name) {
+    name = "Col";
+  }
 
-  BMEditMesh *em;
-  int layernum;
+  if (const CustomDataLayer *layer = BKE_id_attribute_find(
+          &me->id, me->active_color_attribute, CD_PROP_BYTE_COLOR, ATTR_DOMAIN_CORNER)) {
+    int dummy;
+    const CustomData *data = mesh_customdata_get_type(me, BM_LOOP, &dummy);
+    return CustomData_get_named_layer(data, CD_PROP_BYTE_COLOR, layer->name);
+  }
 
-  if (me->edit_mesh) {
-    em = me->edit_mesh;
+  CustomDataLayer *layer = BKE_id_attribute_new(
+      &me->id, name, CD_PROP_BYTE_COLOR, ATTR_DOMAIN_CORNER, reports);
 
-    layernum = CustomData_number_of_layers(&em->bm->ldata, CD_PROP_BYTE_COLOR);
-
-    /* CD_PROP_BYTE_COLOR */
-    BM_data_layer_add_named(em->bm, &em->bm->ldata, CD_PROP_BYTE_COLOR, name);
-    /* copy data from active vertex color layer */
-    if (layernum && do_init) {
-      const int layernum_dst = CustomData_get_active_layer(&em->bm->ldata, CD_PROP_BYTE_COLOR);
-      BM_data_layer_copy(em->bm, &em->bm->ldata, CD_PROP_BYTE_COLOR, layernum_dst, layernum);
-    }
-    if (active_set || layernum == 0) {
-      CustomData_set_layer_active(&em->bm->ldata, CD_PROP_BYTE_COLOR, layernum);
+  if (do_init) {
+    const char *active_name = me->active_color_attribute;
+    if (const CustomDataLayer *active_layer = BKE_id_attributes_color_find(&me->id, active_name)) {
+      if (const BMEditMesh *em = me->edit_mesh) {
+        BMesh &bm = *em->bm;
+        const int src_i = CustomData_get_named_layer(&bm.ldata, CD_PROP_BYTE_COLOR, active_name);
+        const int dst_i = CustomData_get_named_layer(&bm.ldata, CD_PROP_BYTE_COLOR, layer->name);
+        BM_data_layer_copy(&bm, &bm.ldata, CD_PROP_BYTE_COLOR, src_i, dst_i);
+      }
+      else {
+        memcpy(layer->data, active_layer->data, CustomData_get_elem_size(layer) * me->totloop);
+      }
     }
   }
-  else {
-    layernum = CustomData_number_of_layers(&me->ldata, CD_PROP_BYTE_COLOR);
 
-    if (CustomData_get_active_layer(&me->ldata, CD_PROP_BYTE_COLOR) != -1 && do_init) {
-      CustomData_add_layer_named(&me->ldata,
-                                 CD_PROP_BYTE_COLOR,
-                                 CD_DUPLICATE,
-                                 CustomData_get_layer(&me->ldata, CD_PROP_BYTE_COLOR),
-                                 me->totloop,
-                                 name);
-    }
-    else {
-      CustomData_add_layer_named(
-          &me->ldata, CD_PROP_BYTE_COLOR, CD_SET_DEFAULT, nullptr, me->totloop, name);
-    }
-
-    if (active_set || layernum == 0) {
-      CustomData_set_layer_active(&me->ldata, CD_PROP_BYTE_COLOR, layernum);
-    }
-
-    BKE_mesh_tessface_clear(me);
+  if (active_set) {
+    BKE_id_attributes_active_color_set(&me->id, layer->name);
   }
 
   DEG_id_tag_update(&me->id, 0);
   WM_main_add_notifier(NC_GEOM | ND_DATA, me);
 
-  return layernum;
+  int dummy;
+  const CustomData *data = mesh_customdata_get_type(me, BM_LOOP, &dummy);
+  return CustomData_get_named_layer(data, CD_PROP_BYTE_COLOR, layer->name);
 }
 
 bool ED_mesh_color_ensure(Mesh *me, const char *name)
 {
+  using namespace blender;
   BLI_assert(me->edit_mesh == nullptr);
-  CustomDataLayer *layer = BKE_id_attributes_active_color_get(&me->id);
-
-  if (!layer) {
-    CustomData_add_layer_named(
-        &me->ldata, CD_PROP_BYTE_COLOR, CD_SET_DEFAULT, nullptr, me->totloop, name);
-    layer = me->ldata.layers + CustomData_get_layer_index(&me->ldata, CD_PROP_BYTE_COLOR);
-
-    BKE_id_attributes_active_color_set(&me->id, layer);
-    BKE_mesh_tessface_clear(me);
+  if (me->attributes().contains(me->active_color_attribute)) {
+    return true;
   }
 
+  char unique_name[MAX_CUSTOMDATA_LAYER_NAME];
+  BKE_id_attribute_calc_unique_name(&me->id, name, unique_name);
+  if (!me->attributes_for_write().add(
+          unique_name, ATTR_DOMAIN_CORNER, CD_PROP_BYTE_COLOR, bke::AttributeInitDefaultValue())) {
+    return false;
+  }
+
+  BKE_id_attributes_active_color_set(&me->id, unique_name);
+  BKE_mesh_tessface_clear(me);
   DEG_id_tag_update(&me->id, 0);
 
-  return (layer != nullptr);
+  return true;
 }
 
 /*********************** General poll ************************/
@@ -464,57 +456,44 @@ static bool layers_poll(bContext *C)
 
 /*********************** Sculpt Vertex colors operators ************************/
 
-int ED_mesh_sculpt_color_add(Mesh *me,
-                             const char *name,
-                             const bool do_init,
-                             ReportList * /*reports*/)
+int ED_mesh_sculpt_color_add(Mesh *me, const char *name, const bool do_init, ReportList *reports)
 {
-  /* NOTE: keep in sync with #ED_mesh_uv_add. */
-
-  BMEditMesh *em;
-  int layernum;
-
-  if (me->edit_mesh) {
-    em = me->edit_mesh;
-
-    layernum = CustomData_number_of_layers(&em->bm->vdata, CD_PROP_COLOR);
-
-    /* CD_PROP_COLOR */
-    BM_data_layer_add_named(em->bm, &em->bm->vdata, CD_PROP_COLOR, name);
-    /* copy data from active vertex color layer */
-    if (layernum && do_init) {
-      const int layernum_dst = CustomData_get_active_layer(&em->bm->vdata, CD_PROP_COLOR);
-      BM_data_layer_copy(em->bm, &em->bm->vdata, CD_PROP_COLOR, layernum_dst, layernum);
-    }
-    if (layernum == 0) {
-      CustomData_set_layer_active(&em->bm->vdata, CD_PROP_COLOR, layernum);
-    }
+  /* If no name is supplied, provide a backwards compatible default. */
+  if (!name) {
+    name = "Color";
   }
-  else {
-    layernum = CustomData_number_of_layers(&me->vdata, CD_PROP_COLOR);
 
-    if (CustomData_has_layer(&me->vdata, CD_PROP_COLOR) && do_init) {
-      const MPropCol *color_data = (const MPropCol *)CustomData_get_layer(&me->vdata,
-                                                                          CD_PROP_COLOR);
-      CustomData_add_layer_named(
-          &me->vdata, CD_PROP_COLOR, CD_DUPLICATE, (MPropCol *)color_data, me->totvert, name);
-    }
-    else {
-      CustomData_add_layer_named(
-          &me->vdata, CD_PROP_COLOR, CD_SET_DEFAULT, nullptr, me->totvert, name);
-    }
+  if (const CustomDataLayer *layer = BKE_id_attribute_find(
+          &me->id, me->active_color_attribute, CD_PROP_COLOR, ATTR_DOMAIN_POINT)) {
+    int dummy;
+    const CustomData *data = mesh_customdata_get_type(me, BM_LOOP, &dummy);
+    return CustomData_get_named_layer(data, CD_PROP_BYTE_COLOR, layer->name);
+  }
 
-    if (layernum == 0) {
-      CustomData_set_layer_active(&me->vdata, CD_PROP_COLOR, layernum);
-    }
+  CustomDataLayer *layer = BKE_id_attribute_new(
+      &me->id, name, CD_PROP_COLOR, ATTR_DOMAIN_POINT, reports);
 
-    BKE_mesh_tessface_clear(me);
+  if (do_init) {
+    const char *active_name = me->active_color_attribute;
+    if (const CustomDataLayer *active_layer = BKE_id_attributes_color_find(&me->id, active_name)) {
+      if (const BMEditMesh *em = me->edit_mesh) {
+        BMesh &bm = *em->bm;
+        const int src_i = CustomData_get_named_layer(&bm.vdata, CD_PROP_COLOR, active_name);
+        const int dst_i = CustomData_get_named_layer(&bm.vdata, CD_PROP_COLOR, layer->name);
+        BM_data_layer_copy(&bm, &bm.vdata, CD_PROP_COLOR, src_i, dst_i);
+      }
+      else {
+        memcpy(layer->data, active_layer->data, CustomData_get_elem_size(layer) * me->totloop);
+      }
+    }
   }
 
   DEG_id_tag_update(&me->id, 0);
   WM_main_add_notifier(NC_GEOM | ND_DATA, me);
 
-  return layernum;
+  int dummy;
+  const CustomData *data = mesh_customdata_get_type(me, BM_VERT, &dummy);
+  return CustomData_get_named_layer(data, CD_PROP_COLOR, layer->name);
 }
 
 /*********************** UV texture operators ************************/

source/blender/blenkernel/BKE_attribute.h
@@ -105,16 +105,6 @@ int BKE_id_attribute_to_index(const struct ID *id,
                               eAttrDomainMask domain_mask,
                               eCustomDataMask layer_mask);
 
-struct CustomDataLayer *BKE_id_attribute_subset_active_get(const struct ID *id,
-                                                           int active_flag,
-                                                           eAttrDomainMask domain_mask,
-                                                           eCustomDataMask mask);
-void BKE_id_attribute_subset_active_set(struct ID *id,
-                                        struct CustomDataLayer *layer,
-                                        int active_flag,
-                                        eAttrDomainMask domain_mask,
-                                        eCustomDataMask mask);
-
 /**
  * Sets up a temporary ID with arbitrary CustomData domains. `r_id` will
  * be zero initialized with ID type id_type and any non-nullptr
@@ -130,10 +120,13 @@ void BKE_id_attribute_copy_domains_temp(short id_type,
                                         const struct CustomData *cdata,
                                         struct ID *r_id);
 
+const char *BKE_id_attributes_active_color_name(const struct ID *id);
+const char *BKE_id_attributes_default_color_name(const struct ID *id);
+
 struct CustomDataLayer *BKE_id_attributes_active_color_get(const struct ID *id);
-void BKE_id_attributes_active_color_set(struct ID *id, struct CustomDataLayer *active_layer);
-struct CustomDataLayer *BKE_id_attributes_render_color_get(const struct ID *id);
-void BKE_id_attributes_render_color_set(struct ID *id, struct CustomDataLayer *active_layer);
+void BKE_id_attributes_active_color_set(struct ID *id, const char *name);
+struct CustomDataLayer *BKE_id_attributes_default_color_get(const struct ID *id);
+void BKE_id_attributes_default_color_set(struct ID *id, const char *name);
 struct CustomDataLayer *BKE_id_attributes_color_find(const struct ID *id, const char *name);
 
 bool BKE_id_attribute_calc_unique_name(struct ID *id, const char *name, char *outname);

source/blender/blenkernel/BKE_mesh_legacy_convert.h
@@ -85,6 +85,9 @@ void BKE_mesh_legacy_convert_mpoly_to_material_indices(struct Mesh *mesh);
 /** Convert from runtime loose edge cache to legacy edge flag. */
 void BKE_mesh_legacy_convert_loose_edges_to_flag(struct Mesh *mesh);
 
+void BKE_mesh_legacy_attribute_flags_to_strings(struct Mesh *mesh);
+void BKE_mesh_legacy_attribute_strings_to_flags(struct Mesh *mesh);
+
 #endif
 
 /**

source/blender/blenkernel/intern/attribute.cc
@@ -145,6 +145,7 @@ bool BKE_id_attribute_rename(ID *id,
                              const char *new_name,
                              ReportList *reports)
 {
+  using namespace blender;
   if (BKE_id_attribute_required(id, old_name)) {
     BLI_assert_msg(0, "Required attribute name is not editable");
     return false;
@@ -166,6 +167,14 @@ bool BKE_id_attribute_rename(ID *id,
 
   char result_name[MAX_CUSTOMDATA_LAYER_NAME];
   BKE_id_attribute_calc_unique_name(id, new_name, result_name);
+
+  if (StringRef(old_name) == BKE_id_attributes_active_color_name(id)) {
+    BKE_id_attributes_active_color_set(id, result_name);
+  }
+  if (StringRef(old_name) == BKE_id_attributes_default_color_name(id)) {
+    BKE_id_attributes_default_color_set(id, result_name);
+  }
+
   BLI_strncpy_utf8(layer->name, result_name, sizeof(layer->name));
 
   return true;
@@ -287,6 +296,7 @@ CustomDataLayer *BKE_id_attribute_duplicate(ID *id, const char *name, ReportList
 
 bool BKE_id_attribute_remove(ID *id, const char *name, ReportList *reports)
 {
+  using namespace blender;
   using namespace blender::bke;
   if (!name || name[0] == '\0') {
     BKE_report(reports, RPT_ERROR, "The attribute name must not be empty");
@@ -306,6 +316,12 @@ bool BKE_id_attribute_remove(ID *id, const char *name, ReportList *reports)
       for (const int domain : IndexRange(ATTR_DOMAIN_NUM)) {
         if (CustomData *data = info[domain].customdata) {
           if (BM_data_layer_free_named(em->bm, data, name)) {
+            if (name == StringRef(mesh->active_color_attribute)) {
+              MEM_SAFE_FREE(mesh->active_color_attribute);
+            }
+            else if (name == StringRef(mesh->default_color_attribute)) {
+              MEM_SAFE_FREE(mesh->default_color_attribute);
+            }
             return true;
           }
         }
@@ -327,6 +343,9 @@ CustomDataLayer *BKE_id_attribute_find(const ID *id,
                                        const int type,
                                        const eAttrDomain domain)
 {
+  if (!name) {
+    return nullptr;
+  }
   DomainInfo info[ATTR_DOMAIN_NUM];
   get_domains(id, info);
 
@@ -350,6 +369,9 @@ CustomDataLayer *BKE_id_attribute_search(ID *id,
                                          const eCustomDataMask type_mask,
                                          const eAttrDomainMask domain_mask)
 {
+  if (!name) {
+    return nullptr;
+  }
   DomainInfo info[ATTR_DOMAIN_NUM];
   get_domains(id, info);
 
@@ -633,104 +655,69 @@ int BKE_id_attribute_to_index(const ID *id,
   return -1;
 }
 
-CustomDataLayer *BKE_id_attribute_subset_active_get(const ID *id,
-                                                    int active_flag,
-                                                    eAttrDomainMask domain_mask,
-                                                    eCustomDataMask mask)
+const char *BKE_id_attributes_active_color_name(const ID *id)
 {
-  DomainInfo info[ATTR_DOMAIN_NUM];
-  eAttrDomain domains[ATTR_DOMAIN_NUM];
-
-  get_domains_types(domains);
-  get_domains(id, info);
-
-  CustomDataLayer *candidate = nullptr;
-  for (int i = 0; i < ARRAY_SIZE(domains); i++) {
-    if (!((1 << domains[i]) & domain_mask) || !info[domains[i]].customdata) {
-      continue;
-    }
-
-    CustomData *cdata = info[domains[i]].customdata;
-
-    for (int j = 0; j < cdata->totlayer; j++) {
-      CustomDataLayer *layer = cdata->layers + j;
-
-      if (!(CD_TYPE_AS_MASK(layer->type) & mask) || (layer->flag & CD_FLAG_TEMPORARY)) {
-        continue;
-      }
-
-      if (layer->flag & active_flag) {
-        return layer;
-      }
-
-      candidate = layer;
-    }
+  if (GS(id->name) == ID_ME) {
+    return reinterpret_cast<const Mesh *>(id)->active_color_attribute;
   }
-
-  return candidate;
+  return nullptr;
 }
 
-void BKE_id_attribute_subset_active_set(ID *id,
-                                        CustomDataLayer *layer,
-                                        int active_flag,
-                                        eAttrDomainMask domain_mask,
-                                        eCustomDataMask mask)
+const char *BKE_id_attributes_default_color_name(const ID *id)
 {
-  DomainInfo info[ATTR_DOMAIN_NUM];
-  eAttrDomain domains[ATTR_DOMAIN_NUM];
-
-  get_domains_types(domains);
-  get_domains(id, info);
-
-  for (int i = 0; i < ATTR_DOMAIN_NUM; i++) {
-    eAttrDomainMask domain_mask2 = (eAttrDomainMask)(1 << domains[i]);
-
-    if (!(domain_mask2 & domain_mask) || !info[domains[i]].customdata) {
-      continue;
-    }
-
-    CustomData *cdata = info[domains[i]].customdata;
-
-    for (int j = 0; j < cdata->totlayer; j++) {
-      CustomDataLayer *layer_iter = cdata->layers + j;
-
-      if (!(CD_TYPE_AS_MASK(layer_iter->type) & mask) || (layer_iter->flag & CD_FLAG_TEMPORARY)) {
-        continue;
-      }
-
-      layer_iter->flag &= ~active_flag;
-    }
+  if (GS(id->name) == ID_ME) {
+    return reinterpret_cast<const Mesh *>(id)->default_color_attribute;
   }
-
-  layer->flag |= active_flag;
+  return nullptr;
 }
 
 CustomDataLayer *BKE_id_attributes_active_color_get(const ID *id)
 {
-  return BKE_id_attribute_subset_active_get(
-      id, CD_FLAG_COLOR_ACTIVE, ATTR_DOMAIN_MASK_COLOR, CD_MASK_COLOR_ALL);
+  return BKE_id_attributes_color_find(id, BKE_id_attributes_active_color_name(id));
 }
 
-void BKE_id_attributes_active_color_set(ID *id, CustomDataLayer *active_layer)
+void BKE_id_attributes_active_color_set(ID *id, const char *name)
 {
-  BKE_id_attribute_subset_active_set(
-      id, active_layer, CD_FLAG_COLOR_ACTIVE, ATTR_DOMAIN_MASK_COLOR, CD_MASK_COLOR_ALL);
+  switch (GS(id->name)) {
+    case ID_ME: {
+      Mesh *mesh = reinterpret_cast<Mesh *>(id);
+      MEM_SAFE_FREE(mesh->active_color_attribute);
+      if (name) {
+        mesh->active_color_attribute = BLI_strdup(name);
+      }
+      break;
+    }
+    default:
+      break;
+  }
 }
 
-CustomDataLayer *BKE_id_attributes_render_color_get(const ID *id)
+CustomDataLayer *BKE_id_attributes_default_color_get(const ID *id)
 {
-  return BKE_id_attribute_subset_active_get(
-      id, CD_FLAG_COLOR_RENDER, ATTR_DOMAIN_MASK_COLOR, CD_MASK_COLOR_ALL);
+  return BKE_id_attributes_color_find(id, BKE_id_attributes_default_color_name(id));
 }
 
-void BKE_id_attributes_render_color_set(ID *id, CustomDataLayer *active_layer)
+void BKE_id_attributes_default_color_set(ID *id, const char *name)
 {
-  BKE_id_attribute_subset_active_set(
-      id, active_layer, CD_FLAG_COLOR_RENDER, ATTR_DOMAIN_MASK_COLOR, CD_MASK_COLOR_ALL);
+  switch (GS(id->name)) {
+    case ID_ME: {
+      Mesh *mesh = reinterpret_cast<Mesh *>(id);
+      MEM_SAFE_FREE(mesh->default_color_attribute);
+      if (name) {
+        mesh->default_color_attribute = BLI_strdup(name);
+      }
+      break;
+    }
+    default:
+      break;
+  }
 }
 
 CustomDataLayer *BKE_id_attributes_color_find(const ID *id, const char *name)
 {
+  if (!name) {
+    return nullptr;
+  }
   CustomDataLayer *layer = BKE_id_attribute_find(id, name, CD_PROP_COLOR, ATTR_DOMAIN_POINT);
   if (layer == nullptr) {
     layer = BKE_id_attribute_find(id, name, CD_PROP_COLOR, ATTR_DOMAIN_CORNER);

source/blender/blenkernel/intern/customdata.cc
@@ -2347,8 +2347,7 @@ bool CustomData_merge(const CustomData *source,
       newlayer->active_rnd = lastrender;
       newlayer->active_clone = lastclone;
       newlayer->active_mask = lastmask;
-      newlayer->flag |= flag & (CD_FLAG_EXTERNAL | CD_FLAG_IN_MEMORY | CD_FLAG_COLOR_ACTIVE |
-                                CD_FLAG_COLOR_RENDER);
+      newlayer->flag |= flag & (CD_FLAG_EXTERNAL | CD_FLAG_IN_MEMORY);
       changed = true;
 
       if (layer->anonymous_id != nullptr) {

source/blender/blenkernel/intern/mesh.cc
@@ -144,6 +144,10 @@ static void mesh_copy_data(Main *bmain, ID *id_dst, const ID *id_src, const int 
   mesh_dst->mat = (Material **)MEM_dupallocN(mesh_src->mat);
 
   BKE_defgroup_copy_list(&mesh_dst->vertex_group_names, &mesh_src->vertex_group_names);
+  mesh_dst->active_color_attribute = static_cast<char *>(
+      MEM_dupallocN(mesh_src->active_color_attribute));
+  mesh_dst->default_color_attribute = static_cast<char *>(
+      MEM_dupallocN(mesh_src->default_color_attribute));
 
   const eCDAllocType alloc_type = (flag & LIB_ID_COPY_CD_REFERENCE) ? CD_REFERENCE : CD_DUPLICATE;
   CustomData_copy(&mesh_src->vdata, &mesh_dst->vdata, mask.vmask, alloc_type, mesh_dst->totvert);
@@ -253,6 +257,9 @@ static void mesh_blend_write(BlendWriter *writer, ID *id, const void *id_address
       BKE_mesh_legacy_bevel_weight_from_layers(mesh);
       BKE_mesh_legacy_face_set_from_generic(mesh, poly_layers);
       BKE_mesh_legacy_edge_crease_from_layers(mesh);
+      BKE_mesh_legacy_attribute_strings_to_flags(mesh);
+      mesh->active_color_attribute = nullptr;
+      mesh->default_color_attribute = nullptr;
       BKE_mesh_legacy_convert_loose_edges_to_flag(mesh);
       /* When converting to the old mesh format, don't save redundant attributes. */
       names_to_skip.add_multiple_new({".hide_vert",
@@ -288,6 +295,8 @@ static void mesh_blend_write(BlendWriter *writer, ID *id, const void *id_address
   }
 
   BKE_defbase_blend_write(writer, &mesh->vertex_group_names);
+  BLO_write_string(writer, mesh->active_color_attribute);
+  BLO_write_string(writer, mesh->default_color_attribute);
 
   BLO_write_pointer_array(writer, mesh->totcol, mesh->mat);
   BLO_write_raw(writer, sizeof(MSelect) * mesh->totselect, mesh->mselect);
@@ -337,6 +346,8 @@ static void mesh_blend_read_data(BlendDataReader *reader, ID *id)
      * Don't read them again if they were read as part of #CustomData. */
     BKE_defvert_blend_read(reader, mesh->totvert, mesh->dvert);
   }
+  BLO_read_data_address(reader, &mesh->active_color_attribute);
+  BLO_read_data_address(reader, &mesh->default_color_attribute);
 
   mesh->texflag &= ~ME_AUTOSPACE_EVALUATED;
   mesh->edit_mesh = nullptr;
@@ -893,6 +904,8 @@ static void mesh_clear_geometry(Mesh *mesh)
   mesh->totselect = 0;
 
   BLI_freelistN(&mesh->vertex_group_names);
+  MEM_SAFE_FREE(mesh->active_color_attribute);
+  MEM_SAFE_FREE(mesh->default_color_attribute);
 }
 
 void BKE_mesh_clear_geometry(Mesh *mesh)

commit c95f4aa227b225cb7c341c05c2101f46766b5929

Fix #105168: Entering vertexpaint creates a non-default color attribute
Caused by 36ee63ba60

The new attribute was set active, but not default (camera icon).
Now set it default as well.

Pull Request #105169

source/blender/editors/mesh/mesh_data.cc
@@ -511,6 +511,7 @@ bool ED_mesh_color_ensure(Mesh *me, const char *name)
   }
 
   BKE_id_attributes_active_color_set(&me->id, unique_name);
+  BKE_id_attributes_default_color_set(&me->id, unique_name);
   BKE_mesh_tessface_clear(me);
   DEG_id_tag_update(&me->id, 0);
 