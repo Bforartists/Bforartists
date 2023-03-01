aac00e4faa422974223304a6df6fcea78d4b72d5
Iyad Ahmed <iyadahmed430@gmail.com>
2023-03-01 19:51:06 +0200

    Brush Panels addon: format code #3430

--- scripts/addons/bfa_brush_panel/operators.py
+++ scripts/addons/bfa_brush_panel/operators.py
@@ -11,9 +11,7 @@ class BFA_OT_set_brush(bpy.types.Operator):
     dynamic_description: bpy.props.StringProperty()
 
     def execute(self, context):
-        paint_settings = getattr(
-            context.tool_settings, self.tool_settings_attribute_name
-        )
+        paint_settings = getattr(context.tool_settings, self.tool_settings_attribute_name)
         paint_settings.brush = bpy.data.brushes[self.brush_name]
         return {"FINISHED"}
 
