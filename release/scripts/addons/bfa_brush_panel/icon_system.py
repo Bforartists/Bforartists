8cf14eaa5750fcd02beac70f978be3d2fa761c48
Draise <35357409+Draise14@users.noreply.github.com>
2023-03-01 12:14:41 -0500

    Addon - Brush Panel - Iconography for the Grease Pencil brushes #3426 - Added a method to catch grease pencil icons then assign them per icon instead of mix mode - it tags into the concept of the mix system... I have written notes of how this could improve, but for now it's a working start that has no errors.

--- release/scripts/addons/bfa_brush_panel/icon_system.py
+++ release/scripts/addons/bfa_brush_panel/icon_system.py
@@ -42,8 +42,26 @@ DEFAULT_ICON_FOR_TOOLNAME = {
     "MASK": "OVERLAY",
     "CLONE": "BRUSH_CLONE",
     "SOFTEN": "BRUSH_SOFTEN",
-    "FILL": "FLOODFILL"
+    "FILL": "FLOODFILL",
+    "TINT": "BRUSH_TEXDRAW"
+} 
+
+
+DEFAULT_ICON_FOR_GP_DRAW = {
+    "PENCIL": "GPBRUSH_PENCIL",
+    "PEN": "GPBRUSH_PEN",
+    "INK": "GPBRUSH_INK",
+    "INKNOISE": "GPBRUSH_INKNOISE",
+    "BLOCK": "GPBRUSH_BLOCK",
+    "MARKER": "GPBRUSH_MARKER",
+    "AIRBRUSH": "GPBRUSH_AIRBRUSH",
+    "CHISEL": "GPBRUSH_CHISEL",
+    "FILL": "GPBRUSH_FILL",
+    "SOFT": "GPBRUSH_ERASE_SOFT",
+    "HARD": "GPBRUSH_ERASE_HARD",
+    "STROKE": "GPBRUSH_ERASE_STROKE",
 }
+
 
 __icon_cache = dict()
 
@@ -90,6 +108,7 @@ def get_brush_icon(
     icon_name_from_brush: Callable[[bpy.types.Brush], str],
     tool_name_from_brush: Callable[[bpy.types.Brush], str],
 ):
+    # Custom Icon code, so that it override any default icon
     if brush.use_custom_icon and brush.icon_filepath:
         pcoll = preview_collections["main"]
         filepath = os.path.abspath(bpy.path.abspath(brush.icon_filepath))
@@ -101,10 +120,42 @@ def get_brush_icon(
 
         return BrushIcon("NONE", preview.icon_id)
 
+
     if brush.blend == "MIX":
         icon_name = DEFAULT_ICON_FOR_TOOLNAME.get(tool_name_from_brush(brush), None)
+        # Check if the attribute gpencil_tool exists, if not, carry on. 
+        try:
+            if brush.gpencil_tool == "TINT":
+                icon_name = DEFAULT_ICON_FOR_TOOLNAME.get(tool_name_from_brush(brush), None)
+            else:
+                icon_name = DEFAULT_ICON_FOR_GP_DRAW.get(brush.gpencil_settings.gpencil_paint_icon, None)
+        except AttributeError:
+            pass
     else:
         icon_name = DEFAULT_ICON_FOR_BLEND_MODE.get(brush.blend, None)
+
+##### Vertex Paint, Texture Paint and Weight Paint icons based on blend type
+# Override default by blend type if it's not mix. 
+# This works by checking if mix blend is on, if not, it will go normal. 
+# This is applicable for Weight Paint, Vertex Paint and Texture Paint modes. 
+
+    #if brush.blend == "MIX":
+    #    icon_name = DEFAULT_ICON_FOR_TOOLNAME.get(tool_name_from_brush(brush), None)
+    #else:
+    #    icon_name = DEFAULT_ICON_FOR_BLEND_MODE.get(brush.blend, None)
+
+
+##### Grease Pencil Icons based on icon type
+# Override the greae pencil brushes, base code. 
+
+    #if brush.gpencil_tool == "TINT":
+    #    icon_name = DEFAULT_ICON_FOR_TOOLNAME.get(tool_name_from_brush(brush), None)
+    #else:
+    #    icon_name = DEFAULT_ICON_FOR_GP_DRAW.get(brush.gpencil_settings.gpencil_paint_icon, None)
+
+
+# Take 02 - fusion the systems with an attempt to detect grease pencil brushes and iconize them correctly. 
+# This may be revised for later. 
 
     if icon_name is not None:
         return BrushIcon(icon_name, 0)
