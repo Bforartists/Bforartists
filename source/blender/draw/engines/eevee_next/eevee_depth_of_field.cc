Bforartists/Bforartists

commit f622ec4480b7d6f7f58f354e7ddd76a191a44184

GPU: Refactor texture samplers
This patch refactors the texture samples code by mainly splitting the
eGPUSamplerState enum into multiple smaller enums and packing them
inside a GPUSamplerState struct. This was done because many members of
the enum were mutually exclusive, which was worked around during setting
up the samplers in the various backends, and additionally made the API
confusing, like the GPU_texture_wrap_mode function, which had two
mutually exclusive parameters.

The new structure also improved and clarified the backend sampler cache,
reducing the cache size from 514 samplers to just 130 samplers, which
also slightly improved the initialization time. Further, the
GPU_SAMPLER_MAX signal value was naturally incorporated into the
structure using the GPU_SAMPLER_STATE_TYPE_INTERNAL type.

The only expected functional change is in the realtime compositor, which
now supports per-axis repetition control, utilizing new API functions
for that purpose.

This patch is loosely based on an older patch D14366 by Ethan Hall.

Pull Request: https://projects.blender.org/blender/blender/pulls/105642

source/blender/draw/engines/eevee_next/eevee_depth_of_field.cc
@@ -446,7 +446,7 @@ void DepthOfField::hole_fill_pass_sync()
 
 void DepthOfField::resolve_pass_sync()
 {
-  eGPUSamplerState with_filter = GPU_SAMPLER_FILTER;
+  GPUSamplerState with_filter = {GPU_SAMPLER_FILTERING_LINEAR};
   RenderBuffers &render_buffers = inst_.render_buffers;
   eShaderType sh_type = use_bokeh_lut_ ? DOF_RESOLVE_LUT : DOF_RESOLVE;
 

source/blender/draw/engines/eevee_next/eevee_depth_of_field.hh
@@ -45,8 +45,9 @@ class DepthOfField {
   class Instance &inst_;
 
   /** Samplers */
-  static constexpr eGPUSamplerState gather_bilinear = GPU_SAMPLER_MIPMAP | GPU_SAMPLER_FILTER;
-  static constexpr eGPUSamplerState gather_nearest = GPU_SAMPLER_MIPMAP;
+  static constexpr GPUSamplerState gather_bilinear = {GPU_SAMPLER_FILTERING_MIPMAP |
+                                                      GPU_SAMPLER_FILTERING_LINEAR};
+  static constexpr GPUSamplerState gather_nearest = {GPU_SAMPLER_FILTERING_MIPMAP};
 
   /** Input/Output texture references. */
   GPUTexture *input_color_tx_ = nullptr;

commit 13e5a27e40da7f9f812c57525196e04c605ffd74

Fix MSVC C1001 error after texture sampler refactor
This patch fixes the MSVC C1001 error that was introduced after
f622ec4480b7d6f7f58f354e7ddd76a191a44184.

Pull Request: https://projects.blender.org/blender/blender/pulls/106554

source/blender/draw/engines/eevee_next/eevee_depth_of_field.cc
@@ -361,6 +361,10 @@ void DepthOfField::tiles_dilate_pass_sync()
 
 void DepthOfField::gather_pass_sync()
 {
+  const GPUSamplerState gather_bilinear = {GPU_SAMPLER_FILTERING_MIPMAP |
+                                           GPU_SAMPLER_FILTERING_LINEAR};
+  const GPUSamplerState gather_nearest = {GPU_SAMPLER_FILTERING_MIPMAP};
+
   for (int pass = 0; pass < 2; pass++) {
     PassSimple &drw_pass = (pass == 0) ? gather_fg_ps_ : gather_bg_ps_;
     SwapChain<TextureFromPool, 2> &color_chain = (pass == 0) ? color_fg_tx_ : color_bg_tx_;
@@ -429,6 +433,10 @@ void DepthOfField::scatter_pass_sync()
 
 void DepthOfField::hole_fill_pass_sync()
 {
+  const GPUSamplerState gather_bilinear = {GPU_SAMPLER_FILTERING_MIPMAP |
+                                           GPU_SAMPLER_FILTERING_LINEAR};
+  const GPUSamplerState gather_nearest = {GPU_SAMPLER_FILTERING_MIPMAP};
+
   hole_fill_ps_.init();
   inst_.sampling.bind_resources(&hole_fill_ps_);
   hole_fill_ps_.shader_set(inst_.shaders.static_shader_get(DOF_GATHER_HOLE_FILL));

source/blender/draw/engines/eevee_next/eevee_depth_of_field.hh
@@ -44,11 +44,6 @@ class DepthOfField {
  private:
   class Instance &inst_;
 
-  /** Samplers */
-  static constexpr GPUSamplerState gather_bilinear = {GPU_SAMPLER_FILTERING_MIPMAP |
-                                                      GPU_SAMPLER_FILTERING_LINEAR};
-  static constexpr GPUSamplerState gather_nearest = {GPU_SAMPLER_FILTERING_MIPMAP};
-
   /** Input/Output texture references. */
   GPUTexture *input_color_tx_ = nullptr;
   GPUTexture *output_color_tx_ = nullptr;