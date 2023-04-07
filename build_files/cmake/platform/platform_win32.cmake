Bforartists/Bforartists

commit b246f9390abbc23175063c98946f66ce82857fbf

CMake/Win: use manifest for PLATFORM_BUNDLED_LIBRARIES
`PLATFORM_BUNDLED_LIBRARIES` was installing right next to the blender
executable rather than the `blender.shared` folder,
`PLATFORM_BUNDLED_LIBRARIES` wasn't used very much on windows, just
by the ONEAPI code which likely wasn't aware this plumbing was
still missing.

This diff adds support for using `PLATFORM_BUNDLED_LIBRARIES` on
windows in both debug and release configurations.

You can differentiate between a .dll being installed for debug/release
or all configurations, by prefixing the library with either `DEBUG`,
`RELEASE` or `All`, if no prefix is given `ALL` is assumed.

Pull Request: https://projects.blender.org/blender/blender/pulls/106348

build_files/cmake/platform/platform_win32.cmake
@@ -1040,8 +1040,9 @@ if(WITH_CYCLES AND WITH_CYCLES_DEVICE_ONEAPI)
     ${SYCL_ROOT_DIR}/bin/sycl[0-9].dll
   )
   foreach(sycl_runtime_library IN LISTS _sycl_runtime_libraries_glob)
-    string(REPLACE ".dll" "$<$<CONFIG:Debug>:d>.dll" sycl_runtime_library ${sycl_runtime_library})
-    list(APPEND _sycl_runtime_libraries ${sycl_runtime_library})
+    string(REPLACE ".dll" "_d.dll" sycl_runtime_library_debug ${sycl_runtime_library})
+    list(APPEND _sycl_runtime_libraries RELEASE ${sycl_runtime_library})
+    list(APPEND _sycl_runtime_libraries DEBUG ${sycl_runtime_library_debug})
   endforeach()
   unset(_sycl_runtime_libraries_glob)
 

build_files/cmake/macros.cmake
@@ -1364,3 +1364,24 @@ macro(windows_generate_shared_manifest)
     )
   endif()
 endmacro()
+
+macro(windows_process_platform_bundled_libraries library_deps)
+  if(NOT "${library_deps}" STREQUAL "")
+    set(next_library_mode "ALL")
+    foreach(library ${library_deps})
+      string(TOUPPER "${library}" library_upper)
+      if(("${library_upper}" STREQUAL "RELEASE") OR
+         ("${library_upper}" STREQUAL "DEBUG") OR
+         ("${library_upper}" STREQUAL "ALL"))
+        set(next_library_mode "${library_upper}")
+      else()
+        windows_install_shared_manifest(
+            FILES ${library}
+            ${next_library_mode}
+        )
+        set(next_library_mode "ALL")
+      endif()
+    endforeach()
+  endif()
+endmacro()
+

source/creator/CMakeLists.txt
@@ -1293,10 +1293,7 @@ elseif(WIN32)
   endif()
 
   if(PLATFORM_BUNDLED_LIBRARIES)
-    install(
-      FILES ${PLATFORM_BUNDLED_LIBRARIES}
-      DESTINATION ${TARGETDIR_EXE}
-    )
+    windows_process_platform_bundled_libraries("${PLATFORM_BUNDLED_LIBRARIES}")
   endif()
 elseif(APPLE)
   if(NOT WITH_PYTHON_MODULE)

commit a2481b9f189eb21002ec872f311fb02012045750

CMake/Windows: Fix debug ONEAPI build
the debug dll does not have an _d just a regular d as postfix
issue introduced in b246f9390abbc23175063c98946f66ce82857fbf

build_files/cmake/platform/platform_win32.cmake
@@ -1088,7 +1088,7 @@ if(WITH_CYCLES AND (WITH_CYCLES_DEVICE_ONEAPI OR (WITH_CYCLES_EMBREE AND EMBREE_
     ${SYCL_ROOT_DIR}/bin/sycl[0-9].dll
   )
   foreach(sycl_runtime_library IN LISTS _sycl_runtime_libraries_glob)
-    string(REPLACE ".dll" "_d.dll" sycl_runtime_library_debug ${sycl_runtime_library})
+    string(REPLACE ".dll" "d.dll" sycl_runtime_library_debug ${sycl_runtime_library})
     list(APPEND _sycl_runtime_libraries RELEASE ${sycl_runtime_library})
     list(APPEND _sycl_runtime_libraries DEBUG ${sycl_runtime_library_debug})
   endforeach()