ee294b1d615955cf04c4b5e9797c83f5ba83c58b
ReinerBforartists <reiner.prokein@posteo.de>
2023-05-07 11:51:33 +0200

    3D View - Grease Pencil Object - Weight Paint - Option Autonormalize added, align left and document #3519

--- scripts/startup/bl_ui/space_view3d_toolbar.py
+++ scripts/startup/bl_ui/space_view3d_toolbar.py
@@ -2224,7 +2224,7 @@ class VIEW3D_PT_tools_grease_pencil_weight_options(Panel, View3DPanel, GreasePen
 
     def draw(self, context):
         layout = self.layout
-        layout.use_property_split = True
+        layout.use_property_split = False
         layout.use_property_decorate = False
         tool_settings = context.scene.tool_settings
 
