Bforartists/Bforartists

commit 1af860d1536f44c9dad4999a146b8b677c1b3cb9

Fix: scrubbing time always has extra update that is only used by sequencer
The sequencer has a feature where it only draws a preview while scrubbing.
This was introduced in 8abdc89912e4ae042eeeb5f0e33e833a89eefb8f.
When scrubbing ends, the sequencer requires an extra update so that the
non-preview version is shown again.

This is only needed by the sequencer and hence the update should not be
done if the sequencer is not used.

Changing `screen->scrubbing` is moved out of the sequencer specific
functions, because it's used elsewhere as well.

source/blender/editors/animation/anim_ops.c
@@ -194,42 +194,25 @@ static float frame_from_event(bContext *C, const wmEvent *event)
   return frame;
 }
 
-static void change_frame_seq_preview_begin(bContext *C, const wmEvent *event)
+static void change_frame_seq_preview_begin(bContext *C, const wmEvent *event, SpaceSeq *sseq)
 {
-  ScrArea *area = CTX_wm_area(C);
-  bScreen *screen = CTX_wm_screen(C);
-  if (area && area->spacetype == SPACE_SEQ) {
-    SpaceSeq *sseq = area->spacedata.first;
-    ARegion *region = CTX_wm_region(C);
-    if (ED_space_sequencer_check_show_strip(sseq) && !ED_time_scrub_event_in_region(region, event))
-    {
-      ED_sequencer_special_preview_set(C, event->mval);
-    }
-  }
-  if (screen) {
-    screen->scrubbing = true;
+  BLI_assert(sseq != NULL);
+  ARegion *region = CTX_wm_region(C);
+  if (ED_space_sequencer_check_show_strip(sseq) && !ED_time_scrub_event_in_region(region, event)) {
+    ED_sequencer_special_preview_set(C, event->mval);
   }
 }
 
-static void change_frame_seq_preview_end(bContext *C)
+static void change_frame_seq_preview_end(bContext *C, SpaceSeq *sseq)
 {
-  bScreen *screen = CTX_wm_screen(C);
-  bool notify = false;
-
-  if (screen->scrubbing) {
-    screen->scrubbing = false;
-    notify = true;
-  }
-
+  BLI_assert(sseq != NULL);
+  UNUSED_VARS_NDEBUG(sseq);
   if (ED_sequencer_special_preview_get() != NULL) {
     ED_sequencer_special_preview_clear();
-    notify = true;
   }
 
-  if (notify) {
-    Scene *scene = CTX_data_scene(C);
-    WM_event_add_notifier(C, NC_SCENE | ND_FRAME, scene);
-  }
+  Scene *scene = CTX_data_scene(C);
+  WM_event_add_notifier(C, NC_SCENE | ND_FRAME, scene);
 }
 
 static bool use_sequencer_snapping(bContext *C)
@@ -248,6 +231,7 @@ static bool use_sequencer_snapping(bContext *C)
 static int change_frame_invoke(bContext *C, wmOperator *op, const wmEvent *event)
 {
   ARegion *region = CTX_wm_region(C);
+  bScreen *screen = CTX_wm_screen(C);
   if (CTX_wm_space_seq(C) != NULL && region->regiontype == RGN_TYPE_PREVIEW) {
     return OPERATOR_CANCELLED;
   }
@@ -262,7 +246,11 @@ static int change_frame_invoke(bContext *C, wmOperator *op, const wmEvent *event
     RNA_boolean_set(op->ptr, "snap", true);
   }
 
-  change_frame_seq_preview_begin(C, event);
+  screen->scrubbing = true;
+  SpaceSeq *sseq = CTX_wm_space_seq(C);
+  if (sseq) {
+    change_frame_seq_preview_begin(C, event, sseq);
+  }
 
   change_frame_apply(C, op, true);
 
@@ -274,7 +262,13 @@ static int change_frame_invoke(bContext *C, wmOperator *op, const wmEvent *event
 
 static void change_frame_cancel(bContext *C, wmOperator *UNUSED(op))
 {
-  change_frame_seq_preview_end(C);
+  bScreen *screen = CTX_wm_screen(C);
+  screen->scrubbing = false;
+
+  SpaceSeq *sseq = CTX_wm_space_seq(C);
+  if (sseq != NULL) {
+    change_frame_seq_preview_end(C, sseq);
+  }
 }
 
 /* Modal event handling of frame changing */
@@ -324,7 +318,13 @@ static int change_frame_modal(bContext *C, wmOperator *op, const wmEvent *event)
   }
 
   if (ret != OPERATOR_RUNNING_MODAL) {
-    change_frame_seq_preview_end(C);
+    bScreen *screen = CTX_wm_screen(C);
+    screen->scrubbing = false;
+
+    SpaceSeq *sseq = CTX_wm_space_seq(C);
+    if (sseq != NULL) {
+      change_frame_seq_preview_end(C, sseq);
+    }
   }
 
   return ret;

commit 24c596e5d1c802f31912f16a19840b9593a6fee0

Fix #108122: onion skin disappears after scrubbing timeline
This was broken in 1af860d1536f44c9dad4999a146b8b677c1b3cb9.

source/blender/editors/animation/anim_ops.c
@@ -203,16 +203,13 @@ static void change_frame_seq_preview_begin(bContext *C, const wmEvent *event, Sp
   }
 }
 
-static void change_frame_seq_preview_end(bContext *C, SpaceSeq *sseq)
+static void change_frame_seq_preview_end(SpaceSeq *sseq)
 {
   BLI_assert(sseq != NULL);
   UNUSED_VARS_NDEBUG(sseq);
   if (ED_sequencer_special_preview_get() != NULL) {
     ED_sequencer_special_preview_clear();
   }
-
-  Scene *scene = CTX_data_scene(C);
-  WM_event_add_notifier(C, NC_SCENE | ND_FRAME, scene);
 }
 
 static bool use_sequencer_snapping(bContext *C)
@@ -260,6 +257,36 @@ static int change_frame_invoke(bContext *C, wmOperator *op, const wmEvent *event
   return OPERATOR_RUNNING_MODAL;
 }
 
+static bool need_extra_redraw_after_scrubbing_ends(bContext *C)
+{
+  if (CTX_wm_space_seq(C)) {
+    /* During scrubbing in the sequencer, a preview of the final video might be drawn. After
+     * scrubbing, the actual result should be shown again. */
+    return true;
+  }
+  wmWindowManager *wm = CTX_wm_manager(C);
+  Object *object = CTX_data_active_object(C);
+  if (object && object->type == OB_GPENCIL_LEGACY) {
+    LISTBASE_FOREACH (wmWindow *, win, &wm->windows) {
+      bScreen *screen = WM_window_get_active_screen(win);
+      LISTBASE_FOREACH (ScrArea *, area, &screen->areabase) {
+        SpaceLink *sl = (SpaceLink *)area->spacedata.first;
+        if (sl->spacetype == SPACE_VIEW3D) {
+          View3D *v3d = (View3D *)sl;
+          if ((v3d->flag2 & V3D_HIDE_OVERLAYS) == 0) {
+            if (v3d->gp_flag & V3D_GP_SHOW_ONION_SKIN) {
+              /* Grease pencil onion skin is not drawn during scrubbing. Redraw is necessary after
+               * scrubbing ends to show onion skin again. */
+              return true;
+            }
+          }
+        }
+      }
+    }
+  }
+  return false;
+}
+
 static void change_frame_cancel(bContext *C, wmOperator *UNUSED(op))
 {
   bScreen *screen = CTX_wm_screen(C);
@@ -267,7 +294,12 @@ static void change_frame_cancel(bContext *C, wmOperator *UNUSED(op))
 
   SpaceSeq *sseq = CTX_wm_space_seq(C);
   if (sseq != NULL) {
-    change_frame_seq_preview_end(C, sseq);
+    change_frame_seq_preview_end(sseq);
+  }
+
+  if (need_extra_redraw_after_scrubbing_ends(C)) {
+    Scene *scene = CTX_data_scene(C);
+    WM_event_add_notifier(C, NC_SCENE | ND_FRAME, scene);
   }
 }
 
@@ -323,7 +355,11 @@ static int change_frame_modal(bContext *C, wmOperator *op, const wmEvent *event)
 
     SpaceSeq *sseq = CTX_wm_space_seq(C);
     if (sseq != NULL) {
-      change_frame_seq_preview_end(C, sseq);
+      change_frame_seq_preview_end(sseq);
+    }
+    if (need_extra_redraw_after_scrubbing_ends(C)) {
+      Scene *scene = CTX_data_scene(C);
+      WM_event_add_notifier(C, NC_SCENE | ND_FRAME, scene);
     }
   }
 