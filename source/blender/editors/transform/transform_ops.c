Bforartists/Bforartists

commit 00869a5ba69cfa2bd7c71af2eb095a5b8cfcb5f3

Transform: remove precision modification upon starting navigation
In order to execute the navigation of type "VIEW3D_OT_move," the user
is required to press both the `Shift` and `MMB` keys.

However, pressing `Shift` also activates the precision modifier for the
transform.

Consequently, while the `MMB` is unpressed, the transform operation
unintentionally remains in precision mode, altering the mouse position.

To address this issue, the solution was to eliminate the precision
changes immediately after the navigation is initiated.

Unfortunately, we have no way of knowing whether the user actually
unintentionally enabled precision, so this solution serves as a
workaround. Ideally, keymap conflicts should be avoided.

source/blender/editors/transform/transform_ops.c
@@ -428,9 +428,10 @@ static int transform_modal(bContext *C, wmOperator *op, const wmEvent *event)
     }
 
     if (t->modifiers & MOD_PRECISION) {
-      /* Remove Precision modifier, it may have be unintentionally enabled. */
+      /* WORKAROUND: Remove precision modification, it may have be unintentionally enabled. */
       t->modifiers &= ~MOD_PRECISION;
-      t->mouse.precision = 0;
+      t->mouse.precision = false;
+      transform_input_virtual_mval_reset(t);
     }
 
     /* Make sure `t->mval` is up to date before calling #transformViewUpdate. */

source/blender/editors/transform/transform.h
@@ -768,6 +768,7 @@ void applyMouseInput(struct TransInfo *t,
                      const int mval[2],
                      float output[3]);
 void transform_input_update(TransInfo *t, const float fac);
+void transform_input_virtual_mval_reset(TransInfo *t);
 
 void setCustomPoints(TransInfo *t, MouseInput *mi, const int start[2], const int end[2]);
 void setCustomPointsFromDirection(TransInfo *t, MouseInput *mi, const float dir[2]);

source/blender/editors/transform/transform_input.c
@@ -494,4 +494,18 @@ void transform_input_update(TransInfo *t, const float fac)
   }
 }
 
+void transform_input_virtual_mval_reset(TransInfo *t)
+{
+  MouseInput *mi = &t->mouse;
+  if (ELEM(mi->apply, InputAngle, InputAngleSpring)) {
+    struct InputAngle_Data *data = mi->data;
+    data->angle = 0.0;
+    data->mval_prev[0] = mi->imval[0];
+    data->mval_prev[1] = mi->imval[1];
+  }
+  else {
+    memset(&mi->virtual_mval, 0, sizeof(mi->virtual_mval));
+  }
+}
+
 /** \} */

commit 35e5a6a2bd3ad0b605d0bc3636598dcddeb87942

Fix recent change in transform code
00869a5ba6 caused the precision modification to be removed after
navigation was complete.

The ideal is when starting navigation.

This is because the precision button can be released during navigation,
thus confirming the precision change.

source/blender/editors/transform/transform_ops.c
@@ -420,29 +420,42 @@ static int transform_modal(bContext *C, wmOperator *op, const wmEvent *event)
   t->context = NULL;
 
   /* Allow navigation while transforming. */
-  if (t->vod && (exit_code & OPERATOR_PASS_THROUGH) && ED_view3d_navigation_do(C, t->vod, event)) {
+  if (t->vod && (exit_code & OPERATOR_PASS_THROUGH)) {
     RegionView3D *rv3d = t->region->regiondata;
-    if (rv3d->rflag & RV3D_NAVIGATING) {
-      /* Do not update transform while navigating. This can be distracting. */
-      return OPERATOR_RUNNING_MODAL;
+    const bool is_navigating = (rv3d->rflag & RV3D_NAVIGATING) != 0;
+    if (ED_view3d_navigation_do(C, t->vod, event)) {
+      if (!is_navigating) {
+        /* Navigation has started. */
+
+        if (t->modifiers & MOD_PRECISION) {
+          /* WORKAROUND: Remove precision modification, it may have be unintentionally enabled. */
+          t->modifiers &= ~MOD_PRECISION;
+          t->mouse.precision = false;
+          transform_input_virtual_mval_reset(t);
+        }
+      }
+
+      if (rv3d->rflag & RV3D_NAVIGATING) {
+        /* Navigation is running. */
+
+        /* Do not update transform while navigating. This can be distracting. */
+        return OPERATOR_RUNNING_MODAL;
+      }
+
+      {
+        /* Navigation has ended. */
+
+        /* Make sure `t->mval` is up to date before calling #transformViewUpdate. */
+        copy_v2_v2_int(t->mval, event->mval);
+
+        /* Call before #applyMouseInput. */
+        tranformViewUpdate(t);
+
+        /* Mouse input is outdated. */
+        applyMouseInput(t, &t->mouse, t->mval, t->values);
+        t->redraw |= TREDRAW_HARD;
+      }
     }
-
-    if (t->modifiers & MOD_PRECISION) {
-      /* WORKAROUND: Remove precision modification, it may have be unintentionally enabled. */
-      t->modifiers &= ~MOD_PRECISION;
-      t->mouse.precision = false;
-      transform_input_virtual_mval_reset(t);
-    }
-
-    /* Make sure `t->mval` is up to date before calling #transformViewUpdate. */
-    copy_v2_v2_int(t->mval, event->mval);
-
-    /* Call before #applyMouseInput. */
-    tranformViewUpdate(t);
-
-    /* Mouse input is outdated. */
-    applyMouseInput(t, &t->mouse, t->mval, t->values);
-    t->redraw |= TREDRAW_HARD;
   }
 
   transformApply(C, t);