8ad25ed1f8a13a456321c380b2b8d5678d93802f
Iyad Ahmed <iyadahmed430@gmail.com>
2023-06-03 22:43:29 +0300

    Close #3562: Asset Browser - Add toggle Instance to header to avoid linking/appending issues with collections

--- scripts/startup/bl_ui/space_filebrowser.py
+++ scripts/startup/bl_ui/space_filebrowser.py
@@ -24,6 +24,7 @@ class FILEBROWSER_HT_header(Header):
 
         if params.asset_library_ref not in {'LOCAL', 'ESSENTIALS'}:
             row = layout.row(align=True) #BFA - change to make row of buttons
+            row.prop(params, "drop_collections_as_instances")
             row.prop(params, "import_type", text="", expand=True, icon_only=True,) #BFA - change to make row of buttons
 
         #layout.separator_spacer() #BFA
