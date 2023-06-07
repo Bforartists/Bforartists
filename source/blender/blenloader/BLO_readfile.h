Bforartists/Bforartists

commit 5ac25f683681b7acf0411c208dfecca5ce18f5f9

Readfile: Refactor several parts of the process
This commit affects:
* Reading undo steps from memfile (aka 'Global Undo');
* Handling of UI IDs (WindowManager, Workspaces and Screens) when
  opening a .blend file.

While no major changes are expected from a user PoV, there may be some
unexpected changes in rare edge-cases. None has been identified so far.

Undo step loading should be marginally faster (`setup_app_data` itself
is 2-3 times faster, as it does not do remapping anymore, which makes the
whole 'read undo step' process about 20% faster - but the most
time-consuming step on undo is the depsgraph processing, which remains
unchanged here).

This commit also solves some bugs (crashes) in some relatively uncommon
cases, like e.g. if the WM had an IDProperty pointing at an object and
UI is not loaded when opening a new .blend file with the 'Load UI' option
enabled (as in previous code on file opening WM ID would never be
remapped).

From a more technical side, this commit aims mainly at cleaning things
up, in preparation for the introduction of new 'no undo, no readfile'
type of handling (as part of the Brush Assets project):
  - Prevent WM code from doing (too much) horrible ID 'management' on
    its WM when opening a new file. It used to remove current WM from
    the Main database, store it in a temporary own list, and then free
    it itself...
  - Trying to make the complex logic behind WM handling on file reading a
    bit more easy to follow, at least way more documented in code.
  - Keep the handling of 'IDs being re-used from old Main' in a single
    place, as much as possible:
    -- Readfile code itself in undo case (because it's more efficient,
       and undo case is in a way simpler than actual .blend file
       reading case). The whole `blo_lib_link_restore` block of code
       is also removed.
    -- (Mostly) setup_app_data code in actual file reading case.
  - Sanitize the usage of the 'libmap' in readfile code in undo case

source/blender/blenloader/BLO_readfile.h
@@ -66,6 +66,12 @@ typedef struct BlendFileData {
   eBlenFileType type;
 } BlendFileData;
 
+/** Data used by WM readfile code and BKE's setup_app_data to handle the complex preservation logic
+ * of WindowManager and other UI data-blocks across blendfile reading prcess. */
+typedef struct BlendFileReadWMSetupData {
+  struct wmWindowManager *old_wm; /** The existing WM when filereading process is started. */
+} BlendFileReadWMSetupData;
+
 struct BlendFileReadParams {
   uint skip_flags : 3; /* #eBLOReadSkip */
   uint is_startup : 1;
@@ -445,17 +451,6 @@ void BLO_library_temp_free(TempLibraryContext *temp_lib_ctx);
 /** \} */
 
 void *BLO_library_read_struct(struct FileData *fd, struct BHead *bh, const char *blockname);
-
-/* internal function but we need to expose it */
-/**
- * Used to link a file (without UI) to the current UI.
- * Note that it assumes the old pointers in UI are still valid, so old Main is not freed.
- */
-void blo_lib_link_restore(struct Main *oldmain,
-                          struct Main *newmain,
-                          struct wmWindowManager *curwm,
-                          struct Scene *curscene,
-                          struct ViewLayer *cur_view_layer);
 
 typedef void (*BLOExpandDoitCallback)(void *fdhandle, struct Main *mainvar, void *idv);
 

source/blender/blenkernel/BKE_blendfile.h
@@ -14,6 +14,7 @@ extern "C" {
 struct BlendFileData;
 struct BlendFileReadParams;
 struct BlendFileReadReport;
+struct BlendFileReadWMSetupData;
 struct ID;
 struct Main;
 struct MemFile;
@@ -50,33 +51,38 @@ bool BKE_blendfile_library_path_explode(const char *path,
 /**
  * Shared setup function that makes the data from `bfd` into the current blend file,
  * replacing the contents of #G.main.
- * This uses the bfd #BKE_blendfile_read and similarly named functions.
+ * This uses the bfd returned by #BKE_blendfile_read and similarly named functions.
  *
  * This is done in a separate step so the caller may perform actions after it is known the file
  * loaded correctly but before the file replaces the existing blend file contents.
  */
-void BKE_blendfile_read_setup_ex(struct bContext *C,
-                                 struct BlendFileData *bfd,
-                                 const struct BlendFileReadParams *params,
-                                 struct BlendFileReadReport *reports,
-                                 /* Extra args. */
-                                 bool startup_update_defaults,
-                                 const char *startup_app_template);
-
-void BKE_blendfile_read_setup(struct bContext *C,
-                              struct BlendFileData *bfd,
-                              const struct BlendFileReadParams *params,
-                              struct BlendFileReadReport *reports);
+void BKE_blendfile_read_setup_readfile(struct bContext *C,
+                                       struct BlendFileData *bfd,
+                                       const struct BlendFileReadParams *params,
+                                       struct BlendFileReadWMSetupData *wm_setup_data,
+                                       struct BlendFileReadReport *reports,
+                                       bool startup_update_defaults,
+                                       const char *startup_app_template);
 
 /**
- * \return Blend file data, this must be passed to #BKE_blendfile_read_setup when non-NULL.
+ * Simpler version of #BKE_blendfile_read_setup_readfile used when reading undoe steps from
+ * memfile. */
+void BKE_blendfile_read_setup_undo(struct bContext *C,
+                                   struct BlendFileData *bfd,
+                                   const struct BlendFileReadParams *params,
+                                   struct BlendFileReadReport *reports);
+
+/**
+ * \return Blend file data, this must be passed to
+ * #BKE_blendfile_read_setup_readfile/#BKE_blendfile_read_setup_undo when non-NULL.
  */
 struct BlendFileData *BKE_blendfile_read(const char *filepath,
                                          const struct BlendFileReadParams *params,
                                          struct BlendFileReadReport *reports);
 
 /**
- * \return Blend file data, this must be passed to #BKE_blendfile_read_setup when non-NULL.
+ * \return Blend file data, this must be passed to
+ * #BKE_blendfile_read_setup_readfile/#BKE_blendfile_read_setup_undo when non-NULL.
  */
 struct BlendFileData *BKE_blendfile_read_from_memory(const void *filebuf,
                                                      int filelength,
@@ -84,7 +90,9 @@ struct BlendFileData *BKE_blendfile_read_from_memory(const void *filebuf,
                                                      struct ReportList *reports);
 
 /**
- * \return Blend file data, this must be passed to #BKE_blendfile_read_setup when non-NULL.
+ * \return Blend file data, this must be passed to
+ * #BKE_blendfile_read_setup_readfile/#BKE_blendfile_read_setup_undo when non-NULL.
+ *
  * \note `memfile` is the undo buffer.
  */
 struct BlendFileData *BKE_blendfile_read_from_memfile(struct Main *bmain,

source/blender/blenkernel/BKE_idtype.h
@@ -220,6 +220,10 @@ typedef struct IDTypeInfo {
    * Allow an ID type to preserve some of its data across (memfile) undo steps.
    *
    * \note Called from #setup_app_data when undoing or redoing a memfile step.
+   *
+   * \note In case the whole ID should be fully preserved across undo steps, it is better to flag
+   * its type with `IDTYPE_FLAGS_NO_MEMFILE_UNDO`, since that flag allows more aggressive
+   * optimizations in readfile code for memfile undo.
    */
   IDTypeBlendReadUndoPreserve blend_read_undo_preserve;
 

source/blender/blenkernel/intern/blender_undo.cc
@@ -69,7 +69,7 @@ bool BKE_memfile_undo_decode(MemFileUndoData *mfu,
     BlendFileReadReport bf_reports{};
     BlendFileData *bfd = BKE_blendfile_read(mfu->filepath, &params, &bf_reports);
     if (bfd != nullptr) {
-      BKE_blendfile_read_setup(C, bfd, &params, &bf_reports);
+      BKE_blendfile_read_setup_undo(C, bfd, &params, &bf_reports);
       success = true;
     }
   }
@@ -82,7 +82,7 @@ bool BKE_memfile_undo_decode(MemFileUndoData *mfu,
     BlendFileReadReport blend_file_read_report{};
     BlendFileData *bfd = BKE_blendfile_read_from_memfile(bmain, &mfu->memfile, &params, nullptr);
     if (bfd != nullptr) {
-      BKE_blendfile_read_setup(C, bfd, &params, &blend_file_read_report);
+      BKE_blendfile_read_setup_undo(C, bfd, &params, &blend_file_read_report);
       success = true;
     }
   }

source/blender/blenloader/intern/readblenentry.cc
@@ -455,21 +455,20 @@ BlendFileData *BLO_read_from_memfile(Main *oldmain,
     fd->skip_flags = eBLOReadSkip(params->skip_flags);
     STRNCPY(fd->relabase, filepath);
 
-    /* separate libraries from old main */
+    /* Build old ID map for all old IDs. */
+    blo_make_old_idmap_from_main(fd, oldmain);
+
+    /* Separate linked data from old main. */
     blo_split_main(&old_mainlist, oldmain);
-    /* add the library pointers in oldmap lookup */
-    blo_add_library_pointer_map(&old_mainlist, fd);
+    fd->old_mainlist = &old_mainlist;
 
-    if ((params->skip_flags & BLO_READ_SKIP_UNDO_OLD_MAIN) == 0) {
-      /* Build idmap of old main (we only care about local data here, so we can do that after
-       * split_main() call. */
-      blo_make_old_idmap_from_main(fd, static_cast<Main *>(old_mainlist.first));
-    }
-
-    /* removed packed data from this trick - it's internal data that needs saves */
+    /* Removed packed data from this trick - it's internal data that needs saves. */
 
     /* Store all existing ID caches pointers into a mapping, to allow restoring them into newly
-     * read IDs whenever possible. */
+     * read IDs whenever possible.
+     *
+     * Note that this is only required for local data, since linked data are always re-used
+     * 'as-is'. */
     blo_cache_storage_init(fd, oldmain);
 
     bfd = blo_read_file_internal(fd, filepath);

commit bc3ff6d1e048eb15fb87d64e9ad9f63c25078a86

Fix #108643: Blender window does not open to full dimensions of the desktop.
When opening 'homefile' (i.e. startup or factory startup) at Blender
start (i.e. when there is no existing WM yet), the size of the windows
in the newly read WM is reset to zero, which will then cause `WM_check`
to re-size them the the maximum possible size on current monitor.

Regression from 5ac25f6836.

source/blender/blenloader/BLO_readfile.h
@@ -72,6 +72,8 @@ typedef struct BlendFileData {
  */
 typedef struct BlendFileReadWMSetupData {
   struct wmWindowManager *old_wm; /** The existing WM when filereading process is started. */
+
+  bool is_read_homefile;
 } BlendFileReadWMSetupData;
 
 struct BlendFileReadParams {

source/blender/windowmanager/intern/wm_files.cc
@@ -182,11 +182,14 @@ bool wm_file_or_session_data_has_unsaved_changes(const Main *bmain, const wmWind
  * Clear several WM/UI runtime data that would make later complex WM handling impossible.
  *
  * Return data should be cleared by #wm_file_read_setup_wm_finalize. */
-static BlendFileReadWMSetupData *wm_file_read_setup_wm_init(bContext *C, Main *bmain)
+static BlendFileReadWMSetupData *wm_file_read_setup_wm_init(bContext *C,
+                                                            Main *bmain,
+                                                            const bool is_read_homefile)
 {
   BLI_assert(BLI_listbase_count_at_most(&bmain->wm, 2) <= 1);
   wmWindowManager *wm = static_cast<wmWindowManager *>(bmain->wm.first);
   BlendFileReadWMSetupData *wm_setup_data = MEM_cnew<BlendFileReadWMSetupData>(__func__);
+  wm_setup_data->is_read_homefile = is_read_homefile;
 
   if (wm == nullptr) {
     return wm_setup_data;
@@ -400,6 +403,13 @@ static void wm_file_read_setup_wm_finalize(bContext *C,
   BLI_assert(BLI_listbase_count_at_most(&bmain->wm, 2) <= 1);
   BLI_assert(wm_setup_data != nullptr);
   wmWindowManager *wm = static_cast<wmWindowManager *>(bmain->wm.first);
+
+  /* If reading home (startup) file, and there was no previous WM, clear the size of the windows in
+   * newly read WM so that they get resized to occupy the whole available space on current monitor.
+   */
+  if (wm_setup_data->is_read_homefile && wm_setup_data->old_wm == nullptr) {
+    wm_clear_default_size(C);
+  }
 
   if (wm == nullptr) {
     /* Add a default WM in case none exists in newly read main (should only happen when opening
@@ -994,7 +1004,7 @@ bool WM_file_read(bContext *C, const char *filepath, ReportList *reports)
 
       /* Put WM into a stable state for post-readfile processes (kill jobs, removes event handlers,
        * message bus, and so on). */
-      BlendFileReadWMSetupData *wm_setup_data = wm_file_read_setup_wm_init(C, bmain);
+      BlendFileReadWMSetupData *wm_setup_data = wm_file_read_setup_wm_init(C, bmain, false);
 
       /* This flag is initialized by the operator but overwritten on read.
        * need to re-enable it here else drivers and registered scripts won't work. */
@@ -1117,7 +1127,6 @@ void wm_homefile_read_ex(bContext *C,
   /* Context does not always have valid main pointer here. */
   Main *bmain = G_MAIN;
 #endif
-  ListBase wmbase;
   bool success = false;
 
   /* May be enabled, when the user configuration doesn't exist. */
@@ -1223,7 +1232,7 @@ void wm_homefile_read_ex(bContext *C,
   if (use_data) {
     /* Put WM into a stable state for post-readfile processes (kill jobs, removes event handlers,
      * message bus, and so on). */
-    wm_setup_data = wm_file_read_setup_wm_init(C, bmain);
+    wm_setup_data = wm_file_read_setup_wm_init(C, bmain, true);
   }
 
   filepath_startup[0] = '\0';
@@ -1356,10 +1365,6 @@ void wm_homefile_read_ex(bContext *C,
           C, bfd, &read_file_params, wm_setup_data, &read_report, true, nullptr);
       success = true;
       bmain = CTX_data_main(C);
-    }
-
-    if (use_data && BLI_listbase_is_empty(&wmbase)) {
-      wm_clear_default_size(C);
     }
   }
 