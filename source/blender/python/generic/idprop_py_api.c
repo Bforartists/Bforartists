Bforartists/Bforartists

commit a4982796de94f71cc03007fcbaed800daa2795df

Fix #98973: Renaming Custom Python Properties is Incorrect
PyUnicode_AsUTF8AndSize is used when renaming a custom python property,
this method stores the size of the string without including the null
terminator in the size.

Renaming a custom python property now includes the null terminator when
copying the new string name.

Pull Request: https://projects.blender.org/blender/blender/pulls/107983

source/blender/python/generic/idprop_py_api.c
@@ -261,12 +261,12 @@ static int BPy_IDGroup_SetName(BPy_IDProperty *self, PyObject *value, void *UNUS
 
   name = PyUnicode_AsUTF8AndSize(value, &name_size);
 
-  if (name_size >= MAX_IDPROP_NAME) {
+  if (name_size + 1 >= MAX_IDPROP_NAME) {
     PyErr_SetString(PyExc_TypeError, "string length cannot exceed 63 characters!");
     return -1;
   }
 
-  memcpy(self->prop->name, name, name_size);
+  memcpy(self->prop->name, name, name_size + 1);
   return 0;
 }
 

commit a17776a0491ab2a668820e373d08f2f098e93ef9

Fix off by one error introduced by me in recent commit a498279

source/blender/python/generic/idprop_py_api.c
@@ -261,7 +261,7 @@ static int BPy_IDGroup_SetName(BPy_IDProperty *self, PyObject *value, void *UNUS
 
   name = PyUnicode_AsUTF8AndSize(value, &name_size);
 
-  if (name_size + 1 >= MAX_IDPROP_NAME) {
+  if (name_size + 1 > MAX_IDPROP_NAME) {
     PyErr_SetString(PyExc_TypeError, "string length cannot exceed 63 characters!");
     return -1;
   }