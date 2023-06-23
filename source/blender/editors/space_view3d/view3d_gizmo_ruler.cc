Bforartists/Bforartists

commit 5de4b30ccf73aa17d2062ee4efd62f06b910dedd

Fix measure tool snapping to perpendicular while not dragging

source/blender/editors/space_view3d/view3d_gizmo_ruler.cc
@@ -313,6 +313,8 @@ static void ruler_state_set(RulerInfo *ruler_info, int state)
 
   if (state == RULER_STATE_NORMAL) {
     WM_gizmo_set_flag(ruler_info->snap_data.gizmo, WM_GIZMO_DRAW_VALUE, false);
+    RNA_property_float_set_array(
+        ruler_info->snap_data.gizmo->ptr, ruler_info->snap_data.prop_snap_source, nullptr);
   }
   else if (state == RULER_STATE_DRAG) {
     memset(&ruler_info->drag_state_prev, 0x0, sizeof(ruler_info->drag_state_prev));

commit a0b290f8b61705b957e0c6588aac645ef316c20a

Fix compiler error caused by 5de4b30ccf
`prop_snap_source` does not exist.

source/blender/editors/space_view3d/view3d_gizmo_ruler.cc
@@ -314,7 +314,7 @@ static void ruler_state_set(RulerInfo *ruler_info, int state)
   if (state == RULER_STATE_NORMAL) {
     WM_gizmo_set_flag(ruler_info->snap_data.gizmo, WM_GIZMO_DRAW_VALUE, false);
     RNA_property_float_set_array(
-        ruler_info->snap_data.gizmo->ptr, ruler_info->snap_data.prop_snap_source, nullptr);
+        ruler_info->snap_data.gizmo->ptr, ruler_info->snap_data.prop_prevpoint, nullptr);
   }
   else if (state == RULER_STATE_DRAG) {
     memset(&ruler_info->drag_state_prev, 0x0, sizeof(ruler_info->drag_state_prev));