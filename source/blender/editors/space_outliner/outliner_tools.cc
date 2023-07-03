Bforartists/Bforartists

commit 951efaf17e4e1b19fe351fc7ab49a251f220515a

Fix #107913: Creation of liboverrides of Scenes should not be allowed.
It was still possible to create liboverrides of Scene IDs from the
Outliner.

Scenes are currently not officially supported, RNA API allows to create
them for experimental purposes, but UI should not.

source/blender/editors/space_outliner/outliner_tools.cc
@@ -1043,6 +1043,14 @@ static void id_override_library_create_hierarchy_pre_process_fn(bContext *C,
     return;
   }
 
+  if (!ID_IS_OVERRIDABLE_LIBRARY_HIERARCHY(id_root_reference)) {
+    BKE_reportf(reports,
+                RPT_WARNING,
+                "Could not create library override from data-block '%s', as it is not overridable",
+                id_root_reference->name);
+    return;
+  }
+
   BLI_assert(do_hierarchy);
   UNUSED_VARS_NDEBUG(do_hierarchy);
 

commit 54d0a7a778625efbe51c5864c1240060a910fb87

Fix toggling of liboverrides between editable and non-edtiable with "Make" tool in the Outliner.
regression from 951efaf17e.

Found while investigating #109513.

source/blender/editors/space_outliner/outliner_tools.cc
@@ -1102,6 +1102,18 @@ static void id_override_library_create_hierarchy_pre_process_fn(bContext *C,
     return;
   }
 
+  /* Only process a given ID once. Otherwise, all kind of weird things can happen if e.g. a
+   * selected sub-collection is part of more than one override hierarchies. */
+  if (data->selected_id_uid.contains(id_root_reference->session_uuid)) {
+    return;
+  }
+  data->selected_id_uid.add(id_root_reference->session_uuid);
+
+  if (ID_IS_OVERRIDE_LIBRARY_REAL(id_root_reference) && !ID_IS_LINKED(id_root_reference)) {
+    id_root_reference->override_library->flag &= ~LIBOVERRIDE_FLAG_SYSTEM_DEFINED;
+    return;
+  }
+
   if (!ID_IS_OVERRIDABLE_LIBRARY_HIERARCHY(id_root_reference)) {
     if (ID_IS_LINKED(id_root_reference)) {
       BKE_reportf(
@@ -1117,18 +1129,6 @@ static void id_override_library_create_hierarchy_pre_process_fn(bContext *C,
 
   BLI_assert(do_hierarchy);
   UNUSED_VARS_NDEBUG(do_hierarchy);
-
-  /* Only process a given ID once. Otherwise, all kind of weird things can happen if e.g. a
-   * selected sub-collection is part of more than one override hierarchies. */
-  if (data->selected_id_uid.contains(id_root_reference->session_uuid)) {
-    return;
-  }
-  data->selected_id_uid.add(id_root_reference->session_uuid);
-
-  if (ID_IS_OVERRIDE_LIBRARY_REAL(id_root_reference) && !ID_IS_LINKED(id_root_reference)) {
-    id_root_reference->override_library->flag &= ~LIBOVERRIDE_FLAG_SYSTEM_DEFINED;
-    return;
-  }
 
   if (GS(id_root_reference->name) == ID_GR && (tselem->flag & TSE_CLOSED) != 0) {
     /* If selected element is a (closed) collection, check all of its objects recursively, and also