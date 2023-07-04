Bforartists/Bforartists

commit 8cf4e1c5f98f11364eed18ee6540f7c1e5aa6351

Transform: new feature to edit the 'Snap Base'
This commit implements a new modifier key (`B`) for the transform
operators.

This new key allows changing the 'Snap Base' of a transform by snapping
it to a defined point in the scene.

Ref #66424

# Implementation Details

- This feature is only available in the 3D View.
- This feature is only available for the transform modes:
  - `Move`,
  - `Rotate`,
  - `Scale`,
  - `Vert Slide` and
  - `Edge Slide`.
- The `Snap Base Edit` is enabled while we are transforming and we
  press the key `B`
- The `Snap Base Edit` is confirmed when we press any of the keys:
 `B`, `LMB`, `Enter`
- During um operation, if no snap target is set for an element in the
  scene (Vertex, Edge...), the snap targets to geometry Vertex, Edge,
  Face, Center of Edge and Perpendicular of Edge are set automatically.
- Constraint or similar modal features are not available during the
  `Snap Base Edit` mode.
- Text input is not available during the `Snap Base Edit` mode.
- A prone snap base point is indicated with an small cursor drawing.

Pull Request: https://projects.blender.org/blender/blender/pulls/104443

scripts/modules/bl_keymap_utils/versioning.py
@@ -80,4 +80,16 @@ def keyconfig_update(keyconfig_data, keyconfig_version):
                 km_items.append(('ROTATE_NORMALS', {"type": 'N', "value": 'PRESS'}, None))
                 break
 
+    if keyconfig_version <= (3, 6, 3):
+        if not has_copy:
+            keyconfig_data = copy.deepcopy(keyconfig_data)
+            has_copy = True
+
+        # "Snap Source Toggle" did not exist until then.
+        for km_name, _km_parms, km_items_data in keyconfig_data:
+            if km_name == "Transform Modal Map":
+                km_items_data["items"].extend(("EDIT_SNAP_SOURCE_ON", {"type": 'B', "value": 'PRESS'}, None))
+                km_items_data["items"].append(("EDIT_SNAP_SOURCE_OFF", {"type": 'B', "value": 'PRESS'}, None))
+                break
+
     return keyconfig_data

scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -5915,6 +5915,8 @@ def km_transform_modal_map(params):
         ("TRACKBALL", {"type": 'R', "value": 'PRESS'}, None),
         ("RESIZE", {"type": 'S', "value": 'PRESS'}, None),
         ("ROTATE_NORMALS", {"type": 'N', "value": 'PRESS'}, None),
+        ("EDIT_SNAP_SOURCE_ON", {"type": 'B', "value": 'PRESS'}, None),
+        ("EDIT_SNAP_SOURCE_OFF", {"type": 'B', "value": 'PRESS'}, None),
         ("SNAP_TOGGLE", {"type": 'TAB', "value": 'PRESS', "shift": True}, None),
         ("SNAP_INV_ON", {"type": 'LEFT_CTRL', "value": 'PRESS', "any": True}, None),
         ("SNAP_INV_OFF", {"type": 'LEFT_CTRL', "value": 'RELEASE', "any": True}, None),

scripts/presets/keyconfig/keymap_data/industry_compatible_data.py
@@ -4037,6 +4037,8 @@ def km_transform_modal_map(_params):
         ("TRACKBALL", {"type": 'R', "value": 'PRESS'}, None),
         ("RESIZE", {"type": 'S', "value": 'PRESS'}, None),
         ("ROTATE_NORMALS", {"type": 'N', "value": 'PRESS'}, None),
+        ("EDIT_SNAP_SOURCE_ON", {"type": 'B', "value": 'PRESS'}, None),
+        ("EDIT_SNAP_SOURCE_OFF", {"type": 'B', "value": 'PRESS'}, None),
         ("SNAP_TOGGLE", {"type": 'TAB', "value": 'PRESS', "shift": True}, None),
         ("SNAP_INV_ON", {"type": 'LEFT_CTRL', "value": 'PRESS', "any": True}, None),
         ("SNAP_INV_OFF", {"type": 'LEFT_CTRL', "value": 'RELEASE', "any": True}, None),

source/blender/blenkernel/BKE_blender_version.h
@@ -27,7 +27,7 @@ extern "C" {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 3
+#define BLENDER_FILE_SUBVERSION 4
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and show a warning if the file

source/blender/editors/transform/CMakeLists.txt
@@ -84,6 +84,7 @@ set(SRC
   transform_mode_shear.c
   transform_mode_shrink_fatten.c
   transform_mode_skin_resize.c
+  transform_mode_snapsource.c
   transform_mode_tilt.c
   transform_mode_timescale.c
   transform_mode_timeslide.c

source/blender/editors/transform/transform.c
@@ -543,6 +543,20 @@ static void viewRedrawPost(bContext *C, TransInfo *t)
 static bool transform_modal_item_poll(const wmOperator *op, int value)
 {
   const TransInfo *t = op->customdata;
+  if (t->modifiers & MOD_EDIT_SNAP_SOURCE) {
+    if (value == TFM_MODAL_EDIT_SNAP_SOURCE_OFF) {
+      return true;
+    }
+    else if (!ELEM(value,
+                   TFM_MODAL_CANCEL,
+                   TFM_MODAL_CONFIRM,
+                   TFM_MODAL_ADD_SNAP,
+                   TFM_MODAL_REMOVE_SNAP))
+    {
+      return false;
+    }
+  }
+
   switch (value) {
     case TFM_MODAL_CANCEL: {
       /* TODO: Canceling with LMB is not possible when the operator is activated
@@ -670,6 +684,20 @@ static bool transform_modal_item_poll(const wmOperator *op, int value)
       }
       break;
     }
+    case TFM_MODAL_EDIT_SNAP_SOURCE_OFF:
+      return false;
+    case TFM_MODAL_EDIT_SNAP_SOURCE_ON: {
+      if (t->spacetype != SPACE_VIEW3D) {
+        return false;
+      }
+      if (!ELEM(
+              t->mode, TFM_TRANSLATION, TFM_ROTATION, TFM_RESIZE, TFM_EDGE_SLIDE, TFM_VERT_SLIDE))
+      {
+        /* More modes can be added over time if this feature proves useful for them. */
+        return false;
+      }
+      break;
+    }
   }
   return true;
 }
@@ -686,6 +714,8 @@ wmKeyMap *transform_modal_keymap(wmKeyConfig *keyconf)
       {TFM_MODAL_PLANE_Y, "PLANE_Y", 0, "Y Plane", ""},
       {TFM_MODAL_PLANE_Z, "PLANE_Z", 0, "Z Plane", ""},
       {TFM_MODAL_CONS_OFF, "CONS_OFF", 0, "Clear Constraints", ""},
+      {TFM_MODAL_EDIT_SNAP_SOURCE_ON, "EDIT_SNAP_SOURCE_ON", 0, "Set Snap Base", ""},
+      {TFM_MODAL_EDIT_SNAP_SOURCE_OFF, "EDIT_SNAP_SOURCE_OFF", 0, "Set Snap Base (Off)", ""},
       {TFM_MODAL_SNAP_INV_ON, "SNAP_INV_ON", 0, "Snap Invert", ""},
       {TFM_MODAL_SNAP_INV_OFF, "SNAP_INV_OFF", 0, "Snap Invert (Off)", ""},
       {TFM_MODAL_SNAP_TOGGLE, "SNAP_TOGGLE", 0, "Snap Toggle", ""},
@@ -956,12 +986,16 @@ int transformEvent(TransInfo *t, const wmEvent *event)
   else if (event->type == EVT_MODAL_MAP) {
     switch (event->val) {
       case TFM_MODAL_CANCEL:
-        t->state = TRANS_CANCEL;
-        handled = true;
+        if (!(t->modifiers & MOD_EDIT_SNAP_SOURCE)) {
+          t->state = TRANS_CANCEL;
+          handled = true;
+        }
         break;
       case TFM_MODAL_CONFIRM:
-        t->state = TRANS_CONFIRM;
-        handled = true;
+        if (!(t->modifiers & MOD_EDIT_SNAP_SOURCE)) {
+          t->state = TRANS_CONFIRM;
+          handled = true;
+        }
         break;
       case TFM_MODAL_TRANSLATE:
       case TFM_MODAL_ROTATE:
@@ -1245,6 +1279,10 @@ int transformEvent(TransInfo *t, const wmEvent *event)
           t->mouse.precision = 0;
           t->redraw |= TREDRAW_HARD;
         }
+        break;
+      case TFM_MODAL_EDIT_SNAP_SOURCE_ON:
+        transform_mode_snap_source_init(t);
+        t->redraw |= TREDRAW_HARD;
         break;
       default:
         break;

source/blender/editors/transform/transform.h
@@ -158,6 +158,7 @@ typedef enum {
   MOD_CONSTRAINT_SELECT_PLANE = 1 << 4,
   MOD_NODE_ATTACH = 1 << 5,
   MOD_SNAP_FORCED = 1 << 6,
+  MOD_EDIT_SNAP_SOURCE = 1 << 7,
 } eTModifier;
 ENUM_OPERATORS(eTModifier, MOD_NODE_ATTACH)
 
@@ -271,6 +272,9 @@ enum {
   TFM_MODAL_VERT_EDGE_SLIDE = 31,
   TFM_MODAL_TRACKBALL = 32,
   TFM_MODAL_ROTATE_NORMALS = 33,
+
+  TFM_MODAL_EDIT_SNAP_SOURCE_ON = 34,
+  TFM_MODAL_EDIT_SNAP_SOURCE_OFF = 35,
 };
 
 /** \} */
@@ -753,6 +757,7 @@ void applyMouseInput(struct TransInfo *t,
                      float output[3]);
 void transform_input_update(TransInfo *t, const float fac);
 void transform_input_virtual_mval_reset(TransInfo *t);
+void transform_input_reset(TransInfo *t, const int mval[2]);
 
 void setCustomPoints(TransInfo *t, MouseInput *mi, const int start[2], const int end[2]);
 void setCustomPointsFromDirection(TransInfo *t, MouseInput *mi, const float dir[2]);

source/blender/editors/transform/transform_input.c
@@ -510,4 +510,16 @@ void transform_input_virtual_mval_reset(TransInfo *t)
   }
 }
 
+void transform_input_reset(TransInfo *t, const int mval[2])
+{
+  MouseInput *mi = &t->mouse;
+  copy_v2_v2_int(mi->imval, mval);
+  if (ELEM(mi->apply, InputAngle, InputAngleSpring)) {
+    struct InputAngle_Data *data = mi->data;
+    data->mval_prev[0] = mi->imval[0];
+    data->mval_prev[1] = mi->imval[1];
+    data->angle = 0.0f;
+  }
+}
+
 /** \} */

commit eff50007f408e151d2fb8d2e43fc6eb414fe2b4b

Error importing keymaps prior to version 4.0.3
Caused by 8cf4e1c5f9.

The keymap format was incorrect because `extend` was being used instead
of `append`.

scripts/modules/bl_keymap_utils/versioning.py
@@ -90,7 +90,7 @@ def keyconfig_update(keyconfig_data, keyconfig_version):
         # "Snap Source Toggle" did not exist until then.
         for km_name, _km_parms, km_items_data in keyconfig_data:
             if km_name == "Transform Modal Map":
-                km_items_data["items"].extend(("EDIT_SNAP_SOURCE_ON", {"type": 'B', "value": 'PRESS'}, None))
+                km_items_data["items"].append(("EDIT_SNAP_SOURCE_ON", {"type": 'B', "value": 'PRESS'}, None))
                 km_items_data["items"].append(("EDIT_SNAP_SOURCE_OFF", {"type": 'B', "value": 'PRESS'}, None))
                 break
 