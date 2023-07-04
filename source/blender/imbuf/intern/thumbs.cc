Bforartists/Bforartists

commit 42f3223f47035dcba59f56c542a54e0dd921f5de

Cleanup: move BLI_str_replace into BLI_string_utils.h
String search & replace is a higher level function (unlike BLI_string.h)
which handlers lower level replacements for printing and string copying.

Also use BLI_string_* prefix (matching other utilities).

This makes it possible to use BLI_string in Blender's internal utilities
without depending on DynStr, MemArena... etc.

source/blender/imbuf/intern/thumbs.cc
@@ -241,7 +241,7 @@ static bool uri_from_filename(const char *path, char *uri)
     /* Not a correct absolute path with a drive letter or UNC prefix. */
     return false;
   }
-  BLI_str_replace_char(orig_uri, '\\', '/');
+  BLI_string_replace_char(orig_uri, '\\', '/');
 #else
   SNPRINTF(orig_uri, "file://%s", path);
 #endif

source/blender/blenkernel/intern/gpencil_geom_legacy.cc
@@ -25,6 +25,7 @@
 #include "BLI_math_vector_types.hh"
 #include "BLI_polyfill_2d.h"
 #include "BLI_span.hh"
+#include "BLI_string_utils.h"
 
 #include "DNA_gpencil_legacy_types.h"
 #include "DNA_gpencil_modifier_types.h"
@@ -2686,7 +2687,7 @@ static void make_element_name(const char *obname, const char *name, const int ma
   SNPRINTF(str, "%s_%s", obname, name);
 
   /* Replace any point by underscore. */
-  BLI_str_replace_char(str, '.', '_');
+  BLI_string_replace_char(str, '.', '_');
 
   BLI_strncpy_utf8(r_name, str, maxlen);
 }

source/blender/blenkernel/intern/image.cc
@@ -22,6 +22,7 @@
 #include <string>
 
 #include "BLI_array.hh"
+#include "BLI_string_utils.h"
 
 #include "CLG_log.h"
 
@@ -3586,11 +3587,11 @@ char *BKE_image_get_tile_strformat(const char *filepath, eUDIM_TILE_FORMAT *r_ti
 
   if (strstr(filepath, "<UDIM>") != nullptr) {
     *r_tile_format = UDIM_TILE_FORMAT_UDIM;
-    return BLI_str_replaceN(filepath, "<UDIM>", "%d");
+    return BLI_string_replaceN(filepath, "<UDIM>", "%d");
   }
   if (strstr(filepath, "<UVTILE>") != nullptr) {
     *r_tile_format = UDIM_TILE_FORMAT_UVTILE;
-    return BLI_str_replaceN(filepath, "<UVTILE>", "u%d_v%d");
+    return BLI_string_replaceN(filepath, "<UVTILE>", "u%d_v%d");
   }
 
   *r_tile_format = UDIM_TILE_FORMAT_NONE;

source/blender/blenkernel/intern/layer.cc
@@ -2503,7 +2503,7 @@ static void viewlayer_aov_make_name_unique(ViewLayer *view_layer)
 
   /* Don't allow dots, it's incompatible with OpenEXR convention to store channels
    * as "layer.pass.channel". */
-  BLI_str_replace_char(aov->name, '.', '_');
+  BLI_string_replace_char(aov->name, '.', '_');
   BLI_uniquename(
       &view_layer->aovs, aov, DATA_("AOV"), '_', offsetof(ViewLayerAOV, name), sizeof(aov->name));
 }
@@ -2620,7 +2620,7 @@ static void viewlayer_lightgroup_make_name_unique(ViewLayer *view_layer,
 {
   /* Don't allow dots, it's incompatible with OpenEXR convention to store channels
    * as "layer.pass.channel". */
-  BLI_str_replace_char(lightgroup->name, '.', '_');
+  BLI_string_replace_char(lightgroup->name, '.', '_');
   BLI_uniquename(&view_layer->lightgroups,
                  lightgroup,
                  DATA_("Lightgroup"),

source/blender/blenkernel/intern/simulation_state.cc
@@ -15,6 +15,7 @@
 #include "BLI_fileops.hh"
 #include "BLI_hash_md5.h"
 #include "BLI_path_util.h"
+#include "BLI_string_utils.h"
 
 namespace blender::bke::sim {
 
@@ -78,7 +79,7 @@ void ModifierSimulationCache::try_discover_bake(const StringRefNull absolute_bak
       }
       char modified_file_name[FILE_MAX];
       STRNCPY(modified_file_name, dir_entry.relname);
-      BLI_str_replace_char(modified_file_name, '_', '.');
+      BLI_string_replace_char(modified_file_name, '_', '.');
 
       const SubFrame frame = std::stof(modified_file_name);
 

source/blender/blenkernel/intern/workspace.cc
@@ -541,7 +541,7 @@ void BKE_workspace_tool_id_replace_table(WorkSpace *workspace,
       }
       idname_suffix += idname_prefix_len;
     }
-    BLI_str_replace_table_exact(
+    BLI_string_replace_table_exact(
         idname_suffix, idname_suffix_len, replace_table, replace_table_num);
   }
 }

source/blender/blenlib/BLI_string.h
@@ -160,53 +160,6 @@ bool BLI_str_quoted_substr(const char *__restrict str,
                            const char *__restrict prefix,
                            char *result,
                            size_t result_maxncpy);
-/**
- * string with all instances of substr_old replaced with substr_new,
- * Returns a copy of the c-string \a str into a newly #MEM_mallocN'd
- * and returns it.
- *
- * \note A rather wasteful string-replacement utility, though this shall do for now.
- * Feel free to replace this with an even safe + nicer alternative
- *
- * \param str: The string to replace occurrences of substr_old in
- * \param substr_old: The text in the string to find and replace
- * \param substr_new: The text in the string to find and replace
- * \retval Returns the duplicated string
- */
-char *BLI_str_replaceN(const char *__restrict str,
-                       const char *__restrict substr_old,
-                       const char *__restrict substr_new) ATTR_WARN_UNUSED_RESULT
-    ATTR_NONNULL(1, 2, 3) ATTR_MALLOC;
-
-/**
- * In-place replace every \a src to \a dst in \a str.
- *
- * \param str: The string to operate on.
- * \param src: The character to replace.
- * \param dst: The character to replace with.
- */
-void BLI_str_replace_char(char *str, char src, char dst) ATTR_NONNULL(1);
-
-/**
- * Simple exact-match string replacement.
- *
- * \param replace_table: Array of source, destination pairs.
- *
- * \note Larger tables should use a hash table.
- */
-bool BLI_str_replace_table_exact(char *string,
-                                 size_t string_len,
-                                 const char *replace_table[][2],
-                                 int replace_table_len);
-
-/**
- * Write `dst` into the range between `src_beg` & `src_end`,
- * resize within `string_maxncpy` limits, ensure null terminated.
- *
- * \return the length of `string`.
- */
-size_t BLI_str_replace_range(
-    char *string, size_t string_maxncpy, int src_beg, int src_end, const char *dst);
 
 /**
  * Portable replacement for #snprintf

source/blender/blenlib/BLI_string_utils.h
@@ -21,6 +21,64 @@ struct ListBase;
 
 typedef bool (*UniquenameCheckCallback)(void *arg, const char *name);
 
+/* ------------------------------------------------------------------------- */
+/** \name String Replace
+ * \{ */
+
+/**
+ * string with all instances of substr_old replaced with substr_new,
+ * Returns a copy of the c-string \a str into a newly #MEM_mallocN'd
+ * and returns it.
+ *
+ * \note A rather wasteful string-replacement utility, though this shall do for now.
+ * Feel free to replace this with an even safe + nicer alternative
+ *
+ * \param str: The string to replace occurrences of substr_old in
+ * \param substr_old: The text in the string to find and replace
+ * \param substr_new: The text in the string to find and replace
+ * \retval Returns the duplicated string
+ */
+char *BLI_string_replaceN(const char *__restrict str,
+                          const char *__restrict substr_old,
+                          const char *__restrict substr_new) ATTR_WARN_UNUSED_RESULT
+    ATTR_NONNULL(1, 2, 3) ATTR_MALLOC;
+
+/**
+ * In-place replace every \a src to \a dst in \a str.
+ *
+ * \param str: The string to operate on.
+ * \param src: The character to replace.
+ * \param dst: The character to replace with.
+ */
+void BLI_string_replace_char(char *str, char src, char dst) ATTR_NONNULL(1);
+
+/**
+ * Simple exact-match string replacement.
+ *
+ * \param replace_table: Array of source, destination pairs.
+ *
+ * \note Larger tables should use a hash table.
+ */
+bool BLI_string_replace_table_exact(char *string,
+                                    size_t string_len,
+                                    const char *replace_table[][2],
+                                    int replace_table_len);
+
+/**
+ * Write `dst` into the range between `src_beg` & `src_end`,
+ * resize within `string_maxncpy` limits, ensure null terminated.
+ *
+ * \return the length of `string`.
+ */
+size_t BLI_string_replace_range(
+    char *string, size_t string_maxncpy, int src_beg, int src_end, const char *dst);
+
+/** \} */
+
+/* ------------------------------------------------------------------------- */
+/** \name String Split
+ * \{ */
+
 /**
  * Looks for a numeric suffix preceded by `delim` character on the end of
  * name, puts preceding part into *left and value of suffix into *nr.
@@ -50,6 +108,8 @@ void BLI_string_split_suffix(const char *string, size_t string_maxlen, char *r_b
  */
 void BLI_string_split_prefix(const char *string, size_t string_maxlen, char *r_pre, char *r_body)
     ATTR_NONNULL(1, 3, 4);
+
+/** \} */
 
 /**
  * A version of #BLI_string_join_array_by_sep_charN that takes a table array.

commit 5e9d41cfb9d273a5a2b57a09a1c2cd3ef47251c1

Fix: Compiler error due to missing header
Caused by 42f3223f47
`BLI_string_replace_char` is used in thumbs.cc but header
was missing

source/blender/imbuf/intern/thumbs.cc
@@ -18,6 +18,7 @@
 #include "BLI_hash_md5.h"
 #include "BLI_path_util.h"
 #include "BLI_string.h"
+#include "BLI_string_utils.h"
 #include "BLI_system.h"
 #include "BLI_threads.h"
 #include "BLI_utildefines.h"