Bforartists/Bforartists

commit 85e53b4e4bf79dd911d480e7e00f91472c600f67

GHOST: Replace WITH_OPENGL build option
Replaces it by WITH_OPENGL_BACKEND and cleanup its usage.
Limits visibility of opengl enums and cases.

Pull Request: https://projects.blender.org/blender/blender/pulls/109947

source/blender/gpu/intern/gpu_shader_builder.cc
@@ -45,9 +45,11 @@ void ShaderBuilder::init()
 
   GHOST_GPUSettings gpuSettings = {0};
   switch (GPU_backend_type_selection_get()) {
+#ifdef WITH_METAL_BACKEND
     case GPU_BACKEND_OPENGL:
       gpuSettings.context_type = GHOST_kDrawingContextTypeOpenGL;
       break;
+#endif
 
 #ifdef WITH_METAL_BACKEND
     case GPU_BACKEND_METAL:

CMakeLists.txt
@@ -640,17 +640,18 @@ if(UNIX AND NOT APPLE)
 endif()
 
 
-# OpenGL
-
-option(WITH_OPENGL "When off limits visibility of the opengl headers to just bf_gpu and gawain (temporary option for development purposes)" ON)
+# GPU Module
 option(WITH_GPU_BUILDTIME_SHADER_BUILDER "Shader builder is a developer option enabling linting on GLSL during compilation" OFF)
 option(WITH_RENDERDOC "Use Renderdoc API to capture frames" OFF)
 
 mark_as_advanced(
-  WITH_OPENGL
   WITH_GPU_BUILDTIME_SHADER_BUILDER
   WITH_RENDERDOC
 )
+
+# OpenGL
+option(WITH_OPENGL_BACKEND "Enable OpenGL support as graphic backend" ON)
+mark_as_advanced(WITH_OPENGL_BACKEND)
 
 # Vulkan
 option(WITH_VULKAN_BACKEND "Enable Vulkan as graphics backend (only for development)" OFF)
@@ -661,7 +662,6 @@ mark_as_advanced(
 )
 
 # Metal
-
 if(APPLE)
   option(WITH_METAL_BACKEND "Use Metal for graphics instead of (or as well as) OpenGL on macOS." ON)
   mark_as_advanced(WITH_METAL_BACKEND)
@@ -1247,14 +1247,6 @@ if(WITH_OPENVDB)
   endif()
 
   list(APPEND OPENVDB_LIBRARIES ${BOOST_LIBRARIES} ${TBB_LIBRARIES})
-endif()
-
-
-# -----------------------------------------------------------------------------
-# Configure OpenGL
-
-if(WITH_OPENGL)
-  add_definitions(-DWITH_OPENGL)
 endif()
 
 # -----------------------------------------------------------------------------

intern/ghost/CMakeLists.txt
@@ -77,6 +77,10 @@ set(LIB
   PRIVATE bf::dna
 )
 
+if(WITH_OPENGL_BACKEND)
+  add_definitions(-DWITH_OPENGL_BACKEND)
+endif()
+
 if(WITH_VULKAN_BACKEND)
   list(APPEND SRC
     intern/GHOST_ContextVK.cc

intern/ghost/GHOST_Types.h
@@ -193,11 +193,13 @@ typedef enum { GHOST_kWindowOrderTop = 0, GHOST_kWindowOrderBottom } GHOST_TWind
 
 typedef enum {
   GHOST_kDrawingContextTypeNone = 0,
+#if defined(WITH_OPENGL_BACKEND)
   GHOST_kDrawingContextTypeOpenGL,
+#endif
 #ifdef WIN32
   GHOST_kDrawingContextTypeD3D,
 #endif
-#ifdef __APPLE__
+#if defined(__APPLE__) && defined(WITH_METAL_BACKEND)
   GHOST_kDrawingContextTypeMetal,
 #endif
 #ifdef WITH_VULKAN_BACKEND

intern/ghost/intern/GHOST_ContextCGL.mm
@@ -108,7 +108,7 @@ GHOST_ContextCGL::~GHOST_ContextCGL()
   metalFree();
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     if (m_openGLContext != nil) {
       if (m_openGLContext == [NSOpenGLContext currentContext]) {
         [NSOpenGLContext clearCurrentContext];
@@ -145,7 +145,7 @@ GHOST_TSuccess GHOST_ContextCGL::swapBuffers()
   GHOST_TSuccess return_value = GHOST_kFailure;
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     if (m_openGLContext != nil) {
       if (m_metalView) {
         metalSwapBuffers();
@@ -175,7 +175,7 @@ GHOST_TSuccess GHOST_ContextCGL::setSwapInterval(int interval)
 {
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     if (m_openGLContext != nil) {
       NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
       [m_openGLContext setValues:&interval forParameter:NSOpenGLCPSwapInterval];
@@ -197,7 +197,7 @@ GHOST_TSuccess GHOST_ContextCGL::getSwapInterval(int &intervalOut)
 {
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     if (m_openGLContext != nil) {
       GLint interval;
 
@@ -226,7 +226,7 @@ GHOST_TSuccess GHOST_ContextCGL::activateDrawingContext()
 {
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     if (m_openGLContext != nil) {
       NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
       [m_openGLContext makeCurrentContext];
@@ -247,7 +247,7 @@ GHOST_TSuccess GHOST_ContextCGL::releaseDrawingContext()
 {
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     if (m_openGLContext != nil) {
       NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
       [NSOpenGLContext clearCurrentContext];
@@ -278,7 +278,7 @@ GHOST_TSuccess GHOST_ContextCGL::updateDrawingContext()
 {
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     if (m_openGLContext != nil) {
       if (m_metalView) {
         metalUpdateFramebuffer();
@@ -385,7 +385,7 @@ GHOST_TSuccess GHOST_ContextCGL::initializeDrawingContext()
 
     /* Command-line argument would be better. */
     if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
       /* Command-line argument would be better. */
       static bool softwareGL = getenv("BLENDER_SOFTWAREGL");
 
@@ -508,7 +508,7 @@ GHOST_TSuccess GHOST_ContextCGL::initializeDrawingContext()
 
 GHOST_TSuccess GHOST_ContextCGL::releaseNativeHandles()
 {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
   m_openGLContext = nil;
   m_openGLView = nil;
 #endif
@@ -635,14 +635,14 @@ void GHOST_ContextCGL::metalFree()
 void GHOST_ContextCGL::metalInitFramebuffer()
 {
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     glGenFramebuffers(1, &m_defaultFramebuffer);
 #endif
   }
   updateDrawingContext();
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     glBindFramebuffer(GL_FRAMEBUFFER, m_defaultFramebuffer);
 #endif
   }
@@ -651,7 +651,7 @@ void GHOST_ContextCGL::metalInitFramebuffer()
 void GHOST_ContextCGL::metalUpdateFramebuffer()
 {
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     assert(m_defaultFramebuffer != 0);
 #endif
   }
@@ -661,7 +661,7 @@ void GHOST_ContextCGL::metalUpdateFramebuffer()
   size_t width = (size_t)backingSize.width;
   size_t height = (size_t)backingSize.height;
 
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
   unsigned int glTex;
   CVPixelBufferRef cvPixelBuffer = nil;
   CVOpenGLTextureCacheRef cvGLTexCache = nil;
@@ -671,7 +671,7 @@ void GHOST_ContextCGL::metalUpdateFramebuffer()
 #endif
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     /* OPENGL path */
     {
       /* Test if there is anything to update */
@@ -809,7 +809,7 @@ void GHOST_ContextCGL::metalUpdateFramebuffer()
   }
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     glBindFramebuffer(GL_FRAMEBUFFER, m_defaultFramebuffer);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_RECTANGLE, glTex, 0);
 #endif
@@ -817,7 +817,7 @@ void GHOST_ContextCGL::metalUpdateFramebuffer()
 
   [m_metalLayer setDrawableSize:CGSizeMake((CGFloat)width, (CGFloat)height)];
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     CVPixelBufferRelease(cvPixelBuffer);
     CVOpenGLTextureCacheRelease(cvGLTexCache);
     CVOpenGLTextureRelease(cvGLTex);
@@ -835,7 +835,7 @@ void GHOST_ContextCGL::metalSwapBuffers()
     updateDrawingContext();
 
     if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
       glFlush();
       assert(m_defaultFramebufferMetalTexture[current_swapchain_index].texture != nil);
 #endif
@@ -888,7 +888,7 @@ void GHOST_ContextCGL::initClear()
 {
 
   if (!m_useMetalForRendering) {
-#if WITH_OPENGL
+#ifdef WITH_OPENGL_BACKEND
     glClearColor(0.294, 0.294, 0.294, 0.000);
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(0.000, 0.000, 0.000, 0.000);

intern/ghost/intern/GHOST_System.cc
@@ -403,7 +403,13 @@ GHOST_TSuccess GHOST_System::createFullScreenWindow(GHOST_Window **window,
   if (stereoVisual) {
     gpuSettings.flags |= GHOST_gpuStereoVisual;
   }
+#if defined(WITH_OPENGL_BACKEND)
   gpuSettings.context_type = GHOST_kDrawingContextTypeOpenGL;
+#elif defined(WITH_METAL_BACKEND)
+  gpuSettings.context_type = GHOST_kDrawingContextTypeMetal;
+#else
+#  error
+#endif
   /* NOTE: don't use #getCurrentDisplaySetting() because on X11 we may
    * be zoomed in and the desktop may be bigger than the viewport. */
   GHOST_ASSERT(m_displayManager,

intern/ghost/intern/GHOST_WindowCocoa.mm
@@ -829,8 +829,14 @@ GHOST_Context *GHOST_WindowCocoa::newDrawingContext(GHOST_TDrawingContextType ty
   }
 #endif
 
-  if (type == GHOST_kDrawingContextTypeOpenGL || type == GHOST_kDrawingContextTypeMetal) {
-
+  if (true
+#if defined(WITH_OPENGL_BACKEND)
+      || type == GHOST_kDrawingContextTypeOpenGL
+#elif defined(WITH_METAL_BACKEND)
+      || type == GHOST_kDrawingContextTypeMetal
+#endif
+  )
+  {
     GHOST_Context *context = new GHOST_ContextCGL(
         m_wantStereoVisual, m_metalView, m_metalLayer, m_openGLView, type);
 

source/blender/gpu/CMakeLists.txt
@@ -2,14 +2,6 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-# WITH_OPENGL limits the visibility of the opengl headers to just GPU and bg_gpu,
-# to more easily highlight code-paths in other libraries that need to be refactored,
-# bf_gpu is allowed to have opengl regardless of this option.
-
-if(NOT WITH_OPENGL AND NOT WITH_METAL_BACKEND AND NOT WITH_HEADLESS)
-  add_definitions(-DWITH_OPENGL)
-endif()
-
 set(INC
   .
   intern
@@ -331,8 +323,10 @@ set(LIB
 )
 
 # Select Backend source based on availability
-if(WITH_OPENGL)
+if(WITH_OPENGL_BACKEND)
   list(APPEND SRC ${OPENGL_SRC})
+
+  add_definitions(-DWITH_OPENGL_BACKEND)
 endif()
 
 if(WITH_METAL_BACKEND)

commit 3655d7a1244c51d20548ae580f91e5372d9f6592

GPU: Fix shader builder
Breakage caused by 85e53b4e4bf79dd911d480e7e00f91472c600f67

source/blender/gpu/intern/gpu_shader_builder.cc
@@ -45,7 +45,7 @@ void ShaderBuilder::init()
 
   GHOST_GPUSettings gpuSettings = {0};
   switch (GPU_backend_type_selection_get()) {
-#ifdef WITH_METAL_BACKEND
+#ifdef WITH_OPENGL_BACKEND
     case GPU_BACKEND_OPENGL:
       gpuSettings.context_type = GHOST_kDrawingContextTypeOpenGL;
       break;