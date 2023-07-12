Bforartists/Bforartists

commit b98f1bd5a040b9d4a77ab9efe9c65b5eeb7c1209

Geometry Nodes: add run-time data to geometry nodes modifier
This adds a single run-time field and moves the existing run-time data into a new struct.
This approach makes it much easier to add new run-time data.

Pull Request: https://projects.blender.org/blender/blender/pulls/109905

source/blender/editors/object/object_bake_simulation.cc
@@ -50,6 +50,8 @@
 #include "DEG_depsgraph.h"
 #include "DEG_depsgraph_build.h"
 
+#include "MOD_nodes.hh"
+
 #include "object_intern.h"
 
 #include "WM_api.h"
@@ -96,7 +98,7 @@ static void calculate_simulation_job_startjob(void *customdata,
     LISTBASE_FOREACH (ModifierData *, md, &object->modifiers) {
       if (md->type == eModifierType_Nodes) {
         NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
-        nmd->simulation_cache->ptr->reset();
+        nmd->runtime->simulation_cache.reset();
       }
     }
     objects_to_calc.append(object);
@@ -256,7 +258,7 @@ static void bake_simulation_job_startjob(void *customdata,
     LISTBASE_FOREACH (ModifierData *, md, &object->modifiers) {
       if (md->type == eModifierType_Nodes) {
         NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
-        nmd->simulation_cache->ptr->reset();
+        nmd->runtime->simulation_cache.reset();
         char absolute_bake_dir[FILE_MAX];
         STRNCPY(absolute_bake_dir, nmd->simulation_bake_directory);
         BLI_path_abs(absolute_bake_dir, base_path);
@@ -295,10 +297,10 @@ static void bake_simulation_job_startjob(void *customdata,
     for (ObjectBakeData &object_bake_data : objects_to_bake) {
       for (ModifierBakeData &modifier_bake_data : object_bake_data.modifiers) {
         NodesModifierData &nmd = *modifier_bake_data.nmd;
-        if (nmd.simulation_cache == nullptr) {
+        if (!nmd.runtime->simulation_cache) {
           continue;
         }
-        ModifierSimulationCache &sim_cache = *nmd.simulation_cache->ptr;
+        ModifierSimulationCache &sim_cache = *nmd.runtime->simulation_cache;
         const ModifierSimulationState *sim_state = sim_cache.get_state_at_exact_frame(frame);
         if (sim_state == nullptr || sim_state->zone_states_.is_empty()) {
           continue;
@@ -340,9 +342,9 @@ static void bake_simulation_job_startjob(void *customdata,
   for (ObjectBakeData &object_bake_data : objects_to_bake) {
     for (ModifierBakeData &modifier_bake_data : object_bake_data.modifiers) {
       NodesModifierData &nmd = *modifier_bake_data.nmd;
-      if (nmd.simulation_cache) {
+      if (nmd.runtime->simulation_cache) {
         /* Tag the caches as being baked so that they are not changed anymore. */
-        nmd.simulation_cache->ptr->cache_state = CacheState::Baked;
+        nmd.runtime->simulation_cache->cache_state = CacheState::Baked;
       }
     }
     DEG_id_tag_update(&object_bake_data.object->id, ID_RECALC_GEOMETRY);
@@ -594,7 +596,7 @@ static int delete_baked_simulation_exec(bContext *C, wmOperator *op)
     LISTBASE_FOREACH (ModifierData *, md, &object->modifiers) {
       if (md->type == eModifierType_Nodes) {
         NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
-        nmd->simulation_cache->ptr->reset();
+        nmd->runtime->simulation_cache.reset();
         if (StringRef(nmd->simulation_bake_directory).is_empty()) {
           continue;
         }

source/blender/blenkernel/BKE_simulation_state.hh
@@ -196,12 +196,4 @@ class ModifierSimulationCache {
   void reset();
 };
 
-/**
- * Wrap simulation cache in `std::shared_ptr` so that it can be owned by evaluated modifier even if
- * the original modifier has been deleted.
- */
-struct ModifierSimulationCachePtr {
-  std::shared_ptr<ModifierSimulationCache> ptr;
-};
-
 }  // namespace blender::bke::sim

source/blender/blenkernel/intern/object_dupli.cc
@@ -67,6 +67,8 @@
 #include "RNA_prototypes.h"
 #include "RNA_types.h"
 
+#include "MOD_nodes.hh"
+
 using blender::Array;
 using blender::float2;
 using blender::float3;
@@ -1794,7 +1796,7 @@ ListBase *object_duplilist_preview(Depsgraph *depsgraph,
       continue;
     }
     NodesModifierData *nmd_orig = reinterpret_cast<NodesModifierData *>(md_orig);
-    if (nmd_orig->runtime_eval_log == nullptr) {
+    if (!nmd_orig->runtime->eval_log) {
       continue;
     }
     if (const geo_log::ViewerNodeLog *viewer_log =

source/blender/blenkernel/intern/simulation.cc
@@ -48,6 +48,8 @@
 #include "DEG_depsgraph.h"
 #include "DEG_depsgraph_query.h"
 
+#include "MOD_nodes.hh"
+
 #include "BLO_read_write.h"
 
 static void simulation_init_data(ID *id)
@@ -196,7 +198,7 @@ void BKE_simulation_reset_scene(Scene *scene)
         continue;
       }
       NodesModifierData *nmd = (NodesModifierData *)md;
-      nmd->simulation_cache->ptr->reset();
+      nmd->runtime->simulation_cache.reset();
     }
   }
   FOREACH_SCENE_OBJECT_END;

source/blender/editors/space_action/CMakeLists.txt
@@ -10,6 +10,7 @@ set(INC
   ../../depsgraph
   ../../gpu
   ../../makesrna
+  ../../modifiers
   ../../windowmanager
 
   # RNA_prototypes.h

source/blender/editors/space_action/action_draw.cc
@@ -47,6 +47,8 @@
 #include "ED_anim_api.h"
 #include "ED_keyframes_draw.h"
 
+#include "MOD_nodes.hh"
+
 #include "action_intern.hh"
 
 /* -------------------------------------------------------------------- */
@@ -768,14 +770,14 @@ void timeline_draw_cache(const SpaceAction *saction, const Object *ob, const Sce
       if (nmd->node_group == nullptr) {
         continue;
       }
-      if (nmd->simulation_cache == nullptr) {
+      if (!nmd->runtime->simulation_cache) {
         continue;
       }
       if ((nmd->node_group->runtime->runtime_flag & NTREE_RUNTIME_FLAG_HAS_SIMULATION_ZONE) == 0) {
         continue;
       }
       timeline_cache_draw_simulation_nodes(
-          *scene, *nmd->simulation_cache->ptr, y_offset, cache_draw_height, pos_id);
+          *scene, *nmd->runtime->simulation_cache, y_offset, cache_draw_height, pos_id);
       y_offset += cache_draw_height;
     }
   }

source/blender/makesdna/DNA_modifier_types.h
@@ -16,9 +16,12 @@
 namespace blender::bke::sim {
 struct ModifierSimulationCachePtr;
 }
-using ModifierSimulationCachePtrHandle = blender::bke::sim::ModifierSimulationCachePtr;
+namespace blender {
+struct NodesModifierRuntime;
+}
+using NodesModifierRuntimeHandle = blender::NodesModifierRuntime;
 #else
-typedef struct ModifierSimulationCachePtrHandle ModifierSimulationCachePtrHandle;
+typedef struct NodesModifierRuntimeHandle NodesModifierRuntimeHandle;
 #endif
 
 #ifdef __cplusplus
@@ -2331,20 +2334,7 @@ typedef struct NodesModifierData {
    * Directory where baked simulation states are stored. This may be relative to the .blend file.
    */
   char *simulation_bake_directory;
-  void *_pad;
-
-  /**
-   * Contains logged information from the last evaluation.
-   * This can be used to help the user to debug a node tree.
-   */
-  void *runtime_eval_log;
-
-  /**
-   * Simulation cache that is shared between original and evaluated modifiers. This allows the
-   * original modifier to be removed, without also removing the simulation state which may still be
-   * used by the evaluated modifier.
-   */
-  ModifierSimulationCachePtrHandle *simulation_cache;
+  NodesModifierRuntimeHandle *runtime;
 } NodesModifierData;
 
 typedef struct MeshToVolumeModifierData {

source/blender/modifiers/MOD_nodes.hh
@@ -7,9 +7,34 @@
 struct NodesModifierData;
 struct Object;
 
+namespace blender::bke::sim {
+class ModifierSimulationCache;
+}
+namespace blender::nodes::geo_eval_log {
+class GeoModifierLog;
+}
+
 /**
  * Rebuild the list of properties based on the sockets exposed as the modifier's node group
  * inputs. If any properties correspond to the old properties by name and type, carry over
  * the values.
  */
-void MOD_nodes_update_interface(Object *object, NodesModifierData *nmd);
+void MOD_nodes_update_interface(struct Object *object, struct NodesModifierData *nmd);
+
+namespace blender {
+
+struct NodesModifierRuntime {
+  /**
+   * Contains logged information from the last evaluation.
+   * This can be used to help the user to debug a node tree.
+   */
+  std::unique_ptr<nodes::geo_eval_log::GeoModifierLog> eval_log;
+  /**
+   * Simulation cache that is shared between original and evaluated modifiers. This allows the
+   * original modifier to be removed, without also removing the simulation state which may still be
+   * used by the evaluated modifier.
+   */
+  std::shared_ptr<bke::sim::ModifierSimulationCache> simulation_cache;
+};
+
+}  // namespace blender

commit 2686995da435b204610abe17c8bd3167a95b89e4

Fix: simulation nodes baking does not work
This was accidentally broken in b98f1bd5a040b9d4a77ab9efe9c65b5eeb7c1209.
There is a difference between resetting the `std::shared_ptr` and resetting
the cache.

source/blender/blenkernel/intern/simulation_state.cc
@@ -261,7 +261,7 @@ void scene_simulation_states_reset(Scene &scene)
         continue;
       }
       NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
-      nmd->runtime->simulation_cache.reset();
+      nmd->runtime->simulation_cache->reset();
     }
   }
   FOREACH_SCENE_OBJECT_END;

source/blender/editors/object/object_bake_simulation.cc
@@ -98,7 +98,7 @@ static void calculate_simulation_job_startjob(void *customdata,
     LISTBASE_FOREACH (ModifierData *, md, &object->modifiers) {
       if (md->type == eModifierType_Nodes) {
         NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
-        nmd->runtime->simulation_cache.reset();
+        nmd->runtime->simulation_cache->reset();
       }
     }
     objects_to_calc.append(object);
@@ -258,7 +258,7 @@ static void bake_simulation_job_startjob(void *customdata,
     LISTBASE_FOREACH (ModifierData *, md, &object->modifiers) {
       if (md->type == eModifierType_Nodes) {
         NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
-        nmd->runtime->simulation_cache.reset();
+        nmd->runtime->simulation_cache->reset();
         char absolute_bake_dir[FILE_MAX];
         STRNCPY(absolute_bake_dir, nmd->simulation_bake_directory);
         BLI_path_abs(absolute_bake_dir, base_path);
@@ -596,7 +596,7 @@ static int delete_baked_simulation_exec(bContext *C, wmOperator *op)
     LISTBASE_FOREACH (ModifierData *, md, &object->modifiers) {
       if (md->type == eModifierType_Nodes) {
         NodesModifierData *nmd = reinterpret_cast<NodesModifierData *>(md);
-        nmd->runtime->simulation_cache.reset();
+        nmd->runtime->simulation_cache->reset();
         if (StringRef(nmd->simulation_bake_directory).is_empty()) {
           continue;
         }