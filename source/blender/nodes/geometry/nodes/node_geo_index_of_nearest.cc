Bforartists/Bforartists

commit cabb2c839d0351db47c576a7bdaef0bc852e2d20

BLI: refactor IndexMask for better performance and memory usage
Goals of this refactor:
* Reduce memory consumption of `IndexMask`. The old `IndexMask` uses an
  `int64_t` for each index which is more than necessary in pretty much all
  practical cases currently. Using `int32_t` might still become limiting
  in the future in case we use this to index e.g. byte buffers larger than
  a few gigabytes. We also don't want to template `IndexMask`, because
  that would cause a split in the "ecosystem", or everything would have to
  be implemented twice or templated.
* Allow for more multi-threading. The old `IndexMask` contains a single
  array. This is generally good but has the problem that it is hard to fill
  from multiple-threads when the final size is not known from the beginning.
  This is commonly the case when e.g. converting an array of bool to an
  index mask. Currently, this kind of code only runs on a single thread.
* Allow for efficient set operations like join, intersect and difference.
  It should be possible to multi-thread those operations.
* It should be possible to iterate over an `IndexMask` very efficiently.
  The most important part of that is to avoid all memory access when iterating
  over continuous ranges. For some core nodes (e.g. math nodes), we generate
  optimized code for the cases of irregular index masks and simple index ranges.

To achieve these goals, a few compromises had to made:
* Slicing of the mask (at specific indices) and random element access is
  `O(log #indices)` now, but with a low constant factor. It should be possible
  to split a mask into n approximately equally sized parts in `O(n)` though,
  making the time per split `O(1)`.
* Using range-based for loops does not work well when iterating over a nested
  data structure like the new `IndexMask`. Therefor, `foreach_*` functions with
  callbacks have to be used. To avoid extra code complexity at the call site,

source/blender/nodes/geometry/nodes/node_geo_index_of_nearest.cc
@@ -18,12 +18,11 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_output<decl::Bool>("Has Neighbor").field_source();
 }
 
-static KDTree_3d *build_kdtree(const Span<float3> positions, const IndexMask mask)
+static KDTree_3d *build_kdtree(const Span<float3> positions, const IndexMask &mask)
 {
   KDTree_3d *tree = BLI_kdtree_3d_new(mask.size());
-  for (const int index : mask) {
-    BLI_kdtree_3d_insert(tree, index, positions[index]);
-  }
+  mask.foreach_index(
+      [&](const int index) { BLI_kdtree_3d_insert(tree, index, positions[index]); });
   BLI_kdtree_3d_balance(tree);
   return tree;
 }
@@ -38,13 +37,11 @@ static int find_nearest_non_self(const KDTree_3d &tree, const float3 &position, 
 
 static void find_neighbors(const KDTree_3d &tree,
                            const Span<float3> positions,
-                           const IndexMask mask,
+                           const IndexMask &mask,
                            MutableSpan<int> r_indices)
 {
-  threading::parallel_for(mask.index_range(), 1024, [&](const IndexRange range) {
-    for (const int index : mask.slice(range)) {
-      r_indices[index] = find_nearest_non_self(tree, positions[index], index);
-    }
+  mask.foreach_index(GrainSize(1024), [&](const int index) {
+    r_indices[index] = find_nearest_non_self(tree, positions[index], index);
   });
 }
 
@@ -62,7 +59,7 @@ class IndexOfNearestFieldInput final : public bke::GeometryFieldInput {
   }
 
   GVArray get_varray_for_context(const bke::GeometryFieldContext &context,
-                                 const IndexMask mask) const final
+                                 const IndexMask &mask) const final
   {
     if (!context.attributes()) {
       return {};
@@ -87,58 +84,38 @@ class IndexOfNearestFieldInput final : public bke::GeometryFieldInput {
     const VArraySpan<int> group_ids_span(group_ids);
 
     VectorSet<int> group_indexing;
-    for (const int index : mask) {
+    for (const int index : IndexRange(domain_size)) {
       const int group_id = group_ids_span[index];
       group_indexing.add(group_id);
     }
+    const int groups_num = group_indexing.size();
 
-    /* Each group ID has two corresponding index masks. One that contains all the points
-     * in each group and one that contains all the points in the group that should be looked up
-     * (the intersection of the points in the group and `mask`). In many cases, both of these
-     * masks are the same or very similar, so there is not enough benefit for a separate mask
-     * for the lookups. */
-    const bool use_separate_lookup_indices = mask.size() < domain_size / 2;
+    IndexMaskMemory mask_memory;
+    Array<IndexMask> all_indices_by_group_id(groups_num);
+    Array<IndexMask> lookup_indices_by_group_id(groups_num);
 
-    Array<Vector<int64_t>> all_indices_by_group_id(group_indexing.size());
-    Array<Vector<int64_t>> lookup_indices_by_group_id;
-
-    if (use_separate_lookup_indices) {
-      result.reinitialize(mask.min_array_size());
-      lookup_indices_by_group_id.reinitialize(group_indexing.size());
-    }
-    else {
-      result.reinitialize(domain_size);
-    }
-
-    const auto build_group_masks = [&](const IndexMask mask,
-                                       MutableSpan<Vector<int64_t>> r_groups) {
-      for (const int index : mask) {
-        const int group_id = group_ids_span[index];
-        const int index_of_group = group_indexing.index_of_try(group_id);
-        if (index_of_group != -1) {
-          r_groups[index_of_group].append(index);
-        }
-      }
+    const auto get_group_index = [&](const int i) {
+      const int group_id = group_ids_span[i];
+      return group_indexing.index_of(group_id);
     };
 
-    threading::parallel_invoke(
-        domain_size > 1024 && use_separate_lookup_indices,
-        [&]() {
-          if (use_separate_lookup_indices) {
-            build_group_masks(mask, lookup_indices_by_group_id);
-          }
-        },
-        [&]() { build_group_masks(IndexMask(domain_size), all_indices_by_group_id); });
+    IndexMask::from_groups<int>(
+        IndexMask(domain_size), mask_memory, get_group_index, all_indices_by_group_id);
+
+    if (mask.size() == domain_size) {
+      lookup_indices_by_group_id = all_indices_by_group_id;
+    }
+    else {
+      IndexMask::from_groups<int>(mask, mask_memory, get_group_index, all_indices_by_group_id);
+    }
 
     /* The grain size should be larger as each tree gets smaller. */
     const int avg_tree_size = domain_size / group_indexing.size();
     const int grain_size = std::max(8192 / avg_tree_size, 1);
-    threading::parallel_for(group_indexing.index_range(), grain_size, [&](const IndexRange range) {
-      for (const int index : range) {
-        const IndexMask tree_mask = all_indices_by_group_id[index].as_span();
-        const IndexMask lookup_mask = use_separate_lookup_indices ?
-                                          IndexMask(lookup_indices_by_group_id[index]) :
-                                          tree_mask;
+    threading::parallel_for(IndexRange(groups_num), grain_size, [&](const IndexRange range) {
+      for (const int group_index : range) {
+        const IndexMask &tree_mask = all_indices_by_group_id[group_index];
+        const IndexMask &lookup_mask = lookup_indices_by_group_id[group_index];
         KDTree_3d *tree = build_kdtree(positions, tree_mask);
         find_neighbors(*tree, positions, lookup_mask, result);
         BLI_kdtree_3d_free(tree);
@@ -187,7 +164,7 @@ class HasNeighborFieldInput final : public bke::GeometryFieldInput {
   }
 
   GVArray get_varray_for_context(const bke::GeometryFieldContext &context,
-                                 const IndexMask mask) const final
+                                 const IndexMask &mask) const final
   {
     if (!context.attributes()) {
       return {};

commit 446441ddfa405d90aeb62433fea7f6f1d44a3e5f

Fix #110037: uninitialized result array and typos
Regression introduced in cabb2c839d0351db47c576a7bdaef0bc852e2d20.

Pull Request: https://projects.blender.org/blender/blender/pulls/110098

source/blender/nodes/geometry/nodes/node_geo_index_of_nearest.cc
@@ -106,9 +106,11 @@ class IndexOfNearestFieldInput final : public bke::GeometryFieldInput {
 
     if (mask.size() == domain_size) {
       lookup_indices_by_group_id = all_indices_by_group_id;
+      result.reinitialize(domain_size);
     }
     else {
-      IndexMask::from_groups<int>(mask, mask_memory, get_group_index, all_indices_by_group_id);
+      IndexMask::from_groups<int>(mask, mask_memory, get_group_index, lookup_indices_by_group_id);
+      result.reinitialize(mask.min_array_size());
     }
 
     /* The grain size should be larger as each tree gets smaller. */