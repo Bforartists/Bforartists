945f60f39c5a1c6287287e6d60f0cf0587a07ed0
Draise <ondraise15@hotmail.com>
2023-07-22 18:33:18 -0500

    Addons update from last merge

--- scripts/addons/io_scene_3ds/__init__.py
+++ scripts/addons/io_scene_3ds/__init__.py
@@ -140,11 +140,11 @@ class Export3DS(bpy.types.Operator, ExportHelper):
 
 # Add to a menu
 def menu_func_export(self, context):
-    self.layout.operator(Export3DS.bl_idname, text="3D Studio (.3ds)", icon = 'SAVE_3DS') #bfa - added icon
+    self.layout.operator(Export3DS.bl_idname, text="3D Studio (.3ds)")
 
 
 def menu_func_import(self, context):
-    self.layout.operator(Import3DS.bl_idname, text="3D Studio (.3ds)", icon = 'LOAD_3DS') #bfa - added icon
+    self.layout.operator(Import3DS.bl_idname, text="3D Studio (.3ds)")
 
 
 def register():
