Bforartists/Bforartists

commit 027751c3405aba3071b8254e1fdf4b7151d24186

Cycles: Remove Transmission Roughness from Principled BSDF
This was already unsupported in combination with Multiscattering GGX,
prevented the Principled BSDF from using microfaced-based Fresnel for
Glass materials, and would have made future improvements even trickier.

Pull Request: https://projects.blender.org/blender/blender/pulls/109950

source/blender/nodes/shader/nodes/node_shader_bsdf_principled.cc
@@ -87,11 +87,6 @@ static void node_declare(NodeDeclarationBuilder &b)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
-  b.add_input<decl::Float>("Transmission Roughness")
-      .default_value(0.0f)
-      .min(0.0f)
-      .max(1.0f)
-      .subtype(PROP_FACTOR);
   b.add_input<decl::Color>("Emission").default_value({0.0f, 0.0f, 0.0f, 1.0f});
   b.add_input<decl::Float>("Emission Strength").default_value(1.0).min(0.0f).max(1000000.0f);
   b.add_input<decl::Float>("Alpha").default_value(1.0f).min(0.0f).max(1.0f).subtype(PROP_FACTOR);
@@ -213,14 +208,9 @@ static int node_shader_gpu_bsdf_principled(GPUMaterial *mat,
 
 static void node_shader_update_principled(bNodeTree *ntree, bNode *node)
 {
-  const int distribution = node->custom1;
   const int sss_method = node->custom2;
 
   LISTBASE_FOREACH (bNodeSocket *, sock, &node->inputs) {
-    if (STREQ(sock->name, "Transmission Roughness")) {
-      bke::nodeSetSocketAvailability(ntree, sock, distribution == SHD_GLOSSY_GGX);
-    }
-
     if (STR_ELEM(sock->name, "Subsurface IOR", "Subsurface Anisotropy")) {
       bke::nodeSetSocketAvailability(ntree, sock, sss_method != SHD_SUBSURFACE_BURLEY);
     }

intern/cycles/kernel/osl/shaders/node_principled_bsdf.osl
@@ -24,13 +24,11 @@ shader node_principled_bsdf(string distribution = "multi_ggx",
                             float ClearcoatRoughness = 0.03,
                             float IOR = 1.45,
                             float Transmission = 0.0,
-                            float TransmissionRoughness = 0.0,
                             normal Normal = N,
                             normal ClearcoatNormal = N,
                             normal Tangent = normalize(dPdu),
                             output closure color BSDF = 0)
 {
-  float f = max(IOR, 1e-5);
   float diffuse_weight = (1.0 - clamp(Metallic, 0.0, 1.0)) * (1.0 - clamp(Transmission, 0.0, 1.0));
   float final_transmission = clamp(Transmission, 0.0, 1.0) * (1.0 - clamp(Metallic, 0.0, 1.0));
   float specular_weight = (1.0 - final_transmission);
@@ -92,30 +90,11 @@ shader node_principled_bsdf(string distribution = "multi_ggx",
 
   if (final_transmission > 1e-5) {
     color Cspec0 = mix(color(1.0), BaseColor, SpecularTint);
-    float eta = backfacing() ? 1.0 / f : f;
+    float eta = max(IOR, 1e-5);
+    eta = backfacing() ? 1.0 / eta : eta;
 
-    if (distribution == "ggx" || Roughness <= 5e-2) {
-      float cosNI = dot(Normal, I);
-      float Fr = fresnel_dielectric_cos(cosNI, eta);
-
-      float refl_roughness = Roughness;
-      if (Roughness <= 1e-2)
-        refl_roughness = 0.0;
-
-      float transmission_roughness = refl_roughness;
-      if (distribution == "ggx")
-        transmission_roughness = 1.0 - (1.0 - refl_roughness) * (1.0 - TransmissionRoughness);
-
-      closure color refraction = microfacet(
-          "ggx", Normal, transmission_roughness * transmission_roughness, eta, 1);
-      closure color reflection = microfacet_aniso_fresnel(
-          Normal, T, r2, r2, Cspec0, color(1.0), eta, "ggx");
-      BSDF += final_transmission * mix(BaseColor * refraction, reflection, Fr);
-    }
-    else {
-      BSDF += final_transmission *
-              dielectric_bsdf(Normal, vector(0.0), Cspec0, BaseColor, r2, r2, eta, "multi_ggx");
-    }
+    BSDF += final_transmission *
+            dielectric_bsdf(Normal, vector(0.0), Cspec0, BaseColor, r2, r2, eta, distribution);
   }
 
   if (Clearcoat > 1e-5) {

intern/cycles/kernel/svm/closure.h
@@ -69,8 +69,7 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
     case CLOSURE_BSDF_PRINCIPLED_ID: {
       uint specular_offset, roughness_offset, specular_tint_offset, anisotropic_offset,
           sheen_offset, sheen_tint_offset, clearcoat_offset, clearcoat_roughness_offset,
-          eta_offset, transmission_offset, anisotropic_rotation_offset,
-          transmission_roughness_offset;
+          eta_offset, transmission_offset, anisotropic_rotation_offset, pad1;
       uint4 data_node2 = read_node(kg, &offset);
 
       float3 T = stack_load_float3(stack, data_node.y);
@@ -84,11 +83,8 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
                              &sheen_tint_offset,
                              &clearcoat_offset,
                              &clearcoat_roughness_offset);
-      svm_unpack_node_uchar4(data_node2.x,
-                             &eta_offset,
-                             &transmission_offset,
-                             &anisotropic_rotation_offset,
-                             &transmission_roughness_offset);
+      svm_unpack_node_uchar4(
+          data_node2.x, &eta_offset, &transmission_offset, &anisotropic_rotation_offset, &pad1);
 
       // get Disney principled parameters
       float metallic = param1;
@@ -103,7 +99,6 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
       float clearcoat_roughness = stack_load_float(stack, clearcoat_roughness_offset);
       float transmission = stack_load_float(stack, transmission_offset);
       float anisotropic_rotation = stack_load_float(stack, anisotropic_rotation_offset);
-      float transmission_roughness = stack_load_float(stack, transmission_roughness_offset);
       float eta = fmaxf(stack_load_float(stack, eta_offset), 1e-5f);
 
       ClosureType distribution = (ClosureType)data_node2.y;
@@ -113,13 +108,7 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
       if (anisotropic_rotation != 0.0f)
         T = rotate_around_axis(T, N, anisotropic_rotation * M_2PI_F);
 
-      /* calculate ior */
-      float ior = (sd->flag & SD_BACKFACING) ? 1.0f / eta : eta;
-
-      /* Calculate fresnel for refraction. */
       float3 valid_reflection_N = maybe_ensure_valid_specular_reflection(sd, N);
-      float cosNI = dot(valid_reflection_N, sd->wi);
-      float fresnel = fresnel_dielectric_cos(cosNI, ior);
 
       // calculate weights of the diffuse and specular part
       float diffuse_weight = (1.0f - saturatef(metallic)) * (1.0f - saturatef(transmission));
@@ -301,7 +290,7 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
       }
 #endif
 
-      /* BSDF */
+      /* Transmission */
 #ifdef __CAUSTICS_TRICKS__
       if (kernel_data.integrator.caustics_reflective ||
           kernel_data.integrator.caustics_refractive || (path_flag & PATH_RAY_DIFFUSE) == 0)
@@ -311,100 +300,27 @@ ccl_device_noinline int svm_node_closure_bsdf(KernelGlobals kg,
           Spectrum glass_weight = weight * final_transmission;
           Spectrum cspec0 = base_color * specular_tint + make_float3(1.0f - specular_tint);
 
-          /* Use single-scatter GGX. */
-          if (roughness <= 5e-2f || distribution == CLOSURE_BSDF_MICROFACET_GGX_GLASS_ID) {
+          ccl_private MicrofacetBsdf *bsdf = (ccl_private MicrofacetBsdf *)bsdf_alloc(
+              sd, sizeof(MicrofacetBsdf), glass_weight);
+          ccl_private FresnelDielectricTint *fresnel =
+              (bsdf != NULL) ? (ccl_private FresnelDielectricTint *)closure_alloc_extra(
+                                   sd, sizeof(FresnelDielectricTint)) :
+                               NULL;
 
-            float refl_roughness = roughness;
+          if (bsdf && fresnel) {
+            bsdf->N = valid_reflection_N;
+            bsdf->T = zero_float3();
 
-            /* reflection */
-#ifdef __CAUSTICS_TRICKS__
-            if (kernel_data.integrator.caustics_reflective || (path_flag & PATH_RAY_DIFFUSE) == 0)
-#endif
-            {
-              ccl_private MicrofacetBsdf *bsdf = (ccl_private MicrofacetBsdf *)bsdf_alloc(
-                  sd, sizeof(MicrofacetBsdf), glass_weight * fresnel);
-              ccl_private FresnelGeneralizedSchlick *fresnel =
-                  (bsdf != NULL) ? (ccl_private FresnelGeneralizedSchlick *)closure_alloc_extra(
-                                       sd, sizeof(FresnelGeneralizedSchlick)) :
-                                   NULL;
+            bsdf->alpha_x = bsdf->alpha_y = sqr(roughness);
+            bsdf->ior = (sd->flag & SD_BACKFACING) ? 1.0f / eta : eta;
 
-              if (bsdf && fresnel) {
-                bsdf->N = valid_reflection_N;
-                bsdf->T = zero_float3();
+            fresnel->reflection_tint = cspec0;
+            fresnel->transmission_tint = base_color;
 
-                bsdf->alpha_x = refl_roughness * refl_roughness;
-                bsdf->alpha_y = refl_roughness * refl_roughness;
-                bsdf->ior = ior;
-
-                /* setup bsdf */
-                sd->flag |= bsdf_microfacet_ggx_setup(bsdf);
-
-                fresnel->f0 = cspec0;
-                fresnel->f90 = one_spectrum();
-                fresnel->exponent = -1.0f;
-                fresnel->reflection_tint = one_spectrum();
-                fresnel->transmission_tint = zero_spectrum();
-
-                bsdf_microfacet_setup_fresnel_generalized_schlick(kg, bsdf, sd, fresnel, false);
-              }
-            }
-
-            /* refraction */
-#ifdef __CAUSTICS_TRICKS__
-            if (kernel_data.integrator.caustics_refractive || (path_flag & PATH_RAY_DIFFUSE) == 0)
-#endif
-            {
-              /* This is to prevent MNEE from receiving a null BSDF. */
-              float refraction_fresnel = fmaxf(0.0001f, 1.0f - fresnel);
-              ccl_private MicrofacetBsdf *bsdf = (ccl_private MicrofacetBsdf *)bsdf_alloc(
-                  sd,
-                  sizeof(MicrofacetBsdf),
-                  rgb_to_spectrum(base_color) * glass_weight * refraction_fresnel);
-              if (bsdf) {
-                bsdf->N = valid_reflection_N;
-                bsdf->T = zero_float3();
-
-                if (distribution == CLOSURE_BSDF_MICROFACET_GGX_GLASS_ID)
-                  transmission_roughness = 1.0f - (1.0f - refl_roughness) *
-                                                      (1.0f - transmission_roughness);
-                else
-                  transmission_roughness = refl_roughness;
-
-                bsdf->alpha_x = transmission_roughness * transmission_roughness;
-                bsdf->alpha_y = transmission_roughness * transmission_roughness;
-                bsdf->ior = ior;
-
-                /* setup bsdf */
-                sd->flag |= bsdf_microfacet_ggx_refraction_setup(bsdf);
-              }
-            }
-          } /* Use multi-scatter GGX. */
-          else {
-            ccl_private MicrofacetBsdf *bsdf = (ccl_private MicrofacetBsdf *)bsdf_alloc(
-                sd, sizeof(MicrofacetBsdf), glass_weight);
-            ccl_private FresnelGeneralizedSchlick *fresnel =
-                (bsdf != NULL) ? (ccl_private FresnelGeneralizedSchlick *)closure_alloc_extra(
-                                     sd, sizeof(FresnelGeneralizedSchlick)) :
-                                 NULL;
-
-            if (bsdf && fresnel) {
-              bsdf->N = valid_reflection_N;
-              bsdf->T = zero_float3();
-
-              bsdf->alpha_x = roughness * roughness;
-              bsdf->alpha_y = roughness * roughness;
-              bsdf->ior = ior;
-
-              fresnel->f0 = make_spectrum(F0_from_ior(ior));
-              fresnel->f90 = one_spectrum();
-              fresnel->exponent = -1.0f;
-              fresnel->reflection_tint = cspec0;
-              fresnel->transmission_tint = base_color;
-
-              /* setup bsdf */
-              sd->flag |= bsdf_microfacet_ggx_glass_setup(bsdf);
-              bsdf_microfacet_setup_fresnel_generalized_schlick(kg, bsdf, sd, fresnel, true);
-            }
+            /* setup bsdf */
+            sd->flag |= bsdf_microfacet_ggx_glass_setup(bsdf);
+            const bool is_multiggx = (distribution == CLOSURE_BSDF_MICROFACET_MULTI_GGX_GLASS_ID);
+            bsdf_microfacet_setup_fresnel_dielectric_tint(kg, bsdf, sd, fresnel, is_multiggx);
           }
         }
 #ifdef __CAUSTICS_TRICKS__

intern/cycles/scene/shader_nodes.cpp
@@ -2659,7 +2659,6 @@ NODE_DEFINE(PrincipledBsdfNode)
   SOCKET_IN_FLOAT(clearcoat_roughness, "Clearcoat Roughness", 0.03f);
   SOCKET_IN_FLOAT(ior, "IOR", 0.0f);
   SOCKET_IN_FLOAT(transmission, "Transmission", 0.0f);
-  SOCKET_IN_FLOAT(transmission_roughness, "Transmission Roughness", 0.0f);
   SOCKET_IN_FLOAT(anisotropic_rotation, "Anisotropic Rotation", 0.0f);
   SOCKET_IN_COLOR(emission, "Emission", zero_float3());
   SOCKET_IN_FLOAT(emission_strength, "Emission Strength", 1.0f);
@@ -2770,8 +2769,7 @@ void PrincipledBsdfNode::compile(SVMCompiler &compiler,
                                  ShaderInput *p_clearcoat_roughness,
                                  ShaderInput *p_ior,
                                  ShaderInput *p_transmission,
-                                 ShaderInput *p_anisotropic_rotation,
-                                 ShaderInput *p_transmission_roughness)
+                                 ShaderInput *p_anisotropic_rotation)
 {
   ShaderInput *base_color_in = input("Base Color");
   ShaderInput *subsurface_color_in = input("Subsurface Color");
@@ -2796,7 +2794,6 @@ void PrincipledBsdfNode::compile(SVMCompiler &compiler,
   int clearcoat_roughness_offset = compiler.stack_assign(p_clearcoat_roughness);
   int ior_offset = compiler.stack_assign(p_ior);
   int transmission_offset = compiler.stack_assign(p_transmission);
-  int transmission_roughness_offset = compiler.stack_assign(p_transmission_roughness);
   int anisotropic_rotation_offset = compiler.stack_assign(p_anisotropic_rotation);
   int subsurface_radius_offset = compiler.stack_assign(p_subsurface_radius);
   int subsurface_ior_offset = compiler.stack_assign(p_subsurface_ior);
@@ -2818,13 +2815,12 @@ void PrincipledBsdfNode::compile(SVMCompiler &compiler,
       compiler.encode_uchar4(
           sheen_offset, sheen_tint_offset, clearcoat_offset, clearcoat_roughness_offset));
 
-  compiler.add_node(compiler.encode_uchar4(ior_offset,
-                                           transmission_offset,
-                                           anisotropic_rotation_offset,
-                                           transmission_roughness_offset),
-                    distribution,
-                    subsurface_method,
-                    SVM_STACK_INVALID);
+  compiler.add_node(
+      compiler.encode_uchar4(
+          ior_offset, transmission_offset, anisotropic_rotation_offset, SVM_STACK_INVALID),
+      distribution,
+      subsurface_method,
+      SVM_STACK_INVALID);
 
   float3 bc_default = get_float3(base_color_in->socket_type);
 
@@ -2866,8 +2862,7 @@ void PrincipledBsdfNode::compile(SVMCompiler &compiler)
           input("Clearcoat Roughness"),
           input("IOR"),
           input("Transmission"),
-          input("Anisotropic Rotation"),
-          input("Transmission Roughness"));
+          input("Anisotropic Rotation"));
 }
 
 void PrincipledBsdfNode::compile(OSLCompiler &compiler)

intern/cycles/scene/shader_nodes.h
@@ -531,8 +531,7 @@ class PrincipledBsdfNode : public BsdfBaseNode {
                ShaderInput *clearcoat_roughness,
                ShaderInput *ior,
                ShaderInput *transmission,
-               ShaderInput *anisotropic_rotation,
-               ShaderInput *transmission_roughness);
+               ShaderInput *anisotropic_rotation);
 
   NODE_SOCKET_API(float3, base_color)
   NODE_SOCKET_API(float3, subsurface_color)
@@ -552,7 +551,6 @@ class PrincipledBsdfNode : public BsdfBaseNode {
   NODE_SOCKET_API(float, ior)
   NODE_SOCKET_API(float, transmission)
   NODE_SOCKET_API(float, anisotropic_rotation)
-  NODE_SOCKET_API(float, transmission_roughness)
   NODE_SOCKET_API(float3, normal)
   NODE_SOCKET_API(float3, clearcoat_normal)
   NODE_SOCKET_API(float3, tangent)

source/blender/blenloader/intern/versioning_400.cc
@@ -266,6 +266,19 @@ static void version_replace_texcoord_normal_socket(bNodeTree *ntree)
   }
 }
 
+static void version_principled_transmission_roughness(bNodeTree *ntree)
+{
+  LISTBASE_FOREACH (bNode *, node, &ntree->nodes) {
+    if (node->type != SH_NODE_BSDF_PRINCIPLED) {
+      continue;
+    }
+    bNodeSocket *sock = nodeFindSocket(node, SOCK_IN, "Transmission Roughness");
+    if (sock != nullptr) {
+      nodeRemoveSocket(ntree, node, sock);
+    }
+  }
+}
+
 void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
 {
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 400, 1)) {
@@ -374,8 +387,6 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
    * \note Keep this message at the bottom of the function.
    */
   {
-    /* Convert anisotropic BSDF node to glossy BSDF. */
-
     /* Keep this block, even when empty. */
 
     if (!DNA_struct_elem_find(fd->filesdna, "LightProbe", "int", "grid_bake_samples")) {
@@ -415,5 +426,13 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
         layer->opacity = 1.0f;
       }
     }
+
+    /* Remove Transmission Roughness from Principled BSDF. */
+    FOREACH_NODETREE_BEGIN (bmain, ntree, id) {
+      if (ntree->type == NTREE_SHADER) {
+        version_principled_transmission_roughness(ntree);
+      }
+    }
+    FOREACH_NODETREE_END;
   }
 }

source/blender/gpu/shaders/material/gpu_shader_material_principled.glsl
@@ -31,7 +31,6 @@ void node_bsdf_principled(vec4 base_color,
                           float clearcoat_roughness,
                           float ior,
                           float transmission,
-                          float transmission_roughness,
                           vec4 emission,
                           float emission_strength,
                           float alpha,
@@ -52,7 +51,6 @@ void node_bsdf_principled(vec4 base_color,
   float diffuse_weight = (1.0 - transmission) * (1.0 - metallic);
   float specular_weight = (1.0 - transmission);
   float clearcoat_weight = max(clearcoat, 0.0) * 0.25;
-  transmission_roughness = 1.0 - (1.0 - roughness) * (1.0 - transmission_roughness);
   specular = max(0.0, specular);
 
   N = safe_normalize(N);
@@ -145,8 +143,7 @@ void node_bsdf_principled(vec4 base_color,
 
   refraction_data.color = base_color.rgb * btdf;
   refraction_data.N = N;
-  refraction_data.roughness = do_multiscatter != 0.0 ? roughness :
-                                                       max(roughness, transmission_roughness);
+  refraction_data.roughness = roughness;
   refraction_data.ior = ior;
 
   /* Ref. #98190: Defines are optimizations for old compilers.

commit 8244be5c6526b97210f818513ed5a222acd93516

Fix EEVEE: Broken Normals and Transparency
This was caused by 027751c3405aba3071b8254e1fdf4b7151d24186
The removal of the transmission roughness socket messed
with the hardcoded socket indices used to for auto socket
values and optimization detection.

Use defines inbetween sockets declarations to make it more
obvious that it they needs to be updated on socket changes.
This also makes the code a bit more meaningful.

source/blender/nodes/shader/nodes/node_shader_bsdf_principled.cc
@@ -13,88 +13,118 @@ namespace blender::nodes::node_shader_bsdf_principled_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
+  /**
+   * Define static socket numbers to avoid string based lookups for GPU material creation as these
+   * could run on animated materials.
+   */
   b.add_input<decl::Color>("Base Color").default_value({0.8f, 0.8f, 0.8f, 1.0f});
+#define SOCK_BASE_COLOR_ID 0
   b.add_input<decl::Float>("Subsurface")
       .default_value(0.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_SUBSURFACE_ID 1
   b.add_input<decl::Vector>("Subsurface Radius")
       .default_value({1.0f, 0.2f, 0.1f})
       .min(0.0f)
       .max(100.0f)
       .compact();
+#define SOCK_SUBSURFACE_RADIUS_ID 2
   b.add_input<decl::Color>("Subsurface Color").default_value({0.8f, 0.8f, 0.8f, 1.0f});
+#define SOCK_SUBSURFACE_COLOR_ID 3
   b.add_input<decl::Float>("Subsurface IOR")
       .default_value(1.4f)
       .min(1.01f)
       .max(3.8f)
       .subtype(PROP_FACTOR);
+#define SOCK_SUBSURFACE_IOR_ID 4
   b.add_input<decl::Float>("Subsurface Anisotropy")
       .default_value(0.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_SUBSURFACE_ANISOTROPY_ID 5
   b.add_input<decl::Float>("Metallic")
       .default_value(0.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_METALLIC_ID 6
   b.add_input<decl::Float>("Specular")
       .default_value(0.5f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_SPECULAR_ID 7
   b.add_input<decl::Float>("Specular Tint")
       .default_value(0.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_SPECULAR_TINT_ID 8
   b.add_input<decl::Float>("Roughness")
       .default_value(0.5f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_ROUGHNESS_ID 9
   b.add_input<decl::Float>("Anisotropic")
       .default_value(0.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_ANISOTROPIC_ID 10
   b.add_input<decl::Float>("Anisotropic Rotation")
       .default_value(0.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_ANISOTROPIC_ROTATION_ID 11
   b.add_input<decl::Float>("Sheen").default_value(0.0f).min(0.0f).max(1.0f).subtype(PROP_FACTOR);
+#define SOCK_SHEEN_ID 12
   b.add_input<decl::Float>("Sheen Tint")
       .default_value(0.5f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_SHEEN_TINT_ID 13
   b.add_input<decl::Float>("Clearcoat")
       .default_value(0.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_CLEARCOAT_ID 14
   b.add_input<decl::Float>("Clearcoat Roughness")
       .default_value(0.03f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_CLEARCOAT_ROUGHNESS_ID 15
   b.add_input<decl::Float>("IOR").default_value(1.45f).min(0.0f).max(1000.0f);
+#define SOCK_IOR_ID 16
   b.add_input<decl::Float>("Transmission")
       .default_value(0.0f)
       .min(0.0f)
       .max(1.0f)
       .subtype(PROP_FACTOR);
+#define SOCK_TRANSMISSION_ID 17
   b.add_input<decl::Color>("Emission").default_value({0.0f, 0.0f, 0.0f, 1.0f});
+#define SOCK_EMISSION_ID 18
   b.add_input<decl::Float>("Emission Strength").default_value(1.0).min(0.0f).max(1000000.0f);
+#define SOCK_EMISSION_STRENGTH_ID 19
   b.add_input<decl::Float>("Alpha").default_value(1.0f).min(0.0f).max(1.0f).subtype(PROP_FACTOR);
+#define SOCK_ALPHA_ID 20
   b.add_input<decl::Vector>("Normal").hide_value();
+#define SOCK_NORMAL_ID 21
   b.add_input<decl::Vector>("Clearcoat Normal").hide_value();
+#define SOCK_CLEARCOAT_NORMAL_ID 22
   b.add_input<decl::Vector>("Tangent").hide_value();
+#define SOCK_TANGENT_ID 23
   b.add_input<decl::Float>("Weight").unavailable();
+#define SOCK_WEIGHT_ID 24
   b.add_output<decl::Shader>("BSDF");
+#define SOCK_BSDF_ID 25
 }
 
 static void node_shader_buts_principled(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
@@ -120,29 +150,29 @@ static int node_shader_gpu_bsdf_principled(GPUMaterial *mat,
                                            GPUNodeStack *out)
 {
   /* Normals */
-  if (!in[22].link) {
-    GPU_link(mat, "world_normals_get", &in[22].link);
+  if (!in[SOCK_NORMAL_ID].link) {
+    GPU_link(mat, "world_normals_get", &in[SOCK_NORMAL_ID].link);
   }
 
   /* Clearcoat Normals */
-  if (!in[23].link) {
-    GPU_link(mat, "world_normals_get", &in[23].link);
+  if (!in[SOCK_CLEARCOAT_NORMAL_ID].link) {
+    GPU_link(mat, "world_normals_get", &in[SOCK_CLEARCOAT_NORMAL_ID].link);
   }
 
 #if 0 /* Not used at the moment. */
   /* Tangents */
-  if (!in[24].link) {
+  if (!in[SOCK_TANGENT_ID].link) {
     GPUNodeLink *orco = GPU_attribute(CD_ORCO, "");
-    GPU_link(mat, "tangent_orco_z", orco, &in[24].link);
-    GPU_link(mat, "node_tangent", in[24].link, &in[24].link);
+    GPU_link(mat, "tangent_orco_z", orco, &in[SOCK_TANGENT_ID].link);
+    GPU_link(mat, "node_tangent", in[SOCK_TANGENT_ID].link, &in[SOCK_TANGENT_ID].link);
   }
 #endif
 
-  bool use_diffuse = socket_not_one(6) && socket_not_one(17);
-  bool use_subsurf = socket_not_zero(1) && use_diffuse;
-  bool use_refract = socket_not_one(6) && socket_not_zero(17);
-  bool use_transparency = socket_not_one(21);
-  bool use_clear = socket_not_zero(14);
+  bool use_diffuse = socket_not_one(SOCK_METALLIC_ID) && socket_not_one(SOCK_TRANSMISSION_ID);
+  bool use_subsurf = socket_not_zero(SOCK_SUBSURFACE_ID) && use_diffuse;
+  bool use_refract = socket_not_one(SOCK_METALLIC_ID) && socket_not_zero(SOCK_TRANSMISSION_ID);
+  bool use_transparency = socket_not_one(SOCK_ALPHA_ID);
+  bool use_clear = socket_not_zero(SOCK_CLEARCOAT_ID);
 
   eGPUMaterialFlag flag = GPU_MATFLAG_GLOSSY;
   if (use_diffuse) {
@@ -180,7 +210,8 @@ static int node_shader_gpu_bsdf_principled(GPUMaterial *mat,
   }
 
   if (use_subsurf) {
-    bNodeSocket *socket = (bNodeSocket *)BLI_findlink(&node->runtime->original->inputs, 2);
+    bNodeSocket *socket = (bNodeSocket *)BLI_findlink(&node->runtime->original->inputs,
+                                                      SOCK_SUBSURFACE_RADIUS_ID);
     bNodeSocketValueRGBA *socket_data = (bNodeSocketValueRGBA *)socket->default_value;
     /* For some reason it seems that the socket value is in ARGB format. */
     use_subsurf = GPU_material_sss_profile_create(mat, &socket_data->value[1]);