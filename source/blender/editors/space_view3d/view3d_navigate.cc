blender/blender

commit 329cf07499a4eb3814e2cc3db123b393a330ec4c

View3D Refactor: compute 'ViewOpsData::zfac' only when needed

source/blender/editors/space_view3d/view3d_navigate.cc
@@ -302,7 +302,14 @@ void ViewOpsData::init_navigation(bContext *C,
     }
   }
 
+  if (viewops_flag & VIEWOPS_FLAG_INIT_ZFAC) {
+    float tvec[3];
+    negate_v3_v3(tvec, rv3d->ofs);
+    this->init.zfac = ED_view3d_calc_zfac(rv3d, tvec);
+  }
+
   this->init.persp_with_auto_persp_applied = rv3d->persp;
+
   if (event) {
     this->init.event_type = event->type;
     copy_v2_v2_int(this->init.event_xy, event->xy);
@@ -328,12 +335,6 @@ void ViewOpsData::init_navigation(bContext *C,
       /* For rotation with trackball rotation. */
       calctrackballvec(&region->winrct, event_xy_offset, this->init.trackvec);
     }
-  }
-
-  {
-    float tvec[3];
-    negate_v3_v3(tvec, rv3d->ofs);
-    this->init.zfac = ED_view3d_calc_zfac(rv3d, tvec);
   }
 
   copy_qt_qt(this->curr.viewquat, rv3d->viewquat);

source/blender/editors/space_view3d/view3d_navigate.hh
@@ -75,6 +75,8 @@ enum eViewOpsFlag {
   VIEWOPS_FLAG_USE_MOUSE_INIT = (1 << 3),
 
   VIEWOPS_FLAG_ZOOM_TO_MOUSE = (1 << 4),
+
+  VIEWOPS_FLAG_INIT_ZFAC = (1 << 5),
 };
 ENUM_OPERATORS(eViewOpsFlag, VIEWOPS_FLAG_ZOOM_TO_MOUSE);
 

source/blender/editors/space_view3d/view3d_navigate_move.cc
@@ -134,7 +134,7 @@ void VIEW3D_OT_move(wmOperatorType *ot)
 /** \} */
 
 const ViewOpsType ViewOpsType_move = {
-    /*flag*/ (VIEWOPS_FLAG_DEPTH_NAVIGATE | VIEWOPS_FLAG_USE_MOUSE_INIT),
+    /*flag*/ (VIEWOPS_FLAG_DEPTH_NAVIGATE | VIEWOPS_FLAG_USE_MOUSE_INIT | VIEWOPS_FLAG_INIT_ZFAC),
     /*idname*/ "VIEW3D_OT_move",
     /*poll_fn*/ view3d_location_poll,
     /*init_fn*/ viewmove_invoke_impl,

source/blender/editors/space_view3d/view3d_navigate_view_pan.cc
@@ -119,7 +119,7 @@ void VIEW3D_OT_view_pan(wmOperatorType *ot)
 /** \} */
 
 const ViewOpsType ViewOpsType_pan = {
-    /*flag*/ (VIEWOPS_FLAG_DEPTH_NAVIGATE | VIEWOPS_FLAG_USE_MOUSE_INIT),
+    /*flag*/ (VIEWOPS_FLAG_DEPTH_NAVIGATE | VIEWOPS_FLAG_USE_MOUSE_INIT | VIEWOPS_FLAG_INIT_ZFAC),
     /*idname*/ "VIEW3D_OT_view_pan",
     /*poll_fn*/ view3d_location_poll,
     /*init_fn*/ viewpan_invoke_impl,

Bforartists/Bforartists

commit 0cf21b4ef8f3768b7ec81685e3574c0de7a452bb

Fix View Pan operation failing
Caused by 329cf07499a4eb3814e2cc3db123b393a330ec4c

source/blender/editors/space_view3d/view3d_navigate.cc
@@ -52,7 +52,7 @@ static eViewOpsFlag viewops_flag_from_prefs()
   const bool use_zoom_to_mouse = (U.uiflag & USER_ZOOM_TO_MOUSEPOS) != 0;
   const bool use_auto_persp = (U.uiflag & USER_AUTOPERSP) != 0;
 
-  enum eViewOpsFlag flag = VIEWOPS_FLAG_NONE;
+  enum eViewOpsFlag flag = VIEWOPS_FLAG_INIT_ZFAC;
   if (use_select) {
     flag |= VIEWOPS_FLAG_ORBIT_SELECT;
   }

source/blender/editors/space_view3d/view3d_navigate.hh
@@ -78,7 +78,7 @@ enum eViewOpsFlag {
 
   VIEWOPS_FLAG_INIT_ZFAC = (1 << 5),
 };
-ENUM_OPERATORS(eViewOpsFlag, VIEWOPS_FLAG_ZOOM_TO_MOUSE);
+ENUM_OPERATORS(eViewOpsFlag, VIEWOPS_FLAG_INIT_ZFAC);
 
 struct ViewOpsType {
   eViewOpsFlag flag;