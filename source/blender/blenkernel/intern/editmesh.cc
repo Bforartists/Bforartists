Bforartists/Bforartists

commit b661151b38be0eafe759975dddc6c7188c932c48

Refactor: Simplify mesh edit mode modifier evaluation
Instead of keeping track of a local array of positions in the modifier
stack itself, use the existing edit mode SoA "edit cache" which already
contains a contiguous array of positions. Combined with positions as a
generic attribute, this means the state is contained just in the mesh
(and the geometry set) making the code much easier to follow.

To do this we make more use of the mesh wrapper system, where we can
pass a `Mesh` that's actually stored with a `BMesh` and the extra
cached array of positions. This also resolves some confusion-- it was
weird to have the mesh wrapper system for this purpose but not use it.

Since we always created a wrapped mesh in edit mode, there's no need
for `MOD_deform_mesh_eval_get` at all anymore. That function was quite
confusing with "eval" in its name when it really retrieved the original
mesh.

Many deform modifiers had placeholder edit mode evaluation functions.
Since these didn't do anything and since the priority is node-based
deformation now, I removed these. The case is documented more in the
modifier type struct callbacks.

Pull Request: https://projects.blender.org/blender/blender/pulls/108637

source/blender/blenkernel/intern/editmesh.cc
@@ -230,9 +230,9 @@ const float (*BKE_editmesh_vert_coords_when_deformed(Depsgraph *depsgraph,
   Object *object_eval = DEG_get_evaluated_object(depsgraph, ob);
   Mesh *editmesh_eval_final = BKE_object_get_editmesh_eval_final(object_eval);
 
-  if ((me->runtime->edit_data != nullptr) && (me->runtime->edit_data->vertexCos != nullptr)) {
+  if (BKE_mesh_wrapper_vert_coords(me) != nullptr) {
     /* Deformed, and we have deformed coords already. */
-    coords = me->runtime->edit_data->vertexCos;
+    coords = BKE_mesh_wrapper_vert_coords(me);
   }
   else if ((editmesh_eval_final != nullptr) &&
            (editmesh_eval_final->runtime->wrapper_type == ME_WRAPPER_TYPE_BMESH))

source/blender/blenkernel/BKE_DerivedMesh.h
@@ -246,7 +246,7 @@ struct Mesh *editbmesh_get_eval_cage_from_orig(struct Depsgraph *depsgraph,
                                                struct Object *obedit,
                                                const struct CustomData_MeshMasks *dataMask);
 
-float (*editbmesh_vert_coords_alloc(struct BMEditMesh *em, int *r_vert_len))[3];
+float (*editbmesh_vert_coords_alloc(struct BMEditMesh *em))[3];
 bool editbmesh_modifier_is_enabled(const struct Scene *scene,
                                    const struct Object *ob,
                                    struct ModifierData *md,

source/blender/blenkernel/BKE_editmesh_cache.h
@@ -16,7 +16,7 @@ struct BMEditMesh;
 
 typedef struct EditMeshData {
   /** when set, \a vertexNos, polyNos are lazy initialized */
-  const float (*vertexCos)[3];
+  float (*vertexCos)[3];
 
   /** lazy initialize (when \a vertexCos is set) */
   float const (*vertexNos)[3];

source/blender/blenkernel/BKE_mesh_wrapper.h
@@ -15,11 +15,6 @@ struct Mesh;
 extern "C" {
 #endif
 
-struct Mesh *BKE_mesh_wrapper_from_editmesh_with_coords(
-    struct BMEditMesh *em,
-    const struct CustomData_MeshMasks *cd_mask_extra,
-    const float (*vert_coords)[3],
-    const struct Mesh *me_settings);
 struct Mesh *BKE_mesh_wrapper_from_editmesh(struct BMEditMesh *em,
                                             const struct CustomData_MeshMasks *cd_mask_extra,
                                             const struct Mesh *me_settings);
@@ -30,6 +25,21 @@ int BKE_mesh_wrapper_vert_len(const struct Mesh *me);
 int BKE_mesh_wrapper_edge_len(const struct Mesh *me);
 int BKE_mesh_wrapper_loop_len(const struct Mesh *me);
 int BKE_mesh_wrapper_poly_len(const struct Mesh *me);
+
+/**
+ * Return a contiguous array of vertex position values, if available.
+ * Otherwise, vertex positions are stored in BMesh vertices.
+ */
+const float (*BKE_mesh_wrapper_vert_coords(const struct Mesh *mesh))[3];
+
+
+/**
+ * Return a contiguous array of face normal values, if available.
+ * Otherwise, normals are stored in BMesh faces.
+ */
+const float (*BKE_mesh_wrapper_poly_normals(struct Mesh *mesh))[3];
+
+void BKE_mesh_wrapper_tag_positions_changed(struct Mesh *mesh);
 
 void BKE_mesh_wrapper_vert_coords_copy(const struct Mesh *me,
                                        float (*vert_coords)[3],

source/blender/blenkernel/BKE_modifier.h
@@ -187,10 +187,9 @@ typedef struct ModifierTypeInfo {
   /********************* Deform modifier functions *********************/
 
   /**
-   * Only for deform types, should apply the deformation
-   * to the given vertex array. If the deformer requires information from
-   * the object it can obtain it from the mesh argument if non-NULL,
-   * and otherwise the ob argument.
+   * Apply a deformation to the positions in the \a vertexCos array. If the \a mesh argument is
+   * non-null, if will contain proper (not wrapped) mesh data. The \a vertexCos array may or may
+   * not be the same as the mesh's position attribute.
    */
   void (*deformVerts)(struct ModifierData *md,
                       const struct ModifierEvalContext *ctx,
@@ -208,7 +207,8 @@ typedef struct ModifierTypeInfo {
                          float (*defMats)[3][3],
                          int numVerts);
   /**
-   * Like deformVerts but called during editmode (for supporting modifiers)
+   * Like deformVerts but called during edit-mode if supported. The \a mesh argument might be a
+   * wrapper around edit BMesh data.
    */
   void (*deformVertsEM)(struct ModifierData *md,
                         const struct ModifierEvalContext *ctx,

source/blender/blenkernel/intern/DerivedMesh.cc
@@ -1053,14 +1053,12 @@ static void mesh_calc_modifiers(Depsgraph *depsgraph,
   }
 }
 
-float (*editbmesh_vert_coords_alloc(BMEditMesh *em, int *r_vert_len))[3]
+float (*editbmesh_vert_coords_alloc(BMEditMesh *em))[3]
 {
   BMIter iter;
   BMVert *eve;
   float(*cos)[3];
   int i;
-
-  *r_vert_len = em->bm->totvert;
 
   cos = (float(*)[3])MEM_malloc_arrayN(em->bm->totvert, sizeof(float[3]), "vertexcos");
 
@@ -1135,6 +1133,21 @@ static void editbmesh_calc_modifier_final_normals_or_defer(
   editbmesh_calc_modifier_final_normals(mesh_final, final_datamask);
 }
 
+static float (*mesh_wrapper_vert_coords_ensure_for_write(Mesh *mesh))[3]
+{
+  switch (mesh->runtime->wrapper_type) {
+    case ME_WRAPPER_TYPE_BMESH:
+      if (mesh->runtime->edit_data->vertexCos == nullptr) {
+        mesh->runtime->edit_data->vertexCos = editbmesh_vert_coords_alloc(mesh->edit_mesh);
+      }
+      return mesh->runtime->edit_data->vertexCos;
+    case ME_WRAPPER_TYPE_MDATA:
+    case ME_WRAPPER_TYPE_SUBD:
+      return reinterpret_cast<float(*)[3]>(mesh->vert_positions_for_write().data());
+  }
+  return nullptr;
+}
+
 static void editbmesh_calc_modifiers(Depsgraph *depsgraph,
                                      const Scene *scene,
                                      Object *ob,
@@ -1145,20 +1158,10 @@ static void editbmesh_calc_modifiers(Depsgraph *depsgraph,
                                      Mesh **r_final,
                                      GeometrySet **r_geometry_set)
 {
-  /* Input and final mesh. Final mesh is only created the moment the first
-   * constructive modifier is executed, or a deform modifier needs normals
-   * or certain data layers. */
   Mesh *mesh_input = (Mesh *)ob->data;
-  Mesh *mesh_final = nullptr;
   Mesh *mesh_cage = nullptr;
   /* This geometry set contains the non-mesh data that might be generated by modifiers. */
   GeometrySet geometry_set_final;
-
-  /* TODO: Remove use of `deformed_verts` in mesh modifier stack
-   * since mesh positions are now stored in a contiguous array. */
-  float(*deformed_verts)[3] = nullptr;
-  int num_deformed_verts = 0;
-  bool isPrevDeform = false;
 
   /* Mesh with constructive modifiers but no deformation applied. Tracked
    * along with final mesh if undeformed / orco coordinates are requested
@@ -1189,11 +1192,11 @@ static void editbmesh_calc_modifiers(Depsgraph *depsgraph,
   CDMaskLink *md_datamask = datamasks;
   CustomData_MeshMasks append_mask = CD_MASK_BAREMESH;
 
-  /* Evaluate modifiers up to certain index to get the mesh cage. */
+  Mesh *mesh_final = BKE_mesh_wrapper_from_editmesh(em_input, &final_datamask, mesh_input);
+
   int cageIndex = BKE_modifiers_get_cage_index(scene, ob, nullptr, true);
   if (r_cage && cageIndex == -1) {
-    mesh_cage = BKE_mesh_wrapper_from_editmesh_with_coords(
-        em_input, &final_datamask, nullptr, mesh_input);
+    mesh_cage = mesh_final;
   }
 
   /* The mesh from edit mode should not have any original index layers already, since those
@@ -1206,24 +1209,21 @@ static void editbmesh_calc_modifiers(Depsgraph *depsgraph,
   BKE_modifiers_clear_errors(ob);
 
   if (ob->modifier_flag & OB_MODIFIER_FLAG_ADD_REST_POSITION) {
-    if (mesh_final == nullptr) {
-      mesh_final = BKE_mesh_from_bmesh_for_eval_nomain(em_input->bm, nullptr, mesh_input);
-      ASSERT_IS_VALID_MESH(mesh_final);
-    }
+    BKE_mesh_wrapper_ensure_mdata(mesh_final);
     set_rest_position(*mesh_final);
   }
 
+  bool non_deform_modifier_applied = false;
   for (int i = 0; md; i++, md = md->next, md_datamask = md_datamask->next) {
     const ModifierTypeInfo *mti = BKE_modifier_get_info((ModifierType)md->type);
-
-    if (!editbmesh_modifier_is_enabled(scene, ob, md, mesh_final != nullptr)) {
+    if (!editbmesh_modifier_is_enabled(scene, ob, md, non_deform_modifier_applied)) {
       continue;
     }
 
     blender::bke::ScopedModifierTimer modifier_timer{*md};
 
     /* Add an orco mesh as layer if needed by this modifier. */
-    if (mesh_final && mesh_orco && mti->requiredDataMask) {
+    if (mesh_orco && mti->requiredDataMask) {
       CustomData_MeshMasks mask = {0};
       mti->requiredDataMask(md, &mask);
       if (mask.vmask & CD_MASK_ORCO) {
@@ -1231,61 +1231,41 @@ static void editbmesh_calc_modifiers(Depsgraph *depsgraph,
       }
     }
 
-    /* How to apply modifier depends on (a) what we already have as
-     * a result of previous modifiers (could be a mesh or just
-     * deformed vertices) and (b) what type the modifier is. */
-    if (mti->type == eModifierTypeType_OnlyDeform) {
-      /* No existing verts to deform, need to build them. */
-      if (!deformed_verts) {
-        if (mesh_final) {
-          /* Deforming a derived mesh, read the vertex locations
-           * out of the mesh and deform them. Once done with this
-           * run of deformers verts will be written back. */
-          deformed_verts = BKE_mesh_vert_coords_alloc(mesh_final, &num_deformed_verts);
-        }
-        else {
-          deformed_verts = editbmesh_vert_coords_alloc(em_input, &num_deformed_verts);
-        }
+    if (mesh_final == mesh_cage) {
+      /* If the cage mesh has already been assigned, we have passed the cage index in the modifier
+       * list. If the cage and final meshes are still the same, duplicate the final mesh so the
+       * cage mesh isn't modified anymore. */
+      mesh_final = BKE_mesh_copy_for_eval(mesh_final);
+      if (mesh_cage->edit_mesh) {
+        mesh_final->edit_mesh = static_cast<BMEditMesh *>(MEM_dupallocN(mesh_cage->edit_mesh));
+        mesh_final->edit_mesh->is_shallow_copy = true;
+        mesh_final->runtime->is_original_bmesh = true;
+        BKE_mesh_runtime_ensure_edit_data(mesh_final);
       }
-      else if (isPrevDeform && mti->dependsOnNormals && mti->dependsOnNormals(md)) {
-        if (mesh_final == nullptr) {
-          mesh_final = BKE_mesh_from_bmesh_for_eval_nomain(em_input->bm, nullptr, mesh_input);
-          BKE_mesh_ensure_default_orig_index_customdata(mesh_final);
-          ASSERT_IS_VALID_MESH(mesh_final);
-        }
-        BLI_assert(deformed_verts != nullptr);
-        BKE_mesh_vert_coords_apply(mesh_final, deformed_verts);
-      }
+    }
 
+    if (mti->type == eModifierTypeType_OnlyDeform) {
       if (mti->deformVertsEM) {
-        BKE_modifier_deform_vertsEM(
-            md, &mectx, em_input, mesh_final, deformed_verts, num_deformed_verts);
+        BKE_modifier_deform_vertsEM(md,
+                                    &mectx,
+                                    em_input,
+                                    mesh_final,
+                                    mesh_wrapper_vert_coords_ensure_for_write(mesh_final),
+                                    BKE_mesh_wrapper_vert_len(mesh_final));
+        BKE_mesh_wrapper_tag_positions_changed(mesh_final);
       }
       else {
-        BKE_modifier_deform_verts(md, &mectx, mesh_final, deformed_verts, num_deformed_verts);
+        BKE_mesh_wrapper_ensure_mdata(mesh_final);
+        BKE_modifier_deform_verts(md,
+                                  &mectx,
+                                  mesh_final,
+                                  BKE_mesh_vert_positions_for_write(mesh_final),
+                                  mesh_final->totvert);
+        BKE_mesh_tag_positions_changed(mesh_final);
       }
     }
     else {
-      /* apply vertex coordinates or build a DerivedMesh as necessary */
-      if (mesh_final) {
-        if (deformed_verts) {
-          Mesh *mesh_tmp = BKE_mesh_copy_for_eval(mesh_final);
-          if (mesh_final != mesh_cage) {
-            BKE_id_free(nullptr, mesh_final);
-          }
-          mesh_final = mesh_tmp;
-          BKE_mesh_vert_coords_apply(mesh_final, deformed_verts);
-        }
-        else if (mesh_final == mesh_cage) {
-          /* 'me' may be changed by this modifier, so we need to copy it. */
-          mesh_final = BKE_mesh_copy_for_eval(mesh_final);
-        }
-      }
-      else {
-        mesh_final = BKE_mesh_wrapper_from_editmesh_with_coords(
-            em_input, nullptr, deformed_verts, mesh_input);
-        deformed_verts = nullptr;
-      }
+      non_deform_modifier_applied = true;
 
       /* create an orco derivedmesh in parallel */
       CustomData_MeshMasks mask = md_datamask->mask;
@@ -1336,76 +1316,20 @@ static void editbmesh_calc_modifiers(Depsgraph *depsgraph,
       ASSERT_IS_VALID_MESH(mesh_next);
 
       if (mesh_next) {
-        if (mesh_final && mesh_final != mesh_next) {
+        if (mesh_final != mesh_next) {
           BKE_id_free(nullptr, mesh_final);
         }
         mesh_final = mesh_next;
-
-        if (deformed_verts) {
-          MEM_freeN(deformed_verts);
-          deformed_verts = nullptr;
-        }
       }
       mesh_final->runtime->deformed_only = false;
     }
 
     if (r_cage && i == cageIndex) {
-      if (mesh_final && deformed_verts) {
-        mesh_cage = BKE_mesh_copy_for_eval(mesh_final);
-        BKE_mesh_vert_coords_apply(mesh_cage, deformed_verts);
-      }
-      else if (mesh_final) {
-        mesh_cage = mesh_final;
-      }
-      else {
-        Mesh *me_orig = mesh_input;
-        /* Modifying the input mesh is weak, however as there can only be one object in edit mode
-         * even if multiple are sharing the same mesh this should be thread safe. */
-        if ((me_orig->id.tag & LIB_TAG_COPIED_ON_WRITE) && (ob->mode & OB_MODE_EDIT)) {
-          if (!BKE_mesh_runtime_ensure_edit_data(me_orig)) {
-            BKE_mesh_runtime_reset_edit_data(me_orig);
-          }
-          me_orig->runtime->edit_data->vertexCos = (const float(*)[3])MEM_dupallocN(
-              deformed_verts);
-        }
-        mesh_cage = BKE_mesh_wrapper_from_editmesh_with_coords(
-            em_input,
-            &final_datamask,
-            deformed_verts ? (const float(*)[3])MEM_dupallocN(deformed_verts) : nullptr,
-            mesh_input);
-      }
+      mesh_cage = mesh_final;
     }
-
-    isPrevDeform = (mti->type == eModifierTypeType_OnlyDeform);
   }
 
   BLI_linklist_free((LinkNode *)datamasks, nullptr);
-
-  /* Yay, we are done. If we have a DerivedMesh and deformed vertices need
-   * to apply these back onto the DerivedMesh. If we have no DerivedMesh
-   * then we need to build one. */
-  if (mesh_final) {
-    if (deformed_verts) {
-      if (mesh_final == mesh_cage) {
-        mesh_final = BKE_mesh_copy_for_eval(mesh_final);
-      }
-      BKE_mesh_vert_coords_apply(mesh_final, deformed_verts);
-    }
-  }
-  else if (!deformed_verts && mesh_cage) {
-    /* cage should already have up to date normals */
-    mesh_final = mesh_cage;
-  }
-  else {
-    /* this is just a copy of the editmesh, no need to calc normals */
-    mesh_final = BKE_mesh_wrapper_from_editmesh_with_coords(
-        em_input, &final_datamask, deformed_verts, mesh_input);
-    deformed_verts = nullptr;
-  }
-
-  if (deformed_verts) {
-    MEM_freeN(deformed_verts);
-  }
 
   /* Add orco coordinates to final and deformed mesh if requested. */
   if (final_datamask.vmask & CD_MASK_ORCO) {

source/blender/blenkernel/intern/crazyspace.cc
@@ -247,7 +247,8 @@ int BKE_crazyspace_get_first_deform_matrices_editbmesh(Depsgraph *depsgraph,
   ModifierData *md;
   Mesh *me_input = static_cast<Mesh *>(ob->data);
   Mesh *me = nullptr;
-  int i, a, modifiers_left_num = 0, verts_num = 0;
+  int i, a, modifiers_left_num = 0;
+  const int verts_num = em->bm->totvert;
   int cageIndex = BKE_modifiers_get_cage_index(scene, ob, nullptr, true);
   float(*defmats)[3][3] = nullptr, (*deformedVerts)[3] = nullptr;
   VirtualModifierData virtualModifierData;
@@ -276,8 +277,8 @@ int BKE_crazyspace_get_first_deform_matrices_editbmesh(Depsgraph *depsgraph,
         cd_mask_extra = datamasks->mask;
         BLI_linklist_free((LinkNode *)datamasks, nullptr);
 
-        me = BKE_mesh_wrapper_from_editmesh_with_coords(em, &cd_mask_extra, nullptr, me_input);
-        deformedVerts = editbmesh_vert_coords_alloc(em, &verts_num);
+        me = BKE_mesh_wrapper_from_editmesh(em, &cd_mask_extra, me_input);
+        deformedVerts = editbmesh_vert_coords_alloc(em);
         defmats = static_cast<float(*)[3][3]>(
             MEM_mallocN(sizeof(*defmats) * verts_num, "defmats"));
 

source/blender/blenkernel/intern/mesh_normals.cc
@@ -366,10 +366,11 @@ void BKE_mesh_ensure_normals_for_display(Mesh *mesh)
       break;
     case ME_WRAPPER_TYPE_BMESH: {
       BMEditMesh *em = mesh->edit_mesh;
-      EditMeshData *emd = mesh->runtime->edit_data;
-      if (emd->vertexCos) {
-        BKE_editmesh_cache_ensure_vert_normals(em, emd);
-        BKE_editmesh_cache_ensure_poly_normals(em, emd);
+      if (EditMeshData *emd = mesh->runtime->edit_data) {
+        if (emd->vertexCos) {
+          BKE_editmesh_cache_ensure_vert_normals(em, emd);
+          BKE_editmesh_cache_ensure_poly_normals(em, emd);
+        }
       }
       return;
     }

commit 1869b680e341fdc4234fa62ddd3d6b16b96e93c2

Fix #110411: Mesh loop cut preview uses final instead of cage positions
Caused by b661151b38be0eafe759, which caused the final position
arrays to be returned in more situations (in this case, when the final
result is a non-wrapper mesh). Instead, use the cage mesh, which
is more correct anyway-- it just wasn't always available before.

source/blender/blenkernel/intern/editmesh.cc
@@ -226,13 +226,12 @@ const float (*BKE_editmesh_vert_coords_when_deformed(Depsgraph *depsgraph,
   const float(*coords)[3] = nullptr;
   *r_is_alloc = false;
 
-  Mesh *me = static_cast<Mesh *>(ob->data);
   Object *object_eval = DEG_get_evaluated_object(depsgraph, ob);
   Mesh *editmesh_eval_final = BKE_object_get_editmesh_eval_final(object_eval);
 
-  if (BKE_mesh_wrapper_vert_coords(me) != nullptr) {
+  if (Mesh *mesh_cage = BKE_object_get_editmesh_eval_cage(ob)) {
     /* Deformed, and we have deformed coords already. */
-    coords = BKE_mesh_wrapper_vert_coords(me);
+    coords = BKE_mesh_wrapper_vert_coords(mesh_cage);
   }
   else if ((editmesh_eval_final != nullptr) &&
            (editmesh_eval_final->runtime->wrapper_type == ME_WRAPPER_TYPE_BMESH))