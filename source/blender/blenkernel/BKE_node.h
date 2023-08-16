blender/blender

commit f18c45eb6968cf469e1e205e8cc2bf8ed8cb1ecf

Cleanup: avoid using NOD_static_types.h for creating the node.type enum
The overall goal is to get rid of `NOD_static_types.h`. This patch removes
one usage of it to generate the `node.type` rna enum. Not all of the data
was available on `bNodeType` already, so I had to add the missing data there.

Pull Request: https://projects.blender.org/blender/blender/pulls/110810

source/blender/blenkernel/BKE_node.h
@@ -238,6 +238,8 @@ typedef struct bNodeType {
   char ui_name[64]; /* MAX_NAME */
   char ui_description[256];
   int ui_icon;
+  /** Should usually use the idname instead, but this enum type is still exposed in Python. */
+  const char *enum_name_legacy;
 
   float width, minwidth, maxwidth;
   float height, minheight, maxheight;

source/blender/blenkernel/intern/node.cc
@@ -4503,6 +4503,8 @@ void node_type_base(bNodeType *ntype, const int type, const char *name, const sh
     ntype->rna_ext.srna = RNA_struct_find(#Category #StructName); \
     BLI_assert(ntype->rna_ext.srna != nullptr); \
     RNA_struct_blender_type_set(ntype->rna_ext.srna, ntype); \
+    ntype->enum_name_legacy = EnumName; \
+    STRNCPY(ntype->ui_description, UIDesc); \
     break;
 
   switch (type) {

source/blender/makesrna/intern/rna_nodetree.cc
@@ -784,17 +784,19 @@ static const EnumPropertyItem *rna_node_static_type_itemf(bContext * /*C*/,
     category = "FunctionNode";
   }
 
-#  define DefNode(Category, ID, DefFunc, EnumName, StructName, UIName, UIDesc) \
-    if (STREQ(#Category, "Node") || STREQ(#Category, category)) { \
-      tmp.value = ID; \
-      tmp.identifier = EnumName; \
-      tmp.name = UIName; \
-      tmp.description = UIDesc; \
-      tmp.icon = ICON_NONE; \
-      RNA_enum_item_add(&item, &totitem, &tmp); \
+  NODE_TYPES_BEGIN (ntype) {
+    if (ntype->enum_name_legacy &&
+        (BLI_str_startswith(ntype->idname, "Node") || BLI_str_startswith(ntype->idname, category)))
+    {
+      tmp.value = ntype->type;
+      tmp.identifier = ntype->enum_name_legacy;
+      tmp.name = ntype->ui_name;
+      tmp.description = ntype->ui_description;
+      tmp.icon = ICON_NONE;
+      RNA_enum_item_add(&item, &totitem, &tmp);
     }
-#  include "../../nodes/NOD_static_types.h"
-#  undef DefNode
+  }
+  NODE_TYPES_END;
 
   RNA_enum_item_end(&item, &totitem);
   *r_free = true;

Bforartists/Bforartists

commit 1a9ab491b6a2e28986b3564b113d7a9dea4b4880

Fix #111142: bl_static_type is empty for node groups and custom nodes
Caused by f18c45eb6968cf469e1e205e8cc2bf8ed8cb1ecf.
Functions `node_type_base` and `node_type_base_custom`
are the same, but last one have been missed in the cleanup.

Pull Request: https://projects.blender.org/blender/blender/pulls/111154

source/blender/blenkernel/BKE_node.h
@@ -755,6 +755,7 @@ bool nodeGroupPoll(const struct bNodeTree *nodetree,
 void node_type_base_custom(struct bNodeType *ntype,
                            const char *idname,
                            const char *name,
+                           const char *enum_name,
                            short nclass);
 
 /**

source/blender/blenkernel/intern/node.cc
@@ -4301,12 +4301,14 @@ void node_type_base(bNodeType *ntype, const int type, const char *name, const sh
 void node_type_base_custom(bNodeType *ntype,
                            const char *idname,
                            const char *name,
+                           const char *enum_name,
                            const short nclass)
 {
   STRNCPY(ntype->idname, idname);
   ntype->type = NODE_CUSTOM;
   STRNCPY(ntype->ui_name, name);
   ntype->nclass = nclass;
+  ntype->enum_name_legacy = enum_name;
 
   blender::bke::node_type_base_defaults(ntype);
 }

source/blender/makesrna/intern/rna_nodetree.cc
@@ -1794,7 +1794,7 @@ static bNodeType *rna_Node_register_base(Main *bmain,
   /* setup dummy node & node type to store static properties in */
   memset(&dummy_nt, 0, sizeof(bNodeType));
   /* this does some additional initialization of default values */
-  node_type_base_custom(&dummy_nt, identifier, "", 0);
+  node_type_base_custom(&dummy_nt, identifier, "", "CUSTOM", 0);
 
   memset(&dummy_node, 0, sizeof(bNode));
   dummy_node.typeinfo = &dummy_nt;

source/blender/nodes/composite/nodes/node_composite_common.cc
@@ -22,7 +22,7 @@ void register_node_type_cmp_group()
 
   /* NOTE: Cannot use sh_node_type_base for node group, because it would map the node type
    * to the shared NODE_GROUP integer type id. */
-  node_type_base_custom(&ntype, "CompositorNodeGroup", "Group", NODE_CLASS_GROUP);
+  node_type_base_custom(&ntype, "CompositorNodeGroup", "Group", "GROUP", NODE_CLASS_GROUP);
   ntype.type = NODE_GROUP;
   ntype.poll = cmp_node_poll_default;
   ntype.poll_instance = node_group_poll_instance;

source/blender/nodes/geometry/nodes/node_geo_common.cc
@@ -42,7 +42,7 @@ static void register_node_type_geo_group()
 {
   static bNodeType ntype;
 
-  node_type_base_custom(&ntype, "GeometryNodeGroup", "Group", NODE_CLASS_GROUP);
+  node_type_base_custom(&ntype, "GeometryNodeGroup", "Group", "GROUP", NODE_CLASS_GROUP);
   ntype.type = NODE_GROUP;
   ntype.poll = geo_node_poll_default;
   ntype.poll_instance = node_group_poll_instance;

source/blender/nodes/intern/node_register.cc
@@ -34,7 +34,8 @@ static void register_undefined_types()
   STRNCPY(blender::bke::NodeTreeTypeUndefined.ui_name, N_("Undefined"));
   STRNCPY(blender::bke::NodeTreeTypeUndefined.ui_description, N_("Undefined Node Tree Type"));
 
-  node_type_base_custom(&blender::bke::NodeTypeUndefined, "NodeUndefined", "Undefined", 0);
+  node_type_base_custom(
+      &blender::bke::NodeTypeUndefined, "NodeUndefined", "Undefined", "UNDEFINED", 0);
   blender::bke::NodeTypeUndefined.poll = node_undefined_poll;
 
   STRNCPY(blender::bke::NodeSocketTypeUndefined.idname, "NodeSocketUndefined");

source/blender/nodes/shader/nodes/node_shader_common.cc
@@ -85,7 +85,7 @@ void register_node_type_sh_group()
   /* NOTE: cannot use #sh_node_type_base for node group, because it would map the node type
    * to the shared #NODE_GROUP integer type id. */
 
-  node_type_base_custom(&ntype, "ShaderNodeGroup", "Group", NODE_CLASS_GROUP);
+  node_type_base_custom(&ntype, "ShaderNodeGroup", "Group", "GROUP", NODE_CLASS_GROUP);
   ntype.type = NODE_GROUP;
   ntype.poll = sh_node_poll_default;
   ntype.poll_instance = node_group_poll_instance;

source/blender/nodes/texture/nodes/node_texture_common.cc
@@ -143,7 +143,7 @@ void register_node_type_tex_group()
   /* NOTE: Cannot use #sh_node_type_base for node group, because it would map the node type
    * to the shared #NODE_GROUP integer type id. */
 
-  node_type_base_custom(&ntype, "TextureNodeGroup", "Group", NODE_CLASS_GROUP);
+  node_type_base_custom(&ntype, "TextureNodeGroup", "Group", "GROUP", NODE_CLASS_GROUP);
   ntype.type = NODE_GROUP;
   ntype.poll = tex_node_poll_default;
   ntype.poll_instance = node_group_poll_instance;