Bforartists/Bforartists

commit d5b7c0d760394bd49c476c49771704d589d6555c

Fix #109460: vertex weight slider not working correctly
#109460 was caused by the introduction of implicit sharing for custom
data layers (7eee378eccc8f87e17). Due to implicit sharing, the vertex
weight layer is now shared between the original and evaluated object in
object/weight paint mode, but not in edit mode because of bmesh. For
that reason, the original data has to be copied to be able to edit it, which
causes its pointer to change. `uiDefButF` can't deal with the situation when
the pointer changes on every redraw.

The solution is to just use an intermediate variable that is passed to the
`uiBut` and to update the vertex weight only when the intermediate value has
changed.

Pull Request: https://projects.blender.org/blender/blender/pulls/109665

source/blender/editors/space_view3d/view3d_buttons.cc
@@ -101,6 +101,7 @@ struct TransformProperties {
   float ob_dims_orig[3];
   float ob_scale_orig[3];
   float ob_dims[3];
+  float active_vertex_weight;
   /* Floats only (treated as an array). */
   TransformMedian ve_median, median;
   bool tag_for_update;
@@ -1322,6 +1323,18 @@ static bool view3d_panel_vgroup_poll(const bContext *C, PanelType * /*pt*/)
   return false;
 }
 
+static void update_active_vertex_weight(struct bContext *C, void *arg1, void * /*arg2*/)
+{
+  View3D *v3d = CTX_wm_view3d(C);
+  TransformProperties *tfp = v3d_transform_props_ensure(v3d);
+  ViewLayer *view_layer = CTX_data_view_layer(C);
+  Object *ob = BKE_view_layer_active_object_get(view_layer);
+  MDeformVert *dv = ED_mesh_active_dvert_get_only(ob);
+  const int vertex_group_index = POINTER_AS_INT(arg1);
+  MDeformWeight *dw = BKE_defvert_find_index(dv, vertex_group_index);
+  dw->weight = tfp->active_vertex_weight;
+}
+
 static void view3d_panel_vgroup(const bContext *C, Panel *panel)
 {
   uiBlock *block = uiLayoutAbsoluteBlock(panel->layout);
@@ -1329,6 +1342,8 @@ static void view3d_panel_vgroup(const bContext *C, Panel *panel)
   ViewLayer *view_layer = CTX_data_view_layer(C);
   BKE_view_layer_synced_ensure(scene, view_layer);
   Object *ob = BKE_view_layer_active_object_get(view_layer);
+  View3D *v3d = CTX_wm_view3d(C);
+  TransformProperties *tfp = v3d_transform_props_ensure(v3d);
 
   MDeformVert *dv;
 
@@ -1402,6 +1417,7 @@ static void view3d_panel_vgroup(const bContext *C, Panel *panel)
 
           /* The weight group value */
           /* To be reworked still */
+          tfp->active_vertex_weight = dw->weight;
           but = uiDefButF(block,
                           UI_BTYPE_NUM,
                           B_VGRP_PNL_EDIT_SINGLE + i,
@@ -1410,7 +1426,7 @@ static void view3d_panel_vgroup(const bContext *C, Panel *panel)
                           yco,
                           (x = UI_UNIT_X * 4),
                           UI_UNIT_Y,
-                          &dw->weight,
+                          &tfp->active_vertex_weight,
                           0.0,
                           1.0,
                           0,
@@ -1419,6 +1435,7 @@ static void view3d_panel_vgroup(const bContext *C, Panel *panel)
           UI_but_number_step_size_set(but, 1);
           UI_but_number_precision_set(but, 3);
           UI_but_drawflag_enable(but, UI_BUT_TEXT_LEFT);
+          UI_but_func_set(but, update_active_vertex_weight, POINTER_FROM_INT(i), NULL);
           if (locked) {
             lock_count++;
           }

commit c629c99739c779d83af30c6fb84d10a162396833

Fix #111273: multiple vertex weight sliders show the same value
This was broken by d5b7c0d760. There I somehow completely missed that the
panel would not only show the active vertex weights, but the weights in all vertex
groups the vertex is part of.

Pull Request: https://projects.blender.org/blender/blender/pulls/111421

source/blender/editors/space_view3d/space_view3d.cc
@@ -336,8 +336,9 @@ static void view3d_free(SpaceLink *sl)
 
   MEM_SAFE_FREE(vd->runtime.local_stats);
 
-  if (vd->runtime.properties_storage) {
-    MEM_freeN(vd->runtime.properties_storage);
+  if (vd->runtime.properties_storage_free) {
+    vd->runtime.properties_storage_free(vd->runtime.properties_storage);
+    vd->runtime.properties_storage_free = nullptr;
   }
 
   if (vd->shading.prop) {

source/blender/editors/space_view3d/view3d_buttons.cc
@@ -103,7 +103,7 @@ struct TransformProperties {
   float ob_dims_orig[3];
   float ob_scale_orig[3];
   float ob_dims[3];
-  float active_vertex_weight;
+  blender::Vector<float> vertex_weights;
   /* Floats only (treated as an array). */
   TransformMedian ve_median, median;
   bool tag_for_update;
@@ -278,8 +278,15 @@ static void apply_scale_factor_clamp(float *val,
 static TransformProperties *v3d_transform_props_ensure(View3D *v3d)
 {
   if (v3d->runtime.properties_storage == nullptr) {
-    v3d->runtime.properties_storage = MEM_callocN(sizeof(TransformProperties),
-                                                  "TransformProperties");
+    TransformProperties *tfp = static_cast<TransformProperties *>(
+        MEM_callocN(sizeof(TransformProperties), "TransformProperties"));
+    /* Construct C++ structures in otherwise zero initialized struct. */
+    new (tfp) TransformProperties();
+
+    v3d->runtime.properties_storage = tfp;
+    v3d->runtime.properties_storage_free = [](void *properties_storage) {
+      MEM_delete(static_cast<TransformProperties *>(properties_storage));
+    };
   }
   return static_cast<TransformProperties *>(v3d->runtime.properties_storage);
 }
@@ -1334,7 +1341,7 @@ static void update_active_vertex_weight(bContext *C, void *arg1, void * /*arg2*/
   MDeformVert *dv = ED_mesh_active_dvert_get_only(ob);
   const int vertex_group_index = POINTER_AS_INT(arg1);
   MDeformWeight *dw = BKE_defvert_find_index(dv, vertex_group_index);
-  dw->weight = tfp->active_vertex_weight;
+  dw->weight = tfp->vertex_weights[vertex_group_index];
 }
 
 static void view3d_panel_vgroup(const bContext *C, Panel *panel)
@@ -1382,6 +1389,8 @@ static void view3d_panel_vgroup(const bContext *C, Panel *panel)
     vgroup_validmap = BKE_object_defgroup_subset_from_select_type(
         ob, subset_type, &vgroup_tot, &subset_count);
     const ListBase *defbase = BKE_object_defgroup_list(ob);
+    const int vgroup_num = BLI_listbase_count(defbase);
+    tfp->vertex_weights.resize(vgroup_num);
 
     for (i = 0, dg = static_cast<bDeformGroup *>(defbase->first); dg; i++, dg = dg->next) {
       bool locked = (dg->flag & DG_LOCK_WEIGHT) != 0;
@@ -1419,7 +1428,8 @@ static void view3d_panel_vgroup(const bContext *C, Panel *panel)
 
           /* The weight group value */
           /* To be reworked still */
-          tfp->active_vertex_weight = dw->weight;
+          float &vertex_weight = tfp->vertex_weights[i];
+          vertex_weight = dw->weight;
           but = uiDefButF(block,
                           UI_BTYPE_NUM,
                           B_VGRP_PNL_EDIT_SINGLE + i,
@@ -1428,7 +1438,7 @@ static void view3d_panel_vgroup(const bContext *C, Panel *panel)
                           yco,
                           (x = UI_UNIT_X * 4),
                           UI_UNIT_Y,
-                          &tfp->active_vertex_weight,
+                          &vertex_weight,
                           0.0,
                           1.0,
                           0,

source/blender/makesdna/DNA_view3d_types.h
@@ -252,6 +252,7 @@ typedef enum eHandleDisplay {
 typedef struct View3D_Runtime {
   /** Nkey panel stores stuff here. */
   void *properties_storage;
+  void (*properties_storage_free)(void *properties_storage);
   /** Runtime only flags. */
   int flag;
 