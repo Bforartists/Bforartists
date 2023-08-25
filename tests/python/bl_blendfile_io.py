Bforartists/Bforartists

commit 51b71907d6a3121df7eeee985cf11ce37342f020

Readfile: Replace the 'expand' specific code by generic usage of foreach_id.
The `expand` callback is 'trivial' to replace, since it is only iterating
over ID pointers and calling a callback.

The only change in behavior here is that some pointers that were not
processed previously will now be.

In practice this is not expected to have any real effect (usually
the IDs used by these pointers would have been expanded through other
usages anyway). But it may solve a few corner cases, undocumented issues
though.

Part of implementing #105134: Removal of readfile's lib_link & expand code.

source/blender/blenloader/intern/readfile.cc
@@ -597,11 +597,6 @@ struct BlendLibReader {
   Main *main;
 };
 
-struct BlendExpander {
-  FileData *fd;
-  Main *main;
-};
-
 static void switch_endian_bh4(BHead4 *bhead)
 {
   /* the ID_.. codes */
@@ -3933,6 +3928,11 @@ static ID *library_id_is_yet_read(FileData *fd, Main *mainvar, BHead *bhead)
 /** \name Library Linking (expand pointers)
  * \{ */
 
+struct BlendExpander {
+  FileData *fd;
+  Main *main;
+};
+
 static void expand_doit_library(void *fdhandle, Main *mainvar, void *old)
 {
   FileData *fd = static_cast<FileData *>(fdhandle);
@@ -4058,53 +4058,39 @@ static void expand_doit_library(void *fdhandle, Main *mainvar, void *old)
 
 static BLOExpandDoitCallback expand_doit;
 
-static void expand_id(BlendExpander *expander, ID *id);
-
-static void expand_id_embedded_id(BlendExpander *expander, ID *id)
-{
-  /* Handle 'private IDs'. */
-  bNodeTree *nodetree = ntreeFromID(id);
-  if (nodetree != nullptr) {
-    expand_id(expander, &nodetree->id);
-    blender::bke::ntreeBlendReadExpand(expander, nodetree);
-  }
-
-  if (GS(id->name) == ID_SCE) {
-    Scene *scene = (Scene *)id;
-    if (scene->master_collection != nullptr) {
-      expand_id(expander, &scene->master_collection->id);
-      BKE_collection_blend_read_expand(expander, scene->master_collection);
-    }
-  }
-}
-
-static void expand_id(BlendExpander *expander, ID *id)
-{
-  IDP_BlendReadExpand(expander, id->properties);
-
-  if (id->override_library) {
-    BLO_expand(expander, id->override_library->reference);
-    BLO_expand(expander, id->override_library->storage);
-
-    LISTBASE_FOREACH (IDOverrideLibraryProperty *, op, &id->override_library->properties) {
-      LISTBASE_FOREACH (IDOverrideLibraryPropertyOperation *, opop, &op->operations) {
-        BLO_expand(expander, opop->subitem_reference_id);
-        BLO_expand(expander, opop->subitem_local_id);
-      }
-    }
-  }
-
-  AnimData *adt = BKE_animdata_from_id(id);
-  if (adt != nullptr) {
-    BKE_animdata_blend_read_expand(expander, adt);
-  }
-
-  expand_id_embedded_id(expander, id);
-}
-
 void BLO_main_expander(BLOExpandDoitCallback expand_doit_func)
 {
   expand_doit = expand_doit_func;
+}
+
+static int expand_cb(LibraryIDLinkCallbackData *cb_data)
+{
+  /* Embedded IDs are not known by lib_link code, so they would be remapped to `nullptr`. But there
+   * is no need to process them anyway, asthey are already handled during the 'read_data' phase.
+   */
+  if (cb_data->cb_flag & (IDWALK_CB_EMBEDDED | IDWALK_CB_EMBEDDED_NOT_OWNING)) {
+    return IDWALK_RET_NOP;
+  }
+
+  /* Explicitely requested to be ignored during readfile processing. Means the read_data code
+   * already handled this pointer. Typically, the 'owner_id' pointer of an embedded ID. */
+  if (cb_data->cb_flag & IDWALK_CB_READFILE_IGNORE) {
+    return IDWALK_RET_NOP;
+  }
+
+  /* Expand process can be re-entrant or have other complex imteractions that will not work well
+   * with loop-back pointers. Further more, processing such data should not be needed here anyway.
+   */
+  if (cb_data->cb_flag & (IDWALK_CB_LOOPBACK)) {
+    return IDWALK_RET_NOP;
+  }
+
+  BlendExpander *expander = static_cast<BlendExpander *>(cb_data->user_data);
+  ID *id = *(cb_data->id_pointer);
+
+  expand_doit(expander->fd, expander->main, id);
+
+  return IDWALK_RET_NOP;
 }
 
 void BLO_expand_main(void *fdhandle, Main *mainvar)
@@ -4121,12 +4107,14 @@ void BLO_expand_main(void *fdhandle, Main *mainvar)
         continue;
       }
 
-      expand_id(&expander, id_iter);
-
-      const IDTypeInfo *id_type = BKE_idtype_get_info_from_id(id_iter);
-      if (id_type->blend_read_expand != nullptr) {
-        id_type->blend_read_expand(&expander, id_iter);
-      }
+      /* Original (current) ID pointer can be considered as valid, but _not_ its own pointers to
+       * other IDs - the already loaded ones will be valid, but the yet-to-be-read ones will not.
+       * Expanding should _not_ require processing of UI ID pointers.
+       * Expanding should never modify ID pointers themselves.
+       * Handling of DNA deprecated data should never be needed in undo case. */
+      const int flag = IDWALK_READONLY | IDWALK_NO_ORIG_POINTERS_ACCESS |
+                       ((fd->flags & FD_FLAGS_IS_MEMFILE) ? 0 : IDWALK_DO_DEPRECATED_POINTERS);
+      BKE_library_foreach_ID_link(nullptr, id_iter, expand_cb, &expander, flag);
 
       do_it = true;
       id_iter->tag &= ~LIB_TAG_NEED_EXPAND;
@@ -4977,11 +4965,6 @@ Main *BLO_read_lib_get_main(BlendLibReader *reader)
 BlendFileReadReport *BLO_read_lib_reports(BlendLibReader *reader)
 {
   return reader->fd->reports;
-}
-
-void BLO_expand_id(BlendExpander *expander, ID *id)
-{
-  expand_doit(expander->fd, expander->main, id);
 }
 
 /** \} */

commit 1cfbf640282df05661d17c095d1cb444d72e6286

Fix #111488: Copying object crashes Blender after recent refactor.
Caused by 51b71907d6.

Trivial mistake, would have been caught by a basic test, so also added
said test for the 'write partial' feature.

source/blender/blenloader/BLO_readfile.h
@@ -497,7 +497,7 @@ void BLO_main_expander(BLOExpandDoitCallback expand_doit_func);
  * Loop over all ID data in Main to mark relations.
  * Set (id->tag & LIB_TAG_NEED_EXPAND) to mark expanding. Flags get cleared after expanding.
  *
- * \param fdhandle: usually file-data, or own handle.
+ * \param fdhandle: usually file-data, or own handle. May be nullptr.
  * \param mainvar: the Main database to expand.
  */
 void BLO_expand_main(void *fdhandle, struct Main *mainvar);

source/blender/blenloader/intern/readfile.cc
@@ -4148,7 +4148,9 @@ void BLO_expand_main(void *fdhandle, Main *mainvar)
        * Expanding should never modify ID pointers themselves.
        * Handling of DNA deprecated data should never be needed in undo case. */
       const int flag = IDWALK_READONLY | IDWALK_NO_ORIG_POINTERS_ACCESS |
-                       ((fd->flags & FD_FLAGS_IS_MEMFILE) ? 0 : IDWALK_DO_DEPRECATED_POINTERS);
+                       ((!fd || (fd->flags & FD_FLAGS_IS_MEMFILE)) ?
+                            0 :
+                            IDWALK_DO_DEPRECATED_POINTERS);
       BKE_library_foreach_ID_link(nullptr, id_iter, expand_cb, &expander, flag);
 
       do_it = true;

tests/python/bl_blendfile_io.py
@@ -49,6 +49,52 @@ class TestBlendFileSaveLoadBasic(TestHelper):
         assert orig_data == read_data
 
 
+class TestBlendFileSavePartial(TestHelper):
+    OBJECT_MESH_NAME = "ObjectMesh"
+    OBJECT_MATERIAL_NAME = "ObjectMaterial"
+    OBJECT_NAME = "Object"
+    UNUSED_MESH_NAME = "UnusedMesh"
+
+    def __init__(self, args):
+        self.args = args
+
+    def test_save_load(self):
+        bpy.ops.wm.read_homefile(use_empty=True, use_factory_startup=True)
+
+        ob_mesh = bpy.data.meshes.new(self.OBJECT_MESH_NAME)
+        ob_material = bpy.data.materials.new(self.OBJECT_MATERIAL_NAME)
+        ob_mesh.materials.append(ob_material)
+        ob = bpy.data.objects.new(self.OBJECT_NAME, object_data=ob_mesh)
+        bpy.context.collection.objects.link(ob)
+
+        unused_mesh = bpy.data.meshes.new(self.UNUSED_MESH_NAME)
+        unused_mesh.materials.append(ob_material)
+
+        assert ob_mesh.users == 1
+        assert ob_material.users == 2
+        assert ob.users == 1
+        assert unused_mesh.users == 0
+
+        output_dir = self.args.output_dir
+        self.ensure_path(output_dir)
+
+        # Take care to keep the name unique so multiple test jobs can run at once.
+        output_path = os.path.join(output_dir, "blendfile_io_partial.blend")
+
+        bpy.data.libraries.write(filepath=output_path, datablocks={ob, unused_mesh}, fake_user=False)
+        bpy.ops.wm.open_mainfile(filepath=output_path, load_ui=False)
+
+        assert self.OBJECT_MESH_NAME in bpy.data.meshes
+        assert self.OBJECT_MATERIAL_NAME in bpy.data.materials
+        assert self.OBJECT_NAME in bpy.data.objects
+        assert self.UNUSED_MESH_NAME in bpy.data.meshes
+
+        assert bpy.data.meshes[self.OBJECT_MESH_NAME].users == 1
+        assert bpy.data.materials[self.OBJECT_MATERIAL_NAME].users == 2
+        assert bpy.data.objects[self.OBJECT_NAME].users == 0
+        assert bpy.data.meshes[self.UNUSED_MESH_NAME].users == 0
+
+
 # NOTE: Technically this should rather be in `bl_id_management.py` test, but that file uses `unittest` module,
 #       which makes mixing it with tests system used here and passing extra parameters complicated.
 #       Since the main effect of 'RUNTIME' ID tag is on file save, it can as well be here for now.
@@ -145,6 +191,7 @@ class TestIdRuntimeTag(TestHelper):
 
 TESTS = (
     TestBlendFileSaveLoadBasic,
+    TestBlendFileSavePartial,
 
     TestIdRuntimeTag,
 )