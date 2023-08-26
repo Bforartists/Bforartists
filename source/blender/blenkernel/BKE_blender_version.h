98f300ed33cc5b37ceeef842a3ffd002ec34249a
Draise <ondraise15@hotmail.com>
2023-08-25 19:29:53 -0500

    Release 3.6.2 #3709

--- source/blender/blenkernel/BKE_blender_version.h
+++ source/blender/blenkernel/BKE_blender_version.h
@@ -30,7 +30,7 @@ extern "C" {
 /* Bforartists major and minor version. */
 #define BFORARTISTS_VERSION 36
 /* Bforartists patch version for bugfix releases. */
-#define BFORARTISTS_VERSION_PATCH 1
+#define BFORARTISTS_VERSION_PATCH 2
 /** Bforartists release cycle stage: alpha/beta/rc/release. */
 #define BFORARTISTS_VERSION_CYCLE release
 
