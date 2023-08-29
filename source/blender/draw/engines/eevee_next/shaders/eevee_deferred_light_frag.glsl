Bforartists/Bforartists

commit 265c2c2b92b4e5fdaf8f1e7467a1dcb1a4177f2c

EEVEE-Next: Ray-tracing Denoise Pipeline
This is a full rewrite of the raytracing denoise pipeline. It uses the
same principle as before but now uses compute shaders for every stages
and a tile base approach. More aggressive filtering is needed since we
are moving towards having no prefiltered screen radiance buffer. Thus
we introduce a temporal denoise and a bilateral denoise stage to the
denoising. These are optionnal and can be disabled.

Note that this patch does not include any tracing part and only samples
the reflection probes. It is focused on denoising only. Tracing will
come in another PR.

The motivation for this is that having hardware raytracing support
means we can't prefilter the radiance in screen space so we have to
have better denoising. Also this means we can have better surface
appearance with support for other BxDF model than GGX. Also GGX support
is improved.

Technically, the new denoising fixes some implementation mistake the
old pipeline did. It separates all 3 stages (spatial, temporal,
bilateral) and use random sampling for all stages hoping to create
a noisy enough (but still stable) output so that the TAA soaks the
remaining noise. However that's not always the case. Depending on the
nature of the scene, the input can be very high frequency and might
create lots of flickering. That why another solution needs to be found
for the higher roughness material as denoising them becomes expensive
and low quality.

Pull Request: https://projects.blender.org/blender/blender/pulls/110117

source/blender/draw/engines/eevee_next/shaders/eevee_deferred_light_frag.glsl
@@ -16,7 +16,7 @@ void main()
 {
   ivec2 texel = ivec2(gl_FragCoord.xy);
 
-  float depth = texelFetch(hiz_tx, ivec2(gl_FragCoord.xy), 0).r;
+  float depth = texelFetch(hiz_tx, texel, 0).r;
   vec3 P = get_world_space_from_depth(uvcoordsvar.xy, depth);
 
   /* TODO(fclem): High precision derivative. */
@@ -38,6 +38,11 @@ void main()
   diffuse_data.sss_id = 0u;
   float thickness = 0.0;
 
+  ClosureRefraction refraction_data;
+  refraction_data.N = diffuse_data.N;
+  refraction_data.roughness = gbuffer_1_packed.z;
+  refraction_data.ior = 0.0; /* Not needed. */
+
   bool is_refraction = gbuffer_is_refraction(gbuffer_1_packed);
   if (is_refraction) {
     /* Still evaluate the diffuse light so that dithered SSS / Refraction combination still
@@ -52,12 +57,14 @@ void main()
   }
 
   vec3 diffuse_light = vec3(0.0);
-  vec3 reflection_light = vec3(0.0);
-  float shadow = 1.0;
-
 #ifdef DO_REFLECTION_PROBES
-  reflection_probes_eval(reflection_data, P, V, reflection_light);
+  vec3 reflection_light = imageLoad(indirect_reflection_img, texel).rgb;
+  vec3 refraction_light = imageLoad(indirect_refraction_img, texel).rgb;
+#else
+  vec3 reflection_light = vec3(0.0);
+  vec3 refraction_light = vec3(0.0);
 #endif
+  float shadow = 1.0;
 
   lightprobe_eval(diffuse_data, reflection_data, P, Ng, V, diffuse_light, reflection_light);
 
@@ -72,35 +79,32 @@ void main()
              reflection_light,
              shadow);
 
+  /* Apply color and output lighting to render-passes. */
+  vec4 color_0_packed = texelFetch(gbuffer_color_tx, ivec3(texel, 0), 0);
+  vec4 color_1_packed = texelFetch(gbuffer_color_tx, ivec3(texel, 1), 0);
+
+  reflection_data.color = gbuffer_color_unpack(color_0_packed);
+  refraction_data.color = is_refraction ? gbuffer_color_unpack(color_1_packed) : vec3(0.0);
+  diffuse_data.color = is_refraction ? vec3(0.0) : gbuffer_color_unpack(color_1_packed);
+
+  /* Light passes. */
+  if (rp_buf.diffuse_light_id >= 0) {
+    imageStore(rp_color_img, ivec3(texel, rp_buf.diffuse_light_id), vec4(diffuse_light, 1.0));
+  }
+  if (rp_buf.specular_light_id >= 0) {
+    vec3 specular_light = reflection_light + refraction_light;
+    imageStore(rp_color_img, ivec3(texel, rp_buf.specular_light_id), vec4(specular_light, 1.0));
+  }
+  if (rp_buf.shadow_id >= 0) {
+    imageStore(rp_value_img, ivec3(texel, rp_buf.shadow_id), vec4(shadow));
+  }
+
   if (is_last_eval_pass) {
-    /* Apply color and output lighting to render-passes. */
-    vec4 color_0_packed = texelFetch(gbuffer_color_tx, ivec3(texel, 0), 0);
-    vec4 color_1_packed = texelFetch(gbuffer_color_tx, ivec3(texel, 1), 0);
-
-    reflection_data.color = gbuffer_color_unpack(color_0_packed);
-    diffuse_data.color = gbuffer_color_unpack(color_1_packed);
-
-    if (is_refraction) {
-      diffuse_data.color = vec3(0.0);
-    }
-
-    /* Light passes. */
-    if (rp_buf.diffuse_light_id >= 0) {
-      imageStore(rp_color_img, ivec3(texel, rp_buf.diffuse_light_id), vec4(diffuse_light, 1.0));
-    }
-    if (rp_buf.specular_light_id >= 0) {
-      imageStore(
-          rp_color_img, ivec3(texel, rp_buf.specular_light_id), vec4(reflection_light, 1.0));
-    }
-    if (rp_buf.shadow_id >= 0) {
-      imageStore(rp_value_img, ivec3(texel, rp_buf.shadow_id), vec4(shadow));
-    }
-    /** NOTE: AO is done on its own pass. */
-
     diffuse_light *= diffuse_data.color;
     reflection_light *= reflection_data.color;
+    refraction_light *= refraction_data.color;
     /* Add radiance to combined pass. */
-    out_radiance = vec4(diffuse_light + reflection_light, 0.0);
+    out_radiance = vec4(diffuse_light + reflection_light + refraction_light, 0.0);
     out_transmittance = vec4(1.0);
   }
   else {
@@ -110,35 +114,6 @@ void main()
     /* Output object ID for sub-surface screen space processing. */
     diffuse_radiance.w = gbuffer_object_id_f16_pack(diffuse_data.sss_id);
     imageStore(out_diffuse_light_img, texel, diffuse_radiance);
-    imageStore(out_specular_light_img, texel, vec4(reflection_light, 0.0));
+    imageStore(out_specular_light_img, texel, vec4(reflection_light + reflection_light, 0.0));
   }
-
-  /* Apply color and output lighting to render-passes. */
-  vec4 color_0_packed = texelFetch(gbuffer_color_tx, ivec3(texel, 0), 0);
-  vec4 color_1_packed = texelFetch(gbuffer_color_tx, ivec3(texel, 1), 0);
-
-  reflection_data.color = gbuffer_color_unpack(color_0_packed);
-  diffuse_data.color = gbuffer_color_unpack(color_1_packed);
-
-  if (is_refraction) {
-    diffuse_data.color = vec3(0.0);
-  }
-
-  /* Light passes. */
-  if (rp_buf.diffuse_light_id >= 0) {
-    imageStore(rp_color_img, ivec3(texel, rp_buf.diffuse_light_id), vec4(diffuse_light, 1.0));
-  }
-  if (rp_buf.specular_light_id >= 0) {
-    imageStore(rp_color_img, ivec3(texel, rp_buf.specular_light_id), vec4(reflection_light, 1.0));
-  }
-  if (rp_buf.shadow_id >= 0) {
-    imageStore(rp_value_img, ivec3(texel, rp_buf.shadow_id), vec4(shadow));
-  }
-  /* TODO: AO. */
-
-  diffuse_light *= diffuse_data.color;
-  reflection_light *= reflection_data.color;
-  /* Add radiance to combined pass. */
-  out_radiance = vec4(diffuse_light + reflection_light, 0.0);
-  out_transmittance = vec4(1.0);
 }

scripts/startup/bl_ui/properties_render.py
@@ -459,6 +459,156 @@ class RENDER_PT_eevee_screen_space_reflections(RenderButtonsPanel, Panel):
         col.prop(props, "ssr_firefly_fac")
 
 
+class RENDER_PT_eevee_next_raytracing(RenderButtonsPanel, Panel):
+    bl_label = "Raytracing"
+    bl_options = {'DEFAULT_CLOSED'}
+    COMPAT_ENGINES = {'BLENDER_EEVEE_NEXT'}
+
+    @classmethod
+    def poll(cls, context):
+        return (context.engine in cls.COMPAT_ENGINES)
+
+    def draw(self, context):
+        layout = self.layout
+        layout.use_property_split = True
+
+        scene = context.scene
+        props = scene.eevee
+
+        layout.prop(props, "ray_tracing_method", text="Method")
+        layout.prop(props, "ray_split_settings", text="Settings", expand=True)
+
+
+class EeveeRaytracingOptionsPanel(RenderButtonsPanel, Panel):
+    bl_label = "Reflection"
+    bl_options = {'DEFAULT_CLOSED'}
+    bl_parent_id = "RENDER_PT_eevee_next_raytracing"
+    COMPAT_ENGINES = {'BLENDER_EEVEE_NEXT'}
+
+    @classmethod
+    def poll(cls, context):
+        return (context.engine in cls.COMPAT_ENGINES)
+
+    def draw_internal(self, context, props):
+        layout = self.layout
+        layout.use_property_split = True
+
+        scene = context.scene
+        eevee = scene.eevee
+
+        layout.prop(props, "resolution_scale")
+        layout.prop(props, "sample_clamp")
+
+
+class EeveeRaytracingScreenOption(RenderButtonsPanel, Panel):
+    bl_label = "Screen Tracing"
+    bl_options = {'DEFAULT_CLOSED'}
+    COMPAT_ENGINES = {'BLENDER_EEVEE_NEXT'}
+
+    @classmethod
+    def poll(cls, context):
+        use_screen_trace = (context.scene.eevee.ray_tracing_method == 'SCREEN')
+        return (context.engine in cls.COMPAT_ENGINES) and use_screen_trace
+
+    def draw_internal(self, props):
+        layout = self.layout
+        layout.use_property_split = True
+
+        layout.prop(props, "screen_trace_quality", text="Precision")
+        layout.prop(props, "screen_trace_thickness", text="Thickness")
+
+
+class EeveeRaytracingDenoisePanel(RenderButtonsPanel, Panel):
+    bl_label = "Denoising"
+    bl_options = {'DEFAULT_CLOSED'}
+    COMPAT_ENGINES = {'BLENDER_EEVEE_NEXT'}
+
+    @classmethod
+    def poll(cls, context):
+        return (context.engine in cls.COMPAT_ENGINES)
+
+    def draw_header_internal(self, props):
+        self.layout.prop(props, "use_denoise", text="")
+
+    def draw_internal(self, props):
+        layout = self.layout
+        layout.use_property_split = True
+
+        col = layout.column()
+        col.active = props.use_denoise
+        col.prop(props, "denoise_spatial")
+
+        col = layout.column()
+        col.active = props.denoise_spatial
+        col.prop(props, "denoise_temporal")
+
+        col = layout.column()
+        col.active = props.denoise_spatial and props.denoise_temporal
+        col.prop(props, "denoise_bilateral")
+
+
+class RENDER_PT_eevee_next_raytracing_reflection(EeveeRaytracingOptionsPanel):
+    #NOTE: Label is drawn by draw_header
+    bl_label = ""
+    bl_parent_id = "RENDER_PT_eevee_next_raytracing"
+
+    def draw_header(self, context):
+        layout = self.layout
+        if context.scene.eevee.ray_split_settings == "UNIFIED":
+            layout.label(text="Reflection & Refraction")
+        else:
+            layout.label(text="Reflection")
+
+    def draw(self, context):
+        self.draw_internal(context, context.scene.eevee.reflection_options)
+
+
+class RENDER_PT_eevee_next_screen_trace_reflection(EeveeRaytracingScreenOption):
+    bl_parent_id = "RENDER_PT_eevee_next_raytracing_reflection"
+
+    def draw(self, context):
+        self.draw_internal(context.scene.eevee.reflection_options)
+
+
+class RENDER_PT_eevee_next_denoise_reflection(EeveeRaytracingDenoisePanel):
+    bl_parent_id = "RENDER_PT_eevee_next_raytracing_reflection"
+
+    def draw_header(self, context):
+        self.draw_header_internal(context.scene.eevee.reflection_options)
+
+    def draw(self, context):
+        self.draw_internal(context.scene.eevee.reflection_options)
+
+
+class RENDER_PT_eevee_next_raytracing_refraction(EeveeRaytracingOptionsPanel):
+    bl_label = "Refraction"
+    bl_parent_id = "RENDER_PT_eevee_next_raytracing"
+
+    @classmethod
+    def poll(cls, context):
+        return (context.scene.eevee.ray_split_settings == "SPLIT")
+
+    def draw(self, context):
+        self.draw_internal(context, context.scene.eevee.refraction_options)
+
+
+class RENDER_PT_eevee_next_screen_trace_refraction(EeveeRaytracingScreenOption):
+    bl_parent_id = "RENDER_PT_eevee_next_raytracing_refraction"
+
+    def draw(self, context):
+        self.draw_internal(context.scene.eevee.refraction_options)
+
+
+class RENDER_PT_eevee_next_denoise_refraction(EeveeRaytracingDenoisePanel):
+    bl_parent_id = "RENDER_PT_eevee_next_raytracing_refraction"
+
+    def draw_header(self, context):
+        self.draw_header_internal(context.scene.eevee.refraction_options)
+
+    def draw(self, context):
+        self.draw_internal(context.scene.eevee.refraction_options)
+
+
 class RENDER_PT_eevee_shadows(RenderButtonsPanel, Panel):
     bl_label = "Shadows"
     bl_options = {'DEFAULT_CLOSED'}
@@ -961,6 +1111,13 @@ classes = (
     RENDER_PT_eevee_next_depth_of_field,
     RENDER_PT_eevee_subsurface_scattering,
     RENDER_PT_eevee_screen_space_reflections,
+    RENDER_PT_eevee_next_raytracing,
+    RENDER_PT_eevee_next_raytracing_reflection,
+    RENDER_PT_eevee_next_raytracing_refraction,
+    RENDER_PT_eevee_next_screen_trace_reflection,
+    RENDER_PT_eevee_next_screen_trace_refraction,
+    RENDER_PT_eevee_next_denoise_reflection,
+    RENDER_PT_eevee_next_denoise_refraction,
     RENDER_PT_eevee_motion_blur,
     RENDER_PT_eevee_next_motion_blur,
     RENDER_PT_motion_blur_curve,

scripts/startup/bl_ui/space_userpref.py
@@ -2454,6 +2454,7 @@ class USERPREF_PT_experimental_debugging(ExperimentalPanel, Panel):
                 ({"property": "show_asset_debug_info"}, None),
                 ({"property": "use_asset_indexing"}, None),
                 ({"property": "use_viewport_debug"}, None),
+                ({"property": "use_eevee_debug"}, None),
             ),
         )
 

source/blender/blenloader/intern/versioning_400.cc
@@ -533,5 +533,23 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
    */
   {
     /* Keep this block, even when empty. */
+
+    if (!DNA_struct_elem_find(fd->filesdna, "SceneEEVEE", "RaytraceEEVEE", "reflection_options")) {
+      LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
+        scene->eevee.reflection_options.flag = RAYTRACE_EEVEE_USE_DENOISE;
+        scene->eevee.reflection_options.denoise_stages = RAYTRACE_EEVEE_DENOISE_SPATIAL |
+                                                         RAYTRACE_EEVEE_DENOISE_TEMPORAL |
+                                                         RAYTRACE_EEVEE_DENOISE_BILATERAL;
+        scene->eevee.reflection_options.screen_trace_quality = 0.25f;
+        scene->eevee.reflection_options.screen_trace_thickness = 0.2f;
+        scene->eevee.reflection_options.sample_clamp = 10.0f;
+        scene->eevee.reflection_options.resolution_scale = 2;
+
+        scene->eevee.refraction_options = scene->eevee.reflection_options;
+
+        scene->eevee.ray_split_settings = 0;
+        scene->eevee.ray_tracing_method = RAYTRACE_EEVEE_METHOD_SCREEN;
+      }
+    }
   }
 }

source/blender/draw/CMakeLists.txt
@@ -151,6 +151,7 @@ set(SRC
   engines/eevee_next/eevee_material.cc
   engines/eevee_next/eevee_motion_blur.cc
   engines/eevee_next/eevee_pipeline.cc
+  engines/eevee_next/eevee_raytrace.cc
   engines/eevee_next/eevee_reflection_probes.cc
   engines/eevee_next/eevee_renderbuffers.cc
   engines/eevee_next/eevee_sampling.cc
@@ -464,6 +465,8 @@ set(GLSL_SRC
   engines/eevee_next/shaders/eevee_ambient_occlusion_lib.glsl
   engines/eevee_next/shaders/eevee_ambient_occlusion_pass_comp.glsl
   engines/eevee_next/shaders/eevee_attributes_lib.glsl
+  engines/eevee_next/shaders/eevee_bxdf_lib.glsl
+  engines/eevee_next/shaders/eevee_bxdf_sampling_lib.glsl
   engines/eevee_next/shaders/eevee_camera_lib.glsl
   engines/eevee_next/shaders/eevee_colorspace_lib.glsl
   engines/eevee_next/shaders/eevee_cryptomatte_lib.glsl
@@ -519,6 +522,15 @@ set(GLSL_SRC
   engines/eevee_next/shaders/eevee_motion_blur_gather_comp.glsl
   engines/eevee_next/shaders/eevee_motion_blur_lib.glsl
   engines/eevee_next/shaders/eevee_nodetree_lib.glsl
+  engines/eevee_next/shaders/eevee_ray_denoise_bilateral_comp.glsl
+  engines/eevee_next/shaders/eevee_ray_denoise_spatial_comp.glsl
+  engines/eevee_next/shaders/eevee_ray_denoise_temporal_comp.glsl
+  engines/eevee_next/shaders/eevee_ray_generate_comp.glsl
+  engines/eevee_next/shaders/eevee_ray_generate_lib.glsl
+  engines/eevee_next/shaders/eevee_ray_tile_classify_comp.glsl
+  engines/eevee_next/shaders/eevee_ray_tile_compact_comp.glsl
+  engines/eevee_next/shaders/eevee_ray_trace_screen_comp.glsl
+  engines/eevee_next/shaders/eevee_ray_trace_screen_lib.glsl
   engines/eevee_next/shaders/eevee_octahedron_lib.glsl
   engines/eevee_next/shaders/eevee_ray_types_lib.glsl
   engines/eevee_next/shaders/eevee_reflection_probe_eval_lib.glsl

source/blender/draw/engines/eevee_next/eevee_defines.hh
@@ -68,8 +68,11 @@
 #define SHADOW_VIEW_MAX 64 /* Must match DRW_VIEW_MAX. */
 
 /* Ray-tracing. */
-#define RAYTRACE_GROUP_SIZE 16
-#define RAYTRACE_MAX_TILES (16384 / RAYTRACE_GROUP_SIZE) * (16384 / RAYTRACE_GROUP_SIZE)
+#define RAYTRACE_GROUP_SIZE 8
+/* Keep this as a define to avoid shader variations. */
+#define RAYTRACE_RADIANCE_FORMAT GPU_R11F_G11F_B10F
+#define RAYTRACE_VARIANCE_FORMAT GPU_R16F
+#define RAYTRACE_TILEMASK_FORMAT GPU_R8UI
 
 /* Minimum visibility size. */
 #define LIGHTPROBE_FILTER_VIS_GROUP_SIZE 16

source/blender/draw/engines/eevee_next/eevee_instance.cc
@@ -69,6 +69,7 @@ void Instance::init(const int2 &output_res,
   film.init(output_res, output_rect);
   ambient_occlusion.init();
   velocity.init();
+  raytracing.init();
   depth_of_field.init();
   shadows.init();
   motion_blur.init();
@@ -151,6 +152,7 @@ void Instance::begin_sync()
   scene_sync();
 
   depth_of_field.sync();
+  raytracing.sync();
   motion_blur.sync();
   hiz_buffer.sync();
   main_view.sync();

source/blender/draw/engines/eevee_next/eevee_instance.hh
@@ -29,6 +29,7 @@
 #include "eevee_material.hh"
 #include "eevee_motion_blur.hh"
 #include "eevee_pipeline.hh"
+#include "eevee_raytrace.hh"
 #include "eevee_reflection_probes.hh"
 #include "eevee_renderbuffers.hh"
 #include "eevee_sampling.hh"
@@ -58,6 +59,7 @@ class Instance {
   ShadowModule shadows;
   LightModule lights;
   AmbientOcclusion ambient_occlusion;
+  RayTraceModule raytracing;
   ReflectionProbeModule reflection_probes;
   VelocityModule velocity;
   MotionBlurModule motion_blur;
@@ -113,6 +115,7 @@ class Instance {
         shadows(*this),
         lights(*this),
         ambient_occlusion(*this),
+        raytracing(*this),
         reflection_probes(*this),
         velocity(*this),
         motion_blur(*this),

commit 32686c173d5df4f84b3c689fa978e14443d5228d

Fix: EEVEE-Next: Subsurface Scattering regression
Regression caused by 265c2c2b92b4e5fdaf8f1e7467a1dcb1a4177f2c
Subsurface Scattering uses an additive pass over the radiance output,
so it's always needed even if the deferred pass is not the last one.

Pull Request: https://projects.blender.org/blender/blender/pulls/111534

source/blender/draw/engines/eevee_next/shaders/eevee_deferred_light_frag.glsl
@@ -102,15 +102,7 @@ void main()
     imageStore(rp_value_img, ivec3(texel, rp_buf.shadow_id), vec4(shadow));
   }
 
-  if (is_last_eval_pass) {
-    diffuse_light *= diffuse_data.color;
-    reflection_light *= reflection_data.color;
-    refraction_light *= refraction_data.color;
-    /* Add radiance to combined pass. */
-    out_radiance = vec4(diffuse_light + reflection_light + refraction_light, 0.0);
-    out_transmittance = vec4(1.0);
-  }
-  else {
+  if (!is_last_eval_pass) {
     /* Store lighting for next deferred pass. */
     vec4 diffuse_radiance;
     diffuse_radiance.xyz = diffuse_light;
@@ -119,4 +111,11 @@ void main()
     imageStore(out_diffuse_light_img, texel, diffuse_radiance);
     imageStore(out_specular_light_img, texel, vec4(reflection_light + reflection_light, 0.0));
   }
+
+  diffuse_light *= diffuse_data.color;
+  reflection_light *= reflection_data.color;
+  refraction_light *= refraction_data.color;
+  /* Add radiance to combined pass. */
+  out_radiance = vec4(diffuse_light + reflection_light + refraction_light, 0.0);
+  out_transmittance = vec4(1.0);
 }