Bforartists/Bforartists

commit 957fd6901a9346b49a7a163afa914a5c7e805e45

EEVEE Next: Overscan support
Add overscan support for both viewport and final renders.

Pull Request: https://projects.blender.org/blender/blender/pulls/110313

source/blender/draw/engines/eevee_next/eevee_film.cc
@@ -248,16 +248,26 @@ void Film::init(const int2 &extent, const rcti *output_rect)
       output_rect = &fallback_rect;
     }
 
+    display_offset = int2(output_rect->xmin, output_rect->ymin);
+
     FilmData data = data_;
     data.extent = int2(BLI_rcti_size_x(output_rect), BLI_rcti_size_y(output_rect));
-    data.offset = int2(output_rect->xmin, output_rect->ymin);
+    data.offset = display_offset;
     data.extent_inv = 1.0f / float2(data.extent);
-    /* Disable filtering if sample count is 1. */
-    data.filter_radius = (sampling.sample_count() == 1) ? 0.0f :
-                                                          clamp_f(scene.r.gauss, 0.0f, 100.0f);
     /* TODO(fclem): parameter hidden in experimental.
      * We need to figure out LOD bias first in order to preserve texture crispiness. */
     data.scaling_factor = 1;
+    data.render_extent = math::divide_ceil(extent, int2(data.scaling_factor));
+
+    if (inst_.camera.overscan() != 0.0f) {
+      int2 overscan = int2(inst_.camera.overscan() * math::max(UNPACK2(data.render_extent)));
+      data.render_extent += overscan * 2;
+      data.offset += overscan;
+    }
+
+    /* Disable filtering if sample count is 1. */
+    data.filter_radius = (sampling.sample_count() == 1) ? 0.0f :
+                                                          clamp_f(scene.r.gauss, 0.0f, 100.0f);
     data.cryptomatte_samples_len = inst_.view_layer->cryptomatte_levels;
 
     data.background_opacity = (scene.r.alphamode == R_ALPHAPREMUL) ? 0.0f : 1.0f;
@@ -355,9 +365,6 @@ void Film::init(const int2 &extent, const rcti *output_rect)
     data_.cryptomatte_material_id = cryptomatte_index_get(EEVEE_RENDER_PASS_CRYPTOMATTE_MATERIAL);
   }
   {
-    /* TODO(@fclem): Over-scans. */
-
-    data_.render_extent = math::divide_ceil(extent, int2(data_.scaling_factor));
     int2 weight_extent = inst_.camera.is_panoramic() ? data_.extent : int2(data_.scaling_factor);
 
     eGPUTextureFormat color_format = GPU_RGBA16F;
@@ -629,7 +636,7 @@ void Film::accumulate(const DRWView *view, GPUTexture *combined_final_tx)
       float4 clear_color = {0.0f, 0.0f, 0.0f, 0.0f};
       GPU_framebuffer_clear_color(dfbl->default_fb, clear_color);
     }
-    GPU_framebuffer_viewport_set(dfbl->default_fb, UNPACK2(data_.offset), UNPACK2(data_.extent));
+    GPU_framebuffer_viewport_set(dfbl->default_fb, UNPACK2(display_offset), UNPACK2(data_.extent));
   }
 
   update_sample_table();
@@ -661,7 +668,7 @@ void Film::display()
 
   DefaultFramebufferList *dfbl = DRW_viewport_framebuffer_list_get();
   GPU_framebuffer_bind(dfbl->default_fb);
-  GPU_framebuffer_viewport_set(dfbl->default_fb, UNPACK2(data_.offset), UNPACK2(data_.extent));
+  GPU_framebuffer_viewport_set(dfbl->default_fb, UNPACK2(display_offset), UNPACK2(data_.extent));
 
   combined_final_tx_ = inst_.render_buffers.combined_tx;
 

source/blender/draw/engines/eevee_next/eevee_film.hh
@@ -64,6 +64,7 @@ class Film {
   PassSimple cryptomatte_post_ps_ = {"Film.Cryptomatte.Post"};
 
   FilmDataBuf data_;
+  int2 display_offset;
 
   eViewLayerEEVEEPassType enabled_passes_ = eViewLayerEEVEEPassType(0);
 
@@ -92,6 +93,12 @@ class Film {
   int2 render_extent_get() const
   {
     return data_.render_extent;
+  }
+
+  /** Returns render output resolution. */
+  int2 display_extent_get() const
+  {
+    return data_.extent;
   }
 
   float2 pixel_jitter_get() const;

source/blender/draw/engines/eevee_next/shaders/eevee_film_frag.glsl
@@ -4,7 +4,7 @@
 
 void main()
 {
-  ivec2 texel_film = ivec2(gl_FragCoord.xy) - film_buf.offset;
+  ivec2 texel_film = ivec2(gl_FragCoord.xy);
   float out_depth;
 
   if (film_buf.display_only) {

scripts/startup/bl_ui/properties_render.py
@@ -858,10 +858,19 @@ class RENDER_PT_eevee_next_film(RenderButtonsPanel, Panel):
 
         scene = context.scene
         rd = scene.render
+        props = scene.eevee
 
         col = layout.column()
         col.prop(rd, "filter_size")
         col.prop(rd, "film_transparent", text="Transparent")
+
+        col = layout.column(align=False, heading="Overscan")
+        row = col.row(align=True)
+        sub = row.row(align=True)
+        sub.prop(props, "use_overscan", text="")
+        sub = sub.row(align=True)
+        sub.active = props.use_overscan
+        sub.prop(props, "overscan_size", text="")
 
 
 def draw_curves_settings(self, context):

source/blender/draw/engines/eevee_next/eevee_camera.cc
@@ -15,7 +15,9 @@
 
 #include "BKE_camera.h"
 #include "DEG_depsgraph_query.h"
+#include "ED_view3d.h"
 #include "RE_pipeline.h"
+#include "render_types.h"
 
 #include "eevee_camera.hh"
 #include "eevee_instance.hh"
@@ -70,6 +72,12 @@ void Camera::init()
     /* Light-probe baking. */
     data.type = CAMERA_PERSP;
   }
+
+  float overscan = 0.0f;
+  if ((inst_.scene->eevee.flag & SCE_EEVEE_OVERSCAN) && (inst_.drw_view || inst_.render)) {
+    overscan = inst_.scene->eevee.overscan / 100.0f;
+  }
+  overscan_changed_ = assign_if_different(overscan_, overscan);
 }
 
 void Camera::sync()
@@ -78,17 +86,15 @@ void Camera::sync()
 
   CameraData &data = data_;
 
+  data.uv_scale = float2(1.0f);
+  data.uv_bias = float2(0.0f);
+
   if (inst_.is_baking()) {
     /* Any view so that shadows and light culling works during irradiance bake. */
     draw::View &view = inst_.irradiance_cache.bake.view_z_;
     data.viewmat = view.viewmat();
     data.viewinv = view.viewinv();
     data.winmat = view.winmat();
-    data.wininv = view.wininv();
-    data.persmat = data.winmat * data.viewmat;
-    data.persinv = math::invert(data.persmat);
-    data.uv_scale = float2(1.0f);
-    data.uv_bias = float2(0.0f);
     data.type = CAMERA_ORTHO;
 
     /* \note: Follow camera parameters where distances are positive in front of the camera. */
@@ -101,36 +107,46 @@ void Camera::sync()
   else if (inst_.drw_view) {
     DRW_view_viewmat_get(inst_.drw_view, data.viewmat.ptr(), false);
     DRW_view_viewmat_get(inst_.drw_view, data.viewinv.ptr(), true);
-    DRW_view_winmat_get(inst_.drw_view, data.winmat.ptr(), false);
-    DRW_view_winmat_get(inst_.drw_view, data.wininv.ptr(), true);
-    DRW_view_persmat_get(inst_.drw_view, data.persmat.ptr(), false);
-    DRW_view_persmat_get(inst_.drw_view, data.persinv.ptr(), true);
+    if (overscan_ == 0.0f) {
+      DRW_view_winmat_get(inst_.drw_view, data.winmat.ptr(), false);
+    }
+    else {
+      rctf viewplane;
+      float clip_start;
+      float clip_end;
+      bool is_ortho = ED_view3d_viewplane_get(inst_.depsgraph,
+                                              inst_.v3d,
+                                              inst_.rv3d,
+                                              UNPACK2(inst_.film.display_extent_get()),
+                                              &viewplane,
+                                              &clip_start,
+                                              &clip_end,
+                                              nullptr);
+
+      RE_GetWindowMatrixWithOverscan(
+          is_ortho, clip_start, clip_end, viewplane, overscan_, data.winmat.ptr());
+    }
     /* TODO(fclem): Derive from rv3d instead. */
     data.uv_scale = float2(1.0f);
     data.uv_bias = float2(0.0f);
   }
   else if (inst_.render) {
-    /* TODO(@fclem): Over-scan. */
-    // RE_GetCameraWindowWithOverscan(inst_.render->re, g_data->overscan, data.winmat);
-    RE_GetCameraWindow(inst_.render->re, camera_eval, data.winmat.ptr());
     RE_GetCameraModelMatrix(inst_.render->re, camera_eval, data.viewinv.ptr());
-    invert_m4_m4(data.viewmat.ptr(), data.viewinv.ptr());
-    invert_m4_m4(data.wininv.ptr(), data.winmat.ptr());
-    mul_m4_m4m4(data.persmat.ptr(), data.winmat.ptr(), data.viewmat.ptr());
-    invert_m4_m4(data.persinv.ptr(), data.persmat.ptr());
-    data.uv_scale = float2(1.0f);
-    data.uv_bias = float2(0.0f);
+    data.viewmat = math::invert(data.viewinv);
+    RE_GetCameraWindow(inst_.render->re, camera_eval, data.winmat.ptr());
+    if (overscan_ != 0.0f) {
+      RE_GetCameraWindowWithOverscan(inst_.render->re, overscan_, data.winmat.ptr());
+    }
   }
   else {
     data.viewmat = float4x4::identity();
     data.viewinv = float4x4::identity();
     data.winmat = math::projection::perspective(-0.1f, 0.1f, -0.1f, 0.1f, 0.1f, 1.0f);
-    data.wininv = math::invert(data.winmat);
-    data.persmat = data.winmat * data.viewmat;
-    data.persinv = math::invert(data.persmat);
-    data.uv_scale = float2(1.0f);
-    data.uv_bias = float2(0.0f);
   }
+
+  data.wininv = math::invert(data.winmat);
+  data.persmat = data.winmat * data.viewmat;
+  data.persinv = math::invert(data.persmat);
 
   if (camera_eval && camera_eval->type == OB_CAMERA) {
     const ::Camera *cam = reinterpret_cast<const ::Camera *>(camera_eval->data);

source/blender/draw/engines/eevee_next/eevee_camera.hh
@@ -100,6 +100,9 @@ class Camera {
     float radius;
   } bound_sphere;
 
+  float overscan_;
+  bool overscan_changed_;
+
  public:
   Camera(Instance &inst) : inst_(inst){};
   ~Camera(){};
@@ -146,6 +149,14 @@ class Camera {
   const float &bound_radius() const
   {
     return bound_sphere.radius;
+  }
+  float overscan() const
+  {
+    return overscan_;
+  }
+  bool overscan_changed() const
+  {
+    return overscan_changed_;
   }
 
  private:

source/blender/draw/engines/eevee_next/eevee_motion_blur.cc
@@ -125,7 +125,7 @@ void MotionBlurModule::sync()
 {
   /* Disable motion blur in viewport when changing camera projection type.
    * Avoids really high velocities. */
-  if (inst_.velocity.camera_changed_projection()) {
+  if (inst_.velocity.camera_changed_projection() || inst_.camera.overscan_changed()) {
     motion_blur_fx_enabled_ = false;
   }
 

source/blender/render/RE_pipeline.h
@@ -468,6 +468,13 @@ void RE_GetCameraModelMatrix(const struct Render *re,
                              const struct Object *camera,
                              float r_modelmat[4][4]);
 
+void RE_GetWindowMatrixWithOverscan(bool is_ortho,
+                                    float clip_start,
+                                    float clip_end,
+                                    rctf viewplane,
+                                    float overscan,
+                                    float r_winmat[4][4]);
+
 struct Scene *RE_GetScene(struct Render *re);
 void RE_SetScene(struct Render *re, struct Scene *sce);
 

commit 2af01b3e463f42c023c34b414b7d54951c8ae84d

Fix: EEVEE-Next: Render Region
Fix render regions and make them work with overscan:
- Clarify and improve the naming used by the Film module.
- Convert Display texel coordinates to Film texel coordinates before
  calling `film_process_data`, and convert from Film texel coordinates to
  Render texel coordinates in `film_sample_get`.
- Returns the actual display extent (and not the film extent) in
 `Film::display_extent_get` so the overscan camera matrix is computed
  correctly when using render regions.

Regression caused by 957fd6901a9346b49a7a163afa914a5c7e805e45.

Pull Request: https://projects.blender.org/blender/blender/pulls/111691

source/blender/draw/engines/eevee_next/eevee_film.cc
@@ -5,7 +5,7 @@
 /** \file
  * \ingroup eevee
  *
- * A film is a full-screen buffer (usually at output extent)
+ * A film is a buffer (usually at display extent)
  * that will be able to accumulate sample in any distorted camera_type
  * using a pixel filter.
  *
@@ -147,6 +147,7 @@ void Film::sync_mist()
 inline bool operator==(const FilmData &a, const FilmData &b)
 {
   return (a.extent == b.extent) && (a.offset == b.offset) &&
+         (a.render_extent == b.render_extent) && (a.render_offset == b.render_offset) &&
          (a.filter_radius == b.filter_radius) && (a.scaling_factor == b.scaling_factor) &&
          (a.background_opacity == b.background_opacity);
 }
@@ -248,21 +249,22 @@ void Film::init(const int2 &extent, const rcti *output_rect)
       output_rect = &fallback_rect;
     }
 
-    display_offset = int2(output_rect->xmin, output_rect->ymin);
+    display_extent = extent;
 
     FilmData data = data_;
     data.extent = int2(BLI_rcti_size_x(output_rect), BLI_rcti_size_y(output_rect));
-    data.offset = display_offset;
+    data.offset = int2(output_rect->xmin, output_rect->ymin);
     data.extent_inv = 1.0f / float2(data.extent);
     /* TODO(fclem): parameter hidden in experimental.
      * We need to figure out LOD bias first in order to preserve texture crispiness. */
     data.scaling_factor = 1;
     data.render_extent = math::divide_ceil(extent, int2(data.scaling_factor));
+    data.render_offset = data.offset;
 
     if (inst_.camera.overscan() != 0.0f) {
       int2 overscan = int2(inst_.camera.overscan() * math::max(UNPACK2(data.render_extent)));
       data.render_extent += overscan * 2;
-      data.offset += overscan;
+      data.render_offset += overscan;
     }
 
     /* Disable filtering if sample count is 1. */
@@ -636,7 +638,7 @@ void Film::accumulate(const DRWView *view, GPUTexture *combined_final_tx)
       float4 clear_color = {0.0f, 0.0f, 0.0f, 0.0f};
       GPU_framebuffer_clear_color(dfbl->default_fb, clear_color);
     }
-    GPU_framebuffer_viewport_set(dfbl->default_fb, UNPACK2(display_offset), UNPACK2(data_.extent));
+    GPU_framebuffer_viewport_set(dfbl->default_fb, UNPACK2(data.offset), UNPACK2(data_.extent));
   }
 
   update_sample_table();
@@ -668,7 +670,7 @@ void Film::display()
 
   DefaultFramebufferList *dfbl = DRW_viewport_framebuffer_list_get();
   GPU_framebuffer_bind(dfbl->default_fb);
-  GPU_framebuffer_viewport_set(dfbl->default_fb, UNPACK2(display_offset), UNPACK2(data_.extent));
+  GPU_framebuffer_viewport_set(dfbl->default_fb, UNPACK2(data.offset), UNPACK2(data_.extent));
 
   combined_final_tx_ = inst_.render_buffers.combined_tx;
 

source/blender/draw/engines/eevee_next/eevee_film.hh
@@ -14,6 +14,16 @@
  * - For dynamic scene (if an update is detected), we use a more temporally stable accumulation
  *   following the Temporal Anti-Aliasing method (a.k.a. Temporal Super-Sampling). This does
  *   history reprojection and rectification to avoid most of the flickering.
+ *
+ * The Film module uses the following terms to refer to different spaces/extents:
+ *
+ * - Display: The full output extent (matches the full viewport or the final image resolution).
+ *
+ * - Film: The same extent as display, or a subset of it when a Render Region is used.
+ *
+ * - Render: The extent used internally by the engine for rendering the main views.
+ *   Equals to the full display extent + overscan (even when a Render Region is used)
+ *   and its resolution can be scaled.
  */
 
 #pragma once
@@ -66,7 +76,7 @@ class Film {
   PassSimple cryptomatte_post_ps_ = {"Film.Cryptomatte.Post"};
 
   FilmDataBuf data_;
-  int2 display_offset;
+  int2 display_extent;
 
   eViewLayerEEVEEPassType enabled_passes_ = eViewLayerEEVEEPassType(0);
 
@@ -97,10 +107,10 @@ class Film {
     return data_.render_extent;
   }
 
-  /** Returns render output resolution. */
+  /** Returns final output resolution. */
   int2 display_extent_get() const
   {
-    return data_.extent;
+    return display_extent;
   }
 
   float2 pixel_jitter_get() const;

source/blender/draw/engines/eevee_next/eevee_shader_shared.hh
@@ -229,10 +229,12 @@ BLI_STATIC_ASSERT_ALIGN(FilmSample, 16)
 struct FilmData {
   /** Size of the film in pixels. */
   int2 extent;
-  /** Offset of the film in the full-res frame, in pixels. */
+  /** Offset to convert from Display space to Film space, in pixels. */
   int2 offset;
-  /** Extent used by the render buffers when rendering the main views. */
+  /** Size of the render buffers when rendering the main views, in pixels. */
   int2 render_extent;
+  /** Offset to convert from Film space to Render space, in pixels. */
+  int2 render_offset;
   /**
    * Sub-pixel offset applied to the window matrix.
    * NOTE: In final film pixel unit.
@@ -254,7 +256,7 @@ struct FilmData {
   bool1 any_render_pass_2;
   /** Controlled by user in lookdev mode or by render settings. */
   float background_opacity;
-  float _pad0;
+  float _pad0, _pad1, _pad2;
   /** Output counts per type. */
   int color_len, value_len;
   /** Index in color_accum_img or value_accum_img of each pass. -1 if pass is not enabled. */

source/blender/draw/engines/eevee_next/shaders/eevee_film_frag.glsl
@@ -7,7 +7,7 @@
 
 void main()
 {
-  ivec2 texel_film = ivec2(gl_FragCoord.xy);
+  ivec2 texel_film = ivec2(gl_FragCoord.xy) - film_buf.offset;
   float out_depth;
 
   if (film_buf.display_only) {

source/blender/draw/engines/eevee_next/shaders/eevee_film_lib.glsl
@@ -59,7 +59,7 @@ FilmSample film_sample_get(int sample_n, ivec2 texel_film)
 #  endif
 
   FilmSample film_sample = film_buf.samples[sample_n];
-  film_sample.texel += texel_film + film_buf.offset;
+  film_sample.texel += texel_film + film_buf.render_offset;
   /* Use extend on borders. */
   film_sample.texel = clamp(film_sample.texel, ivec2(0, 0), film_buf.render_extent - 1);
 