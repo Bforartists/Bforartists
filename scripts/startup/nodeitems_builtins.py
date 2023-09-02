Bforartists/Bforartists

commit 407ef4d8825ee0b2a7fb4087efc3d883dba00faf

Nodes: Remove unused shader and compositor NodeItem definitions
Unused after:
- 2fed2701b5db9c70487fa81eae7aef100a42eb3b
- 820ddb1262cff050ac05ece1639d33d4db73c351

See the geometry nodes commit for more background:
- deee700aa2bd56129953a7e52838165e6f08a906

scripts/startup/nodeitems_builtins.py
@@ -101,291 +101,6 @@ def group_input_output_item_poll(context):
     return False
 
 
-# only show input/output nodes when editing line style node trees
-def line_style_shader_nodes_poll(context):
-    snode = context.space_data
-    return (snode.tree_type == 'ShaderNodeTree' and
-            snode.shader_type == 'LINESTYLE')
-
-
-# only show nodes working in world node trees
-def world_shader_nodes_poll(context):
-    snode = context.space_data
-    return (snode.tree_type == 'ShaderNodeTree' and
-            snode.shader_type == 'WORLD')
-
-
-# only show nodes working in object node trees
-def object_shader_nodes_poll(context):
-    snode = context.space_data
-    return (snode.tree_type == 'ShaderNodeTree' and
-            snode.shader_type == 'OBJECT')
-
-
-def cycles_shader_nodes_poll(context):
-    return context.engine == 'CYCLES'
-
-
-def eevee_shader_nodes_poll(context):
-    return context.engine == 'BLENDER_EEVEE'
-
-
-def eevee_cycles_shader_nodes_poll(context):
-    return (cycles_shader_nodes_poll(context) or
-            eevee_shader_nodes_poll(context))
-
-
-def object_cycles_shader_nodes_poll(context):
-    return (object_shader_nodes_poll(context) and
-            cycles_shader_nodes_poll(context))
-
-
-def object_eevee_shader_nodes_poll(context):
-    return (object_shader_nodes_poll(context) and
-            eevee_shader_nodes_poll(context))
-
-
-def object_eevee_cycles_shader_nodes_poll(context):
-    return (object_shader_nodes_poll(context) and
-            eevee_cycles_shader_nodes_poll(context))
-
-
-# All standard node categories currently used in nodes.
-
-shader_node_categories = [
-    # Shader Nodes (Cycles and Eevee)
-    ShaderNodeCategory("SH_NEW_INPUT", "Input", items=[
-        NodeItem("ShaderNodeTexCoord"),
-        NodeItem("ShaderNodeAttribute"),
-        NodeItem("ShaderNodeLightPath"),
-        NodeItem("ShaderNodeFresnel"),
-        NodeItem("ShaderNodeLayerWeight"),
-        NodeItem("ShaderNodeRGB"),
-        NodeItem("ShaderNodeValue"),
-        NodeItem("ShaderNodeTangent"),
-        NodeItem("ShaderNodeNewGeometry"),
-        NodeItem("ShaderNodeWireframe"),
-        NodeItem("ShaderNodeBevel"),
-        NodeItem("ShaderNodeAmbientOcclusion"),
-        NodeItem("ShaderNodeObjectInfo"),
-        NodeItem("ShaderNodeHairInfo"),
-        NodeItem("ShaderNodePointInfo"),
-        NodeItem("ShaderNodeVolumeInfo"),
-        NodeItem("ShaderNodeParticleInfo"),
-        NodeItem("ShaderNodeCameraData"),
-        NodeItem("ShaderNodeUVMap"),
-        NodeItem("ShaderNodeVertexColor"),
-        NodeItem("ShaderNodeUVAlongStroke", poll=line_style_shader_nodes_poll),
-    ]),
-    ShaderNodeCategory("SH_NEW_OUTPUT", "Output", items=[
-        NodeItem("ShaderNodeOutputMaterial", poll=object_eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeOutputLight", poll=object_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeOutputAOV"),
-        NodeItem("ShaderNodeOutputWorld", poll=world_shader_nodes_poll),
-        NodeItem("ShaderNodeOutputLineStyle", poll=line_style_shader_nodes_poll),
-    ]),
-    ShaderNodeCategory("SH_NEW_SHADER", "Shader", items=[
-        NodeItem("ShaderNodeMixShader", poll=eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeAddShader", poll=eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfDiffuse", poll=object_eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfPrincipled", poll=object_eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfGlossy", poll=object_eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfTransparent", poll=object_eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfRefraction", poll=object_eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfGlass", poll=object_eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfTranslucent", poll=object_eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfSheen", poll=object_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfToon", poll=object_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeSubsurfaceScattering", poll=object_eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeEmission", poll=eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfHair", poll=object_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeBackground", poll=world_shader_nodes_poll),
-        NodeItem("ShaderNodeHoldout", poll=object_eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeVolumeAbsorption", poll=eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeVolumeScatter", poll=eevee_cycles_shader_nodes_poll),
-        NodeItem("ShaderNodeVolumePrincipled"),
-        NodeItem("ShaderNodeEeveeSpecular", poll=object_eevee_shader_nodes_poll),
-        NodeItem("ShaderNodeBsdfHairPrincipled", poll=object_cycles_shader_nodes_poll),
-    ]),
-    ShaderNodeCategory("SH_NEW_TEXTURE", "Texture", items=[
-        NodeItem("ShaderNodeTexImage"),
-        NodeItem("ShaderNodeTexEnvironment"),
-        NodeItem("ShaderNodeTexSky"),
-        NodeItem("ShaderNodeTexNoise"),
-        NodeItem("ShaderNodeTexWave"),
-        NodeItem("ShaderNodeTexVoronoi"),
-        NodeItem("ShaderNodeTexMusgrave"),
-        NodeItem("ShaderNodeTexGradient"),
-        NodeItem("ShaderNodeTexMagic"),
-        NodeItem("ShaderNodeTexChecker"),
-        NodeItem("ShaderNodeTexBrick"),
-        NodeItem("ShaderNodeTexPointDensity"),
-        NodeItem("ShaderNodeTexIES"),
-        NodeItem("ShaderNodeTexWhiteNoise"),
-    ]),
-    ShaderNodeCategory("SH_NEW_OP_COLOR", "Color", items=[
-        NodeItem("ShaderNodeMix", label="Mix Color", settings={"data_type": "'RGBA'"}),
-        NodeItem("ShaderNodeRGBCurve"),
-        NodeItem("ShaderNodeInvert"),
-        NodeItem("ShaderNodeLightFalloff"),
-        NodeItem("ShaderNodeHueSaturation"),
-        NodeItem("ShaderNodeGamma"),
-        NodeItem("ShaderNodeBrightContrast"),
-    ]),
-    ShaderNodeCategory("SH_NEW_OP_VECTOR", "Vector", items=[
-        NodeItem("ShaderNodeMapping"),
-        NodeItem("ShaderNodeBump"),
-        NodeItem("ShaderNodeDisplacement"),
-        NodeItem("ShaderNodeVectorDisplacement"),
-        NodeItem("ShaderNodeNormalMap"),
-        NodeItem("ShaderNodeNormal"),
-        NodeItem("ShaderNodeVectorCurve"),
-        NodeItem("ShaderNodeVectorRotate"),
-        NodeItem("ShaderNodeVectorTransform"),
-    ]),
-    ShaderNodeCategory("SH_NEW_CONVERTOR", "Converter", items=[
-        NodeItem("ShaderNodeMapRange"),
-        NodeItem("ShaderNodeFloatCurve"),
-        NodeItem("ShaderNodeClamp"),
-        NodeItem("ShaderNodeMath"),
-        NodeItem("ShaderNodeMix"),
-        NodeItem("ShaderNodeValToRGB"),
-        NodeItem("ShaderNodeRGBToBW"),
-        NodeItem("ShaderNodeShaderToRGB", poll=object_eevee_shader_nodes_poll),
-        NodeItem("ShaderNodeVectorMath"),
-        NodeItem("ShaderNodeSeparateColor"),
-        NodeItem("ShaderNodeCombineColor"),
-        NodeItem("ShaderNodeSeparateXYZ"),
-        NodeItem("ShaderNodeCombineXYZ"),
-        NodeItem("ShaderNodeWavelength"),
-        NodeItem("ShaderNodeBlackbody"),
-    ]),
-    ShaderNodeCategory("SH_NEW_SCRIPT", "Script", items=[
-        NodeItem("ShaderNodeScript"),
-    ]),
-    ShaderNodeCategory("SH_NEW_GROUP", "Group", items=node_group_items),
-    ShaderNodeCategory("SH_NEW_LAYOUT", "Layout", items=[
-        NodeItem("NodeFrame"),
-        NodeItem("NodeReroute"),
-    ]),
-]
-
-compositor_node_categories = [
-    # Compositor Nodes
-    CompositorNodeCategory("CMP_INPUT", "Input", items=[
-        NodeItem("CompositorNodeRLayers"),
-        NodeItem("CompositorNodeImage"),
-        NodeItem("CompositorNodeMovieClip"),
-        NodeItem("CompositorNodeMask"),
-        NodeItem("CompositorNodeRGB"),
-        NodeItem("CompositorNodeValue"),
-        NodeItem("CompositorNodeTexture"),
-        NodeItem("CompositorNodeBokehImage"),
-        NodeItem("CompositorNodeTime"),
-        NodeItem("CompositorNodeSceneTime"),
-        NodeItem("CompositorNodeTrackPos"),
-    ]),
-    CompositorNodeCategory("CMP_OUTPUT", "Output", items=[
-        NodeItem("CompositorNodeComposite"),
-        NodeItem("CompositorNodeViewer"),
-        NodeItem("CompositorNodeSplitViewer"),
-        NodeItem("CompositorNodeOutputFile"),
-        NodeItem("CompositorNodeLevels"),
-    ]),
-    CompositorNodeCategory("CMP_OP_COLOR", "Color", items=[
-        NodeItem("CompositorNodeMixRGB"),
-        NodeItem("CompositorNodeAlphaOver"),
-        NodeItem("CompositorNodeInvert"),
-        NodeItem("CompositorNodeCurveRGB"),
-        NodeItem("CompositorNodeHueSat"),
-        NodeItem("CompositorNodeColorBalance"),
-        NodeItem("CompositorNodeHueCorrect"),
-        NodeItem("CompositorNodeBrightContrast"),
-        NodeItem("CompositorNodeGamma"),
-        NodeItem("CompositorNodeExposure"),
-        NodeItem("CompositorNodeColorCorrection"),
-        NodeItem("CompositorNodePosterize"),
-        NodeItem("CompositorNodeTonemap"),
-        NodeItem("CompositorNodeZcombine"),
-    ]),
-    CompositorNodeCategory("CMP_CONVERTOR", "Converter", items=[
-        NodeItem("CompositorNodeMath"),
-        NodeItem("CompositorNodeValToRGB"),
-        NodeItem("CompositorNodeSetAlpha"),
-        NodeItem("CompositorNodePremulKey"),
-        NodeItem("CompositorNodeIDMask"),
-        NodeItem("CompositorNodeRGBToBW"),
-        NodeItem("CompositorNodeSeparateColor"),
-        NodeItem("CompositorNodeCombineColor"),
-        NodeItem("CompositorNodeSeparateXYZ"),
-        NodeItem("CompositorNodeCombineXYZ"),
-        NodeItem("CompositorNodeSwitchView"),
-        NodeItem("CompositorNodeConvertColorSpace"),
-    ]),
-    CompositorNodeCategory("CMP_OP_FILTER", "Filter", items=[
-        NodeItem("CompositorNodeBlur"),
-        NodeItem("CompositorNodeBilateralblur"),
-        NodeItem("CompositorNodeDilateErode"),
-        NodeItem("CompositorNodeDespeckle"),
-        NodeItem("CompositorNodeFilter"),
-        NodeItem("CompositorNodeBokehBlur"),
-        NodeItem("CompositorNodeVecBlur"),
-        NodeItem("CompositorNodeDefocus"),
-        NodeItem("CompositorNodeGlare"),
-        NodeItem("CompositorNodeInpaint"),
-        NodeItem("CompositorNodeDBlur"),
-        NodeItem("CompositorNodePixelate"),
-        NodeItem("CompositorNodeSunBeams"),
-        NodeItem("CompositorNodeDenoise"),
-        NodeItem("CompositorNodeAntiAliasing"),
-        NodeItem("CompositorNodeKuwahara"),
-    ]),
-    CompositorNodeCategory("CMP_OP_VECTOR", "Vector", items=[
-        NodeItem("CompositorNodeNormal"),
-        NodeItem("CompositorNodeMapValue"),
-        NodeItem("CompositorNodeMapRange"),
-        NodeItem("CompositorNodeNormalize"),
-        NodeItem("CompositorNodeCurveVec"),
-    ]),
-    CompositorNodeCategory("CMP_MATTE", "Matte", items=[
-        NodeItem("CompositorNodeKeying"),
-        NodeItem("CompositorNodeKeyingScreen"),
-        NodeItem("CompositorNodeChannelMatte"),
-        NodeItem("CompositorNodeColorSpill"),
-        NodeItem("CompositorNodeBoxMask"),
-        NodeItem("CompositorNodeEllipseMask"),
-        NodeItem("CompositorNodeLumaMatte"),
-        NodeItem("CompositorNodeDiffMatte"),
-        NodeItem("CompositorNodeDistanceMatte"),
-        NodeItem("CompositorNodeChromaMatte"),
-        NodeItem("CompositorNodeColorMatte"),
-        NodeItem("CompositorNodeDoubleEdgeMask"),
-        NodeItem("CompositorNodeCryptomatte"),
-        NodeItem("CompositorNodeCryptomatteV2"),
-    ]),
-    CompositorNodeCategory("CMP_DISTORT", "Distort", items=[
-        NodeItem("CompositorNodeScale"),
-        NodeItem("CompositorNodeLensdist"),
-        NodeItem("CompositorNodeMovieDistortion"),
-        NodeItem("CompositorNodeTranslate"),
-        NodeItem("CompositorNodeRotate"),
-        NodeItem("CompositorNodeFlip"),
-        NodeItem("CompositorNodeCrop"),
-        NodeItem("CompositorNodeDisplace"),
-        NodeItem("CompositorNodeMapUV"),
-        NodeItem("CompositorNodeTransform"),
-        NodeItem("CompositorNodeStabilize"),
-        NodeItem("CompositorNodePlaneTrackDeform"),
-        NodeItem("CompositorNodeCornerPin"),
-    ]),
-    CompositorNodeCategory("CMP_GROUP", "Group", items=node_group_items),
-    CompositorNodeCategory("CMP_LAYOUT", "Layout", items=[
-        NodeItem("NodeFrame"),
-        NodeItem("NodeReroute"),
-        NodeItem("CompositorNodeSwitch"),
-    ]),
-]
-
 texture_node_categories = [
     # Texture Nodes
     TextureNodeCategory("TEX_INPUT", "Input", items=[

scripts/startup/bl_ui/node_add_menu_shader.py
@@ -5,17 +5,58 @@
 import bpy
 from bpy.types import Menu
 from bl_ui import node_add_menu
-from nodeitems_builtins import (
-    eevee_cycles_shader_nodes_poll,
-    line_style_shader_nodes_poll,
-    object_cycles_shader_nodes_poll,
-    object_eevee_cycles_shader_nodes_poll,
-    object_eevee_shader_nodes_poll,
-    world_shader_nodes_poll,
-)
 from bpy.app.translations import (
     pgettext_iface as iface_,
 )
+
+
+# only show input/output nodes when editing line style node trees
+def line_style_shader_nodes_poll(context):
+    snode = context.space_data
+    return (snode.tree_type == 'ShaderNodeTree' and
+            snode.shader_type == 'LINESTYLE')
+
+
+# only show nodes working in world node trees
+def world_shader_nodes_poll(context):
+    snode = context.space_data
+    return (snode.tree_type == 'ShaderNodeTree' and
+            snode.shader_type == 'WORLD')
+
+
+# only show nodes working in object node trees
+def object_shader_nodes_poll(context):
+    snode = context.space_data
+    return (snode.tree_type == 'ShaderNodeTree' and
+            snode.shader_type == 'OBJECT')
+
+
+def cycles_shader_nodes_poll(context):
+    return context.engine == 'CYCLES'
+
+
+def eevee_shader_nodes_poll(context):
+    return context.engine == 'BLENDER_EEVEE'
+
+
+def eevee_cycles_shader_nodes_poll(context):
+    return (cycles_shader_nodes_poll(context) or
+            eevee_shader_nodes_poll(context))
+
+
+def object_cycles_shader_nodes_poll(context):
+    return (object_shader_nodes_poll(context) and
+            cycles_shader_nodes_poll(context))
+
+
+def object_eevee_shader_nodes_poll(context):
+    return (object_shader_nodes_poll(context) and
+            eevee_shader_nodes_poll(context))
+
+
+def object_eevee_cycles_shader_nodes_poll(context):
+    return (object_shader_nodes_poll(context) and
+            eevee_cycles_shader_nodes_poll(context))
 
 
 class NODE_MT_category_shader_input(Menu):

commit 001fe6c56cb973b8094178106a2220c627867866

Cleanup: fix node registration
This was broken in 407ef4d8825ee0b2a7fb4087efc3d883dba00faf.

scripts/startup/nodeitems_builtins.py
@@ -159,14 +159,10 @@ texture_node_categories = [
 
 
 def register():
-    nodeitems_utils.register_node_categories('SHADER', shader_node_categories)
-    nodeitems_utils.register_node_categories('COMPOSITING', compositor_node_categories)
     nodeitems_utils.register_node_categories('TEXTURE', texture_node_categories)
 
 
 def unregister():
-    nodeitems_utils.unregister_node_categories('SHADER')
-    nodeitems_utils.unregister_node_categories('COMPOSITING')
     nodeitems_utils.unregister_node_categories('TEXTURE')
 
 