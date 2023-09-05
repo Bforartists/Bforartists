blender/blender

commit 4e94db97e2baafd6e05c91ec0ac7744f73b95509

Mesh: Add three cached topology maps
Add three cached topology maps to `Mesh`, to avoid computations when
mesh data isn't changed. Choosing the right maps to cache is a bit
arbitrary, but generally we have to start somewhere. The limiting
factor is memory usage (all the new caches combined have a
comparable footprint to a UV map).

For now, the caches added are:
- Vertex to face corner
- Vertex to face
- Face corner to face

These caches are used in quite a few places already;
- Face corner normal calculation
- UV value merging
- Setting sharp edges from face angles
- Data transfer modifier
- Voxel remesh attribute remapping
- Sculpt mode painting
- Sculpt mode normal calculation
- Vertex paint mode
- Split edges geometry node
- Mesh topology geometry nodes

Caching topology maps means they don't have to be rebuilt every time
they're used. Meshes copied but without topology changes can share
the cache, further reducing re-computations. For example, FPS with a
large mesh using the "Corners of Vertex" node went from 1.8 to 2.3.
Entering sculpt mode is slightly faster too.

There is some obvious work for future commits:
- Use caches in attribute domain interpolation
- More multithreading of second phase of map building
- Update/build caches eagerly in some geometry nodes

Pull Request: https://projects.blender.org/blender/blender/pulls/107816

source/blender/blenkernel/intern/mesh_runtime.cc
@@ -14,6 +14,7 @@
 #include "DNA_meshdata_types.h"
 #include "DNA_object_types.h"
 
+#include "BLI_array_utils.hh"
 #include "BLI_math_geom.h"
 #include "BLI_task.hh"
 #include "BLI_timeit.hh"
@@ -22,6 +23,7 @@
 #include "BKE_editmesh_cache.hh"
 #include "BKE_lib_id.h"
 #include "BKE_mesh.hh"
+#include "BKE_mesh_mapping.hh"
 #include "BKE_mesh_runtime.hh"
 #include "BKE_shrinkwrap.h"
 #include "BKE_subdiv_ccg.hh"
@@ -124,6 +126,58 @@ static void try_tag_verts_no_face_none(const Mesh &mesh)
 }
 
 }  // namespace blender::bke
+
+blender::Span<int> Mesh::corner_to_face_map() const
+{
+  using namespace blender;
+  this->runtime->corner_to_face_map_cache.ensure([&](Array<int> &r_data) {
+    const OffsetIndices faces = this->faces();
+    r_data = bke::mesh::build_loop_to_face_map(faces);
+  });
+  return this->runtime->corner_to_face_map_cache.data();
+}
+
+blender::OffsetIndices<int> Mesh::vert_to_face_map_offsets() const
+{
+  using namespace blender;
+  this->runtime->vert_to_face_offset_cache.ensure([&](Array<int> &r_data) {
+    r_data = Array<int>(this->totvert + 1, 0);
+    offset_indices::build_reverse_offsets(this->corner_verts(), r_data);
+  });
+  return OffsetIndices<int>(this->runtime->vert_to_face_offset_cache.data());
+}
+
+blender::GroupedSpan<int> Mesh::vert_to_face_map() const
+{
+  using namespace blender;
+  const OffsetIndices offsets = this->vert_to_face_map_offsets();
+  this->runtime->vert_to_face_map_cache.ensure([&](Array<int> &r_data) {
+    r_data.reinitialize(this->totloop);
+    if (this->runtime->vert_to_corner_map_cache.is_cached() &&
+        this->runtime->corner_to_face_map_cache.is_cached())
+    {
+      /* The vertex to face cache can be built from the vertex to face corner
+       * and face corner to face maps if they are both already cached. */
+      array_utils::gather(this->runtime->vert_to_corner_map_cache.data().as_span(),
+                          this->runtime->corner_to_face_map_cache.data().as_span(),
+                          r_data.as_mutable_span());
+    }
+    else {
+      bke::mesh::build_vert_to_face_indices(this->faces(), this->corner_verts(), offsets, r_data);
+    }
+  });
+  return {offsets, this->runtime->vert_to_face_map_cache.data()};
+}
+
+blender::GroupedSpan<int> Mesh::vert_to_corner_map() const
+{
+  using namespace blender;
+  const OffsetIndices offsets = this->vert_to_face_map_offsets();
+  this->runtime->vert_to_corner_map_cache.ensure([&](Array<int> &r_data) {
+    r_data = bke::mesh::build_vert_to_corner_indices(this->corner_verts(), offsets);
+  });
+  return {offsets, this->runtime->vert_to_corner_map_cache.data()};
+}
 
 const blender::bke::LooseVertCache &Mesh::loose_verts() const
 {
@@ -250,6 +304,10 @@ void BKE_mesh_runtime_clear_geometry(Mesh *mesh)
   free_bvh_cache(*mesh->runtime);
   free_subdiv_ccg(*mesh->runtime);
   mesh->runtime->bounds_cache.tag_dirty();
+  mesh->runtime->vert_to_face_offset_cache.tag_dirty();
+  mesh->runtime->vert_to_face_map_cache.tag_dirty();
+  mesh->runtime->vert_to_corner_map_cache.tag_dirty();
+  mesh->runtime->corner_to_face_map_cache.tag_dirty();
   mesh->runtime->vert_normals_cache.tag_dirty();
   mesh->runtime->face_normals_cache.tag_dirty();
   mesh->runtime->loose_edges_cache.tag_dirty();
@@ -271,6 +329,9 @@ void BKE_mesh_tag_edges_split(Mesh *mesh)
   free_bvh_cache(*mesh->runtime);
   mesh->runtime->vert_normals_cache.tag_dirty();
   free_subdiv_ccg(*mesh->runtime);
+  mesh->runtime->vert_to_face_offset_cache.tag_dirty();
+  mesh->runtime->vert_to_face_map_cache.tag_dirty();
+  mesh->runtime->vert_to_corner_map_cache.tag_dirty();
   if (mesh->runtime->loose_edges_cache.is_cached() &&
       mesh->runtime->loose_edges_cache.data().count != 0)
   {
@@ -298,6 +359,7 @@ void BKE_mesh_tag_face_winding_changed(Mesh *mesh)
 {
   mesh->runtime->vert_normals_cache.tag_dirty();
   mesh->runtime->face_normals_cache.tag_dirty();
+  mesh->runtime->vert_to_corner_map_cache.tag_dirty();
 }
 
 void BKE_mesh_tag_positions_changed(Mesh *mesh)

source/blender/blenkernel/BKE_mesh.hh
@@ -146,7 +146,6 @@ short2 lnor_space_custom_normal_to_data(const CornerNormalSpace &lnor_space,
  * Useful to materialize sharp edges (or non-smooth faces) without actually modifying the geometry
  * (splitting edges).
  *
- * \param loop_to_face_map: Optional pre-created map from corners to their face.
  * \param sharp_edges: Optional array of sharp edge tags, used to split the evaluated normals on
  * each side of the edge.
  * \param r_lnors_spacearr: Optional return data filled with information about the custom
@@ -204,6 +203,7 @@ void edges_sharp_from_angle_set(OffsetIndices<int> faces,
                                 Span<int> corner_verts,
                                 Span<int> corner_edges,
                                 Span<float3> face_normals,
+                                Span<int> loop_to_face,
                                 const bool *sharp_faces,
                                 const float split_angle,
                                 MutableSpan<bool> sharp_edges);

source/blender/blenkernel/BKE_mesh_mapping.hh
@@ -280,12 +280,17 @@ GroupedSpan<int> build_vert_to_edge_map(Span<int2> edges,
                                         Array<int> &r_offsets,
                                         Array<int> &r_indices);
 
+void build_vert_to_face_indices(OffsetIndices<int> faces,
+                                Span<int> corner_verts,
+                                OffsetIndices<int> offsets,
+                                MutableSpan<int> face_indices);
 GroupedSpan<int> build_vert_to_face_map(OffsetIndices<int> faces,
                                         Span<int> corner_verts,
                                         int verts_num,
                                         Array<int> &r_offsets,
                                         Array<int> &r_indices);
 
+Array<int> build_vert_to_corner_indices(Span<int> corner_verts, OffsetIndices<int> offsets);
 GroupedSpan<int> build_vert_to_loop_map(Span<int> corner_verts,
                                         int verts_num,
                                         Array<int> &r_offsets,

source/blender/blenkernel/BKE_mesh_types.hh
@@ -140,6 +140,17 @@ struct MeshRuntime {
   SharedCache<Vector<float3>> vert_normals_cache;
   SharedCache<Vector<float3>> face_normals_cache;
 
+  /**
+   * Cache of offsets for vert to face/corner maps. The same offsets array is used to group
+   * indices for both the vertex to face and vertex to corner maps.
+   */
+  SharedCache<Array<int>> vert_to_face_offset_cache;
+  /** Cache of indices for vert to face map. */
+  SharedCache<Array<int>> vert_to_face_map_cache;
+  /** Cache of indices for vert to corner map. */
+  SharedCache<Array<int>> vert_to_corner_map_cache;
+  /** Cache of face indices for each face corner. */
+  SharedCache<Array<int>> corner_to_face_map_cache;
   /** Cache of data about edges not used by faces. See #Mesh::loose_edges(). */
   SharedCache<LooseEdgeCache> loose_edges_cache;
   /** Cache of data about vertices not used by edges. See #Mesh::loose_verts(). */

source/blender/blenkernel/BKE_paint.hh
@@ -267,12 +267,7 @@ void BKE_paint_blend_read_data(BlendDataReader *reader, const Scene *scene, Pain
 
 /** Used for both vertex color and weight paint. */
 struct SculptVertexPaintGeomMap {
-  blender::Array<int> vert_to_loop_offsets;
-  blender::Array<int> vert_to_loop_indices;
   blender::GroupedSpan<int> vert_to_loop;
-
-  blender::Array<int> vert_to_face_offsets;
-  blender::Array<int> vert_to_face_indices;
   blender::GroupedSpan<int> vert_to_face;
 };
 
@@ -591,9 +586,7 @@ struct SculptSession {
   float *vmask;
 
   /* Mesh connectivity maps. */
-  /* Vertices to adjacent faces. */
-  blender::Array<int> vert_to_face_offsets;
-  blender::Array<int> vert_to_face_indices;
+  /* Vertices to adjacent polys. */
   blender::GroupedSpan<int> pmap;
 
   /* Edges to adjacent faces. */

source/blender/blenkernel/intern/data_transfer.cc
@@ -393,7 +393,7 @@ static void data_transfer_dtdata_type_preprocess(const Mesh *me_src,
                                             me_dst->faces(),
                                             me_dst->corner_verts(),
                                             me_dst->corner_edges(),
-                                            {},
+                                            me_dst->corner_to_face_map(),
                                             me_dst->vert_normals(),
                                             me_dst->face_normals(),
                                             sharp_edges,

source/blender/blenkernel/intern/key.cc
@@ -2278,7 +2278,7 @@ void BKE_keyblock_mesh_calc_normals(const KeyBlock *kb,
         faces,
         corner_verts,
         corner_edges,
-        {},
+        mesh->corner_to_face_map(),
         {reinterpret_cast<blender::float3 *>(vert_normals), mesh->totvert},
         {reinterpret_cast<blender::float3 *>(face_normals), faces.size()},
         sharp_edges,

source/blender/blenkernel/intern/mesh.cc
@@ -139,6 +139,10 @@ static void mesh_copy_data(Main *bmain, ID *id_dst, const ID *id_src, const int 
   mesh_dst->runtime->loose_edges_cache = mesh_src->runtime->loose_edges_cache;
   mesh_dst->runtime->looptris_cache = mesh_src->runtime->looptris_cache;
   mesh_dst->runtime->looptri_faces_cache = mesh_src->runtime->looptri_faces_cache;
+  mesh_dst->runtime->vert_to_face_offset_cache = mesh_src->runtime->vert_to_face_offset_cache;
+  mesh_dst->runtime->vert_to_face_map_cache = mesh_src->runtime->vert_to_face_map_cache;
+  mesh_dst->runtime->vert_to_corner_map_cache = mesh_src->runtime->vert_to_corner_map_cache;
+  mesh_dst->runtime->corner_to_face_map_cache = mesh_src->runtime->corner_to_face_map_cache;
 
   /* Only do tessface if we have no faces. */
   const bool do_tessface = ((mesh_src->totface_legacy != 0) && (mesh_src->faces_num == 0));
@@ -1778,7 +1782,7 @@ void BKE_mesh_calc_normals_split_ex(const Mesh *mesh,
       mesh->faces(),
       mesh->corner_verts(),
       mesh->corner_edges(),
-      {},
+      mesh->corner_to_face_map(),
       mesh->vert_normals(),
       mesh->face_normals(),
       sharp_edges,

Bforartists/Bforartists

commit 6913053dbda3b12d42454ab62692ba4cd256d734

Fix #111934: crash sculpting on mesh
The order of the parameters was reversed.
This bug was caused by 4e94db97e2baafd6e05c91ec0ac7744f73b95509.

source/blender/blenkernel/intern/mesh_runtime.cc
@@ -158,8 +158,8 @@ blender::GroupedSpan<int> Mesh::vert_to_face_map() const
     {
       /* The vertex to face cache can be built from the vertex to face corner
        * and face corner to face maps if they are both already cached. */
-      array_utils::gather(this->runtime->vert_to_corner_map_cache.data().as_span(),
-                          this->runtime->corner_to_face_map_cache.data().as_span(),
+      array_utils::gather(this->runtime->corner_to_face_map_cache.data().as_span(),
+                          this->runtime->vert_to_corner_map_cache.data().as_span(),
                           r_data.as_mutable_span());
     }
     else {