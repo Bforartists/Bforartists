blender/blender

commit 16fbadde363c8074ec725fa1e1079add096bd741

Mesh: Replace MLoop struct with generic attributes
Implements #102359.

Split the `MLoop` struct into two separate integer arrays called
`corner_verts` and `corner_edges`, referring to the vertex each corner
is attached to and the next edge around the face at each corner. These
arrays can be sliced to give access to the edges or vertices in a face.
Then they are often referred to as "poly_verts" or "poly_edges".

The main benefits are halving the necessary memory bandwidth when only
one array is used and simplifications from using regular integer indices
instead of a special-purpose struct.

The commit also starts a renaming from "loop" to "corner" in mesh code.

Like the other mesh struct of array refactors, forward compatibility is
kept by writing files with the older format. This will be done until 4.0
to ease the transition process.

Looking at a small portion of the patch should give a good impression
for the rest of the changes. I tried to make the changes as small as
possible so it's easy to tell the correctness from the diff. Though I
found Blender developers have been very inventive over the last decade
when finding different ways to loop over the corners in a face.

For performance, nearly every piece of code that deals with `Mesh` is
slightly impacted. Any algorithm that is memory bottle-necked should
see an improvement. For example, here is a comparison of interpolating
a vertex float attribute to face corners (Ryzen 3700x):

**Before** (Average: 3.7 ms, Min: 3.4 ms)
```
threading::parallel_for(loops.index_range(), 4096, [&](IndexRange range) {
  for (const int64_t i : range) {
    dst[i] = src[loops[i].v];
  }
});
```

**After** (Average: 2.9 ms, Min: 2.6 ms)
```
array_utils::gather(src, corner_verts, dst);
```

That's an improvement of 28% to the average timings, and it's also a
simplification, since an index-based routine can be used instead.
For more examples using the new arrays, see the design task.

source/blender/modifiers/intern/MOD_particleinstance.cc
@@ -315,11 +315,13 @@ static Mesh *modifyMesh(ModifierData *md, const ModifierEvalContext *ctx, Mesh *
   result = BKE_mesh_new_nomain_from_template(mesh, maxvert, maxedge, maxloop, maxpoly);
 
   const blender::Span<MPoly> orig_polys = mesh->polys();
-  const blender::Span<MLoop> orig_loops = mesh->loops();
+  const blender::Span<int> orig_corner_verts = mesh->corner_verts();
+  const blender::Span<int> orig_corner_edges = mesh->corner_edges();
   float(*positions)[3] = BKE_mesh_vert_positions_for_write(result);
   blender::MutableSpan<MEdge> edges = result->edges_for_write();
   blender::MutableSpan<MPoly> polys = result->polys_for_write();
-  blender::MutableSpan<MLoop> loops = result->loops_for_write();
+  blender::MutableSpan<int> corner_verts = result->corner_verts_for_write();
+  blender::MutableSpan<int> corner_edges = result->corner_edges_for_write();
 
   MLoopCol *mloopcols_index = static_cast<MLoopCol *>(CustomData_get_layer_named_for_write(
       &result->ldata, CD_PROP_BYTE_COLOR, pimd->index_layer_name, result->totloop));
@@ -485,23 +487,23 @@ static Mesh *modifyMesh(ModifierData *md, const ModifierEvalContext *ctx, Mesh *
       poly.loopstart += p_skip * totloop;
 
       {
-        const MLoop *inML = &orig_loops[in_poly.loopstart];
-        MLoop *ml = &loops[poly.loopstart];
+        int orig_corner_i = in_poly.loopstart;
+        int dst_corner_i = poly.loopstart;
         int j = poly.totloop;
 
         CustomData_copy_data(&mesh->ldata, &result->ldata, in_poly.loopstart, poly.loopstart, j);
-        for (; j; j--, ml++, inML++) {
-          ml->v = inML->v + (p_skip * totvert);
-          ml->e = inML->e + (p_skip * totedge);
-          const int ml_index = (ml - loops.data());
+        for (; j; j--, orig_corner_i++, dst_corner_i++) {
+          corner_verts[dst_corner_i] = orig_corner_verts[orig_corner_i] + (p_skip * totvert);
+          corner_edges[dst_corner_i] = orig_corner_edges[orig_corner_i] + (p_skip * totedge);
+          const int vert = corner_verts[orig_corner_i];
           if (mloopcols_index != nullptr) {
-            const int part_index = vert_part_index[ml->v];
-            store_float_in_vcol(&mloopcols_index[ml_index],
+            const int part_index = vert_part_index[vert];
+            store_float_in_vcol(&mloopcols_index[dst_corner_i],
                                 float(part_index) / float(psys->totpart - 1));
           }
           if (mloopcols_value != nullptr) {
-            const float part_value = vert_part_value[ml->v];
-            store_float_in_vcol(&mloopcols_value[ml_index], part_value);
+            const float part_value = vert_part_value[vert];
+            store_float_in_vcol(&mloopcols_value[dst_corner_i], part_value);
           }
         }
       }

Bforartists/Bforartists

commit 06883b8f3eb5d4b64cd35cd224503b9afb9d3d18

Fix #112335: particle instance modifier outputs wrong attributes
This was caused by a mistake in 16fbadde36.

source/blender/modifiers/intern/MOD_particleinstance.cc
@@ -495,7 +495,7 @@ static Mesh *modify_mesh(ModifierData *md, const ModifierEvalContext *ctx, Mesh 
         for (; j; j--, orig_corner_i++, dst_corner_i++) {
           corner_verts[dst_corner_i] = orig_corner_verts[orig_corner_i] + (p_skip * totvert);
           corner_edges[dst_corner_i] = orig_corner_edges[orig_corner_i] + (p_skip * totedge);
-          const int vert = corner_verts[orig_corner_i];
+          const int vert = corner_verts[dst_corner_i];
           if (mloopcols_index != nullptr) {
             const int part_index = vert_part_index[vert];
             store_float_in_vcol(&mloopcols_index[dst_corner_i],