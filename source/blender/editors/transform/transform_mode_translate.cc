Bforartists/Bforartists

commit cfe5b0e6d0619d2102c3f10bbd5bd45891de1252

Fix: Show modal keymap for operators called inside macros
Display the modal keymap in the status bar for modal operators that
are called within macros. Most notably, the keymap for the transform
operator is displayed when it's called as part of the duplicate,
extrude, or loopcut operators or when moving nodes.

Pull Request: https://projects.blender.org/blender/blender/pulls/108027

source/blender/editors/transform/transform_mode_translate.cc
@@ -303,24 +303,8 @@ static void headerTranslation(TransInfo *t, const float vec[3], char str[UI_MAX_
         const char *str_dir = (snode->insert_ofs_dir == SNODE_INSERTOFS_DIR_RIGHT) ?
                                   TIP_("right") :
                                   TIP_("left");
-        char str_dir_km[64];
-        WM_modalkeymap_items_to_string(
-            t->keymap, TFM_MODAL_INSERTOFS_TOGGLE_DIR, true, str_dir_km, sizeof(str_dir_km));
-        ofs += BLI_snprintf_rlen(str,
-                                 UI_MAX_DRAW_STR,
-                                 TIP_("%s: Toggle auto-offset direction (%s)"),
-                                 str_dir_km,
-                                 str_dir);
+        ofs += BLI_snprintf_rlen(str, UI_MAX_DRAW_STR, TIP_("Auto-offset direction: %s"), str_dir);
       }
-
-      char str_attach_km[64];
-      WM_modalkeymap_items_to_string(
-          t->keymap, TFM_MODAL_NODE_ATTACH_OFF, true, str_attach_km, sizeof(str_attach_km));
-      ofs += BLI_snprintf_rlen(str + ofs,
-                               UI_MAX_DRAW_STR - ofs,
-                               TIP_(", %s: Toggle auto-attach (%s)"),
-                               str_attach_km,
-                               WM_bool_as_string((t->modifiers & MOD_NODE_ATTACH) != 0));
     }
     else {
       if (t->flag & T_2D_EDIT) {

source/blender/windowmanager/intern/wm_event_system.cc
@@ -6277,10 +6277,11 @@ bool WM_window_modal_keymap_status_draw(bContext *C, wmWindow *win, uiLayout *la
       wmEventHandler_Op *handler = (wmEventHandler_Op *)handler_base;
       if (handler->op != nullptr) {
         /* 'handler->keymap' could be checked too, seems not to be used. */
-        wmKeyMap *keymap_test = WM_keymap_active(wm, handler->op->type->modalkeymap);
+        wmOperator *op_test = handler->op->opm ? handler->op->opm : handler->op;
+        wmKeyMap *keymap_test = WM_keymap_active(wm, op_test->type->modalkeymap);
         if (keymap_test && keymap_test->modal_items) {
           keymap = keymap_test;
-          op = handler->op;
+          op = op_test;
           break;
         }
       }

commit 68b116689013e9b3b5c9690186c7d15dc2ef2b90

Fix #112650: UI: Missing Header Status in Node Editor after Turning off Auto-offset
Caused by cfe5b0e6d0619d2102c3f10bbd5bd45891de1252
When auto-offset is OFF, garbage characters was displayed due to
uninitialized string.

Pull Request: https://projects.blender.org/blender/blender/pulls/112657

source/blender/editors/transform/transform_mode_translate.cc
@@ -564,7 +564,7 @@ static bool clip_uv_transform_translation(TransInfo *t, float vec[2])
 
 static void applyTranslation(TransInfo *t)
 {
-  char str[UI_MAX_DRAW_STR];
+  char str[UI_MAX_DRAW_STR] = "";
   float global_dir[3] = {0.0f};
 
   if (t->flag & T_INPUT_IS_VALUES_FINAL) {
@@ -640,7 +640,7 @@ static void applyTranslation(TransInfo *t)
   headerTranslation(t, (t->con.mode & CON_APPLY) ? t->values_final : global_dir, str);
 
   recalc_data(t);
-  ED_area_status_text(t->area, str);
+  ED_area_status_text(t->area, (str[0] == '\0') ? nullptr : str);
 }
 
 static void applyTranslationMatrix(TransInfo *t, float mat_xform[4][4])