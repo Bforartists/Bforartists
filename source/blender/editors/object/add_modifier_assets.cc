Bforartists/Bforartists

commit 0181d3ff977ca420ca2a4a16cd298d28e093e9d2

Cleanup: Move logic out of loops, remove unnecessary ui sublayouts

source/blender/editors/object/add_modifier_assets.cc
@@ -95,11 +95,10 @@ static void catalog_assets_draw(const bContext *C, Menu *menu)
   uiLayout *layout = menu->layout;
   uiItemS(layout);
 
+  wmOperatorType *ot = WM_operatortype_find("OBJECT_OT_modifier_add_node_group", true);
   for (const asset_system::AssetRepresentation *asset : assets) {
-    uiLayout *col = uiLayoutColumn(layout, false);
-    wmOperatorType *ot = WM_operatortype_find("OBJECT_OT_modifier_add_asset", true);
     PointerRNA props_ptr;
-    uiItemFullO_ptr(col,
+    uiItemFullO_ptr(layout,
                     ot,
                     IFACE_(asset->get_name().c_str()),
                     ICON_NONE,
@@ -125,10 +124,11 @@ static void catalog_assets_draw(const bContext *C, Menu *menu)
 static void unassigned_assets_draw(const bContext * /*C*/, Menu *menu)
 {
   asset::AssetItemTree &tree = *get_static_item_tree();
+  uiLayout *layout = menu->layout;
+  wmOperatorType *ot = WM_operatortype_find("OBJECT_OT_modifier_add_asset", true);
   for (const asset_system::AssetRepresentation *asset : tree.unassigned_assets) {
-    wmOperatorType *ot = WM_operatortype_find("OBJECT_OT_modifier_add_asset", true);
     PointerRNA props_ptr;
-    uiItemFullO_ptr(menu->layout,
+    uiItemFullO_ptr(layout,
                     ot,
                     IFACE_(asset->get_name().c_str()),
                     ICON_NONE,

source/blender/editors/geometry/node_group_operator.cc
@@ -651,11 +651,10 @@ static void catalog_assets_draw(const bContext *C, Menu *menu)
   uiLayout *layout = menu->layout;
   uiItemS(layout);
 
+  wmOperatorType *ot = WM_operatortype_find("GEOMETRY_OT_execute_node_group", true);
   for (const asset_system::AssetRepresentation *asset : assets) {
-    uiLayout *col = uiLayoutColumn(layout, false);
-    wmOperatorType *ot = WM_operatortype_find("GEOMETRY_OT_execute_node_group", true);
     PointerRNA props_ptr;
-    uiItemFullO_ptr(col,
+    uiItemFullO_ptr(layout,
                     ot,
                     IFACE_(asset->get_name().c_str()),
                     ICON_NONE,
@@ -695,10 +694,11 @@ static void catalog_assets_draw_unassigned(const bContext *C, Menu *menu)
   if (!tree) {
     return;
   }
+  uiLayout *layout = menu->layout;
+  wmOperatorType *ot = WM_operatortype_find("GEOMETRY_OT_execute_node_group", true);
   for (const asset_system::AssetRepresentation *asset : tree->unassigned_assets) {
-    wmOperatorType *ot = WM_operatortype_find("GEOMETRY_OT_execute_node_group", true);
     PointerRNA props_ptr;
-    uiItemFullO_ptr(menu->layout,
+    uiItemFullO_ptr(layout,
                     ot,
                     IFACE_(asset->get_name().c_str()),
                     ICON_NONE,

commit 6cd8bca954381f210a9b7a6a369f2f2528c0c4c5

Fix: try to call unavailable operator
This issue was introduced in 0181d3ff977ca420ca2a4a16cd298d28e093e9d2,
not exactly sure why this was renamed.

source/blender/editors/object/add_modifier_assets.cc
@@ -95,7 +95,7 @@ static void catalog_assets_draw(const bContext *C, Menu *menu)
   uiLayout *layout = menu->layout;
   uiItemS(layout);
 
-  wmOperatorType *ot = WM_operatortype_find("OBJECT_OT_modifier_add_node_group", true);
+  wmOperatorType *ot = WM_operatortype_find("OBJECT_OT_modifier_add_asset", true);
   for (const asset_system::AssetRepresentation *asset : assets) {
     PointerRNA props_ptr;
     uiItemFullO_ptr(layout,