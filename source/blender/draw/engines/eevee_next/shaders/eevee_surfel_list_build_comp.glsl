Bforartists/Bforartists

commit fbb9679436681fff7c01c6544d5f70a0e9549efb

EEVEE-Next: Port SSS profile to Pre-compute shader
This moves the pre-computation offline and store the pre-computed
table in the binary. The pre-computed tables are quite small and are
not a concern with respect to binary size increase.

This rewrites the precomputation to use manually fitted
approximations for both burley and random walk.
The approximations fix a discrepancy between cycles and EEVEE
SSS translucency look. The absolute maximum error is below 2%.
I believe better results could be achieved with automatic fitting
tools.

Note that Cycles Burley translucency profile has some issues as it
does not give a smooth profile. The profile is biased near the end
of the lower radii. For this reason, the fit was done on a white
diffuse with (1,1,1) radii which does not exhibit this artifact.

Note that while this adds the profile for random walk, it isn't
currently used because the profile type is not yet passed down
the deferred path.

The fitting data can be found attached to this PR.

Pull Request: https://projects.blender.org/blender/blender/pulls/112512

source/blender/draw/engines/eevee_next/shaders/eevee_surfel_list_build_comp.glsl
@@ -20,7 +20,7 @@
 void main()
 {
   int surfel_index = int(gl_GlobalInvocationID.x);
-  if (surfel_index >= capture_info_buf.surfel_len) {
+  if (surfel_index >= int(capture_info_buf.surfel_len)) {
     return;
   }
 
@@ -32,5 +32,5 @@ void main()
   surfel_buf[surfel_index].ray_distance = ray_distance;
   /* NOTE: We only need to init the `list_start_buf` to -1 for the whole list to be valid since
    * every surfel will load its `next` value from the list head. */
-  surfel_buf[surfel_index].next = atomicExchange(list_start_buf[list_index], surfel_index);
+  // surfel_buf[surfel_index].next = atomicExchange(list_start_buf[list_index], surfel_index);
 }

source/blender/draw/engines/eevee_next/eevee_lut.hh
@@ -26,5 +26,16 @@ extern const float bsdf_ggx[16][64][64][3];
 extern const float btdf_ggx[16][64][64][1];
 /* 4 different blue noise, one per channel. */
 extern const float blue_noise[64][64][4];
+/* Precomputed translucency profile of a slab of material with varying thickness. */
+extern const float burley_sss_profile[64][1];
+/* Precomputed translucency profile of a slab of material with varying thickness. */
+extern const float random_walk_sss_profile[64][1];
+
+#ifdef CYCLES_REFERENCE_LUT
+/* Precomputed translucency profile of a slab of material with varying thickness. */
+extern const float cycles_burley_reference_sss_profile[256][3];
+/* Precomputed translucency profile of a slab of material with varying thickness. */
+extern const float cycles_random_walk_reference_sss_profile[256][3];
+#endif
 
 }  // namespace blender::eevee::lut

source/blender/draw/engines/eevee_next/eevee_pipeline.hh
@@ -382,15 +382,16 @@ class UtilityTexture : public Texture {
     }
     {
       Layer &layer = data[UTIL_SSS_TRANSMITTANCE_PROFILE_LAYER];
-      const Vector<float> &transmittance_profile = SubsurfaceModule::transmittance_profile();
-      BLI_assert(transmittance_profile.size() == lut_size);
-      /* Repeatedly stored on every row for correct interpolation. */
       for (auto y : IndexRange(lut_size)) {
         for (auto x : IndexRange(lut_size)) {
-          /* Only the first channel is used. */
-          layer.data[y][x] = float4(transmittance_profile[x]);
+          /* Repeatedly stored on every row for correct interpolation. */
+          layer.data[y][x][0] = lut::burley_sss_profile[x][0];
+          layer.data[y][x][1] = lut::random_walk_sss_profile[x][0];
+          layer.data[y][x][2] = 0.0f;
+          layer.data[y][x][UTIL_DISK_INTEGRAL_COMP] = lut::ltc_disk_integral[y][x][0];
         }
       }
+      BLI_assert(UTIL_SSS_TRANSMITTANCE_PROFILE_LAYER == UTIL_DISK_INTEGRAL_LAYER);
     }
     {
       Layer &layer = data[UTIL_LTC_MAT_LAYER];
@@ -407,14 +408,6 @@ class UtilityTexture : public Texture {
         }
       }
       BLI_assert(UTIL_LTC_MAG_LAYER == UTIL_BSDF_LAYER);
-    }
-    {
-      Layer &layer = data[UTIL_DISK_INTEGRAL_LAYER];
-      for (auto x : IndexRange(lut_size)) {
-        for (auto y : IndexRange(lut_size)) {
-          layer.data[y][x][UTIL_DISK_INTEGRAL_COMP] = lut::ltc_disk_integral[y][x][0];
-        }
-      }
     }
     {
       for (auto layer_id : IndexRange(16)) {

source/blender/draw/engines/eevee_next/eevee_precompute.hh
@@ -105,7 +105,7 @@ class Precompute {
     std::ofstream file;
 
     file.open(std::string(name) + ".hh");
-    file << "const VecBase<float, " << VecT::type_length << "> " << name;
+    file << "const float " << name;
     if (n_w > 1) {
       file << "[" << n_w << "]";
     }
@@ -118,6 +118,7 @@ class Precompute {
     if (n_x > 1) {
       file << "[" << n_x << "]";
     }
+    file << "[" << VecT::type_length << "]";
     file << " = {\n";
     /* Print data formatted as C++ array. */
     for (auto w : IndexRange(n_w)) {

source/blender/draw/engines/eevee_next/eevee_shader_shared.hh
@@ -90,6 +90,8 @@ enum PrecomputeType : uint32_t {
   LUT_GGX_BRDF_SPLIT_SUM = 0u,
   LUT_GGX_BTDF_IOR_GT_ONE = 1u,
   LUT_GGX_BSDF_SPLIT_SUM = 2u,
+  LUT_BURLEY_SSS_PROFILE = 3u,
+  LUT_RANDOM_WALK_SSS_PROFILE = 4u,
 };
 
 /** \} */
@@ -1199,7 +1201,7 @@ BLI_STATIC_ASSERT_ALIGN(AOData, 16)
 #define SSS_BURLEY_TRUNCATE 16.0
 #define SSS_BURLEY_TRUNCATE_CDF 0.9963790093708328
 #define SSS_TRANSMIT_LUT_SIZE 64.0
-#define SSS_TRANSMIT_LUT_RADIUS 1.218
+#define SSS_TRANSMIT_LUT_RADIUS 2.0
 #define SSS_TRANSMIT_LUT_SCALE ((SSS_TRANSMIT_LUT_SIZE - 1.0) / float(SSS_TRANSMIT_LUT_SIZE))
 #define SSS_TRANSMIT_LUT_BIAS (0.5 / float(SSS_TRANSMIT_LUT_SIZE))
 #define SSS_TRANSMIT_LUT_STEP_RES 64.0
@@ -1216,6 +1218,37 @@ struct SubsurfaceData {
   int _pad1;
 };
 BLI_STATIC_ASSERT_ALIGN(SubsurfaceData, 16)
+
+static inline float3 burley_setup(float3 radius, float3 albedo)
+{
+  /* TODO(fclem): Avoid constant duplication. */
+  const float m_1_pi = 0.318309886183790671538;
+
+  float3 A = albedo;
+  /* Diffuse surface transmission, equation (6). */
+  float3 s = 1.9 - A + 3.5 * ((A - 0.8) * (A - 0.8));
+  /* Mean free path length adapted to fit ancient Cubic and Gaussian models. */
+  float3 l = 0.25 * m_1_pi * radius;
+
+  return l / s;
+}
+
+static inline float3 burley_eval(float3 d, float r)
+{
+  /* Slide 33. */
+  float3 exp_r_3_d;
+  /* TODO(fclem): Vectorize. */
+  exp_r_3_d.x = expf(-r / (3.0 * d.x));
+  exp_r_3_d.y = expf(-r / (3.0 * d.y));
+  exp_r_3_d.z = expf(-r / (3.0 * d.z));
+  float3 exp_r_d = exp_r_3_d * exp_r_3_d * exp_r_3_d;
+  /* NOTE:
+   * - Surface albedo is applied at the end.
+   * - This is normalized diffuse model, so the equation is multiplied
+   *   by 2*pi, which also matches `cdf()`.
+   */
+  return (exp_r_d + exp_r_3_d) / (4.0 * d);
+}
 
 /** \} */
 
@@ -1294,7 +1327,7 @@ BLI_STATIC_ASSERT_ALIGN(UniformData, 16)
 #define UTIL_LTC_MAG_LAYER 3
 #define UTIL_BSDF_LAYER 3
 #define UTIL_BTDF_LAYER 5
-#define UTIL_DISK_INTEGRAL_LAYER 4
+#define UTIL_DISK_INTEGRAL_LAYER UTIL_SSS_TRANSMITTANCE_PROFILE_LAYER
 #define UTIL_DISK_INTEGRAL_COMP 3
 
 /* __cplusplus is true when compiling with MSL, so include if inside a shader. */

source/blender/draw/engines/eevee_next/eevee_subsurface.cc
@@ -59,7 +59,7 @@ void SubsurfaceModule::render(View &view, Framebuffer &fb, Texture &diffuse_ligh
 void SubsurfaceModule::precompute_samples_location()
 {
   /* Precompute sample position with white albedo. */
-  float d = burley_setup(1.0f, 1.0f);
+  float d = burley_setup(float3(1.0f), float3(1.0f)).x;
 
   float rand_u = inst_.sampling.rng_get(SAMPLING_SSS_U);
   float rand_v = inst_.sampling.rng_get(SAMPLING_SSS_V);
@@ -69,72 +69,13 @@ void SubsurfaceModule::precompute_samples_location()
     float theta = golden_angle * i + M_PI * 2.0f * rand_u;
     /* Scale using rand_v in order to keep first sample always at center. */
     float x = (1.0f + (rand_v / data_.sample_len)) * (i / float(data_.sample_len));
-    float r = burley_sample(d, x);
+    float r = SubsurfaceModule::burley_sample(d, x);
     data_.samples[i].x = cosf(theta) * r;
     data_.samples[i].y = sinf(theta) * r;
     data_.samples[i].z = 1.0f / burley_pdf(d, r);
   }
 
   inst_.push_uniform_data();
-}
-
-const Vector<float> &SubsurfaceModule::transmittance_profile()
-{
-  static Vector<float> profile;
-  if (!profile.is_empty()) {
-    return profile;
-  }
-  profile.resize(SSS_TRANSMIT_LUT_SIZE);
-
-  /* Precompute sample position with white albedo. */
-  float radius = 1.0f;
-  float d = burley_setup(radius, 1.0f);
-
-  /* For each distance d we compute the radiance incoming from an hypothetical parallel plane. */
-  for (auto i : IndexRange(SSS_TRANSMIT_LUT_SIZE)) {
-    /* Distance from the lit surface plane.
-     * Compute to a larger maximum distance to have a smoother falloff for all channels. */
-    float lut_radius = SSS_TRANSMIT_LUT_RADIUS * radius;
-    float distance = lut_radius * (i + 1e-5f) / profile.size();
-    /* Compute radius of the footprint on the hypothetical plane. */
-    float r_fp = sqrtf(square_f(lut_radius) - square_f(distance));
-
-    profile[i] = 0.0f;
-    float area_accum = 0.0f;
-    for (auto j : IndexRange(SSS_TRANSMIT_LUT_STEP_RES)) {
-      /* Compute distance to the "shading" point through the medium. */
-      float r = (r_fp * (j + 0.5f)) / SSS_TRANSMIT_LUT_STEP_RES;
-      float r_prev = (r_fp * (j + 0.0f)) / SSS_TRANSMIT_LUT_STEP_RES;
-      float r_next = (r_fp * (j + 1.0f)) / SSS_TRANSMIT_LUT_STEP_RES;
-      r = hypotf(r, distance);
-      float R = burley_eval(d, r);
-      /* Since the profile and configuration are radially symmetrical we
-       * can just evaluate it once and weight it accordingly */
-      float disk_area = square_f(r_next) - square_f(r_prev);
-
-      profile[i] += R * disk_area;
-      area_accum += disk_area;
-    }
-    /* Normalize over the disk. */
-    profile[i] /= area_accum;
-  }
-
-  /** NOTE: There's something very wrong here.
-   * This should be a small remap,
-   * but current profile range goes from 0.0399098 to 0.0026898. */
-
-  /* Make a smooth gradient from 1 to 0. */
-  float range = profile.first() - profile.last();
-  float offset = profile.last();
-  for (float &value : profile) {
-    value = (value - offset) / range;
-    /** HACK: Remap the curve to better fit Cycles values. */
-    value = std::pow(value, 1.6f);
-  }
-  profile.first() = 1;
-  profile.last() = 0;
-
-  return profile;
 }
 
 /** \} */
@@ -146,17 +87,6 @@ const Vector<float> &SubsurfaceModule::transmittance_profile()
  * by Per Christensen
  * https://graphics.pixar.com/library/ApproxBSSRDF/approxbssrdfslides.pdf
  * \{ */
-
-float SubsurfaceModule::burley_setup(float radius, float albedo)
-{
-  float A = albedo;
-  /* Diffuse surface transmission, equation (6). */
-  float s = 1.9f - A + 3.5f * square_f(A - 0.8f);
-  /* Mean free path length adapted to fit ancient Cubic and Gaussian models. */
-  float l = 0.25 * M_1_PI * radius;
-
-  return l / s;
-}
 
 float SubsurfaceModule::burley_sample(float d, float x_rand)
 {

source/blender/draw/engines/eevee_next/eevee_subsurface.hh
@@ -49,9 +49,6 @@ struct SubsurfaceModule {
 
   ~SubsurfaceModule(){};
 
-  /** Contains translucence profile for a single color channel. */
-  static const Vector<float> &transmittance_profile();
-
   void end_sync();
 
   void render(View &view, Framebuffer &fb, Texture &diffuse_light_tx);
@@ -60,7 +57,6 @@ struct SubsurfaceModule {
   void precompute_samples_location();
 
   /** Christensen-Burley implementation. */
-  static float burley_setup(float radius, float albedo);
   static float burley_sample(float d, float x_rand);
   static float burley_eval(float d, float r);
   static float burley_pdf(float d, float r);

commit b386fe5122e1ff09879149b9ef56e8e3ca68d47e

Fix: EEVEE-Next: Irradiance Grid baking
Regression caused by fbb9679436

source/blender/draw/engines/eevee_next/shaders/eevee_surfel_list_build_comp.glsl
@@ -32,5 +32,5 @@ void main()
   surfel_buf[surfel_index].ray_distance = ray_distance;
   /* NOTE: We only need to init the `list_start_buf` to -1 for the whole list to be valid since
    * every surfel will load its `next` value from the list head. */
-  // surfel_buf[surfel_index].next = atomicExchange(list_start_buf[list_index], surfel_index);
+  surfel_buf[surfel_index].next = atomicExchange(list_start_buf[list_index], surfel_index);
 }