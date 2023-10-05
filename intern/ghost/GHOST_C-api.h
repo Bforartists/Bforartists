Bforartists/Bforartists

commit a5597495703b6a0adb7a43afed4abe3522215231

UI: Window Title With Version
Include blender version information in title (including cycle), and
also indicate unsaved and dirty status better.

Pull Request: https://projects.blender.org/blender/blender/pulls/111998

source/blender/windowmanager/intern/wm_window.cc
@@ -30,6 +30,7 @@
 
 #include "BLT_translation.h"
 
+#include "BKE_blender_version.h"
 #include "BKE_context.h"
 #include "BKE_global.h"
 #include "BKE_icons.h"
@@ -481,20 +482,23 @@ void wm_window_title(wmWindowManager *wm, wmWindow *win)
      * because #WM_window_open always sets window title. */
   }
   else if (win->ghostwin) {
-    /* this is set to 1 if you don't have startup.blend open */
-    const char *blendfile_path = BKE_main_blendfile_path_from_global();
-    if (blendfile_path[0] != '\0') {
-      char str[sizeof(Main::filepath) + 24];
-      SNPRINTF(str,
-               "Blender%s [%s%s]",
-               wm->file_saved ? "" : "*",
-               blendfile_path,
-               G_MAIN->recovered ? " (Recovered)" : "");
-      GHOST_SetTitle(static_cast<GHOST_WindowHandle>(win->ghostwin), str);
+    char str[sizeof(Main::filepath) + 24];
+    const char *filepath = BKE_main_blendfile_path_from_global();
+    char basepath[FILE_MAXDIR] = {0};
+    char filename[FILE_MAXFILE] = {0};
+    std::string location;
+    if (filepath[0]) {
+      BLI_path_split_dir_file(filepath, basepath, sizeof(basepath), filename, sizeof(filename));
+      location = " [" + std::string(basepath) + "]";
     }
-    else {
-      GHOST_SetTitle(static_cast<GHOST_WindowHandle>(win->ghostwin), "Blender");
-    }
+    SNPRINTF(str,
+             "%s %s%s%s - Blender %s",
+             wm->file_saved ? "" : "*",
+             filename[0] ? filename : IFACE_("(Unsaved)"),
+             G_MAIN->recovered ? " (Recovered)" : "",
+             basepath[0] ? location.c_str() : "",
+             BKE_blender_version_string_compact());
+    GHOST_SetTitle(static_cast<GHOST_WindowHandle>(win->ghostwin), str);
 
     /* Informs GHOST of unsaved changes, to set window modified visual indicator (macOS)
      * and to give hint of unsaved changes for a user warning mechanism in case of OS application

source/blender/blenkernel/BKE_blender_version.h
@@ -42,6 +42,9 @@ extern "C" {
 /** User readable version string. */
 const char *BKE_blender_version_string(void);
 
+/** As above but does not show patch version. */
+const char *BKE_blender_version_string_compact(void);
+
 /** Returns true when version cycle is alpha, otherwise (beta, rc) returns false. */
 bool BKE_blender_version_is_alpha(void);
 

source/blender/blenkernel/intern/blender.cc
@@ -93,6 +93,9 @@ void BKE_blender_free()
 
 static char blender_version_string[48] = "";
 
+/* Only includes patch if non-zero. */
+static char blender_version_string_compact[48] = "";
+
 static void blender_version_init()
 {
   const char *version_cycle = "";
@@ -118,11 +121,22 @@ static void blender_version_init()
            BLENDER_VERSION % 100,
            BLENDER_VERSION_PATCH,
            version_cycle);
+
+  SNPRINTF(blender_version_string_compact,
+           "%d.%01d%s",
+           BLENDER_VERSION / 100,
+           BLENDER_VERSION % 100,
+           version_cycle);
 }
 
 const char *BKE_blender_version_string()
 {
   return blender_version_string;
+}
+
+const char *BKE_blender_version_string_compact()
+{
+  return blender_version_string_compact;
 }
 
 void BKE_blender_version_blendfile_string_from_values(char *str_buff,

commit 7b539e69afab061aee6dde8042231f8e84a89170

Fix #112978: MacOS no longer has a path associated with the window
The problem was introduced in a559749570

MacOS has a different way of handling the title.

In this OS the directory is searched and taken from the title string
which is then formatted.

This limits the title format and makes it prone to errors.

This commit makes the code more generic by splitting each component of
the title beforehand and allowing the associated directory to be informed
with `GHOST_SetTitle`.

intern/ghost/GHOST_C-api.h
@@ -589,6 +589,13 @@ extern void GHOST_SetTitle(GHOST_WindowHandle windowhandle, const char *title);
 extern char *GHOST_GetTitle(GHOST_WindowHandle windowhandle);
 
 /**
+ * Sets the file name represented by this window.
+ * \param filepath: The file directory.
+ * \return Indication if the backend implements file associated with window.
+ */
+extern GHOST_TSuccess GHOST_SetPath(GHOST_WindowHandle windowhandle, const char *filepath);
+
+/**
  * Returns the window rectangle dimensions.
  * These are screen coordinates.
  * \param windowhandle: The handle to the window.

intern/ghost/GHOST_IWindow.hh
@@ -82,6 +82,12 @@ class GHOST_IWindow {
   virtual std::string getTitle() const = 0;
 
   /**
+   * Sets the file name represented by this window.
+   * \param filepath: The file directory.
+   */
+  virtual GHOST_TSuccess setPath(const char *filepath) = 0;
+
+  /**
    * Returns the window rectangle dimensions.
    * These are screen coordinates.
    * \param bounds: The bounding rectangle of the window.

intern/ghost/intern/GHOST_C-api.cc
@@ -600,6 +600,13 @@ char *GHOST_GetTitle(GHOST_WindowHandle windowhandle)
   return ctitle;
 }
 
+GHOST_TSuccess GHOST_SetPath(GHOST_WindowHandle windowhandle, const char *filepath)
+{
+  GHOST_IWindow *window = (GHOST_IWindow *)windowhandle;
+
+  return window->setPath(filepath);
+}
+
 GHOST_RectangleHandle GHOST_GetWindowBounds(GHOST_WindowHandle windowhandle)
 {
   const GHOST_IWindow *window = (const GHOST_IWindow *)windowhandle;

intern/ghost/intern/GHOST_Window.hh
@@ -84,6 +84,11 @@ class GHOST_Window : public GHOST_IWindow {
    */
   virtual void *getOSWindow() const override;
 
+  virtual GHOST_TSuccess setPath(const char * /*filepath*/) override
+  {
+    return GHOST_kFailure;
+  }
+
   /**
    * Returns the current cursor shape.
    * \return The current cursor shape.

intern/ghost/intern/GHOST_WindowCocoa.hh
@@ -86,6 +86,12 @@ class GHOST_WindowCocoa : public GHOST_Window {
   std::string getTitle() const;
 
   /**
+   * Sets the file name represented by this window.
+   * \param filepath: The file directory.
+   */
+  GHOST_TSuccess setPath(const char *filepath);
+
+  /**
    * Returns the window rectangle dimensions.
    * The dimensions are given in screen coordinates that are
    * relative to the upper-left corner of the screen.

intern/ghost/intern/GHOST_WindowCocoa.mm
@@ -494,39 +494,7 @@ void GHOST_WindowCocoa::setTitle(const char *title)
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
   NSString *windowTitle = [[NSString alloc] initWithCString:title encoding:NSUTF8StringEncoding];
-
-  // Set associated file if applicable
-  if (windowTitle && [windowTitle hasPrefix:@"Blender"]) {
-    NSRange fileStrRange;
-    NSString *associatedFileName;
-    int len;
-
-    fileStrRange.location = [windowTitle rangeOfString:@"["].location + 1;
-    len = [windowTitle rangeOfString:@"]"].location - fileStrRange.location;
-
-    if (len > 0) {
-      fileStrRange.length = len;
-      associatedFileName = [windowTitle substringWithRange:fileStrRange];
-      [m_window setTitle:[associatedFileName lastPathComponent]];
-
-      @try
-      {
-        [m_window setRepresentedFilename:associatedFileName];
-      }
-      @catch (NSException *e)
-      {
-        printf("\nInvalid file path given in window title");
-      }
-    }
-    else {
-      [m_window setTitle:windowTitle];
-      [m_window setRepresentedFilename:@""];
-    }
-  }
-  else {
-    [m_window setTitle:windowTitle];
-    [m_window setRepresentedFilename:@""];
-  }
+  [m_window setTitle:windowTitle];
 
   [windowTitle release];
   [pool drain];
@@ -548,6 +516,30 @@ std::string GHOST_WindowCocoa::getTitle() const
   [pool drain];
 
   return title;
+}
+
+GHOST_TSuccess GHOST_WindowCocoa::setPath(const char *filepath)
+{
+  GHOST_ASSERT(getValid(), "GHOST_WindowCocoa::setAssociatedFile(): window invalid");
+  GHOST_TSuccess success = GHOST_kSuccess;
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+  NSString *associatedFileName = [[NSString alloc] initWithCString:filepath encoding:NSUTF8StringEncoding];
+
+  @try
+  {
+    [m_window setRepresentedFilename:associatedFileName];
+  }
+  @catch (NSException *e)
+  {
+    printf("\nInvalid file path given for window");
+    success = GHOST_kFailure;
+  }
+
+  [associatedFileName release];
+  [pool drain];
+
+  return success;
 }
 
 void GHOST_WindowCocoa::getWindowBounds(GHOST_Rect &bounds) const

intern/ghost/intern/GHOST_WindowNULL.hh
@@ -75,6 +75,10 @@ class GHOST_WindowNULL : public GHOST_Window {
   {
     return "untitled";
   }
+  GHOST_TSuccess setPath(const char * /*filepath*/) override
+  {
+    return GHOST_kFailure;
+  }
   void getWindowBounds(GHOST_Rect &bounds) const override
   {
     getClientBounds(bounds);

source/blender/windowmanager/intern/wm_window.cc
@@ -485,28 +485,34 @@ void wm_window_title(wmWindowManager *wm, wmWindow *win)
      * because #WM_window_open always sets window title. */
   }
   else if (win->ghostwin) {
-    char str[sizeof(Main::filepath) + 24];
-    const char *filepath = BKE_main_blendfile_path_from_global();
-    const char *filename = BLI_path_basename(filepath);
-    const bool has_filepath = filepath[0] != '\0';
-    const bool has_directory = has_filepath && (filepath != filename);
-    SNPRINTF(str,
-             "%s %s%s%s%.*s%s - Blender %s",
-             wm->file_saved ? "" : "*",
-             has_filepath ? filename : IFACE_("(Unsaved)"),
-             G_MAIN->recovered ? IFACE_(" (Recovered)") : "",
-             has_directory ? " [" : "",
-             has_directory ? int(filename - filepath) : 0,
-             has_directory ? filepath : "",
-             has_directory ? "]" : "",
-             BKE_blender_version_string_compact());
-    GHOST_SetTitle(static_cast<GHOST_WindowHandle>(win->ghostwin), str);
+    GHOST_WindowHandle handle = static_cast<GHOST_WindowHandle>(win->ghostwin);
 
-    /* Informs GHOST of unsaved changes, to set window modified visual indicator (macOS)
-     * and to give hint of unsaved changes for a user warning mechanism in case of OS application
-     * terminate request (e.g. OS Shortcut Alt+F4, Command+Q, (...), or session end). */
-    GHOST_SetWindowModifiedState(static_cast<GHOST_WindowHandle>(win->ghostwin),
-                                 bool(!wm->file_saved));
+    std::string filepath = BKE_main_blendfile_path_from_global();
+    std::string filename = BLI_path_basename(filepath.c_str());
+    bool has_filepath = !filepath.empty();
+    bool include_directory = has_filepath && (filepath != filename) &&
+                             GHOST_SetPath(handle, filepath.c_str()) == GHOST_kFailure;
+
+    std::string str;
+    str += wm->file_saved ? " " : "* ";
+    str += has_filepath ? filename : IFACE_("(Unsaved)");
+    if (G_MAIN->recovered) {
+      str += IFACE_(" (Recovered)");
+    }
+
+    if (include_directory) {
+      str += " [" + filepath.substr(0, filepath.length() - filename.length()) + "]";
+    }
+
+    str += " - Blender ";
+    str += BKE_blender_version_string_compact();
+
+    GHOST_SetTitle(handle, str.c_str());
+
+    /* Informs GHOST of unsaved changes to set the window modified visual indicator (macOS)
+     * and to give a hint of unsaved changes for a user warning mechanism in case of OS application
+     * terminate request (e.g., OS Shortcut Alt+F4, Command+Q, (...) or session end). */
+    GHOST_SetWindowModifiedState(handle, static_cast<bool>(!wm->file_saved));
   }
 }
 