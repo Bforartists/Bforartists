Bforartists/Bforartists

commit 32874e59b4314d3e936b1a73034ad3a3b0d563dd

Fix 'Automatic Constraint' appearing in statusbar although not supported

source/blender/editors/transform/transform.cc
@@ -601,6 +601,7 @@ static bool transform_modal_item_poll(const wmOperator *op, int value)
     case TFM_MODAL_PLANE_X:
     case TFM_MODAL_PLANE_Y:
     case TFM_MODAL_PLANE_Z:
+    case TFM_MODAL_AUTOCONSTRAINT:
     case TFM_MODAL_AUTOCONSTRAINTPLANE: {
       if (t->flag & T_NO_CONSTRAINT) {
         return false;

commit 7598e6b4a6ab0be88fc6e95a704115466401436e

Fix #113320: 'Automatic Constraint' not working in 2D editors
Caused by 32874e59b4

The condition `(t->flag & T_2D_EDIT)` went unnoticed.

source/blender/editors/transform/transform.cc
@@ -595,24 +595,22 @@ static bool transform_modal_item_poll(const wmOperator *op, int value)
       }
       break;
     }
-    case TFM_MODAL_AXIS_X:
-    case TFM_MODAL_AXIS_Y:
     case TFM_MODAL_AXIS_Z:
     case TFM_MODAL_PLANE_X:
     case TFM_MODAL_PLANE_Y:
     case TFM_MODAL_PLANE_Z:
+    case TFM_MODAL_AUTOCONSTRAINTPLANE:
+      if (t->flag & T_2D_EDIT) {
+        return false;
+      }
+      [[fallthrough]];
+    case TFM_MODAL_AXIS_X:
+    case TFM_MODAL_AXIS_Y:
     case TFM_MODAL_AUTOCONSTRAINT:
-    case TFM_MODAL_AUTOCONSTRAINTPLANE: {
       if (t->flag & T_NO_CONSTRAINT) {
         return false;
       }
-      if (!ELEM(value, TFM_MODAL_AXIS_X, TFM_MODAL_AXIS_Y)) {
-        if (t->flag & T_2D_EDIT) {
-          return false;
-        }
-      }
       break;
-    }
     case TFM_MODAL_CONS_OFF: {
       if ((t->con.mode & CON_APPLY) == 0) {
         return false;