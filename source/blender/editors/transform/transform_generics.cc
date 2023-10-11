Bforartists/Bforartists

commit b8391c09783d2368b91edd120fae15937b78d462

Fix #110722: Wrong value when starting a transformation
Caused by c71e7ce03f.

That commit accidentally removed the `copy_v2_v2_int(t->mval, mval);`
line which was responsible for initializing the value of `mval`.

source/blender/editors/transform/transform_generics.cc
@@ -186,7 +186,7 @@ void initTransInfo(bContext *C, TransInfo *t, wmOperator *op, const wmEvent *eve
     mval = float2(0, 0);
   }
 
-  t->mouse.imval = mval;
+  t->mval = mval;
 
   t->mode_info = nullptr;
 

source/blender/editors/transform/transform.cc
@@ -2023,14 +2023,7 @@ bool initTransform(bContext *C, TransInfo *t, wmOperator *op, const wmEvent *eve
       }
     }
 
-    float2 mval;
-    if (t->flag & T_EVENT_DRAG_START) {
-      WM_event_drag_start_mval_fl(event, t->region, mval);
-    }
-    else {
-      mval = float2(event->mval);
-    }
-    initMouseInput(t, &t->mouse, t->center2d, mval, use_accurate);
+    initMouseInput(t, &t->mouse, t->center2d, t->mval, use_accurate);
   }
 
   transform_mode_init(t, op, mode);

commit 632e1ecd703bc1baca45640cd7d325151f056b4f

Fix 'Transform Extend' not identifying the correct transformation side
Caused by b8391c0978.

`t->mouse.imval` was being used before initialization.

source/blender/editors/transform/transform_generics.cc
@@ -189,6 +189,10 @@ void initTransInfo(bContext *C, TransInfo *t, wmOperator *op, const wmEvent *eve
 
   t->mval = mval;
 
+  /* Initialize this mouse variable in advance as it is required by
+   * `transform_convert_frame_side_dir_get` which is called before `initMouseInput`. */
+  t->mouse.imval = mval;
+
   t->mode_info = nullptr;
 
   t->data_len_all = 0;