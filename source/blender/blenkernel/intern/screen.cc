Bforartists/Bforartists

commit a0161e5e72b9805eaf9e999fb6e85e1053d23866

Fix: copying panels in regions did not handle runtime data
Caused by 69fb9dd2.

source/blender/blenkernel/intern/screen.cc
@@ -315,16 +315,14 @@ void BKE_spacedata_freelist(ListBase *lb)
 static void panel_list_copy(ListBase *newlb, const ListBase *lb)
 {
   BLI_listbase_clear(newlb);
-  BLI_duplicatelist(newlb, lb);
 
-  /* copy panel pointers */
-  Panel *new_panel = static_cast<Panel *>(newlb->first);
-  Panel *panel = static_cast<Panel *>(lb->first);
-  for (; new_panel; new_panel = new_panel->next, panel = panel->next) {
+  LISTBASE_FOREACH (const Panel *, old_panel, lb) {
+    Panel *new_panel = BKE_panel_new(old_panel->type);
+    *new_panel = *old_panel;
     new_panel->activedata = nullptr;
     new_panel->drawname = nullptr;
-    memset(&new_panel->runtime, 0x0, sizeof(new_panel->runtime));
-    panel_list_copy(&new_panel->children, &panel->children);
+    BLI_addtail(newlb, new_panel);
+    panel_list_copy(&new_panel->children, &old_panel->children);
   }
 }
 
@@ -362,6 +360,9 @@ ARegion *BKE_area_region_copy(const SpaceType *st, const ARegion *region)
   }
 
   panel_list_copy(&newar->panels, &region->panels);
+  LISTBASE_FOREACH (Panel *, p, &newar->panels) {
+    BLI_assert(p->runtime);
+  }
 
   BLI_listbase_clear(&newar->ui_previews);
   BLI_duplicatelist(&newar->ui_previews, &region->ui_previews);

commit beba62f2dabad3d8c105bde22caff1c4896a8dd8

Fix #113545: crash when trying to create panel with unknown type
Caused by a0161e5e72b9805eaf9e999fb6e85e1053d23866.

source/blender/blenkernel/intern/screen.cc
@@ -490,7 +490,9 @@ Panel *BKE_panel_new(PanelType *panel_type)
   Panel *panel = MEM_cnew<Panel>(__func__);
   panel->runtime = MEM_new<Panel_Runtime>(__func__);
   panel->type = panel_type;
-  STRNCPY(panel->panelname, panel_type->idname);
+  if (panel_type) {
+    STRNCPY(panel->panelname, panel_type->idname);
+  }
   return panel;
 }
 