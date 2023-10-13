Bforartists/Bforartists

commit ac36a3e9e8e6fd6667d45ae966572f675341014a

EEVEE-Next: Remove common lib usage
Replaces all usage by the the gpu_shader_math
equivalent. This is because the old shader
library was quite tangled.

This avoids dependency hell trying to
mix libraries.

Changes are split into isolated commits until
I had to do mass changes because of inter-
dependencies.

Pull Request: https://projects.blender.org/blender/blender/pulls/113631

source/blender/draw/engines/eevee_next/shaders/eevee_surf_world_frag.glsl
@@ -8,11 +8,11 @@
  * Outputs shading parameter per pixel using a set of randomized BSDFs.
  */
 
-#pragma BLENDER_REQUIRE(common_view_lib.glsl)
-#pragma BLENDER_REQUIRE(common_math_lib.glsl)
+#pragma BLENDER_REQUIRE(draw_view_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_attributes_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_surf_lib.glsl)
 #pragma BLENDER_REQUIRE(eevee_nodetree_lib.glsl)
+#pragma BLENDER_REQUIRE(eevee_colorspace_lib.glsl)
 
 void main()
 {
@@ -21,7 +21,7 @@ void main()
 
   init_globals();
   /* View position is passed to keep accuracy. */
-  g_data.N = normal_view_to_world(viewCameraVec(interp.P));
+  g_data.N = normal_view_to_world(drw_view_incident_vector(interp.P));
   g_data.Ng = g_data.N;
   g_data.P = -g_data.N;
   attrib_load();
@@ -30,8 +30,8 @@ void main()
 
   g_holdout = saturate(g_holdout);
 
-  out_background.rgb = safe_color(g_emission) * (1.0 - g_holdout);
-  out_background.a = saturate(avg(g_transmittance)) * g_holdout;
+  out_background.rgb = colorspace_safe_color(g_emission) * (1.0 - g_holdout);
+  out_background.a = saturate(average(g_transmittance)) * g_holdout;
 
   /* World opacity. */
   out_background = mix(vec4(0.0, 0.0, 0.0, 1.0), out_background, world_opacity_fade);

source/blender/draw/intern/shaders/draw_view_lib.glsl
@@ -1,0 +1,188 @@
+/* SPDX-FileCopyrightText: 2018-2023 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef DRAW_VIEW_CREATE_INFO
+#  error Missing draw_view additional create info on shader create info
+#endif
+
+/* Returns true if the current view has a perspective projection matrix. */
+bool drw_view_is_perspective()
+{
+  return drw_view.winmat[3][3] == 0.0;
+}
+
+/* Returns the view forward vector, going towards the viewer. */
+vec3 drw_view_forward()
+{
+  return drw_view.viewinv[2].xyz;
+}
+
+/* Returns the view origin. */
+vec3 drw_view_position()
+{
+  return drw_view.viewinv[3].xyz;
+}
+
+/* Returns the projection matrix far clip distance. */
+float drw_view_far()
+{
+  if (drw_view_is_perspective()) {
+    return -drw_view.winmat[3][2] / (drw_view.winmat[2][2] + 1.0);
+  }
+  return -(drw_view.winmat[3][2] - 1.0) / drw_view.winmat[2][2];
+}
+
+/* Returns the projection matrix near clip distance. */
+float drw_view_near()
+{
+  if (drw_view_is_perspective()) {
+    return -drw_view.winmat[3][2] / (drw_view.winmat[2][2] - 1.0);
+  }
+  return -(drw_view.winmat[3][2] + 1.0) / drw_view.winmat[2][2];
+}
+
+/**
+ * Returns the world incident vector `V` (going towards the viewer)
+ * from the world position `P` and the current view.
+ */
+vec3 drw_world_incident_vector(vec3 P)
+{
+  return drw_view_is_perspective() ? normalize(drw_view_position() - P) : drw_view_forward();
+}
+
+/**
+ * Returns the view incident vector `vV` (going towards the viewer)
+ * from the view position `vP` and the current view.
+ */
+vec3 drw_view_incident_vector(vec3 vP)
+{
+  return drw_view_is_perspective() ? normalize(vP) : vec3(0.0, 0.0, -1.0);
+}
+
+/**
+ * Transform position on screen UV space [0..1] to Normalized Device Coordinate space [-1..1].
+ */
+vec3 drw_screen_to_ndc(vec3 ss_P)
+{
+  return ss_P * 2.0 - 1.0;
+}
+vec2 drw_screen_to_ndc(vec2 ss_P)
+{
+  return ss_P * 2.0 - 1.0;
+}
+float drw_screen_to_ndc(float ss_P)
+{
+  return ss_P * 2.0 - 1.0;
+}
+
+/**
+ * Transform position in Normalized Device Coordinate [-1..1] to screen UV space [0..1].
+ */
+vec3 drw_ndc_to_screen(vec3 ndc_P)
+{
+  return ndc_P * 0.5 + 0.5;
+}
+vec2 drw_ndc_to_screen(vec2 ndc_P)
+{
+  return ndc_P * 0.5 + 0.5;
+}
+float drw_ndc_to_screen(float ndc_P)
+{
+  return ndc_P * 0.5 + 0.5;
+}
+
+/* -------------------------------------------------------------------- */
+/** \name Transform Normal
+ * \{ */
+
+vec3 drw_normal_view_to_world(vec3 vN)
+{
+  return (mat3x3(drw_view.viewinv) * vN);
+}
+
+vec3 drw_normal_world_to_view(vec3 N)
+{
+  return (mat3x3(drw_view.viewmat) * N);
+}
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Transform Position
+ * \{ */
+
+vec3 drw_perspective_divide(vec4 hs_P)
+{
+  return hs_P.xyz / hs_P.w;
+}
+
+vec3 drw_point_view_to_world(vec3 vP)
+{
+  return (drw_view.viewinv * vec4(vP, 1.0)).xyz;
+}
+vec4 drw_point_view_to_homogenous(vec3 vP)
+{
+  return (drw_view.winmat * vec4(vP, 1.0));
+}
+vec3 drw_point_view_to_ndc(vec3 vP)
+{
+  return drw_perspective_divide(drw_point_view_to_homogenous(vP));
+}
+
+vec3 drw_point_world_to_view(vec3 P)
+{
+  return (drw_view.viewmat * vec4(P, 1.0)).xyz;
+}
+vec4 drw_point_world_to_homogenous(vec3 P)
+{
+  return (drw_view.winmat * (drw_view.viewmat * vec4(P, 1.0)));
+}
+vec3 drw_point_world_to_ndc(vec3 P)
+{
+  return drw_perspective_divide(drw_point_world_to_homogenous(P));
+}
+
+vec3 drw_point_ndc_to_view(vec3 ssP)
+{
+  return drw_perspective_divide(drw_view.wininv * vec4(ssP, 1.0));
+}
+vec3 drw_point_ndc_to_world(vec3 ssP)
+{
+  return drw_point_view_to_world(drw_point_ndc_to_view(ssP));
+}
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
+/** \name Transform Screen Postions
+ * \{ */
+
+vec3 drw_point_view_to_screen(vec3 vP)
+{
+  return drw_ndc_to_screen(drw_point_view_to_ndc(vP));
+}
+vec3 drw_point_world_to_screen(vec3 vP)
+{
+  return drw_ndc_to_screen(drw_point_world_to_ndc(vP));
+}
+
+vec3 drw_point_screen_to_view(vec3 ssP)
+{
+  return drw_point_ndc_to_view(drw_screen_to_ndc(ssP));
+}
+vec3 drw_point_screen_to_world(vec3 ssP)
+{
+  return drw_point_view_to_world(drw_point_screen_to_view(ssP));
+}
+
+float drw_depth_view_to_screen(float v_depth)
+{
+  return drw_point_view_to_screen(vec3(0.0, 0.0, v_depth)).z;
+}
+float drw_depth_screen_to_view(float ss_depth)
+{
+  return drw_point_screen_to_view(vec3(0.0, 0.0, ss_depth)).z;
+}
+
+/** \} */

commit 2bffadd8342c3aff5ae32a82fb6925af47416d24

Fix EEVEE-Next: Inverted world coordinates
Caused by ac36a3e9e8e6fd6667d45ae966572f675341014a

source/blender/draw/engines/eevee_next/shaders/eevee_surf_world_frag.glsl
@@ -21,7 +21,7 @@ void main()
 
   init_globals();
   /* View position is passed to keep accuracy. */
-  g_data.N = normal_view_to_world(drw_view_incident_vector(interp.P));
+  g_data.N = drw_normal_view_to_world(drw_view_incident_vector(interp.P));
   g_data.Ng = g_data.N;
   g_data.P = -g_data.N;
   attrib_load();

source/blender/draw/intern/shaders/draw_view_lib.glsl
@@ -57,7 +57,7 @@ vec3 drw_world_incident_vector(vec3 P)
  */
 vec3 drw_view_incident_vector(vec3 vP)
 {
-  return drw_view_is_perspective() ? normalize(vP) : vec3(0.0, 0.0, -1.0);
+  return drw_view_is_perspective() ? normalize(-vP) : vec3(0.0, 0.0, 1.0);
 }
 
 /**