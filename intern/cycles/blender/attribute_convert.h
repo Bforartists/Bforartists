Bforartists/Bforartists

commit 1972896dac4c99eda3ac8d023cf5a4f30c13c88f

Cycles: Use Blender headers to access geometry data, avoid copy
Since 7e9d47f68d8bf50c64d9, attributes are always made mutable when
accessed from the RNA API. This can result in unnecessary copies, which
increases memory usage and reduces performance.

Cycles is the only user of the C++ RNA API, which we'd like to remove
in the future since it doesn't really make sense in the big picture.
Hydra is now a better alternative for external render engines.

To start that change and fix the unnecessary copies, this commit
moves to use Blender headers directly for accessing attribute and
other geometry data. This also removes the few places that still had
overhead from the RNA API after the changes ([0]) in 3.6. In a simple
test with a large grid, I observed a 1.76x performance improvement,
from 1.04 to 0.59 seconds to extract the mesh data to Cycles.

[0]: https://wiki.blender.org/wiki/Reference/Release_Notes/3.6/Cycles#Performance

Pull Request: https://projects.blender.org/blender/blender/pulls/112306

intern/cycles/blender/attribute_convert.h
@@ -1,0 +1,94 @@
+/* SPDX-FileCopyrightText: 2023 Blender Foundation
+ *
+ * SPDX-License-Identifier: Apache-2.0 */
+
+#ifndef __BLENDER_ATTRIBUTE_CONVERT_H__
+#define __BLENDER_ATTRIBUTE_CONVERT_H__
+
+#include "util/array.h"
+#include "util/color.h"
+#include "util/param.h"
+#include "util/types.h"
+
+#include "BKE_attribute.hh"
+#include "BLI_math_color.hh"
+#include "BLI_math_quaternion_types.hh"
+#include "BLI_math_vector_types.hh"
+
+CCL_NAMESPACE_BEGIN
+
+template<typename BlenderT> struct AttributeConverter {
+  using CyclesT = void;
+};
+
+template<> struct AttributeConverter<float> {
+  using CyclesT = float;
+  static constexpr auto type_desc = TypeFloat;
+  static CyclesT convert(const float &value)
+  {
+    return value;
+  }
+};
+template<> struct AttributeConverter<int> {
+  using CyclesT = float;
+  static constexpr auto type_desc = TypeFloat;
+  static CyclesT convert(const int &value)
+  {
+    return float(value);
+  }
+};
+template<> struct AttributeConverter<blender::float3> {
+  using CyclesT = float3;
+  static constexpr auto type_desc = TypeVector;
+  static CyclesT convert(const blender::float3 &value)
+  {
+    return make_float3(value[0], value[1], value[2]);
+  }
+};
+template<> struct AttributeConverter<blender::ColorGeometry4f> {
+  using CyclesT = float4;
+  static constexpr auto type_desc = TypeRGBA;
+  static CyclesT convert(const blender::ColorGeometry4f &value)
+  {
+    return make_float4(value[0], value[1], value[2], value[3]);
+  }
+};
+template<> struct AttributeConverter<blender::ColorGeometry4b> {
+  using CyclesT = float4;
+  static constexpr auto type_desc = TypeRGBA;
+  static CyclesT convert(const blender::ColorGeometry4b &value)
+  {
+    return color_srgb_to_linear(make_float4(byte_to_float(value[0]),
+                                            byte_to_float(value[1]),
+                                            byte_to_float(value[2]),
+                                            byte_to_float(value[3])));
+  }
+};
+template<> struct AttributeConverter<bool> {
+  using CyclesT = float;
+  static constexpr auto type_desc = TypeFloat;
+  static CyclesT convert(const bool &value)
+  {
+    return float(value);
+  }
+};
+template<> struct AttributeConverter<int8_t> {
+  using CyclesT = float;
+  static constexpr auto type_desc = TypeFloat;
+  static CyclesT convert(const int8_t &value)
+  {
+    return float(value);
+  }
+};
+template<> struct AttributeConverter<blender::math::Quaternion> {
+  using CyclesT = float4;
+  static constexpr auto type_desc = TypeFloat4;
+  static CyclesT convert(const blender::math::Quaternion &value)
+  {
+    return make_float4(value.w, value.x, value.y, value.z);
+  }
+};
+
+CCL_NAMESPACE_END
+
+#endif /* __BLENDER_ATTRIBUTE_CONVERT_H__ */

intern/cycles/blender/CMakeLists.txt
@@ -41,6 +41,7 @@ set(SRC
   viewport.cpp
   volume.cpp
 
+  attribute_convert.h
   CCL_api.h
   device.h
   display_driver.h

intern/cycles/blender/pointcloud.cpp
@@ -8,6 +8,7 @@
 #include "scene/pointcloud.h"
 #include "scene/scene.h"
 
+#include "blender/attribute_convert.h"
 #include "blender/sync.h"
 #include "blender/util.h"
 
@@ -15,19 +16,16 @@
 #include "util/foreach.h"
 #include "util/hash.h"
 
+#include "BKE_attribute.hh"
+#include "BKE_attribute_math.hh"
+#include "BKE_pointcloud.h"
+
 CCL_NAMESPACE_BEGIN
 
 static void attr_create_motion(PointCloud *pointcloud,
-                               BL::Attribute &b_attribute,
+                               const blender::Span<blender::float3> b_attribute,
                                const float motion_scale)
 {
-  if (!(b_attribute.domain() == BL::Attribute::domain_POINT) &&
-      (b_attribute.data_type() == BL::Attribute::data_type_FLOAT_VECTOR))
-  {
-    return;
-  }
-
-  BL::FloatVectorAttribute b_vector_attribute(b_attribute);
   const int num_points = pointcloud->get_points().size();
 
   /* Find or add attribute */
@@ -46,196 +44,91 @@ static void attr_create_motion(PointCloud *pointcloud,
     float4 *mP = attr_mP->data_float4() + step * num_points;
 
     for (int i = 0; i < num_points; i++) {
-      float3 Pi = P[i] + get_float3(b_vector_attribute.data[i].vector()) * relative_time;
+      float3 Pi = P[i] + make_float3(b_attribute[i][0], b_attribute[i][1], b_attribute[i][2]) *
+                             relative_time;
       mP[i] = make_float4(Pi.x, Pi.y, Pi.z, radius[i]);
     }
   }
 }
 
 static void copy_attributes(PointCloud *pointcloud,
-                            BL::PointCloud b_pointcloud,
+                            const ::PointCloud &b_pointcloud,
                             const bool need_motion,
                             const float motion_scale)
 {
-  const int num_points = b_pointcloud.points.length();
-  if (num_points == 0) {
+  const blender::bke::AttributeAccessor b_attributes = b_pointcloud.attributes();
+  if (b_attributes.domain_size(ATTR_DOMAIN_POINT) == 0) {
     return;
   }
 
   AttributeSet &attributes = pointcloud->attributes;
   static const ustring u_velocity("velocity");
-  for (BL::Attribute &b_attribute : b_pointcloud.attributes) {
-    const ustring name{b_attribute.name().c_str()};
+  b_attributes.for_all([&](const blender::bke::AttributeIDRef &id,
+                           const blender::bke::AttributeMetaData /*meta_data*/) {
+    const ustring name{std::string_view(id.name())};
 
     if (need_motion && name == u_velocity) {
-      attr_create_motion(pointcloud, b_attribute, motion_scale);
+      const blender::VArraySpan b_attr = *b_attributes.lookup<blender::float3>(id);
+      attr_create_motion(pointcloud, b_attr, motion_scale);
     }
 
     if (attributes.find(name)) {
-      continue;
+      return true;
     }
 
-    const AttributeElement element = ATTR_ELEMENT_VERTEX;
-    const BL::Attribute::data_type_enum b_data_type = b_attribute.data_type();
-    switch (b_data_type) {
-      case BL::Attribute::data_type_FLOAT: {
-        BL::FloatAttribute b_float_attribute{b_attribute};
-        const float *src = static_cast<const float *>(b_float_attribute.data[0].ptr.data);
-        Attribute *attr = attributes.add(name, TypeFloat, element);
-        float *data = attr->data_float();
-        std::copy(src, src + num_points, data);
-        break;
-      }
-      case BL::Attribute::data_type_BOOLEAN: {
-        BL::BoolAttribute b_bool_attribute{b_attribute};
-        const bool *src = static_cast<const bool *>(b_bool_attribute.data[0].ptr.data);
-        Attribute *attr = attributes.add(name, TypeFloat, element);
-        float *data = attr->data_float();
-        for (int i = 0; i < num_points; i++) {
-          data[i] = float(src[i]);
-        }
-        break;
-      }
-      case BL::Attribute::data_type_INT: {
-        BL::IntAttribute b_int_attribute{b_attribute};
-        const int *src = static_cast<const int *>(b_int_attribute.data[0].ptr.data);
-        Attribute *attr = attributes.add(name, TypeFloat, element);
-        float *data = attr->data_float();
-        for (int i = 0; i < num_points; i++) {
-          data[i] = float(src[i]);
-        }
-        break;
-      }
-      case BL::Attribute::data_type_INT32_2D: {
-        BL::Int2Attribute b_int2_attribute{b_attribute};
-        const int2 *src = static_cast<const int2 *>(b_int2_attribute.data[0].ptr.data);
-        Attribute *attr = attributes.add(name, TypeFloat2, element);
-        float2 *data = attr->data_float2();
-        for (int i = 0; i < num_points; i++) {
-          data[i] = make_float2(float(src[i][0]), float(src[i][1]));
-        }
-        break;
-      }
-      case BL::Attribute::data_type_FLOAT_VECTOR: {
-        BL::FloatVectorAttribute b_vector_attribute{b_attribute};
-        const float(*src)[3] = static_cast<const float(*)[3]>(b_vector_attribute.data[0].ptr.data);
-        Attribute *attr = attributes.add(name, TypeVector, element);
-        float3 *data = attr->data_float3();
-        for (int i = 0; i < num_points; i++) {
-          data[i] = make_float3(src[i][0], src[i][1], src[i][2]);
-        }
-        break;
-      }
-      case BL::Attribute::data_type_BYTE_COLOR: {
-        BL::ByteColorAttribute b_color_attribute{b_attribute};
-        const uchar(*src)[4] = static_cast<const uchar(*)[4]>(b_color_attribute.data[0].ptr.data);
-        Attribute *attr = attributes.add(name, TypeRGBA, element);
-        float4 *data = attr->data_float4();
-        for (int i = 0; i < num_points; i++) {
-          data[i] = make_float4(color_srgb_to_linear(byte_to_float(src[i][0])),
-                                color_srgb_to_linear(byte_to_float(src[i][1])),
-                                color_srgb_to_linear(byte_to_float(src[i][2])),
-                                color_srgb_to_linear(byte_to_float(src[i][3])));
-        }
-        break;
-      }
-      case BL::Attribute::data_type_FLOAT_COLOR: {
-        BL::FloatColorAttribute b_color_attribute{b_attribute};
-        const float(*src)[4] = static_cast<const float(*)[4]>(b_color_attribute.data[0].ptr.data);
-        Attribute *attr = attributes.add(name, TypeRGBA, element);
-        float4 *data = attr->data_float4();
-        for (int i = 0; i < num_points; i++) {
-          data[i] = make_float4(src[i][0], src[i][1], src[i][2], src[i][3]);
-        }
-        break;
-      }
-      case BL::Attribute::data_type_FLOAT2: {
-        BL::Float2Attribute b_float2_attribute{b_attribute};
-        const float(*src)[2] = static_cast<const float(*)[2]>(b_float2_attribute.data[0].ptr.data);
-        Attribute *attr = attributes.add(name, TypeFloat2, element);
-        float2 *data = attr->data_float2();
-        for (int i = 0; i < num_points; i++) {
-          data[i] = make_float2(src[i][0], src[i][1]);
-        }
-        break;
-      }
-      default:
-        /* Not supported. */
-        break;
-    }
-  }
-}
+    const blender::bke::GAttributeReader b_attr = b_attributes.lookup(id);
+    blender::bke::attribute_math::convert_to_static_type(b_attr.varray.type(), [&](auto dummy) {
+      using BlenderT = decltype(dummy);
+      using Converter = typename ccl::AttributeConverter<BlenderT>;
+      using CyclesT = typename Converter::CyclesT;
+      if constexpr (!std::is_void_v<CyclesT>) {
+        Attribute *attr = attributes.add(name, Converter::type_desc, ATTR_ELEMENT_VERTEX);
+        CyclesT *data = reinterpret_cast<CyclesT *>(attr->data());
 
-static const float *find_radius_attribute(BL::PointCloud b_pointcloud)
-{
-  for (BL::Attribute &b_attribute : b_pointcloud.attributes) {
-    if (b_attribute.name() != "radius") {
-      continue;
-    }
-    if (b_attribute.data_type() != BL::Attribute::data_type_FLOAT) {
-      continue;
-    }
-    BL::FloatAttribute b_float_attribute{b_attribute};
-    if (b_float_attribute.data.length() == 0) {
-      return nullptr;
-    }
-    return static_cast<const float *>(b_float_attribute.data[0].ptr.data);
-  }
-  return nullptr;
-}
+        const blender::VArraySpan src = b_attr.varray.typed<BlenderT>();
+        for (const int i : src.index_range()) {
+          data[i] = Converter::convert(src[i]);
+        }
+      }
+    });
 
-static const float (*find_position_attribute(BL::PointCloud b_pointcloud))[3]
-{
-  for (BL::Attribute &b_attribute : b_pointcloud.attributes) {
-    if (b_attribute.name() != "position") {
-      continue;
-    }
-    if (b_attribute.data_type() != BL::Attribute::data_type_FLOAT_VECTOR) {
-      continue;
-    }
-    BL::FloatVectorAttribute b_float3_attribute{b_attribute};
-    if (b_float3_attribute.data.length() == 0) {
-      return nullptr;
-    }
-    return static_cast<const float(*)[3]>(b_float3_attribute.data[0].ptr.data);
-  }
-  /* The position attribute must exist. */
-  assert(false);
-  return nullptr;
+    return true;
+  });
 }
 
 static void export_pointcloud(Scene *scene,
                               PointCloud *pointcloud,
-                              BL::PointCloud b_pointcloud,
+                              const ::PointCloud &b_pointcloud,
                               const bool need_motion,
                               const float motion_scale)
 {
-  const int num_points = b_pointcloud.points.length();
-  pointcloud->resize(num_points);
+  const blender::Span<blender::float3> b_positions = b_pointcloud.positions();
+  const blender::VArraySpan b_radius = *b_pointcloud.attributes().lookup<float>("radius",
+                                                                                ATTR_DOMAIN_POINT);
 
-  const float(*b_attr_position)[3] = find_position_attribute(b_pointcloud);
+  pointcloud->resize(b_positions.size());
+
   float3 *points = pointcloud->get_points().data();
 
-  for (int i = 0; i < num_points; i++) {
-    points[i] = make_float3(b_attr_position[i][0], b_attr_position[i][1], b_attr_position[i][2]);
+  for (const int i : b_positions.index_range()) {
+    points[i] = make_float3(b_positions[i][0], b_positions[i][1], b_positions[i][2]);
   }
 
-  const float *b_attr_radius = find_radius_attribute(b_pointcloud);
   float *radius = pointcloud->get_radius().data();
-  if (b_attr_radius) {
-    std::copy(b_attr_radius, b_attr_radius + num_points, radius);
+  if (!b_radius.is_empty()) {
+    std::copy(b_radius.data(), b_radius.data() + b_positions.size(), radius);
   }
   else {
-    std::fill(radius, radius + num_points, 0.01f);
+    std::fill(radius, radius + b_positions.size(), 0.01f);
   }
 
   int *shader = pointcloud->get_shader().data();
-  std::fill(shader, shader + num_points, 0);
+  std::fill(shader, shader + b_positions.size(), 0);
 
   if (pointcloud->need_attribute(scene, ATTR_STD_POINT_RANDOM)) {
     Attribute *attr_random = pointcloud->attributes.add(ATTR_STD_POINT_RANDOM);
     float *data = attr_random->data_float();
-    for (int i = 0; i < num_points; i++) {
+    for (const int i : b_positions.index_range()) {
       data[i] = hash_uint2_to_float(i, 0);
     }
   }
@@ -244,7 +137,7 @@ static void export_pointcloud(Scene *scene,
 }
 
 static void export_pointcloud_motion(PointCloud *pointcloud,
-                                     BL::PointCloud b_pointcloud,
+                                     const ::PointCloud &b_pointcloud,
                                      int motion_step)
 {
   /* Find or add attribute. */
@@ -264,21 +157,20 @@ static void export_pointcloud_motion(PointCloud *pointcloud,
   bool have_motion = false;
   const array<float3> &pointcloud_points = pointcloud->get_points();
 
-  const int b_points_num = b_pointcloud.points.length();
-  const float(*b_attr_position)[3] = find_position_attribute(b_pointcloud);
-  const float *b_attr_radius = find_radius_attribute(b_pointcloud);
+  const blender::Span<blender::float3> b_positions = b_pointcloud.positions();
+  const blender::VArraySpan b_radius = *b_pointcloud.attributes().lookup<float>("radius",
+                                                                                ATTR_DOMAIN_POINT);
 
-  for (int i = 0; i < std::min(num_points, b_points_num); i++) {
-    const float3 P = make_float3(
-        b_attr_position[i][0], b_attr_position[i][1], b_attr_position[i][2]);
-    const float radius = b_attr_radius ? b_attr_radius[i] : 0.01f;
+  for (int i = 0; i < std::min<int>(num_points, b_positions.size()); i++) {
+    const float3 P = make_float3(b_positions[i][0], b_positions[i][1], b_positions[i][2]);
+    const float radius = b_radius.is_empty() ? 0.01f : b_radius[i];
     mP[i] = make_float4(P.x, P.y, P.z, radius);
     have_motion = have_motion || (P != pointcloud_points[i]);
   }
 
   /* In case of new attribute, we verify if there really was any motion. */
   if (new_attribute) {
-    if (b_points_num != num_points || !have_motion) {
+    if (b_positions.size() != num_points || !have_motion) {
       pointcloud->attributes.remove(ATTR_STD_MOTION_VERTEX_POSITION);
     }
     else if (motion_step > 0) {
@@ -311,7 +203,11 @@ void BlenderSync::sync_pointcloud(PointCloud *pointcloud, BObjectInfo &b_ob_info
                                  scene->motion_shutter_time() /
                                      (b_scene.render().fps() / b_scene.render().fps_base()) :
                                  0.0f;
-  export_pointcloud(scene, &new_pointcloud, b_pointcloud, need_motion, motion_scale);
+  export_pointcloud(scene,
+                    &new_pointcloud,
+                    *static_cast<const ::PointCloud *>(b_pointcloud.ptr.data),
+                    need_motion,
+                    motion_scale);
 
   /* Update original sockets. */
   for (const SocketType &socket : new_pointcloud.type->inputs) {
@@ -347,7 +243,8 @@ void BlenderSync::sync_pointcloud_motion(PointCloud *pointcloud,
   if (ccl::BKE_object_is_deform_modified(b_ob_info, b_scene, preview)) {
     /* PointCloud object. */
     BL::PointCloud b_pointcloud(b_ob_info.object_data);
-    export_pointcloud_motion(pointcloud, b_pointcloud, motion_step);
+    export_pointcloud_motion(
+        pointcloud, *static_cast<const ::PointCloud *>(b_pointcloud.ptr.data), motion_step);
   }
   else {
     /* No deformation on this frame, copy coordinates if other frames did have it. */

intern/cycles/blender/util.h
@@ -17,6 +17,8 @@
 #include "util/types.h"
 #include "util/vector.h"
 
+#include "BKE_mesh.hh"
+
 /* Hacks to hook into Blender API
  * todo: clean this up ... */
 
@@ -524,10 +526,13 @@ static inline string get_text_datablock_content(const PointerRNA &ptr)
 
 /* Texture Space */
 
-static inline void mesh_texture_space(BL::Mesh &b_mesh, float3 &loc, float3 &size)
+static inline void mesh_texture_space(const ::Mesh &b_mesh, float3 &loc, float3 &size)
 {
-  loc = get_float3(b_mesh.texspace_location());
-  size = get_float3(b_mesh.texspace_size());
+  float texspace_location[3], texspace_size[3];
+  BKE_mesh_texspace_get(const_cast<::Mesh *>(&b_mesh), texspace_location, texspace_size);
+
+  loc = make_float3(texspace_location[0], texspace_location[1], texspace_location[2]);
+  size = make_float3(texspace_size[0], texspace_size[1], texspace_size[2]);
 
   if (size.x != 0.0f)
     size.x = 0.5f / size.x;

intern/cycles/blender/volume.cpp
@@ -25,8 +25,8 @@ class BlenderSmokeLoader : public ImageLoader {
   BlenderSmokeLoader(BL::Object &b_ob, AttributeStandard attribute)
       : b_domain(object_fluid_gas_domain_find(b_ob)), attribute(attribute)
   {
-    BL::Mesh b_mesh(b_ob.data());
-    mesh_texture_space(b_mesh, texspace_loc, texspace_size);
+    mesh_texture_space(
+        *static_cast<const ::Mesh *>(b_ob.data().ptr.data), texspace_loc, texspace_size);
   }
 
   bool load_metadata(const ImageDeviceFeatures &, ImageMetaData &metadata) override

source/blender/blenkernel/BKE_curves.hh
@@ -443,6 +443,8 @@ class CurvesEditHints {
   bool is_valid() const;
 };
 
+void curves_normals_point_domain_calc(const CurvesGeometry &curves, MutableSpan<float3> normals);
+
 namespace curves {
 
 /* -------------------------------------------------------------------- */

commit 2db4d704e39f13f60a15b2d5917b9305803b9c71

Fix #113763: Float2 attributes on curves not rendering in Cycles
Regression from 1972896dac.

Above commit was missing an AttributeConverter for `blender::float2`,
now added.

Pull Request: https://projects.blender.org/blender/blender/pulls/113884

intern/cycles/blender/attribute_convert.h
@@ -37,6 +37,14 @@ template<> struct AttributeConverter<int> {
     return float(value);
   }
 };
+template<> struct AttributeConverter<blender::float2> {
+  using CyclesT = float2;
+  static constexpr auto type_desc = TypeFloat2;
+  static CyclesT convert(const blender::float2 &value)
+  {
+    return make_float2(value[0], value[1]);
+  }
+};
 template<> struct AttributeConverter<blender::float3> {
   using CyclesT = float3;
   static constexpr auto type_desc = TypeVector;