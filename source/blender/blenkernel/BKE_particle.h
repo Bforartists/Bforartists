Bforartists/Bforartists

commit f7c1d31fbd0c6eb690f1869f736c7eaf2872836d

Mesh: Replace EdgeHash and EdgeSet with C++ classes
The `EdgeHash` and `EdgeSet` data structures are designed specifically
as a hash of an order agnostic pair of integers. This specialization can
be achieved much more easily with the templated C++ data structures,
which gives improved performance, readability, and type safety.

This PR removes the older data structures and replaces their use with
`Map`, `Set`, or `VectorSet` depending on the situation. The changes
are mostly straightforward, but there are a few places where the old
API made the goals of the code confusing.

The last time these removed data structures were significantly changed,
they were already moving closer to the implementation of the newer
C++ data structures (a7ff43726fa92e1d100a).

Pull Request: https://projects.blender.org/blender/blender/pulls/111391

source/blender/blenkernel/BKE_particle.h
@@ -11,6 +11,8 @@
 
 #include "BLI_buffer.h"
 #include "BLI_compiler_attrs.h"
+#include "BLI_map.hh"
+#include "BLI_ordered_edge.hh"
 #include "BLI_utildefines.h"
 
 #include "DNA_particle_types.h"
@@ -31,7 +33,6 @@ struct BlendLibReader;
 struct BlendWriter;
 struct CustomData_MeshMasks;
 struct Depsgraph;
-struct EdgeHash;
 struct KDTree_3d;
 struct LinkNode;
 struct MCol;
@@ -84,7 +85,7 @@ typedef struct SPHData {
   ParticleSystem *psys[10];
   ParticleData *pa;
   float mass;
-  struct EdgeHash *eh;
+  blender::Map<blender::OrderedEdge, int> eh;
   float *gravity;
   float hfac;
   /* Average distance to neighbors (other particles in the support domain),

source/blender/blenkernel/intern/particle_system.cc
@@ -30,7 +30,6 @@
 #include "DNA_texture_types.h"
 
 #include "BLI_blenlib.h"
-#include "BLI_edgehash.h"
 #include "BLI_kdopbvh.h"
 #include "BLI_kdtree.h"
 #include "BLI_linklist.h"
@@ -1666,17 +1665,16 @@ static void sph_springs_modify(ParticleSystem *psys, float dtime)
     }
   }
 }
-static EdgeHash *sph_springhash_build(ParticleSystem *psys)
+static blender::Map<blender::OrderedEdge, int> sph_springhash_build(ParticleSystem *psys)
 {
-  EdgeHash *springhash = nullptr;
+  blender::Map<blender::OrderedEdge, int> springhash;
+  springhash.reserve(psys->tot_fluidsprings);
+
   ParticleSpring *spring;
   int i = 0;
 
-  springhash = BLI_edgehash_new_ex(__func__, psys->tot_fluidsprings);
-
   for (i = 0, spring = psys->fluid_springs; i < psys->tot_fluidsprings; i++, spring++) {
-    BLI_edgehash_insert(
-        springhash, spring->particle_index[0], spring->particle_index[1], POINTER_FROM_INT(i + 1));
+    springhash.add({spring->particle_index[0], spring->particle_index[1]}, i + 1);
   }
 
   return springhash;
@@ -1808,7 +1806,7 @@ static void sph_force_cb(void *sphdata_v, ParticleKey *state, float *force, floa
   SPHRangeData pfr;
   SPHNeighbor *pfn;
   float *gravity = sphdata->gravity;
-  EdgeHash *springhash = sphdata->eh;
+  const blender::Map<blender::OrderedEdge, int> &springhash = sphdata->eh;
 
   float q, u, rij, dv[3];
   float pressure, near_pressure;
@@ -1892,9 +1890,9 @@ static void sph_force_cb(void *sphdata_v, ParticleKey *state, float *force, floa
 
     if (spring_constant > 0.0f) {
       /* Viscoelastic spring force */
-      if (pfn->psys == psys[0] && fluid->flag & SPH_VISCOELASTIC_SPRINGS && springhash) {
-        /* BLI_edgehash_lookup appears to be thread-safe. - z0r */
-        spring_index = POINTER_AS_INT(BLI_edgehash_lookup(springhash, index, pfn->index));
+      if (pfn->psys == psys[0] && fluid->flag & SPH_VISCOELASTIC_SPRINGS && !springhash.is_empty())
+      {
+        spring_index = springhash.lookup({index, pfn->index});
 
         if (spring_index) {
           spring = psys[0]->fluid_springs + spring_index - 1;
@@ -2183,11 +2181,6 @@ static void psys_sph_flush_springs(SPHData *sphdata)
 void psys_sph_finalize(SPHData *sphdata)
 {
   psys_sph_flush_springs(sphdata);
-
-  if (sphdata->eh) {
-    BLI_edgehash_free(sphdata->eh, nullptr);
-    sphdata->eh = nullptr;
-  }
 }
 
 void psys_sph_density(BVHTree *tree, SPHData *sphdata, float co[3], float vars[2])

source/blender/blenkernel/BKE_cloth.hh
@@ -8,6 +8,8 @@
  */
 
 #include "BLI_math_vector_types.hh"
+#include "BLI_ordered_edge.hh"
+#include "BLI_set.hh"
 
 #include <float.h>
 
@@ -17,8 +19,6 @@ struct ClothModifierData;
 struct CollisionModifierData;
 struct Implicit_Data;
 struct Depsgraph;
-struct EdgeSet;
-struct GHash;
 struct LinkNode;
 struct Mesh;
 struct MVertTri;
@@ -78,13 +78,13 @@ struct Cloth {
   BVHTree *bvhtree;     /* collision tree for this cloth object */
   BVHTree *bvhselftree; /* collision tree for this cloth object (may be same as bvhtree) */
   MVertTri *tri;
-  Implicit_Data *implicit; /* our implicit solver connects to this pointer */
-  EdgeSet *edgeset;        /* used for selfcollisions */
+  Implicit_Data *implicit;                    /* our implicit solver connects to this pointer */
+  blender::Set<blender::OrderedEdge> edgeset; /* used for selfcollisions */
   int last_frame;
   float initial_mesh_volume;     /* Initial volume of the mesh. Used for pressure */
   float average_acceleration[3]; /* Moving average of overall acceleration. */
   const blender::int2 *edges;    /* Used for hair collisions. */
-  EdgeSet *sew_edge_graph;       /* Sewing edges represented using a GHash */
+  blender::Set<blender::OrderedEdge> sew_edge_graph; /* Sewing edges. */
 };
 
 /**

source/blender/blenkernel/BKE_paint.hh
@@ -13,6 +13,8 @@
 #include "BLI_compiler_compat.h"
 #include "BLI_math_vector_types.hh"
 #include "BLI_offset_indices.hh"
+#include "BLI_ordered_edge.hh"
+#include "BLI_set.hh"
 #include "BLI_utildefines.h"
 
 #include "DNA_brush_enums.h"
@@ -31,7 +33,6 @@ struct BlendWriter;
 struct Brush;
 struct CurveMapping;
 struct Depsgraph;
-struct EdgeSet;
 struct EnumPropertyItem;
 struct ExpandCache;
 struct FilterCache;
@@ -353,7 +354,7 @@ struct SculptClothLengthConstraint {
 struct SculptClothSimulation {
   SculptClothLengthConstraint *length_constraints;
   int tot_length_constraints;
-  EdgeSet *created_length_constraints;
+  blender::Set<blender::OrderedEdge> created_length_constraints;
   int capacity_length_constraints;
   float *length_constraint_tweak;
 

source/blender/blenkernel/BKE_subsurf.hh
@@ -11,8 +11,10 @@
 #include "BKE_DerivedMesh.h"
 
 /* Thread sync primitives used directly. */
+#include "BLI_ordered_edge.hh"
 #include "BLI_threads.h"
 #include "BLI_utildefines.h"
+#include "BLI_vector_set.hh"
 
 struct CCGEdge;
 struct CCGElem;
@@ -21,7 +23,6 @@ struct CCGSubSurf;
 struct CCGVert;
 struct DMFlagMat;
 struct DerivedMesh;
-struct EdgeHash;
 struct Mesh;
 struct MeshElemMap;
 struct MultiresModifierData;
@@ -122,7 +123,7 @@ struct CCGDerivedMesh {
     MultiresModifiedFlags modified_flags;
   } multires;
 
-  EdgeHash *ehash;
+  blender::VectorSet<blender::OrderedEdge> *ehash;
 
   ThreadMutex loops_cache_lock;
   ThreadRWMutex origindex_cache_rwlock;

source/blender/blenkernel/intern/cloth.cc
@@ -14,7 +14,6 @@
 #include "DNA_object_types.h"
 #include "DNA_scene_types.h"
 
-#include "BLI_edgehash.h"
 #include "BLI_linklist.h"
 #include "BLI_math_geom.h"
 #include "BLI_math_matrix.h"
@@ -482,21 +481,12 @@ void cloth_free_modifier(ClothModifierData *clmd)
       MEM_freeN(cloth->tri);
     }
 
-    if (cloth->edgeset) {
-      BLI_edgeset_free(cloth->edgeset);
-    }
-
-    if (cloth->sew_edge_graph) {
-      BLI_edgeset_free(cloth->sew_edge_graph);
-      cloth->sew_edge_graph = nullptr;
-    }
-
 #if 0
     if (clmd->clothObject->facemarks) {
       MEM_freeN(clmd->clothObject->facemarks);
     }
 #endif
-    MEM_freeN(cloth);
+    MEM_delete(cloth);
     clmd->clothObject = nullptr;
   }
 }
@@ -559,21 +549,12 @@ void cloth_free_modifier_extern(ClothModifierData *clmd)
       MEM_freeN(cloth->tri);
     }
 
-    if (cloth->edgeset) {
-      BLI_edgeset_free(cloth->edgeset);
-    }
-
-    if (cloth->sew_edge_graph) {
-      BLI_edgeset_free(cloth->sew_edge_graph);
-      cloth->sew_edge_graph = nullptr;
-    }
-
 #if 0
     if (clmd->clothObject->facemarks) {
       MEM_freeN(clmd->clothObject->facemarks);
     }
 #endif
-    MEM_freeN(cloth);
+    MEM_delete(cloth);
     clmd->clothObject = nullptr;
   }
 }
@@ -742,10 +723,9 @@ static bool cloth_from_object(
   }
 
   /* Allocate a new cloth object. */
-  clmd->clothObject = MEM_cnew<Cloth>(__func__);
+  clmd->clothObject = MEM_new<Cloth>(__func__);
   if (clmd->clothObject) {
     clmd->clothObject->old_solver_type = 255;
-    clmd->clothObject->edgeset = nullptr;
   }
   else {
     BKE_modifier_set_error(ob, &(clmd->modifier), "Out of memory on allocating clmd->clothObject");
@@ -762,8 +742,6 @@ static bool cloth_from_object(
   /* create springs */
   clmd->clothObject->springs = nullptr;
   clmd->clothObject->numsprings = -1;
-
-  clmd->clothObject->sew_edge_graph = nullptr;
 
   if (clmd->sim_parms->shapekey_rest &&
       !(clmd->sim_parms->flags & CLOTH_SIMSETTINGS_FLAG_DYNAMIC_BASEMESH))
@@ -944,10 +922,7 @@ static void cloth_free_errorsprings(Cloth *cloth,
 
   MEM_SAFE_FREE(spring_ref);
 
-  if (cloth->edgeset) {
-    BLI_edgeset_free(cloth->edgeset);
-    cloth->edgeset = nullptr;
-  }
+  cloth->edgeset.clear_and_shrink();
 }
 
 BLI_INLINE void cloth_bend_poly_dir(
@@ -1407,7 +1382,7 @@ static bool find_internal_spring_target_vertex(BVHTreeFromMesh *treedata,
                                                float max_length,
                                                float max_diversion,
                                                bool check_normal,
-                                               uint *r_tar_v_idx)
+                                               int *r_tar_v_idx)
 {
   float co[3], no[3], new_co[3];
   float radius;
@@ -1444,7 +1419,7 @@ static bool find_internal_spring_target_vertex(BVHTreeFromMesh *treedata,
   BLI_bvhtree_ray_cast(
       treedata->tree, new_co, no, radius, &rayhit, treedata->raycast_callback, treedata);
 
-  uint vert_idx = -1;
+  int vert_idx = -1;
   const int *corner_verts = treedata->corner_verts;
   const MLoopTri *lt = nullptr;
 
@@ -1458,7 +1433,7 @@ static bool find_internal_spring_target_vertex(BVHTreeFromMesh *treedata,
     lt = &treedata->looptri[rayhit.index];
 
     for (int i = 0; i < 3; i++) {
-      uint tmp_vert_idx = corner_verts[lt->tri[i]];
+      int tmp_vert_idx = corner_verts[lt->tri[i]];
       if (tmp_vert_idx == v_idx) {
         /* We managed to hit ourselves. */
         return false;
@@ -1495,7 +1470,6 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
   const Span<int> corner_edges = mesh->corner_edges();
   int index2 = 0; /* our second vertex index */
   LinkNodePair *edgelist = nullptr;
-  EdgeSet *edgeset = nullptr;
   LinkNode *search = nullptr, *search2 = nullptr;
   BendSpringRef *spring_ref = nullptr;
 
@@ -1507,10 +1481,9 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
   /* NOTE: handling ownership of springs and edgeset is quite sloppy
    * currently they are never initialized but assert just to be sure */
   BLI_assert(cloth->springs == nullptr);
-  BLI_assert(cloth->edgeset == nullptr);
+  BLI_assert(cloth->edgeset.is_empty());
 
   cloth->springs = nullptr;
-  cloth->edgeset = nullptr;
 
   if (clmd->sim_parms->bending_model == CLOTH_BENDING_ANGULAR) {
     spring_ref = static_cast<BendSpringRef *>(
@@ -1532,7 +1505,7 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
 
   if (use_internal_springs && numface > 0) {
     BVHTreeFromMesh treedata = {nullptr};
-    uint tar_v_idx;
+    int tar_v_idx;
     Mesh *tmp_mesh = nullptr;
     RNG *rng;
 
@@ -1543,7 +1516,7 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
       tmp_mesh = cloth_make_rest_mesh(clmd, mesh);
     }
 
-    EdgeSet *existing_vert_pairs = BLI_edgeset_new("cloth_sewing_edges_graph");
+    Set<OrderedEdge> existing_vert_pairs;
     BKE_bvhtree_from_mesh_get(&treedata, tmp_mesh ? tmp_mesh : mesh, BVHTREE_FROM_LOOPTRI, 2);
     rng = BLI_rng_new_srandom(0);
 
@@ -1561,12 +1534,12 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
               (clmd->sim_parms->flags & CLOTH_SIMSETTINGS_FLAG_INTERNAL_SPRINGS_NORMAL),
               &tar_v_idx))
       {
-        if (BLI_edgeset_haskey(existing_vert_pairs, i, tar_v_idx)) {
+        if (existing_vert_pairs.contains({i, tar_v_idx})) {
           /* We have already created a spring between these verts! */
           continue;
         }
 
-        BLI_edgeset_insert(existing_vert_pairs, i, tar_v_idx);
+        existing_vert_pairs.add({i, tar_v_idx});
 
         spring = (ClothSpring *)MEM_callocN(sizeof(ClothSpring), "cloth spring");
 
@@ -1592,7 +1565,6 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
         }
         else {
           cloth_free_errorsprings(cloth, edgelist, spring_ref);
-          BLI_edgeset_free(existing_vert_pairs);
           free_bvhtree_from_mesh(&treedata);
           if (tmp_mesh) {
             BKE_id_free(nullptr, &tmp_mesh->id);
@@ -1601,7 +1573,7 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
         }
       }
     }
-    BLI_edgeset_free(existing_vert_pairs);
+    existing_vert_pairs.clear_and_shrink();
     free_bvhtree_from_mesh(&treedata);
     if (tmp_mesh) {
       BKE_id_free(nullptr, &tmp_mesh->id);
@@ -1616,8 +1588,7 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
 
   if (clmd->sim_parms->flags & CLOTH_SIMSETTINGS_FLAG_SEW) {
     /* cloth->sew_edge_graph should not exist before this */
-    BLI_assert(cloth->sew_edge_graph == nullptr);
-    cloth->sew_edge_graph = BLI_edgeset_new("cloth_sewing_edges_graph");
+    BLI_assert(cloth->sew_edge_graph.is_empty());
   }
 
   /* Structural springs. */
@@ -1635,7 +1606,7 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
         spring->lin_stiffness = 1.0f;
         spring->type = CLOTH_SPRING_TYPE_SEWING;
 
-        BLI_edgeset_insert(cloth->sew_edge_graph, edges[i][0], edges[i][1]);
+        cloth->sew_edge_graph.add({edges[i][0], edges[i][1]});
       }
       else {
         shrink_factor = cloth_shrink_factor(clmd, cloth->verts, spring->ij, spring->kl);
@@ -1681,8 +1652,7 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
     }
   }
 
-  edgeset = BLI_edgeset_new_ex(__func__, numedges);
-  cloth->edgeset = edgeset;
+  cloth->edgeset.reserve(numedges);
 
   if (numface) {
     for (int i = 0; i < numface; i++) {
@@ -1799,7 +1769,7 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
 
           /* Check for existing spring. */
           /* Check also if start-point is equal to endpoint. */
-          if ((index2 != tspring2->ij) && !BLI_edgeset_haskey(edgeset, tspring2->ij, index2)) {
+          if ((index2 != tspring2->ij) && !cloth->edgeset.contains({tspring2->ij, index2})) {
             spring = (ClothSpring *)MEM_callocN(sizeof(ClothSpring), "cloth spring");
 
             if (!spring) {
@@ -1816,7 +1786,7 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
             spring->lin_stiffness = (cloth->verts[spring->kl].bend_stiff +
                                      cloth->verts[spring->ij].bend_stiff) /
                                     2.0f;
-            BLI_edgeset_insert(edgeset, spring->ij, spring->kl);
+            cloth->edgeset.add({spring->ij, spring->kl});
             bend_springs++;
 
             BLI_linklist_prepend(&cloth->springs, spring);
@@ -1911,15 +1881,13 @@ static bool cloth_build_springs(ClothModifierData *clmd, Mesh *mesh)
   /* Insert other near springs in `edgeset` AFTER bending springs are calculated
    * (for self-collision). */
   for (int i = 0; i < numedges; i++) { /* struct springs */
-    BLI_edgeset_add(edgeset, edges[i][0], edges[i][1]);
+    cloth->edgeset.add({edges[i][0], edges[i][1]});
   }
 
   for (int i = 0; i < numface; i++) { /* edge springs */
     if (faces[i].size() == 4) {
-      BLI_edgeset_add(
-          edgeset, corner_verts[faces[i].start() + 0], corner_verts[faces[i].start() + 2]);
-      BLI_edgeset_add(
-          edgeset, corner_verts[faces[i].start() + 1], corner_verts[faces[i].start() + 3]);
+      cloth->edgeset.add({corner_verts[faces[i].start() + 0], corner_verts[faces[i].start() + 2]});
+      cloth->edgeset.add({corner_verts[faces[i].start() + 1], corner_verts[faces[i].start() + 3]});
     }
   }
 

source/blender/blenkernel/intern/collision.cc
@@ -17,7 +17,6 @@
 #include "DNA_scene_types.h"
 
 #include "BLI_blenlib.h"
-#include "BLI_edgehash.h"
 #include "BLI_linklist.h"
 #include "BLI_math_geom.h"
 #include "BLI_math_vector.h"
@@ -1079,7 +1078,7 @@ static bool cloth_bvh_selfcollision_is_active(const ClothModifierData *clmd,
       }
 
       if (sewing_active) {
-        if (BLI_edgeset_haskey(cloth->sew_edge_graph, tri_a->tri[i], tri_b->tri[j])) {
+        if (cloth->sew_edge_graph.contains({tri_a->tri[i], tri_b->tri[j]})) {
           return false;
         }
       }

source/blender/blenkernel/intern/mesh.cc
@@ -19,7 +19,6 @@
 #include "DNA_object_types.h"
 
 #include "BLI_bounds.hh"
-#include "BLI_edgehash.h"
 #include "BLI_endian_switch.h"
 #include "BLI_ghash.h"
 #include "BLI_hash.h"
@@ -30,7 +29,9 @@
 #include "BLI_math_matrix.h"
 #include "BLI_math_vector.hh"
 #include "BLI_memarena.h"
+#include "BLI_ordered_edge.hh"
 #include "BLI_resource_scope.hh"
+#include "BLI_set.hh"
 #include "BLI_span.hh"
 #include "BLI_string.h"
 #include "BLI_task.hh"
@@ -468,6 +469,7 @@ static bool is_uv_bool_sublayer(const CustomDataLayer &layer)
 static int customdata_compare(
     CustomData *c1, CustomData *c2, const int total_length, Mesh *m1, const float thresh)
 {
+  using namespace blender;
   CustomDataLayer *l1, *l2;
   int layer_count1 = 0, layer_count2 = 0, j;
   const uint64_t cd_mask_non_generic = CD_MASK_MDEFORMVERT;
@@ -535,18 +537,17 @@ static int customdata_compare(
 
           if (StringRef(l1->name) == ".edge_verts") {
             int etot = m1->totedge;
-            EdgeHash *eh = BLI_edgehash_new_ex(__func__, etot);
-
-            for (j = 0; j < etot; j++, e1++) {
-              BLI_edgehash_insert(eh, (*e1)[0], (*e1)[1], e1);
+            Set<OrderedEdge> ordered_edges;
+            ordered_edges.reserve(etot);
+            for (const int2 value : Span(e1, etot)) {
+              ordered_edges.add(value);
             }
 
-            for (j = 0; j < etot; j++, e2++) {
-              if (!BLI_edgehash_lookup(eh, (*e2)[0], (*e2)[1])) {
+            for (j = 0; j < etot; j++) {
+              if (!ordered_edges.contains(e2[j])) {
                 return MESHCMP_EDGEUNKNOWN;
               }
             }
-            BLI_edgehash_free(eh, nullptr);
           }
           else {
             for (j = 0; j < total_length; j++) {

commit 7b9c57db0659befcc52b5673aaecb639460218d4

Fix #113851: viscoelastic springs do not work
Caused by f7c1d31fbd.

The root issue was that these two states of the hash table were
mixed up in f7c1d31fbd: (1) the hash table exists and (2) the hash
table is empty.

The use of `std::optional` restores these two different states again.

source/blender/blenkernel/BKE_particle.h
@@ -9,6 +9,8 @@
  * \ingroup bke
  */
 
+#include <optional>
+
 #include "BLI_buffer.h"
 #include "BLI_compiler_attrs.h"
 #include "BLI_map.hh"
@@ -85,7 +87,7 @@ typedef struct SPHData {
   ParticleSystem *psys[10];
   ParticleData *pa;
   float mass;
-  blender::Map<blender::OrderedEdge, int> eh;
+  std::optional<blender::Map<blender::OrderedEdge, int>> eh;
   float *gravity;
   float hfac;
   /* Average distance to neighbors (other particles in the support domain),

source/blender/blenkernel/intern/particle_system.cc
@@ -1806,7 +1806,7 @@ static void sph_force_cb(void *sphdata_v, ParticleKey *state, float *force, floa
   SPHRangeData pfr;
   SPHNeighbor *pfn;
   float *gravity = sphdata->gravity;
-  const blender::Map<blender::OrderedEdge, int> &springhash = sphdata->eh;
+  const std::optional<blender::Map<blender::OrderedEdge, int>> &springhash = sphdata->eh;
 
   float q, u, rij, dv[3];
   float pressure, near_pressure;
@@ -1890,9 +1890,9 @@ static void sph_force_cb(void *sphdata_v, ParticleKey *state, float *force, floa
 
     if (spring_constant > 0.0f) {
       /* Viscoelastic spring force */
-      if (pfn->psys == psys[0] && fluid->flag & SPH_VISCOELASTIC_SPRINGS && !springhash.is_empty())
+      if (pfn->psys == psys[0] && fluid->flag & SPH_VISCOELASTIC_SPRINGS && springhash.has_value())
       {
-        spring_index = springhash.lookup({index, pfn->index});
+        spring_index = springhash->lookup_default({index, pfn->index}, 0);
 
         if (spring_index) {
           spring = psys[0]->fluid_springs + spring_index - 1;