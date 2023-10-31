Bforartists/Bforartists

commit a8fbf2fd19b03a8a5daed13a268a0866004a57f6

Fix background drawing in tool header after changing tools
After changing tools (and possibly other context), the buttons section
background drawing would be broken for one redraw, so the full region
would be drawn with a background, rather than just behind the button
boundaries.

source/blender/editors/interface/interface_button_sections.cc
@@ -61,6 +61,10 @@ static Vector<rcti> button_section_bounds_calc(const ARegion *region, const bool
     BLI_rcti_init_minmax(&cur_section_bounds);
 
     LISTBASE_FOREACH (uiBlock *, block, &region->uiblocks) {
+      if (!block->active) {
+        continue;
+      }
+
       LISTBASE_FOREACH (uiBut *, but, &block->buttons) {
         if (but->type == UI_BTYPE_SEPR_SPACER) {
           /* Start a new section. */

commit 3133e6f5dd76de0bb3f61f87faa4d25c431cf2ab

Fix asset shelf can't resized by dragging header edge anymore
Regression from a8fbf2fd19 (I assume). While the block active state is
relevant during drawing to avoid glitches, it should be ignored during
handling. Explained further in comment.

source/blender/editors/interface/interface_button_sections.cc
@@ -60,8 +60,13 @@ static Vector<rcti> button_section_bounds_calc(const ARegion *region, const bool
     rcti cur_section_bounds;
     BLI_rcti_init_minmax(&cur_section_bounds);
 
+    /* A bit annoying, but this function is called for both drawing and event handling. When
+     * drawing, we need to exclude inactive blocks since they mess with the result. However, this
+     * active state is only useful during drawing and must be ignored for handling (at which point
+     * #uiBlock::active is false for all blocks). */
+    const bool is_drawing = region->do_draw & RGN_DRAWING;
     LISTBASE_FOREACH (uiBlock *, block, &region->uiblocks) {
-      if (!block->active) {
+      if (is_drawing && !block->active) {
         continue;
       }
 