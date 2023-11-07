Bforartists/Bforartists

commit 03fa6bebc16e21b393308414c7c9e3417a674792

UI: improve mesh edge highlighting
Changes to edit mode mesh overlays, use hue shift instead of color
fading/darkening for selection mode visual differentiation, and some
theme changes to improve the display of mesh edges and faces with good
selection visibility.

- Removed "edge" toggle from edit mode overlays panel.
- No longer halves the edge and face alpha depending on selection mode.
  Half the face alpha in wire-frame mode. For better visibility on most
  themes.

Ref !111431

source/blender/draw/engines/overlay/shaders/overlay_edit_mesh_common_lib.glsl
@@ -16,23 +16,20 @@ vec4 EDIT_MESH_edge_color_outer(uint edge_flag, uint face_flag, float crease, fl
 vec4 EDIT_MESH_edge_color_inner(uint edge_flag)
 {
   vec4 color = colorWireEdit;
-  vec4 color_select = (selectEdges) ? colorEdgeSelect : mix(colorEdgeSelect, colorWireEdit, .45);
-  color = ((edge_flag & EDGE_SELECTED) != 0u) ? color_select : color;
+  vec4 selected_edge_col = (selectEdge) ? colorEdgeModeSelect : colorEdgeSelect;
+  color = ((edge_flag & EDGE_SELECTED) != 0u) ? selected_edge_col : color;
   color = ((edge_flag & EDGE_ACTIVE) != 0u) ? colorEditMeshActive : color;
-
-  color.a = (selectEdges || (edge_flag & (EDGE_SELECTED | EDGE_ACTIVE)) != 0u) ? 1.0 : 0.7;
+  color.a = 1.0;
   return color;
 }
 
 vec4 EDIT_MESH_edge_vertex_color(uint vertex_flag)
 {
-  vec4 color = colorWireEdit;
-  vec4 color_select = (selectEdges) ? colorEdgeSelect : mix(colorEdgeSelect, colorWireEdit, .45);
-
+  /* Edge color in vextex selection mode. */
+  vec4 selected_edge_col = (selectEdge) ? colorEdgeModeSelect : colorEdgeSelect;
   bool edge_selected = (vertex_flag & (VERT_ACTIVE | VERT_SELECTED)) != 0u;
-  color = (edge_selected) ? color_select : color;
-
-  color.a = (selectEdges || edge_selected) ? 1.0 : 0.7;
+  vec4 color = (edge_selected) ? selected_edge_col : colorWireEdit;
+  color.a = 1.0;
   return color;
 }
 
@@ -55,15 +52,26 @@ vec4 EDIT_MESH_vertex_color(uint vertex_flag, float vertex_crease)
 
 vec4 EDIT_MESH_face_color(uint face_flag)
 {
+  bool face_freestyle = (face_flag & FACE_FREESTYLE) != 0u;
+  bool face_selected = (face_flag & FACE_SELECTED) != 0u;
+  bool face_active = (face_flag & FACE_ACTIVE) != 0u;
+  vec4 selected_face_col = (selectFace) ? colorFaceModeSelect : colorFaceSelect;
   vec4 color = colorFace;
-  vec4 color_active = mix(colorFaceSelect, colorEditMeshActive, 0.5);
   color = (retopologyOffset > 0.0) ? colorFaceRetopology : color;
-  color = ((face_flag & FACE_FREESTYLE) != 0u) ? colorFaceFreestyle : color;
-  color = ((face_flag & FACE_SELECTED) != 0u) ? colorFaceSelect : color;
-  color = ((face_flag & FACE_ACTIVE) != 0u) ? color_active : color;
-  color.a *= ((face_flag & (FACE_FREESTYLE | FACE_SELECTED | FACE_ACTIVE)) == 0u || selectFaces) ?
-                 1.0 :
-                 0.5;
+  color = face_freestyle ? colorFaceFreestyle : color;
+  color = face_selected ? selected_face_col : color;
+  if (selectFace && face_active) {
+    color = mix(selected_face_col, colorEditMeshActive, 0.5);
+    color.a = selected_face_col.a;
+  }
+  if (wireShading) {
+    /* Lower face selection opacity for better wireframe visibility. */
+    color.a = (face_selected) ? color.a * 0.6 : color.a;
+  }
+  else {
+    /* Don't always fill 'colorFace'. */
+    color.a = (selectFace || face_selected || face_active || face_freestyle) ? color.a : 0.0;
+  }
   return color;
 }
 

release/datafiles/userdef/userdef_default_theme.c
@@ -317,14 +317,16 @@ const bTheme U_theme_default = {
     .vertex_select = RGBA(0xff7a00ff),
     .vertex_bevel = RGBA(0x00a5ffff),
     .edge = RGBA(0x000000ff),
-    .edge_select = RGBA(0xffa000ff),
+    .edge_select = RGBA(0xff9900ff),
+    .edge_mode_select = RGBA(0xffd800ff),
     .edge_seam = RGBA(0xdb2512ff),
     .edge_sharp = RGBA(0x00ffffff),
     .edge_facesel = RGBA(0x4b4b4bff),
     .edge_crease = RGBA(0xcc0099ff),
     .edge_bevel = RGBA(0x00a5ffff),
     .face = RGBA(0xffffff02),
-    .face_select = RGBA(0xffa5522e),
+    .face_select = RGBA(0xffa30033),
+    .face_mode_select = RGBA(0xffb70033),
     .face_retopology = RGBA(0x50c8ff0f),
     .face_back = RGBA(0xff0000b3),
     .face_front = RGBA(0x0000ffb3),

scripts/presets/interface_theme/Blender_Light.xml
@@ -368,12 +368,13 @@
         light="#00000028"
         speaker="#000000"
         vertex="#000000"
-        vertex_select="#ff8500"
+        vertex_select="#ff7a00"
         vertex_active="#000000"
         vertex_size="3"
         vertex_bevel="#00a5ff"
         vertex_unreferenced="#000000"
-        edge_select="#ffa000"
+        edge_select="#ff9900"
+        edge_mode_select="#ffd800"
         edge_seam="#db2512"
         edge_sharp="#00ffff"
         edge_crease="#cc0099"
@@ -381,7 +382,8 @@
         edge_facesel="#4b4b4b"
         freestyle_edge_mark="#7fff7f"
         face="#00000012"
-        face_select="#ff85004d"
+        face_select="#ffa30033"
+        face_mode_select="#ffb70033"
         face_dot="#ff8500"
         facedot_size="4"
         freestyle_face_mark="#7fff7f33"

scripts/startup/bl_ui/space_view3d.py
@@ -7056,9 +7056,6 @@ class VIEW3D_PT_overlay_edit_mesh(Panel):
         split = col.split()
 
         sub = split.column()
-        sub.active = is_any_solid_shading
-        sub.prop(overlay, "show_edges", text="Edges")
-        sub = split.column()
         sub.prop(overlay, "show_faces", text="Faces")
         sub = split.column()
         sub.active = is_any_solid_shading

source/blender/blenkernel/BKE_blender_version.h
@@ -29,7 +29,7 @@ extern "C" {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 3
+#define BLENDER_FILE_SUBVERSION 4
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenloader/intern/versioning_280.cc
@@ -4148,8 +4148,8 @@ void blo_do_versions_280(FileData *fd, Library * /*lib*/, Main *bmain)
             View3D *v3d = (View3D *)sl;
             v3d->overlay.edit_flag |= V3D_OVERLAY_EDIT_FACES | V3D_OVERLAY_EDIT_SEAMS |
                                       V3D_OVERLAY_EDIT_SHARP | V3D_OVERLAY_EDIT_FREESTYLE_EDGE |
-                                      V3D_OVERLAY_EDIT_FREESTYLE_FACE | V3D_OVERLAY_EDIT_EDGES |
-                                      V3D_OVERLAY_EDIT_CREASES | V3D_OVERLAY_EDIT_BWEIGHTS;
+                                      V3D_OVERLAY_EDIT_FREESTYLE_FACE | V3D_OVERLAY_EDIT_CREASES |
+                                      V3D_OVERLAY_EDIT_BWEIGHTS;
           }
         }
       }

source/blender/blenloader/intern/versioning_defaults.cc
@@ -188,8 +188,8 @@ static void blo_update_defaults_screen(bScreen *screen,
       v3d->overlay.texture_paint_mode_opacity = 1.0f;
       v3d->overlay.weight_paint_mode_opacity = 1.0f;
       v3d->overlay.vertex_paint_mode_opacity = 1.0f;
-      /* Use dimmed selected edges. */
-      v3d->overlay.edit_flag &= ~V3D_OVERLAY_EDIT_EDGES;
+      /* Clear this deprecated bit for later reuse. */
+      v3d->overlay.edit_flag &= ~V3D_OVERLAY_EDIT_EDGES_DEPRECATED;
       /* grease pencil settings */
       v3d->vertex_opacity = 1.0f;
       v3d->gp_flag |= V3D_GP_SHOW_EDIT_LINES;

source/blender/blenloader/intern/versioning_userdef.cc
@@ -133,6 +133,13 @@ static void do_versions_theme(const UserDef *userdef, bTheme *btheme)
     memcpy(btheme, &U_theme_default, sizeof(*btheme));
   }
 
+  if (!USER_VERSION_ATLEAST(401, 4)) {
+    FROM_DEFAULT_V4_UCHAR(space_view3d.edge_select);
+    FROM_DEFAULT_V4_UCHAR(space_view3d.edge_mode_select);
+    FROM_DEFAULT_V4_UCHAR(space_view3d.face_select);
+    FROM_DEFAULT_V4_UCHAR(space_view3d.face_mode_select);
+  }
+
   /**
    * Versioning code until next subversion bump goes here.
    *

commit 8243e0aac75ef11ca050e9e4966a4e78f4a3f273

Fix #114571: Retopology Overlay Face Color Regression
Fix #114571: Retopology Overlay Face Color Regression

Missed in 03fa6bebc1

Pull Request: https://projects.blender.org/blender/blender/pulls/114589

source/blender/draw/engines/overlay/shaders/overlay_edit_mesh_common_lib.glsl
@@ -55,9 +55,10 @@ vec4 EDIT_MESH_face_color(uint face_flag)
   bool face_freestyle = (face_flag & FACE_FREESTYLE) != 0u;
   bool face_selected = (face_flag & FACE_SELECTED) != 0u;
   bool face_active = (face_flag & FACE_ACTIVE) != 0u;
+  bool face_retopo = (retopologyOffset > 0.0);
   vec4 selected_face_col = (selectFace) ? colorFaceModeSelect : colorFaceSelect;
   vec4 color = colorFace;
-  color = (retopologyOffset > 0.0) ? colorFaceRetopology : color;
+  color = face_retopo ? colorFaceRetopology : color;
   color = face_freestyle ? colorFaceFreestyle : color;
   color = face_selected ? selected_face_col : color;
   if (selectFace && face_active) {
@@ -70,7 +71,7 @@ vec4 EDIT_MESH_face_color(uint face_flag)
   }
   else {
     /* Don't always fill 'colorFace'. */
-    color.a = (selectFace || face_selected || face_active || face_freestyle) ? color.a : 0.0;
+    color.a = (selectFace || face_selected || face_active || face_freestyle || face_retopo) ? color.a : 0.0;
   }
   return color;
 }