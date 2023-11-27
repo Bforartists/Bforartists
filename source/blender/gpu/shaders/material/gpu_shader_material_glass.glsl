Bforartists/Bforartists

commit 71ea2636bd8d7514209506d4097606e835e7707d

GPU: Sanitize closure nodes inputs
Ensure all Closures are filled with correct values,
like the principled bsdf node already does.

The main reason is that the new AgX color transform doesn't play well
with negative values (see #113220), but it's probably best to ensure we
use sanitized values in the rendering code as a whole.

Pull Request: https://projects.blender.org/blender/blender/pulls/115059

source/blender/gpu/shaders/material/gpu_shader_material_glass.glsl
@@ -10,7 +10,11 @@ void node_bsdf_glass(vec4 color,
                      const float do_multiscatter,
                      out Closure result)
 {
+  color = max(color, vec4(0));
+  roughness = saturate(roughness);
+  ior = max(ior, 1e-5);
   N = safe_normalize(N);
+
   vec3 V = coordinate_incoming(g_data.P);
   float NV = dot(N, V);
 

source/blender/gpu/shaders/material/gpu_shader_material_glossy.glsl
@@ -12,7 +12,10 @@ void node_bsdf_glossy(vec4 color,
                       const float do_multiscatter,
                       out Closure result)
 {
+  color = max(color, vec4(0));
+  roughness = saturate(roughness);
   N = safe_normalize(N);
+
   vec3 V = coordinate_incoming(g_data.P);
   float NV = dot(N, V);
 

source/blender/gpu/shaders/material/gpu_shader_material_hair.glsl
@@ -10,6 +10,8 @@ void node_bsdf_hair(vec4 color,
                     float weight,
                     out Closure result)
 {
+  color = max(color, vec4(0));
+
 #if 0
   /* NOTE(fclem): This is the way it should be. But we don't have proper implementation of the hair
    * closure yet. For now fall back to a simpler diffuse surface so that we have at least a color

source/blender/gpu/shaders/material/gpu_shader_material_refraction.glsl
@@ -5,6 +5,9 @@
 void node_bsdf_refraction(
     vec4 color, float roughness, float ior, vec3 N, float weight, out Closure result)
 {
+  color = max(color, vec4(0));
+  roughness = saturate(roughness);
+  ior = max(ior, 1e-5);
   N = safe_normalize(N);
 
   ClosureRefraction refraction_data;

source/blender/gpu/shaders/material/gpu_shader_material_sheen.glsl
@@ -4,6 +4,8 @@
 
 void node_bsdf_sheen(vec4 color, float roughness, vec3 N, float weight, out Closure result)
 {
+  color = max(color, vec4(0));
+  roughness = saturate(roughness);
   N = safe_normalize(N);
 
   /* Fallback to diffuse. */

source/blender/gpu/shaders/material/gpu_shader_material_subsurface_scattering.glsl
@@ -12,6 +12,10 @@ void node_subsurface_scattering(vec4 color,
                                 float do_sss,
                                 out Closure result)
 {
+  color = max(color, vec4(0));
+  scale = max(scale, 0);
+  radius = max(radius, vec3(0));
+  ior = max(ior, 1e-5);
   N = safe_normalize(N);
 
   ClosureDiffuse diffuse_data;

source/blender/gpu/shaders/material/gpu_shader_material_toon.glsl
@@ -5,6 +5,7 @@
 void node_bsdf_toon(
     vec4 color, float size, float tsmooth, vec3 N, float weight, out Closure result)
 {
+  color = max(color, vec4(0));
   N = safe_normalize(N);
 
   /* Fallback to diffuse. */

source/blender/gpu/shaders/material/gpu_shader_material_translucent.glsl
@@ -4,6 +4,7 @@
 
 void node_bsdf_translucent(vec4 color, vec3 N, float weight, out Closure result)
 {
+  color = max(color, vec4(0));
   N = safe_normalize(N);
 
   ClosureTranslucent translucent_data;

commit a9471a6906fa9904214697849ff676a3ae049368

GPU: Resolve compilation error in Metal caused by type ambiguity
Recent change in commit 71ea2636bd8d7514209506d4097606e835e7707d
caused compilation errors in Metal due to type ambiguity. Updating call to
explicitly utilise floats where appropriate.

Authored by Apple: Michael Parkin-White

Co-authored-by: Michael Parkin-White <mparkinwhite@apple.com>
Pull Request: https://projects.blender.org/blender/blender/pulls/115301

source/blender/gpu/shaders/material/gpu_shader_material_glass.glsl
@@ -10,7 +10,7 @@ void node_bsdf_glass(vec4 color,
                      const float do_multiscatter,
                      out Closure result)
 {
-  color = max(color, vec4(0));
+  color = max(color, vec4(0.0));
   roughness = saturate(roughness);
   ior = max(ior, 1e-5);
   N = safe_normalize(N);

source/blender/gpu/shaders/material/gpu_shader_material_glossy.glsl
@@ -12,7 +12,7 @@ void node_bsdf_glossy(vec4 color,
                       const float do_multiscatter,
                       out Closure result)
 {
-  color = max(color, vec4(0));
+  color = max(color, vec4(0.0));
   roughness = saturate(roughness);
   N = safe_normalize(N);
 

source/blender/gpu/shaders/material/gpu_shader_material_hair.glsl
@@ -10,7 +10,7 @@ void node_bsdf_hair(vec4 color,
                     float weight,
                     out Closure result)
 {
-  color = max(color, vec4(0));
+  color = max(color, vec4(0.0));
 
 #if 0
   /* NOTE(fclem): This is the way it should be. But we don't have proper implementation of the hair

source/blender/gpu/shaders/material/gpu_shader_material_refraction.glsl
@@ -5,7 +5,7 @@
 void node_bsdf_refraction(
     vec4 color, float roughness, float ior, vec3 N, float weight, out Closure result)
 {
-  color = max(color, vec4(0));
+  color = max(color, vec4(0.0));
   roughness = saturate(roughness);
   ior = max(ior, 1e-5);
   N = safe_normalize(N);

source/blender/gpu/shaders/material/gpu_shader_material_sheen.glsl
@@ -4,7 +4,7 @@
 
 void node_bsdf_sheen(vec4 color, float roughness, vec3 N, float weight, out Closure result)
 {
-  color = max(color, vec4(0));
+  color = max(color, vec4(0.0));
   roughness = saturate(roughness);
   N = safe_normalize(N);
 

source/blender/gpu/shaders/material/gpu_shader_material_subsurface_scattering.glsl
@@ -12,7 +12,7 @@ void node_subsurface_scattering(vec4 color,
                                 float do_sss,
                                 out Closure result)
 {
-  color = max(color, vec4(0));
+  color = max(color, vec4(0.0));
   scale = max(scale, 0);
   radius = max(radius, vec3(0));
   ior = max(ior, 1e-5);

source/blender/gpu/shaders/material/gpu_shader_material_toon.glsl
@@ -5,7 +5,7 @@
 void node_bsdf_toon(
     vec4 color, float size, float tsmooth, vec3 N, float weight, out Closure result)
 {
-  color = max(color, vec4(0));
+  color = max(color, vec4(0.0));
   N = safe_normalize(N);
 
   /* Fallback to diffuse. */

source/blender/gpu/shaders/material/gpu_shader_material_translucent.glsl
@@ -4,7 +4,7 @@
 
 void node_bsdf_translucent(vec4 color, vec3 N, float weight, out Closure result)
 {
-  color = max(color, vec4(0));
+  color = max(color, vec4(0.0));
   N = safe_normalize(N);
 
   ClosureTranslucent translucent_data;

source/blender/gpu/shaders/material/gpu_shader_material_transparent.glsl
@@ -4,7 +4,7 @@
 
 void node_bsdf_transparent(vec4 color, float weight, out Closure result)
 {
-  color = max(color, vec4(0));
+  color = max(color, vec4(0.0));
 
   ClosureTransparency transparency_data;
   transparency_data.weight = weight;

source/blender/gpu/shaders/material/gpu_shader_material_volume_absorption.glsl
@@ -4,8 +4,8 @@
 
 void node_volume_absorption(vec4 color, float density, float weight, out Closure result)
 {
-  color = max(color, vec4(0));
-  density = max(density, 0);
+  color = max(color, vec4(0.0));
+  density = max(density, 0.0);
 
   ClosureVolumeAbsorption volume_absorption_data;
   volume_absorption_data.weight = weight;

source/blender/gpu/shaders/material/gpu_shader_material_volume_principled.glsl
@@ -21,8 +21,8 @@ void node_volume_principled(vec4 color,
                             float layer,
                             out Closure result)
 {
-  color = max(color, vec4(0));
-  density = max(density, 0);
+  color = max(color, vec4(0.0));
+  density = max(density, 0.0);
   absorption_color = max(absorption_color, vec4(0));
   emission_strength = max(emission_strength, 0);
   emission_color = max(emission_color, vec4(0));

source/blender/gpu/shaders/material/gpu_shader_material_volume_scatter.glsl
@@ -5,8 +5,8 @@
 void node_volume_scatter(
     vec4 color, float density, float anisotropy, float weight, out Closure result)
 {
-  color = max(color, vec4(0));
-  density = max(density, 0);
+  color = max(color, vec4(0.0));
+  density = max(density, 0.0);
 
   ClosureVolumeScatter volume_scatter_data;
   volume_scatter_data.weight = weight;