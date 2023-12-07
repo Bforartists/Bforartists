Bforartists/Bforartists

commit b3aa2c5cc4d9dc6f5c65a82798cb5c70fa4dfb23

Fix: GPv3: Memory leaks in curve fitting functions
There were some memory leaks in the `polyline_fit_curve` and
`polyline_detect_corners` functions, because the
c-style arrays that the library returns are never freed.
We only copy the data and return it.

The fix makes sure we free the temporary arrays.

source/blender/editors/grease_pencil/intern/grease_pencil_geom.cc
@@ -119,8 +119,9 @@ Array<float2> polyline_fit_curve(Span<float2> points,
 
   Span<float2> r_cubic_array_span(reinterpret_cast<float2 *>(r_cubic_array),
                                   r_cubic_array_len * 3);
-
   Array<float2> curve_positions(r_cubic_array_span);
+  /* Free the c-style array. */
+  MEM_freeN(r_cubic_array);
   return curve_positions;
 }
 
@@ -154,7 +155,10 @@ IndexMask polyline_detect_corners(Span<float2> points,
   }
   BLI_assert(samples_max < std::numeric_limits<int>::max());
   Span<int> indices(reinterpret_cast<int *>(r_corners), r_corner_len);
-  return IndexMask::from_indices<int>(indices, memory);
+  const IndexMask corner_mask = IndexMask::from_indices<int>(indices, memory);
+  /* Free the c-style array. */
+  MEM_freeN(r_corners);
+  return corner_mask;
 }
 
 }  // namespace blender::ed::greasepencil

commit 7cec7daddeb08c5f4f01a172b9aff39c030f533f

Fix: GPv3: Crash when drawing
The commit b3aa2c5cc4d9dc6f5c65a82798cb5c70fa4dfb23 introduced
a crash. The memory from `curve_fit_cubic_to_points_fl` and
`curve_fit_corners_detect_fl` needs to be freed with `free` and
we need to check for `nullptr`.

source/blender/editors/grease_pencil/intern/grease_pencil_geom.cc
@@ -117,11 +117,15 @@ Array<float2> polyline_fit_curve(Span<float2> points,
     return {};
   }
 
+  if (r_cubic_array == nullptr) {
+    return {};
+  }
+
   Span<float2> r_cubic_array_span(reinterpret_cast<float2 *>(r_cubic_array),
                                   r_cubic_array_len * 3);
   Array<float2> curve_positions(r_cubic_array_span);
   /* Free the c-style array. */
-  MEM_freeN(r_cubic_array);
+  free(r_cubic_array);
   return curve_positions;
 }
 
@@ -153,11 +157,16 @@ IndexMask polyline_detect_corners(Span<float2> points,
     /* Error occurred, return. */
     return IndexMask();
   }
+
+  if (r_corners == nullptr) {
+    return IndexMask();
+  }
+
   BLI_assert(samples_max < std::numeric_limits<int>::max());
   Span<int> indices(reinterpret_cast<int *>(r_corners), r_corner_len);
   const IndexMask corner_mask = IndexMask::from_indices<int>(indices, memory);
   /* Free the c-style array. */
-  MEM_freeN(r_corners);
+  free(r_corners);
   return corner_mask;
 }
 