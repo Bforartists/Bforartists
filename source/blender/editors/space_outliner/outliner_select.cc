Bforartists/Bforartists

commit 7e1c6170af7cff121e5d20a80f16a258cefba5f4

ViewLayer: Lazy sync of scene data.
When a change happens which invalidates view layers the syncing will be postponed until the first usage.
This will improve importing or adding many objects in a single operation/script.

`BKE_view_layer_need_resync_tag` is used to tag the view layer to be out of sync. Before accessing
`BKE_view_layer_active_base_get`, `BKE_view_layer_active_object_get`, `BKE_view_layer_active_collection`
or `BKE_view_layer_object_bases` the caller should call `BKE_view_layer_synced_ensure`.

Having two functions ensures that partial syncing could be added as smaller patches in the future. Tagging a
view layer out of sync could be replaced with a partial sync. Eventually the number of full resyncs could be
reduced. After all tagging has been replaced with partial syncs the ensure_sync could be phased out.

This patch has been added to discuss the details and consequences of the current approach. For clarity
the call to BKE_view_layer_ensure_sync is placed close to the getters.
In the future this could be placed in more strategical places to reduce the number of calls or improve
performance. Finding those strategical places isn't that clear. When multiple operations are grouped
in a single script you might want to always check for resync.

Some areas found that can be improved. This list isn't complete.
These areas aren't addressed by this patch as these changes would be hard to detect to the reviewer.
The idea is to add changes to these areas as a separate patch. It might be that the initial commit would reduce
performance compared to master, but will be fixed by the additional patches.

**Object duplication**
During object duplication the syncing is temporarily disabled. With this patch this isn't useful as when disabled
the view_layer is accessed to locate bases. This can be improved by first locating the source bases, then duplicate
and sync and locate the new bases. Will be solved in a separate patch for clarity reasons ({D15886}).

source/blender/editors/space_outliner/outliner_select.cc
@@ -164,6 +164,7 @@ static void do_outliner_item_mode_toggle_generic(bContext *C, TreeViewContext *t
   ED_undo_group_begin(C);
 
   if (ED_object_mode_set(C, OB_MODE_OBJECT)) {
+    BKE_view_layer_synced_ensure(tvc->scene, tvc->view_layer);
     Base *base_active = BKE_view_layer_base_find(tvc->view_layer, tvc->obact);
     if (base_active != base) {
       BKE_view_layer_base_deselect_all(tvc->scene, tvc->view_layer);
@@ -188,6 +189,7 @@ void outliner_item_mode_toggle(bContext *C,
 
   if ((tselem->type == TSE_SOME_ID) && (te->idcode == ID_OB)) {
     Object *ob = (Object *)tselem->id;
+    BKE_view_layer_synced_ensure(tvc->scene, tvc->view_layer);
     Base *base = BKE_view_layer_base_find(tvc->view_layer, ob);
 
     /* Hidden objects can be removed from the mode. */
@@ -234,11 +236,13 @@ static void tree_element_viewlayer_activate(bContext *C, TreeElement *te)
 /**
  * Select object tree
  */
-static void do_outliner_object_select_recursive(ViewLayer *view_layer,
+static void do_outliner_object_select_recursive(const Scene *scene,
+                                                ViewLayer *view_layer,
                                                 Object *ob_parent,
                                                 bool select)
 {
-  LISTBASE_FOREACH (Base *, base, &view_layer->object_bases) {
+  BKE_view_layer_synced_ensure(scene, view_layer);
+  LISTBASE_FOREACH (Base *, base, BKE_view_layer_object_bases_get(view_layer)) {
     Object *ob = base->object;
     if ((((base->flag & BASE_ENABLED_AND_MAYBE_VISIBLE_IN_VIEWPORT) != 0) &&
          BKE_object_is_child_recursive(ob_parent, ob))) {
@@ -300,6 +304,7 @@ static void tree_element_object_activate(bContext *C,
       ob = (Object *)parent_tselem->id;
 
       /* Don't return when activating children of the previous active object. */
+      BKE_view_layer_synced_ensure(scene, view_layer);
       if (ob == BKE_view_layer_active_object_get(view_layer) && set == OL_SETSEL_NONE) {
         return;
       }
@@ -316,6 +321,7 @@ static void tree_element_object_activate(bContext *C,
   }
 
   /* find associated base in current scene */
+  BKE_view_layer_synced_ensure(sce, view_layer);
   base = BKE_view_layer_base_find(view_layer, ob);
 
   if (scene->toolsettings->object_flag & SCE_OBJECT_MODE_LOCK) {
@@ -371,7 +377,8 @@ static void tree_element_object_activate(bContext *C,
 
     if (recursive) {
       /* Recursive select/deselect for Object hierarchies */
-      do_outliner_object_select_recursive(view_layer, ob, (base->flag & BASE_SELECTED) != 0);
+      do_outliner_object_select_recursive(
+          scene, view_layer, ob, (base->flag & BASE_SELECTED) != 0);
     }
 
     if (set != OL_SETSEL_NONE) {
@@ -382,11 +389,15 @@ static void tree_element_object_activate(bContext *C,
   }
 }
 
-static void tree_element_material_activate(bContext *C, ViewLayer *view_layer, TreeElement *te)
+static void tree_element_material_activate(bContext *C,
+                                           const Scene *scene,
+                                           ViewLayer *view_layer,
+                                           TreeElement *te)
 {
   /* we search for the object parent */
   Object *ob = (Object *)outliner_search_back(te, ID_OB);
   /* Note : ob->matbits can be nullptr when a local object points to a library mesh. */
+  BKE_view_layer_synced_ensure(scene, view_layer);
   if (ob == nullptr || ob != BKE_view_layer_active_object_get(view_layer) ||
       ob->matbits == nullptr) {
     return; /* just paranoia */
@@ -536,6 +547,7 @@ static void tree_element_posechannel_activate(bContext *C,
 }
 
 static void tree_element_bone_activate(bContext *C,
+                                       const Scene *scene,
                                        ViewLayer *view_layer,
                                        TreeElement *te,
                                        TreeStoreElem *tselem,
@@ -546,6 +558,7 @@ static void tree_element_bone_activate(bContext *C,
   Bone *bone = static_cast<Bone *>(te->directdata);
 
   if (!(bone->flag & BONE_HIDDEN_P)) {
+    BKE_view_layer_synced_ensure(scene, view_layer);
     Object *ob = BKE_view_layer_active_object_get(view_layer);
     if (ob) {
       if (set != OL_SETSEL_EXTEND) {
@@ -769,7 +782,7 @@ void tree_element_activate(bContext *C,
       }
       break;
     case ID_MA:
-      tree_element_material_activate(C, tvc->view_layer, te);
+      tree_element_material_activate(C, tvc->scene, tvc->view_layer, te);
       break;
     case ID_WO:
       tree_element_world_activate(C, tvc->scene, te);
@@ -796,7 +809,7 @@ void tree_element_type_active_set(bContext *C,
       tree_element_defgroup_activate(C, te, tselem);
       break;
     case TSE_BONE:
-      tree_element_bone_activate(C, tvc->view_layer, te, tselem, set, recursive);
+      tree_element_bone_activate(C, tvc->scene, tvc->view_layer, te, tselem, set, recursive);
       break;
     case TSE_EBONE:
       tree_element_ebone_activate(C, tvc->scene, tvc->view_layer, te, tselem, set, recursive);
@@ -844,11 +857,13 @@ void tree_element_type_active_set(bContext *C,
   }
 }
 
-static eOLDrawState tree_element_defgroup_state_get(const ViewLayer *view_layer,
+static eOLDrawState tree_element_defgroup_state_get(const Scene *scene,
+                                                    ViewLayer *view_layer,
                                                     const TreeElement *te,
                                                     const TreeStoreElem *tselem)
 {
   const Object *ob = (const Object *)tselem->id;
+  BKE_view_layer_synced_ensure(scene, view_layer);
   if (ob == BKE_view_layer_active_object_get(view_layer)) {
     if (BKE_object_defgroup_active_index_get(ob) == te->index + 1) {
       return OL_DRAWSEL_NORMAL;
@@ -857,12 +872,14 @@ static eOLDrawState tree_element_defgroup_state_get(const ViewLayer *view_layer,
   return OL_DRAWSEL_NONE;
 }
 
-static eOLDrawState tree_element_bone_state_get(const ViewLayer *view_layer,
+static eOLDrawState tree_element_bone_state_get(const Scene *scene,
+                                                ViewLayer *view_layer,
                                                 const TreeElement *te,
                                                 const TreeStoreElem *tselem)
 {
   const bArmature *arm = (const bArmature *)tselem->id;
   const Bone *bone = static_cast<Bone *>(te->directdata);
+  BKE_view_layer_synced_ensure(scene, view_layer);
   const Object *ob = BKE_view_layer_active_object_get(view_layer);
   if (ob && ob->data == arm) {
     if (bone->flag & BONE_SELECTED) {
@@ -896,11 +913,13 @@ static eOLDrawState tree_element_object_state_get(const TreeViewContext *tvc,
   return (tselem->id == (const ID *)tvc->obact) ? OL_DRAWSEL_NORMAL : OL_DRAWSEL_NONE;
 }
 
-static eOLDrawState tree_element_pose_state_get(const ViewLayer *view_layer,
+static eOLDrawState tree_element_pose_state_get(const Scene *scene,
+                                                const ViewLayer *view_layer,
                                                 const TreeStoreElem *tselem)
 {
   const Object *ob = (const Object *)tselem->id;
   /* This will just lookup in a cache, it will not change the arguments. */
+  BKE_view_layer_synced_ensure(scene, (ViewLayer *)view_layer);
   const Base *base = BKE_view_layer_base_find((ViewLayer *)view_layer, (Object *)ob);
   if (base == nullptr) {
     /* Armature not instantiated in current scene (e.g. inside an appended group). */
@@ -942,12 +961,14 @@ static eOLDrawState tree_element_viewlayer_state_get(const bContext *C, const Tr
   return OL_DRAWSEL_NONE;
 }
 
-static eOLDrawState tree_element_posegroup_state_get(const ViewLayer *view_layer,
+static eOLDrawState tree_element_posegroup_state_get(const Scene *scene,
+                                                     ViewLayer *view_layer,
                                                      const TreeElement *te,
                                                      const TreeStoreElem *tselem)
 {
   const Object *ob = (const Object *)tselem->id;
 
+  BKE_view_layer_synced_ensure(scene, view_layer);
   if (ob == BKE_view_layer_active_object_get(view_layer) && ob->pose) {
     if (ob->pose->active_group == te->index + 1) {
       return OL_DRAWSEL_NORMAL;
@@ -1008,12 +1029,14 @@ static eOLDrawState tree_element_layer_collection_state_get(const bContext *C,
   return OL_DRAWSEL_NONE;
 }
 
-static eOLDrawState tree_element_active_material_get(const ViewLayer *view_layer,
+static eOLDrawState tree_element_active_material_get(const Scene *scene,
+                                                     ViewLayer *view_layer,
                                                      const TreeElement *te)
 {
   /* we search for the object parent */
   const Object *ob = (const Object *)outliner_search_back((TreeElement *)te, ID_OB);
   /* Note : ob->matbits can be nullptr when a local object points to a library mesh. */
+  BKE_view_layer_synced_ensure(scene, view_layer);
   if (ob == nullptr || ob != BKE_view_layer_active_object_get(view_layer) ||
       ob->matbits == nullptr) {
     return OL_DRAWSEL_NONE; /* just paranoia */
@@ -1084,7 +1107,7 @@ eOLDrawState tree_element_active_state_get(const TreeViewContext *tvc,
       return OL_DRAWSEL_NONE;
       break;
     case ID_MA:
-      return tree_element_active_material_get(tvc->view_layer, te);
+      return tree_element_active_material_get(tvc->scene, tvc->view_layer, te);
     case ID_WO:
       return tree_element_active_world_get(tvc->scene, te);
     case ID_CA:
@@ -1100,9 +1123,9 @@ eOLDrawState tree_element_type_active_state_get(const bContext *C,
 {
   switch (tselem->type) {
     case TSE_DEFGROUP:
-      return tree_element_defgroup_state_get(tvc->view_layer, te, tselem);
+      return tree_element_defgroup_state_get(tvc->scene, tvc->view_layer, te, tselem);
     case TSE_BONE:
-      return tree_element_bone_state_get(tvc->view_layer, te, tselem);
+      return tree_element_bone_state_get(tvc->scene, tvc->view_layer, te, tselem);
     case TSE_EBONE:
       return tree_element_ebone_state_get(te);
     case TSE_MODIFIER:
@@ -1112,7 +1135,7 @@ eOLDrawState tree_element_type_active_state_get(const bContext *C,
     case TSE_LINKED_PSYS:
       return OL_DRAWSEL_NONE;
     case TSE_POSE_BASE:
-      return tree_element_pose_state_get(tvc->view_layer, tselem);
+      return tree_element_pose_state_get(tvc->scene, tvc->view_layer, tselem);
     case TSE_POSE_CHANNEL:
       return tree_element_posechannel_state_get(tvc->ob_pose, te, tselem);
     case TSE_CONSTRAINT_BASE:
@@ -1121,7 +1144,7 @@ eOLDrawState tree_element_type_active_state_get(const bContext *C,
     case TSE_R_LAYER:
       return tree_element_viewlayer_state_get(C, te);
     case TSE_POSEGRP:
-      return tree_element_posegroup_state_get(tvc->view_layer, te, tselem);
+      return tree_element_posegroup_state_get(tvc->scene, tvc->view_layer, te, tselem);
     case TSE_SEQUENCE:
       return tree_element_sequence_state_get(tvc->scene, te);
     case TSE_SEQUENCE_DUP:
@@ -1402,6 +1425,7 @@ static void do_outliner_item_activate_tree_element(bContext *C,
     }
     else if ((te->idcode == ID_GR) && (space_outliner->outlinevis != SO_VIEW_LAYER)) {
       Collection *gr = (Collection *)tselem->id;
+      BKE_view_layer_synced_ensure(tvc->scene, tvc->view_layer);
 
       if (extend) {
         eObjectSelect_Mode sel = BA_SELECT;
@@ -1575,7 +1599,9 @@ static bool outliner_is_co_within_active_mode_column(bContext *C,
                                                      SpaceOutliner *space_outliner,
                                                      const float view_mval[2])
 {
+  const Scene *scene = CTX_data_scene(C);
   ViewLayer *view_layer = CTX_data_view_layer(C);
+  BKE_view_layer_synced_ensure(scene, view_layer);
   Object *obact = BKE_view_layer_active_object_get(view_layer);
 
   return outliner_is_co_within_mode_column(space_outliner, view_mval) && obact &&

commit 8e57359b7d9f58eea002807508c1c9be13bd2d55

Fix #116193: Assert on selecting object in outliner.
The asserts added in 93df7e930f revealed the bug, which was passing the
wrong Scene pointer to `BKE_view_layer_synced_ensure` in affected code.

Issue likely introduced in 7e1c6170af7cff, was probably never actually a
crash-case because the viewlayer would always be in sync already when
this was called from the Outliner (besides perhaps some extremely rare
edge cases).

To be backported to potential bugfix release of 4.0 together with
93df7e930f commit.

source/blender/editors/space_outliner/outliner_select.cc
@@ -328,7 +328,7 @@ static void tree_element_object_activate(bContext *C,
   }
 
   /* find associated base in current scene */
-  BKE_view_layer_synced_ensure(sce, view_layer);
+  BKE_view_layer_synced_ensure(scene, view_layer);
   base = BKE_view_layer_base_find(view_layer, ob);
 
   if (scene->toolsettings->object_flag & SCE_OBJECT_MODE_LOCK) {