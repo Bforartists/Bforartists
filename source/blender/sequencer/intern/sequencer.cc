Bforartists/Bforartists

commit 72e90ae2a4985dbbeb54c7664575bd848a1adb65

Fix: #113084 VSE equalizer stutters when any strip is animated
Only update equalizer data, when sound is tagged to be recalculated.

Co-authored-by: menda <alguien@aqui.es>
Pull Request: https://projects.blender.org/blender/blender/pulls/113156

source/blender/sequencer/intern/sequencer.cc
@@ -917,16 +917,15 @@ static bool seq_update_seq_cb(Sequence *seq, void *user_data)
     if (seq->sound != nullptr) {
       if (scene->id.recalc & ID_RECALC_AUDIO || seq->sound->id.recalc & ID_RECALC_AUDIO) {
         BKE_sound_update_scene_sound(seq->scene_sound, seq->sound);
-      }
+        void *sound = seq->sound->playback_handle;
 
-      void *sound = seq->sound->playback_handle;
-
-      if (!BLI_listbase_is_empty(&seq->modifiers)) {
-        LISTBASE_FOREACH (SequenceModifierData *, smd, &seq->modifiers) {
-          sound = SEQ_sound_modifier_recreator(seq, smd, sound);
+        if (!BLI_listbase_is_empty(&seq->modifiers)) {
+          LISTBASE_FOREACH (SequenceModifierData *, smd, &seq->modifiers) {
+            sound = SEQ_sound_modifier_recreator(seq, smd, sound);
+          }
         }
+        BKE_sound_update_sequence_handle(seq->scene_sound, sound);
       }
-      BKE_sound_update_sequence_handle(seq->scene_sound, sound);
     }
     BKE_sound_set_scene_sound_volume(
         seq->scene_sound, seq->volume, (seq->flag & SEQ_AUDIO_VOLUME_ANIMATED) != 0);

commit 6b81be0d5cc30eeef8cf9a432ab080e01a7dd9ed

Fix #115305: Sound equalizer not applied when rendering
This was caused by 72e90ae2a4, which only updated sound when tagged by
`ID_RECALC_AUDIO`. On undo or rendering, this flag is missing.

Update equalizer when recalc is flagged by `ID_RECALC_COPY_ON_WRITE`.

Pull Request: https://projects.blender.org/blender/blender/pulls/116282

source/blender/sequencer/intern/sequencer.cc
@@ -927,11 +927,15 @@ static void seq_update_sound_modifiers(Sequence *seq)
   BKE_sound_update_sequence_handle(seq->scene_sound, sound_handle);
 }
 
+static bool must_update_strip_sound(Scene *scene, Sequence *seq)
+{
+  return (scene->id.recalc & ID_RECALC_AUDIO | ID_RECALC_COPY_ON_WRITE) != 0 ||
+         (seq->sound->id.recalc & ID_RECALC_AUDIO | ID_RECALC_COPY_ON_WRITE) != 0;
+}
+
 static void seq_update_sound_strips(Scene *scene, Sequence *seq)
 {
-  if (seq->sound == nullptr || ((scene->id.recalc & ID_RECALC_AUDIO) == 0 &&
-                                (seq->sound->id.recalc & ID_RECALC_AUDIO) == 0))
-  {
+  if (seq->sound == nullptr || !must_update_strip_sound(scene, seq)) {
     return;
   }
   /* Ensure strip is playing correct sound. */