Bforartists/Bforartists

commit 5f2dfe48aaf26e5be6e2954bf1a87e67b5ed5c9b

EEVEE-Next: Gbuffer Optimization
This modify the GBuffer layout to store less bits per closures.
This allows packing all closures into 64 bits or 96 bits.
In turn, this reduces the amount of data stored for most
usual materials.

Moreover, this contain some groundwork for the getting rid of the
hard-coded closure type. But evaluation shaders still use
the hard-coded types.

This adds tests for checking packing and unpacking of the gbuffer
doesn't loose any data.

Related to #115966

Pull Request: https://projects.blender.org/blender/blender/pulls/116476

source/blender/draw/engines/eevee_next/shaders/eevee_subsurface_convolve_comp.glsl
@@ -92,13 +92,13 @@ void main(void)
   float depth = texelFetch(depth_tx, texel, 0).r;
   vec3 vP = drw_point_screen_to_view(vec3(center_uv, depth));
 
-  GBufferData gbuf = gbuffer_read(gbuf_header_tx, gbuf_closure_tx, gbuf_color_tx, texel);
+  GBufferReader gbuf = gbuffer_read(gbuf_header_tx, gbuf_closure_tx, gbuf_normal_tx, texel);
 
-  if (gbuf.diffuse.sss_id == 0u) {
+  if (gbuf.data.diffuse.sss_id == 0u) {
     return;
   }
 
-  float max_radius = reduce_max(gbuf.diffuse.sss_radius);
+  float max_radius = reduce_max(gbuf.data.diffuse.sss_radius);
 
   float homcoord = ProjectionMatrix[2][3] * vP.z + ProjectionMatrix[3][3];
   vec2 sample_scale = vec2(ProjectionMatrix[0][0], ProjectionMatrix[1][1]) *
@@ -111,9 +111,10 @@ void main(void)
   }
 
   /* Avoid too small radii that have float imprecision. */
-  vec3 clamped_sss_radius = max(vec3(1e-4), gbuf.diffuse.sss_radius / max_radius) * max_radius;
+  vec3 clamped_sss_radius = max(vec3(1e-4), gbuf.data.diffuse.sss_radius / max_radius) *
+                            max_radius;
   /* Scale albedo because we can have HDR value caused by BSDF sampling. */
-  vec3 albedo = gbuf.diffuse.color / max(1e-6, reduce_max(gbuf.diffuse.color));
+  vec3 albedo = gbuf.data.diffuse.color / max(1e-6, reduce_max(gbuf.data.diffuse.color));
   vec3 d = burley_setup(clamped_sss_radius, albedo);
 
   /* Do not rotate too much to avoid too much cache misses. */
@@ -131,7 +132,7 @@ void main(void)
 
     SubSurfaceSample samp = sample_neighborhood(sample_uv);
     /* Reject radiance from other surfaces. Avoids light leak between objects. */
-    if (samp.sss_id != gbuf.diffuse.sss_id) {
+    if (samp.sss_id != gbuf.data.diffuse.sss_id) {
       continue;
     }
     /* Slide 34. */

source/blender/draw/engines/eevee_next/shaders/eevee_subsurface_setup_comp.glsl
@@ -23,16 +23,16 @@ void main(void)
 
   barrier();
 
-  GBufferData gbuf = gbuffer_read(gbuf_header_tx, gbuf_closure_tx, gbuf_color_tx, texel);
+  GBufferReader gbuf = gbuffer_read(gbuf_header_tx, gbuf_closure_tx, gbuf_normal_tx, texel);
 
   if (gbuf.has_sss) {
     vec3 radiance = imageLoad(direct_light_img, texel).rgb +
                     imageLoad(indirect_light_img, texel).rgb;
 
-    float max_radius = reduce_max(gbuf.diffuse.sss_radius);
+    float max_radius = reduce_max(gbuf.data.diffuse.sss_radius);
 
     imageStore(radiance_img, texel, vec4(radiance, 0.0));
-    imageStore(object_id_img, texel, uvec4(gbuf.diffuse.sss_id));
+    imageStore(object_id_img, texel, uvec4(gbuf.data.diffuse.sss_id));
 
     vec2 center_uv = (vec2(texel) + 0.5) / vec2(textureSize(gbuf_header_tx, 0));
     float depth = texelFetch(depth_tx, texel, 0).r;

source/blender/draw/CMakeLists.txt
@@ -509,6 +509,7 @@ set(GLSL_SRC
   engines/eevee_next/shaders/eevee_film_lib.glsl
   engines/eevee_next/shaders/eevee_forward_lib.glsl
   engines/eevee_next/shaders/eevee_gbuffer_lib.glsl
+  engines/eevee_next/shaders/eevee_gbuffer_test.glsl
   engines/eevee_next/shaders/eevee_geom_curves_vert.glsl
   engines/eevee_next/shaders/eevee_geom_gpencil_vert.glsl
   engines/eevee_next/shaders/eevee_geom_mesh_vert.glsl

source/blender/draw/engines/eevee_next/eevee_defines.hh
@@ -184,7 +184,7 @@
 #define RBUFS_VALUE_SLOT 1
 #define RBUFS_CRYPTOMATTE_SLOT 2
 #define GBUF_CLOSURE_SLOT 3
-#define GBUF_COLOR_SLOT 4
+#define GBUF_NORMAL_SLOT 4
 #define GBUF_HEADER_SLOT 5
 /* Volume properties pass do not write to `rbufs`. Reuse the same bind points. */
 #define VOLUME_PROP_SCATTERING_IMG_SLOT 0

source/blender/draw/engines/eevee_next/eevee_gbuffer.hh
@@ -127,28 +127,28 @@ struct GBuffer {
   /* TODO(fclem): Use texture from pool once they support texture array and layer views. */
   Texture header_tx = {"GBufferHeader"};
   Texture closure_tx = {"GBufferClosure"};
-  Texture color_tx = {"GBufferColor"};
+  Texture normal_tx = {"GBufferNormal"};
   /* References to the GBuffer layer range [1..max]. */
   GPUTexture *closure_img_tx = nullptr;
-  GPUTexture *color_img_tx = nullptr;
+  GPUTexture *normal_img_tx = nullptr;
 
-  void acquire(int2 extent, int closure_layer_count, int color_layer_count)
+  void acquire(int2 extent, int data_count, int normal_count)
   {
-    /* Always allocating 2 layers so that the image view is always valid. */
-    closure_layer_count = max_ii(2, closure_layer_count);
-    color_layer_count = max_ii(2, color_layer_count);
+    /* Always allocating enough layers so that the image view is always valid. */
+    data_count = max_ii(3, data_count);
+    normal_count = max_ii(2, normal_count);
 
     eGPUTextureUsage usage = GPU_TEXTURE_USAGE_SHADER_READ | GPU_TEXTURE_USAGE_SHADER_WRITE |
                              GPU_TEXTURE_USAGE_ATTACHMENT;
     header_tx.ensure_2d(GPU_R16UI, extent, usage);
-    closure_tx.ensure_2d_array(GPU_RGBA16, extent, closure_layer_count, usage);
-    color_tx.ensure_2d_array(GPU_RGB10_A2, extent, color_layer_count, usage);
+    closure_tx.ensure_2d_array(GPU_RGB10_A2, extent, data_count, usage);
+    normal_tx.ensure_2d_array(GPU_RG16, extent, normal_count, usage);
     /* Ensure layer view for frame-buffer attachment. */
     closure_tx.ensure_layer_views();
-    color_tx.ensure_layer_views();
+    normal_tx.ensure_layer_views();
     /* Ensure layer view for image store. */
-    closure_img_tx = closure_tx.layer_range_view(1, closure_layer_count - 1);
-    color_img_tx = color_tx.layer_range_view(1, color_layer_count - 1);
+    closure_img_tx = closure_tx.layer_range_view(2, data_count - 2);
+    normal_img_tx = normal_tx.layer_range_view(1, normal_count - 1);
   }
 
   void release()
@@ -156,17 +156,17 @@ struct GBuffer {
     /* TODO(fclem): Use texture from pool once they support texture array. */
     // header_tx.release();
     // closure_tx.release();
-    // color_tx.release();
+    // normal_tx.release();
 
     closure_img_tx = nullptr;
-    color_img_tx = nullptr;
+    normal_img_tx = nullptr;
   }
 
   template<typename PassType> void bind_resources(PassType &pass)
   {
     pass.bind_texture("gbuf_header_tx", &header_tx);
     pass.bind_texture("gbuf_closure_tx", &closure_tx);
-    pass.bind_texture("gbuf_color_tx", &color_tx);
+    pass.bind_texture("gbuf_normal_tx", &normal_tx);
   }
 };
 

source/blender/draw/engines/eevee_next/eevee_pipeline.cc
@@ -422,8 +422,8 @@ void DeferredLayerBase::gbuffer_pass_sync(Instance &inst)
                                   GPU_ATTACHEMENT_WRITE,
                                   GPU_ATTACHEMENT_WRITE});
   /* G-buffer. */
+  gbuffer_ps_.bind_image(GBUF_NORMAL_SLOT, &inst.gbuffer.normal_img_tx);
   gbuffer_ps_.bind_image(GBUF_CLOSURE_SLOT, &inst.gbuffer.closure_img_tx);
-  gbuffer_ps_.bind_image(GBUF_COLOR_SLOT, &inst.gbuffer.color_img_tx);
   /* RenderPasses & AOVs. */
   gbuffer_ps_.bind_image(RBUFS_COLOR_SLOT, &inst.render_buffers.rp_color_tx);
   gbuffer_ps_.bind_image(RBUFS_VALUE_SLOT, &inst.render_buffers.rp_value_tx);

source/blender/draw/engines/eevee_next/eevee_pipeline.hh
@@ -190,15 +190,26 @@ struct DeferredLayerBase {
   /* Return the amount of gbuffer layer needed. */
   int closure_layer_count() const
   {
-    return count_bits_i(closure_bits_ & (CLOSURE_REFRACTION | CLOSURE_REFLECTION |
-                                         CLOSURE_DIFFUSE | CLOSURE_TRANSLUCENT | CLOSURE_SSS));
+    /* Diffuse and translucent require only one layer. */
+    int count = count_bits_i(closure_bits_ & (CLOSURE_DIFFUSE | CLOSURE_TRANSLUCENT));
+    /* SSS require an additional layer compared to diffuse. */
+    count += count_bits_i(closure_bits_ & CLOSURE_SSS);
+    /* Reflection and refraction can have at most two layers. */
+    count += 2 * count_bits_i(closure_bits_ & (CLOSURE_REFRACTION | CLOSURE_REFLECTION));
+    return count;
   }
 
   /* Return the amount of gbuffer layer needed. */
-  int color_layer_count() const
+  int normal_layer_count() const
   {
-    return count_bits_i(closure_bits_ & (CLOSURE_REFRACTION | CLOSURE_REFLECTION |
-                                         CLOSURE_DIFFUSE | CLOSURE_TRANSLUCENT));
+    /* TODO(fclem): We could count the number of different tangent frame in the shader and use
+     * min(tangent_frame_count, closure_count) once we have the normal reuse optimization.
+     * For now, allocate a split normal layer for each Closure. */
+    int count = count_bits_i(closure_bits_ & (CLOSURE_REFRACTION | CLOSURE_REFLECTION |
+                                              CLOSURE_DIFFUSE | CLOSURE_TRANSLUCENT));
+    /* Count the additional infos layer needed by some closures. */
+    count += count_bits_i(closure_bits_ & (CLOSURE_SSS | CLOSURE_TRANSLUCENT));
+    return count;
   }
 
   void gbuffer_pass_sync(Instance &inst);
@@ -309,9 +320,9 @@ class DeferredPipeline {
   }
 
   /* Return the maximum amount of gbuffer layer needed. */
-  int color_layer_count() const
+  int normal_layer_count() const
   {
-    return max_ii(opaque_layer_.color_layer_count(), refraction_layer_.color_layer_count());
+    return max_ii(opaque_layer_.normal_layer_count(), refraction_layer_.normal_layer_count());
   }
 
   void debug_draw(draw::View &view, GPUFrameBuffer *combined_fb);
@@ -525,9 +536,9 @@ class DeferredProbePipeline {
   }
 
   /* Return the maximum amount of gbuffer layer needed. */
-  int color_layer_count() const
+  int normal_layer_count() const
   {
-    return opaque_layer_.color_layer_count();
+    return opaque_layer_.normal_layer_count();
   }
 };
 

commit 48028f0b3a420c9574ede58b76fc5840cde3f17a

Fix: EEVEE-Next: Broken Support for SSS
Broken by 5f2dfe48aaf26e5be6e2954bf1a87e67b5ed5c9b

source/blender/draw/engines/eevee_next/shaders/eevee_gbuffer_lib.glsl
@@ -610,6 +610,16 @@ GBufferWriter gbuffer_pack(GBufferDataUndetermined data_in)
 
   /* Check special configurations first. */
 
+  if (has_diffuse) {
+    if (has_sss) {
+      /* Subsurface need to be first to be outputed in first lighting texture. */
+      gbuffer_closure_subsurface_pack(gbuf, data_in.diffuse);
+    }
+    else {
+      gbuffer_closure_diffuse_pack(gbuf, data_in.diffuse);
+    }
+  }
+
   if (has_refraction) {
     if (color_is_grayscale(data_in.refraction.color)) {
       gbuffer_closure_refraction_colorless_pack(gbuf, data_in.refraction);
@@ -625,15 +635,6 @@ GBufferWriter gbuffer_pack(GBufferDataUndetermined data_in)
     }
     else {
       gbuffer_closure_reflection_pack(gbuf, data_in.reflection);
-    }
-  }
-
-  if (has_diffuse) {
-    if (has_sss) {
-      gbuffer_closure_subsurface_pack(gbuf, data_in.diffuse);
-    }
-    else {
-      gbuffer_closure_diffuse_pack(gbuf, data_in.diffuse);
     }
   }
 
@@ -712,7 +713,7 @@ GBufferReader gbuffer_read(samplerGBufferHeader header_tx,
   gbuf.has_translucent = false;
   gbuf.has_sss = false;
   gbuf.data.thickness = 0.0;
-  gbuf.closure_count = 0u;
+  gbuf.closure_count = 0;
   gbuf.layer_data = 0;
   gbuf.layer_normal = 0;
 

source/blender/draw/engines/eevee_next/shaders/eevee_subsurface_convolve_comp.glsl
@@ -94,7 +94,7 @@ void main(void)
 
   GBufferReader gbuf = gbuffer_read(gbuf_header_tx, gbuf_closure_tx, gbuf_normal_tx, texel);
 
-  if (gbuf.data.diffuse.sss_id == 0u) {
+  if (!gbuf.has_sss) {
     return;
   }
 
@@ -132,7 +132,7 @@ void main(void)
 
     SubSurfaceSample samp = sample_neighborhood(sample_uv);
     /* Reject radiance from other surfaces. Avoids light leak between objects. */
-    if (samp.sss_id != gbuf.data.diffuse.sss_id) {
+    if (samp.sss_id != gbuf.data.object_id) {
       continue;
     }
     /* Slide 34. */

source/blender/draw/engines/eevee_next/shaders/eevee_subsurface_setup_comp.glsl
@@ -32,7 +32,7 @@ void main(void)
     float max_radius = reduce_max(gbuf.data.diffuse.sss_radius);
 
     imageStore(radiance_img, texel, vec4(radiance, 0.0));
-    imageStore(object_id_img, texel, uvec4(gbuf.data.diffuse.sss_id));
+    imageStore(object_id_img, texel, uvec4(gbuf.data.object_id));
 
     vec2 center_uv = (vec2(texel) + 0.5) / vec2(textureSize(gbuf_header_tx, 0));
     float depth = texelFetch(depth_tx, texel, 0).r;