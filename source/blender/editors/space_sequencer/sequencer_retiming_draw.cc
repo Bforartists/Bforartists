Bforartists/Bforartists

commit ba78629f4a889463faf188315d0fbdfdd8724e5b

Fix: Incorrect retiming key type
Right "fake key" was shown as transition type. This was used for
debugging, but forgot to change it back.

source/blender/editors/space_sequencer/sequencer_retiming_draw.cc
@@ -408,8 +408,6 @@ static bool fake_keys_draw(const bContext *C,
     SeqRetimingKey fake_key;
     fake_key.strip_frame_index = (right_key_frame - SEQ_time_start_frame_get(seq)) *
                                  SEQ_time_media_playback_rate_factor_get(scene, seq);
-
-    fake_key.flag = SEQ_SPEED_TRANSITION_IN;
     retime_key_draw(C, seq, &fake_key, sh_bindings, selection);
   }
   return true;

commit 91e824ae6b104da2b665c80c0e1e43d6b93bef85

Fix: Uninitialized variable in retiming drawing
Caused by ba78629f4a88.

source/blender/editors/space_sequencer/sequencer_retiming_draw.cc
@@ -408,6 +408,7 @@ static bool fake_keys_draw(const bContext *C,
     SeqRetimingKey fake_key;
     fake_key.strip_frame_index = (right_key_frame - SEQ_time_start_frame_get(seq)) *
                                  SEQ_time_media_playback_rate_factor_get(scene, seq);
+    fake_key.flag = 0;
     retime_key_draw(C, seq, &fake_key, sh_bindings, selection);
   }
   return true;