blender/blender

commit 089c389b5c00ec2794299a18fd5b05dd7c4c54cc

Cleanup: Store UI button drawstr with std::string
Similar to bff51ae66c9babf1d2337eecd5fa38e0717aac6a

source/blender/editors/interface/interface.cc
@@ -378,7 +378,7 @@ static void ui_block_bounds_calc_text(uiBlock *block, float offset)
   uiBut *init_col_bt = static_cast<uiBut *>(block->buttons.first);
   LISTBASE_FOREACH (uiBut *, bt, &block->buttons) {
     if (!ELEM(bt->type, UI_BTYPE_SEPR, UI_BTYPE_SEPR_LINE, UI_BTYPE_SEPR_SPACER)) {
-      j = BLF_width(style->widget.uifont_id, bt->drawstr, sizeof(bt->drawstr));
+      j = BLF_width(style->widget.uifont_id, bt->drawstr.c_str(), bt->drawstr.size());
 
       if (j > i) {
         i = j;
@@ -2519,7 +2519,7 @@ bool ui_but_is_rna_valid(uiBut *but)
   if (but->rnaprop == nullptr || RNA_struct_contains_property(&but->rnapoin, but->rnaprop)) {
     return true;
   }
-  printf("property removed %s: %p\n", but->drawstr, but->rnaprop);
+  printf("property removed %s: %p\n", but->drawstr.c_str(), but->rnaprop);
   return false;
 }
 
@@ -3694,9 +3694,6 @@ void UI_block_set_search_only(uiBlock *block, bool search_only)
 
 static void ui_but_build_drawstr_float(uiBut *but, double value)
 {
-  size_t slen = 0;
-  STR_CONCAT(but->drawstr, slen, but->str.c_str());
-
   PropertySubType subtype = PROP_NONE;
   if (but->rnaprop) {
     subtype = RNA_property_subtype(but->rnaprop);
@@ -3706,57 +3703,54 @@ static void ui_but_build_drawstr_float(uiBut *but, double value)
   value += +0.0f;
 
   if (value == double(FLT_MAX)) {
-    STR_CONCAT(but->drawstr, slen, "inf");
+    but->drawstr = but->str + "inf";
   }
   else if (value == double(-FLT_MAX)) {
-    STR_CONCAT(but->drawstr, slen, "-inf");
+    but->drawstr = but->str + "-inf";
   }
   else if (subtype == PROP_PERCENTAGE) {
     const int prec = ui_but_calc_float_precision(but, value);
-    STR_CONCATF(but->drawstr, slen, "%.*f%%", prec, value);
+    but->drawstr = fmt::format("{}{:.{}f}%", but->str, value, prec);
   }
   else if (subtype == PROP_PIXEL) {
     const int prec = ui_but_calc_float_precision(but, value);
-    STR_CONCATF(but->drawstr, slen, "%.*f px", prec, value);
+    but->drawstr = fmt::format("{}{:.{}f} px", but->str, value, prec);
   }
   else if (subtype == PROP_FACTOR) {
     const int precision = ui_but_calc_float_precision(but, value);
 
     if (U.factor_display_type == USER_FACTOR_AS_FACTOR) {
-      STR_CONCATF(but->drawstr, slen, "%.*f", precision, value);
+      but->drawstr += fmt::format("{}{:.{}f}", but->str, value, precision);
     }
     else {
-      STR_CONCATF(but->drawstr, slen, "%.*f%%", std::max(0, precision - 2), value * 100);
+      but->drawstr += fmt::format("{}{:.{}f}", but->str, value * 100, std::max(0, precision - 2));
     }
   }
   else if (ui_but_is_unit(but)) {
-    char new_str[sizeof(but->drawstr)];
+    char new_str[UI_MAX_DRAW_STR];
     ui_get_but_string_unit(but, new_str, sizeof(new_str), value, true, -1);
-    STR_CONCAT(but->drawstr, slen, new_str);
+    but->drawstr = but->str + new_str;
   }
   else {
     const int prec = ui_but_calc_float_precision(but, value);
-    STR_CONCATF(but->drawstr, slen, "%.*f", prec, value);
+    but->drawstr = fmt::format("{}{:.{}f}", but->str, value, prec);
   }
 }
 
 static void ui_but_build_drawstr_int(uiBut *but, int value)
 {
-  size_t slen = 0;
-  STR_CONCAT(but->drawstr, slen, but->str.c_str());
-
   PropertySubType subtype = PROP_NONE;
   if (but->rnaprop) {
     subtype = RNA_property_subtype(but->rnaprop);
   }
 
-  STR_CONCATF(but->drawstr, slen, "%d", value);
+  but->drawstr = but->str + std::to_string(value);
 
   if (subtype == PROP_PERCENTAGE) {
-    STR_CONCAT(but->drawstr, slen, "%");
+    but->drawstr += "%";
   }
   else if (subtype == PROP_PIXEL) {
-    STR_CONCAT(but->drawstr, slen, " px");
+    but->drawstr += " px";
   }
 }
 
@@ -3843,7 +3837,7 @@ static void ui_but_update_ex(uiBut *but, const bool validate)
             }
           }
         }
-        STRNCPY(but->drawstr, but->str.c_str());
+        but->drawstr = but->str;
       }
       break;
 
@@ -3865,10 +3859,10 @@ static void ui_but_update_ex(uiBut *but, const bool validate)
       if (ui_but_is_float(but)) {
         UI_GET_BUT_VALUE_INIT(but, value);
         const int prec = ui_but_calc_float_precision(but, value);
-        SNPRINTF(but->drawstr, "%s%.*f", but->str.c_str(), prec, value);
+        but->drawstr = fmt::format("{}{:.{}f}", but->str, value, prec);
       }
       else {
-        STRNCPY(but->drawstr, but->str.c_str());
+        but->drawstr = but->str;
       }
 
       break;
@@ -3877,9 +3871,8 @@ static void ui_but_update_ex(uiBut *but, const bool validate)
     case UI_BTYPE_SEARCH_MENU:
       if (!but->editstr) {
         char str[UI_MAX_DRAW_STR];
-
         ui_but_string_get(but, str, UI_MAX_DRAW_STR);
-        SNPRINTF(but->drawstr, "%s%s", but->str.c_str(), str);
+        but->drawstr = fmt::format("{}{}", but->str, str);
       }
       break;
 
@@ -3892,7 +3885,7 @@ static void ui_but_update_ex(uiBut *but, const bool validate)
         UI_GET_BUT_VALUE_INIT(but, value);
         str = WM_key_event_string(short(value), false);
       }
-      SNPRINTF(but->drawstr, "%s%s", but->str.c_str(), str);
+      but->drawstr = fmt::format("{}{}", but->str, str);
       break;
     }
     case UI_BTYPE_HOTKEY_EVENT:
@@ -3906,14 +3899,17 @@ static void ui_but_update_ex(uiBut *but, const bool validate)
           kmi_dummy.ctrl = (hotkey_but->modifier_key & KM_CTRL) ? KM_PRESS : KM_NOTHING;
           kmi_dummy.alt = (hotkey_but->modifier_key & KM_ALT) ? KM_PRESS : KM_NOTHING;
           kmi_dummy.oskey = (hotkey_but->modifier_key & KM_OSKEY) ? KM_PRESS : KM_NOTHING;
-          WM_keymap_item_to_string(&kmi_dummy, true, but->drawstr, sizeof(but->drawstr));
+
+          char kmi_str[128];
+          WM_keymap_item_to_string(&kmi_dummy, true, kmi_str, sizeof(kmi_str));
+          but->drawstr = kmi_str;
         }
         else {
-          STRNCPY_UTF8(but->drawstr, IFACE_("Press a key"));
+          but->drawstr = IFACE_("Press a key");
         }
       }
       else {
-        STRNCPY_UTF8(but->drawstr, but->str.c_str());
+        but->drawstr = but->str;
       }
 
       break;
@@ -3922,13 +3918,13 @@ static void ui_but_update_ex(uiBut *but, const bool validate)
     case UI_BTYPE_HSVCIRCLE:
       break;
     default:
-      STRNCPY(but->drawstr, but->str.c_str());
+      but->drawstr = but->str;
       break;
   }
 
   /* if we are doing text editing, this will override the drawstr */
   if (but->editstr) {
-    but->drawstr[0] = '\0';
+    but->drawstr.clear();
   }
 
   /* text clipping moved to widget drawing code itself */
@@ -6359,7 +6355,7 @@ void UI_but_func_search_set(uiBut *but,
    * buttons where any result is valid anyway, since any string will be valid anyway. */
   if (0 == (but->block->flag & UI_BLOCK_LOOP) && !search_but->results_are_suggestions) {
     /* skip empty buttons, not all buttons need input, we only show invalid */
-    if (but->drawstr[0]) {
+    if (!but->drawstr.empty()) {
       ui_but_search_refresh(search_but);
     }
   }

source/blender/editors/interface/interface_context_menu.cc
@@ -375,8 +375,7 @@ static void ui_but_user_menu_add(bContext *C, uiBut *but, bUserMenu *um)
 {
   BLI_assert(ui_but_is_user_menu_compatible(C, but));
 
-  char drawstr[sizeof(but->drawstr)];
-  ui_but_drawstr_without_sep_char(but, drawstr, sizeof(drawstr));
+  std::string drawstr = ui_but_drawstr_without_sep_char(but);
 
   /* Used for USER_MENU_TYPE_MENU. */
   MenuType *mt = nullptr;
@@ -401,12 +400,12 @@ static void ui_but_user_menu_add(bContext *C, uiBut *but, bUserMenu *um)
                    idname);
           char *expr_result = nullptr;
           if (BPY_run_string_as_string(C, expr_imports, expr, nullptr, &expr_result)) {
-            STRNCPY(drawstr, expr_result);
+            drawstr = expr_result;
             MEM_freeN(expr_result);
           }
           else {
             BLI_assert(0);
-            STRNCPY(drawstr, idname);
+            drawstr = idname;
           }
         }
 #else
@@ -416,7 +415,7 @@ static void ui_but_user_menu_add(bContext *C, uiBut *but, bUserMenu *um)
     }
     ED_screen_user_menu_item_add_operator(
         &um->items,
-        drawstr,
+        drawstr.c_str(),
         but->optype,
         but->opptr ? static_cast<const IDProperty *>(but->opptr->data) : nullptr,
         "",
@@ -434,7 +433,7 @@ static void ui_but_user_menu_add(bContext *C, uiBut *but, bUserMenu *um)
     MEM_freeN(member_id_data_path);
   }
   else if ((mt = UI_but_menutype_get(but))) {
-    ED_screen_user_menu_item_add_menu(&um->items, drawstr, mt);
+    ED_screen_user_menu_item_add_menu(&um->items, drawstr.c_str(), mt);
   }
   else if ((ot = UI_but_operatortype_get_from_enum_menu(but, &prop))) {
     ED_screen_user_menu_item_add_operator(&um->items,

source/blender/editors/interface/interface_handlers.cc
@@ -506,7 +506,7 @@ struct uiAfterFunc {
   std::optional<bContextStore> context;
 
   char undostr[BKE_UNDO_STR_MAX];
-  char drawstr[UI_MAX_DRAW_STR];
+  std::string drawstr;
 };
 
 static void button_activate_init(bContext *C,
@@ -794,7 +794,7 @@ static void ui_handle_afterfunc_add_operator_ex(wmOperatorType *ot,
   }
 
   if (context_but) {
-    ui_but_drawstr_without_sep_char(context_but, after->drawstr, sizeof(after->drawstr));
+    after->drawstr = ui_but_drawstr_without_sep_char(context_but);
   }
 }
 
@@ -909,7 +909,7 @@ static void ui_apply_but_func(bContext *C, uiBut *but)
     after->context = *but->context;
   }
 
-  ui_but_drawstr_without_sep_char(but, after->drawstr, sizeof(after->drawstr));
+  after->drawstr = ui_but_drawstr_without_sep_char(but);
 
   but->optype = nullptr;
   but->opcontext = wmOperatorCallContext(0);
@@ -929,11 +929,11 @@ static void ui_apply_but_undo(uiBut *but)
 
   /* define which string to use for undo */
   if (but->type == UI_BTYPE_MENU) {
-    str = but->drawstr;
+    str = but->drawstr.empty() ? nullptr : but->drawstr.c_str();
     str_len_clip = ui_but_drawstr_len_without_sep_char(but);
   }
-  else if (but->drawstr[0]) {
-    str = but->drawstr;
+  else if (!but->drawstr.empty()) {
+    str = but->drawstr.c_str();
     str_len_clip = ui_but_drawstr_len_without_sep_char(but);
   }
   else {
@@ -1042,7 +1042,7 @@ static void ui_apply_but_funcs_after(bContext *C)
                                                        after.opcontext,
                                                        (after.opptr) ? &opptr : nullptr,
                                                        nullptr,
-                                                       after.drawstr);
+                                                       after.drawstr.c_str());
     }
 
     if (after.opptr) {
@@ -2946,7 +2946,7 @@ void ui_but_clipboard_free()
 
 static int ui_text_position_from_hidden(uiBut *but, int pos)
 {
-  const char *butstr = (but->editstr) ? but->editstr : but->drawstr;
+  const char *butstr = (but->editstr) ? but->editstr : but->drawstr.c_str();
   const char *strpos = butstr;
   const char *str_end = butstr + strlen(butstr);
   for (int i = 0; i < pos; i++) {
@@ -2958,7 +2958,7 @@ static int ui_text_position_from_hidden(uiBut *but, int pos)
 
 static int ui_text_position_to_hidden(uiBut *but, int pos)
 {
-  const char *butstr = (but->editstr) ? but->editstr : but->drawstr;
+  const char *butstr = (but->editstr) ? but->editstr : but->drawstr.c_str();
   return BLI_strnlen_utf8(butstr, pos);
 }
 
@@ -2970,7 +2970,7 @@ void ui_but_text_password_hide(char password_str[UI_MAX_PASSWORD_STR],
     return;
   }
 
-  char *butstr = (but->editstr) ? but->editstr : but->drawstr;
+  char *butstr = (but->editstr) ? but->editstr : but->drawstr.data();
 
   if (restore) {
     /* restore original string */
@@ -4616,7 +4616,7 @@ static int ui_do_but_HOTKEYEVT(bContext *C,
     if (ELEM(event->type, LEFTMOUSE, EVT_PADENTER, EVT_RETKEY, EVT_BUT_OPEN) &&
         (event->val == KM_PRESS))
     {
-      but->drawstr[0] = 0;
+      but->drawstr.clear();
       hotkey_but->modifier_key = 0;
       button_activate_state(C, but, BUTTON_STATE_WAIT_KEY_EVENT);
       return WM_UI_HANDLER_BREAK;

source/blender/editors/interface/interface_intern.hh
@@ -13,6 +13,7 @@
 #include "BLI_compiler_attrs.h"
 #include "BLI_math_vector_types.hh"
 #include "BLI_rect.h"
+#include "BLI_string_ref.hh"
 #include "BLI_vector.hh"
 
 #include "DNA_listBase.h"
@@ -180,7 +181,7 @@ struct uiBut {
 
   std::string str;
 
-  char drawstr[UI_MAX_DRAW_STR] = "";
+  std::string drawstr;
 
   char *placeholder = nullptr;
 
@@ -1254,7 +1255,7 @@ void ui_draw_preview_item(const uiFontStyle *fstyle,
  */
 void ui_draw_preview_item_stateless(const uiFontStyle *fstyle,
                                     rcti *rect,
-                                    const char *name,
+                                    blender::StringRef name,
                                     int iconid,
                                     const uchar text_col[4],
                                     eFontStyle_Align text_align,
@@ -1428,8 +1429,7 @@ uiBut *ui_list_find_mouse_over_ex(const ARegion *region, const int xy[2])
 
 bool ui_but_contains_password(const uiBut *but) ATTR_WARN_UNUSED_RESULT;
 
-size_t ui_but_drawstr_without_sep_char(const uiBut *but, char *str, size_t str_maxncpy)
-    ATTR_NONNULL(1, 2);
+blender::StringRef ui_but_drawstr_without_sep_char(const uiBut *but) ATTR_NONNULL();
 size_t ui_but_drawstr_len_without_sep_char(const uiBut *but);
 size_t ui_but_tip_len_only_first_line(const uiBut *but);
 

source/blender/editors/interface/interface_layout.cc
@@ -1312,7 +1312,7 @@ static void ui_item_menu_hold(bContext *C, ARegion *butregion, uiBut *but)
   block->flag |= UI_BLOCK_POPUP_HOLD;
 
   char direction = UI_DIR_DOWN;
-  if (!but->drawstr[0]) {
+  if (but->drawstr.empty()) {
     switch (RGN_ALIGN_ENUM_FROM_MASK(butregion->alignment)) {
       case RGN_ALIGN_LEFT:
         direction = UI_DIR_RIGHT;
@@ -6265,7 +6265,7 @@ static void ui_layout_introspect_button(DynStr *ds, uiButtonItem *bitem)
 {
   uiBut *but = bitem->but;
   BLI_dynstr_appendf(ds, "'type':%d, ", int(but->type));
-  BLI_dynstr_appendf(ds, "'draw_string':'''%s''', ", but->drawstr);
+  BLI_dynstr_appendf(ds, "'draw_string':'''%s''', ", but->drawstr.c_str());
   /* Not exactly needed, rna has this. */
   BLI_dynstr_appendf(ds, "'tip':'''%s''', ", but->tip ? but->tip : "");
 

source/blender/editors/interface/interface_ops.cc
@@ -1801,8 +1801,7 @@ static bool ui_editsource_uibut_match(uiBut *but_a, uiBut *but_b)
    * if this fails it only means edit-source fails - campbell */
   if (BLI_rctf_compare(&but_a->rect, &but_b->rect, FLT_EPSILON) && (but_a->type == but_b->type) &&
       (but_a->rnaprop == but_b->rnaprop) && (but_a->optype == but_b->optype) &&
-      (but_a->unit_type == but_b->unit_type) &&
-      STREQLEN(but_a->drawstr, but_b->drawstr, UI_MAX_DRAW_STR))
+      (but_a->unit_type == but_b->unit_type) && but_a->drawstr == but_b->drawstr)
   {
     return true;
   }

source/blender/editors/interface/interface_query.cc
@@ -590,18 +590,18 @@ bool ui_but_contains_password(const uiBut *but)
 size_t ui_but_drawstr_len_without_sep_char(const uiBut *but)
 {
   if (but->flag & UI_BUT_HAS_SEP_CHAR) {
-    const char *str_sep = strrchr(but->drawstr, UI_SEP_CHAR);
-    if (str_sep != nullptr) {
-      return (str_sep - but->drawstr);
+    const size_t sep_index = but->drawstr.find(UI_SEP_CHAR);
+    if (sep_index != std::string::npos) {
+      return sep_index;
     }
   }
-  return strlen(but->drawstr);
+  return but->drawstr.size();
 }
 
-size_t ui_but_drawstr_without_sep_char(const uiBut *but, char *str, size_t str_maxncpy)
+blender::StringRef ui_but_drawstr_without_sep_char(const uiBut *but)
 {
   size_t str_len_clip = ui_but_drawstr_len_without_sep_char(but);
-  return BLI_strncpy_rlen(str, but->drawstr, min_zz(str_len_clip + 1, str_maxncpy));
+  return blender::StringRef(but->drawstr).substr(0, str_len_clip);
 }
 
 size_t ui_but_tip_len_only_first_line(const uiBut *but)

source/blender/editors/interface/interface_region_menu_popup.cc
@@ -247,7 +247,7 @@ static uiBlock *ui_block_func_POPUP(bContext *C, uiPopupBlockHandle *handle, voi
   }
   else if (pup->but) {
     /* Minimum width to enforce. */
-    if (pup->but->drawstr[0]) {
+    if (!pup->but->drawstr.empty()) {
       minwidth = BLI_rctf_size_x(&pup->but->rect);
     }
     else {

Bforartists/Bforartists

commit 49a7e5a483e2cd55fb00caf8147db134b8c67139

Fix #117423: Broken UI button float display after cleanup
Caused by 089c389b5c00ec279429. Previously the draw string was created
by appending text inside the buffer multiple times. Now it's formatted
in one step. Part of that change was missing for factor display.

source/blender/editors/interface/interface.cc
@@ -3720,10 +3720,10 @@ static void ui_but_build_drawstr_float(uiBut *but, double value)
     const int precision = ui_but_calc_float_precision(but, value);
 
     if (U.factor_display_type == USER_FACTOR_AS_FACTOR) {
-      but->drawstr += fmt::format("{}{:.{}f}", but->str, value, precision);
+      but->drawstr = fmt::format("{}{:.{}f}", but->str, value, precision);
     }
     else {
-      but->drawstr += fmt::format("{}{:.{}f}", but->str, value * 100, std::max(0, precision - 2));
+      but->drawstr = fmt::format("{}{:.{}f}", but->str, value * 100, std::max(0, precision - 2));
     }
   }
   else if (ui_but_is_unit(but)) {