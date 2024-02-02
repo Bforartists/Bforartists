88130613cf576b201f3fdb44bd8b19d487c4ea4f
Andres Stephens <ondraise15@hotmail.com>
2024-02-02 14:32:21 -0500

    3D View - Delete Keyframe - Remove confirmation dialogue #4060

--- source/blender/editors/animation/keyframing.cc
+++ source/blender/editors/animation/keyframing.cc
@@ -858,7 +858,7 @@ void ANIM_OT_keyframe_delete_v3d(wmOperatorType *ot)
 
   /* flags */
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
-  WM_operator_properties_confirm_or_exec(ot);
+  /*WM_operator_properties_confirm_or_exec(ot); */ /* BFA - we don't confirm delete*/
 }
 
 /* bfa - Apply animation to all selected pose bones through UI animate property */
