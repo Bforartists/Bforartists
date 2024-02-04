ca5ee19d2ec7b303ab712a289db4409780ecef50
Quackarooni <83491032+Quackarooni@users.noreply.github.com>
2024-02-04 14:15:13 +0800

    Cleanup: Remove code comment that does not apply to `InvokeMenuOperator`

--- scripts/startup/bl_ui/generic_column_menu.py
+++ scripts/startup/bl_ui/generic_column_menu.py
@@ -42,7 +42,6 @@ class InvokeMenuOperator:
 
     @classmethod
     def poll(cls, context):
-        # NOTE: This operator only exists to add a poll to the add modifier shortcut in the property editor.
         space = context.space_data
         return space and space.type == cls.space_type and space.context == cls.space_context
 
