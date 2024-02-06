Bforartists/Bforartists

commit b74a495e988ba60f82f3038aa5cbcf3634d74cdf

VSE: Use C++ containers for strip iteration
Use `VectorSet`, `Vector` or `Span` instead of `SeqCollection` struct.
It is now possible to use native `for` loops and `SEQ_ITERATOR_FOREACH`
macro can be removed.

Another feature is, sets of strips no longer needs to be freed. However,
this poses a limitation, that query functions can not be used in case,
where these sets need to be available outside of scope where they are
created.

Pull Request: https://projects.blender.org/blender/blender/pulls/111909

source/blender/sequencer/intern/prefetch.cc
@@ -397,7 +397,7 @@ static bool seq_prefetch_seq_has_disk_cache(PrefetchJob *pfjob,
 static bool seq_prefetch_scene_strip_is_rendered(PrefetchJob *pfjob,
                                                  ListBase *channels,
                                                  ListBase *seqbase,
-                                                 SeqCollection *scene_strips,
+                                                 blender::Span<Sequence *> scene_strips,
                                                  bool is_recursive_check)
 {
   float cfra = seq_prefetch_cfra(pfjob);
@@ -421,8 +421,7 @@ static bool seq_prefetch_scene_strip_is_rendered(PrefetchJob *pfjob,
     }
 
     /* Check if strip is effect of scene strip or uses it as modifier. This is recursive check. */
-    Sequence *seq_scene;
-    SEQ_ITERATOR_FOREACH (seq_scene, scene_strips) {
+    for (Sequence *seq_scene : scene_strips) {
       if (SEQ_relations_render_loop_check(seq, seq_scene)) {
         return true;
       }
@@ -431,27 +430,25 @@ static bool seq_prefetch_scene_strip_is_rendered(PrefetchJob *pfjob,
   return false;
 }
 
-static SeqCollection *query_scene_strips(ListBase *seqbase)
+static blender::VectorSet<Sequence *> query_scene_strips(ListBase *seqbase)
 {
-  SeqCollection *collection = SEQ_query_all_strips_recursive(seqbase);
+  blender::VectorSet<Sequence *> strips;
   LISTBASE_FOREACH (Sequence *, seq, seqbase) {
     if (seq->type != SEQ_TYPE_SCENE || (seq->flag & SEQ_SCENE_STRIPS) != 0) {
-      SEQ_collection_remove_strip(seq, collection);
+      strips.add(seq);
     }
   }
-  return collection;
+  return strips;
 }
 
 /* Prefetch must avoid rendering scene strips, because rendering in background locks UI and can
  * make it unresponsive for long time periods. */
 static bool seq_prefetch_must_skip_frame(PrefetchJob *pfjob, ListBase *channels, ListBase *seqbase)
 {
-  SeqCollection *scene_strips = query_scene_strips(seqbase);
+  blender::VectorSet<Sequence *> scene_strips = query_scene_strips(seqbase);
   if (seq_prefetch_scene_strip_is_rendered(pfjob, channels, seqbase, scene_strips, false)) {
-    SEQ_collection_free(scene_strips);
     return true;
   }
-  SEQ_collection_free(scene_strips);
   return false;
 }
 

source/blender/editors/animation/anim_ops.cc
@@ -107,18 +107,15 @@ static int seq_frame_apply_snap(bContext *C, Scene *scene, const int timeline_fr
 {
 
   ListBase *seqbase = SEQ_active_seqbase_get(SEQ_editing_get(scene));
-  SeqCollection *strips = SEQ_query_all_strips(seqbase);
 
   int best_frame = 0;
   int best_distance = MAXFRAME;
-  Sequence *seq;
-  SEQ_ITERATOR_FOREACH (seq, strips) {
+  for (Sequence *seq : SEQ_query_all_strips(seqbase)) {
     seq_frame_snap_update_best(
         SEQ_time_left_handle_frame_get(scene, seq), timeline_frame, &best_frame, &best_distance);
     seq_frame_snap_update_best(
         SEQ_time_right_handle_frame_get(scene, seq), timeline_frame, &best_frame, &best_distance);
   }
-  SEQ_collection_free(strips);
 
   if (best_distance < seq_snap_threshold_get_frame_distance(C)) {
     return best_frame;

source/blender/editors/space_sequencer/sequencer_add.cc
@@ -351,15 +351,13 @@ static void seq_load_apply_generic_options(bContext *C, wmOperator *op, Sequence
 
   if (RNA_boolean_get(op->ptr, "overlap_shuffle_override")) {
     /* Use set overlap_mode to fix overlaps. */
-    SeqCollection *strip_col = SEQ_collection_create(__func__);
-    SEQ_collection_append_strip(seq, strip_col);
+    blender::VectorSet<Sequence *> strip_col;
+    strip_col.add(seq);
 
     ScrArea *area = CTX_wm_area(C);
     const bool use_sync_markers = (((SpaceSeq *)area->spacedata.first)->flag & SEQ_MARKER_TRANS) !=
                                   0;
-    SEQ_transform_handle_overlap(scene, ed->seqbasep, strip_col, nullptr, use_sync_markers);
-
-    SEQ_collection_free(strip_col);
+    SEQ_transform_handle_overlap(scene, ed->seqbasep, strip_col, use_sync_markers);
   }
   else {
     /* Shuffle strip channel to fix overlaps. */
@@ -370,8 +368,7 @@ static void seq_load_apply_generic_options(bContext *C, wmOperator *op, Sequence
 /* In this alternative version we only check for overlap, but do not do anything about them. */
 static bool seq_load_apply_generic_options_only_test_overlap(bContext *C,
                                                              wmOperator *op,
-                                                             Sequence *seq,
-                                                             SeqCollection *strip_col)
+                                                             Sequence *seq)
 {
   Scene *scene = CTX_data_scene(C);
   Editing *ed = SEQ_editing_get(scene);
@@ -384,8 +381,6 @@ static bool seq_load_apply_generic_options_only_test_overlap(bContext *C,
     seq->flag |= SELECT;
     SEQ_select_active_set(scene, seq);
   }
-
-  SEQ_collection_append_strip(seq, strip_col);
 
   return SEQ_transform_test_overlap(scene, ed->seqbasep, seq);
 }
@@ -779,7 +774,7 @@ static IMB_Proxy_Size seq_get_proxy_size_flags(bContext *C)
   return proxy_sizes;
 }
 
-static void seq_build_proxy(bContext *C, SeqCollection *movie_strips)
+static void seq_build_proxy(bContext *C, blender::Span<Sequence *> movie_strips)
 {
   if (U.sequencer_proxy_setup != USER_SEQ_PROXY_SETUP_AUTOMATIC) {
     return;
@@ -788,8 +783,7 @@ static void seq_build_proxy(bContext *C, SeqCollection *movie_strips)
   wmJob *wm_job = ED_seq_proxy_wm_job_get(C);
   ProxyJob *pj = ED_seq_proxy_job_get(C, wm_job);
 
-  Sequence *seq;
-  SEQ_ITERATOR_FOREACH (seq, movie_strips) {
+  for (Sequence *seq : movie_strips) {
     /* Enable and set proxy size. */
     SEQ_proxy_set(seq, true);
     seq->strip->proxy->build_size_flags = seq_get_proxy_size_flags(C);
@@ -821,7 +815,7 @@ static void sequencer_add_movie_clamp_sound_strip_length(Scene *scene,
 static void sequencer_add_movie_multiple_strips(bContext *C,
                                                 wmOperator *op,
                                                 SeqLoadData *load_data,
-                                                SeqCollection *r_movie_strips)
+                                                blender::VectorSet<Sequence *> &r_movie_strips)
 {
   Main *bmain = CTX_data_main(C);
   Scene *scene = CTX_data_scene(C);
@@ -829,11 +823,7 @@ static void sequencer_add_movie_multiple_strips(bContext *C,
   bool overlap_shuffle_override = RNA_boolean_get(op->ptr, "overlap") == false &&
                                   RNA_boolean_get(op->ptr, "overlap_shuffle_override");
   bool has_seq_overlap = false;
-  SeqCollection *strip_col = nullptr;
-
-  if (overlap_shuffle_override) {
-    strip_col = SEQ_collection_create(__func__);
-  }
+  blender::Vector<Sequence *> added_strips;
 
   RNA_BEGIN (op->ptr, itemptr, "files") {
     char dir_only[FILE_MAX];
@@ -854,10 +844,12 @@ static void sequencer_add_movie_multiple_strips(bContext *C,
       if (RNA_boolean_get(op->ptr, "sound")) {
         seq_sound = SEQ_add_sound_strip(bmain, scene, ed->seqbasep, load_data);
         sequencer_add_movie_clamp_sound_strip_length(scene, seq_movie, seq_sound);
+        added_strips.append(seq_movie);
 
         if (seq_sound) {
           /* The video has sound, shift the video strip up a channel to make room for the sound
            * strip. */
+          added_strips.append(seq_sound);
           seq_movie->machine++;
         }
       }
@@ -865,16 +857,14 @@ static void sequencer_add_movie_multiple_strips(bContext *C,
       load_data->start_frame += SEQ_time_right_handle_frame_get(scene, seq_movie) -
                                 SEQ_time_left_handle_frame_get(scene, seq_movie);
       if (overlap_shuffle_override) {
-        has_seq_overlap |= seq_load_apply_generic_options_only_test_overlap(
-            C, op, seq_sound, strip_col);
-        has_seq_overlap |= seq_load_apply_generic_options_only_test_overlap(
-            C, op, seq_movie, strip_col);
+        has_seq_overlap |= seq_load_apply_generic_options_only_test_overlap(C, op, seq_sound);
+        has_seq_overlap |= seq_load_apply_generic_options_only_test_overlap(C, op, seq_movie);
       }
       else {
         seq_load_apply_generic_options(C, op, seq_sound);
         seq_load_apply_generic_options(C, op, seq_movie);
       }
-      SEQ_collection_append_strip(seq_movie, r_movie_strips);
+      r_movie_strips.add(seq_movie);
     }
   }
   RNA_END;
@@ -884,17 +874,15 @@ static void sequencer_add_movie_multiple_strips(bContext *C,
       ScrArea *area = CTX_wm_area(C);
       const bool use_sync_markers = (((SpaceSeq *)area->spacedata.first)->flag &
                                      SEQ_MARKER_TRANS) != 0;
-      SEQ_transform_handle_overlap(scene, ed->seqbasep, strip_col, nullptr, use_sync_markers);
+      SEQ_transform_handle_overlap(scene, ed->seqbasep, added_strips, use_sync_markers);
     }
-
-    SEQ_collection_free(strip_col);
   }
 }
 
 static bool sequencer_add_movie_single_strip(bContext *C,
                                              wmOperator *op,
                                              SeqLoadData *load_data,
-                                             SeqCollection *r_movie_strips)
+                                             blender::VectorSet<Sequence *> &r_movie_strips)
 {
   Main *bmain = CTX_data_main(C);
   Scene *scene = CTX_data_scene(C);
@@ -902,6 +890,7 @@ static bool sequencer_add_movie_single_strip(bContext *C,
 
   Sequence *seq_movie = nullptr;
   Sequence *seq_sound = nullptr;
+  blender::Vector<Sequence *> added_strips;
 
   seq_movie = SEQ_add_movie_strip(bmain, scene, ed->seqbasep, load_data);
 
@@ -912,7 +901,10 @@ static bool sequencer_add_movie_single_strip(bContext *C,
   if (RNA_boolean_get(op->ptr, "sound")) {
     seq_sound = SEQ_add_sound_strip(bmain, scene, ed->seqbasep, load_data);
     sequencer_add_movie_clamp_sound_strip_length(scene, seq_movie, seq_sound);
+    added_strips.append(seq_movie);
+
     if (seq_sound) {
+      added_strips.append(seq_sound);
       /* The video has sound, shift the video strip up a channel to make room for the sound
        * strip. */
       seq_movie->machine++;
@@ -922,28 +914,23 @@ static bool sequencer_add_movie_single_strip(bContext *C,
   bool overlap_shuffle_override = RNA_boolean_get(op->ptr, "overlap") == false &&
                                   RNA_boolean_get(op->ptr, "overlap_shuffle_override");
   if (overlap_shuffle_override) {
-    SeqCollection *strip_col = SEQ_collection_create(__func__);
     bool has_seq_overlap = false;
 
-    has_seq_overlap |= seq_load_apply_generic_options_only_test_overlap(
-        C, op, seq_sound, strip_col);
-    has_seq_overlap |= seq_load_apply_generic_options_only_test_overlap(
-        C, op, seq_movie, strip_col);
+    has_seq_overlap |= seq_load_apply_generic_options_only_test_overlap(C, op, seq_sound);
+    has_seq_overlap |= seq_load_apply_generic_options_only_test_overlap(C, op, seq_movie);
 
     if (has_seq_overlap) {
       ScrArea *area = CTX_wm_area(C);
       const bool use_sync_markers = (((SpaceSeq *)area->spacedata.first)->flag &
                                      SEQ_MARKER_TRANS) != 0;
-      SEQ_transform_handle_overlap(scene, ed->seqbasep, strip_col, nullptr, use_sync_markers);
+      SEQ_transform_handle_overlap(scene, ed->seqbasep, added_strips, use_sync_markers);
     }
-
-    SEQ_collection_free(strip_col);
   }
   else {
     seq_load_apply_generic_options(C, op, seq_sound);
     seq_load_apply_generic_options(C, op, seq_movie);
   }
-  SEQ_collection_append_strip(seq_movie, r_movie_strips);
+  r_movie_strips.add(seq_movie);
 
   return true;
 }
@@ -960,7 +947,7 @@ static int sequencer_add_movie_strip_exec(bContext *C, wmOperator *op)
     ED_sequencer_deselect_all(scene);
   }
 
-  SeqCollection *movie_strips = SEQ_collection_create(__func__);
+  blender::VectorSet<Sequence *> movie_strips;
   const int tot_files = RNA_property_collection_length(op->ptr,
                                                        RNA_struct_find_property(op->ptr, "files"));
   if (tot_files > 1) {
@@ -970,9 +957,8 @@ static int sequencer_add_movie_strip_exec(bContext *C, wmOperator *op)
     sequencer_add_movie_single_strip(C, op, &load_data, movie_strips);
   }
 
-  if (SEQ_collection_len(movie_strips) == 0) {
+  if (movie_strips.is_empty()) {
     sequencer_add_cancel(C, op);
-    SEQ_collection_free(movie_strips);
     return OPERATOR_CANCELLED;
   }
 
@@ -983,7 +969,6 @@ static int sequencer_add_movie_strip_exec(bContext *C, wmOperator *op)
 
   /* Free custom data. */
   sequencer_add_cancel(C, op);
-  SEQ_collection_free(movie_strips);
 
   return OPERATOR_FINISHED;
 }

source/blender/editors/space_sequencer/sequencer_drag_drop.cc
@@ -299,20 +299,18 @@ static void sequencer_drop_copy(bContext *C, wmDrag *drag, wmDropBox *drop)
     ListBase *channels = SEQ_channels_displayed_get(ed);
     SpaceSeq *sseq = CTX_wm_space_seq(C);
 
-    SeqCollection *strips = SEQ_query_rendered_strips(
+    blender::VectorSet strips = SEQ_query_rendered_strips(
         scene, channels, seqbase, scene->r.cfra, sseq->chanshown);
 
     /* Get the top most strip channel that is in view. */
-    Sequence *seq;
     int max_channel = -1;
-    SEQ_ITERATOR_FOREACH (seq, strips) {
+    for (Sequence *seq : strips) {
       max_channel = max_ii(seq->machine, max_channel);
     }
 
     if (max_channel != -1) {
       RNA_int_set(drop->ptr, "channel", max_channel);
     }
-    SEQ_collection_free(strips);
   }
 }
 

source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -1733,18 +1733,13 @@ static int sequencer_delete_exec(bContext *C, wmOperator *op)
 
   SEQ_prefetch_stop(scene);
 
-  SeqCollection *selected_strips = selected_strips_from_context(C);
-  Sequence *seq;
-
-  SEQ_ITERATOR_FOREACH (seq, selected_strips) {
+  for (Sequence *seq : selected_strips_from_context(C)) {
     SEQ_edit_flag_for_removal(scene, seqbasep, seq);
     if (delete_data) {
       sequencer_delete_strip_data(C, seq);
     }
   }
   SEQ_edit_remove_flagged_sequences(scene, seqbasep);
-
-  SEQ_collection_free(selected_strips);
 
   DEG_id_tag_update(&scene->id, ID_RECALC_SEQUENCER_STRIPS);
   DEG_relations_tag_update(bmain);

source/blender/editors/space_sequencer/sequencer_intern.hh
@@ -9,6 +9,7 @@
 #pragma once
 
 #include "BLI_vector.hh"
+#include "BLI_vector_set.hh"
 #include "DNA_sequence_types.h"
 #include "RNA_access.hh"
 
@@ -21,7 +22,6 @@ struct wmGizmoGroupType;
 struct wmGizmoType;
 struct Main;
 struct Scene;
-struct SeqCollection;
 struct SeqRetimingKey;
 struct Sequence;
 struct SpaceSeq;
@@ -153,7 +153,7 @@ bool sequencer_view_strips_poll(bContext *C);
  * \param C: context
  * \return collection of strips (`Sequence`)
  */
-SeqCollection *all_strips_from_context(bContext *C);
+blender::VectorSet<Sequence *> all_strips_from_context(bContext *C);
 
 /**
  * Returns collection with selected strips presented to user. If operation is done in preview,
@@ -163,7 +163,7 @@ SeqCollection *all_strips_from_context(bContext *C);
  * \param C: context
  * \return collection of strips (`Sequence`)
  */
-SeqCollection *selected_strips_from_context(bContext *C);
+blender::VectorSet<Sequence *> selected_strips_from_context(bContext *C);
 
 /* Externals. */
 

source/blender/editors/space_sequencer/sequencer_preview_draw.cc
@@ -797,14 +797,12 @@ void sequencer_draw_preview(const bContext *C,
   if (!draw_backdrop && scene->ed != nullptr) {
     Editing *ed = SEQ_editing_get(scene);
     ListBase *channels = SEQ_channels_displayed_get(ed);
-    SeqCollection *collection = SEQ_query_rendered_strips(
+    blender::VectorSet strips = SEQ_query_rendered_strips(
         scene, channels, ed->seqbasep, timeline_frame, 0);
-    Sequence *seq;
     Sequence *active_seq = SEQ_select_active_get(scene);
-    SEQ_ITERATOR_FOREACH (seq, collection) {
+    for (Sequence *seq : strips) {
       seq_draw_image_origin_and_outline(C, seq, seq == active_seq);
     }
-    SEQ_collection_free(collection);
   }
 
   if (draw_gpencil && show_imbuf && (sseq->flag & SEQ_SHOW_OVERLAY)) {

source/blender/editors/space_sequencer/sequencer_retiming.cc
@@ -148,14 +148,13 @@ static bool retiming_poll(bContext *C)
 static void retiming_key_overlap(Scene *scene, Sequence *seq)
 {
   ListBase *seqbase = SEQ_active_seqbase_get(SEQ_editing_get(scene));
-  SeqCollection *strips = SEQ_collection_create(__func__);
-  SEQ_collection_append_strip(seq, strips);
-  SeqCollection *dependant = SEQ_collection_create(__func__);
-  SEQ_collection_expand(scene, seqbase, strips, SEQ_query_strip_effect_chain);
-  SEQ_collection_remove_strip(seq, dependant);
+  blender::VectorSet<Sequence *> strips;
+  blender::VectorSet<Sequence *> dependant;
+  strips.add(seq);
+  dependant.add(seq);
+  SEQ_iterator_set_expand(scene, seqbase, dependant, SEQ_query_strip_effect_chain);
+  dependant.remove(seq);
   SEQ_transform_handle_overlap(scene, seqbase, strips, dependant, true);
-  SEQ_collection_free(strips);
-  SEQ_collection_free(dependant);
 }
 
 /*-------------------------------------------------------------------- */
@@ -222,13 +221,12 @@ static bool retiming_key_add_new_for_seq(bContext *C,
 
 static int retiming_key_add_from_selection(bContext *C,
                                            wmOperator *op,
-                                           SeqCollection *strips,
+                                           blender::Span<Sequence *> strips,
                                            const int timeline_frame)
 {
   bool inserted = false;
 
-  Sequence *seq;
-  SEQ_ITERATOR_FOREACH (seq, strips) {
+  for (Sequence *seq : strips) {
     inserted |= retiming_key_add_new_for_seq(C, op, seq, timeline_frame);
   }
 
@@ -268,14 +266,13 @@ static int sequencer_retiming_key_add_exec(bContext *C, wmOperator *op)
   }
 
   int ret_val;
-  SeqCollection *strips = selected_strips_from_context(C);
-  if (SEQ_collection_len(strips) != 0) {
+  blender::VectorSet<Sequence *> strips = selected_strips_from_context(C);
+  if (!strips.is_empty()) {
     ret_val = retiming_key_add_from_selection(C, op, strips, timeline_frame);
   }
   else {
     ret_val = retiming_key_add_to_editable_strips(C, op, timeline_frame);
   }
-  SEQ_collection_free(strips);
 
   WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER, scene);
   return ret_val;
@@ -352,16 +349,14 @@ static bool freeze_frame_add_from_strip_selection(bContext *C,
                                                   const int duration)
 {
   Scene *scene = CTX_data_scene(C);
-  SeqCollection *strips = selected_strips_from_context(C);
+  blender::VectorSet<Sequence *> strips = selected_strips_from_context(C);
   const int timeline_frame = BKE_scene_frame_get(scene);
   bool success = false;
 
-  Sequence *seq;
-  SEQ_ITERATOR_FOREACH (seq, strips) {
+  for (Sequence *seq : strips) {
     success |= freeze_frame_add_new_for_seq(C, op, seq, timeline_frame, duration);
     SEQ_relations_invalidate_cache_raw(scene, seq);
   }
-  SEQ_collection_free(strips);
   return success;
 }
 
@@ -543,10 +538,9 @@ static SeqRetimingKey *ensure_left_and_right_keys(const bContext *C, Sequence *s
 static int strip_speed_set_exec(bContext *C, const wmOperator *op)
 {
   Scene *scene = CTX_data_scene(C);
-  SeqCollection *strips = selected_strips_from_context(C);
+  blender::VectorSet<Sequence *> strips = selected_strips_from_context(C);
 
-  Sequence *seq;
-  SEQ_ITERATOR_FOREACH (seq, strips) {
+  for (Sequence *seq : strips) {
     SEQ_retiming_data_ensure(seq);
     SeqRetimingKey *key = ensure_left_and_right_keys(C, seq);
 
@@ -557,7 +551,6 @@ static int strip_speed_set_exec(bContext *C, const wmOperator *op)
     SEQ_retiming_key_speed_set(scene, seq, key, RNA_float_get(op->ptr, "speed"));
     SEQ_relations_invalidate_cache_raw(scene, seq);
   }
-  SEQ_collection_free(strips);
 
   WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER, scene);
   return OPERATOR_FINISHED;
@@ -811,12 +804,11 @@ int sequencer_retiming_select_all_exec(bContext *C, wmOperator *op)
   Scene *scene = CTX_data_scene(C);
   int action = RNA_enum_get(op->ptr, "action");
 
-  SeqCollection *strips = all_strips_from_context(C);
-  Sequence *seq;
+  blender::VectorSet<Sequence *> strips = all_strips_from_context(C);
 
   if (action == SEL_TOGGLE) {
     action = SEL_SELECT;
-    SEQ_ITERATOR_FOREACH (seq, strips) {
+    for (Sequence *seq : strips) {
       if (!SEQ_retiming_data_is_editable(seq)) {
         continue;
       }
@@ -833,7 +825,7 @@ int sequencer_retiming_select_all_exec(bContext *C, wmOperator *op)
     SEQ_retiming_selection_clear(SEQ_editing_get(scene));
   }
 
-  SEQ_ITERATOR_FOREACH (seq, strips) {
+  for (Sequence *seq : strips) {
     if (!SEQ_retiming_data_is_editable(seq)) {
       continue;
     }

commit 821f139de21eb08b9c837b49f17c4cd2d0b5ae2c

Fix: VSE prefetching doesn't work
Caused by error in b74a495e98. Commit reversed logic for getting scene strips.

source/blender/sequencer/intern/prefetch.cc
@@ -433,7 +433,7 @@ static blender::VectorSet<Sequence *> query_scene_strips(ListBase *seqbase)
 {
   blender::VectorSet<Sequence *> strips;
   LISTBASE_FOREACH (Sequence *, seq, seqbase) {
-    if (seq->type != SEQ_TYPE_SCENE || (seq->flag & SEQ_SCENE_STRIPS) != 0) {
+    if (seq->type == SEQ_TYPE_SCENE && (seq->flag & SEQ_SCENE_STRIPS) == 0) {
       strips.add(seq);
     }
   }