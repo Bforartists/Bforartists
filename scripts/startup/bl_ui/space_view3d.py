34a503d9d3f6bb102832f03b0fd5817684b4a3b1
iyadahmed <iyadahmed430@gmail.com>
2024-02-06 09:53:03 +0200

    Revert "3D View - Delete Keyframe - Remove confirmation dialogue"
    This reverts commit ab65a7cc1894b8a9eaccbe94986e4b21cea2dd5a.

--- scripts/startup/bl_ui/space_view3d.py
+++ scripts/startup/bl_ui/space_view3d.py
@@ -3391,10 +3391,11 @@ class VIEW3D_MT_object_animation(Menu):
         layout = self.layout
 
         layout.operator("anim.keyframe_insert", text="Insert Keyframe", icon='KEYFRAMES_INSERT')
+        layout.operator("anim.keyframe_insert_menu", text="Insert Keyframe with Keying Set", icon='KEYFRAMES_INSERT').always_prompt = True
         layout.operator("anim.keyframe_delete_v3d", text="Delete Keyframes", icon='KEYFRAMES_REMOVE')
         layout.operator("anim.keyframe_clear_v3d", text="Clear Keyframes", icon='KEYFRAMES_CLEAR')
         layout.operator("anim.keying_set_active_set", text="Change Keying Set", icon='KEYINGSET')
-        layout.operator("anim.keyframe_insert_menu", text="Insert Keyframe with Keying Set", icon='KEY_HLT').always_prompt = True
+
         layout.separator()
 
         layout.operator("nla.bake", text="Bake Action", icon='BAKE_ACTION')
@@ -3674,7 +3675,7 @@ class VIEW3D_MT_object_context_menu(Menu):
         layout.separator()
 
         layout.operator("anim.keyframe_insert", text="Insert Keyframe", icon='KEYFRAMES_INSERT')
-        layout.operator("anim.keyframe_insert_menu", text="Insert Keyframe with Keying Set", icon='KEY_HLT').always_prompt = True
+        layout.operator("anim.keyframe_insert_menu", text="Insert Keyframe with Keying Set", icon='KEYFRAMES_INSERT').always_prompt = True
 
         layout.separator()
 
@@ -5030,7 +5031,7 @@ class VIEW3D_MT_pose_context_menu(Menu):
         layout.operator_context = 'INVOKE_REGION_WIN'
 
         layout.operator("anim.keyframe_insert", text="Insert Keyframe", icon='KEYFRAMES_INSERT')
-        layout.operator("anim.keyframe_insert_menu", text="Insert Keyframe with Keying Set", icon='KEY_HLT').always_prompt = True
+        layout.operator("anim.keyframe_insert_menu", text="Insert Keyframe with Keying Set", icon='KEYFRAMES_INSERT').always_prompt = True
 
         layout.separator()
 
