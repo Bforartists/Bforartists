Bforartists/Bforartists

commit bdf01a966b6ad998075ea7038b63c2b2c6abb1b7

Cleanup: unused includes in source/blender/blenkernel
Remove 397 includes.

source/blender/blenkernel/intern/curve_bevel.cc
@@ -11,9 +11,7 @@
 
 #include <cstring>
 
-#include "BLI_alloca.h"
 #include "BLI_listbase.h"
-#include "BLI_math_base.h"
 
 #include "MEM_guardedalloc.h"
 
@@ -22,7 +20,6 @@
 #include "DNA_object_types.h"
 
 #include "BKE_curve.hh"
-#include "BKE_curveprofile.h"
 #include "BKE_displist.h"
 #include "BKE_object_types.hh"
 

commit 015db75028b371ab7d7b538abe647e75f59016ec

Windows: Compile error due to missing header
Caused by bdf01a966b
Error: `undeclared identifier M_PI and M_PI_2`

Pull Request: https://projects.blender.org/blender/blender/pulls/118185

source/blender/blenkernel/intern/curve_bevel.cc
@@ -12,6 +12,8 @@
 #include <cstring>
 
 #include "BLI_listbase.h"
+/* Needed with MSVC for M_PI & M_PI_2 */
+#include "BLI_math_base.h"
 
 #include "MEM_guardedalloc.h"
 