Bforartists/Bforartists

commit 7d2b57330b840ef4c7f8199aa785ec7bc94e4e9c

Fix #108407: Crash on deleting shape key from Blender File view.
`BKE_library_id_can_use_filter_id` was missing the ShapeKey type for
geometry IDs (meshes, legacy curves, and lattices), leading to the
remapping code to fail to do its job when deleting the ShapeKey only.

source/blender/blenkernel/intern/lib_query.cc
@@ -441,9 +441,9 @@ uint64_t BKE_library_id_can_use_filter_id(const ID *owner_id, const bool include
       /* Could be more specific, but simpler to just always say 'yes' here. */
       return FILTER_ID_ALL;
     case ID_ME:
-      return FILTER_ID_ME | FILTER_ID_MA | FILTER_ID_IM;
+      return FILTER_ID_ME | FILTER_ID_MA | FILTER_ID_IM | FILTER_ID_KE;
     case ID_CU_LEGACY:
-      return FILTER_ID_OB | FILTER_ID_MA | FILTER_ID_VF;
+      return FILTER_ID_OB | FILTER_ID_MA | FILTER_ID_VF | FILTER_ID_KE;
     case ID_MB:
       return FILTER_ID_MA;
     case ID_MA:
@@ -453,7 +453,7 @@ uint64_t BKE_library_id_can_use_filter_id(const ID *owner_id, const bool include
     case ID_LT:
       return 0;
     case ID_LA:
-      return FILTER_ID_TE;
+      return FILTER_ID_TE | FILTER_ID_KE;
     case ID_CA:
       return FILTER_ID_OB | FILTER_ID_IM;
     case ID_KE:

commit 00daece84841de125213e07185bb3fdd7ac3bcf3

Fix mistake in fix for #108407 (crash on deleting shapekeys).
Own 7d2b57330b mistakenly added the 'FILTER_ID_KE' to the list IDTypes
usable by Lights (`ID_LA`) instead of Lattices (`ID_LT`)...

Note that this did not cause any additional issue, but only missed part
of the expected fix.

source/blender/blenkernel/intern/lib_query.cc
@@ -451,9 +451,9 @@ uint64_t BKE_library_id_can_use_filter_id(const ID *owner_id, const bool include
     case ID_TE:
       return FILTER_ID_IM | FILTER_ID_OB;
     case ID_LT:
-      return 0;
+      return FILTER_ID_KE;
     case ID_LA:
-      return FILTER_ID_TE | FILTER_ID_KE;
+      return FILTER_ID_TE;
     case ID_CA:
       return FILTER_ID_OB | FILTER_ID_IM;
     case ID_KE: