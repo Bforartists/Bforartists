Bforartists/Bforartists

commit 1b2ae656884ecc91f748911eaf3426c68dbb670e

GPU: Shaders fail to compile on NVIDIA
NVIDIA fails with segmentation fault when compiling shaders due to recent changes.
This PR tweaks the shader code to work around the segmentation fault.

Issue introduced by: 932ac961ea244164c2ab6c899dac6aa4e03ed740

Pull Request: https://projects.blender.org/blender/blender/pulls/118744

source/blender/gpu/shaders/gpu_shader_index_2d_array_lines.glsl
@@ -15,7 +15,7 @@
 void main()
 {
   ivec3 gid = ivec3(gl_GlobalInvocationID);
-  ivec3 nthreads = ivec3(gl_NumWorkGroups) * ivec3(gl_WorkGroupSize);
+  ivec3 nthreads = ivec3(gl_NumWorkGroups * gl_WorkGroupSize);
   for (int y = gid.y + gid.z * nthreads.y; y < ncurves; y += nthreads.y * nthreads.z) {
     for (int x = gid.x; x < elements_per_curve; x += nthreads.x) {
       int store_index = (x + y * elements_per_curve) * 2;

source/blender/gpu/shaders/gpu_shader_index_2d_array_points.glsl
@@ -11,7 +11,7 @@
 void main()
 {
   ivec3 gid = ivec3(gl_GlobalInvocationID);
-  ivec3 nthreads = ivec3(gl_NumWorkGroups) * ivec3(gl_WorkGroupSize);
+  ivec3 nthreads = ivec3(gl_NumWorkGroups * gl_WorkGroupSize);
   for (int y = gid.y + gid.z * nthreads.y; y < ncurves; y += nthreads.y * nthreads.z) {
     for (int x = gid.x; x < elements_per_curve; x += nthreads.x) {
       int store_index = x + y * elements_per_curve;

source/blender/gpu/shaders/gpu_shader_index_2d_array_tris.glsl
@@ -11,8 +11,8 @@
 void main()
 {
   ivec3 gid = ivec3(gl_GlobalInvocationID);
-  ivec3 nthreads = ivec3(gl_NumWorkGroups) * ivec3(gl_WorkGroupSize);
-  for (int y = gid.y + gid.z * nthreads.y; y < ncurves; y += nthreads.y * nthreads.z)
+  ivec3 nthreads = ivec3(gl_NumWorkGroups * gl_WorkGroupSize);
+  for (int y = gid.y + gid.z * nthreads.y; y < ncurves; y += nthreads.y * nthreads.z) {
     for (int x = gid.x; x < elements_per_curve; x += nthreads.x) {
       int store_index = (x + y * elements_per_curve) * 6;
       uint t = x + y * (elements_per_curve * 2 + 2);
@@ -23,4 +23,5 @@ void main()
       out_indices[store_index + 4] = t + 3u;
       out_indices[store_index + 5] = t + 2u;
     }
+  }
 }

commit 62cba76164f57ef11ffd5c44dcaf4496ad89ef69

GPU: Fix shader compilation Metal
Metal uses an union to store the `gl_WorkGroupSize` the union needs to
be unpacked. We first unpack to uvec3 before in order to work around an
NVIDIA driver bug.

Issue introduced by: 1b2ae656884ecc91f748911eaf3426c68dbb670e

Pull Request: https://projects.blender.org/blender/blender/pulls/118749

source/blender/gpu/shaders/gpu_shader_index_2d_array_lines.glsl
@@ -15,7 +15,7 @@
 void main()
 {
   ivec3 gid = ivec3(gl_GlobalInvocationID);
-  ivec3 nthreads = ivec3(gl_NumWorkGroups * gl_WorkGroupSize);
+  ivec3 nthreads = ivec3(gl_NumWorkGroups * uvec3(gl_WorkGroupSize));
   for (int y = gid.y + gid.z * nthreads.y; y < ncurves; y += nthreads.y * nthreads.z) {
     for (int x = gid.x; x < elements_per_curve; x += nthreads.x) {
       int store_index = (x + y * elements_per_curve) * 2;

source/blender/gpu/shaders/gpu_shader_index_2d_array_points.glsl
@@ -11,7 +11,7 @@
 void main()
 {
   ivec3 gid = ivec3(gl_GlobalInvocationID);
-  ivec3 nthreads = ivec3(gl_NumWorkGroups * gl_WorkGroupSize);
+  ivec3 nthreads = ivec3(gl_NumWorkGroups * uvec3(gl_WorkGroupSize));
   for (int y = gid.y + gid.z * nthreads.y; y < ncurves; y += nthreads.y * nthreads.z) {
     for (int x = gid.x; x < elements_per_curve; x += nthreads.x) {
       int store_index = x + y * elements_per_curve;

source/blender/gpu/shaders/gpu_shader_index_2d_array_tris.glsl
@@ -11,7 +11,7 @@
 void main()
 {
   ivec3 gid = ivec3(gl_GlobalInvocationID);
-  ivec3 nthreads = ivec3(gl_NumWorkGroups * gl_WorkGroupSize);
+  ivec3 nthreads = ivec3(gl_NumWorkGroups * uvec3(gl_WorkGroupSize));
   for (int y = gid.y + gid.z * nthreads.y; y < ncurves; y += nthreads.y * nthreads.z) {
     for (int x = gid.x; x < elements_per_curve; x += nthreads.x) {
       int store_index = (x + y * elements_per_curve) * 6;