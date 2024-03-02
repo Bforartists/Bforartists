Bforartists/Bforartists

commit 44d7d36b7fd4f34231226363ad7d8bf4fd112051

Fix #114946: Mesh.clear_geometry should not clear required built-in attributes
Pull Request: https://projects.blender.org/blender/blender/pulls/119012

source/blender/blenkernel/intern/mesh.cc
@@ -456,6 +456,41 @@ bool BKE_mesh_has_custom_loop_normals(Mesh *mesh)
   return CustomData_has_layer(&mesh->corner_data, CD_CUSTOMLOOPNORMAL);
 }
 
+namespace blender::bke {
+
+void mesh_ensure_default_color_attribute_on_add(Mesh &mesh,
+                                                const AttributeIDRef &id,
+                                                AttrDomain domain,
+                                                eCustomDataType data_type)
+{
+  if (id.is_anonymous()) {
+    return;
+  }
+  if (!(CD_TYPE_AS_MASK(data_type) & CD_MASK_COLOR_ALL) ||
+      !(ATTR_DOMAIN_AS_MASK(domain) & ATTR_DOMAIN_MASK_COLOR))
+  {
+    return;
+  }
+  if (mesh.default_color_attribute) {
+    return;
+  }
+  mesh.default_color_attribute = BLI_strdupn(id.name().data(), id.name().size());
+}
+
+static void mesh_ensure_cdlayers_primary(Mesh &mesh)
+{
+  MutableAttributeAccessor attributes = mesh.attributes_for_write();
+  AttributeInitConstruct attribute_init;
+
+  /* Try to create attributes if they do not exist. */
+  attributes.add("position", AttrDomain::Point, CD_PROP_FLOAT3, attribute_init);
+  attributes.add(".edge_verts", AttrDomain::Edge, CD_PROP_INT32_2D, attribute_init);
+  attributes.add(".corner_vert", AttrDomain::Corner, CD_PROP_INT32, attribute_init);
+  attributes.add(".corner_edge", AttrDomain::Corner, CD_PROP_INT32, attribute_init);
+}
+
+}  // namespace blender::bke
+
 void BKE_mesh_free_data_for_undo(Mesh *mesh)
 {
   mesh_free_data(&mesh->id);
@@ -513,6 +548,8 @@ void BKE_mesh_clear_geometry_and_metadata(Mesh *mesh)
   BKE_mesh_runtime_clear_cache(mesh);
   mesh_clear_geometry(*mesh);
   clear_attribute_names(*mesh);
+
+  blender::bke::mesh_ensure_cdlayers_primary(*mesh);
 }
 
 static void mesh_tessface_clear_intern(Mesh *mesh, int free_customdata)
@@ -646,41 +683,6 @@ MutableSpan<MDeformVert> Mesh::deform_verts_for_write()
               &this->vert_data, CD_MDEFORMVERT, CD_SET_DEFAULT, this->verts_num)),
           this->verts_num};
 }
-
-namespace blender::bke {
-
-void mesh_ensure_default_color_attribute_on_add(Mesh &mesh,
-                                                const AttributeIDRef &id,
-                                                AttrDomain domain,
-                                                eCustomDataType data_type)
-{
-  if (id.is_anonymous()) {
-    return;
-  }
-  if (!(CD_TYPE_AS_MASK(data_type) & CD_MASK_COLOR_ALL) ||
-      !(ATTR_DOMAIN_AS_MASK(domain) & ATTR_DOMAIN_MASK_COLOR))
-  {
-    return;
-  }
-  if (mesh.default_color_attribute) {
-    return;
-  }
-  mesh.default_color_attribute = BLI_strdupn(id.name().data(), id.name().size());
-}
-
-static void mesh_ensure_cdlayers_primary(Mesh &mesh)
-{
-  MutableAttributeAccessor attributes = mesh.attributes_for_write();
-  AttributeInitConstruct attribute_init;
-
-  /* Try to create attributes if they do not exist. */
-  attributes.add("position", AttrDomain::Point, CD_PROP_FLOAT3, attribute_init);
-  attributes.add(".edge_verts", AttrDomain::Edge, CD_PROP_INT32_2D, attribute_init);
-  attributes.add(".corner_vert", AttrDomain::Corner, CD_PROP_INT32, attribute_init);
-  attributes.add(".corner_edge", AttrDomain::Corner, CD_PROP_INT32, attribute_init);
-}
-
-}  // namespace blender::bke
 
 Mesh *BKE_mesh_new_nomain(const int verts_num,
                           const int edges_num,

commit 294214b2ef42559d12dc206dbe2d84af1df4db74

Fix: issue in previous commit
Many tests started failing after 44d7d36b7fd4f34231226363ad7d8bf4fd112051
due to memory leaks.

source/blender/blenkernel/BKE_mesh.hh
@@ -311,6 +311,8 @@ void mesh_calc_edges(Mesh &mesh, bool keep_existing_edges, bool select_new_edges
 
 void mesh_flip_faces(Mesh &mesh, const IndexMask &selection);
 
+void mesh_ensure_required_data_layers(Mesh &mesh);
+
 /** Set mesh vertex normals to known-correct values, avoiding future lazy computation. */
 void mesh_vert_normals_assign(Mesh &mesh, Span<float3> vert_normals);
 

source/blender/blenkernel/intern/mesh.cc
@@ -477,7 +477,7 @@ void mesh_ensure_default_color_attribute_on_add(Mesh &mesh,
   mesh.default_color_attribute = BLI_strdupn(id.name().data(), id.name().size());
 }
 
-static void mesh_ensure_cdlayers_primary(Mesh &mesh)
+void mesh_ensure_required_data_layers(Mesh &mesh)
 {
   MutableAttributeAccessor attributes = mesh.attributes_for_write();
   AttributeInitConstruct attribute_init;
@@ -548,8 +548,6 @@ void BKE_mesh_clear_geometry_and_metadata(Mesh *mesh)
   BKE_mesh_runtime_clear_cache(mesh);
   mesh_clear_geometry(*mesh);
   clear_attribute_names(*mesh);
-
-  blender::bke::mesh_ensure_cdlayers_primary(*mesh);
 }
 
 static void mesh_tessface_clear_intern(Mesh *mesh, int free_customdata)
@@ -698,7 +696,7 @@ Mesh *BKE_mesh_new_nomain(const int verts_num,
   mesh->faces_num = faces_num;
   mesh->corners_num = corners_num;
 
-  blender::bke::mesh_ensure_cdlayers_primary(*mesh);
+  blender::bke::mesh_ensure_required_data_layers(*mesh);
   BKE_mesh_face_offsets_ensure_alloc(mesh);
 
   return mesh;
@@ -800,7 +798,7 @@ Mesh *BKE_mesh_new_nomain_from_template_ex(const Mesh *me_src,
 
   /* The destination mesh should at least have valid primary CD layers,
    * even in cases where the source mesh does not. */
-  blender::bke::mesh_ensure_cdlayers_primary(*me_dst);
+  blender::bke::mesh_ensure_required_data_layers(*me_dst);
   BKE_mesh_face_offsets_ensure_alloc(me_dst);
   if (do_tessface && !CustomData_get_layer(&me_dst->fdata_legacy, CD_MFACE)) {
     CustomData_add_layer(&me_dst->fdata_legacy, CD_MFACE, CD_SET_DEFAULT, me_dst->totface_legacy);

source/blender/makesrna/intern/rna_mesh_api.cc
@@ -201,6 +201,8 @@ static void rna_Mesh_clear_geometry(Mesh *mesh)
   BKE_mesh_clear_geometry_and_metadata(mesh);
   BKE_animdata_free(&mesh->id, false);
 
+  blender::bke::mesh_ensure_required_data_layers(*mesh);
+
   DEG_id_tag_update(&mesh->id, ID_RECALC_GEOMETRY_ALL_MODES);
   WM_main_add_notifier(NC_GEOM | ND_DATA, mesh);
 }