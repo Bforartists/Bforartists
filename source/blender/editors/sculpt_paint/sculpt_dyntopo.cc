Bforartists/Bforartists

commit 5900044d0dcadeee2c7c0ac12df1485207b0f51f

Mesh: Move sculpt mask to a generic attribute
Store paint masks as generic float attributes, with the name
`".sculpt_mask"`. This is similar to f44b4fd6c8f2, which made
the same change for face sets. The benefits are general
consistency, nicer code, and more support in newer areas
that deal with attributes like geometry nodes.

The RNA API is replaced with one created in Python. The new
API only presents a single layer as an attribute class, so it
should be simpler to use in general:
- Before: `object.data.vertex_paint_masks[0].data[0].value`
- After: `object.data.vertex_paint_mask.data[0].value`

Pull Request: https://projects.blender.org/blender/blender/pulls/115119

source/blender/editors/sculpt_paint/sculpt_dyntopo.cc
@@ -101,7 +101,7 @@ void SCULPT_dynamic_topology_enable_ex(Main *bmain, Depsgraph *depsgraph, Object
   BM_mesh_bm_from_me(ss->bm, me, &convert_params);
   SCULPT_dynamic_topology_triangulate(ss->bm);
 
-  BM_data_layer_add(ss->bm, &ss->bm->vdata, CD_PAINT_MASK);
+  BM_data_layer_add_named(ss->bm, &ss->bm->vdata, CD_PROP_FLOAT, ".sculpt_mask");
 
   /* Make sure the data for existing faces are initialized. */
   if (me->faces_num != ss->bm->totface) {
@@ -293,31 +293,20 @@ static int dyntopo_warning_popup(bContext *C, wmOperatorType *ot, enum eDynTopoW
   return OPERATOR_INTERFACE;
 }
 
-static bool dyntopo_supports_layer(const CustomDataLayer &layer, const int elem_num)
+static bool dyntopo_supports_layer(const CustomDataLayer &layer)
 {
   if (CD_TYPE_AS_MASK(layer.type) & CD_MASK_PROP_ALL) {
-    if (STREQ(layer.name, ".sculpt_face_set")) {
-      /* Check if only one face set exists. */
-      const blender::Span<int> face_sets(static_cast<const int *>(layer.data), elem_num);
-      for (const int i : face_sets.index_range()) {
-        if (face_sets[i] != face_sets.first()) {
-          return false;
-        }
-      }
-      return true;
-    }
     /* Some data is stored as generic attributes on #Mesh but in flags or fields on #BMesh. */
     return BM_attribute_stored_in_bmesh_builtin(layer.name);
   }
   /* Some layers just encode #Mesh topology or are handled as special cases for dyntopo. */
-  return ELEM(layer.type, CD_PAINT_MASK, CD_ORIGINDEX);
+  return ELEM(layer.type, CD_ORIGINDEX);
 }
 
-static bool dyntopo_supports_customdata_layers(const blender::Span<CustomDataLayer> layers,
-                                               const int elem_num)
+static bool dyntopo_supports_customdata_layers(const blender::Span<CustomDataLayer> layers)
 {
   return std::all_of(layers.begin(), layers.end(), [&](const CustomDataLayer &layer) {
-    return dyntopo_supports_layer(layer, elem_num);
+    return dyntopo_supports_layer(layer);
   });
 }
 
@@ -331,24 +320,16 @@ enum eDynTopoWarnFlag SCULPT_dynamic_topology_check(Scene *scene, Object *ob)
   BLI_assert(ss->bm == nullptr);
   UNUSED_VARS_NDEBUG(ss);
 
-  if (!dyntopo_supports_customdata_layers({me->vert_data.layers, me->vert_data.totlayer},
-                                          me->totvert))
-  {
+  if (!dyntopo_supports_customdata_layers({me->vert_data.layers, me->vert_data.totlayer})) {
     flag |= DYNTOPO_WARN_VDATA;
   }
-  if (!dyntopo_supports_customdata_layers({me->edge_data.layers, me->edge_data.totlayer},
-                                          me->totedge))
-  {
+  if (!dyntopo_supports_customdata_layers({me->edge_data.layers, me->edge_data.totlayer})) {
     flag |= DYNTOPO_WARN_EDATA;
   }
-  if (!dyntopo_supports_customdata_layers({me->face_data.layers, me->face_data.totlayer},
-                                          me->faces_num))
-  {
+  if (!dyntopo_supports_customdata_layers({me->face_data.layers, me->face_data.totlayer})) {
     flag |= DYNTOPO_WARN_LDATA;
   }
-  if (!dyntopo_supports_customdata_layers({me->loop_data.layers, me->loop_data.totlayer},
-                                          me->totloop))
-  {
+  if (!dyntopo_supports_customdata_layers({me->loop_data.layers, me->loop_data.totlayer})) {
     flag |= DYNTOPO_WARN_LDATA;
   }
 

scripts/modules/bpy_types.py
@@ -669,6 +669,19 @@ class Mesh(bpy_types.ID):
     def edge_creases_remove(self):
         _name_convention_attribute_remove(self.attributes, "crease_edge")
 
+    @property
+    def vertex_paint_mask(self):
+        """
+        Mask values for sculpting and painting, corresponding to the ".sculpt_mask" attribute.
+        """
+        return _name_convention_attribute_get(self.attributes, ".sculpt_mask", 'POINT', 'FLOAT')
+
+    def vertex_paint_mask_ensure(self):
+        return _name_convention_attribute_ensure(self.attributes, ".sculpt_mask", 'POINT', 'FLOAT')
+
+    def vertex_paint_mask_remove(self):
+        _name_convention_attribute_remove(self.attributes, ".sculpt_mask")
+
     def shade_flat(self):
         """
         Render and display faces uniform, using face normals,

source/blender/blenkernel/BKE_mesh_legacy_convert.hh
@@ -17,6 +17,11 @@ struct Main;
 struct Mesh;
 struct MFace;
 
+namespace blender::bke {
+void mesh_sculpt_mask_to_legacy(MutableSpan<CustomDataLayer> vert_layers);
+void mesh_sculpt_mask_to_generic(Mesh &mesh);
+}  // namespace blender::bke
+
 void BKE_mesh_legacy_convert_uvs_to_generic(Mesh *mesh);
 
 /**

source/blender/blenkernel/intern/customdata.cc
@@ -770,27 +770,6 @@ static void layerFree_bmesh_elem_py_ptr(void *data, const int count, const int s
 /** \} */
 
 /* -------------------------------------------------------------------- */
-/** \name Callbacks for (`float`, #CD_PAINT_MASK)
- * \{ */
-
-static void layerInterp_paint_mask(const void **sources,
-                                   const float *weights,
-                                   const float * /*sub_weights*/,
-                                   int count,
-                                   void *dest)
-{
-  float mask = 0.0f;
-  for (int i = 0; i < count; i++) {
-    const float interp_weight = weights[i];
-    const float *src = static_cast<const float *>(sources[i]);
-    mask += (*src) * interp_weight;
-  }
-  *(float *)dest = mask;
-}
-
-/** \} */
-
-/* -------------------------------------------------------------------- */
 /** \name Callbacks for (#GridPaintMask, #CD_GRID_PAINT_MASK)
  * \{ */
 
@@ -1821,8 +1800,8 @@ static const LayerTypeInfo LAYERTYPEINFO[CD_NUMTYPES] = {
      nullptr,
      nullptr,
      nullptr},
-    /* 34: CD_PAINT_MASK */
-    {sizeof(float), "", 0, nullptr, nullptr, nullptr, layerInterp_paint_mask, nullptr, nullptr},
+    /* 34: CD_PAINT_MASK */ /* DEPRECATED */
+    {sizeof(float), "", 0, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr},
     /* 35: CD_GRID_PAINT_MASK */
     {sizeof(GridPaintMask),
      "GridPaintMask",
@@ -2038,8 +2017,7 @@ const CustomData_MeshMasks CD_MASK_BAREMESH_ORIGINDEX = {
     /*lmask*/ CD_MASK_PROP_INT32,
 };
 const CustomData_MeshMasks CD_MASK_MESH = {
-    /*vmask*/ (CD_MASK_PROP_FLOAT3 | CD_MASK_MDEFORMVERT | CD_MASK_MVERT_SKIN |
-               CD_MASK_PAINT_MASK | CD_MASK_PROP_ALL),
+    /*vmask*/ (CD_MASK_PROP_FLOAT3 | CD_MASK_MDEFORMVERT | CD_MASK_MVERT_SKIN | CD_MASK_PROP_ALL),
     /*emask*/
     (CD_MASK_FREESTYLE_EDGE | CD_MASK_PROP_ALL),
     /*fmask*/ 0,
@@ -2050,7 +2028,7 @@ const CustomData_MeshMasks CD_MASK_MESH = {
 };
 const CustomData_MeshMasks CD_MASK_DERIVEDMESH = {
     /*vmask*/ (CD_MASK_ORIGINDEX | CD_MASK_MDEFORMVERT | CD_MASK_SHAPEKEY | CD_MASK_MVERT_SKIN |
-               CD_MASK_PAINT_MASK | CD_MASK_ORCO | CD_MASK_CLOTH_ORCO | CD_MASK_PROP_ALL),
+               CD_MASK_ORCO | CD_MASK_CLOTH_ORCO | CD_MASK_PROP_ALL),
     /*emask*/
     (CD_MASK_ORIGINDEX | CD_MASK_FREESTYLE_EDGE | CD_MASK_PROP_ALL),
     /*fmask*/ (CD_MASK_ORIGINDEX | CD_MASK_ORIGSPACE | CD_MASK_PREVIEW_MCOL | CD_MASK_TANGENT),
@@ -2062,7 +2040,7 @@ const CustomData_MeshMasks CD_MASK_DERIVEDMESH = {
 };
 const CustomData_MeshMasks CD_MASK_BMESH = {
     /*vmask*/ (CD_MASK_MDEFORMVERT | CD_MASK_MVERT_SKIN | CD_MASK_SHAPEKEY |
-               CD_MASK_SHAPE_KEYINDEX | CD_MASK_PAINT_MASK | CD_MASK_PROP_ALL),
+               CD_MASK_SHAPE_KEYINDEX | CD_MASK_PROP_ALL),
     /*emask*/ (CD_MASK_FREESTYLE_EDGE | CD_MASK_PROP_ALL),
     /*fmask*/ 0,
     /*pmask*/
@@ -2073,7 +2051,7 @@ const CustomData_MeshMasks CD_MASK_BMESH = {
 const CustomData_MeshMasks CD_MASK_EVERYTHING = {
     /*vmask*/ (CD_MASK_BM_ELEM_PYPTR | CD_MASK_ORIGINDEX | CD_MASK_MDEFORMVERT |
                CD_MASK_MVERT_SKIN | CD_MASK_ORCO | CD_MASK_CLOTH_ORCO | CD_MASK_SHAPEKEY |
-               CD_MASK_SHAPE_KEYINDEX | CD_MASK_PAINT_MASK | CD_MASK_PROP_ALL),
+               CD_MASK_SHAPE_KEYINDEX | CD_MASK_PROP_ALL),
     /*emask*/
     (CD_MASK_BM_ELEM_PYPTR | CD_MASK_ORIGINDEX | CD_MASK_FREESTYLE_EDGE | CD_MASK_PROP_ALL),
     /*fmask*/

source/blender/blenkernel/intern/mesh.cc
@@ -244,6 +244,7 @@ static void mesh_foreach_path(ID *id, BPathForeachPathData *bpath_data)
 static void mesh_blend_write(BlendWriter *writer, ID *id, const void *id_address)
 {
   using namespace blender;
+  using namespace blender::bke;
   Mesh *mesh = reinterpret_cast<Mesh *>(id);
   const bool is_undo = BLO_write_is_undo(writer);
 
@@ -277,6 +278,7 @@ static void mesh_blend_write(BlendWriter *writer, ID *id, const void *id_address
     CustomData_blend_write_prepare(mesh->edge_data, edge_layers, {});
     CustomData_blend_write_prepare(mesh->loop_data, loop_layers, {});
     CustomData_blend_write_prepare(mesh->face_data, face_layers, {});
+    mesh_sculpt_mask_to_legacy(vert_layers);
   }
 
   mesh->runtime = nullptr;

source/blender/blenkernel/intern/mesh_flip_faces.cc
@@ -58,7 +58,6 @@ void mesh_flip_faces(Mesh &mesh, const IndexMask &selection)
   flip_custom_data_type<float4x4>(faces, mesh.loop_data, selection, CD_TANGENT);
   flip_custom_data_type<float4>(faces, mesh.loop_data, selection, CD_MLOOPTANGENT);
   flip_custom_data_type<short2>(faces, mesh.loop_data, selection, CD_CUSTOMLOOPNORMAL);
-  flip_custom_data_type<float>(faces, mesh.loop_data, selection, CD_PAINT_MASK);
   flip_custom_data_type<GridPaintMask>(faces, mesh.loop_data, selection, CD_GRID_PAINT_MASK);
   flip_custom_data_type<OrigSpaceLoop>(faces, mesh.loop_data, selection, CD_ORIGSPACE_MLOOP);
   flip_custom_data_type<MDisps>(faces, mesh.loop_data, selection, CD_MDISPS);

source/blender/blenkernel/intern/mesh_legacy_convert.cc
@@ -2298,4 +2298,57 @@ void BKE_main_mesh_legacy_convert_auto_smooth(Main &bmain)
   }
 }
 
+namespace blender::bke {
+
+void mesh_sculpt_mask_to_legacy(MutableSpan<CustomDataLayer> vert_layers)
+{
+  bool changed = false;
+  for (CustomDataLayer &layer : vert_layers) {
+    if (StringRef(layer.name) == ".sculpt_mask") {
+      layer.type = CD_PAINT_MASK;
+      layer.name[0] = '\0';
+      changed = true;
+      break;
+    }
+  }
+  if (!changed) {
+    return;
+  }
+  /* #CustomData expects the layers to be sorted in increasing order based on type. */
+  std::stable_sort(
+      vert_layers.begin(),
+      vert_layers.end(),
+      [](const CustomDataLayer &a, const CustomDataLayer &b) { return a.type < b.type; });
+}
+
+void mesh_sculpt_mask_to_generic(Mesh &mesh)
+{
+  if (mesh.attributes().contains(".sculpt_mask")) {
+    return;
+  }
+  void *data = nullptr;
+  const ImplicitSharingInfo *sharing_info = nullptr;
+  for (const int i : IndexRange(mesh.vert_data.totlayer)) {
+    CustomDataLayer &layer = mesh.vert_data.layers[i];
+    if (layer.type == CD_PAINT_MASK) {
+      data = layer.data;
+      sharing_info = layer.sharing_info;
+      layer.data = nullptr;
+      layer.sharing_info = nullptr;
+      CustomData_free_layer(&mesh.vert_data, CD_PAINT_MASK, mesh.totvert, i);
+      break;
+    }
+  }
+  if (data != nullptr) {
+    CustomData_add_layer_named_with_data(
+        &mesh.vert_data, CD_PROP_FLOAT, data, mesh.totvert, ".sculpt_mask", sharing_info);
+  }
+  if (sharing_info != nullptr) {
+    sharing_info->remove_user_and_delete_if_last();
+  }
+}
+
+//
+}  // namespace blender::bke
+
 /** \} */

source/blender/blenkernel/intern/mesh_remesh_voxel.cc
@@ -282,19 +282,20 @@ void BKE_mesh_remesh_reproject_paint_mask(Mesh *target, const Mesh *source)
   BVHTreeFromMesh bvhtree = {nullptr};
   BKE_bvhtree_from_mesh_get(&bvhtree, source, BVHTREE_FROM_VERTS, 2);
   const Span<float3> target_positions = target->vert_positions();
-  const float *source_mask = (const float *)CustomData_get_layer(&source->vert_data,
-                                                                 CD_PAINT_MASK);
+  const float *source_mask = (const float *)CustomData_get_layer_named(
+      &source->vert_data, CD_PROP_FLOAT, ".sculpt_mask");
   if (source_mask == nullptr) {
     return;
   }
 
   float *target_mask;
-  if (CustomData_has_layer(&target->vert_data, CD_PAINT_MASK)) {
-    target_mask = (float *)CustomData_get_layer(&target->vert_data, CD_PAINT_MASK);
+  if (CustomData_has_layer_named(&target->vert_data, CD_PROP_FLOAT, ".sculpt_mask")) {
+    target_mask = (float *)CustomData_get_layer_named(
+        &target->vert_data, CD_PROP_FLOAT, ".sculpt_mask");
   }
   else {
-    target_mask = (float *)CustomData_add_layer(
-        &target->vert_data, CD_PAINT_MASK, CD_CONSTRUCT, target->totvert);
+    target_mask = (float *)CustomData_add_layer_named(
+        &target->vert_data, CD_PROP_FLOAT, CD_CONSTRUCT, target->totvert, ".sculpt_mask");
   }
 
   blender::threading::parallel_for(IndexRange(target->totvert), 4096, [&](const IndexRange range) {

commit 1a56d3d237a0c88f68930be221f43de8df59ab68

Fix #119426: Enabling dyntopo creates duplicate mask attribute
The CustomData API automatically creates a unique attribute with
a unique name for generic data types. Caused by 5900044d0dcadeee2c7c.

Pull Request: https://projects.blender.org/blender/blender/pulls/119441

source/blender/editors/sculpt_paint/sculpt_dyntopo.cc
@@ -100,7 +100,7 @@ void enable_ex(Main *bmain, Depsgraph *depsgraph, Object *ob)
   BM_mesh_bm_from_me(ss->bm, mesh, &convert_params);
   triangulate(ss->bm);
 
-  BM_data_layer_add_named(ss->bm, &ss->bm->vdata, CD_PROP_FLOAT, ".sculpt_mask");
+  BM_data_layer_ensure_named(ss->bm, &ss->bm->vdata, CD_PROP_FLOAT, ".sculpt_mask");
 
   /* Make sure the data for existing faces are initialized. */
   if (mesh->faces_num != ss->bm->totface) {