Bforartists/Bforartists

commit 6ba089fa8410b2c360abb17bf7c6f6da677d6454

Fix: Geometry Nodes: GPv3 component is always in Separate Component node
Makes sure the GPv3 component doesn't show up unless the experimental
flag is set.

Pull Request: https://projects.blender.org/blender/blender/pulls/118915

source/blender/nodes/geometry/nodes/node_geo_separate_components.cc
@@ -11,7 +11,9 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_input<decl::Geometry>("Geometry");
   b.add_output<decl::Geometry>("Mesh").propagate_all();
   b.add_output<decl::Geometry>("Curve").propagate_all();
-  b.add_output<decl::Geometry>("Grease Pencil").propagate_all();
+  if (U.experimental.use_grease_pencil_version3) {
+    b.add_output<decl::Geometry>("Grease Pencil").propagate_all();
+  }
   b.add_output<decl::Geometry>("Point Cloud").propagate_all();
   b.add_output<decl::Geometry>("Volume")
       .translation_context(BLT_I18NCONTEXT_ID_ID)
@@ -36,7 +38,7 @@ static void node_geo_exec(GeoNodeExecParams params)
   if (geometry_set.has<CurveComponent>()) {
     curves.add(*geometry_set.get_component<CurveComponent>());
   }
-  if (geometry_set.has<GreasePencilComponent>()) {
+  if (geometry_set.has<GreasePencilComponent>() && U.experimental.use_grease_pencil_version3) {
     grease_pencil.add(*geometry_set.get_component<GreasePencilComponent>());
   }
   if (geometry_set.has<PointCloudComponent>()) {
@@ -51,7 +53,9 @@ static void node_geo_exec(GeoNodeExecParams params)
 
   params.set_output("Mesh", meshes);
   params.set_output("Curve", curves);
-  params.set_output("Grease Pencil", grease_pencil);
+  if (U.experimental.use_grease_pencil_version3) {
+    params.set_output("Grease Pencil", grease_pencil);
+  }
   params.set_output("Point Cloud", point_clouds);
   params.set_output("Volume", volumes);
   params.set_output("Instances", instances);

commit a7254a8cd21db6b6d9ac15590a204021b859269a

Fix #119617: crash because node sockets depend on experimental feature flag
This was caused by 6ba089fa8410b2c360abb17bf7c6f6da677d6454. Now the socket is
always created but made unavailable if the feature is disabled.

Pull Request: https://projects.blender.org/blender/blender/pulls/119620

source/blender/makesrna/intern/rna_userdef.cc
@@ -31,6 +31,7 @@
 #include "BKE_addon.h"
 #include "BKE_appdir.hh"
 #include "BKE_callbacks.hh"
+#include "BKE_node_tree_update.hh"
 #include "BKE_sound.h"
 #include "BKE_studiolight.h"
 
@@ -48,6 +49,8 @@
 #include "WM_types.hh"
 
 #include "BLT_lang.hh"
+
+#include "ED_node.hh"
 
 const EnumPropertyItem rna_enum_preference_section_items[] = {
     {USER_SECTION_INTERFACE, "INTERFACE", 0, "Interface", ""},
@@ -651,6 +654,18 @@ static void rna_userdef_keyconfig_reload_update(bContext *C,
 {
   WM_keyconfig_reload(C);
   USERDEF_TAG_DIRTY;
+}
+
+static void rna_userdef_use_grease_pencil_version3_update(bContext *C, PointerRNA *ptr)
+{
+  Main *bmain = CTX_data_main(C);
+  rna_userdef_keyconfig_reload_update(C, bmain, nullptr, ptr);
+  /* Update nodes because some sockets may only exist depending on whether grease pencil 3 is
+   * enabled. */
+  LISTBASE_FOREACH (bNodeTree *, ntree, &bmain->nodetrees) {
+    BKE_ntree_update_tag_all(ntree);
+  }
+  ED_node_tree_propagate_change(C, bmain, nullptr);
 }
 
 static void rna_userdef_timecode_style_set(PointerRNA *ptr, int value)
@@ -7145,7 +7160,7 @@ static void rna_def_userdef_experimental(BlenderRNA *brna)
   RNA_def_property_ui_text(prop, "Grease Pencil 3.0", "Enable the new grease pencil 3.0 codebase");
   /* The key-map depends on this setting, it needs to be reloaded. */
   RNA_def_property_flag(prop, PROP_CONTEXT_UPDATE);
-  RNA_def_property_update(prop, 0, "rna_userdef_keyconfig_reload_update");
+  RNA_def_property_update(prop, 0, "rna_userdef_use_grease_pencil_version3_update");
 
   prop = RNA_def_property(srna, "use_new_matrix_socket", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "use_new_matrix_socket", 1);

source/blender/nodes/geometry/nodes/node_geo_separate_components.cc
@@ -11,14 +11,22 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_input<decl::Geometry>("Geometry");
   b.add_output<decl::Geometry>("Mesh").propagate_all();
   b.add_output<decl::Geometry>("Curve").propagate_all();
-  if (U.experimental.use_grease_pencil_version3) {
-    b.add_output<decl::Geometry>("Grease Pencil").propagate_all();
-  }
+  b.add_output<decl::Geometry>("Grease Pencil").propagate_all();
   b.add_output<decl::Geometry>("Point Cloud").propagate_all();
   b.add_output<decl::Geometry>("Volume")
       .translation_context(BLT_I18NCONTEXT_ID_ID)
       .propagate_all();
   b.add_output<decl::Geometry>("Instances").propagate_all();
+}
+
+static void node_update(bNodeTree *ntree, bNode *node)
+{
+  LISTBASE_FOREACH (bNodeSocket *, socket, &node->outputs) {
+    if (STREQ(socket->identifier, "Grease Pencil")) {
+      bke::nodeSetSocketAvailability(ntree, socket, U.experimental.use_grease_pencil_version3);
+      break;
+    }
+  }
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
@@ -69,6 +77,7 @@ static void node_register()
       &ntype, GEO_NODE_SEPARATE_COMPONENTS, "Separate Components", NODE_CLASS_GEOMETRY);
   ntype.declare = node_declare;
   ntype.geometry_node_execute = node_geo_exec;
+  ntype.updatefunc = node_update;
   nodeRegisterType(&ntype);
 }
 NOD_REGISTER_NODE(node_register)