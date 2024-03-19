Bforartists/Bforartists

commit b4b830646dcd2e848e8ab65035e15c19dbe5258c

Fix: Tree-view items always collapsed by default
Since c1d4322cd5, having tree-view items uncollapsed by default using
the `set_collapsed()` functions wouldn't work anymore. An attempt to do
this would assert even, so 21ba838780 disabled the assert.

I think a function designed to handle exactly this is the best solution,
it makes the intent & behavior more clear than before, and highlights
that this is a special case.

Mitigates #117957, in that it solves the regression, but tree-views still
don't remember their state on screen-layout changes. This is a known
limitation and not supported.

Pull Request: https://projects.blender.org/blender/blender/pulls/119166

source/blender/editors/interface/interface_template_grease_pencil_layer_tree.cc
@@ -370,12 +370,12 @@ void LayerTreeView::build_tree_node_recursive(TreeViewOrItem &parent, TreeNode &
   if (node.is_layer()) {
     LayerViewItem &item = parent.add_tree_item<LayerViewItem>(this->grease_pencil_,
                                                               node.as_layer());
-    item.set_collapsed(false);
+    item.uncollapse_by_default();
   }
   else if (node.is_group()) {
     LayerGroupViewItem &group_item = parent.add_tree_item<LayerGroupViewItem>(this->grease_pencil_,
                                                                               node.as_group());
-    group_item.set_collapsed(false);
+    group_item.uncollapse_by_default();
     LISTBASE_FOREACH_BACKWARD (GreasePencilLayerTreeNode *, node_, &node.as_group().children) {
       build_tree_node_recursive(group_item, node_->wrap());
     }

source/blender/editors/asset/intern/asset_shelf_catalog_selector.cc
@@ -66,8 +66,7 @@ class AssetCatalogSelectorTree : public ui::AbstractTreeView {
 
     catalog_tree_.foreach_root_item([this](asset_system::AssetCatalogTreeItem &catalog_item) {
       Item &item = build_catalog_items_recursive(*this, catalog_item);
-      /* Uncollapse root items by default (user edits will override this just fine). */
-      item.set_collapsed(false);
+      item.uncollapse_by_default();
     });
   }
 

source/blender/editors/include/UI_tree_view.hh
@@ -213,6 +213,13 @@ class AbstractTreeViewItem : public AbstractViewItem, public TreeViewItemContain
    * \returns true when the collapsed state was changed, false otherwise.
    */
   virtual bool set_collapsed(bool collapsed);
+  /**
+   * Make this item be uncollapsed on first draw (may later be overriden by
+   * #should_be_collapsed()). Must only be done during tree building.
+   *
+   * \note this does not call #on_collapse_change() or #set_collapsed() overrides.
+   */
+  void uncollapse_by_default();
 
   /**
    * Requires the tree to have completed reconstruction, see #is_reconstructed(). Otherwise we

source/blender/editors/interface/interface_template_node_tree_interface.cc
@@ -266,7 +266,7 @@ class NodeTreeInterfaceView : public AbstractTreeView {
               item);
           NodeSocketViewItem &socket_item = parent_item.add_tree_item<NodeSocketViewItem>(
               nodetree_, interface_, *socket);
-          socket_item.set_collapsed(false);
+          socket_item.uncollapse_by_default();
           break;
         }
         case NODE_INTERFACE_PANEL: {
@@ -274,7 +274,7 @@ class NodeTreeInterfaceView : public AbstractTreeView {
               item);
           NodePanelViewItem &panel_item = parent_item.add_tree_item<NodePanelViewItem>(
               nodetree_, interface_, *panel);
-          panel_item.set_collapsed(false);
+          panel_item.uncollapse_by_default();
           add_items_for_panel_recursive(*panel, panel_item);
           break;
         }

source/blender/editors/interface/views/tree_view.cc
@@ -527,10 +527,19 @@ bool AbstractTreeViewItem::set_collapsed(const bool collapsed)
   return true;
 }
 
+void AbstractTreeViewItem::uncollapse_by_default()
+{
+  BLI_assert_msg(this->get_tree_view().is_reconstructed() == false,
+                 "Default state should only be set while building the tree");
+  BLI_assert(this->supports_collapsing());
+  /* Set the open state. Note that this may be overridden later by #should_be_collapsed(). */
+  is_open_ = true;
+}
+
 bool AbstractTreeViewItem::is_collapsible() const
 {
-  // BLI_assert_msg(get_tree_view().is_reconstructed(),
-  //  "State can't be queried until reconstruction is completed");
+  BLI_assert_msg(get_tree_view().is_reconstructed(),
+                 "State can't be queried until reconstruction is completed");
   if (children_.is_empty()) {
     return false;
   }

source/blender/editors/space_file/asset_catalog_tree_view.cc
@@ -191,7 +191,7 @@ AssetCatalogTreeView::AssetCatalogTreeView(asset_system::AssetLibrary *library,
 void AssetCatalogTreeView::build_tree()
 {
   AssetCatalogTreeViewAllItem &all_item = add_all_item();
-  all_item.set_collapsed(false);
+  all_item.uncollapse_by_default();
 
   if (catalog_tree_) {
     /* Pass the "All" item on as parent of the actual catalog items. */

source/blender/editors/space_spreadsheet/spreadsheet_dataset_draw.cc
@@ -92,6 +92,7 @@ class GeometryDataSetTreeView : public ui::AbstractTreeView {
         bke::GeometryComponent::Type::GreasePencil,
         IFACE_("Grease Pencil"),
         ICON_OUTLINER_DATA_GREASEPENCIL);
+    grease_pencil.uncollapse_by_default();
     GeometryDataSetTreeViewItem &grease_pencil_layers =
         grease_pencil.add_tree_item<GeometryDataSetTreeViewItem>(
             bke::GeometryComponent::Type::GreasePencil,
@@ -127,6 +128,7 @@ class GeometryDataSetTreeView : public ui::AbstractTreeView {
   {
     GeometryDataSetTreeViewItem &mesh = this->add_tree_item<GeometryDataSetTreeViewItem>(
         bke::GeometryComponent::Type::Mesh, IFACE_("Mesh"), ICON_MESH_DATA);
+    mesh.uncollapse_by_default();
     mesh.add_tree_item<GeometryDataSetTreeViewItem>(bke::GeometryComponent::Type::Mesh,
                                                     bke::AttrDomain::Point,
                                                     IFACE_("Vertex"),
@@ -142,6 +144,7 @@ class GeometryDataSetTreeView : public ui::AbstractTreeView {
 
     GeometryDataSetTreeViewItem &curve = this->add_tree_item<GeometryDataSetTreeViewItem>(
         bke::GeometryComponent::Type::Curve, IFACE_("Curve"), ICON_CURVE_DATA);
+    curve.uncollapse_by_default();
     curve.add_tree_item<GeometryDataSetTreeViewItem>(bke::GeometryComponent::Type::Curve,
                                                      bke::AttrDomain::Point,
                                                      IFACE_("Control Point"),
@@ -155,6 +158,7 @@ class GeometryDataSetTreeView : public ui::AbstractTreeView {
 
     GeometryDataSetTreeViewItem &pointcloud = this->add_tree_item<GeometryDataSetTreeViewItem>(
         bke::GeometryComponent::Type::PointCloud, IFACE_("Point Cloud"), ICON_POINTCLOUD_DATA);
+    pointcloud.uncollapse_by_default();
     pointcloud.add_tree_item<GeometryDataSetTreeViewItem>(bke::GeometryComponent::Type::PointCloud,
                                                           bke::AttrDomain::Point,
                                                           IFACE_("Point"),
@@ -175,7 +179,6 @@ GeometryDataSetTreeViewItem::GeometryDataSetTreeViewItem(
     : component_type_(component_type), domain_(std::nullopt), icon_(icon)
 {
   label_ = label;
-  this->set_collapsed(false);
 }
 GeometryDataSetTreeViewItem::GeometryDataSetTreeViewItem(
     bke::GeometryComponent::Type component_type, int layer_index, StringRef label, BIFIconID icon)

commit 842d28f5ba65f980633b3a5d182e3a7d2e12d099

Fix: GPv3: Assert in `LayerTreeView::build_tree()`
This was caused by b4b830646dcd2e848e8ab65035e15c19dbe5258c.
The issue was that there was an attempt at calling
`uncollapse_by_default` on a `LayerViewItem`
which can't be collapsed (because it can't have any children).
This then triggered the assert.

The fix removes the call to `uncollapse_by_default` for
`LayerViewItem`.

source/blender/editors/interface/interface_template_grease_pencil_layer_tree.cc
@@ -178,6 +178,8 @@ class LayerViewItem : public AbstractTreeViewItem {
 
   bool supports_collapsing() const override
   {
+    /* This is a bit redundant since `LayerViewItem` can't have children. But being expplicit might
+     * catch errors. */
     return false;
   }
 
@@ -392,9 +394,7 @@ void LayerTreeView::build_tree_node_recursive(TreeViewOrItem &parent, TreeNode &
 {
   using namespace blender::bke::greasepencil;
   if (node.is_layer()) {
-    LayerViewItem &item = parent.add_tree_item<LayerViewItem>(this->grease_pencil_,
-                                                              node.as_layer());
-    item.uncollapse_by_default();
+    parent.add_tree_item<LayerViewItem>(this->grease_pencil_, node.as_layer());
   }
   else if (node.is_group()) {
     LayerGroupViewItem &group_item = parent.add_tree_item<LayerGroupViewItem>(this->grease_pencil_,