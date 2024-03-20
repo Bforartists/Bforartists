Bforartists/Bforartists

commit 3c3c3fd91838f97c70f8b10f1d437b573968f951

Fix performance issue when tagging relations for update
The relations update code does tagging needed to ensure that the
array of bases is updated when relations are updated. It was done
by tagging the Scene ID node, and potentially recursing into all
dependent depsgraph nodes. If there is a driver on a scene property
it was unnecessarily re-evaluated.

This solves the slow behavior of adding objects in the test file
from #117335.

Pull Request: https://projects.blender.org/blender/blender/pulls/117403

source/blender/depsgraph/intern/depsgraph_build.cc
@@ -281,15 +281,19 @@ void DEG_graph_tag_relations_update(Depsgraph *graph)
   DEG_DEBUG_PRINTF(graph, TAG, "%s: Tagging relations for update.\n", __func__);
   deg::Depsgraph *deg_graph = reinterpret_cast<deg::Depsgraph *>(graph);
   deg_graph->need_update_relations = true;
-  /* NOTE: When relations are updated, it's quite possible that
-   * we've got new bases in the scene. This means, we need to
-   * re-create flat array of bases in view layer.
-   *
-   * TODO(sergey): Try to make it so we don't flush updates
-   * to the whole depsgraph. */
+
+  /* NOTE: When relations are updated, it's quite possible that we've got new bases in the scene.
+   * This means, we need to re-create flat array of bases in view layer. */
+  /* TODO(sergey): It is expected that bases manipulation tags scene for update to tag bases array
+   * for re-creation. Once it is ensured to happen from all places this implicit tag can be
+   * removed. */
   deg::IDNode *id_node = deg_graph->find_id_node(&deg_graph->scene->id);
   if (id_node != nullptr) {
-    id_node->tag_update(deg_graph, deg::DEG_UPDATE_SOURCE_RELATIONS);
+    graph_id_tag_update(deg_graph->bmain,
+                        deg_graph,
+                        &deg_graph->scene->id,
+                        ID_RECALC_BASE_FLAGS,
+                        deg::DEG_UPDATE_SOURCE_RELATIONS);
   }
 }
 

commit 986d502e0e4169be6d495fc09f2a93b1f79edcf6

Fix #119697: Incorrect update after disabling light linking
The issue was caused by 3c3c3fd9183.

With the change from the offended commit there was nothing that
would have informed objects previously linked to a light that they
need to re-calculate their light linking collections.

This change makes it so scene's hierarchy is tagged for update,
which propagates to all dependencies which do depend on hierarchy
of objects and collections in the scene.

This brings behavior closer to what it used to be, without causing
slowdown with full re-evaluation (which was fixed by the offending
commit).

Pull Request: https://projects.blender.org/blender/blender/pulls/119698

source/blender/depsgraph/intern/depsgraph_build.cc
@@ -292,7 +292,7 @@ void DEG_graph_tag_relations_update(Depsgraph *graph)
     graph_id_tag_update(deg_graph->bmain,
                         deg_graph,
                         &deg_graph->scene->id,
-                        ID_RECALC_BASE_FLAGS,
+                        ID_RECALC_BASE_FLAGS | ID_RECALC_HIERARCHY,
                         deg::DEG_UPDATE_SOURCE_RELATIONS);
   }
 }