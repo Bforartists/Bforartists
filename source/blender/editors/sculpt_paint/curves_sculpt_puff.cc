Bforartists/Bforartists

commit 7cc25855ac93abe725a39324d159886579f8444e

Fix #116188: hair puff works incorrect with symmetry
Originally caused by 3bd461f659 .

Solution is the same as 191a6fe94584
Only this time it does the same thing `Sphere` falloff (whereas
191a6fe94584 fixed it for `Projected` falloff)

Pull Request: https://projects.blender.org/blender/blender/pulls/117987

source/blender/editors/sculpt_paint/curves_sculpt_puff.cc
@@ -260,7 +260,7 @@ struct PuffOperationExecutor {
             brush_, dist_to_brush_cu, brush_radius_cu);
         math::max_inplace(max_weight, radius_falloff);
       }
-      r_curve_weights[curve_i] = max_weight;
+      math::max_inplace(r_curve_weights[curve_i], max_weight);
     });
   }
 

commit 55702866ce1bf47bd37c30a1b64d6d04413d9139

Fix #119715: Curve sculpt puff brush causes hairs to move erratically
Caused by 7cc25855ac93 / 191a6fe94584

Since the `curve_weights` array was created uninitialized, and the above
commits were doing `math::max_inplace` on the uninitialized data, this
could end up with garbage values.

Now initialize the array properly to prevent this.

Pull Request: https://projects.blender.org/blender/blender/pulls/119730

source/blender/editors/sculpt_paint/curves_sculpt_puff.cc
@@ -135,7 +135,7 @@ struct PuffOperationExecutor {
           *curves_, curve_selection_, curves_id_->flag & CV_SCULPT_COLLISION_ENABLED);
     }
 
-    Array<float> curve_weights(curves_->curves_num());
+    Array<float> curve_weights(curves_->curves_num(), 0.0f);
 
     if (falloff_shape == PAINT_FALLOFF_SHAPE_TUBE) {
       this->find_curve_weights_projected_with_symmetry(curve_weights);