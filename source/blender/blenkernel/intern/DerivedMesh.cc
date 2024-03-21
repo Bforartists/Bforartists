Bforartists/Bforartists

commit 66684c1bcd4e1317e8362e7efdffcb072b65b60a

Fix #119544: Cage modifier deformation ignored in next modifier
When the edit mesh modifier stack first deforms the original edit mesh,
it creates deformed positions in a separate array. That array is copied
to the mesh's vertex positions if a subsequent modifier requires an
actual mesh rather than a mesh wrapper.

However, if that last deform modifier is the last "on cage" modifier
and the next modifier requires a real Mesh, we hit a code path that
didn't copy the temporary position array, since it was contained in the
separate `EditMeshData` struct that must be handled manually.

This was a regression in d82192f9722a628b2bfb. In the future I hope to
make this simpler by expanding the use of implicit sharing and making
the conversion from original BMesh to a Mesh more lazy / const correct.

Pull Request: https://projects.blender.org/blender/blender/pulls/119718

source/blender/blenkernel/intern/DerivedMesh.cc
@@ -1148,6 +1148,9 @@ static void editbmesh_calc_modifiers(Depsgraph *depsgraph,
         mesh_final->edit_mesh->is_shallow_copy = true;
         mesh_final->runtime->is_original_bmesh = true;
         BKE_mesh_runtime_ensure_edit_data(mesh_final);
+        if (!mesh_cage->runtime->edit_data->vertexCos.is_empty()) {
+          mesh_final->runtime->edit_data->vertexCos = mesh_cage->runtime->edit_data->vertexCos;
+        }
       }
     }
 

commit 16c8afce18baa88514d348fc982c1d205a139407

Fix #119754: Edit mode modifier deform data copy crash
Caused by 66684c1bcd4e1317e8362e7efdffcb072b65b60a.

That commit assumed that the cage mesh always had the deformed
position and normal data. But that isn't true, as shown in the example
in the report. To fix the bug, simplify things, and make the goal clearer,
just copy the struct directly.

Pull Request: https://projects.blender.org/blender/blender/pulls/119755

source/blender/blenkernel/intern/DerivedMesh.cc
@@ -1147,9 +1147,9 @@ static void editbmesh_calc_modifiers(Depsgraph *depsgraph,
         mesh_final->edit_mesh = static_cast<BMEditMesh *>(MEM_dupallocN(mesh_cage->edit_mesh));
         mesh_final->edit_mesh->is_shallow_copy = true;
         mesh_final->runtime->is_original_bmesh = true;
-        BKE_mesh_runtime_ensure_edit_data(mesh_final);
-        if (!mesh_cage->runtime->edit_data->vertexCos.is_empty()) {
-          mesh_final->runtime->edit_data->vertexCos = mesh_cage->runtime->edit_data->vertexCos;
+        if (mesh_cage->runtime->edit_data) {
+          mesh_final->runtime->edit_data = std::make_unique<blender::bke::EditMeshData>(
+              *mesh_cage->runtime->edit_data);
         }
       }
     }