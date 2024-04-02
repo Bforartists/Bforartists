Bforartists/Bforartists

commit 1b1469ad29af209983a749bb8fd5a6418deefd77

GPv3: Show stats in edit mode
GPv3 uses `OB_MODE_EDIT` so it falsely shows empty mesh data in stats.
This worked in legacy gp because it had special edit_mode type.
To show correct grease pencil data in edit mode, rearrange if conditions and
add special check for `OB_GREASE_PENCIL`

Pull Request: https://projects.blender.org/blender/blender/pulls/119689

source/blender/editors/space_info/info_stats.cc
@@ -398,7 +398,7 @@ static void stats_update(Depsgraph *depsgraph,
 
   memset(stats, 0x0, sizeof(*stats));
 
-  if (obedit) {
+  if (obedit && (ob->type != OB_GREASE_PENCIL)) {
     /* Edit Mode. */
     FOREACH_OBJECT_BEGIN (scene, view_layer, ob_iter) {
       if (ob_iter->base_flag & BASE_ENABLED_AND_VISIBLE_IN_DEFAULT_VIEWPORT) {
@@ -823,7 +823,26 @@ void ED_info_draw_stats(
     stats_row(col1, labels[OBJ], col2, stats_fmt.totobj, nullptr, y, height);
   }
 
-  if (obedit) {
+  if (!any_selected) {
+    if (any_objects) {
+      /* Show scene totals if nothing is selected. */
+      stats_row(col1, labels[VERTS], col2, stats_fmt.totvert, nullptr, y, height);
+      stats_row(col1, labels[EDGES], col2, stats_fmt.totedge, nullptr, y, height);
+      stats_row(col1, labels[FACES], col2, stats_fmt.totface, nullptr, y, height);
+      stats_row(col1, labels[TRIS], col2, stats_fmt.tottri, nullptr, y, height);
+    }
+    else {
+      /* No objects in scene. */
+      stats_row(col1, labels[OBJ], col2, stats_fmt.totobj, nullptr, y, height);
+    }
+  }
+  else if ((ob) && ELEM(ob->type, OB_GPENCIL_LEGACY, OB_GREASE_PENCIL)) {
+    stats_row(col1, labels[LAYERS], col2, stats_fmt.totgplayer, nullptr, y, height);
+    stats_row(col1, labels[FRAMES], col2, stats_fmt.totgpframe, nullptr, y, height);
+    stats_row(col1, labels[STROKES], col2, stats_fmt.totgpstroke, nullptr, y, height);
+    stats_row(col1, labels[POINTS], col2, stats_fmt.totgppoint, nullptr, y, height);
+  }
+  else if (obedit) {
     if (obedit->type == OB_MESH) {
       stats_row(col1, labels[VERTS], col2, stats_fmt.totvertsel, stats_fmt.totvert, y, height);
       stats_row(col1, labels[EDGES], col2, stats_fmt.totedgesel, stats_fmt.totedge, y, height);
@@ -848,27 +867,8 @@ void ED_info_draw_stats(
       stats_row(col1, labels[FACES], col2, stats_fmt.totfacesculpt, nullptr, y, height);
     }
   }
-  else if (!any_selected) {
-    if (any_objects) {
-      /* Show scene totals if nothing is selected. */
-      stats_row(col1, labels[VERTS], col2, stats_fmt.totvert, nullptr, y, height);
-      stats_row(col1, labels[EDGES], col2, stats_fmt.totedge, nullptr, y, height);
-      stats_row(col1, labels[FACES], col2, stats_fmt.totface, nullptr, y, height);
-      stats_row(col1, labels[TRIS], col2, stats_fmt.tottri, nullptr, y, height);
-    }
-    else {
-      /* No objects in scene. */
-      stats_row(col1, labels[OBJ], col2, stats_fmt.totobj, nullptr, y, height);
-    }
-  }
   else if (ob && (object_mode & OB_MODE_POSE)) {
     stats_row(col1, labels[BONES], col2, stats_fmt.totbonesel, stats_fmt.totbone, y, height);
-  }
-  else if ((ob) && ELEM(ob->type, OB_GPENCIL_LEGACY, OB_GREASE_PENCIL)) {
-    stats_row(col1, labels[LAYERS], col2, stats_fmt.totgplayer, nullptr, y, height);
-    stats_row(col1, labels[FRAMES], col2, stats_fmt.totgpframe, nullptr, y, height);
-    stats_row(col1, labels[STROKES], col2, stats_fmt.totgpstroke, nullptr, y, height);
-    stats_row(col1, labels[POINTS], col2, stats_fmt.totgppoint, nullptr, y, height);
   }
   else if ((ob) && (ob->type == OB_LAMP)) {
     stats_row(col1, labels[LIGHTS], col2, stats_fmt.totlampsel, stats_fmt.totlamp, y, height);

commit 215ae4c663ef7238ff305d35da1fe715a1c12929

Fix #120095: Missing stats in sculpt mode
Caused by 1b1469ad29af209983a749bb8fd5a6418deefd77
totobj and selected objects are not counted for sculpt mode stats (can
do that but it's unnecessary). Hence the above commit falsely showed 0
objects in statistics. To fix this, skip showing object count in sculpt
mode, otherwise print 0 objects if selobj and totobj are none.

Pull Request: https://projects.blender.org/blender/blender/pulls/120106

source/blender/editors/space_info/info_stats.cc
@@ -821,22 +821,20 @@ void ED_info_draw_stats(
   }
   else if (any_objects) {
     stats_row(col1, labels[OBJ], col2, stats_fmt.totobj, nullptr, y, height);
+    /* Show scene totals if nothing is selected. */
+    stats_row(col1, labels[VERTS], col2, stats_fmt.totvert, nullptr, y, height);
+    stats_row(col1, labels[EDGES], col2, stats_fmt.totedge, nullptr, y, height);
+    stats_row(col1, labels[FACES], col2, stats_fmt.totface, nullptr, y, height);
+    stats_row(col1, labels[TRIS], col2, stats_fmt.tottri, nullptr, y, height);
+    return;
+  }
+  else if (!(object_mode & OB_MODE_SCULPT)) {
+    /* No objects in scene. */
+    stats_row(col1, labels[OBJ], col2, 0, nullptr, y, height);
+    return;
   }
 
-  if (!any_selected) {
-    if (any_objects) {
-      /* Show scene totals if nothing is selected. */
-      stats_row(col1, labels[VERTS], col2, stats_fmt.totvert, nullptr, y, height);
-      stats_row(col1, labels[EDGES], col2, stats_fmt.totedge, nullptr, y, height);
-      stats_row(col1, labels[FACES], col2, stats_fmt.totface, nullptr, y, height);
-      stats_row(col1, labels[TRIS], col2, stats_fmt.tottri, nullptr, y, height);
-    }
-    else {
-      /* No objects in scene. */
-      stats_row(col1, labels[OBJ], col2, stats_fmt.totobj, nullptr, y, height);
-    }
-  }
-  else if ((ob) && ELEM(ob->type, OB_GPENCIL_LEGACY, OB_GREASE_PENCIL)) {
+  if ((ob) && ELEM(ob->type, OB_GPENCIL_LEGACY, OB_GREASE_PENCIL)) {
     stats_row(col1, labels[LAYERS], col2, stats_fmt.totgplayer, nullptr, y, height);
     stats_row(col1, labels[FRAMES], col2, stats_fmt.totgpframe, nullptr, y, height);
     stats_row(col1, labels[STROKES], col2, stats_fmt.totgpstroke, nullptr, y, height);