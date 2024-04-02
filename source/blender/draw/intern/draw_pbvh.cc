Bforartists/Bforartists

commit 0211c7c8f8f9811ee6f0ddb7c2b08f14359a1f66

Cleanup: Use C++ attribute API instead of CustomData API
This gives better asserts in debug builds through use of Span, more
safety when name convention attributes happen to have different types
or domains, and simpler code in some cases. But the main reasoning is to
avoid relying on the specifics of CustomData more to allow us to replace
it in the future.

source/blender/draw/intern/draw_pbvh.cc
@@ -129,11 +129,11 @@ void extract_data_vert_faces(const PBVH_GPU_Args &args, const Span<T> attribute,
   const Span<int> corner_verts = args.corner_verts;
   const Span<MLoopTri> looptris = args.mlooptri;
   const Span<int> looptri_faces = args.looptri_faces;
-  const bool *hide_poly = args.hide_poly;
+  const Span<bool> hide_poly = args.hide_poly;
 
   VBOType *data = static_cast<VBOType *>(GPU_vertbuf_get_data(&vbo));
   for (const int looptri_i : args.prim_indices) {
-    if (hide_poly && hide_poly[looptri_faces[looptri_i]]) {
+    if (!hide_poly.is_empty() && hide_poly[looptri_faces[looptri_i]]) {
       continue;
     }
     for (int i : IndexRange(3)) {
@@ -151,12 +151,12 @@ void extract_data_face_faces(const PBVH_GPU_Args &args, const Span<T> attribute,
   using VBOType = typename Converter::VBOType;
 
   const Span<int> looptri_faces = args.looptri_faces;
-  const bool *hide_poly = args.hide_poly;
+  const Span<bool> hide_poly = args.hide_poly;
 
   VBOType *data = static_cast<VBOType *>(GPU_vertbuf_get_data(&vbo));
   for (const int looptri_i : args.prim_indices) {
     const int face = looptri_faces[looptri_i];
-    if (hide_poly && hide_poly[face]) {
+    if (!hide_poly.is_empty() && hide_poly[face]) {
       continue;
     }
     std::fill_n(data, 3, Converter::convert(attribute[face]));
@@ -172,11 +172,11 @@ void extract_data_corner_faces(const PBVH_GPU_Args &args, const Span<T> attribut
 
   const Span<MLoopTri> looptris = args.mlooptri;
   const Span<int> looptri_faces = args.looptri_faces;
-  const bool *hide_poly = args.hide_poly;
+  const Span<bool> hide_poly = args.hide_poly;
 
   VBOType *data = static_cast<VBOType *>(GPU_vertbuf_get_data(&vbo));
   for (const int looptri_i : args.prim_indices) {
-    if (hide_poly && hide_poly[looptri_faces[looptri_i]]) {
+    if (!hide_poly.is_empty() && hide_poly[looptri_faces[looptri_i]]) {
       continue;
     }
     for (int i : IndexRange(3)) {
@@ -338,7 +338,7 @@ struct PBVHBatches {
 
     switch (args.pbvh_type) {
       case PBVH_FACES: {
-        if (args.hide_poly) {
+        if (!args.hide_poly.is_empty()) {
           for (const int looptri_i : args.prim_indices) {
             if (!args.hide_poly[args.looptri_faces[looptri_i]]) {
               count++;
@@ -438,18 +438,19 @@ struct PBVHBatches {
 
   void fill_vbo_normal_faces(const PBVH_GPU_Args &args, GPUVertBuf &vert_buf)
   {
-    const bool *sharp_faces = static_cast<const bool *>(
-        CustomData_get_layer_named(args.face_data, CD_PROP_BOOL, "sharp_face"));
+    const bke::AttributeAccessor attributes = args.mesh->attributes();
+    const VArraySpan sharp_faces = *attributes.lookup<bool>("sharp_face", ATTR_DOMAIN_FACE);
+
     short4 *data = static_cast<short4 *>(GPU_vertbuf_get_data(&vert_buf));
 
     short4 face_no;
     int last_face = -1;
     for (const int looptri_i : args.prim_indices) {
       const int face_i = args.looptri_faces[looptri_i];
-      if (args.hide_poly && args.hide_poly[face_i]) {
+      if (!args.hide_poly.is_empty() && args.hide_poly[face_i]) {
         continue;
       }
-      if (sharp_faces && sharp_faces[face_i]) {
+      if (!sharp_faces.is_empty() && sharp_faces[face_i]) {
         if (face_i != last_face) {
           face_no = normal_float_to_short(args.face_normals[face_i]);
           last_face = face_i;
@@ -499,12 +500,14 @@ struct PBVHBatches {
         }
         case CustomRequest::Normal: {
           const Span<int> grid_to_face_map = args.subdiv_ccg->grid_to_face_map;
-          const bool *sharp_faces = static_cast<const bool *>(
-              CustomData_get_layer_named(args.face_data, CD_PROP_BOOL, "sharp_face"));
+          const bke::AttributeAccessor attributes = args.mesh->attributes();
+          const VArraySpan sharp_faces = *attributes.lookup<bool>("sharp_face", ATTR_DOMAIN_FACE);
+
           foreach_grids([&](int /*x*/, int /*y*/, int grid_index, CCGElem *elems[4], int /*i*/) {
             float3 no(0.0f, 0.0f, 0.0f);
 
-            const bool smooth = !(sharp_faces && sharp_faces[grid_to_face_map[grid_index]]);
+            const bool smooth = !(!sharp_faces.is_empty() &&
+                                  sharp_faces[grid_to_face_map[grid_index]]);
 
             if (smooth) {
               no = CCG_elem_no(&args.ccg_key, elems[0]);
@@ -684,10 +687,10 @@ struct PBVHBatches {
             const Span<int> corner_verts = args.corner_verts;
             const Span<MLoopTri> looptris = args.mlooptri;
             const Span<int> looptri_faces = args.looptri_faces;
-            const bool *hide_poly = args.hide_poly;
+            const Span<bool> hide_poly = args.hide_poly;
 
             for (const int looptri_i : args.prim_indices) {
-              if (hide_poly && hide_poly[looptri_faces[looptri_i]]) {
+              if (!hide_poly.is_empty() && hide_poly[looptri_faces[looptri_i]]) {
                 continue;
               }
               for (int i : IndexRange(3)) {
@@ -711,7 +714,7 @@ struct PBVHBatches {
             uchar4 fset_color(UCHAR_MAX);
 
             for (const int looptri_i : args.prim_indices) {
-              if (args.hide_poly && args.hide_poly[args.looptri_faces[looptri_i]]) {
+              if (!args.hide_poly.is_empty() && args.hide_poly[args.looptri_faces[looptri_i]]) {
                 continue;
               }
               const int face_i = args.looptri_faces[looptri_i];
@@ -1026,14 +1029,10 @@ struct PBVHBatches {
 
   void create_index_faces(const PBVH_GPU_Args &args)
   {
-    const int *mat_index = static_cast<const int *>(
-        CustomData_get_layer_named(args.face_data, CD_PROP_INT32, "material_index"));
-
-    if (mat_index && !args.prim_indices.is_empty()) {
-      const int looptri_i = args.prim_indices[0];
-      const int face_i = args.looptri_faces[looptri_i];
-      material_index = mat_index[face_i];
-    }
+    const bke::AttributeAccessor attributes = args.mesh->attributes();
+    const VArray material_indices = *attributes.lookup_or_default<int>(
+        "material_index", ATTR_DOMAIN_FACE, 0);
+    material_index = material_indices[args.looptri_faces[args.prim_indices.first()]];
 
     const Span<int2> edges = args.mesh->edges();
 
@@ -1041,7 +1040,7 @@ struct PBVHBatches {
     int edge_count = 0;
     for (const int looptri_i : args.prim_indices) {
       const int face_i = args.looptri_faces[looptri_i];
-      if (args.hide_poly && args.hide_poly[face_i]) {
+      if (!args.hide_poly.is_empty() && args.hide_poly[face_i]) {
         continue;
       }
 
@@ -1065,7 +1064,7 @@ struct PBVHBatches {
     int vertex_i = 0;
     for (const int looptri_i : args.prim_indices) {
       const int face_i = args.looptri_faces[looptri_i];
-      if (args.hide_poly && args.hide_poly[face_i]) {
+      if (!args.hide_poly.is_empty() && args.hide_poly[face_i]) {
         continue;
       }
 
@@ -1114,18 +1113,15 @@ struct PBVHBatches {
 
   void create_index_grids(const PBVH_GPU_Args &args, bool do_coarse)
   {
+    const bke::AttributeAccessor attributes = args.mesh->attributes();
+    const VArraySpan sharp_faces = *attributes.lookup<bool>("sharp_face", ATTR_DOMAIN_FACE);
+    const VArray material_indices = *attributes.lookup_or_default<int>(
+        "material_index", ATTR_DOMAIN_FACE, 0);
     const BitGroupVector<> &grid_hidden = args.subdiv_ccg->grid_hidden;
     const Span<int> grid_to_face_map = args.subdiv_ccg->grid_to_face_map;
 
-    const bool *sharp_faces = static_cast<const bool *>(
-        CustomData_get_layer_named(args.face_data, CD_PROP_BOOL, "sharp_face"));
-    const int *mat_index = static_cast<const int *>(
-        CustomData_get_layer_named(args.face_data, CD_PROP_INT32, "material_index"));
-
-    if (mat_index && !args.grid_indices.is_empty()) {
-      int face_i = BKE_subdiv_ccg_grid_to_face_index(*args.subdiv_ccg, args.grid_indices[0]);
-      material_index = mat_index[face_i];
-    }
+    material_index = material_indices[BKE_subdiv_ccg_grid_to_face_index(
+        *args.subdiv_ccg, args.grid_indices.first())];
 
     needs_tri_index = true;
     int gridsize = args.ccg_key.grid_size;
@@ -1141,7 +1137,7 @@ struct PBVHBatches {
     }
 
     for (const int grid_index : args.grid_indices) {
-      bool smooth = !(sharp_faces && sharp_faces[grid_to_face_map[grid_index]]);
+      bool smooth = !(!sharp_faces.is_empty() && sharp_faces[grid_to_face_map[grid_index]]);
       if (!grid_hidden.is_empty()) {
         const BoundedBitSpan gh = grid_hidden[grid_index];
         for (int y = 0; y < gridsize - 1; y += skip) {

source/blender/blenkernel/BKE_mesh.hh
@@ -155,6 +155,8 @@ short2 lnor_space_custom_normal_to_data(const CornerNormalSpace &lnor_space,
  *
  * \param sharp_edges: Optional array of sharp edge tags, used to split the evaluated normals on
  * each side of the edge.
+ * \param sharp_faces: Optional array of sharp face tags, used to split the evaluated normals on
+ * the face's edges.
  * \param r_lnors_spacearr: Optional return data filled with information about the custom
  * normals spaces for each grouped fan of face corners.
  */
@@ -166,12 +168,15 @@ void normals_calc_loop(Span<float3> vert_positions,
                        Span<int> loop_to_face_map,
                        Span<float3> vert_normals,
                        Span<float3> face_normals,
-                       const bool *sharp_edges,
-                       const bool *sharp_faces,
+                       Span<bool> sharp_edges,
+                       Span<bool> sharp_faces,
                        const short2 *clnors_data,
                        CornerNormalSpaceArray *r_lnors_spacearr,
                        MutableSpan<float3> r_loop_normals);
 
+/**
+ * \param sharp_faces: Optional array used to mark specific faces for sharp shading.
+ */
 void normals_loop_custom_set(Span<float3> vert_positions,
                              Span<int2> edges,
                              OffsetIndices<int> faces,
@@ -179,11 +184,14 @@ void normals_loop_custom_set(Span<float3> vert_positions,
                              Span<int> corner_edges,
                              Span<float3> vert_normals,
                              Span<float3> face_normals,
-                             const bool *sharp_faces,
+                             Span<bool> sharp_faces,
                              MutableSpan<bool> sharp_edges,
                              MutableSpan<float3> r_custom_loop_normals,
                              MutableSpan<short2> r_clnors_data);
 
+/**
+ * \param sharp_faces: Optional array used to mark specific faces for sharp shading.
+ */
 void normals_loop_custom_set_from_verts(Span<float3> vert_positions,
                                         Span<int2> edges,
                                         OffsetIndices<int> faces,
@@ -191,7 +199,7 @@ void normals_loop_custom_set_from_verts(Span<float3> vert_positions,
                                         Span<int> corner_edges,
                                         Span<float3> vert_normals,
                                         Span<float3> face_normals,
-                                        const bool *sharp_faces,
+                                        Span<bool> sharp_faces,
                                         MutableSpan<bool> sharp_edges,
                                         MutableSpan<float3> r_custom_vert_normals,
                                         MutableSpan<short2> r_clnors_data);
@@ -209,7 +217,7 @@ void edges_sharp_from_angle_set(OffsetIndices<int> faces,
                                 Span<int> corner_edges,
                                 Span<float3> face_normals,
                                 Span<int> loop_to_face,
-                                const bool *sharp_faces,
+                                Span<bool> sharp_faces,
                                 const float split_angle,
                                 MutableSpan<bool> sharp_edges);
 

source/blender/blenkernel/BKE_mesh_mapping.hh
@@ -248,6 +248,8 @@ bool BKE_mesh_calc_islands_loop_face_uvmap(float (*vert_positions)[3],
 /**
  * Calculate smooth groups from sharp edges.
  *
+ * \param sharp_edges: Optional (possibly empty) span.
+ * \param sharp_faces: Optional (possibly empty) span.
  * \param r_totgroup: The total number of groups, 1 or more.
  * \return Polygon aligned array of group index values (bitflags if use_bitflags is true),
  * starting at 1 (0 being used as 'invalid' flag).
@@ -256,8 +258,8 @@ bool BKE_mesh_calc_islands_loop_face_uvmap(float (*vert_positions)[3],
 int *BKE_mesh_calc_smoothgroups(int edges_num,
                                 blender::OffsetIndices<int> faces,
                                 blender::Span<int> corner_edges,
-                                const bool *sharp_edges,
-                                const bool *sharp_faces,
+                                blender::Span<bool> sharp_edges,
+                                blender::Span<bool> sharp_faces,
                                 int *r_totgroup,
                                 bool use_bitflags);
 

source/blender/blenkernel/BKE_mesh_tangent.hh
@@ -51,7 +51,7 @@ void BKE_mesh_calc_loop_tangent_ex(const float (*vert_positions)[3],
                                    const MLoopTri *looptri,
                                    const int *looptri_faces,
                                    uint looptri_len,
-                                   const bool *sharp_faces,
+                                   const blender::Span<bool> sharp_faces,
 
                                    CustomData *loopdata,
                                    bool calc_active_tangent,

source/blender/blenkernel/BKE_pbvh_api.hh
@@ -244,7 +244,7 @@ bool BKE_pbvh_node_raycast(PBVH *pbvh,
                            float (*origco)[3],
                            bool use_origco,
                            blender::Span<int> corner_verts,
-                           const bool *hide_poly,
+                           blender::Span<bool> hide_poly,
                            const float ray_start[3],
                            const float ray_normal[3],
                            IsectRayPrecalc *isect_precalc,
@@ -284,7 +284,7 @@ bool BKE_pbvh_node_find_nearest_to_ray(PBVH *pbvh,
                                        float (*origco)[3],
                                        bool use_origco,
                                        blender::Span<int> corner_verts,
-                                       const bool *hide_poly,
+                                       blender::Span<bool> hide_poly,
                                        const float ray_start[3],
                                        const float ray_normal[3],
                                        float *depth,

source/blender/blenkernel/intern/data_transfer.cc
@@ -378,8 +378,7 @@ static void data_transfer_dtdata_type_postprocess(Mesh *me_dst,
     bke::MutableAttributeAccessor attributes = me_dst->attributes_for_write();
     bke::SpanAttributeWriter<bool> sharp_edges = attributes.lookup_or_add_for_write_span<bool>(
         "sharp_edge", ATTR_DOMAIN_EDGE);
-    const bool *sharp_faces = static_cast<const bool *>(
-        CustomData_get_layer_named(&me_dst->face_data, CD_PROP_BOOL, "sharp_face"));
+    const VArraySpan sharp_faces = *attributes.lookup<bool>("sharp_face", ATTR_DOMAIN_FACE);
     /* Note loop_nors_dst contains our custom normals as transferred from source... */
     blender::bke::mesh::normals_loop_custom_set(me_dst->vert_positions(),
                                                 me_dst->edges(),

source/blender/blenkernel/intern/key.cc
@@ -24,6 +24,7 @@
 /* Allow using deprecated functionality for .blend file I/O. */
 #define DNA_DEPRECATED_ALLOW
 
+#include "BKE_attribute.hh"
 #include "DNA_ID.h"
 #include "DNA_anim_types.h"
 #include "DNA_key_types.h"
@@ -2224,6 +2225,7 @@ void BKE_keyblock_mesh_calc_normals(const KeyBlock *kb,
                                     float (*r_face_normals)[3],
                                     float (*r_loop_normals)[3])
 {
+  using namespace blender;
   if (r_vert_normals == nullptr && r_face_normals == nullptr && r_loop_normals == nullptr) {
     return;
   }
@@ -2274,10 +2276,9 @@ void BKE_keyblock_mesh_calc_normals(const KeyBlock *kb,
   if (loop_normals_needed) {
     const blender::short2 *clnors = static_cast<const blender::short2 *>(
         CustomData_get_layer(&mesh->loop_data, CD_CUSTOMLOOPNORMAL));
-    const bool *sharp_edges = static_cast<const bool *>(
-        CustomData_get_layer_named(&mesh->edge_data, CD_PROP_BOOL, "sharp_edge"));
-    const bool *sharp_faces = static_cast<const bool *>(
-        CustomData_get_layer_named(&mesh->face_data, CD_PROP_BOOL, "sharp_face"));
+    const bke::AttributeAccessor attributes = mesh->attributes();
+    const VArraySpan sharp_edges = *attributes.lookup<bool>("sharp_edge", ATTR_DOMAIN_EDGE);
+    const VArraySpan sharp_faces = *attributes.lookup<bool>("sharp_face", ATTR_DOMAIN_FACE);
     blender::bke::mesh::normals_calc_loop(
         positions,
         edges,

source/blender/blenkernel/intern/mesh.cc
@@ -1069,8 +1069,7 @@ void BKE_mesh_sharp_edges_set_from_angle(Mesh *mesh, const float angle)
   }
   bke::SpanAttributeWriter<bool> sharp_edges = attributes.lookup_or_add_for_write_span<bool>(
       "sharp_edge", ATTR_DOMAIN_EDGE);
-  const bool *sharp_faces = static_cast<const bool *>(
-      CustomData_get_layer_named(&mesh->face_data, CD_PROP_BOOL, "sharp_face"));
+  const VArraySpan<bool> sharp_faces = *attributes.lookup<bool>("sharp_face", ATTR_DOMAIN_FACE);
   bke::mesh::edges_sharp_from_angle_set(mesh->faces(),
                                         mesh->corner_verts(),
                                         mesh->corner_edges(),

commit 5da8afb0fa2e6314fc590b38a6806203251ede38

Fix #120179: Assert failure after sculpt trim operation
Seems to be caused by 0211c7c8f8. I didn't think our PBVH building would
ever create a node with no triangles, but I guess I was wrong, since I
checked and the PBVH is clearly being rebuilt after the trim operation,
so it seems there isn't anything else weird going on here.

Rather than trying to figure that out, I'll just add the empty check
back here. Eventually we should completely replace the algorithm that
creates the PBVH nodes anyway.

source/blender/draw/intern/draw_pbvh.cc
@@ -1053,10 +1053,12 @@ struct PBVHBatches {
 
   void create_index_faces(const PBVH_GPU_Args &args)
   {
-    const bke::AttributeAccessor attributes = args.mesh->attributes();
-    const VArray material_indices = *attributes.lookup_or_default<int>(
-        "material_index", bke::AttrDomain::Face, 0);
-    material_index = material_indices[args.tri_faces[args.prim_indices.first()]];
+    if (!args.prim_indices.is_empty()) {
+      const bke::AttributeAccessor attributes = args.mesh->attributes();
+      const VArray material_indices = *attributes.lookup_or_default<int>(
+          "material_index", bke::AttrDomain::Face, 0);
+      material_index = material_indices[args.tri_faces[args.prim_indices.first()]];
+    }
 
     const Span<int2> edges = args.mesh->edges();
 
@@ -1144,8 +1146,10 @@ struct PBVHBatches {
     const BitGroupVector<> &grid_hidden = args.subdiv_ccg->grid_hidden;
     const Span<int> grid_to_face_map = args.subdiv_ccg->grid_to_face_map;
 
-    material_index = material_indices[BKE_subdiv_ccg_grid_to_face_index(
-        *args.subdiv_ccg, args.grid_indices.first())];
+    if (!args.grid_indices.is_empty()) {
+      material_index = material_indices[BKE_subdiv_ccg_grid_to_face_index(
+          *args.subdiv_ccg, args.grid_indices.first())];
+    }
 
     needs_tri_index = true;
     int gridsize = args.ccg_key.grid_size;