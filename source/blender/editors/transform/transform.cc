Bforartists/Bforartists

commit 12f13508d35d4c9cb519d6bee7165766c88a3325

Transform: Implement Snap to Grid mode
Addresses two improvements to `Snap to Grid` as suggested in #73993:
- Make 'Absolute Grid Snapping' a new Snap Mode
- Snap to Grid in Non-Side View performed at ground level

Implementation details:

- Snap to Grid has no cursor symbol, unless we are in `Set Snap Base`
  mode. Similar to the previous 'Absolute Grid Snap' behavior.
- In Non-Side view, Snap to Grid is performed at ground level.
- If `Snap Base` is `Closest`, Snap to Grid uses the transform pivot
  point instead. Similar to the previous 'Absolute Grid Snap' behavior.
- The "Absolute Grid Snap" option has been removed.

Pull Request: https://projects.blender.org/blender/blender/pulls/116109

source/blender/editors/transform/transform.cc
@@ -1884,11 +1884,7 @@ static void initSnapSpatial(TransInfo *t, float r_snap[3], float *r_snap_precisi
   *r_snap_precision = 0.1f;
 
   if (t->spacetype == SPACE_VIEW3D) {
-    if (t->region->regiondata) {
-      View3D *v3d = static_cast<View3D *>(t->area->spacedata.first);
-      r_snap[0] = r_snap[1] = r_snap[2] = ED_view3d_grid_view_scale(
-          t->scene, v3d, t->region, nullptr);
-    }
+    /* Pass. Done in #ED_transform_snap_object_project_view3d_ex. */
   }
   else if (t->spacetype == SPACE_IMAGE) {
     SpaceImage *sima = static_cast<SpaceImage *>(t->area->spacedata.first);

scripts/startup/bl_ui/space_view3d.py
@@ -7548,9 +7548,6 @@ class VIEW3D_PT_snapping(Panel):
 
         col.separator()
 
-        if 'INCREMENT' in tool_settings.snap_elements:
-            col.prop(tool_settings, "use_snap_grid_absolute")
-
         if 'VOLUME' in tool_settings.snap_elements:
             col.prop(tool_settings, "use_snap_peel_object")
 

source/blender/editors/space_view3d/view3d_cursor_snap.cc
@@ -131,38 +131,6 @@ static void v3d_cursor_poject_surface_normal(const float normal[3],
 }
 
 /**
- * Calculate 3D view incremental (grid) snapping.
- *
- * \note This could be moved to a public function.
- */
-static bool v3d_cursor_snap_calc_incremental(
-    Scene *scene, View3D *v3d, ARegion *region, const float co_relative[3], float co[3])
-{
-  const float grid_size = ED_view3d_grid_view_scale(scene, v3d, region, nullptr);
-  if (UNLIKELY(grid_size == 0.0f)) {
-    return false;
-  }
-
-  if (scene->toolsettings->snap_flag & SCE_SNAP_ABS_GRID) {
-    co_relative = nullptr;
-  }
-
-  if (co_relative != nullptr) {
-    sub_v3_v3(co, co_relative);
-  }
-  mul_v3_fl(co, 1.0f / grid_size);
-  co[0] = roundf(co[0]);
-  co[1] = roundf(co[1]);
-  co[2] = roundf(co[2]);
-  mul_v3_fl(co, grid_size);
-  if (co_relative != nullptr) {
-    add_v3_v3(co, co_relative);
-  }
-
-  return true;
-}
-
-/**
  * Re-order \a mat so \a axis_align uses its own axis which is closest to \a v.
  */
 static bool mat3_align_axis_to_v3(float mat[3][3], const int axis_align, const float v[3])
@@ -377,6 +345,11 @@ static void cursor_box_draw(const float dimensions[3], uchar color[4])
 static void cursor_point_draw(
     uint attr_pos, const float loc[3], const float size, eSnapMode snap_type, const uchar color[4])
 {
+  if (snap_type == SCE_SNAP_TO_GRID) {
+    /* No drawing. */
+    return;
+  }
+
   immUniformColor4ubv(color);
 
   GPU_matrix_push();
@@ -686,7 +659,7 @@ static void v3d_cursor_snap_update(V3DSnapCursorState *state,
       snap_elements |= SCE_SNAP_TO_FACE;
     }
 
-    if (snap_elements & SCE_SNAP_TO_GEOM) {
+    if (snap_elements & (SCE_SNAP_TO_GEOM | SCE_SNAP_TO_GRID)) {
       float prev_co[3] = {0.0f};
       if (state->prevpoint) {
         copy_v3_v3(prev_co, state->prevpoint);
@@ -809,10 +782,6 @@ static void v3d_cursor_snap_update(V3DSnapCursorState *state,
 
     if (!do_plane_isect) {
       ED_view3d_win_to_3d(v3d, region, co_depth, mval_fl, co);
-    }
-
-    if (snap_data->is_enabled && (snap_elements & SCE_SNAP_TO_INCREMENT)) {
-      v3d_cursor_snap_calc_incremental(scene, v3d, region, state->prevpoint, co);
     }
   }
   else if (snap_elem & SCE_SNAP_TO_VERTEX) {

source/blender/editors/space_view3d/view3d_draw.cc
@@ -920,7 +920,7 @@ float ED_view3d_grid_view_scale(const Scene *scene,
                                 const char **r_grid_unit)
 {
   float grid_scale;
-  RegionView3D *rv3d = static_cast<RegionView3D *>(region->regiondata);
+  const RegionView3D *rv3d = static_cast<RegionView3D *>(region->regiondata);
   if (!rv3d->is_persp && RV3D_VIEW_IS_AXIS(rv3d->view)) {
     /* Decrease the distance between grid snap points depending on zoom. */
     float dist = 12.0f / (region->sizex * rv3d->winmat[0][0]);

source/blender/editors/space_view3d/view3d_placement.cc
@@ -155,7 +155,9 @@ struct InteractivePlaceData {
   /** When activated without a tool. */
   bool wait_for_input;
 
-  eSnapMode snap_to;
+  /* WORKAROUND: We need to remove #SCE_SNAP_TO_GRID temporarily. */
+  short *snap_to_ptr;
+  eSnapMode snap_to_restore;
 };
 
 /** \} */
@@ -225,7 +227,7 @@ static bool idp_snap_calc_incremental(
     return false;
   }
 
-  if (scene->toolsettings->snap_flag & SCE_SNAP_ABS_GRID) {
+  if (scene->toolsettings->snap_mode & SCE_SNAP_TO_GRID) {
     co_relative = nullptr;
   }
 
@@ -764,10 +766,11 @@ static void view3d_interactive_add_begin(bContext *C, wmOperator *op, const wmEv
 
   ipd->step_index = STEP_BASE;
 
-  ipd->snap_to = eSnapMode(tool_settings->snap_mode_tools);
-  if (ipd->snap_to == SCE_SNAP_TO_NONE) {
-    ipd->snap_to = eSnapMode(tool_settings->snap_mode);
+  ipd->snap_to_ptr = &tool_settings->snap_mode_tools;
+  if (eSnapMode(*ipd->snap_to_ptr) == SCE_SNAP_TO_NONE) {
+    ipd->snap_to_ptr = &tool_settings->snap_mode;
   }
+  ipd->snap_to_restore = eSnapMode(*ipd->snap_to_ptr);
 
   plane_from_point_normal_v3(ipd->step[0].plane, ipd->co_src, ipd->matrix_orient[plane_axis]);
 
@@ -1038,6 +1041,10 @@ static int view3d_interactive_add_modal(bContext *C, wmOperator *op, const wmEve
     if (ELEM(event->type, ipd->launch_event, LEFTMOUSE)) {
       if (event->val == KM_RELEASE) {
         ED_view3d_cursor_snap_state_prevpoint_set(ipd->snap_state, ipd->co_src);
+        if (ipd->snap_to_restore & SCE_SNAP_TO_GRID) {
+          /* Don't snap to grid in #STEP_DEPTH. */
+          *ipd->snap_to_ptr = ipd->snap_to_restore & ~SCE_SNAP_TO_GRID;
+        }
 
         /* Set secondary plane. */
 
@@ -1075,7 +1082,10 @@ static int view3d_interactive_add_modal(bContext *C, wmOperator *op, const wmEve
   else if (ipd->step_index == STEP_DEPTH) {
     if (ELEM(event->type, ipd->launch_event, LEFTMOUSE)) {
       if (event->val == KM_PRESS) {
+        /* Restore snap mode. */
+        *ipd->snap_to_ptr = ipd->snap_to_restore;
 
+        /* Confirm. */
         BoundBox bounds;
         calc_bbox(ipd, &bounds);
 
@@ -1208,7 +1218,7 @@ static int view3d_interactive_add_modal(bContext *C, wmOperator *op, const wmEve
           /* pass */
         }
 
-        if (ipd->use_snap && (ipd->snap_to & SCE_SNAP_TO_INCREMENT)) {
+        if (ipd->use_snap && (ipd->snap_to_restore & (SCE_SNAP_TO_GRID | SCE_SNAP_TO_INCREMENT))) {
           if (idp_snap_calc_incremental(
                   ipd->scene, ipd->v3d, ipd->region, ipd->co_src, ipd->step[STEP_BASE].co_dst))
           {
@@ -1232,7 +1242,7 @@ static int view3d_interactive_add_modal(bContext *C, wmOperator *op, const wmEve
           /* pass */
         }
 
-        if (ipd->use_snap && (ipd->snap_to & SCE_SNAP_TO_INCREMENT)) {
+        if (ipd->use_snap && (ipd->snap_to_restore & (SCE_SNAP_TO_GRID | SCE_SNAP_TO_INCREMENT))) {
           if (idp_snap_calc_incremental(
                   ipd->scene, ipd->v3d, ipd->region, ipd->co_src, ipd->step[STEP_DEPTH].co_dst))
           {

source/blender/editors/transform/transform.hh
@@ -174,9 +174,8 @@ enum eTSnap {
   SNAP_RESETTED = 0,
   SNAP_SOURCE_FOUND = 1 << 0,
   /* Special flag for snap to grid. */
-  SNAP_TARGET_GRID_FOUND = 1 << 1,
-  SNAP_TARGET_FOUND = 1 << 2,
-  SNAP_MULTI_POINTS = 1 << 3,
+  SNAP_TARGET_FOUND = 1 << 1,
+  SNAP_MULTI_POINTS = 1 << 2,
 };
 ENUM_OPERATORS(eTSnap, SNAP_MULTI_POINTS)
 
@@ -316,7 +315,6 @@ struct TransSnap {
   float snap_source[3];
   /** To this point (in global-space). */
   float snap_target[3];
-  float snap_target_grid[3];
   float snapNormal[3];
   char snapNodeBorder;
   ListBase points;

source/blender/editors/transform/transform_constraints.cc
@@ -404,9 +404,6 @@ static void applyAxisConstraintVec(const TransInfo *t,
         is_snap_to_face = (t->tsnap.target_type & SCE_SNAP_TO_FACE) != 0;
         is_snap_to_point = !is_snap_to_edge && !is_snap_to_face;
       }
-      else if (t->tsnap.target_type & SCE_SNAP_TO_GRID) {
-        is_snap_to_point = true;
-      }
     }
 
     /* Fallback for when axes are aligned. */

source/blender/editors/transform/transform_mode_snapsource.cc
@@ -205,9 +205,9 @@ void transform_mode_snap_source_init(TransInfo *t, wmOperator * /*op*/)
   t->tsnap.mode &= ~(SCE_SNAP_TO_EDGE_PERPENDICULAR | SCE_SNAP_INDIVIDUAL_PROJECT |
                      SCE_SNAP_INDIVIDUAL_NEAREST);
 
-  if ((t->tsnap.mode & ~(SCE_SNAP_TO_INCREMENT | SCE_SNAP_TO_GRID)) == 0) {
+  if ((t->tsnap.mode & ~SCE_SNAP_TO_INCREMENT) == 0) {
     /* Initialize snap modes for geometry. */
-    t->tsnap.mode &= ~(SCE_SNAP_TO_INCREMENT | SCE_SNAP_TO_GRID);
+    t->tsnap.mode &= ~SCE_SNAP_TO_INCREMENT;
     t->tsnap.mode |= SCE_SNAP_TO_GEOM & ~SCE_SNAP_TO_EDGE_PERPENDICULAR;
 
     if (!(customdata->snap_mode_confirm & SCE_SNAP_TO_EDGE_PERPENDICULAR)) {

commit 057404bc9f43041d30c2069fc8a9fe31122e1fba

Fix: Incremental snap not matching grid resolution
Caused by 12f13508d3

`t->snap_spatial` is still required for Incremental snap.

source/blender/editors/transform/transform.cc
@@ -1884,7 +1884,12 @@ static void initSnapSpatial(TransInfo *t, float r_snap[3], float *r_snap_precisi
   *r_snap_precision = 0.1f;
 
   if (t->spacetype == SPACE_VIEW3D) {
-    /* Pass. Done in #ED_transform_snap_object_project_view3d_ex. */
+    /* Used by incremental snap. */
+    if (t->region->regiondata) {
+      View3D *v3d = static_cast<View3D *>(t->area->spacedata.first);
+      r_snap[0] = r_snap[1] = r_snap[2] = ED_view3d_grid_view_scale(
+          t->scene, v3d, t->region, nullptr);
+    }
   }
   else if (t->spacetype == SPACE_IMAGE) {
     SpaceImage *sima = static_cast<SpaceImage *>(t->area->spacedata.first);