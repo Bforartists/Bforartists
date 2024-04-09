Bforartists/Bforartists

commit 04f7ba554cdefab877f1299293996eb8cc624dcc

Sculpt: Separate invert visibility operator from face sets
Remove "Invert Visible" from the face sets menu, since those concepts
are no longer tied together, except for some convenience features to
toggle visibility of some face sets.

On the code side, the `SCULPT_OT_face_set_invert_visibility` operator
is replaced by `PAINT_OT_visibility_invert`. It inverts vertex visibility
instead of face visibility, which mostly has the same results, except
for multires sculpting where it no longer "downgrades" visibility to
base mesh faces.

source/blender/editors/sculpt_paint/paint_hide.cc
@@ -107,6 +107,7 @@ static void vert_show_all(Object &object, const Span<PBVHNode *> nodes)
     BKE_pbvh_node_fully_hidden_set(node, false);
   }
   attributes.remove(".hide_vert");
+  BKE_mesh_flush_hidden_from_verts(&mesh);
 }
 
 static bool vert_hide_is_changed(const Span<int> verts,
@@ -123,18 +124,19 @@ static bool vert_hide_is_changed(const Span<int> verts,
 
 static void vert_hide_update(Object &object,
                              const Span<PBVHNode *> nodes,
-                             FunctionRef<void(Span<int>, MutableSpan<bool>)> calc_hide)
+                             const FunctionRef<void(Span<int>, MutableSpan<bool>)> calc_hide)
 {
   Mesh &mesh = *static_cast<Mesh *>(object.data);
   bke::MutableAttributeAccessor attributes = mesh.attributes_for_write();
   bke::SpanAttributeWriter<bool> hide_vert = attributes.lookup_or_add_for_write_span<bool>(
       ".hide_vert", ATTR_DOMAIN_POINT);
 
+  bool any_changed = false;
   threading::EnumerableThreadSpecific<Vector<bool>> all_new_hide;
   threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
     Vector<bool> &new_hide = all_new_hide.local();
     for (PBVHNode *node : nodes.slice(range)) {
-      const Span<int> verts = BKE_pbvh_node_get_vert_indices(node);
+      const Span<int> verts = BKE_pbvh_node_get_unique_vert_indices(node);
 
       new_hide.reinitialize(verts.size());
       array_utils::gather(hide_vert.span.as_span(), verts, new_hide.as_mutable_span());
@@ -142,6 +144,8 @@ static void vert_hide_update(Object &object,
       if (!vert_hide_is_changed(verts, hide_vert.span, new_hide)) {
         continue;
       }
+
+      any_changed = true;
       SCULPT_undo_push_node(&object, node, SCULPT_UNDO_HIDDEN);
       array_utils::scatter(new_hide.as_span(), verts, hide_vert.span);
 
@@ -149,7 +153,11 @@ static void vert_hide_update(Object &object,
       bke::pbvh::node_update_visibility_mesh(hide_vert.span, *node);
     }
   });
+
   hide_vert.finish();
+  if (any_changed) {
+    BKE_mesh_flush_hidden_from_verts(&mesh);
+  }
 }
 
 static void partialvis_update_mesh(Object &object,
@@ -209,8 +217,6 @@ static void partialvis_update_mesh(Object &object,
       break;
     }
   }
-
-  BKE_mesh_flush_hidden_from_verts(&mesh);
 }
 
 static void grids_show_all(Depsgraph &depsgraph, Object &object, const Span<PBVHNode *> nodes)
@@ -606,6 +612,124 @@ void PAINT_OT_hide_show(wmOperatorType *ot)
   RNA_def_enum(
       ot->srna, "area", area_items, VisArea::Inside, "VisArea", "Which vertices to hide or show");
   WM_operator_properties_border(ot);
+}
+
+static void invert_visibility_mesh(Object &object, const Span<PBVHNode *> nodes)
+{
+  Mesh &mesh = *static_cast<Mesh *>(object.data);
+  bke::MutableAttributeAccessor attributes = mesh.attributes_for_write();
+  bke::SpanAttributeWriter<bool> hide_vert = attributes.lookup_or_add_for_write_span<bool>(
+      ".hide_vert", ATTR_DOMAIN_POINT);
+
+  threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+    for (PBVHNode *node : nodes.slice(range)) {
+      SCULPT_undo_push_node(&object, node, SCULPT_UNDO_HIDDEN);
+      for (const int vert : BKE_pbvh_node_get_unique_vert_indices(node)) {
+        hide_vert.span[vert] = !hide_vert.span[vert];
+      }
+      BKE_pbvh_node_mark_update_visibility(node);
+      bke::pbvh::node_update_visibility_mesh(hide_vert.span, *node);
+    }
+  });
+
+  hide_vert.finish();
+  BKE_mesh_flush_hidden_from_verts(&mesh);
+}
+
+static void invert_visibility_grids(Depsgraph &depsgraph,
+                                    Object &object,
+                                    const Span<PBVHNode *> nodes)
+{
+  Mesh &mesh = *static_cast<Mesh *>(object.data);
+  PBVH &pbvh = *object.sculpt->pbvh;
+  SubdivCCG &subdiv_ccg = *object.sculpt->subdiv_ccg;
+  BitGroupVector<> &grid_hidden = BKE_subdiv_ccg_grid_hidden_ensure(subdiv_ccg);
+
+  threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+    for (PBVHNode *node : nodes.slice(range)) {
+      SCULPT_undo_push_node(&object, node, SCULPT_UNDO_HIDDEN);
+      for (const int i : BKE_pbvh_node_get_grid_indices(*node)) {
+        bits::invert(grid_hidden[i]);
+      }
+      BKE_pbvh_node_mark_update_visibility(node);
+      bke::pbvh::node_update_visibility_grids(grid_hidden, *node);
+    }
+  });
+
+  multires_mark_as_modified(&depsgraph, &object, MULTIRES_HIDDEN_MODIFIED);
+  BKE_pbvh_sync_visibility_from_verts(&pbvh, &mesh);
+}
+
+static void invert_visibility_bmesh(Object &object, const Span<PBVHNode *> nodes)
+{
+  threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+    for (PBVHNode *node : nodes.slice(range)) {
+      SCULPT_undo_push_node(&object, node, SCULPT_UNDO_HIDDEN);
+      bool fully_hidden = true;
+      for (BMVert *vert : BKE_pbvh_bmesh_node_unique_verts(node)) {
+        BM_elem_flag_toggle(vert, BM_ELEM_HIDDEN);
+        fully_hidden &= BM_elem_flag_test(vert, BM_ELEM_HIDDEN);
+      }
+      BKE_pbvh_node_fully_hidden_set(node, fully_hidden);
+      BKE_pbvh_node_mark_rebuild_draw(node);
+    }
+  });
+  threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+    for (PBVHNode *node : nodes.slice(range)) {
+      partialvis_update_bmesh_faces(BKE_pbvh_bmesh_node_faces(node));
+    }
+  });
+}
+
+static int visibility_invert_exec(bContext *C, wmOperator *op)
+{
+  ARegion &region = *CTX_wm_region(C);
+  Object &object = *CTX_data_active_object(C);
+  Depsgraph &depsgraph = *CTX_data_ensure_evaluated_depsgraph(C);
+
+  PBVH *pbvh = BKE_sculpt_object_pbvh_ensure(&depsgraph, &object);
+  BLI_assert(BKE_object_sculpt_pbvh_get(&object) == pbvh);
+
+  Vector<PBVHNode *> nodes = bke::pbvh::search_gather(pbvh, {});
+  SCULPT_undo_push_begin(&object, op);
+  switch (BKE_pbvh_type(pbvh)) {
+    case PBVH_FACES:
+      invert_visibility_mesh(object, nodes);
+      break;
+    case PBVH_GRIDS:
+      invert_visibility_grids(depsgraph, object, nodes);
+      break;
+    case PBVH_BMESH:
+      invert_visibility_bmesh(object, nodes);
+      break;
+  }
+
+  SCULPT_undo_push_end(&object);
+
+  SCULPT_topology_islands_invalidate(object.sculpt);
+
+  RegionView3D *rv3d = CTX_wm_region_view3d(C);
+  if (!BKE_sculptsession_use_pbvh_draw(&object, rv3d)) {
+    DEG_id_tag_update(&object.id, ID_RECALC_GEOMETRY);
+  }
+
+  DEG_id_tag_update(&object.id, ID_RECALC_SHADING);
+  ED_region_tag_redraw(&region);
+
+  return OPERATOR_FINISHED;
+}
+
+void PAINT_OT_visibility_invert(wmOperatorType *ot)
+{
+  ot->name = "Invert Visibility";
+  ot->idname = "PAINT_OT_visibility_invert";
+  ot->description = "Invert the visibility of all vertices";
+
+  ot->modal = WM_gesture_box_modal;
+  ot->exec = visibility_invert_exec;
+  ot->poll = SCULPT_mode_poll_view3d;
+
+  ot->flag = OPTYPE_REGISTER;
 }
 
 }  // namespace blender::ed::sculpt_paint::hide

scripts/startup/bl_ui/space_view3d.py
@@ -3565,7 +3565,7 @@ class VIEW3D_MT_sculpt(Menu):
         props.action = 'SHOW'
         props.area = 'ALL'
 
-        layout.operator("sculpt.face_set_invert_visibility", text="Invert Visible")
+        layout.operator("paint.visibility_invert", text="Invert Visible")
 
         props = layout.operator("paint.hide_show", text="Hide Masked")
         props.action = 'HIDE'
@@ -3783,14 +3783,6 @@ class VIEW3D_MT_face_sets(Menu):
         layout.separator()
 
         props = layout.operator("mesh.face_set_extract", text="Extract Face Set")
-
-        layout.separator()
-
-        layout.operator("sculpt.face_set_invert_visibility", text="Invert Visible Face Sets")
-
-        props = layout.operator("paint.hide_show", text="Show All Face Sets")
-        props.action = "SHOW"
-        props.area = "ALL"
 
         layout.separator()
 
@@ -6142,7 +6134,7 @@ class VIEW3D_MT_sculpt_face_sets_edit_pie(Menu):
         props = pie.operator("sculpt.face_sets_create", text="Face Set from Visible")
         props.mode = 'VISIBLE'
 
-        pie.operator("sculpt.face_set_invert_visibility", text="Invert Visible")
+        pie.operator("paint.visibility_invert", text="Invert Visible")
 
         props = pie.operator("paint.hide_show", text="Show All")
         props.action = "SHOW"

source/blender/blenlib/BLI_bit_span_ops.hh
@@ -190,6 +190,11 @@ inline void foreach_1_index_expr(ExprFn &&expr,
       expr, handle, to_best_bit_span(first_arg), to_best_bit_span(args)...);
 }
 
+template<typename BitSpanT> inline void invert(const BitSpanT &data)
+{
+  mix_into_first_expr([](const BitInt x) { return ~x; }, data);
+}
+
 template<typename FirstBitSpanT, typename... BitSpanT>
 inline void inplace_or(FirstBitSpanT &first_arg, const BitSpanT &...args)
 {

source/blender/editors/sculpt_paint/paint_intern.hh
@@ -456,7 +456,8 @@ enum BrushStrokeMode {
 
 namespace blender::ed::sculpt_paint::hide {
 void PAINT_OT_hide_show(wmOperatorType *ot);
-}
+void PAINT_OT_visibility_invert(wmOperatorType *ot);
+}  // namespace blender::ed::sculpt_paint::hide
 
 /* `paint_mask.cc` */
 

source/blender/editors/sculpt_paint/paint_ops.cc
@@ -1549,6 +1549,7 @@ void ED_operatortypes_paint()
 
   /* partial visibility */
   WM_operatortype_append(blender::ed::sculpt_paint::hide::PAINT_OT_hide_show);
+  WM_operatortype_append(blender::ed::sculpt_paint::hide::PAINT_OT_visibility_invert);
 
   /* paint masking */
   WM_operatortype_append(blender::ed::sculpt_paint::mask::PAINT_OT_mask_flood_fill);

source/blender/editors/sculpt_paint/sculpt.cc
@@ -444,26 +444,6 @@ void SCULPT_face_set_visibility_set(SculptSession *ss, int face_set, bool visibl
   }
 }
 
-void SCULPT_face_visibility_all_invert(SculptSession *ss)
-{
-  SCULPT_topology_islands_invalidate(ss);
-  switch (BKE_pbvh_type(ss->pbvh)) {
-    case PBVH_FACES:
-    case PBVH_GRIDS:
-      blender::array_utils::invert_booleans({ss->hide_poly, ss->totfaces});
-      break;
-    case PBVH_BMESH: {
-      BMIter iter;
-      BMFace *f;
-
-      BM_ITER_MESH (f, &iter, ss->bm, BM_FACES_OF_MESH) {
-        BM_elem_flag_toggle(f, BM_ELEM_HIDDEN);
-      }
-      break;
-    }
-  }
-}
-
 void SCULPT_face_visibility_all_set(SculptSession *ss, bool visible)
 {
   SCULPT_topology_islands_invalidate(ss);

source/blender/editors/sculpt_paint/sculpt_face_set.cc
@@ -1593,63 +1593,3 @@ void SCULPT_OT_face_sets_edit(wmOperatorType *ot)
                              "Modify Hidden",
                              "Apply the edit operation to hidden Face Sets");
 }
-
-static int sculpt_face_sets_invert_visibility_exec(bContext *C, wmOperator *op)
-{
-  Object *ob = CTX_data_active_object(C);
-  SculptSession *ss = ob->sculpt;
-  Mesh *mesh = static_cast<Mesh *>(ob->data);
-  Depsgraph *depsgraph = CTX_data_depsgraph_pointer(C);
-
-  BKE_sculpt_update_object_for_edit(depsgraph, ob, false);
-
-  /* Not supported for dyntopo. */
-  if (BKE_pbvh_type(ss->pbvh) == PBVH_BMESH) {
-    return OPERATOR_CANCELLED;
-  }
-
-  PBVH *pbvh = ob->sculpt->pbvh;
-  Vector<PBVHNode *> nodes = blender::bke::pbvh::search_gather(pbvh, {});
-
-  if (nodes.is_empty()) {
-    return OPERATOR_CANCELLED;
-  }
-
-  ss->hide_poly = BKE_sculpt_hide_poly_ensure(mesh);
-
-  SCULPT_undo_push_begin(ob, op);
-  for (PBVHNode *node : nodes) {
-    SCULPT_undo_push_node(ob, node, SCULPT_UNDO_HIDDEN);
-  }
-
-  SCULPT_face_visibility_all_invert(ss);
-
-  SCULPT_undo_push_end(ob);
-
-  /* Sync face sets visibility and vertex visibility. */
-  SCULPT_visibility_sync_all_from_faces(ob);
-
-  for (PBVHNode *node : nodes) {
-    BKE_pbvh_node_mark_update_visibility(node);
-  }
-
-  BKE_pbvh_update_visibility(ss->pbvh);
-
-  SCULPT_tag_update_overlays(C);
-
-  return OPERATOR_FINISHED;
-}
-
-void SCULPT_OT_face_sets_invert_visibility(wmOperatorType *ot)
-{
-  /* Identifiers. */
-  ot->name = "Invert Face Set Visibility";
-  ot->idname = "SCULPT_OT_face_set_invert_visibility";
-  ot->description = "Invert the visibility of the Face Sets of the sculpt";
-
-  /* Api callbacks. */
-  ot->exec = sculpt_face_sets_invert_visibility_exec;
-  ot->poll = SCULPT_mode_poll;
-
-  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
-}

source/blender/editors/sculpt_paint/sculpt_intern.hh
@@ -985,7 +985,6 @@ bool SCULPT_vertex_visible_get(const SculptSession *ss, PBVHVertRef vertex);
 bool SCULPT_vertex_all_faces_visible_get(const SculptSession *ss, PBVHVertRef vertex);
 bool SCULPT_vertex_any_face_visible_get(SculptSession *ss, PBVHVertRef vertex);
 
-void SCULPT_face_visibility_all_invert(SculptSession *ss);
 void SCULPT_face_visibility_all_set(SculptSession *ss, bool visible);
 
 void SCULPT_visibility_sync_all_from_faces(Object *ob);
@@ -1572,7 +1571,6 @@ void SCULPT_OT_project_line_gesture(wmOperatorType *ot);
 
 void SCULPT_OT_face_sets_randomize_colors(wmOperatorType *ot);
 void SCULPT_OT_face_set_change_visibility(wmOperatorType *ot);
-void SCULPT_OT_face_sets_invert_visibility(wmOperatorType *ot);
 void SCULPT_OT_face_sets_init(wmOperatorType *ot);
 void SCULPT_OT_face_sets_create(wmOperatorType *ot);
 void SCULPT_OT_face_sets_edit(wmOperatorType *ot);

commit 355604c443ce89a47dd8743a896880f622e99219

Fix #120144: Invert Visible doesn't invert Face Sets properly
Caused by 04f7ba554cdefab877f1299293996eb8cc624dcc.
corner vertices of hidden face sets are not `1/true/hidden` (which
is expected). So inverting then hides these corner verts, hence the
small region of face sets is visible afterwards.
This could be avoided if `.hide_poly` attribute is used for changing the
visibility.

Pull Request: https://projects.blender.org/blender/blender/pulls/120160

source/blender/editors/sculpt_paint/paint_hide.cc
@@ -609,24 +609,27 @@ void PAINT_OT_hide_show_all(wmOperatorType *ot)
 
 static void invert_visibility_mesh(Object &object, const Span<PBVHNode *> nodes)
 {
+  PBVH &pbvh = *object.sculpt->pbvh;
   Mesh &mesh = *static_cast<Mesh *>(object.data);
   bke::MutableAttributeAccessor attributes = mesh.attributes_for_write();
-  bke::SpanAttributeWriter<bool> hide_vert = attributes.lookup_or_add_for_write_span<bool>(
-      ".hide_vert", bke::AttrDomain::Point);
+  bke::SpanAttributeWriter<bool> hide_poly = attributes.lookup_or_add_for_write_span<bool>(
+      ".hide_poly", bke::AttrDomain::Face);
 
+  threading::EnumerableThreadSpecific<Vector<int>> all_index_data;
   threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+    Vector<int> &faces = all_index_data.local();
     for (PBVHNode *node : nodes.slice(range)) {
-      undo::push_node(&object, node, undo::Type::HideVert);
-      for (const int vert : BKE_pbvh_node_get_unique_vert_indices(node)) {
-        hide_vert.span[vert] = !hide_vert.span[vert];
+      undo::push_node(&object, node, undo::Type::HideFace);
+      bke::pbvh::node_face_indices_calc_mesh(pbvh, *node, faces);
+      for (const int face : faces) {
+        hide_poly.span[face] = !hide_poly.span[face];
       }
       BKE_pbvh_node_mark_update_visibility(node);
-      bke::pbvh::node_update_visibility_mesh(hide_vert.span, *node);
     }
   });
 
-  hide_vert.finish();
-  bke::mesh_hide_vert_flush(mesh);
+  hide_poly.finish();
+  bke::mesh_hide_face_flush(mesh);
 }
 
 static void invert_visibility_grids(Depsgraph &depsgraph,