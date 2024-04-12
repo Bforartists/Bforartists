Bforartists/Bforartists

commit 7e6334380a410ccacb18b8270e6ee835a55e3748

Fix: GPv3: Crash rendering cyclic strokes with single point
Creating the batch cache for rendering crashes when
there is a stroke with one point that is set to cyclic.
There was a missing check for the number of points
because we're accessing the curve lengths, which
needs at least 2 points to be populated.

source/blender/draw/intern/draw_cache_impl_grease_pencil.cc
@@ -633,7 +633,7 @@ static void grease_pencil_geom_batch_ensure(Object &object,
                        cols_slice[idx]);
       }
 
-      if (is_cyclic) {
+      if (is_cyclic && points.size() > 1) {
         const int idx = points.size() + 1;
         const float u_stroke = u_scale * lengths[points.size() - 1] + u_translation;
         populate_point(verts_range,

commit 06d99eb7d6041d4eef3989bad920027961ea655e

Fix #120559: GPv3: Rendering issue if single point curve is set to cyclic
Caused by https://projects.blender.org/blender/blender/commit/7e6334380a410ccacb18b8270e6ee835a55e3748.
The `points.size() > 1` was added because we can't access
`lengths[points.size() - 1]` if the curve has a single point.
The fix just makes sure we default to a `u` value of `0.0f`
for single point cyclic curves.

Pull Request: https://projects.blender.org/blender/blender/pulls/120565

source/blender/draw/intern/draw_cache_impl_grease_pencil.cc
@@ -631,9 +631,10 @@ static void grease_pencil_geom_batch_ensure(Object &object,
                        cols_slice[idx]);
       }
 
-      if (is_cyclic && points.size() > 1) {
+      if (is_cyclic) {
         const int idx = points.size() + 1;
-        const float u_stroke = u_scale * lengths[points.size() - 1] + u_translation;
+        const float u = points.size() > 1 ? lengths[points.size() - 1] : 0.0f;
+        const float u_stroke = u_scale * u + u_translation;
         populate_point(verts_range,
                        curve_i,
                        start_caps[curve_i],