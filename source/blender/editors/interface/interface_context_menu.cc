52f1458ee9190bc0d70f68d3d40440ae7f83596d
Andres Stephens <ondraise15@hotmail.com>
2024-04-22 11:08:41 -0500

    File - New operator "Open Folder of Recent Items" in context menu #4195 Added icon to context menu

--- source/blender/editors/interface/interface_context_menu.cc
+++ source/blender/editors/interface/interface_context_menu.cc
@@ -1030,7 +1030,7 @@ bool ui_popup_context_menu_for_button(bContext *C, uiBut *but, const wmEvent *ev
         uiItemFullO_ptr(layout,
                         ot,
                         CTX_IFACE_(BLT_I18NCONTEXT_OPERATOR_DEFAULT, "Open File Location"),
-                        ICON_NONE,
+                        ICON_FILE_FOLDER, /*BFA*/
                         nullptr,
                         WM_OP_INVOKE_DEFAULT,
                         UI_ITEM_NONE,
