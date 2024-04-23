75b9d75203cb73c6aea625343d8449c16a186b62
Andres Stephens <ondraise15@hotmail.com>
2024-04-22 20:02:22 -0500

    Build Warning - SPACE_TOOLBAR missing from grease_pencil_convert_legacy.cc #4219

--- source/blender/blenkernel/intern/grease_pencil_convert_legacy.cc
+++ source/blender/blenkernel/intern/grease_pencil_convert_legacy.cc
@@ -2557,6 +2557,7 @@ static void legacy_gpencil_sanitize_annotations(Main &bmain)
           case SPACE_CLIP:
           case SPACE_TOPBAR:
           case SPACE_STATUSBAR:
+          case SPACE_TOOLBAR: /*bfa - the toolbar editor*/
           case SPACE_SPREADSHEET:
             break;
         }
