Bforartists/Bforartists

commit 1008bd6439be1f24887478fcf36d4f292d817743

Geometry Nodes: Avoid repeated node tools lookup when empty
Currently the node tools asset tree for extending 3D view headers is
rebuilt whenever it's cleared, which is done by clearing the tree's storage.
That means the data is rebuilt on every redraw if there are no node tools
and the tree storage is empty. With larger asset libraries that can be
quite expensive.

Now, instead of clearing the map, use a new dirty tag to store whether
the asset tree is out of date compared to the assets.

This should resolve #120494

source/blender/editors/asset/intern/asset_filter.cc
@@ -150,7 +150,8 @@ AssetItemTree build_filtered_all_catalog_tree(
 
   return {std::move(catalogs_with_node_assets),
           std::move(assets_per_path),
-          std::move(unassigned_assets)};
+          std::move(unassigned_assets),
+          false};
 }
 
 }  // namespace blender::ed::asset

source/blender/editors/asset/ED_asset_filter.hh
@@ -54,6 +54,8 @@ struct AssetItemTree {
       assets_per_path;
   /** Assets not added to a catalog, not part of #assets_per_path. */
   Vector<asset_system::AssetRepresentation *> unassigned_assets;
+  /** True if the tree is out of date compared to asset libraries and must be rebuilt. */
+  bool dirty = true;
 };
 
 asset_system::AssetCatalogTree build_filtered_catalog_tree(

source/blender/editors/geometry/node_group_operator.cc
@@ -787,7 +787,7 @@ void clear_operator_asset_trees()
   for (const ObjectType type : {OB_MESH, OB_CURVES, OB_POINTCLOUD}) {
     for (const eObjectMode mode : {OB_MODE_OBJECT, OB_MODE_EDIT, OB_MODE_SCULPT_CURVES}) {
       if (asset::AssetItemTree *tree = get_static_item_tree(type, mode)) {
-        *tree = {};
+        tree->dirty = true;
       }
     }
   }
@@ -1105,7 +1105,7 @@ void ui_template_node_operator_asset_root_items(uiLayout &layout, const bContext
   if (!tree) {
     return;
   }
-  if (tree->assets_per_path.size() == 0) {
+  if (tree->dirty) {
     *tree = build_catalog_tree(C, *active_object);
   }
 

commit 815f9de0ac0c2037cb83854188904fb866a024ba

Fix #121041: Node tools asset list missing on startup
Caused by 1008bd6439be1f248874 which marked the asset tree "not dirty"
when it was built even if asset loading wasn't finished.

source/blender/editors/asset/intern/asset_filter.cc
@@ -108,6 +108,9 @@ AssetItemTree build_filtered_all_catalog_tree(
     return {};
   }
 
+  const bool loading_finished = list::is_loaded(&library_ref);
+  const bool dirty = !loading_finished;
+
   list::iterate(library_ref, [&](asset_system::AssetRepresentation &asset) {
     if (!filter_matches_asset(&filter_settings, asset)) {
       return true;
@@ -151,7 +154,7 @@ AssetItemTree build_filtered_all_catalog_tree(
   return {std::move(catalogs_with_node_assets),
           std::move(assets_per_path),
           std::move(unassigned_assets),
-          false};
+          dirty};
 }
 
 }  // namespace blender::ed::asset