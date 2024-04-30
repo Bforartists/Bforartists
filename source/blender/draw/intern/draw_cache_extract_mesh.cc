Bforartists/Bforartists

commit 79a2a3dca01b2cdc8caa7e18fdf38cb3a5de3e76

Mesh: Lines index buffer creation improvement
Implements another phase of #116901, this time for the `lines` and
`lines_loose` index buffers that store indices for wireframe drawing.
The key improvement is removing loose edge's dependency on the main
edge index buffer. That means for the majority of meshes with no loose
edges, edge index extraction can be completely skipped. Even when there
are loose edges, only the loose edges need to be extracted with
wireframe turned off.

Besides that improvement, there are more changes to use data-oriented
code with visible hot loops instead of the virtual function call design
used for the existing mesh extractor system. For this step I completely
replaced the `extract_lines` object, which is in line with the general
plan for this area.

Additionally, hidden edge filtering is done ahead of time using several
`IndexMask` operations. This means only indices for visible edges need
to be uploaded to the GPU, and no restart index stripping needs to be
performed on macOS.

On my usual test file with 1.9 million vertices, I observed an
improvement from 26 to 33 FPS with wireframe off, and from 9.15 to 9.5
FPS with wireframe on.

Pull Request: https://projects.blender.org/blender/blender/pulls/120720

source/blender/draw/intern/draw_cache_extract_mesh.cc
@@ -654,27 +654,6 @@ void mesh_buffer_cache_create_requested(TaskGraph *task_graph,
   EXTRACT_ADD_REQUESTED(vbo, vnor);
 
   EXTRACT_ADD_REQUESTED(ibo, tris);
-  if (DRW_ibo_requested(mbuflist->ibo.lines_loose)) {
-    /* `ibo.lines_loose` require the `ibo.lines` buffer. */
-    if (mbuflist->ibo.lines == nullptr) {
-      DRW_ibo_request(nullptr, &mbuflist->ibo.lines);
-    }
-    const MeshExtract *extractor = DRW_ibo_requested(mbuflist->ibo.lines) ?
-                                       &extract_lines_with_lines_loose :
-                                       &extract_lines_loose_only;
-    extractors.append(extractor);
-  }
-  else if (DRW_ibo_requested(mbuflist->ibo.lines)) {
-    const MeshExtract *extractor;
-    if (mbuflist->ibo.lines_loose != nullptr) {
-      /* Update `ibo.lines_loose` as it depends on `ibo.lines`. */
-      extractor = &extract_lines_with_lines_loose;
-    }
-    else {
-      extractor = &extract_lines;
-    }
-    extractors.append(extractor);
-  }
   EXTRACT_ADD_REQUESTED(ibo, points);
   EXTRACT_ADD_REQUESTED(ibo, fdots);
   EXTRACT_ADD_REQUESTED(ibo, lines_paint_mask);
@@ -686,7 +665,9 @@ void mesh_buffer_cache_create_requested(TaskGraph *task_graph,
 
 #undef EXTRACT_ADD_REQUESTED
 
-  if (extractors.is_empty()) {
+  if (extractors.is_empty() && !DRW_ibo_requested(mbuflist->ibo.lines) &&
+      !DRW_ibo_requested(mbuflist->ibo.lines_loose))
+  {
     return;
   }
 
@@ -720,6 +701,26 @@ void mesh_buffer_cache_create_requested(TaskGraph *task_graph,
 
   /* Simple heuristic. */
   const bool use_thread = (mr->corners_num + mr->loose_indices_num) > MIN_RANGE_LEN;
+
+  if (DRW_ibo_requested(mbuflist->ibo.lines) || DRW_ibo_requested(mbuflist->ibo.lines_loose)) {
+    struct LooseEdgedata {
+      MeshRenderData &mr;
+      MeshBufferList &buffers;
+      MeshBatchCache &cache;
+    };
+    TaskNode *task_node = BLI_task_graph_node_create(
+        task_graph,
+        [](void *__restrict task_data) {
+          const LooseEdgedata &data = *static_cast<LooseEdgedata *>(task_data);
+          extract_lines(data.mr,
+                        data.buffers.ibo.lines,
+                        data.buffers.ibo.lines_loose,
+                        data.cache.no_loose_wire);
+        },
+        new LooseEdgedata{*mr, *mbuflist, cache},
+        [](void *task_data) { delete static_cast<LooseEdgedata *>(task_data); });
+    BLI_task_graph_edge_create(task_node_mesh_render_data, task_node);
+  }
 
   if (use_thread) {
     /* First run the requested extractors that do not support asynchronous ranges. */
@@ -830,27 +831,6 @@ void mesh_buffer_cache_create_requested_subdiv(MeshBatchCache &cache,
     extractors.append(&extract_fdots_pos);
   }
 
-  if (DRW_ibo_requested(mbuflist->ibo.lines_loose)) {
-    /* `ibo.lines_loose` require the `ibo.lines` buffer. */
-    if (mbuflist->ibo.lines == nullptr) {
-      DRW_ibo_request(nullptr, &mbuflist->ibo.lines);
-    }
-    const MeshExtract *extractor = DRW_ibo_requested(mbuflist->ibo.lines) ?
-                                       &extract_lines_with_lines_loose :
-                                       &extract_lines_loose_only;
-    extractors.append(extractor);
-  }
-  else if (DRW_ibo_requested(mbuflist->ibo.lines)) {
-    const MeshExtract *extractor;
-    if (mbuflist->ibo.lines_loose != nullptr) {
-      /* Update `ibo.lines_loose` as it depends on `ibo.lines`. */
-      extractor = &extract_lines_with_lines_loose;
-    }
-    else {
-      extractor = &extract_lines;
-    }
-    extractors.append(extractor);
-  }
   EXTRACT_ADD_REQUESTED(ibo, edituv_points);
   EXTRACT_ADD_REQUESTED(ibo, edituv_tris);
   EXTRACT_ADD_REQUESTED(ibo, edituv_lines);
@@ -873,7 +853,9 @@ void mesh_buffer_cache_create_requested_subdiv(MeshBatchCache &cache,
 
 #undef EXTRACT_ADD_REQUESTED
 
-  if (extractors.is_empty()) {
+  if (extractors.is_empty() && !DRW_ibo_requested(mbuflist->ibo.lines) &&
+      !DRW_ibo_requested(mbuflist->ibo.lines_loose))
+  {
     return;
   }
 
@@ -882,6 +864,9 @@ void mesh_buffer_cache_create_requested_subdiv(MeshBatchCache &cache,
   mesh_render_data_update_loose_geom(
       mr, mbc, MR_ITER_LOOSE_EDGE | MR_ITER_LOOSE_VERT, MR_DATA_LOOSE_GEOM);
   DRW_subdivide_loose_geom(&subdiv_cache, &mbc);
+
+  extract_lines_subdiv(
+      subdiv_cache, mr, mbuflist->ibo.lines, mbuflist->ibo.lines_loose, cache.no_loose_wire);
 
   void *data_stack = MEM_mallocN(extractors.data_size_total(), __func__);
   uint32_t data_offset = 0;

source/blender/draw/intern/draw_cache_extract.hh
@@ -113,7 +113,7 @@ struct MeshBufferList {
     gpu::IndexBuf *tris;
     /* Loose edges last. */
     gpu::IndexBuf *lines;
-    /* Sub buffer of `lines` only containing the loose edges. */
+    /* Potentially a sub buffer of `lines` only containing the loose edges. */
     gpu::IndexBuf *lines_loose;
     gpu::IndexBuf *points;
     gpu::IndexBuf *fdots;

source/blender/draw/intern/draw_cache_impl_mesh.cc
@@ -1576,7 +1576,6 @@ void DRW_mesh_batch_cache_create_requested(TaskGraph *task_graph,
   }
   assert_deps_valid(MBC_LOOSE_EDGES, {BUFFER_INDEX(ibo.lines_loose), BUFFER_INDEX(vbo.pos)});
   if (DRW_batch_requested(cache.batch.loose_edges, GPU_PRIM_LINES)) {
-    DRW_ibo_request(nullptr, &mbuflist->ibo.lines);
     DRW_ibo_request(cache.batch.loose_edges, &mbuflist->ibo.lines_loose);
     DRW_vbo_request(cache.batch.loose_edges, &mbuflist->vbo.pos);
   }

source/blender/draw/intern/draw_cache_impl_subdivision.cc
@@ -1332,7 +1332,8 @@ static void draw_subdiv_init_ubo_storage(const DRWSubdivCache &cache,
                                          const int src_offset,
                                          const int dst_offset,
                                          const uint total_dispatch_size,
-                                         const bool has_sculpt_mask)
+                                         const bool has_sculpt_mask,
+                                         const uint edge_loose_offset)
 {
   ubo->src_offset = src_offset;
   ubo->dst_offset = dst_offset;
@@ -1342,7 +1343,7 @@ static void draw_subdiv_init_ubo_storage(const DRWSubdivCache &cache,
   ubo->patches_are_triangular = cache.gpu_patch_map.patches_are_triangular;
   ubo->coarse_face_count = cache.num_coarse_faces;
   ubo->num_subdiv_loops = cache.num_subdiv_loops;
-  ubo->edge_loose_offset = cache.num_subdiv_loops * 2;
+  ubo->edge_loose_offset = edge_loose_offset;
   ubo->has_sculpt_mask = has_sculpt_mask;
   ubo->coarse_face_smooth_mask = SUBDIV_COARSE_FACE_FLAG_SMOOTH_MASK;
   ubo->coarse_face_select_mask = SUBDIV_COARSE_FACE_FLAG_SELECT_MASK;
@@ -1359,11 +1360,17 @@ static void draw_subdiv_ubo_update_and_bind(const DRWSubdivCache &cache,
                                             const int src_offset,
                                             const int dst_offset,
                                             const uint total_dispatch_size,
-                                            const bool has_sculpt_mask = false)
+                                            const bool has_sculpt_mask = false,
+                                            const uint edge_loose_offset = 0)
 {
   DRWSubdivUboStorage storage;
-  draw_subdiv_init_ubo_storage(
-      cache, &storage, src_offset, dst_offset, total_dispatch_size, has_sculpt_mask);
+  draw_subdiv_init_ubo_storage(cache,
+                               &storage,
+                               src_offset,
+                               dst_offset,
+                               total_dispatch_size,
+                               has_sculpt_mask,
+                               edge_loose_offset);
 
   if (!cache.ubo) {
     const_cast<DRWSubdivCache *>(&cache)->ubo = GPU_uniformbuf_create_ex(
@@ -1396,7 +1403,8 @@ static void drw_subdiv_compute_dispatch(const DRWSubdivCache &cache,
                                         const int src_offset,
                                         const int dst_offset,
                                         uint total_dispatch_size,
-                                        const bool has_sculpt_mask = false)
+                                        const bool has_sculpt_mask = false,
+                                        const uint edge_loose_offset = 0)
 {
   const uint max_res_x = uint(GPU_max_work_group_count(0));
 
@@ -1423,8 +1431,13 @@ static void drw_subdiv_compute_dispatch(const DRWSubdivCache &cache,
    * we presume it all fits. */
   BLI_assert(dispatch_ry < uint(GPU_max_work_group_count(1)));
 
-  draw_subdiv_ubo_update_and_bind(
-      cache, shader, src_offset, dst_offset, total_dispatch_size, has_sculpt_mask);
+  draw_subdiv_ubo_update_and_bind(cache,
+                                  shader,
+                                  src_offset,
+                                  dst_offset,
+                                  total_dispatch_size,
+                                  has_sculpt_mask,
+                                  edge_loose_offset);
 
   GPU_compute_dispatch(shader, dispatch_rx, dispatch_ry, 1);
 }
@@ -1858,6 +1871,7 @@ void draw_subdiv_build_lines_buffer(const DRWSubdivCache &cache, gpu::IndexBuf *
 void draw_subdiv_build_lines_loose_buffer(const DRWSubdivCache &cache,
                                           gpu::IndexBuf *lines_indices,
                                           gpu::VertBuf *lines_flags,
+                                          uint edge_loose_offset,
                                           uint num_loose_edges)
 {
   GPUShader *shader = get_subdiv_shader(SHADER_BUFFER_LINES_LOOSE);
@@ -1866,7 +1880,7 @@ void draw_subdiv_build_lines_loose_buffer(const DRWSubdivCache &cache,
   GPU_indexbuf_bind_as_ssbo(lines_indices, 3);
   GPU_vertbuf_bind_as_ssbo(lines_flags, 4);
 
-  drw_subdiv_compute_dispatch(cache, shader, 0, 0, num_loose_edges);
+  drw_subdiv_compute_dispatch(cache, shader, 0, 0, num_loose_edges, false, edge_loose_offset);
 
   /* This generates an index buffer, so we need to put a barrier on the element array. */
   GPU_memory_barrier(GPU_BARRIER_ELEMENT_ARRAY);

source/blender/draw/intern/draw_subdivision.hh
@@ -277,6 +277,7 @@ void draw_subdiv_build_lines_buffer(const DRWSubdivCache &cache, gpu::IndexBuf *
 void draw_subdiv_build_lines_loose_buffer(const DRWSubdivCache &cache,
                                           gpu::IndexBuf *lines_indices,
                                           gpu::VertBuf *lines_flags,
+                                          uint edge_loose_offset,
                                           uint num_loose_edges);
 
 void draw_subdiv_build_fdots_buffers(const DRWSubdivCache &cache,

source/blender/draw/intern/mesh_extractors/extract_mesh.hh
@@ -337,10 +337,17 @@ void mesh_render_data_loop_edge_flag(const MeshRenderData &mr,
 template<typename GPUType>
 void extract_vert_normals(const MeshRenderData &mr, MutableSpan<GPUType> normals);
 
+void extract_lines(const MeshRenderData &mr,
+                   gpu::IndexBuf *lines,
+                   gpu::IndexBuf *lines_loose,
+                   bool &no_loose_wire);
+void extract_lines_subdiv(const DRWSubdivCache &subdiv_cache,
+                          const MeshRenderData &mr,
+                          gpu::IndexBuf *lines,
+                          gpu::IndexBuf *lines_loose,
+                          bool &no_loose_wire);
+
 extern const MeshExtract extract_tris;
-extern const MeshExtract extract_lines;
-extern const MeshExtract extract_lines_with_lines_loose;
-extern const MeshExtract extract_lines_loose_only;
 extern const MeshExtract extract_points;
 extern const MeshExtract extract_fdots;
 extern const MeshExtract extract_lines_paint_mask;

commit 3fd1cd148c515be03f6b41f8534124cae6762524

Fix: EEVEE render crash after recent mesh index buffer commit
Caused by 79a2a3dca01b2cdc8caa7e18fdf38cb3a5de3e76.
The GPU subdivision extraction was missing the check for whether
neither lines nor lines_loose was requested.

source/blender/draw/intern/draw_cache_extract_mesh.cc
@@ -865,8 +865,10 @@ void mesh_buffer_cache_create_requested_subdiv(MeshBatchCache &cache,
       mr, mbc, MR_ITER_LOOSE_EDGE | MR_ITER_LOOSE_VERT, MR_DATA_LOOSE_GEOM);
   DRW_subdivide_loose_geom(&subdiv_cache, &mbc);
 
-  extract_lines_subdiv(
-      subdiv_cache, mr, mbuflist->ibo.lines, mbuflist->ibo.lines_loose, cache.no_loose_wire);
+  if (DRW_ibo_requested(mbuflist->ibo.lines) || DRW_ibo_requested(mbuflist->ibo.lines_loose)) {
+    extract_lines_subdiv(
+        subdiv_cache, mr, mbuflist->ibo.lines, mbuflist->ibo.lines_loose, cache.no_loose_wire);
+  }
 
   void *data_stack = MEM_mallocN(extractors.data_size_total(), __func__);
   uint32_t data_offset = 0;