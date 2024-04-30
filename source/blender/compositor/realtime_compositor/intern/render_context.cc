Bforartists/Bforartists

commit 1e9581cfcf458208902d9ca1e5841cef5b129003

Compositor: Refactor File Output node
This patches refactors the compositor File Output mechanism and
implements the file output node for the Realtime Compositor. The
refactor was done for the following reasons:

1. The existing file output mechanism relied on a global EXR image
   resource where the result of each compositor execution for each
   view was accumulated and stored in the global resource, until the
   last view is executed, when the EXR is finally saved. Aside from
   relying on global resources, this can cause effective memory leaks
   since the compositor can be interrupted before the EXR is written and
   closed.
2. We need common code to share between all compositors since we now
   have multiple compositor implementations.
3. We needed to take the opportunity to fix some of the issues with the
   existing implementation, like lossy compression of data passes,
   and inability to save single values passes.

The refactor first introduced a new structure called the Compositor
Render Context. This context stores compositor information related to
the render pipeline and is persistent across all compositor executions
of all views. Its extended lifetime relative to a single compositor
execution lends itself well to store data that is accumulated across
views. The context currently has a map of File Output objects. Those
objects wrap a Render Result structure and can be used to construct
multi-view images which can then be saved after all views are executed
using the existing BKE_image_render_write function.

Minor adjustments were made to the BKE and RE modules to allow saving
using the BKE_image_render_write function. Namely, the function now
allows the use of a source image format for saving as well as the
ability to not save the render result as a render by introducing two new
default arguments. Further, for multi-layer EXR saving, the existent of
a single unnamed render layer will omit the layer name from the EXR

source/blender/compositor/realtime_compositor/intern/render_context.cc
@@ -1,0 +1,151 @@
+/* SPDX-FileCopyrightText: 2023 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include <memory>
+#include <string>
+
+#include "BLI_assert.h"
+#include "BLI_listbase.h"
+#include "BLI_map.hh"
+#include "BLI_math_base.hh"
+#include "BLI_math_vector_types.hh"
+#include "BLI_string.h"
+#include "BLI_utildefines.h"
+
+#include "MEM_guardedalloc.h"
+
+#include "IMB_imbuf.h"
+#include "IMB_imbuf_types.h"
+
+#include "DNA_scene_types.h"
+#include "DNA_windowmanager_types.h"
+
+#include "BKE_image.h"
+#include "BKE_image_save.h"
+#include "BKE_report.h"
+
+#include "RE_pipeline.h"
+
+#include "COM_render_context.hh"
+
+namespace blender::realtime_compositor {
+
+/* ------------------------------------------------------------------------------------------------
+ * File Output
+ */
+
+FileOutput::FileOutput(std::string path, ImageFormatData format, int2 size, bool save_as_render)
+    : path_(path), format_(format), save_as_render_(save_as_render)
+{
+  render_result_ = MEM_cnew<RenderResult>("Temporary Render Result For File Output");
+
+  render_result_->rectx = size.x;
+  render_result_->recty = size.y;
+
+  /* File outputs are always single layer, as images are actually stored in passes on that single
+   * layer. Create a single unnamed layer to add the passes to. A single unnamed layer is treated
+   * by the EXR writer as a special case where the channel names take the form:
+   *   <pass-name>.<view-name>.<channel-id>
+   * Otherwise, the layer name would have preceded in the pass name in yet another section. */
+  RenderLayer *render_layer = MEM_cnew<RenderLayer>("Render Layer For File Output.");
+  BLI_addtail(&render_result_->layers, render_layer);
+  render_layer->name[0] = '\0';
+}
+
+FileOutput::~FileOutput()
+{
+  RE_FreeRenderResult(render_result_);
+}
+
+void FileOutput::add_view(const char *view_name)
+{
+  /* Empty views can only be added for EXR images. */
+  BLI_assert(ELEM(format_.imtype, R_IMF_IMTYPE_OPENEXR, R_IMF_IMTYPE_MULTILAYER));
+
+  RenderView *render_view = MEM_cnew<RenderView>("Render View For File Output.");
+  BLI_addtail(&render_result_->views, render_view);
+  STRNCPY(render_view->name, view_name);
+}
+
+void FileOutput::add_view(const char *view_name, int channels, float *buffer)
+{
+  RenderView *render_view = MEM_cnew<RenderView>("Render View For File Output.");
+  BLI_addtail(&render_result_->views, render_view);
+  STRNCPY(render_view->name, view_name);
+
+  render_view->ibuf = IMB_allocImBuf(
+      render_result_->rectx, render_result_->recty, channels * 8, 0);
+  render_view->ibuf->channels = channels;
+  IMB_assign_float_buffer(render_view->ibuf, buffer, IB_TAKE_OWNERSHIP);
+}
+
+void FileOutput::add_pass(const char *pass_name,
+                          const char *view_name,
+                          const char *channels,
+                          float *buffer)
+{
+  /* Passes can only be added for EXR images. */
+  BLI_assert(ELEM(format_.imtype, R_IMF_IMTYPE_OPENEXR, R_IMF_IMTYPE_MULTILAYER));
+
+  RenderLayer *render_layer = static_cast<RenderLayer *>(render_result_->layers.first);
+  RenderPass *render_pass = MEM_cnew<RenderPass>("Render Pass For File Output.");
+  BLI_addtail(&render_layer->passes, render_pass);
+  STRNCPY(render_pass->name, pass_name);
+  STRNCPY(render_pass->view, view_name);
+  STRNCPY(render_pass->chan_id, channels);
+
+  const int channels_count = BLI_strnlen(channels, 4);
+  render_pass->rectx = render_result_->rectx;
+  render_pass->recty = render_result_->recty;
+  render_pass->channels = channels_count;
+
+  render_pass->ibuf = IMB_allocImBuf(
+      render_result_->rectx, render_result_->recty, channels_count * 8, 0);
+  render_pass->ibuf->channels = channels_count;
+  IMB_assign_float_buffer(render_pass->ibuf, buffer, IB_TAKE_OWNERSHIP);
+}
+
+void FileOutput::add_meta_data(std::string key, std::string value)
+{
+  meta_data_.add(key, value);
+}
+
+void FileOutput::save(Scene *scene)
+{
+  ReportList reports;
+  BKE_reports_init(&reports, RPT_STORE);
+
+  /* Add scene stamp data as meta data as well as the custom meta data. */
+  BKE_render_result_stamp_info(scene, nullptr, render_result_, false);
+  for (const auto &field : meta_data_.items()) {
+    BKE_render_result_stamp_data(render_result_, field.key.c_str(), field.value.c_str());
+  }
+
+  BKE_image_render_write(
+      &reports, render_result_, scene, true, path_.c_str(), &format_, save_as_render_);
+
+  BKE_reports_free(&reports);
+}
+
+/* ------------------------------------------------------------------------------------------------
+ * Render Context
+ */
+
+FileOutput &RenderContext::get_file_output(std::string path,
+                                           ImageFormatData format,
+                                           int2 size,
+                                           bool save_as_render)
+{
+  return *file_outputs_.lookup_or_add_cb(
+      path, [&]() { return std::make_unique<FileOutput>(path, format, size, save_as_render); });
+}
+
+void RenderContext::save_file_outputs(Scene *scene)
+{
+  for (std::unique_ptr<FileOutput> &file_output : file_outputs_.values()) {
+    file_output->save(scene);
+  }
+}
+
+}  // namespace blender::realtime_compositor

source/blender/blenkernel/BKE_image_save.h
@@ -74,12 +74,15 @@ bool BKE_image_render_write_exr(struct ReportList *reports,
 
 /**
  * \param filepath_basis: May be used as-is, or used as a basis for multi-view images.
+ * \param format: The image format to use for saving, if null, the scene format will be used.
  */
 bool BKE_image_render_write(struct ReportList *reports,
                             struct RenderResult *rr,
                             const struct Scene *scene,
                             const bool stamp,
-                            const char *filepath_basis);
+                            const char *filepath_basis,
+                            const struct ImageFormatData *format = nullptr,
+                            bool save_as_render = true);
 
 #ifdef __cplusplus
 }

source/blender/blenkernel/intern/image_save.cc
@@ -785,6 +785,7 @@ bool BKE_image_render_write_exr(ReportList *reports,
 
   /* Other render layers. */
   int nr = (rr->have_combined) ? 1 : 0;
+  const bool has_multiple_layers = BLI_listbase_count_at_most(&rr->layers, 2) > 1;
   LISTBASE_FOREACH (RenderLayer *, rl, &rr->layers) {
     /* Skip other render layers if requested. */
     if (!multi_layer && nr != layer) {
@@ -830,8 +831,18 @@ bool BKE_image_render_write_exr(ReportList *reports,
         char layname[EXR_PASS_MAXNAME];
 
         if (multi_layer) {
-          RE_render_result_full_channel_name(passname, nullptr, rp->name, nullptr, rp->chan_id, a);
-          STRNCPY(layname, rl->name);
+          /* A single unnamed layer indicates that the pass name should be used as the layer name,
+           * while the pass name should be the channel ID. */
+          if (!has_multiple_layers && rl->name[0] == '\0') {
+            passname[0] = rp->chan_id[a];
+            passname[1] = '\0';
+            STRNCPY(layname, rp->name);
+          }
+          else {
+            RE_render_result_full_channel_name(
+                passname, nullptr, rp->name, nullptr, rp->chan_id, a);
+            STRNCPY(layname, rl->name);
+          }
         }
         else {
           passname[0] = rp->chan_id[a];
@@ -920,7 +931,9 @@ bool BKE_image_render_write(ReportList *reports,
                             RenderResult *rr,
                             const Scene *scene,
                             const bool stamp,
-                            const char *filepath_basis)
+                            const char *filepath_basis,
+                            const ImageFormatData *format,
+                            bool save_as_render)
 {
   bool ok = true;
 
@@ -929,7 +942,7 @@ bool BKE_image_render_write(ReportList *reports,
   }
 
   ImageFormatData image_format;
-  BKE_image_format_init_for_write(&image_format, scene, nullptr);
+  BKE_image_format_init_for_write(&image_format, scene, format);
 
   const bool is_mono = BLI_listbase_count_at_most(&rr->views, 2) < 2;
   const bool is_exr_rr = ELEM(
@@ -938,7 +951,8 @@ bool BKE_image_render_write(ReportList *reports,
   const float dither = scene->r.dither_intensity;
 
   if (image_format.views_format == R_IMF_VIEWS_MULTIVIEW && is_exr_rr) {
-    ok = BKE_image_render_write_exr(reports, rr, filepath_basis, &image_format, true, nullptr, -1);
+    ok = BKE_image_render_write_exr(
+        reports, rr, filepath_basis, &image_format, save_as_render, nullptr, -1);
     image_render_print_save_message(reports, filepath_basis, ok, errno);
   }
 
@@ -956,7 +970,8 @@ bool BKE_image_render_write(ReportList *reports,
       }
 
       if (is_exr_rr) {
-        ok = BKE_image_render_write_exr(reports, rr, filepath, &image_format, true, rv->name, -1);
+        ok = BKE_image_render_write_exr(
+            reports, rr, filepath, &image_format, save_as_render, rv->name, -1);
         image_render_print_save_message(reports, filepath, ok, errno);
 
         /* optional preview images for exr */
@@ -971,7 +986,7 @@ bool BKE_image_render_write(ReportList *reports,
 
           ImBuf *ibuf = RE_render_result_rect_to_ibuf(rr, &image_format, dither, view_id);
           ibuf->planes = 24;
-          IMB_colormanagement_imbuf_for_write(ibuf, true, false, &image_format);
+          IMB_colormanagement_imbuf_for_write(ibuf, save_as_render, false, &image_format);
 
           ok = image_render_write_stamp_test(
               reports, scene, rr, ibuf, filepath, &image_format, stamp);
@@ -982,7 +997,7 @@ bool BKE_image_render_write(ReportList *reports,
       else {
         ImBuf *ibuf = RE_render_result_rect_to_ibuf(rr, &image_format, dither, view_id);
 
-        IMB_colormanagement_imbuf_for_write(ibuf, true, false, &image_format);
+        IMB_colormanagement_imbuf_for_write(ibuf, save_as_render, false, &image_format);
 
         ok = image_render_write_stamp_test(
             reports, scene, rr, ibuf, filepath, &image_format, stamp);
@@ -1009,7 +1024,7 @@ bool BKE_image_render_write(ReportList *reports,
       for (i = 0; i < 2; i++) {
         int view_id = BLI_findstringindex(&rr->views, names[i], offsetof(RenderView, name));
         ibuf_arr[i] = RE_render_result_rect_to_ibuf(rr, &image_format, dither, view_id);
-        IMB_colormanagement_imbuf_for_write(ibuf_arr[i], true, false, &image_format);
+        IMB_colormanagement_imbuf_for_write(ibuf_arr[i], save_as_render, false, &image_format);
       }
 
       ibuf_arr[2] = IMB_stereo3d_ImBuf(&image_format, ibuf_arr[0], ibuf_arr[1]);

source/blender/compositor/CMakeLists.txt
@@ -10,6 +10,7 @@ if(WITH_COMPOSITOR_CPU)
     intern
     nodes
     operations
+    realtime_compositor
     ../blenkernel
     ../blentranslation
     ../imbuf
@@ -117,8 +118,6 @@ if(WITH_COMPOSITOR_CPU)
     nodes/COM_MaskNode.h
     nodes/COM_MovieClipNode.cc
     nodes/COM_MovieClipNode.h
-    nodes/COM_OutputFileNode.cc
-    nodes/COM_OutputFileNode.h
     nodes/COM_RenderLayersNode.cc
     nodes/COM_RenderLayersNode.h
     nodes/COM_SceneTimeNode.cc
@@ -139,6 +138,8 @@ if(WITH_COMPOSITOR_CPU)
     # output nodes
     nodes/COM_CompositorNode.cc
     nodes/COM_CompositorNode.h
+    nodes/COM_FileOutputNode.cc
+    nodes/COM_FileOutputNode.h
     nodes/COM_ViewLevelsNode.cc
     nodes/COM_ViewLevelsNode.h
     nodes/COM_ViewerNode.cc
@@ -419,10 +420,8 @@ if(WITH_COMPOSITOR_CPU)
     operations/COM_CompositorOperation.h
     operations/COM_ConvertDepthToRadiusOperation.cc
     operations/COM_ConvertDepthToRadiusOperation.h
-    operations/COM_OutputFileMultiViewOperation.cc
-    operations/COM_OutputFileMultiViewOperation.h
-    operations/COM_OutputFileOperation.cc
-    operations/COM_OutputFileOperation.h
+    operations/COM_FileOutputOperation.cc
+    operations/COM_FileOutputOperation.h
     operations/COM_PreviewOperation.cc
     operations/COM_PreviewOperation.h
     operations/COM_SplitOperation.cc
@@ -602,6 +601,7 @@ if(WITH_COMPOSITOR_CPU)
     PRIVATE bf::dna
     PRIVATE bf::intern::clog
     PRIVATE bf::intern::guardedalloc
+    bf_realtime_compositor
     extern_clew
     PRIVATE bf::intern::atomic
   )

source/blender/compositor/COM_compositor.hh
@@ -7,6 +7,10 @@
 #include "DNA_color_types.h"
 #include "DNA_node_types.h"
 
+namespace blender::realtime_compositor {
+class RenderContext;
+}
+
 struct Render;
 
 /* Keep ascii art. */
@@ -332,7 +336,8 @@ void COM_execute(Render *render,
                  Scene *scene,
                  bNodeTree *node_tree,
                  bool rendering,
-                 const char *view_name);
+                 const char *view_name,
+                 blender::realtime_compositor::RenderContext *render_context);
 
 /**
  * \brief Deinitialize the compositor caches and allocated memory.

source/blender/compositor/intern/COM_CompositorContext.h
@@ -12,6 +12,10 @@
 
 struct bNodeInstanceHash;
 
+namespace blender::realtime_compositor {
+class RenderContext;
+}
+
 namespace blender::compositor {
 
 /**
@@ -69,6 +73,12 @@ class CompositorContext {
    * \brief active rendering view name
    */
   const char *view_name_;
+
+  /**
+   * \brief Render context that contains information about active render. Can be null if the
+   * compositor is not executing as part of the render pipeline.
+   */
+  realtime_compositor::RenderContext *render_context_;
 
  public:
   /**
@@ -190,6 +200,22 @@ class CompositorContext {
   bool has_explicit_view() const
   {
     return view_name_ && view_name_[0] != '\0';
+  }
+
+  /**
+   * \brief get the render context
+   */
+  realtime_compositor::RenderContext *get_render_context() const
+  {
+    return render_context_;
+  }
+
+  /**
+   * \brief set the render context
+   */
+  void set_render_context(realtime_compositor::RenderContext *render_context)
+  {
+    render_context_ = render_context;
   }
 
   /**

source/blender/compositor/intern/COM_Converter.cc
@@ -50,6 +50,7 @@
 #include "COM_DistanceMatteNode.h"
 #include "COM_DoubleEdgeMaskNode.h"
 #include "COM_EllipseMaskNode.h"
+#include "COM_FileOutputNode.h"
 #include "COM_FilterNode.h"
 #include "COM_FlipNode.h"
 #include "COM_GammaNode.h"
@@ -75,7 +76,6 @@
 #include "COM_MovieDistortionNode.h"
 #include "COM_NormalNode.h"
 #include "COM_NormalizeNode.h"
-#include "COM_OutputFileNode.h"
 #include "COM_PixelateNode.h"
 #include "COM_PlaneTrackDeformNode.h"
 #include "COM_PosterizeNode.h"
@@ -347,7 +347,7 @@ Node *COM_convert_bnode(bNode *b_node)
       node = new ColorSpillNode(b_node);
       break;
     case CMP_NODE_OUTPUT_FILE:
-      node = new OutputFileNode(b_node);
+      node = new FileOutputNode(b_node);
       break;
     case CMP_NODE_MAP_VALUE:
       node = new MapValueNode(b_node);

source/blender/compositor/intern/COM_ExecutionSystem.cc
@@ -24,9 +24,11 @@ ExecutionSystem::ExecutionSystem(RenderData *rd,
                                  bNodeTree *editingtree,
                                  bool rendering,
                                  bool fastcalculation,
-                                 const char *view_name)
+                                 const char *view_name,
+                                 realtime_compositor::RenderContext *render_context)
 {
   num_work_threads_ = WorkScheduler::get_num_cpu_threads();
+  context_.set_render_context(render_context);
   context_.set_view_name(view_name);
   context_.set_scene(scene);
   context_.set_bnodetree(editingtree);

commit 07af45837afa65f22ca75c8dedebf5978c292be8

Fix #121264: Crash when File Output has preview
Blender crashes when a File Output was created when the Use Preview
option of the render output settings was enabled. This is a regression
introduced in 1e9581cfcf, where the image format of the node was
initialized from the scene image settings, so the preview option was
carried over, yet it is not supported nor exposed to the user in the
File Output node. To fix this, ensure the file output code will ignore
previews.

source/blender/compositor/realtime_compositor/intern/render_context.cc
@@ -53,6 +53,9 @@ FileOutput::FileOutput(const std::string &path,
   RenderLayer *render_layer = MEM_cnew<RenderLayer>("Render Layer For File Output.");
   BLI_addtail(&render_result_->layers, render_layer);
   render_layer->name[0] = '\0';
+
+  /* File outputs do not support previews. */
+  format_.flag &= ~R_IMF_FLAG_PREVIEW_JPG;
 }
 
 FileOutput::~FileOutput()