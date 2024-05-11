Bforartists/Bforartists

commit 097ccb767401a48622597dbff141be3991c1e1ea

GPv3: Remove Groups
This implements removing of groups and adds an operator
`GREASE_PENCIL_OT_layer_group_remove`.

Groups can be removed in two ways:
 * Remove the group and keep the children.
 * Remove the group together with all its children.

Based on an earlier attempt by @PratikPB2123 here: !121611

Pull Request: https://projects.blender.org/blender/blender/pulls/121663

scripts/startup/bl_ui/properties_data_grease_pencil.py
@@ -195,6 +195,15 @@ class GREASE_PENCIL_MT_grease_pencil_add_layer_extra(Menu):
         layout.prop(grease_pencil, "use_autolock_layers", text="Autolock Inactive Layers")
 
 
+class GreasePencil_MT_group_context_menu(Menu):
+    bl_label = "Layer Group"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("grease_pencil.layer_group_remove", text="Delete Group").keep_children = True
+        layout.operator("grease_pencil.layer_group_remove", text="Delete Group and children").keep_children = False
+
+
 class DATA_PT_grease_pencil_layers(DataButtonsPanel, Panel):
     bl_label = "Layers"
 
@@ -349,6 +358,7 @@ classes = (
     DATA_PT_grease_pencil_onion_skinning_display,
     DATA_PT_grease_pencil_settings,
     GREASE_PENCIL_MT_grease_pencil_add_layer_extra,
+    GreasePencil_MT_group_context_menu,
 )
 
 if _has_gpv3:

source/blender/editors/interface/templates/interface_template_grease_pencil_layer_tree.cc
@@ -317,6 +317,15 @@ class LayerGroupViewItem : public AbstractTreeViewItem {
     return {};
   }
 
+  void build_context_menu(bContext &C, uiLayout &layout) const override
+  {
+    MenuType *mt = WM_menutype_find("GreasePencil_MT_group_context_menu", true);
+    if (!mt) {
+      return;
+    }
+    UI_menutype_draw(&C, mt, &layout);
+  }
+
   void on_activate(bContext &C) override
   {
     PointerRNA grease_pencil_ptr = RNA_pointer_create(

source/blender/blenkernel/BKE_grease_pencil.hh
@@ -582,6 +582,11 @@ class LayerGroup : public ::GreasePencilLayerTreeGroup {
   TreeNode &as_node();
 
   /**
+   * Returns true if the group is empty.
+   */
+  bool is_empty() const;
+
+  /**
    * Returns the number of direct nodes in this group.
    */
   int64_t num_direct_nodes() const;
@@ -669,7 +674,7 @@ class LayerGroup : public ::GreasePencilLayerTreeGroup {
    * Unlink the node from the list of nodes in this group.
    * \returns true, if the node was successfully unlinked.
    */
-  bool unlink_node(TreeNode &link);
+  bool unlink_node(TreeNode &link, bool keep_children = false);
 
  private:
   void ensure_nodes_cache() const;
@@ -764,6 +769,10 @@ inline const TreeNode &LayerGroup::as_node() const
 inline TreeNode &LayerGroup::as_node()
 {
   return *reinterpret_cast<TreeNode *>(this);
+}
+inline bool LayerGroup::is_empty() const
+{
+  return BLI_listbase_is_empty(&this->children);
 }
 
 inline const TreeNode &Layer::as_node() const

source/blender/blenkernel/intern/grease_pencil.cc
@@ -1445,11 +1445,53 @@ int64_t LayerGroup::num_nodes_total() const
   return this->runtime->nodes_cache_.size();
 }
 
-bool LayerGroup::unlink_node(TreeNode &link)
+bool LayerGroup::unlink_node(TreeNode &link, const bool keep_children)
 {
-  if (BLI_remlink_safe(&this->children, &link)) {
-    this->tag_nodes_cache_dirty();
+  if (link.is_group() && !link.as_group().is_empty() && keep_children) {
+    if (BLI_findindex(&this->children, &link) == -1) {
+      return false;
+    }
+
+    /* Take ownership of the children of `link` by replacing the node with the listbase of its
+     * children. */
+    ListBase link_children = link.as_group().children;
+    GreasePencilLayerTreeNode *first = static_cast<GreasePencilLayerTreeNode *>(
+        link_children.first);
+    GreasePencilLayerTreeNode *last = static_cast<GreasePencilLayerTreeNode *>(link_children.last);
+
+    /* Rewrite the parent pointers. */
+    LISTBASE_FOREACH (GreasePencilLayerTreeNode *, child, &link_children) {
+      child->parent = this;
+    }
+
+    /* Update previous and/or next link(s). */
+    if (link.next != nullptr) {
+      link.next->prev = last;
+      last->next = link.next;
+    }
+    if (link.prev != nullptr) {
+      link.prev->next = first;
+      first->prev = link.prev;
+    }
+
+    /* Update first and/or last link(s). */
+    if (this->children.last == &link) {
+      this->children.last = last;
+    }
+    if (this->children.first == &link) {
+      this->children.first = first;
+    }
+
+    /* Listbase has been inserted in `this->children` we can clear the pointers in `link`. */
+    BLI_listbase_clear(&link.as_group().children);
     link.parent = nullptr;
+
+    this->tag_nodes_cache_dirty();
+    return true;
+  }
+  else if (BLI_remlink_safe(&this->children, &link)) {
+    link.parent = nullptr;
+    this->tag_nodes_cache_dirty();
     return true;
   }
   return false;
@@ -2940,28 +2982,45 @@ static void shrink_customdata(CustomData &data, const int index_to_remove, const
   data = new_data;
 }
 
+static void update_active_node(GreasePencil &grease_pencil,
+                               const blender::bke::greasepencil::TreeNode &node)
+{
+  using namespace blender::bke::greasepencil;
+  if (grease_pencil.active_node == nullptr) {
+    return;
+  }
+  const TreeNode &active_node = grease_pencil.active_node->wrap();
+  if (&active_node != &node) {
+    return;
+  }
+
+  if (node.is_group()) {
+    grease_pencil.set_active_layer(nullptr);
+    return;
+  }
+
+  Span<const Layer *> layers = grease_pencil.layers();
+  /* If there is no other layer available, unset the active layer. */
+  if (layers.size() == 1) {
+    grease_pencil.set_active_layer(nullptr);
+  }
+  else {
+    /* Make the layer below active (if possible). */
+    if (&active_node.as_layer() == layers.first()) {
+      grease_pencil.set_active_layer(layers[1]);
+    }
+    else {
+      int64_t active_index = layers.first_index(&active_node.as_layer());
+      grease_pencil.set_active_layer(layers[active_index - 1]);
+    }
+  }
+}
+
 void GreasePencil::remove_layer(blender::bke::greasepencil::Layer &layer)
 {
   using namespace blender::bke::greasepencil;
   /* If the layer is active, update the active layer. */
-  const Layer *active_layer = this->get_active_layer();
-  if (active_layer == &layer) {
-    Span<const Layer *> layers = this->layers();
-    /* If there is no other layer available, unset the active layer. */
-    if (layers.size() == 1) {
-      this->set_active_layer(nullptr);
-    }
-    else {
-      /* Make the layer below active (if possible). */
-      if (active_layer == layers.first()) {
-        this->set_active_layer(layers[1]);
-      }
-      else {
-        int64_t active_index = layers.first_index(active_layer);
-        this->set_active_layer(layers[active_index - 1]);
-      }
-    }
-  }
+  update_active_node(*this, layer.as_node());
 
   /* Remove all the layer attributes and shrink the `CustomData`. */
   const int64_t layer_index = this->layers().first_index(&layer);
@@ -2974,6 +3033,7 @@ void GreasePencil::remove_layer(blender::bke::greasepencil::Layer &layer)
   for (const GreasePencilFrame frame : layer.frames().values()) {
     GreasePencilDrawingBase *drawing_base = this->drawing(frame.drawing_index);
     if (drawing_base->type != GP_DRAWING) {
+      /* TODO: Remove drawing reference. */
       continue;
     }
     GreasePencilDrawing *drawing = reinterpret_cast<GreasePencilDrawing *>(drawing_base);
@@ -2983,6 +3043,31 @@ void GreasePencil::remove_layer(blender::bke::greasepencil::Layer &layer)
 
   /* Delete the layer. */
   MEM_delete(&layer);
+}
+
+void GreasePencil::remove_group(blender::bke::greasepencil::LayerGroup &group,
+                                const bool keep_children)
+{
+  using namespace blender::bke::greasepencil;
+  /* If the group is active, update the active layer. */
+  update_active_node(*this, group.as_node());
+
+  if (!keep_children) {
+    /* Recursively remove sub groups. */
+    for (LayerGroup *sub_group : group.groups_for_write()) {
+      this->remove_group(*sub_group, false);
+    }
+
+    /* Remove all the layers. */
+    for (Layer *layer : group.layers_for_write()) {
+      this->remove_layer(*layer);
+    }
+  }
+  /* Unlink the group. Keep the children if we want to keep them. */
+  group.as_node().parent_group()->unlink_node(group.as_node(), keep_children);
+
+  /* Delete the group. */
+  MEM_delete(&group);
 }
 
 void GreasePencil::print_layer_tree()

source/blender/editors/grease_pencil/intern/grease_pencil_layers.cc
@@ -294,6 +294,45 @@ static void GREASE_PENCIL_OT_layer_group_add(wmOperatorType *ot)
   ot->prop = prop;
 }
 
+static int grease_pencil_layer_group_remove_exec(bContext *C, wmOperator *op)
+{
+  using namespace blender::bke::greasepencil;
+  Object *object = CTX_data_active_object(C);
+  const bool keep_children = RNA_boolean_get(op->ptr, "keep_children");
+  GreasePencil &grease_pencil = *static_cast<GreasePencil *>(object->data);
+
+  if (!grease_pencil.has_active_group()) {
+    return OPERATOR_CANCELLED;
+  }
+
+  grease_pencil.remove_group(*grease_pencil.get_active_group(), keep_children);
+
+  DEG_id_tag_update(&grease_pencil.id, ID_RECALC_GEOMETRY);
+  WM_event_add_notifier(C, NC_GPENCIL | ND_DATA | NA_SELECTED, &grease_pencil);
+
+  return OPERATOR_FINISHED;
+}
+
+static void GREASE_PENCIL_OT_layer_group_remove(wmOperatorType *ot)
+{
+  /* identifiers */
+  ot->name = "Remove Layer Group";
+  ot->idname = "GREASE_PENCIL_OT_layer_group_remove";
+  ot->description = "Remove Grease Pencil layer group in the active object";
+
+  /* callbacks */
+  ot->exec = grease_pencil_layer_group_remove_exec;
+  ot->poll = active_grease_pencil_poll;
+
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+
+  RNA_def_boolean(ot->srna,
+                  "keep_children",
+                  false,
+                  "Keep children nodes",
+                  "Keep the children nodes of the group and only delete the group itself");
+}
+
 static int grease_pencil_layer_hide_exec(bContext *C, wmOperator *op)
 {
   using namespace blender::bke::greasepencil;
@@ -720,6 +759,7 @@ void ED_operatortypes_grease_pencil_layers()
   WM_operatortype_append(GREASE_PENCIL_OT_layer_duplicate);
 
   WM_operatortype_append(GREASE_PENCIL_OT_layer_group_add);
+  WM_operatortype_append(GREASE_PENCIL_OT_layer_group_remove);
 
   WM_operatortype_append(GREASE_PENCIL_OT_layer_mask_add);
   WM_operatortype_append(GREASE_PENCIL_OT_layer_mask_remove);

source/blender/makesdna/DNA_grease_pencil_types.h
@@ -555,6 +555,7 @@ typedef struct GreasePencil {
   void rename_node(blender::bke::greasepencil::TreeNode &node, blender::StringRefNull new_name);
 
   void remove_layer(blender::bke::greasepencil::Layer &layer);
+  void remove_group(blender::bke::greasepencil::LayerGroup &group, bool keep_children = false);
 
   /* Frames API functions. */
 

commit 465293f55e2cb9b5d4d508ac7239818357b27d7b

Fix: Warning due to lower case gpv3 menu identifier
Caused by 097ccb767401a48622597dbff141be3991c1e1ea

Pull Request: https://projects.blender.org/blender/blender/pulls/121684

scripts/startup/bl_ui/properties_data_grease_pencil.py
@@ -195,7 +195,7 @@ class GREASE_PENCIL_MT_grease_pencil_add_layer_extra(Menu):
         layout.prop(grease_pencil, "use_autolock_layers", text="Autolock Inactive Layers")
 
 
-class GreasePencil_MT_group_context_menu(Menu):
+class GREASE_PENCIL_MT_group_context_menu(Menu):
     bl_label = "Layer Group"
 
     def draw(self, context):
@@ -358,7 +358,7 @@ classes = (
     DATA_PT_grease_pencil_onion_skinning_display,
     DATA_PT_grease_pencil_settings,
     GREASE_PENCIL_MT_grease_pencil_add_layer_extra,
-    GreasePencil_MT_group_context_menu,
+    GREASE_PENCIL_MT_group_context_menu,
 )
 
 if _has_gpv3:

source/blender/editors/interface/templates/interface_template_grease_pencil_layer_tree.cc
@@ -319,7 +319,7 @@ class LayerGroupViewItem : public AbstractTreeViewItem {
 
   void build_context_menu(bContext &C, uiLayout &layout) const override
   {
-    MenuType *mt = WM_menutype_find("GreasePencil_MT_group_context_menu", true);
+    MenuType *mt = WM_menutype_find("GREASE_PENCIL_MT_group_context_menu", true);
     if (!mt) {
       return;
     }