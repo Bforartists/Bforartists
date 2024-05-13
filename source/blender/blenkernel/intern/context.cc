Bforartists/Bforartists

commit e7effb4192ff8e07f102b8e461395f89675f0e72

Cleanup: assert that popup regions are temporary

source/blender/blenkernel/intern/context.cc
@@ -997,6 +997,7 @@ void CTX_wm_region_set(bContext *C, ARegion *region)
 
 void CTX_wm_region_popup_set(bContext *C, ARegion *region_popup)
 {
+  BLI_assert(region_popup == nullptr || region_popup->regiontype == RGN_TYPE_TEMPORARY);
   C->wm.region_popup = region_popup;
 }
 

source/blender/editors/interface/regions/interface_region_popup.cc
@@ -393,6 +393,8 @@ static void ui_popup_block_position(wmWindow *window,
 
 static void ui_block_region_refresh(const bContext *C, ARegion *region)
 {
+  BLI_assert(region->regiontype == RGN_TYPE_TEMPORARY);
+
   ScrArea *ctx_area = CTX_wm_area(C);
   ARegion *ctx_region = CTX_wm_region(C);
 

commit 0f33e05866c3035559b6c7cd1ecd580884c2187f

Temp fix: Assert in `CTX_wm_region_popup_set` for debug builds
Caused by e7effb4192ff8e07f102b8e461395f89675f0e72.
Opening files and templates would cause an assert to be hit in
debug mode.

For now, comment the assert out to temporarily fix the issue.

source/blender/blenkernel/intern/context.cc
@@ -997,7 +997,7 @@ void CTX_wm_region_set(bContext *C, ARegion *region)
 
 void CTX_wm_region_popup_set(bContext *C, ARegion *region_popup)
 {
-  BLI_assert(region_popup == nullptr || region_popup->regiontype == RGN_TYPE_TEMPORARY);
+  /* BLI_assert(region_popup == nullptr || region_popup->regiontype == RGN_TYPE_TEMPORARY); */
   C->wm.region_popup = region_popup;
 }
 