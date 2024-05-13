Bforartists/Bforartists

commit a4bddea9d200ff8455460637103d92d106e18504

Nodes: Add default socket value to tooltip of socket
Even if node is not used, socket value is still there.
This value can be placed in tooltip instead of evaluated one.

Pull Request: https://projects.blender.org/blender/blender/pulls/120497

source/blender/editors/space_node/node_draw.cc
@@ -1583,6 +1583,31 @@ static void create_inspection_string_for_geometry_socket(std::stringstream &ss,
   }
 }
 
+static void create_inspection_string_for_default_socket_value(const bNodeSocket &socket,
+                                                              std::stringstream &ss)
+{
+  if (!socket.is_input()) {
+    return;
+  }
+  if (socket.is_directly_linked()) {
+    return;
+  }
+  if (const nodes::SocketDeclaration *socket_decl = socket.runtime->declaration) {
+    if (socket_decl->input_field_type == nodes::InputSocketFieldType::Implicit) {
+      return;
+    }
+  }
+  if (socket.typeinfo->base_cpp_type == nullptr) {
+    return;
+  }
+
+  const CPPType &value_type = *socket.typeinfo->base_cpp_type;
+  BUFFER_FOR_CPP_TYPE_VALUE(value_type, socket_value);
+  socket.typeinfo->get_base_cpp_value(socket.default_value, socket_value);
+  create_inspection_string_for_generic_value(socket, GPointer(value_type, socket_value), ss);
+  value_type.destruct(socket_value);
+}
+
 static std::optional<std::string> create_description_inspection_string(const bNodeSocket &socket)
 {
   if (socket.runtime->declaration == nullptr) {
@@ -1732,6 +1757,18 @@ static std::optional<std::string> create_multi_input_log_inspection_string(
   return str;
 }
 
+static std::optional<std::string> create_default_value_inspection_string(const bNodeSocket &socket)
+{
+  std::stringstream ss;
+  create_inspection_string_for_default_socket_value(socket, ss);
+
+  std::string str = ss.str();
+  if (str.empty()) {
+    return std::nullopt;
+  }
+  return str;
+}
+
 static std::string node_socket_get_tooltip(const SpaceNode *snode,
                                            const bNodeTree &ntree,
                                            const bNodeSocket &socket)
@@ -1752,6 +1789,9 @@ static std::string node_socket_get_tooltip(const SpaceNode *snode,
     inspection_strings.append(std::move(*info));
   }
   if (std::optional<std::string> info = create_log_inspection_string(geo_tree_log, socket)) {
+    inspection_strings.append(std::move(*info));
+  }
+  else if (std::optional<std::string> info = create_default_value_inspection_string(socket)) {
     inspection_strings.append(std::move(*info));
   }
   else if (std::optional<std::string> info = create_multi_input_log_inspection_string(

commit 378ca893d4caeacd37bfc6e80df6580017b0d86e

Fix: Nodes: Don't show default value in reroute dummy tooltip
This is issue that was acsidentially introduced in a4bddea9d200ff845546.

Pull Request: https://projects.blender.org/blender/blender/pulls/121689

source/blender/editors/space_node/node_draw.cc
@@ -1615,6 +1615,9 @@ static void create_inspection_string_for_default_socket_value(const bNodeSocket 
   if (socket.is_multi_input()) {
     return;
   }
+  if (socket.owner_node().is_reroute()) {
+    return;
+  }
   const Span<const bNodeSocket *> connected_sockets = socket.directly_linked_sockets();
   if (!connected_sockets.is_empty() && !connected_sockets[0]->owner_node().is_dangling_reroute()) {
     return;