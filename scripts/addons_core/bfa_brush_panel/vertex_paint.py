11b48849d7f92aae8ea4d0117d3f2f45a2f309b4
Andres Stephens <ondraise15@hotmail.com>
2024-05-16 08:03:00 -0500

    Add Bforartists addon to Core addons, to ship as a default

---
+++ scripts/addons_core/bfa_brush_panel/vertex_paint.py
@@ -0,0 +1,25 @@
+import bpy
+
+from .panels import panel_factory_view3d
+
+
+panel_classes = list(
+    panel_factory_view3d(
+        tools=["DRAW", "SMEAR", "AVERAGE", "BLUR"],
+        icon_prefix="brush.paint_vertex.",
+        tool_name_attr="vertex_tool",
+        use_paint_attr="use_paint_vertex",
+        tool_settings_attr="vertex_paint",
+        mode="PAINT_VERTEX",
+    )
+)
+
+
+def register():
+    for cls in panel_classes:
+        bpy.utils.register_class(cls)
+
+
+def unregister():
+    for cls in panel_classes:
+        bpy.utils.unregister_class(cls)
