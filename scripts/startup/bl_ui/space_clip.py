blender/blender

commit 0e3b594edb910ccb811bd5ac1998868d9ec72ea1

Refactor: scripts under scripts/startup/ to use `str.format`
Part of modernizing scripts in Blender, where the previous convention
was to use percentage formatting which has become the "old" way to
format strings in Python.

See proposal for details #120453.

Ref !120552

scripts/startup/bl_ui/space_clip.py
@@ -169,7 +169,7 @@ class CLIP_HT_header(Header):
                 r = active_object.reconstruction
 
                 if r.is_valid and sc.view == 'CLIP':
-                    layout.label(text=rpt_("Solve error: %.2f px") % (r.average_error), translate=False)
+                    layout.label(text=rpt_("Solve error: {.2f} px").format(r.average_error), translate=False)
 
                 row = layout.row()
                 row.prop(sc, "pivot_point", text="", icon_only=True)
@@ -761,7 +761,7 @@ class CLIP_PT_track(CLIP_PT_tracking_panel, Panel):
         layout.prop(act_track, "weight_stab")
 
         if act_track.has_bundle:
-            label_text = rpt_("Average Error: %.2f px") % (act_track.average_error)
+            label_text = rpt_("Average Error: {:.2f} px").format(act_track.average_error)
             layout.label(text=label_text, translate=False)
 
         layout.use_property_split = False
@@ -1301,7 +1301,7 @@ class CLIP_MT_view_zoom(Menu):
             percent = ratio * 100.0
             layout.operator(
                 "clip.view_zoom_ratio",
-                text="%g%% (%d:%d)" % (percent, a, b),
+                text="{:g}% ({:d}:{:d})".format(percent, a, b),
                 translate=False,
                 icon='LAYER_ACTIVE' if isclose(percent, current_zoom, abs_tol=0.5) else 'NONE',
             ).ratio = ratio

scripts/startup/bl_ui/space_sequencer.py
@@ -382,7 +382,7 @@ class SEQUENCER_MT_preview_zoom(Menu):
 
             layout.operator(
                 "sequencer.view_zoom_ratio",
-                text="%g%% (%d:%d)" % (percent, a, b),
+                text="{:g}% ({:d}:{:d})".format(percent, a, b),
                 translate=False,
                 icon='LAYER_ACTIVE' if isclose(percent, current_zoom, abs_tol=0.5) else 'NONE',
             ).ratio = ratio
@@ -1334,8 +1334,8 @@ class SEQUENCER_PT_color_tag_picker(SequencerColorTagPicker, Panel):
         row = layout.row(align=True)
         row.operator("sequencer.strip_color_tag_set", icon='X').color = 'NONE'
         for i in range(1, 10):
-            icon = 'SEQUENCE_COLOR_%02d' % i
-            row.operator("sequencer.strip_color_tag_set", icon=icon).color = 'COLOR_%02d' % i
+            icon = 'SEQUENCE_COLOR_{:02d}'.format(i)
+            row.operator("sequencer.strip_color_tag_set", icon=icon).color = 'COLOR_{:02d}'.format(i)
 
 
 class SEQUENCER_MT_color_tag_picker(SequencerColorTagPicker, Menu):
@@ -1561,11 +1561,11 @@ class SEQUENCER_PT_effect(SequencerButtonsPanel, Panel):
                     if i == strip.multicam_source:
                         sub = row.row(align=True)
                         sub.enabled = False
-                        sub.operator("sequencer.split_multicam", text="%d" % i).camera = i
+                        sub.operator("sequencer.split_multicam", text="{:d}".format(i)).camera = i
                     else:
                         sub_1 = row.row(align=True)
                         sub_1.enabled = True
-                        sub_1.operator("sequencer.split_multicam", text="%d" % i).camera = i
+                        sub_1.operator("sequencer.split_multicam", text="{:d}".format(i)).camera = i
 
                 if strip.channel > BT_ROW and (strip_channel - 1) % BT_ROW:
                     for i in range(strip.channel, strip_channel + ((BT_ROW + 1 - strip_channel) % BT_ROW)):
@@ -1726,7 +1726,7 @@ class SEQUENCER_PT_source(SequencerButtonsPanel, Panel):
                 if sound.samplerate <= 0:
                     split.label(text="Unknown")
                 else:
-                    split.label(text="%d Hz" % sound.samplerate, translate=False)
+                    split.label(text="{:d} Hz".format(sound.samplerate), translate=False)
 
                 split = col.split(factor=0.5, align=False)
                 split.alignment = 'RIGHT'
@@ -1782,7 +1782,7 @@ class SEQUENCER_PT_source(SequencerButtonsPanel, Panel):
             size = (elem.orig_width, elem.orig_height) if elem else (0, 0)
             if size[0] and size[1]:
                 split.alignment = 'LEFT'
-                split.label(text="%dx%d" % size, translate=False)
+                split.label(text="{:d}x{:d}".format(*size), translate=False)
             else:
                 split.label(text="None")
             # FPS
@@ -1791,7 +1791,7 @@ class SEQUENCER_PT_source(SequencerButtonsPanel, Panel):
                 split.alignment = 'RIGHT'
                 split.label(text="FPS")
                 split.alignment = 'LEFT'
-                split.label(text="%.2f" % elem.orig_fps, translate=False)
+                split.label(text="{:.2f}".format(elem.orig_fps), translate=False)
 
 
 class SEQUENCER_PT_scene(SequencerButtonsPanel, Panel):
@@ -1896,7 +1896,10 @@ class SEQUENCER_PT_mask(SequencerButtonsPanel, Panel):
         if mask:
             sta = mask.frame_start
             end = mask.frame_end
-            layout.label(text=rpt_("Original frame range: %d-%d (%d)") % (sta, end, end - sta + 1), translate=False)
+            layout.label(
+                text=rpt_("Original frame range: {:d}-{:d} ({:d})").format(sta, end, end - sta + 1),
+                translate=False,
+            )
 
 
 class SEQUENCER_PT_time(SequencerButtonsPanel, Panel):
@@ -1993,7 +1996,7 @@ class SEQUENCER_PT_time(SequencerButtonsPanel, Panel):
         split.alignment = 'RIGHT'
         split.label(text="End")
         split = split.split(factor=factor + 0.3 + max_factor, align=True)
-        split.label(text="%14s" % smpte_from_frame(frame_final_end))
+        split.label(text="{:>14s}".format(smpte_from_frame(frame_final_end)))
         split.alignment = 'RIGHT'
         split.label(text=str(frame_final_end) + " ")
 
@@ -2037,7 +2040,7 @@ class SEQUENCER_PT_time(SequencerButtonsPanel, Panel):
         split.label(text="Current Frame")
         split = split.split(factor=factor + 0.3 + max_factor, align=True)
         frame_display = frame_current - frame_final_start
-        split.label(text="%14s" % smpte_from_frame(frame_display))
+        split.label(text="{:>14s}".format(smpte_from_frame(frame_display)))
         split.alignment = 'RIGHT'
         split.label(text=str(frame_display) + " ")
 
@@ -2051,7 +2054,7 @@ class SEQUENCER_PT_time(SequencerButtonsPanel, Panel):
                 split.alignment = 'RIGHT'
                 split.label(text="Original Frame Range")
                 split.alignment = 'LEFT'
-                split.label(text="%d-%d (%d)" % (sta, end, end - sta + 1), translate=False)
+                split.label(text="{:d}-{:d} ({:d})".format(sta, end, end - sta + 1), translate=False)
 
 
 class SEQUENCER_PT_adjust_sound(SequencerButtonsPanel, Panel):
@@ -2090,7 +2093,7 @@ class SEQUENCER_PT_adjust_sound(SequencerButtonsPanel, Panel):
 
             audio_channels = context.scene.render.ffmpeg.audio_channels
             pan_enabled = sound.use_mono and audio_channels != 'MONO'
-            pan_text = "%.2f°" % (strip.pan * 90)
+            pan_text = "{:.2f}°".format(strip.pan * 90.0)
 
             split = col.split(factor=0.4)
             split.alignment = 'RIGHT'
@@ -2683,10 +2686,10 @@ class SEQUENCER_PT_modifiers(SequencerButtonsPanel, Panel):
                             col = flow.column()
                             box = col.box()
                             split = box.split(factor=0.4)
-                            split.label(text="%.2f" % sound_eq.curve_mapping.clip_min_x)
+                            split.label(text="{.2f}".format(sound_eq.curve_mapping.clip_min_x))
                             split.label(text="Hz")
                             split.alignment = "RIGHT"
-                            split.label(text="%.2f" % sound_eq.curve_mapping.clip_max_x)
+                            split.label(text="{.2f}".format(sound_eq.curve_mapping.clip_max_x))
                             box.template_curve_mapping(
                                 sound_eq,
                                 "curve_mapping",

scripts/startup/bl_app_templates_system/Video_Editing/__init__.py
@@ -21,7 +21,7 @@ def update_factory_startup_ffmpeg_preset():
     preset = "H264_in_MP4"
     preset_filepath = bpy.utils.preset_find(preset, preset_path="ffmpeg")
     if not preset_filepath:
-        print("Preset %r not found" % preset)
+        print("Preset {!r} not found".format(preset))
 
     for scene in bpy.data.scenes:
         render = scene.render

scripts/startup/bl_operators/anim.py
@@ -56,23 +56,23 @@ class ANIM_OT_keying_set_export(Operator):
         scene = context.scene
         ks = scene.keying_sets.active
 
-        f.write("# Keying Set: %s\n" % ks.bl_idname)
+        f.write("# Keying Set: {!s}\n".format(ks.bl_idname))
 
         f.write("import bpy\n\n")
         f.write("scene = bpy.context.scene\n\n")
 
         # Add KeyingSet and set general settings
         f.write("# Keying Set Level declarations\n")
-        f.write("ks = scene.keying_sets.new(idname=%r, name=%r)\n" % (ks.bl_idname, ks.bl_label))
-        f.write("ks.bl_description = %r\n" % ks.bl_description)
+        f.write("ks = scene.keying_sets.new(idname={!r}, name={!r})\n".format(ks.bl_idname, ks.bl_label))
+        f.write("ks.bl_description = {!r}\n".format(ks.bl_description))
 
         # TODO: this isn't editable, it should be possible to set this flag for `scene.keying_sets.new`.
         # if not ks.is_path_absolute:
         #     f.write("ks.is_path_absolute = False\n")
         f.write("\n")
 
-        f.write("ks.use_insertkey_needed = %r\n" % ks.use_insertkey_needed)
-        f.write("ks.use_insertkey_visual = %r\n" % ks.use_insertkey_visual)
+        f.write("ks.use_insertkey_needed = {!r}\n".format(ks.use_insertkey_needed))
+        f.write("ks.use_insertkey_visual = {!r}\n".format(ks.use_insertkey_visual))
         f.write("\n")
 
         # --------------------------------------------------------
@@ -96,51 +96,54 @@ class ANIM_OT_keying_set_export(Operator):
             #   (e.g. node-tree in Material).
             if ksp.id.bl_rna.identifier.startswith("ShaderNodeTree"):
                 # Find material or light using this node tree...
-                id_bpy_path = "bpy.data.nodes[\"%s\"]"
+                id_bpy_path = "bpy.data.nodes[\"{!s}\"]"
                 found = False
 
                 for mat in bpy.data.materials:
                     if mat.node_tree == ksp.id:
-                        id_bpy_path = "bpy.data.materials[\"%s\"].node_tree" % escape_identifier(mat.name)
+                        id_bpy_path = "bpy.data.materials[\"{!s}\"].node_tree".format(escape_identifier(mat.name))
                         found = True
                         break
 
                 if not found:
                     for light in bpy.data.lights:
                         if light.node_tree == ksp.id:
-                            id_bpy_path = "bpy.data.lights[\"%s\"].node_tree" % escape_identifier(light.name)
+                            id_bpy_path = "bpy.data.lights[\"{!s}\"].node_tree".format(escape_identifier(light.name))
                             found = True
                             break
 
                 if not found:
                     self.report(
                         {'WARN'},
-                        rpt_("Could not find material or light using Shader Node Tree - %s") %
-                        (ksp.id))
+                        rpt_("Could not find material or light using Shader Node Tree - {!s}").format(str(ksp.id))
+                    )
             elif ksp.id.bl_rna.identifier.startswith("CompositorNodeTree"):
                 # Find compositor node-tree using this node tree.
                 for scene in bpy.data.scenes:
                     if scene.node_tree == ksp.id:
-                        id_bpy_path = "bpy.data.scenes[\"%s\"].node_tree" % escape_identifier(scene.name)
+                        id_bpy_path = "bpy.data.scenes[\"{!s}\"].node_tree".format(escape_identifier(scene.name))
                         break
                 else:
-                    self.report({'WARN'}, rpt_("Could not find scene using Compositor Node Tree - %s") % (ksp.id))
+                    self.report(
+                        {'WARN'},
+                        rpt_("Could not find scene using Compositor Node Tree - {!s}").format(str(ksp.id))
+                    )
             elif ksp.id.bl_rna.name == "Key":
                 # "keys" conflicts with a Python keyword, hence the simple solution won't work
-                id_bpy_path = "bpy.data.shape_keys[\"%s\"]" % escape_identifier(ksp.id.name)
+                id_bpy_path = "bpy.data.shape_keys[\"{!s}\"]".format(escape_identifier(ksp.id.name))
             else:
                 idtype_list = ksp.id.bl_rna.name.lower() + "s"
-                id_bpy_path = "bpy.data.%s[\"%s\"]" % (idtype_list, escape_identifier(ksp.id.name))
+                id_bpy_path = "bpy.data.{!s}[\"{!s}\"]".format(idtype_list, escape_identifier(ksp.id.name))
 
             # shorthand ID for the ID-block (as used in the script)
-            short_id = "id_%d" % len(id_to_paths_cache)
+            short_id = "id_{:d}".format(len(id_to_paths_cache))
 
             # store this in the cache now
             id_to_paths_cache[ksp.id] = [short_id, id_bpy_path]
 
         f.write("# ID's that are commonly used\n")
         for id_pair in id_to_paths_cache.values():
-            f.write("%s = %s\n" % (id_pair[0], id_pair[1]))
+            f.write("{!s} = {!s}\n".format(id_pair[0], id_pair[1]))
         f.write("\n")
 
         # write paths
@@ -154,22 +157,21 @@ class ANIM_OT_keying_set_export(Operator):
                 id_bpy_path = id_to_paths_cache[ksp.id][0]
             else:
                 id_bpy_path = "None"  # XXX...
-            f.write("%s, %r" % (id_bpy_path, ksp.data_path))
+            f.write("{!s}, {!r}".format(id_bpy_path, ksp.data_path))
 
             # array index settings (if applicable)
             if ksp.use_entire_array:
                 f.write(", index=-1")
             else:
-                f.write(", index=%d" % ksp.array_index)
+                f.write(", index={:d}".format(ksp.array_index))
 
             # grouping settings (if applicable)
             # NOTE: the current default is KEYINGSET, but if this changes,
             # change this code too
             if ksp.group_method == 'NAMED':
-                f.write(", group_method=%r, group_name=%r" %
-                        (ksp.group_method, ksp.group))
+                f.write(", group_method={!r}, group_name={!r}".format(ksp.group_method, ksp.group))
             elif ksp.group_method != 'KEYINGSET':
-                f.write(", group_method=%r" % ksp.group_method)
+                f.write(", group_method={!r}".format(ksp.group_method))
 
             # finish off
             f.write(")\n")
@@ -364,8 +366,7 @@ class ClearUselessActions(Operator):
                     action.user_clear()
                     removed += 1
 
-        self.report({'INFO'}, rpt_("Removed %d empty and/or fake-user only Actions")
-                    % removed)
+        self.report({'INFO'}, rpt_("Removed {:d} empty and/or fake-user only Actions").format(removed))
         return {'FINISHED'}
 
 
@@ -449,7 +450,7 @@ class UpdateAnimatedTransformConstraint(Operator):
             print(log)
             text = bpy.data.texts.new("UpdateAnimatedTransformConstraint Report")
             text.from_string(log)
-            self.report({'INFO'}, rpt_("Complete report available on '%s' text datablock") % text.name)
+            self.report({'INFO'}, rpt_("Complete report available on '{!s}' text datablock").format(text.name))
         return {'FINISHED'}
 
 
@@ -495,7 +496,7 @@ class ARMATURE_OT_copy_bone_color_to_selected(Operator):
 
             # Anything else:
             case _:
-                self.report({'ERROR'}, "Cannot do anything in mode %r" % context.mode)
+                self.report({'ERROR'}, "Cannot do anything in mode {!r}".format(context.mode))
                 return {'CANCELLED'}
 
         if not bone_source:
@@ -524,8 +525,10 @@ class ARMATURE_OT_copy_bone_color_to_selected(Operator):
         if num_pose_color_overrides:
             self.report(
                 {'INFO'},
-                "Bone colors were synced; for %d bones this will not be visible due to pose bone color overrides" %
-                num_pose_color_overrides)
+                "Bone colors were synced; "
+                "for {:d} bones this will not be visible due to pose bone color overrides".format(
+                    num_pose_color_overrides,
+                ))
 
         return {'FINISHED'}
 
@@ -660,8 +663,9 @@ class ARMATURE_OT_collection_remove_unused(Operator):
         for bcoll in reversed(list(bcolls_to_remove)):
             armature.collections.remove(bcoll)
 
-        self.report({'INFO'}, "Removed %d of %d bone collections" %
-                    (num_bcolls_to_remove, num_bcolls_before_removal))
+        self.report(
+            {'INFO'}, "Removed {:d} of {:d} bone collections".format(num_bcolls_to_remove, num_bcolls_before_removal),
+        )
 
 
 classes = (

scripts/startup/bl_operators/assets.py
@@ -125,7 +125,7 @@ class ASSET_OT_open_containing_blend_file(Operator):
             return {'RUNNING_MODAL'}
 
         if returncode:
-            self.report({'WARNING'}, rpt_("Blender sub-process exited with error code %d") % returncode)
+            self.report({'WARNING'}, rpt_("Blender sub-process exited with error code {:d}").format(returncode))
 
         if bpy.ops.asset.library_refresh.poll():
             bpy.ops.asset.library_refresh()

scripts/startup/bl_operators/clip.py
@@ -197,7 +197,7 @@ class CLIP_OT_filter_tracks(Operator):
 
     def execute(self, context):
         num_tracks = self._filter_values(context, self.track_threshold)
-        self.report({'INFO'}, rpt_("Identified %d problematic tracks") % num_tracks)
+        self.report({'INFO'}, rpt_("Identified {:d} problematic tracks").format(num_tracks))
         return {'FINISHED'}
 
 
@@ -364,11 +364,11 @@ class CLIP_OT_delete_proxy(Operator):
 
         # proxy_<quality>[_undistorted]
         for x in (25, 50, 75, 100):
-            d = os.path.join(absproxy, "proxy_%d" % x)
+            d = os.path.join(absproxy, "proxy_{:d}".format(x))
 
             self._rmproxy(d)
             self._rmproxy(d + "_undistorted")
-            self._rmproxy(os.path.join(absproxy, "proxy_%d.avi" % x))
+            self._rmproxy(os.path.join(absproxy, "proxy_{:d}.avi".format(x)))
 
         tc = (
             "free_run.blen_tc",

scripts/startup/bl_operators/console.py
@@ -42,7 +42,7 @@ class ConsoleExec(Operator):
         if execute is not None:
             return execute(context, self.interactive)
         else:
-            print("Error: bpy.ops.console.execute_%s - not found" % sc.language)
+            print("Error: bpy.ops.console.execute_{!s} - not found".format(sc.language))
             return {'FINISHED'}
 
 
@@ -64,7 +64,7 @@ class ConsoleAutocomplete(Operator):
         if autocomplete:
             return autocomplete(context)
         else:
-            print("Error: bpy.ops.console.autocomplete_%s - not found" % sc.language)
+            print("Error: bpy.ops.console.autocomplete_{!s} - not found".format(sc.language))
             return {'FINISHED'}
 
 
@@ -86,7 +86,7 @@ class ConsoleCopyAsScript(Operator):
         if copy_as_script:
             return copy_as_script(context)
         else:
-            print("Error: copy_as_script - not found for %r" % sc.language)
+            print("Error: copy_as_script - not found for {!r}".format(sc.language))
             return {'FINISHED'}
 
 
@@ -112,7 +112,7 @@ class ConsoleBanner(Operator):
         if banner:
             return banner(context)
         else:
-            print("Error: bpy.ops.console.banner_%s - not found" % sc.language)
+            print("Error: bpy.ops.console.banner_{!s} - not found".format(sc.language))
             return {'FINISHED'}
 
 

scripts/startup/bl_operators/file.py
@@ -125,7 +125,7 @@ class WM_OT_previews_batch_generate(Operator):
             if not self.use_backups:
                 cmd.append("--no_backups")
             if subprocess.call(cmd):
-                self.report({'ERROR'}, rpt_("Previews generation process failed for file '%s'!") % blen_path)
+                self.report({'ERROR'}, rpt_("Previews generation process failed for file '{!s}'!").format(blen_path))
                 context.window_manager.progress_end()
                 return {'CANCELLED'}
             context.window_manager.progress_update(i + 1)
@@ -235,7 +235,7 @@ class WM_OT_previews_batch_clear(Operator):
             if not self.use_backups:
                 cmd.append("--no_backups")
             if subprocess.call(cmd):
-                self.report({'ERROR'}, rpt_("Previews clear process failed for file '%s'!") % blen_path)
+                self.report({'ERROR'}, rpt_("Previews clear process failed for file '{!s}'!").format(blen_path))
                 context.window_manager.progress_end()
                 return {'CANCELLED'}
             context.window_manager.progress_update(i + 1)
@@ -273,8 +273,9 @@ class WM_OT_blend_strings_utf8_validate(Operator):
                 val_utf8 = val_bytes.decode("utf-8", "replace")
                 val_bytes_valid = val_utf8.encode("utf-8")
                 if val_bytes_valid != val_bytes:
-                    print("found bad utf8 encoded string %r, fixing to %r (%r)..."
-                          "" % (val_bytes, val_bytes_valid, val_utf8))
+                    print("found bad utf8 encoded string {!r}, fixing to {!r} ({!r})...".format(
+                        val_bytes, val_bytes_valid, val_utf8,
+                    ))
                     setattr(item, prop.identifier, val_utf8)
                     changed = True
             elif prop.type == 'POINTER':

Bforartists/Bforartists

commit 159ec8cad2abc9e13cea7357e28747a32c230b68

Fix #121892: string formatting errors in the clip editor & sequencer
Regression in 0e3b594edb910ccb811bd5ac1998868d9ec72ea1.

scripts/startup/bl_ui/space_clip.py
@@ -171,7 +171,7 @@ class CLIP_HT_header(Header):
                 r = active_object.reconstruction
 
                 if r.is_valid and sc.view == 'CLIP':
-                    layout.label(text=rpt_("Solve error: {.2f} px").format(r.average_error), translate=False)
+                    layout.label(text=rpt_("Solve error: {:.2f} px").format(r.average_error), translate=False)
 
                 row = layout.row()
                 row.prop(sc, "pivot_point", text="", icon_only=True)

scripts/startup/bl_ui/space_sequencer.py
@@ -2742,10 +2742,10 @@ class SEQUENCER_PT_modifiers(SequencerButtonsPanel, Panel):
                             col = flow.column()
                             box = col.box()
                             split = box.split(factor=0.4)
-                            split.label(text="{.2f}".format(sound_eq.curve_mapping.clip_min_x))
+                            split.label(text="{:.2f}".format(sound_eq.curve_mapping.clip_min_x))
                             split.label(text="Hz")
                             split.alignment = 'RIGHT'
-                            split.label(text="{.2f}".format(sound_eq.curve_mapping.clip_max_x))
+                            split.label(text="{:.2f}".format(sound_eq.curve_mapping.clip_max_x))
                             box.template_curve_mapping(
                                 sound_eq,
                                 "curve_mapping",