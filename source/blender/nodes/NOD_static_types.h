5664869572de8e24e273a82e758356546f4fe9d4
ReinerBforartists <reiner.prokein@posteo.de>
2024-05-24 18:15:53 +0200

    Geometry Nodes - Input - Constant - New Rotation node #4261

--- source/blender/nodes/NOD_static_types.h
+++ source/blender/nodes/NOD_static_types.h
@@ -278,7 +278,7 @@ DefNode(FunctionNode, FN_NODE_FLOAT_TO_INT, def_float_to_int,  "FLOAT_TO_INT",  
 DefNode(FunctionNode, FN_NODE_INPUT_BOOL, def_fn_input_bool,  "INPUT_BOOL",  InputBool,  "Boolean",  "Add a Boolean Function node\nAdds a boolean that can be on or off")
 DefNode(FunctionNode, FN_NODE_INPUT_COLOR, def_fn_input_color,  "INPUT_COLOR",  InputColor,  "Color",  "Add a Color Function node\nAdds a color picker that allows you to set a color")
 DefNode(FunctionNode, FN_NODE_INPUT_INT, def_fn_input_int,  "INPUT_INT",  InputInt,  "Integer",  "Add a Integer Function node\nAllows to add a integer number")
-DefNode(FunctionNode, FN_NODE_INPUT_ROTATION, def_fn_input_rotation, "INPUT_ROTATION", InputRotation, "Rotation", "")
+DefNode(FunctionNode, FN_NODE_INPUT_ROTATION, def_fn_input_rotation, "INPUT_ROTATION", InputRotation, "Rotation", "Add a Rotation Function node\nAllows a constant rotation")
 DefNode(FunctionNode, FN_NODE_INPUT_SPECIAL_CHARACTERS,  0,  "INPUT_SPECIAL_CHARACTERS",  InputSpecialCharacters,  "Special Characters",  "Add a Special Character Function node\nAdd special characters")
 DefNode(FunctionNode, FN_NODE_INPUT_STRING, def_fn_input_string,  "INPUT_STRING",  InputString,  "String",  "Add a String Function node\nAdds a string")
 DefNode(FunctionNode, FN_NODE_INPUT_VECTOR, def_fn_input_vector,  "INPUT_VECTOR",  InputVector,  "Vector",  "Add a Vector Function Function node\nCreates a single vector of three values that can be used as an input")
