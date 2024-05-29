Bforartists/Bforartists

commit b1ba714339f7917a033c3cde2358f4edcf231883

Fix #122159: GPv3: Missing update after selecting frames  in multiframe edit
The `ANIM_animdata_update` wasn't handling the GPv3 cases.
This meant that e.g. changing the selection of keyframes did not
cause an update to be triggered.

This fixes the issue by making sure we call `ANIM_list_elem_update`
for all the grease pencil channel types.

Pull Request: https://projects.blender.org/blender/blender/pulls/122167

source/blender/editors/animation/anim_deps.cc
@@ -362,7 +362,10 @@ void ANIM_animdata_update(bAnimContext *ac, ListBase *anim_data)
                   ANIMTYPE_ANIMDATA,
                   ANIMTYPE_NLAACTION,
                   ANIMTYPE_NLATRACK,
-                  ANIMTYPE_NLACURVE))
+                  ANIMTYPE_NLACURVE,
+                  ANIMTYPE_GREASE_PENCIL_LAYER,
+                  ANIMTYPE_GREASE_PENCIL_LAYER_GROUP,
+                  ANIMTYPE_GREASE_PENCIL_DATABLOCK))
     {
       if (ale->update & ANIM_UPDATE_DEPS) {
         ale->update &= ~ANIM_UPDATE_DEPS;

commit a97b1c95c25e8f2f71b2dbeea1ad888aea2bbe6a

GPv3: Assert on translating keys
Caused by b1ba714339.
`ale->update` should be 0 after animdata update. Otherwise it would
cause the assert (it also ensures all update cases are handled for keys).

Resolves #122259

Pull Request: https://projects.blender.org/blender/blender/pulls/122435

source/blender/editors/animation/anim_deps.cc
@@ -362,7 +362,14 @@ void ANIM_animdata_update(bAnimContext *ac, ListBase *anim_data)
                   ANIMTYPE_ANIMDATA,
                   ANIMTYPE_NLAACTION,
                   ANIMTYPE_NLATRACK,
-                  ANIMTYPE_NLACURVE,
+                  ANIMTYPE_NLACURVE))
+    {
+      if (ale->update & ANIM_UPDATE_DEPS) {
+        ale->update &= ~ANIM_UPDATE_DEPS;
+        ANIM_list_elem_update(ac->bmain, ac->scene, ale);
+      }
+    }
+    else if (ELEM(ale->type,
                   ANIMTYPE_GREASE_PENCIL_LAYER,
                   ANIMTYPE_GREASE_PENCIL_LAYER_GROUP,
                   ANIMTYPE_GREASE_PENCIL_DATABLOCK))
@@ -371,6 +378,9 @@ void ANIM_animdata_update(bAnimContext *ac, ListBase *anim_data)
         ale->update &= ~ANIM_UPDATE_DEPS;
         ANIM_list_elem_update(ac->bmain, ac->scene, ale);
       }
+      /* Order appears to be already handled in `grease_pencil_layer_apply_trans_data` when
+       * translating. */
+      ale->update &= ~(ANIM_UPDATE_HANDLES | ANIM_UPDATE_ORDER);
     }
     else if (ale->update) {
 #if 0