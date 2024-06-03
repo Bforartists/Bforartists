Bforartists/Bforartists

commit c929816fcf18adbada192e718b1605f27145b76e

GPv3: Lock material layer property
This ports the `lock_material` layer property to GPv3.
For this, the `layer_index` is also required in `retrieve_editable_points`
and `retrieve_editable_strokes` to choose strokes based on
the `lock_material` property.
In `retrieve_editable_elements` we now pass the `MutableDrawingInfo`
to get both the `drawing` and `layer_index`.

Pull Request: https://projects.blender.org/blender/blender/pulls/119913

scripts/startup/bl_ui/properties_data_grease_pencil.py
@@ -174,6 +174,7 @@ class GREASE_PENCIL_MT_grease_pencil_add_layer_extra(Menu):
         layout = self.layout
         ob = context.object
         grease_pencil = ob.data
+        layer = grease_pencil.layers.active_layer
         space = context.space_data
 
         if space.type == 'PROPERTIES':
@@ -193,6 +194,7 @@ class GREASE_PENCIL_MT_grease_pencil_add_layer_extra(Menu):
 
         layout.separator()
         layout.prop(grease_pencil, "use_autolock_layers", text="Autolock Inactive Layers")
+        layout.prop(layer, "use_locked_material")
 
 
 class GREASE_PENCIL_MT_group_context_menu(Menu):

source/blender/blenkernel/BKE_grease_pencil.hh
@@ -185,6 +185,7 @@ class Layer;
   void set_selected(bool selected); \
   bool use_onion_skinning() const; \
   bool use_masks() const; \
+  bool use_locked_material() const; \
   bool is_child_of(const LayerGroup &group) const;
 
 /* Implements the forwarding of the methods defined by #TREENODE_COMMON_METHODS. */
@@ -232,6 +233,10 @@ class Layer;
   inline bool class_name::use_masks() const \
   { \
     return this->as_node().use_masks(); \
+  } \
+  inline bool class_name::use_locked_material() const \
+  { \
+    return this->as_node().use_locked_material(); \
   } \
   inline bool class_name::is_child_of(const LayerGroup &group) const \
   { \
@@ -770,6 +775,10 @@ inline bool TreeNode::use_masks() const
 {
   return ((this->flag & GP_LAYER_TREE_NODE_HIDE_MASKS) == 0) &&
          (!this->parent_group() || this->parent_group()->as_node().use_masks());
+}
+inline bool TreeNode::use_locked_material() const
+{
+  return (this->flag & GP_LAYER_TREE_NODE_USE_LOCKED_MATERIAL) != 0;
 }
 inline bool TreeNode::is_child_of(const LayerGroup &group) const
 {

source/blender/draw/intern/draw_cache_impl_grease_pencil.cc
@@ -461,7 +461,7 @@ static void grease_pencil_edit_batch_ensure(Object &object,
     const VArray<bool> cyclic = curves.cyclic();
     IndexMaskMemory memory;
     const IndexMask editable_strokes = ed::greasepencil::retrieve_editable_strokes(
-        object, info.drawing, memory);
+        object, info.drawing, info.layer_index, memory);
 
     /* Assumes that if the ".selection" attribute does not exist, all points are selected. */
     const VArray<float> selection_float = *attributes.lookup_or_default<float>(
@@ -531,7 +531,7 @@ static void grease_pencil_edit_batch_ensure(Object &object,
     const VArray<bool> cyclic = curves.cyclic();
     IndexMaskMemory memory;
     const IndexMask editable_strokes = ed::greasepencil::retrieve_editable_strokes(
-        object, info.drawing, memory);
+        object, info.drawing, info.layer_index, memory);
 
     /* Fill line indices. */
     editable_strokes.foreach_index([&](const int curve_i) {

source/blender/editors/grease_pencil/intern/grease_pencil_edit.cc
@@ -93,7 +93,7 @@ static int grease_pencil_stroke_smooth_exec(bContext *C, wmOperator *op)
 
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return;
     }
@@ -255,7 +255,7 @@ static int grease_pencil_stroke_simplify_exec(bContext *C, wmOperator *op)
 
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return;
     }
@@ -461,7 +461,7 @@ static int grease_pencil_delete_exec(bContext *C, wmOperator * /*op*/)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask elements = ed::greasepencil::retrieve_editable_and_selected_elements(
-        *object, info.drawing, selection_domain, memory);
+        *object, info.drawing, info.layer_index, selection_domain, memory);
     if (elements.is_empty()) {
       return;
     }
@@ -608,7 +608,7 @@ static int grease_pencil_dissolve_exec(bContext *C, wmOperator *op)
 
     IndexMaskMemory memory;
     const IndexMask points = ed::greasepencil::retrieve_editable_and_selected_points(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (points.is_empty()) {
       return;
     }
@@ -777,7 +777,7 @@ static int grease_pencil_stroke_material_set_exec(bContext *C, wmOperator *op)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return;
     }
@@ -852,7 +852,7 @@ static int grease_pencil_cyclical_set_exec(bContext *C, wmOperator *op)
 
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return;
     }
@@ -927,7 +927,7 @@ static int grease_pencil_set_active_material_exec(bContext *C, wmOperator * /*op
   for (const MutableDrawingInfo &info : drawings) {
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       continue;
     }
@@ -975,7 +975,7 @@ static int grease_pencil_set_uniform_thickness_exec(bContext *C, wmOperator *op)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return;
     }
@@ -1032,7 +1032,7 @@ static int grease_pencil_set_uniform_opacity_exec(bContext *C, wmOperator *op)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return;
     }
@@ -1087,7 +1087,7 @@ static int grease_pencil_stroke_switch_direction_exec(bContext *C, wmOperator * 
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return;
     }
@@ -1164,7 +1164,7 @@ static int grease_pencil_caps_set_exec(bContext *C, wmOperator *op)
     bke::CurvesGeometry &curves = info.drawing.strokes_for_write();
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return;
     }
@@ -1339,7 +1339,7 @@ static int grease_pencil_duplicate_exec(bContext *C, wmOperator * /*op*/)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask elements = retrieve_editable_and_selected_elements(
-        *object, info.drawing, selection_domain, memory);
+        *object, info.drawing, info.layer_index, selection_domain, memory);
     if (elements.is_empty()) {
       return;
     }
@@ -1391,7 +1391,7 @@ static int grease_pencil_clean_loose_exec(bContext *C, wmOperator *op)
 
     IndexMaskMemory memory;
     const IndexMask editable_strokes = ed::greasepencil::retrieve_editable_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
 
     const IndexMask curves_to_delete = IndexMask::from_predicate(
         editable_strokes, GrainSize(4096), memory, [&](const int i) {
@@ -1460,7 +1460,7 @@ static int gpencil_stroke_subdivide_exec(bContext *C, wmOperator *op)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return;
     }
@@ -1656,7 +1656,7 @@ static int grease_pencil_stroke_reorder_exec(bContext *C, wmOperator *op)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return;
     }
@@ -2004,7 +2004,8 @@ static bool grease_pencil_separate_layer(bContext &C,
     for (const MutableDrawingInfo &info : drawings_src) {
       bke::CurvesGeometry &curves_src = info.drawing.strokes_for_write();
       IndexMaskMemory memory;
-      const IndexMask strokes = retrieve_editable_strokes(object_src, info.drawing, memory);
+      const IndexMask strokes = retrieve_editable_strokes(
+          object_src, info.drawing, info.layer_index, memory);
       if (strokes.is_empty()) {
         continue;
       }
@@ -2514,10 +2515,11 @@ static int grease_pencil_stroke_merge_by_distance_exec(bContext *C, wmOperator *
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     bke::greasepencil::Drawing &drawing = info.drawing;
     IndexMaskMemory memory;
-    const IndexMask points =
-        use_unselected ?
-            ed::greasepencil::retrieve_editable_points(*object, drawing, memory) :
-            ed::greasepencil::retrieve_editable_and_selected_points(*object, drawing, memory);
+    const IndexMask points = use_unselected ?
+                                 ed::greasepencil::retrieve_editable_points(
+                                     *object, drawing, info.layer_index, memory) :
+                                 ed::greasepencil::retrieve_editable_and_selected_points(
+                                     *object, info.drawing, info.layer_index, memory);
     if (points.is_empty()) {
       return;
     }
@@ -2678,7 +2680,7 @@ static int grease_pencil_extrude_exec(bContext *C, wmOperator * /*op*/)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask points_to_extrude = retrieve_editable_and_selected_points(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (points_to_extrude.is_empty()) {
       return;
     }

source/blender/editors/grease_pencil/intern/grease_pencil_material.cc
@@ -278,7 +278,7 @@ static int grease_pencil_material_lock_unselected_exec(bContext *C, wmOperator *
   for (const MutableDrawingInfo &info : drawings) {
     IndexMaskMemory memory;
     const IndexMask strokes = ed::greasepencil::retrieve_editable_and_selected_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (strokes.is_empty()) {
       return OPERATOR_CANCELLED;
     }

source/blender/editors/grease_pencil/intern/grease_pencil_select.cc
@@ -40,7 +40,7 @@ static int select_all_exec(bContext *C, wmOperator *op)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask selectable_elements = retrieve_editable_elements(
-        *object, info.drawing, selection_domain, memory);
+        *object, info, selection_domain, memory);
     if (selectable_elements.is_empty()) {
       return;
     }
@@ -80,7 +80,7 @@ static int select_more_exec(bContext *C, wmOperator * /*op*/)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask selectable_strokes = ed::greasepencil::retrieve_editable_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (selectable_strokes.is_empty()) {
       return;
     }
@@ -118,7 +118,7 @@ static int select_less_exec(bContext *C, wmOperator * /*op*/)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask selectable_strokes = ed::greasepencil::retrieve_editable_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (selectable_strokes.is_empty()) {
       return;
     }
@@ -156,7 +156,7 @@ static int select_linked_exec(bContext *C, wmOperator * /*op*/)
   threading::parallel_for_each(drawings, [&](const MutableDrawingInfo &info) {
     IndexMaskMemory memory;
     const IndexMask selectable_strokes = ed::greasepencil::retrieve_editable_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (selectable_strokes.is_empty()) {
       return;
     }
@@ -199,7 +199,7 @@ static int select_random_exec(bContext *C, wmOperator *op)
 
     IndexMaskMemory memory;
     const IndexMask selectable_elements = retrieve_editable_elements(
-        *object, info.drawing, selection_domain, memory);
+        *object, info, selection_domain, memory);
     if (selectable_elements.is_empty()) {
       return;
     }
@@ -298,7 +298,7 @@ static int select_ends_exec(bContext *C, wmOperator *op)
 
     IndexMaskMemory memory;
     const IndexMask selectable_strokes = ed::greasepencil::retrieve_editable_strokes(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     if (selectable_strokes.is_empty()) {
       return;
     }
@@ -306,7 +306,7 @@ static int select_ends_exec(bContext *C, wmOperator *op)
         curves, selectable_strokes, amount_start, amount_end, true, memory);
 
     const IndexMask selectable_points = ed::greasepencil::retrieve_editable_points(
-        *object, info.drawing, memory);
+        *object, info.drawing, info.layer_index, memory);
     const bool was_anything_selected = ed::curves::has_anything_selected(curves,
                                                                          selectable_points);
     bke::GSpanAttributeWriter selection = ed::curves::ensure_selection_attribute(

source/blender/editors/grease_pencil/intern/grease_pencil_utils.cc
@@ -686,6 +686,7 @@ static VectorSet<int> get_hidden_material_indices(Object &object)
 
 IndexMask retrieve_editable_strokes(Object &object,
                                     const bke::greasepencil::Drawing &drawing,
+                                    int layer_index,
                                     IndexMaskMemory &memory)
 {
   using namespace blender;
@@ -703,6 +704,8 @@ IndexMask retrieve_editable_strokes(Object &object,
   }
 
   const bke::AttributeAccessor attributes = curves.attributes();
+  GreasePencil &grease_pencil = *static_cast<GreasePencil *>(object.data);
+  const bke::greasepencil::Layer &layer = *grease_pencil.layers()[layer_index];
 
   const VArray<int> materials = *attributes.lookup<int>("material_index", bke::AttrDomain::Curve);
   if (!materials) {
@@ -716,7 +719,9 @@ IndexMask retrieve_editable_strokes(Object &object,
   return IndexMask::from_predicate(
       curves_range, GrainSize(4096), memory, [&](const int64_t curve_i) {
         const int material_index = materials[curve_i];
-        return editable_material_indices.contains(material_index);
+        /* The stroke is editable if the material is editable. If the material is not editable,
+         * then the stroke is only editable if the layer disables the locked material option. */
+        return editable_material_indices.contains(material_index) || layer.use_locked_material();
       });
 }
 
@@ -758,6 +763,7 @@ IndexMask retrieve_editable_strokes_by_material(Object &object,
 
 IndexMask retrieve_editable_points(Object &object,
                                    const bke::greasepencil::Drawing &drawing,
+                                   int layer_index,
                                    IndexMaskMemory &memory)
 {
   const bke::CurvesGeometry &curves = drawing.strokes();
@@ -774,6 +780,8 @@ IndexMask retrieve_editable_points(Object &object,
   }
 
   const bke::AttributeAccessor attributes = curves.attributes();
+  GreasePencil &grease_pencil = *static_cast<GreasePencil *>(object.data);
+  const bke::greasepencil::Layer &layer = *grease_pencil.layers()[layer_index];
 
   /* Propagate the material index to the points. */
   const VArray<int> materials = *attributes.lookup<int>("material_index", bke::AttrDomain::Point);
@@ -788,20 +796,24 @@ IndexMask retrieve_editable_points(Object &object,
   return IndexMask::from_predicate(
       points_range, GrainSize(4096), memory, [&](const int64_t point_i) {
         const int material_index = materials[point_i];
-        return editable_material_indices.contains(material_index);
+        /* The stroke is editable if the material is editable. If the material is not editable,
+         * then the stroke is only editable if the layer disables the locked material option. */
+        return editable_material_indices.contains(material_index) || layer.use_locked_material();
       });
 }
 
 IndexMask retrieve_editable_elements(Object &object,
-                                     const bke::greasepencil::Drawing &drawing,
+                                     const MutableDrawingInfo &info,
                                      const bke::AttrDomain selection_domain,
                                      IndexMaskMemory &memory)
 {
+
+  const bke::greasepencil::Drawing &drawing = info.drawing;
   if (selection_domain == bke::AttrDomain::Curve) {
-    return ed::greasepencil::retrieve_editable_strokes(object, drawing, memory);
+    return ed::greasepencil::retrieve_editable_strokes(object, drawing, info.layer_index, memory);
   }
   else if (selection_domain == bke::AttrDomain::Point) {
-    return ed::greasepencil::retrieve_editable_points(object, drawing, memory);
+    return ed::greasepencil::retrieve_editable_points(object, drawing, info.layer_index, memory);
   }
   return {};
 }
@@ -868,13 +880,14 @@ IndexMask retrieve_visible_points(Object &object,
 
 IndexMask retrieve_editable_and_selected_strokes(Object &object,
                                                  const bke::greasepencil::Drawing &drawing,
+                                                 int layer_index,
                                                  IndexMaskMemory &memory)
 {
   using namespace blender;
-
   const bke::CurvesGeometry &curves = drawing.strokes();
 
-  const IndexMask editable_strokes = retrieve_editable_strokes(object, drawing, memory);
+  const IndexMask editable_strokes = retrieve_editable_strokes(
+      object, drawing, layer_index, memory);
   const IndexMask selected_strokes = ed::curves::retrieve_selected_curves(curves, memory);
 
   return IndexMask::from_intersection(editable_strokes, selected_strokes, memory);
@@ -882,11 +895,12 @@ IndexMask retrieve_editable_and_selected_strokes(Object &object,
 
 IndexMask retrieve_editable_and_selected_points(Object &object,
                                                 const bke::greasepencil::Drawing &drawing,
+                                                int layer_index,
                                                 IndexMaskMemory &memory)
 {
   const bke::CurvesGeometry &curves = drawing.strokes();
 
-  const IndexMask editable_points = retrieve_editable_points(object, drawing, memory);
+  const IndexMask editable_points = retrieve_editable_points(object, drawing, layer_index, memory);
   const IndexMask selected_points = ed::curves::retrieve_selected_points(curves, memory);
 
   return IndexMask::from_intersection(editable_points, selected_points, memory);
@@ -894,14 +908,17 @@ IndexMask retrieve_editable_and_selected_points(Object &object,
 
 IndexMask retrieve_editable_and_selected_elements(Object &object,
                                                   const bke::greasepencil::Drawing &drawing,
+                                                  int layer_index,
                                                   const bke::AttrDomain selection_domain,
                                                   IndexMaskMemory &memory)
 {
   if (selection_domain == bke::AttrDomain::Curve) {
-    return ed::greasepencil::retrieve_editable_and_selected_strokes(object, drawing, memory);
+    return ed::greasepencil::retrieve_editable_and_selected_strokes(
+        object, drawing, layer_index, memory);
   }
   else if (selection_domain == bke::AttrDomain::Point) {
-    return ed::greasepencil::retrieve_editable_and_selected_points(object, drawing, memory);
+    return ed::greasepencil::retrieve_editable_and_selected_points(
+        object, drawing, layer_index, memory);
   }
   return {};
 }

source/blender/editors/include/ED_grease_pencil.hh
@@ -277,6 +277,7 @@ Vector<DrawingInfo> retrieve_visible_drawings(const Scene &scene,
 
 IndexMask retrieve_editable_strokes(Object &grease_pencil_object,
                                     const bke::greasepencil::Drawing &drawing,
+                                    int layer_index,
                                     IndexMaskMemory &memory);
 IndexMask retrieve_editable_strokes_by_material(Object &object,
                                                 const bke::greasepencil::Drawing &drawing,
@@ -284,9 +285,10 @@ IndexMask retrieve_editable_strokes_by_material(Object &object,
                                                 IndexMaskMemory &memory);
 IndexMask retrieve_editable_points(Object &object,
                                    const bke::greasepencil::Drawing &drawing,
+                                   int layer_index,
                                    IndexMaskMemory &memory);
 IndexMask retrieve_editable_elements(Object &object,
-                                     const bke::greasepencil::Drawing &drawing,
+                                     const MutableDrawingInfo &info,
                                      bke::AttrDomain selection_domain,
                                      IndexMaskMemory &memory);
 
@@ -299,12 +301,15 @@ IndexMask retrieve_visible_points(Object &object,
 
 IndexMask retrieve_editable_and_selected_strokes(Object &grease_pencil_object,
                                                  const bke::greasepencil::Drawing &drawing,
+                                                 int layer_index,
                                                  IndexMaskMemory &memory);
 IndexMask retrieve_editable_and_selected_points(Object &object,
                                                 const bke::greasepencil::Drawing &drawing,
+                                                int layer_index,
                                                 IndexMaskMemory &memory);
 IndexMask retrieve_editable_and_selected_elements(Object &object,
                                                   const bke::greasepencil::Drawing &drawing,
+                                                  int layer_index,
                                                   bke::AttrDomain selection_domain,
                                                   IndexMaskMemory &memory);
 

commit dd4333a5a6874d0ec98c77ebb9004eac2968320b

GPv3: Console error after opening layer extras panel
Caused by c929816fcf18adbada192e718b1605f27145b76e.
TypeError in console after opening layer extras panel while group is
active.

The fix makes sure `layer` is not `None`.

Pull Request: https://projects.blender.org/blender/blender/pulls/122586

scripts/startup/bl_ui/properties_data_grease_pencil.py
@@ -194,7 +194,9 @@ class GREASE_PENCIL_MT_grease_pencil_add_layer_extra(Menu):
 
         layout.separator()
         layout.prop(grease_pencil, "use_autolock_layers", text="Autolock Inactive Layers")
-        layout.prop(layer, "use_locked_material")
+
+        if layer:
+            layout.prop(layer, "use_locked_material")
 
 
 class GREASE_PENCIL_MT_group_context_menu(Menu):