blender/blender

commit 1597e9386fb6ec5a5fa1c300f5812275ac9ac259

Fix #118602: Build depsgraph on main thread for Alembic
Building the depsgraph from a worker thread is unsafe currently, as it
may trigger some deferred processing on the Main data itself.

Mimic what was already done for USD (from `5cf3654d9d`) and build the
graph before launching `export_startjob`.

Pull Request: https://projects.blender.org/blender/blender/pulls/118974

source/blender/io/alembic/exporter/abc_export_capi.cc
@@ -83,7 +83,6 @@ static void export_startjob(void *customdata, wmJobWorkerStatus *worker_status)
   worker_status->progress = 0.0f;
   worker_status->do_update = true;
 
-  build_depsgraph(data->depsgraph, data->params.visible_objects_only);
   SubdivModifierDisabler subdiv_disabler(data->depsgraph);
   if (!data->params.apply_subdiv) {
     subdiv_disabler.disable_modifiers();
@@ -207,6 +206,12 @@ bool ABC_export(Scene *scene,
 
   job->depsgraph = DEG_graph_new(job->bmain, scene, view_layer, params->evaluation_mode);
   job->params = *params;
+
+  /* Construct the depsgraph for exporting.
+   *
+   * Has to be done from main thread currently, as it may affect Main original data (e.g. when
+   * doing deferred update of the view-layers, see #112534 for details). */
+  blender::io::alembic::build_depsgraph(job->depsgraph, job->params.visible_objects_only);
 
   bool export_ok = false;
   if (as_background_job) {

Bforartists/Bforartists

commit 0ad7df2bbfd5f9f0efa09fe76f0d14d5c9844cda

Fix #122517: Ensure depsgraph is updated before disabling subdiv
Ensure depsgraph is updated both before and after disabling subdiv
modifiers during Alembic export.

Looks like this regressed from 1597e9386fb6ec5a5fa1c300f5812275ac9ac259.

Pull Request: https://projects.blender.org/blender/blender/pulls/122534

source/blender/io/alembic/exporter/abc_export_capi.cc
@@ -94,11 +94,13 @@ static void export_startjob(void *customdata, wmJobWorkerStatus *worker_status)
   worker_status->progress = 0.0f;
   worker_status->do_update = true;
 
+  BKE_scene_graph_update_tagged(data->depsgraph, data->bmain);
+
   SubdivModifierDisabler subdiv_disabler(data->depsgraph);
   if (!data->params.apply_subdiv) {
     subdiv_disabler.disable_modifiers();
+    BKE_scene_graph_update_tagged(data->depsgraph, data->bmain);
   }
-  BKE_scene_graph_update_tagged(data->depsgraph, data->bmain);
 
   /* For restoring the current frame after exporting animation is done. */
   Scene *scene = DEG_get_input_scene(data->depsgraph);