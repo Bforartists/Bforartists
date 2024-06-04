Bforartists/Bforartists

commit 69eeb3dfc9b29d7bfe0fc6de253d5b0bade2fa96

UI: Fade all non-ID children if parent is hidden in outliner
Part of !120397.
For non-id tree elements, call `element_should_draw_faded` and get the
fade status of their parent. For example, this will fix the fading of
modifier, actions, bones, etc. in outliner.

Pull Request: https://projects.blender.org/blender/blender/pulls/120813

source/blender/editors/space_outliner/outliner_draw.cc
@@ -3326,6 +3326,11 @@ static bool element_should_draw_faded(const TreeViewContext *tvc,
           tree_element_cast<TreeElementGreasePencilNode>(te)->node();
       return !node.is_visible();
     }
+    default: {
+      if (te->parent) {
+        return element_should_draw_faded(tvc, te->parent, te->parent->store_elem);
+      }
+    }
   }
 
   if (te->flag & TE_CHILD_NOT_IN_COLLECTION) {

commit 102e69b53daad39a532bf56e9c4d555c6563e700

Fix #122609: Child object dimmed in outliner when parent is hidden
Caused by 69eeb3dfc9b29d7bfe0fc6de253d5b0bade2fa96
Return visibility state of ID_OB instead of early exit from switch case.

Pull Request: https://projects.blender.org/blender/blender/pulls/122653

source/blender/editors/space_outliner/outliner_draw.cc
@@ -3300,10 +3300,8 @@ static bool element_should_draw_faded(const TreeViewContext *tvc,
                                                   (ViewLayer *)tvc->view_layer, (Object *)ob);
         const bool is_visible = (base != nullptr) &&
                                 (base->flag & BASE_ENABLED_AND_VISIBLE_IN_DEFAULT_VIEWPORT);
-        if (!is_visible) {
-          return true;
-        }
-        break;
+
+        return !is_visible;
       }
       default: {
         if (te->parent) {