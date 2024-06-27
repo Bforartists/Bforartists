Bforartists/Bforartists

commit 285429135ca1fc545eb3e9faa6876a7ae7413f7a

Refactor: Get asset data via asset representation, not file
The "active_file" context member as a way to get the active asset should be
avoided, in favor of "asset". This moves us away from the file browser backend
as basis of the asset system,

scripts/startup/bl_operators/assets.py
@@ -40,8 +40,8 @@ class ASSET_OT_tag_add(AssetBrowserMetadataOperator, Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        active_asset = SpaceAssetInfo.get_active_asset(context)
-        active_asset.tags.new(data_("Tag"))
+        active_asset = context.asset
+        active_asset.asset_data.tags.new(data_("Tag"))
 
         return {'FINISHED'}
 

scripts/startup/bl_ui/space_filebrowser.py
@@ -792,7 +792,8 @@ class ASSETBROWSER_PT_metadata_tags(asset_utils.AssetMetaDataPanel, Panel):
 
     def draw(self, context):
         layout = self.layout
-        asset_data = asset_utils.SpaceAssetInfo.get_active_asset(context)
+        active_asset = context.asset
+        asset_data = active_asset.asset_data
 
         row = layout.row()
         row.template_list(

scripts/modules/bpy_extras/asset_utils.py
@@ -25,11 +25,6 @@ class SpaceAssetInfo:
     def is_asset_browser_poll(cls, context: Context):
         return cls.is_asset_browser(context.space_data)
 
-    @classmethod
-    def get_active_asset(cls, context: Context):
-        if active_file := getattr(context, "active_file", None):
-            return active_file.asset_data
-
 
 class AssetBrowserPanel:
     bl_space_type = 'FILE_BROWSER'

commit 833db5bdc90e2c62f77df4992c8334ebac9c1fb1

Fix #123782: Asset Browser does not show tags for active asset
Caused by 285429135ca1

Tags live on the `AssetMetaData` of an `AssetRepresentation` (so
`metadata` in py),
`asset_data`(also pointing to `AssetMetaData`) is a member of
`FileSelectEntry` only [which the above commit tried to get rid off...]

Pull Request: https://projects.blender.org/blender/blender/pulls/123818

scripts/startup/bl_operators/assets.py
@@ -41,7 +41,7 @@ class ASSET_OT_tag_add(AssetBrowserMetadataOperator, Operator):
 
     def execute(self, context):
         active_asset = context.asset
-        active_asset.asset_data.tags.new(data_("Tag"))
+        active_asset.metadata.tags.new(data_("Tag"))
 
         return {'FINISHED'}
 

scripts/startup/bl_ui/space_filebrowser.py
@@ -793,12 +793,12 @@ class ASSETBROWSER_PT_metadata_tags(asset_utils.AssetMetaDataPanel, Panel):
     def draw(self, context):
         layout = self.layout
         active_asset = context.asset
-        asset_data = active_asset.asset_data
+        asset_metadata = active_asset.metadata
 
         row = layout.row()
         row.template_list(
-            "ASSETBROWSER_UL_metadata_tags", "asset_tags", asset_data, "tags",
-            asset_data, "active_tag", rows=4,
+            "ASSETBROWSER_UL_metadata_tags", "asset_tags", asset_metadata, "tags",
+            asset_metadata, "active_tag", rows=4,
         )
 
         col = row.column(align=True)