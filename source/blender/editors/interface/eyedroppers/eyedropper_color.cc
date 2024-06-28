Bforartists/Bforartists

commit d2026ff3d9e5aad9778e018647b1d03862534778

Color management: Support white balance as part of the display transform
This implements a von-Kries-style chromatic adaption using the Bradford matrix.
The adaption is performed in scene linear space in the OCIO GLSL shader, with
the matrix being computed on the host.

The parameters specify the white point of the input, which is to be mapped to
the white point of the scene linear space. The main parameter is temperature,
specified in Kelvin, which defines the blackbody spectrum that is used as the
input white point. Additionally, a tint parameter can be used to shift the
white point away from pure blackbody spectra (e.g. to match a D illuminant).

The defaults are set to match D65 so there is no immediate color shift when
enabling the option. Tint = 10 is needed since the D-series illuminants aren't
perfect blackbody emitters.

As an alternative to manually specifying the values, there's also a color
picker. When a color is selected, temperature and tint are set such that this
color ends up being balanced to white.
This only works if the color is close enough to a blackbody emitter -
specifically, for tint values within +-150. Beyond this, there can be ambiguity
in the representation.
Currently, in this case, the input is just ignored and temperature/tint aren't
changed. Ideally, we'd eventually give UI feedback for this.

Presets are supported, and all the CIE standard illuminants are included.

One part that I'm not quite happy with is that the tint parameter starts to
give weird results at moderate values when the temperature is low.
The reason for this can be seen here:
https://commons.wikimedia.org/wiki/File:Planckian-locus.png
Tint is moving along the isotherm lines (with the plot corresponding to +-150),
but below 4000K some of that range is outside of the gamut. Not much can
be done there, other than possibly clipping those values...

Adding support for this to the compositor should be quite easy and is planned
as a next step.

source/blender/editors/interface/eyedroppers/eyedropper_color.cc
@@ -23,11 +23,14 @@
 #include "BKE_context.hh"
 #include "BKE_cryptomatte.h"
 #include "BKE_image.h"
+#include "BKE_report.hh"
 #include "BKE_screen.hh"
 
 #include "NOD_composite.hh"
 
 #include "RNA_access.hh"
+#include "RNA_define.hh"
+#include "RNA_path.hh"
 #include "RNA_prototypes.h"
 
 #include "UI_interface.hh"
@@ -84,7 +87,29 @@ static bool eyedropper_init(bContext *C, wmOperator *op)
 {
   Eyedropper *eye = MEM_cnew<Eyedropper>(__func__);
 
-  uiBut *but = UI_context_active_but_prop_get(C, &eye->ptr, &eye->prop, &eye->index);
+  PropertyRNA *prop;
+  if ((prop = RNA_struct_find_property(op->ptr, "prop_data_path")) &&
+      RNA_property_is_set(op->ptr, prop))
+  {
+    char *prop_data_path = RNA_string_get_alloc(op->ptr, "prop_data_path", nullptr, 0, nullptr);
+    BLI_SCOPED_DEFER([&] { MEM_SAFE_FREE(prop_data_path); });
+    if (!prop_data_path || prop_data_path[0] == '\0') {
+      MEM_freeN(eye);
+      return false;
+    }
+    PointerRNA ctx_ptr = RNA_pointer_create(nullptr, &RNA_Context, C);
+    if (!RNA_path_resolve(&ctx_ptr, prop_data_path, &eye->ptr, &eye->prop)) {
+      BKE_reportf(op->reports, RPT_ERROR, "Could not resolve path '%s'", prop_data_path);
+      MEM_freeN(eye);
+      return false;
+    }
+    eye->is_undo = true;
+  }
+  else {
+    uiBut *but = UI_context_active_but_prop_get(C, &eye->ptr, &eye->prop, &eye->index);
+    eye->is_undo = UI_but_flag_is_set(but, UI_BUT_UNDO);
+  }
+
   const enum PropertySubType prop_subtype = eye->prop ? RNA_property_subtype(eye->prop) :
                                                         PropertySubType(0);
 
@@ -98,8 +123,6 @@ static bool eyedropper_init(bContext *C, wmOperator *op)
     return false;
   }
   op->customdata = eye;
-
-  eye->is_undo = UI_but_flag_is_set(but, UI_BUT_UNDO);
 
   float col[4];
   RNA_property_float_get_array(&eye->ptr, eye->prop, col);
@@ -585,4 +608,14 @@ void UI_OT_eyedropper_color(wmOperatorType *ot)
 
   /* flags */
   ot->flag = OPTYPE_UNDO | OPTYPE_BLOCKING | OPTYPE_INTERNAL;
+
+  /* Paths relative to the context. */
+  PropertyRNA *prop;
+  prop = RNA_def_string(ot->srna,
+                        "prop_data_path",
+                        nullptr,
+                        0,
+                        "Data Path",
+                        "Path of property to be set with the depth");
+  RNA_def_property_flag(prop, PROP_HIDDEN | PROP_SKIP_SAVE);
 }

intern/opencolorio/fallback_impl.cc
@@ -494,6 +494,9 @@ OCIO_ConstProcessorRcPtr *FallbackImpl::createDisplayProcessor(OCIO_ConstConfigR
                                                                const char * /*look*/,
                                                                const float scale,
                                                                const float exponent,
+                                                               const float /*temperature*/,
+                                                               const float /*tint*/,
+                                                               const bool /*use_white_balance*/,
                                                                const bool inverse)
 {
   FallbackTransform transform;

intern/opencolorio/gpu_shader_display_transform_frag.glsl
@@ -159,8 +159,8 @@ vec4 OCIO_ProcessColor(vec4 col, vec4 col_overlay)
   /* Convert to scene linear (usually a no-op). */
   col = OCIO_to_scene_linear(col);
 
-  /* Apply exposure in scene linear. */
-  col.rgb *= parameters.scale;
+  /* Apply exposure and white balance in scene linear. */
+  col = parameters.scene_linear_matrix * col;
 
   /* Convert to display space. */
   col = OCIO_to_display(col);

intern/opencolorio/ocio_capi.cc
@@ -257,10 +257,22 @@ OCIO_ConstProcessorRcPtr *OCIO_createDisplayProcessor(OCIO_ConstConfigRcPtr *con
                                                       const char *look,
                                                       const float scale,
                                                       const float exponent,
+                                                      const float temperature,
+                                                      const float tint,
+                                                      const bool use_white_balance,
                                                       const bool inverse)
 {
-  return impl->createDisplayProcessor(
-      config, input, view, display, look, scale, exponent, inverse);
+  return impl->createDisplayProcessor(config,
+                                      input,
+                                      view,
+                                      display,
+                                      look,
+                                      scale,
+                                      exponent,
+                                      temperature,
+                                      tint,
+                                      use_white_balance,
+                                      inverse);
 }
 
 OCIO_PackedImageDesc *OCIO_createOCIO_PackedImageDesc(float *data,
@@ -294,9 +306,12 @@ bool OCIO_gpuDisplayShaderBind(OCIO_ConstConfigRcPtr *config,
                                const float scale,
                                const float exponent,
                                const float dither,
+                               const float temperature,
+                               const float tint,
                                const bool use_predivide,
                                const bool use_overlay,
-                               const bool use_hdr)
+                               const bool use_hdr,
+                               const bool use_white_balance)
 {
   return impl->gpuDisplayShaderBind(config,
                                     input,
@@ -307,9 +322,12 @@ bool OCIO_gpuDisplayShaderBind(OCIO_ConstConfigRcPtr *config,
                                     scale,
                                     exponent,
                                     dither,
+                                    temperature,
+                                    tint,
                                     use_predivide,
                                     use_overlay,
-                                    use_hdr);
+                                    use_hdr,
+                                    use_white_balance);
 }
 
 void OCIO_gpuDisplayShaderUnbind()

intern/opencolorio/ocio_capi.h
@@ -175,6 +175,9 @@ OCIO_ConstProcessorRcPtr *OCIO_createDisplayProcessor(OCIO_ConstConfigRcPtr *con
                                                       const char *look,
                                                       const float scale,
                                                       const float exponent,
+                                                      const float temperature,
+                                                      const float tint,
+                                                      const bool use_white_balance,
                                                       const bool inverse);
 
 struct OCIO_PackedImageDesc *OCIO_createOCIO_PackedImageDesc(float *data,
@@ -197,9 +200,12 @@ bool OCIO_gpuDisplayShaderBind(OCIO_ConstConfigRcPtr *config,
                                const float scale,
                                const float exponent,
                                const float dither,
+                               const float temperature,
+                               const float tint,
                                const bool use_predivide,
                                const bool use_overlay,
-                               const bool use_hdr);
+                               const bool use_hdr,
+                               const bool use_white_balance);
 void OCIO_gpuDisplayShaderUnbind(void);
 void OCIO_gpuCacheFree(void);
 

intern/opencolorio/ocio_impl.cc
@@ -22,7 +22,9 @@ using namespace OCIO_NAMESPACE;
 #include "MEM_guardedalloc.h"
 
 #include "BLI_math_color.h"
+#include "BLI_math_color.hh"
 #include "BLI_math_matrix.h"
+#include "BLI_math_matrix.hh"
 
 #include "ocio_impl.h"
 
@@ -36,6 +38,10 @@ using namespace OCIO_NAMESPACE;
 #if defined(_MSC_VER)
 #  define __func__ __FUNCTION__
 #endif
+
+using blender::double4x4;
+using blender::float3;
+using blender::float3x3;
 
 static void OCIO_reportError(const char *err)
 {
@@ -670,15 +676,18 @@ OCIO_ConstProcessorRcPtr *OCIOImpl::createDisplayProcessor(OCIO_ConstConfigRcPtr
                                                            const char *look,
                                                            const float scale,
                                                            const float exponent,
+                                                           const float temperature,
+                                                           const float tint,
+                                                           const bool use_white_balance,
                                                            const bool inverse)
 
 {
   ConstConfigRcPtr config = *(ConstConfigRcPtr *)config_;
   GroupTransformRcPtr group = GroupTransform::Create();
 
-  /* Exposure. */
-  if (scale != 1.0f) {
-    /* Always apply exposure in scene linear. */
+  /* Linear transforms. */
+  if (scale != 1.0f || use_white_balance) {
+    /* Always apply exposure and/or white balance in scene linear. */
     ColorSpaceTransformRcPtr ct = ColorSpaceTransform::Create();
     ct->setSrc(input);
     ct->setDst(ROLE_SCENE_LINEAR);
@@ -689,9 +698,26 @@ OCIO_ConstProcessorRcPtr *OCIOImpl::createDisplayProcessor(OCIO_ConstConfigRcPtr
 
     /* Apply scale. */
     MatrixTransformRcPtr mt = MatrixTransform::Create();
-    const double matrix[16] = {
-        scale, 0.0, 0.0, 0.0, 0.0, scale, 0.0, 0.0, 0.0, 0.0, scale, 0.0, 0.0, 0.0, 0.0, 1.0};
-    mt->setMatrix(matrix);
+    float3x3 matrix = float3x3::identity() * scale;
+
+    /* Apply white balance. */
+    if (use_white_balance) {
+      /* Compute white point of the scene space in XYZ.*/
+      float3x3 xyz_to_scene;
+      configGetXYZtoSceneLinear(config_, xyz_to_scene.ptr());
+      float3x3 scene_to_xyz = blender::math::invert(xyz_to_scene);
+      float3 target = scene_to_xyz * float3(1.0f);
+
+      /* Add operations to the matrix.
+       * Note: Since we're multiplying from the right, the operations here will be performed in
+       * reverse list order (scene-to-XYZ, then adaption, then XYZ-to-scene, then exposure). */
+      matrix *= xyz_to_scene;
+      matrix *= blender::math::chromatic_adaption_matrix(
+          blender::math::whitepoint_from_temp_tint(temperature, tint), target);
+      matrix *= scene_to_xyz;
+    }
+
+    mt->setMatrix(double4x4(blender::math::transpose(matrix)).base_ptr());
     group->appendTransform(mt);
   }
 

intern/opencolorio/ocio_impl.h
@@ -87,6 +87,9 @@ class IOCIOImpl {
                                                            const char *look,
                                                            const float scale,
                                                            const float exponent,
+                                                           const float temperature,
+                                                           const float tint,
+                                                           const bool use_white_balance,
                                                            const bool inverse) = 0;
 
   virtual OCIO_PackedImageDesc *createOCIO_PackedImageDesc(float *data,
@@ -113,9 +116,12 @@ class IOCIOImpl {
                                     const float /*scale*/,
                                     const float /*exponent*/,
                                     const float /*dither*/,
+                                    const float /*temperature*/,
+                                    const float /*tint*/,
                                     const bool /*use_predivide*/,
                                     const bool /*use_overlay*/,
-                                    const bool /*use_hdr*/)
+                                    const bool /*use_hdr*/,
+                                    const bool /*use_white_balance*/)
   {
     return false;
   }
@@ -200,6 +206,9 @@ class FallbackImpl : public IOCIOImpl {
                                                    const char *look,
                                                    const float scale,
                                                    const float exponent,
+                                                   const float temperature,
+                                                   const float tint,
+                                                   const bool use_white_balance,
                                                    const bool inverse);
 
   OCIO_PackedImageDesc *createOCIO_PackedImageDesc(float *data,
@@ -291,6 +300,9 @@ class OCIOImpl : public IOCIOImpl {
                                                    const char *look,
                                                    const float scale,
                                                    const float exponent,
+                                                   const float temperature,
+                                                   const float tint,
+                                                   const bool use_white_balance,
                                                    const bool inverse);
 
   OCIO_PackedImageDesc *createOCIO_PackedImageDesc(float *data,
@@ -313,9 +325,12 @@ class OCIOImpl : public IOCIOImpl {
                             const float scale,
                             const float exponent,
                             const float dither,
+                            const float temperature,
+                            const float tint,
                             const bool use_predivide,
                             const bool use_overlay,
-                            const bool use_hdr);
+                            const bool use_hdr,
+                            const bool use_white_balance);
   void gpuDisplayShaderUnbind(void);
   void gpuCacheFree(void);
 

intern/opencolorio/ocio_impl_glsl.cc
@@ -27,10 +27,16 @@
 
 using namespace OCIO_NAMESPACE;
 
+#include "BLI_math_color.h"
+#include "BLI_math_color.hh"
+#include "BLI_math_matrix.hh"
+
 #include "MEM_guardedalloc.h"
 
 #include "ocio_impl.h"
 #include "ocio_shader_shared.hh"
+
+using blender::float3x3;
 
 /* **** OpenGL drawing routines using GLSL for color space transform ***** */
 
@@ -547,8 +553,8 @@ static void updateGPUCurveMapping(OCIO_GPUCurveMappping &curvemap,
 }
 
 static void updateGPUDisplayParameters(OCIO_GPUShader &shader,
-                                       float scale,
                                        float exponent,
+                                       float4x4 scene_linear_matrix,
                                        float dither,
                                        bool use_predivide,
                                        bool use_overlay,
@@ -560,8 +566,8 @@ static void updateGPUDisplayParameters(OCIO_GPUShader &shader,
     do_update = true;
   }
   OCIO_GPUParameters &data = shader.parameters;
-  if (data.scale != scale) {
-    data.scale = scale;
+  if (data.scene_linear_matrix != scene_linear_matrix) {
+    data.scene_linear_matrix = scene_linear_matrix;
     do_update = true;
   }
   if (data.exponent != exponent) {
@@ -645,20 +651,22 @@ static OCIO_GPUDisplayShader &getGPUDisplayShader(
 
   /* Create Processors.
    *
-   * Scale and exponent are handled outside of OCIO shader so we can handle them
-   * as uniforms at the binding stage. OCIO would otherwise bake them into the
-   * shader code, requiring slow recompiles when interactively adjusting them.
+   * Scale, white balance and exponent are handled outside of OCIO shader so we
+   * can handle them as uniforms at the binding stage. OCIO would otherwise bake
+   * them into the shader code, requiring slow recompiles when interactively
+   * adjusting them.
    *
    * Note that OCIO does have the concept of dynamic properties, however there
    * is no dynamic gamma and exposure is part of more expensive operations only.
    *
-   * Since exposure must happen in scene linear, we use two processors. The input
-   * is usually scene linear already and so that conversion is often a no-op.
+   * Since exposure and white balance must happen in scene linear, we use two
+   * processors. The input is usually scene linear already and so that conversion
+   * is often a no-op.
    */
   OCIO_ConstProcessorRcPtr *processor_to_scene_linear = OCIO_configGetProcessorWithNames(
       config, input, ROLE_SCENE_LINEAR);
   OCIO_ConstProcessorRcPtr *processor_to_display = OCIO_createDisplayProcessor(
-      config, ROLE_SCENE_LINEAR, view, display, look, 1.0f, 1.0f, false);
+      config, ROLE_SCENE_LINEAR, view, display, look, 1.0f, 1.0f, 0.0f, 0.0f, false, false);
 
   /* Create shader descriptions. */
   if (processor_to_scene_linear && processor_to_display) {
@@ -724,9 +732,12 @@ bool OCIOImpl::gpuDisplayShaderBind(OCIO_ConstConfigRcPtr *config,
                                     const float scale,
                                     const float exponent,
                                     const float dither,
+                                    const float temperature,
+                                    const float tint,
                                     const bool use_predivide,
                                     const bool use_overlay,
-                                    const bool use_hdr)
+                                    const bool use_hdr,
+                                    const bool use_white_balance)
 {
   /* Get GPU shader from cache or create new one. */
   OCIO_GPUDisplayShader &display_shader = getGPUDisplayShader(
@@ -761,7 +772,24 @@ bool OCIOImpl::gpuDisplayShaderBind(OCIO_ConstConfigRcPtr *config,
     GPU_uniformbuf_bind(textures.uniforms_buffer, UNIFORMBUF_SLOT_LUTS);
   }
 
-  updateGPUDisplayParameters(shader, scale, exponent, dither, use_predivide, use_overlay, use_hdr);
+  float3x3 matrix = float3x3::identity() * scale;
+  if (use_white_balance) {
+    /* Compute white point of the scene space in XYZ.*/
+    float3x3 xyz_to_scene;
+    configGetXYZtoSceneLinear(config, xyz_to_scene.ptr());
+    float3x3 scene_to_xyz = blender::math::invert(xyz_to_scene);
+    float3 target = scene_to_xyz * float3(1.0f);
+
+    /* Add operations to the matrix.
+     * Note: Since we're multiplying from the right, the operations here will be performed in
+     * reverse list order (scene-to-XYZ, then adaption, then XYZ-to-scene, then exposure). */
+    matrix *= xyz_to_scene;
+    matrix *= blender::math::chromatic_adaption_matrix(
+        blender::math::whitepoint_from_temp_tint(temperature, tint), target);
+    matrix *= scene_to_xyz;
+  }
+  updateGPUDisplayParameters(
+      shader, exponent, float4x4(matrix), dither, use_predivide, use_overlay, use_hdr);
   GPU_uniformbuf_bind(shader.parameters_buffer, UNIFORMBUF_SLOT_DISPLAY);
 
   /* TODO(fclem): remove remains of IMM. */

commit 97ff0589e06b251ce4c90069872b87b6e5e05b6b

Fix #123876: Crash using eyedropper tool
Caused by https://projects.blender.org/blender/blender/commit/d2026ff3d9e5aad9778e018647b1d03862534778.
The code was assuming that `UI_context_active_but_prop_get`
would return a valid button.
This simply checks if the returned value is not `nullptr`.

Pull Request: https://projects.blender.org/blender/blender/pulls/123879

source/blender/editors/interface/eyedroppers/eyedropper_color.cc
@@ -107,7 +107,9 @@ static bool eyedropper_init(bContext *C, wmOperator *op)
   }
   else {
     uiBut *but = UI_context_active_but_prop_get(C, &eye->ptr, &eye->prop, &eye->index);
-    eye->is_undo = UI_but_flag_is_set(but, UI_BUT_UNDO);
+    if (but != nullptr) {
+      eye->is_undo = UI_but_flag_is_set(but, UI_BUT_UNDO);
+    }
   }
 
   const enum PropertySubType prop_subtype = eye->prop ? RNA_property_subtype(eye->prop) :