Bforartists/Bforartists

commit 800ed54f717b45f8248df32d6717dd50030d49ba

Fix #123543: Delete retiming keys deletes strip
Delete operator was shared for strips and retiming keys, which was not
ideal. Operator logic relied on invoke function setting proper context
which was also hidden from users.

Split these operators and make dedicated operators for removing retiming
keys - `SEQUENCER_OT_retiming_key_delete`.
This operator returns `OPERATOR_CANCELLED | OPERATOR_PASS_THROUGH` if
executed in non-retiming context, or no key is selected.

Pull Request: https://projects.blender.org/blender/blender/pulls/123767

source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -1663,10 +1663,6 @@ static int sequencer_delete_exec(bContext *C, wmOperator *op)
     return OPERATOR_CANCELLED;
   }
 
-  if (RNA_boolean_get(op->ptr, "use_retiming_mode")) {
-    sequencer_retiming_key_remove_exec(C, op);
-  }
-
   SEQ_prefetch_stop(scene);
 
   for (Sequence *seq : ED_sequencer_selected_strips_from_context(C)) {
@@ -1699,20 +1695,16 @@ static int sequencer_delete_invoke(bContext *C, wmOperator *op, const wmEvent *e
     }
   }
 
-  if (sequencer_retiming_mode_is_active(C)) {
-    RNA_boolean_set(op->ptr, "use_retiming_mode", true);
-  }
-
   return sequencer_delete_exec(C, op);
 }
 
 static bool sequencer_delete_poll_property(const bContext * /* C */,
-                                           wmOperator *op,
+                                           wmOperator * /*op*/,
                                            const PropertyRNA *prop)
 {
   const char *prop_id = RNA_property_identifier(prop);
 
-  if (STREQ(prop_id, "delete_data") && RNA_boolean_get(op->ptr, "use_retiming_mode")) {
+  if (STREQ(prop_id, "delete_data")) {
     return false;
   }
 
@@ -1743,13 +1735,6 @@ void SEQUENCER_OT_delete(wmOperatorType *ot)
                              "Delete Data",
                              "After removing the Strip, delete the associated data also");
   RNA_def_property_flag(ot->prop, PROP_SKIP_SAVE);
-
-  ot->prop = RNA_def_boolean(ot->srna,
-                             "use_retiming_mode",
-                             false,
-                             "Use Retiming Data",
-                             "Operate on retiming data instead of strips");
-  RNA_def_property_flag(ot->prop, PROP_HIDDEN);
 }
 
 /** \} */

scripts/presets/keyconfig/keymap_data/blender_default.py
@@ -2967,6 +2967,8 @@ def km_sequencer(params):
          {"properties": [("adjust_length", True)]}),
         ("sequencer.offset_clear", {"type": 'O', "value": 'PRESS', "alt": True}, None),
         ("sequencer.duplicate_move", {"type": 'D', "value": 'PRESS', "shift": True}, None),
+        ("sequencer.retiming_key_delete", {"type": 'X', "value": 'PRESS'}, None),
+        ("sequencer.retiming_key_delete", {"type": 'DEL', "value": 'PRESS'}, None),
         ("sequencer.delete", {"type": 'X', "value": 'PRESS'}, None),
         ("sequencer.delete", {"type": 'DEL', "value": 'PRESS'}, None),
         ("sequencer.copy", {"type": 'C', "value": 'PRESS', "ctrl": True}, None),

scripts/presets/keyconfig/keymap_data/industry_compatible_data.py
@@ -1798,6 +1798,8 @@ def km_sequencer(params):
          {"properties": [("adjust_length", True)]}),
         ("sequencer.offset_clear", {"type": 'O', "value": 'PRESS', "alt": True}, None),
         ("sequencer.duplicate_move", {"type": 'D', "value": 'PRESS', "ctrl": True}, None),
+        ("sequencer.retiming_key_delete", {"type": 'BACK_SPACE', "value": 'PRESS'}, None),
+        ("sequencer.retiming_key_delete", {"type": 'DEL', "value": 'PRESS'}, None),
         ("sequencer.delete", {"type": 'BACK_SPACE', "value": 'PRESS'}, None),
         ("sequencer.delete", {"type": 'DEL', "value": 'PRESS'}, None),
         ("sequencer.copy", {"type": 'C', "value": 'PRESS', "ctrl": True}, None),

scripts/startup/bl_ui/space_sequencer.py
@@ -967,7 +967,7 @@ class SEQUENCER_MT_strip_retiming(Menu):
 
         layout.separator()
 
-        layout.operator("sequencer.delete", text="Delete Retiming Keys")
+        layout.operator("sequencer.retiming_key_delete")
         col = layout.column()
         col.operator("sequencer.retiming_reset")
         col.enabled = not is_retiming

source/blender/editors/space_sequencer/sequencer_intern.hh
@@ -319,12 +319,12 @@ void SEQUENCER_OT_retiming_show(wmOperatorType *ot);
 void SEQUENCER_OT_retiming_key_add(wmOperatorType *ot);
 void SEQUENCER_OT_retiming_freeze_frame_add(wmOperatorType *ot);
 void SEQUENCER_OT_retiming_transition_add(wmOperatorType *ot);
+void SEQUENCER_OT_retiming_key_delete(wmOperatorType *ot);
 void SEQUENCER_OT_retiming_segment_speed_set(wmOperatorType *ot);
 int sequencer_retiming_key_select_exec(bContext *C, wmOperator *op);
 /* Select a key and all following keys. */
 int sequencer_retiming_select_linked_time(bContext *C, wmOperator *op);
 int sequencer_select_exec(bContext *C, wmOperator *op);
-int sequencer_retiming_key_remove_exec(bContext *C, wmOperator *op);
 int sequencer_retiming_select_all_exec(bContext *C, wmOperator *op);
 int sequencer_retiming_box_select_exec(bContext *C, wmOperator *op);
 

source/blender/editors/space_sequencer/sequencer_ops.cc
@@ -76,6 +76,7 @@ void sequencer_operatortypes()
   WM_operatortype_append(SEQUENCER_OT_retiming_freeze_frame_add);
   WM_operatortype_append(SEQUENCER_OT_retiming_transition_add);
   WM_operatortype_append(SEQUENCER_OT_retiming_segment_speed_set);
+  WM_operatortype_append(SEQUENCER_OT_retiming_key_delete);
 
   /* `sequencer_select.cc` */
   WM_operatortype_append(SEQUENCER_OT_select_all);

source/blender/editors/space_sequencer/sequencer_retiming.cc
@@ -526,6 +526,83 @@ void SEQUENCER_OT_retiming_transition_add(wmOperatorType *ot)
 /** \} */
 
 /* -------------------------------------------------------------------- */
+/** \name Retiming Delete Key
+ * \{ */
+
+static int sequencer_retiming_key_delete_exec(bContext *C, wmOperator * /*op*/)
+{
+  Scene *scene = CTX_data_scene(C);
+
+  blender::Map selection = SEQ_retiming_selection_get(SEQ_editing_get(scene));
+  blender::Vector<Sequence *> strips_to_handle;
+
+  if (!sequencer_retiming_mode_is_active(C) || selection.size() == 0) {
+    return OPERATOR_CANCELLED | OPERATOR_PASS_THROUGH;
+  }
+
+  for (Sequence *seq : selection.values()) {
+    strips_to_handle.append_non_duplicates(seq);
+  }
+
+  for (Sequence *seq : strips_to_handle) {
+    blender::Vector<SeqRetimingKey *> keys_to_delete;
+    for (auto item : selection.items()) {
+      if (item.value != seq) {
+        continue;
+      }
+      keys_to_delete.append(item.key);
+    }
+
+    SEQ_retiming_remove_multiple_keys(seq, keys_to_delete);
+  }
+
+  for (Sequence *seq : strips_to_handle) {
+    SEQ_relations_invalidate_cache_raw(scene, seq);
+  }
+
+  WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER, scene);
+  return OPERATOR_FINISHED;
+}
+
+static int sequencer_retiming_key_delete_invoke(bContext *C, wmOperator *op, const wmEvent *event)
+{
+  Scene *scene = CTX_data_scene(C);
+  ListBase *markers = &scene->markers;
+
+  if (!BLI_listbase_is_empty(markers)) {
+    ARegion *region = CTX_wm_region(C);
+    if (region && (region->regiontype == RGN_TYPE_WINDOW)) {
+      /* Bounding box of 30 pixels is used for markers shortcuts,
+       * prevent conflict with markers shortcuts here. */
+      if (event->mval[1] <= 30) {
+        return OPERATOR_PASS_THROUGH;
+      }
+    }
+  }
+
+  return sequencer_retiming_key_delete_exec(C, op);
+}
+
+void SEQUENCER_OT_retiming_key_delete(wmOperatorType *ot)
+{
+
+  /* Identifiers. */
+  ot->name = "Delete Retiming Keys";
+  ot->idname = "SEQUENCER_OT_retiming_key_delete";
+  ot->description = "Delete selected strips from the sequencer";
+
+  /* Api callbacks. */
+  ot->invoke = sequencer_retiming_key_delete_invoke;
+  ot->exec = sequencer_retiming_key_delete_exec;
+  ot->poll = retiming_poll;
+
+  /* Flags. */
+  ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
+}
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
 /** \name Retiming Set Segment Speed
  * \{ */
 
@@ -914,37 +991,6 @@ int sequencer_retiming_select_all_exec(bContext *C, wmOperator *op)
           break;
       }
     }
-  }
-
-  WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER, scene);
-  return OPERATOR_FINISHED;
-}
-
-int sequencer_retiming_key_remove_exec(bContext *C, wmOperator * /* op */)
-{
-  Scene *scene = CTX_data_scene(C);
-
-  blender::Map selection = SEQ_retiming_selection_get(SEQ_editing_get(scene));
-  blender::Vector<Sequence *> strips_to_handle;
-
-  for (Sequence *seq : selection.values()) {
-    strips_to_handle.append_non_duplicates(seq);
-  }
-
-  for (Sequence *seq : strips_to_handle) {
-    blender::Vector<SeqRetimingKey *> keys_to_delete;
-    for (auto item : selection.items()) {
-      if (item.value != seq) {
-        continue;
-      }
-      keys_to_delete.append(item.key);
-    }
-
-    SEQ_retiming_remove_multiple_keys(seq, keys_to_delete);
-  }
-
-  for (Sequence *seq : strips_to_handle) {
-    SEQ_relations_invalidate_cache_raw(scene, seq);
   }
 
   WM_event_add_notifier(C, NC_SCENE | ND_SEQUENCER, scene);

commit 8c84637e7d86348ac9a7975ff29f06e751a8477d

Fix #124090: Delete strip operator missing redo panel property
Caused by 800ed54f717b45f8248df32d6717dd50030d49ba
`poll_property` callback is not required, both delete strip and retiming
key operator are separated now.

Pull Request: https://projects.blender.org/blender/blender/pulls/124095

source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -1698,19 +1698,6 @@ static int sequencer_delete_invoke(bContext *C, wmOperator *op, const wmEvent *e
   return sequencer_delete_exec(C, op);
 }
 
-static bool sequencer_delete_poll_property(const bContext * /* C */,
-                                           wmOperator * /*op*/,
-                                           const PropertyRNA *prop)
-{
-  const char *prop_id = RNA_property_identifier(prop);
-
-  if (STREQ(prop_id, "delete_data")) {
-    return false;
-  }
-
-  return true;
-}
-
 void SEQUENCER_OT_delete(wmOperatorType *ot)
 {
 
@@ -1723,7 +1710,6 @@ void SEQUENCER_OT_delete(wmOperatorType *ot)
   ot->invoke = sequencer_delete_invoke;
   ot->exec = sequencer_delete_exec;
   ot->poll = sequencer_edit_poll;
-  ot->poll_property = sequencer_delete_poll_property;
 
   /* Flags. */
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;