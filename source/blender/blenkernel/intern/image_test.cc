Bforartists/Bforartists

commit 210f1f53dc75ac79b1c0a70ee04bbc854bab1e81

Fix #101227: Crash and other issues with non-multiview multipart EXR
Some software stores passes or layers as parts. This case was not
supported by the OpenEXR reader yet.

Pull Request: https://projects.blender.org/blender/blender/pulls/118867

source/blender/imbuf/intern/openexr/openexr_api.cpp
@@ -1430,13 +1430,33 @@ static int imb_exr_split_token(const char *str, const char *end, const char **to
   return int(end - *token);
 }
 
+static void imb_exr_pass_name_from_channel(char *passname,
+                                           const ExrChannel *echan,
+                                           const char *channelname,
+                                           const bool has_xyz_channels)
+{
+  const int passname_maxncpy = EXR_TOT_MAXNAME;
+
+  if (echan->chan_id == 'Z' && (!has_xyz_channels || BLI_strcaseeq(channelname, "depth"))) {
+    BLI_strncpy(passname, "Depth", passname_maxncpy);
+  }
+  else if (echan->chan_id == 'Y' && !has_xyz_channels) {
+    BLI_strncpy(passname, channelname, passname_maxncpy);
+  }
+  else if (ELEM(echan->chan_id, 'R', 'G', 'B', 'A', 'V', 'X', 'Y', 'Z')) {
+    BLI_strncpy(passname, "Combined", passname_maxncpy);
+  }
+  else {
+    BLI_strncpy(passname, channelname, passname_maxncpy);
+  }
+}
+
 static int imb_exr_split_channel_name(ExrChannel *echan,
                                       char *layname,
                                       char *passname,
                                       bool has_xyz_channels)
 {
   const int layname_maxncpy = EXR_TOT_MAXNAME;
-  const int passname_maxncpy = EXR_TOT_MAXNAME;
   const char *name = echan->m->name.c_str();
   const char *end = name + strlen(name);
   const char *token;
@@ -1451,20 +1471,7 @@ static int imb_exr_split_channel_name(ExrChannel *echan,
      * versions of the listed channels. */
     echan->chan_id = BLI_toupper_ascii(name[0]);
     layname[0] = '\0';
-
-    if (echan->chan_id == 'Z' && !has_xyz_channels) {
-      BLI_strncpy(passname, "Depth", passname_maxncpy);
-    }
-    else if (echan->chan_id == 'Y' && !has_xyz_channels) {
-      BLI_strncpy(passname, name, passname_maxncpy);
-    }
-    else if (ELEM(echan->chan_id, 'R', 'G', 'B', 'A', 'V', 'X', 'Y', 'Z')) {
-      BLI_strncpy(passname, "Combined", passname_maxncpy);
-    }
-    else {
-      BLI_strncpy(passname, name, passname_maxncpy);
-    }
-
+    imb_exr_pass_name_from_channel(passname, echan, name, has_xyz_channels);
     return 1;
   }
 
@@ -1520,14 +1527,20 @@ static int imb_exr_split_channel_name(ExrChannel *echan,
   }
   end -= len + 1; /* +1 to skip '.' separator */
 
-  /* second token is pass name */
-  len = imb_exr_split_token(name, end, &token);
-  if (len == 0) {
-    printf("multilayer read: bad channel name: %s\n", name);
-    return 0;
+  if (end > name) {
+    /* second token is pass name */
+    len = imb_exr_split_token(name, end, &token);
+    if (len == 0) {
+      printf("multilayer read: bad channel name: %s\n", name);
+      return 0;
+    }
+    BLI_strncpy(passname, token, len + 1);
+    end -= len + 1; /* +1 to skip '.' separator */
   }
-  BLI_strncpy(passname, token, len + 1);
-  end -= len + 1; /* +1 to skip '.' separator */
+  else {
+    /* Single token, determine pass name from channel name. */
+    imb_exr_pass_name_from_channel(passname, echan, channelname, has_xyz_channels);
+  }
 
   /* all preceding tokens combined as layer name */
   if (end > name) {
@@ -1593,10 +1606,65 @@ static bool exr_has_xyz_channels(ExrHandle *exr_handle)
   return x_found && y_found && z_found;
 }
 
-static bool imb_exr_multilayer_parse_channels_from_file(ExrHandle *data)
+/* Replacement for OpenEXR GetChannelsInMultiPartFile, that also handles the
+ * case where parts are used for passes instead of multiview. */
+static std::vector<MultiViewChannelName> exr_channels_in_multi_part_file(
+    const MultiPartInputFile &file)
 {
   std::vector<MultiViewChannelName> channels;
-  GetChannelsInMultiPartFile(*data->ifile, channels);
+
+  /* Detect if file has multiview. */
+  StringVector multiview;
+  bool has_multiview = false;
+  if (file.parts() == 1) {
+    if (hasMultiView(file.header(0))) {
+      multiview = multiView(file.header(0));
+      has_multiview = true;
+    }
+  }
+
+  /* Get channels from each part. */
+  for (int p = 0; p < file.parts(); p++) {
+    const ChannelList &c = file.header(p).channels();
+
+    std::string part_view = "";
+    if (file.header(p).hasView()) {
+      part_view = file.header(p).view();
+    }
+    std::string part_name = "";
+    if (file.header(p).hasName()) {
+      part_name = file.header(p).name();
+    }
+
+    for (ChannelList::ConstIterator i = c.begin(); i != c.end(); i++) {
+      MultiViewChannelName m;
+      m.name = std::string(i.name());
+      m.internal_name = m.name;
+
+      if (has_multiview) {
+        m.view = viewFromChannelName(m.name, multiview);
+        m.name = removeViewName(m.internal_name, m.view);
+      }
+      else {
+        m.view = part_view;
+      }
+
+      /* Prepend part name as potential layer or pass name. */
+      if (!part_name.empty()) {
+        m.name = part_name + "." + m.name;
+      }
+
+      m.part_number = p;
+      channels.push_back(m);
+    }
+  }
+
+  return channels;
+}
+
+static bool imb_exr_multilayer_parse_channels_from_file(ExrHandle *data)
+{
+  std::vector<MultiViewChannelName> channels = exr_channels_in_multi_part_file(*data->ifile);
 
   imb_exr_get_views(*data->ifile, *data->multiView);
 

commit d9ae8cbc6584e7eda6ac50ffec04faa8c8d80616

Fix #124217: Crashes with certain multi-layer/multi-part EXRs
Caused by 210f1f53dc

This change restores the old behavior of pass name detection from
channel name prior to the offending commit.

The fix includes regression test based on the files from related
reports, to help catching possible issues in the future.

Being so close to the release this commit restored behavior prior
to the previous fix. Potentially this makes some files to detect
wrong pass name for some specific files, although it is not really
clear if such files exists in the wild.

Pull Request: https://projects.blender.org/blender/blender/pulls/124458

source/blender/imbuf/intern/openexr/openexr_api.cpp
@@ -1450,6 +1450,21 @@ static void imb_exr_pass_name_from_channel(char *passname,
   }
 }
 
+static void imb_exr_pass_name_from_channel_name(char *passname,
+                                                const ExrChannel * /*echan*/,
+                                                const char *channelname,
+                                                const bool /*has_xyz_channels*/)
+{
+  const int passname_maxncpy = EXR_TOT_MAXNAME;
+
+  /* TODO: Are special tricks similar to imb_exr_pass_name_from_channel() needed here?
+   * Note that unknown passes are default to chan_id='X'. The place where this function is called
+   * is when the channel name is more than 1 character, so perhaps using just channel ID is not
+   * fully correct here. */
+
+  BLI_strncpy(passname, channelname, passname_maxncpy);
+}
+
 static int imb_exr_split_channel_name(ExrChannel *echan,
                                       char *layname,
                                       char *passname,
@@ -1538,7 +1553,7 @@ static int imb_exr_split_channel_name(ExrChannel *echan,
   }
   else {
     /* Single token, determine pass name from channel name. */
-    imb_exr_pass_name_from_channel(passname, echan, channelname, has_xyz_channels);
+    imb_exr_pass_name_from_channel_name(passname, echan, channelname, has_xyz_channels);
   }
 
   /* all preceding tokens combined as layer name */

source/blender/blenkernel/intern/image_test.cc
@@ -2,16 +2,34 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "BLI_listbase.h"
 #include "BLI_path_util.h"
 #include "BLI_string.h"
+#include "BLI_string_ref.hh"
+#include "BLI_vector.hh"
 
+#include "BKE_global.hh"
+#include "BKE_idtype.hh"
 #include "BKE_image.h"
+#include "BKE_main.hh"
 
 #include "MEM_guardedalloc.h"
 
 #include "testing/testing.h"
+#include "gmock/gmock.h"
+
+#include "IMB_moviecache.hh"
+
+#include "DNA_image_types.h"
+
+#include "RE_pipeline.h"
+
+#include "CLG_log.h"
 
 namespace blender::bke::tests {
+
+using testing::Eq;
+using testing::Pointwise;
 
 TEST(udim, image_ensure_tile_token)
 {
@@ -194,6 +212,171 @@ TEST(udim, image_set_filepath_from_tile_number)
   BKE_image_set_filepath_from_tile_number(filepath, udim_pattern, tile_format, 1028);
   EXPECT_STREQ(filepath, "test.u8_v3.png");
   MEM_freeN(udim_pattern);
+}
+
+class ImageTest : public ::testing::Test {
+  Main *bmain_ = nullptr;
+
+  RenderResult *get_image_render_result(Image &image)
+  {
+    ImageUser iuser{};
+    BKE_imageuser_default(&iuser);
+
+    ImBuf *temp_ibuf = BKE_image_acquire_ibuf(&image, &iuser, nullptr);
+    BKE_image_release_ibuf(&image, temp_ibuf, nullptr);
+
+    return image.rr;
+  }
+
+ protected:
+  static void SetUpTestSuite()
+  {
+    CLG_init();
+    BKE_idtype_init();
+  }
+
+  static void TearDownTestSuite()
+  {
+    CLG_exit();
+  }
+
+  void SetUp() override
+  {
+    IMB_moviecache_init();
+
+    bmain_ = BKE_main_new();
+    G_MAIN = bmain_;
+  }
+
+  void TearDown() override
+  {
+    BKE_main_free(bmain_);
+    G_MAIN = nullptr;
+
+    IMB_moviecache_destruct();
+  }
+
+  Image *load_image(const char *path)
+  {
+    const std::string asset_dir = blender::tests::flags_test_asset_dir().c_str();
+    return BKE_image_load(bmain_, (asset_dir + SEP_STR + "imbuf_io" + SEP_STR + path).c_str());
+  }
+
+  Vector<std::string> get_image_layer_names(Image &image)
+  {
+    RenderResult *render_result = get_image_render_result(image);
+    if (!render_result) {
+      ADD_FAILURE() << "Missing image RenderResult";
+      return {};
+    }
+
+    Vector<std::string> layer_names;
+    LISTBASE_FOREACH (const RenderLayer *, layer, &render_result->layers) {
+      layer_names.append(layer->name);
+    }
+
+    return layer_names;
+  }
+
+  Vector<std::string> get_image_pass_names_for_layer(Image &image, StringRefNull layer_name)
+  {
+    RenderResult *render_result = get_image_render_result(image);
+    if (!render_result) {
+      ADD_FAILURE() << "Missing image RenderResult";
+      return {};
+    }
+
+    LISTBASE_FOREACH (const RenderLayer *, layer, &render_result->layers) {
+      if (layer->name == layer_name) {
+        Vector<std::string> pass_names;
+        LISTBASE_FOREACH (const RenderPass *, pass, &layer->passes) {
+          pass_names.append(pass->name);
+        }
+        return pass_names;
+      }
+    }
+
+    return {};
+  }
+};
+
+TEST_F(ImageTest, multilayer)
+{
+  /* Multi-layer file from another DCC originally reported as #108980.
+   * The expected passes are obtained from Blender 4.2 Beta f069692caf8, with the
+   * !118867 reverted. File Scene_RenderLayer_000.exr from the report was used. */
+  {
+    Image *image = load_image("multilayer" SEP_STR "108980.exr");
+    ASSERT_NE(image, nullptr);
+
+    EXPECT_THAT(get_image_layer_names(*image), Pointwise(Eq(), {""}));
+    EXPECT_THAT(get_image_pass_names_for_layer(*image, ""),
+                Pointwise(Eq(),
+                          {"Combined",
+                           "Albedo",
+                           "Nsx",
+                           "Nsy",
+                           "Nsz",
+                           "Nx",
+                           "Ny",
+                           "Nz",
+                           "Px",
+                           "Py",
+                           "Pz",
+                           "RelativeVariance",
+                           "Variance",
+                           "dzdx",
+                           "dzdy",
+                           "u"}));
+  }
+
+  /* Multi-layer file from another DCC originally reported as #124217.
+   * The expected passes are obtained from Blender 4.2 Beta f069692caf8, with the
+   * !118867 reverted. File test.exr from the report was used. */
+  {
+    Image *image = load_image("multilayer" SEP_STR "124217.exr");
+    ASSERT_NE(image, nullptr);
+
+    EXPECT_THAT(get_image_layer_names(*image), Pointwise(Eq(), {""}));
+    EXPECT_THAT(get_image_pass_names_for_layer(*image, ""),
+                Pointwise(Eq(),
+                          {"Combined",
+                           "Depth",
+                           "AO",
+                           "ID",
+                           "crypto_material",
+                           "crypto_material00",
+                           "crypto_material01",
+                           "crypto_material02",
+                           "crypto_material03",
+                           "crypto_material04",
+                           "crypto_material05",
+                           "crypto_material06",
+                           "crypto_object",
+                           "crypto_object00",
+                           "crypto_object01",
+                           "crypto_object02",
+                           "crypto_object03",
+                           "crypto_object04",
+                           "crypto_object05",
+                           "crypto_object06",
+                           "diffuse",
+                           "opacity",
+                           "specular",
+                           "v"}));
+  }
+
+  /* Multi-part file from another DCC, originally reported as #101227.
+   * The expected passes are obtained from Blender 4.2 Beta f069692caf8, with the
+   * !118867 landed. */
+  {
+    Image *image = load_image("multilayer" SEP_STR "101227.exr");
+    ASSERT_NE(image, nullptr);
+
+    EXPECT_THAT(get_image_layer_names(*image), Pointwise(Eq(), {""}));
+    EXPECT_THAT(get_image_pass_names_for_layer(*image, ""),
+                Pointwise(Eq(), {"C", "N", "albedo", "depth"}));
+  }
 }
 
 }  // namespace blender::bke::tests