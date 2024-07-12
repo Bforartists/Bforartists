Bforartists/Bforartists

commit 35a51b921358be43dc18136574603a2d3ee73f4d

Fix: EEVEE: Hardware discrepancy with math wrap function
The math render tests were not passing on the AMD hardware.
This was due to some compiler behavior not returning 1
on the `floor((a - c) / (b - c))` calculation even if
`a` and `b` were equal.

source/blender/gpu/shaders/common/gpu_shader_common_math_utils.glsl
@@ -78,7 +78,9 @@ float fallback_pow(float x, float y, float fallback)
 float wrap(float a, float b, float c)
 {
   float range = b - c;
-  return (range != 0.0) ? a - (range * floor((a - c) / range)) : c;
+  /* Avoid discrepancy on some hardware due to floating point accuracy and fast math. */
+  float s = (a != b) ? floor((a - c) / range) : 1.0;
+  return (range == 0.0) ? a - range * s : c;
 }
 
 vec3 wrap(vec3 a, vec3 b, vec3 c)

commit 7ced9050594278783141efe624ed66d03cd84c8b

Fix #124530: EEVEE: Math wrap function not working
Due to incorrect check the result was always returning the min
parameter.

Found issue by comparing the implementation with cycles.
Regression introduced by 35a51b921358be43dc18136574603a2d3ee73f4d

Pull Request: https://projects.blender.org/blender/blender/pulls/124604

source/blender/gpu/shaders/common/gpu_shader_common_math_utils.glsl
@@ -80,7 +80,7 @@ float wrap(float a, float b, float c)
   float range = b - c;
   /* Avoid discrepancy on some hardware due to floating point accuracy and fast math. */
   float s = (a != b) ? floor((a - c) / range) : 1.0;
-  return (range == 0.0) ? a - range * s : c;
+  return (range != 0.0) ? a - range * s : c;
 }
 
 vec3 wrap(vec3 a, vec3 b, vec3 c)