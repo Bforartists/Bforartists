Bforartists/Bforartists

commit 8d5d17a7c811f792d72d8d5228e9ae3c870e50f7

Fix: correct potentially wrongly initialized curve attributes
The `resolution` and `nurbs_order` attributes of curves are expected to be `>=
1`. Due to a bug, it was possible that they are initialized to zero: #124534.
This fix adds versioning code to fix the curves that have been written
incorrectly.

Pull Request: https://projects.blender.org/blender/blender/pulls/124539

source/blender/blenloader/intern/versioning_400.cc
@@ -2715,6 +2715,31 @@ static void add_image_editor_asset_shelf(Main &bmain)
   }
 }
 
+/**
+ * It was possible that curve attributes were initialized to 0 even if that is not allowed for some
+ * attributes.
+ */
+static void fix_built_in_curve_attribute_defaults(Main *bmain)
+{
+  LISTBASE_FOREACH (Curves *, curves, &bmain->hair_curves) {
+    const int curves_num = curves->geometry.curve_num;
+    if (int *resolutions = static_cast<int *>(CustomData_get_layer_named_for_write(
+            &curves->geometry.curve_data, CD_PROP_INT32, "resolution", curves_num)))
+    {
+      for (int &resolution : blender::MutableSpan{resolutions, curves_num}) {
+        resolution = std::max(resolution, 1);
+      }
+    }
+    if (int *nurb_orders = static_cast<int *>(CustomData_get_layer_named_for_write(
+            &curves->geometry.curve_data, CD_PROP_INT8, "nurbs_order", curves_num)))
+    {
+      for (int &nurbs_order : blender::MutableSpan{nurb_orders, curves_num}) {
+        nurbs_order = std::max(nurbs_order, 1);
+      }
+    }
+  }
+}
+
 void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
 {
   if (!MAIN_VERSION_FILE_ATLEAST(bmain, 400, 1)) {
@@ -4284,6 +4309,10 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
                                          MA_SURFACE_METHOD_DEFERRED;
       }
     }
+  }
+
+  if (!MAIN_VERSION_FILE_ATLEAST(bmain, 402, 66)) {
+    fix_built_in_curve_attribute_defaults(bmain);
   }
 
   /**

source/blender/blenkernel/BKE_blender_version.h
@@ -29,7 +29,7 @@ extern "C" {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 65
+#define BLENDER_FILE_SUBVERSION 66
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

commit 01de5f699c00b96af6a16f8682fbf7e830142c83

Fix: wrong type cast in previous commit
This was broken in 8d5d17a7c811f792d72d8d5228e9ae3c870e50f7.

source/blender/blenloader/intern/versioning_400.cc
@@ -2730,11 +2730,11 @@ static void fix_built_in_curve_attribute_defaults(Main *bmain)
         resolution = std::max(resolution, 1);
       }
     }
-    if (int *nurb_orders = static_cast<int *>(CustomData_get_layer_named_for_write(
+    if (int8_t *nurb_orders = static_cast<int8_t *>(CustomData_get_layer_named_for_write(
             &curves->geometry.curve_data, CD_PROP_INT8, "nurbs_order", curves_num)))
     {
-      for (int &nurbs_order : blender::MutableSpan{nurb_orders, curves_num}) {
-        nurbs_order = std::max(nurbs_order, 1);
+      for (int8_t &nurbs_order : blender::MutableSpan{nurb_orders, curves_num}) {
+        nurbs_order = std::max<int8_t>(nurbs_order, 1);
       }
     }
   }