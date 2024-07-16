Bforartists/Bforartists

commit 60e53ba4f375f955ba127179de8688ac036ab799

Geometry Nodes: support attaching gizmos to input values
This adds support for attaching gizmos for input values. The goal is to make it
easier for users to set input values intuitively in the 3D viewport.

We went through multiple different possible designs until we settled on the one
implemented here. We picked it for it's flexibility and ease of use when using
geometry node assets. The core principle in the design is that **gizmos are
attached to existing input values instead of being the input value themselves**.
This actually fits the existing concept of gizmos in Blender well, but may be a
bit unintutitive in a node setup at first. The attachment is done using links in
the node editor.

The most basic usage of the node is to link a Value node to the new Linear Gizmo
node. This attaches the gizmo to the input value and allows you to change it
from the 3D view. The attachment is indicated by the gizmo icon in the sockets
which are controlled by a gizmo as well as the back-link (notice the double
link) when the gizmo is active.

The core principle makes it straight forward to control the same node setup from
the 3D view with gizmos, or by manually changing input values, or by driving the
input values procedurally.

If the input value is controlled indirectly by other inputs, it's often possible
to **automatically propagate** the gizmo to the actual input.

Backpropagation does not work for all nodes, although more nodes can be
supported over time.

This patch adds the first three gizmo nodes which cover common use cases:
* **Linear Gizmo**: Creates a gizmo that controls a float or integer value using
  a linear movement of e.g. an arrow in the 3D viewport.
* **Dial Gizmo**: Creates a circular gizmo in the 3D viewport that can be
  rotated to change the attached angle input.
* **Transform Gizmo**: Creates a simple gizmo for location, rotation and scale.

In the future, more built-in gizmos and potentially the ability for custom
gizmos could be added.

source/blender/gpu/shaders/gpu_shader_2D_nodelink_frag.glsl
@@ -39,6 +39,21 @@ void main()
     dash_frag_alpha = alpha;
   }
 
-  fragColor = finalColor;
-  fragColor.a *= get_line_alpha(0.5, 0.5) * dash_frag_alpha;
+  if (hasBackLink == 0) {
+    fragColor = finalColor;
+    fragColor.a *= get_line_alpha(0.5, 0.5);
+  }
+  else {
+    /* Draw two links right next to each other, the main link and the back-link. */
+    vec4 main_link_color = finalColor;
+    main_link_color.a *= get_line_alpha(0.75, 0.3);
+
+    vec4 back_link_color = vec4(vec3(0.8), 1.0);
+    back_link_color.a *= get_line_alpha(0.2, 0.25);
+
+    /* Combine both links. */
+    fragColor.rgb = main_link_color.rgb * main_link_color.a +
+                    back_link_color.rgb * back_link_color.a;
+    fragColor.a = main_link_color.a + back_link_color.a;
+  }
 }

commit 320460f749f158a50a94e1a37534ea391b539803

Fix: Nodes: Lack of dash on fields links
Regression after 60e53ba4f375f955ba127179de8688ac036ab799.

Pull Request: https://projects.blender.org/blender/blender/pulls/124740

source/blender/gpu/shaders/gpu_shader_2D_nodelink_frag.glsl
@@ -41,7 +41,7 @@ void main()
 
   if (hasBackLink == 0) {
     fragColor = finalColor;
-    fragColor.a *= get_line_alpha(0.5, 0.5);
+    fragColor.a *= get_line_alpha(0.5, 0.5) * dash_frag_alpha;
   }
   else {
     /* Draw two links right next to each other, the main link and the back-link. */
@@ -54,6 +54,6 @@ void main()
     /* Combine both links. */
     fragColor.rgb = main_link_color.rgb * main_link_color.a +
                     back_link_color.rgb * back_link_color.a;
-    fragColor.a = main_link_color.a + back_link_color.a;
+    fragColor.a = main_link_color.a * dash_frag_alpha + back_link_color.a;
   }
 }