Bforartists/Bforartists

commit 21f9ca7bfa3652a996bca0a1115ac3292d21aad3

Fix: MEM_new/MEM_freeN mismatch in file browser code
Caused by e92ffc0598701fe4abb97213c9c5b5ba1826ab90

source/blender/editors/space_file/folder_history.cc
@@ -113,10 +113,10 @@ bool folderlist_clear_next(SpaceFile *sfile)
 void folderlist_free(ListBase *folderlist)
 {
   if (folderlist) {
-    LISTBASE_FOREACH (FolderList *, folder, folderlist) {
+    LISTBASE_FOREACH_MUTABLE (FolderList *, folder, folderlist) {
       MEM_freeN(folder->foldername);
+      MEM_delete(folder);
     }
-    BLI_freelistN(folderlist);
   }
 }
 

commit 36e5d3793983e0fa54e294ab110d59e635a75340

Fix #124770: File Browser: Crash when going in and out of folders
Caused by 21f9ca7bfa
Crash due to attempt of double freeing. Fix is to make first and
last nullptr of list after freeing the memory. This avoids execution of
`folderlist_free` (`if(folderlist_clear_next())` is false)

Pull Request: https://projects.blender.org/blender/blender/pulls/124781

source/blender/editors/space_file/folder_history.cc
@@ -117,6 +117,7 @@ void folderlist_free(ListBase *folderlist)
       MEM_freeN(folder->foldername);
       MEM_delete(folder);
     }
+    BLI_listbase_clear(folderlist);
   }
 }
 