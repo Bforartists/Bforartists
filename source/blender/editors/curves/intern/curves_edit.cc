Bforartists/Bforartists

commit 30ac3d9f267099266052dde54240507ed4fb8ffd

Curves: Add Python API to remove and resize curves
This adds two new python functions:
* `curves.remove_curves(indices=None)`
* `curves.resize_curves(sizes, indices=None)`

By default, `remove_curves` will remove all curves. If `indices`
are provided, only the curves with the given indices are removed.
The indices have to be in ascending order and mustn't repeat.

The `resize_curves`function  will change the number of points
for each curve. If no indices are provided, there must be as
many elements in `sizes` as the number of curves there is.
Otherwise, there must be one element in `sizes` per element
in `indices`. The `sizes` are the new size for a given curve.
If the new size for a curve is smaller, then the curve is
trimmed from the end. If the new size for a curve is greater,
then the end values will be default initialized.

Pull Request: https://projects.blender.org/blender/blender/pulls/125502

source/blender/editors/curves/intern/curves_edit.cc
@@ -10,6 +10,7 @@
 
 #include "BKE_attribute.hh"
 #include "BKE_curves.hh"
+#include "BKE_curves_utils.hh"
 
 #include "ED_curves.hh"
 
@@ -223,6 +224,104 @@ void duplicate_curves(bke::CurvesGeometry &curves, const IndexMask &mask)
     selection.span.take_back(mask.size()).fill(true);
     selection.finish();
   }
+}
+
+void add_curves(bke::CurvesGeometry &curves, const Span<int> new_sizes)
+{
+  const int orig_points_num = curves.points_num();
+  const int orig_curves_num = curves.curves_num();
+  curves.resize(orig_points_num, orig_curves_num + new_sizes.size());
+
+  /* Find the final number of points by accumulating the new */
+  MutableSpan<int> new_offsets = curves.offsets_for_write().drop_front(orig_curves_num);
+  new_offsets.drop_back(1).copy_from(new_sizes);
+  offset_indices::accumulate_counts_to_offsets(new_offsets, orig_points_num);
+  /* First, resize the curve domain. */
+  curves.resize(curves.offsets().last(), curves.curves_num());
+
+  /* Initialize new attribute values, since #CurvesGeometry::resize() doesn't do that. */
+  bke::MutableAttributeAccessor attributes = curves.attributes_for_write();
+  bke::fill_attribute_range_default(
+      attributes, bke::AttrDomain::Point, {}, curves.points_range().drop_front(orig_points_num));
+  bke::fill_attribute_range_default(
+      attributes, bke::AttrDomain::Curve, {}, curves.curves_range().drop_front(orig_curves_num));
+
+  curves.update_curve_types();
+}
+
+void resize_curves(bke::CurvesGeometry &curves,
+                   const IndexMask &curves_to_resize,
+                   const Span<int> new_sizes)
+{
+  if (curves_to_resize.is_empty()) {
+    return;
+  }
+  BLI_assert(curves_to_resize.size() == new_sizes.size());
+  bke::CurvesGeometry dst_curves = bke::curves::copy_only_curve_domain(curves);
+
+  IndexMaskMemory memory;
+  IndexMask curves_to_copy;
+  std::optional<IndexRange> range = curves_to_resize.to_range();
+  /* Check if we need to copy some curves over. Write the new sizes into the offests. */
+  if (range && curves.curves_range() == *range) {
+    curves_to_copy = curves_to_resize.complement(curves.curves_range(), memory);
+    offset_indices::copy_group_sizes(
+        curves.offsets(), curves_to_copy, dst_curves.offsets_for_write());
+    array_utils::scatter(new_sizes, curves_to_resize, dst_curves.offsets_for_write());
+  }
+  else {
+    curves_to_copy = {};
+    dst_curves.offsets_for_write().drop_back(1).copy_from(new_sizes);
+  }
+  /* Accumulate the sizes written from `new_sizes` into offsets. */
+  offset_indices::accumulate_counts_to_offsets(dst_curves.offsets_for_write());
+
+  /* Resize the points domain.*/
+  dst_curves.resize(dst_curves.offsets().last(), dst_curves.curves_num());
+
+  /* Copy point attributes and default initialize newly added point ranges. */
+  const bke::AttrDomain domain(bke::AttrDomain::Point);
+  const OffsetIndices<int> src_offsets = curves.points_by_curve();
+  const OffsetIndices<int> dst_offsets = dst_curves.points_by_curve();
+  const bke::AttributeAccessor src_attributes = curves.attributes();
+  bke::MutableAttributeAccessor dst_attributes = dst_curves.attributes_for_write();
+  src_attributes.for_all(
+      [&](const bke::AttributeIDRef &id, const bke::AttributeMetaData meta_data) {
+        if (meta_data.domain != domain || id.is_anonymous()) {
+          return true;
+        }
+        const GVArraySpan src = *src_attributes.lookup(id, domain);
+        const CPPType &type = src.type();
+        bke::GSpanAttributeWriter dst = dst_attributes.lookup_or_add_for_write_only_span(
+            id, domain, meta_data.data_type);
+        if (!dst) {
+          return true;
+        }
+
+        curves_to_resize.foreach_index(GrainSize(512), [&](const int curve_i) {
+          const IndexRange src_points = src_offsets[curve_i];
+          const IndexRange dst_points = dst_offsets[curve_i];
+          if (dst_points.size() < src_points.size()) {
+            const int src_excees = src_points.size() - dst_points.size();
+            dst.span.slice(dst_points).copy_from(src.slice(src_points.drop_back(src_excees)));
+          }
+          else {
+            const int dst_excees = dst_points.size() - src_points.size();
+            dst.span.slice(dst_points.drop_back(dst_excees)).copy_from(src.slice(src_points));
+            GMutableSpan dst_end_slice = dst.span.slice(dst_points.take_back(dst_excees));
+            type.value_initialize_n(dst_end_slice.data(), dst_end_slice.size());
+          }
+        });
+        array_utils::copy_group_to_group(src_offsets, dst_offsets, curves_to_copy, src, dst.span);
+        dst.finish();
+        return true;
+      });
+
+  dst_curves.update_curve_types();
+
+  /* Move the result into `curves`. */
+  curves = std::move(dst_curves);
+  curves.tag_topology_changed();
 }
 
 }  // namespace blender::ed::curves

source/blender/editors/include/ED_curves.hh
@@ -348,6 +348,23 @@ bool remove_selection(bke::CurvesGeometry &curves, bke::AttrDomain selection_dom
 void duplicate_points(bke::CurvesGeometry &curves, const IndexMask &mask);
 void duplicate_curves(bke::CurvesGeometry &curves, const IndexMask &mask);
 
+/**
+ * Adds new curves to \a curves.
+ * \param new_sizes: The new size for each curve. Sizes must be > 0.
+ */
+void add_curves(bke::CurvesGeometry &curves, Span<int> new_sizes);
+
+/**
+ * Resizes the curves in \a curves.
+ * \param curves_to_resize: The curves that should be resized.
+ * \param new_sizes: The new size for each curve in \a curves_to_resize. If the size is smaller,
+ * the curve is trimmed from the end. If the size is larger, the end is extended and all attributes
+ * are default initialized. Sizes must be > 0.
+ */
+void resize_curves(bke::CurvesGeometry &curves,
+                   const IndexMask &curves_to_resize,
+                   Span<int> new_sizes);
+
 /** \} */
 
 }  // namespace blender::ed::curves

source/blender/makesrna/intern/CMakeLists.txt
@@ -119,6 +119,7 @@ set(APISRC
   rna_armature_api.cc
   rna_camera_api.cc
   rna_curve_api.cc
+  rna_curves_api.cc
   rna_fcurve_api.cc
   rna_grease_pencil_api.cc
   rna_image_api.cc
@@ -487,6 +488,7 @@ set(SRC
   rna_internal.hh
   rna_internal_types.hh
   rna_mesh_utils.hh
+  rna_curves_utils.hh
 )
 
 set(LIB

source/blender/makesrna/intern/makesrna.cc
@@ -4816,7 +4816,7 @@ static RNAProcessItem PROCESS_ITEMS[] = {
     {"rna_fcurve.cc", "rna_fcurve_api.cc", RNA_def_fcurve},
     {"rna_annotations.cc", nullptr, RNA_def_annotations},
     {"rna_grease_pencil.cc", "rna_grease_pencil_api.cc", RNA_def_grease_pencil},
-    {"rna_curves.cc", nullptr, RNA_def_curves},
+    {"rna_curves.cc", "rna_curves_api.cc", RNA_def_curves},
     {"rna_image.cc", "rna_image_api.cc", RNA_def_image},
     {"rna_key.cc", nullptr, RNA_def_key},
     {"rna_light.cc", nullptr, RNA_def_light},

source/blender/makesrna/intern/rna_curves.cc
@@ -301,46 +301,6 @@ static void rna_Curves_normals_begin(CollectionPropertyIterator *iter, PointerRN
   rna_iterator_array_begin(iter, positions, sizeof(float[3]), size, true, nullptr);
 }
 
-static void rna_Curves_add_curves(Curves *curves_id,
-                                  ReportList *reports,
-                                  const int *sizes,
-                                  const int sizes_num)
-{
-  using namespace blender;
-  if (std::any_of(sizes, sizes + sizes_num, [](const int size) { return size < 1; })) {
-    BKE_report(reports, RPT_ERROR, "Curve sizes must be greater than zero");
-    return;
-  }
-
-  bke::CurvesGeometry &curves = curves_id->geometry.wrap();
-
-  const int orig_points_num = curves.points_num();
-  const int orig_curves_num = curves.curves_num();
-  curves.resize(orig_points_num, orig_curves_num + sizes_num);
-
-  /* Find the final number of points by accumulating the new */
-  MutableSpan<int> new_offsets = curves.offsets_for_write().drop_front(orig_curves_num);
-  new_offsets.drop_back(1).copy_from({sizes, sizes_num});
-  offset_indices::accumulate_counts_to_offsets(new_offsets, orig_points_num);
-
-  curves.resize(curves.offsets().last(), curves.curves_num());
-
-  /* Initialize new attribute values, since #CurvesGeometry::resize() doesn't do that. */
-  bke::MutableAttributeAccessor attributes = curves.attributes_for_write();
-  bke::fill_attribute_range_default(
-      attributes, bke::AttrDomain::Point, {}, curves.points_range().drop_front(orig_points_num));
-  bke::fill_attribute_range_default(
-      attributes, bke::AttrDomain::Curve, {}, curves.curves_range().drop_front(orig_curves_num));
-
-  curves.update_curve_types();
-
-  /* Avoid updates for importers creating curves. */
-  if (curves_id->id.us > 0) {
-    DEG_id_tag_update(&curves_id->id, ID_RECALC_GEOMETRY);
-    WM_main_add_notifier(NC_GEOM | ND_DATA, curves_id);
-  }
-}
-
 static void rna_Curves_update_data(Main * /*bmain*/, Scene * /*scene*/, PointerRNA *ptr)
 {
   ID *id = ptr->owner_id;
@@ -442,33 +402,6 @@ static void rna_def_curves_curve(BlenderRNA *brna)
   RNA_def_property_clear_flag(prop, PROP_EDITABLE);
   RNA_def_property_int_funcs(prop, "rna_CurveSlice_index_get", nullptr, nullptr);
   RNA_def_property_ui_text(prop, "Index", "Index of this curve");
-}
-
-static void rna_def_curves_api(StructRNA *srna)
-{
-  FunctionRNA *func;
-  PropertyRNA *parm;
-
-  func = RNA_def_function(srna, "add_curves", "rna_Curves_add_curves");
-  RNA_def_function_flag(func, FUNC_USE_REPORTS);
-  parm = RNA_def_int_array(func,
-                           "sizes",
-                           1,
-                           nullptr,
-                           0,
-                           INT_MAX,
-                           "Sizes",
-                           "The number of points in each curve",
-                           1,
-                           10000);
-  RNA_def_property_array(parm, RNA_MAX_ARRAY_LENGTH);
-  RNA_def_parameter_flags(parm, PROP_DYNAMIC, PARM_REQUIRED);
-
-  // parm = RNA_def_int(
-  //     func, "sizes", 1, 1, INT_MAX, "Sizes", "The number of points in each curve", 1, 10000);
-  // RNA_def_property_array(parm, 0); /* Dynamic length, see next line. */
-  // RNA_def_property_flag(parm, PROP_DYNAMIC);
-  // RNA_def_parameter_flags(parm, PROP_DYNAMIC, PARM_REQUIRED);
 }
 
 static void rna_def_curves(BlenderRNA *brna)
@@ -625,7 +558,7 @@ static void rna_def_curves(BlenderRNA *brna)
   /* common */
   rna_def_animdata_common(srna);
 
-  rna_def_curves_api(srna);
+  RNA_api_curves(srna);
 }
 
 void RNA_def_curves(BlenderRNA *brna)

source/blender/makesrna/intern/rna_curves_api.cc
@@ -1,0 +1,264 @@
+/* SPDX-FileCopyrightText: 2024 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup RNA
+ */
+
+#include "RNA_define.hh"
+
+#include "rna_internal.hh" /* own include */
+
+#ifdef RNA_RUNTIME
+
+#  include "BKE_curves.hh"
+#  include "BKE_report.hh"
+
+#  include "BLI_index_mask.hh"
+
+#  include "ED_curves.hh"
+
+#  include "rna_curves_utils.hh"
+
+/* Common `CurvesGeometry` API functions. */
+
+bool rna_CurvesGeometry_add_curves(blender::bke::CurvesGeometry &curves,
+                                   ReportList *reports,
+                                   const int *sizes_ptr,
+                                   const int sizes_num)
+{
+  using namespace blender;
+  if (std::any_of(sizes_ptr, sizes_ptr + sizes_num, [](const int size) { return size < 1; })) {
+    BKE_report(reports, RPT_ERROR, "Curve sizes must be greater than zero");
+    return false;
+  }
+
+  ed::curves::add_curves(curves, {sizes_ptr, sizes_num});
+  curves.tag_topology_changed();
+  return true;
+}
+
+bool rna_CurvesGeometry_remove_curves(blender::bke::CurvesGeometry &curves,
+                                      ReportList *reports,
+                                      const int *indices_ptr,
+                                      const int indices_num)
+{
+  using namespace blender;
+  if (indices_ptr != nullptr) {
+    const Span<int> indices(indices_ptr, indices_num);
+    if (std::any_of(indices.begin(), indices.end(), [&](const int index) {
+          return !curves.curves_range().contains(index);
+        }))
+    {
+      BKE_report(reports, RPT_ERROR, "Indices must be in range");
+      return false;
+    }
+    if (!std::is_sorted(indices.begin(), indices.end())) {
+      BKE_report(reports, RPT_ERROR, "Indices must be sorted in acending order");
+      return false;
+    }
+    if (std::adjacent_find(indices.begin(), indices.end(), std::greater_equal<int>()) !=
+        indices.end())
+    {
+      BKE_report(reports, RPT_ERROR, "Indices can't have duplicates");
+      return false;
+    }
+    /* Remove the curves by their indices. */
+    IndexMaskMemory memory;
+    IndexMask curves_to_delete = IndexMask::from_indices(indices, memory);
+    curves.remove_curves(curves_to_delete, {});
+  }
+  else {
+    /* Clear the CurvesGeometry. */
+    curves = {};
+  }
+  return true;
+}
+
+bool rna_CurvesGeometry_resize_curves(blender::bke::CurvesGeometry &curves,
+                                      ReportList *reports,
+                                      const int *sizes_ptr,
+                                      const int sizes_num,
+                                      const int *indices_ptr,
+                                      const int indices_num)
+{
+  using namespace blender;
+  const Span<int> new_sizes(sizes_ptr, sizes_num);
+  if (std::any_of(new_sizes.begin(), new_sizes.end(), [](const int size) { return size < 1; })) {
+    BKE_report(reports, RPT_ERROR, "Sizes must be greater than zero");
+    return false;
+  }
+  IndexMaskMemory memory;
+  IndexMask curves_to_resize;
+  if (indices_ptr != nullptr) {
+    if (indices_num != sizes_num) {
+      BKE_report(reports, RPT_ERROR, "Length of sizes and length of indices must be the same");
+      return false;
+    }
+    const Span<int> indices(indices_ptr, indices_num);
+    if (std::any_of(indices.begin(), indices.end(), [&](const int index) {
+          return !curves.curves_range().contains(index);
+        }))
+    {
+      BKE_report(reports, RPT_ERROR, "Indices must be in range");
+      return false;
+    }
+    if (!std::is_sorted(indices.begin(), indices.end())) {
+      BKE_report(reports, RPT_ERROR, "Indices must be sorted in acending order");
+      return false;
+    }
+    if (std::adjacent_find(indices.begin(), indices.end(), std::greater_equal<int>()) !=
+        indices.end())
+    {
+      BKE_report(reports, RPT_ERROR, "Indices can't have duplicates");
+      return false;
+    }
+    curves_to_resize = IndexMask::from_indices(indices, memory);
+  }
+  else {
+    if (sizes_num != curves.curves_num()) {
+      BKE_report(reports, RPT_ERROR, "Length of sizes and number of strokes must be the same");
+      return false;
+    }
+    curves_to_resize = curves.curves_range();
+  }
+
+  ed::curves::resize_curves(curves, curves_to_resize, {sizes_ptr, sizes_num});
+  return true;
+}
+
+/* Curves API functions. */
+
+static void rna_Curves_add_curves(Curves *curves_id,
+                                  ReportList *reports,
+                                  const int *sizes,
+                                  const int sizes_num)
+{
+  using namespace blender;
+  bke::CurvesGeometry &curves = curves_id->geometry.wrap();
+  if (!rna_CurvesGeometry_add_curves(curves, reports, sizes, sizes_num)) {
+    return;
+  }
+
+  /* Avoid updates for importers creating curves. */
+  if (curves_id->id.us > 0) {
+    DEG_id_tag_update(&curves_id->id, ID_RECALC_GEOMETRY);
+    WM_main_add_notifier(NC_GEOM | ND_DATA, curves_id);
+  }
+}
+
+static void rna_Curves_remove_curves(Curves *curves_id,
+                                     ReportList *reports,
+                                     const int *indices_ptr,
+                                     const int indices_num)
+{
+  using namespace blender;
+  bke::CurvesGeometry &curves = curves_id->geometry.wrap();
+  if (!rna_CurvesGeometry_remove_curves(curves, reports, indices_ptr, indices_num)) {
+    return;
+  }
+
+  /* Avoid updates for importers creating curves. */
+  if (curves_id->id.us > 0) {
+    DEG_id_tag_update(&curves_id->id, ID_RECALC_GEOMETRY);
+    WM_main_add_notifier(NC_GEOM | ND_DATA, curves_id);
+  }
+}
+
+static void rna_Curves_resize_curves(Curves *curves_id,
+                                     ReportList *reports,
+                                     const int *sizes_ptr,
+                                     const int sizes_num,
+                                     const int *indices_ptr,
+                                     const int indices_num)
+{
+  using namespace blender;
+  bke::CurvesGeometry &curves = curves_id->geometry.wrap();
+  if (!rna_CurvesGeometry_resize_curves(
+          curves, reports, sizes_ptr, sizes_num, indices_ptr, indices_num))
+  {
+    return;
+  }
+
+  /* Avoid updates for importers creating curves. */
+  if (curves_id->id.us > 0) {
+    DEG_id_tag_update(&curves_id->id, ID_RECALC_GEOMETRY);
+    WM_main_add_notifier(NC_GEOM | ND_DATA, curves_id);
+  }
+}
+
+#else
+
+void RNA_api_curves(StructRNA *srna)
+{
+  FunctionRNA *func;
+  PropertyRNA *parm;
+
+  func = RNA_def_function(srna, "add_curves", "rna_Curves_add_curves");
+  RNA_def_function_flag(func, FUNC_USE_REPORTS);
+  parm = RNA_def_int_array(func,
+                           "sizes",
+                           1,
+                           nullptr,
+                           0,
+                           INT_MAX,
+                           "Sizes",
+                           "The number of points in each curve",
+                           1,
+                           10000);
+  RNA_def_property_array(parm, RNA_MAX_ARRAY_LENGTH);
+  RNA_def_parameter_flags(parm, PROP_DYNAMIC, PARM_REQUIRED);
+
+  func = RNA_def_function(srna, "remove_curves", "rna_Curves_remove_curves");
+  RNA_def_function_ui_description(func,
+                                  "Remove all curves. If indices are provided, remove only the "
+                                  "curves with the given indices.");
+  RNA_def_function_flag(func, FUNC_USE_REPORTS);
+  parm = RNA_def_int_array(func,
+                           "indices",
+                           1,
+                           nullptr,
+                           0,
+                           INT_MAX,
+                           "Indices",
+                           "The indices of the curves to remove",
+                           0,
+                           10000);
+  RNA_def_property_array(parm, RNA_MAX_ARRAY_LENGTH);
+  RNA_def_parameter_flags(parm, PROP_DYNAMIC, ParameterFlag(0));
+
+  func = RNA_def_function(srna, "resize_curves", "rna_Curves_resize_curves");
+  RNA_def_function_ui_description(
+      func,
+      "Resize all existing curves. If indices are provided, resize only the curves with the given "
+      "indices. If the new size for a curve is smaller, the curve is trimmed. If "
+      "the new size for a curve is larger, the new end values are default initialized.");
+  RNA_def_function_flag(func, FUNC_USE_REPORTS);
+  parm = RNA_def_int_array(func,
+                           "sizes",
+                           1,
+                           nullptr,
+                           1,
+                           INT_MAX,
+                           "Sizes",
+                           "The number of points in each curve",
+                           1,
+                           10000);
+  RNA_def_property_array(parm, RNA_MAX_ARRAY_LENGTH);
+  RNA_def_parameter_flags(parm, PROP_DYNAMIC, PARM_REQUIRED);
+  parm = RNA_def_int_array(func,
+                           "indices",
+                           1,
+                           nullptr,
+                           0,
+                           INT_MAX,
+                           "Indices",
+                           "The indices of the curves to resize",
+                           0,
+                           10000);
+  RNA_def_property_array(parm, RNA_MAX_ARRAY_LENGTH);
+  RNA_def_parameter_flags(parm, PROP_DYNAMIC, ParameterFlag(0));
+}
+
+#endif

source/blender/makesrna/intern/rna_curves_utils.hh
@@ -1,0 +1,38 @@
+/* SPDX-FileCopyrightText: 2024 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup RNA
+ */
+
+#pragma once
+
+/* Common utility functions for CurvesGeometry */
+
+#ifdef RNA_RUNTIME
+
+namespace blender::bke {
+class CurvesGeometry;
+}
+
+struct ReportList;
+
+bool rna_CurvesGeometry_add_curves(blender::bke::CurvesGeometry &curves,
+                                   ReportList *reports,
+                                   const int *sizes,
+                                   int sizes_num);
+
+bool rna_CurvesGeometry_remove_curves(blender::bke::CurvesGeometry &curves,
+                                      ReportList *reports,
+                                      const int *indices_ptr,
+                                      int indices_num);
+
+bool rna_CurvesGeometry_resize_curves(blender::bke::CurvesGeometry &curves,
+                                      ReportList *reports,
+                                      const int *sizes_ptr,
+                                      int sizes_num,
+                                      const int *indices_ptr,
+                                      int indices_num);
+
+#endif

source/blender/makesrna/intern/rna_internal.hh
@@ -417,6 +417,7 @@ void RNA_api_bone(StructRNA *srna);
 void RNA_api_bonecollection(StructRNA *srna);
 void RNA_api_camera(StructRNA *srna);
 void RNA_api_curve(StructRNA *srna);
+void RNA_api_curves(StructRNA *srna);
 void RNA_api_curve_nurb(StructRNA *srna);
 void RNA_api_fcurves(StructRNA *srna);
 void RNA_api_drivers(StructRNA *srna);

commit be4634fbdaef013608e32db68207a59c77f32fc8

Fix: Curves: Crash in `resize_curves`
Introduced with 30ac3d9f267099266052dde54240507ed4fb8ffd.

The condition that checks if all the curves are resized is flipped.
Fix swaps the code in the `if` and the `else`.

source/blender/editors/curves/intern/curves_edit.cc
@@ -264,14 +264,14 @@ void resize_curves(bke::CurvesGeometry &curves,
   std::optional<IndexRange> range = curves_to_resize.to_range();
   /* Check if we need to copy some curves over. Write the new sizes into the offsets. */
   if (range && curves.curves_range() == *range) {
+    curves_to_copy = {};
+    dst_curves.offsets_for_write().drop_back(1).copy_from(new_sizes);
+  }
+  else {
     curves_to_copy = curves_to_resize.complement(curves.curves_range(), memory);
     offset_indices::copy_group_sizes(
         curves.offsets(), curves_to_copy, dst_curves.offsets_for_write());
     array_utils::scatter(new_sizes, curves_to_resize, dst_curves.offsets_for_write());
-  }
-  else {
-    curves_to_copy = {};
-    dst_curves.offsets_for_write().drop_back(1).copy_from(new_sizes);
   }
   /* Accumulate the sizes written from `new_sizes` into offsets. */
   offset_indices::accumulate_counts_to_offsets(dst_curves.offsets_for_write());