d2ecf74b1b74b1214811cab8233a751a84edb61a
blenux <blenux@outlook.com>
2024-08-12 17:05:40 +1000

    fix for 4521-top-menu-help-add-about-bforartists-hidden-operator
    - updated the about window to mention Bforartists
    - remove used donate button to Blender
    - added coloured logo to about window
    - added about window to help list under splash screen
    
    - also added in blf color/multicolor code (, needed and more upcoming changes anyways)

--- source/blender/blenfont/intern/blf_font.cc
+++ source/blender/blenfont/intern/blf_font.cc
@@ -525,8 +525,14 @@ int blf_font_draw_mono(
 }
 
 #ifndef WITH_HEADLESS
-void blf_draw_svg_icon(
-    FontBLF *font, uint icon_id, float x, float y, float size, float color[4], float outline_alpha)
+void blf_draw_svg_icon(FontBLF *font,
+                       uint icon_id,
+                       float x,
+                       float y,
+                       float size,
+                       float color[4],
+                       float outline_alpha,
+                       bool multicolor)
 {
   blf_font_size(font, size);
   font->pos[0] = int(x);
@@ -551,7 +557,7 @@ void blf_draw_svg_icon(
   GlyphCacheBLF *gc = blf_glyph_cache_acquire(font);
   blf_batch_draw_begin(font);
 
-  GlyphBLF *g = blf_glyph_ensure_icon(gc, icon_id, color == nullptr);
+  GlyphBLF *g = blf_glyph_ensure_icon(gc, icon_id, multicolor);
   if (g) {
     blf_glyph_draw(font, gc, g, 0, 0);
   }
@@ -565,11 +571,11 @@ void blf_draw_svg_icon(
 }
 
 blender::Array<uchar> blf_svg_icon_bitmap(
-    FontBLF *font, uint icon_id, float size, int *r_width, int *r_height)
+    FontBLF *font, uint icon_id, float size, int *r_width, int *r_height, bool multicolor)
 {
   blf_font_size(font, size);
   GlyphCacheBLF *gc = blf_glyph_cache_acquire(font);
-  GlyphBLF *g = blf_glyph_ensure_icon(gc, icon_id, false);
+  GlyphBLF *g = blf_glyph_ensure_icon(gc, icon_id, multicolor);
 
   if (!g) {
     blf_glyph_cache_release(font);
@@ -580,7 +586,7 @@ blender::Array<uchar> blf_svg_icon_bitmap(
 
   *r_width = g->dims[0];
   *r_height = g->dims[1];
-  blender::Array<uchar> bitmap(g->dims[0] * g->dims[1] * 4, 255);
+  blender::Array<uchar> bitmap(g->dims[0] * g->dims[1] * 4);
 
   if (g->num_channels == 4) {
     memcpy(bitmap.data(), g->bitmap, size_t(bitmap.size()));
@@ -589,6 +595,9 @@ blender::Array<uchar> blf_svg_icon_bitmap(
     for (int64_t y = 0; y < int64_t(g->dims[1]); y++) {
       for (int64_t x = 0; x < int64_t(g->dims[0]); x++) {
         int64_t offs_in = (y * int64_t(g->pitch)) + x;
+        bitmap[int64_t(offs_in * 4)] = g->bitmap[offs_in];
+        bitmap[int64_t(offs_in * 4 + 1)] = g->bitmap[offs_in];
+        bitmap[int64_t(offs_in * 4 + 2)] = g->bitmap[offs_in];
         bitmap[int64_t(offs_in * 4 + 3)] = g->bitmap[offs_in];
       }
     }
@@ -901,8 +910,8 @@ static void blf_font_boundbox_ex(FontBLF *font,
     }
     const ft_pix pen_x_next = pen_x + g->advance_x;
 
-    const ft_pix gbox_xmin = pen_x;
-    const ft_pix gbox_xmax = pen_x_next;
+    const ft_pix gbox_xmin = std::min(pen_x, pen_x + g->box_xmin);
+    const ft_pix gbox_xmax = std::max(pen_x_next, pen_x + g->box_xmax);
     const ft_pix gbox_ymin = g->box_ymin + pen_y;
     const ft_pix gbox_ymax = g->box_ymax + pen_y;
 
