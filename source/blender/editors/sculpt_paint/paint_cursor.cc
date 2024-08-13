Bforartists/Bforartists

commit fbd1e1532c03db2cc682cb49be5766f76c71f83e

Refactor: Sculpt: Add active_vert_index() and active_vert_position()
* Adds two helper methods, the first replacing direct access to the
  PBVHVertRef i value, the second replacing the previously removed
  SCULPT_active_vertex_co_get
* Removes most trivial usage of active_vert_ref(), the remaining cases
  are a bit more complex and need separate PRs

Pull Request: https://projects.blender.org/blender/blender/pulls/126186

source/blender/editors/sculpt_paint/paint_cursor.cc
@@ -1251,7 +1251,7 @@ struct PaintCursorContext {
   SculptSession *ss;
 
   /* Previous active vertex, used to determine if the preview is updated for the pose brush.  */
-  PBVHVertRef prev_active_vertex;
+  int prev_active_vert_index;
 
   bool is_stroke_active;
   bool is_cursor_over_mesh;
@@ -1408,7 +1408,7 @@ static void paint_cursor_sculpt_session_update_and_init(PaintCursorContext *pcon
 
   /* This updates the active vertex, which is needed for most of the Sculpt/Vertex Colors tools to
    * work correctly */
-  pcontext->prev_active_vertex = ss.active_vert_ref();
+  pcontext->prev_active_vert_index = ss.active_vert_index();
   if (!ups.stroke_active) {
     pcontext->is_cursor_over_mesh = SCULPT_cursor_geometry_info_update(
         C, &gi, mval_fl, (pcontext->brush->falloff_shape == PAINT_FALLOFF_SHAPE_SPHERE));
@@ -1750,9 +1750,11 @@ static void paint_cursor_draw_3d_view_brush_cursor_inactive(PaintCursorContext *
     return;
   }
 
+  BLI_assert(pcontext->vc.obact);
+  Object &active_object = *pcontext->vc.obact;
   paint_cursor_update_object_space_radius(pcontext);
 
-  const PBVHVertRef active_vert = pcontext->ss->active_vert_ref();
+  SCULPT_vertex_random_access_ensure(*pcontext->ss);
 
   /* Setup drawing. */
   wmViewport(&pcontext->region->winrct);
@@ -1761,19 +1763,19 @@ static void paint_cursor_draw_3d_view_brush_cursor_inactive(PaintCursorContext *
 
   /* Cursor location symmetry points. */
 
-  const float *active_vertex_co;
+  float3 active_vertex_co;
   if (brush.sculpt_tool == SCULPT_TOOL_GRAB && brush.flag & BRUSH_GRAB_ACTIVE_VERTEX) {
     SculptSession &ss = *pcontext->ss;
     if (ss.pbvh->type() == bke::pbvh::Type::Mesh) {
-      const Span<float3> positions = vert_positions_for_grab_active_get(*pcontext->vc.obact);
-      active_vertex_co = positions[active_vert.i];
+      const Span<float3> positions = vert_positions_for_grab_active_get(active_object);
+      active_vertex_co = positions[std::get<int>(ss.active_vert())];
     }
     else {
-      active_vertex_co = SCULPT_vertex_co_get(*pcontext->ss, active_vert);
+      active_vertex_co = pcontext->ss->active_vert_position(active_object);
     }
   }
   else {
-    active_vertex_co = SCULPT_vertex_co_get(*pcontext->ss, active_vert);
+    active_vertex_co = pcontext->ss->active_vert_position(active_object);
   }
   if (len_v3v3(active_vertex_co, pcontext->location) < pcontext->radius) {
     immUniformColor3fvAlpha(pcontext->outline_col, pcontext->outline_alpha);
@@ -1781,7 +1783,7 @@ static void paint_cursor_draw_3d_view_brush_cursor_inactive(PaintCursorContext *
                                     pcontext->region,
                                     active_vertex_co,
                                     *pcontext->sd,
-                                    *pcontext->vc.obact,
+                                    active_object,
                                     pcontext->radius);
   }
 
@@ -1794,9 +1796,10 @@ static void paint_cursor_draw_3d_view_brush_cursor_inactive(PaintCursorContext *
      * cursor won't be tagged to update, so always initialize the preview chain if it is
      * nullptr before drawing it. */
     SculptSession &ss = *pcontext->ss;
-    const bool update_previews = pcontext->prev_active_vertex.i != active_vert.i;
+    const bool update_previews = pcontext->prev_active_vert_index !=
+                                 pcontext->ss->active_vert_index();
     if (update_previews || !ss.pose_ik_chain_preview) {
-      BKE_sculpt_update_object_for_edit(pcontext->depsgraph, pcontext->vc.obact, false);
+      BKE_sculpt_update_object_for_edit(pcontext->depsgraph, &active_object, false);
 
       /* Free the previous pose brush preview. */
       if (ss.pose_ik_chain_preview) {
@@ -1805,7 +1808,7 @@ static void paint_cursor_draw_3d_view_brush_cursor_inactive(PaintCursorContext *
 
       /* Generate a new pose brush preview from the current cursor location. */
       ss.pose_ik_chain_preview = pose::preview_ik_chain_init(
-          *pcontext->vc.obact, ss, brush, pcontext->location, pcontext->radius);
+          active_object, ss, brush, pcontext->location, pcontext->radius);
     }
 
     /* Draw the pose brush rotation origins. */
@@ -1818,7 +1821,7 @@ static void paint_cursor_draw_3d_view_brush_cursor_inactive(PaintCursorContext *
         pcontext->pos,
         pcontext->region,
         SCULPT_vertex_co_get(*pcontext->ss, pcontext->ss->expand_cache->initial_active_vertex),
-        pcontext->vc.obact->object_to_world().ptr(),
+        active_object.object_to_world().ptr(),
         2);
   }
 
@@ -1840,14 +1843,14 @@ static void paint_cursor_draw_3d_view_brush_cursor_inactive(PaintCursorContext *
                             nullptr);
 
   GPU_matrix_push();
-  GPU_matrix_mul(pcontext->vc.obact->object_to_world().ptr());
+  GPU_matrix_mul(active_object.object_to_world().ptr());
 
   /* Drawing Cursor overlays in 3D object space. */
   if (is_brush_tool && brush.sculpt_tool == SCULPT_TOOL_GRAB &&
       (brush.flag & BRUSH_GRAB_ACTIVE_VERTEX))
   {
     geometry_preview_lines_update(pcontext->C, *pcontext->ss, pcontext->radius);
-    sculpt_geometry_preview_lines_draw(pcontext->pos, *pcontext->brush, *pcontext->vc.obact);
+    sculpt_geometry_preview_lines_draw(pcontext->pos, *pcontext->brush, active_object);
   }
 
   if (is_brush_tool && brush.sculpt_tool == SCULPT_TOOL_POSE) {

source/blender/blenkernel/BKE_paint.hh
@@ -592,6 +592,28 @@ struct SculptSession : blender::NonCopyable, blender::NonMovable {
   PBVHVertRef active_vert_ref() const;
   ActiveVert active_vert() const;
 
+  /**
+   * Retrieves the corresponding index of the ActiveVert inside a mesh-sized array.
+   *
+   * Helpful in generic cases where we are unlikely to already be processing data in a backing-type
+   * specific manner.
+   *
+   * \note For BMesh, a call to SCULPT_vertex_random_access_ensure is needed to get valid results.
+   * \returns -1 if there is no currently active vertex.
+   */
+  int active_vert_index() const;
+
+  /**
+   * Retrieves the active vertex position.
+   *
+   * This method should be avoided if already working with the relevant position-backing structures
+   * for each of the mesh types. In cases where we want more generic code, this abstraction helps
+   * to remove boilerplate.
+   *
+   * \returns float3 at negative infinity if there is no currently active vertex
+   */
+  blender::float3 active_vert_position(const Object &object) const;
+
   void set_active_vert(PBVHVertRef vert);
 
  private:

source/blender/blenkernel/intern/paint.cc
@@ -1767,6 +1767,48 @@ ActiveVert SculptSession::active_vert() const
   return {};
 }
 
+int SculptSession::active_vert_index() const
+{
+  const ActiveVert vert = this->active_vert();
+  if (std::holds_alternative<int>(vert)) {
+    return std::get<int>(vert);
+  }
+  else if (std::holds_alternative<SubdivCCGCoord>(vert)) {
+    const SubdivCCGCoord coord = std::get<SubdivCCGCoord>(vert);
+    return coord.to_index(BKE_subdiv_ccg_key_top_level(*this->subdiv_ccg));
+  }
+  else if (std::holds_alternative<BMVert *>(vert)) {
+    BMVert *bm_vert = std::get<BMVert *>(vert);
+    return BM_elem_index_get(bm_vert);
+  }
+
+  return -1;
+}
+
+blender::float3 SculptSession::active_vert_position(const Object & /*object*/) const
+{
+  const ActiveVert vert = this->active_vert();
+  if (std::holds_alternative<int>(vert)) {
+    /* TODO: When we remove mesh positions from PBVH, this should be replaced with the positions
+     * array accessed via the object param */
+    const Span<float3> positions = BKE_pbvh_get_vert_positions(*this->pbvh);
+    return positions[std::get<int>(vert)];
+  }
+  else if (std::holds_alternative<SubdivCCGCoord>(vert)) {
+    const CCGKey key = BKE_subdiv_ccg_key_top_level(*this->subdiv_ccg);
+    const SubdivCCGCoord coord = std::get<SubdivCCGCoord>(vert);
+
+    return CCG_grid_elem_co(key, this->subdiv_ccg->grids[coord.grid_index], coord.x, coord.y);
+  }
+  else if (std::holds_alternative<BMVert *>(vert)) {
+    BMVert *bm_vert = std::get<BMVert *>(vert);
+    return bm_vert->co;
+  }
+
+  BLI_assert_unreachable();
+  return float3(std::numeric_limits<float>::infinity());
+}
+
 void SculptSession::set_active_vert(const PBVHVertRef vert)
 {
   active_vert_ = vert;

source/blender/editors/sculpt_paint/sculpt.cc
@@ -4475,10 +4475,10 @@ static void sculpt_update_brush_delta(UnifiedPaintSettings &ups,
     if (tool == SCULPT_TOOL_GRAB && brush.flag & BRUSH_GRAB_ACTIVE_VERTEX) {
       if (ss.pbvh->type() == bke::pbvh::Type::Mesh) {
         const Span<float3> positions = vert_positions_for_grab_active_get(ob);
-        cache->orig_grab_location = positions[ss.active_vert_ref().i];
+        cache->orig_grab_location = positions[std::get<int>(ss.active_vert())];
       }
       else {
-        cache->orig_grab_location = SCULPT_vertex_co_get(ss, ss.active_vert_ref());
+        cache->orig_grab_location = ss.active_vert_position(ob);
       }
     }
     else {

source/blender/editors/sculpt_paint/sculpt_automasking.cc
@@ -1014,10 +1014,10 @@ std::unique_ptr<Cache> cache_init(const Sculpt &sd, const Brush *brush, Object &
 
   int mode = calc_effective_bits(sd, brush);
 
-  if (mode & BRUSH_AUTOMASKING_TOPOLOGY && ss.active_vert_ref().i != PBVH_REF_NONE) {
+  SCULPT_vertex_random_access_ensure(ss);
+  if (mode & BRUSH_AUTOMASKING_TOPOLOGY && ss.active_vert_index() != -1) {
     islands::ensure_cache(ob);
-    automasking->settings.initial_island_nr = islands::vert_id_get(
-        ss, BKE_pbvh_vertex_to_index(*ss.pbvh, ss.active_vert_ref()));
+    automasking->settings.initial_island_nr = islands::vert_id_get(ss, ss.active_vert_index());
   }
 
   bool use_stroke_id = false;

source/blender/editors/sculpt_paint/sculpt_cloth.cc
@@ -2312,7 +2312,7 @@ static int sculpt_cloth_filter_invoke(bContext *C, wmOperator *op, const wmEvent
       use_collisions,
       cloth_filter_is_deformation_filter(filter_type));
 
-  ss.filter_cache->cloth_sim_pinch_point = SCULPT_vertex_co_get(ss, ss.active_vert_ref());
+  ss.filter_cache->cloth_sim_pinch_point = ss.active_vert_position(ob);
 
   float3 origin(0);
   ensure_nodes_constraints(

source/blender/editors/sculpt_paint/sculpt_detail.cc
@@ -199,7 +199,7 @@ static void sample_detail_voxel(bContext *C, ViewContext *vc, const int mval[2])
   BKE_sculpt_update_object_for_edit(depsgraph, &ob, false);
 
   /* Average the edge length of the connected edges to the active vertex. */
-  const int active_vert = ss.active_vert_ref().i;
+  const int active_vert = std::get<int>(ss.active_vert());
   const float3 active_vert_position = positions[active_vert];
   float edge_length = 0.0f;
   Vector<int> neighbors;
@@ -589,7 +589,7 @@ static void dyntopo_detail_size_sample_from_surface(Object &ob,
                                                     DyntopoDetailSizeEditCustomData *cd)
 {
   SculptSession &ss = *ob.sculpt;
-  BMVert *active_vertex = reinterpret_cast<BMVert *>(ss.active_vert_ref().i);
+  BMVert *active_vertex = std::get<BMVert *>(ss.active_vert());
 
   float len_accum = 0;
   Vector<BMVert *, 64> neighbors;

source/blender/editors/sculpt_paint/sculpt_face_set.cc
@@ -889,7 +889,7 @@ static int change_visibility_exec(bContext *C, wmOperator *op)
     UnifiedPaintSettings *ups = &CTX_data_tool_settings(C)->unified_paint_settings;
 
     float location[3];
-    copy_v3_v3(location, SCULPT_vertex_co_get(ss, ss.active_vert_ref()));
+    copy_v3_v3(location, ss.active_vert_position(object));
     mul_m4_v3(object.object_to_world().ptr(), location);
     copy_v3_v3(ups->average_stroke_accum, location);
     ups->average_stroke_counter = 1;

commit 9ef8c7a55cb4282428c5d1a7d881a5a318fd71cc

Fix #126293: Crash when moving mouse after enabling Dyntopo
Introduced in fbd1e1532c03db2cc682cb49be5766f76c71f83e

We currently do not invalidate the `active_vert` variable when changing
between different mesh implementations. Therefore, it is incorrect to
assume the value can be turned into a valid concrete reference simply
based on the PBVH type.

Pull Request: https://projects.blender.org/blender/blender/pulls/126299

source/blender/editors/sculpt_paint/paint_cursor.cc
@@ -1250,8 +1250,14 @@ struct PaintCursorContext {
   Sculpt *sd;
   SculptSession *ss;
 
-  /* Previous active vertex, used to determine if the preview is updated for the pose brush.  */
-  int prev_active_vert_index;
+  /**
+   * Previous active vertex, used to determine if the preview is updated for the pose brush.
+   *
+   * For now, this cannot be switched to using a straight index via #prev_active_vert_index() as
+   * the underlying SculptSession variable is not invalidated upon changing modes. Once we no
+   * longer persist PBVHVertRef inside SculptSession, this can be updated as well.
+   */
+  PBVHVertRef prev_active_vert_ref;
 
   bool is_stroke_active;
   bool is_cursor_over_mesh;
@@ -1408,7 +1414,7 @@ static void paint_cursor_sculpt_session_update_and_init(PaintCursorContext *pcon
 
   /* This updates the active vertex, which is needed for most of the Sculpt/Vertex Colors tools to
    * work correctly */
-  pcontext->prev_active_vert_index = ss.active_vert_index();
+  pcontext->prev_active_vert_ref = ss.active_vert_ref();
   if (!ups.stroke_active) {
     pcontext->is_cursor_over_mesh = SCULPT_cursor_geometry_info_update(
         C, &gi, mval_fl, (pcontext->brush->falloff_shape == PAINT_FALLOFF_SHAPE_SPHERE));
@@ -1796,8 +1802,8 @@ static void paint_cursor_draw_3d_view_brush_cursor_inactive(PaintCursorContext *
      * cursor won't be tagged to update, so always initialize the preview chain if it is
      * nullptr before drawing it. */
     SculptSession &ss = *pcontext->ss;
-    const bool update_previews = pcontext->prev_active_vert_index !=
-                                 pcontext->ss->active_vert_index();
+    const bool update_previews = pcontext->prev_active_vert_ref.i !=
+                                 pcontext->ss->active_vert_ref().i;
     if (update_previews || !ss.pose_ik_chain_preview) {
       BKE_sculpt_update_object_for_edit(pcontext->depsgraph, &active_object, false);
 