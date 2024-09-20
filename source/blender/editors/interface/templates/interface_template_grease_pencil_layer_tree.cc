720775d4f407f576505160db394391dd09f39f52
ReinerBforartists <reiner.prokein@posteo.de>
2024-09-20 09:15:54 +0200

    Properties Editor - Grease Pencil - Grease pencil layer menu shows wrong icon for group #4654 - in list

--- source/blender/editors/interface/templates/interface_template_grease_pencil_layer_tree.cc
+++ source/blender/editors/interface/templates/interface_template_grease_pencil_layer_tree.cc
@@ -384,7 +384,8 @@ class LayerGroupViewItem : public AbstractTreeViewItem {
   {
     uiItemS_ex(&row, 0.8f);
 
-    short icon = ICON_FILE_FOLDER;
+    /*short icon = ICON_FILE_FOLDER;*/ 
+    short icon = ICON_GROUP; /*bfa - we use group icon for groups*/
     if (group_.color_tag != LAYERGROUP_COLOR_NONE) {
       icon = ICON_LAYERGROUP_COLOR_01 + group_.color_tag;
     }
