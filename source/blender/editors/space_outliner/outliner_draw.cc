Bforartists/Bforartists

commit ed619595ea6a8ba6ebcff981c3acc3b1440c78e6

Fix #128007: Outliner visibility toggles can be edited on linked data
When a linked/overridden collection is child of a local collection,
object/collection level visibility toogles can be edited of library data
if shift clicked on local collection's properties. To fix this, skip further
recursive calls when the property is not editable.

Pull Request: https://projects.blender.org/blender/blender/pulls/128011

source/blender/editors/space_outliner/outliner_draw.cc
@@ -378,6 +378,10 @@ static void outliner_collection_set_flag_recursive(Scene *scene,
   }
   PointerRNA ptr;
   outliner_layer_or_collection_pointer_create(scene, layer_collection, collection, &ptr);
+  if (!RNA_property_editable(&ptr, base_or_object_prop)) {
+    return;
+  }
+
   RNA_property_boolean_set(&ptr, layer_or_collection_prop, value);
 
   /* Set the same flag for the nested objects as well. */
@@ -387,6 +391,10 @@ static void outliner_collection_set_flag_recursive(Scene *scene,
     LISTBASE_FOREACH (CollectionObject *, cob, &layer_collection->collection->gobject) {
 
       outliner_base_or_object_pointer_create(scene, view_layer, collection, cob->ob, &ptr);
+      if (!RNA_property_editable(&ptr, base_or_object_prop)) {
+        continue;
+      }
+
       RNA_property_boolean_set(&ptr, base_or_object_prop, value);
 
       if (collection) {

commit 4b7b4d89dbc2cddee9d4cbc7273dc0ddb98495d8

Fix #128056: Crash when ctrl click on Outliner collection toggles
Caused by ed619595ea6a8ba6ebcff981c3acc3b1440c78e6
Missing null check.

Pull Request: https://projects.blender.org/blender/blender/pulls/128058

source/blender/editors/space_outliner/outliner_draw.cc
@@ -378,7 +378,7 @@ static void outliner_collection_set_flag_recursive(Scene *scene,
   }
   PointerRNA ptr;
   outliner_layer_or_collection_pointer_create(scene, layer_collection, collection, &ptr);
-  if (!RNA_property_editable(&ptr, base_or_object_prop)) {
+  if (base_or_object_prop && !RNA_property_editable(&ptr, base_or_object_prop)) {
     return;
   }
 