Bforartists/Bforartists

commit 364c7ef22561784b03de06ff6e243e07f97c1dd3

Refactor: Nodes: handle unavailable sockets in declaration instead of update function
This refactor changes many nodes to set socket availability in the node
declaration instead of using the more generic `bNodeType::updatefunc`.

This fixes issues where sockets that should be unavailable in evaluated data are
actually available. Also it moves us closer to a state where the generic update
function is not necessary anymore. The update function being so generic makes it
hard to integrate it well into the high level tree update code, because
"anything could happen" in that callback.

Pull Request: https://projects.blender.org/blender/blender/pulls/127845

source/blender/nodes/geometry/nodes/node_geo_mesh_primitive_line.cc
@@ -23,13 +23,13 @@ NODE_STORAGE_FUNCS(NodeGeometryMeshLine)
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_input<decl::Int>("Count").default_value(10).min(1).max(10000).description(
+  auto &count = b.add_input<decl::Int>("Count").default_value(10).min(1).max(10000).description(
       "Number of vertices on the line");
-  b.add_input<decl::Float>("Resolution")
-      .default_value(1.0f)
-      .min(0.1f)
-      .subtype(PROP_DISTANCE)
-      .description("Length of each individual edge");
+  auto &resolution = b.add_input<decl::Float>("Resolution")
+                         .default_value(1.0f)
+                         .min(0.1f)
+                         .subtype(PROP_DISTANCE)
+                         .description("Length of each individual edge");
   b.add_input<decl::Vector>("Start Location")
       .subtype(PROP_TRANSLATION)
       .description("Position of the first vertex");
@@ -40,6 +40,18 @@ static void node_declare(NodeDeclarationBuilder &b)
           "In offset mode, the distance between each socket on each axis. In end points mode, the "
           "position of the final vertex");
   b.add_output<decl::Geometry>("Mesh");
+
+  const bNode *node = b.node_or_null();
+  if (node != nullptr) {
+    const NodeGeometryMeshLine &storage = node_storage(*node);
+    const GeometryNodeMeshLineMode mode = GeometryNodeMeshLineMode(storage.mode);
+    const GeometryNodeMeshLineCountMode count_mode = GeometryNodeMeshLineCountMode(
+        storage.count_mode);
+    count.available(mode == GEO_NODE_MESH_LINE_MODE_OFFSET ||
+                    count_mode == GEO_NODE_MESH_LINE_COUNT_TOTAL);
+    resolution.available(mode == GEO_NODE_MESH_LINE_MODE_END_POINTS &&
+                         count_mode == GEO_NODE_MESH_LINE_COUNT_RESOLUTION);
+  }
 }
 
 static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
@@ -60,32 +72,6 @@ static void node_init(bNodeTree * /*tree*/, bNode *node)
   node_storage->count_mode = GEO_NODE_MESH_LINE_COUNT_TOTAL;
 
   node->storage = node_storage;
-}
-
-static void node_update(bNodeTree *ntree, bNode *node)
-{
-  bNodeSocket *count_socket = static_cast<bNodeSocket *>(node->inputs.first);
-  bNodeSocket *resolution_socket = count_socket->next;
-  bNodeSocket *start_socket = resolution_socket->next;
-  bNodeSocket *end_and_offset_socket = start_socket->next;
-
-  const NodeGeometryMeshLine &storage = node_storage(*node);
-  const GeometryNodeMeshLineMode mode = (GeometryNodeMeshLineMode)storage.mode;
-  const GeometryNodeMeshLineCountMode count_mode = (GeometryNodeMeshLineCountMode)
-                                                       storage.count_mode;
-
-  node_sock_label(end_and_offset_socket,
-                  (mode == GEO_NODE_MESH_LINE_MODE_END_POINTS) ? N_("End Location") :
-                                                                 N_("Offset"));
-
-  bke::node_set_socket_availability(ntree,
-                                    resolution_socket,
-                                    mode == GEO_NODE_MESH_LINE_MODE_END_POINTS &&
-                                        count_mode == GEO_NODE_MESH_LINE_COUNT_RESOLUTION);
-  bke::node_set_socket_availability(ntree,
-                                    count_socket,
-                                    mode == GEO_NODE_MESH_LINE_MODE_OFFSET ||
-                                        count_mode == GEO_NODE_MESH_LINE_COUNT_TOTAL);
 }
 
 static void node_gather_link_searches(GatherLinkSearchOpParams &params)
@@ -225,7 +211,6 @@ static void node_register()
   geo_node_type_base(&ntype, GEO_NODE_MESH_PRIMITIVE_LINE, "Mesh Line", NODE_CLASS_GEOMETRY);
   ntype.declare = node_declare;
   ntype.initfunc = node_init;
-  ntype.updatefunc = node_update;
   blender::bke::node_type_storage(
       &ntype, "NodeGeometryMeshLine", node_free_standard_storage, node_copy_standard_storage);
   ntype.geometry_node_execute = node_geo_exec;

source/blender/nodes/geometry/nodes/node_geo_attribute_domain_size.cc
@@ -16,27 +16,68 @@ namespace blender::nodes::node_geo_attribute_domain_size_cc {
 static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_input<decl::Geometry>("Geometry");
-  b.add_output<decl::Int>("Point Count").make_available([](bNode &node) {
-    node.custom1 = int16_t(GeometryComponent::Type::Mesh);
-  });
-  b.add_output<decl::Int>("Edge Count").make_available([](bNode &node) {
-    node.custom1 = int16_t(GeometryComponent::Type::Mesh);
-  });
-  b.add_output<decl::Int>("Face Count").make_available([](bNode &node) {
-    node.custom1 = int16_t(GeometryComponent::Type::Mesh);
-  });
-  b.add_output<decl::Int>("Face Corner Count").make_available([](bNode &node) {
-    node.custom1 = int16_t(GeometryComponent::Type::Mesh);
-  });
-  b.add_output<decl::Int>("Spline Count").make_available([](bNode &node) {
-    node.custom1 = int16_t(GeometryComponent::Type::Curve);
-  });
-  b.add_output<decl::Int>("Instance Count").make_available([](bNode &node) {
-    node.custom1 = int16_t(GeometryComponent::Type::Instance);
-  });
-  b.add_output<decl::Int>("Layer Count").make_available([](bNode &node) {
-    node.custom1 = int16_t(GeometryComponent::Type::GreasePencil);
-  });
+  auto &total_points = b.add_output<decl::Int>("Point Count")
+                           .make_available([](bNode &node) {
+                             node.custom1 = int16_t(GeometryComponent::Type::Mesh);
+                           })
+                           .available(false);
+  auto &total_edges = b.add_output<decl::Int>("Edge Count")
+                          .make_available([](bNode &node) {
+                            node.custom1 = int16_t(GeometryComponent::Type::Mesh);
+                          })
+                          .available(false);
+  auto &total_faces = b.add_output<decl::Int>("Face Count")
+                          .make_available([](bNode &node) {
+                            node.custom1 = int16_t(GeometryComponent::Type::Mesh);
+                          })
+                          .available(false);
+  auto &total_corners = b.add_output<decl::Int>("Face Corner Count")
+                            .make_available([](bNode &node) {
+                              node.custom1 = int16_t(GeometryComponent::Type::Mesh);
+                            })
+                            .available(false);
+  auto &total_curves = b.add_output<decl::Int>("Spline Count")
+                           .make_available([](bNode &node) {
+                             node.custom1 = int16_t(GeometryComponent::Type::Curve);
+                           })
+                           .available(false);
+  auto &total_instances = b.add_output<decl::Int>("Instance Count")
+                              .make_available([](bNode &node) {
+                                node.custom1 = int16_t(GeometryComponent::Type::Instance);
+                              })
+                              .available(false);
+  auto &total_layers = b.add_output<decl::Int>("Layer Count")
+                           .make_available([](bNode &node) {
+                             node.custom1 = int16_t(GeometryComponent::Type::GreasePencil);
+                           })
+                           .available(false);
+
+  const bNode *node = b.node_or_null();
+  if (node != nullptr) {
+    switch (GeometryComponent::Type(node->custom1)) {
+      case GeometryComponent::Type::Mesh:
+        total_points.available(true);
+        total_edges.available(true);
+        total_faces.available(true);
+        total_corners.available(true);
+        break;
+      case GeometryComponent::Type::Curve:
+        total_points.available(true);
+        total_curves.available(true);
+        break;
+      case GeometryComponent::Type::PointCloud:
+        total_points.available(true);
+        break;
+      case GeometryComponent::Type::Instance:
+        total_instances.available(true);
+        break;
+      case GeometryComponent::Type::GreasePencil:
+        total_layers.available(true);
+        break;
+      default:
+        BLI_assert_unreachable();
+    }
+  }
 }
 
 static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
@@ -47,36 +88,6 @@ static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
 static void node_init(bNodeTree * /*tree*/, bNode *node)
 {
   node->custom1 = int16_t(GeometryComponent::Type::Mesh);
-}
-
-static void node_update(bNodeTree *ntree, bNode *node)
-{
-  bNodeSocket *point_socket = static_cast<bNodeSocket *>(node->outputs.first);
-  bNodeSocket *edge_socket = point_socket->next;
-  bNodeSocket *face_socket = edge_socket->next;
-  bNodeSocket *face_corner_socket = face_socket->next;
-  bNodeSocket *spline_socket = face_corner_socket->next;
-  bNodeSocket *instances_socket = spline_socket->next;
-  bNodeSocket *layers_socket = instances_socket->next;
-
-  bke::node_set_socket_availability(ntree,
-                                    point_socket,
-                                    ELEM(node->custom1,
-                                         int16_t(GeometryComponent::Type::Mesh),
-                                         int16_t(GeometryComponent::Type::Curve),
-                                         int16_t(GeometryComponent::Type::PointCloud)));
-  bke::node_set_socket_availability(
-      ntree, edge_socket, node->custom1 == int16_t(GeometryComponent::Type::Mesh));
-  bke::node_set_socket_availability(
-      ntree, face_socket, node->custom1 == int16_t(GeometryComponent::Type::Mesh));
-  bke::node_set_socket_availability(
-      ntree, face_corner_socket, node->custom1 == int16_t(GeometryComponent::Type::Mesh));
-  bke::node_set_socket_availability(
-      ntree, spline_socket, node->custom1 == int16_t(GeometryComponent::Type::Curve));
-  bke::node_set_socket_availability(
-      ntree, instances_socket, node->custom1 == int16_t(GeometryComponent::Type::Instance));
-  bke::node_set_socket_availability(
-      ntree, layers_socket, node->custom1 == int16_t(GeometryComponent::Type::GreasePencil));
 }
 
 static void node_geo_exec(GeoNodeExecParams params)
@@ -166,7 +177,6 @@ static void node_register()
   ntype.declare = node_declare;
   ntype.draw_buttons = node_layout;
   ntype.initfunc = node_init;
-  ntype.updatefunc = node_update;
 
   blender::bke::node_register_type(&ntype);
 

source/blender/nodes/geometry/nodes/node_geo_boolean.cc
@@ -23,17 +23,51 @@ namespace blender::nodes::node_geo_boolean_cc {
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  b.add_input<decl::Geometry>("Mesh 1").only_realized_data().supported_type(
+  const bNode *node = b.node_or_null();
+
+  auto &first_geometry = b.add_input<decl::Geometry>("Mesh 1").only_realized_data().supported_type(
       GeometryComponent::Type::Mesh);
-  b.add_input<decl::Geometry>("Mesh 2")
-      .supported_type(GeometryComponent::Type::Mesh)
-      .multi_input();
+
+  if (node != nullptr) {
+    switch (geometry::boolean::Operation(node->custom1)) {
+      case geometry::boolean::Operation::Intersect:
+      case geometry::boolean::Operation::Union:
+        b.add_input<decl::Geometry>("Mesh", "Mesh 2")
+            .supported_type(GeometryComponent::Type::Mesh)
+            .multi_input();
+        break;
+      case geometry::boolean::Operation::Difference:
+        b.add_input<decl::Geometry>("Mesh 2")
+            .supported_type(GeometryComponent::Type::Mesh)
+            .multi_input();
+        break;
+    }
+  }
+
   b.add_input<decl::Bool>("Self Intersection");
   b.add_input<decl::Bool>("Hole Tolerant");
   b.add_output<decl::Geometry>("Mesh").propagate_all();
-  b.add_output<decl::Bool>("Intersecting Edges").field_on_all().make_available([](bNode &node) {
-    node.custom2 = int16_t(geometry::boolean::Solver::MeshArr);
-  });
+  auto &output_edges = b.add_output<decl::Bool>("Intersecting Edges")
+                           .field_on_all()
+                           .make_available([](bNode &node) {
+                             node.custom2 = int16_t(geometry::boolean::Solver::MeshArr);
+                           });
+
+  if (node != nullptr) {
+    const auto operation = geometry::boolean::Operation(node->custom1);
+    const auto solver = geometry::boolean::Solver(node->custom2);
+
+    output_edges.available(solver == geometry::boolean::Solver::MeshArr);
+
+    switch (operation) {
+      case geometry::boolean::Operation::Intersect:
+      case geometry::boolean::Operation::Union:
+        first_geometry.available(false);
+        break;
+      case geometry::boolean::Operation::Difference:
+        break;
+    }
+  }
 }
 
 static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
@@ -45,32 +79,6 @@ static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
 struct AttributeOutputs {
   std::optional<std::string> intersecting_edges_id;
 };
-
-static void node_update(bNodeTree *ntree, bNode *node)
-{
-  const geometry::boolean::Operation operation = geometry::boolean::Operation(node->custom1);
-  const geometry::boolean::Solver solver = geometry::boolean::Solver(node->custom2);
-
-  bNodeSocket *geometry_1_socket = static_cast<bNodeSocket *>(node->inputs.first);
-  bNodeSocket *geometry_2_socket = geometry_1_socket->next;
-
-  bNodeSocket *intersecting_edges_socket = static_cast<bNodeSocket *>(node->outputs.last);
-
-  switch (operation) {
-    case geometry::boolean::Operation::Intersect:
-    case geometry::boolean::Operation::Union:
-      bke::node_set_socket_availability(ntree, geometry_1_socket, false);
-      node_sock_label(geometry_2_socket, "Mesh");
-      break;
-    case geometry::boolean::Operation::Difference:
-      bke::node_set_socket_availability(ntree, geometry_1_socket, true);
-      node_sock_label(geometry_2_socket, "Mesh 2");
-      break;
-  }
-
-  bke::node_set_socket_availability(
-      ntree, intersecting_edges_socket, solver == geometry::boolean::Solver::MeshArr);
-}
 
 static void node_init(bNodeTree * /*tree*/, bNode *node)
 {
@@ -285,7 +293,6 @@ static void node_register()
   geo_node_type_base(&ntype, GEO_NODE_MESH_BOOLEAN, "Mesh Boolean", NODE_CLASS_GEOMETRY);
   ntype.declare = node_declare;
   ntype.draw_buttons = node_layout;
-  ntype.updatefunc = node_update;
   ntype.initfunc = node_init;
   ntype.geometry_node_execute = node_geo_exec;
   blender::bke::node_register_type(&ntype);

source/blender/nodes/geometry/nodes/node_geo_curve_fillet.cc
@@ -23,8 +23,14 @@ static void node_declare(NodeDeclarationBuilder &b)
 {
   b.add_input<decl::Geometry>("Curve").supported_type(
       {GeometryComponent::Type::Curve, GeometryComponent::Type::GreasePencil});
-  b.add_input<decl::Int>("Count").default_value(1).min(1).max(1000).field_on_all().make_available(
-      [](bNode &node) { node_storage(node).mode = GEO_NODE_CURVE_FILLET_POLY; });
+  auto &count_input = b.add_input<decl::Int>("Count")
+                          .default_value(1)
+                          .min(1)
+                          .max(1000)
+                          .field_on_all()
+                          .make_available([](bNode &node) {
+                            node_storage(node).mode = GEO_NODE_CURVE_FILLET_POLY;
+                          });
   b.add_input<decl::Float>("Radius")
       .min(0.0f)
       .max(FLT_MAX)
@@ -34,6 +40,12 @@ static void node_declare(NodeDeclarationBuilder &b)
   b.add_input<decl::Bool>("Limit Radius")
       .description("Limit the maximum value of the radius in order to avoid overlapping fillets");
   b.add_output<decl::Geometry>("Curve").propagate_all();
+
+  const bNode *node = b.node_or_null();
+  if (node != nullptr) {
+    const NodeGeometryCurveFillet &storage = node_storage(*node);
+    count_input.available(GeometryNodeCurveFilletMode(storage.mode) == GEO_NODE_CURVE_FILLET_POLY);
+  }
 }
 
 static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
@@ -46,14 +58,6 @@ static void node_init(bNodeTree * /*tree*/, bNode *node)
   NodeGeometryCurveFillet *data = MEM_cnew<NodeGeometryCurveFillet>(__func__);
   data->mode = GEO_NODE_CURVE_FILLET_BEZIER;
   node->storage = data;
-}
-
-static void node_update(bNodeTree *ntree, bNode *node)
-{
-  const NodeGeometryCurveFillet &storage = node_storage(*node);
-  const GeometryNodeCurveFilletMode mode = (GeometryNodeCurveFilletMode)storage.mode;
-  bNodeSocket *poly_socket = static_cast<bNodeSocket *>(node->inputs.first)->next;
-  bke::node_set_socket_availability(ntree, poly_socket, mode == GEO_NODE_CURVE_FILLET_POLY);
 }
 
 static bke::CurvesGeometry fillet_curve(const bke::CurvesGeometry &src_curves,
@@ -199,7 +203,6 @@ static void node_register()
       &ntype, "NodeGeometryCurveFillet", node_free_standard_storage, node_copy_standard_storage);
   ntype.declare = node_declare;
   ntype.initfunc = node_init;
-  ntype.updatefunc = node_update;
   ntype.geometry_node_execute = node_geo_exec;
   blender::bke::node_register_type(&ntype);
 

commit d91cb993470ce399db99e0469be2f72428973eec

Fix #128373: conditional socket visibility in Mesh Line node not working
This was broken by 364c7ef22561784b03de06ff6e243e07f97c1dd3 because
we forget to update the label in the declaration.

This is now reverted for the Mesh Line node, because there is no way to
change the label in the declaration yet. A new patch can be created that properly
updates the Mesh Line node to use the declaration.

source/blender/nodes/geometry/nodes/node_geo_mesh_primitive_line.cc
@@ -23,13 +23,13 @@ NODE_STORAGE_FUNCS(NodeGeometryMeshLine)
 
 static void node_declare(NodeDeclarationBuilder &b)
 {
-  auto &count = b.add_input<decl::Int>("Count").default_value(10).min(1).max(10000).description(
+  b.add_input<decl::Int>("Count").default_value(10).min(1).max(10000).description(
       "Number of vertices on the line");
-  auto &resolution = b.add_input<decl::Float>("Resolution")
-                         .default_value(1.0f)
-                         .min(0.1f)
-                         .subtype(PROP_DISTANCE)
-                         .description("Length of each individual edge");
+  b.add_input<decl::Float>("Resolution")
+      .default_value(1.0f)
+      .min(0.1f)
+      .subtype(PROP_DISTANCE)
+      .description("Length of each individual edge");
   b.add_input<decl::Vector>("Start Location")
       .subtype(PROP_TRANSLATION)
       .description("Position of the first vertex");
@@ -40,18 +40,6 @@ static void node_declare(NodeDeclarationBuilder &b)
           "In offset mode, the distance between each socket on each axis. In end points mode, the "
           "position of the final vertex");
   b.add_output<decl::Geometry>("Mesh");
-
-  const bNode *node = b.node_or_null();
-  if (node != nullptr) {
-    const NodeGeometryMeshLine &storage = node_storage(*node);
-    const GeometryNodeMeshLineMode mode = GeometryNodeMeshLineMode(storage.mode);
-    const GeometryNodeMeshLineCountMode count_mode = GeometryNodeMeshLineCountMode(
-        storage.count_mode);
-    count.available(mode == GEO_NODE_MESH_LINE_MODE_OFFSET ||
-                    count_mode == GEO_NODE_MESH_LINE_COUNT_TOTAL);
-    resolution.available(mode == GEO_NODE_MESH_LINE_MODE_END_POINTS &&
-                         count_mode == GEO_NODE_MESH_LINE_COUNT_RESOLUTION);
-  }
 }
 
 static void node_layout(uiLayout *layout, bContext * /*C*/, PointerRNA *ptr)
@@ -72,6 +60,32 @@ static void node_init(bNodeTree * /*tree*/, bNode *node)
   node_storage->count_mode = GEO_NODE_MESH_LINE_COUNT_TOTAL;
 
   node->storage = node_storage;
+}
+
+static void node_update(bNodeTree *ntree, bNode *node)
+{
+  bNodeSocket *count_socket = static_cast<bNodeSocket *>(node->inputs.first);
+  bNodeSocket *resolution_socket = count_socket->next;
+  bNodeSocket *start_socket = resolution_socket->next;
+  bNodeSocket *end_and_offset_socket = start_socket->next;
+
+  const NodeGeometryMeshLine &storage = node_storage(*node);
+  const GeometryNodeMeshLineMode mode = (GeometryNodeMeshLineMode)storage.mode;
+  const GeometryNodeMeshLineCountMode count_mode = (GeometryNodeMeshLineCountMode)
+                                                       storage.count_mode;
+
+  node_sock_label(end_and_offset_socket,
+                  (mode == GEO_NODE_MESH_LINE_MODE_END_POINTS) ? N_("End Location") :
+                                                                 N_("Offset"));
+
+  bke::node_set_socket_availability(ntree,
+                                    resolution_socket,
+                                    mode == GEO_NODE_MESH_LINE_MODE_END_POINTS &&
+                                        count_mode == GEO_NODE_MESH_LINE_COUNT_RESOLUTION);
+  bke::node_set_socket_availability(ntree,
+                                    count_socket,
+                                    mode == GEO_NODE_MESH_LINE_MODE_OFFSET ||
+                                        count_mode == GEO_NODE_MESH_LINE_COUNT_TOTAL);
 }
 
 static void node_gather_link_searches(GatherLinkSearchOpParams &params)
@@ -215,6 +229,7 @@ static void node_register()
       &ntype, "NodeGeometryMeshLine", node_free_standard_storage, node_copy_standard_storage);
   ntype.geometry_node_execute = node_geo_exec;
   ntype.draw_buttons = node_layout;
+  ntype.updatefunc = node_update;
   ntype.gather_link_search_ops = node_gather_link_searches;
   blender::bke::node_register_type(&ntype);
 