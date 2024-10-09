Bforartists/Bforartists

commit 809ef328926f8423ead3272c46a41af9e8590a3b

Fix #128006:  Liboverride objects can be renamed using batch rename.
Do not allow renaming liboverride IDs from batch-rename either.

Users who really want to rename liboverrides should use python/RNA API
instead, this should not be exposed to standard UI tools (as renaming
liboverride IDs can lead to quite some confusing setups).

scripts/startup/bl_operators/wm.py
@@ -2751,6 +2751,8 @@ class WM_OT_batch_rename(Operator):
 
     @classmethod
     def _data_from_context(cls, context, data_type, only_selected, *, check_context=False):
+        def _is_editable(data):
+            return data.is_editable and not data.override_library
 
         mode = context.mode
         scene = context.scene
@@ -2949,6 +2951,7 @@ class WM_OT_batch_rename(Operator):
                     "name",
                     descr,
                 )
+        data = ([id for id in data[0] if _is_editable(id)], data[1], data[2])
 
         return data
 

commit 600f4ca93da6f14cf7e08f5d8dd98e4f1c180714

Fix #128767: Regression: Batch Rename failed to rename bones.
Regression from 809ef32892, did not realize all items were not
necessarily actual IDs.

scripts/startup/bl_operators/wm.py
@@ -2809,7 +2809,7 @@ class WM_OT_batch_rename(Operator):
     @classmethod
     def _data_from_context(cls, context, data_type, only_selected, *, check_context=False):
         def _is_editable(data):
-            return data.is_editable and not data.override_library
+            return data.id_data.is_editable and not data.id_data.override_library
 
         mode = context.mode
         scene = context.scene
@@ -3008,7 +3008,7 @@ class WM_OT_batch_rename(Operator):
                     "name",
                     descr,
                 )
-        data = ([id for id in data[0] if _is_editable(id)], data[1], data[2])
+        data = ([it for it in data[0] if _is_editable(it)], data[1], data[2])
 
         return data
 