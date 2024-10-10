Bforartists/Bforartists

commit 55fd1a2fe1aaa563ac3bdb10905073767e8ea089

Fix #126129: Bake Action not working
With layered actions, the bake action wasn't working.
More specifically it failed to create a slot on the action
and link that slot on the animation data.

The fix is to create the slot.

Pull Request: https://projects.blender.org/blender/blender/pulls/126546

scripts/modules/bpy_extras/anim_utils.py
@@ -364,6 +364,9 @@ def bake_action_iter(
             atd.use_tweak_mode = False
 
         atd.action = action
+        if action.is_action_layered:
+            slot = action.slots.new(for_id=obj)
+            atd.action_slot = slot
 
     # Baking the action only makes sense in Replace mode, so force it (#69105)
     if not atd.use_tweak_mode:

commit 81122aa1fd7e76faed427dcfd198101413cb2423

Fix #128760: NLA bake action throws error
Caused by 55fd1a2fe1aa

Above commit was accessing action properties which are only available if
experimental baklava is enabled.
Now check for this.

Pull Request: https://projects.blender.org/blender/blender/pulls/128762

scripts/modules/bpy_extras/anim_utils.py
@@ -364,7 +364,7 @@ def bake_action_iter(
             atd.use_tweak_mode = False
 
         atd.action = action
-        if action.is_action_layered:
+        if bpy.context.preferences.experimental.use_animation_baklava and action.is_action_layered:
             slot = action.slots.new(for_id=obj)
             atd.action_slot = slot
 