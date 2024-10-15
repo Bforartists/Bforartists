Bforartists/Bforartists

commit ad9ec12217db588c3d850f598105fa05b00e08ee

Fix: GPv3: Solid View: Show vertex colors in Draw and Vertex paint mode
There was a bug where vertex colors were not shown in solid view in
draw or vertex paint mode. The check to use `V3D_SHADING_VERTEX_COLOR`
was outdated and checked the wrong mode flags.

The fix makes sure that we use `V3D_SHADING_VERTEX_COLOR` in
draw and vertex paint mode.

Pull Request: https://projects.blender.org/blender/blender/pulls/128581

source/blender/draw/engines/gpencil/gpencil_engine_c.cc
@@ -117,11 +117,9 @@ void GPENCIL_engine_init(void *ved)
     use_scene_world = V3D_USES_SCENE_WORLD(v3d);
 
     stl->pd->v3d_color_type = (v3d->shading.type == OB_SOLID) ? v3d->shading.color_type : -1;
-    /* Special case: If Vertex Paint mode, use always Vertex mode. */
+    /* Special case: If we're in Draw or Vertex Paint mode, show vertex colors. */
     if (v3d->shading.type == OB_SOLID && ctx->obact &&
-        ((ctx->obact->type == OB_GPENCIL_LEGACY &&
-          ctx->obact->mode == OB_MODE_VERTEX_GPENCIL_LEGACY) ||
-         (ctx->obact->type == OB_GREASE_PENCIL && ctx->obact->mode == OB_MODE_VERTEX_PAINT)))
+        ELEM(ctx->obact->mode, OB_MODE_PAINT_GPENCIL_LEGACY, OB_MODE_VERTEX_GPENCIL_LEGACY))
     {
       stl->pd->v3d_color_type = V3D_SHADING_VERTEX_COLOR;
     }

commit 16a228e8526ebd3b829860616c982e999ea269fa

Fix #129049: Solid mode doesn't render strokes correctly
Caused by ad9ec12217db588c3d850f598105fa05b00e08ee.

In draw mode, we want to make sure to show vertex colors by default
because users can draw with them. Otherwise this can lead to unexpected
behavior in solid shading mode when vertex colors used to not be
rendered in draw mode.

The previous fix tried to do this by enforcing the `V3D_SHADING_VERTEX_COLOR`
mode in draw mode. But this has the side effect that the material
stroke and fill color are overriden with pure white.

To fix the issue correctly, simply ensure that `vert_col_opacity` in
`grease_pencil_layer_cache_add` is set to 1.0f. This means that
vertex colors and material colors are shown just like in the render.

Also resolves #128680.

Pull Request: https://projects.blender.org/blender/blender/pulls/129054

source/blender/draw/engines/gpencil/gpencil_cache_utils.cc
@@ -543,8 +543,11 @@ GPENCIL_tLayer *grease_pencil_layer_cache_add(GPENCIL_PrivateData *pd,
   const bool is_in_front = (ob->dtx & OB_DRAW_IN_FRONT);
 
   const bool override_vertcol = (pd->v3d_color_type != -1);
+  /* In draw mode and vertex paint mode it's possible to draw vertex colors so we want to make sure
+   * to render them. Otherwise this can lead to unexpected behavior. */
   const bool is_vert_col_mode = (pd->v3d_color_type == V3D_SHADING_VERTEX_COLOR) ||
-                                (ob->mode == OB_MODE_VERTEX_PAINT) || pd->is_render;
+                                (ob->mode & OB_MODE_VERTEX_PAINT) != 0 ||
+                                (ob->mode & OB_MODE_PAINT_GREASE_PENCIL) != 0 || pd->is_render;
   const bool is_viewlayer_render = pd->is_render && !layer.view_layer_name().is_empty() &&
                                    STREQ(pd->view_layer->name, layer.view_layer_name().c_str());
   const bool disable_masks_render = is_viewlayer_render &&

source/blender/draw/engines/gpencil/gpencil_engine_c.cc
@@ -117,9 +117,9 @@ void GPENCIL_engine_init(void *ved)
     use_scene_world = V3D_USES_SCENE_WORLD(v3d);
 
     stl->pd->v3d_color_type = (v3d->shading.type == OB_SOLID) ? v3d->shading.color_type : -1;
-    /* Special case: If we're in Draw or Vertex Paint mode, show vertex colors. */
+    /* Special case: If we're in Vertex Paint mode, enforce V3D_SHADING_VERTEX_COLOR setting.*/
     if (v3d->shading.type == OB_SOLID && ctx->obact &&
-        ELEM(ctx->obact->mode, OB_MODE_PAINT_GREASE_PENCIL, OB_MODE_VERTEX_GREASE_PENCIL))
+        (ctx->obact->mode & OB_MODE_VERTEX_GREASE_PENCIL) != 0)
     {
       stl->pd->v3d_color_type = V3D_SHADING_VERTEX_COLOR;
     }