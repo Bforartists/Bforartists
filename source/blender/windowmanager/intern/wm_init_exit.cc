Bforartists/Bforartists

commit f3f4ded331e320a364457336df16b8825239d423

Fix massive amount of memleaks on exit in BPY.
Essentially, our current code would not properly remove (dereference)
its python objects matching various RNA data created during execution.

Some cases are fairly trivial to understand (like the lack of handling
of unregstering for our 'startup' operators and UI), other were more
subtle (like unregistered PropertyGroups who would remove/free their RNA
struct definition, without releasing first the potential matching python
object).

Co-authored-by: Campbell Barton <campbell@blender.org>

Pull Request: https://projects.blender.org/blender/blender/pulls/128899

source/blender/windowmanager/intern/wm_init_exit.cc
@@ -533,8 +533,9 @@ void WM_exit_ex(bContext *C, const bool do_python_exit, const bool do_user_exit_
    * Which can happen when the GPU backend fails to initialize.
    */
   if (C && CTX_py_init_get(C)) {
-    const char *imports[2] = {"addon_utils", nullptr};
-    BPY_run_string_eval(C, imports, "addon_utils.disable_all()");
+    /* Calls `addon_utils.disable_all()` as well as unregistering all "startup" modules.  */
+    const char *imports[] = {"bpy.utils", nullptr};
+    BPY_run_string_eval(C, imports, "bpy.utils._on_exit()");
   }
 #endif
 

scripts/modules/bpy/utils/__init__.py
@@ -201,6 +201,31 @@ _registered_module_names = []
 import bpy_types as _bpy_types
 
 
+def _register_module_call(mod):
+    register = getattr(mod, "register", None)
+    if register:
+        try:
+            register()
+        except Exception:
+            import traceback
+            traceback.print_exc()
+    else:
+        print(
+            "\nWarning! {!r} has no register function, "
+            "this is now a requirement for registerable scripts".format(mod.__file__)
+        )
+
+
+def _unregister_module_call(mod):
+    unregister = getattr(mod, "unregister", None)
+    if unregister:
+        try:
+            unregister()
+        except Exception:
+            import traceback
+            traceback.print_exc()
+
+
 def load_scripts(*, reload_scripts=False, refresh_scripts=False, extensions=True):
     """
     Load scripts and run each modules register function.
@@ -234,29 +259,6 @@ def load_scripts(*, reload_scripts=False, refresh_scripts=False, extensions=True
         for addon_module_name in [ext.module for ext in _preferences.addons]:
             _addon_utils.disable(addon_module_name)
 
-    def register_module_call(mod):
-        register = getattr(mod, "register", None)
-        if register:
-            try:
-                register()
-            except Exception:
-                import traceback
-                traceback.print_exc()
-        else:
-            print(
-                "\nWarning! {!r} has no register function, "
-                "this is now a requirement for registerable scripts".format(mod.__file__)
-            )
-
-    def unregister_module_call(mod):
-        unregister = getattr(mod, "unregister", None)
-        if unregister:
-            try:
-                unregister()
-            except Exception:
-                import traceback
-                traceback.print_exc()
-
     def test_reload(mod):
         import importlib
         # reloading this causes internal errors
@@ -281,7 +283,7 @@ def load_scripts(*, reload_scripts=False, refresh_scripts=False, extensions=True
             mod = test_reload(mod)
 
         if mod:
-            register_module_call(mod)
+            _register_module_call(mod)
             _registered_module_names.append(mod.__name__)
 
     if reload_scripts:
@@ -304,7 +306,7 @@ def load_scripts(*, reload_scripts=False, refresh_scripts=False, extensions=True
 
         # Loop over and unload all scripts.
         for mod in registered_modules:
-            unregister_module_call(mod)
+            _unregister_module_call(mod)
 
         for mod in registered_modules:
             test_reload(mod)
@@ -354,6 +356,22 @@ def load_scripts(*, reload_scripts=False, refresh_scripts=False, extensions=True
                 for subcls in cls.__subclasses__():
                     if not subcls.is_registered:
                         print("Warning, unregistered class: {:s}({:s})".format(subcls.__name__, cls.__name__))
+
+
+# Internal only, called on exit by `WM_exit_ex`.
+def _on_exit():
+    # Disable all add-ons.
+    _addon_utils.disable_all()
+
+    # Call `unregister` function on internal startup module.
+    # Must only be used as part of Blender 'exit' process.
+    from bpy_restrict_state import RestrictBlend
+    with RestrictBlend():
+        for mod_name in reversed(_registered_module_names):
+            if (mod := _sys.modules.get(mod_name)) is None:
+                print("Warning: module", repr(mod_name), "not found in sys.modules")
+                continue
+            _unregister_module_call(mod)
 
 
 def load_scripts_extensions(*, reload_scripts=False):

source/blender/makesrna/RNA_define.hh
@@ -56,6 +56,7 @@ void RNA_define_fallback_property_update(int noteflag, const char *updatefunc);
 void RNA_define_lib_overridable(bool make_overridable);
 
 void RNA_init();
+void RNA_bpy_exit();
 void RNA_exit();
 
 /* Struct */

source/blender/makesrna/intern/rna_ID.cc
@@ -660,6 +660,9 @@ IDProperty **rna_PropertyGroup_idprops(PointerRNA *ptr)
 
 bool rna_PropertyGroup_unregister(Main * /*bmain*/, StructRNA *type)
 {
+  /* Ensure that a potential py object representing this RNA type is properly dereferenced. */
+  BPY_free_srna_pytype(type);
+
   RNA_struct_free(&BLENDER_RNA, type);
   return true;
 }

source/blender/makesrna/intern/rna_access.cc
@@ -55,6 +55,7 @@
 #include "RNA_define.hh"
 #include "RNA_enum_types.hh"
 #include "RNA_path.hh"
+#include "RNA_types.hh"
 
 #include "WM_api.hh"
 #include "WM_message.hh"
@@ -62,6 +63,10 @@
 /* flush updates */
 #include "DNA_object_types.h"
 #include "WM_types.hh"
+
+#ifdef WITH_PYTHON
+#  include "BPY_extern.hh"
+#endif
 
 #include "rna_access_internal.hh"
 #include "rna_internal.hh"
@@ -93,6 +98,21 @@ void RNA_init()
     BLI_ghash_insert(BLENDER_RNA.structs_map, (void *)srna->identifier, srna);
     BLENDER_RNA.structs_len += 1;
   }
+}
+
+void RNA_bpy_exit()
+{
+#ifdef WITH_PYTHON
+  StructRNA *srna;
+
+  for (srna = static_cast<StructRNA *>(BLENDER_RNA.structs.first); srna;
+       srna = static_cast<StructRNA *>(srna->cont.next))
+  {
+    /* NOTE(@ideasman42): each call locks the Python's GIL. Only locking/unlocking once
+     * is possible but gives barely measurable speedup (< ~1millisecond) so leave as-is. */
+    BPY_free_srna_pytype(srna);
+  }
+#endif
 }
 
 void RNA_exit()

source/blender/makesrna/intern/rna_define.cc
@@ -789,21 +789,19 @@ void RNA_struct_free(BlenderRNA *brna, StructRNA *srna)
   PropertyRNA *prop, *nextprop;
   PropertyRNA *parm, *nextparm;
 
-#  if 0
   if (srna->flag & STRUCT_RUNTIME) {
     if (RNA_struct_py_type_get(srna)) {
       /* NOTE: Since this is called after finalizing python/BPY in WM_exit process, it may end
        * up accessing freed memory in `srna->identifier`, which will trigger an ASAN crash. */
       const char *srna_identifier = "UNKNOWN";
-#    ifndef WITH_ASAN
+#  ifndef WITH_ASAN
       srna_identifier = srna->identifier;
-#    endif
+#  endif
       fprintf(stderr,
               "RNA Struct definition '%s' freed while holding a Python reference.\n",
               srna_identifier);
     }
   }
-#  endif
 
   for (prop = static_cast<PropertyRNA *>(srna->cont.properties.first); prop; prop = nextprop) {
     nextprop = prop->next;

source/blender/python/BPY_extern.hh
@@ -22,6 +22,7 @@ struct bConstraintTarget; /* DNA_constraint_types.h */
 struct bContext;
 struct bContextDataResult;
 struct bPythonConstraint; /* DNA_constraint_types.h */
+struct StructRNA;
 struct wmWindowManager;
 
 #include "BLI_utildefines.h"
@@ -122,6 +123,12 @@ void BPY_context_dict_clear_members_array(void **dict_p,
                                           uint context_members_len);
 
 void BPY_id_release(ID *id);
+
+/**
+ * Free (actually dereference) the Python type object representing the given #StrucRNA type,
+ * if it is defined.
+ */
+void BPY_free_srna_pytype(StructRNA *srna);
 
 /**
  * Avoids duplicating keyword list.

source/blender/python/intern/bpy.cc
@@ -765,10 +765,6 @@ void BPy_init_modules(bContext *C)
 
   PointerRNA ctx_ptr = RNA_pointer_create(nullptr, &RNA_Context, C);
   bpy_context_module = (BPy_StructRNA *)pyrna_struct_CreatePyObject(&ctx_ptr);
-  /* odd that this is needed, 1 ref on creation and another for the module
-   * but without we get a crash on exit */
-  Py_INCREF(bpy_context_module);
-
   PyModule_AddObject(mod, "context", (PyObject *)bpy_context_module);
 
   /* Register methods and property get/set for RNA types. */

commit 1bc632efecc697a758c5f852170e34baac9040f6

Fix #129177: name 'bpy' is not defined at exit
Own regression in f3f4ded331e320a364457336df16b8825239d423.

source/blender/windowmanager/intern/wm_init_exit.cc
@@ -534,7 +534,7 @@ void WM_exit_ex(bContext *C, const bool do_python_exit, const bool do_user_exit_
    */
   if (C && CTX_py_init_get(C)) {
     /* Calls `addon_utils.disable_all()` as well as unregistering all "startup" modules.  */
-    const char *imports[] = {"bpy.utils", nullptr};
+    const char *imports[] = {"bpy", "bpy.utils", nullptr};
     BPY_run_string_eval(C, imports, "bpy.utils._on_exit()");
   }
 #endif