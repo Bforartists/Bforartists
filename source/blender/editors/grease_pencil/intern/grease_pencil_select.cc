Bforartists/Bforartists

commit bd92b5faa5c0660581300de7a518798d77f7c1b3

Fix #129142: GPv3: gesture select tools selecting only right handles
When selection type is set/and for gesture selection tools, Instead of
current attribute, all three selection attributes (`.selection`, `left_handle`,
`right_handle`) are reset in every iteration of for loop.

Pull Request: https://projects.blender.org/blender/blender/pulls/129147

source/blender/editors/grease_pencil/intern/grease_pencil_select.cc
@@ -265,13 +265,9 @@ bool selection_update(const ViewContext *vc,
 
         /* Modes that un-set all elements not in the mask. */
         if (ELEM(sel_op, SEL_OP_SET, SEL_OP_AND)) {
-          ed::curves::foreach_selection_attribute_writer(
-              curves, selection_domain, [&](bke::GSpanAttributeWriter &writer) {
-                for (const int element_i : IndexRange(writer.span.size())) {
-                  ed::curves::apply_selection_operation_at_index(
-                      writer.span, element_i, SEL_OP_SUB);
-                }
-              });
+          bke::GSpanAttributeWriter selection =
+              curves.attributes_for_write().lookup_for_write_span(attribute_name);
+          ed::curves::fill_selection_false(selection.span);
         }
 
         if (use_segment_selection) {

commit d6e7a962dc0ed278324331b941d1edbc06376eac

Fix #129333: GPv3: Crash when selecting bezier curves
Introduced by bd92b5faa5c0660581300de7a518798d77f7c1b3.
The issue was that the code didn't check if the selection attribute
exists which it might not.

Pull Request: https://projects.blender.org/blender/blender/pulls/129623

source/blender/editors/grease_pencil/intern/grease_pencil_select.cc
@@ -265,15 +265,17 @@ bool selection_update(const ViewContext *vc,
 
         /* Modes that un-set all elements not in the mask. */
         if (ELEM(sel_op, SEL_OP_SET, SEL_OP_AND)) {
-          bke::GSpanAttributeWriter selection =
-              curves.attributes_for_write().lookup_for_write_span(attribute_name);
-          ed::curves::fill_selection_false(selection.span);
+          if (bke::GSpanAttributeWriter selection =
+                  curves.attributes_for_write().lookup_for_write_span(attribute_name))
+          {
+            ed::curves::fill_selection_false(selection.span);
+            selection.finish();
+          }
         }
 
         if (use_segment_selection) {
           /* Range of points in tree data matching this curve, for re-using screen space
-           * positions.
-           */
+           * positions. */
           const IndexRange tree_data_range = tree_data_by_drawing[i_drawing];
           changed |= ed::greasepencil::apply_mask_as_segment_selection(curves,
                                                                        changed_element_mask,