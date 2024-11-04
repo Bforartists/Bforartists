Bforartists/Bforartists

commit 822ad528186753bd7d00c7326381c230d47d376d

Anim: update collada export code for slotted actions
The collada export code was directly using `action->curves` in its
export code, which doesn't work with slotted actions. This commit
updates that code to use wrapper functions that provide access to the
correct fcurves regardless of whether the action is slotted or not.

Note that the import code has not yet been updated, and is left for
a future PR.

Ref: #123424
Pull Request: https://projects.blender.org/blender/blender/pulls/128061

source/blender/io/collada/collada_utils.cpp
@@ -855,9 +855,9 @@ bool bc_is_animated(BCMatrixSampleMap &values)
 bool bc_has_animations(Object *ob)
 {
   /* Check for object, light and camera transform animations */
-  if ((bc_getSceneObjectAction(ob) && bc_getSceneObjectAction(ob)->curves.first) ||
-      (bc_getSceneLightAction(ob) && bc_getSceneLightAction(ob)->curves.first) ||
-      (bc_getSceneCameraAction(ob) && bc_getSceneCameraAction(ob)->curves.first))
+  if (blender::animrig::legacy::assigned_action_has_keyframes(ob->adt) ||
+      blender::animrig::legacy::assigned_action_has_keyframes(bc_getSceneLightAnimData(ob)) ||
+      blender::animrig::legacy::assigned_action_has_keyframes(bc_getSceneCameraAnimData(ob)))
   {
     return true;
   }
@@ -868,13 +868,13 @@ bool bc_has_animations(Object *ob)
     if (!ma) {
       continue;
     }
-    if (ma->adt && ma->adt->action && ma->adt->action->curves.first) {
+    if (blender::animrig::legacy::assigned_action_has_keyframes(bc_getSceneMaterialAnimData(ma))) {
       return true;
     }
   }
 
   Key *key = BKE_key_from_object(ob);
-  if ((key && key->adt && key->adt->action) && key->adt->action->curves.first) {
+  if (blender::animrig::legacy::assigned_action_has_keyframes(key->adt)) {
     return true;
   }
 

source/blender/animrig/ANIM_action_legacy.hh
@@ -75,4 +75,15 @@ Vector<const FCurve *> fcurves_for_action_slot(const bAction *action, slot_handl
 Vector<FCurve *> fcurves_for_assigned_action(AnimData *adt);
 Vector<const FCurve *> fcurves_for_assigned_action(const AnimData *adt);
 
+/**
+ * Return whether the action (+slot), if any, assigned to `adt` has keyframes.
+ *
+ * This works for both layered and legacy actions. For layered actions this only
+ * considers the assigned slot.
+ *
+ * A null `adt` or a lack of assigned action are both handled, and are
+ * considered to mean no key frames (and thus will return false).
+ */
+bool assigned_action_has_keyframes(AnimData *adt);
+
 }  // namespace blender::animrig::legacy

source/blender/animrig/intern/action_legacy.cc
@@ -194,4 +194,19 @@ Vector<const FCurve *> fcurves_for_assigned_action(const AnimData *adt)
                                          adt->slot_handle);
 }
 
+bool assigned_action_has_keyframes(AnimData *adt)
+{
+  if (adt == nullptr || adt->action == nullptr) {
+    return false;
+  }
+
+  Action &action = adt->action->wrap();
+
+  if (action.is_action_legacy()) {
+    return action.curves.first != nullptr;
+  }
+
+  return action.has_keyframes(adt->slot_handle);
+}
+
 }  // namespace blender::animrig::legacy

source/blender/io/collada/AnimationExporter.cpp
@@ -816,22 +816,15 @@ std::string AnimationExporter::get_collada_sid(const BCAnimationCurve &curve,
  * So we have to update BCSample for this to work. */
 void AnimationExporter::export_morph_animation(Object *ob, BCAnimationSampler &sampler)
 {
-  FCurve *fcu;
   Key *key = BKE_key_from_object(ob);
   if (!key) {
     return;
   }
 
-  if (key->adt && key->adt->action) {
-    fcu = (FCurve *)key->adt->action->curves.first;
+  for (FCurve *fcu : blender::animrig::legacy::fcurves_for_assigned_action(key->adt)) {
+    BC_animation_transform_type tm_type = get_transform_type(fcu->rna_path);
 
-    while (fcu) {
-      BC_animation_transform_type tm_type = get_transform_type(fcu->rna_path);
-
-      create_keyframed_animation(ob, fcu, tm_type, true, sampler);
-
-      fcu = fcu->next;
-    }
+    create_keyframed_animation(ob, fcu, tm_type, true, sampler);
   }
 }
 #endif

source/blender/io/collada/BCAnimationSampler.cpp
@@ -28,6 +28,8 @@
 
 #include "ED_object.hh"
 
+#include "ANIM_action_legacy.hh"
+
 static std::string EMPTY_STRING;
 static BCAnimationCurveMap BCEmptyAnimationCurves;
 
@@ -98,15 +100,13 @@ static bool is_object_keyframe(Object *ob, int frame_index)
   return false;
 }
 
-static void add_keyframes_from(bAction *action, BCFrameSet &frameset)
+static void add_keyframes_from(AnimData *adt, BCFrameSet &frameset)
 {
-  if (action) {
-    LISTBASE_FOREACH (FCurve *, fcu, &action->curves) {
-      BezTriple *bezt = fcu->bezt;
-      for (int i = 0; i < fcu->totvert; bezt++, i++) {
-        int frame_index = nearbyint(bezt->vec[1][0]);
-        frameset.insert(frame_index);
-      }
+  for (FCurve *fcu : blender::animrig::legacy::fcurves_for_assigned_action(adt)) {
+    BezTriple *bezt = fcu->bezt;
+    for (int i = 0; i < fcu->totvert; bezt++, i++) {
+      int frame_index = nearbyint(bezt->vec[1][0]);
+      frameset.insert(frame_index);
     }
   }
 }
@@ -402,13 +402,13 @@ void BCAnimationSampler::generate_transforms(Object *ob, Bone *bone, BCAnimation
 void BCAnimationSampler::initialize_keyframes(BCFrameSet &frameset, Object *ob)
 {
   frameset.clear();
-  add_keyframes_from(bc_getSceneObjectAction(ob), frameset);
-  add_keyframes_from(bc_getSceneCameraAction(ob), frameset);
-  add_keyframes_from(bc_getSceneLightAction(ob), frameset);
+  add_keyframes_from(ob->adt, frameset);
+  add_keyframes_from(bc_getSceneCameraAnimData(ob), frameset);
+  add_keyframes_from(bc_getSceneLightAnimData(ob), frameset);
 
   for (int a = 0; a < ob->totcol; a++) {
     Material *ma = BKE_object_material_get(ob, a + 1);
-    add_keyframes_from(bc_getSceneMaterialAction(ma), frameset);
+    add_keyframes_from(bc_getSceneMaterialAnimData(ma), frameset);
   }
 }
 
@@ -416,23 +416,18 @@ void BCAnimationSampler::initialize_curves(BCAnimationCurveMap &curves, Object *
 {
   BC_animation_type object_type = BC_ANIMATION_TYPE_OBJECT;
 
-  bAction *action = bc_getSceneObjectAction(ob);
-  if (action) {
-    FCurve *fcu = (FCurve *)action->curves.first;
-
-    for (; fcu; fcu = fcu->next) {
-      object_type = BC_ANIMATION_TYPE_OBJECT;
-      if (ob->type == OB_ARMATURE) {
-        char boneName[MAXBONENAME];
-        if (BLI_str_quoted_substr(fcu->rna_path, "pose.bones[", boneName, sizeof(boneName))) {
-          object_type = BC_ANIMATION_TYPE_BONE;
-        }
+  for (FCurve *fcu : blender::animrig::legacy::fcurves_for_assigned_action(ob->adt)) {
+    object_type = BC_ANIMATION_TYPE_OBJECT;
+    if (ob->type == OB_ARMATURE) {
+      char boneName[MAXBONENAME];
+      if (BLI_str_quoted_substr(fcu->rna_path, "pose.bones[", boneName, sizeof(boneName))) {
+        object_type = BC_ANIMATION_TYPE_BONE;
       }
-
-      /* Adding action curves on object */
-      BCCurveKey key(object_type, fcu->rna_path, fcu->array_index);
-      curves[key] = new BCAnimationCurve(key, ob, fcu);
     }
+
+    /* Adding action curves on object */
+    BCCurveKey key(object_type, fcu->rna_path, fcu->array_index);
+    curves[key] = new BCAnimationCurve(key, ob, fcu);
   }
 
   /* Add missing curves */
@@ -446,23 +441,20 @@ void BCAnimationSampler::initialize_curves(BCAnimationCurveMap &curves, Object *
   }
 
   /* Add curves on Object->data actions */
-  action = nullptr;
+  AnimData *adt = nullptr;
   if (ob->type == OB_CAMERA) {
-    action = bc_getSceneCameraAction(ob);
+    adt = bc_getSceneCameraAnimData(ob);
     object_type = BC_ANIMATION_TYPE_CAMERA;
   }
   else if (ob->type == OB_LAMP) {
-    action = bc_getSceneLightAction(ob);
+    adt = bc_getSceneLightAnimData(ob);
     object_type = BC_ANIMATION_TYPE_LIGHT;
   }
 
-  if (action) {
-    /* Add light action or Camera action */
-    FCurve *fcu = (FCurve *)action->curves.first;
-    for (; fcu; fcu = fcu->next) {
-      BCCurveKey key(object_type, fcu->rna_path, fcu->array_index);
-      curves[key] = new BCAnimationCurve(key, ob, fcu);
-    }
+  /* Add light action or Camera action */
+  for (FCurve *fcu : blender::animrig::legacy::fcurves_for_assigned_action(adt)) {
+    BCCurveKey key(object_type, fcu->rna_path, fcu->array_index);
+    curves[key] = new BCAnimationCurve(key, ob, fcu);
   }
 
   /* Add curves on Object->material actions. */
@@ -471,14 +463,11 @@ void BCAnimationSampler::initialize_curves(BCAnimationCurveMap &curves, Object *
     /* Export Material parameter animations. */
     Material *ma = BKE_object_material_get(ob, a + 1);
     if (ma) {
-      action = bc_getSceneMaterialAction(ma);
-      if (action) {
-        // isMatAnim = true;
-        FCurve *fcu = (FCurve *)action->curves.first;
-        for (; fcu; fcu = fcu->next) {
-          BCCurveKey key(object_type, fcu->rna_path, fcu->array_index, a);
-          curves[key] = new BCAnimationCurve(key, ob, fcu);
-        }
+      adt = bc_getSceneMaterialAnimData(ma);
+      // isMatAnim = true;
+      for (FCurve *fcu : blender::animrig::legacy::fcurves_for_assigned_action(adt)) {
+        BCCurveKey key(object_type, fcu->rna_path, fcu->array_index, a);
+        curves[key] = new BCAnimationCurve(key, ob, fcu);
       }
     }
   }

source/blender/io/collada/collada_utils.h
@@ -70,43 +70,44 @@ extern void bc_update_scene(BlenderContext &blender_context, float ctime);
 
 std::vector<bAction *> bc_getSceneActions(const bContext *C, Object *ob, bool all_actions);
 
-/* Action helpers */
+/* Action and Animdata helpers */
 
+/* Return Object's Action or NULL. */
 inline bAction *bc_getSceneObjectAction(Object *ob)
 {
   return (ob->adt && ob->adt->action) ? ob->adt->action : NULL;
 }
 
-/* Returns Light Action or NULL */
-inline bAction *bc_getSceneLightAction(Object *ob)
+/* Return Light's AnimData or NULL. */
+inline AnimData *bc_getSceneLightAnimData(Object *ob)
 {
   if (ob->type != OB_LAMP) {
     return NULL;
   }
 
   Light *lamp = (Light *)ob->data;
-  return (lamp->adt && lamp->adt->action) ? lamp->adt->action : NULL;
+  return lamp->adt;
 }
 
-/* Return Camera Action or NULL */
-inline bAction *bc_getSceneCameraAction(Object *ob)
+/* Return Camera's AnimData or NULL. */
+inline AnimData *bc_getSceneCameraAnimData(Object *ob)
 {
   if (ob->type != OB_CAMERA) {
     return NULL;
   }
 
   const Camera *camera = (const Camera *)ob->data;
-  return (camera->adt && camera->adt->action) ? camera->adt->action : NULL;
+  return camera->adt;
 }
 
-/* returns material action or NULL */
-inline bAction *bc_getSceneMaterialAction(Material *ma)
+/* Return Material's AnimData or NULL. */
+inline AnimData *bc_getSceneMaterialAnimData(Material *ma)
 {
   if (ma == NULL) {
     return NULL;
   }
 
-  return (ma->adt && ma->adt->action) ? ma->adt->action : NULL;
+  return ma->adt;
 }
 
 std::string bc_get_action_id(std::string action_name,

commit a16efbaae8e1b1c3f1e03e072f58d27440b949df

Fix #129725: Handle case of no animation keys during Collada export
Regression from 822ad528186753bd7d00c7326381c230d47d376d

Guard against `BKE_key_from_object` returning null.

Pull Request: https://projects.blender.org/blender/blender/pulls/129760

source/blender/io/collada/collada_utils.cpp
@@ -874,7 +874,7 @@ bool bc_has_animations(Object *ob)
   }
 
   Key *key = BKE_key_from_object(ob);
-  if (blender::animrig::legacy::assigned_action_has_keyframes(key->adt)) {
+  if (key && blender::animrig::legacy::assigned_action_has_keyframes(key->adt)) {
     return true;
   }
 