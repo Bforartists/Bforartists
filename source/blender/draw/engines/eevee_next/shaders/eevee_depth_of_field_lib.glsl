Bforartists/Bforartists

commit e41f64a900b4e338b77d71fcc7f847ef0ed01d04

GPU: Change global scope variable to defines to avoid overhead on Metal

source/blender/draw/engines/eevee_next/shaders/eevee_depth_of_field_lib.glsl
@@ -21,41 +21,41 @@
 #endif
 
 #ifdef DOF_RESOLVE_PASS
-const bool is_resolve = true;
+#  define IS_RESOLVE true
 #else
-const bool is_resolve = false;
+#  define IS_RESOLVE false
 #endif
 #ifdef DOF_FOREGROUND_PASS
-const bool is_foreground = DOF_FOREGROUND_PASS;
+#  define IS_FOREGROUND DOF_FOREGROUND_PASS
 #else
-const bool is_foreground = false;
+#  define IS_FOREGROUND false
 #endif
 /* Debug options */
-const bool debug_gather_perf = false;
-const bool debug_scatter_perf = false;
-const bool debug_resolve_perf = false;
+#define debug_gather_perf false
+#define debug_scatter_perf false
+#define debug_resolve_perf false
 
-const bool no_smooth_intersection = false;
-const bool no_gather_occlusion = false;
-const bool no_gather_mipmaps = false;
-const bool no_gather_random = false;
-const bool no_gather_filtering = false;
-const bool no_scatter_occlusion = false;
-const bool no_scatter_pass = false;
-const bool no_foreground_pass = false;
-const bool no_background_pass = false;
-const bool no_slight_focus_pass = false;
-const bool no_focus_pass = false;
-const bool no_hole_fill_pass = false;
+#define no_smooth_intersection false
+#define no_gather_occlusion false
+#define no_gather_mipmaps false
+#define no_gather_random false
+#define no_gather_filtering false
+#define no_scatter_occlusion false
+#define no_scatter_pass false
+#define no_foreground_pass false
+#define no_background_pass false
+#define no_slight_focus_pass false
+#define no_focus_pass false
+#define no_hole_fill_pass false
 
 /* Distribute weights between near/slight-focus/far fields (slide 117). */
-const float dof_layer_threshold = 4.0;
+#define dof_layer_threshold 4.0
 /* Make sure it overlaps. */
-const float dof_layer_offset_fg = 0.5 + 1.0;
+#define dof_layer_offset_fg 0.5 + 1.0
 /* Extra offset for convolution layers to avoid light leaking from background. */
-const float dof_layer_offset = 0.5 + 0.5;
+#define dof_layer_offset 0.5 + 0.5
 
-const int dof_max_slight_focus_radius = DOF_MAX_SLIGHT_FOCUS_RADIUS;
+#define dof_max_slight_focus_radius DOF_MAX_SLIGHT_FOCUS_RADIUS
 
 const uvec2 quad_offsets_u[4] = uint2_array(uvec2(0, 1), uvec2(1, 1), uvec2(1, 0), uvec2(0, 0));
 const vec2 quad_offsets[4] = float2_array(
@@ -135,7 +135,7 @@ float dof_coc_from_depth(DepthOfFieldData dof_data, vec2 uv, float depth)
 float dof_layer_weight(float coc, const bool is_foreground)
 {
   /* NOTE: These are full-resolution pixel CoC value. */
-  if (is_resolve) {
+  if (IS_RESOLVE) {
     return saturate(-abs(coc) + dof_layer_threshold + dof_layer_offset) *
            float(is_foreground ? (coc <= 0.5) : (coc > -0.5));
   }
@@ -193,7 +193,7 @@ struct CocTile {
 };
 
 /* WATCH: Might have to change depending on the texture format. */
-const float dof_tile_large_coc = 1024.0;
+#define dof_tile_large_coc 1024.0
 
 /* Init a CoC tile for reduction algorithms. */
 CocTile dof_coc_tile_init()

source/blender/compositor/realtime_compositor/shaders/compositor_motion_blur.glsl
@@ -15,7 +15,7 @@
 #include "gpu_shader_compositor_motion_blur_lib.glsl"
 #include "gpu_shader_compositor_texture_utilities.glsl"
 
-const float g_depth_scale = 100.0;
+#define g_depth_scale 100.0
 
 /* Interleaved gradient noise by Jorge Jimenez
  * http://www.iryoku.com/next-generation-post-processing-in-call-of-duty-advanced-warfare. */

source/blender/draw/engines/eevee_next/shaders/eevee_depth_of_field_accumulator_lib.glsl
@@ -24,15 +24,15 @@
 /* Quality options */
 #ifdef DOF_HOLEFILL_PASS
 /* No need for very high density for hole_fill. */
-const int gather_ring_count = 3;
-const int gather_ring_density = 3;
-const int gather_max_density_change = 0;
-const int gather_density_change_ring = 1;
+#  define gather_ring_count int(3)
+#  define gather_ring_density int(3)
+#  define gather_max_density_change int(0)
+#  define gather_density_change_ring int(1)
 #else
-const int gather_ring_count = DOF_GATHER_RING_COUNT;
-const int gather_ring_density = 3;
-const int gather_max_density_change = 50; /* Dictates the maximum good quality blur. */
-const int gather_density_change_ring = 1;
+#  define gather_ring_count int(DOF_GATHER_RING_COUNT)
+#  define gather_ring_density int(3)
+#  define gather_max_density_change int(50) /* Dictates the maximum good quality blur. */
+#  define gather_density_change_ring int(1)
 #endif
 
 /** \} */
@@ -41,14 +41,14 @@ const int gather_density_change_ring = 1;
 /** \name Constants.
  * \{ */
 
-const float unit_ring_radius = 1.0 / float(gather_ring_count);
-const float unit_sample_radius = 1.0 / float(gather_ring_count + 0.5);
-const float large_kernel_radius = 0.5 + float(gather_ring_count);
-const float smaller_kernel_radius = 0.5 + float(gather_ring_count - gather_density_change_ring);
+#define unit_ring_radius float(1.0 / float(gather_ring_count))
+#define unit_sample_radius float(1.0 / float(gather_ring_count + 0.5))
+#define large_kernel_radius float(0.5 + float(gather_ring_count))
+#define smaller_kernel_radius float(0.5 + float(gather_ring_count - gather_density_change_ring))
 /* NOTE(fclem) the bias is reducing issues with density change visible transition. */
-const float radius_downscale_factor = smaller_kernel_radius / large_kernel_radius;
-const int change_density_at_ring = (gather_ring_count - gather_density_change_ring + 1);
-const float coc_radius_error = 2.0;
+#define radius_downscale_factor float(smaller_kernel_radius / large_kernel_radius)
+#define change_density_at_ring int((gather_ring_count - gather_density_change_ring + 1))
+#define coc_radius_error float(2.0)
 
 /** \} */
 
@@ -231,7 +231,7 @@ void dof_gather_accumulate_sample_ring(DofGatherData ring_data,
    * cases. We might need to make it a parameter or find a relative bias. */
   float accum_occlu = saturate((ring_avg_coc - accum_avg_coc) * 0.1 - 1.0);
 
-  if (is_resolve) {
+  if (IS_RESOLVE) {
     ring_occlu = accum_occlu = 0.0;
   }
 
@@ -352,7 +352,7 @@ void dof_gather_accumulate_resolve(int total_sample_count,
   out_col = accum_data.color * weight_inv;
   out_occlusion = vec2(abs(accum_data.coc), accum_data.coc_sqr) * weight_inv;
 
-  if (is_foreground) {
+  if (IS_FOREGROUND) {
     out_weight = 1.0 - accum_data.transparency;
   }
   else if (accum_data.weight > 0.0) {
@@ -492,7 +492,7 @@ void dof_gather_accumulator(sampler2D color_tx,
         if (DOF_BOKEH_TEXTURE) {
           /* Scaling to 0.25 for speed. Improves texture cache hit. */
           offset_co = texture(bkh_lut_tx, offset_co * 0.25 + 0.5).rg;
-          offset_co *= (is_foreground) ? -dof_buf.bokeh_anisotropic_scale :
+          offset_co *= (IS_FOREGROUND) ? -dof_buf.bokeh_anisotropic_scale :
                                          dof_buf.bokeh_anisotropic_scale;
         }
         vec2 sample_co = center_co + offset_co * ring_radius;
@@ -512,12 +512,12 @@ void dof_gather_accumulator(sampler2D color_tx,
                                         isect_mul,
                                         first_ring,
                                         do_fast_gather,
-                                        is_foreground,
+                                        IS_FOREGROUND,
                                         ring_data,
                                         accum_data);
     }
 
-    if (is_foreground) {
+    if (IS_FOREGROUND) {
       /* Reduce issue with closer foreground over distant foreground. */
       /* TODO(fclem) this seems to not be completely correct as the issue remains. */
       float ring_area = (square(float(ring) + 0.5 + coc_radius_error) -
@@ -527,7 +527,7 @@ void dof_gather_accumulator(sampler2D color_tx,
     }
 
     dof_gather_accumulate_sample_ring(
-        ring_data, sample_pair_count * 2, first_ring, do_fast_gather, is_foreground, accum_data);
+        ring_data, sample_pair_count * 2, first_ring, do_fast_gather, IS_FOREGROUND, accum_data);
 
     first_ring = false;
 
@@ -541,7 +541,7 @@ void dof_gather_accumulator(sampler2D color_tx,
          * For that multiply old kernel data by its area divided by the new kernel area. */
         const float outer_rings_weight = 1.0 / (radius_downscale_factor * radius_downscale_factor);
         /* Samples are already weighted per ring in foreground pass. */
-        if (!is_foreground) {
+        if (!IS_FOREGROUND) {
           dof_gather_amend_weight(accum_data, outer_rings_weight);
         }
         /* Re-init kernel position & sampling parameters. */
@@ -568,7 +568,7 @@ void dof_gather_accumulator(sampler2D color_tx,
     float bordering_radius = (0.5 + coc_radius_error) * base_radius * unit_sample_radius;
 
     dof_gather_accumulate_center_sample(
-        center_data, bordering_radius, 0, do_fast_gather, is_foreground, false, accum_data);
+        center_data, bordering_radius, 0, do_fast_gather, IS_FOREGROUND, false, accum_data);
   }
 
   int total_sample_count = dof_gather_total_sample_count_with_density_change(

source/blender/draw/engines/eevee_next/shaders/eevee_depth_of_field_gather_comp.glsl
@@ -25,7 +25,7 @@ void main()
 
   float base_radius, min_radius, min_intersectable_radius;
   bool can_early_out;
-  if (is_foreground) {
+  if (IS_FOREGROUND) {
     base_radius = -coc_tile.fg_min_coc;
     min_radius = -coc_tile.fg_max_coc;
     min_intersectable_radius = -coc_tile.fg_max_intersectable_coc;
@@ -38,7 +38,7 @@ void main()
     can_early_out = !prediction.do_background;
   }
 
-  bool do_fast_gather = dof_do_fast_gather(base_radius, min_radius, is_foreground);
+  bool do_fast_gather = dof_do_fast_gather(base_radius, min_radius, IS_FOREGROUND);
 
   /* Gather at half resolution. Divide CoC by 2. */
   base_radius *= 0.5;

source/blender/draw/engines/eevee_next/shaders/eevee_depth_of_field_hole_fill_comp.glsl
@@ -25,7 +25,7 @@ void main()
   float min_intersectable_radius = dof_tile_large_coc;
   bool can_early_out = !prediction.do_hole_fill;
 
-  bool do_fast_gather = dof_do_fast_gather(base_radius, min_radius, is_foreground);
+  bool do_fast_gather = dof_do_fast_gather(base_radius, min_radius, IS_FOREGROUND);
 
   /* Gather at half resolution. Divide CoC by 2. */
   base_radius *= 0.5;

source/blender/draw/engines/eevee_next/shaders/eevee_depth_of_field_tiles_dilate_comp.glsl
@@ -13,8 +13,8 @@
 #include "eevee_depth_of_field_lib.glsl"
 
 /* Error introduced by the random offset of the gathering kernel's center. */
-const float bluring_radius_error = 1.0 + 1.0 / (float(DOF_GATHER_RING_COUNT) + 0.5);
-const float tile_to_fullres_factor = float(DOF_TILES_SIZE * 2);
+#define bluring_radius_error float(1.0 + 1.0 / (float(DOF_GATHER_RING_COUNT) + 0.5))
+#define tile_to_fullres_factor float(float(DOF_TILES_SIZE * 2))
 
 void main()
 {

source/blender/draw/engines/eevee_next/shaders/eevee_depth_of_field_tiles_flatten_comp.glsl
@@ -29,7 +29,7 @@ shared uint bg_min_coc;
 shared uint bg_max_coc;
 shared uint bg_min_intersectable_coc;
 
-uint dof_tile_large_coc_uint = floatBitsToUint(dof_tile_large_coc);
+#define dof_tile_large_coc_uint floatBitsToUint(dof_tile_large_coc)
 
 void main()
 {

source/blender/draw/engines/eevee_next/shaders/eevee_motion_blur_gather_comp.glsl
@@ -18,7 +18,7 @@
 #include "eevee_sampling_lib.glsl"
 #include "eevee_velocity_lib.glsl"
 
-const int gather_sample_count = 8;
+#define gather_sample_count 8
 
 /* Converts uv velocity into pixel space. Assumes velocity_tx is the same resolution as the
  * target post-FX frame-buffer. */

commit 30d8ad65f9ea7cc60109b35df9282e14b5575033

Fix: EEVEE: Depth of Field regression
Fix regression introduced by e41f64a900b4e338b77d71fcc7f847ef0ed01d04

source/blender/draw/engines/eevee_next/shaders/eevee_depth_of_field_lib.glsl
@@ -49,11 +49,11 @@
 #define no_hole_fill_pass false
 
 /* Distribute weights between near/slight-focus/far fields (slide 117). */
-#define dof_layer_threshold 4.0
+#define dof_layer_threshold (4.0)
 /* Make sure it overlaps. */
-#define dof_layer_offset_fg 0.5 + 1.0
+#define dof_layer_offset_fg (0.5 + 1.0)
 /* Extra offset for convolution layers to avoid light leaking from background. */
-#define dof_layer_offset 0.5 + 0.5
+#define dof_layer_offset (0.5 + 0.5)
 
 #define dof_max_slight_focus_radius DOF_MAX_SLIGHT_FOCUS_RADIUS
 