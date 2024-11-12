blender/blender

commit 78b9218c9851aaa5b438141cd8009911f87a1628

I18n: Fix translations for Rigify
Rigify was recently added to the core add-ons, and is now
translatable. This change fixes many issues that made messages
poorly or not translatable.

- Replace f-strings and % formatting with str.format().
- Use iface_ and rpt_ to translate formatted strings.
- Rename metarig submenus from "<Category> (submenu)" to simply
  "<Category>" to allow the menu entries and submenus to have the same
  name and be translated. The label was awkward anyway in my opinion.
- Remove trailing "." in operator reports.

scripts/addons_core/rigify/rigs/limbs/limb_rigs.py
@@ -985,7 +985,7 @@ class BaseLimbRig(BaseRig):
         ControlLayersOption.TWEAK.add_parameters(params)
 
     @classmethod
-    def parameters_ui(cls, layout, params, end='End'):
+    def parameters_ui(cls, layout, params, end="End"):
         """ Create the ui for the rig parameters."""
 
         r = layout.row()
@@ -993,7 +993,12 @@ class BaseLimbRig(BaseRig):
 
         if 'auto' not in params.rotation_axis.lower():
             r = layout.row()
-            r.prop(params, "auto_align_extremity", text="Auto Align " + end)
+            # Not a great translation but other "end" types can be specified.
+            if end == "End":
+                text = iface_("Auto Align End")
+            else:
+                text = iface_("Auto Align {:s}").format(end)
+            r.prop(params, "auto_align_extremity", text=text, translate=False)
 
         r = layout.row()
         r.prop(params, "segments")

scripts/addons_core/rigify/__init__.py
@@ -313,11 +313,11 @@ def check_feature_set_error(_feature_set: RigifyFeatureSets, info: dict, layout:
             split.label(text="Error:")
             sub = split.row()
             sub.alert = True
-            sub.label(
-                text=iface_("This feature set requires Blender %s or newer to work properly."
-                            ) % ".".join(str(x) for x in info['blender']),
-                icon='ERROR', translate=False
+            text = (
+                iface_("This feature set requires Blender {:s} or newer to work properly.")
+                .format(".".join(str(x) for x in info['blender']))
             )
+            sub.label(icon='ERROR', text=text, translate=False)
 
     for dep_link in info.get("dependencies", []):
         if not feature_set_list.get_module_by_link_safe(dep_link):
@@ -377,7 +377,7 @@ def draw_feature_set_prefs(layout: bpy.types.UILayout, _context: bpy.types.Conte
     if 'author' in info:
         split = col.row().split(factor=split_factor)
         split.label(text="Author:")
-        split.label(text=info["author"])
+        split.label(text=info["author"], translate=False)
 
     if 'version' in info:
         split = col.row().split(factor=split_factor)
@@ -425,21 +425,21 @@ class RigifyColorSet(bpy.types.PropertyGroup):
         subtype='COLOR',
         default=(1.0, 1.0, 1.0),
         min=0.0, max=1.0,
-        description="color picker"
+        description="Color picker"
     )
     normal: FloatVectorProperty(
         name="object_color",
         subtype='COLOR',
         default=(1.0, 1.0, 1.0),
         min=0.0, max=1.0,
-        description="color picker"
+        description="Color picker"
     )
     select: FloatVectorProperty(
         name="object_color",
         subtype='COLOR',
         default=(1.0, 1.0, 1.0),
         min=0.0, max=1.0,
-        description="color picker"
+        description="Color picker"
     )
     standard_colors_lock: BoolProperty(default=True)
 

scripts/addons_core/rigify/feature_set_list.py
@@ -6,6 +6,8 @@ from typing import TYPE_CHECKING, List, Sequence, Optional
 
 import bpy
 from bpy.props import StringProperty
+from bpy.app.translations import pgettext_rpt as rpt_
+
 import os
 import re
 import importlib
@@ -246,27 +248,27 @@ class DATA_OT_rigify_add_feature_set(bpy.types.Operator):
             base_dirname, init_found, data_found = verify_feature_set_archive(zip_archive)
 
             if not base_dirname:
-                self.report({'ERROR'}, "The feature set archive must contain one base directory.")
+                self.report({'ERROR'}, "The feature set archive must contain one base directory")
                 return {'CANCELLED'}
 
             # Patch up some invalid characters to allow using 'Download ZIP' on GitHub.
             fixed_dirname = re.sub(r'[.-]', '_', base_dirname)
 
             if not re.fullmatch(r'[a-zA-Z][a-zA-Z_0-9]*', fixed_dirname):
-                self.report({'ERROR'},
-                            f"The feature set archive base directory name is not a valid "
-                            f"identifier: '{base_dirname}'.")
+                message = rpt_("The feature set archive base directory name is not a valid "
+                               "identifier: '{:s}'").format(base_dirname)
+                self.report({'ERROR'}, message)
                 return {'CANCELLED'}
 
             if fixed_dirname == DEFAULT_NAME:
-                self.report(
-                    {'ERROR'}, f"The '{DEFAULT_NAME}' name is not allowed for feature sets.")
+                message = rpt_("The name '{:s}' is not allowed for feature sets").format(DEFAULT_NAME)
+                self.report({'ERROR'}, message)
                 return {'CANCELLED'}
 
             if not init_found or not data_found:
                 self.report(
                     {'ERROR'},
-                    "The feature set archive has no rigs or metarigs, or is missing __init__.py.")
+                    "The feature set archive has no rigs or metarigs, or is missing __init__.py")
                 return {'CANCELLED'}
 
             base_dir = os.path.join(rigify_config_path, base_dirname)
@@ -274,7 +276,8 @@ class DATA_OT_rigify_add_feature_set(bpy.types.Operator):
 
             for path, name in [(base_dir, base_dirname), (fixed_dir, fixed_dirname)]:
                 if os.path.exists(path):
-                    self.report({'ERROR'}, f"Feature set directory already exists: '{name}'.")
+                    message = rpt_("Feature set directory already exists: '{:s}'").format(name)
+                    self.report({'ERROR'}, message)
                     return {'CANCELLED'}
 
             # Unpack the validated archive and fix the directory name if necessary

scripts/addons_core/rigify/metarig_menu.py
@@ -190,14 +190,14 @@ def create_armature_submenus(dic: dict | None = None):
 
         armature_submenus.append(type('Class_' + metarig_category + '_submenu',
                                       (ArmatureSubMenu,), {}))
-        armature_submenus[-1].bl_label = metarig_category + ' (submenu)'
+        armature_submenus[-1].bl_label = metarig_category
         armature_submenus[-1].bl_idname = 'ARMATURE_MT_%s_class' % metarig_category
         armature_submenus[-1].operators = []
         menu_funcs += [make_submenu_func(armature_submenus[-1].bl_idname, metarig_category)]
 
         for mop, name in metarig_ops[metarig_category]:
             arm_sub = next((e for e in armature_submenus
-                            if e.bl_label == metarig_category + ' (submenu)'),
+                            if e.bl_label == metarig_category),
                            '')
             arm_sub.operators.append((mop.bl_idname, name,))
 

scripts/addons_core/rigify/operators/action_layers.py
@@ -9,6 +9,10 @@ from typing import Tuple, Optional, Sequence, Any
 from bpy.types import PropertyGroup, Action, UIList, UILayout, Context, Panel, Operator, Armature
 from bpy.props import (EnumProperty, IntProperty, BoolProperty, StringProperty, FloatProperty,
                        PointerProperty, CollectionProperty)
+from bpy.app.translations import (
+    pgettext_iface as iface_,
+    pgettext_rpt as rpt_,
+)
 
 from .generic_ui_list import draw_ui_list
 
@@ -183,7 +187,7 @@ def find_duplicate_slot(metarig_data: Armature, action_slot: ActionSlot) -> Opti
 
 # noinspection PyPep8Naming
 class RIGIFY_OT_action_create(Operator):
-    """Create new Action"""
+    """Create a new action"""
     # This is needed because bpy.ops.action.new() has a poll function that blocks
     # the operator unless it's drawn in an animation UI panel.
 
@@ -430,7 +434,8 @@ class DATA_PT_rigify_actions(Panel):
                 row = layout.split(factor=0.4)
                 row.column()
                 row.alert = True
-                row.label(text=f"Bone not found: {slot.subtarget}", icon='ERROR')
+                text = rpt_("Bone not found: {:s}").format(slot.subtarget)
+                row.label(text=text, translate=False, icon='ERROR')
         else:
             row.prop(slot, 'subtarget', icon=bone_icon)
 
@@ -441,7 +446,8 @@ class DATA_PT_rigify_actions(Panel):
 
             row = layout.row()
             row.use_property_split = True
-            row.prop(slot, 'symmetrical', text=f"Symmetrical ({flipped_subtarget})")
+            text = iface_("Symmetrical ({:s})").format(flipped_subtarget)
+            row.prop(slot, 'symmetrical', text=text, translate=False)
 
             if slot.symmetrical and not flipped_subtarget_exists:
                 row.alert = True
@@ -449,7 +455,8 @@ class DATA_PT_rigify_actions(Panel):
                 row = layout.split(factor=0.4)
                 row.column()
                 row.alert = True
-                row.label(text=f"Bone not found: {flipped_subtarget}", icon='ERROR')
+                text = rpt_("Bone not found: {:s}").format(flipped_subtarget)
+                row.label(text=text, icon='ERROR', translate=False)
 
         layout.prop(slot, 'frame_start', text="Frame Start")
         layout.prop(slot, 'frame_end', text="End")
@@ -475,23 +482,27 @@ class DATA_PT_rigify_actions(Panel):
         if slot.trans_min == slot.trans_max:
             col = split.column(align=True)
             col.alert = True
-            col.label(text="Min and max value are the same!")
-            col.label(text=f"Will be stuck reading frame {slot.frame_start}!")
+            text = rpt_("Min and max values are the same!")
+            col.label(text=text, translate=False)
+            text = rpt_("Will be stuck reading frame {:d}!").format(slot.frame_start)
+            col.label(text=text, translate=False)
             return
 
         if slot.frame_start == slot.frame_end:
             col = split.column(align=True)
             col.alert = True
-            col.label(text="Start and end frame cannot be the same!")
+            text = rpt_("Start and end frame cannot be the same!")
+            col.label(text=text, translate=False)
 
         default_frame = slot.get_default_frame()
 
         if slot.is_default_frame_integer():
-            split.label(text=f"Default Frame: {round(default_frame)}")
+            text = rpt_("Default Frame: {:.0f}").format(default_frame)
+            split.label(text=text, translate=False)
         else:
             split.alert = True
-            split.label(text=f"Default Frame: {round(default_frame, 2)} "
-                             "(Should be a whole number!)")
+            text = rpt_("Default Frame: {:.02f} (Should be a whole number!)").format(default_frame)
+            split.label(text=text, translate=False)
 
 
 # =============================================

scripts/addons_core/rigify/operators/copy_mirror_parameters.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import bpy
+from bpy.app.translations import pgettext_rpt as rpt_
 import importlib
 
 from ..utils.layers import REFS_TOGGLE_SUFFIX, REFS_LIST_SUFFIX, is_collection_ref_list_prop, copy_ref_list
@@ -47,8 +48,9 @@ class POSE_OT_rigify_copy_single_parameter(bpy.types.Operator):
             module = importlib.import_module(self.module_name)
             filter_rig_class = getattr(module, self.class_name)
         except (KeyError, AttributeError, ImportError):
+            message = rpt_("Cannot find class {:s} in {:s}").format(self.class_name, self.module_name)
             self.report(
-                {'ERROR'}, f"Cannot find class {self.class_name} in {self.module_name}")
+                {'ERROR'}, message)
             return {'CANCELLED'}
 
         active_pbone = context.active_pose_bone
@@ -97,10 +99,11 @@ class POSE_OT_rigify_copy_single_parameter(bpy.types.Operator):
                     num_copied += 1
 
         if num_copied:
-            self.report({'INFO'}, f"Copied the value to {num_copied} bones.")
+            message = rpt_("Copied the value to {:d} bones").format(num_copied)
+            self.report({'INFO'}, message)
             return {'FINISHED'}
         else:
-            self.report({'WARNING'}, "No suitable selected bones to copy to.")
+            self.report({'WARNING'}, "No suitable selected bones to copy to")
             return {'CANCELLED'}
 
 
@@ -200,10 +203,9 @@ class POSE_OT_rigify_mirror_parameters(bpy.types.Operator):
                 # Bones without an opposite will just be ignored.
                 continue
             if flip_bone != pb and flip_bone.bone.select:
-                self.report(
-                    {'ERROR'},
-                    f"Bone {pb.name} selected on both sides, mirroring would be ambiguous, "
-                    f"aborting. Only select the left or right side, not both!")
+                message = rpt_("Bone {:s} selected on both sides, mirroring would be ambiguous, "
+                               "aborting. Only select the left or right side, not both").format(pb.name)
+                self.report({'ERROR'}, message)
                 return {'CANCELLED'}
 
         # Then mirror the parameters.
@@ -215,7 +217,8 @@ class POSE_OT_rigify_mirror_parameters(bpy.types.Operator):
 
             num_mirrored += copy_rigify_params(pb, flip_bone, match_type=False, x_mirror=True)
 
-        self.report({'INFO'}, f"Mirrored parameters of {num_mirrored} bones.")
+        message = rpt_("Mirrored parameters of {:d} bones").format(num_mirrored)
+        self.report({'INFO'}, message)
 
         return {'FINISHED'}
 
@@ -260,8 +263,8 @@ class POSE_OT_rigify_copy_parameters(bpy.types.Operator):
                 continue
             num_copied += copy_rigify_params(active_bone, pb, match_type=self.match_type)
 
-        self.report({'INFO'},
-                    f"Copied {get_rigify_type(active_bone)} parameters to {num_copied} bones.")
+        message = rpt_("Copied {:s} parameters to {:d} bones").format(get_rigify_type(active_bone), num_copied)
+        self.report({'INFO'}, message)
 
         return {'FINISHED'}
 

scripts/addons_core/rigify/rigs/limbs/spline_tentacle.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import bpy
+from bpy.app.translations import pgettext_iface as iface_
 
 import re
 import itertools
@@ -1120,9 +1121,14 @@ def add_spline_snap_ik_to_fk(panel: 'PanelLayout', *,
         'use_stretch': use_stretch,
     }
 
+    text = iface_("IK->FK ({:s})").format(rig_name)
     panel.operator(
-        'pose.rigify_spline_tentacle_ik2fk_{rig_id}',
-        text=f'IK->FK ({rig_name})', icon='SNAP_ON', properties=op_props)
+        f"pose.rigify_spline_tentacle_ik2fk_{rig_id}",
+        text=text,
+        translate=False,
+        icon='SNAP_ON',
+        properties=op_props
+    )
 
 
 SCRIPT_REGISTER_OP_TOGGLE_CONTROLS = ['POSE_OT_rigify_spline_tentacle_toggle_control']

scripts/addons_core/rigify/rot_mode.py
@@ -200,7 +200,7 @@ class POSE_OT_convert_rotation(bpy.types.Operator):
         default='SELECT',
     )
     affected_actions: EnumProperty(
-        name="Affected Action",
+        name="Affected Actions",
         items=[
             ('SINGLE', 'Single', 'Single'),
             ('ALL', 'All', 'All'),

Bforartists/Bforartists

commit 313c45fea8ee3071310756db38f6145da3c09a79

Fix #130059: Rigify Limb menu disppears for some rotation fields
Caused by 78b9218c9851aaa5b438141cd8009911f87a1628
Undefined `iface_` is used, this terminates execution of further UI panel draw
code. Fix this by importing iface_ from module.

Pull Request: https://projects.blender.org/blender/blender/pulls/130063

scripts/addons_core/rigify/rigs/limbs/limb_rigs.py
@@ -28,6 +28,7 @@ from math import pi
 from itertools import count
 from mathutils import Vector
 
+from bpy.app.translations import pgettext_iface as iface_
 
 class SegmentEntry(NamedTuple):
     org: str                 # ORG bone