Bforartists/Bforartists

commit 78c477a4cbbe0bd3b117cef60bea52df2185a31f

Fix #126983: custom nodes and groups icon missing
Fix regression caused by commit a03214c966.

Pull Request: https://projects.blender.org/blender/blender/pulls/127263

source/blender/editors/space_node/node_draw.cc
@@ -3435,7 +3435,7 @@ static void node_draw_basis(const bContext &C,
   float iconofs = rct.xmax - 0.35f * U.widget_unit;
 
   /* Group edit. This icon should be the first for the node groups. */
-  if (node.type == NODE_GROUP) {
+  if (node.is_group()) {
     iconofs -= iconbutw;
     UI_block_emboss_set(&block, UI_EMBOSS_NONE);
     uiBut *but = uiDefIconBut(&block,
@@ -3482,7 +3482,7 @@ static void node_draw_basis(const bContext &C,
                     (void *)"NODE_OT_preview_toggle");
     UI_block_emboss_set(&block, UI_EMBOSS);
   }
-  if (node.is_group() && node.typeinfo->ui_icon != ICON_NONE) {
+  if (ELEM(node.type, NODE_CUSTOM, NODE_CUSTOM_GROUP) && node.typeinfo->ui_icon != ICON_NONE) {
     iconofs -= iconbutw;
     UI_block_emboss_set(&block, UI_EMBOSS_NONE);
     uiDefIconBut(&block,

commit 5a21db2eee3594ee600ec9468cdc591fc2ddddeb

Fix: Nodes: group edit button unintentionally drawn on NodeCustomGroup
Fix regression caused by 78c477a4cbb.

Pull Request: https://projects.blender.org/blender/blender/pulls/130204

source/blender/editors/space_node/node_draw.cc
@@ -3424,7 +3424,7 @@ static void node_draw_basis(const bContext &C,
   float iconofs = rct.xmax - 0.35f * U.widget_unit;
 
   /* Group edit. This icon should be the first for the node groups. */
-  if (node.is_group()) {
+  if (node.type == NODE_GROUP) {
     iconofs -= iconbutw;
     UI_block_emboss_set(&block, UI_EMBOSS_NONE);
     uiBut *but = uiDefIconBut(&block,