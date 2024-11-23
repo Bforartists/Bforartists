Bforartists/Bforartists

commit 937fdfc6d0d291f94d7593df023294cbc0ab2db0

Outliner: Port deform group elements to new tree-element code design
No user visible changes expected.

Part of #96713, continuation of work started in 986698b2dc and d80959372e.
Refer to these for a motivation and design overview.

Adds new classes for deform group elements.

Pull Request: https://projects.blender.org/blender/blender/pulls/110003

source/blender/editors/space_outliner/outliner_tree.cc
@@ -245,6 +245,9 @@ TreeElement *outliner_add_element(SpaceOutliner *space_outliner,
   else if (type == TSE_EBONE) {
     id = static_cast<EditBoneElementCreateData *>(idv)->armature_id;
   }
+  else if (type == TSE_DEFGROUP) {
+    id = &static_cast<DeformGroupElementCreateData *>(idv)->object->id;
+  }
   else if (type == TSE_LINKED_PSYS) {
     id = &static_cast<ParticleSystemElementCreateData *>(idv)->object->id;
   }
@@ -306,6 +309,9 @@ TreeElement *outliner_add_element(SpaceOutliner *space_outliner,
   else if (ELEM(type, TSE_BONE, TSE_EBONE)) {
     /* pass */
   }
+  else if (ELEM(type, TSE_DEFGROUP, TSE_DEFGROUP_BASE)) {
+    /* pass */
+  }
   else if (type == TSE_LINKED_PSYS) {
     /* pass */
   }
@@ -358,7 +364,8 @@ TreeElement *outliner_add_element(SpaceOutliner *space_outliner,
                 TSE_SEQUENCE,
                 TSE_SEQ_STRIP,
                 TSE_SEQUENCE_DUP,
-                TSE_GENERIC_LABEL))
+                TSE_GENERIC_LABEL) ||
+           ELEM(type, TSE_DEFGROUP, TSE_DEFGROUP_BASE))
   {
     BLI_assert_msg(false, "Element type should already use new AbstractTreeElement design");
   }

source/blender/editors/space_outliner/CMakeLists.txt
@@ -50,6 +50,7 @@ set(SRC
   tree/tree_element_anim_data.cc
   tree/tree_element_bone.cc
   tree/tree_element_collection.cc
+  tree/tree_element_defgroup.cc
   tree/tree_element_driver.cc
   tree/tree_element_edit_bone.cc
   tree/tree_element_gpencil_layer.cc
@@ -82,6 +83,7 @@ set(SRC
   tree/tree_element_anim_data.hh
   tree/tree_element_bone.hh
   tree/tree_element_collection.hh
+  tree/tree_element_defgroup.hh
   tree/tree_element_driver.hh
   tree/tree_element_edit_bone.hh
   tree/tree_element_gpencil_layer.hh

source/blender/editors/space_outliner/outliner_intern.hh
@@ -29,6 +29,7 @@ struct TreeStoreElem;
 struct ViewLayer;
 struct bContext;
 struct bContextDataResult;
+struct bDeformGroup;
 struct bPoseChannel;
 struct ParticleSystem;
 struct View2D;
@@ -295,6 +296,11 @@ struct BoneElementCreateData {
 struct EditBoneElementCreateData {
   ID *armature_id;
   EditBone *ebone;
+};
+
+struct DeformGroupElementCreateData {
+  Object *object;
+  bDeformGroup *defgroup;
 };
 
 struct ParticleSystemElementCreateData {

source/blender/editors/space_outliner/tree/tree_element.cc
@@ -20,6 +20,7 @@
 #include "tree_element_anim_data.hh"
 #include "tree_element_bone.hh"
 #include "tree_element_collection.hh"
+#include "tree_element_defgroup.hh"
 #include "tree_element_driver.hh"
 #include "tree_element_edit_bone.hh"
 #include "tree_element_gpencil_layer.hh"
@@ -114,6 +115,14 @@ std::unique_ptr<AbstractTreeElement> AbstractTreeElement::createFromType(const i
       EditBoneElementCreateData *ebone_data = static_cast<EditBoneElementCreateData *>(idv);
       return std::make_unique<TreeElementEditBone>(
           legacy_te, *ebone_data->armature_id, *ebone_data->ebone);
+    }
+    case TSE_DEFGROUP_BASE:
+      return std::make_unique<TreeElementDeformGroupBase>(legacy_te, *static_cast<Object *>(idv));
+    case TSE_DEFGROUP: {
+      DeformGroupElementCreateData *defgroup_data = static_cast<DeformGroupElementCreateData *>(
+          idv);
+      return std::make_unique<TreeElementDeformGroup>(
+          legacy_te, *defgroup_data->object, *defgroup_data->defgroup);
     }
     case TSE_LINKED_PSYS: {
       ParticleSystemElementCreateData *psys_data = static_cast<ParticleSystemElementCreateData *>(

source/blender/editors/space_outliner/tree/tree_element_defgroup.cc
@@ -1,0 +1,51 @@
+/* SPDX-FileCopyrightText: 2023 Blender Foundation
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup spoutliner
+ */
+
+#include "DNA_object_types.h"
+#include "DNA_outliner_types.h"
+
+#include "BKE_deform.h"
+
+#include "BLT_translation.h"
+
+#include "../outliner_intern.hh"
+
+#include "tree_element_defgroup.hh"
+
+namespace blender::ed::outliner {
+
+TreeElementDeformGroupBase::TreeElementDeformGroupBase(TreeElement &legacy_te, Object &object)
+    : AbstractTreeElement(legacy_te), object_(object)
+{
+  legacy_te.name = IFACE_("Vertex Groups");
+}
+
+void TreeElementDeformGroupBase::expand(SpaceOutliner &space_outliner) const
+{
+  const ListBase *defbase = BKE_object_defgroup_list(&object_);
+
+  int index;
+  LISTBASE_FOREACH_INDEX (bDeformGroup *, defgroup, defbase, index) {
+
+    DeformGroupElementCreateData defgroup_data = {&object_, defgroup};
+
+    outliner_add_element(
+        &space_outliner, &legacy_te_.subtree, &defgroup_data, &legacy_te_, TSE_DEFGROUP, index);
+  }
+}
+
+TreeElementDeformGroup::TreeElementDeformGroup(TreeElement &legacy_te,
+                                               Object & /* object */,
+                                               bDeformGroup &defgroup)
+    : AbstractTreeElement(legacy_te), /* object_(object), */ defgroup_(defgroup)
+{
+  legacy_te.name = defgroup_.name;
+  legacy_te.directdata = &defgroup_;
+}
+
+}  // namespace blender::ed::outliner

source/blender/editors/space_outliner/tree/tree_element_defgroup.hh
@@ -1,0 +1,35 @@
+/* SPDX-FileCopyrightText: 2023 Blender Foundation
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup spoutliner
+ */
+
+#pragma once
+
+#include "tree_element.hh"
+
+struct Object;
+struct bDeformGroup;
+
+namespace blender::ed::outliner {
+
+class TreeElementDeformGroupBase final : public AbstractTreeElement {
+  Object &object_;
+
+ public:
+  TreeElementDeformGroupBase(TreeElement &legacy_te, Object &object);
+  void expand(SpaceOutliner &) const override;
+};
+
+class TreeElementDeformGroup final : public AbstractTreeElement {
+  /* Not needed right now, avoid unused member variable warning. */
+  // Object &object_;
+  bDeformGroup &defgroup_;
+
+ public:
+  TreeElementDeformGroup(TreeElement &legacy_te, Object &object, bDeformGroup &defgroup);
+};
+
+}  // namespace blender::ed::outliner

source/blender/editors/space_outliner/tree/tree_element_id_object.cc
@@ -290,17 +290,8 @@ void TreeElementIDObject::expand_vertex_groups(SpaceOutliner &space_outliner) co
   if (BLI_listbase_is_empty(defbase)) {
     return;
   }
-  TreeElement *tenla = outliner_add_element(
+  outliner_add_element(
       &space_outliner, &legacy_te_.subtree, &object_, &legacy_te_, TSE_DEFGROUP_BASE, 0);
-  tenla->name = IFACE_("Vertex Groups");
-
-  int index;
-  LISTBASE_FOREACH_INDEX (bDeformGroup *, defgroup, defbase, index) {
-    TreeElement *ten = outliner_add_element(
-        &space_outliner, &tenla->subtree, &object_, tenla, TSE_DEFGROUP, index);
-    ten->name = defgroup->name;
-    ten->directdata = defgroup;
-  }
 }
 
 void TreeElementIDObject::expand_duplicated_group(SpaceOutliner &space_outliner) const

commit 5c33c5b05cb8ad2555b7d404c1468f25ac75b2a5

Fix #130686: Outliner vertex groups sorting is broken
Caused by 937fdfc6d0d

Before said commit, a vertexgroup `TreeElement` would take its `idcode`
in a questionable way described in the commit messages of a9ee7c41bc1b
and 02c6384826ad.

Since 937fdfc6d0d this is not the case anymore, it is just 0.

42709b9d791b made this comment (guess this never happened):
>/* We need to figure out how to deal with the idcode for non-
TSE_SOME_ID types still. Some rely on it... */

So in order to return to the prior behavior, we could just enable the
following line (even for the new type design), this will work, but feels
rather shaky (the basic question here is just "do we want idcode to
point to the owner ID for something like vertexgroups?" -- still cannot
answer this question tbh.):
`te->idcode = GS(owner_id->name);`

So one of the places where the logic relied on `idcode` is alphabetical
sorting. The logic excluded some elements from the sorting (by
explicitly setting their `idcode` to zero) and this is where we now
coughing up because... well we are always having a zero `idcode` for the
new type design.

To resolve, just explicitly enable sorting for `TSE_DEFGROUP` again by
explicitly setting their `idcode` to non-zero(one).

Pull Request: https://projects.blender.org/blender/blender/pulls/130750

source/blender/editors/space_outliner/outliner_tree.cc
@@ -583,7 +583,7 @@ static void outliner_sort(ListBase *lb)
         if (!ELEM(tselem->type, TSE_SOME_ID, TSE_DEFGROUP)) {
           tp->idcode = 0; /* Don't sort this. */
         }
-        if (tselem->type == TSE_ID_BASE) {
+        if (ELEM(tselem->type, TSE_ID_BASE, TSE_DEFGROUP)) {
           tp->idcode = 1; /* Do sort this. */
         }
 