2d5e40135f5709dece04c944ccda5595626adb75
Justin <blenux@outlook.com>
2024-12-06 12:41:21 +1100

    fix for 4842-launching-bforartists-from-the-provided-command-scripts-are
    -- make the debug logs to use the bforartists executable instead of blender

--- release/windows/batch/blender_oculus.cmd
+++ release/windows/batch/blender_oculus.cmd
@@ -3,7 +3,7 @@
 REM Helper setting hints to get the OpenXR preview support enabled for Oculus.
 REM Of course this is not meant as a permanent solution. Oculus will likely provide a better setup at some point.
 
-echo Starting Blender with Oculus OpenXR support. This assumes the Oculus runtime
+echo Starting Bforartists with Oculus OpenXR support. This assumes the Oculus runtime
 echo is installed in the default location. If this is not the case, please adjust
 echo the path inside oculus.json.
 echo.
@@ -11,4 +11,4 @@ echo Note that OpenXR support in Oculus is considered a preview. Use with care!
 echo.
 pause
 set XR_RUNTIME_JSON=%~dp0oculus.json
-"%~dp0\blender"
+"%~dp0\bforartists"
