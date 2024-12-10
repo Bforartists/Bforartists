Bforartists/Bforartists

commit 861ac79f9a534dd292eb91cad2098e7069759555

Cleanup: BKE: Use StringRefNull instead of char *
Use StringRefNull for all function arguments and return types.
Not a StringRef but StringRefNull since there is still large
interaction with C api so null-termination usually necessary.

If string is expected to be not only empty but also a null then
optional is used. This change depends on #130935.

Pull Request: https://projects.blender.org/blender/blender/pulls/131204

source/blender/makesrna/intern/rna_node_tree_interface.cc
@@ -30,6 +30,8 @@ static const EnumPropertyItem node_tree_interface_socket_in_out_items[] = {
 
 #  include <fmt/format.h>
 
+#  include "BLI_string_ref.hh"
+
 #  include "BKE_attribute.hh"
 #  include "BKE_node.hh"
 #  include "BKE_node_enum.hh"
@@ -195,7 +197,7 @@ static void rna_NodeTreeInterfaceSocket_init_socket_custom(
     const bNodeTreeInterfaceSocket *interface_socket,
     bNode *node,
     bNodeSocket *socket,
-    const char *data_path)
+    const blender::StringRefNull data_path)
 {
   blender::bke::bNodeSocketType *typeinfo = blender::bke::node_socket_type_find(
       interface_socket->socket_type);

source/blender/blenkernel/BKE_node.hh
@@ -157,7 +157,7 @@ struct bNodeSocketType {
   char subtype_label[64];
 
   void (*draw)(
-      bContext *C, uiLayout *layout, PointerRNA *ptr, PointerRNA *node_ptr, const char *text);
+      bContext *C, uiLayout *layout, PointerRNA *ptr, PointerRNA *node_ptr, StringRefNull text);
   void (*draw_color)(bContext *C, PointerRNA *ptr, PointerRNA *node_ptr, float *r_color);
   void (*draw_color_simple)(const bNodeSocketType *socket_type, float *r_color);
 
@@ -166,7 +166,7 @@ struct bNodeSocketType {
                                 const bNodeTreeInterfaceSocket *interface_socket,
                                 bNode *node,
                                 bNodeSocket *socket,
-                                const char *data_path);
+                                StringRefNull data_path);
   void (*interface_from_socket)(ID *id,
                                 bNodeTreeInterfaceSocket *interface_socket,
                                 const bNode *node,
@@ -443,7 +443,7 @@ enum class NodeGroupColorTag {
   Vector = 13,
 };
 
-using bNodeClassCallback = void (*)(void *calldata, int nclass, const char *name);
+using bNodeClassCallback = void (*)(void *calldata, int nclass, StringRefNull name);
 
 struct bNodeTreeType {
   int type;        /* type identifier */
@@ -489,7 +489,7 @@ struct bNodeTreeType {
 /** \name Generic API, Trees
  * \{ */
 
-bNodeTreeType *node_tree_type_find(const char *idname);
+bNodeTreeType *node_tree_type_find(StringRefNull idname);
 void node_tree_type_add(bNodeTreeType *nt);
 void node_tree_type_free_link(const bNodeTreeType *nt);
 bool node_tree_is_registered(const bNodeTree *ntree);
@@ -521,7 +521,7 @@ GHashIterator *node_tree_type_get_iterator();
  */
 void node_tree_set_type(const bContext *C, bNodeTree *ntree);
 
-bNodeTree *node_tree_add_tree(Main *bmain, const char *name, const char *idname);
+bNodeTree *node_tree_add_tree(Main *bmain, StringRefNull name, StringRefNull idname);
 
 /**
  * Add a new (non-embedded) node tree, like #node_tree_add_tree, but allows to create it inside a
@@ -529,8 +529,8 @@ bNodeTree *node_tree_add_tree(Main *bmain, const char *name, const char *idname)
  */
 bNodeTree *node_tree_add_in_lib(Main *bmain,
                                 Library *owner_library,
-                                const char *name,
-                                const char *idname);
+                                StringRefNull name,
+                                StringRefNull idname);
 
 /**
  * Free tree which is embedded into another data-block.
@@ -586,11 +586,11 @@ void node_tree_blend_write(BlendWriter *writer, bNodeTree *ntree);
 /** \name Generic API, Nodes
  * \{ */
 
-bNodeType *node_type_find(const char *idname);
-const char *node_type_find_alias(const char *idname);
+bNodeType *node_type_find(StringRefNull idname);
+StringRefNull node_type_find_alias(StringRefNull idname);
 void node_register_type(bNodeType *ntype);
 void node_unregister_type(bNodeType *ntype);
-void node_register_alias(bNodeType *nt, const char *alias);
+void node_register_alias(bNodeType *nt, StringRefNull alias);
 GHashIterator *node_type_get_iterator();
 
 /* Helper macros for iterating over node types. */
@@ -608,16 +608,16 @@ GHashIterator *node_type_get_iterator();
   } \
   ((void)0)
 
-bNodeSocketType *node_socket_type_find(const char *idname);
+bNodeSocketType *node_socket_type_find(StringRefNull idname);
 void node_register_socket_type(bNodeSocketType *stype);
 void node_unregister_socket_type(bNodeSocketType *stype);
 bool node_socket_is_registered(const bNodeSocket *sock);
 GHashIterator *node_socket_type_get_iterator();
-const char *node_socket_type_label(const bNodeSocketType *stype);
+StringRefNull node_socket_type_label(const bNodeSocketType *stype);
 
-const char *node_static_socket_type(int type, int subtype);
-const char *node_static_socket_interface_type_new(int type, int subtype);
-const char *node_static_socket_label(int type, int subtype);
+std::optional<StringRefNull> node_static_socket_type(int type, int subtype);
+std::optional<StringRefNull> node_static_socket_interface_type_new(int type, int subtype);
+std::optional<StringRefNull> node_static_socket_label(int type, int subtype);
 
 /* Helper macros for iterating over node types. */
 #define NODE_SOCKET_TYPES_BEGIN(stype) \
@@ -642,22 +642,22 @@ const bNodeSocket *node_find_socket(const bNode *node,
 bNodeSocket *node_add_socket(bNodeTree *ntree,
                              bNode *node,
                              eNodeSocketInOut in_out,
-                             const char *idname,
-                             const char *identifier,
-                             const char *name);
+                             StringRefNull idname,
+                             StringRefNull identifier,
+                             StringRefNull name);
 bNodeSocket *node_add_static_socket(bNodeTree *ntree,
                                     bNode *node,
                                     eNodeSocketInOut in_out,
                                     int type,
                                     int subtype,
-                                    const char *identifier,
-                                    const char *name);
+                                    StringRefNull identifier,
+                                    StringRefNull name);
 void node_remove_socket(bNodeTree *ntree, bNode *node, bNodeSocket *sock);
 
 void node_modify_socket_type_static(
     bNodeTree *ntree, bNode *node, bNodeSocket *sock, int type, int subtype);
 
-bNode *node_add_node(const bContext *C, bNodeTree *ntree, const char *idname);
+bNode *node_add_node(const bContext *C, bNodeTree *ntree, StringRefNull idname);
 bNode *node_add_static_node(const bContext *C, bNodeTree *ntree, int type);
 
 /**
@@ -704,7 +704,7 @@ void node_find_node(bNodeTree *ntree, bNodeSocket *sock, bNode **r_node, int *r_
 /**
  * Finds a node based on its name.
  */
-bNode *node_find_node_by_name(bNodeTree *ntree, const char *name);
+bNode *node_find_node_by_name(bNodeTree *ntree, StringRefNull name);
 
 bool node_is_parent_and_child(const bNode *parent, const bNode *child);
 
@@ -748,7 +748,7 @@ bNodeInstanceKey node_instance_key(bNodeInstanceKey parent_key,
                                    const bNodeTree *ntree,
                                    const bNode *node);
 
-bNodeInstanceHash *node_instance_hash_new(const char *info);
+bNodeInstanceHash *node_instance_hash_new(StringRefNull info);
 void node_instance_hash_free(bNodeInstanceHash *hash, bNodeInstanceValueFP valfreefp);
 void node_instance_hash_insert(bNodeInstanceHash *hash, bNodeInstanceKey key, void *value);
 void *node_instance_hash_lookup(bNodeInstanceHash *hash, bNodeInstanceKey key);
@@ -775,15 +775,19 @@ bool node_group_poll(const bNodeTree *nodetree,
                      const bNodeTree *grouptree,
                      const char **r_disabled_hint);
 
-void node_type_base_custom(
-    bNodeType *ntype, const char *idname, const char *name, const char *enum_name, short nclass);
+void node_type_base_custom(bNodeType *ntype,
+                           StringRefNull idname,
+                           StringRefNull name,
+                           StringRefNull enum_name,
+                           short nclass);
 
 /**
  * \warning Nodes defining a storage type _must_ allocate this for new nodes.
  * Otherwise nodes will reload as undefined (#46619).
+ * #storagename is optional due to some compositor nodes use non-DNA storage type.
  */
 void node_type_storage(bNodeType *ntype,
-                       const char *storagename,
+                       std::optional<StringRefNull> storagename,
                        void (*freefunc)(bNode *node),
                        void (*copyfunc)(bNodeTree *dest_ntree,
                                         bNode *dest_node,
@@ -1438,8 +1442,8 @@ void node_system_exit();
 
 bNodeTree *node_tree_add_tree_embedded(Main *bmain,
                                        ID *owner_id,
-                                       const char *name,
-                                       const char *idname);
+                                       StringRefNull name,
+                                       StringRefNull idname);
 
 /* Copy/free functions, need to manage ID users. */
 
@@ -1477,11 +1481,14 @@ bool node_type_is_undefined(const bNode *node);
 
 bool node_is_static_socket_type(const bNodeSocketType *stype);
 
-const char *node_socket_sub_type_label(int subtype);
+StringRefNull node_socket_sub_type_label(int subtype);
 
 void node_remove_socket_ex(bNodeTree *ntree, bNode *node, bNodeSocket *sock, bool do_id_user);
 
-void node_modify_socket_type(bNodeTree *ntree, bNode *node, bNodeSocket *sock, const char *idname);
+void node_modify_socket_type(bNodeTree *ntree,
+                             bNode *node,
+                             bNodeSocket *sock,
+                             StringRefNull idname);
 
 /**
  * \note Goes over entire tree.
@@ -1705,18 +1712,18 @@ void nodeLabel(const bNodeTree *ntree, const bNode *node, char *label, int maxle
 /**
  * Get node socket label if it is set.
  */
-const char *nodeSocketLabel(const bNodeSocket *sock);
+StringRefNull nodeSocketLabel(const bNodeSocket *sock);
 
 /**
  * Get node socket short label if it is set.
  * It is used when grouping sockets under panels, to avoid redundancy in the label.
  */
-const char *nodeSocketShortLabel(const bNodeSocket *sock);
+std::optional<StringRefNull> nodeSocketShortLabel(const bNodeSocket *sock);
 
 /**
  * Initialize a new node type struct with default values and callbacks.
  */
-void node_type_base(bNodeType *ntype, int type, const char *name, short nclass);
+void node_type_base(bNodeType *ntype, int type, StringRefNull name, short nclass);
 
 void node_type_socket_templates(bNodeType *ntype,
                                 bNodeSocketTemplate *inputs,

source/blender/blenkernel/intern/bake_items_socket.cc
@@ -236,7 +236,7 @@ static void restore_data_blocks(const Span<GeometrySet *> geometries,
 
 static void default_initialize_socket_value(const eNodeSocketDatatype socket_type, void *r_value)
 {
-  const char *socket_idname = bke::node_static_socket_type(socket_type, 0);
+  const StringRefNull socket_idname = *bke::node_static_socket_type(socket_type, 0);
   const bke::bNodeSocketType *typeinfo = bke::node_socket_type_find(socket_idname);
   if (typeinfo->geometry_nodes_default_cpp_value) {
     typeinfo->geometry_nodes_cpp_type->copy_construct(typeinfo->geometry_nodes_default_cpp_value,

commit 65852c5b45f4b88f8bc2093b4f030e1ffa206fc1

Fix #131565: missing null check for socket type
This was probably accidentally broken by 861ac79f9a534dd.

source/blender/makesrna/intern/rna_node_tree_interface.cc
@@ -73,10 +73,12 @@ static StructRNA *rna_NodeTreeInterfaceItem_refine(PointerRNA *ptr)
     case NODE_INTERFACE_SOCKET: {
       bNodeTreeInterfaceSocket &socket = node_interface::get_item_as<bNodeTreeInterfaceSocket>(
           *item);
-      blender::bke::bNodeSocketType *socket_typeinfo = blender::bke::node_socket_type_find(
-          socket.socket_type);
-      if (socket_typeinfo && socket_typeinfo->ext_interface.srna) {
-        return socket_typeinfo->ext_interface.srna;
+      if (socket.socket_type) {
+        blender::bke::bNodeSocketType *socket_typeinfo = blender::bke::node_socket_type_find(
+            socket.socket_type);
+        if (socket_typeinfo && socket_typeinfo->ext_interface.srna) {
+          return socket_typeinfo->ext_interface.srna;
+        }
       }
       return &RNA_NodeTreeInterfaceSocket;
     }