Bforartists/Bforartists

commit cb5f771db003c4705e4eaafdfc95af1b1b17b6e7

Fix #80356: UI Icon Alpha Should Not Affect Matcaps & Studio Lighting
Changing Icon Alpha in Preferences / Themes / User Interface / Styles /
Icon Alpha is meant to change the opacity of the UI icons. It should not
change the opacity of the previews of Matcaps and Studio lights as this
changes the color because of mixing with the background. This PR just
skips this icon type when changing the alpha for this preference.

Pull Request: https://projects.blender.org/blender/blender/pulls/131690

source/blender/editors/interface/interface_icons.cc
@@ -1468,7 +1468,11 @@ static void icon_draw_size(float x,
   const float fdraw_size = float(draw_size);
 
   Icon *icon = BKE_icon_get(icon_id);
-  alpha *= btheme->tui.icon_alpha;
+
+  if (icon->obj_type != ICON_DATA_STUDIOLIGHT) {
+    /* Icon Alpha should not apply to Mapcaps/Studio lighting. #80356. */
+    alpha *= btheme->tui.icon_alpha;
+  }
 
   if (icon == nullptr) {
     if (G.debug & G_DEBUG) {

commit 76ec7c8d36bb808bc3e76b7e89d0fc37c5e010e8

Fix: UI: Crash when trying to display invalid icon
Requesting an icon from an invalid icon ID was always handled
gracefully, resulting in the icon not being shown and an error print in
debug mode. cb5f771db0 caused a null pointer dereference in that case,
leading to a crash.

source/blender/editors/interface/interface_icons.cc
@@ -1469,16 +1469,16 @@ static void icon_draw_size(float x,
 
   Icon *icon = BKE_icon_get(icon_id);
 
-  if (icon->obj_type != ICON_DATA_STUDIOLIGHT) {
-    /* Icon alpha should not apply to MatCap/Studio lighting. #80356. */
-    alpha *= btheme->tui.icon_alpha;
-  }
-
   if (icon == nullptr) {
     if (G.debug & G_DEBUG) {
       printf("%s: Internal error, no icon for icon ID: %d\n", __func__, icon_id);
     }
     return;
+  }
+
+  if (icon->obj_type != ICON_DATA_STUDIOLIGHT) {
+    /* Icon alpha should not apply to MatCap/Studio lighting. #80356. */
+    alpha *= btheme->tui.icon_alpha;
   }
 
   /* scale width and height according to aspect */