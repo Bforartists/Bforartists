Bforartists/Bforartists

commit d59b0ebd20fe8a3261c3da7fe3a7a581e442e875

Fix #128702: Edge Slide can cause Blender to freeze
In non-manifold geometry, the original loop could fail to identify the
correct slide direction because it checked for loop equality
(`l_other != l_edge`), which might never be met.

The condition has been revised to check for face equality
(`l_other->f != l_edge->f`), ensuring the loop terminates correctly and
preventing infinite iterations in problematic geometry cases.

source/blender/editors/transform/transform_convert_mesh.cc
@@ -2456,7 +2456,7 @@ Array<TransDataEdgeSlideVert> transform_mesh_edge_slide_data_create(const TransD
           int best_dir = -1;
           const BMLoop *l_edge = l_src->next->v == v_dst ? l_src : l_src->prev;
           const BMLoop *l_other = l_edge->radial_next;
-          while (l_other != l_edge) {
+          do {
             if (l_other->f == curr_side_other->fdata[0].f) {
               best_dir = 0;
               break;
@@ -2466,7 +2466,7 @@ Array<TransDataEdgeSlideVert> transform_mesh_edge_slide_data_create(const TransD
               break;
             }
             l_other = (l_other->v == this->v ? l_other->prev : l_other->next)->radial_next;
-          }
+          } while (l_other->f != l_edge->f);
 
           if (best_dir != -1) {
             *r_do_isect_curr_dirs = true;

commit 43339aa706717033dea72497d387b82005152c91

Fix #132264: Vertex Edge slide wrong
Caused by d59b0ebd20fe

The above commit fixed the condition that caused the hang in #128702 but
it also changed the `while` loop to a `do while`.
The do while loop executes the content of the loop once before checking
the condition of the while.
Whereas a while loop will check the condition first before executing the
content.

As a consequence, we could (falsely) enter the loop and return with a
(false) best direction.

To resolve, keep the check from d59b0ebd20fe but go back to a `while`
loop (instead of the `do while).

Needs to go into 4.2 LTS

Pull Request: https://projects.blender.org/blender/blender/pulls/132270

source/blender/editors/transform/transform_convert_mesh.cc
@@ -2456,7 +2456,7 @@ Array<TransDataEdgeSlideVert> transform_mesh_edge_slide_data_create(const TransD
           int best_dir = -1;
           const BMLoop *l_edge = l_src->next->v == v_dst ? l_src : l_src->prev;
           const BMLoop *l_other = l_edge->radial_next;
-          do {
+          while (l_other->f != l_edge->f) {
             if (l_other->f == curr_side_other->fdata[0].f) {
               best_dir = 0;
               break;
@@ -2466,7 +2466,7 @@ Array<TransDataEdgeSlideVert> transform_mesh_edge_slide_data_create(const TransD
               break;
             }
             l_other = (l_other->v == this->v ? l_other->prev : l_other->next)->radial_next;
-          } while (l_other->f != l_edge->f);
+          }
 
           if (best_dir != -1) {
             *r_do_isect_curr_dirs = true;