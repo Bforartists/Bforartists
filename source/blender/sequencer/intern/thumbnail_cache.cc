Bforartists/Bforartists

commit de47d27f99b28de876c30729c1449d6c5c80129f

Cleanup: VSE: Rename `SequenceType` to `StripType` as well as flags
Rename the flags from `SEQ_TYPE_*` to `STRIP_TYPE_*`.

Pull Request: https://projects.blender.org/blender/blender/pulls/132753

source/blender/sequencer/intern/thumbnail_cache.cc
@@ -65,7 +65,7 @@ struct ThumbnailCache {
     explicit Request(const std::string &path,
                      int frame,
                      int stream,
-                     SequenceType type,
+                     StripType type,
                      int64_t logical_time,
                      float time_frame,
                      int ch,
@@ -86,7 +86,7 @@ struct ThumbnailCache {
     std::string file_path;
     int frame_index = 0;  /* Frame index (for movies) or image index (for image sequences). */
     int stream_index = 0; /* Stream index (only for multi-stream movies). */
-    SequenceType strip_type = SEQ_TYPE_IMAGE;
+    SequenceType strip_type = STRIP_TYPE_IMAGE;
 
     /* The following members are payload and do not contribute to uniqueness. */
     int64_t requested_at = 0;
@@ -162,7 +162,7 @@ bool strip_can_have_thumbnail(const Scene *scene, const Strip *strip)
   if (scene == nullptr || scene->ed == nullptr || strip == nullptr) {
     return false;
   }
-  if (!ELEM(strip->type, SEQ_TYPE_MOVIE, SEQ_TYPE_IMAGE)) {
+  if (!ELEM(strip->type, STRIP_TYPE_MOVIE, STRIP_TYPE_IMAGE)) {
     return false;
   }
   const StripElem *se = strip->data->stripdata;
@@ -177,14 +177,14 @@ static std::string get_path_from_seq(Scene *scene, const Strip *strip, float tim
   char filepath[FILE_MAX];
   filepath[0] = 0;
   switch (strip->type) {
-    case SEQ_TYPE_IMAGE: {
+    case STRIP_TYPE_IMAGE: {
       const StripElem *s_elem = SEQ_render_give_stripelem(scene, strip, timeline_frame);
       if (s_elem != nullptr) {
         BLI_path_join(filepath, sizeof(filepath), strip->data->dirpath, s_elem->filename);
         BLI_path_abs(filepath, ID_BLEND_PATH_FROM_GLOBAL(&scene->id));
       }
     } break;
-    case SEQ_TYPE_MOVIE:
+    case STRIP_TYPE_MOVIE:
       BLI_path_join(
           filepath, sizeof(filepath), strip->data->dirpath, strip->data->stripdata->filename);
       BLI_path_abs(filepath, ID_BLEND_PATH_FROM_GLOBAL(&scene->id));
@@ -335,14 +335,14 @@ void ThumbGenerationJob::run_fn(void *customdata, wmJobWorkerStatus *worker_stat
         ++total_thumbs;
 #endif
         ImBuf *thumb = nullptr;
-        if (request.strip_type == SEQ_TYPE_IMAGE) {
+        if (request.strip_type == STRIP_TYPE_IMAGE) {
           /* Load thumbnail for an image. */
 #ifdef DEBUG_PRINT_THUMB_JOB_TIMES
           ++total_images;
 #endif
           thumb = make_thumb_for_image(job->scene_, request);
         }
-        else if (request.strip_type == SEQ_TYPE_MOVIE) {
+        else if (request.strip_type == STRIP_TYPE_MOVIE) {
           /* Load thumbnail for an movie. */
 #ifdef DEBUG_PRINT_THUMB_JOB_TIMES
           ++total_movies;
@@ -461,7 +461,7 @@ static ImBuf *query_thumbnail(ThumbnailCache &cache,
     ThumbnailCache::Request request(key,
                                     frame_index,
                                     strip->streamindex,
-                                    SequenceType(strip->type),
+                                    StripType(strip->type),
                                     cur_time,
                                     timeline_frame,
                                     strip->machine,
@@ -494,7 +494,7 @@ ImBuf *thumbnail_cache_get(const bContext *C,
 
   const std::string key = get_path_from_seq(scene, strip, timeline_frame);
   int frame_index = SEQ_give_frame_index(scene, strip, timeline_frame);
-  if (strip->type == SEQ_TYPE_MOVIE) {
+  if (strip->type == STRIP_TYPE_MOVIE) {
     frame_index += strip->anim_startofs;
   }
 
@@ -520,11 +520,11 @@ void thumbnail_cache_invalidate_strip(Scene *scene, const Strip *strip)
   std::scoped_lock lock(thumb_cache_mutex);
   ThumbnailCache *cache = query_thumbnail_cache(scene);
   if (cache != nullptr) {
-    if (ELEM((strip)->type, SEQ_TYPE_MOVIE, SEQ_TYPE_IMAGE)) {
+    if (ELEM((strip)->type, STRIP_TYPE_MOVIE, STRIP_TYPE_IMAGE)) {
       const StripElem *elem = strip->data->stripdata;
       if (elem != nullptr) {
         int paths_count = 1;
-        if (strip->type == SEQ_TYPE_IMAGE) {
+        if (strip->type == STRIP_TYPE_IMAGE) {
           /* Image strip has array of file names. */
           paths_count = int(MEM_allocN_len(elem) / sizeof(*elem));
         }

source/blender/blenkernel/intern/ipo.cc
@@ -2115,14 +2115,14 @@ static bool strip_convert_callback(Strip *strip, void *userdata)
 
   /* Patch `adrcode`, so that we can map to different DNA variables later (semi-hack (tm)). */
   switch (strip->type) {
-    case SEQ_TYPE_IMAGE:
-    case SEQ_TYPE_META:
-    case SEQ_TYPE_SCENE:
-    case SEQ_TYPE_MOVIE:
-    case SEQ_TYPE_COLOR:
+    case STRIP_TYPE_IMAGE:
+    case STRIP_TYPE_META:
+    case STRIP_TYPE_SCENE:
+    case STRIP_TYPE_MOVIE:
+    case STRIP_TYPE_COLOR:
       adrcode = SEQ_FAC_OPACITY;
       break;
-    case SEQ_TYPE_SPEED:
+    case STRIP_TYPE_SPEED:
       adrcode = SEQ_FAC_SPEED;
       break;
   }

source/blender/blenkernel/intern/scene.cc
@@ -828,7 +828,7 @@ static bool strip_foreach_member_id_cb(Strip *strip, void *user_data)
     FOREACHID_PROCESS_IDSUPER(data, smd->mask_id, IDWALK_CB_USER);
   }
 
-  if (strip->type == SEQ_TYPE_TEXT && strip->effectdata) {
+  if (strip->type == STRIP_TYPE_TEXT && strip->effectdata) {
     TextVars *text_data = static_cast<TextVars *>(strip->effectdata);
     FOREACHID_PROCESS_IDSUPER(data, text_data->text_font, IDWALK_CB_USER);
   }
@@ -948,14 +948,14 @@ static bool strip_foreach_path_callback(Strip *strip, void *user_data)
     StripElem *se = strip->data->stripdata;
     BPathForeachPathData *bpath_data = (BPathForeachPathData *)user_data;
 
-    if (ELEM(strip->type, SEQ_TYPE_MOVIE, SEQ_TYPE_SOUND_RAM) && se) {
+    if (ELEM(strip->type, STRIP_TYPE_MOVIE, STRIP_TYPE_SOUND_RAM) && se) {
       BKE_bpath_foreach_path_dirfile_fixed_process(bpath_data,
                                                    strip->data->dirpath,
                                                    sizeof(strip->data->dirpath),
                                                    se->filename,
                                                    sizeof(se->filename));
     }
-    else if ((strip->type == SEQ_TYPE_IMAGE) && se) {
+    else if ((strip->type == STRIP_TYPE_IMAGE) && se) {
       /* NOTE: An option not to loop over all strips could be useful? */
       uint len = uint(MEM_allocN_len(se)) / uint(sizeof(*se));
       uint i;

source/blender/blenloader/intern/versioning_260.cc
@@ -1029,7 +1029,7 @@ static bool strip_set_alpha_mode_cb(Strip *strip, void * /*user_data*/)
 
 static bool strip_set_wipe_angle_cb(Strip *strip, void * /*user_data*/)
 {
-  if (strip->type == SEQ_TYPE_WIPE) {
+  if (strip->type == STRIP_TYPE_WIPE) {
     WipeVars *wv = static_cast<WipeVars *>(strip->effectdata);
     wv->angle = DEG2RADF(wv->angle);
   }

source/blender/blenloader/intern/versioning_270.cc
@@ -444,7 +444,7 @@ static bool strip_update_proxy_cb(Strip *strip, void * /*user_data*/)
 
 static bool strip_update_effectdata_cb(Strip *strip, void * /*user_data*/)
 {
-  if (strip->type != SEQ_TYPE_TEXT) {
+  if (strip->type != STRIP_TYPE_TEXT) {
     return true;
   }
 

source/blender/blenloader/intern/versioning_280.cc
@@ -615,7 +615,7 @@ static void do_version_constraints_copy_rotation_mix_mode(ListBase *lb)
 static void do_versions_seq_alloc_transform_and_crop(ListBase *seqbase)
 {
   LISTBASE_FOREACH (Strip *, seq, seqbase) {
-    if (ELEM(seq->type, SEQ_TYPE_SOUND_RAM, SEQ_TYPE_SOUND_HD) == 0) {
+    if (ELEM(seq->type, STRIP_TYPE_SOUND_RAM, STRIP_TYPE_SOUND_HD) == 0) {
       if (seq->data->transform == nullptr) {
         seq->data->transform = static_cast<StripTransform *>(
             MEM_callocN(sizeof(StripTransform), "StripTransform"));
@@ -3012,7 +3012,7 @@ static void do_versions_seq_set_cache_defaults(Editing *ed)
 static bool strip_update_flags_cb(Strip *strip, void * /*user_data*/)
 {
   strip->flag &= ~((1 << 6) | (1 << 18) | (1 << 19) | (1 << 21));
-  if (strip->type == SEQ_TYPE_SPEED) {
+  if (strip->type == STRIP_TYPE_SPEED) {
     SpeedControlVars *s = (SpeedControlVars *)strip->effectdata;
     s->flags &= ~(SEQ_SPEED_UNUSED_1);
   }

source/blender/blenloader/intern/versioning_290.cc
@@ -258,10 +258,10 @@ static void strip_convert_transform_crop_lb(const Scene *scene,
 {
 
   LISTBASE_FOREACH (Strip *, seq, lb) {
-    if (!ELEM(seq->type, SEQ_TYPE_SOUND_RAM, SEQ_TYPE_SOUND_HD)) {
+    if (!ELEM(seq->type, STRIP_TYPE_SOUND_RAM, STRIP_TYPE_SOUND_HD)) {
       strip_convert_transform_crop(scene, seq, render_size);
     }
-    if (seq->type == SEQ_TYPE_META) {
+    if (seq->type == STRIP_TYPE_META) {
       strip_convert_transform_crop_lb(scene, &seq->seqbase, render_size);
     }
   }
@@ -344,10 +344,10 @@ static void strip_convert_transform_crop_lb_2(const Scene *scene,
 {
 
   LISTBASE_FOREACH (Strip *, seq, lb) {
-    if (!ELEM(seq->type, SEQ_TYPE_SOUND_RAM, SEQ_TYPE_SOUND_HD)) {
+    if (!ELEM(seq->type, STRIP_TYPE_SOUND_RAM, STRIP_TYPE_SOUND_HD)) {
       strip_convert_transform_crop_2(scene, seq, render_size);
     }
-    if (seq->type == SEQ_TYPE_META) {
+    if (seq->type == STRIP_TYPE_META) {
       strip_convert_transform_crop_lb_2(scene, &seq->seqbase, render_size);
     }
   }
@@ -785,7 +785,7 @@ static void do_versions_strip_cache_settings_recursive(const ListBase *seqbase)
 {
   LISTBASE_FOREACH (Strip *, seq, seqbase) {
     seq->cache_flag = 0;
-    if (seq->type == SEQ_TYPE_META) {
+    if (seq->type == STRIP_TYPE_META) {
       do_versions_strip_cache_settings_recursive(&seq->seqbase);
     }
   }

source/blender/blenloader/intern/versioning_300.cc
@@ -291,7 +291,7 @@ static void do_versions_idproperty_seq_recursive(ListBase *seqbase)
 {
   LISTBASE_FOREACH (Strip *, strip, seqbase) {
     version_idproperty_ui_data(strip->prop);
-    if (strip->type == SEQ_TYPE_META) {
+    if (strip->type == STRIP_TYPE_META) {
       do_versions_idproperty_seq_recursive(&strip->seqbase);
     }
   }
@@ -426,7 +426,7 @@ static void do_versions_sequencer_speed_effect_recursive(Scene *scene, const Lis
 #define SEQ_SPEED_COMPRESS_IPO_Y (1 << 2)
 
   LISTBASE_FOREACH (Strip *, strip, seqbase) {
-    if (strip->type == SEQ_TYPE_SPEED) {
+    if (strip->type == STRIP_TYPE_SPEED) {
       SpeedControlVars *v = (SpeedControlVars *)strip->effectdata;
       const char *substr = nullptr;
       float globalSpeed = v->globalSpeed;
@@ -481,7 +481,7 @@ static void do_versions_sequencer_speed_effect_recursive(Scene *scene, const Lis
         }
       }
     }
-    else if (strip->type == SEQ_TYPE_META) {
+    else if (strip->type == STRIP_TYPE_META) {
       do_versions_sequencer_speed_effect_recursive(scene, &strip->seqbase);
     }
   }
@@ -647,7 +647,7 @@ static void strip_speed_factor_fix_rna_path(Strip *strip, ListBase *fcurves)
 static bool version_fix_seq_meta_range(Strip *strip, void *user_data)
 {
   Scene *scene = (Scene *)user_data;
-  if (strip->type == SEQ_TYPE_META) {
+  if (strip->type == STRIP_TYPE_META) {
     SEQ_time_update_meta_strip_range(scene, strip);
   }
   return true;
@@ -656,7 +656,7 @@ static bool version_fix_seq_meta_range(Strip *strip, void *user_data)
 static bool strip_speed_factor_set(Strip *strip, void *user_data)
 {
   const Scene *scene = static_cast<const Scene *>(user_data);
-  if (strip->type == SEQ_TYPE_SOUND_RAM) {
+  if (strip->type == STRIP_TYPE_SOUND_RAM) {
     /* Move `pitch` animation to `speed_factor` */
     if (scene->adt && scene->adt->action) {
       strip_speed_factor_fix_rna_path(strip, &scene->adt->action->curves);
@@ -1523,7 +1523,7 @@ static bool strip_transform_filter_set(Strip *strip, void * /*user_data*/)
 
 static bool strip_meta_channels_ensure(Strip *strip, void * /*user_data*/)
 {
-  if (strip->type == SEQ_TYPE_META) {
+  if (strip->type == STRIP_TYPE_META) {
     SEQ_channels_ensure(&strip->channels);
   }
   return true;
@@ -1768,8 +1768,8 @@ static bool version_fix_delete_flag(Strip *strip, void * /*user_data*/)
 static bool version_set_seq_single_frame_content(Strip *strip, void * /*user_data*/)
 {
   if ((strip->len == 1) &&
-      (strip->type == SEQ_TYPE_IMAGE ||
-       ((strip->type & SEQ_TYPE_EFFECT) && SEQ_effect_get_num_inputs(strip->type) == 0)))
+      (strip->type == STRIP_TYPE_IMAGE ||
+       ((strip->type & STRIP_TYPE_EFFECT) && SEQ_effect_get_num_inputs(strip->type) == 0)))
   {
     strip->flag |= SEQ_SINGLE_FRAME_CONTENT;
   }
@@ -1778,7 +1778,7 @@ static bool version_set_seq_single_frame_content(Strip *strip, void * /*user_dat
 
 static bool version_seq_fix_broken_sound_strips(Strip *strip, void * /*user_data*/)
 {
-  if (strip->type != SEQ_TYPE_SOUND_RAM || strip->speed_factor != 0.0f) {
+  if (strip->type != STRIP_TYPE_SOUND_RAM || strip->speed_factor != 0.0f) {
     return true;
   }
 

commit 0763fe15b81def78ae7df367ee3853be5077e7c8

Fix: Build error after cleanup commit
Broken by de47d27f99b28de876c30729c1449d6c5c80129f.

source/blender/sequencer/intern/thumbnail_cache.cc
@@ -86,7 +86,7 @@ struct ThumbnailCache {
     std::string file_path;
     int frame_index = 0;  /* Frame index (for movies) or image index (for image sequences). */
     int stream_index = 0; /* Stream index (only for multi-stream movies). */
-    SequenceType strip_type = STRIP_TYPE_IMAGE;
+    StripType strip_type = STRIP_TYPE_IMAGE;
 
     /* The following members are payload and do not contribute to uniqueness. */
     int64_t requested_at = 0;