Bforartists/Bforartists

commit 4f799d7a88d59ce9f36b8d7ff5e01ecd455985f8

Fix #125098: uv_seam attribute not accessible procedurally
When moving UV seams from special custom data types to attributes,
I considered them similar to mesh selection or visibility which are
"internal" attributes that aren't accessible procedurally and are
hidden from the UI. In retrospect that was the wrong decision; users
expect UV seams to behave more like bevel weights, and that makes sense.

This PR makes UV seams accessible in modifiers (geometry nodes) by
removing the leading period from the attribute name that indicated their
internal status.

The change of the attribute name is a breaking change of the API to some
extent, even though it's technically only mesh data. To mitigate that
issue, the `mesh.attributes["name"]` lookup function is modified to
support both the old and new names. Versioning code renames the
attribute to the new name when loading older files, and renames the
new name to the old name when saving files. That handling will be
removed as a breaking change in 5.0.

Pull Request: https://projects.blender.org/blender/blender/pulls/129803

source/blender/makesrna/intern/rna_attribute.cc
@@ -640,6 +640,32 @@ int rna_AttributeGroup_length(PointerRNA *ptr)
   return BKE_attributes_length(owner, ATTR_DOMAIN_MASK_ALL, CD_MASK_PROP_ALL);
 }
 
+bool rna_AttributeGroup_lookup_string(PointerRNA *ptr, const char *key, PointerRNA *r_ptr)
+{
+  AttributeOwner owner = owner_from_attribute_pointer_rna(ptr);
+
+  if (CustomDataLayer *layer = BKE_attribute_search_for_write(
+          owner, key, CD_MASK_PROP_ALL, ATTR_DOMAIN_MASK_ALL))
+  {
+    *r_ptr = RNA_pointer_create(ptr->owner_id, &RNA_Attribute, layer);
+    return true;
+  }
+
+  /* Support retrieving UV seam name convention with older name. To be removed as part of 5.0
+   * breaking changes. */
+  if (STREQ(key, ".uv_seam")) {
+    if (CustomDataLayer *layer = BKE_attribute_search_for_write(
+            owner, "uv_seam", CD_MASK_PROP_ALL, ATTR_DOMAIN_MASK_ALL))
+    {
+      *r_ptr = RNA_pointer_create(ptr->owner_id, &RNA_Attribute, layer);
+      return true;
+    }
+  }
+
+  *r_ptr = PointerRNA_NULL;
+  return false;
+}
+
 static int rna_AttributeGroupID_active_index_get(PointerRNA *ptr)
 {
   AttributeOwner owner = AttributeOwner::from_id(ptr->owner_id);
@@ -1776,7 +1802,7 @@ void rna_def_attributes_common(StructRNA *srna, const AttributeOwnerType type)
                                     "rna_AttributeGroup_iterator_get",
                                     "rna_AttributeGroup_length",
                                     nullptr,
-                                    nullptr,
+                                    "rna_AttributeGroup_lookup_string",
                                     nullptr);
   RNA_def_property_struct_type(prop, "Attribute");
   RNA_def_property_ui_text(prop, "Attributes", "Geometry attributes");

source/blender/blenkernel/intern/data_transfer.cc
@@ -966,9 +966,9 @@ static bool data_transfer_layersmapping_generate(ListBase *r_map,
       return true;
     }
     if (r_map && cddata_type == CD_FAKE_SEAM) {
-      if (!CustomData_has_layer_named(&me_dst->edge_data, CD_PROP_BOOL, ".uv_seam")) {
+      if (!CustomData_has_layer_named(&me_dst->edge_data, CD_PROP_BOOL, "uv_seam")) {
         CustomData_add_layer_named(
-            &me_dst->edge_data, CD_PROP_BOOL, CD_SET_DEFAULT, me_dst->edges_num, ".uv_seam");
+            &me_dst->edge_data, CD_PROP_BOOL, CD_SET_DEFAULT, me_dst->edges_num, "uv_seam");
       }
       data_transfer_layersmapping_add_item_cd(
           r_map,
@@ -976,9 +976,9 @@ static bool data_transfer_layersmapping_generate(ListBase *r_map,
           mix_mode,
           mix_factor,
           mix_weights,
-          CustomData_get_layer_named(&me_src->edge_data, CD_PROP_BOOL, ".uv_seam"),
+          CustomData_get_layer_named(&me_src->edge_data, CD_PROP_BOOL, "uv_seam"),
           CustomData_get_layer_named_for_write(
-              &me_dst->edge_data, CD_PROP_BOOL, ".uv_seam", me_dst->edges_num),
+              &me_dst->edge_data, CD_PROP_BOOL, "uv_seam", me_dst->edges_num),
           interp,
           interp_data);
       return true;

source/blender/blenkernel/intern/mesh.cc
@@ -37,6 +37,7 @@
 #include "BLI_set.hh"
 #include "BLI_span.hh"
 #include "BLI_string.h"
+#include "BLI_string_utils.hh"
 #include "BLI_task.hh"
 #include "BLI_time.h"
 #include "BLI_utildefines.h"
@@ -263,6 +264,53 @@ static void mesh_foreach_path(ID *id, BPathForeachPathData *bpath_data)
   }
 }
 
+static void rename_seam_layer_to_old_name(const ListBase vertex_groups,
+                                          const Span<CustomDataLayer> vert_layers,
+                                          MutableSpan<CustomDataLayer> edge_layers,
+                                          const Span<CustomDataLayer> face_layers,
+                                          const Span<CustomDataLayer> corner_layers)
+{
+  CustomDataLayer *seam_layer = nullptr;
+  for (CustomDataLayer &layer : edge_layers) {
+    if (STREQ(layer.name, ".uv_seam")) {
+      return;
+    }
+    if (layer.type == CD_PROP_BOOL && STREQ(layer.name, "uv_seam")) {
+      seam_layer = &layer;
+    }
+  }
+
+  if (!seam_layer) {
+    return;
+  }
+
+  /* Current files are not expected to have a ".uv_seam" attribute (the old name) except in the
+   * rare case users created it themselves. If that happens, avoid renaming the current UV seam
+   * attribute so that at least it's not hidden in the old version. */
+  for (const CustomDataLayer &layer : vert_layers) {
+    if (STREQ(layer.name, ".uv_seam")) {
+      return;
+    }
+  }
+  for (const CustomDataLayer &layer : face_layers) {
+    if (STREQ(layer.name, ".uv_seam")) {
+      return;
+    }
+  }
+  for (const CustomDataLayer &layer : corner_layers) {
+    if (STREQ(layer.name, ".uv_seam")) {
+      return;
+    }
+  }
+  LISTBASE_FOREACH (const bDeformGroup *, vertex_group, &vertex_groups) {
+    if (STREQ(vertex_group->name, ".uv_seam")) {
+      return;
+    }
+  }
+
+  STRNCPY(seam_layer->name, ".uv_seam");
+}
+
 static void mesh_blend_write(BlendWriter *writer, ID *id, const void *id_address)
 {
   using namespace blender;
@@ -301,6 +349,9 @@ static void mesh_blend_write(BlendWriter *writer, ID *id, const void *id_address
     CustomData_blend_write_prepare(mesh->corner_data, loop_layers, {});
     CustomData_blend_write_prepare(mesh->face_data, face_layers, {});
     if (!is_undo) {
+      /* Write forward compatible format. To be removed in 5.0. */
+      rename_seam_layer_to_old_name(
+          mesh->vertex_group_names, vert_layers, edge_layers, face_layers, loop_layers);
       mesh_sculpt_mask_to_legacy(vert_layers);
       mesh_custom_normals_to_legacy(loop_layers);
     }

source/blender/blenkernel/intern/mesh_remap.cc
@@ -1356,7 +1356,7 @@ void BKE_mesh_remap_calc_loops_from_mesh(const int mode,
     /* First, generate the islands, if possible. */
     if (gen_islands_src) {
       const bool *uv_seams = static_cast<const bool *>(
-          CustomData_get_layer_named(&me_src->edge_data, CD_PROP_BOOL, ".uv_seam"));
+          CustomData_get_layer_named(&me_src->edge_data, CD_PROP_BOOL, "uv_seam"));
       use_islands = gen_islands_src(reinterpret_cast<const float(*)[3]>(positions_src.data()),
                                     num_verts_src,
                                     edges_src.data(),

source/blender/blenloader/intern/versioning_400.cc
@@ -50,6 +50,7 @@
 #include "BLI_string.h"
 #include "BLI_string_ref.hh"
 #include "BLI_string_utf8.h"
+#include "BLI_string_utils.hh"
 
 #include "BKE_anim_data.hh"
 #include "BKE_animsys.h"
@@ -3296,6 +3297,47 @@ static void version_node_locations_to_global(bNodeTree &ntree)
   }
 }
 
+static CustomDataLayer *find_old_seam_layer(CustomData &custom_data, const blender::StringRef name)
+{
+  for (CustomDataLayer &layer : blender::MutableSpan(custom_data.layers, custom_data.totlayer)) {
+    if (layer.name == name) {
+      return &layer;
+    }
+  }
+  return nullptr;
+}
+
+static void rename_mesh_uv_seam_attribute(Mesh &mesh)
+{
+  using namespace blender;
+  CustomDataLayer *old_seam_layer = find_old_seam_layer(mesh.edge_data, ".uv_seam");
+  if (!old_seam_layer) {
+    return;
+  }
+  Set<StringRef> names;
+  for (const CustomDataLayer &layer : Span(mesh.vert_data.layers, mesh.vert_data.totlayer)) {
+    names.add_new(layer.name);
+  }
+  for (const CustomDataLayer &layer : Span(mesh.edge_data.layers, mesh.edge_data.totlayer)) {
+    names.add_new(layer.name);
+  }
+  for (const CustomDataLayer &layer : Span(mesh.face_data.layers, mesh.face_data.totlayer)) {
+    names.add_new(layer.name);
+  }
+  for (const CustomDataLayer &layer : Span(mesh.corner_data.layers, mesh.corner_data.totlayer)) {
+    names.add_new(layer.name);
+  }
+  LISTBASE_FOREACH (const bDeformGroup *, vertex_group, &mesh.vertex_group_names) {
+    names.add(vertex_group->name);
+  }
+
+  /* If the new UV name is already taken, still rename the attribute so it becomes visible in the
+   * list. Then the user can deal with the name conflict themselves. */
+  const std::string new_name = BLI_uniquename_cb(
+      [&](const StringRef name) { return names.contains(name); }, '.', "uv_seam");
+  STRNCPY(old_seam_layer->name, new_name.c_str());
+}
+
 /**
  * Clear unnecessary pointers to data blocks on output sockets group input nodes.
  * These values should never have been set in the first place. They are not harmful on their own,
@@ -5325,6 +5367,7 @@ void blo_do_versions_400(FileData *fd, Library * /*lib*/, Main *bmain)
   LISTBASE_FOREACH (Mesh *, mesh, &bmain->meshes) {
     blender::bke::mesh_sculpt_mask_to_generic(*mesh);
     blender::bke::mesh_custom_normals_to_generic(*mesh);
+    rename_mesh_uv_seam_attribute(*mesh);
   }
 
   /**

source/blender/bmesh/intern/bmesh_mesh_convert.cc
@@ -127,7 +127,7 @@ bool BM_attribute_stored_in_bmesh_builtin(const StringRef name)
               ".hide_vert",
               ".hide_edge",
               ".hide_poly",
-              ".uv_seam",
+              "uv_seam",
               ".select_vert",
               ".select_edge",
               ".select_poly",
@@ -426,7 +426,7 @@ void BM_mesh_bm_from_me(BMesh *bm, const Mesh *mesh, const BMeshFromMeshParams *
   const VArraySpan material_indices = *attributes.lookup<int>("material_index", AttrDomain::Face);
   const VArraySpan sharp_faces = *attributes.lookup<bool>("sharp_face", AttrDomain::Face);
   const VArraySpan sharp_edges = *attributes.lookup<bool>("sharp_edge", AttrDomain::Edge);
-  const VArraySpan uv_seams = *attributes.lookup<bool>(".uv_seam", AttrDomain::Edge);
+  const VArraySpan uv_seams = *attributes.lookup<bool>("uv_seam", AttrDomain::Edge);
 
   const Span<float3> positions = mesh->vert_positions();
   Array<BMVert *> vtable(mesh->verts_num);
@@ -1502,7 +1502,7 @@ void BM_mesh_bm_to_me(Main *bmain, BMesh *bm, Mesh *mesh, const BMeshToMeshParam
     sharp_edge = attrs.lookup_or_add_for_write_only_span<bool>("sharp_edge", AttrDomain::Edge);
   }
   if (need_uv_seams) {
-    uv_seams = attrs.lookup_or_add_for_write_only_span<bool>(".uv_seam", AttrDomain::Edge);
+    uv_seams = attrs.lookup_or_add_for_write_only_span<bool>("uv_seam", AttrDomain::Edge);
   }
   if (need_hide_edge) {
     hide_edge = attrs.lookup_or_add_for_write_only_span<bool>(".hide_edge", AttrDomain::Edge);
@@ -1729,7 +1729,7 @@ void BM_mesh_bm_to_me_compact(BMesh &bm,
       sharp_edge = attrs.lookup_or_add_for_write_only_span<bool>("sharp_edge", AttrDomain::Edge);
     }
     if (need_uv_seams) {
-      uv_seams = attrs.lookup_or_add_for_write_only_span<bool>(".uv_seam", AttrDomain::Edge);
+      uv_seams = attrs.lookup_or_add_for_write_only_span<bool>("uv_seam", AttrDomain::Edge);
     }
     if (need_hide_edge) {
       hide_edge = attrs.lookup_or_add_for_write_only_span<bool>(".hide_edge", AttrDomain::Edge);

source/blender/editors/mesh/editface.cc
@@ -236,7 +236,7 @@ static void build_poly_connections(blender::AtomicDisjointSet &islands,
 
   const bke::AttributeAccessor attributes = mesh.attributes();
   const VArray<bool> uv_seams = *attributes.lookup_or_default<bool>(
-      ".uv_seam", bke::AttrDomain::Edge, false);
+      "uv_seam", bke::AttrDomain::Edge, false);
   const VArray<bool> hide_poly = *attributes.lookup_or_default<bool>(
       ".hide_poly", bke::AttrDomain::Face, false);
 
@@ -286,7 +286,7 @@ static void paintface_select_linked_faces(Mesh &mesh,
 
   bke::MutableAttributeAccessor attributes = mesh.attributes_for_write();
   const VArray<bool> uv_seams = *attributes.lookup_or_default<bool>(
-      ".uv_seam", bke::AttrDomain::Edge, false);
+      "uv_seam", bke::AttrDomain::Edge, false);
   bke::SpanAttributeWriter<bool> select_poly = attributes.lookup_or_add_for_write_span<bool>(
       ".select_poly", bke::AttrDomain::Face);
 

source/blender/editors/sculpt_paint/sculpt_face_set.cc
@@ -769,7 +769,7 @@ static int init_op_exec(bContext *C, wmOperator *op)
     }
     case InitMode::UVSeams: {
       const VArraySpan<bool> uv_seams = *mesh->attributes().lookup_or_default<bool>(
-          ".uv_seam", bke::AttrDomain::Edge, false);
+          "uv_seam", bke::AttrDomain::Edge, false);
       init_flood_fill(ob,
                       [&](const int /*from_face*/, const int edge, const int /*to_face*/) -> bool {
                         return !uv_seams[edge];

commit 1bbed6608a9338e8639b2975f3adcc15bd4761fd

Fix #132692: Grease Pencil Python attribute lookup broken
Caused by 4f799d7a88d59ce9f36b8d7ff5e01ecd455985f8.

source/blender/makesrna/intern/rna_attribute.cc
@@ -642,7 +642,7 @@ int rna_AttributeGroup_length(PointerRNA *ptr)
 
 bool rna_AttributeGroup_lookup_string(PointerRNA *ptr, const char *key, PointerRNA *r_ptr)
 {
-  AttributeOwner owner = owner_from_attribute_pointer_rna(ptr);
+  AttributeOwner owner = owner_from_pointer_rna(ptr);
 
   if (CustomDataLayer *layer = BKE_attribute_search_for_write(
           owner, key, CD_MASK_PROP_ALL, ATTR_DOMAIN_MASK_ALL))