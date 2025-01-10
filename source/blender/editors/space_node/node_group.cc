Bforartists/Bforartists

commit 3a05bb69806eb8599ac2dbf0896edd746f0059d8

Cleanup: Remove unused includes in space node folder
Pull Request: https://projects.blender.org/blender/blender/pulls/132882

source/blender/editors/space_node/node_group.cc
@@ -13,7 +13,6 @@
 #include "DNA_anim_types.h"
 #include "DNA_node_types.h"
 
-#include "BLI_linklist.h"
 #include "BLI_listbase.h"
 #include "BLI_map.hh"
 #include "BLI_math_vector_types.hh"
@@ -37,7 +36,6 @@
 
 #include "DEG_depsgraph_build.hh"
 
-#include "ED_node.hh" /* own include */
 #include "ED_node.hh"
 #include "ED_node_preview.hh"
 #include "ED_render.hh"

source/blender/editors/space_node/drawnode.cc
@@ -9,11 +9,9 @@
 
 #include "BLI_color.hh"
 #include "BLI_string.h"
-#include "BLI_system.h"
 #include "BLI_threads.h"
 
 #include "DNA_node_types.h"
-#include "DNA_object_types.h"
 #include "DNA_screen_types.h"
 #include "DNA_space_types.h"
 #include "DNA_userdef_types.h"
@@ -30,7 +28,6 @@
 #include "BKE_scene.hh"
 #include "BKE_tracking.h"
 
-#include "BLF_api.hh"
 #include "BLT_translation.hh"
 
 #include "BIF_glutil.hh"
@@ -40,7 +37,6 @@
 #include "GPU_framebuffer.hh"
 #include "GPU_immediate.hh"
 #include "GPU_matrix.hh"
-#include "GPU_platform.hh"
 #include "GPU_shader_shared.hh"
 #include "GPU_state.hh"
 #include "GPU_uniform_buffer.hh"
@@ -68,9 +64,7 @@
 #include "NOD_geometry_nodes_gizmos.hh"
 #include "NOD_node_declaration.hh"
 #include "NOD_partial_eval.hh"
-#include "NOD_shader.h"
 #include "NOD_socket.hh"
-#include "NOD_texture.h"
 #include "node_intern.hh" /* own include */
 
 namespace blender::ed::space_node {

source/blender/editors/space_node/node_edit.cc
@@ -32,6 +32,7 @@
 #include "BKE_scene_runtime.hh"
 
 #include "BLI_math_vector.h"
+#include "BLI_math_vector.hh"
 #include "BLI_string.h"
 #include "BLI_string_utf8.h"
 

source/blender/editors/space_node/node_geometry_attribute_search.cc
@@ -2,17 +2,12 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include "BLI_index_range.hh"
-#include "BLI_listbase.h"
 #include "BLI_map.hh"
-#include "BLI_rect.h"
 #include "BLI_set.hh"
 #include "BLI_string.h"
 #include "BLI_string_ref.hh"
 
-#include "DNA_modifier_types.h"
 #include "DNA_node_types.h"
-#include "DNA_object_types.h"
 #include "DNA_space_types.h"
 
 #include "BKE_context.hh"
@@ -20,7 +15,6 @@
 #include "BKE_node_runtime.hh"
 #include "BKE_node_tree_update.hh"
 #include "BKE_node_tree_zones.hh"
-#include "BKE_object.hh"
 
 #include "RNA_access.hh"
 #include "RNA_enum_types.hh"
@@ -28,8 +22,6 @@
 #include "ED_node.hh"
 #include "ED_screen.hh"
 #include "ED_undo.hh"
-
-#include "BLT_translation.hh"
 
 #include "UI_interface.hh"
 #include "UI_resources.hh"

source/blender/editors/space_node/node_intern.hh
@@ -9,9 +9,7 @@
 #pragma once
 
 #include "BLI_compute_context.hh"
-#include "BLI_math_vector.hh"
 #include "BLI_vector.hh"
-#include "BLI_vector_set.hh"
 
 #include "BKE_node.hh"
 

source/blender/editors/space_node/node_ops.cc
@@ -8,7 +8,7 @@
 
 #include "DNA_node_types.h"
 
-#include "ED_node.hh" /* own include */
+#include "ED_node_c.hh"
 #include "ED_screen.hh"
 
 #include "RNA_access.hh"

source/blender/editors/space_node/node_relationships.cc
@@ -38,11 +38,8 @@
 #include "UI_resources.hh"
 #include "UI_view2d.hh"
 
-#include "BLT_translation.hh"
-
 #include "NOD_node_declaration.hh"
 #include "NOD_socket.hh"
-#include "NOD_socket_declarations_geometry.hh"
 
 #include "node_intern.hh" /* own include */
 

source/blender/editors/space_node/node_select.cc
@@ -8,7 +8,6 @@
 
 #include <array>
 #include <cstdlib>
-#include <iostream>
 
 #include "DNA_node_types.h"
 #include "DNA_windowmanager_types.h"
@@ -47,8 +46,6 @@
 #include "UI_view2d.hh"
 
 #include "DEG_depsgraph.hh"
-
-#include "MEM_guardedalloc.h"
 
 #include "node_intern.hh" /* own include */
 

commit 363cea3bbf41eef584e46e04dd1893fa55e2adde

Fix #132893: Node Groups are broken
Caused by 3a05bb69806eb8599ac2dbf0896edd746f0059d8.
The root cause is probably implicit cast from `VecBase<T>` to `T *`.

Pull Request: https://projects.blender.org/blender/blender/pulls/132918

source/blender/editors/space_node/node_group.cc
@@ -15,6 +15,7 @@
 
 #include "BLI_listbase.h"
 #include "BLI_map.hh"
+#include "BLI_math_vector.hh"
 #include "BLI_math_vector_types.hh"
 #include "BLI_rand.hh"
 #include "BLI_set.hh"