Bforartists/Bforartists

commit 65469ac3a47157a35fe4f20d8f93b0bb3c9a5905

Cleanup: VSE: Rename `SequenceColorTag` to `StripColorTag` and flags
Also rename the flags from `SEQUENCE_COLOR_*` to `STRIP_COLOR_*`.

scripts/startup/bl_ui/space_sequencer.py
@@ -1406,7 +1406,7 @@ class SEQUENCER_PT_color_tag_picker(SequencerColorTagPicker, Panel):
         row = layout.row(align=True)
         row.operator("sequencer.strip_color_tag_set", icon='X').color = 'NONE'
         for i in range(1, 10):
-            icon = 'SEQUENCE_COLOR_{:02d}'.format(i)
+            icon = 'STRIP_COLOR_{:02d}'.format(i)
             row.operator("sequencer.strip_color_tag_set", icon=icon).color = 'COLOR_{:02d}'.format(i)
 
 

source/blender/blenloader/intern/versioning_300.cc
@@ -492,7 +492,7 @@ static void do_versions_sequencer_speed_effect_recursive(Scene *scene, const Lis
 
 static bool do_versions_sequencer_color_tags(Strip *strip, void * /*user_data*/)
 {
-  strip->color_tag = SEQUENCE_COLOR_NONE;
+  strip->color_tag = STRIP_COLOR_NONE;
   return true;
 }
 
@@ -3088,7 +3088,7 @@ void blo_do_versions_300(FileData *fd, Library * /*lib*/, Main *bmain)
       }
     }
 
-    /* Set strip color tags to SEQUENCE_COLOR_NONE. */
+    /* Set strip color tags to STRIP_COLOR_NONE. */
     LISTBASE_FOREACH (Scene *, scene, &bmain->scenes) {
       if (scene->ed != nullptr) {
         SEQ_for_each_callback(&scene->ed->seqbase, do_versions_sequencer_color_tags, nullptr);

source/blender/editors/include/UI_icons.hh
@@ -904,15 +904,15 @@ DEF_ICON_VECTOR(COLLECTION_COLOR_06)
 DEF_ICON_VECTOR(COLLECTION_COLOR_07)
 DEF_ICON_VECTOR(COLLECTION_COLOR_08)
 
-DEF_ICON_VECTOR(SEQUENCE_COLOR_01)
-DEF_ICON_VECTOR(SEQUENCE_COLOR_02)
-DEF_ICON_VECTOR(SEQUENCE_COLOR_03)
-DEF_ICON_VECTOR(SEQUENCE_COLOR_04)
-DEF_ICON_VECTOR(SEQUENCE_COLOR_05)
-DEF_ICON_VECTOR(SEQUENCE_COLOR_06)
-DEF_ICON_VECTOR(SEQUENCE_COLOR_07)
-DEF_ICON_VECTOR(SEQUENCE_COLOR_08)
-DEF_ICON_VECTOR(SEQUENCE_COLOR_09)
+DEF_ICON_VECTOR(STRIP_COLOR_01)
+DEF_ICON_VECTOR(STRIP_COLOR_02)
+DEF_ICON_VECTOR(STRIP_COLOR_03)
+DEF_ICON_VECTOR(STRIP_COLOR_04)
+DEF_ICON_VECTOR(STRIP_COLOR_05)
+DEF_ICON_VECTOR(STRIP_COLOR_06)
+DEF_ICON_VECTOR(STRIP_COLOR_07)
+DEF_ICON_VECTOR(STRIP_COLOR_08)
+DEF_ICON_VECTOR(STRIP_COLOR_09)
 
 DEF_ICON_VECTOR(LIBRARY_DATA_INDIRECT)
 DEF_ICON_VECTOR(LIBRARY_DATA_OVERRIDE_NONEDITABLE)

source/blender/editors/interface/interface_icons.cc
@@ -436,15 +436,15 @@ static void vicon_strip_color_draw(
       vicon_strip_color_draw(color, x, y, w, h, alpha); \
     }
 
-DEF_ICON_STRIP_COLOR_DRAW(01, SEQUENCE_COLOR_01);
-DEF_ICON_STRIP_COLOR_DRAW(02, SEQUENCE_COLOR_02);
-DEF_ICON_STRIP_COLOR_DRAW(03, SEQUENCE_COLOR_03);
-DEF_ICON_STRIP_COLOR_DRAW(04, SEQUENCE_COLOR_04);
-DEF_ICON_STRIP_COLOR_DRAW(05, SEQUENCE_COLOR_05);
-DEF_ICON_STRIP_COLOR_DRAW(06, SEQUENCE_COLOR_06);
-DEF_ICON_STRIP_COLOR_DRAW(07, SEQUENCE_COLOR_07);
-DEF_ICON_STRIP_COLOR_DRAW(08, SEQUENCE_COLOR_08);
-DEF_ICON_STRIP_COLOR_DRAW(09, SEQUENCE_COLOR_09);
+DEF_ICON_STRIP_COLOR_DRAW(01, STRIP_COLOR_01);
+DEF_ICON_STRIP_COLOR_DRAW(02, STRIP_COLOR_02);
+DEF_ICON_STRIP_COLOR_DRAW(03, STRIP_COLOR_03);
+DEF_ICON_STRIP_COLOR_DRAW(04, STRIP_COLOR_04);
+DEF_ICON_STRIP_COLOR_DRAW(05, STRIP_COLOR_05);
+DEF_ICON_STRIP_COLOR_DRAW(06, STRIP_COLOR_06);
+DEF_ICON_STRIP_COLOR_DRAW(07, STRIP_COLOR_07);
+DEF_ICON_STRIP_COLOR_DRAW(08, STRIP_COLOR_08);
+DEF_ICON_STRIP_COLOR_DRAW(09, STRIP_COLOR_09);
 
 #  undef DEF_ICON_STRIP_COLOR_DRAW
 
@@ -861,15 +861,15 @@ static void init_internal_icons()
   def_internal_vicon(ICON_COLLECTION_COLOR_07, vicon_collection_color_draw_07);
   def_internal_vicon(ICON_COLLECTION_COLOR_08, vicon_collection_color_draw_08);
 
-  def_internal_vicon(ICON_SEQUENCE_COLOR_01, vicon_strip_color_draw_01);
-  def_internal_vicon(ICON_SEQUENCE_COLOR_02, vicon_strip_color_draw_02);
-  def_internal_vicon(ICON_SEQUENCE_COLOR_03, vicon_strip_color_draw_03);
-  def_internal_vicon(ICON_SEQUENCE_COLOR_04, vicon_strip_color_draw_04);
-  def_internal_vicon(ICON_SEQUENCE_COLOR_05, vicon_strip_color_draw_05);
-  def_internal_vicon(ICON_SEQUENCE_COLOR_06, vicon_strip_color_draw_06);
-  def_internal_vicon(ICON_SEQUENCE_COLOR_07, vicon_strip_color_draw_07);
-  def_internal_vicon(ICON_SEQUENCE_COLOR_08, vicon_strip_color_draw_08);
-  def_internal_vicon(ICON_SEQUENCE_COLOR_09, vicon_strip_color_draw_09);
+  def_internal_vicon(ICON_STRIP_COLOR_01, vicon_strip_color_draw_01);
+  def_internal_vicon(ICON_STRIP_COLOR_02, vicon_strip_color_draw_02);
+  def_internal_vicon(ICON_STRIP_COLOR_03, vicon_strip_color_draw_03);
+  def_internal_vicon(ICON_STRIP_COLOR_04, vicon_strip_color_draw_04);
+  def_internal_vicon(ICON_STRIP_COLOR_05, vicon_strip_color_draw_05);
+  def_internal_vicon(ICON_STRIP_COLOR_06, vicon_strip_color_draw_06);
+  def_internal_vicon(ICON_STRIP_COLOR_07, vicon_strip_color_draw_07);
+  def_internal_vicon(ICON_STRIP_COLOR_08, vicon_strip_color_draw_08);
+  def_internal_vicon(ICON_STRIP_COLOR_09, vicon_strip_color_draw_09);
 
   def_internal_vicon(ICON_LIBRARY_DATA_INDIRECT, vicon_strip_color_draw_library_data_indirect);
   def_internal_vicon(ICON_LIBRARY_DATA_OVERRIDE_NONEDITABLE,

source/blender/editors/space_sequencer/sequencer_edit.cc
@@ -3423,8 +3423,7 @@ void SEQUENCER_OT_strip_color_tag_set(wmOperatorType *ot)
   /* Flags. */
   ot->flag = OPTYPE_REGISTER | OPTYPE_UNDO;
 
-  RNA_def_enum(
-      ot->srna, "color", rna_enum_strip_color_items, SEQUENCE_COLOR_NONE, "Color Tag", "");
+  RNA_def_enum(ot->srna, "color", rna_enum_strip_color_items, STRIP_COLOR_NONE, "Color Tag", "");
 }
 
 /** \} */

source/blender/editors/space_sequencer/sequencer_timeline_draw.cc
@@ -295,8 +295,8 @@ static void color3ubv_from_seq(const Scene *curscene,
                                const bool is_muted,
                                uchar r_col[3])
 {
-  if (show_strip_color_tag && uint(strip->color_tag) < SEQUENCE_COLOR_TOT &&
-      strip->color_tag != SEQUENCE_COLOR_NONE)
+  if (show_strip_color_tag && uint(strip->color_tag) < STRIP_COLOR_TOT &&
+      strip->color_tag != STRIP_COLOR_NONE)
   {
     bTheme *btheme = UI_GetTheme();
     const ThemeStripColor *strip_color = &btheme->strip_color[strip->color_tag];

source/blender/makesdna/DNA_sequence_types.h
@@ -868,20 +868,20 @@ enum {
 };
 
 /** #Strip.color_tag. */
-typedef enum SequenceColorTag {
-  SEQUENCE_COLOR_NONE = -1,
-  SEQUENCE_COLOR_01,
-  SEQUENCE_COLOR_02,
-  SEQUENCE_COLOR_03,
-  SEQUENCE_COLOR_04,
-  SEQUENCE_COLOR_05,
-  SEQUENCE_COLOR_06,
-  SEQUENCE_COLOR_07,
-  SEQUENCE_COLOR_08,
-  SEQUENCE_COLOR_09,
+typedef enum StripColorTag {
+  STRIP_COLOR_NONE = -1,
+  STRIP_COLOR_01,
+  STRIP_COLOR_02,
+  STRIP_COLOR_03,
+  STRIP_COLOR_04,
+  STRIP_COLOR_05,
+  STRIP_COLOR_06,
+  STRIP_COLOR_07,
+  STRIP_COLOR_08,
+  STRIP_COLOR_09,
 
-  SEQUENCE_COLOR_TOT,
-} SequenceColorTag;
+  STRIP_COLOR_TOT,
+} StripColorTag;
 
 /* Strip->StripTransform->filter */
 enum {

source/blender/makesdna/DNA_userdef_types.h
@@ -540,7 +540,7 @@ typedef struct bTheme {
   /* See COLLECTION_COLOR_TOT for the number of collection colors. */
   ThemeCollectionColor collection_color[8];
 
-  /* See SEQUENCE_COLOR_TOT for the total number of strip colors. */
+  /* See STRIP_COLOR_TOT for the total number of strip colors. */
   ThemeStripColor strip_color[9];
 
   int active_theme_area;

commit 60213eb1826f4af87533c72fa0c0274777c5753a

Fix #133812: Strip color picker uses invalid icon enum
Regression in 65469ac3a4 - missed "sequence" -> "strip" renaming.

Pull Request: https://projects.blender.org/blender/blender/pulls/133814

scripts/startup/bl_ui/space_sequencer.py
@@ -1524,7 +1524,7 @@ class SEQUENCER_PT_strip(SequencerButtonsPanel, Panel):
         if strip.color_tag == 'NONE':
             sub.popover(panel="SEQUENCER_PT_color_tag_picker", text="", icon='COLOR')
         else:
-            icon = 'SEQUENCE_' + strip.color_tag
+            icon = 'STRIP_' + strip.color_tag
             sub.popover(panel="SEQUENCER_PT_color_tag_picker", text="", icon=icon)
 
         row.separator()