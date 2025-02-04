pixelsandpointers/blender

Build: Enable custom USD namespace for Blender again
This was removed to make building USD plugins easier, without the need to
compile them for Blender specifically. However it also causes errors when
using the bpy module together with the usd-core pip package.

In practice there are not many USD plugins, and often need to be compiled
for Blender specifically anyway due to the monolithic library and DCCs being
on different USD versions.

Additionally, this adds a patch to avoid conflicts when loading the
same USD library version through Blender and usd-core on Windows and macOS.

Fix #127132: Crash using bpy and usd-core pip package together.

Pull Request: https://projects.blender.org/blender/blender/pulls/133083

build_files/build_environment/cmake/usd.cmake
@@ -49,6 +49,10 @@ elseif(UNIX)
   endif()
 endif()
 
+# Custom namespace to prevent conflicts when importing both bpy module
+# and usd-core pip packages with the same version but different libs.
+string(REPLACE "." "_" USD_NAMESPACE "pxrBlender_v${USD_VERSION}")
+
 set(USD_EXTRA_ARGS
   ${DEFAULT_BOOST_FLAGS}
   ${USD_PLATFORM_FLAGS}
@@ -60,6 +64,7 @@ set(USD_EXTRA_ARGS
   -DOPENEXR_INCLUDE_DIR=${LIBDIR}/imath/include
   -DImath_DIR=${LIBDIR}/imath/lib/cmake/Imath
   -DOPENVDB_LOCATION=${LIBDIR}/openvdb
+  -DPXR_SET_INTERNAL_NAMESPACE=${USD_NAMESPACE}
   -DPXR_ENABLE_PYTHON_SUPPORT=ON
   -DPXR_USE_PYTHON_3=ON
   -DPXR_BUILD_IMAGING=ON
@@ -89,7 +94,7 @@ set(USD_EXTRA_ARGS
   # Tracking ticket https://github.com/PixarAnimationStudios/USD/issues/1386
   -DPXR_BUILD_OPENCOLORIO_PLUGIN=OFF
   # We'd like Vulkan support on, but it has trouble not finding the SDK since we have
-  # the invididual components in the deps builder. 
+  # the invididual components in the deps builder.
   -DPXR_ENABLE_VULKAN_SUPPORT=OFF
   -DPXR_ENABLE_PTEX_SUPPORT=OFF
   -DPXR_BUILD_USD_TOOLS=OFF
@@ -128,13 +133,16 @@ ExternalProject_Add(external_usd
       ${PATCH_DIR}/usd_core_profile.diff &&
     ${PATCH_CMD} -p 1 -d
       ${BUILD_DIR}/usd/src/external_usd <
+      ${PATCH_DIR}/usd_ctor.diff &&
+    ${PATCH_CMD} -p 1 -d
+      ${BUILD_DIR}/usd/src/external_usd <
       ${PATCH_DIR}/usd_3204.diff &&
     ${PATCH_CMD} -p 1 -d
       ${BUILD_DIR}/usd/src/external_usd <
       ${PATCH_DIR}/usd_3243.diff  &&
     ${PATCH_CMD} -p 1 -d
       ${BUILD_DIR}/usd/src/external_usd <
-      ${PATCH_DIR}/usd_3434.diff 
+      ${PATCH_DIR}/usd_3434.diff
   CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX=${LIBDIR}/usd
     -Wno-dev

Bforartists/Bforartists

Co-authored-by: Ray Molenkamp <github@lazydodo.com>

build_files/build_environment/cmake/usd.cmake
@@ -49,6 +49,10 @@ elseif(UNIX)
   endif()
 endif()
 
+# Custom namespace to prevent conflicts when importing both bpy module
+# and usd-core pip packages with the same version but different libs.
+string(REPLACE "." "_" USD_NAMESPACE "pxrBlender_v${USD_VERSION}")
+
 set(USD_EXTRA_ARGS
   ${DEFAULT_BOOST_FLAGS}
   ${USD_PLATFORM_FLAGS}
@@ -59,6 +63,7 @@ set(USD_EXTRA_ARGS
   -DOPENEXR_INCLUDE_DIR=${LIBDIR}/imath/include
   -DImath_DIR=${LIBDIR}/imath
   -DOPENVDB_LOCATION=${LIBDIR}/openvdb
+  -DPXR_SET_INTERNAL_NAMESPACE=${USD_NAMESPACE}
   -DPXR_ENABLE_PYTHON_SUPPORT=ON
   -DPXR_USE_PYTHON_3=ON
   -DPXR_BUILD_IMAGING=ON
@@ -124,7 +129,10 @@ ExternalProject_Add(external_usd
       ${PATCH_DIR}/usd_core_profile.diff &&
     ${PATCH_CMD} -p 1 -d
       ${BUILD_DIR}/usd/src/external_usd <
-      ${PATCH_DIR}/usd_metal_edf.diff
+      ${PATCH_DIR}/usd_metal_edf.diff &&
+    ${PATCH_CMD} -p 1 -d
+      ${BUILD_DIR}/usd/src/external_usd <
+      ${PATCH_DIR}/usd_ctor.diff
 
   CMAKE_ARGS
     -DCMAKE_INSTALL_PREFIX=${LIBDIR}/usd