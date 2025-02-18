Bforartists/Bforartists

commit 9f902404227a7591c22da0b5de3aaf579e68d4a1

UI: Remove "Adjust Last Operation" region when button sends undo push
Addresses #78171, #61828, #62473 and many closed reports about this.

Until now, the "Adjust Last Operation" would keep being available after
changing various properties in the UI. Actually using it to adjust the
last operation parameters would undo the other changes done through the
UI, which is unexpected and easy to miss. Users would keep running into
this, thinking it was a bug, as the many reports merged into #78171
indicate.

As agreed on in the report, adjusting the last operation shouldn't be
possible after changing properties in the UI that send an undo push.
That's what this PR implements, together with removing the "Adjust Last
Operation" region at that point.

Pull Request: https://projects.blender.org/blender/blender/pulls/133003

source/blender/editors/interface/interface_handlers.cc
@@ -1125,6 +1125,9 @@ static void ui_apply_but_funcs_after(bContext *C)
     ui_afterfunc_update_preferences_dirty(&after);
 
     if (after.undostr[0]) {
+      /* Remove "Adjust Last Operation" HUD. Using it would revert this undo push which isn't
+       * obvious, see #78171. */
+      WM_operator_stack_clear(CTX_wm_manager(C));
       ED_undo_push(C, after.undostr);
     }
   }

source/blender/editors/interface/regions/interface_region_hud.cc
@@ -217,11 +217,26 @@ static void hud_region_draw(const bContext *C, ARegion *region)
   }
 }
 
+static void hud_region_listener(const wmRegionListenerParams *params)
+{
+  ARegion *region = params->region;
+  const wmNotifier *wmn = params->notifier;
+
+  switch (wmn->category) {
+    case NC_WM:
+      if (wmn->data == ND_HISTORY) {
+        ED_region_tag_redraw(region);
+      }
+      break;
+  }
+}
+
 ARegionType *ED_area_type_hud(int space_type)
 {
   ARegionType *art = MEM_cnew<ARegionType>(__func__);
   art->regionid = RGN_TYPE_HUD;
   art->keymapflag = ED_KEYMAP_UI | ED_KEYMAP_VIEW2D;
+  art->listener = hud_region_listener;
   art->layout = hud_region_layout;
   art->draw = hud_region_draw;
   art->init = hud_region_init;

commit cde5411cf351cdf86de328a00abd3ac12d0558a7

Fix #134505: Bevel redo panel closes when defining profile
Caused by 9f90240422
`UI_BTYPE_CURVEPROFILE` button has undo flag set. see button-layout in:
`edbm_bevel_ui() -> uiTemplateCurveProfile()`.

This  forces an undo push with the help of `ui_apply_but_undo/ui_apply_but_funcs_after`.
To handle the situation, skip undo push for buttons in hud region.

Pull Request: https://projects.blender.org/blender/blender/pulls/134552

source/blender/editors/interface/interface_handlers.cc
@@ -943,6 +943,12 @@ static void ui_apply_but_undo(uiBut *but)
     return;
   }
 
+  /* Skip undo push for buttons in redo panel, see: #134505. */
+  const ARegion *region = CTX_wm_region(static_cast<bContext *>(but->block->evil_C));
+  if (region->regiontype == RGN_TYPE_HUD) {
+    return;
+  }
+
   const char *str = nullptr;
   size_t str_len_clip = SIZE_MAX - 1;
   bool skip_undo = false;