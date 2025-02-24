Bforartists/Bforartists

commit da4a9b1055f0620a288c4f763364b1e9a9c7aa85

Cleanup: DRW: Make Open subdiv evaluator part of a DRW module
This avoids the global variable access with race condition.
Rel #134690

Pull Request: https://projects.blender.org/blender/blender/pulls/134855

source/blender/draw/intern/draw_cache_impl_subdivision.cc
@@ -2,6 +2,7 @@
  *
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
+#include "draw_manager_c.hh"
 #include "draw_subdivision.hh"
 
 #include "DNA_mesh_types.h"
@@ -43,6 +44,7 @@
 #include "draw_cache_extract.hh"
 #include "draw_cache_impl.hh"
 #include "draw_cache_inline.hh"
+#include "draw_common_c.hh"
 #include "draw_shader.hh"
 #include "draw_subdiv_shader_shared.hh"
 #include "mesh_extractors/extract_mesh.hh"
@@ -1926,7 +1928,18 @@ void DRW_subdivide_loose_geom(DRWSubdivCache &subdiv_cache, const MeshBufferCach
   });
 }
 
-static OpenSubdiv_EvaluatorCache *g_evaluator_cache = nullptr;
+/**
+ * OpenSubdiv GPU evaluation module.
+ */
+struct SubdivModule {
+  OpenSubdiv_EvaluatorCache *evaluator_cache = openSubdiv_createEvaluatorCache(
+      OPENSUBDIV_EVALUATOR_GPU);
+
+  ~SubdivModule()
+  {
+    openSubdiv_deleteEvaluatorCache(evaluator_cache);
+  }
+};
 
 void DRW_create_subdivision(Object &ob,
                             Mesh &mesh,
@@ -1941,8 +1954,9 @@ void DRW_create_subdivision(Object &ob,
                             const ToolSettings *ts,
                             const bool use_hide)
 {
-  if (g_evaluator_cache == nullptr) {
-    g_evaluator_cache = openSubdiv_createEvaluatorCache(OPENSUBDIV_EVALUATOR_GPU);
+  DRWData &drw_data = *DST.vmempool;
+  if (drw_data.subdiv_module == nullptr) {
+    drw_data.subdiv_module = MEM_new<SubdivModule>("SubdivModule");
   }
 
 #undef TIME_SUBDIV
@@ -1963,7 +1977,7 @@ void DRW_create_subdivision(Object &ob,
                                             do_cage,
                                             ts,
                                             use_hide,
-                                            g_evaluator_cache))
+                                            drw_data.subdiv_module->evaluator_cache))
   {
     return;
   }
@@ -1975,16 +1989,18 @@ void DRW_create_subdivision(Object &ob,
 #endif
 }
 
-void DRW_subdiv_free()
+void DRW_subdiv_module_free(SubdivModule *subdiv_module)
 {
-  DRW_cache_free_old_subdiv();
-
-  if (g_evaluator_cache) {
-    openSubdiv_deleteEvaluatorCache(g_evaluator_cache);
-    g_evaluator_cache = nullptr;
-  }
+  MEM_delete(subdiv_module);
 }
 
+/**
+ * The `bke::subdiv::Subdiv` data is being owned the modifier.
+ * Since the modifier can be freed from any thread (e.g. from depsgraph multithreaded update)
+ * which may not have a valid GPUContext active, we move the data to discard to this free list
+ * until a codepath with a active GPUContext is hit.
+ * This is kindof garbage collection.
+ */
 static LinkNode *gpu_subdiv_free_queue = nullptr;
 static ThreadMutex gpu_subdiv_queue_mutex = BLI_MUTEX_INITIALIZER;
 

source/blender/draw/intern/draw_common_c.hh
@@ -29,6 +29,7 @@ namespace blender::draw {
 class Manager;
 struct CurvesModule;
 struct PointCloudModule;
+struct SubdivModule;
 struct VolumeModule;
 }  // namespace blender::draw
 
@@ -70,6 +71,10 @@ void DRW_pointcloud_module_free(draw::PointCloudModule *module);
 /* If drw_data is nullptr, DST global is accessed to get it. */
 void DRW_volume_init(DRWData *drw_data = nullptr);
 void DRW_volume_module_free(draw::VolumeModule *module);
+
+/* draw_cache_impl_subdivision.cc */
+
+void DRW_subdiv_module_free(draw::SubdivModule *module);
 
 }  // namespace blender::draw
 

source/blender/draw/intern/draw_manager_c.cc
@@ -355,6 +355,7 @@ void DRW_viewport_data_free(DRWData *drw_data)
   DRW_volume_module_free(drw_data->volume_module);
   DRW_pointcloud_module_free(drw_data->pointcloud_module);
   DRW_curves_module_free(drw_data->curves_module);
+  DRW_subdiv_module_free(drw_data->subdiv_module);
   delete drw_data->default_view;
   MEM_freeN(drw_data);
 }

source/blender/draw/intern/draw_manager_c.hh
@@ -32,6 +32,7 @@ struct Object;
 struct Mesh;
 namespace blender::draw {
 struct CurvesModule;
+struct SubdivModule;
 struct VolumeModule;
 struct PointCloudModule;
 struct DRW_Attributes;
@@ -72,6 +73,7 @@ struct DRWData {
   DRWViewData *view_data[2];
   /** Module storage. */
   blender::draw::CurvesModule *curves_module;
+  blender::draw::SubdivModule *subdiv_module;
   blender::draw::VolumeModule *volume_module;
   blender::draw::PointCloudModule *pointcloud_module;
   /** Default view that feeds every engine. */

source/blender/draw/DRW_engine.hh
@@ -171,10 +171,8 @@ void DRW_cache_free_old_batches(Main *bmain);
 
 namespace blender::draw {
 
+/* Free garbage collected subdivision data. */
 void DRW_cache_free_old_subdiv();
-
-/* For the OpenGL evaluators and garbage collected subdivision data. */
-void DRW_subdiv_free();
 
 }  // namespace blender::draw
 

source/blender/windowmanager/intern/wm_init_exit.cc
@@ -596,7 +596,7 @@ void WM_exit_ex(bContext *C, const bool do_python_exit, const bool do_user_exit_
   /* Free the GPU subdivision data after the database to ensure that subdivision structs used by
    * the modifiers were garbage collected. */
   if (gpu_is_init) {
-    blender::draw::DRW_subdiv_free();
+    blender::draw::DRW_cache_free_old_subdiv();
   }
 
   ANIM_fcurves_copybuf_free();

commit abef23cb87078e5d74b4980d873ba2dd1b9d0a8a

Fix #134886: Crash with Subdivision modifier update
This was caused by da4a9b1055 refactor which replaced
the evaluator cache singleton by local variable.

It was unknown that the evaluators in the cache
are actually referenced by the modifier data.

To fix this and fix the thread-unsafety of the global
variable, a mutex is introced around a reduced critical
section inside `draw_subdiv_create_requested_buffers`.

The global evaluator cache is now also refcounted to allow
freeing of the cache when no evaluator is referenced
anymore.

Pull Request: https://projects.blender.org/blender/blender/pulls/134926

source/blender/draw/intern/draw_cache_impl_subdivision.cc
@@ -1771,6 +1771,16 @@ static void draw_subdiv_cache_ensure_mat_offsets(DRWSubdivCache &cache,
   MEM_freeN(per_face_mat_offset);
 }
 
+/**
+ * The evaluators are owned by the `OpenSubdiv_EvaluatorCache` which is being referenced by
+ * `bke::subdiv::Subdiv->evaluator`. So the evaluator cache cannot be freed until all references
+ * are gone. The user counting allows to free the evaluator when there is no more subdiv.
+ */
+static OpenSubdiv_EvaluatorCache *g_subdiv_evaluator_cache = nullptr;
+static uint64_t g_subdiv_evaluator_users = 0;
+/* The evaluator cache is global, so we cannot allow concurent usage and need synchronization. */
+static std::mutex g_subdiv_eval_mutex;
+
 static bool draw_subdiv_create_requested_buffers(Object &ob,
                                                  Mesh &mesh,
                                                  MeshBatchCache &batch_cache,
@@ -1782,8 +1792,7 @@ static bool draw_subdiv_create_requested_buffers(Object &ob,
                                                  const bool do_uvedit,
                                                  const bool do_cage,
                                                  const ToolSettings *ts,
-                                                 const bool use_hide,
-                                                 OpenSubdiv_EvaluatorCache *evaluator_cache)
+                                                 const bool use_hide)
 {
   SubsurfRuntimeData *runtime_data = mesh.runtime->subsurf_runtime_data;
   BLI_assert(runtime_data && runtime_data->has_gpu_subdiv);
@@ -1809,8 +1818,24 @@ static bool draw_subdiv_create_requested_buffers(Object &ob,
     return false;
   }
 
+  /* Lock the entire evaluation to avoid concurent usage of shader objects in evaluator cache. */
+  std::scoped_lock lock(g_subdiv_eval_mutex);
+
+  if (g_subdiv_evaluator_cache == nullptr) {
+    g_subdiv_evaluator_cache = openSubdiv_createEvaluatorCache(OPENSUBDIV_EVALUATOR_GPU);
+  }
+
+  /* Increment evaluator cache reference if an evaluator has been assigned to it. */
+  bool evaluator_might_be_assigned = subdiv->evaluator == nullptr;
+  auto maybe_increment_cache_ref = [evaluator_might_be_assigned](bke::subdiv::Subdiv *subdiv) {
+    if (evaluator_might_be_assigned && subdiv->evaluator != nullptr) {
+      /* An evaluator was assigned. */
+      g_subdiv_evaluator_users++;
+    }
+  };
+
   if (!bke::subdiv::eval_begin_from_mesh(
-          subdiv, mesh_eval, {}, bke::subdiv::SUBDIV_EVALUATOR_TYPE_GPU, evaluator_cache))
+          subdiv, mesh_eval, {}, bke::subdiv::SUBDIV_EVALUATOR_TYPE_GPU, g_subdiv_evaluator_cache))
   {
     /* This could happen in two situations:
      * - OpenSubdiv is disabled.
@@ -1819,6 +1844,7 @@ static bool draw_subdiv_create_requested_buffers(Object &ob,
      * In either way, we can't safely continue. However, we still have to handle potential loose
      * geometry, which is done separately. */
     if (mesh_eval->faces_num) {
+      maybe_increment_cache_ref(subdiv);
       return false;
     }
   }
@@ -1834,6 +1860,7 @@ static bool draw_subdiv_create_requested_buffers(Object &ob,
 
 #ifdef WITH_OPENSUBDIV
   if (!draw_subdiv_build_cache(draw_cache, subdiv, mesh_eval, runtime_data)) {
+    maybe_increment_cache_ref(subdiv);
     return false;
   }
 #endif
@@ -1866,6 +1893,7 @@ static bool draw_subdiv_create_requested_buffers(Object &ob,
 
   mesh_buffer_cache_create_requested_subdiv(batch_cache, mbc, draw_cache, *mr);
 
+  maybe_increment_cache_ref(subdiv);
   return true;
 }
 
@@ -1916,17 +1944,14 @@ void DRW_subdivide_loose_geom(DRWSubdivCache &subdiv_cache, const MeshBufferCach
 }
 
 /**
- * OpenSubdiv GPU evaluation module.
+ * The `bke::subdiv::Subdiv` data is being owned the modifier.
+ * Since the modifier can be freed from any thread (e.g. from depsgraph multithreaded update)
+ * which may not have a valid GPUContext active, we move the data to discard to this free list
+ * until a code-path with a active GPUContext is hit.
+ * This is kindof garbage collection.
  */
-struct SubdivModule {
-  OpenSubdiv_EvaluatorCache *evaluator_cache = openSubdiv_createEvaluatorCache(
-      OPENSUBDIV_EVALUATOR_GPU);
-
-  ~SubdivModule()
-  {
-    openSubdiv_deleteEvaluatorCache(evaluator_cache);
-  }
-};
+static LinkNode *gpu_subdiv_free_queue = nullptr;
+static ThreadMutex gpu_subdiv_queue_mutex = BLI_MUTEX_INITIALIZER;
 
 void DRW_create_subdivision(Object &ob,
                             Mesh &mesh,
@@ -1941,10 +1966,6 @@ void DRW_create_subdivision(Object &ob,
                             const ToolSettings *ts,
                             const bool use_hide)
 {
-  DRWData &drw_data = *DST.vmempool;
-  if (drw_data.subdiv_module == nullptr) {
-    drw_data.subdiv_module = MEM_new<SubdivModule>("SubdivModule");
-  }
 
 #undef TIME_SUBDIV
 
@@ -1963,9 +1984,9 @@ void DRW_create_subdivision(Object &ob,
                                             do_uvedit,
                                             do_cage,
                                             ts,
-                                            use_hide,
-                                            drw_data.subdiv_module->evaluator_cache))
+                                            use_hide))
   {
+    /* Did not run*/
     return;
   }
 
@@ -1975,21 +1996,6 @@ void DRW_create_subdivision(Object &ob,
   fprintf(stderr, "Maximum FPS: %f\n", 1.0 / (end_time - begin_time));
 #endif
 }
-
-void DRW_subdiv_module_free(SubdivModule *subdiv_module)
-{
-  MEM_delete(subdiv_module);
-}
-
-/**
- * The `bke::subdiv::Subdiv` data is being owned the modifier.
- * Since the modifier can be freed from any thread (e.g. from depsgraph multithreaded update)
- * which may not have a valid GPUContext active, we move the data to discard to this free list
- * until a code-path with a active GPUContext is hit.
- * This is kind of garbage collection.
- */
-static LinkNode *gpu_subdiv_free_queue = nullptr;
-static ThreadMutex gpu_subdiv_queue_mutex = BLI_MUTEX_INITIALIZER;
 
 void DRW_subdiv_cache_free(bke::subdiv::Subdiv *subdiv)
 {
@@ -2009,6 +2015,13 @@ void DRW_cache_free_old_subdiv()
   while (gpu_subdiv_free_queue != nullptr) {
     bke::subdiv::Subdiv *subdiv = static_cast<bke::subdiv::Subdiv *>(
         BLI_linklist_pop(&gpu_subdiv_free_queue));
+
+    {
+      std::scoped_lock lock(g_subdiv_eval_mutex);
+      if (subdiv->evaluator != nullptr) {
+        g_subdiv_evaluator_users--;
+      }
+    }
 #ifdef WITH_OPENSUBDIV
     /* Set the type to CPU so that we do actually free the cache. */
     subdiv->evaluator->type = OPENSUBDIV_EVALUATOR_CPU;
@@ -2017,6 +2030,15 @@ void DRW_cache_free_old_subdiv()
   }
 
   BLI_mutex_unlock(&gpu_subdiv_queue_mutex);
+
+  {
+    std::scoped_lock lock(g_subdiv_eval_mutex);
+    /* Free evaluator cache if there is no more reference to it.. */
+    if (g_subdiv_evaluator_users == 0) {
+      openSubdiv_deleteEvaluatorCache(g_subdiv_evaluator_cache);
+      g_subdiv_evaluator_cache = nullptr;
+    }
+  }
 }
 
 }  // namespace blender::draw

source/blender/draw/intern/draw_common_c.hh
@@ -29,7 +29,6 @@ namespace blender::draw {
 class Manager;
 struct CurvesModule;
 struct PointCloudModule;
-struct SubdivModule;
 struct VolumeModule;
 }  // namespace blender::draw
 
@@ -71,10 +70,6 @@ void DRW_pointcloud_module_free(draw::PointCloudModule *module);
 /* If drw_data is nullptr, DST global is accessed to get it. */
 void DRW_volume_init(DRWData *drw_data = nullptr);
 void DRW_volume_module_free(draw::VolumeModule *module);
-
-/* draw_cache_impl_subdivision.cc */
-
-void DRW_subdiv_module_free(draw::SubdivModule *module);
 
 }  // namespace blender::draw
 

source/blender/draw/intern/draw_manager_c.cc
@@ -355,7 +355,6 @@ void DRW_viewport_data_free(DRWData *drw_data)
   DRW_volume_module_free(drw_data->volume_module);
   DRW_pointcloud_module_free(drw_data->pointcloud_module);
   DRW_curves_module_free(drw_data->curves_module);
-  DRW_subdiv_module_free(drw_data->subdiv_module);
   delete drw_data->default_view;
   MEM_freeN(drw_data);
 }

source/blender/draw/intern/draw_manager_c.hh
@@ -32,7 +32,6 @@ struct Object;
 struct Mesh;
 namespace blender::draw {
 struct CurvesModule;
-struct SubdivModule;
 struct VolumeModule;
 struct PointCloudModule;
 struct DRW_Attributes;
@@ -73,7 +72,6 @@ struct DRWData {
   DRWViewData *view_data[2];
   /** Module storage. */
   blender::draw::CurvesModule *curves_module;
-  blender::draw::SubdivModule *subdiv_module;
   blender::draw::VolumeModule *volume_module;
   blender::draw::PointCloudModule *pointcloud_module;
   /** Default view that feeds every engine. */