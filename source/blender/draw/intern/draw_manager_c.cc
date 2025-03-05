blender/blender

commit b4b1d010fb2216a5a7492a4c0745a23ca1f65b97

Fix: DRW: Asan crash because of use after free
Caused by 2038ba699dfda20419841bd71ace36d8aa7a4383

source/blender/draw/intern/draw_manager_c.cc
@@ -623,20 +623,25 @@ void DupliCacheManager::extract_all()
     return;
   }
 
+  /* Note these can referenced by the temporary object pointer `Object *ob` and needs to have at
+   * least the same lifetime. */
+  blender::bke::ObjectRuntime tmp_runtime;
+  Object tmp_object;
+  tmp_object.runtime = &tmp_runtime;
+
   using Iter = blender::Set<DupliKey>::Iterator;
   Iter begin = dupli_set_->begin();
   Iter end = dupli_set_->end();
   for (Iter iter = begin; iter != end; ++iter) {
     const DupliKey &key = *iter;
     Object *ob = iter->ob;
-    Object tmp_object;
 
     if (key.ob_data != ob->data) {
-      blender::bke::ObjectRuntime runtime = *ob->runtime;
+      /* Copy both object data and runtime. */
+      tmp_runtime = *ob->runtime;
       tmp_object = blender::dna::shallow_copy(*ob);
       /* Geometry instances shouldn't be rendered with edit mode overlays. */
       tmp_object.mode = OB_MODE_OBJECT;
-      tmp_object.runtime = &runtime;
       /* Do not modify the original bound-box. */
       BKE_object_replace_data_on_shallow_copy(&tmp_object, key.ob_data);
 

Bforartists/Bforartists

commit 2594c47e9a647c1a7b450fb93bc6cdeac905d505

Fix: DRW: Fix the fix
The `tmp_object.runtime` is getting overwritten by the shallow
copy.
Caused by b4b1d010fb2216a5a7492a4c0745a23ca1f65b97

source/blender/draw/intern/draw_manager_c.cc
@@ -627,7 +627,6 @@ void DupliCacheManager::extract_all()
    * least the same lifetime. */
   blender::bke::ObjectRuntime tmp_runtime;
   Object tmp_object;
-  tmp_object.runtime = &tmp_runtime;
 
   using Iter = blender::Set<DupliKey>::Iterator;
   Iter begin = dupli_set_->begin();
@@ -640,6 +639,7 @@ void DupliCacheManager::extract_all()
       /* Copy both object data and runtime. */
       tmp_runtime = *ob->runtime;
       tmp_object = blender::dna::shallow_copy(*ob);
+      tmp_object.runtime = &tmp_runtime;
       /* Geometry instances shouldn't be rendered with edit mode overlays. */
       tmp_object.mode = OB_MODE_OBJECT;
       /* Do not modify the original bound-box. */