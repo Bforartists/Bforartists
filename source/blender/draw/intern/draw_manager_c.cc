blender/blender

commit 2038ba699dfda20419841bd71ace36d8aa7a4383

Refactor: DRW: Make Dupli batch extraction use C++ types
And add documentation about why do we need this.

Removes the potential engine data and only keep a `Set`
of instanciated object.

source/blender/draw/intern/draw_manager_c.cc
@@ -541,92 +541,109 @@ void DRW_viewport_request_redraw()
 /** \name Duplis
  * \{ */
 
-static uint dupli_key_hash(const void *key)
-{
-  const DupliKey *dupli_key = (const DupliKey *)key;
-  return BLI_ghashutil_ptrhash(dupli_key->ob) ^ BLI_ghashutil_ptrhash(dupli_key->ob_data);
-}
+/* The Dupli systems generate a lot of transient objects that share the batch caches.
+ * So we ensure to only clear and generate the cache once per source instance type using this
+ * set. */
+/* TODO(fclem): This should be reconsidered as this has some uneeded overhead and complexity.
+ * Maybe it isn't needed at all. */
+struct DupliCacheManager {
+ private:
+  /* Key identifying a single instance source. */
+  struct DupliKey {
+    Object *ob = nullptr;
+    ID *ob_data = nullptr;
 
-static bool dupli_key_cmp(const void *key1, const void *key2)
-{
-  const DupliKey *dupli_key1 = (const DupliKey *)key1;
-  const DupliKey *dupli_key2 = (const DupliKey *)key2;
-  return dupli_key1->ob != dupli_key2->ob || dupli_key1->ob_data != dupli_key2->ob_data;
-}
+    bool operator==(const DupliObject *ob_dupli)
+    {
+      return this->ob == ob_dupli->ob && this->ob_data == ob_dupli->ob_data;
+    }
 
-static void drw_duplidata_load(Object *ob)
+    uint64_t hash() const
+    {
+      return blender::get_default_hash(this->ob, this->ob_data);
+    }
+
+    friend bool operator==(const DupliKey &a, const DupliKey &b)
+    {
+      return a.ob == b.ob && a.ob_data == b.ob_data;
+    }
+  };
+
+  /* Last key used. Allows to avoid the overhead of polling the `dupli_set` for each instance.
+   * This helps when a Dupli system generates a lot of similar geometry consecutively. */
+  DupliKey last_key_ = {};
+
+  /* Set containing all visited Dupli source object. */
+  blender::Set<DupliKey> *dupli_set_ = nullptr;
+
+ public:
+  void try_add(blender::draw::ObjectRef &ob_ref);
+  void extract_all();
+};
+
+void DupliCacheManager::try_add(blender::draw::ObjectRef &ob_ref)
 {
-  DupliObject *dupli = drw_get().dupli_source;
-  if (dupli == nullptr) {
+  if (ob_ref.is_dupli() == false) {
     return;
   }
-
-  if (drw_get().dupli_origin != dupli->ob || (drw_get().dupli_origin_data != dupli->ob_data)) {
-    drw_get().dupli_origin = dupli->ob;
-    drw_get().dupli_origin_data = dupli->ob_data;
-  }
-  else {
+  if (last_key_ == ob_ref.dupli_object) {
     /* Same data as previous iter. No need to poll ghash for this. */
     return;
   }
 
-  if (drw_get().dupli_ghash == nullptr) {
-    drw_get().dupli_ghash = BLI_ghash_new(dupli_key_hash, dupli_key_cmp, __func__);
+  last_key_.ob = ob_ref.dupli_object->ob;
+  last_key_.ob_data = ob_ref.dupli_object->ob_data;
+
+  if (dupli_set_ == nullptr) {
+    dupli_set_ = MEM_new<blender::Set<DupliKey>>("DupliCacheManager::dupli_set_");
   }
 
-  DupliKey *key = static_cast<DupliKey *>(MEM_callocN(sizeof(DupliKey), __func__));
-  key->ob = dupli->ob;
-  key->ob_data = dupli->ob_data;
-
-  void **value;
-  if (!BLI_ghash_ensure_p(drw_get().dupli_ghash, key, &value)) {
-    *value = MEM_callocN(sizeof(void *) * g_registered_engines.len, __func__);
-
+  if (dupli_set_->add(last_key_)) {
+    /* Key is newly added. It is the first time we sync this object. */
     /* TODO: Meh a bit out of place but this is nice as it is
      * only done once per instance type. */
-    drw_batch_cache_validate(ob);
+    /* Note that this can happen for geometry data whose type is different from the original
+     * object (e.g. Text evaluated as Mesh, Geometry node instance etc...).
+     * In this case, key.ob is not going to have the same data type as ob_ref.object nor the same
+     * data at all. */
+    drw_batch_cache_validate(ob_ref.object);
   }
-  else {
-    MEM_freeN(key);
-  }
-  drw_get().dupli_datas = *(void ***)value;
 }
 
-static void duplidata_value_free(void *val)
+void DupliCacheManager::extract_all()
 {
-  void **dupli_datas = static_cast<void **>(val);
-  for (int i = 0; i < g_registered_engines.len; i++) {
-    MEM_SAFE_FREE(dupli_datas[i]);
-  }
-  MEM_freeN(val);
-}
+  /* Reset for next iter. */
+  last_key_ = {};
 
-static void duplidata_key_free(void *key)
-{
-  DupliKey *dupli_key = (DupliKey *)key;
-  if (dupli_key->ob_data == dupli_key->ob->data) {
-    drw_batch_cache_generate_requested(dupli_key->ob);
+  if (dupli_set_ == nullptr) {
+    return;
   }
-  else {
-    /* Geometry instances shouldn't be rendered with edit mode overlays. */
-    Object temp_object = blender::dna::shallow_copy(*dupli_key->ob);
-    temp_object.mode = OB_MODE_OBJECT;
-    blender::bke::ObjectRuntime runtime = *dupli_key->ob->runtime;
-    temp_object.runtime = &runtime;
 
-    /* Do not modify the original bound-box. */
-    BKE_object_replace_data_on_shallow_copy(&temp_object, dupli_key->ob_data);
-    drw_batch_cache_generate_requested(&temp_object);
-  }
-  MEM_freeN(key);
-}
+  using Iter = blender::Set<DupliKey>::Iterator;
+  Iter begin = dupli_set_->begin();
+  Iter end = dupli_set_->end();
+  for (Iter iter = begin; iter != end; ++iter) {
+    const DupliKey &key = *iter;
+    Object *ob = iter->ob;
+    Object tmp_object;
 
-static void drw_duplidata_free()
-{
-  if (drw_get().dupli_ghash != nullptr) {
-    BLI_ghash_free(drw_get().dupli_ghash, duplidata_key_free, duplidata_value_free);
-    drw_get().dupli_ghash = nullptr;
+    if (key.ob_data != ob->data) {
+      blender::bke::ObjectRuntime runtime = *ob->runtime;
+      tmp_object = blender::dna::shallow_copy(*ob);
+      /* Geometry instances shouldn't be rendered with edit mode overlays. */
+      tmp_object.mode = OB_MODE_OBJECT;
+      tmp_object.runtime = &runtime;
+      /* Do not modify the original bound-box. */
+      BKE_object_replace_data_on_shallow_copy(&tmp_object, key.ob_data);
+
+      ob = &tmp_object;
+    }
+
+    drw_batch_cache_generate_requested(ob);
   }
+
+  /* TODO(fclem): Could eventually keep the set allocated. */
+  MEM_SAFE_DELETE(dupli_set_);
 }
 
 /** \} */
@@ -1549,11 +1566,10 @@ void DRW_draw_render_loop_ex(Depsgraph *depsgraph,
   {
     drw_engines_cache_init();
     drw_engines_world_update(scene);
+    DupliCacheManager dupli_handler;
 
     /* Only iterate over objects for internal engines or when overlays are enabled */
     if (do_populate_loop) {
-      drw_get().dupli_origin = nullptr;
-      drw_get().dupli_origin_data = nullptr;
       DEGObjectIterSettings deg_iter_settings = {nullptr};
       deg_iter_settings.depsgraph = depsgraph;
       deg_iter_settings.flags = DEG_OBJECT_ITER_FOR_RENDER_ENGINE_FLAGS;
@@ -1567,18 +1583,16 @@ void DRW_draw_render_loop_ex(Depsgraph *depsgraph,
         if (!BKE_object_is_visible_in_viewport(v3d, ob)) {
           continue;
         }
-        drw_get().dupli_parent = data_.dupli_parent;
-        drw_get().dupli_source = data_.dupli_object_current;
-        drw_duplidata_load(ob);
         blender::draw::ObjectRef ob_ref(data_, ob);
+        dupli_handler.try_add(ob_ref);
         drw_engines_cache_populate(ob_ref);
       }
       DEG_OBJECT_ITER_END;
     }
 
-    drw_duplidata_free();
     drw_engines_cache_finish();
 
+    dupli_handler.extract_all();
     drw_task_graph_deinit();
   }
 
@@ -1903,22 +1917,22 @@ void DRW_render_object_iter(void *vedata,
   DRW_volume_init(drw_get().data);
   DRW_smoke_init(drw_get().data);
 
+  DupliCacheManager dupli_handler;
+
   drw_task_graph_init();
   const int object_type_exclude_viewport = draw_ctx->v3d ?
                                                draw_ctx->v3d->object_type_exclude_viewport :
                                                0;
-  drw_get().dupli_origin = nullptr;
-  drw_get().dupli_origin_data = nullptr;
   DEGObjectIterSettings deg_iter_settings = {nullptr};
   deg_iter_settings.depsgraph = depsgraph;
   deg_iter_settings.flags = DEG_OBJECT_ITER_FOR_RENDER_ENGINE_FLAGS;
   DEG_OBJECT_ITER_BEGIN (&deg_iter_settings, ob) {
     if ((object_type_exclude_viewport & (1 << ob->type)) == 0) {
+      blender::draw::ObjectRef ob_ref(data_, ob);
       drw_get().dupli_parent = data_.dupli_parent;
       drw_get().dupli_source = data_.dupli_object_current;
-      drw_duplidata_load(ob);
+      dupli_handler.try_add(ob_ref);
 
-      blender::draw::ObjectRef ob_ref(data_, ob);
       if (ob_ref.is_dupli() == false) {
         drw_batch_cache_validate(ob);
       }
@@ -1930,7 +1944,7 @@ void DRW_render_object_iter(void *vedata,
   }
   DEG_OBJECT_ITER_END;
 
-  drw_duplidata_free();
+  dupli_handler.extract_all();
   drw_task_graph_deinit();
 }
 
@@ -2297,6 +2311,7 @@ void DRW_draw_select_loop(Depsgraph *depsgraph,
   {
     drw_engines_cache_init();
     drw_engines_world_update(scene);
+    DupliCacheManager dupli_handler;
 
     if (use_obedit) {
       FOREACH_OBJECT_IN_MODE_BEGIN (scene, view_layer, v3d, object_type, object_mode, ob_iter) {
@@ -2313,8 +2328,6 @@ void DRW_draw_select_loop(Depsgraph *depsgraph,
       const int object_type_exclude_select = (v3d->object_type_exclude_viewport |
                                               v3d->object_type_exclude_select);
       bool filter_exclude = false;
-      drw_get().dupli_origin = nullptr;
-      drw_get().dupli_origin_data = nullptr;
       DEGObjectIterSettings deg_iter_settings = {nullptr};
       deg_iter_settings.depsgraph = depsgraph;
       deg_iter_settings.flags = DEG_OBJECT_ITER_FOR_RENDER_ENGINE_FLAGS;
@@ -2353,13 +2366,14 @@ void DRW_draw_select_loop(Depsgraph *depsgraph,
           drw_get().dupli_parent = data_.dupli_parent;
           drw_get().dupli_source = data_.dupli_object_current;
           blender::draw::ObjectRef ob_ref(data_, ob);
+          dupli_handler.try_add(ob_ref);
           drw_engines_cache_populate(ob_ref);
         }
       }
       DEG_OBJECT_ITER_END;
     }
 
-    drw_duplidata_free();
+    dupli_handler.extract_all();
     drw_task_graph_deinit();
     drw_engines_cache_finish();
   }
@@ -2467,12 +2481,11 @@ void DRW_draw_depth_loop(Depsgraph *depsgraph,
   DRW_smoke_init(drw_get().data);
 
   {
+    DupliCacheManager dupli_handler;
     drw_engines_cache_init();
     drw_engines_world_update(drw_get().draw_ctx.scene);
 
     const int object_type_exclude_viewport = v3d->object_type_exclude_viewport;
-    drw_get().dupli_origin = nullptr;
-    drw_get().dupli_origin_data = nullptr;
     DEGObjectIterSettings deg_iter_settings = {nullptr};
     deg_iter_settings.depsgraph = drw_get().draw_ctx.depsgraph;
     deg_iter_settings.flags = DEG_OBJECT_ITER_FOR_RENDER_ENGINE_FLAGS;
@@ -2492,11 +2505,12 @@ void DRW_draw_depth_loop(Depsgraph *depsgraph,
       drw_get().dupli_parent = data_.dupli_parent;
       drw_get().dupli_source = data_.dupli_object_current;
       blender::draw::ObjectRef ob_ref(data_, ob);
+      dupli_handler.try_add(ob_ref);
       drw_engines_cache_populate(ob_ref);
     }
     DEG_OBJECT_ITER_END;
 
-    drw_duplidata_free();
+    dupli_handler.extract_all();
     drw_engines_cache_finish();
 
     drw_task_graph_deinit();

source/blender/draw/intern/draw_manager_c.hh
@@ -98,14 +98,6 @@ struct DRWContext {
   DupliObject *dupli_source = nullptr;
   /** Object that created the dupli-list the current object is part of. */
   Object *dupli_parent = nullptr;
-  /** Object referenced by the current dupli object. */
-  Object *dupli_origin = nullptr;
-  /** Object-data referenced by the current dupli object. */
-  ID *dupli_origin_data = nullptr;
-  /** Hash-map: #DupliKey -> void pointer for each enabled engine. */
-  GHash *dupli_ghash = nullptr;
-  /* Dupli data for the current dupli for each enabled engine. */
-  void **dupli_datas = nullptr;
 
   /* Optional associated viewport. Can be nullptr. */
   GPUViewport *viewport = nullptr;
@@ -139,6 +131,7 @@ struct DRWContext {
   /* Contains list of objects that needs to be extracted from other objects. */
   GSet *delayed_extraction = nullptr;
 
+  /* Contains debug drawcall infos. Persistent across usage. */
   DRWDebugModule *debug = nullptr;
 
   /* Reset all members before drawing in order to avoid undefined state. */

Bforartists/Bforartists

commit 04ab3c50cead9c851628f4a68e9d80d28a2a66e2

Fix: DRW: Asan crash because of use after free
Caused by 2038ba699dfda20419841bd71ace36d8aa7a4383

source/blender/draw/intern/draw_manager_c.cc
@@ -623,20 +623,25 @@ void DupliCacheManager::extract_all()
     return;
   }
 
+  /* Note these can referenced by the temporary object pointer `Object *ob` and needs to have at
+   * least the same lifetime. */
+  blender::bke::ObjectRuntime tmp_runtime;
+  Object tmp_object;
+  tmp_object.runtime = &tmp_runtime;
+
   using Iter = blender::Set<DupliKey>::Iterator;
   Iter begin = dupli_set_->begin();
   Iter end = dupli_set_->end();
   for (Iter iter = begin; iter != end; ++iter) {
     const DupliKey &key = *iter;
     Object *ob = iter->ob;
-    Object tmp_object;
 
     if (key.ob_data != ob->data) {
-      blender::bke::ObjectRuntime runtime = *ob->runtime;
+      /* Copy both object data and runtime. */
+      tmp_runtime = *ob->runtime;
       tmp_object = blender::dna::shallow_copy(*ob);
       /* Geometry instances shouldn't be rendered with edit mode overlays. */
       tmp_object.mode = OB_MODE_OBJECT;
-      tmp_object.runtime = &runtime;
       /* Do not modify the original bound-box. */
       BKE_object_replace_data_on_shallow_copy(&tmp_object, key.ob_data);
 