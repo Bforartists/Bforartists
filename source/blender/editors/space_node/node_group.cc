Bforartists/Bforartists

commit c217af698161a940c16980411c3adedfd0d2df3a

Nodes: Simplify location storage, remove hidden offfsets
Currently each node's position is stored in the coordinate space of
its parent. To find the location of a node on the canvas, we have to
apply the translation of each of its parents. Also, nodes have hidden
"offset" values used while transforming frame nodes. Together,
those made the system much more complicated than necessary,
and they made the Python API ineffective.

This commit removes usage of the offset values and moves nodes
to be stored in the "global" space of the node canvas. It also resolves
some weird behavior when resizing frame nodes, and fixes a few bugs.

The change is forward compatible, so we still write files with nodes in
the old parent-space format. In 5.0 the conversion when writing can be
removed. The existing Python API also stays the same. A new
"location_absolute" property gives node locations in global space,
and changing the old property also moves the child nodes of frames.

Resolves #92458, #72904.

Pull Request: https://projects.blender.org/blender/blender/pulls/131335

source/blender/editors/space_node/node_group.cc
@@ -321,8 +321,8 @@ static void node_group_ungroup(Main *bmain, bNodeTree *ntree, bNode *gnode)
     }
 
     if (!node->parent) {
-      node->locx += gnode->locx;
-      node->locy += gnode->locy;
+      node->location[0] += gnode->location[0];
+      node->location[1] += gnode->location[1];
     }
 
     node->flag |= NODE_SELECT;
@@ -557,8 +557,8 @@ static bool node_group_separate_selected(
     }
 
     if (!newnode->parent) {
-      newnode->locx += offset.x;
-      newnode->locy += offset.y;
+      newnode->location[0] += offset.x;
+      newnode->location[1] += offset.y;
     }
   }
   if (!make_copy) {
@@ -820,8 +820,7 @@ static void get_min_max_of_nodes(const Span<bNode *> nodes,
 
   INIT_MINMAX2(min, max);
   for (const bNode *node : nodes) {
-    const float2 node_offset = {node->offsetx, node->offsety};
-    float2 loc = bke::node_to_view(node, node_offset);
+    float2 loc(node->location);
     math::min_max(loc, min, max);
     if (use_size) {
       loc.x += node->width;
@@ -955,13 +954,13 @@ static void node_group_make_insert_selected(const bContext &C,
       return node;
     }
     bNode *output_node = bke::node_add_static_node(&C, &group, NODE_GROUP_OUTPUT);
-    output_node->locx = real_max[0] - center[0] + 50.0f;
+    output_node->location[0] = real_max[0] - center[0] + 50.0f;
     return output_node;
   }();
 
   /* Create new group input node for easier organization of the new nodes inside the group. */
   bNode *input_node = bke::node_add_static_node(&C, &group, NODE_GROUP_INPUT);
-  input_node->locx = real_min[0] - center[0] - 200.0f;
+  input_node->location[0] = real_min[0] - center[0] - 200.0f;
 
   struct InputSocketInfo {
     /* The unselected node the original link came from. */
@@ -1124,8 +1123,8 @@ static void node_group_make_insert_selected(const bContext &C,
   /* move nodes in the group to the center */
   for (bNode *node : nodes_to_move) {
     if (!node->parent) {
-      node->locx -= center[0];
-      node->locy -= center[1];
+      node->location[0] -= center[0];
+      node->location[1] -= center[1];
     }
   }
 
@@ -1226,8 +1225,8 @@ static bNode *node_group_make_from_nodes(const bContext &C,
   bNode *gnode = bke::node_add_node(&C, &ntree, ntype);
   gnode->id = (ID *)ngroup;
 
-  gnode->locx = 0.5f * (min[0] + max[0]);
-  gnode->locy = 0.5f * (min[1] + max[1]);
+  gnode->location[0] = 0.5f * (min[0] + max[0]);
+  gnode->location[1] = 0.5f * (min[1] + max[1]);
 
   node_group_make_insert_selected(C, ntree, gnode, nodes_to_group);
 

source/blender/blenkernel/BKE_blender_version.h
@@ -31,7 +31,7 @@ extern "C" {
 
 /* Blender file format version. */
 #define BLENDER_FILE_VERSION BLENDER_VERSION
-#define BLENDER_FILE_SUBVERSION 11
+#define BLENDER_FILE_SUBVERSION 12
 
 /* Minimum Blender version that supports reading file written with the current
  * version. Older Blender versions will test this and cancel loading the file, showing a warning to

source/blender/blenkernel/BKE_node.hh
@@ -1557,10 +1557,6 @@ bool node_link_is_selected(const bNodeLink *link);
 
 void node_internal_relink(bNodeTree *ntree, bNode *node);
 
-float2 node_to_view(const bNode *node, float2 loc);
-
-float2 node_from_view(const bNode *node, float2 view_loc);
-
 void node_position_relative(bNode *from_node,
                             const bNode *to_node,
                             const bNodeSocket *from_sock,

source/blender/blenkernel/BKE_node_runtime.hh
@@ -334,7 +334,7 @@ class bNodeRuntime : NonCopyable, NonMovable {
    */
   /** Reserved size of the preview rect. */
   short preview_xsize, preview_ysize = 0;
-  /** Entire bound-box (world-space). */
+  /** Calculated bounding box of node in the view space of the node editor (including UI scale). */
   rctf totr{};
 
   /** Used at runtime when going through the tree. Initialize before use. */

source/blender/blenkernel/intern/grease_pencil_convert_legacy.cc
@@ -1208,31 +1208,31 @@ static bNodeTree *offset_radius_node_tree_add(ConversionData &conversion_data, L
       DATA_("Layer"), "", "NodeSocketString", NODE_INTERFACE_SOCKET_INPUT, nullptr);
 
   bNode *group_output = bke::node_add_node(nullptr, group, "NodeGroupOutput");
-  group_output->locx = 800;
-  group_output->locy = 160;
+  group_output->location[0] = 800;
+  group_output->location[1] = 160;
   bNode *group_input = bke::node_add_node(nullptr, group, "NodeGroupInput");
-  group_input->locx = 0;
-  group_input->locy = 160;
+  group_input->location[0] = 0;
+  group_input->location[1] = 160;
 
   bNode *set_curve_radius = bke::node_add_node(nullptr, group, "GeometryNodeSetCurveRadius");
-  set_curve_radius->locx = 600;
-  set_curve_radius->locy = 160;
+  set_curve_radius->location[0] = 600;
+  set_curve_radius->location[1] = 160;
   bNode *named_layer_selection = bke::node_add_node(
       nullptr, group, "GeometryNodeInputNamedLayerSelection");
-  named_layer_selection->locx = 200;
-  named_layer_selection->locy = 100;
+  named_layer_selection->location[0] = 200;
+  named_layer_selection->location[1] = 100;
   bNode *input_radius = bke::node_add_node(nullptr, group, "GeometryNodeInputRadius");
-  input_radius->locx = 0;
-  input_radius->locy = 0;
+  input_radius->location[0] = 0;
+  input_radius->location[1] = 0;
 
   bNode *add = bke::node_add_node(nullptr, group, "ShaderNodeMath");
   add->custom1 = NODE_MATH_ADD;
-  add->locx = 200;
-  add->locy = 0;
+  add->location[0] = 200;
+  add->location[1] = 0;
 
   bNode *clamp_radius = bke::node_add_node(nullptr, group, "ShaderNodeClamp");
-  clamp_radius->locx = 400;
-  clamp_radius->locy = 0;
+  clamp_radius->location[0] = 400;
+  clamp_radius->location[1] = 0;
   bNodeSocket *sock_max = bke::node_find_socket(clamp_radius, SOCK_IN, "Max");
   static_cast<bNodeSocketValueFloat *>(sock_max->default_value)->value = FLT_MAX;
 

source/blender/blenkernel/intern/linestyle.cc
@@ -1935,17 +1935,17 @@ void BKE_linestyle_default_shader(const bContext *C, FreestyleLineStyle *linesty
       nullptr, &linestyle->id, "stroke_shader", "ShaderNodeTree");
 
   uv_along_stroke = blender::bke::node_add_static_node(C, ntree, SH_NODE_UVALONGSTROKE);
-  uv_along_stroke->locx = 0.0f;
-  uv_along_stroke->locy = 300.0f;
+  uv_along_stroke->location[0] = 0.0f;
+  uv_along_stroke->location[1] = 300.0f;
   uv_along_stroke->custom1 = 0; /* use_tips */
 
   input_texture = blender::bke::node_add_static_node(C, ntree, SH_NODE_TEX_IMAGE);
-  input_texture->locx = 200.0f;
-  input_texture->locy = 300.0f;
+  input_texture->location[0] = 200.0f;
+  input_texture->location[1] = 300.0f;
 
   output_linestyle = blender::bke::node_add_static_node(C, ntree, SH_NODE_OUTPUT_LINESTYLE);
-  output_linestyle->locx = 400.0f;
-  output_linestyle->locy = 300.0f;
+  output_linestyle->location[0] = 400.0f;
+  output_linestyle->location[1] = 300.0f;
   output_linestyle->custom1 = MA_RAMP_BLEND;
   output_linestyle->custom2 = 0; /* use_clamp */
 

source/blender/blenkernel/intern/material.cc
@@ -1999,10 +1999,10 @@ static void material_default_surface_init(Material *ma)
                               output,
                               blender::bke::node_find_socket(output, SOCK_IN, "Surface"));
 
-  principled->locx = 10.0f;
-  principled->locy = 300.0f;
-  output->locx = 300.0f;
-  output->locy = 300.0f;
+  principled->location[0] = 10.0f;
+  principled->location[1] = 300.0f;
+  output->location[0] = 300.0f;
+  output->location[1] = 300.0f;
 
   blender::bke::node_set_active(ntree, output);
 }
@@ -2025,10 +2025,10 @@ static void material_default_volume_init(Material *ma)
                               output,
                               blender::bke::node_find_socket(output, SOCK_IN, "Volume"));
 
-  principled->locx = 10.0f;
-  principled->locy = 300.0f;
-  output->locx = 300.0f;
-  output->locy = 300.0f;
+  principled->location[0] = 10.0f;
+  principled->location[1] = 300.0f;
+  output->location[0] = 300.0f;
+  output->location[1] = 300.0f;
 
   blender::bke::node_set_active(ntree, output);
 }
@@ -2050,10 +2050,10 @@ static void material_default_holdout_init(Material *ma)
                               output,
                               blender::bke::node_find_socket(output, SOCK_IN, "Surface"));
 
-  holdout->locx = 10.0f;
-  holdout->locy = 300.0f;
-  output->locx = 300.0f;
-  output->locy = 300.0f;
+  holdout->location[0] = 10.0f;
+  holdout->location[1] = 300.0f;
+  output->location[0] = 300.0f;
+  output->location[1] = 300.0f;
 
   blender::bke::node_set_active(ntree, output);
 }

source/blender/blenkernel/intern/mesh_legacy_convert.cc
@@ -2142,19 +2142,19 @@ static bNodeTree *add_auto_smooth_node_tree(Main &bmain, Library *owner_library)
   angle_data.subtype = PROP_ANGLE;
 
   bNode *group_output = node_add_node(nullptr, group, "NodeGroupOutput");
-  group_output->locx = 480.0f;
-  group_output->locy = -100.0f;
+  group_output->location[0] = 480.0f;
+  group_output->location[1] = -100.0f;
   bNode *group_input_angle = node_add_node(nullptr, group, "NodeGroupInput");
-  group_input_angle->locx = -420.0f;
-  group_input_angle->locy = -300.0f;
+  group_input_angle->location[0] = -420.0f;
+  group_input_angle->location[1] = -300.0f;
   LISTBASE_FOREACH (bNodeSocket *, socket, &group_input_angle->outputs) {
     if (!STREQ(socket->identifier, "Socket_2")) {
       socket->flag |= SOCK_HIDDEN;
     }
   }
   bNode *group_input_mesh = node_add_node(nullptr, group, "NodeGroupInput");
-  group_input_mesh->locx = -60.0f;
-  group_input_mesh->locy = -100.0f;
+  group_input_mesh->location[0] = -60.0f;
+  group_input_mesh->location[1] = -100.0f;
   LISTBASE_FOREACH (bNodeSocket *, socket, &group_input_mesh->outputs) {
     if (!STREQ(socket->identifier, "Socket_1")) {
       socket->flag |= SOCK_HIDDEN;
@@ -2162,30 +2162,30 @@ static bNodeTree *add_auto_smooth_node_tree(Main &bmain, Library *owner_library)
   }
   bNode *shade_smooth_edge = node_add_node(nullptr, group, "GeometryNodeSetShadeSmooth");
   shade_smooth_edge->custom1 = int16_t(bke::AttrDomain::Edge);
-  shade_smooth_edge->locx = 120.0f;
-  shade_smooth_edge->locy = -100.0f;
+  shade_smooth_edge->location[0] = 120.0f;
+  shade_smooth_edge->location[1] = -100.0f;
   bNode *shade_smooth_face = node_add_node(nullptr, group, "GeometryNodeSetShadeSmooth");
   shade_smooth_face->custom1 = int16_t(bke::AttrDomain::Face);
-  shade_smooth_face->locx = 300.0f;
-  shade_smooth_face->locy = -100.0f;
+  shade_smooth_face->location[0] = 300.0f;
+  shade_smooth_face->location[1] = -100.0f;
   bNode *edge_angle = node_add_node(nullptr, group, "GeometryNodeInputMeshEdgeAngle");
-  edge_angle->locx = -420.0f;
-  edge_angle->locy = -220.0f;
+  edge_angle->location[0] = -420.0f;
+  edge_angle->location[1] = -220.0f;
   bNode *edge_smooth = node_add_node(nullptr, group, "GeometryNodeInputEdgeSmooth");
-  edge_smooth->locx = -60.0f;
-  edge_smooth->locy = -160.0f;
+  edge_smooth->location[0] = -60.0f;
+  edge_smooth->location[1] = -160.0f;
   bNode *face_smooth = node_add_node(nullptr, group, "GeometryNodeInputShadeSmooth");
-  face_smooth->locx = -240.0f;
-  face_smooth->locy = -340.0f;
+  face_smooth->location[0] = -240.0f;
+  face_smooth->location[1] = -340.0f;
   bNode *boolean_and = node_add_node(nullptr, group, "FunctionNodeBooleanMath");
   boolean_and->custom1 = NODE_BOOLEAN_MATH_AND;
-  boolean_and->locx = -60.0f;
-  boolean_and->locy = -220.0f;
+  boolean_and->location[0] = -60.0f;
+  boolean_and->location[1] = -220.0f;
   bNode *less_than_or_equal = node_add_node(nullptr, group, "FunctionNodeCompare");
   static_cast<NodeFunctionCompare *>(less_than_or_equal->storage)->operation =
       NODE_COMPARE_LESS_EQUAL;
-  less_than_or_equal->locx = -240.0f;
-  less_than_or_equal->locy = -180.0f;
+  less_than_or_equal->location[0] = -240.0f;
+  less_than_or_equal->location[1] = -180.0f;
 
   node_add_link(group,
                 edge_angle,

commit bdf785a729ed445d5879b62ddc3b552bced0f986

Fix #135643: Node ungroup operator doesn't move frame nodes
Caused by c217af698161a940c16980411c3adedfd0d2df3a.

Nodes within frames were ignored because previously their location was
relative to the frame, so moving both the frame and the node would cause
"double" movement. Now each node location is independent.

source/blender/editors/space_node/node_group.cc
@@ -320,10 +320,8 @@ static void node_group_ungroup(Main *bmain, bNodeTree *ntree, bNode *gnode)
                   animation_basepath_change_new(*old_animation_basepath, *new_animation_basepath));
     }
 
-    if (!node->parent) {
-      node->location[0] += gnode->location[0];
-      node->location[1] += gnode->location[1];
-    }
+    node->location[0] += gnode->location[0];
+    node->location[1] += gnode->location[1];
 
     node->flag |= NODE_SELECT;
   }