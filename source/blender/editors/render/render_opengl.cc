Bforartists/Bforartists

commit 5a18eaf7ade9c7a6c34b5ced80b8afacaf9796f7

Refactor: move ffmpeg/video related code into one place
Previously, code related to reading/writing movie files via ffmpeg was
scattered around: some under blenkernel, some directly in generic
imbuf headers, some under intern/ffmpeg. Some of the files were named
with not exactly clear names. Some parts not directly related to movies
were including ffmpeg headers directly (rna_scene.cc).

What is in this PR:

Movie and ffmpeg related code is now under imbuf/movie:
- IMB_anim.hh: movie reading, proxy querying, various utility functions.
- IMB_movie_enums.hh: simple enum definitions,
- IMB_movie_write.hh: movie writing functions.
- intern: actual implementation and private headers.
    - ffmpeg_compat.h: various ffmpeg version difference handling
      utilities,
    - ffmpeg_swscale.hh/cc: scaling and format conversion utilities
      for ffmpeg libswscale,
    - ffmpeg_util.hh/cc: misc utilities related to ffmpeg,
    - movie_proxy_indexer.hh/cc: proxies and timecode indexing for movies,
    - movie_read.hh/cc: decoding of movies into images,
    - movie_write.cc: encoding of images into movies.
- tests: basic ffmpeg library unit tests that previously
  lived under intern/ffmpeg.

Interface changes (at C++ level, no Python API changes):
- Mostly just movie related functions that were BKE_ previously, are now IMB_.
- I did one large-ish change though, and that is to remove bMovieHandle
  struct that had pointers to several functions. Now that is
  IMB_movie_write_begin, IMB_movie_write_append, IMB_movie_write_end
  functions using a single opaque struct handle. As a result, usages
  of that in pipeline.cc and render_opengl.cc have changed.

Pull Request: https://projects.blender.org/blender/blender/pulls/132074

source/blender/editors/render/render_opengl.cc
@@ -19,6 +19,7 @@
 #include "BLI_task.hh"
 #include "BLI_threads.h"
 #include "BLI_utildefines.h"
+#include "BLI_vector.hh"
 
 #include "DNA_action_types.h"
 #include "DNA_anim_types.h"
@@ -40,7 +41,6 @@
 #include "BKE_main.hh"
 #include "BKE_report.hh"
 #include "BKE_scene.hh"
-#include "BKE_writemovie.hh"
 
 #include "DEG_depsgraph.hh"
 #include "DEG_depsgraph_query.hh"
@@ -57,6 +57,7 @@
 
 #include "IMB_imbuf.hh"
 #include "IMB_imbuf_types.hh"
+#include "IMB_movie_write.hh"
 
 #include "RE_pipeline.h"
 
@@ -120,7 +121,6 @@ struct OGLRender {
   GPUViewport *viewport;
 
   ReportList *reports;
-  bMovieHandle *mh;
   int cfrao, nfra;
 
   int totvideos;
@@ -138,7 +138,7 @@ struct OGLRender {
   /** Use to check if running modal or not (invoked or executed). */
   wmTimer *timer;
 
-  void **movie_ctx_arr;
+  blender::Vector<ImbMovieWriter *> movie_writers;
 
   TaskPool *task_pool;
   bool pool_ok;
@@ -847,8 +847,6 @@ static bool screen_opengl_render_init(bContext *C, wmOperator *op)
   oglrender->win = win;
 
   oglrender->totvideos = 0;
-  oglrender->mh = nullptr;
-  oglrender->movie_ctx_arr = nullptr;
 
   if (is_animation) {
     if (is_render_keyed_only) {
@@ -881,7 +879,6 @@ static bool screen_opengl_render_init(bContext *C, wmOperator *op)
 static void screen_opengl_render_end(bContext *C, OGLRender *oglrender)
 {
   Scene *scene = oglrender->scene;
-  int i;
 
   if (oglrender->is_animation) {
     /* Trickery part for movie output:
@@ -913,17 +910,13 @@ static void screen_opengl_render_end(bContext *C, OGLRender *oglrender)
 
   MEM_SAFE_FREE(oglrender->render_frames);
 
-  if (oglrender->mh) {
+  if (!oglrender->movie_writers.is_empty()) {
     if (BKE_imtype_is_movie(scene->r.im_format.imtype)) {
-      for (i = 0; i < oglrender->totvideos; i++) {
-        oglrender->mh->end_movie(oglrender->movie_ctx_arr[i]);
-        oglrender->mh->context_free(oglrender->movie_ctx_arr[i]);
+      for (ImbMovieWriter *writer : oglrender->movie_writers) {
+        IMB_movie_write_end(writer);
       }
     }
-
-    if (oglrender->movie_ctx_arr) {
-      MEM_freeN(oglrender->movie_ctx_arr);
-    }
+    oglrender->movie_writers.clear_and_shrink();
   }
 
   if (oglrender->timer) { /* exec will not have a timer */
@@ -982,34 +975,25 @@ static bool screen_opengl_render_anim_init(bContext *C, wmOperator *op)
 
     BKE_scene_multiview_videos_dimensions_get(
         &scene->r, oglrender->sizex, oglrender->sizey, &width, &height);
-    oglrender->mh = BKE_movie_handle_get(scene->r.im_format.imtype);
-
-    if (oglrender->mh == nullptr) {
-      BKE_report(oglrender->reports, RPT_ERROR, "Movie format unsupported");
-      screen_opengl_render_end(C, oglrender);
-      return false;
-    }
-
-    oglrender->movie_ctx_arr = static_cast<void **>(
-        MEM_mallocN(sizeof(void *) * oglrender->totvideos, "Movies"));
+    oglrender->movie_writers.reserve(oglrender->totvideos);
 
     for (i = 0; i < oglrender->totvideos; i++) {
       Scene *scene_eval = DEG_get_evaluated_scene(oglrender->depsgraph);
       const char *suffix = BKE_scene_multiview_view_id_suffix_get(&scene->r, i);
-
-      oglrender->movie_ctx_arr[i] = oglrender->mh->context_create();
-      if (!oglrender->mh->start_movie(oglrender->movie_ctx_arr[i],
-                                      scene_eval,
-                                      &scene->r,
-                                      oglrender->sizex,
-                                      oglrender->sizey,
-                                      oglrender->reports,
-                                      PRVRANGEON != 0,
-                                      suffix))
-      {
+      ImbMovieWriter *writer = IMB_movie_write_begin(scene->r.im_format.imtype,
+                                                     scene_eval,
+                                                     &scene->r,
+                                                     oglrender->sizex,
+                                                     oglrender->sizey,
+                                                     oglrender->reports,
+                                                     PRVRANGEON != 0,
+                                                     suffix);
+      if (writer == nullptr) {
+        BKE_report(oglrender->reports, RPT_ERROR, "Movie format unsupported");
         screen_opengl_render_end(C, oglrender);
         return false;
       }
+      oglrender->movie_writers.append(writer);
     }
   }
 
@@ -1059,8 +1043,7 @@ static void write_result(TaskPool *__restrict pool, WriteTaskData *task_data)
                                   rr,
                                   scene,
                                   &scene->r,
-                                  oglrender->mh,
-                                  oglrender->movie_ctx_arr,
+                                  oglrender->movie_writers.data(),
                                   oglrender->totvideos,
                                   PRVRANGEON != 0);
   }

source/blender/render/intern/pipeline.cc
@@ -63,7 +63,6 @@
 #include "BKE_report.hh"
 #include "BKE_scene.hh"
 #include "BKE_sound.h"
-#include "BKE_writemovie.hh"
 
 #include "NOD_composite.hh"
 
@@ -78,6 +77,7 @@
 #include "IMB_imbuf.hh"
 #include "IMB_imbuf_types.hh"
 #include "IMB_metadata.hh"
+#include "IMB_movie_write.hh"
 
 #include "RE_engine.h"
 #include "RE_pipeline.h"
@@ -167,12 +167,8 @@ static void render_callback_exec_id(Render *re, Main *bmain, ID *id, eCbEvent ev
 /** \name Allocation & Free
  * \{ */
 
-static bool do_write_image_or_movie(Render *re,
-                                    Main *bmain,
-                                    Scene *scene,
-                                    bMovieHandle *mh,
-                                    const int totvideos,
-                                    const char *filepath_override);
+static bool do_write_image_or_movie(
+    Render *re, Main *bmain, Scene *scene, const int totvideos, const char *filepath_override);
 
 /* default callbacks, set in each new render */
 static void result_nothing(void * /*arg*/, RenderResult * /*rr*/) {}
@@ -2053,9 +2049,7 @@ void RE_RenderFrame(Render *re,
                                      (rd.scemode & R_EXTENSION) != 0,
                                      false,
                                      nullptr);
-
-        /* reports only used for Movie */
-        do_write_image_or_movie(re, bmain, scene, nullptr, 0, filepath_override);
+        do_write_image_or_movie(re, bmain, scene, 0, filepath_override);
       }
     }
 
@@ -2152,8 +2146,7 @@ bool RE_WriteRenderViewsMovie(ReportList *reports,
                               RenderResult *rr,
                               Scene *scene,
                               RenderData *rd,
-                              bMovieHandle *mh,
-                              void **movie_ctx_arr,
+                              ImbMovieWriter **movie_writers,
                               const int totvideos,
                               bool preview)
 {
@@ -2177,13 +2170,14 @@ bool RE_WriteRenderViewsMovie(ReportList *reports,
 
       IMB_colormanagement_imbuf_for_write(ibuf, true, false, &image_format);
 
-      if (!mh->append_movie(movie_ctx_arr[view_id],
-                            rd,
-                            preview ? scene->r.psfra : scene->r.sfra,
-                            scene->r.cfra,
-                            ibuf,
-                            suffix,
-                            reports))
+      BLI_assert(movie_writers[view_id] != nullptr);
+      if (!IMB_movie_write_append(movie_writers[view_id],
+                                  rd,
+                                  preview ? scene->r.psfra : scene->r.sfra,
+                                  scene->r.cfra,
+                                  ibuf,
+                                  suffix,
+                                  reports))
       {
         ok = false;
       }
@@ -2211,13 +2205,14 @@ bool RE_WriteRenderViewsMovie(ReportList *reports,
 
     ibuf_arr[2] = IMB_stereo3d_ImBuf(&image_format, ibuf_arr[0], ibuf_arr[1]);
 
-    if (!mh->append_movie(movie_ctx_arr[0],
-                          rd,
-                          preview ? scene->r.psfra : scene->r.sfra,
-                          scene->r.cfra,
-                          ibuf_arr[2],
-                          "",
-                          reports))
+    BLI_assert(movie_writers[0] != nullptr);
+    if (!IMB_movie_write_append(movie_writers[0],
+                                rd,
+                                preview ? scene->r.psfra : scene->r.sfra,
+                                scene->r.cfra,
+                                ibuf_arr[2],
+                                "",
+                                reports))
     {
       ok = false;
     }
@@ -2233,12 +2228,8 @@ bool RE_WriteRenderViewsMovie(ReportList *reports,
   return ok;
 }
 
-static bool do_write_image_or_movie(Render *re,
-                                    Main *bmain,
-                                    Scene *scene,
-                                    bMovieHandle *mh,
-                                    const int totvideos,
-                                    const char *filepath_override)
+static bool do_write_image_or_movie(
+    Render *re, Main *bmain, Scene *scene, const int totvideos, const char *filepath_override)
 {
   char filepath[FILE_MAX];
   RenderResult rres;
@@ -2256,7 +2247,7 @@ static bool do_write_image_or_movie(Render *re,
     /* write movie or image */
     if (BKE_imtype_is_movie(scene->r.im_format.imtype)) {
       RE_WriteRenderViewsMovie(
-          re->reports, &rres, scene, &re->r, mh, re->movie_ctx_arr, totvideos, false);
+          re->reports, &rres, scene, &re->r, re->movie_writers.data(), totvideos, false);
     }
     else {
       if (filepath_override) {
@@ -2334,16 +2325,12 @@ static void get_videos_dimensions(const Render *re,
   BKE_scene_multiview_videos_dimensions_get(rd, width, height, r_width, r_height);
 }
 
-static void re_movie_free_all(Render *re, bMovieHandle *mh, int totvideos)
+static void re_movie_free_all(Render *re)
 {
-  int i;
-
-  for (i = 0; i < totvideos; i++) {
-    mh->end_movie(re->movie_ctx_arr[i]);
-    mh->context_free(re->movie_ctx_arr[i]);
+  for (ImbMovieWriter *writer : re->movie_writers) {
+    IMB_movie_write_end(writer);
   }
-
-  MEM_SAFE_FREE(re->movie_ctx_arr);
+  re->movie_writers.clear_and_shrink();
 }
 
 void RE_RenderAnim(Render *re,
@@ -2361,7 +2348,6 @@ void RE_RenderAnim(Render *re,
 
   RenderData rd;
   memcpy(&rd, &scene->r, sizeof(rd));
-  bMovieHandle *mh = nullptr;
   const int cfra_old = rd.cfra;
   const float subframe_old = rd.subframe;
   int nfra, totrendered = 0, totskipped = 0;
@@ -2385,42 +2371,30 @@ void RE_RenderAnim(Render *re,
 
   if (is_movie && do_write_file) {
     size_t width, height;
-    int i;
-    bool is_error = false;
-
     get_videos_dimensions(re, &rd, &width, &height);
 
-    mh = BKE_movie_handle_get(rd.im_format.imtype);
-    if (mh == nullptr) {
-      render_pipeline_free(re);
-      BKE_report(re->reports, RPT_ERROR, "Movie format unsupported");
-      return;
-    }
-
-    re->movie_ctx_arr = MEM_cnew_array<void *>(totvideos, "Movies' Context");
-
-    for (i = 0; i < totvideos; i++) {
+    bool is_error = false;
+    re->movie_writers.reserve(totvideos);
+    for (int i = 0; i < totvideos; i++) {
       const char *suffix = BKE_scene_multiview_view_id_suffix_get(&re->r, i);
-
-      re->movie_ctx_arr[i] = mh->context_create();
-
-      if (!mh->start_movie(re->movie_ctx_arr[i],
-                           re->pipeline_scene_eval,
-                           &re->r,
-                           width,
-                           height,
-                           re->reports,
-                           false,
-                           suffix))
-      {
+      ImbMovieWriter *writer = IMB_movie_write_begin(rd.im_format.imtype,
+                                                     re->pipeline_scene_eval,
+                                                     &re->r,
+                                                     width,
+                                                     height,
+                                                     re->reports,
+                                                     false,
+                                                     suffix);
+      if (writer == nullptr) {
         is_error = true;
         break;
       }
+      re->movie_writers.append(writer);
     }
 
     if (is_error) {
-      /* report is handled above */
-      re_movie_free_all(re, mh, i + 1);
+      BKE_report(re->reports, RPT_ERROR, "Movie format unsupported");
+      re_movie_free_all(re);
       render_pipeline_free(re);
       return;
     }
@@ -2558,7 +2532,7 @@ void RE_RenderAnim(Render *re,
     const bool should_write = !(re->flag & R_SKIP_WRITE);
     if (re->test_break_cb(re->tbh) == 0) {
       if (!G.is_break && should_write) {
-        if (!do_write_image_or_movie(re, bmain, scene, mh, totvideos, nullptr)) {
+        if (!do_write_image_or_movie(re, bmain, scene, totvideos, nullptr)) {
           G.is_break = true;
         }
       }
@@ -2610,7 +2584,7 @@ void RE_RenderAnim(Render *re,
 
   /* end movie */
   if (is_movie && do_write_file) {
-    re_movie_free_all(re, mh, totvideos);
+    re_movie_free_all(re);
   }
 
   if (totskipped && totrendered == 0) {

commit 65ad299430ab3598359fd1e0a75899b7c5a789c5

Fix #135806: Wrong error message on render to directory without permission
Make MOV_write_begin always report an error on failure, and remove the
generic one from the caller.

Regression from 5a18eaf7ad.

Pull Request: https://projects.blender.org/blender/blender/pulls/135859

source/blender/editors/render/render_opengl.cc
@@ -973,7 +973,6 @@ static bool screen_opengl_render_anim_init(wmOperator *op)
                                             PRVRANGEON != 0,
                                             suffix);
       if (writer == nullptr) {
-        BKE_report(oglrender->reports, RPT_ERROR, "Movie format unsupported");
         screen_opengl_render_end(oglrender);
         MEM_delete(oglrender);
         return false;

source/blender/imbuf/movie/intern/movie_write.cc
@@ -1279,7 +1279,8 @@ static MovieWriter *ffmpeg_movie_open(const Scene *scene,
                                scene,
                                preview ? rd->psfra : rd->sfra,
                                rd->ffcodecdata.audio_mixrate,
-                               rd->ffcodecdata.audio_volume);
+                               rd->ffcodecdata.audio_volume,
+                               reports);
   }
 
   if (!success) {
@@ -1426,6 +1427,7 @@ MovieWriter *MOV_write_begin(const char imtype,
                              const char *suffix)
 {
   if (!is_imtype_ffmpeg(imtype)) {
+    BKE_report(reports, RPT_ERROR, "Image format is not a movie format");
     return nullptr;
   }
 

source/blender/imbuf/movie/intern/movie_write.hh
@@ -37,6 +37,7 @@ extern "C" {
 #  endif
 
 struct Scene;
+struct ReportList;
 struct StampData;
 
 struct MovieWriter {
@@ -81,8 +82,12 @@ struct MovieWriter {
 #  endif
 };
 
-bool movie_audio_open(
-    MovieWriter *context, const Scene *scene, int start_frame, int mixrate, float volume);
+bool movie_audio_open(MovieWriter *context,
+                      const Scene *scene,
+                      int start_frame,
+                      int mixrate,
+                      float volume,
+                      ReportList *reports);
 void movie_audio_close(MovieWriter *context, bool is_autosplit);
 
 AVStream *alloc_audio_stream(MovieWriter *context,

source/blender/imbuf/movie/intern/movie_write_audio.cc
@@ -25,6 +25,7 @@
 #  include "BLI_threads.h"
 #  include "BLI_utildefines.h"
 
+#  include "BKE_report.hh"
 #  include "BKE_sound.h"
 
 /* If any of these codecs, we prefer the float sample format (if supported) */
@@ -132,8 +133,12 @@ static int write_audio_frame(MovieWriter *context)
 }
 #  endif /* #ifdef WITH_AUDASPACE */
 
-bool movie_audio_open(
-    MovieWriter *context, const Scene *scene, int start_frame, int mixrate, float volume)
+bool movie_audio_open(MovieWriter *context,
+                      const Scene *scene,
+                      int start_frame,
+                      int mixrate,
+                      float volume,
+                      ReportList *reports)
 {
   bool success = true;
 #  ifdef WITH_AUDASPACE
@@ -164,6 +169,7 @@ bool movie_audio_open(
         specs.format = AUD_FORMAT_FLOAT64;
         break;
       default:
+        BKE_report(reports, RPT_ERROR, "Audio sample format unsupported");
         success = false;
         break;
     }

source/blender/render/intern/pipeline.cc
@@ -2446,7 +2446,6 @@ void RE_RenderAnim(Render *re,
     }
 
     if (is_error) {
-      BKE_report(re->reports, RPT_ERROR, "Movie format unsupported");
       re_movie_free_all(re);
       render_pipeline_free(re);
       return;