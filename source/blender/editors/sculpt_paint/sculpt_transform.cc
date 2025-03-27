Bforartists/Bforartists

commit f8a24d05a6afecdd44b046a3e4f1134ca153cab7

Fix #134895: Cancelling sculpt mode elastic transforms deforms mesh
Since the transform system uses previous positions to update to
"original" positions upon cancel, the elastic transforms are not working
well with this (due to their `TransformDisplacementMode::Incremental`
nature).

To resolve, rely on restoring positions from undo (as done elsewhere in
sculpt).

Pull Request: https://projects.blender.org/blender/blender/pulls/134919

source/blender/editors/sculpt_paint/sculpt_transform.cc
@@ -576,6 +576,16 @@ void update_modal_transform(bContext *C, Object &ob)
   flush_update_step(C, UpdateType::Position);
 }
 
+void cancel_modal_transform(bContext *C, Object &ob)
+{
+  /* Cancelling "Elastic" transforms (due to its TransformDisplacementMode::Incremental nature),
+   * requires restoring positions from undo. For "All Vertices" there is no benefit in using the
+   * transform system to update to original positions either. */
+  Depsgraph *depsgraph = CTX_data_depsgraph_pointer(C);
+
+  undo::restore_position_from_undo_step(*depsgraph, ob);
+}
+
 void end_transform(bContext *C, Object &ob)
 {
   SculptSession &ss = *ob.sculpt;

source/blender/editors/include/ED_sculpt.hh
@@ -47,6 +47,7 @@ void keymap_sculpt(wmKeyConfig *keyconf);
 /* `sculpt_transform.cc` */
 
 void update_modal_transform(bContext *C, Object &ob);
+void cancel_modal_transform(bContext *C, Object &ob);
 void init_transform(bContext *C, Object &ob, const float mval_fl[2], const char *undo_name);
 void end_transform(bContext *C, Object &ob);
 

source/blender/editors/transform/transform_convert_sculpt.cc
@@ -110,7 +110,13 @@ static void recalcData_sculpt(TransInfo *t)
   using namespace blender::ed;
   BKE_view_layer_synced_ensure(t->scene, t->view_layer);
   Object *ob = BKE_view_layer_active_object_get(t->view_layer);
-  sculpt_paint::update_modal_transform(t->context, *ob);
+
+  if (t->state == TRANS_CANCEL) {
+    sculpt_paint::cancel_modal_transform(t->context, *ob);
+  }
+  else {
+    sculpt_paint::update_modal_transform(t->context, *ob);
+  }
 }
 
 static void special_aftertrans_update__sculpt(bContext *C, TransInfo *t)

commit 6bcf0204f0a1f180197e196b621dc08f0c8d44dd

Fix: Canceling sculpt transform tool leads to invalid display of mesh
Caused by f8a24d05a6a

`cancel_modal_transform` restores the original positions, but it was
missing calls to update the vertex normals and the bounds of the BVH
nodes.

Pull Request: https://projects.blender.org/blender/blender/pulls/136580

source/blender/editors/sculpt_paint/sculpt_transform.cc
@@ -581,9 +581,12 @@ void cancel_modal_transform(bContext *C, Object &ob)
   /* Canceling "Elastic" transforms (due to its #TransformDisplacementMode::Incremental nature),
    * requires restoring positions from undo. For "All Vertices" there is no benefit in using the
    * transform system to update to original positions either. */
-  Depsgraph *depsgraph = CTX_data_depsgraph_pointer(C);
+  Depsgraph &depsgraph = *CTX_data_depsgraph_pointer(C);
+  undo::restore_position_from_undo_step(depsgraph, ob);
 
-  undo::restore_position_from_undo_step(*depsgraph, ob);
+  bke::pbvh::Tree &pbvh = *bke::object::pbvh_get(ob);
+  bke::pbvh::update_normals(depsgraph, ob, pbvh);
+  pbvh.update_bounds(depsgraph, ob);
 }
 
 void end_transform(bContext *C, Object &ob)