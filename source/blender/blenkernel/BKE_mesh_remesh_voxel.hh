Bforartists/Bforartists

commit 7f766fb57c63d1bbfec3bd6e8b250d570e05984b

UI: mitigate problem where remesh could accidentally hang/crash
Having a small voxel size meant typing in zero would clamp the
voxel size to a small number which was often small enough to hang
calculating large voxels which would eventually fail to allocate
memory & crash.

Instead, allow a zero value but bypass calculation.

See #130526.

source/blender/modifiers/intern/MOD_remesh.cc
@@ -127,7 +127,7 @@ static void dualcon_add_quad(void *output_v, const int vert_indices[4])
   output->curface++;
 }
 
-static Mesh *modify_mesh(ModifierData *md, const ModifierEvalContext * /*ctx*/, Mesh *mesh)
+static Mesh *modify_mesh(ModifierData *md, const ModifierEvalContext *ctx, Mesh *mesh)
 {
   using namespace blender;
   RemeshModifierData *rmd;
@@ -142,6 +142,7 @@ static Mesh *modify_mesh(ModifierData *md, const ModifierEvalContext * /*ctx*/, 
   if (rmd->mode == MOD_REMESH_VOXEL) {
     /* OpenVDB modes. */
     if (rmd->voxel_size == 0.0f) {
+      BKE_modifier_set_error(ctx->object, md, "Zero voxel size cannot be solved");
       return nullptr;
     }
     result = BKE_mesh_remesh_voxel(mesh, rmd->voxel_size, rmd->adaptivity, 0.0f);
@@ -150,6 +151,11 @@ static Mesh *modify_mesh(ModifierData *md, const ModifierEvalContext * /*ctx*/, 
     }
   }
   else {
+    if (rmd->scale == 0.0f) {
+      BKE_modifier_set_error(ctx->object, md, "Zero scale cannot be solved");
+      return nullptr;
+    }
+
     /* Dualcon modes. */
     init_dualcon_mesh(&input, mesh);
 

source/blender/makesrna/intern/rna_modifier.cc
@@ -6350,9 +6350,11 @@ static void rna_def_modifier_remesh(BlenderRNA *brna)
       "edges closer to the input");
   RNA_def_property_update(prop, 0, "rna_Modifier_update");
 
+  /* NOTE: allow zero (which skips computation), to avoid zero clamping
+   * to a small value which is likely to run out of memory, see: #130526. */
   prop = RNA_def_property(srna, "voxel_size", PROP_FLOAT, PROP_DISTANCE);
   RNA_def_property_float_sdna(prop, nullptr, "voxel_size");
-  RNA_def_property_range(prop, 0.0001f, FLT_MAX);
+  RNA_def_property_range(prop, 0.0f, FLT_MAX);
   RNA_def_property_ui_range(prop, 0.0001, 2, 0.1, 3);
   RNA_def_property_ui_scale_type(prop, PROP_SCALE_LOG);
   RNA_def_property_ui_text(prop,
@@ -8172,6 +8174,8 @@ static void rna_def_modifier_mesh_to_volume(BlenderRNA *brna)
       prop, "Resolution Mode", "Mode for how the desired voxel size is specified");
   RNA_def_property_update(prop, 0, "rna_Modifier_update");
 
+  /* NOTE: allow zero (which skips computation), to avoid zero clamping
+   * to a small value which is likely to run out of memory, see: #130526. */
   prop = RNA_def_property(srna, "voxel_size", PROP_FLOAT, PROP_NONE);
   RNA_def_property_ui_text(
       prop, "Voxel Size", "Smaller values result in a higher resolution output");
@@ -8336,6 +8340,8 @@ static void rna_def_modifier_volume_to_mesh(BlenderRNA *brna)
       prop, "Resolution Mode", "Mode for how the desired voxel size is specified");
   RNA_def_property_update(prop, 0, "rna_Modifier_update");
 
+  /* NOTE: allow zero (which skips computation), to avoid zero clamping
+   * to a small value which is likely to run out of memory, see: #130526. */
   prop = RNA_def_property(srna, "voxel_size", PROP_FLOAT, PROP_NONE);
   RNA_def_property_ui_text(
       prop, "Voxel Size", "Smaller values result in a higher resolution output");

commit 5c2bafc18c8aa0fd5b94a2a04171c808a21dddf2

Fix #136637: Voxel remesh crashes with small input
Caused by 7f766fb57c63d1bbfec3bd6e8b250d570e05984b

OpenVDB does not handle transformations with determinants smaller than
3e-15 and throws an `ArithmeticError` when a `Transform` is created
with invalid values.

To avoid abrupt crashes on the user side, this commit catches the error
and displays a warning message.

Pull Request: https://projects.blender.org/blender/blender/pulls/136690

source/blender/blenkernel/BKE_mesh_remesh_voxel.hh
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
 #pragma once
+struct ModifierData;
+struct ReportList;
 
 /** \file
  * \ingroup bke
@@ -11,7 +13,14 @@
 struct Mesh;
 
 Mesh *BKE_mesh_remesh_voxel_fix_poles(const Mesh *mesh);
-Mesh *BKE_mesh_remesh_voxel(const Mesh *mesh, float voxel_size, float adaptivity, float isovalue);
+Mesh *BKE_mesh_remesh_voxel(const Mesh *mesh,
+                            float voxel_size,
+                            float adaptivity,
+                            float isovalue,
+                            const Object *object,
+                            ModifierData *modifier_data);
+Mesh *BKE_mesh_remesh_voxel(
+    const Mesh *mesh, float voxel_size, float adaptivity, float isovalue, ReportList *reports);
 Mesh *BKE_mesh_remesh_quadriflow(const Mesh *mesh,
                                  int target_faces,
                                  int seed,

source/blender/blenkernel/intern/mesh_remesh_voxel.cc
@@ -27,6 +27,8 @@
 #include "BKE_mesh.hh"
 #include "BKE_mesh_remesh_voxel.hh" /* own include */
 #include "BKE_mesh_sample.hh"
+#include "BKE_modifier.hh"
+#include "BKE_report.hh"
 
 #include "bmesh.hh"
 #include "bmesh_tools.hh"
@@ -164,8 +166,8 @@ Mesh *BKE_mesh_remesh_quadriflow(const Mesh *mesh,
 }
 
 #ifdef WITH_OPENVDB
-static openvdb::FloatGrid::Ptr remesh_voxel_level_set_create(const Mesh *mesh,
-                                                             const float voxel_size)
+static openvdb::FloatGrid::Ptr remesh_voxel_level_set_create(
+    const Mesh *mesh, openvdb::math::Transform::Ptr transform)
 {
   const Span<float3> positions = mesh->vert_positions();
   const Span<int> corner_verts = mesh->corner_verts();
@@ -185,8 +187,6 @@ static openvdb::FloatGrid::Ptr remesh_voxel_level_set_create(const Mesh *mesh,
         corner_verts[tri[0]], corner_verts[tri[1]], corner_verts[tri[2]]);
   }
 
-  openvdb::math::Transform::Ptr transform = openvdb::math::Transform::createLinearTransform(
-      voxel_size);
   openvdb::FloatGrid::Ptr grid = openvdb::tools::meshToLevelSet<openvdb::FloatGrid>(
       *transform, points, triangles, 1.0f);
 
@@ -247,15 +247,57 @@ static Mesh *remesh_voxel_volume_to_mesh(const openvdb::FloatGrid::Ptr level_set
 Mesh *BKE_mesh_remesh_voxel(const Mesh *mesh,
                             const float voxel_size,
                             const float adaptivity,
-                            const float isovalue)
+                            const float isovalue,
+                            const Object *object,
+                            ModifierData *modifier_data)
 {
 #ifdef WITH_OPENVDB
-  openvdb::FloatGrid::Ptr level_set = remesh_voxel_level_set_create(mesh, voxel_size);
+  openvdb::math::Transform::Ptr transform;
+  try {
+    transform = openvdb::math::Transform::createLinearTransform(voxel_size);
+  }
+  catch (const openvdb::ArithmeticError & /*e*/) {
+    /* OpenVDB internally has a limit of 3e-15 for the matrix's determinant and throws
+     * ArithmeticError if the provided value is too low.
+     * See #136637 for more details. */
+    BKE_modifier_set_error(
+        object, modifier_data, "Voxel size of %f too small to be solved", voxel_size);
+    return nullptr;
+  }
+  openvdb::FloatGrid::Ptr level_set = remesh_voxel_level_set_create(mesh, transform);
   Mesh *result = remesh_voxel_volume_to_mesh(level_set, isovalue, adaptivity, false);
   BKE_mesh_copy_parameters(result, mesh);
   return result;
 #else
-  UNUSED_VARS(mesh, voxel_size, adaptivity, isovalue);
+  UNUSED_VARS(mesh, voxel_size, adaptivity, isovalue, object, modifier_data);
+  return nullptr;
+#endif
+}
+
+Mesh *BKE_mesh_remesh_voxel(const Mesh *mesh,
+                            const float voxel_size,
+                            const float adaptivity,
+                            const float isovalue,
+                            ReportList *reports)
+{
+#ifdef WITH_OPENVDB
+  openvdb::math::Transform::Ptr transform;
+  try {
+    transform = openvdb::math::Transform::createLinearTransform(voxel_size);
+  }
+  catch (const openvdb::ArithmeticError & /*e*/) {
+    /* OpenVDB internally has a limit of 3e-15 for the matrix's determinant and throws
+     * ArithmeticError if the provided value is too low.
+     * See #136637 for more details. */
+    BKE_reportf(reports, RPT_ERROR, "Voxel size of %f too small to be solved", voxel_size);
+    return nullptr;
+  }
+  openvdb::FloatGrid::Ptr level_set = remesh_voxel_level_set_create(mesh, transform);
+  Mesh *result = remesh_voxel_volume_to_mesh(level_set, isovalue, adaptivity, false);
+  BKE_mesh_copy_parameters(result, mesh);
+  return result;
+#else
+  UNUSED_VARS(mesh, voxel_size, adaptivity, isovalue, reports);
   return nullptr;
 #endif
 }

source/blender/editors/object/object_remesh.cc
@@ -128,7 +128,7 @@ static wmOperatorStatus voxel_remesh_exec(bContext *C, wmOperator *op)
   }
 
   Mesh *new_mesh = BKE_mesh_remesh_voxel(
-      mesh, mesh->remesh_voxel_size, mesh->remesh_voxel_adaptivity, isovalue);
+      mesh, mesh->remesh_voxel_size, mesh->remesh_voxel_adaptivity, isovalue, op->reports);
 
   if (!new_mesh) {
     BKE_report(op->reports, RPT_ERROR, "Voxel remesher failed to create mesh");

source/blender/modifiers/intern/MOD_remesh.cc
@@ -139,7 +139,7 @@ static Mesh *modify_mesh(ModifierData *md, const ModifierEvalContext *ctx, Mesh 
       BKE_modifier_set_error(ctx->object, md, "Zero voxel size cannot be solved");
       return nullptr;
     }
-    result = BKE_mesh_remesh_voxel(mesh, rmd->voxel_size, rmd->adaptivity, 0.0f);
+    result = BKE_mesh_remesh_voxel(mesh, rmd->voxel_size, rmd->adaptivity, 0.0f, ctx->object, md);
     if (result == nullptr) {
       return nullptr;
     }