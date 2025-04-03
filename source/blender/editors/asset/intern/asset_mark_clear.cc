Bforartists/Bforartists

commit f46bdadf28436abd07315dbfd6d264dd4d1f437b

Fix #124518: Crash modifying material and marking it as an asset in quick succession
The preview generation job is not safe from cases when the underlying data is
changed outside of the job.

Ideally we'd solve possible threading conflict by making the job somehow
smarter. Until then kill the preview job manually, following similar pattern
from other places (for example, lib_id_generate_preview_exec).

Pull Request: https://projects.blender.org/blender/blender/pulls/131372

source/blender/editors/asset/intern/asset_mark_clear.cc
@@ -25,6 +25,7 @@
 #include "ED_asset_list.hh"
 #include "ED_asset_mark_clear.hh"
 #include "ED_asset_type.hh"
+#include "ED_render.hh"
 
 #include "WM_types.hh"
 
@@ -56,6 +57,8 @@ bool mark_id(ID *id)
 
 void generate_preview(const bContext *C, ID *id)
 {
+  ED_preview_kill_jobs(CTX_wm_manager(C), CTX_data_main(C));
+
   PreviewImage *preview = BKE_previewimg_id_get(id);
   if (preview) {
     BKE_previewimg_clear(preview);

commit 08c22e72223eda3063fac77c2a630f8d933fa785

Fix #136645: Regression: Preview generation fails for multiple IDs
Fixes: #136842, #136645

Caused by f46bdadf28
When multiple objects are selected to mark as assets, preview generation
job of previous ID in the for loop is cleared by `ED_preview_kill_jobs`
inside `generate_preview`. To fix this, check if `id->preview` exists then
clear the preview job if exists inside the new function
`ED_preview_kill_jobs_for_id`.
Solution proposed by @JulianEisel. I came up with idea to use `wm_job_find`

Pull Request: https://projects.blender.org/blender/blender/pulls/136918

source/blender/editors/asset/intern/asset_mark_clear.cc
@@ -61,7 +61,7 @@ void generate_preview(const bContext *C, ID *id)
     return;
   }
 
-  ED_preview_kill_jobs(CTX_wm_manager(C), CTX_data_main(C));
+  ED_preview_kill_jobs_for_id(CTX_wm_manager(C), id);
 
   PreviewImage *preview = BKE_previewimg_id_get(id);
   if (preview) {

source/blender/editors/include/ED_render.hh
@@ -113,6 +113,7 @@ void ED_preview_restart_queue_add(ID *id, enum eIconSizes size);
 void ED_preview_restart_queue_work(const bContext *C);
 
 void ED_preview_kill_jobs(wmWindowManager *wm, Main *bmain);
+void ED_preview_kill_jobs_for_id(wmWindowManager *wm, const ID *id);
 
 void ED_preview_draw(
     const bContext *C, void *idp, void *parentp, void *slotp, uiPreview *ui_preview, rcti *rect);

source/blender/editors/render/render_preview.cc
@@ -2171,6 +2171,14 @@ void ED_preview_kill_jobs(wmWindowManager *wm, Main * /*bmain*/)
   }
 }
 
+void ED_preview_kill_jobs_for_id(wmWindowManager *wm, const ID *id)
+{
+  const PreviewImage *preview = BKE_previewimg_id_get(id);
+  if (wm && preview) {
+    WM_jobs_kill_type(wm, preview, WM_JOB_TYPE_RENDER_PREVIEW);
+  }
+}
+
 struct PreviewRestartQueueEntry {
   PreviewRestartQueueEntry *next, *prev;
 