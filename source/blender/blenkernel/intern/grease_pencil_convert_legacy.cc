Bforartists/Bforartists

commit e936dec532ba93080e5a1c6c1c7fcc5fcfab79c2

Fix: Grease Pencil: Channel color versioning
When file with legacy GP is opened in newer version, dopesheet
channel color is not transferred, they appear dark instead. To
fix this, copy color value from legacy GP layers in versioning code.

Pull Request: https://projects.blender.org/blender/blender/pulls/136876

source/blender/blenkernel/intern/grease_pencil_convert_legacy.cc
@@ -1064,6 +1064,8 @@ static void legacy_gpencil_to_grease_pencil(ConversionData &conversion_data,
     SET_FLAG_FROM_TEST(
         new_layer.base.flag, (gpl->flag & GP_LAYER_USE_MASK) == 0, GP_LAYER_TREE_NODE_HIDE_MASKS);
 
+    /* Copy Dopesheet channel color. */
+    copy_v4_v4(new_layer.base.color, gpl->color);
     new_layer.blend_mode = int8_t(gpl->blend_mode);
 
     new_layer.parent = gpl->parent;

commit 648a3599a4dc06bdd72952c7fe777d549ae00054

Fix #137152: Grease Pencil: Layer flag is overridden during conversion
Broken by e936dec532ba93080e5a1c6c1c7fcc5fcfab79c2.

When Grease Pencil objects get converted (<= 4.2 to >= 4.3)
the code copies the legacy color over to the new one.
The issue is that the new color is a `float[3]` not a
`float[4]`! Thus the `flag` after the `color` was getting
overridden.

This fixes the issue by replacing the `copy_v4_v4` with a
`copy_v3_v3`.

Pull Request: https://projects.blender.org/blender/blender/pulls/137161

source/blender/blenkernel/intern/grease_pencil_convert_legacy.cc
@@ -1065,7 +1065,7 @@ static void legacy_gpencil_to_grease_pencil(ConversionData &conversion_data,
         new_layer.base.flag, (gpl->flag & GP_LAYER_USE_MASK) == 0, GP_LAYER_TREE_NODE_HIDE_MASKS);
 
     /* Copy Dope-sheet channel color. */
-    copy_v4_v4(new_layer.base.color, gpl->color);
+    copy_v3_v3(new_layer.base.color, gpl->color);
     new_layer.blend_mode = int8_t(gpl->blend_mode);
 
     new_layer.parent = gpl->parent;