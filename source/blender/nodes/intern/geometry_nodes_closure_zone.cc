Bforartists/Bforartists

commit 5334c94f6b883cc1d3876c25daad1b8a222acab0

Cleanup: simplify method name

source/blender/nodes/intern/geometry_nodes_closure_zone.cc
@@ -104,7 +104,7 @@ class LazyFunctionForClosureZone : public LazyFunction {
     const auto &storage = *static_cast<const NodeGeometryClosureOutput *>(output_bnode_.storage);
 
     std::unique_ptr<ResourceScope> closure_scope = std::make_unique<ResourceScope>();
-    LinearAllocator<> &closure_allocator = closure_scope->linear_allocator();
+    LinearAllocator<> &closure_allocator = closure_scope->allocator();
 
     lf::Graph &lf_graph = closure_scope->construct<lf::Graph>("Closure Graph");
     lf::FunctionNode &lf_body_node = lf_graph.add_function(*body_fn_.function);
@@ -511,8 +511,8 @@ class LazyFunctionForEvaluateClosureNode : public LazyFunction {
 
     auto get_output_default_value = [&](const bke::bNodeSocketType &type) {
       const CPPType &cpp_type = *type.geometry_nodes_cpp_type;
-      void *fallback_value = eval_storage.scope.linear_allocator().allocate(cpp_type.size(),
-                                                                            cpp_type.alignment());
+      void *fallback_value = eval_storage.scope.allocator().allocate(cpp_type.size(),
+                                                                     cpp_type.alignment());
       construct_socket_default_value(type, fallback_value);
       if (!cpp_type.is_trivially_destructible()) {
         eval_storage.scope.add_destruct_call(
@@ -603,7 +603,7 @@ class LazyFunctionForEvaluateClosureNode : public LazyFunction {
     lf_graph.update_node_indices();
     eval_storage.graph_executor.emplace(lf_graph, nullptr, nullptr, nullptr);
     eval_storage.graph_executor_storage = eval_storage.graph_executor->init_storage(
-        eval_storage.scope.linear_allocator());
+        eval_storage.scope.allocator());
 
     /* Log graph for debugging purposes. */
     bNodeTree &btree_orig = *reinterpret_cast<bNodeTree *>(
@@ -625,7 +625,7 @@ void evaluate_closure_eagerly(const Closure &closure, ClosureEagerEvalParams &pa
   const int fn_outputs_num = fn.outputs().size();
 
   ResourceScope scope;
-  LinearAllocator<> &allocator = scope.linear_allocator();
+  LinearAllocator<> &allocator = scope.allocator();
 
   GeoNodesLFLocalUserData local_user_data(*params.user_data);
   void *storage = fn.init_storage(allocator);

source/blender/blenlib/BLI_resource_scope.hh
@@ -86,7 +86,7 @@ class ResourceScope : NonCopyable, NonMovable {
    * Returns a reference to a linear allocator that is owned by the #ResourceScope. Memory
    * allocated through this allocator will be freed when the collector is destructed.
    */
-  LinearAllocator<> &linear_allocator();
+  LinearAllocator<> &allocator();
 };
 
 /* -------------------------------------------------------------------- */
@@ -152,7 +152,7 @@ template<typename T, typename... Args> inline T &ResourceScope::construct(Args &
   return value_ref;
 }
 
-inline LinearAllocator<> &ResourceScope::linear_allocator()
+inline LinearAllocator<> &ResourceScope::allocator()
 {
   return allocator_;
 }

source/blender/editors/interface/templates/interface_template_search_menu.cc
@@ -224,10 +224,10 @@ static bool menu_items_from_ui_create_item_from_button(MenuSearch_Data *data,
                                            "" :
                                            StringRef(but->drawstr).drop_prefix(sep_index);
       std::string drawstr = std::string("(") + drawstr_override + ")" + drawstr_suffix;
-      item->drawstr = scope.linear_allocator().copy_string(drawstr);
+      item->drawstr = scope.allocator().copy_string(drawstr);
     }
     else {
-      item->drawstr = scope.linear_allocator().copy_string(but->drawstr);
+      item->drawstr = scope.allocator().copy_string(but->drawstr);
     }
 
     item->icon = ui_but_icon(but);
@@ -385,7 +385,7 @@ static void menu_items_from_all_operators(bContext *C, MenuSearch_Data *data)
 
       SNPRINTF(uiname, "%s " UI_MENU_ARROW_SEP "%s", idname_as_py, ot_ui_name);
 
-      item.drawwstr_full = scope.linear_allocator().copy_string(uiname);
+      item.drawwstr_full = scope.allocator().copy_string(uiname);
       item.drawstr = ot_ui_name;
 
       item.wm_context = nullptr;
@@ -683,8 +683,7 @@ static MenuSearch_Data *menu_items_from_ui_create(bContext *C,
           }
 
           if (but_test < block->buttons.begin()) {
-            menu_display_name_map.add(mt,
-                                      scope.linear_allocator().copy_string(but->drawstr).c_str());
+            menu_display_name_map.add(mt, scope.allocator().copy_string(but->drawstr).c_str());
           }
         }
         else if (menu_items_from_ui_create_item_from_button(
@@ -723,7 +722,7 @@ static MenuSearch_Data *menu_items_from_ui_create(bContext *C,
               }
               str_buf.append(StringRef(drawstr, drawstr_len));
               fmt::format_to(fmt::appender(str_buf), " ({})", drawstr_sep + 1);
-              menu_parent->drawstr = scope.linear_allocator().copy_string(
+              menu_parent->drawstr = scope.allocator().copy_string(
                   StringRef(str_buf.data(), str_buf.size()));
               str_buf.clear();
             }
@@ -735,7 +734,7 @@ static MenuSearch_Data *menu_items_from_ui_create(bContext *C,
                   drawstr_is_empty = true;
                 }
               }
-              menu_parent->drawstr = scope.linear_allocator().copy_string(drawstr);
+              menu_parent->drawstr = scope.allocator().copy_string(drawstr);
             }
             menu_parent->parent = current_menu.self_as_parent;
 
@@ -785,7 +784,7 @@ static MenuSearch_Data *menu_items_from_ui_create(bContext *C,
 
           if (poll_success) {
             MenuSearch_Parent *menu_parent = &scope.construct<MenuSearch_Parent>();
-            menu_parent->drawstr = scope.linear_allocator().copy_string(but->drawstr);
+            menu_parent->drawstr = scope.allocator().copy_string(but->drawstr);
             menu_parent->parent = current_menu.self_as_parent;
 
             for (const std::unique_ptr<uiBut> &sub_but : sub_block->buttons) {
@@ -865,8 +864,7 @@ static MenuSearch_Data *menu_items_from_ui_create(bContext *C,
 
     str_buf.append(item.drawstr);
 
-    item.drawwstr_full = scope.linear_allocator().copy_string(
-        StringRef(str_buf.data(), str_buf.size()));
+    item.drawwstr_full = scope.allocator().copy_string(StringRef(str_buf.data(), str_buf.size()));
     str_buf.clear();
   }
 

source/blender/functions/intern/field.cc
@@ -391,7 +391,7 @@ Vector<GVArray> evaluate_fields(ResourceScope &scope,
       void *buffer;
       if (!dst_varray || !dst_varray.is_span()) {
         /* Allocate a new buffer for the computed result. */
-        buffer = scope.linear_allocator().allocate(type.size() * array_size, type.alignment());
+        buffer = scope.allocator().allocate(type.size() * array_size, type.alignment());
 
         if (!type.is_trivially_destructible()) {
           /* Destruct values in the end. */
@@ -437,7 +437,7 @@ Vector<GVArray> evaluate_fields(ResourceScope &scope,
       const GFieldRef &field = constant_fields_to_evaluate[i];
       const CPPType &type = field.cpp_type();
       /* Allocate memory where the computed value will be stored in. */
-      void *buffer = scope.linear_allocator().allocate(type.size(), type.alignment());
+      void *buffer = scope.allocator().allocate(type.size(), type.alignment());
 
       if (!type.is_trivially_destructible()) {
         /* Destruct value in the end. */

source/blender/functions/intern/multi_function_params.cc
@@ -9,8 +9,7 @@ namespace blender::fn::multi_function {
 void ParamsBuilder::add_unused_output_for_unsupporting_function(const CPPType &type)
 {
   ResourceScope &scope = this->resource_scope();
-  void *buffer = scope.linear_allocator().allocate(type.size() * min_array_size_,
-                                                   type.alignment());
+  void *buffer = scope.allocator().allocate(type.size() * min_array_size_, type.alignment());
   const GMutableSpan span{type, buffer, min_array_size_};
   actual_params_.append_unchecked_as(std::in_place_type<GMutableSpan>, span);
   if (!type.is_trivially_destructible()) {

source/blender/nodes/intern/geometry_nodes_execute.cc
@@ -1081,8 +1081,8 @@ void get_geometry_nodes_input_base_values(const bNodeTree &btree,
       continue;
     }
 
-    void *value_buffer = scope.linear_allocator().allocate(
-        stype->geometry_nodes_cpp_type->size(), stype->geometry_nodes_cpp_type->alignment());
+    void *value_buffer = scope.allocator().allocate(stype->geometry_nodes_cpp_type->size(),
+                                                    stype->geometry_nodes_cpp_type->alignment());
     init_socket_cpp_value_from_property(*property, socket_type, value_buffer);
     if (!stype->geometry_nodes_cpp_type->is_trivially_destructible()) {
       scope.add_destruct_call([type = stype->geometry_nodes_cpp_type, value_buffer]() {

source/blender/nodes/intern/geometry_nodes_lazy_function.cc
@@ -3834,7 +3834,7 @@ struct GeometryNodesLazyFunctionBuilder {
     if (this->try_add_implicit_input(input_bsocket, input_lf_socket, graph_params)) {
       return;
     }
-    GMutablePointer value = get_socket_default_value(scope_.linear_allocator(), input_bsocket);
+    GMutablePointer value = get_socket_default_value(scope_.allocator(), input_bsocket);
     if (value.get() == nullptr) {
       /* Not possible to add a default value. */
       return;

source/blender/nodes/intern/socket_usage_inference.cc
@@ -71,7 +71,7 @@ struct SocketUsageInferencer {
                         const std::optional<Span<GPointer>> tree_input_values)
       : root_tree_(tree)
   {
-    scope_.linear_allocator().provide_buffer(scope_buffer_);
+    scope_.allocator().provide_buffer(scope_buffer_);
     root_tree_.ensure_topology_cache();
     root_tree_.ensure_interface_cache();
     this->ensure_animation_data_processed(root_tree_);
@@ -746,7 +746,7 @@ struct SocketUsageInferencer {
       }
       /* Allocate memory for the output value. */
       const CPPType &base_type = *output_socket->typeinfo->base_cpp_type;
-      void *value = scope_.linear_allocator().allocate(base_type.size(), base_type.alignment());
+      void *value = scope_.allocator().allocate(base_type.size(), base_type.alignment());
       params.add_uninitialized_single_output(GMutableSpan(base_type, value, 1));
       all_socket_values_.add_new(output_socket, value);
       if (!base_type.is_trivially_destructible()) {
@@ -828,8 +828,7 @@ struct SocketUsageInferencer {
     }
 
     const CPPType &base_type = *socket->typeinfo->base_cpp_type;
-    void *value_buffer = scope_.linear_allocator().allocate(base_type.size(),
-                                                            base_type.alignment());
+    void *value_buffer = scope_.allocator().allocate(base_type.size(), base_type.alignment());
     socket->typeinfo->get_base_cpp_value(socket->default_value, value_buffer);
     all_socket_values_.add_new(socket, value_buffer);
     if (!base_type.is_trivially_destructible()) {
@@ -870,7 +869,7 @@ struct SocketUsageInferencer {
     if (!conversions.is_convertible(*from_type, *to_type)) {
       return nullptr;
     }
-    void *dst = scope_.linear_allocator().allocate(to_type->size(), to_type->alignment());
+    void *dst = scope_.allocator().allocate(to_type->size(), to_type->alignment());
     conversions.convert_to_uninitialized(*from_type, *to_type, src, dst);
     if (!to_type->is_trivially_destructible()) {
       scope_.add_destruct_call([to_type, dst]() { to_type->destruct(dst); });

commit 9f809ca57a6da521620ea016ca3ae3e3a6fed553

Fix: merge conflict after rename
Caused by 5334c94f6b883cc1d3876c25daad1b8a222acab0.

source/blender/nodes/intern/geometry_nodes_closure_zone.cc
@@ -669,8 +669,8 @@ class LazyFunctionForEvaluateClosureNode : public LazyFunction {
         }
       }
       const CPPType &cpp_type = *output_type.geometry_nodes_cpp_type;
-      void *default_output_value = eval_storage.scope.linear_allocator().allocate(
-          cpp_type.size(), cpp_type.alignment());
+      void *default_output_value = eval_storage.scope.allocator().allocate(cpp_type.size(),
+                                                                           cpp_type.alignment());
       construct_socket_default_value(output_type, default_output_value);
       lf_main_output.set_default_value(default_output_value);
       if (!cpp_type.is_trivially_destructible()) {
@@ -690,7 +690,7 @@ class LazyFunctionForEvaluateClosureNode : public LazyFunction {
     lf_graph.update_node_indices();
     eval_storage.graph_executor.emplace(lf_graph, nullptr, nullptr, nullptr);
     eval_storage.graph_executor_storage = eval_storage.graph_executor->init_storage(
-        eval_storage.scope.linear_allocator());
+        eval_storage.scope.allocator());
   }
 };
 