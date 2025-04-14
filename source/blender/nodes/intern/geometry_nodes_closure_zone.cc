Bforartists/Bforartists

commit 4d77920293e6c46a5705a944ec94e4ca59e463df

Refactor: BLI: Make some CPPType properties public instead of using methods
This makes accessing these properties more convenient. Since we only ever have
const references to `CPPType`, there isn't really a benefit to using methods to
avoid mutation.

Pull Request: https://projects.blender.org/blender/blender/pulls/137482

source/blender/nodes/intern/geometry_nodes_closure_zone.cc
@@ -126,10 +126,10 @@ class LazyFunctionForClosureZone : public LazyFunction {
       lf_graph.add_link(lf_body_node.output(body_fn_.indices.outputs.input_usages[i]),
                         lf_graph_input_usage);
 
-      void *default_value = closure_allocator.allocate(cpp_type.size(), cpp_type.alignment());
+      void *default_value = closure_allocator.allocate(cpp_type.size, cpp_type.alignment);
       construct_socket_default_value(*bsocket.typeinfo, default_value);
       default_input_values.append(default_value);
-      if (!cpp_type.is_trivially_destructible()) {
+      if (!cpp_type.is_trivially_destructible) {
         closure_scope->add_destruct_call(
             [&cpp_type, default_value]() { cpp_type.destruct(default_value); });
       }
@@ -160,9 +160,9 @@ class LazyFunctionForClosureZone : public LazyFunction {
     for (const int i : zone_.border_links.index_range()) {
       const CPPType &cpp_type = *zone_.border_links[i]->tosock->typeinfo->geometry_nodes_cpp_type;
       void *input_ptr = params.try_get_input_data_ptr(zone_info_.indices.inputs.border_links[i]);
-      void *stored_ptr = closure_allocator.allocate(cpp_type.size(), cpp_type.alignment());
+      void *stored_ptr = closure_allocator.allocate(cpp_type.size, cpp_type.alignment);
       cpp_type.move_construct(input_ptr, stored_ptr);
-      if (!cpp_type.is_trivially_destructible()) {
+      if (!cpp_type.is_trivially_destructible) {
         closure_scope->add_destruct_call(
             [&cpp_type, stored_ptr]() { cpp_type.destruct(stored_ptr); });
       }
@@ -521,10 +521,10 @@ class LazyFunctionForEvaluateClosureNode : public LazyFunction {
 
     auto get_output_default_value = [&](const bke::bNodeSocketType &type) {
       const CPPType &cpp_type = *type.geometry_nodes_cpp_type;
-      void *fallback_value = eval_storage.scope.allocator().allocate(cpp_type.size(),
-                                                                     cpp_type.alignment());
+      void *fallback_value = eval_storage.scope.allocator().allocate(cpp_type.size,
+                                                                     cpp_type.alignment);
       construct_socket_default_value(type, fallback_value);
-      if (!cpp_type.is_trivially_destructible()) {
+      if (!cpp_type.is_trivially_destructible) {
         eval_storage.scope.add_destruct_call(
             [fallback_value, type = &cpp_type]() { type->destruct(fallback_value); });
       }
@@ -730,7 +730,7 @@ void evaluate_closure_eagerly(const Closure &closure, ClosureEagerEvalParams &pa
       const bke::bNodeSocketType &from_type = *item.type;
       const bke::bNodeSocketType &to_type = *signature.inputs[*mapped_i].type;
       const CPPType &to_cpp_type = *to_type.geometry_nodes_cpp_type;
-      void *value = allocator.allocate(to_cpp_type.size(), to_cpp_type.alignment());
+      void *value = allocator.allocate(to_cpp_type.size, to_cpp_type.alignment);
       if (&from_type == &to_type) {
         to_cpp_type.copy_construct(item.value, value);
       }
@@ -762,7 +762,7 @@ void evaluate_closure_eagerly(const Closure &closure, ClosureEagerEvalParams &pa
       const bke::bNodeSocketType &type = *signature.inputs[main_input_i].type;
       const CPPType &cpp_type = *type.geometry_nodes_cpp_type;
       const void *default_value = closure.default_input_value(main_input_i);
-      void *value = allocator.allocate(cpp_type.size(), cpp_type.alignment());
+      void *value = allocator.allocate(cpp_type.size, cpp_type.alignment);
       cpp_type.copy_construct(default_value, value);
       lf_input_values[lf_input_i] = {cpp_type, value};
     }
@@ -787,7 +787,7 @@ void evaluate_closure_eagerly(const Closure &closure, ClosureEagerEvalParams &pa
     const bke::bNodeSocketType &type = *signature.outputs[main_output_i].type;
     const CPPType &cpp_type = *type.geometry_nodes_cpp_type;
     lf_output_values[indices.outputs.main[main_output_i]] = {
-        cpp_type, allocator.allocate(cpp_type.size(), cpp_type.alignment())};
+        cpp_type, allocator.allocate(cpp_type.size, cpp_type.alignment)};
   }
 
   lf::BasicParams lf_params{

source/blender/blenkernel/intern/bake_items.cc
@@ -178,7 +178,7 @@ void VolumeGridBakeItem::count_memory(MemoryCounter &memory) const
 
 PrimitiveBakeItem::PrimitiveBakeItem(const CPPType &type, const void *value) : type_(type)
 {
-  value_ = MEM_mallocN_aligned(type.size(), type.alignment(), __func__);
+  value_ = MEM_mallocN_aligned(type.size, type.alignment, __func__);
   type.copy_construct(value, value_);
 }
 

source/blender/blenkernel/intern/bake_items_serialize.cc
@@ -416,8 +416,8 @@ static std::shared_ptr<DictionaryValue> write_blob_simple_gspan(BlobWriter &blob
                                                                 const GSpan data)
 {
   const CPPType &type = data.type();
-  BLI_assert(type.is_trivial());
-  if (type.size() == 1 || type.is<ColorGeometry4b>()) {
+  BLI_assert(type.is_trivial);
+  if (type.size == 1 || type.is<ColorGeometry4b>()) {
     return write_blob_raw_bytes(blob_writer, blob_sharing, data.data(), data.size_in_bytes());
   }
   return write_blob_raw_data_with_endian(
@@ -429,13 +429,13 @@ static std::shared_ptr<DictionaryValue> write_blob_simple_gspan(BlobWriter &blob
                                                  GMutableSpan r_data)
 {
   const CPPType &type = r_data.type();
-  BLI_assert(type.is_trivial());
-  if (type.size() == 1 || type.is<ColorGeometry4b>()) {
+  BLI_assert(type.is_trivial);
+  if (type.size == 1 || type.is<ColorGeometry4b>()) {
     return read_blob_raw_bytes(blob_reader, io_data, r_data.size_in_bytes(), r_data.data());
   }
   if (type.is_any<int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t, float>()) {
     return read_blob_raw_data_with_endian(
-        blob_reader, io_data, type.size(), r_data.size(), r_data.data());
+        blob_reader, io_data, type.size, r_data.size(), r_data.data());
   }
   if (type.is_any<float2, int2>()) {
     return read_blob_raw_data_with_endian(
@@ -485,7 +485,7 @@ static std::shared_ptr<DictionaryValue> write_blob_shared_simple_gspan(
   const char *func = __func__;
   const std::optional<ImplicitSharingInfoAndData> sharing_info_and_data = blob_sharing.read_shared(
       io_data, [&]() -> std::optional<ImplicitSharingInfoAndData> {
-        void *data_mem = MEM_mallocN_aligned(size * cpp_type.size(), cpp_type.alignment(), func);
+        void *data_mem = MEM_mallocN_aligned(size * cpp_type.size, cpp_type.alignment, func);
         if (!read_blob_simple_gspan(blob_reader, io_data, {cpp_type, data_mem, size})) {
           MEM_freeN(data_mem);
           return std::nullopt;

source/blender/blenkernel/intern/curve_to_mesh_convert.cc
@@ -402,20 +402,20 @@ static GSpan evaluate_attribute(const GVArray &src,
     if (src.is_span()) {
       return src.get_internal_span();
     }
-    buffer.reinitialize(curves.points_num() * src.type().size());
+    buffer.reinitialize(curves.points_num() * src.type().size);
     src.materialize(buffer.data());
     GMutableSpan eval{src.type(), buffer.data(), curves.points_num()};
     return eval;
   }
 
   if (src.is_span()) {
-    buffer.reinitialize(curves.evaluated_points_num() * src.type().size());
+    buffer.reinitialize(curves.evaluated_points_num() * src.type().size);
     GMutableSpan eval{src.type(), buffer.data(), curves.evaluated_points_num()};
     curves.interpolate_to_evaluated(src.get_internal_span(), eval);
     return eval;
   }
   GVArraySpan src_buffer(src);
-  buffer.reinitialize(curves.evaluated_points_num() * src.type().size());
+  buffer.reinitialize(curves.evaluated_points_num() * src.type().size);
   GMutableSpan eval{src.type(), buffer.data(), curves.evaluated_points_num()};
   curves.interpolate_to_evaluated(src_buffer, eval);
   return eval;

source/blender/blenkernel/intern/geometry_fields.cc
@@ -866,7 +866,7 @@ bool try_capture_fields_on_geometry(MutableAttributeAccessor attributes,
 
     /* Could avoid allocating a new buffer if:
      * - The field does not depend on that attribute (we can't easily check for that yet). */
-    void *buffer = MEM_mallocN_aligned(type.size() * domain_size, type.alignment(), __func__);
+    void *buffer = MEM_mallocN_aligned(type.size * domain_size, type.alignment, __func__);
     if (!selection_is_full) {
       const GAttributeReader old_attribute = attributes.lookup_or_default(id, domain, data_type);
       old_attribute.varray.materialize(buffer);

source/blender/blenkernel/intern/geometry_nodes_bundle.cc
@@ -82,7 +82,7 @@ void Bundle::add_new(SocketInterfaceKey key, const bNodeSocketType &type, const 
   BLI_assert(!this->contains(key));
   BLI_assert(type.geometry_nodes_cpp_type);
   const CPPType &cpp_type = *type.geometry_nodes_cpp_type;
-  void *buffer = MEM_mallocN_aligned(cpp_type.size(), cpp_type.alignment(), __func__);
+  void *buffer = MEM_mallocN_aligned(cpp_type.size, cpp_type.alignment, __func__);
   cpp_type.copy_construct(value, buffer);
   items_.append(StoredItem{std::move(key), &type, buffer});
   buffers_.append(buffer);

source/blender/blenlib/BLI_cpp_type.hh
@@ -98,13 +98,61 @@ ENUM_OPERATORS(CPPTypeFlags, CPPTypeFlags::EqualityComparable)
 namespace blender {
 
 class CPPType : NonCopyable, NonMovable {
+ public:
+  /**
+   * Required memory in bytes for an instance of this type.
+   *
+   * C++ equivalent:
+   *   `sizeof(T);`
+   */
+  int64_t size = 0;
+
+  /**
+   * Required memory alignment for an instance of this type.
+   *
+   * C++ equivalent:
+   *   alignof(T);
+   */
+  int64_t alignment = 0;
+
+  /**
+   * When true, the value is like a normal C type, it can be copied around with #memcpy and does
+   * not have to be destructed.
+   *
+   * C++ equivalent:
+   *   std::is_trivial_v<T>;
+   */
+  bool is_trivial = false;
+
+  /**
+   * When true, the destructor does not have to be called on this type. This can sometimes be used
+   * for optimization purposes.
+   *
+   * C++ equivalent:
+   *   std::is_trivially_destructible_v<T>;
+   */
+  bool is_trivially_destructible = false;
+
+  /**
+   * Returns true, when the type has the following functions:
+   * - Default constructor.
+   * - Copy constructor.
+   * - Move constructor.
+   * - Copy assignment operator.
+   * - Move assignment operator.
+   * - Destructor.
+   */
+  bool has_special_member_functions = false;
+
+  bool is_default_constructible = false;
+  bool is_copy_constructible = false;
+  bool is_move_constructible = false;
+  bool is_destructible = false;
+  bool is_copy_assignable = false;
+  bool is_move_assignable = false;
+
  private:
-  int64_t size_ = 0;
-  int64_t alignment_ = 0;
   uintptr_t alignment_mask_ = 0;
-  bool is_trivial_ = false;
-  bool is_trivially_destructible_ = false;
-  bool has_special_member_functions_ = false;
 
   void (*default_construct_)(void *ptr) = nullptr;
   void (*default_construct_indices_)(void *ptr, const IndexMask &mask) = nullptr;
@@ -165,59 +213,9 @@ class CPPType : NonCopyable, NonMovable {
    */
   StringRefNull name() const;
 
-  /**
-   * Required memory in bytes for an instance of this type.
-   *
-   * C++ equivalent:
-   *   `sizeof(T);`
-   */
-  int64_t size() const;
-
-  /**
-   * Required memory alignment for an instance of this type.
-   *
-   * C++ equivalent:
-   *   alignof(T);
-   */
-  int64_t alignment() const;
-
-  /**
-   * When true, the destructor does not have to be called on this type. This can sometimes be used
-   * for optimization purposes.
-   *
-   * C++ equivalent:
-   *   std::is_trivially_destructible_v<T>;
-   */
-  bool is_trivially_destructible() const;
-
-  /**
-   * When true, the value is like a normal C type, it can be copied around with #memcpy and does
-   * not have to be destructed.
-   *
-   * C++ equivalent:
-   *   std::is_trivial_v<T>;
-   */
-  bool is_trivial() const;
-  bool is_default_constructible() const;
-  bool is_copy_constructible() const;
-  bool is_move_constructible() const;
-  bool is_destructible() const;
-  bool is_copy_assignable() const;
-  bool is_move_assignable() const;
   bool is_printable() const;
   bool is_equality_comparable() const;
   bool is_hashable() const;
-
-  /**
-   * Returns true, when the type has the following functions:
-   * - Default constructor.
-   * - Copy constructor.
-   * - Move constructor.
-   * - Copy assignment operator.
-   * - Move assignment operator.
-   * - Destructor.
-   */
-  bool has_special_member_functions() const;
 
   /**
    * Returns true, when the given pointer fulfills the alignment requirement of this type.
@@ -432,8 +430,8 @@ void register_cpp_types();
 
 /* Utility for allocating an uninitialized buffer for a single value of the given #CPPType. */
 #define BUFFER_FOR_CPP_TYPE_VALUE(type, variable_name) \
-  blender::DynamicStackBuffer<64, 64> stack_buffer_for_##variable_name((type).size(), \
-                                                                       (type).alignment()); \
+  blender::DynamicStackBuffer<64, 64> stack_buffer_for_##variable_name((type).size, \
+                                                                       (type).alignment); \
   void *variable_name = stack_buffer_for_##variable_name.buffer();
 
 namespace blender {
@@ -464,56 +462,6 @@ inline StringRefNull CPPType::name() const
   return debug_name_;
 }
 
-inline int64_t CPPType::size() const
-{
-  return size_;
-}
-
-inline int64_t CPPType::alignment() const
-{
-  return alignment_;
-}
-
-inline bool CPPType::is_trivially_destructible() const
-{
-  return is_trivially_destructible_;
-}
-
-inline bool CPPType::is_trivial() const
-{
-  return is_trivial_;
-}
-
-inline bool CPPType::is_default_constructible() const
-{
-  return default_construct_ != nullptr;
-}
-
-inline bool CPPType::is_copy_constructible() const
-{
-  return copy_assign_ != nullptr;
-}
-
-inline bool CPPType::is_move_constructible() const
-{
-  return move_assign_ != nullptr;
-}
-
-inline bool CPPType::is_destructible() const
-{
-  return destruct_ != nullptr;
-}
-
-inline bool CPPType::is_copy_assignable() const
-{
-  return copy_assign_ != nullptr;
-}
-
-inline bool CPPType::is_move_assignable() const
-{
-  return copy_construct_ != nullptr;
-}
-
 inline bool CPPType::is_printable() const
 {
   return print_ != nullptr;
@@ -527,11 +475,6 @@ inline bool CPPType::is_equality_comparable() const
 inline bool CPPType::is_hashable() const
 {
   return hash_ != nullptr;
-}
-
-inline bool CPPType::has_special_member_functions() const
-{
-  return has_special_member_functions_;
 }
 
 inline bool CPPType::pointer_has_valid_alignment(const void *ptr) const
@@ -636,7 +579,7 @@ inline void CPPType::copy_assign_compressed(const void *src,
 
 inline void CPPType::copy_construct(const void *src, void *dst) const
 {
-  BLI_assert(src != dst || is_trivial_);
+  BLI_assert(src != dst || this->is_trivial);
   BLI_assert(this->pointer_can_point_to_instance(src));
   BLI_assert(this->pointer_can_point_to_instance(dst));
 
@@ -694,7 +637,7 @@ inline void CPPType::move_assign_indices(void *src, void *dst, const IndexMask &
 
 inline void CPPType::move_construct(void *src, void *dst) const
 {
-  BLI_assert(src != dst || is_trivial_);
+  BLI_assert(src != dst || this->is_trivial);
   BLI_assert(this->pointer_can_point_to_instance(src));
   BLI_assert(this->pointer_can_point_to_instance(dst));
 
@@ -717,7 +660,7 @@ inline void CPPType::move_construct_indices(void *src, void *dst, const IndexMas
 
 inline void CPPType::relocate_assign(void *src, void *dst) const
 {
-  BLI_assert(src != dst || is_trivial_);
+  BLI_assert(src != dst || this->is_trivial);
   BLI_assert(this->pointer_can_point_to_instance(src));
   BLI_assert(this->pointer_can_point_to_instance(dst));
 
@@ -740,7 +683,7 @@ inline void CPPType::relocate_assign_indices(void *src, void *dst, const IndexMa
 
 inline void CPPType::relocate_construct(void *src, void *dst) const
 {
-  BLI_assert(src != dst || is_trivial_);
+  BLI_assert(src != dst || this->is_trivial);
   BLI_assert(this->pointer_can_point_to_instance(src));
   BLI_assert(this->pointer_can_point_to_instance(dst));
 
@@ -792,7 +735,7 @@ inline void CPPType::fill_construct_indices(const void *value,
 inline bool CPPType::can_exist_in_buffer(const int64_t buffer_size,
                                          const int64_t buffer_alignment) const
 {
-  return size_ <= buffer_size && alignment_ <= buffer_alignment;
+  return this->size <= buffer_size && this->alignment <= buffer_alignment;
 }
 
 inline void CPPType::print(const void *value, std::stringstream &ss) const

source/blender/blenlib/BLI_cpp_type_make.hh
@@ -222,10 +222,10 @@ CPPType::CPPType(TypeTag<T> /*type*/,
   using namespace cpp_type_util;
 
   debug_name_ = debug_name;
-  size_ = int64_t(sizeof(T));
-  alignment_ = int64_t(alignof(T));
-  is_trivial_ = std::is_trivial_v<T>;
-  is_trivially_destructible_ = std::is_trivially_destructible_v<T>;
+  this->size = int64_t(sizeof(T));
+  this->alignment = int64_t(alignof(T));
+  this->is_trivial = std::is_trivial_v<T>;
+  this->is_trivially_destructible = std::is_trivially_destructible_v<T>;
   if constexpr (std::is_default_constructible_v<T>) {
     default_construct_ = default_construct_cb<T>;
     default_construct_indices_ = default_construct_indices_cb<T>;
@@ -324,9 +324,15 @@ CPPType::CPPType(TypeTag<T> /*type*/,
     is_equal_ = is_equal_cb<T>;
   }
 
-  alignment_mask_ = uintptr_t(alignment_) - uintptr_t(1);
-  has_special_member_functions_ = (default_construct_ && copy_construct_ && copy_assign_ &&
-                                   move_construct_ && move_assign_ && destruct_);
+  alignment_mask_ = uintptr_t(this->alignment) - uintptr_t(1);
+  this->has_special_member_functions = (default_construct_ && copy_construct_ && copy_assign_ &&
+                                        move_construct_ && move_assign_ && destruct_);
+  this->is_default_constructible = default_construct_ != nullptr;
+  this->is_copy_constructible = copy_construct_ != nullptr;
+  this->is_move_constructible = move_construct_ != nullptr;
+  this->is_destructible = destruct_ != nullptr;
+  this->is_copy_assignable = copy_assign_ != nullptr;
+  this->is_move_assignable = move_assign_ != nullptr;
 }
 
 }  // namespace blender

commit 2248168dca600fd556f979436930eada12a81bf4

Fix: another merge conflict
Caused by 4d77920293e6c46a5705a944ec94e4ca59e463df.

source/blender/nodes/intern/geometry_nodes_closure_zone.cc
@@ -669,11 +669,11 @@ class LazyFunctionForEvaluateClosureNode : public LazyFunction {
         }
       }
       const CPPType &cpp_type = *output_type.geometry_nodes_cpp_type;
-      void *default_output_value = eval_storage.scope.allocator().allocate(cpp_type.size(),
-                                                                           cpp_type.alignment());
+      void *default_output_value = eval_storage.scope.allocator().allocate(cpp_type.size,
+                                                                           cpp_type.alignment);
       construct_socket_default_value(output_type, default_output_value);
       lf_main_output.set_default_value(default_output_value);
-      if (!cpp_type.is_trivially_destructible()) {
+      if (!cpp_type.is_trivially_destructible) {
         eval_storage.scope.add_destruct_call(
             [default_output_value, type = &cpp_type]() { type->destruct(default_output_value); });
       }