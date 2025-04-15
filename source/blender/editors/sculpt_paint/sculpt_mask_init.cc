Bforartists/Bforartists

commit e18f6513aa0f36261fc5b681041c37e1078e3049

Sculpt: Replace PBVH iteration macro for mask init operator
Part of #118145.
There is more boilerplate now, but hot loops are generally simpler,
and I think we can reuse the structure for other operators that create
masks, similar to `vert_hide_update`.

source/blender/editors/sculpt_paint/sculpt_mask_init.cc
@@ -8,6 +8,8 @@
 
 #include "MEM_guardedalloc.h"
 
+#include "BLI_bit_span_ops.hh"
+#include "BLI_enumerable_thread_specific.hh"
 #include "BLI_hash.h"
 #include "BLI_task.h"
 #include "BLI_time.h"
@@ -17,9 +19,11 @@
 #include "BKE_ccg.hh"
 #include "BKE_context.hh"
 #include "BKE_layer.hh"
+#include "BKE_mesh.hh"
 #include "BKE_multires.hh"
 #include "BKE_paint.hh"
 #include "BKE_pbvh_api.hh"
+#include "BKE_subdiv_ccg.hh"
 
 #include "WM_api.hh"
 #include "WM_types.hh"
@@ -42,78 +46,226 @@ enum class InitMode {
   Island,
 };
 
-static void mask_init_task(Object &ob,
-                           const InitMode mode,
-                           const int seed,
-                           const SculptMaskWriteInfo mask_write,
-                           PBVHNode *node)
+static void init_mask_mesh(Object &object,
+                           const Span<PBVHNode *> nodes,
+                           FunctionRef<void(MutableSpan<float>, Span<int>)> write_fn)
 {
-  SculptSession &ss = *ob.sculpt;
-  PBVHVertexIter vd;
-  undo::push_node(ob, node, undo::Type::Mask);
-  BKE_pbvh_vertex_iter_begin (*ss.pbvh, node, vd, PBVH_ITER_UNIQUE) {
-    float mask;
-    switch (mode) {
-      case InitMode::Random:
-        mask = BLI_hash_int_01(vd.index + seed);
-        break;
-      case InitMode::FaceSet: {
-        const int face_set = face_set::vert_face_set_get(ss, vd.vertex);
-        mask = BLI_hash_int_01(face_set + seed);
-        break;
-      }
-      case InitMode::Island:
-        mask = BLI_hash_int_01(SCULPT_vertex_island_get(ss, vd.vertex) + seed);
-        break;
+  Mesh &mesh = *static_cast<Mesh *>(object.data);
+  bke::MutableAttributeAccessor attributes = mesh.attributes_for_write();
+  const VArraySpan hide_vert = *attributes.lookup<bool>(".hide_vert", bke::AttrDomain::Point);
+  threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+    for (const int i : range) {
+      undo::push_node(object, nodes[i], undo::Type::Mask);
     }
-    SCULPT_mask_vert_set(BKE_pbvh_type(*ss.pbvh), mask_write, mask, vd);
+  });
+  bke::SpanAttributeWriter mask = attributes.lookup_or_add_for_write_span<float>(
+      ".sculpt_mask", bke::AttrDomain::Point);
+  if (!mask) {
+    return;
   }
-  BKE_pbvh_vertex_iter_end;
-  BKE_pbvh_node_mark_update_mask(node);
+  threading::EnumerableThreadSpecific<Vector<int>> all_index_data;
+  threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+    Vector<int> &index_data = all_index_data.local();
+    for (const int i : range) {
+      write_fn(mask.span, hide::node_visible_verts(*nodes[i], hide_vert, index_data));
+      BKE_pbvh_node_mark_update_mask(nodes[i]);
+    }
+  });
+  mask.finish();
+}
+
+template<typename Fn>
+static void foreach_visible_grid_vert(const CCGKey &key,
+                                      const BitGroupVector<> &grid_hidden,
+                                      const int grid,
+                                      const Fn &fn)
+{
+  if (grid_hidden.is_empty()) {
+    for (const int i : IndexRange(key.grid_area)) {
+      fn(i);
+    }
+  }
+  else {
+    bits::foreach_0_index(grid_hidden[grid], fn);
+  }
+}
+
+static void init_mask_grids(Main &bmain,
+                            Scene &scene,
+                            Depsgraph &depsgraph,
+                            Object &object,
+                            const Span<PBVHNode *> nodes,
+                            FunctionRef<void(const BitGroupVector<> &, int, CCGElem *)> write_fn)
+{
+  MultiresModifierData *mmd = BKE_sculpt_multires_active(&scene, &object);
+  BKE_sculpt_mask_layers_ensure(&depsgraph, &bmain, &object, mmd);
+
+  SculptSession &ss = *object.sculpt;
+  SubdivCCG &subdiv_ccg = *ss.subdiv_ccg;
+  const Span<CCGElem *> grids = subdiv_ccg.grids;
+  const BitGroupVector<> &grid_hidden = subdiv_ccg.grid_hidden;
+
+  threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+    for (const int i : range) {
+      undo::push_node(object, nodes[i], undo::Type::Mask);
+      for (const int grid : bke::pbvh::node_grid_indices(*nodes[i])) {
+        write_fn(grid_hidden, grid, grids[grid]);
+      }
+      BKE_pbvh_node_mark_update_mask(nodes[i]);
+    }
+  });
+
+  BKE_subdiv_ccg_average_grids(subdiv_ccg);
 }
 
 static int sculpt_mask_init_exec(bContext *C, wmOperator *op)
 {
-  Object &ob = *CTX_data_active_object(C);
-  SculptSession &ss = *ob.sculpt;
-  Depsgraph *depsgraph = CTX_data_ensure_evaluated_depsgraph(C);
-
   const View3D *v3d = CTX_wm_view3d(C);
   const Base *base = CTX_data_active_base(C);
   if (!BKE_base_is_visible(v3d, base)) {
     return OPERATOR_CANCELLED;
   }
+  Object &ob = *CTX_data_active_object(C);
+  SculptSession &ss = *ob.sculpt;
+  Depsgraph &depsgraph = *CTX_data_ensure_evaluated_depsgraph(C);
 
-  const InitMode mode = InitMode(RNA_enum_get(op->ptr, "mode"));
-
-  MultiresModifierData *mmd = BKE_sculpt_multires_active(CTX_data_scene(C), &ob);
-  BKE_sculpt_mask_layers_ensure(depsgraph, CTX_data_main(C), &ob, mmd);
-
-  BKE_sculpt_update_object_for_edit(depsgraph, &ob, false);
+  BKE_sculpt_update_object_for_edit(&depsgraph, &ob, false);
 
   PBVH &pbvh = *ob.sculpt->pbvh;
   Vector<PBVHNode *> nodes = bke::pbvh::search_gather(pbvh, {});
-
   if (nodes.is_empty()) {
     return OPERATOR_CANCELLED;
   }
 
   undo::push_begin(ob, op);
 
-  if (mode == InitMode::Island) {
-    SCULPT_topology_islands_ensure(ob);
-  }
+  const InitMode mode = InitMode(RNA_enum_get(op->ptr, "mode"));
+  const int seed = BLI_time_now_seconds();
 
-  const int mask_init_seed = BLI_time_now_seconds();
-
-  const SculptMaskWriteInfo mask_write = SCULPT_mask_get_for_write(ss);
-  threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
-    for (const int i : range) {
-      mask_init_task(ob, mode, mask_init_seed, mask_write, nodes[i]);
+  switch (BKE_pbvh_type(pbvh)) {
+    case PBVH_FACES: {
+      switch (mode) {
+        case InitMode::Random:
+          init_mask_mesh(ob, nodes, [&](MutableSpan<float> mask, Span<int> verts) {
+            for (const int vert : verts) {
+              mask[vert] = BLI_hash_int_01(vert + seed);
+            }
+          });
+          break;
+        case InitMode::FaceSet:
+          init_mask_mesh(ob, nodes, [&](MutableSpan<float> mask, Span<int> verts) {
+            for (const int vert : verts) {
+              const int face_set = face_set::vert_face_set_get(ss, PBVHVertRef{vert});
+              mask[vert] = BLI_hash_int_01(face_set + seed);
+            }
+          });
+          break;
+        case InitMode::Island:
+          SCULPT_topology_islands_ensure(ob);
+          init_mask_mesh(ob, nodes, [&](MutableSpan<float> mask, Span<int> verts) {
+            for (const int vert : verts) {
+              const int island = SCULPT_vertex_island_get(ss, PBVHVertRef{vert});
+              mask[vert] = BLI_hash_int_01(island + seed);
+            }
+          });
+          break;
+      }
+      break;
     }
-  });
-
-  multires_stitch_grids(&ob);
+    case PBVH_GRIDS: {
+      Main &bmain = *CTX_data_main(C);
+      Scene &scene = *CTX_data_scene(C);
+      const CCGKey key = *BKE_pbvh_get_grid_key(pbvh);
+      switch (mode) {
+        case InitMode::Random: {
+          init_mask_grids(
+              bmain,
+              scene,
+              depsgraph,
+              ob,
+              nodes,
+              [&](const BitGroupVector<> &grid_hidden, const int grid_index, CCGElem *grid) {
+                const int verts_start = grid_index * key.grid_area;
+                foreach_visible_grid_vert(key, grid_hidden, grid_index, [&](const int i) {
+                  CCG_elem_offset_mask(key, grid, i) = BLI_hash_int_01(verts_start + i + seed);
+                });
+              });
+          break;
+        }
+        case InitMode::FaceSet: {
+          const Mesh &mesh = *static_cast<const Mesh *>(ob.data);
+          const bke::AttributeAccessor attributes = mesh.attributes();
+          const VArraySpan face_sets = *attributes.lookup_or_default<int>(
+              ".sculpt_face_set", bke::AttrDomain::Face, 1);
+          const SubdivCCG &subdiv_ccg = *ss.subdiv_ccg;
+          const Span<int> grid_to_face = subdiv_ccg.grid_to_face_map;
+          init_mask_grids(
+              bmain,
+              scene,
+              depsgraph,
+              ob,
+              nodes,
+              [&](const BitGroupVector<> &grid_hidden, const int grid_index, CCGElem *grid) {
+                const int face_set = face_sets[grid_to_face[grid_index]];
+                const float value = BLI_hash_int_01(face_set + seed);
+                foreach_visible_grid_vert(key, grid_hidden, grid_index, [&](const int i) {
+                  CCG_elem_offset_mask(key, grid, i) = value;
+                });
+              });
+          break;
+        }
+        case InitMode::Island: {
+          SCULPT_topology_islands_ensure(ob);
+          init_mask_grids(
+              bmain,
+              scene,
+              depsgraph,
+              ob,
+              nodes,
+              [&](const BitGroupVector<> &grid_hidden, const int grid_index, CCGElem *grid) {
+                const int verts_start = grid_index * key.grid_area;
+                foreach_visible_grid_vert(key, grid_hidden, grid_index, [&](const int i) {
+                  const int island = SCULPT_vertex_island_get(ss, PBVHVertRef{verts_start + i});
+                  CCG_elem_offset_mask(key, grid, i) = BLI_hash_int_01(island + seed);
+                });
+              });
+          break;
+        }
+      }
+      break;
+    }
+    case PBVH_BMESH: {
+      const int offset = CustomData_get_offset_named(&ss.bm->vdata, CD_PROP_FLOAT, ".sculpt_mask");
+      threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
+        for (const int i : range) {
+          for (BMVert *vert : BKE_pbvh_bmesh_node_unique_verts(nodes[i])) {
+            if (BM_elem_flag_test(vert, BM_ELEM_HIDDEN)) {
+              continue;
+            }
+            switch (mode) {
+              case InitMode::Random:
+                BM_ELEM_CD_SET_FLOAT(
+                    vert, offset, BLI_hash_int_01(BM_elem_index_get(vert) + seed));
+                break;
+              case InitMode::FaceSet: {
+                BM_ELEM_CD_SET_FLOAT(vert, offset, 0.0f);
+                break;
+              }
+              case InitMode::Island:
+                BM_ELEM_CD_SET_FLOAT(
+                    vert,
+                    offset,
+                    BLI_hash_int_01(SCULPT_vertex_island_get(ss, PBVHVertRef{intptr_t(vert)}) +
+                                    seed));
+                break;
+            }
+          }
+          BKE_pbvh_node_mark_update_mask(nodes[i]);
+        }
+      });
+      break;
+    }
+  }
 
   undo::push_end(ob);
 

source/blender/editors/sculpt_paint/paint_hide.cc
@@ -55,6 +55,25 @@ namespace blender::ed::sculpt_paint::hide {
 /** \name Public API
  * \{ */
 
+Span<int> node_visible_verts(const PBVHNode &node,
+                             const Span<bool> hide_vert,
+                             Vector<int> &indices)
+{
+  if (BKE_pbvh_node_fully_hidden_get(&node)) {
+    return {};
+  }
+  const Span<int> verts = bke::pbvh::node_unique_verts(node);
+  if (hide_vert.is_empty()) {
+    return verts;
+  }
+  indices.resize(verts.size());
+  const int *end = std::copy_if(verts.begin(), verts.end(), indices.begin(), [&](const int vert) {
+    return !hide_vert[vert];
+  });
+  indices.resize(end - indices.begin());
+  return indices;
+}
+
 void sync_all_from_faces(Object &object)
 {
   SculptSession &ss = *object.sculpt;

source/blender/editors/sculpt_paint/paint_mask.cc
@@ -135,25 +135,6 @@ static const EnumPropertyItem mode_items[] = {
     {int(FloodFillMode::InverseMeshValue), "INVERT", 0, "Invert", "Invert the mask"},
     {0}};
 
-static Span<int> get_visible_verts(const PBVHNode &node,
-                                   const Span<bool> hide_vert,
-                                   Vector<int> &indices)
-{
-  if (BKE_pbvh_node_fully_hidden_get(&node)) {
-    return {};
-  }
-  const Span<int> verts = bke::pbvh::node_unique_verts(node);
-  if (hide_vert.is_empty()) {
-    return verts;
-  }
-  indices.resize(verts.size());
-  const int *end = std::copy_if(verts.begin(), verts.end(), indices.begin(), [&](const int vert) {
-    return !hide_vert[vert];
-  });
-  indices.resize(end - indices.begin());
-  return indices;
-}
-
 static Span<int> get_hidden_verts(const PBVHNode &node,
                                   const Span<bool> hide_vert,
                                   Vector<int> &indices)
@@ -243,7 +224,7 @@ static void fill_mask_mesh(Object &object, const float value, const Span<PBVHNod
   threading::parallel_for(nodes.index_range(), 1, [&](const IndexRange range) {
     Vector<int> &index_data = all_index_data.local();
     for (PBVHNode *node : nodes.slice(range)) {
-      const Span<int> verts = get_visible_verts(*node, hide_vert, index_data);
+      const Span<int> verts = hide::node_visible_verts(*node, hide_vert, index_data);
       if (std::all_of(verts.begin(), verts.end(), [&](int i) { return mask.span[i] == value; })) {
         continue;
       }

source/blender/editors/sculpt_paint/sculpt_intern.hh
@@ -978,6 +978,8 @@ namespace blender::ed::sculpt_paint {
 
 namespace hide {
 
+Span<int> node_visible_verts(const PBVHNode &node, Span<bool> hide_vert, Vector<int> &indices);
+
 bool vert_visible_get(const SculptSession &ss, PBVHVertRef vertex);
 bool vert_all_faces_visible_get(const SculptSession &ss, PBVHVertRef vertex);
 bool vert_any_face_visible_get(const SculptSession &ss, PBVHVertRef vertex);

commit 1e34910ac5105e37b5a7b52ea2103f9b1dc94c17

Fix #137494: Dyntopo crash after undoing Random Mask
Introduced with e18f6513aa0f36261fc5b681041c37e1078e3049.

Pull Request: https://projects.blender.org/blender/blender/pulls/137495

source/blender/editors/sculpt_paint/sculpt_mask_init.cc
@@ -237,6 +237,7 @@ static wmOperatorStatus sculpt_mask_init_exec(bContext *C, wmOperator *op)
     case bke::pbvh::Type::BMesh: {
       MutableSpan<bke::pbvh::BMeshNode> nodes = pbvh.nodes<bke::pbvh::BMeshNode>();
       const int offset = CustomData_get_offset_named(&ss.bm->vdata, CD_PROP_FLOAT, ".sculpt_mask");
+      undo::push_nodes(depsgraph, ob, node_mask, undo::Type::Mask);
       node_mask.foreach_index(GrainSize(1), [&](const int i) {
         for (BMVert *vert : BKE_pbvh_bmesh_node_unique_verts(&nodes[i])) {
           if (BM_elem_flag_test(vert, BM_ELEM_HIDDEN)) {