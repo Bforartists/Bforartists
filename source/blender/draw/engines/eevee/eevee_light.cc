Bforartists/Bforartists

commit 838af0d84d52f3536acf6ff08ebc2c5d647ec4fc

Fix: EEVEE: Missing update from shading popover properties
These properties were triggering a `view_update` call
but this function callback was removed during the
modernization of the DRW module.

source/blender/draw/engines/eevee/eevee_light.cc
@@ -343,7 +343,10 @@ LightModule::~LightModule()
 
 void LightModule::begin_sync()
 {
-  use_scene_lights_ = inst_.use_scene_lights();
+  if (assign_if_different(use_scene_lights_, inst_.use_scene_lights())) {
+    inst_.sampling.reset();
+  }
+
   /* Disable sunlight if world has a volume shader as we consider the light cannot go through an
    * infinite opaque medium. */
   use_sun_lights_ = (inst_.world.has_volume_absorption() == false);

source/blender/draw/engines/eevee/eevee_world.cc
@@ -143,6 +143,10 @@ void World::sync()
 
   inst_.light_probes.sync_world(bl_world, has_update);
 
+  if (has_update) {
+    inst_.sampling.reset();
+  }
+
   GPUMaterial *gpumat = inst_.shaders.world_shader_get(bl_world, ntree, MAT_PIPE_DEFERRED);
 
   inst_.manager->register_layer_attributes(gpumat);

commit 285971096070343d75d3ec92d11fdfbb554c8b1c

Fix: EEVEE: Assert in render
Caused by 838af0d84d52f3536acf6ff08ebc2c5d647ec4fc

source/blender/draw/engines/eevee/eevee_light.cc
@@ -344,7 +344,10 @@ LightModule::~LightModule()
 void LightModule::begin_sync()
 {
   if (assign_if_different(use_scene_lights_, inst_.use_scene_lights())) {
-    inst_.sampling.reset();
+    if (inst_.is_viewport()) {
+      /* Catch lookdev viewport properties updates. */
+      inst_.sampling.reset();
+    }
   }
 
   /* Disable sunlight if world has a volume shader as we consider the light cannot go through an

source/blender/draw/engines/eevee/eevee_world.cc
@@ -143,7 +143,8 @@ void World::sync()
 
   inst_.light_probes.sync_world(bl_world, has_update);
 
-  if (has_update) {
+  if (inst_.is_viewport() && has_update) {
+    /* Catch lookdev viewport properties updates. */
     inst_.sampling.reset();
   }
 