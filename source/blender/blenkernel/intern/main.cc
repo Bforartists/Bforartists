Bforartists/Bforartists

commit 1e47ac0a0a7cfcd56bec8ab0e8f7b5216ef88006

Refactor: Eliminate various void pointers passed to MEM_freeN
It's safer to pass a type so that it can be checked if delete should be
used instead. Also changes a few void pointer casts to const_cast so that
if the data becomes typed it's an error.

Pull Request: https://projects.blender.org/blender/blender/pulls/137404

source/blender/blenkernel/intern/main.cc
@@ -167,7 +167,7 @@ void BKE_main_destroy(Main &bmain)
   BKE_main_clear(bmain);
 
   BLI_spin_end(reinterpret_cast<SpinLock *>(bmain.lock));
-  MEM_freeN(static_cast<void *>(bmain.lock));
+  MEM_freeN(reinterpret_cast<SpinLock *>(bmain.lock));
   bmain.lock = nullptr;
 }
 

source/blender/blenkernel/intern/sound.cc
@@ -105,7 +105,7 @@ static void sound_free_data(ID *id)
 
   if (sound->spinlock) {
     BLI_spin_end(static_cast<SpinLock *>(sound->spinlock));
-    MEM_freeN(sound->spinlock);
+    MEM_freeN(static_cast<SpinLock *>(sound->spinlock));
     sound->spinlock = nullptr;
   }
 }

source/blender/editors/sculpt_paint/paint_image_proj.cc
@@ -4647,15 +4647,15 @@ static void project_paint_end(ProjPaintState *ps)
     /* must be set for non-shared */
     BLI_assert(ps->poly_to_loop_uv || ps->is_shared_user);
     if (ps->poly_to_loop_uv) {
-      MEM_freeN((void *)ps->poly_to_loop_uv);
+      MEM_freeN(ps->poly_to_loop_uv);
     }
 
     if (ps->do_layer_clone) {
-      MEM_freeN((void *)ps->poly_to_loop_uv_clone);
+      MEM_freeN(ps->poly_to_loop_uv_clone);
     }
     if (ps->thread_tot > 1) {
       BLI_spin_end(ps->tile_lock);
-      MEM_freeN((void *)ps->tile_lock);
+      MEM_freeN(ps->tile_lock);
     }
 
     ED_image_paint_tile_lock_end();

commit 07aa2aae62ae25427c5ec2159d9c41ee5cc33bbb

Fix: building WITH_TBB=OFF
Caused by 1e47ac0a0a7cfcd56bec8ab0e8f7b5216ef88006

source/blender/blenkernel/intern/main.cc
@@ -167,7 +167,8 @@ void BKE_main_destroy(Main &bmain)
   BKE_main_clear(bmain);
 
   BLI_spin_end(reinterpret_cast<SpinLock *>(bmain.lock));
-  MEM_freeN(reinterpret_cast<SpinLock *>(bmain.lock));
+  /* The void cast is needed when building without TBB. */
+  MEM_freeN((void *)reinterpret_cast<SpinLock *>(bmain.lock));
   bmain.lock = nullptr;
 }
 

source/blender/blenkernel/intern/sound.cc
@@ -105,7 +105,8 @@ static void sound_free_data(ID *id)
 
   if (sound->spinlock) {
     BLI_spin_end(static_cast<SpinLock *>(sound->spinlock));
-    MEM_freeN(static_cast<SpinLock *>(sound->spinlock));
+    /* The void cast is needed when building without TBB. */
+    MEM_freeN((void *)static_cast<SpinLock *>(sound->spinlock));
     sound->spinlock = nullptr;
   }
 }

source/blender/editors/sculpt_paint/paint_image_proj.cc
@@ -4655,7 +4655,8 @@ static void project_paint_end(ProjPaintState *ps)
     }
     if (ps->thread_tot > 1) {
       BLI_spin_end(ps->tile_lock);
-      MEM_freeN(ps->tile_lock);
+      /* The void cast is needed when building without TBB. */
+      MEM_freeN((void *)ps->tile_lock);
     }
 
     ED_image_paint_tile_lock_end();