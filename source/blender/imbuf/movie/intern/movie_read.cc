Bforartists/Bforartists

commit 9402f58f773386a1f02db402deb48baca6b3cd68

Refactor: Modify colorspace handling for image buffer reading
The file formats now fill in ImColorSpaceInfo with the metadata colorspace
and a boolean saying if the pixels have HDR colors. And then the actual
colorspace is decided in imb_handle_colorspace_and_alpha.

This centralizes the logic in one place to make it possible to add
OpenColorIO file rules.

Pull Request: https://projects.blender.org/blender/blender/pulls/136516

source/blender/imbuf/movie/intern/movie_read.cc
@@ -108,13 +108,12 @@ MovieReader *MOV_open_file(const char *filepath,
 
   anim = MEM_new<MovieReader>("anim struct");
   if (anim != nullptr) {
+    const char *byte_colorspace = IMB_colormanagement_role_colorspace_name_get(
+        COLOR_ROLE_DEFAULT_BYTE);
+    STRNCPY(anim->colorspace, byte_colorspace);
+
     if (colorspace) {
-      colorspace_set_default_role(colorspace, IM_MAX_SPACE, COLOR_ROLE_DEFAULT_BYTE);
-      STRNCPY(anim->colorspace, colorspace);
-    }
-    else {
-      colorspace_set_default_role(
-          anim->colorspace, sizeof(anim->colorspace), COLOR_ROLE_DEFAULT_BYTE);
+      BLI_strncpy(colorspace, anim->colorspace, IM_MAX_SPACE);
     }
 
     STRNCPY(anim->filepath, filepath);

source/blender/imbuf/intern/IMB_colormanagement_intern.hh
@@ -107,7 +107,5 @@ ColorManagedLook *colormanage_look_add(const char *name, const char *process_spa
 ColorManagedLook *colormanage_look_get_named(const char *name);
 ColorManagedLook *colormanage_look_get_indexed(int index);
 
-void colorspace_set_default_role(char *colorspace, int size, int role);
-
 void colormanage_imbuf_set_default_spaces(ImBuf *ibuf);
 void colormanage_imbuf_make_linear(ImBuf *ibuf, const char *from_colorspace);

source/blender/imbuf/intern/IMB_filetype.hh
@@ -10,11 +10,12 @@
 
 #include "IMB_imbuf.hh"
 
+struct ImBuf;
+struct ImFileColorSpace;
+
 /* -------------------------------------------------------------------- */
 /** \name Generic File Type
  * \{ */
-
-struct ImBuf;
 
 #define IM_FTYPE_FLOAT 1
 
@@ -32,9 +33,9 @@ struct ImFileType {
   bool (*is_a)(const unsigned char *buf, size_t size);
 
   /** Load an image from memory. */
-  ImBuf *(*load)(const unsigned char *mem, size_t size, int flags, char colorspace[IM_MAX_SPACE]);
+  ImBuf *(*load)(const unsigned char *mem, size_t size, int flags, ImFileColorSpace &r_colorspace);
   /** Load an image from a file. */
-  ImBuf *(*load_filepath)(const char *filepath, int flags, char colorspace[IM_MAX_SPACE]);
+  ImBuf *(*load_filepath)(const char *filepath, int flags, ImFileColorSpace &r_colorspace);
   /**
    * Load/Create a thumbnail image from a filepath. `max_thumb_size` is maximum size of either
    * dimension, so can return less on either or both. Should, if possible and performant, return
@@ -43,7 +44,7 @@ struct ImFileType {
   ImBuf *(*load_filepath_thumbnail)(const char *filepath,
                                     int flags,
                                     size_t max_thumb_size,
-                                    char colorspace[IM_MAX_SPACE],
+                                    ImFileColorSpace &r_colorspace,
                                     size_t *r_width,
                                     size_t *r_height);
   /** Save to a file (or memory if #IB_mem is set in `flags` and the format supports it). */
@@ -55,6 +56,14 @@ struct ImFileType {
   int filetype;
 
   int default_save_role;
+};
+
+/* Color space information provided by the file. */
+struct ImFileColorSpace {
+  /* Color space from metadata. */
+  char metadata_colorspace[IM_MAX_SPACE] = "";
+  /* Is image HDR with range potentially outside 0..1? */
+  bool is_hdr_float = false;
 };
 
 extern const ImFileType IMB_FILE_TYPES[];
@@ -78,7 +87,7 @@ bool imb_is_a_png(const unsigned char *mem, size_t size);
 ImBuf *imb_load_png(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
 bool imb_save_png(ImBuf *ibuf, const char *filepath, int flags);
 
 /** \} */
@@ -91,7 +100,7 @@ bool imb_is_a_tga(const unsigned char *mem, size_t size);
 ImBuf *imb_load_tga(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
 bool imb_save_tga(ImBuf *ibuf, const char *filepath, int flags);
 
 /** \} */
@@ -107,7 +116,7 @@ bool imb_is_a_iris(const unsigned char *mem, size_t size);
 ImBuf *imb_loadiris(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
 bool imb_saveiris(ImBuf *ibuf, const char *filepath, int flags);
 
 /** \} */
@@ -120,8 +129,8 @@ bool imb_is_a_jp2(const unsigned char *buf, size_t size);
 ImBuf *imb_load_jp2(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
-ImBuf *imb_load_jp2_filepath(const char *filepath, int flags, char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
+ImBuf *imb_load_jp2_filepath(const char *filepath, int flags, ImFileColorSpace &r_colorspace);
 bool imb_save_jp2(ImBuf *ibuf, const char *filepath, int flags);
 
 /** \} */
@@ -135,11 +144,11 @@ bool imb_savejpeg(ImBuf *ibuf, const char *filepath, int flags);
 ImBuf *imb_load_jpeg(const unsigned char *buffer,
                      size_t size,
                      int flags,
-                     char colorspace[IM_MAX_SPACE]);
+                     ImFileColorSpace &r_colorspace);
 ImBuf *imb_thumbnail_jpeg(const char *filepath,
                           int flags,
                           size_t max_thumb_size,
-                          char colorspace[IM_MAX_SPACE],
+                          ImFileColorSpace &r_colorspace,
                           size_t *r_width,
                           size_t *r_height);
 
@@ -153,7 +162,7 @@ bool imb_is_a_bmp(const unsigned char *mem, size_t size);
 ImBuf *imb_load_bmp(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
 /* Found write info at http://users.ece.gatech.edu/~slabaugh/personal/c/bitmapUnix.c */
 bool imb_save_bmp(ImBuf *ibuf, const char *filepath, int flags);
 
@@ -168,7 +177,7 @@ bool imb_save_cineon(ImBuf *buf, const char *filepath, int flags);
 ImBuf *imb_load_cineon(const unsigned char *mem,
                        size_t size,
                        int flags,
-                       char colorspace[IM_MAX_SPACE]);
+                       ImFileColorSpace &r_colorspace);
 
 /** \} */
 
@@ -181,7 +190,7 @@ bool imb_save_dpx(ImBuf *ibuf, const char *filepath, int flags);
 ImBuf *imb_load_dpx(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
 
 /** \} */
 
@@ -193,7 +202,7 @@ bool imb_is_a_hdr(const unsigned char *mem, size_t size);
 ImBuf *imb_load_hdr(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
 bool imb_save_hdr(ImBuf *ibuf, const char *filepath, int flags);
 
 /** \} */
@@ -215,7 +224,7 @@ bool imb_is_a_tiff(const unsigned char *mem, size_t size);
 ImBuf *imb_load_tiff(const unsigned char *mem,
                      size_t size,
                      int flags,
-                     char colorspace[IM_MAX_SPACE]);
+                     ImFileColorSpace &r_colorspace);
 /**
  * Saves a TIFF file.
  *
@@ -242,11 +251,11 @@ bool imb_is_a_webp(const unsigned char *mem, size_t size);
 ImBuf *imb_loadwebp(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
 ImBuf *imb_load_filepath_thumbnail_webp(const char *filepath,
                                         const int flags,
                                         const size_t max_thumb_size,
-                                        char colorspace[],
+                                        ImFileColorSpace &r_colorspace,
                                         size_t *r_width,
                                         size_t *r_height);
 bool imb_savewebp(ImBuf *ibuf, const char *filepath, int flags);
@@ -264,7 +273,7 @@ bool imb_is_a_dds(const unsigned char *mem, size_t size);
 ImBuf *imb_load_dds(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
 
 /** \} */
 
@@ -277,7 +286,7 @@ bool imb_is_a_psd(const unsigned char *mem, size_t size);
 ImBuf *imb_load_psd(const unsigned char *mem,
                     size_t size,
                     int flags,
-                    char colorspace[IM_MAX_SPACE]);
+                    ImFileColorSpace &r_colorspace);
 
 /** \} */
 
@@ -288,7 +297,7 @@ ImBuf *imb_load_psd(const unsigned char *mem,
 ImBuf *imb_load_filepath_thumbnail_svg(const char *filepath,
                                        const int flags,
                                        const size_t max_thumb_size,
-                                       char colorspace[],
+                                       ImFileColorSpace &r_colorspace,
                                        size_t *r_width,
                                        size_t *r_height);
 

source/blender/imbuf/intern/cineon/cineon_dpx.cc
@@ -20,13 +20,11 @@
 #include "MEM_guardedalloc.h"
 
 static ImBuf *imb_load_dpx_cineon(
-    const uchar *mem, size_t size, int use_cineon, int flags, char colorspace[IM_MAX_SPACE])
+    const uchar *mem, size_t size, int use_cineon, int flags, ImFileColorSpace &r_colorspace)
 {
   ImBuf *ibuf;
   LogImageFile *image;
   int width, height, depth;
-
-  colorspace_set_default_role(colorspace, IM_MAX_SPACE, COLOR_ROLE_DEFAULT_FLOAT);
 
   logImageSetVerbose((G.debug & G_DEBUG) ? 1 : 0);
 
@@ -60,6 +58,8 @@ static ImBuf *imb_load_dpx_cineon(
   if (flags & IB_alphamode_detect) {
     ibuf->flags |= IB_alphamode_premul;
   }
+
+  r_colorspace.is_hdr_float = true;
 
   return ibuf;
 }
@@ -173,10 +173,10 @@ bool imb_is_a_cineon(const uchar *mem, size_t size)
   return logImageIsCineon(mem, size);
 }
 
-ImBuf *imb_load_cineon(const uchar *mem, size_t size, int flags, char colorspace[IM_MAX_SPACE])
+ImBuf *imb_load_cineon(const uchar *mem, size_t size, int flags, ImFileColorSpace &r_colorspace)
 {
   if (!imb_is_a_cineon(mem, size)) {
     return nullptr;
   }
-  return imb_load_dpx_cineon(mem, size, 1, flags, colorspace);
+  return imb_load_dpx_cineon(mem, size, 1, flags, r_colorspace);
 }

source/blender/imbuf/intern/colormanagement.cc
@@ -1110,17 +1110,6 @@ static void curve_mapping_apply_pixel(const CurveMapping *curve_mapping,
   }
 }
 
-void colorspace_set_default_role(char *colorspace, int size, int role)
-{
-  if (colorspace && colorspace[0] == '\0') {
-    const char *role_colorspace;
-
-    role_colorspace = IMB_colormanagement_role_colorspace_name_get(role);
-
-    BLI_strncpy(colorspace, role_colorspace, size);
-  }
-}
-
 void colormanage_imbuf_set_default_spaces(ImBuf *ibuf)
 {
   ibuf->byte_buffer.colorspace = colormanage_colorspace_get_named(global_role_default_byte);

source/blender/imbuf/intern/format_bmp.cc
@@ -19,7 +19,7 @@ bool imb_is_a_bmp(const uchar *mem, size_t size)
   return imb_oiio_check(mem, size, "bmp");
 }
 
-ImBuf *imb_load_bmp(const uchar *mem, size_t size, int flags, char colorspace[IM_MAX_SPACE])
+ImBuf *imb_load_bmp(const uchar *mem, size_t size, int flags, ImFileColorSpace &r_colorspace)
 {
   ImageSpec config, spec;
 
@@ -27,7 +27,7 @@ ImBuf *imb_load_bmp(const uchar *mem, size_t size, int flags, char colorspace[IM
   config.attribute("bmp:monochrome_detect", 0);
 
   ReadContext ctx{mem, size, "bmp", IMB_FTYPE_BMP, flags};
-  return imb_oiio_read(ctx, config, colorspace, spec);
+  return imb_oiio_read(ctx, config, r_colorspace, spec);
 }
 
 bool imb_save_bmp(ImBuf *ibuf, const char *filepath, int flags)

source/blender/imbuf/intern/format_dds.cc
@@ -48,12 +48,12 @@ bool imb_is_a_dds(const uchar *mem, size_t size)
   return imb_oiio_check(mem, size, "dds");
 }
 
-ImBuf *imb_load_dds(const uchar *mem, size_t size, int flags, char colorspace[IM_MAX_SPACE])
+ImBuf *imb_load_dds(const uchar *mem, size_t size, int flags, ImFileColorSpace &r_colorspace)
 {
   ImageSpec config, spec;
   ReadContext ctx{mem, size, "dds", IMB_FTYPE_DDS, flags};
 
-  ImBuf *ibuf = imb_oiio_read(ctx, config, colorspace, spec);
+  ImBuf *ibuf = imb_oiio_read(ctx, config, r_colorspace, spec);
 
   /* Load compressed DDS information if available. */
   if (ibuf && (flags & IB_test) == 0) {

source/blender/imbuf/intern/format_dpx.cc
@@ -20,20 +20,20 @@ bool imb_is_a_dpx(const uchar *mem, size_t size)
   return imb_oiio_check(mem, size, "dpx");
 }
 
-ImBuf *imb_load_dpx(const uchar *mem, size_t size, int flags, char colorspace[IM_MAX_SPACE])
+ImBuf *imb_load_dpx(const uchar *mem, size_t size, int flags, ImFileColorSpace &r_colorspace)
 {
   ImageSpec config, spec;
 
   ReadContext ctx{mem, size, "dpx", IMB_FTYPE_DPX, flags};
 
-  ctx.use_colorspace_role = COLOR_ROLE_DEFAULT_FLOAT;
-
-  ImBuf *ibuf = imb_oiio_read(ctx, config, colorspace, spec);
+  ImBuf *ibuf = imb_oiio_read(ctx, config, r_colorspace, spec);
   if (ibuf) {
     if (flags & IB_alphamode_detect) {
       ibuf->flags |= IB_alphamode_premul;
     }
   }
+
+  r_colorspace.is_hdr_float = true;
 
   return ibuf;
 }

commit 1d003e892ad323e19270bd62aa11b2eb90c768ef

Fix #138262: Movie file colorspace can't be changed
Regression from recent 9402f58f77338, restore previous logic and add
comments.

source/blender/imbuf/movie/intern/movie_read.cc
@@ -108,13 +108,15 @@ MovieReader *MOV_open_file(const char *filepath,
 
   anim = MEM_new<MovieReader>("anim struct");
   if (anim != nullptr) {
-    const char *byte_colorspace = IMB_colormanagement_role_colorspace_name_get(
+    /* Initialize colorspace to default if not yet set. */
+    const char *default_colorspace = IMB_colormanagement_role_colorspace_name_get(
         COLOR_ROLE_DEFAULT_BYTE);
-    STRNCPY(anim->colorspace, byte_colorspace);
-
-    if (colorspace) {
-      BLI_strncpy(colorspace, anim->colorspace, IM_MAX_SPACE);
+    if (colorspace && colorspace[0] == '\0') {
+      BLI_strncpy(colorspace, default_colorspace, IM_MAX_SPACE);
     }
+
+    /* Inherit colorspace from argument if provided. */
+    STRNCPY(anim->colorspace, colorspace ? colorspace : default_colorspace);
 
     STRNCPY(anim->filepath, filepath);
     anim->ib_flags = ib_flags;