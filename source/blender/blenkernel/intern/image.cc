Bforartists/Bforartists

commit a25d89626f4a5c5b0bf3afd73ea4f87f7dd3f179

Cleanup: Use blender::Mutex for image pool

source/blender/blenkernel/intern/image.cc
@@ -4971,25 +4971,22 @@ struct ImagePoolItem {
 };
 
 struct ImagePool {
-  ListBase image_buffers;
-  BLI_mempool *memory_pool;
-  ThreadMutex mutex;
+  ListBase image_buffers = {};
+  BLI_mempool *memory_pool = nullptr;
+  blender::Mutex mutex;
 };
 
 ImagePool *BKE_image_pool_new()
 {
-  ImagePool *pool = MEM_callocN<ImagePool>("Image Pool");
+  ImagePool *pool = MEM_new<ImagePool>(__func__);
   pool->memory_pool = BLI_mempool_create(sizeof(ImagePoolItem), 0, 128, BLI_MEMPOOL_NOP);
-
-  BLI_mutex_init(&pool->mutex);
-
   return pool;
 }
 
 void BKE_image_pool_free(ImagePool *pool)
 {
   /* Use single lock to dereference all the image buffers. */
-  BLI_mutex_lock(&pool->mutex);
+  std::scoped_lock lock(pool->mutex);
   for (ImagePoolItem *item = static_cast<ImagePoolItem *>(pool->image_buffers.first);
        item != nullptr;
        item = item->next)
@@ -4999,13 +4996,10 @@ void BKE_image_pool_free(ImagePool *pool)
       IMB_freeImBuf(item->ibuf);
     }
   }
-  BLI_mutex_unlock(&pool->mutex);
 
   BLI_mempool_destroy(pool->memory_pool);
 
-  BLI_mutex_end(&pool->mutex);
-
-  MEM_freeN(pool);
+  MEM_delete(pool);
 }
 
 BLI_INLINE ImBuf *image_pool_find_item(
@@ -5048,7 +5042,7 @@ ImBuf *BKE_image_pool_acquire_ibuf(Image *ima, ImageUser *iuser, ImagePool *pool
   }
 
   /* Lock the pool, to allow thread-safe modification of the content of the pool. */
-  BLI_mutex_lock(&pool->mutex);
+  std::scoped_lock lock(pool->mutex);
 
   ibuf = image_pool_find_item(pool, ima, entry, index, &found);
 
@@ -5070,8 +5064,6 @@ ImBuf *BKE_image_pool_acquire_ibuf(Image *ima, ImageUser *iuser, ImagePool *pool
 
     BLI_addtail(&pool->image_buffers, item);
   }
-
-  BLI_mutex_unlock(&pool->mutex);
 
   return ibuf;
 }

commit 7a9f9f17434ddce8d0d8a201f09894c9df0b792a

Fix: Use after free after recent image pool lock cleanup
Caused by a25d89626f4a5c5b0bf3afd73ea4f87f7dd3f179.
We need to release the lock before freeing it.

source/blender/blenkernel/intern/image.cc
@@ -4986,19 +4986,20 @@ ImagePool *BKE_image_pool_new()
 void BKE_image_pool_free(ImagePool *pool)
 {
   /* Use single lock to dereference all the image buffers. */
-  std::scoped_lock lock(pool->mutex);
-  for (ImagePoolItem *item = static_cast<ImagePoolItem *>(pool->image_buffers.first);
-       item != nullptr;
-       item = item->next)
   {
-    if (item->ibuf != nullptr) {
-      std::scoped_lock lock(item->image->runtime->cache_mutex);
-      IMB_freeImBuf(item->ibuf);
+    std::scoped_lock lock(pool->mutex);
+    for (ImagePoolItem *item = static_cast<ImagePoolItem *>(pool->image_buffers.first);
+         item != nullptr;
+         item = item->next)
+    {
+      if (item->ibuf != nullptr) {
+        std::scoped_lock lock(item->image->runtime->cache_mutex);
+        IMB_freeImBuf(item->ibuf);
+      }
     }
+
+    BLI_mempool_destroy(pool->memory_pool);
   }
-
-  BLI_mempool_destroy(pool->memory_pool);
-
   MEM_delete(pool);
 }
 