Bforartists/Bforartists

commit e5879e9e652cd403986bd8b8dc71504048a479b1

Fix #135370: Crash when baking textures with a modified UV map
The report was storing a non-2D-Vector attribute with geometry nodes
over a valid UVMap, resulting in the evaluated mesh not having a valid
`CD_PROP_FLOAT2` layer anymore. There is already a check to early out
for the original mesh in case it has no UVs, but as mentioned, not for
the **evaluated** (cage/multi-no-res) mesh which is actually/rightfully
used to get the UVs.

To resolve, check again if we have UVs right after getting he mesh that
is actually used.
(we might even want to remove the first check, seems redundant now -
even though it would early out a bit sooner...)

Pull Request: https://projects.blender.org/blender/blender/pulls/135388

source/blender/editors/object/object_bake_api.cc
@@ -1518,6 +1518,13 @@ static int bake(const BakeAPIRender *bkr,
   pixel_array_low = static_cast<BakePixel *>(
       MEM_mallocN(sizeof(BakePixel) * targets.pixels_num, "bake pixels low poly"));
   if ((bkr->is_selected_to_active && (ob_cage == nullptr) && bkr->is_cage) == false) {
+    if (!CustomData_has_layer(&me_low_eval->corner_data, CD_PROP_FLOAT2)) {
+      BKE_reportf(reports,
+                  RPT_ERROR,
+                  "No UV map found in the evaluated object \"%s\"",
+                  ob_low->id.name + 2);
+      goto cleanup;
+    }
     bake_targets_populate_pixels(bkr, &targets, ob_low, me_low_eval, pixel_array_low);
   }
 
@@ -1570,6 +1577,13 @@ static int bake(const BakeAPIRender *bkr,
       }
 
       me_cage_eval = BKE_mesh_new_from_object(nullptr, ob_low_eval, false, preserve_origindex);
+      if (!CustomData_has_layer(&me_cage_eval->corner_data, CD_PROP_FLOAT2)) {
+        BKE_reportf(reports,
+                    RPT_ERROR,
+                    "No UV map found in the evaluated object \"%s\"",
+                    ob_low->id.name + 2);
+        goto cleanup;
+      }
       bake_targets_populate_pixels(bkr, &targets, ob_low, me_cage_eval, pixel_array_low);
     }
 
@@ -1752,6 +1766,14 @@ static int bake(const BakeAPIRender *bkr,
 
             /* Evaluate modifiers again. */
             me_nores = BKE_mesh_new_from_object(nullptr, ob_low_eval, false, false);
+            if (!CustomData_has_layer(&me_nores->corner_data, CD_PROP_FLOAT2)) {
+              BKE_reportf(reports,
+                          RPT_ERROR,
+                          "No UV map found in the evaluated object \"%s\"",
+                          ob_low->id.name + 2);
+              BKE_id_free(nullptr, &me_nores->id);
+              goto cleanup;
+            }
             bake_targets_populate_pixels(bkr, &targets, ob_low, me_nores, pixel_array_low);
           }
 

commit d5aa6348d4f92f841a31c3294600e1199d1dd18f

Fix #138537: Baking to Color Attribute not possible without UV Map
Caused by e5879e9e65

Should only check UV maps if we are actually baking to textures
(R_BAKE_TARGET_IMAGE_TEXTURES).

Pull Request: https://projects.blender.org/blender/blender/pulls/138542

source/blender/editors/object/object_bake_api.cc
@@ -1432,6 +1432,7 @@ static wmOperatorStatus bake(const BakeAPIRender *bkr,
   BakeTargets targets = {nullptr};
 
   const bool preserve_origindex = (bkr->target == R_BAKE_TARGET_VERTEX_COLORS);
+  const bool check_valid_uv_map = (bkr->target == R_BAKE_TARGET_IMAGE_TEXTURES);
 
   RE_bake_engine_set_engine_parameters(re, bmain, scene);
 
@@ -1516,7 +1517,7 @@ static wmOperatorStatus bake(const BakeAPIRender *bkr,
    * it is populated later with the cage mesh (smoothed version of the mesh). */
   pixel_array_low = MEM_malloc_arrayN<BakePixel>(targets.pixels_num, "bake pixels low poly");
   if ((bkr->is_selected_to_active && (ob_cage == nullptr) && bkr->is_cage) == false) {
-    if (!CustomData_has_layer(&me_low_eval->corner_data, CD_PROP_FLOAT2)) {
+    if (check_valid_uv_map && !CustomData_has_layer(&me_low_eval->corner_data, CD_PROP_FLOAT2)) {
       BKE_reportf(reports,
                   RPT_ERROR,
                   "No UV map found in the evaluated object \"%s\"",
@@ -1576,7 +1577,8 @@ static wmOperatorStatus bake(const BakeAPIRender *bkr,
 
       me_cage_eval = BKE_mesh_new_from_object(
           nullptr, ob_low_eval, false, preserve_origindex, true);
-      if (!CustomData_has_layer(&me_cage_eval->corner_data, CD_PROP_FLOAT2)) {
+      if (check_valid_uv_map && !CustomData_has_layer(&me_cage_eval->corner_data, CD_PROP_FLOAT2))
+      {
         BKE_reportf(reports,
                     RPT_ERROR,
                     "No UV map found in the evaluated object \"%s\"",
@@ -1763,7 +1765,9 @@ static wmOperatorStatus bake(const BakeAPIRender *bkr,
 
             /* Evaluate modifiers again. */
             me_nores = BKE_mesh_new_from_object(nullptr, ob_low_eval, false, false, true);
-            if (!CustomData_has_layer(&me_nores->corner_data, CD_PROP_FLOAT2)) {
+            if (check_valid_uv_map &&
+                !CustomData_has_layer(&me_nores->corner_data, CD_PROP_FLOAT2))
+            {
               BKE_reportf(reports,
                           RPT_ERROR,
                           "No UV map found in the evaluated object \"%s\"",