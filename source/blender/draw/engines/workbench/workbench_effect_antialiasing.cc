Bforartists/Bforartists

commit ee56ed2fb3ee0b3e9ce6d925c57e844edfe21884

Fix: Workbench: Static Viewport Animation Render
Fix viewport animation renders where there has been no scene updates
between frames.

source/blender/draw/engines/workbench/workbench_effect_antialiasing.cc
@@ -301,7 +301,7 @@ void AntiAliasingPass::draw(const DRWContext *draw_ctx,
                         GPU_RG8,
                         GPU_TEXTURE_USAGE_SHADER_READ | GPU_TEXTURE_USAGE_ATTACHMENT);
 
-  if (!draw_ctx->is_image_render() || last_sample) {
+  if (!draw_ctx->is_image_render() || taa_finished) {
     /* After a certain point SMAA is no longer necessary. */
     if (smaa_mix_factor_ > 0.0f) {
       smaa_edge_fb_.ensure(GPU_ATTACHMENT_NONE, GPU_ATTACHMENT_TEXTURE(smaa_edge_tx_));

source/blender/draw/engines/workbench/workbench_engine.cc
@@ -528,6 +528,13 @@ class Instance : public DrawEngine {
                          GPUTexture *color_tx,
                          RenderEngine *engine = nullptr)
   {
+    if (scene_state_.render_finished) {
+      /* This can happen in viewport animation renders, if the scene didn't have any updates
+       * between frames. */
+      this->draw(manager, depth_tx, depth_in_front_tx, color_tx);
+      return;
+    }
+
     BLI_assert(scene_state_.sample == 0);
     for (auto i : IndexRange(scene_state_.samples_len)) {
       if (engine && RE_engine_test_break(engine)) {

commit 0dd27c62a7496facedd0c57e0d742883af887430

Fix: Workbench: Image Render AA
Regression from ee56ed2fb3.
taa_finished is only true after the last sample.

source/blender/draw/engines/workbench/workbench_effect_antialiasing.cc
@@ -301,7 +301,7 @@ void AntiAliasingPass::draw(const DRWContext *draw_ctx,
                         GPU_RG8,
                         GPU_TEXTURE_USAGE_SHADER_READ | GPU_TEXTURE_USAGE_ATTACHMENT);
 
-  if (!draw_ctx->is_image_render() || taa_finished) {
+  if (!draw_ctx->is_image_render() || last_sample || taa_finished) {
     /* After a certain point SMAA is no longer necessary. */
     if (smaa_mix_factor_ > 0.0f) {
       smaa_edge_fb_.ensure(GPU_ATTACHMENT_NONE, GPU_ATTACHMENT_TEXTURE(smaa_edge_tx_));