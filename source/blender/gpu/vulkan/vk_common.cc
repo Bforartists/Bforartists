Bforartists/Bforartists

commit eacf949cf57e23de2a81017ea81745c5c4d41096

GPU: Remove int float fetch mode combination
This commit finishes removing the uses of the integer to float
vertex buffer fetch mode. Previous commits noted below already started
that process. The last usage was geometry attributes. Now integers are
converted to floats as part of the existing upload process.

The change makes the Vulkan vertex buffer type conversion unused, so
it's removed. That's nice because Vulkan vertex buffers go from 1040 to
568 bytes in size and have significantly less overhead on creation.

Related:
- 59986fa757e3f062122a1e06e434def2cb6f6f34
- 36a47b2829df819a2e24dc349a6eee81f096f7d9
- e717ed3baf1d92e8d532aa2bb8fc504d70d17f96

Pull Request: https://projects.blender.org/blender/blender/pulls/138873

source/blender/gpu/vulkan/vk_common.cc
@@ -359,112 +359,6 @@ static VkFormat to_vk_format_norm(const GPUVertCompType type, const uint32_t siz
   return VK_FORMAT_R32_SFLOAT;
 }
 
-static VkFormat to_vk_format_float(const GPUVertCompType type, const uint32_t size)
-{
-  switch (type) {
-    case GPU_COMP_I8:
-      switch (size) {
-        case 1:
-          return VK_FORMAT_R8_SSCALED;
-        case 2:
-          return VK_FORMAT_R8G8_SSCALED;
-        case 3:
-          return VK_FORMAT_R8G8B8_SSCALED;
-        case 4:
-          return VK_FORMAT_R8G8B8A8_SSCALED;
-        default:
-          BLI_assert_unreachable();
-          return VK_FORMAT_R8_SSCALED;
-      }
-    case GPU_COMP_U8:
-      switch (size) {
-        case 1:
-          return VK_FORMAT_R8_USCALED;
-        case 2:
-          return VK_FORMAT_R8G8_USCALED;
-        case 3:
-          return VK_FORMAT_R8G8B8_USCALED;
-        case 4:
-          return VK_FORMAT_R8G8B8A8_USCALED;
-        default:
-          BLI_assert_unreachable();
-          return VK_FORMAT_R8_USCALED;
-      }
-    case GPU_COMP_I16:
-      switch (size) {
-        case 2:
-          return VK_FORMAT_R16_SSCALED;
-        case 4:
-          return VK_FORMAT_R16G16_SSCALED;
-        case 6:
-          return VK_FORMAT_R16G16B16_SSCALED;
-        case 8:
-          return VK_FORMAT_R16G16B16A16_SSCALED;
-        default:
-          BLI_assert_unreachable();
-          return VK_FORMAT_R16_SSCALED;
-      }
-    case GPU_COMP_U16:
-      switch (size) {
-        case 2:
-          return VK_FORMAT_R16_USCALED;
-        case 4:
-          return VK_FORMAT_R16G16_USCALED;
-        case 6:
-          return VK_FORMAT_R16G16B16_USCALED;
-        case 8:
-          return VK_FORMAT_R16G16B16A16_USCALED;
-        default:
-          BLI_assert_unreachable();
-          return VK_FORMAT_R16_USCALED;
-      }
-
-    case GPU_COMP_I32:
-    case GPU_COMP_U32:
-      /* NOTE: GPU_COMP_I32/U32 using GPU_FETCH_INT_TO_FLOAT isn't natively supported. These
-       * are converted on host-side to signed floats. */
-      switch (size) {
-        case 4:
-          return VK_FORMAT_R32_SFLOAT;
-        case 8:
-          return VK_FORMAT_R32G32_SFLOAT;
-        case 12:
-          return VK_FORMAT_R32G32B32_SFLOAT;
-        case 16:
-          return VK_FORMAT_R32G32B32A32_SFLOAT;
-        default:
-          BLI_assert_unreachable();
-          return VK_FORMAT_R32_SFLOAT;
-      }
-
-    case GPU_COMP_F32:
-      switch (size) {
-        case 4:
-          return VK_FORMAT_R32_SFLOAT;
-        case 8:
-          return VK_FORMAT_R32G32_SFLOAT;
-        case 12:
-          return VK_FORMAT_R32G32B32_SFLOAT;
-        case 16:
-          return VK_FORMAT_R32G32B32A32_SFLOAT;
-        case 64:
-          return VK_FORMAT_R32G32B32A32_SFLOAT;
-        default:
-          BLI_assert_unreachable();
-          return VK_FORMAT_R32_SFLOAT;
-      }
-
-    case GPU_COMP_I10:
-      BLI_assert(size == 4);
-      return VK_FORMAT_A2B10G10R10_SSCALED_PACK32;
-
-    default:
-      break;
-  }
-  BLI_assert_unreachable();
-  return VK_FORMAT_R32_SFLOAT;
-}
-
 static VkFormat to_vk_format_int(const GPUVertCompType type, const uint32_t size)
 {
   switch (type) {
@@ -596,9 +490,6 @@ VkFormat to_vk_format(const GPUVertCompType type, const uint32_t size, GPUVertFe
 {
   switch (fetch_mode) {
     case GPU_FETCH_FLOAT:
-    case GPU_FETCH_INT_TO_FLOAT:
-      return to_vk_format_float(type, size);
-      break;
     case GPU_FETCH_INT:
       return to_vk_format_int(type, size);
       break;

source/blender/draw/intern/attribute_convert.hh
@@ -37,43 +37,43 @@ template<typename T> struct AttributeConverter {
 };
 
 template<> struct AttributeConverter<bool> {
-  using VBOType = VecBase<int, COMPONENT_LEN_SCALAR>;
-  static constexpr GPUVertCompType gpu_component_type = GPU_COMP_I32;
+  using VBOType = VecBase<float, COMPONENT_LEN_SCALAR>;
+  static constexpr GPUVertCompType gpu_component_type = GPU_COMP_F32;
   static constexpr int gpu_component_len = COMPONENT_LEN_SCALAR;
-  static constexpr GPUVertFetchMode gpu_fetch_mode = GPU_FETCH_INT_TO_FLOAT;
+  static constexpr GPUVertFetchMode gpu_fetch_mode = GPU_FETCH_FLOAT;
   static VBOType convert(const bool &value)
   {
     return VBOType(value);
   }
 };
 template<> struct AttributeConverter<int8_t> {
-  using VBOType = VecBase<int, COMPONENT_LEN_SCALAR>;
-  static constexpr GPUVertCompType gpu_component_type = GPU_COMP_I32;
+  using VBOType = VecBase<float, COMPONENT_LEN_SCALAR>;
+  static constexpr GPUVertCompType gpu_component_type = GPU_COMP_F32;
   static constexpr int gpu_component_len = COMPONENT_LEN_SCALAR;
-  static constexpr GPUVertFetchMode gpu_fetch_mode = GPU_FETCH_INT_TO_FLOAT;
+  static constexpr GPUVertFetchMode gpu_fetch_mode = GPU_FETCH_FLOAT;
   static VBOType convert(const int8_t &value)
   {
-    return VecBase<int, COMPONENT_LEN_SCALAR>(value);
+    return VecBase<float, COMPONENT_LEN_SCALAR>(value);
   }
 };
 template<> struct AttributeConverter<int> {
-  using VBOType = VecBase<int, COMPONENT_LEN_SCALAR>;
-  static constexpr GPUVertCompType gpu_component_type = GPU_COMP_I32;
+  using VBOType = VecBase<float, COMPONENT_LEN_SCALAR>;
+  static constexpr GPUVertCompType gpu_component_type = GPU_COMP_F32;
   static constexpr int gpu_component_len = COMPONENT_LEN_SCALAR;
-  static constexpr GPUVertFetchMode gpu_fetch_mode = GPU_FETCH_INT_TO_FLOAT;
+  static constexpr GPUVertFetchMode gpu_fetch_mode = GPU_FETCH_FLOAT;
   static VBOType convert(const int &value)
   {
-    return int3(value);
+    return VecBase<float, COMPONENT_LEN_SCALAR>(value);
   }
 };
 template<> struct AttributeConverter<int2> {
-  using VBOType = int2;
-  static constexpr GPUVertCompType gpu_component_type = GPU_COMP_I32;
+  using VBOType = float2;
+  static constexpr GPUVertCompType gpu_component_type = GPU_COMP_F32;
   static constexpr int gpu_component_len = 2;
-  static constexpr GPUVertFetchMode gpu_fetch_mode = GPU_FETCH_INT_TO_FLOAT;
+  static constexpr GPUVertFetchMode gpu_fetch_mode = GPU_FETCH_FLOAT;
   static VBOType convert(const int2 &value)
   {
-    return int2(value.x, value.y);
+    return float2(value.x, value.y);
   }
 };
 template<> struct AttributeConverter<float> {

source/blender/gpu/GPU_vertex_format.hh
@@ -113,7 +113,6 @@ enum GPUVertFetchMode {
   GPU_FETCH_FLOAT = 0,
   GPU_FETCH_INT,
   GPU_FETCH_INT_TO_FLOAT_UNIT, /* 127 (ubyte) -> 0.5 (and so on for other int types) */
-  GPU_FETCH_INT_TO_FLOAT,      /* 127 (any int type) -> 127.0 */
   /* Warning! adjust GPUVertAttr if changing. */
 };
 

source/blender/gpu/intern/gpu_texture_private.hh
@@ -1146,8 +1146,6 @@ static inline eGPUTextureFormat to_texture_format(const GPUVertFormat *format)
               return GPU_RGBA16UI;
             case GPU_FETCH_INT_TO_FLOAT_UNIT:
               return GPU_RGBA16;
-            case GPU_FETCH_INT_TO_FLOAT:
-              return GPU_RGBA16F;
             case GPU_FETCH_FLOAT:
               return GPU_RGBA16F;
           }

source/blender/gpu/intern/gpu_vertex_format.cc
@@ -136,18 +136,6 @@ static VertAttrType vertex_format_combine(GPUVertCompType component_type,
               return VertAttrType::SINT_32_32_32_32;
           }
           break;
-        case GPU_FETCH_INT_TO_FLOAT:
-          switch (component_len) {
-            case 1:
-              return VertAttrType::SINT_TO_FLT_32;
-            case 2:
-              return VertAttrType::SINT_TO_FLT_32_32;
-            case 3:
-              return VertAttrType::SINT_TO_FLT_32_32_32;
-            case 4:
-              return VertAttrType::SINT_TO_FLT_32_32_32_32;
-          }
-          break;
         default:
           break;
       }

source/blender/gpu/metal/mtl_shader.hh
@@ -421,9 +421,6 @@ inline MTLVertexFormat to_mtl(GPUVertCompType component_type,
     case GPU_FETCH_FLOAT: \
       BLI_assert_msg(0, "Invalid fetch mode for integer attribute"); \
       break; \
-    case GPU_FETCH_INT_TO_FLOAT: \
-      /* Fallback to manual conversion */ \
-      break; \
   } \
   break;
 
@@ -435,7 +432,6 @@ inline MTLVertexFormat to_mtl(GPUVertCompType component_type,
       BLI_assert_msg(0, "Invalid fetch mode for integer attribute"); \
       break; \
     case GPU_FETCH_INT_TO_FLOAT_UNIT: \
-    case GPU_FETCH_INT_TO_FLOAT: \
       /* Fallback to manual conversion */ \
       break; \
   } \
@@ -461,7 +457,6 @@ inline MTLVertexFormat to_mtl(GPUVertCompType component_type,
           break;
         case GPU_FETCH_INT:
         case GPU_FETCH_INT_TO_FLOAT_UNIT:
-        case GPU_FETCH_INT_TO_FLOAT:
           BLI_assert_msg(0, "Invalid fetch mode for float attribute");
           break;
       }
@@ -471,7 +466,6 @@ inline MTLVertexFormat to_mtl(GPUVertCompType component_type,
           return MTLVertexFormatInt1010102Normalized;
         case GPU_FETCH_FLOAT:
         case GPU_FETCH_INT:
-        case GPU_FETCH_INT_TO_FLOAT:
           BLI_assert_msg(0, "Invalid fetch mode for compressed attribute");
           break;
       }

source/blender/gpu/metal/mtl_shader.mm
@@ -992,9 +992,7 @@ MTLRenderPipelineStateInstance *MTLShader::bake_pipeline_state(
         [values setConstantValue:&MTL_attribute_conversion_mode
                             type:MTLDataTypeInt
                         withName:[NSString stringWithFormat:@"MTL_AttributeConvert%d", i]];
-        if (MTL_attribute_conversion_mode == GPU_FETCH_INT_TO_FLOAT_UNIT ||
-            MTL_attribute_conversion_mode == GPU_FETCH_INT_TO_FLOAT)
-        {
+        if (MTL_attribute_conversion_mode == GPU_FETCH_INT_TO_FLOAT_UNIT) {
           shader_debug_printf(
               "TODO(Metal): Shader %s needs to support internal format conversion\n",
               mtl_interface->get_name());

source/blender/gpu/opengl/gl_vertex_array.cc
@@ -77,9 +77,6 @@ static uint16_t vbo_bind(const ShaderInterface *interface,
 
         switch (a->fetch_mode) {
           case GPU_FETCH_FLOAT:
-          case GPU_FETCH_INT_TO_FLOAT:
-            glVertexAttribPointer(input->location, a->comp_len, type, GL_FALSE, stride, pointer);
-            break;
           case GPU_FETCH_INT_TO_FLOAT_UNIT:
             glVertexAttribPointer(input->location, a->comp_len, type, GL_TRUE, stride, pointer);
             break;

commit 600cef46a0c759a86b9c4477f174909b4679e54b

Fix: Vulkan: Incorrect vertex fetch mode
Issue introduced by eacf949cf57e23de2a81017ea81745c5c4d41096
When reading floats as floats they were actually read as ints.

Pull Request: https://projects.blender.org/blender/blender/pulls/138969

source/blender/gpu/vulkan/vk_common.cc
@@ -359,6 +359,33 @@ static VkFormat to_vk_format_norm(const GPUVertCompType type, const uint32_t siz
   return VK_FORMAT_R32_SFLOAT;
 }
 
+static VkFormat to_vk_format_float(const GPUVertCompType type, const uint32_t size)
+{
+  switch (type) {
+    case GPU_COMP_F32:
+      switch (size) {
+        case 4:
+          return VK_FORMAT_R32_SFLOAT;
+        case 8:
+          return VK_FORMAT_R32G32_SFLOAT;
+        case 12:
+          return VK_FORMAT_R32G32B32_SFLOAT;
+        case 16:
+          return VK_FORMAT_R32G32B32A32_SFLOAT;
+        case 64:
+          return VK_FORMAT_R32G32B32A32_SFLOAT;
+        default:
+          BLI_assert_unreachable();
+          return VK_FORMAT_R32_SFLOAT;
+      }
+
+    default:
+      break;
+  }
+  BLI_assert_unreachable();
+  return VK_FORMAT_R32_SFLOAT;
+}
+
 static VkFormat to_vk_format_int(const GPUVertCompType type, const uint32_t size)
 {
   switch (type) {
@@ -490,6 +517,7 @@ VkFormat to_vk_format(const GPUVertCompType type, const uint32_t size, GPUVertFe
 {
   switch (fetch_mode) {
     case GPU_FETCH_FLOAT:
+      return to_vk_format_float(type, size);
     case GPU_FETCH_INT:
       return to_vk_format_int(type, size);
       break;