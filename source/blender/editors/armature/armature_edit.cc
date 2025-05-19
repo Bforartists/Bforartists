Bforartists/Bforartists

commit 6c36481d475713b32be728f30a4373e66900402d

Refactor: Move bone functions to separate file and into namespace
No functional changes intended.

This moves the functions
* ANIM_bone_is_visible
* ANIM_bone_is_visible_ebone
* ANIM_bone_is_visible_pchan

into new files `ANIM_armature.hh`/`armature.cc`.
They were previously in `ANIM_bone_collections.hh` but don't
directly have anything to do with bone collections.

It also puts the functions into the `blender::animrig::` namespace
and removes the `ANIM_` prefix as is the standard for C++ files.

Part of #138482

Pull Request: https://projects.blender.org/blender/blender/pulls/138833

source/blender/editors/armature/armature_edit.cc
@@ -46,7 +46,7 @@
 #include "ED_screen.hh"
 #include "ED_view3d.hh"
 
-#include "ANIM_bone_collections.hh"
+#include "ANIM_armature.hh"
 
 #include "DEG_depsgraph.hh"
 
@@ -316,7 +316,7 @@ static wmOperatorStatus armature_calc_roll_exec(bContext *C, wmOperator *op)
 
       /* cursor */
       LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
-        if (ANIM_bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone)) {
+        if (blender::animrig::bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone)) {
           float cursor_rel[3];
           sub_v3_v3v3(cursor_rel, cursor_local, ebone->head);
           if (axis_flip) {
@@ -332,8 +332,9 @@ static wmOperatorStatus armature_calc_roll_exec(bContext *C, wmOperator *op)
     else if (ELEM(type, CALC_ROLL_TAN_POS_X, CALC_ROLL_TAN_POS_Z)) {
       LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
         if (ebone->parent) {
-          bool is_edit = (ANIM_bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone));
-          bool is_edit_parent = (ANIM_bone_is_visible_editbone(arm, ebone->parent) &&
+          bool is_edit = (blender::animrig::bone_is_visible_editbone(arm, ebone) &&
+                          EBONE_EDITABLE(ebone));
+          bool is_edit_parent = (blender::animrig::bone_is_visible_editbone(arm, ebone->parent) &&
                                  EBONE_EDITABLE(ebone->parent));
 
           if (is_edit || is_edit_parent) {
@@ -428,7 +429,7 @@ static wmOperatorStatus armature_calc_roll_exec(bContext *C, wmOperator *op)
       }
 
       LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
-        if (ANIM_bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone)) {
+        if (blender::animrig::bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone)) {
           /* roll func is a callback which assumes that all is well */
           ebone->roll = ED_armature_ebone_roll_to_vector(ebone, vec, axis_only);
           changed = true;
@@ -438,10 +439,11 @@ static wmOperatorStatus armature_calc_roll_exec(bContext *C, wmOperator *op)
 
     if (arm->flag & ARM_MIRROR_EDIT) {
       LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
-        if ((ANIM_bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone)) == 0) {
+        if ((blender::animrig::bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone)) == 0)
+        {
           EditBone *ebone_mirr = ED_armature_ebone_get_mirrored(arm->edbo, ebone);
-          if (ebone_mirr &&
-              (ANIM_bone_is_visible_editbone(arm, ebone_mirr) && EBONE_EDITABLE(ebone_mirr)))
+          if (ebone_mirr && (blender::animrig::bone_is_visible_editbone(arm, ebone_mirr) &&
+                             EBONE_EDITABLE(ebone_mirr)))
           {
             ebone->roll = -ebone_mirr->roll;
           }
@@ -497,7 +499,7 @@ static wmOperatorStatus armature_roll_clear_exec(bContext *C, wmOperator *op)
     bool changed = false;
 
     LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
-      if (ANIM_bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone)) {
+      if (blender::animrig::bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone)) {
         /* Roll func is a callback which assumes that all is well. */
         ebone->roll = roll;
         changed = true;
@@ -506,10 +508,11 @@ static wmOperatorStatus armature_roll_clear_exec(bContext *C, wmOperator *op)
 
     if (arm->flag & ARM_MIRROR_EDIT) {
       LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
-        if ((ANIM_bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone)) == 0) {
+        if ((blender::animrig::bone_is_visible_editbone(arm, ebone) && EBONE_EDITABLE(ebone)) == 0)
+        {
           EditBone *ebone_mirr = ED_armature_ebone_get_mirrored(arm->edbo, ebone);
-          if (ebone_mirr &&
-              (ANIM_bone_is_visible_editbone(arm, ebone_mirr) && EBONE_EDITABLE(ebone_mirr)))
+          if (ebone_mirr && (blender::animrig::bone_is_visible_editbone(arm, ebone_mirr) &&
+                             EBONE_EDITABLE(ebone_mirr)))
           {
             ebone->roll = -ebone_mirr->roll;
             changed = true;
@@ -945,7 +948,7 @@ static wmOperatorStatus armature_switch_direction_exec(bContext *C, wmOperator *
         /* skip bone if already handled, see #34123. */
         if ((ebo->flag & BONE_TRANSFORM) == 0) {
           /* only if selected and editable */
-          if (ANIM_bone_is_visible_editbone(arm, ebo) && EBONE_EDITABLE(ebo)) {
+          if (blender::animrig::bone_is_visible_editbone(arm, ebo) && EBONE_EDITABLE(ebo)) {
             /* swap head and tail coordinates */
             swap_v3_v3(ebo->head, ebo->tail);
 
@@ -970,7 +973,8 @@ static wmOperatorStatus armature_switch_direction_exec(bContext *C, wmOperator *
             /* not swapping this bone, however, if its 'parent' got swapped, unparent us from it
              * as it will be facing in opposite direction
              */
-            if ((parent) && (ANIM_bone_is_visible_editbone(arm, parent) && EBONE_EDITABLE(parent)))
+            if ((parent) && (blender::animrig::bone_is_visible_editbone(arm, parent) &&
+                             EBONE_EDITABLE(parent)))
             {
               ebo->parent = nullptr;
               ebo->flag &= ~BONE_CONNECTED;
@@ -1227,7 +1231,8 @@ static bool armature_delete_ebone_cb(const char *bone_name, void *arm_p)
   EditBone *ebone;
 
   ebone = ED_armature_ebone_find_name(arm->edbo, bone_name);
-  return (ebone && (ebone->flag & BONE_SELECTED) && ANIM_bonecoll_is_visible_editbone(arm, ebone));
+  return (ebone && (ebone->flag & BONE_SELECTED) &&
+          blender::animrig::bone_is_visible_editbone(arm, ebone));
 }
 
 /* previously delete_armature */
@@ -1256,7 +1261,7 @@ static wmOperatorStatus armature_delete_selected_exec(bContext *C, wmOperator * 
 
     for (curBone = static_cast<EditBone *>(arm->edbo->first); curBone; curBone = ebone_next) {
       ebone_next = curBone->next;
-      if (ANIM_bone_is_visible_editbone(arm, curBone)) {
+      if (blender::animrig::bone_is_visible_editbone(arm, curBone)) {
         if (curBone->flag & BONE_SELECTED) {
           if (curBone == arm->act_edbone) {
             arm->act_edbone = nullptr;
@@ -1400,13 +1405,13 @@ static wmOperatorStatus armature_dissolve_selected_exec(bContext *C, wmOperator 
 
     LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
       /* break connections for unseen bones */
-      if ((ANIM_bone_is_visible_editbone(arm, ebone) &&
+      if ((blender::animrig::bone_is_visible_editbone(arm, ebone) &&
            (ED_armature_ebone_selectflag_get(ebone) & (BONE_TIPSEL | BONE_SELECTED))) == 0)
       {
         ebone->temp.ebone = nullptr;
       }
 
-      if ((ANIM_bone_is_visible_editbone(arm, ebone) &&
+      if ((blender::animrig::bone_is_visible_editbone(arm, ebone) &&
            (ED_armature_ebone_selectflag_get(ebone) & (BONE_ROOTSEL | BONE_SELECTED))) == 0)
       {
         if (ebone->parent && (ebone->flag & BONE_CONNECTED)) {
@@ -1516,7 +1521,7 @@ static wmOperatorStatus armature_hide_exec(bContext *C, wmOperator *op)
     bool changed = false;
 
     LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
-      if (ANIM_bone_is_visible_editbone(arm, ebone)) {
+      if (blender::animrig::bone_is_visible_editbone(arm, ebone)) {
         if ((ebone->flag & BONE_SELECTED) != invert) {
           ebone->flag &= ~(BONE_TIPSEL | BONE_SELECTED | BONE_ROOTSEL);
           ebone->flag |= BONE_HIDDEN_A;
@@ -1573,7 +1578,7 @@ static wmOperatorStatus armature_reveal_exec(bContext *C, wmOperator *op)
     bool changed = false;
 
     LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
-      if (ANIM_bonecoll_is_visible_editbone(arm, ebone)) {
+      if (blender::animrig::bone_is_visible_editbone(arm, ebone)) {
         if (ebone->flag & BONE_HIDDEN_A) {
           if (!(ebone->flag & BONE_UNSELECTABLE)) {
             SET_FLAG_FROM_TEST(ebone->flag, select, (BONE_TIPSEL | BONE_SELECTED | BONE_ROOTSEL));

source/blender/animrig/ANIM_armature.hh
@@ -1,0 +1,39 @@
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+/** \file
+ * \ingroup animrig
+ *
+ * \brief Functions to deal with Armatures.
+ */
+
+#pragma once
+
+#include "ANIM_bone_collections.hh"
+
+#include "DNA_armature_types.h"
+
+namespace blender::animrig {
+
+/**
+ * Returns true if the given Bone is visible. This includes bone collection visibility.
+ */
+inline bool bone_is_visible(const bArmature *armature, const Bone *bone)
+{
+  const bool bone_itself_visible = (bone->flag & BONE_HIDDEN_P) == 0;
+  return bone_itself_visible && ANIM_bone_in_visible_collection(armature, bone);
+}
+
+inline bool bone_is_visible_pchan(const bArmature *armature, const bPoseChannel *pchan)
+{
+  return bone_is_visible(armature, pchan->bone);
+}
+
+inline bool bone_is_visible_editbone(const bArmature *armature, const EditBone *ebone)
+{
+  const bool bone_itself_visible = (ebone->flag & BONE_HIDDEN_A) == 0;
+  return bone_itself_visible && ANIM_bonecoll_is_visible_editbone(armature, ebone);
+}
+
+}  // namespace blender::animrig

source/blender/animrig/ANIM_bone_collections.hh
@@ -305,34 +305,17 @@ void ANIM_armature_bonecoll_reconstruct(bArmature *armature);
 /** Return true when any of the bone's collections is visible. */
 bool ANIM_bone_in_visible_collection(const bArmature *armature, const Bone *bone);
 
-inline bool ANIM_bone_is_visible(const bArmature *armature, const Bone *bone)
-{
-  const bool bone_itself_visible = (bone->flag & BONE_HIDDEN_P) == 0;
-  return bone_itself_visible && ANIM_bone_in_visible_collection(armature, bone);
-}
-
 /**
  * Returns true when the edit-bone's collection is visible.
  *
  * \note This alone is not enough to check bone visibility since the user may have hidden the bone.
- * Use `ANIM_bone_is_visible_editbone` to check bone visibility.
+ * Use `blender::animrig::bone_is_visible_editbone` to check bone visibility.
  */
 bool ANIM_bonecoll_is_visible_editbone(const bArmature *armature, const EditBone *ebone);
-
-inline bool ANIM_bone_is_visible_editbone(const bArmature *armature, const EditBone *ebone)
-{
-  const bool bone_itself_visible = (ebone->flag & BONE_HIDDEN_A) == 0;
-  return bone_itself_visible && ANIM_bonecoll_is_visible_editbone(armature, ebone);
-}
 
 inline bool ANIM_bonecoll_is_visible_pchan(const bArmature *armature, const bPoseChannel *pchan)
 {
   return ANIM_bone_in_visible_collection(armature, pchan->bone);
-}
-
-inline bool ANIM_bone_is_visible_pchan(const bArmature *armature, const bPoseChannel *pchan)
-{
-  return ANIM_bone_is_visible(armature, pchan->bone);
 }
 
 inline bool ANIM_bonecoll_is_visible_actbone(const bArmature *armature)

source/blender/animrig/CMakeLists.txt
@@ -41,6 +41,7 @@ set(SRC
   ANIM_action_iterators.hh
   ANIM_action_legacy.hh
   ANIM_animdata.hh
+  ANIM_armature.hh
   ANIM_armature_iter.hh
   ANIM_bone_collections.hh
   ANIM_bonecolor.hh

source/blender/blenkernel/BKE_armature.hh
@@ -563,17 +563,17 @@ void BKE_pchan_bbone_deform_segment_index(const bPoseChannel *pchan,
                                           float *r_blend_next);
 
 #define PBONE_SELECTABLE(arm, bone) \
-  (ANIM_bone_is_visible(arm, bone) && !((bone)->flag & BONE_UNSELECTABLE))
+  (blender::animrig::bone_is_visible(arm, bone) && !((bone)->flag & BONE_UNSELECTABLE))
 
 #define PBONE_SELECTED(arm, bone) \
-  (((bone)->flag & BONE_SELECTED) & ANIM_bone_is_visible(arm, bone))
+  (((bone)->flag & BONE_SELECTED) & blender::animrig::bone_is_visible(arm, bone))
 
 /* context.selected_pose_bones */
 #define FOREACH_PCHAN_SELECTED_IN_OBJECT_BEGIN(_ob, _pchan) \
   for (bPoseChannel *_pchan = (bPoseChannel *)(_ob)->pose->chanbase.first; _pchan; \
        _pchan = _pchan->next) \
   { \
-    if (ANIM_bone_is_visible(((bArmature *)(_ob)->data), (_pchan)->bone) && \
+    if (blender::animrig::bone_is_visible(((bArmature *)(_ob)->data), (_pchan)->bone) && \
         ((_pchan)->bone->flag & BONE_SELECTED)) \
     {
 #define FOREACH_PCHAN_SELECTED_IN_OBJECT_END \
@@ -585,7 +585,7 @@ void BKE_pchan_bbone_deform_segment_index(const bPoseChannel *pchan,
   for (bPoseChannel *_pchan = (bPoseChannel *)(_ob)->pose->chanbase.first; _pchan; \
        _pchan = _pchan->next) \
   { \
-    if (ANIM_bone_is_visible(((bArmature *)(_ob)->data), (_pchan)->bone)) {
+    if (blender::animrig::bone_is_visible(((bArmature *)(_ob)->data), (_pchan)->bone)) {
 #define FOREACH_PCHAN_VISIBLE_IN_OBJECT_END \
   } \
   } \

source/blender/blenkernel/intern/action.cc
@@ -69,6 +69,7 @@
 
 #include "ANIM_action.hh"
 #include "ANIM_action_legacy.hh"
+#include "ANIM_armature.hh"
 #include "ANIM_bone_collections.hh"
 #include "ANIM_bonecolor.hh"
 #include "ANIM_versioning.hh"
@@ -1230,13 +1231,17 @@ bPoseChannel *BKE_pose_channel_active_or_first_selected(Object *ob)
   }
 
   bPoseChannel *pchan = BKE_pose_channel_active_if_bonecoll_visible(ob);
-  if (pchan && (pchan->bone->flag & BONE_SELECTED) && ANIM_bone_is_visible_pchan(arm, pchan)) {
+  if (pchan && (pchan->bone->flag & BONE_SELECTED) &&
+      blender::animrig::bone_is_visible_pchan(arm, pchan))
+  {
     return pchan;
   }
 
   LISTBASE_FOREACH (bPoseChannel *, pchan, &ob->pose->chanbase) {
     if (pchan->bone != nullptr) {
-      if ((pchan->bone->flag & BONE_SELECTED) && ANIM_bone_is_visible_pchan(arm, pchan)) {
+      if ((pchan->bone->flag & BONE_SELECTED) &&
+          blender::animrig::bone_is_visible_pchan(arm, pchan))
+      {
         return pchan;
       }
     }

source/blender/blenkernel/intern/armature.cc
@@ -51,6 +51,7 @@
 #include "BKE_object.hh"
 #include "BKE_scene.hh"
 
+#include "ANIM_armature.hh"
 #include "ANIM_bone_collections.hh"
 
 #include "DEG_depsgraph_build.hh"
@@ -3164,7 +3165,7 @@ std::optional<blender::Bounds<blender::float3>> BKE_pose_minmax(const Object *ob
     if (!pchan->bone) {
       continue;
     }
-    if (!ANIM_bone_is_visible_pchan(arm, pchan)) {
+    if (!blender::animrig::bone_is_visible_pchan(arm, pchan)) {
       continue;
     }
     if (use_select && !(pchan->bone->flag & BONE_SELECTED)) {

source/blender/blenkernel/intern/armature_selection.cc
@@ -6,7 +6,7 @@
  * \ingroup bke
  */
 
-#include "ANIM_bone_collections.hh"
+#include "ANIM_armature.hh"
 
 #include "BKE_armature.hh"
 

commit 23774361d804c661f1714b0fecc657abc3c4a14c

Fix #139031: Armature Edit mode can't reveal hidden bones
Caused by 6c36481d4757

More like a typo, in that commit, `ANIM_bonecoll_is_visible_editbone`
was swapped with `blender::animrig::bone_is_visible_editbone` (but that
was only meant to replace `ANIM_bone_is_visible_editbone`)

So to resolve, just use `ANIM_bonecoll_is_visible_editbone` again

Pull Request: https://projects.blender.org/blender/blender/pulls/139077

source/blender/editors/armature/armature_edit.cc
@@ -1578,7 +1578,7 @@ static wmOperatorStatus armature_reveal_exec(bContext *C, wmOperator *op)
     bool changed = false;
 
     LISTBASE_FOREACH (EditBone *, ebone, arm->edbo) {
-      if (blender::animrig::bone_is_visible_editbone(arm, ebone)) {
+      if (ANIM_bonecoll_is_visible_editbone(arm, ebone)) {
         if (ebone->flag & BONE_HIDDEN_A) {
           if (!(ebone->flag & BONE_UNSELECTABLE)) {
             SET_FLAG_FROM_TEST(ebone->flag, select, (BONE_TIPSEL | BONE_SELECTED | BONE_ROOTSEL));