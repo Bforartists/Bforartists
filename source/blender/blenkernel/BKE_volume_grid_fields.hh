Bforartists/Bforartists

commit 6e813dec829cfa3a2188b983e30aff15d0264de5

Geometry Nodes: initial support for volume grids in function nodes
This patch implements basic support for evaluating function nodes on volume
grids. Conceptually, a function node always creates a new grid for the output,
though the output is often a modified version of the input. The topology of the
output grid is a union of all the input grids.

All input grids have to have the same transform. Otherwise one has to use
resampling to make grids compatible.

Non-grid inputs are allowed to be single values or fields. The fields are
evaluated in a voxel/tile context, so they compute a value per voxel or per
tile.

One optimization is missing that will probably be key in the future: the ability
to merge multiple function nodes and execute them at the same time. Currently
the entire function evaluation is started and finished for every function node
that outputs a grid. This will add significant overhead in some situations.
Implementing this optimization requires some more changes outside of the scope
of this patch though. It's good to have something that works first.

Note: Not all function nodes are supported yet, because we don't have grid types
for all of them yet. Most notably, there are no color/float4 grids yet.
Implementing those properly is not super straight forward and may require some
more changes, because there isn't a 1-to-1 mapping between grid types and socket
types (a float4 grid may correspond to a color or vector socket later on).

Using grids with function nodes and fields can result in false positive warnings
in the UI currently. That's a limitation of our current socket type inferencing
and can be improved once we have better socket shape inferencing.

Pull Request: https://projects.blender.org/blender/blender/pulls/125110

source/blender/blenkernel/BKE_volume_grid_fields.hh
@@ -1,0 +1,48 @@
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#include "FN_field.hh"
+
+#include "openvdb_fwd.hh"
+
+namespace blender::bke {
+
+/**
+ * A field context that allows computing a value per voxel. Each voxel is defined by a 3D integer
+ * coordinate and a transform matrix.
+ */
+class VoxelFieldContext : public fn::FieldContext {
+ private:
+  const openvdb::math::Transform &transform_;
+  Span<openvdb::Coord> voxels_;
+
+ public:
+  VoxelFieldContext(const openvdb::math::Transform &transform, Span<openvdb::Coord> voxels);
+
+  GVArray get_varray_for_input(const fn::FieldInput &field_input,
+                               const IndexMask &mask,
+                               ResourceScope &scope) const override;
+};
+
+/**
+ * Similar to #VoxelFieldContext, but allows computing values for tiles. A tile contains multiple
+ * voxels.
+ */
+class TilesFieldContext : public fn::FieldContext {
+ private:
+  const openvdb::math::Transform &transform_;
+  Span<openvdb::CoordBBox> tiles_;
+
+ public:
+  TilesFieldContext(const openvdb::math::Transform &transform,
+                    const Span<openvdb::CoordBBox> tiles);
+
+  GVArray get_varray_for_input(const fn::FieldInput &field_input,
+                               const IndexMask &mask,
+                               ResourceScope &scope) const override;
+};
+
+}  // namespace blender::bke

source/blender/blenkernel/intern/volume_grid_fields.cc
@@ -1,0 +1,73 @@
+/* SPDX-FileCopyrightText: 2025 Blender Authors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include "BKE_geometry_fields.hh"
+#include "BKE_volume_grid_fields.hh"
+#include <openvdb/math/Transform.h>
+
+namespace blender::bke {
+
+VoxelFieldContext::VoxelFieldContext(const openvdb::math::Transform &transform,
+                                     const Span<openvdb::Coord> voxels)
+    : transform_(transform), voxels_(voxels)
+{
+}
+
+GVArray VoxelFieldContext::get_varray_for_input(const fn::FieldInput &field_input,
+                                                const IndexMask & /*mask*/,
+                                                ResourceScope & /*scope*/) const
+{
+  const bke::AttributeFieldInput *attribute_field_input =
+      dynamic_cast<const bke::AttributeFieldInput *>(&field_input);
+  if (!attribute_field_input) {
+    return {};
+  }
+  if (attribute_field_input->attribute_name() != "position") {
+    return {};
+  }
+
+  /* Support retrieving voxel positions. */
+  Array<float3> positions(voxels_.size());
+  threading::parallel_for(positions.index_range(), 1024, [&](const IndexRange range) {
+    for (const int64_t i : range) {
+      const openvdb::Coord &voxel = voxels_[i];
+      const openvdb::Vec3d position = transform_.indexToWorld(voxel);
+      positions[i] = float3(position.x(), position.y(), position.z());
+    }
+  });
+  return VArray<float3>::ForContainer(std::move(positions));
+}
+
+TilesFieldContext::TilesFieldContext(const openvdb::math::Transform &transform,
+                                     const Span<openvdb::CoordBBox> tiles)
+    : transform_(transform), tiles_(tiles)
+{
+}
+
+GVArray TilesFieldContext::get_varray_for_input(const fn::FieldInput &field_input,
+                                                const IndexMask & /*mask*/,
+                                                ResourceScope & /*scope*/) const
+{
+  const bke::AttributeFieldInput *attribute_field_input =
+      dynamic_cast<const bke::AttributeFieldInput *>(&field_input);
+  if (attribute_field_input == nullptr) {
+    return {};
+  }
+  if (attribute_field_input->attribute_name() != "position") {
+    return {};
+  }
+
+  /* Support retrieving tile positions. */
+  Array<float3> positions(tiles_.size());
+  threading::parallel_for(positions.index_range(), 1024, [&](const IndexRange range) {
+    for (const int64_t i : range) {
+      const openvdb::CoordBBox &tile = tiles_[i];
+      const openvdb::Vec3d position = transform_.indexToWorld(tile.getCenter());
+      positions[i] = float3(position.x(), position.y(), position.z());
+    }
+  });
+  return VArray<float3>::ForContainer(std::move(positions));
+}
+
+}  // namespace blender::bke

source/blender/blenkernel/BKE_volume_openvdb.hh
@@ -17,10 +17,13 @@
 #  include "BLI_bounds_types.hh"
 #  include "BLI_math_matrix_types.hh"
 #  include "BLI_math_vector_types.hh"
+#  include "BLI_parameter_pack_utils.hh"
 #  include "BLI_string_ref.hh"
 
 #  include "BKE_volume_enums.hh"
 #  include "BKE_volume_grid_fwd.hh"
+
+#  include "openvdb_fwd.hh"
 
 struct Volume;
 
@@ -75,6 +78,36 @@ auto BKE_volume_grid_type_operation(const VolumeGridType grid_type, OpType &&op)
   /* Should never be called. */
   BLI_assert_msg(0, "should never be reached");
   return op.template operator()<openvdb::FloatGrid>();
+}
+
+template<typename Fn>
+void BKE_volume_grid_type_to_static_type(const VolumeGridType grid_type, Fn &&fn)
+{
+  switch (grid_type) {
+    case VOLUME_GRID_FLOAT:
+      return fn(blender::TypeTag<openvdb::FloatGrid>());
+    case VOLUME_GRID_VECTOR_FLOAT:
+      return fn(blender::TypeTag<openvdb::Vec3fGrid>());
+    case VOLUME_GRID_BOOLEAN:
+      return fn(blender::TypeTag<openvdb::BoolGrid>());
+    case VOLUME_GRID_DOUBLE:
+      return fn(blender::TypeTag<openvdb::DoubleGrid>());
+    case VOLUME_GRID_INT:
+      return fn(blender::TypeTag<openvdb::Int32Grid>());
+    case VOLUME_GRID_INT64:
+      return fn(blender::TypeTag<openvdb::Int64Grid>());
+    case VOLUME_GRID_VECTOR_INT:
+      return fn(blender::TypeTag<openvdb::Vec3IGrid>());
+    case VOLUME_GRID_VECTOR_DOUBLE:
+      return fn(blender::TypeTag<openvdb::Vec3dGrid>());
+    case VOLUME_GRID_MASK:
+      return fn(blender::TypeTag<openvdb::MaskGrid>());
+    case VOLUME_GRID_POINTS:
+      return fn(blender::TypeTag<openvdb::points::PointDataGrid>());
+    case VOLUME_GRID_UNKNOWN:
+      break;
+  }
+  BLI_assert_unreachable();
 }
 
 openvdb::GridBase::Ptr BKE_volume_grid_create_with_changed_resolution(

source/blender/blenkernel/CMakeLists.txt
@@ -312,6 +312,7 @@ set(SRC
   intern/viewer_path.cc
   intern/volume.cc
   intern/volume_grid.cc
+  intern/volume_grid_fields.cc
   intern/volume_grid_file_cache.cc
   intern/volume_render.cc
   intern/volume_to_mesh.cc
@@ -530,6 +531,7 @@ set(SRC
   BKE_volume.hh
   BKE_volume_enums.hh
   BKE_volume_grid.hh
+  BKE_volume_grid_fields.hh
   BKE_volume_grid_file_cache.hh
   BKE_volume_grid_fwd.hh
   BKE_volume_grid_type_traits.hh

source/blender/blenlib/BLI_generic_span.hh
@@ -46,6 +46,12 @@ class GSpan {
   {
   }
 
+  template<typename T>
+  GSpan(MutableSpan<T> array)
+      : GSpan(CPPType::get<T>(), static_cast<const void *>(array.data()), array.size())
+  {
+  }
+
   const CPPType &type() const
   {
     BLI_assert(type_ != nullptr);

source/blender/nodes/CMakeLists.txt
@@ -95,6 +95,7 @@ set(SRC
   intern/socket_search_link.cc
   intern/socket_usage_inference.cc
   intern/value_elem.cc
+  intern/volume_grid_function_eval.cc
 
   NOD_common.hh
   NOD_composite.hh
@@ -139,6 +140,7 @@ set(SRC
   intern/node_common.h
   intern/node_exec.hh
   intern/node_util.hh
+  intern/volume_grid_function_eval.hh
 )
 
 set(LIB

source/blender/nodes/intern/geometry_nodes_lazy_function.cc
@@ -51,6 +51,8 @@
 
 #include "DEG_depsgraph_query.hh"
 
+#include "volume_grid_function_eval.hh"
+
 #include <fmt/format.h>
 #include <iostream>
 #include <sstream>
@@ -530,26 +532,36 @@ static void execute_multi_function_on_value_variant__field(
  * Executes a multi-function. If all inputs are single values, the results will also be single
  * values. If any input is a field, the outputs will also be fields.
  */
-static void execute_multi_function_on_value_variant(const MultiFunction &fn,
-                                                    const std::shared_ptr<MultiFunction> &owned_fn,
-                                                    const Span<SocketValueVariant *> input_values,
-                                                    const Span<SocketValueVariant *> output_values)
+[[nodiscard]] static bool execute_multi_function_on_value_variant(
+    const MultiFunction &fn,
+    const std::shared_ptr<MultiFunction> &owned_fn,
+    const Span<SocketValueVariant *> input_values,
+    const Span<SocketValueVariant *> output_values,
+    std::string &r_error_message)
 {
   /* Check input types which determine how the function is evaluated. */
   bool any_input_is_field = false;
+  bool any_input_is_volume_grid = false;
   for (const int i : input_values.index_range()) {
     const SocketValueVariant &value = *input_values[i];
     if (value.is_context_dependent_field()) {
       any_input_is_field = true;
     }
+    else if (value.is_volume_grid()) {
+      any_input_is_volume_grid = true;
+    }
   }
 
+  if (any_input_is_volume_grid) {
+    return execute_multi_function_on_value_variant__volume_grid(
+        fn, input_values, output_values, r_error_message);
+  }
   if (any_input_is_field) {
     execute_multi_function_on_value_variant__field(fn, owned_fn, input_values, output_values);
+    return true;
   }
-  else {
-    execute_multi_function_on_value_variant__single(fn, input_values, output_values);
-  }
+  execute_multi_function_on_value_variant__single(fn, input_values, output_values);
+  return true;
 }
 
 bool implicitly_convert_socket_value(const bke::bNodeSocketType &from_type,
@@ -573,7 +585,13 @@ bool implicitly_convert_socket_value(const bke::bNodeSocketType &from_type,
         mf::DataType::ForSingle(*from_cpp_type), mf::DataType::ForSingle(*to_cpp_type));
     SocketValueVariant input_variant = *static_cast<const SocketValueVariant *>(from_value);
     SocketValueVariant *output_variant = new (r_to_value) SocketValueVariant();
-    execute_multi_function_on_value_variant(multi_fn, {}, {&input_variant}, {output_variant});
+    std::string error_message;
+    if (!execute_multi_function_on_value_variant(
+            multi_fn, {}, {&input_variant}, {output_variant}, error_message))
+    {
+      std::destroy_at(output_variant);
+      return false;
+    }
     return true;
   }
   return false;
@@ -582,9 +600,11 @@ bool implicitly_convert_socket_value(const bke::bNodeSocketType &from_type,
 class LazyFunctionForImplicitConversion : public LazyFunction {
  private:
   const MultiFunction &fn_;
+  const bke::bNodeSocketType &dst_type_;
 
  public:
-  LazyFunctionForImplicitConversion(const MultiFunction &fn) : fn_(fn)
+  LazyFunctionForImplicitConversion(const MultiFunction &fn, const bke::bNodeSocketType &dst_type)
+      : fn_(fn), dst_type_(dst_type)
   {
     debug_name_ = "Convert";
     inputs_.append_as("From", CPPType::get<SocketValueVariant>());
@@ -597,7 +617,12 @@ class LazyFunctionForImplicitConversion : public LazyFunction {
     SocketValueVariant *to_value = new (params.get_output_data_ptr(0)) SocketValueVariant();
     BLI_assert(from_value != nullptr);
     BLI_assert(to_value != nullptr);
-    execute_multi_function_on_value_variant(fn_, {}, {from_value}, {to_value});
+    std::string error_message;
+    if (!execute_multi_function_on_value_variant(fn_, {}, {from_value}, {to_value}, error_message))
+    {
+      std::destroy_at(to_value);
+      construct_socket_default_value(dst_type_, to_value);
+    }
     params.output_set(0);
   }
 };
@@ -618,7 +643,7 @@ const LazyFunction *build_implicit_conversion_lazy_function(const bke::bNodeSock
   if (conversions.is_convertible(from_base_type, to_base_type)) {
     const MultiFunction &multi_fn = *conversions.get_conversion_multi_function(
         mf::DataType::ForSingle(from_base_type), mf::DataType::ForSingle(to_base_type));
-    return &scope.construct<LazyFunctionForImplicitConversion>(multi_fn);
+    return &scope.construct<LazyFunctionForImplicitConversion>(multi_fn, to_type);
   }
   return nullptr;
 }
@@ -703,20 +728,21 @@ class LazyFunctionForMutedNode : public LazyFunction {
  */
 class LazyFunctionForMultiFunctionNode : public LazyFunction {
  private:
+  const bNode &node_;
   const NodeMultiFunctions::Item fn_item_;
 
  public:
   LazyFunctionForMultiFunctionNode(const bNode &node,
                                    NodeMultiFunctions::Item fn_item,
                                    MutableSpan<int> r_lf_index_by_bsocket)
-      : fn_item_(std::move(fn_item))
+      : node_(node), fn_item_(std::move(fn_item))
   {
     BLI_assert(fn_item_.fn != nullptr);
     debug_name_ = node.name;
     lazy_function_interface_from_node(node, inputs_, outputs_, r_lf_index_by_bsocket);
   }
 
-  void execute_impl(lf::Params &params, const lf::Context & /*context*/) const override
+  void execute_impl(lf::Params &params, const lf::Context &context) const override
   {
     Vector<SocketValueVariant *> input_values(inputs_.size());
     Vector<SocketValueVariant *> output_values(outputs_.size());
@@ -731,8 +757,37 @@ class LazyFunctionForMultiFunctionNode : public LazyFunction {
         output_values[i] = nullptr;
       }
     }
-    execute_multi_function_on_value_variant(
-        *fn_item_.fn, fn_item_.owned_fn, input_values, output_values);
+    std::string error_message;
+    if (!execute_multi_function_on_value_variant(
+            *fn_item_.fn, fn_item_.owned_fn, input_values, output_values, error_message))
+    {
+      int available_output_index = 0;
+      for (const bNodeSocket *bsocket : node_.output_sockets()) {
+        if (!bsocket->is_available()) {
+          continue;
+        }
+        SocketValueVariant *output_value = output_values[available_output_index];
+        if (!output_value) {
+          continue;
+        }
+        std::destroy_at(output_value);
+        construct_socket_default_value(*bsocket->typeinfo, output_value);
+        available_output_index++;
+      }
+
+      if (!error_message.empty()) {
+        const auto &user_data = *static_cast<GeoNodesUserData *>(context.user_data);
+        const auto &local_user_data = *static_cast<GeoNodesLocalUserData *>(
+            context.local_user_data);
+        if (geo_eval_log::GeoTreeLogger *tree_logger = local_user_data.try_get_tree_logger(
+                user_data))
+        {
+          tree_logger->node_warnings.append(
+              *tree_logger->allocator,
+              {node_.identifier, {NodeWarningType::Error, error_message}});
+        }
+      }
+    }
     for (const int i : outputs_.index_range()) {
       if (params.get_output_usage(i) != lf::ValueUsage::Unused) {
         params.output_set(i);

commit 1cb0e9bb62483dcadb3cfe5505a98671c6cfb706

Fix: Build failure without OpenVDB
Caused by 6e813dec829cfa3a2188b983e30aff15d0264de5.

source/blender/blenkernel/BKE_volume_grid_fields.hh
@@ -6,7 +6,9 @@
 
 #include "FN_field.hh"
 
-#include "openvdb_fwd.hh"
+#ifdef WITH_OPENVDB
+
+#  include "openvdb_fwd.hh"
 
 namespace blender::bke {
 
@@ -46,3 +48,5 @@ class TilesFieldContext : public fn::FieldContext {
 };
 
 }  // namespace blender::bke
+
+#endif

source/blender/blenkernel/intern/volume_grid_fields.cc
@@ -4,7 +4,10 @@
 
 #include "BKE_geometry_fields.hh"
 #include "BKE_volume_grid_fields.hh"
-#include <openvdb/math/Transform.h>
+
+#ifdef WITH_OPENVDB
+
+#  include <openvdb/math/Transform.h>
 
 namespace blender::bke {
 
@@ -71,3 +74,5 @@ GVArray TilesFieldContext::get_varray_for_input(const fn::FieldInput &field_inpu
 }
 
 }  // namespace blender::bke
+
+#endif

source/blender/nodes/intern/volume_grid_function_eval.cc
@@ -14,13 +14,20 @@
 #include "BKE_volume_openvdb.hh"
 
 #include <fmt/format.h>
-#include <openvdb/Grid.h>
-#include <openvdb/math/Transform.h>
-#include <openvdb/tools/Merge.h>
+
+#ifdef WITH_OPENVDB
+
+#  include <openvdb/Grid.h>
+#  include <openvdb/math/Transform.h>
+#  include <openvdb/tools/Merge.h>
+
+#endif
 
 #include "volume_grid_function_eval.hh"
 
 namespace blender::nodes {
+
+#ifdef WITH_OPENVDB
 
 template<typename GridT>
 static constexpr bool is_supported_grid_type = is_same_any_v<GridT,
@@ -709,5 +716,19 @@ bool execute_multi_function_on_value_variant__volume_grid(
 
   return true;
 }
+
+#else
+
+bool execute_multi_function_on_value_variant__volume_grid(
+    const mf::MultiFunction & /*fn*/,
+    const Span<bke::SocketValueVariant *> /*input_values*/,
+    const Span<bke::SocketValueVariant *> /*output_values*/,
+    std::string &r_error_message)
+{
+  r_error_message = TIP_("Compiled without OpenVDB");
+  return false;
+}
+
+#endif
 
 }  // namespace blender::nodes