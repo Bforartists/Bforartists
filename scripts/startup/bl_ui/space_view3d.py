0ebdb495e80e3131c4936c525b51431f49913070
ReinerBforartists <reiner.prokein@posteo.de>
2025-07-12 18:04:58 +0200

    Point cloud in edit mode - Select menu - remove term Select from Select random #5230

--- scripts/startup/bl_ui/space_view3d.py
+++ scripts/startup/bl_ui/space_view3d.py
@@ -3448,7 +3448,7 @@ class VIEW3D_MT_select_edit_pointcloud(Menu):
 
         layout.separator()
 
-        layout.operator("pointcloud.select_random", icon="RANDOMIZE")
+        layout.operator("pointcloud.select_random", text="Random", icon="RANDOMIZE")
 
         layout.template_node_operator_asset_menu_items(catalog_path=self.bl_label)
 
