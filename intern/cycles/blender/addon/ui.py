97c1ba89c3edc376533cd31457b391c6938527d3
blenux <blenux@outlook.com>
2025-08-21 16:16:44 +1000

    Fix: 5445-properties-editor---render---volumes-panel---float-unbiased-left
    - Float Unbiased to the left

--- intern/cycles/blender/addon/ui.py
+++ intern/cycles/blender/addon/ui.py
@@ -595,7 +595,7 @@ class CYCLES_RENDER_PT_volumes(CyclesButtonsPanel, Panel):
 
     def draw(self, context):
         layout = self.layout
-        layout.use_property_split = True
+        layout.use_property_split = False # BFA
         layout.use_property_decorate = False
 
         scene = context.scene
