d3fbc63c0ac70e0151d5fae58d686db699bb84ef
Quackers <quackadoobers@gmail.com>
2025-09-10 17:08:21 +0800

    Preferences - Default Corner Handles to ON #5510

--- source/blender/makesrna/intern/rna_userdef.cc
+++ source/blender/makesrna/intern/rna_userdef.cc
@@ -4958,6 +4958,7 @@ static void rna_def_userdef_view(BlenderRNA *brna)
 
   prop = RNA_def_property(srna, "show_area_handle", PROP_BOOLEAN, PROP_NONE);
   RNA_def_property_boolean_sdna(prop, nullptr, "uiflag", USER_AREA_CORNER_HANDLE);
+  RNA_def_property_boolean_default(prop, true);  // BFA - Set default to true
   RNA_def_property_ui_text(prop, "Corner Handles", "Show visible area maintenance corner handles");
   RNA_def_property_update(prop, 0, "rna_userdef_gpu_update");
 
