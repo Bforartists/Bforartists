b31b9245ce10d68cbee8345ac72fbcfffef3eb96
Draise <35357409+Draise14@users.noreply.github.com>
2025-11-04 14:56:24 -0500

    Geometry Nodes & Compositor - New Enable Output node #5848 (#5869)

--- scripts/startup/bl_ui/space_node_toolshelf.py
+++ scripts/startup/bl_ui/space_node_toolshelf.py
@@ -846,7 +846,7 @@ class NODES_PT_toolshelf_compositor_add_output(bpy.types.Panel, NodePanel):
         # There is currently no way to determine the correct padding length other than trial-and-error.
         # When adding a new node, test different padding amounts until the button text is left-aligned with the rest of the panel items.
         entries = (
-            OperatorEntry("NodeGroupOutput", pad=9),
+            OperatorEntry("NodeEnableOutput", pad=15),
             OperatorEntry("CompositorNodeViewer", pad=15),
             Separator,
             OperatorEntry("CompositorNodeOutputFile", pad=8),
@@ -1653,6 +1653,7 @@ class NODES_PT_toolshelf_gn_add_output(bpy.types.Panel, NodePanel):
         # There is currently no way to determine the correct padding length other than trial-and-error.
         # When adding a new node, test different padding amounts until the button text is left-aligned with the rest of the panel items.
         entries = (
+            OperatorEntry("NodeEnableOutput", pad=10),
             OperatorEntry("GeometryNodeViewer", pad=4),
             OperatorEntry("GeometryNodeWarning", pad=2),
         )
