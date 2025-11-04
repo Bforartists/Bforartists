65341af1794f151fe8b6a3def68b73284ee5e704
Andres Stephens <ondraise15@hotmail.com>
2025-11-04 10:13:05 -0500

    Geometry Nodes & Compositor - New Enable Output node #5848

--- source/blender/makesrna/intern/rna_nodetree.cc
+++ source/blender/makesrna/intern/rna_nodetree.cc
@@ -9923,7 +9923,7 @@ static void rna_def_nodes(BlenderRNA *brna)
   define(brna, "NodeInternal", "NodeClosureInput", def_closure_input, ICON_NODE_CLOSURE);
   define(brna, "NodeInternal", "NodeClosureOutput", def_closure_output, ICON_NODE_CLOSURE);
   define(brna, "NodeInternal", "NodeCombineBundle", def_combine_bundle, ICON_NODE_BUNDLE_COMBINE);
-  define(brna, "NodeInternal", "NodeEnableOutput", nullptr, ICON_NONE);
+  define(brna, "NodeInternal", "NodeEnableOutput", nullptr, ICON_NODE_ENABLE_OUTPUT);
   define(brna, "NodeInternal", "NodeEvaluateClosure", def_evaluate_closure, ICON_NODE_CLOSURE_EVALUATE);
   define(brna, "NodeInternal", "NodeJoinBundle", nullptr, ICON_NODE_BUNDLE_JOIN);
   define(brna, "NodeInternal", "NodeSeparateBundle", def_separate_bundle, ICON_NODE_BUNDLE_SEPARATE);
