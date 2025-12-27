ad1f4fd209a9d59a6e52405a73161d27f8e85c31
Quackers <quackadoobers@gmail.com>
2025-12-27 23:45:04 +0800

    Geometry Nodes - Make Bone Info node follow alphabetical order in Add Menu and Toolshelf

--- scripts/startup/bl_ui/space_node_toolshelf.py
+++ scripts/startup/bl_ui/space_node_toolshelf.py
@@ -1581,8 +1581,8 @@ class NODES_PT_toolshelf_gn_add_input_scene(bpy.types.Panel, NodePanel):
         entries = (
             OperatorEntry("GeometryNodeTool3DCursor", pad=21, poll=is_tool),
             OperatorEntry("GeometryNodeInputActiveCamera", pad=14),
-            OperatorEntry("GeometryNodeCameraInfo", pad=18),
             OperatorEntry("GeometryNodeBoneInfo", pad=22),
+            OperatorEntry("GeometryNodeCameraInfo", pad=18),
             OperatorEntry("GeometryNodeCollectionInfo", pad=14),
             OperatorEntry("GeometryNodeImageInfo", pad=21),
             OperatorEntry("GeometryNodeIsViewport", pad=20),
