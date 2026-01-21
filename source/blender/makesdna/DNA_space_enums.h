2af65a5c1778a51e0931fca2c1e92e1bf4140f46
Eyad Senosy <esnosy@proton.me>
2026-01-21 22:15:28 +0200

    Fix: Files imported from Blender don't work correctly #6193

--- source/blender/makesdna/DNA_space_enums.h
+++ source/blender/makesdna/DNA_space_enums.h
@@ -1129,9 +1129,9 @@ enum eSpace_Type {
   SPACE_TOPBAR = 21,
   SPACE_STATUSBAR = 22,
   SPACE_SPREADSHEET = 24,
-  SPACE_TOOLBAR = 25 /* BFA - toolbar*/
+  SPACE_TOOLBAR = 40 /* BFA - toolbar*/
 
-#define SPACE_TYPE_NUM (SPACE_SPREADSHEET + 1)
+#define SPACE_TYPE_NUM (SPACE_TOOLBAR + 1)
 };
 
 /* use for function args */
