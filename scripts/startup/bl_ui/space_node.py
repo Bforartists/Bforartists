e0675b16280ecf19eb357e4147c8ee3fd92a3d7d
Quackers <quackadoobers@gmail.com>
2026-04-16 23:32:54 +0800

    Compositor - Sidebar > Options - Performance panel - Remove broken property "use_view_border" #6468

--- scripts/startup/bl_ui/space_node.py
+++ scripts/startup/bl_ui/space_node.py
@@ -1270,10 +1270,6 @@ class NODE_PT_quality(Panel):
         if rd.compositor_device == 'GPU':
             col.prop(rd, "compositor_precision", text="Precision")
 
-        col = layout.column()
-        col.use_property_split = False
-        col.prop(tree, "use_viewer_border")
-
 
 class NODE_PT_overlay(Panel):
     bl_space_type = 'NODE_EDITOR'
