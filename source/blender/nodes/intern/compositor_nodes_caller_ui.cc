83b44fa027de415216541bc30c149b73e8e74cac
blenux <blenux@outlook.com>
2026-05-31 10:54:29 +1000

    Moved open editor window operator

--- source/blender/nodes/intern/compositor_nodes_caller_ui.cc
+++ source/blender/nodes/intern/compositor_nodes_caller_ui.cc
@@ -297,15 +297,6 @@ void draw_compositor_nodes_modifier_ui(const bContext &C,
     template_id(&layout, &C, modifier_ptr, "node_group", newop, nullptr, nullptr);
   }
 
-  /* BFA - Open Compositor Modifier Editor Operator */
-  if (cmd.node_group != nullptr && !ID_MISSING(cmd.node_group)) {
-    ui::Layout &row = layout.row(true);
-    PointerRNA op_ptr = row.op("SEQUENCER_OT_strip_modifier_compositor_open_editor",
-                                IFACE_("Open Compositor Editor"),
-                                ICON_NODE_COMPOSITING);
-    RNA_string_set(&op_ptr, "modifier", cmd.modifier.name);
-  }
-
   const StripModifierData &smd = cmd.modifier;
   const bool is_mask_used = smd.mask_input_type == STRIP_MASK_INPUT_STRIP ?
                                 smd.mask_strip != nullptr :
@@ -327,6 +318,15 @@ void draw_compositor_nodes_modifier_ui(const bContext &C,
           &C, modifier_ptr, "open_mask_input_panel", IFACE_("Mask Input")))
   {
     draw_mask_input_type_settings(C, *mask_input_layout, modifier_ptr);
+  }
+
+  /* BFA - Open Compositor Modifier Editor Operator */
+  if (cmd.node_group != nullptr && !ID_MISSING(cmd.node_group)) {
+    layout.separator();
+    PointerRNA op_ptr = layout.op("SEQUENCER_OT_strip_modifier_compositor_open_editor",
+                                    IFACE_("Open Compositor Editor"),
+                                    ICON_NODE_COMPOSITING);
+    RNA_string_set(&op_ptr, "modifier", cmd.modifier.name);
   }
 }
 
