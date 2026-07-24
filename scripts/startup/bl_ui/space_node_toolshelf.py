Bforartists/Bforartists

commit c941a3ddd0bbfabd761e3f7e07626e87e2a8f886

Geometry Nodes - Grease Pencil - Read - New nodes #6654 (#6703)
Also updated the Set Grease Pencil Color icon to be "set" per standards.

scripts/startup/bl_ui/space_node_toolshelf.py
@@ -2217,7 +2217,13 @@ class NODES_PT_toolshelf_gn_add_grease_pencil_read(bpy.types.Panel, NodePanel):
         # There is currently no way to determine the correct padding length other than trial-and-error.
         # When adding a new node, test different padding amounts until the button text is left-aligned with the rest of the panel items.
         entries = (
-            OperatorEntry("GeometryNodeInputNamedLayerSelection"),
+            OperatorEntry("GeometryNodeGreasePencilColor", pad=1),
+            OperatorEntry("GeometryNodeGreasePencilDrawTime", pad=1),
+            OperatorEntry("GeometryNodeGreasePencilFillId", pad=1),
+            OperatorEntry("GeometryNodeGreasePencilOpacity", pad=1),
+            OperatorEntry("GeometryNodeGreasePencilStrokeSoftness", pad=1),
+            OperatorEntry("GeometryNodeGreasePencilStrokeVisibility", pad=1),
+            OperatorEntry("GeometryNodeInputNamedLayerSelection", pad=1),
         )
 
         self.draw_entries(context, layout, entries)

source/blender/makesrna/intern/rna_nodetree.cc
@@ -10715,12 +10715,12 @@ static void rna_def_nodes(BlenderRNA *brna)
   define(brna, "GeometryNode", "GeometryNodeGizmoDial", nullptr, ICON_DIAL_GIZMO);
   define(brna, "GeometryNode", "GeometryNodeGizmoLinear", nullptr, ICON_LINEAR_GIZMO);
   define(brna, "GeometryNode", "GeometryNodeGizmoTransform", rna_def_geo_gizmo_transform, ICON_TRANSFORM_GIZMO);
-  define(brna, "GeometryNode", "GeometryNodeGreasePencilColor", nullptr, ICON_NONE); /*BFA - WIP*/
-  define(brna, "GeometryNode", "GeometryNodeGreasePencilDrawTime", nullptr, ICON_NONE); /*BFA - WIP*/
-  define(brna, "GeometryNode", "GeometryNodeGreasePencilFillID", nullptr, ICON_NONE); /*BFA - WIP*/
-  define(brna, "GeometryNode", "GeometryNodeGreasePencilOpacity", nullptr, ICON_NONE); /*BFA - WIP*/
-  define(brna, "GeometryNode", "GeometryNodeGreasePencilStrokeSoftness", nullptr, ICON_NONE); /*BFA - WIP*/
-  define(brna, "GeometryNode", "GeometryNodeGreasePencilStrokeVisibility", nullptr, ICON_NONE); /*BFA - WIP*/
+  define(brna, "GeometryNode", "GeometryNodeGreasePencilColor", nullptr, ICON_NODE_GP_COLOR);
+  define(brna, "GeometryNode", "GeometryNodeGreasePencilDrawTime", nullptr, ICON_NODE_GP_DRAWTIME);
+  define(brna, "GeometryNode", "GeometryNodeGreasePencilFillId", nullptr, ICON_NODE_GP_FILLID);
+  define(brna, "GeometryNode", "GeometryNodeGreasePencilOpacity", nullptr, ICON_GP_OPACITY);
+  define(brna, "GeometryNode", "GeometryNodeGreasePencilStrokeSoftness", nullptr, ICON_NODE_GP_STROKESOFTNESS);
+  define(brna, "GeometryNode", "GeometryNodeGreasePencilStrokeVisibility", nullptr, ICON_NODE_GP_STROKEVISIBILITY);
   define(brna, "GeometryNode", "GeometryNodeGreasePencilToCurves", nullptr, ICON_OUTLINER_OB_CURVES);
   define(brna, "GeometryNode", "GeometryNodeGridAdvect", nullptr, ICON_NODE_ADVECTGRID);
   define(brna, "GeometryNode", "GeometryNodeGridCurl", nullptr, ICON_NODE_GRIDCURL);
@@ -10875,7 +10875,7 @@ static void rna_def_nodes(BlenderRNA *brna)
   define(brna, "GeometryNode", "GeometryNodeSetShadeSmooth", nullptr, ICON_SET_SHADE_SMOOTH);
   define(brna, "GeometryNode", "GeometryNodeSetSplineCyclic", nullptr, ICON_TOGGLE_CYCLIC);
   define(brna, "GeometryNode", "GeometryNodeSetSplineResolution", nullptr, ICON_SET_SPLINE_RESOLUTION);
-  define(brna, "GeometryNode", "GeometryNodeSetGreasePencilColor", nullptr, ICON_COLOR);
+  define(brna, "GeometryNode", "GeometryNodeSetGreasePencilColor", nullptr, ICON_NODE_GP_SETCOLOR);
   define(brna, "GeometryNode", "GeometryNodeSetGreasePencilDepth", nullptr, ICON_DEPTH);
   define(brna, "GeometryNode", "GeometryNodeSetGreasePencilSoftness", nullptr, ICON_FALLOFFSTROKE);
   define(brna, "GeometryNode", "GeometryNodeSetGridBackground", nullptr, ICON_NODE_SETGRIDBACKGROUND);

release/datafiles/icons_svg/node_gp_fillid.svg
@@ -1,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<svg
+   id="svg4"
+   height="1600"
+   viewBox="0 0 1600 1600"
+   width="1600"
+   version="1.1"
+   sodipodi:docname="node_greasepencilfillid.svg"
+   inkscape:version="1.4.3 (0d15f75, 2025-12-25)"
+   xml:space="preserve"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg"><defs
+     id="defs1" /><sodipodi:namedview
+     pagecolor="#303030"
+     showgrid="true"
+     id="namedview1"
+     bordercolor="#eeeeee"
+     borderopacity="1"
+     inkscape:showpageshadow="0"
+     inkscape:pageopacity="0"
+     inkscape:pagecheckerboard="true"
+     inkscape:deskcolor="#505050"
+     inkscape:zoom="0.40375001"
+     inkscape:cx="756.65633"
+     inkscape:cy="1077.3994"
+     inkscape:window-width="2560"
+     inkscape:window-height="1009"
+     inkscape:window-x="1912"
+     inkscape:window-y="-8"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="SET_ID"><inkscape:grid
+       id="grid5"
+       units="px"
+       spacingx="100"
+       spacingy="100"
+       color="#4772b3"
+       opacity="0.2"
+       visible="true"
+       originx="0"
+       originy="0" /></sodipodi:namedview><g
+     id="GREASEPENCIL"
+     style="stroke-width:0.0139494"
+     transform="matrix(81.929201,0,0,81.929201,314.76574,332.21353)"><path
+       style="fill:none;stroke-width:0.000435917"
+       d="M 0,8 V 0 h 8 8 v 8 8 H 8 0 Z m 1.3588939,7.437666 c 0.099648,-0.03428 0.1984819,-0.0767 0.2196309,-0.09425 0.021149,-0.01755 0.1405003,-0.06662 0.2652252,-0.109043 0.1247249,-0.04242 0.2425456,-0.08956 0.2618238,-0.104743 0.019278,-0.01519 0.1123949,-0.05214 0.2069262,-0.08211 0.094531,-0.02997 0.2149647,-0.07904 0.2676299,-0.109029 0.052665,-0.02999 0.1932903,-0.08628 0.3125,-0.125076 0.1192098,-0.0388 0.2448701,-0.08787 0.2792451,-0.109059 0.034375,-0.02118 0.1398437,-0.06334 0.234375,-0.09367 0.094531,-0.03034 0.2149647,-0.0797 0.26763,-0.109691 0.052665,-0.02999 0.1932903,-0.08628 0.3125,-0.125076 0.1192097,-0.0388 0.24487,-0.08739 0.279245,-0.107986 C 4.3,14.247338 4.4195313,14.198283 4.53125,14.15892 4.6429687,14.11956 4.7625,14.06987 4.796875,14.048495 c 0.034375,-0.02137 0.1609375,-0.07075 0.28125,-0.109738 0.1203125,-0.03899 0.246875,-0.08824 0.28125,-0.109457 0.034375,-0.02122 0.11875,-0.05654 0.1875,-0.07851 0.06875,-0.02196 0.1955231,-0.07277 0.2817181,-0.112909 0.086195,-0.04014 0.2188232,-0.09489 0.2947291,-0.121667 0.1671622,-0.05898 0.2801725,-0.142711 0.4983581,-0.36925 0.091232,-0.09472 2.1051197,-2.110684 4.4753067,-4.4799092 2.90855,-2.9073709 4.32972,-4.3489847 4.371846,-4.4347365 0.07492,-0.1524963 0.07859,-0.2581129 0.01434,-0.4118866 C 15.417514,3.6632838 12.410469,0.64941172 12.232321,0.56219928 12.071649,0.483542 11.959353,0.48360234 11.798929,0.56243366 11.713162,0.60457653 10.245108,2.0522193 7.28125,5.0172984 4.8664062,7.4331369 2.8540477,9.4423472 2.8093421,9.48221 2.6817965,9.5959388 2.4516006,9.9940513 2.3744268,10.234375 c -0.038635,0.120313 -0.081648,0.233303 -0.095583,0.251089 -0.013935,0.01779 -0.063135,0.137125 -0.1093334,0.265197 -0.046198,0.128072 -0.1016335,0.261175 -0.1231899,0.295786 -0.021557,0.03461 -0.070794,0.161365 -0.1094167,0.281678 -0.038623,0.120313 -0.081554,0.233218 -0.095403,0.250901 -0.013849,0.01768 -0.070554,0.158307 -0.1260114,0.3125 -0.055457,0.154192 -0.1179942,0.308474 -0.1389715,0.342849 -0.020977,0.03438 -0.070038,0.160937 -0.109024,0.28125 -0.038986,0.120313 -0.088368,0.246875 -0.1097387,0.28125 -0.02137,0.03438 -0.071062,0.153906 -0.1104246,0.265625 -0.039363,0.111719 -0.088418,0.23125 -0.1090113,0.265625 -0.020593,0.03438 -0.069339,0.160937 -0.1083254,0.28125 -0.038986,0.120313 -0.0883684,0.246875 -0.10973874,0.28125 -0.0213703,0.03438 -0.0710614,0.153906 -0.1104246,0.265625 -0.0393632,0.111719 -0.0884183,0.23125 -0.10901128,0.265625 -0.0818615,0.136648 -0.17650215,0.492043 -0.15928422,0.598144 0.0285694,0.176053 0.13400113,0.312178 0.30953785,0.399652 0.19399989,0.09667 0.27118519,0.09941 0.50782149,0.018 z"
+       id="path833"
+       inkscape:connector-curvature="0" /><path
+       style="fill:#fedbb2;stroke-width:0.000435917"
+       d="m 1.0768577,14.882813 c 0.011623,-0.04727 0.042418,-0.128126 0.068434,-0.179688 0.026015,-0.05156 0.083164,-0.191995 0.1269975,-0.312072 0.043833,-0.120077 0.097334,-0.246639 0.1188908,-0.28125 0.021556,-0.03461 0.070794,-0.161366 0.1094167,-0.281678 0.038623,-0.120313 0.081625,-0.233303 0.09556,-0.251089 0.013935,-0.01779 0.063135,-0.137125 0.1093334,-0.265197 0.046198,-0.128072 0.1016335,-0.261175 0.1231899,-0.295786 0.021556,-0.03461 0.070794,-0.161365 0.1094167,-0.281678 0.038623,-0.120313 0.081674,-0.233362 0.095669,-0.25122 0.013995,-0.01786 0.068448,-0.151453 0.121007,-0.296875 0.052559,-0.145423 0.1147307,-0.299561 0.1381602,-0.34253 0.02343,-0.04297 0.074445,-0.176562 0.113368,-0.296875 0.038923,-0.120313 0.082684,-0.233921 0.097246,-0.252463 0.014563,-0.01854 0.062749,-0.131043 0.1070812,-0.25 0.044332,-0.118958 0.1072866,-0.272537 0.1398987,-0.341287 0.032612,-0.06875 0.079658,-0.203697 0.1045471,-0.299881 0.052555,-0.203103 0.1895634,-0.4884709 0.2723704,-0.5673065 0.068038,-0.064775 0.091306,-0.068725 0.091306,-0.0155 0,0.051057 2.91,2.9608125 2.9610609,2.9608125 0.1978322,0 -0.3099322,0.303736 -0.8082821,0.483501 -0.1257988,0.04538 -0.2432779,0.09391 -0.2610644,0.107843 -0.017787,0.01394 -0.1307769,0.05694 -0.2510894,0.09556 -0.1203125,0.03862 -0.2470678,0.08786 -0.2816781,0.109417 -0.034611,0.02156 -0.1677141,0.07699 -0.295786,0.123189 -0.1280715,0.0462 -0.24741,0.09539 -0.2651965,0.109322 -0.017787,0.01393 -0.1482088,0.06346 -0.2898269,0.110079 -0.1416178,0.04661 -0.2648175,0.09662 -0.2737769,0.111112 -0.00896,0.0145 -0.1110587,0.05718 -0.2268875,0.09486 -0.115829,0.03767 -0.2387231,0.08581 -0.2730981,0.106978 -0.034375,0.02116 -0.1783693,0.07692 -0.3199874,0.123895 -0.141618,0.04698 -0.2648177,0.09727 -0.2737769,0.111771 -0.00896,0.0145 -0.1110587,0.05718 -0.2268876,0.09486 -0.115829,0.03767 -0.2387231,0.08583 -0.2730981,0.107013 -0.034375,0.02118 -0.1398438,0.06371 -0.234375,0.0945 -0.094531,0.03079 -0.2350444,0.0892 -0.3122513,0.129796 -0.1949819,0.102524 -0.2632223,0.09902 -0.235891,-0.01212 z"
+       id="path831"
+       inkscape:connector-curvature="0" /><path
+       style="fill:#7ffefe;stroke-width:0.000435917"
+       d="m 4.9019634,10.729086 c -0.045326,-0.0076 -0.1565168,-0.05873 -0.24709,-0.113634 C 4.3271597,10.416794 4.2025931,9.9925594 4.3732259,9.65625 4.4237756,9.5566191 5.2702591,8.6891669 7.276975,6.6805712 l 2.8324,-2.8350537 0.53838,0.5371622 0.53838,0.5371619 -2.8196303,2.8203421 C 6.5049975,9.6021616 5.49909,10.586478 5.40625,10.636898 5.2626781,10.71487 5.0496081,10.753821 4.9019634,10.729086 Z"
+       id="path829"
+       inkscape:connector-curvature="0" /><path
+       style="fill:#fe7f7f;stroke-width:0.000435917"
+       d="m 11.381287,3.6498559 -0.536185,-0.5379921 0.319636,-0.323094 c 0.334626,-0.3382457 0.509073,-0.4648962 0.703341,-0.5106346 0.438671,-0.1032803 0.89364,0.2710955 0.89364,0.73534 0,0.2685911 -0.09845,0.4314104 -0.506852,0.8382614 l -0.337395,0.3361115 z"
+       id="path827"
+       inkscape:connector-curvature="0" /><path
+       style="fill:#007ffe;stroke-width:0.000435917"
+       d="M 4.6740034,11.314629 3.2076309,9.8480075 3.3158353,9.7286913 C 3.4383856,9.5935556 9.4837034,3.5485666 9.5897903,3.4550778 l 0.068706,-0.060548 0.2176268,0.2159594 c 0.1196947,0.1187775 0.2176269,0.2241487 0.2176269,0.2341581 0,0.01001 -1.2651841,1.2830431 -2.8115203,2.8289631 C 5.2616881,8.6936088 4.4468059,9.5281563 4.3857453,9.6399947 4.1993284,9.9814344 4.3166181,10.410377 4.6520047,10.613738 4.9226816,10.777862 5.1715134,10.778022 5.4444206,10.614247 5.5179581,10.570116 6.8359447,9.2747947 8.37328,7.735755 9.9106153,6.1967144 11.17613,4.9375 11.185536,4.9375 c 0.0094,0 0.342094,0.3247728 0.739308,0.7217172 L 12.647052,6.3809344 9.4473453,9.5810922 C 7.6875066,11.341179 6.2235044,12.78125 6.1940072,12.78125 c -0.029497,0 -0.7134991,-0.65998 -1.5200038,-1.466621 z"
+       id="path825-4"
+       inkscape:connector-curvature="0" /><path
+       style="fill:#fe0000;stroke-width:0.000435917"
+       d="m 12.668095,4.9364384 c -0.392407,-0.39225 -0.717576,-0.7255072 -0.722598,-0.7405715 -0.005,-0.015064 0.132475,-0.1672503 0.305547,-0.338191 0.330007,-0.3259428 0.456489,-0.5009037 0.502071,-0.6945053 0.10328,-0.4386704 -0.271096,-0.8936393 -0.73534,-0.8936393 -0.2687,0 -0.431415,0.098452 -0.839132,0.5077257 L 10.841661,3.1155253 10.61187,2.8819819 10.38208,2.6484385 11.194407,1.8398443 C 11.641187,1.3951174 12.0123,1.03125 12.019104,1.03125 12.033571,1.03125 15,4.0010791 15,4.0155622 c 0,0.00537 -0.364148,0.3752334 -0.809219,0.8219131 L 13.381563,5.64962 Z"
+       id="path823-0"
+       inkscape:connector-curvature="0" /><path
+       style="fill:#19197f;stroke-width:0.000435917"
+       d="M 6.3975706,13.021619 C 6.2982659,12.932385 6.2174066,12.84824 6.2178831,12.834631 6.2183612,12.821023 7.6651041,11.363416 9.43287,9.595505 L 12.64699,6.3811222 11.153115,4.88749 9.6592394,3.3938581 9.5900206,3.4547416 C 9.5519506,3.4882275 8.10418,4.9320247 6.3727531,6.66318 L 3.2247044,9.8107353 3.0172015,9.6505453 2.8096988,9.4903553 6.2407869,6.0576487 C 8.5423956,3.7549544 9.7137,2.6043774 9.7989288,2.5624711 c 0.1524878,-0.074977 0.2580832,-0.07866 0.4119512,-0.01437 0.157183,0.065675 3.170882,3.0725958 3.258156,3.2508273 0.07868,0.16068 0.07862,0.2729754 -2.5e-4,0.4333925 -0.0419,0.085239 -1.186069,1.2500507 -3.4765622,3.5392976 L 6.578125,13.183863 Z"
+       id="path821-8"
+       inkscape:connector-curvature="0" /><path
+       style="fill:#980000;stroke-width:0.000435917"
+       d="M 0.85107241,15.419671 C 0.67553569,15.332197 0.570104,15.196072 0.54153456,15.020019 c -0.0172179,-0.106102 0.0774227,-0.461497 0.15928422,-0.598144 0.020593,-0.03438 0.0696481,-0.153906 0.10901128,-0.265625 0.0393632,-0.111719 0.0890543,-0.23125 0.1104246,-0.265625 0.0213703,-0.03438 0.0707528,-0.160938 0.10973874,-0.28125 0.038986,-0.120313 0.087732,-0.246875 0.1083254,-0.28125 0.020593,-0.03438 0.069648,-0.153906 0.1090113,-0.265625 0.039363,-0.111719 0.089054,-0.23125 0.1104246,-0.265625 0.02137,-0.03438 0.070753,-0.160938 0.1097387,-0.28125 0.038986,-0.120313 0.088047,-0.246875 0.109024,-0.28125 0.020977,-0.03438 0.083514,-0.188657 0.1389715,-0.342849 0.055457,-0.154193 0.1121622,-0.294818 0.1260114,-0.3125 0.013849,-0.01768 0.056781,-0.130589 0.095403,-0.250901 0.038623,-0.120313 0.08786,-0.247068 0.1094167,-0.281678 0.021556,-0.03461 0.076992,-0.167714 0.1231899,-0.295786 0.046198,-0.128072 0.095398,-0.24741 0.1093334,-0.265197 0.013935,-0.01779 0.056866,-0.130776 0.095402,-0.251089 C 2.4441379,10.016168 2.7388307,9.5 2.7935185,9.5 2.84631,9.5 3.15625,9.7569153 3.15625,9.8006753 c 0,0.023691 -0.01386,0.043075 -0.0308,0.043075 -0.045265,0 -0.2182625,0.3580907 -0.2704679,0.5598457 -0.024838,0.09599 -0.071843,0.230779 -0.1044555,0.299529 -0.032612,0.06875 -0.095567,0.222329 -0.1398987,0.341287 -0.044332,0.118957 -0.092519,0.231457 -0.1070812,0.25 -0.014563,0.01854 -0.058323,0.132151 -0.097246,0.252463 -0.038923,0.120313 -0.089938,0.253906 -0.113368,0.296875 -0.023429,0.04297 -0.085602,0.197107 -0.1381602,0.34253 -0.052559,0.145422 -0.1070118,0.279016 -0.121007,0.296875 -0.013995,0.01786 -0.057046,0.130908 -0.095669,0.25122 -0.038623,0.120313 -0.08786,0.247068 -0.1094167,0.281678 -0.021556,0.03461 -0.076992,0.167714 -0.1231899,0.295786 -0.046198,0.128072 -0.095398,0.24741 -0.1093334,0.265197 -0.013935,0.01779 -0.056937,0.130776 -0.09556,0.251089 -0.038623,0.120312 -0.08786,0.247068 -0.1094167,0.281678 -0.021556,0.03461 -0.075057,0.161173 -0.1188908,0.28125 -0.043834,0.120077 -0.1009824,0.260509 -0.1269975,0.312072 -0.026015,0.05156 -0.05681,0.132422 -0.068434,0.179688 -0.027331,0.111141 0.040909,0.114649 0.235891,0.01212 0.077207,-0.0406 0.21772,-0.09901 0.3122513,-0.129796 0.094531,-0.03079 0.2,-0.07332 0.234375,-0.0945 0.034375,-0.02118 0.1572691,-0.06934 0.2730981,-0.107013 0.1158289,-0.03768 0.2179283,-0.08036 0.2268876,-0.09486 0.00896,-0.0145 0.1321589,-0.06479 0.2737769,-0.111771 0.1416181,-0.04698 0.2856124,-0.102731 0.3199874,-0.123895 0.034375,-0.02116 0.1572691,-0.0693 0.2730981,-0.106978 0.1158288,-0.03768 0.2179285,-0.08036 0.2268875,-0.09486 0.00896,-0.0145 0.1321591,-0.0645 0.2737769,-0.111112 0.1416181,-0.04662 0.2720403,-0.09615 0.2898269,-0.110079 0.017787,-0.01393 0.137125,-0.06312 0.2651965,-0.109322 0.1280719,-0.0462 0.2611753,-0.101633 0.295786,-0.123189 0.03461,-0.02156 0.1613656,-0.07079 0.2816781,-0.109417 0.1203125,-0.03862 0.2333028,-0.08163 0.2510894,-0.09556 0.017787,-0.01394 0.1352656,-0.06246 0.2610644,-0.107843 0.125799,-0.04538 0.3680115,-0.152617 0.5382499,-0.238308 l 0.309525,-0.155803 0.1715982,0.146601 c 0.094379,0.08063 0.1709237,0.162471 0.1700994,0.181867 -0.00268,0.06307 -0.2912747,0.268455 -0.4515825,0.321383 -0.085962,0.02838 -0.1844188,0.0691 -0.2187938,0.09048 -0.034375,0.02138 -0.1539062,0.07109 -0.265625,0.110448 -0.1117187,0.03936 -0.23125,0.08842 -0.265625,0.109011 -0.034375,0.02059 -0.1609375,0.06934 -0.28125,0.108326 -0.1203125,0.03899 -0.246875,0.08837 -0.28125,0.109738 -0.034375,0.02137 -0.1539062,0.07106 -0.265625,0.110425 -0.1117187,0.03936 -0.23125,0.08842 -0.265625,0.109011 -0.034375,0.02059 -0.1600353,0.06919 -0.279245,0.107986 -0.1192097,0.0388 -0.2598347,0.09508 -0.3125,0.125075 -0.052665,0.02999 -0.1730987,0.07935 -0.26763,0.109692 -0.094531,0.03034 -0.2,0.07249 -0.234375,0.09367 -0.034375,0.02118 -0.1600353,0.07026 -0.2792451,0.109059 -0.1192097,0.0388 -0.2598347,0.09508 -0.3125,0.125075 -0.052665,0.02999 -0.1730986,0.07906 -0.2676299,0.10903 -0.094531,0.02997 -0.1876481,0.06692 -0.2069262,0.08211 -0.019278,0.01519 -0.1370989,0.06232 -0.2618238,0.104743 C 1.7190249,15.276777 1.5996736,15.325847 1.5785246,15.3434 1.5192327,15.392626 1.187658,15.5 1.0949936,15.5 1.0494966,15.5 0.93973191,15.46385 0.85107241,15.419671 Z M 13.469175,6.2335419 c 0.07875,-0.1544663 0.07863,-0.2816103 -4.25e-4,-0.4366669 -0.03438,-0.067425 -0.0625,-0.1339891 -0.0625,-0.1479212 0,-0.013932 0.358593,-0.3848088 0.796875,-0.8241704 C 14.641406,4.3854222 15,4.0214106 15,4.0158694 15,4.0009944 12.033671,1.03125 12.018813,1.03125 c -0.007,0 -0.37252,0.3585937 -0.812345,0.796875 C 10.766643,2.2664063 10.395577,2.625 10.381876,2.625 10.368175,2.625 10.3018,2.596875 10.234375,2.5625 10.079293,2.483435 9.9521719,2.4833252 9.797615,2.5621228 9.7121272,2.605707 9.9736025,2.3259117 10.67382,1.6245277 11.670908,0.62577816 11.820989,0.5 12.015625,0.5 c 0.203662,0 0.339938,0.12058059 1.881613,1.6648877 1.542532,1.5451657 1.634012,1.6490451 1.634012,1.8554923 0,0.1894222 -0.131167,0.3453756 -1.124462,1.3369569 -0.700897,0.6996862 -0.981135,0.9615706 -0.937613,0.876205 z"
+       id="path819-8"
+       inkscape:connector-curvature="0" /></g><g
+     id="SET_ID"
+     transform="matrix(99.997394,0,0,99.997394,-325.26315,-124.65857)"
+     style="stroke-width:0.0100003"
+     inkscape:label="ID"><g
+       id="path821-0"
+       style="opacity:1" /><path
+       style="stroke-width:0.0100003;opacity:1;baseline-shift:baseline;display:inline;overflow:visible;vector-effect:none;fill:#333333;enable-background:accumulate;stop-color:#000000;stop-opacity:1"
+       d="m 3.2675781,1.2460937 v 4.078125 4.0781251 h 1.25 1.25 V 5.3242187 1.2460937 h -1.25 z m 2.84375,0 v 4.0800782 4.0839844 l 2.2910157,-0.013672 1.8554692,-0.011719 0.365234,-0.099609 C 11.055469,9.1675746 11.479086,8.9714164 11.802734,8.7167969 12.414586,8.2354419 12.850839,7.418198 13.019531,6.5800781 13.197279,5.6969493 13.166248,4.5819353 12.933594,3.7402344 12.828924,3.361555 12.624576,2.9209997 12.402344,2.59375 12.020499,2.0314578 11.463237,1.6114812 10.824219,1.4082031 10.605765,1.3387108 10.366784,1.2922097 10.003906,1.2714844 9.641028,1.250759 9.1326402,1.2461892 8.2753906,1.2460937 Z m 2.46875,2.2304688 c 0.2497418,0.00624 0.5081152,0.013077 0.7285157,0.027344 0.1521495,0.00985 0.2877869,0.020321 0.3886718,0.03125 0.1008849,0.010929 0.1937915,0.035237 0.1425782,0.019531 0.3607792,0.1106426 0.4746862,0.2187734 0.6210942,0.6269531 0.111132,0.3098356 0.131448,0.478741 0.130859,1.1582031 -6.16e-4,0.7110154 -0.03379,0.9402742 -0.171875,1.2988282 -0.119061,0.3091543 -0.251915,0.4219023 -0.6328126,0.4941406 0.071536,-0.013568 -0.1115152,0.00883 -0.2832031,0.017578 -0.171688,0.00874 -0.390986,0.017881 -0.6132813,0.023437 l -0.3105469,0.00781 V 5.3300781 Z"
+       id="path2"
+       sodipodi:nodetypes="cccccccccccccccscsssscccsscssscccccc" /><path
+       style="fill:#fefefe;stroke-width:0.000312508"
+       d="m 3.7671643,5.3248136 v -3.578125 h 0.75 0.75 v 3.578125 3.578125 h -0.75 -0.75 z m 2.84375,0.00186 v -3.579985 l 1.6640625,1.844e-4 c 1.7110154,1.906e-4 2.0247372,0.018107 2.3984372,0.1369852 0.52806,0.1679812 0.994433,0.5194562 1.314329,0.9905246 0.173709,0.2557969 0.383301,0.7070416 0.463984,0.99894 0.202186,0.7314738 0.234661,1.8262072 0.07736,2.607741 -0.144576,0.718304 -0.571673,1.4787846 -1.035393,1.8436015 -0.242779,0.1909988 -0.634404,0.3777731 -1.00128,0.4775319 l -0.303375,0.082491 -1.7890622,0.010982 -1.7890625,0.010983 V 5.3266709 Z m 3.269956,2.2971596 C 10.386851,7.5278723 10.714675,7.2654989 10.886702,6.8188136 11.045545,6.4063658 11.092065,6.0725045 11.092699,5.3404377 11.093302,4.6449445 11.060091,4.3721039 10.93104,4.0123127 10.74873,3.5040367 10.464829,3.2227452 9.9859142,3.075873 9.8247718,3.0264545 8.9879521,2.972287 8.3062268,2.9671467 l -0.2265625,-0.00171 v 2.3643878 2.3643878 l 0.8235922,-0.020587 c 0.4529759,-0.011323 0.8929022,-0.033731 0.9776138,-0.049798 z"
+       id="path821"
+       inkscape:connector-curvature="0"
+       sodipodi:nodetypes="cccccccccccsssssssccccccssssscccsc" /></g></svg>

commit 4477a7178ed62d68bd185cde18e66643d3d00656

Geometry Nodes - Mesh - Operation - New Mesh Bevel Node #6644 (#6704)
* Geometry Nodes - Mesh - Operation - New Mesh Bevel Node
#6644
Also fixes an issues caused in #6703 with a case typo.

* Padding corrections #6644
Also fixed a case typo.

* Bevel padding  #6644

scripts/startup/bl_ui/space_node_toolshelf.py
@@ -2217,13 +2217,13 @@ class NODES_PT_toolshelf_gn_add_grease_pencil_read(bpy.types.Panel, NodePanel):
         # There is currently no way to determine the correct padding length other than trial-and-error.
         # When adding a new node, test different padding amounts until the button text is left-aligned with the rest of the panel items.
         entries = (
-            OperatorEntry("GeometryNodeGreasePencilColor", pad=1),
-            OperatorEntry("GeometryNodeGreasePencilDrawTime", pad=1),
-            OperatorEntry("GeometryNodeGreasePencilFillId", pad=1),
-            OperatorEntry("GeometryNodeGreasePencilOpacity", pad=1),
-            OperatorEntry("GeometryNodeGreasePencilStrokeSoftness", pad=1),
+            OperatorEntry("GeometryNodeGreasePencilColor", pad=17),
+            OperatorEntry("GeometryNodeGreasePencilDrawTime", pad=12),
+            OperatorEntry("GeometryNodeGreasePencilFillID", pad=16),
+            OperatorEntry("GeometryNodeGreasePencilOpacity", pad=14),
+            OperatorEntry("GeometryNodeGreasePencilStrokeSoftness", pad=4),
             OperatorEntry("GeometryNodeGreasePencilStrokeVisibility", pad=1),
-            OperatorEntry("GeometryNodeInputNamedLayerSelection", pad=1),
+            OperatorEntry("GeometryNodeInputNamedLayerSelection", pad=8),
         )
 
         self.draw_entries(context, layout, entries)
@@ -2452,6 +2452,7 @@ class NODES_PT_toolshelf_gn_add_mesh_operations(bpy.types.Panel, NodePanel):
             OperatorEntry("GeometryNodeEdgePathsToSelection", pad=0),
             OperatorEntry("GeometryNodeExtrudeMesh", pad=17),
             OperatorEntry("GeometryNodeFlipFaces", pad=23),
+            OperatorEntry("GeometryNodeMeshBevel", pad=22),
             Separator,
             OperatorEntry("GeometryNodeMeshBoolean", pad=16),
             OperatorEntry("GeometryNodeMeshToCurve", pad=15),
@@ -3106,10 +3107,10 @@ class NODES_PT_toolshelf_gn_add_utilities_lists(bpy.types.Panel, NodePanel):
 
         entries = (
             OperatorEntry("GeometryNodeClosureToList", pad=6),
-            OperatorEntry("GeometryNodeFieldToList", pad=8),
-            OperatorEntry("GeometryNodeFilterList", pad=10),
-            OperatorEntry("GeometryNodeListGetItem", pad=8),
-            OperatorEntry("GeometryNodeListLength", pad=10),
+            OperatorEntry("GeometryNodeFieldToList", pad=10),
+            OperatorEntry("GeometryNodeFilterList", pad=12),
+            OperatorEntry("GeometryNodeListGetItem", pad=10),
+            OperatorEntry("GeometryNodeListLength", pad=12),
             OperatorEntry("GeometryNodeSortList", pad=17),
         )
 

source/blender/makesrna/intern/rna_nodetree.cc
@@ -10717,7 +10717,7 @@ static void rna_def_nodes(BlenderRNA *brna)
   define(brna, "GeometryNode", "GeometryNodeGizmoTransform", rna_def_geo_gizmo_transform, ICON_TRANSFORM_GIZMO);
   define(brna, "GeometryNode", "GeometryNodeGreasePencilColor", nullptr, ICON_NODE_GP_COLOR);
   define(brna, "GeometryNode", "GeometryNodeGreasePencilDrawTime", nullptr, ICON_NODE_GP_DRAWTIME);
-  define(brna, "GeometryNode", "GeometryNodeGreasePencilFillId", nullptr, ICON_NODE_GP_FILLID);
+  define(brna, "GeometryNode", "GeometryNodeGreasePencilFillID", nullptr, ICON_NODE_GP_FILLID);
   define(brna, "GeometryNode", "GeometryNodeGreasePencilOpacity", nullptr, ICON_GP_OPACITY);
   define(brna, "GeometryNode", "GeometryNodeGreasePencilStrokeSoftness", nullptr, ICON_NODE_GP_STROKESOFTNESS);
   define(brna, "GeometryNode", "GeometryNodeGreasePencilStrokeVisibility", nullptr, ICON_NODE_GP_STROKEVISIBILITY);
@@ -10799,7 +10799,7 @@ static void rna_def_nodes(BlenderRNA *brna)
   define(brna, "GeometryNode", "GeometryNodeMergeByDistance", nullptr, ICON_REMOVE_DOUBLES);
   define(brna, "GeometryNode", "GeometryNodeMergeLayers", nullptr, ICON_MERGE);
   define(brna, "GeometryNode", "GeometryNodeMergePoints", nullptr, ICON_MERGE_CENTER);
-  define(brna, "GeometryNode", "GeometryNodeMeshBevel", nullptr, ICON_NONE); /*BFA - WIP*/
+  define(brna, "GeometryNode", "GeometryNodeMeshBevel", nullptr, ICON_BEVEL);
   define(brna, "GeometryNode", "GeometryNodeMeshBoolean", nullptr, ICON_MOD_BOOLEAN);
   define(brna, "GeometryNode", "GeometryNodeMeshCircle", nullptr, ICON_MESH_CIRCLE);
   define(brna, "GeometryNode", "GeometryNodeMeshCone", nullptr, ICON_MESH_CONE);